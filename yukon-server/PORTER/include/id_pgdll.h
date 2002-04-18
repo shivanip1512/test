7dd2f8ed63edfcc395a43c2c62783e2a2671035f
Plender, Corey <>
2002-04-18 16:40:53 +0000

    HUH

--- yukon-server/PORTER/include/id_pgdll.h
+++ yukon-server/PORTER/include/id_pgdll.h
@@ -9,8 +9,8 @@
 *
 * PVCS KEYWORDS:
 * ARCHIVE      :  $Archive:   Z:/SOFTWAREARCHIVES/YUKON/PORTER/INCLUDE/id_pgdll.h-arc  $
-* REVISION     :  $Revision: 1.6 $
-* DATE         :  $Date: 2002/04/16 17:50:07 $
+* REVISION     :  $Revision: 1.7 $
+* DATE         :  $Date: 2002/04/18 16:40:53 $
 *
 * Copyright (c) 1999, 2000, 2001 Cannon Technologies Inc. All rights reserved.
 *-----------------------------------------------------------------------------*/
@@ -20,7 +20,7 @@
 
 #define MAJORREVISION   2
 #define MINORREVISION   16
-#define BUILDNUMBER     4
+#define BUILDNUMBER     5
 
 CTICOMPILEINFO CompileInfo = {
    PROJECT,
