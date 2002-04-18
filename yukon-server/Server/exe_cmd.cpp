6de67b289570d31157577abea037164bca84c8fa
Plender, Corey <>
2002-04-18 15:06:52 +0000

    NewClient as a message is deprecated and should not be used

--- yukon-server/Server/exe_cmd.cpp
+++ yukon-server/Server/exe_cmd.cpp
@@ -8,8 +8,8 @@
 *
 * PVCS KEYWORDS:
 * ARCHIVE      :  $Archive:   Z:/SOFTWAREARCHIVES/YUKON/SERVER/exe_cmd.cpp-arc  $
-* REVISION     :  $Revision: 1.3 $
-* DATE         :  $Date: 2002/04/16 16:00:47 $
+* REVISION     :  $Revision: 1.4 $
+* DATE         :  $Date: 2002/04/18 15:06:52 $
 *
 * Copyright (c) 1999, 2000, 2001 Cannon Technologies Inc. All rights reserved.
 *-----------------------------------------------------------------------------*/
@@ -132,10 +132,11 @@ INT CtiCommandExecutor::ServerExecute(CtiServer *Svr)
             }
         case (CtiCommandMsg::NewClient):
             {
-                if( DebugLevel & 0x00001000)
+                if( DebugLevel & 0x00000001)
                 {
                     CtiLockGuard<CtiLogger> doubt_guard(dout);
                     dout << RWTime() << " MainThread: " << rwThreadId() << " New connection" << endl;
+                    dout << RWTime() << " *** WARNING *** Use of this message is deprecated " << endl;
                 }
                 Svr->clientConnect((CtiConnectionManager*)getConnectionHandle());
 
