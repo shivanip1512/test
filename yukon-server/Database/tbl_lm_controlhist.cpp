5faf4689705d0bdc75b5399cbc411762b04a3bb3
Plender, Corey <>
2002-04-18 15:02:49 +0000

    Log entries for control duration now represent the stop minus start times

--- yukon-server/Database/tbl_lm_controlhist.cpp
+++ yukon-server/Database/tbl_lm_controlhist.cpp
@@ -12,8 +12,8 @@
 *
 * PVCS KEYWORDS:
 * ARCHIVE      :  $Archive:   Z:/SOFTWAREARCHIVES/YUKON/DATABASE/tbl_lm_controlhist.cpp-arc  $
-* REVISION     :  $Revision: 1.3 $
-* DATE         :  $Date: 2002/04/16 15:58:02 $
+* REVISION     :  $Revision: 1.4 $
+* DATE         :  $Date: 2002/04/18 15:02:49 $
 *
 * Copyright (c) 1999, 2000, 2001 Cannon Technologies Inc. All rights reserved.
 *-----------------------------------------------------------------------------*/
@@ -431,7 +431,7 @@ RWDBStatus CtiTableLMControlHistory::Insert(RWDBConnection &conn)
     getPAOID() <<
     RWDBDateTime(getStartTime()) <<
     getSoeTag() <<
-    getControlDuration() <<
+    (getStopTime().seconds() - getStartTime().seconds()) <<     // getControlDuration() <<
     getControlType() <<
     getCurrentDailyTime() <<
     getCurrentMonthlyTime() <<
