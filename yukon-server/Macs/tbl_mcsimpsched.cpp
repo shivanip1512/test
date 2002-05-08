d7c7787fda2017520a93d756e25ba9be0981465d
Lauinger, Aaron C <AaronCLauinger@Eaton.com>
2002-05-08 22:15:45 +0000

    removed redundant temporary

--- yukon-server/Macs/tbl_mcsimpsched.cpp
+++ yukon-server/Macs/tbl_mcsimpsched.cpp
@@ -8,8 +8,8 @@
 *
 * PVCS KEYWORDS:
 * ARCHIVE      :  $Archive:   Z:/SOFTWAREARCHIVES/YUKON/MACS/tbl_mcsimpsched.cpp-arc  $
-* REVISION     :  $Revision: 1.4 $
-* DATE         :  $Date: 2002/05/02 17:02:29 $
+* REVISION     :  $Revision: 1.5 $
+* DATE         :  $Date: 2002/05/08 22:15:45 $
 *
 * Copyright (c) 1999, 2000, 2001 Cannon Technologies Inc. All rights reserved.
 *-----------------------------------------------------------------------------*/
@@ -167,14 +167,11 @@ bool CtiTableMCSimpleSchedule::Update()
 
             updater.where( t["ScheduleID"] == getScheduleID() );
 
-            updater << t["TargetSelect"].assign(
-                                               RWCString( (const char*) getTargetSelect().data() ));
+            updater << t["TargetSelect"].assign((const char*) getTargetSelect().data());
 
-            updater << t["StartCommand"].assign(
-                                               RWCString( (const char*) getStartCommand().data() ));
+            updater << t["StartCommand"].assign((const char*) getStartCommand().data());
 
-            updater << t["StopCommand"].assign(
-                                              RWCString( (const char*) getStopCommand().data() ));
+            updater << t["StopCommand"].assign((const char*) getStopCommand().data());
 
             updater << t["RepeatInterval"].assign(getRepeatInterval());
 
@@ -223,14 +220,11 @@ bool CtiTableMCSimpleSchedule::Insert()
 
             inserter << getScheduleID();
 
-            inserter <<
-            RWCString( (const char*) getTargetSelect().data() );
+            inserter << (const char*) getTargetSelect().data();
 
-            inserter <<
-            RWCString( (const char*) getStartCommand().data() );
+            inserter << (const char*) getStartCommand().data();
 
-            inserter <<
-            RWCString( (const char*) getStopCommand().data() );
+            inserter << (const char*) getStopCommand().data();
 
             inserter << getRepeatInterval();
 
