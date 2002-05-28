a921d32fda077c932bb8c6fa747649eecca8bfaf
Plender, Corey <>
2002-05-28 18:28:18 +0000

    Do not timesync an inhibited device

--- yukon-server/PORTER/PORTTIME.cpp
+++ yukon-server/PORTER/PORTTIME.cpp
@@ -8,8 +8,8 @@
 *
 * PVCS KEYWORDS:
 * ARCHIVE      :  $Archive:   Z:/SOFTWAREARCHIVES/YUKON/PORTER/PORTTIME.cpp-arc  $
-* REVISION     :  $Revision: 1.3 $
-* DATE         :  $Date: 2002/04/16 15:59:33 $
+* REVISION     :  $Revision: 1.4 $
+* DATE         :  $Date: 2002/05/28 18:28:18 $
 *
 * Copyright (c) 1999, 2000, 2001 Cannon Technologies Inc. All rights reserved.
 *-----------------------------------------------------------------------------*/
@@ -540,7 +540,7 @@ INT SendTime(VOID)
       {
          RemoteRecord = itr_dev.value();
 
-         if(RemoteRecord->getPortID() == PortRecord->getPortID())
+         if(!RemoteRecord->isInhibited() && RemoteRecord->getPortID() == PortRecord->getPortID())
          {
             if(RemoteRecord->getAddress() == 0xffff)
             {
