f1adaeac3a389751e9e5c11c57990e1910e545c4
Plender, Corey <>
2002-05-28 18:24:14 +0000

    cosmetic

--- yukon-server/RTDB/dev_tap.cpp
+++ yukon-server/RTDB/dev_tap.cpp
@@ -7,8 +7,8 @@
 *
 * PVCS KEYWORDS:
 * ARCHIVE      :  $Archive:   Z:/SOFTWAREARCHIVES/YUKON/RTDB/dev_tap.cpp-arc  $
-* REVISION     :  $Revision: 1.3 $
-* DATE         :  $Date: 2002/04/16 16:00:09 $
+* REVISION     :  $Revision: 1.4 $
+* DATE         :  $Date: 2002/05/28 18:24:14 $
 *
 * Copyright (c) 1999, 2000, 2001 Cannon Technologies Inc. All rights reserved.
 *-----------------------------------------------------------------------------*/
@@ -41,12 +41,7 @@ CtiDeviceTapPagingTerminal::~CtiDeviceTapPagingTerminal()
     freeDataBins();
 }
 
-INT CtiDeviceTapPagingTerminal::ExecuteRequest(CtiRequestMsg                  *pReq,
-                                               CtiCommandParser               &parse,
-                                               OUTMESS                        *&OutMessage,
-                                               RWTPtrSlist< CtiMessage >      &vgList,
-                                               RWTPtrSlist< CtiMessage >      &retList,
-                                               RWTPtrSlist< OUTMESS >         &outList)
+INT CtiDeviceTapPagingTerminal::ExecuteRequest(CtiRequestMsg *pReq, CtiCommandParser &parse, OUTMESS *&OutMessage, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList)
 {
     INT nRet = NORMAL;
     /*
