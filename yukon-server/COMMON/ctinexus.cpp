15c006733cda3e49025822e9906ac688e64dd566
Plender, Corey <>
2002-08-28 13:53:38 +0000

    A bit of printout if a message is lost

--- yukon-server/COMMON/ctinexus.cpp
+++ yukon-server/COMMON/ctinexus.cpp
@@ -426,10 +426,10 @@ INT CTINEXUS::CTINexusWrite(VOID *buf, ULONG len, PULONG BytesWritten, LONG Time
                     }
                     else
                     {
-                        if( !(++wbLoops % 20) )  //  gripe every 10 seconds
+                        if( !(wbLoops++ % 20) )  //  gripe every 10 seconds
                         {
                             CtiLockGuard<CtiLogger> doubt_guard(dout);
-                            dout << RWTime() << " Outbound socket full, will wait and retry. " << endl;
+                            dout << RWTime() << " Outbound nexus to " << Name << " is full, will wait up to " << TimeOut << " seconds and retry. " << endl;
                         }
                         Sleep(500);
                     }
@@ -451,6 +451,12 @@ INT CTINEXUS::CTINexusWrite(VOID *buf, ULONG len, PULONG BytesWritten, LONG Time
                 now = now.now();
 
             } while( (nReason == WSAEWOULDBLOCK && now < then) && len > 0);
+
+            if(len > 0)
+            {
+                CtiLockGuard<CtiLogger> doubt_guard(dout);
+                dout << RWTime() << " Outbound nexus could not be written. " << len << " bytes were abandoned." << endl;
+            }
         }
         else
         {
