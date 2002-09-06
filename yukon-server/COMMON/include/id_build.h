63244ffd34fa12f2f0c36dec6be32ef88756eee2
Plender, Corey <>
2002-09-06 21:26:18 +0000

    IR

---
+++ yukon-server/COMMON/include/id_build.h
@@ -0,0 +1,26 @@
+
+#pragma warning( disable : 4786)
+#ifndef __ID_BUILD_H__
+#define __ID_BUILD_H__
+
+/*-----------------------------------------------------------------------------*
+*
+* File:   id_build
+*
+* Class:
+* Date:   9/6/2002
+*
+* Author: Corey G. Plender
+*
+* CVS KEYWORDS:
+* REVISION     :  $Revision: 1.1 $
+* DATE         :  $Date: 2002/09/06 21:26:18 $
+*
+* Copyright (c) 2002 Cannon Technologies Inc. All rights reserved.
+*-----------------------------------------------------------------------------*/
+
+#define MAJORREVISION   2
+#define MINORREVISION   35
+#define BUILDNUMBER     0
+
+#endif // #ifndef __ID_BUILD_H__
