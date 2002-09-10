e31bc4a025a46d82b7d70236f26ae3655ad473cc
Lauinger, Aaron C <AaronCLauinger@Eaton.com>
2002-09-10 14:26:22 +0000

    Added method to get a LiteUnitMeasure for a pointid

--- yukon-client/common/src/com/cannontech/database/cache/functions/UnitMeasureFuncs.java
+++ yukon-client/common/src/com/cannontech/database/cache/functions/UnitMeasureFuncs.java
@@ -1,5 +1,11 @@
 package com.cannontech.database.cache.functions;
 
+import java.util.Iterator;
+
+import com.cannontech.database.cache.DefaultDatabaseCache;
+import com.cannontech.database.data.lite.LitePointUnit;
+import com.cannontech.database.data.lite.LiteUnitMeasure;
+
 /**
  * Insert the type's description here.
  * Creation date: (3/26/2001 9:40:33 AM)
@@ -12,13 +18,38 @@ public final class UnitMeasureFuncs {
 private UnitMeasureFuncs() {
 	super();
 }
+
+/**
+ * Returns the unit of measure information for a given point id.
+ * @param pointID
+ * @return LiteUnitMeasure
+ */
+public static LiteUnitMeasure getLiteUnitMeasureByPointID(int pointID) {
+	//find the lite point unit for this pointid then
+	//use it to find the actual lite unit of measure
+						   
+	DefaultDatabaseCache cache = DefaultDatabaseCache.getInstance();
+	synchronized(cache) { 
+		Iterator pUnitIter = cache.getAllPointsUnits().iterator();
+		
+		while(pUnitIter.hasNext()) {
+			LitePointUnit lpu = (LitePointUnit) pUnitIter.next();
+			
+			if( lpu.getPointID() == pointID ) {
+				return getLiteUnitMeasure(lpu.getUomID());
+			}
+		}
+	}
+	return null;
+}
+
 /**
  * Insert the method's description here.
  * Creation date: (3/26/2001 9:41:59 AM)
  * @return com.cannontech.database.data.lite.LitePoint
  * @param pointID int
  */
-public static com.cannontech.database.data.lite.LiteUnitMeasure getLiteUnitMeasure(int uomid) 
+public static LiteUnitMeasure getLiteUnitMeasure(int uomid) 
 {
 	com.cannontech.database.cache.DefaultDatabaseCache cache = com.cannontech.database.cache.DefaultDatabaseCache.getInstance();
 	synchronized( cache )
