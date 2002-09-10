d9361f23c8bd2dceaa8464977b95420d8a4a38d0
Lauinger, Aaron C <AaronCLauinger@Eaton.com>
2002-09-10 22:16:51 +0000

    stff

---
+++ yukon-client/tools/src/com/cannontech/datagenerator/PointChangePlayer.java
@@ -0,0 +1,116 @@
+package com.cannontech.datagenerator;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.cannontech.common.util.CtiProperties;
+import com.cannontech.common.util.CtiUtilities;
+import com.cannontech.database.PoolManager;
+import com.cannontech.database.data.point.PointTypes;
+import com.cannontech.message.dispatch.ClientConnection;
+import com.cannontech.message.dispatch.message.Multi;
+import com.cannontech.message.dispatch.message.PointData;
+
+/**
+ * Replays the point changes of a given day in the past.
+ * Connects to the database to get the data to generate point changes
+ * Connects to dispatch to send the point changes.
+ * @author alauinger
+ */
+public class PointChangePlayer {
+
+	public static void main(String[] args) throws IOException {
+				
+		if( args.length != 1 ) {
+			System.out.println("Specify a date to replay, ie 9/12/2002");
+			System.exit(0);
+		}
+		
+		Date begin = new Date(args[0]);
+		PointChangePlayer pcl = new PointChangePlayer();
+		pcl.doIt(begin);
+	}
+	
+	private void doIt(Date begin) {
+		System.out.println("Using " + begin.toString() + " to source point changes");
+		
+		PointData[] pChanges = loadChanges(begin);
+		System.out.println("loaded " + pChanges.length + " point changes");
+		
+		ClientConnection conn = new ClientConnection();
+		conn.setHost(CtiProperties.getInstance().get(CtiProperties.KEY_DISPATCH_MACHINE).toString());
+		conn.setPort(Integer.parseInt(CtiProperties.getInstance().get(CtiProperties.KEY_DISPATCH_PORT).toString()));
+		try {
+			conn.connect();
+		} catch (IOException e) {
+		}
+		
+		while(true) {
+			for( int i = 0; i < pChanges.length; i++ ) {
+				Multi multi = new Multi();
+				
+				for( int j = 0; j < 10000 && i < pChanges.length; j++, i++ ) {
+					PointData pch = pChanges[i];
+					Date now = new Date();
+					pch.setTime(now);
+					pch.setTimeStamp(now);				
+					multi.getVector().add(pch);										
+				}
+				System.out.println("wrote: " + multi.getVector().size());
+				conn.write(multi);				
+				
+			    try {
+					Thread.currentThread().sleep(200);
+				} catch (InterruptedException e) {
+				}
+				} 	
+		}
+	}
+	
+	private PointData[] loadChanges(Date d) {
+		
+		ArrayList changeList = new ArrayList();
+		
+		Timestamp start = new Timestamp(d.getTime());
+		Timestamp end = new Timestamp(d.getTime() * 86400 * 1000 );
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		
+		try {
+			conn = PoolManager.getInstance().getConnection(CtiUtilities.getDatabaseAlias());
+			pstmt = conn.prepareStatement("select pointid,quality,value from rawpointhistory where timestamp > ? and timestamp < ? order by changeid");
+			pstmt.setTimestamp(1, start);
+			pstmt.setTimestamp(2, end);
+			
+			rset = pstmt.executeQuery();
+		
+			while( rset.next() ) {
+				PointData pch = new PointData();
+				pch.setType(PointTypes.ANALOG_POINT);
+				pch.setId(rset.getInt(1));				
+				pch.setQuality(rset.getInt(2));
+				pch.setValue(rset.getDouble(3));
+				changeList.add(pch);
+			}	
+			
+		} catch(Exception e ) {
+		}
+		finally {
+			try {
+			pstmt.close();
+			conn.close();
+			}catch(Exception e2) {}				
+		}		
+		
+		PointData[] retVal = new PointData[changeList.size()];
+		changeList.toArray(retVal);
+		return retVal;
+	}
+}
---
+++ yukon-client/tools/src/com/cannontech/datagenerator/PointChangeRecorder.java
@@ -0,0 +1,65 @@
+package com.cannontech.datagenerator;
+
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.util.Date;
+
+import com.cannontech.common.util.CtiUtilities;
+import com.cannontech.database.PoolManager;
+
+/**
+ * Saves a days worth of point changes by looking at the rawpointhistory table
+ * @author alauinger
+ */
+public class PointChangeRecorder {
+
+	public static void main(String[] args) {		
+		
+		if( args.length != 2 ) {
+			System.out.println("Example usage:\ncom.cannontech.datagenerator.PointChangeRecorder output.dat 9/15/2002");			
+			System.exit(-1);
+		}
+		
+		String outputFile = args[0];
+		Date startDate = new Date(args[1]);
+		Timestamp start = new Timestamp(startDate.getTime());
+		Timestamp end = new Timestamp(startDate.getTime() * 86400 * 1000 );
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		
+		try {
+			conn = PoolManager.getInstance().getConnection(CtiUtilities.getDatabaseAlias());
+			pstmt = conn.prepareStatement("select pointid,timestamp,quality,value from rawpointhistory where timestamp > ? and timestamp < ? order by changeid");
+			pstmt.setTimestamp(1, start);
+			pstmt.setTimestamp(2, end);
+			
+			rset = pstmt.executeQuery();
+		
+			DataOutputStream das = new DataOutputStream( new BufferedOutputStream(new FileOutputStream(outputFile)) );	
+			while( rset.next() ) {
+				das.writeInt( rset.getInt(1));
+				das.writeLong( rset.getTimestamp(2).getTime() );
+				das.writeInt( rset.getInt(3));
+				das.writeDouble( rset.getDouble(4));
+			}	
+			
+			das.flush();
+			das.close();
+			
+		} catch(Exception e ) {
+		}
+		finally {
+			try {
+			pstmt.close();
+			conn.close();
+			}catch(Exception e2) {}				
+		}		
+	}
+}
