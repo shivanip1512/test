eb815e3eb842caa5078a38598cceb56f93942e25
Fisher, Matthew S <MatthewSFisher@Eaton.com>
2002-09-18 21:37:38 +0000

    now retMsgHandler sends TAG_POINT_MUST_ARCHIVE points to vgList as well, moved comm status point code to dev_single

--- yukon-server/RTDB/dev_dlcbase.cpp
+++ yukon-server/RTDB/dev_dlcbase.cpp
@@ -11,8 +11,8 @@
 *
 * PVCS KEYWORDS:
 * ARCHIVE      :  $Archive:   Z:/SOFTWAREARCHIVES/YUKON/RTDB/dev_dlcbase.cpp-arc  $
-* REVISION     :  $Revision: 1.9 $
-* DATE         :  $Date: 2002/09/06 19:03:40 $
+* REVISION     :  $Revision: 1.10 $
+* DATE         :  $Date: 2002/09/18 21:37:38 $
 *
 * Copyright (c) 1999, 2000 Cannon Technologies Inc. All rights reserved.
 *-----------------------------------------------------------------------------*/
@@ -87,7 +87,7 @@ void CtiDeviceDLCBase::getSQL(RWDBDatabase &db,  RWDBTable &keyTable, RWDBSelect
 {
     Inherited::getSQL(db, keyTable, selector);
     CtiTableDeviceCarrier::getSQL(db, keyTable, selector);
-    CtiTableDeviceRoute::getSQL(db, keyTable, selector);        // leftOuter'd.
+    CtiTableDeviceRoute::getSQL(db, keyTable, selector);        //  leftOuter'd.
 }
 
 
@@ -96,7 +96,7 @@ void CtiDeviceDLCBase::DecodeDatabaseReader(RWDBReader &rdr)
     INT iTemp;
     RWDBNullIndicator isNull;
 
-    Inherited::DecodeDatabaseReader(rdr);       // get the base class handled
+    Inherited::DecodeDatabaseReader(rdr);       //  get the base class handled
 
     if( getDebugLevel() & 0x0800 )
     {
@@ -109,13 +109,13 @@ void CtiDeviceDLCBase::DecodeDatabaseReader(RWDBReader &rdr)
     DeviceRoutes.DecodeDatabaseReader(rdr);
 }
 
-LONG CtiDeviceDLCBase::getAddress() const   { return CarrierSettings.getAddress();}
-LONG CtiDeviceDLCBase::getRouteID() const   { return DeviceRoutes.getRouteID();}            // From CtiTableDeviceRoute
+LONG CtiDeviceDLCBase::getAddress() const   {   return CarrierSettings.getAddress();    }
+LONG CtiDeviceDLCBase::getRouteID() const   {   return DeviceRoutes.getRouteID();       }   //  From CtiTableDeviceRoute
 
 
 INT CtiDeviceDLCBase::retMsgHandler( RWCString commandStr, CtiReturnMsg *retMsg, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList )
 {
-    CtiReturnMsg *tmpVGRetMsg;
+    CtiReturnMsg *tmpVGRetMsg = NULL;
     RWOrdered subMsgs;
     CtiCommandParser parse(commandStr);
     int retVal;
@@ -142,15 +142,16 @@ INT CtiDeviceDLCBase::retMsgHandler( RWCString commandStr, CtiReturnMsg *retMsg,
 
     if( retMsg != NULL)
     {
-        if(!(retMsg->ResultString().isNull()) || retMsg->getData().entries() > 0)
+        if(!(retMsg->ResultString().isNull()) || retMsg->PointData().entries() > 0)
         {
             //  if it's an update command
+
             if( parse.isKeyValid("flag") && (parse.getFlags( ) & CMD_FLAG_UPDATE) )
             {
                 //  make a copy for VanGogh
                 tmpVGRetMsg = (CtiReturnMsg *)retMsg->replicateMessage( );
 
-                subMsgs = tmpVGRetMsg->getData( );
+                subMsgs = tmpVGRetMsg->PointData( );
 
                 //  iterate through the points in the retMsg and set the pointdatas to "MUST ARCHIVE"
                 for( int i = 0; i < subMsgs.entries( ); i++ )
@@ -160,6 +161,36 @@ INT CtiDeviceDLCBase::retMsgHandler( RWCString commandStr, CtiReturnMsg *retMsg,
                 }
 
                 vgList.append( tmpVGRetMsg );
+            }
+            else
+            {
+                //  Check for any "Must Archive" points and send them to Dispatch
+
+                subMsgs = retMsg->getData( );
+
+                CtiPointDataMsg *tmpMsg;
+
+                for( int i = 0; i < subMsgs.entries( ); i++ )
+                {
+                    if( (subMsgs[i])->isA( ) == MSG_POINTDATA &&
+                        (((CtiPointDataMsg *)(subMsgs[i]))->getTags( ) & TAG_POINT_MUST_ARCHIVE) )
+                    {
+                        //  only allocate this object if you need to
+                        if( tmpVGRetMsg == NULL )
+                        {
+                            tmpVGRetMsg = new CtiReturnMsg(*((CtiReturnMsg *)retMsg));
+                        }
+
+                        tmpMsg = new CtiPointDataMsg( *((CtiPointDataMsg *)(subMsgs[i])) );
+
+                        tmpVGRetMsg->PointData().append(tmpMsg);
+                    }
+                }
+
+                if( tmpVGRetMsg != NULL )
+                {
+                    vgList.append( tmpVGRetMsg );
+                }
             }
 
             retList.append( retMsg );
@@ -181,32 +212,29 @@ INT CtiDeviceDLCBase::decodeCheckErrorReturn(INMESS *InMessage, RWTPtrSlist< Cti
 {
     CtiCommandParser parse(InMessage->Return.CommandStr);
 
-    INT ErrReturn = InMessage->EventCode & 0x3fff,
-        sendMsg   = TRUE;
+    INT ErrReturn = InMessage->EventCode & 0x3fff;
 
-    CtiCommandMsg   *pMsg = 0;
     CtiReturnMsg    *retMsg;
+    RWCString        resultString;
 
-    CtiPointDataMsg *commStatus;
-    CtiPointBase    *commPoint;
+    CtiCommandMsg   *pMsg;
 
-    if(!(ErrReturn))
+
+    if(!ErrReturn)
     {
-        /* verify we heard back from the correct device (only if we heard it)
+        //  verify we heard back from the correct device (only if we heard it)
+        //
+        //    Note:  The returned address from the device is only the lower 13 bits,
+        //           which means we would not have to mask of the Dst address, but for some
+        //           reason when the reading is queued into the CCU711 we get the whole address
+        //           returned.  So by comparing only 13 bit for both it will not break in
+        //           either case.
 
-            Note:  The returned address from the device is only the lower 13 bits,
-                   which means we would not have to mask of the Dst address, but for some
-                   reason when the reading is queued into the CCU711 we get the whole address
-                   returned.  So by comparing only 13 bit for both it will not break in
-                   either case.
-         */
-
-
-        if((getAddress() & 0x1fff) != (InMessage->Buffer.DSt.Address & 0x1fff) &&
-           (InMessage->Buffer.DSt.Length))  //  make sure it's not just an ACK
+        if((getAddress() & 0x1fff) != (InMessage->Buffer.DSt.Address & 0x1fff) && (InMessage->Buffer.DSt.Length))  //  make sure it's not just an ACK
         {
-            /* Address did not match so it's a com error */
+            //  Address did not match, so it's a comm error
             ErrReturn = WRONGADDRESS;
+
             {
                 CtiLockGuard<CtiLogger> doubt_guard(dout);
                 dout << RWTime() << " Wrong Address:  " << getName() << " " << InMessage->Buffer.DSt.Address << endl;
@@ -215,93 +243,77 @@ INT CtiDeviceDLCBase::decodeCheckErrorReturn(INMESS *InMessage, RWTPtrSlist< Cti
         }
     }
 
-    retMsg = new CtiReturnMsg(getID(),
-                              RWCString(InMessage->Return.CommandStr),
-                              RWCString(),
-                              InMessage->EventCode & 0x7fff,
-                              InMessage->Return.RouteID,
-                              InMessage->Return.MacroOffset,
-                              InMessage->Return.Attempt,
-                              InMessage->Return.TrxID,
-                              InMessage->Return.UserID);
-
-
-    //  update performace stats for device and route
+    //  ACH: update performace stats for device and route... ?
 
     //  check for communication failure
     if(ErrReturn)
     {
-        //  Log the communication failure on this route.
-        if( commPoint = getDevicePointOffsetTypeEqual(2000, StatusPointType) )
-        {
-            commStatus = new CtiPointDataMsg(commPoint->getPointID(), 0.0, NormalQuality, StatusPointType);
-
-            if( commStatus != NULL )
-            {
-                retMsg->PointData().insert(commStatus);
-                commStatus = NULL;
-            }
-        }
-
-        //  send a Device Failed/Point Failed message to dispatch, if applicable
-        pMsg = new CtiCommandMsg(CtiCommandMsg::UpdateFailed);
-
-        if( pMsg != NULL )
-        {
-            switch( InMessage->Sequence )
-            {
-                case CtiProtocolEmetcon::Scan_General:
-                    pMsg->insert( -1 );             // This is the dispatch token and is unimplemented at this time
-                    pMsg->insert(OP_DEVICEID);      // This device failed.  OP_POINTID indicates a point fail situation.  defined in msg_cmd.h
-                    pMsg->insert(getID());    // The id (device or point which failed)
-                    pMsg->insert(ScanRateGeneral);  // defined in yukon.h
-
-                    if(InMessage->EventCode != 0)
-                        pMsg->insert(InMessage->EventCode);
-                    else
-                        pMsg->insert(GeneralScanAborted);
-
-                    break;
-
-                case CtiProtocolEmetcon::Scan_Accum:
-                    pMsg->insert( -1 );             // This is the dispatch token and is unimplemented at this time
-                    pMsg->insert(OP_DEVICEID);      // This device failed.  OP_POINTID indicates a point fail situation.  defined in msg_cmd.h
-                    pMsg->insert(getID());    // The id (device or point which failed)
-                    pMsg->insert(ScanRateAccum);
-
-                    if(InMessage->EventCode != 0)
-                        pMsg->insert(InMessage->EventCode);
-                    else
-                        pMsg->insert(GeneralScanAborted);
-
-                    break;
-
-                case CtiProtocolEmetcon::Scan_Integrity:
-                    pMsg->insert( -1 );             // This is the dispatch token and is unimplemented at this time
-                    pMsg->insert(OP_DEVICEID);      // This device failed.  OP_POINTID indicates a point fail situation.  defined in msg_cmd.h
-                    pMsg->insert(getID());    // The id (device or point which failed)
-                    pMsg->insert(ScanRateIntegrity);
-
-                    if(InMessage->EventCode != 0)
-                        pMsg->insert(InMessage->EventCode);
-                    else
-                        pMsg->insert(GeneralScanAborted);
-
-                    break;
-
-                default:
-                    delete pMsg;
-                    pMsg = NULL;
-                    break;
-            }
-        }
+        retMsg = new CtiReturnMsg(getID(),
+                                  RWCString(InMessage->Return.CommandStr),
+                                  RWCString(),
+                                  InMessage->EventCode & 0x7fff,
+                                  InMessage->Return.RouteID,
+                                  InMessage->Return.MacroOffset,
+                                  InMessage->Return.Attempt,
+                                  InMessage->Return.TrxID,
+                                  InMessage->Return.UserID);
 
         if( retMsg != NULL )
         {
-            RWCString resultString;
+            //  send a Device Failed/Point Failed message to dispatch, if applicable
+            pMsg = new CtiCommandMsg(CtiCommandMsg::UpdateFailed);
 
-            if(pMsg != NULL)
-                retMsg->insert(pMsg);
+            if( pMsg != NULL )
+            {
+                switch( InMessage->Sequence )
+                {
+                    case CtiProtocolEmetcon::Scan_General:
+                    {
+                        pMsg->insert( -1 );             //  This is the dispatch token and is unimplemented at this time
+                        pMsg->insert(OP_DEVICEID);      //  This device failed.  OP_POINTID indicates a point fail situation.  defined in msg_cmd.h
+                        pMsg->insert(getID());          //  The id (device or point which failed)
+                        pMsg->insert(ScanRateGeneral);  //  defined in yukon.h
+                        pMsg->insert(InMessage->EventCode);
+
+                        break;
+                    }
+
+                    case CtiProtocolEmetcon::Scan_Accum:
+                    {
+                        pMsg->insert( -1 );             //  This is the dispatch token and is unimplemented at this time
+                        pMsg->insert(OP_DEVICEID);      //  This device failed.  OP_POINTID indicates a point fail situation.  defined in msg_cmd.h
+                        pMsg->insert(getID());          //  The id (device or point which failed)
+                        pMsg->insert(ScanRateAccum);
+                        pMsg->insert(InMessage->EventCode);
+
+                        break;
+                    }
+
+                    case CtiProtocolEmetcon::Scan_Integrity:
+                    {
+                        pMsg->insert( -1 );             //  This is the dispatch token and is unimplemented at this time
+                        pMsg->insert(OP_DEVICEID);      //  This device failed.  OP_POINTID indicates a point fail situation.  defined in msg_cmd.h
+                        pMsg->insert(getID());          //  The id (device or point which failed)
+                        pMsg->insert(ScanRateIntegrity);
+                        pMsg->insert(InMessage->EventCode);
+
+                        break;
+                    }
+
+                    default:
+                    {
+                        delete pMsg;
+                        pMsg = NULL;
+
+                        break;
+                    }
+                }
+
+                if( pMsg != NULL )
+                {
+                    retMsg->insert(pMsg);
+                }
+            }
 
             resultString = getName() + " / operation failed";
 
@@ -309,18 +321,9 @@ INT CtiDeviceDLCBase::decodeCheckErrorReturn(INMESS *InMessage, RWTPtrSlist< Cti
 
             retList.append(retMsg);
         }
-        else
-        {
-            if(pMsg != NULL)
-            {
-                delete pMsg;
-            }
-        }
 
-        /*
-         *  ACH: Find the next route and resubmit request to porter
-         *   if no more routes exist, plug the value points!
-         */
+        //  Find the next route and resubmit request to porter
+        //    ACH:  if no more routes exist, plug the value points... ?
 
         if( InMessage->Return.MacroOffset > 0 )
         {
@@ -347,29 +350,12 @@ INT CtiDeviceDLCBase::decodeCheckErrorReturn(INMESS *InMessage, RWTPtrSlist< Cti
     }
     else
     {
-        //  Log the communication success on this route.
-        if( commPoint = getDevicePointOffsetTypeEqual(2000, StatusPointType) )
-        {
-            if( retMsg != NULL )
-            {
-                commStatus = new CtiPointDataMsg(commPoint->getPointID(), 1.0, NormalQuality, StatusPointType);
-
-                if( commStatus != NULL )
-                {
-                    retMsg->PointData().insert(commStatus);
-                    commStatus = NULL;
-                }
-
-                RWCString resultString = getName() + " / operation succeeded";
-
-                retMsg->setResultString(resultString);
-                retList.append(retMsg);
-            }
-        }
+        //  ACH:  Log the communication success on this route... ?
     }
 
     return ErrReturn;
 }
+
 
 bool CtiDeviceDLCBase::processAdditionalRoutes( INMESS *InMessage ) const
 {
@@ -390,6 +376,7 @@ bool CtiDeviceDLCBase::processAdditionalRoutes( INMESS *InMessage ) const
     }
     return bret;
 }
+
 
 inline ULONG CtiDeviceDLCBase::selectInitialMacroRouteOffset(LONG routeid) const
 {
