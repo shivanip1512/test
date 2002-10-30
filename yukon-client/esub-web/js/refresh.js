ac96cc7d7af293e3f70b5ae559ef5e75e0c52028
Lauinger, Aaron C <AaronCLauinger@Eaton.com>
2002-10-30 21:55:38 +0000

    removed alert

--- yukon-client/esub-web/js/refresh.js
+++ yukon-client/esub-web/js/refresh.js
@@ -142,7 +142,6 @@ function updateNode(node) {
 function updateImage(node) {
 	var imageID = node.getAttribute('id');
     if( !isNaN(imageID) ) {        
-        alert(imageID);
         url = '/servlet/StateImageServlet' + '?' + 'id=' + node.getAttribute('id');
         getURL(url, fn);
     }
