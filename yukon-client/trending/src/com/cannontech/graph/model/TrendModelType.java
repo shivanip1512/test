18bd9bc0b7ee8f282014ce214135b491a125decb
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2002-11-06 23:46:23 +0000

    Renamed, moved around.

--- yukon-client/trending/src/com/cannontech/graph/model/TrendModelType.java
+++ yukon-client/trending/src/com/cannontech/graph/model/TrendModelType.java
@@ -8,32 +8,26 @@ package com.cannontech.graph.model;
 public interface TrendModelType
 {
 	//model type less then 0 yields previous model type (in code). 6/7/02 SN
-	public final static int DONT_CHANGE_MODEL = -1;
-	public final static int LINE_MODEL = 0;
-	public final static int BAR_MODEL = 1;
-	public final static int BAR_3D_MODEL = 2;
-	public final static int STEP_MODEL = 3;
-
-	public final static int LOAD_DURATION_LINE_MODEL = 4;
-	public final static int LOAD_DURATION_STEP_MODEL = 5;
-
-//	public final static int MULTIPLE_DAYS_LINE_MODEL = 6;
-	
-	public final static int SHAPES_LINE_MODEL = 7;
-
+	public final static int DONT_CHANGE_VIEW = -1;
+	public final static int LINE_VIEW = 0;
+	public final static int BAR_VIEW = 1;
+	public final static int BAR_3D_VIEW = 2;
+	public final static int STEP_VIEW = 3;
+	public final static int SHAPES_LINE_VIEW = 4;
+		
 	// ** NOTE ** Add more int model values here with the addition of new models.	
 
 	// Optional model/chart choices.
-		// Additional Graph Plotting
-//	public final static int PLOT_YESTERDAY_MASK = 0x0001;
-//	public final static int PLOT_MULTIPLE_DAY_MASK = 0x0002;
-	public final static int GRAPH_MULTIPLIER = 0x0002;
-	public final static int PLOT_MIN_MAX_MASK = 0x0004;
+	// Additional Graph Plotting
+	public final static int BASIC_MASK= 0x0001;
+	public final static int LOAD_DURATION_MASK= 0x0002;
+	public final static int GRAPH_MULTIPLIER = 0x0004;
+	public final static int PLOT_MIN_MAX_MASK = 0x0008;
 	
-		// Legend Display
-	public final static int LEGEND_LOAD_FACTOR_MASK = 0x0008;
-	public final static int LEGEND_MIN_MAX_MASK = 0x0010;
+	// Legend Display
+	public final static int LEGEND_LOAD_FACTOR_MASK = 0x0010;
+	public final static int LEGEND_MIN_MAX_MASK = 0x0020;
 		// Extra stuff.
 //	public final static int DWELL_LABELS_MASK = 0x0040;
-	
+
 }
