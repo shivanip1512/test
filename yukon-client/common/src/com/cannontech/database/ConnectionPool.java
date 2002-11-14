c309596b99c4cfc027043780b24e54ee2ba8f768
Neuharth, Ryan <>
2002-11-14 20:26:34 +0000

    Added the ability to reconnect connections after disconnected

--- yukon-client/common/src/com/cannontech/database/ConnectionPool.java
+++ yukon-client/common/src/com/cannontech/database/ConnectionPool.java
@@ -19,6 +19,7 @@ public class ConnectionPool
    private String password;
    private int maxConns;
    private int timeOut;
+   private int initConns;
 
    private int checkedOut;
    private Vector freeConnections = new Vector();
@@ -33,8 +34,9 @@ public class ConnectionPool
 	  this.password = password;
 	  this.maxConns = maxConns;
 	  this.timeOut = timeOut > 0 ? timeOut : 5;
-
-	  initPool(initConns);
+	  this.initConns = initConns;
+	  
+	  initPool();
 
 	  CTILogger.debug("New pool created");
 	  String lf = System.getProperty("line.separator");
@@ -52,7 +54,7 @@ public class ConnectionPool
 	  freeConnections.addElement(conn);
 	  checkedOut--;
 	  notifyAll();
-	  CTILogger.debug("Returned connection to pool");
+	  CTILogger.debug("Returned/Added connection to pool");
 	  CTILogger.debug( getStats() );
    }   
    public Connection getConnection() throws SQLException 
@@ -84,7 +86,8 @@ public class ConnectionPool
 	  long startTime = System.currentTimeMillis();
 	  long remaining = timeout;
 	  Connection conn = null;
-	  while ((conn = getPooledConnection()) == null)
+
+	  while( (conn = getPooledConnection()) == null )
 	  {
 		 try
 		 {
@@ -94,6 +97,7 @@ public class ConnectionPool
 		 }
 		 catch (InterruptedException e)
 		 { }
+
 		 remaining = timeout - (System.currentTimeMillis() - startTime);
 		 if (remaining <= 0)
 		 {
@@ -107,24 +111,59 @@ public class ConnectionPool
 	  // Check if the Connection is still OK
 	  if (!isConnectionOK(conn))
 	  {
+		 closeConnection( conn );
+
+		 //try to create a new one	  	
+		 freeConnections.addElement( newConnection() );
+	  	
 		 // It was bad. Try again with the remaining timeout
-		 CTILogger.debug("Removed selected bad connection from pool" );
+		 CTILogger.debug("Removed bad connection from pool" );
 
 		 return getConnection(remaining);
 	  }
-	  checkedOut++;
-	  CTILogger.debug( "Delivered connection from pool" );
-	  CTILogger.debug( getStats() );
-	  return conn;
+	  else
+	  {
+		  checkedOut++;
+		  CTILogger.debug( "Delivered connection from pool" );
+		  CTILogger.debug( getStats() );
+		  
+		
+		  // Great we have a good conn, Be sure we have our fair share of DB conns
+		  int totalConns = freeConnections.size() + checkedOut;
+		    for( int i = totalConns; i < initConns; i++ )
+			   freeConnections.add( newConnection() );
+		  
+		  return conn;
+	  }
+
    }   
-/**
- * Insert the method's description here.
- * Creation date: (1/17/00 11:49:47 AM)
- * @return java.lang.String
- */
-public String getName() {
-	return name;
-}
+
+	/* closes a free connection that is not being used */
+	private void closeConnection ( Connection conn )
+	{
+	 	freeConnections.remove( conn );
+
+		try
+		{
+			conn.close();
+			CTILogger.debug( "Closed connection" );
+		}
+		catch (SQLException e)
+		{
+			CTILogger.error( "Couldn't close connection", e );
+		}
+	}
+
+	
+	/**
+	 * Insert the method's description here.
+	 * Creation date: (1/17/00 11:49:47 AM)
+	 * @return java.lang.String
+	 */
+	public String getName() {
+		return name;
+	}
+
    private Connection getPooledConnection() throws SQLException
    {
 	  Connection conn = null;
@@ -132,23 +171,27 @@ public String getName() {
 	  {
 		 // Pick the first Connection in the Vector
 		 // to get round-robin usage
-		 conn = (Connection) freeConnections.firstElement();
-		 freeConnections.removeElementAt(0);
+		 conn = (Connection) freeConnections.firstElement();		 
+	 	 freeConnections.removeElementAt(0);
 	  }
-	  else if (maxConns == 0 || checkedOut < maxConns)
+	  else if (maxConns == 0 || checkedOut < maxConns )
 	  {
 		 conn = newConnection();
 	  }
+	  
+
 	  return conn;
    }
-private String getStats() 
-{
-	return "POOL STATE: Total connections: " + 
-			(freeConnections.size() + checkedOut) + ", " +
-			checkedOut + "/" + freeConnections.size() +
-			" (Checked-out/Available)";
-}      
-   private void initPool(int initConns)
+   
+	private String getStats() 
+	{
+		return "POOL STATE: Total connections: " + 
+				(freeConnections.size() + checkedOut) + ", " +
+				checkedOut + "/" + freeConnections.size() +
+				" (Checked-out/Available)";
+	}
+	      
+   private void initPool()
    {
 	  for (int i = 0; i < initConns; i++)
 	  {
@@ -161,16 +204,16 @@ private String getStats()
 		 { }
 	  }
    }
+
    private boolean isConnectionOK(Connection conn)
    {
-	  Statement testStmt = null;
+	  
 	  try
 	  {
 		 if (!conn.isClosed())
 		 {
-			// Try to createStatement to see if it's really alive
-			testStmt = conn.createStatement();
-			testStmt.close();
+		 	//something used to query the DB, works for all drivers
+		 	conn.getMetaData().getTypeInfo();		 	
 		 }
 		 else
 		 {
@@ -179,21 +222,15 @@ private String getStats()
 	  }
 	  catch (SQLException e)
 	  {
-		 if (testStmt != null)
-		 {
-			try
-			{
-			   testStmt.close();
-			}
-			catch (SQLException se)
-			{ }
-		 }
-		 CTILogger.error( "Pooled Connection was not okay", e );
+		 CTILogger.info( "Pooled Connection was NOT okay" );
+		 CTILogger.error( "Pooled Connection was NOT okay", e );
 
 		 return false;
 	  }
+
 	  return true;
    }
+
    private Connection newConnection() throws SQLException
    {
 	  Connection conn = null;
@@ -224,21 +261,23 @@ private String getStats()
 	  }
 	  freeConnections.removeAllElements();
    }
-/**
- * Insert the method's description here.
- * Creation date: (2/20/2002 10:43:51 AM)
- * @return java.lang.String
- */
-public String toString() 
-{
-   //private String name;
-   //private String URL;
-   //private String user;
-   //private String password;
-	return user + " @ " + URL;
-}
-   synchronized void wrapperClosed(Connection conn)
-	  {
+   
+	/**
+	 * Insert the method's description here.
+	 * Creation date: (2/20/2002 10:43:51 AM)
+	 * @return java.lang.String
+	 */
+	public String toString() 
+	{
+	   //private String name;
+	   //private String URL;
+	   //private String user;
+	   //private String password;
+		return user + " @ " + URL;
+	}
+   
+   protected synchronized void wrapperClosed(Connection conn)
+	{
 	  // Put the connection at the end of the Vector
 	  freeConnections.addElement(conn);
 	  checkedOut--;
