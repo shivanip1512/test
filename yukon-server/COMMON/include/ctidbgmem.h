b722862d00ad0e0b637f57c17a47038c86fb1eeb
Plender, Corey <>
2002-11-15 17:10:24 +0000

    IR

---
+++ yukon-server/COMMON/include/ctidbgmem.h
@@ -0,0 +1,54 @@
+
+#pragma warning( disable : 4786)
+#ifndef __CTIDBGMEM_H__
+#define __CTIDBGMEM_H__
+
+/*-----------------------------------------------------------------------------*
+*
+* File:   ctidbgmem
+*
+* Class:
+* Date:   11/14/2002
+*
+* Author: Corey G. Plender
+*
+* CVS KEYWORDS:
+* REVISION     :  $Revision: 1.1 $
+* DATE         :  $Date: 2002/11/15 17:10:24 $
+*
+* Copyright (c) 2002 Cannon Technologies Inc. All rights reserved.
+*-----------------------------------------------------------------------------*/
+#define _CRTDBG_MAP_ALLOC
+
+#include <windows.h>
+#include <crtdbg.h>
+
+#ifdef   _DEBUG
+ #define SET_CRT_OUTPUT_MODES \
+           _CrtSetReportMode( _CRT_WARN, _CRTDBG_MODE_FILE ); \
+           _CrtSetReportFile( _CRT_WARN, _CRTDBG_FILE_STDOUT ); \
+           _CrtSetReportMode( _CRT_ERROR, _CRTDBG_MODE_FILE ); \
+           _CrtSetReportFile( _CRT_ERROR, _CRTDBG_FILE_STDOUT ); \
+           _CrtSetReportMode( _CRT_ASSERT, _CRTDBG_MODE_FILE ); \
+           _CrtSetReportFile( _CRT_ASSERT, _CRTDBG_FILE_STDOUT );
+ #define ENABLE_CRT_SHUTDOWN_CHECK \
+           _CrtSetDbgFlag((_CRTDBG_LEAK_CHECK_DF) | _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG));
+ #define SET_CRT_DEBUG_FIELD(a) \
+            _CrtSetDbgFlag((a) | _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG))
+ #define CLEAR_CRT_DEBUG_FIELD(a) \
+            _CrtSetDbgFlag(~(a) & _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG))
+
+ #define CTIDBG_new new(_NORMAL_BLOCK, __FILE__, __LINE__)
+
+#else
+
+ #define SET_CRT_OUTPUT_MODES
+ #define ENABLE_CRT_SHUTDOWN_CHECK
+ #define SET_CRT_DEBUG_FIELD(a)   ((void) 0)
+ #define CLEAR_CRT_DEBUG_FIELD(a) ((void) 0)
+ #define CTIDBG_new new
+
+#endif
+
+
+#endif // #ifndef __CTIDBGMEM_H__
