eed049c3fde064b214fbe1dfe4e082ba5b044426
Neuharth, Ryan <>
2002-12-10 17:58:53 +0000

    initial version

---
+++ yukon-client/common/src/com/cannontech/user/UserUtils.java
@@ -0,0 +1,23 @@
+package com.cannontech.user;
+
+/**
+ * @author rneuharth
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of type comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+public final class UserUtils 
+{	
+	public static final String STATUS_DISABLED = "Disabled";
+	public static final String STATUS_ENABLED = "Enabled";
+	public static final String STATUS_LOGGEDIN = "LoggedIn";
+	public static final String STATUS_LOGGEDOUT = "LoggedOut";
+	
+
+	/* Precanned existing users in the system */
+	public static final int USER_YUKON_ID 				= -1;
+	public static final int USER_WEB_USER_ID 			= -2;
+	public static final int USER_WEB_OPERATOR_ID 	= -3;
+}
