faa44a9152a85e69d775e9b78b8af38564550c5c
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2002-12-17 23:01:55 +0000

    Added GraphDataFormats class variables to here.

--- yukon-client/trending/src/com/cannontech/graph/GraphDefines.java
+++ yukon-client/trending/src/com/cannontech/graph/GraphDefines.java
@@ -5,6 +5,9 @@ package com.cannontech.graph;
  * Creation date: (6/7/2002 2:40:14 PM)
  * @author: 
  */
+import java.text.SimpleDateFormat;
+import java.text.DecimalFormat;
+
 public interface GraphDefines 
 {
 	public static final String HELP_FILE = com.cannontech.common.util.CtiUtilities.getHelpDirPath() + "Yukon Trending Help.chm";
@@ -22,4 +25,22 @@ public interface GraphDefines
 	public static final String TABULAR_PANE_STRING = "tab";
 	public static final String SUMMARY_PANE_STRING = "summary";
 
+
+	//old GraphDataFormats interface removed and fields moved into here.
+	public static SimpleDateFormat timestampQueryFormat = new SimpleDateFormat("dd-MMM-yyyy");
+	public static SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+	public static SimpleDateFormat extendedDateFormat = new SimpleDateFormat( "MMM d, yyyy");//old datePopupFormat
+
+	
+	public static SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+	public static SimpleDateFormat extendedTimeFormat = new SimpleDateFormat("HH:mm:ss");
+	
+		
+	public static SimpleDateFormat dateTimeformat = new SimpleDateFormat("M/dd/yyyy  HH:mm:ss");
+	public static SimpleDateFormat extendedDateTimeformat = new SimpleDateFormat("EEE MMM d HH:mm:ss a z yyyy");		
+
+	public static SimpleDateFormat dwellValuesDateTimeformat = new SimpleDateFormat("MMM d, HH:mm:ss");
+
+	public static DecimalFormat valueFormat = new DecimalFormat();
+	public static DecimalFormat percentFormat = new DecimalFormat(" ##0.0% ");
 }
