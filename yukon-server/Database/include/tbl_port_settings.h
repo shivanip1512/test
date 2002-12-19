7de8e7be390dfdfd9a9281be3fbdfd3c7e4ab0ea
Plender, Corey <>
2002-12-19 20:20:27 +0000

    None

--- yukon-server/Database/include/tbl_port_settings.h
+++ yukon-server/Database/include/tbl_port_settings.h
@@ -6,8 +6,8 @@
 *
 * PVCS KEYWORDS:
 * ARCHIVE      :  $Archive:   Z:/SOFTWAREARCHIVES/YUKON/DATABASE/INCLUDE/tbl_port_settings.h-arc  $
-* REVISION     :  $Revision: 1.3 $
-* DATE         :  $Date: 2002/04/16 15:58:16 $
+* REVISION     :  $Revision: 1.4 $
+* DATE         :  $Date: 2002/12/19 20:20:27 $
 *
 * Copyright (c) 1999, 2000, 2001 Cannon Technologies Inc. All rights reserved.
 *-----------------------------------------------------------------------------*/
@@ -53,10 +53,9 @@ public:
    CtiTablePortSettings&   setLineSettings(const RWCString str);
 
    INT getBits() const;
-
    INT getParity() const;
-
    INT getStopBits() const;
+
    static void getSQL(RWDBDatabase &db,  RWDBTable &keyTable, RWDBSelector &selector);
 
    virtual void DecodeDatabaseReader(RWDBReader &rdr);
