27adcefd0832c810bad61015e02c259c6146758d
Neuharth, Ryan <>
2003-01-03 21:01:47 +0000

    Made LoadAllTag points faster

--- yukon-client/yukonappserver/src/com/cannontech/yukon/server/cache/PointLoader.java
+++ yukon-client/yukonappserver/src/com/cannontech/yukon/server/cache/PointLoader.java
@@ -1,5 +1,7 @@
 package com.cannontech.yukon.server.cache;
 
+import com.cannontech.database.data.point.PointTypes;
+
 /**
  * Insert the type's description here.
  * Creation date: (3/15/00 3:57:58 PM)
@@ -28,13 +30,17 @@ java.util.Date timerStop = null;
 //temp code
 timerStart = new java.util.Date();
 //temp code
-//	String sqlString = "SELECT POINTID,POINTNAME,POINTTYPE,PAOBJECTID, " +
-//		"POINTOFFSET,STATEGROUPID FROM POINT WHERE POINTID > 0 ORDER BY PAObjectID, POINTOFFSET";
 
-	String sqlString = "SELECT P.POINTID, POINTNAME, POINTTYPE, PAOBJECTID, POINTOFFSET, STATEGROUPID, FORMULA"+
-						" FROM ( POINT P LEFT OUTER JOIN POINTUNIT PU "+
-						" ON P.POINTID = PU.POINTID )  LEFT OUTER JOIN UNITMEASURE UM ON PU.UOMID = UM.UOMID "+
-						" WHERE P.POINTID > 0 ORDER BY PAObjectID, POINTOFFSET ";
+	String sqlString =
+		"SELECT p.POINTID,p.POINTNAME, p.POINTTYPE,p.PAOBJECTID, " +
+		"p.POINTOFFSET,p.STATEGROUPID,um.formula " + 
+		"FROM POINT p left outer join pointunit pu " +
+		"on p.pointid = pu.pointid " +
+		"left outer join unitmeasure um " +
+		"on pu.uomid = um.uomid " +
+		"where p.POINTID > " + PointTypes.SYSTEM_POINT +
+		"ORDER BY p.PAObjectID, p.POINTOFFSET";
+
 	java.sql.Connection conn = null;
 	java.sql.Statement stmt = null;
 	java.sql.ResultSet rset = null;
@@ -47,24 +53,27 @@ timerStart = new java.util.Date();
 
 		while (rset.next())
 		{
+			long tags = com.cannontech.database.data.lite.LitePoint.POINT_UOFM_GRAPH;
+			
 			int pointID = rset.getInt(1);
 			String pointName = rset.getString(2).trim();
 			String pointType = rset.getString(3).trim();
 			int paobjectID = rset.getInt(4);
 			int pointOffset = rset.getInt(5);
 			int stateGroupID = rset.getInt(6);
-			long tags = com.cannontech.database.data.lite.LitePoint.POINT_UOFM_GRAPH;
 			String formula = rset.getString(7);
 			
-			if( formula != null )
-			{
-				if( formula.trim().equalsIgnoreCase("usage"))
-					tags = com.cannontech.database.data.lite.LitePoint.POINT_UOFM_USAGE;
-			}
-									
+			//process all the bit mask tags here
+			if( "usage".equalsIgnoreCase(formula) )
+				tags = com.cannontech.database.data.lite.LitePoint.POINT_UOFM_USAGE;
+
+
+			
 			com.cannontech.database.data.lite.LitePoint lp =
-				new com.cannontech.database.data.lite.LitePoint( pointID, pointName, com.cannontech.database.data.point.PointTypes.getType(pointType),
-																						paobjectID, pointOffset, stateGroupID, tags );
+				new com.cannontech.database.data.lite.LitePoint( 
+						pointID, pointName, 
+						PointTypes.getType(pointType),
+						paobjectID, pointOffset, stateGroupID, tags );
 
 			allPoints.add(lp);
 		}
@@ -86,7 +95,7 @@ timerStart = new java.util.Date();
 		{
 			com.cannontech.clientutils.CTILogger.error( e.getMessage(), e );
 		}
-		
+
 		//temp code
 		timerStop = new java.util.Date();
 		com.cannontech.clientutils.CTILogger.info( (timerStop.getTime() - timerStart.getTime())*.001 + 
@@ -95,96 +104,8 @@ timerStart = new java.util.Date();
 		//temp code
 	}
 
-	// we've only defaulted the point tags to GRAPH in our constructor (mainly because status points have no unitmeasure)
-	// now we must load the points that have unitmeasures tag values
-//	loadPointTags();
-
 }
 
-//  removed this function, load time was awful.  Fixed (bettered, rather) by using a query outer join
-// in the PointLoader.
-/*
-private synchronized void loadPointTags()
-{
-	if( allPoints == null )
-		return;
-	
-	//temp code
-	java.util.Date timerStart = null;
-	java.util.Date timerStop = null;
-	//temp code
-	
-	//temp code
-	timerStart = new java.util.Date();
-	//temp code
-	
-	int ptUpdateCnt = 0;
-	
-	String sqlString = "SELECT PU.POINTID, UM.FORMULA " +
-		"FROM POINTUNIT PU , UNITMEASURE UM WHERE PU.UOMID = UM.UOMID";
-	
-	java.sql.Connection conn = null;
-	java.sql.Statement stmt = null;
-	java.sql.ResultSet rset = null;
-	try
-	{
-		System.out.println(" START TAG LOADER QUERY");
-		conn = com.cannontech.database.PoolManager.getInstance().getConnection( this.databaseAlias );
-		stmt = conn.createStatement();
-		rset = stmt.executeQuery(sqlString);
-		System.out.println(" END TAG LOADER QUERY");
-		//All points NOT in the unitmeasure table have been defaulted to GRAPH tag in the allPoints loader.
-		while (rset.next())
-		{
-			boolean isStatus = true;
-			int pointID = rset.getInt(1);
-			String formula = rset.getString(2);
 
-			for( int i = 0; i < allPoints.size(); i++ )
-			{
-				 com.cannontech.database.data.lite.LitePoint point =((com.cannontech.database.data.lite.LitePoint)allPoints.get(i));
-				 if( point.getPointID() == pointID )
-				 {
-					// tags may need to be changed here if there are more tags added to this bit field
-					long tags = com.cannontech.database.data.lite.LitePoint.POINT_UOFM_GRAPH;      //default value of tags for now.
 
-					if( formula.equalsIgnoreCase("usage"))
-					{
-						 tags = com.cannontech.database.data.lite.LitePoint.POINT_UOFM_USAGE;
-						 ptUpdateCnt++;
-					}
-
-					((com.cannontech.database.data.lite.LitePoint)allPoints.get(i)).setTags(tags);
-					break;
-				 }
-			}
-		}
-	}
-	catch( java.sql.SQLException e )
-	{
-	  com.cannontech.clientutils.CTILogger.error( e.getMessage(), e );
-	}
-	finally
-	{
-		try
-		{
-			if( stmt != null )
-				 stmt.close();
-			if( conn != null )
-				conn.close();
-		}
-		catch( java.sql.SQLException e )
-		{
-			com.cannontech.clientutils.CTILogger.error( e.getMessage(), e );
-		}
-
-		//temp code
-		timerStop = new java.util.Date();
-		com.cannontech.clientutils.CTILogger.info( 
-			(timerStop.getTime() - timerStart.getTime())*.001 + " Secs for loadAllPointTags(" + ptUpdateCnt + " updated)" );
-		//temp code
-
-	}
-}
-*/
 }
