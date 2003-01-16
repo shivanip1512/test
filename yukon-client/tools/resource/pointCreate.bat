b84ee4226ff4595a28c6aba47e379aa33c0dd274
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2003-01-16 17:11:28 +0000

    Added to cvs.  Point creation batch file for disconnect and power fail points (more to come?).

---
+++ yukon-client/tools/resource/pointCreate.bat
@@ -0,0 +1,15 @@
+@echo off
+if "%1" == "" goto usage
+
+java -Djava.class.path=.;../config;classes12.zip;tools.jar;common.jar;SqlServer.jar;j2ee.jar;log4j-1.2.4.jar;yukonappserver.jar;yukonappclient.jar; com.cannontech.dbtools.dbcreator.point.PointCreate %1 %2
+goto done
+
+:usage
+echo:
+echo Usage:	pointcreate.bat [ p[owerfail] d[isconnect] ]
+echo:
+echo Ex1:  pointcreate.bat p d     - creates POWERFAIL and DISCONNECT points
+echo Ex2:  pointcreate.bat disc    - creates only DISCONNECT points
+echo Ex3:  pointcreate.bat power   - creates only POWER FAIL points
+echo:
+:done
