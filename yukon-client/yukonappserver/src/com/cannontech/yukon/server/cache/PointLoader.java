d28b26e0bbf0c931d896661325bb581a701ab0db
Neuharth, Ryan <>
2003-02-13 15:38:30 +0000

    Added code to try a non SQL92 query if the query fails

--- yukon-client/yukonappserver/src/com/cannontech/yukon/server/cache/PointLoader.java
+++ yukon-client/yukonappserver/src/com/cannontech/yukon/server/cache/PointLoader.java
@@ -5,7 +5,7 @@ package com.cannontech.yukon.server.cache;
  * Creation date: (3/15/00 3:57:58 PM)
  * @author: 
  */
-class PointLoader implements Runnable {
+public class PointLoader implements Runnable {
 	private java.util.ArrayList allPoints = null;
 	private String databaseAlias = null;
 /**
