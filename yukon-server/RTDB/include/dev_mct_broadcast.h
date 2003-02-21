20d7bfb9acbbc43c1efd3447efc8972ce793fa65
Plender, Corey <>
2003-02-21 23:12:24 +0000

    IR

---
+++ yukon-server/RTDB/include/dev_mct_broadcast.h
@@ -0,0 +1,75 @@
+
+/*-----------------------------------------------------------------------------*
+*
+* File:   dev_mct_broadcast
+*
+* Class:  CtiDeviceMCTBroadcast
+* Date:   2/7/2003
+*
+* Author: Corey G. Plender
+*
+* CVS KEYWORDS:
+* REVISION     :  $Revision: 1.1 $
+* DATE         :  $Date: 2003/02/21 23:12:24 $
+*
+* Copyright (c) 2002 Cannon Technologies Inc. All rights reserved.
+*-----------------------------------------------------------------------------*/
+
+#pragma warning( disable : 4786)
+#ifndef __DEV_MCT_BROADCAST_H__
+#define __DEV_MCT_BROADCAST_H__
+
+#include <set>
+#include <utility>
+using namespace std;
+
+#include <rw\cstring.h>
+#include <rw\thr\mutex.h>
+
+#include "dev_dlcbase.h"
+
+#define LEADMETER_OFFSET 4186112 - 1
+
+class IM_EX_DEVDB CtiDeviceMCTBroadcast : public CtiDeviceDLCBase
+{
+protected:
+
+    enum
+    {
+        MCTBCAST_ResetPF            = 0x50,
+        MCTBCAST_ResetPFLen         =    0,
+
+        MCTBCAST_FreezeZero         = 0x51,
+        MCTBCAST_FreezeOne          = 0x52,
+        MCTBCAST_FreezeLen          =    0,
+    };
+
+private:
+
+    bool _lastFreeze;                   // if false... last freeze sent was a zero if true, last freeze was a One;
+
+    static CTICMDSET _commandStore;
+
+public:
+
+    typedef CtiDeviceDLCBase Inherited;
+
+    CtiDeviceMCTBroadcast() {}
+    virtual ~CtiDeviceMCTBroadcast() {}
+
+    static bool initCommandStore( );
+    bool getOperation( const UINT &cmdType, USHORT &function, USHORT &length, USHORT &io );
+
+    INT executePutStatus( CtiRequestMsg *pReq, CtiCommandParser &parse, OUTMESS *&OutMessage, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
+    INT executePutConfig( CtiRequestMsg *pReq, CtiCommandParser &parse, OUTMESS *&OutMessage, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
+    INT executePutValue( CtiRequestMsg *pReq, CtiCommandParser &parse, OUTMESS *&OutMessage, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
+
+    virtual INT ExecuteRequest ( CtiRequestMsg *pReq, CtiCommandParser &parse, OUTMESS *&OutMessage, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
+    // virtual INT ErrorDecode ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist<OUTMESS> &outList );
+    virtual INT ResultDecode( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist<OUTMESS> &outList );
+
+    virtual void getSQL(RWDBDatabase &db,  RWDBTable &keyTable, RWDBSelector &selector);
+    virtual void DecodeDatabaseReader(RWDBReader &rdr);
+};
+
+#endif // #ifndef __DEV_MCT_BROADCAST_H__
