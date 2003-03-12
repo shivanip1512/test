db3a64dd45d2dbf71683c7edb7fc23c611e75fdf
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2003-03-12 14:59:13 +0000

    Renamed package from com.cannontech.dbtools.dbcreator.point.

---
+++ yukon-client/tools/src/com/cannontech/datagenerator/point/OneDevice_AnalogPointCreate.java
@@ -0,0 +1,130 @@
+package com.cannontech.datagenerator.point;
+/**
+ * Insert the type's description here.
+ * Creation date: (1/10/2001 11:18:45 PM)
+ * @author: 
+ */
+import com.cannontech.database.data.lite.LitePoint;
+import com.cannontech.database.data.point.AccumulatorPoint;
+public class OneDevice_AnalogPointCreate extends PointCreate
+{
+	int deviceID = -1;
+	int createCount = 0;
+	/**
+	 * PowerFailPointCreate constructor comment.
+	 */
+	public OneDevice_AnalogPointCreate() 
+	{
+		super();
+	}
+
+	public OneDevice_AnalogPointCreate(int devID, int count) 
+	{
+		super();
+		deviceID = devID;
+		createCount = count;
+	}
+	
+	/**
+	 * Returns true if the Device is a valid container of Power Fail points.
+	 * A valid type_ is of type MCT (Not including LMT2 or DCT501
+	 * Creation date: (4/22/2002 4:11:23 PM)
+	 * @param _type int
+	 * @return boolean
+	 */
+	public boolean isDeviceValid( int type_ )
+	{
+		// All MCT's are valid (with the exception of LMT2 and DCT501 //
+		return com.cannontech.database.data.device.DeviceTypesFuncs.isMCTOnly(type_);
+	}
+
+	/**
+	 * Returns true if the Device is a valid container of Power Fail points.
+	 * A valid type_ is of type MCT (Not including LMT2 or DCT501
+	 * Creation date: (4/22/2002 4:11:23 PM)
+	 * @param _type int
+	 * @return boolean
+	 */
+	public boolean isPointCreated( int pointOffset_, int pointType_)
+	{
+		return ((pointOffset_ == 20) && (pointType_ == com.cannontech.database.data.point.PointTypes.PULSE_ACCUMULATOR_POINT));
+	}
+	/**
+	 * Parses through the powerFailPointsDeviceList and creates a mutiDBPersistent
+	 *  of PulseAccumulator points to be inserted as Power Fail points.
+	 * Creation date: (5/29/2001 9:13:14 AM)
+	 * @return boolean
+	 */
+	public boolean create() 
+	{
+		com.cannontech.clientutils.CTILogger.info("Starting One Device Analog Point creation process...");
+
+		com.cannontech.database.data.lite.LiteYukonPAObject litePaobject = com.cannontech.database.cache.functions.PAOFuncs.getLiteYukonPAO(deviceID);
+	
+		//create an object to hold all of our DBPersistant objects
+		com.cannontech.database.data.multi.MultiDBPersistent multi = new com.cannontech.database.data.multi.MultiDBPersistent();
+		
+		// if this is not set to false it will create its own PointIDs
+		multi.setCreateNewPAOIDs( false );
+	
+		int addCount = 0;
+		int pointID = com.cannontech.database.db.point.Point.getNextPointID();
+		for (int i = 1; i < createCount+1; i++)
+		{
+			com.cannontech.clientutils.CTILogger.info("Adding PointId " + pointID + " to Device " + litePaobject.getPaoName());
+			String pointType = "Analog";
+			
+			com.cannontech.database.data.point.AnalogPoint analogPoint = new com.cannontech.database.data.point.AnalogPoint();
+			analogPoint.setPointID(new Integer(pointID));
+			analogPoint.getPoint().setPointType(pointType);
+		    analogPoint.getPoint().setPointName( pointType + i);
+			analogPoint.getPoint().setPaoID(new Integer(deviceID));
+			analogPoint.getPoint().setLogicalGroup(new String("Default"));
+			analogPoint.getPoint().setStateGroupID( new Integer(-1) );
+			analogPoint.getPoint().setServiceFlag(new Character('N'));
+			analogPoint.getPoint().setAlarmInhibit(new Character('N'));
+//			analogPoint.getPoint().setPseudoFlag(new Character('N'));	//derive attribute
+			analogPoint.getPoint().setPointOffset(new Integer( i ));	
+			analogPoint.getPoint().setArchiveType("None");	//default?
+			analogPoint.getPoint().setArchiveInterval(new Integer(0));
+			
+			// set POINTALARMING defaults
+			analogPoint.getPointAlarming().setPointID(new Integer(pointID));
+			analogPoint.getPointAlarming().setAlarmStates( analogPoint.getPointAlarming().DEFAULT_ALARM_STATES );
+			analogPoint.getPointAlarming().setExcludeNotifyStates( analogPoint.getPointAlarming().DEFAULT_EXCLUDE_NOTIFY );
+			analogPoint.getPointAlarming().setNotifyOnAcknowledge( new String("N") );
+			analogPoint.getPointAlarming().setNotificationGroupID(  new Integer(analogPoint.getPointAlarming().NONE_NOTIFICATIONID) );
+			analogPoint.getPointAlarming().setRecipientID(new Integer(analogPoint.getPointAlarming().NONE_NOTIFICATIONID));
+			
+	
+			// set POINTUNIT defaults
+			analogPoint.getPointUnit().setPointID(new Integer(pointID));
+			analogPoint.getPointUnit().setUomID(new Integer(com.cannontech.database.data.point.PointUnits.UOMID_KW));
+			analogPoint.getPointUnit().setDecimalPlaces(new Integer(2));
+	
+			// set POINTANALOG defuaults
+			analogPoint.getPointAnalog().setPointID(new Integer(pointID));
+			analogPoint.getPointAnalog().setDeadband(new Double(10.0));
+			analogPoint.getPointAnalog().setTransducerType(new String("None"));
+			analogPoint.getPointAnalog().setMultiplier(new Double(1.0));
+			analogPoint.getPointAnalog().setDataOffset(new Double(0.0));
+	
+			multi.getDBPersistentVector().add( analogPoint );
+			
+			++addCount;
+			pointID++;
+		}
+		boolean success = writeToSQLDatabase(multi);
+	
+		if( success )
+		{
+			com.cannontech.clientutils.CTILogger.info(addCount + " One Device Analog Points were processed and inserted Successfully");
+		}
+		else
+			com.cannontech.clientutils.CTILogger.info("One Device Analog Points failed insertion");
+			
+		return success;
+	}
+	
+	
+}
