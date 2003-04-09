a198a5318e265064e12a9dbccf4297ccd7da4ada
Zhihong, Yao <>
2003-04-09 16:37:27 +0000

    remove some unused keys

--- yukon-client/common/src/com/cannontech/common/util/CtiProperties.java
+++ yukon-client/common/src/com/cannontech/common/util/CtiProperties.java
@@ -78,8 +78,6 @@ public class CtiProperties extends java.util.Properties implements ClientRights
    public static final String KEY_PFACTOR_FORMAT = "pfactor_decimal_places";   
    
     public static String KEY_SMTP_HOST = "mail.smtp.host";
-    public static String KEY_ADMIN_EMAIL_ADDRESS = "admin_email_address";
-    public static String KEY_BATCH_COMMAND_FOLDER = "batch_command_folder";
     
    
 	public static final String[] ALL_CONFIG_KEYS =
@@ -125,8 +123,6 @@ public class CtiProperties extends java.util.Properties implements ClientRights
    	KEY_PFACTOR_FORMAT,
       
       KEY_SMTP_HOST,
-      KEY_ADMIN_EMAIL_ADDRESS,
-      KEY_BATCH_COMMAND_FOLDER
       
 	};
 
