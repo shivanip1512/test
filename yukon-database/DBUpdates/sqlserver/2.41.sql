5659e89cea667113e7b12e5869fc2eec88d90aa6
Neuharth, Ryan <>
2003-04-10 19:59:56 +0000

    Changed PK from pointlimits

--- yukon-database/DBUpdates/sqlserver/2.41.sql
+++ yukon-database/DBUpdates/sqlserver/2.41.sql
@@ -6,3 +6,11 @@ insert into CTIDatabase values('2.41', 'Ryan', '20-APR-2003', '');
 
 insert into yukonrole values (-300,'READMETER_MAIN_LOGO','Readmeter','DemoHeader.gif','(none)')
 go
+
+
+/**** CHANGE THE POINT LIMITS PK ****/
+alter table pointlimits drop constraint PK_POINTLIMITS
+go
+alter table pointlimits 
+	add constraint PK__POINTID_LIMITNUM primary key (pointid, limitnumber)
+go
