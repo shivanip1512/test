c21b22b3e601c070fcf330aff5f436630ca30475
Neuharth, Ryan <>
2003-04-10 18:35:23 +0000

    2.41 initial add

---
+++ yukon-database/DBUpdates/oracle/2.41.sql
@@ -0,0 +1,7 @@
+/******************************************************************************/
+/* VERSION INFO                                                               */
+/******************************************************************************/
+insert into CTIDatabase values('2.41', 'Ryan', '20-APR-2003', '');
+
+
+insert into yukonrole values (-300,'READMETER_MAIN_LOGO','Readmeter','DemoHeader.gif','(none)');
---
+++ yukon-database/DBUpdates/sqlserver/2.41.sql
@@ -0,0 +1,8 @@
+/******************************************************************************/
+/* VERSION INFO                                                               */
+/******************************************************************************/
+insert into CTIDatabase values('2.41', 'Ryan', '20-APR-2003', '');
+
+
+insert into yukonrole values (-300,'READMETER_MAIN_LOGO','Readmeter','DemoHeader.gif','(none)')
+go
