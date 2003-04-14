7b5b25a72588a355c45e6d3741d607a77581adf6
Neuharth, Ryan <>
2003-04-14 16:46:49 +0000

    Added lc_reduction_col

--- yukon-client/common/src/com/cannontech/common/util/CtiProperties.java
+++ yukon-client/common/src/com/cannontech/common/util/CtiProperties.java
@@ -77,7 +77,8 @@ public class CtiProperties extends java.util.Properties implements ClientRights
    public static final String KEY_LOGIN_PAGE_HELP_EMAIL = "login_page_help_email";
    public static final String KEY_PFACTOR_FORMAT = "pfactor_decimal_places";   
    
-    public static String KEY_SMTP_HOST = "mail.smtp.host";
+   public static String KEY_SMTP_HOST = "mail.smtp.host";
+   public static final String KEY_LC_REDUCTION_COL = "lc_reduction_col";
     
    
 	public static final String[] ALL_CONFIG_KEYS =
@@ -123,7 +124,8 @@ public class CtiProperties extends java.util.Properties implements ClientRights
    	KEY_PFACTOR_FORMAT,
       
       KEY_SMTP_HOST,
-      
+      KEY_LC_REDUCTION_COL
+
 	};
 
 
