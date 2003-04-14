b50afd670ec7a63c058bc5e6fdf4276c19f3ba37
Zhihong, Yao <>
2003-04-14 21:37:35 +0000

    added selection list defs

--- yukon-client/common/src/com/cannontech/common/constants/YukonSelectionListDefs.java
+++ yukon-client/common/src/com/cannontech/common/constants/YukonSelectionListDefs.java
@@ -63,5 +63,6 @@ public interface YukonSelectionListDefs {
 	public static final String YUK_LIST_NAME_STANDBY_SOURCE = "StandbySource";
 	public static final String YUK_LIST_NAME_IRRIGATION_TYPE = "IrrigationType";
 	public static final String YUK_LIST_NAME_SOIL_TYPE = "SoilType";
+	public static final String YUK_LIST_NAME_OPT_OUT_PERIOD = "OptOutPeriod";
 	
 }
