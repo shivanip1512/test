b918062971dfcdc911f83fd4f1345f2991d9a9c9
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2003-04-16 18:56:39 +0000

    Added load group point creator.

--- yukon-client/tools/resource/pointCreate.bat
+++ yukon-client/tools/resource/pointCreate.bat
@@ -1,12 +1,12 @@
 @echo off
 if "%1" == "" goto usage
 
-java -Djava.class.path=.;../config;classes12.zip;tools.jar;common.jar;SqlServer.jar;j2ee.jar;log4j-1.2.4.jar;yukonappserver.jar;yukonappclient.jar; com.cannontech.datagenerator.point.PointCreate %1 %2
+java -Djava.class.path=.;../config;classes12.zip;tools.jar;common.jar;SqlServer.jar;j2ee.jar;log4j-1.2.4.jar;yukonappserver.jar;yukonappclient.jar; com.cannontech.datagenerator.point.PointCreate %1 %2 %3
 goto done
 
 :usage
 echo:
-echo Usage:	pointcreate.bat [ p[owerfail] d[isconnect] ]
+echo Usage:	pointcreate.bat [ p[owerfail] d[isconnect] lg[oadgroup] ]
 echo:
 echo Ex1:  pointcreate.bat p d     - creates POWERFAIL and DISCONNECT points
 echo Ex2:  pointcreate.bat disc    - creates only DISCONNECT STATUS points
