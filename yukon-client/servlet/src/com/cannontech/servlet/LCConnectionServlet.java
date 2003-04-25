b7bcd376a80cb40b527ca3e25535969fbeb3411a
Lauinger, Aaron C <AaronCLauinger@Eaton.com>
2003-04-25 19:08:32 +0000

    added database change support, should this really be here?

--- yukon-client/servlet/src/com/cannontech/servlet/LCConnectionServlet.java
+++ yukon-client/servlet/src/com/cannontech/servlet/LCConnectionServlet.java
@@ -7,10 +7,13 @@ package com.cannontech.servlet;
  * Creation date: (3/21/2001 11:31:54 AM)
  * @author: Aaron Lauinger
  */
-
+ 
 import com.cannontech.clientutils.CTILogger;
 import com.cannontech.common.util.CtiProperties;
+import com.cannontech.database.cache.DBChangeListener;
+import com.cannontech.database.cache.DefaultDatabaseCache;
 import com.cannontech.loadcontrol.LoadControlClientConnection;
+import com.cannontech.message.dispatch.ClientConnection;
 
 public class LCConnectionServlet extends javax.servlet.http.HttpServlet implements java.util.Observer {
 		
@@ -103,6 +106,60 @@ public void init(javax.servlet.ServletConfig config) throws javax.servlet.Servle
 
 	// Add this to the context so other servlets can access the connection
 	getServletContext().setAttribute(SERVLET_CONTEXT_ID, this);		
+	
+	DBChangeListener dbl = new DBChangeListener() {
+		ClientConnection dispatchConnection = null;
+		public com.cannontech.message.util.ClientConnection getClientConnection() 
+		{
+			if (dispatchConnection == null) {
+
+			String host;
+			int port;
+
+			try {
+				host =
+					com.cannontech.common.util.CtiProperties.getInstance().getProperty(
+						com.cannontech.common.util.CtiProperties.KEY_DISPATCH_MACHINE,
+						"127.0.0.1");
+				port =
+					Integer.parseInt(
+						CtiProperties.getInstance().getProperty(
+							CtiProperties.KEY_DISPATCH_PORT,
+							"1510"));
+
+			} catch (Exception e) {
+				com.cannontech.clientutils.CTILogger.error(e.getMessage(), e);
+				return null;
+			}
+
+			dispatchConnection = new com.cannontech.message.dispatch.ClientConnection();
+			com.cannontech.message.dispatch.message.Registration reg =
+				new com.cannontech.message.dispatch.message.Registration();
+			reg.setAppName(
+				"LCConnectionServlet @" + com.cannontech.common.util.CtiUtilities.getUserName());
+			reg.setAppIsUnique(0);
+			reg.setAppKnownPort(0);
+			reg.setAppExpirationDelay(300); // 5 minutes should be OK
+
+			//conn.addObserver(this);
+			dispatchConnection.setHost(host);
+			dispatchConnection.setPort(port);
+			dispatchConnection.setAutoReconnect(true);
+			dispatchConnection.setRegistrationMsg(reg);
+
+			try {
+				dispatchConnection.connectWithoutWait();
+			} catch (Exception e) {
+				com.cannontech.clientutils.CTILogger.error(e.getMessage(), e);
+			}
+		}
+		
+			return dispatchConnection;
+		}
+		public void handleDBChangeMsg(com.cannontech.message.dispatch.message.DBChangeMsg msg, com.cannontech.database.data.lite.LiteBase lBase ) { }	
+	};
+	
+	DefaultDatabaseCache.getInstance().addDBChangeListener(dbl);
 }
 /**
  * This is registered with the MACS Connection to receive updates
