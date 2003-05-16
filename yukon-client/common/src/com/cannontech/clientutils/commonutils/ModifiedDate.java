06852d80b2338675c229c763588284892221448e
Neuharth, Ryan <>
2003-05-16 20:34:56 +0000

    Added a method to set the format pattern

--- yukon-client/common/src/com/cannontech/clientutils/commonutils/ModifiedDate.java
+++ yukon-client/common/src/com/cannontech/clientutils/commonutils/ModifiedDate.java
@@ -5,82 +5,88 @@ package com.cannontech.clientutils.commonutils;
  * Creation date: (6/14/00 11:08:03 AM)
  * @author: 
  */
-import com.cannontech.clientutils.CommonUtils;
+import java.text.SimpleDateFormat;
 
 public class ModifiedDate extends java.util.Date 
 {
-/**
- * ModifiedDate constructor comment.
- */
-public ModifiedDate() {
-	super();
-}
-/**
- * ModifiedDate constructor comment.
- * @param year int
- * @param month int
- * @param date int
- */
-public ModifiedDate(int year, int month, int date) {
-	super(year, month, date);
-}
-/**
- * ModifiedDate constructor comment.
- * @param year int
- * @param month int
- * @param date int
- * @param hrs int
- * @param min int
- */
-public ModifiedDate(int year, int month, int date, int hrs, int min) {
-	super(year, month, date, hrs, min);
-}
-/**
- * ModifiedDate constructor comment.
- * @param year int
- * @param month int
- * @param date int
- * @param hrs int
- * @param min int
- * @param sec int
- */
-public ModifiedDate(int year, int month, int date, int hrs, int min, int sec) {
-	super(year, month, date, hrs, min, sec);
-}
-/**
- * ModifiedDate constructor comment.
- * @param date long
- */
-public ModifiedDate(long date) {
-	super(date);
-}
+	//default pattern for this guy
+	private static SimpleDateFormat formatter = 
+				new SimpleDateFormat("MM-dd-yyyy HH:mm:ss");
 
-/**
- * Insert the method's description here.
- * Creation date: (6/14/00 11:08:21 AM)
- */
-public String getDateString() 
-{
-	java.text.SimpleDateFormat formatter = new java.text.SimpleDateFormat("MM-dd-yyyy");
+	/**
+	 * ModifiedDate constructor comment.
+	 */
+	public ModifiedDate() {
+		super();
+	}
+	/**
+	 * ModifiedDate constructor comment.
+	 * @param year int
+	 * @param month int
+	 * @param date int
+	 */
+	public ModifiedDate(int year, int month, int date) {
+		super(year, month, date);
+	}
+	/**
+	 * ModifiedDate constructor comment.
+	 * @param year int
+	 * @param month int
+	 * @param date int
+	 * @param hrs int
+	 * @param min int
+	 */
+	public ModifiedDate(int year, int month, int date, int hrs, int min) {
+		super(year, month, date, hrs, min);
+	}
+	/**
+	 * ModifiedDate constructor comment.
+	 * @param year int
+	 * @param month int
+	 * @param date int
+	 * @param hrs int
+	 * @param min int
+	 * @param sec int
+	 */
+	public ModifiedDate(int year, int month, int date, int hrs, int min, int sec) {
+		super(year, month, date, hrs, min, sec);
+	}
+	/**
+	 * ModifiedDate constructor comment.
+	 * @param date long
+	 */
+	public ModifiedDate(long date) {
+		super(date);
+	}
+	
+	public static synchronized void setFormatPattern( String pattern )
+	{
+		formatter.applyPattern( pattern );
+	}
+	
+	/**
+	 * Insert the method's description here.
+	 * Creation date: (6/14/00 11:08:21 AM)
+	 */
+	public String getDateString() 
+	{
+		return formatter.format( this );
+	}
+	/**
+	 * Insert the method's description here.
+	 * Creation date: (6/14/00 11:08:21 AM)
+	 */
+	public String getTimeString() 
+	{
+		return formatter.format( this );
+	}
+	/**
+	 * Insert the method's description here.
+	 * Creation date: (6/14/00 11:08:21 AM)
+	 */
+	public String toString() 
+	{
+		return formatter.format( this );
+	}
 
-	return formatter.format( this );
-}
-/**
- * Insert the method's description here.
- * Creation date: (6/14/00 11:08:21 AM)
- */
-public String getTimeString() 
-{
-	java.text.SimpleDateFormat formatter = new java.text.SimpleDateFormat("HH:mm:ss");
-
-	return formatter.format( this );
-}
-/**
- * Insert the method's description here.
- * Creation date: (6/14/00 11:08:21 AM)
- */
-public String toString() 
-{
-	return CommonUtils.formatDate( this );
-}
 }
