802636dc1e270697894d0d1172f9b1f1553a8ee3
Neuharth, Ryan <>
2003-05-20 14:31:03 +0000

    Added some YukonGroup functions to process several instances

--- yukon-client/common/src/com/cannontech/database/db/user/YukonGroup.java
+++ yukon-client/common/src/com/cannontech/database/db/user/YukonGroup.java
@@ -1,15 +1,21 @@
 package com.cannontech.database.db.user;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import com.cannontech.database.db.DBPersistent;
 
 /**
  * @author alauinger
  */
-public class YukonGroup extends DBPersistent {
-
+public class YukonGroup extends DBPersistent 
+{
 	public static final String TABLE_NAME = "YukonGroup";
+
+	//a mapping table the does not have a DBPersistent
+	public static final String TBL_YUKON_USER_GROUP = "YukonUserGroup";
+
+
 	
 	private Integer groupID;
 	private String groupName;
@@ -31,6 +37,69 @@ public class YukonGroup extends DBPersistent {
 		setGroupID(groupID);
 		setGroupName(groupName);
 	}
+
+
+	/**
+	 * This method returns a list of YukonGroups that a user belongs to. UserID is the user.
+	 * Creation date: (2/14/2003 10:31:33 AM)
+	 * @return YukonGroup[]
+	 */
+	public static synchronized YukonGroup[] getYukonGroups( int userID_, java.sql.Connection conn_ )
+	{
+		if( conn_ == null )
+			throw new IllegalStateException("Database connection should not be null.");
+	
+		
+		java.sql.PreparedStatement pstmt = null;
+		java.sql.ResultSet rset = null;
+		ArrayList list = new ArrayList(16);
+		
+		try 
+		{		
+			 pstmt = conn_.prepareStatement(
+					"select g.GroupID id, g.GroupName name, g.GroupDescription descr " +
+					"from " + TABLE_NAME + " g, " + TBL_YUKON_USER_GROUP + " u " +
+					"where u.UserID = ? " +
+					"and g.GroupID = u.GroupID " + 
+					"order by g.GroupName");
+		    	
+			 pstmt.setInt( 1, userID_ );
+			 
+			 rset = pstmt.executeQuery();
+				
+			 //get the first returned result
+			 while( rset.next() )
+			 {
+				YukonGroup group = new YukonGroup(
+					new Integer(rset.getInt("id")),
+					rset.getString("name") );
+
+				group.setGroupDescription( rset.getString("descr") );
+			 	
+				list.add( group );
+			 }
+		    
+		}
+		catch (java.sql.SQLException e) 
+		{
+			 e.printStackTrace();
+		}
+		finally 
+		{
+			 try 
+			 {
+				if ( pstmt != null) pstmt.close();
+			 }
+			 catch (java.sql.SQLException e2) 
+			 {
+				e2.printStackTrace();
+			 }
+		}
+		
+		YukonGroup[] groups = new YukonGroup[list.size()];
+		return (YukonGroup[])list.toArray( groups );
+	}
+
 	
 	/**
 	 * @see com.cannontech.database.db.DBPersistent#add()
