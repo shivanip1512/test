980ce24aa5a46a4b37d9f4190c884aebaa1402f6
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2003-05-20 21:04:47 +0000

    Added styleSheet, Bullets, and headerLogo properties.

--- yukon-client/common/src/com/cannontech/roles/application/WebClientRole.java
+++ yukon-client/common/src/com/cannontech/roles/application/WebClientRole.java
@@ -10,4 +10,10 @@ public interface WebClientRole {
 	
 	public static final int HOME_URL = ApplicationRoleDefs.WEB_CLIENT_PROPERTYID_BASE;
 	public static final int LOG_LEVEL = ApplicationRoleDefs.WEB_CLIENT_PROPERTYID_BASE - 1;
+
+	public static final int STYLE_SHEET = ApplicationRoleDefs.WEB_CLIENT_PROPERTYID_BASE - 2;
+	public static final int NAV_BULLET_SELECTED = ApplicationRoleDefs.WEB_CLIENT_PROPERTYID_BASE - 3;	//Bullet.gif
+	public static final int NAV_BULLET = ApplicationRoleDefs.WEB_CLIENT_PROPERTYID_BASE - 4;	//Bullet2.gif
+	
+	public static final int HEADER_LOGO = ApplicationRoleDefs.WEB_CLIENT_PROPERTYID_BASE - 5;
 }
