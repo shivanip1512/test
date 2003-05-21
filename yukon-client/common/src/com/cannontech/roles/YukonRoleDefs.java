faba618fc08e4dc4ce7f9552f7ade51b05bc4164
Zhihong, Yao <>
2003-05-21 20:35:09 +0000

    added constant definitions for EnergyCompany role

--- yukon-client/common/src/com/cannontech/roles/YukonRoleDefs.java
+++ yukon-client/common/src/com/cannontech/roles/YukonRoleDefs.java
@@ -7,6 +7,8 @@ import com.cannontech.roles.*;
  */
 public interface YukonRoleDefs extends RoleDefs {
 	public static final int SYSTEM_ROLEID = YUKON_ROLEID_BASE;
+	public static final int ENERGY_COMPANY_ROLDID = YUKON_ROLEID_BASE - 1;
 	
 	public static final int SYSTEM_PROPERTYID_BASE = YUKON_PROPERTYID_BASE;
+	public static final int ENERGY_COMPANY_PROPERTYID_BASE = YUKON_PROPERTYID_BASE - 100;
 }
