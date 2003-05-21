b4c5bee7b544539487e8d5869fd6f462028066f5
Zhihong, Yao <>
2003-05-21 20:33:32 +0000

    class for OddsForControl role

---
+++ yukon-client/common/src/com/cannontech/roles/operator/OddsForControlRole.java
@@ -0,0 +1,18 @@
+package com.cannontech.roles.operator;
+
+import com.cannontech.roles.*;
+
+/**
+ * @author yao
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of type comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+public interface OddsForControlRole {
+	public static final int ROLEID = OperatorRoleDefs.ODDS_FOR_CONTROL_ROLEID;
+	
+	public static final int ODDS_FOR_CONTROL_LABEL = OperatorRoleDefs.ODDS_FOR_CONTROL_PROPERTYID_BASE;
+
+}
