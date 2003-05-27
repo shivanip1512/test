c6a05135c44863aa02374b35b71a53b99a8b505d
Neuharth, Ryan <>
2003-05-27 15:01:26 +0000

    A new class to say when a problem occurs because of PointQuality inconsistencies

---
+++ yukon-client/common/src/com/cannontech/database/data/point/CTIPointQuailtyException.java
@@ -0,0 +1,51 @@
+/*
+ * Created on May 27, 2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package com.cannontech.database.data.point;
+
+/**
+ * @author rneuharth
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class CTIPointQuailtyException extends Exception
+{
+
+	/**
+	 * 
+	 */
+	public CTIPointQuailtyException()
+	{
+		super();
+	}
+
+	/**
+	 * @param message
+	 */
+	public CTIPointQuailtyException(String message)
+	{
+		super(message);
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public CTIPointQuailtyException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
+
+	/**
+	 * @param cause
+	 */
+	public CTIPointQuailtyException(Throwable cause)
+	{
+		super(cause);
+	}
+
+}
