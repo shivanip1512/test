4b9b094878e5d51a0fcbfba69503eeefb5cadcea
Lauinger, Aaron C <AaronCLauinger@Eaton.com>
2003-05-28 16:39:05 +0000

    Added functions to get: the EnergyCompany user by id and energy company object an energycompany by id

--- yukon-client/common/src/com/cannontech/database/cache/functions/EnergyCompanyFuncs.java
+++ yukon-client/common/src/com/cannontech/database/cache/functions/EnergyCompanyFuncs.java
@@ -1,7 +1,9 @@
 package com.cannontech.database.cache.functions;
 
+import java.util.Iterator;
 import java.util.Map;
 
+import com.cannontech.database.cache.DefaultDatabaseCache;
 import com.cannontech.database.data.lite.LiteEnergyCompany;
 import com.cannontech.database.data.lite.LiteYukonUser;
 
@@ -16,15 +18,53 @@ private EnergyCompanyFuncs() {
 }
 
 /**
+ * Returns the lite energy company with the given energy company id.
+ * @param energyCompanyID
+ * @return LiteEnergyCompany
+ */
+public static LiteEnergyCompany getEnergyCompany(int energyCompanyID) {
+	DefaultDatabaseCache cache = DefaultDatabaseCache.getInstance();
+		synchronized( cache ) {
+			for(Iterator i = cache.getAllEnergyCompanies().iterator(); i.hasNext();) {
+				LiteEnergyCompany e = (LiteEnergyCompany) i.next();
+				if(e.getEnergyCompanyID() == energyCompanyID) {
+					return e;
+				}
+			}	
+		}
+	return null;
+}
+
+/**
  * Return the energy company this user belongs to.
  * @param user
  * @return LiteEnergyCompany
  */
 public static LiteEnergyCompany getEnergyCompany(LiteYukonUser user) {
-	com.cannontech.database.cache.DefaultDatabaseCache cache = com.cannontech.database.cache.DefaultDatabaseCache.getInstance();
+	DefaultDatabaseCache cache = DefaultDatabaseCache.getInstance();
 	synchronized( cache ) {
 		Map m = cache.getAllUserEnergyCompanies();
 		return (LiteEnergyCompany) m.get(user);
 	}	
 }
+
+/**
+ * Returns the energy companies lite user 
+ * @param company
+ * @return LiteYukonUser
+ */
+public static LiteYukonUser getEnergyCompanyUser(LiteEnergyCompany company) {
+	return YukonUserFuncs.getLiteYukonUser(company.getUserID());
+}
+
+/**
+ * Returns the energy companies lite user
+ * @param energyCompanyID
+ * @return LiteYukonUser
+ */
+public static LiteYukonUser getEnergyCompanyUser(int energyCompanyID) {
+	LiteEnergyCompany ec = getEnergyCompany(energyCompanyID);
+	return (ec == null ? null : getEnergyCompanyUser(ec));
+}
+
 }
