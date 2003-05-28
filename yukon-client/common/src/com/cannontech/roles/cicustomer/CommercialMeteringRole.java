13ad086be57949a018d1644279807a8881647fcc
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2003-05-28 22:02:32 +0000

    Added Get Data Now buttong property.

--- yukon-client/common/src/com/cannontech/roles/cicustomer/CommercialMeteringRole.java
+++ yukon-client/common/src/com/cannontech/roles/cicustomer/CommercialMeteringRole.java
@@ -9,4 +9,5 @@ public interface CommercialMeteringRole {
 	public static final int ROLEID = CICustomerRoleDefs.COMMERCIAL_METERING_ROLEID;
 	
 	public static final int TRENDING_DISCLAIMER = CICustomerRoleDefs.COMMERCIAL_METERING_PROPERTYID_BASE;
+	public static final int TRENDING_GET_DATA_NOW_BUTTON = CICustomerRoleDefs.COMMERCIAL_METERING_PROPERTYID_BASE - 1;
 }
