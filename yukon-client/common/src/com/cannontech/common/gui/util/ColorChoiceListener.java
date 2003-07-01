37a3981b76f7392f7e55ccaf5d7c2d6534b58560
Lauinger, Aaron C <AaronCLauinger@Eaton.com>
2003-07-01 22:42:11 +0000

    Not used

--- yukon-client/common/src/com/cannontech/common/gui/util/ColorChoiceListener.java
+++
@@ -1,9 +0,0 @@
-package com.cannontech.common.gui.util;
-
-public interface ColorChoiceListener extends java.util.EventListener {
-/**
- * 
- * @param newEvent java.util.EventObject
- */
-void JRadioButtonItem_itemStateChanged(java.util.EventObject newEvent);
-}
