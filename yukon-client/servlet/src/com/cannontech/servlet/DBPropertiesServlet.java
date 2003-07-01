d1998e69885336f12abc35a7a230eefaaac978d8
Lauinger, Aaron C <AaronCLauinger@Eaton.com>
2003-07-01 22:45:51 +0000

    Exists solely to send client application their database properties

---
+++ yukon-client/servlet/src/com/cannontech/servlet/DBPropertiesServlet.java
@@ -0,0 +1,60 @@
+/*
+ * Created on Jun 30, 2003
+ */
+package com.cannontech.servlet;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.cannontech.clientutils.CTILogger;
+import com.cannontech.common.constants.LoginController;
+import com.cannontech.database.PoolManager;
+import com.cannontech.database.data.lite.LiteYukonUser;
+
+/**
+ * Writes the contents of db.properties out the responses output stream if
+ * a user is found in the session.
+ * @author aaron
+ */
+public class DBPropertiesServlet extends HttpServlet {
+
+	protected void service(HttpServletRequest req, HttpServletResponse resp)
+		throws ServletException, IOException {
+
+		HttpSession session = req.getSession(false);
+		
+		if(session != null) {
+			LiteYukonUser user = (LiteYukonUser) session.getAttribute(LoginController.YUKON_USER);
+			if(user != null) {
+				sendDBProps(resp.getOutputStream());		
+			}
+		}	
+	}
+		
+	private void sendDBProps(OutputStream out) {
+
+		InputStream is = null;		
+		try {		
+			is = getClass().getResourceAsStream( PoolManager.DB_PROPERTIES_FILE );
+			byte[] buf = new byte[4096];
+			int r = -1;
+			while((r = is.read(buf, 0, buf.length)) != -1) {
+				out.write(buf, 0, r);
+			}
+		}
+		catch(Exception e) {
+			CTILogger.error("Couldn't load " + PoolManager.DB_PROPERTIES_FILE, e);
+		}
+		finally {
+			try { if(is != null) is.close(); } catch(IOException e2) {} 			
+		}
+	}
+
+}
