13a382cc628f9b1807ef96c4d291315267a45349
Lauinger, Aaron C <AaronCLauinger@Eaton.com>
2003-07-02 20:50:04 +0000

    *** empty log message ***

--- yukon-client/common/src/com/cannontech/common/login/LoginSupport.java
+++ yukon-client/common/src/com/cannontech/common/login/LoginSupport.java
@@ -14,6 +14,7 @@ import com.cannontech.clientutils.CTILogger;
 import com.cannontech.common.constants.LoginController;
 
 /**
+ * Handles the dirty work of contacting servlets.
  * @author aaron
  */
 class LoginSupport {
