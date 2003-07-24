0d75cf77db7644ab762727692a84c0f81032fdf2
Neuharth, Ryan <>
2003-07-24 18:33:22 +0000

    Changed JCheckBox in trees to only fire when they are pressed

--- yukon-client/common/src/com/cannontech/common/gui/tree/CheckNodeSelectionListener.java
+++ yukon-client/common/src/com/cannontech/common/gui/tree/CheckNodeSelectionListener.java
@@ -1,8 +1,11 @@
 package com.cannontech.common.gui.tree;
 
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultTreeModel;
@@ -33,26 +36,10 @@ public class CheckNodeSelectionListener extends MouseAdapter
       int row = tree.getRowForLocation(x, y);
       TreePath  path = tree.getPathForRow(row);
 
-/*
-int cnt = ((JPanel)tree.getCellRenderer()).getComponentCount();
-for( int i = 0; i < cnt; i++ )
-	 System.out.println("   cmp=("+i+") " + 
-	 	((JPanel)tree.getCellRenderer()).getComponent(i).getLocation() );
 
-System.out.println("   x=" + 
-		((JPanel)tree.getCellRenderer()).getComponentAt(
-			e.getPoint()) );
-		
-System.out.println("   y=" + 
-		((JPanel)tree.getCellRenderer()).getComponentCount() );
-
-System.out.println("   l=" + 
-		((JPanel)tree.getCellRenderer()).getLocation() );
-		
-System.out.println("   pt=" + e.getPoint() );
-
-System.out.println();
-*/
+		//only do events if the actual check box is selected
+		if( !isCheckBoxSelected(e.getPoint(), tree.getRowBounds(row)) )
+			return;
 
       if( path != null
       	 && path.getLastPathComponent() instanceof CheckNode ) 
@@ -76,12 +63,38 @@ System.out.println();
     }
     
 
+
+	/**
+	 * takes a mouse clicks point and the rows visible rectangle attribute to know
+	 */
+	private boolean isCheckBoxSelected( Point pt, Rectangle visRowRect_ )
+	{
+		if( visRowRect_ == null || pt == null )
+			return false;
+
+
+		JPanel rend = (JPanel)tree.getCellRenderer();		
+		int checkW = 0;
+		for( int i = 0; i < rend.getComponentCount(); i++ )
+			if( rend.getComponent(i) instanceof JCheckBox )
+			{
+				checkW = rend.getComponent(i).getWidth();
+				break;
+			}
+
+
+		return 
+			pt.getX() >= visRowRect_.getLocation().getX()
+			&& pt.getX() <= (checkW + visRowRect_.getLocation().getX());		
+	}
+
 	private void selectNode( CheckNode node, boolean selected, int row )
 	{		
 		node.setSelected( selected );
 
 		((DefaultTreeModel)tree.getModel()).nodeChanged(node);
 		
-    }
-    
+   }
+
+	
 }
