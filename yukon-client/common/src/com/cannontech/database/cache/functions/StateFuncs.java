d3724051f5f340e06e5b73b978e47eead59aabc0
Lauinger, Aaron C <AaronCLauinger@Eaton.com>
2003-07-24 22:15:03 +0000

    Added method to get a litestategroup by id

--- yukon-client/common/src/com/cannontech/database/cache/functions/StateFuncs.java
+++ yukon-client/common/src/com/cannontech/database/cache/functions/StateFuncs.java
@@ -1,6 +1,9 @@
 package com.cannontech.database.cache.functions;
 
+import java.util.Iterator;
+
 import com.cannontech.clientutils.CTILogger;
+import com.cannontech.database.cache.DefaultDatabaseCache;
 import com.cannontech.database.data.lite.LiteState;
 import com.cannontech.database.data.lite.LiteStateGroup;
 
@@ -56,5 +59,36 @@ public final class StateFuncs
 			" and rawState=" + rawState );
 
 		return null;
+	}
+	
+	public static LiteStateGroup getLiteStateGroup(int stateGroupID) 
+	{
+		DefaultDatabaseCache cache = DefaultDatabaseCache.getInstance();
+		synchronized(cache) 
+		{
+			for(Iterator i = cache.getAllStateGroups().iterator(); i.hasNext(); ) 
+			{
+				LiteStateGroup lsg = (LiteStateGroup) i.next();
+				if(lsg.getStateGroupID() == stateGroupID)
+				{
+					return lsg;		
+				}
+			}
+		}
+		
+		return null;
+	}
+	
+	public static LiteState[] getLiteStates(int stateGroupID) {
+		DefaultDatabaseCache cache = DefaultDatabaseCache.getInstance();
+		LiteStateGroup lsg = null;
+		synchronized(cache) 
+		{
+			lsg = getLiteStateGroup(stateGroupID);
+		}
+		
+		LiteState[] ls = new LiteState[lsg.getStatesList().size()];
+		lsg.getStatesList().toArray(ls);
+		return ls;
 	}
 }
