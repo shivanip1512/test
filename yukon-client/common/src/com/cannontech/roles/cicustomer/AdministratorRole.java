574c0d7d989da89628c79b17fb24c44b2e17a6f7
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2003-08-01 20:49:31 +0000

    CICustomer Administrative role.

---
+++ yukon-client/common/src/com/cannontech/roles/cicustomer/AdministratorRole.java
@@ -0,0 +1,13 @@
+package com.cannontech.roles.cicustomer;
+
+import com.cannontech.roles.CICustomerRoleDefs;
+
+/**
+ * @author snebben
+ */
+public interface AdministratorRole{
+	public static final int ROLEID = CICustomerRoleDefs.ADMINISTRATOR_ROLEID;
+	
+//	public static final int TRENDING_DISCLAIMER = CICustomerRoleDefs.COMMERCIAL_METERING_PROPERTYID_BASE;
+//	public static final int TRENDING_GET_DATA_NOW_BUTTON = CICustomerRoleDefs.COMMERCIAL_METERING_PROPERTYID_BASE - 1;
+}
