0ac0c5fc6465ea2017b6e4e650e3fbecfe704286
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2003-08-25 17:12:32 +0000

    contact information editable.

--- yukon-client/common/src/com/cannontech/roles/cicustomer/AdministratorRole.java
+++ yukon-client/common/src/com/cannontech/roles/cicustomer/AdministratorRole.java
@@ -8,6 +8,6 @@ import com.cannontech.roles.CICustomerRoleDefs;
 public interface AdministratorRole{
 	public static final int ROLEID = CICustomerRoleDefs.ADMINISTRATOR_ROLEID;
 	
-//	public static final int TRENDING_DISCLAIMER = CICustomerRoleDefs.COMMERCIAL_METERING_PROPERTYID_BASE;
-//	public static final int TRENDING_GET_DATA_NOW_BUTTON = CICustomerRoleDefs.COMMERCIAL_METERING_PROPERTYID_BASE - 1;
+	public static final int CONTACT_INFO_EDITABLE = CICustomerRoleDefs.ADMINISTRATOR_PROPERTYID_BASE;
+
 }
