f812fce53d278be760e5035d571ea275387e80e4
Lauinger, Aaron C <AaronCLauinger@Eaton.com>
2003-08-26 21:45:48 +0000

    Major rework to how it looks

--- yukon-client/esub/src/com/cannontech/esub/table/Table.java
+++ yukon-client/esub/src/com/cannontech/esub/table/Table.java
@@ -14,14 +14,14 @@ import javax.swing.table.TableModel;
  */
 public class Table {
 	private static final Color COMPONENT_BACKGROUND_COLOR = Color.WHITE;
-	private static final Color MAIN_TITLE_COLOR = Color.RED;
-	private static final Color COLUMN_HEADING_TEXT_COLOR = Color.WHITE;
-	private static final Color COLUMN_HEADING_BACKGROUND_COLOR = new Color(.35f,.22f,0.0f);
-	private static final Color TABLE_BACKGROUND_COLOR = new Color(0.0f, 0.53f,0.73f);
-	private static final Color TABLE_TEXT_COLOR = Color.BLACK;
-	private static final Color COLUMN_SEPARATOR_COLOR = new Color(0.0f, 0.06f,0.48f);
+	private static final Color MAIN_TITLE_COLOR = new Color(0,0,153);
+	private static final Color COLUMN_HEADING_TEXT_COLOR = Color.BLACK;
+	private static final Color COLUMN_HEADING_BACKGROUND_COLOR = new Color(204,204,128);
+	private static final Color TABLE_BACKGROUND_COLOR = Color.BLACK;
+	private static final Color TABLE_TEXT_COLOR = Color.WHITE;
+	private static final Color COLUMN_SEPARATOR_COLOR = Color.WHITE;
 	
-	private static final int COMPONENT_INSETS = 3;
+	private static final int COMPONENT_INSETS = 0;
 	
 	private static final Font TITLE_FONT = new Font("Arial",Font.BOLD,12);
 	private static final Font COLUMN_HEADER_FONT = new Font("Arial",Font.PLAIN,12);
@@ -44,34 +44,33 @@ public class Table {
 		
 		// background
 		g.setColor(COMPONENT_BACKGROUND_COLOR);
-		g.fillRect(0,0,width,height);
+		g.fillRect(0,0,width+1,height+1);
 		
 		// header
 		g.setColor(MAIN_TITLE_COLOR);
 		g.setFont(TITLE_FONT);
 		
 		int textHeight = g.getFontMetrics().getHeight();
-		g.drawString(getTitle(), COMPONENT_INSETS, textHeight + COMPONENT_INSETS);
+		int titleHeaderHeight = textHeight + 10;
+		g.drawString(getTitle(), 5, titleHeaderHeight/2 + textHeight/2);
 		
 	
 		// table background
 		g.setColor(COLUMN_SEPARATOR_COLOR);
-		x = COMPONENT_INSETS;
-		y = textHeight + (2*COMPONENT_INSETS);
-		w = width - (2*COMPONENT_INSETS);
-		h = height - (COMPONENT_INSETS + y);
-		g.fillRect(x,y,w,h);
+		x = 2;
+		y = titleHeaderHeight;
+		w = width;
+		h = height - y;
 			
 		// column backgrounds
-		int colWidth = w / model.getColumnCount();		
+		int[] colWidth = calcColumnWidths(g, model, w);
 		int colHeight = h;
 				
 		// draw columns
 		y = y+2;
 		for(int i = 0; i < model.getColumnCount(); i++ ) {
-//x = drawColumn(g,x+2,y,model,i,colWidth,//lHeight);
-			drawColumn(g,x+2,y,model,i,colWidth,colHeight);
-			x += colWidth;
+			drawColumn(g,x,y,model,i,colWidth[i],colHeight-3);
+			x += colWidth[i]+1;
 		}
 					
 		g.translate((int)rect.getMinX() * -1, (int)rect.getMinY() * -1);
@@ -79,31 +78,68 @@ public class Table {
 	
 
 	private void drawColumn(Graphics g, int x, int y, TableModel model, int col, int colWidth, int colHeight) {
-		//int textHeight =  g.getFontMetrics().getHeight() + 4;
 		String text = model.getColumnName(col);
 		FontMetrics fm = g.getFontMetrics();
 		Rectangle2D strBounds = fm.getStringBounds(text,null);
 
 		int cx = x + (colWidth/2);
+		int colHeadingHeight = (int) strBounds.getHeight()+4+6;
 		
 		g.setColor(TABLE_BACKGROUND_COLOR);
-		g.fillRect(x,y,colWidth-4,colHeight);
+		g.fillRect(x,y,colWidth,colHeight);
 		
 		g.setColor(COLUMN_HEADING_BACKGROUND_COLOR);
-		g.fillRect(x,y,colWidth-4,(int)strBounds.getHeight()+4);
+		g.fillRect(x,y,colWidth,colHeadingHeight);
 		
 		g.setColor(COLUMN_HEADING_TEXT_COLOR);
-	  	g.drawString(text, cx - (int)(strBounds.getWidth()/2), (int) (y+strBounds.getHeight()));
+	  	g.drawString(text, cx - (int)(strBounds.getWidth()/2), (int) (y+strBounds.getHeight())+4);
+	  	
+	  	y += colHeadingHeight;
 	  	
 	  	g.setColor(TABLE_TEXT_COLOR);
 	  	for(int i = 0; i < model.getRowCount(); i++) {
 	  		text = model.getValueAt(i,col).toString();
-	  		g.drawString(text, x+2, y + (int)(strBounds.getHeight()*(i+2)));
+	  		int valWidth = fm.stringWidth(text);
+	  		g.drawString(text, x+2, y + (int)(strBounds.getHeight()*(i+1)));
 	  		
 	  	}
 	}
 
-	
+
+	private int[] calcColumnWidths(Graphics g, TableModel model, int tableWidth) {
+		int[] widths = new int[model.getColumnCount()];
+		FontMetrics fm = g.getFontMetrics();
+
+		tableWidth = tableWidth - (model.getColumnCount()+2);
+
+		int allocatedWidth = 0;
+		
+		// find the minimum width needed for each column
+		for(int col = model.getColumnCount()-1; col >= 0; col--) {
+			for(int row = model.getRowCount()-1; row >= 0; row--) {
+				String val = model.getValueAt(row,col).toString();
+				int valWidth = fm.stringWidth(val);
+				if(widths[col] < valWidth) {
+					widths[col] = valWidth;
+				}
+			}
+			allocatedWidth += widths[col];	
+		}
+		
+		// if the min width is less than the table width, allocate the rest
+		// of the space evenly, if it isn't then doh!
+		int extraWidth = Math.max(tableWidth - allocatedWidth, 0); 
+		int extraCellWidth = extraWidth / model.getColumnCount();
+		int remainingWidth = extraWidth % model.getColumnCount();
+		
+		System.out.println("tablewidth:" + tableWidth + " allocated: " + allocatedWidth + "  extra:" + extraCellWidth);
+		for(int col = model.getColumnCount()-1; col >= 0; col--) {
+			widths[col] += extraCellWidth;	
+		}
+		widths[widths.length-1] += remainingWidth;
+		
+		return widths;
+	}
 
 
 
