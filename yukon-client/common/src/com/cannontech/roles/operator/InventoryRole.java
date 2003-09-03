54bdaf12cfe64a098a0ece2628fe75f88d776aa5
Zhihong, Yao <>
2003-09-03 16:34:51 +0000

    Add InventoryRole

---
+++ yukon-client/common/src/com/cannontech/roles/operator/InventoryRole.java
@@ -0,0 +1,21 @@
+package com.cannontech.roles.operator;
+
+import com.cannontech.roles.*;
+
+/**
+ * @author yao
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of type comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+public interface InventoryRole {
+	public static final int ROLEID = OperatorRoleDefs.INVENTORY_ROLEID;
+	
+	public static final int INVENTORY_SHOW_ALL = OperatorRoleDefs.INVENTORY_PROPERTYID_BASE;
+	public static final int SN_ADD_RANGE = OperatorRoleDefs.INVENTORY_PROPERTYID_BASE - 1;
+	public static final int SN_UPDATE_RANGE = OperatorRoleDefs.INVENTORY_PROPERTYID_BASE - 2;
+	public static final int SN_CONFIG_RANGE = OperatorRoleDefs.INVENTORY_PROPERTYID_BASE - 3;
+
+}
