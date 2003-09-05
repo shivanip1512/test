ceabb2cca0880755c442a06a02562b377813330c
Neuharth, Ryan <>
2003-09-05 18:39:14 +0000

    Updates to the model will not cause it to collapse all nodes.

--- yukon-client/common/src/com/cannontech/database/model/CommChannelTreeModel.java
+++ yukon-client/common/src/com/cannontech/database/model/CommChannelTreeModel.java
@@ -3,13 +3,28 @@ package com.cannontech.database.model;
 /**
  * This type was created in VisualAge.
  */
+import javax.swing.tree.TreePath;
+
+import com.cannontech.common.util.CtiUtilities;
+import com.cannontech.database.cache.functions.PAOFuncs;
 import com.cannontech.database.data.lite.LiteBase;
+import com.cannontech.database.data.lite.LiteComparators;
+import com.cannontech.database.data.lite.LiteYukonPAObject;
+import com.cannontech.database.data.pao.PAOGroups;
 //This models has the following:
 //		1st Level = Ports YukonPAOBjects
 //		2nd Level = Devices YukonPAOBjects
 
 public class CommChannelTreeModel extends DBTreeModel 
 {
+	
+	//a mutable lite point used for comparisons
+	private static final LiteYukonPAObject DUMMY_LITE_PAO = 
+					new LiteYukonPAObject(Integer.MIN_VALUE, "**DUMMY**");
+
+	//a Vector only needed to store temporary things
+	private java.util.List tempList = new java.util.Vector(32);
+
 /**
  * PortModel constructor comment.
  * @param root javax.swing.tree.TreeNode
@@ -17,20 +32,7 @@ public class CommChannelTreeModel extends DBTreeModel
 public CommChannelTreeModel() {
 	super( new DBTreeNode("Comm Channels") );
 }
-/**
- * Insert the method's description here.
- * Creation date: (4/17/2002 1:58:45 PM)
- * @param lite com.cannontech.database.data.lite.LiteBase
- */
-public boolean insertTreeObject( LiteBase lb ) 
-{
-	if( lb == null || !isLiteTypeSupported(lb.getLiteType()) )
-		return false;
 
-	update();
-
-	return false;
-}
 /**
  * Insert the method's description here.
  * Creation date: (4/22/2002 2:05:03 PM)
@@ -80,19 +82,11 @@ public void update()
 			
 			for( int j = 0; j < devices.size(); j++ )
 			{
-				deviceClass = ((com.cannontech.database.data.lite.LiteYukonPAObject)devices.get(j)).getPaoClass();
+				LiteYukonPAObject liteYuk = (LiteYukonPAObject)devices.get(j);
 				
-				if( deviceClass == com.cannontech.database.data.pao.DeviceClasses.LOADMANAGEMENT 
-						|| deviceClass == com.cannontech.database.data.pao.DeviceClasses.IED 
-						|| deviceClass == com.cannontech.database.data.pao.DeviceClasses.METER 
-						|| deviceClass == com.cannontech.database.data.pao.DeviceClasses.RTU
-						|| deviceClass == com.cannontech.database.data.pao.DeviceClasses.TRANSMITTER
-						|| deviceClass == com.cannontech.database.data.pao.DeviceClasses.VIRTUAL
-						|| deviceClass == com.cannontech.database.data.pao.DeviceClasses.RTU
-						|| deviceClass == com.cannontech.database.data.pao.DeviceClasses.SYSTEM );
-
+				if( isDeviceValid(liteYuk.getCategory(), liteYuk.getPaoClass(), liteYuk.getType() ) )
 				{
-					devicePortID = ((com.cannontech.database.data.lite.LiteYukonPAObject)devices.get(j)).getPortID();
+					devicePortID = ((LiteYukonPAObject)devices.get(j)).getPortID();
 					if( devicePortID == portID )
 					{
 						devicesFound = true;
@@ -110,18 +104,168 @@ public void update()
 
 	reload();
 }
+
+
+
 /**
  * Insert the method's description here.
  * Creation date: (4/17/2002 1:58:45 PM)
  * @param lite com.cannontech.database.data.lite.LiteBase
  */
-public boolean updateTreeObject(LiteBase lb) 
+public boolean insertTreeObject( LiteBase lb ) 
 {
 	if( lb == null || !isLiteTypeSupported(lb.getLiteType()) )
 		return false;
 
-	update();
+	DBTreeNode rootNode = (DBTreeNode) getRoot();
 
+	if( lb instanceof LiteYukonPAObject 
+		 && ((LiteYukonPAObject)lb).getPortID() > PAOGroups.INVALID )
+	{
+		int devID = ((LiteYukonPAObject)lb).getPortID();
+
+		rootNode = findLiteObject( null, 
+				PAOFuncs.getLiteYukonPAO(devID) );
+
+		if( rootNode != null )
+		{
+
+			//this will force us to reload ALL the children for this PAObject
+			rootNode.setWillHaveChildren(true);
+			TreePath rootPath = new TreePath( rootNode );
+			treePathWillExpand( rootPath );
+
+			updateTreeNodeStructure( rootNode );
+
+			return true;
+		}
+	}
+	else if( lb instanceof LiteYukonPAObject )
+	{
+		LiteYukonPAObject liteYuk = (LiteYukonPAObject)lb;
+
+		if( isDeviceValid(liteYuk.getCategory(), liteYuk.getPaoClass(), liteYuk.getType() ) )
+		{
+			DBTreeNode node = new DBTreeNode(lb);
+
+			//add all new tree nodes to the top, for now
+			int[] ind = { 0 };
+			
+			rootNode.insert( node, ind[0] );
+			
+			nodesWereInserted(
+				rootNode,
+				ind );
+
+			return true;
+		}
+
+	}
+	
 	return false;
 }
+
+/**
+ * Insert the method's description here.
+ * Creation date: (4/22/2002 4:11:23 PM)
+ * @param deviceType int
+ */
+public boolean isDeviceValid( int category_, int deviceClass, int type_ )
+{
+	return( deviceClass == com.cannontech.database.data.pao.DeviceClasses.LOADMANAGEMENT 
+			|| deviceClass == com.cannontech.database.data.pao.DeviceClasses.IED 
+			|| deviceClass == com.cannontech.database.data.pao.DeviceClasses.METER 
+			|| deviceClass == com.cannontech.database.data.pao.DeviceClasses.RTU
+			|| deviceClass == com.cannontech.database.data.pao.DeviceClasses.TRANSMITTER
+			|| deviceClass == com.cannontech.database.data.pao.DeviceClasses.VIRTUAL
+			|| deviceClass == com.cannontech.database.data.pao.DeviceClasses.RTU
+			|| deviceClass == com.cannontech.database.data.pao.DeviceClasses.SYSTEM );
+
+}
+
+
+/**
+ * Insert the method's description here.
+ * Creation date: (2/27/2002 10:37:56 AM)
+ * @param points java.util.List
+ * @param destList java.util.Vector
+ */
+private boolean createPaoChildList(java.util.List paos_, java.util.List destList_ )
+{
+	//searches and sorts the list!
+	CtiUtilities.binarySearchRepetition( 
+					paos_,
+					DUMMY_LITE_PAO, //must have the needed PortID set!!
+					LiteComparators.litePaoPortIDComparator,
+					destList_ );
+						
+	for( int i = destList_.size()-1; i >= 0; i-- )
+	{
+		LiteYukonPAObject lp = (LiteYukonPAObject)destList_.get(i);
+	}
+
+	return destList_.size() > 0;
+}
+
+/**
+ * Insert the method's description here.
+ * Creation date: (4/25/2002 12:35:32 PM)
+ * @param path javax.swing.tree.TreePath
+ */
+public synchronized void treePathWillExpand(javax.swing.tree.TreePath path)
+{
+	//Watch out, this reloads the children every TIME!!!
+	DBTreeNode node = (DBTreeNode)path.getLastPathComponent();
+
+	if( node.willHaveChildren() &&
+		 node.getUserObject() instanceof LiteYukonPAObject )
+	{
+		com.cannontech.database.cache.DefaultDatabaseCache cache =
+			com.cannontech.database.cache.DefaultDatabaseCache.getInstance();
+
+		synchronized (cache)
+		{
+			int portID = ((LiteYukonPAObject)node.getUserObject()).getYukonID();
+			java.util.List paos = cache.getAllYukonPAObjects();
+
+			//change our dummy points device ID to the current DeviceID
+			DUMMY_LITE_PAO.setPortID( portID );
+			
+			//lock our point list down
+			synchronized( tempList )
+			{
+				node.removeAllChildren();
+				tempList.clear();
+				
+				//makes a list of points associated with the current deviceNode
+				createPaoChildList( paos, tempList );
+
+				//add all points and point types to the deviceNode
+				addPaos( node );
+			}
+		}
+	}
+
+	node.setWillHaveChildren(false);
+}
+
+
+
+/**
+ * Insert the method's description here.
+ * Creation date: (4/19/2002 1:35:08 PM)
+ * @param deviceNode DBTreeNode
+ */
+private void addPaos( DBTreeNode deviceNode )
+{
+	for (int j = 0; j < tempList.size(); j++)
+	{
+		deviceNode.add( new DBTreeNode(tempList.get(j)) );
+	}
+
+	//tempList is cleared
+	tempList.clear();
+}
+
+
 }
