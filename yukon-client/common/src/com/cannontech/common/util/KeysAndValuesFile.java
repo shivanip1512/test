36ef6fe34ab46e09f0bc70c9291026248b083ac7
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2003-09-05 16:42:50 +0000

    Added prompt parameter to file.  Replace prompt parse with an input text string from user.

--- yukon-client/common/src/com/cannontech/common/util/KeysAndValuesFile.java
+++ yukon-client/common/src/com/cannontech/common/util/KeysAndValuesFile.java
@@ -13,7 +13,9 @@ public class KeysAndValuesFile extends java.io.File
 	public static final String DEFAULT_FILENAME = "keyAndValue.dat";
 	public static final char DEFAULT_SEPARATOR = '=';
 	public static final char DEFAULT_COMMENT = '#';
-	public static final String DEFAULT_INCLUDE = "@include";
+	public static final String DEFAULT_INCLUDE = "@include";// represents that the following string is a file to be included.
+	public static final char DEFAULT_VALUE_PROMPT = '?'; //the chars that follow the question mark(?), may be grouped by quotes(" or '), is the value 
+														 //that will be displayed on a prompt.  The returned prompted value will replace the ?string value.
 	
 	private char separator = DEFAULT_SEPARATOR;
 	private char comment = DEFAULT_COMMENT;
@@ -272,6 +274,59 @@ public class KeysAndValuesFile extends java.io.File
 	public void setComment(char comment)
 	{
 		this.comment = comment;
+	}
+	
+	/** 
+	 * Replace the Default value prompt with the prompted input text.
+	 * @param valueString
+	 * @param parent
+	 * @return
+	 */
+	public static String loadPromptValue(String valueString, java.awt.Component parent )
+	{
+		int promptIndex = valueString.indexOf(DEFAULT_VALUE_PROMPT);
+		while (promptIndex > -1)
+		{
+			int endIndex = -1;
+			String promptString = valueString.substring(promptIndex+1).trim();	//clear all blanks at the begining of the prompt text
+			String stringEnding = "";
+			if( promptString.charAt(0) == '\'' || promptString.charAt(0) == '\"')//quoted prompt string
+			{
+				promptString = promptString.substring(1);	//remove quote from begining of strng
+				endIndex = promptString.indexOf('\'');	//locate ending quote
+				if( endIndex < 0 )
+					endIndex = promptString.indexOf('\"');
+			}
+			else
+			{
+				endIndex = promptString.indexOf(" ");	//locate string end, space is separator
+				
+				if( promptString.indexOf('\'') > 0 && (promptString.indexOf('\'')< endIndex || endIndex < 0))
+					endIndex = promptString.indexOf('\'');
+				else if( promptString.indexOf('\"') > 0 && (promptString.indexOf('\"')< endIndex || endIndex < 0))
+					endIndex = promptString.indexOf('\"');
+				
+				if( endIndex > 0)
+					stringEnding +=promptString.charAt(endIndex);	//add the char back to the begining of the command.
+			}
+				
+			if( endIndex > 0)
+			{
+				stringEnding += promptString.substring(endIndex+1);	//Store the end of the command string.
+				promptString = promptString.substring(0, endIndex);	//truncate the end of the string to get just the prompt value.
+			}
+				
+			String value = javax.swing.JOptionPane.showInputDialog(parent, promptString + ": ", "Enter the parameter value", javax.swing.JOptionPane.QUESTION_MESSAGE );
+				
+			if( value != null)
+			{
+				valueString = (valueString.substring(0, promptIndex) + value + stringEnding).trim();
+				promptIndex = valueString.trim().indexOf(DEFAULT_VALUE_PROMPT);	//look for another prompt value
+			}
+			else
+				return null;	//CANCEL
+		}
+		return valueString;
 	}
 
 }
