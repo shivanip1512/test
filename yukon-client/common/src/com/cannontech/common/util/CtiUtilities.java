798feb37730f2b94a419143ac7ad6b9e72a9944d
Neuharth, Ryan <>
2003-09-05 14:41:00 +0000

    Added a copyright string

--- yukon-client/common/src/com/cannontech/common/util/CtiUtilities.java
+++ yukon-client/common/src/com/cannontech/common/util/CtiUtilities.java
@@ -21,6 +21,9 @@ public final class CtiUtilities
 	public static final double INVALID_MIN_DOUBLE = -1E30;
 	public static final double INVALID_MAX_DOUBLE = 1E30;
 	
+	
+	public static final String COPYRIGHT = "Copyright (C)1999-2003 Cannon Technologies";
+	
 	public static final String USER_DIR = System.getProperty("user.dir") + "/";
 	//+ System.getProperty("file.seperator"); //for some reason, this is null in VA
 	
