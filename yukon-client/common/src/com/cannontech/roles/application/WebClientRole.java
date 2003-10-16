4cb20af8f9bfa048feb6f8beecb43d7da0b9babe
Zhihong, Yao <>
2003-10-16 15:50:35 +0000

    Changed property order so LOG_IN_URL has the same property id as previous LOG_OFF_URL property

--- yukon-client/common/src/com/cannontech/roles/application/WebClientRole.java
+++ yukon-client/common/src/com/cannontech/roles/application/WebClientRole.java
@@ -15,8 +15,9 @@ public interface WebClientRole {
 	public static final int NAV_BULLET_SELECTED = ApplicationRoleDefs.WEB_CLIENT_PROPERTYID_BASE - 3;	//Bullet.gif
 	public static final int NAV_BULLET_EXPAND = ApplicationRoleDefs.WEB_CLIENT_PROPERTYID_BASE - 4;	//BulletExpand.gif
 	public static final int HEADER_LOGO = ApplicationRoleDefs.WEB_CLIENT_PROPERTYID_BASE - 5;
-	public static final int NAV_CONNECTOR_MIDDLE = ApplicationRoleDefs.WEB_CLIENT_PROPERTYID_BASE - 6;	// MidConnector.gif
-	public static final int NAV_CONNECTOR_BOTTOM = ApplicationRoleDefs.WEB_CLIENT_PROPERTYID_BASE - 7;	// BottomConnector.gif
 	
-	public static final int LOG_IN_URL = ApplicationRoleDefs.WEB_CLIENT_PROPERTYID_BASE - 8;
+	public static final int LOG_IN_URL = ApplicationRoleDefs.WEB_CLIENT_PROPERTYID_BASE - 6;
+	
+	public static final int NAV_CONNECTOR_BOTTOM = ApplicationRoleDefs.WEB_CLIENT_PROPERTYID_BASE - 7;	// BottomConnector.gif
+	public static final int NAV_CONNECTOR_MIDDLE = ApplicationRoleDefs.WEB_CLIENT_PROPERTYID_BASE - 8;	// MidConnector.gif
 }
