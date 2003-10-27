b5e99752c8bc716ee3163b3f9921020765fa87cf
Fisher, Matthew S <MatthewSFisher@Eaton.com>
2003-10-27 22:05:50 +0000

    Initial revision

---
+++ yukon-server/RTDB/dev_mct410.cpp
@@ -0,0 +1,430 @@
+/*-----------------------------------------------------------------------------*
+*
+* File:   dev_mct310
+*
+* Date:   4/24/2001
+*
+* Author: Corey G. Plender
+*
+* PVCS KEYWORDS:
+* ARCHIVE      :  $Archive:   Z:/SOFTWAREARCHIVES/YUKON/RTDB/dev_mct310.cpp-arc  $
+* REVISION     :  $Revision: 1.2 $
+* DATE         :  $Date: 2003/10/27 22:05:50 $
+*
+* Copyright (c) 1999, 2000 Cannon Technologies Inc. All rights reserved.
+*-----------------------------------------------------------------------------*/
+
+#pragma warning( disable : 4786)
+
+
+#include <windows.h>
+#include "device.h"
+#include "devicetypes.h"
+#include "dev_mct410.h"
+#include "logger.h"
+#include "mgr_point.h"
+#include "numstr.h"
+#include "porter.h"
+#include "utility.h"
+
+set< CtiDLCCommandStore > CtiDeviceMCT410::_commandStore;
+
+CtiDeviceMCT410::CtiDeviceMCT410( )  {  }
+
+CtiDeviceMCT410::CtiDeviceMCT410( const CtiDeviceMCT410 &aRef )
+{
+   *this = aRef;
+}
+
+CtiDeviceMCT410::~CtiDeviceMCT410( ) { }
+
+CtiDeviceMCT410 &CtiDeviceMCT410::operator=( const CtiDeviceMCT410 &aRef )
+{
+   if( this != &aRef )
+   {
+      Inherited::operator=( aRef );
+   }
+
+   return *this;
+}
+
+
+bool CtiDeviceMCT410::initCommandStore( )
+{
+    bool failed = false;
+
+    CtiDLCCommandStore cs;
+
+    cs._cmd     = CtiProtocolEmetcon::Scan_Accum;
+    cs._io      = IO_FCT_READ;
+    cs._funcLen = make_pair( (int)MCT410_FuncReadMReadPos,
+                             (int)MCT410_FuncReadMReadLen );
+    _commandStore.insert( cs );
+
+    cs._cmd     = CtiProtocolEmetcon::GetValue_Default;
+    cs._io      = IO_FCT_READ;
+    cs._funcLen = make_pair( (int)MCT410_FuncReadMReadPos,
+                             (int)MCT410_FuncReadMReadLen );
+    _commandStore.insert( cs );
+
+    cs._cmd      = CtiProtocolEmetcon::Scan_Integrity;
+    cs._io       = IO_FCT_READ;
+    cs._funcLen  = make_pair( (int)MCT410_FuncReadDemandPos,
+                              (int)MCT410_FuncReadDemandLen );
+    _commandStore.insert( cs );
+
+    cs._cmd      = CtiProtocolEmetcon::GetValue_Demand;
+    cs._io       = IO_FCT_READ;
+    cs._funcLen  = make_pair( (int)MCT410_FuncReadDemandPos,
+                              (int)MCT410_FuncReadDemandLen );
+    _commandStore.insert( cs );
+
+    cs._cmd     = CtiProtocolEmetcon::GetStatus_Internal;
+    cs._io      = IO_READ;
+    cs._funcLen = make_pair( (int)MCT410_StatusLen,
+                             (int)MCT410_StatusPos );
+    _commandStore.insert( cs );
+
+    cs._cmd     = CtiProtocolEmetcon::GetValue_PFCount;
+    cs._io      = IO_READ;
+    cs._funcLen = make_pair( (int)MCT410_PowerfailCountPos,
+                             (int)MCT410_PowerfailCountLen );
+    _commandStore.insert( cs );
+
+    return failed;
+}
+
+
+bool CtiDeviceMCT410::getOperation( const UINT &cmd, USHORT &function, USHORT &length, USHORT &io )
+{
+    bool found = false;
+
+    if( _commandStore.empty( ) )  // Must initialize!
+    {
+        CtiDeviceMCT410::initCommandStore( );
+    }
+
+    DLCCommandSet::iterator itr = _commandStore.find( CtiDLCCommandStore( cmd ) );
+
+    if( itr != _commandStore.end( ) )
+    {
+        CtiDLCCommandStore &cs = *itr;
+        function = cs._funcLen.first;   //  Copy the relevant bits from the commandStore
+        length   = cs._funcLen.second;  //
+        io       = cs._io;              //
+
+        found = true;
+    }
+    else                                //  Look in the parent if not found in the child!
+    {
+        found = Inherited::getOperation( cmd, function, length, io );
+    }
+
+    return found;
+}
+
+
+/*
+ *  ResultDecode MUST decode all CtiDLCCommand_t which are defined in the initCommandStore object.  The only exception to this
+ *  would be a child whose decode was identical to the parent, but whose request was done differently..
+ *  This MAY be the case for example in an IED scan.
+ */
+INT CtiDeviceMCT410::ResultDecode(INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList)
+{
+    INT status = NORMAL;
+
+    switch(InMessage->Sequence)
+    {
+        case (CtiProtocolEmetcon::Scan_Accum):
+        case (CtiProtocolEmetcon::GetValue_Default):
+        {
+            status = decodeGetValueKWH(InMessage, TimeNow, vgList, retList, outList);
+            break;
+        }
+
+        case (CtiProtocolEmetcon::Scan_Integrity):
+        case (CtiProtocolEmetcon::GetValue_Demand):
+        {
+            status = decodeGetValueDemand(InMessage, TimeNow, vgList, retList, outList);
+            break;
+        }
+
+        case (CtiProtocolEmetcon::GetStatus_Internal):
+        {
+            status = decodeGetStatusInternal(InMessage, TimeNow, vgList, retList, outList);
+            break;
+        }
+
+        case (CtiProtocolEmetcon::GetConfig_Model):
+        {
+            status = decodeGetConfigModel(InMessage, TimeNow, vgList, retList, outList);
+            break;
+        }
+
+        case (CtiProtocolEmetcon::GetValue_PFCount):
+        default:
+        {
+            status = Inherited::ResultDecode(InMessage, TimeNow, vgList, retList, outList);
+
+            if(status != NORMAL)
+            {
+                CtiLockGuard<CtiLogger> doubt_guard(dout);
+                dout << RWTime() << " **** Checkpoint **** " << __FILE__ << " (" << __LINE__ << ")" << endl;
+                dout << " IM->Sequence = " << InMessage->Sequence << " " << getName() << endl;
+            }
+            break;
+        }
+    }
+
+
+    return status;
+}
+
+
+INT CtiDeviceMCT410::decodeGetValueKWH(INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList)
+{
+    INT status = NORMAL;
+    ULONG i,x;
+    INT pid;
+    RWCString resultString;
+
+    INT ErrReturn  = InMessage->EventCode & 0x3fff;
+    DSTRUCT *DSt   = &InMessage->Buffer.DSt;
+
+    DOUBLE Value;
+    CtiPointBase         *pPoint = NULL;
+    CtiReturnMsg         *ReturnMsg = NULL;    // Message sent to VanGogh, inherits from Multi
+    CtiPointDataMsg      *pData = NULL;
+
+    {
+        CtiLockGuard<CtiLogger> doubt_guard(dout);
+        dout << RWTime() << " **** Accumulator Decode for \"" << getName() << "\" **** " << __FILE__ << " (" << __LINE__ << ")" << endl;
+    }
+
+    ULONG RecentValue = 0;
+    USHORT TempDevType;
+
+    //  ACH:  are these necessary?  /mskf
+    resetScanFreezePending();
+    resetScanFreezeFailed();
+
+
+    if(!(status = decodeCheckErrorReturn(InMessage, retList, outList)))
+    {
+        // No error occured, we must do a real decode!
+
+        if((ReturnMsg = CTIDBG_new CtiReturnMsg(getID(), InMessage->Return.CommandStr)) == NULL)
+        {
+            CtiLockGuard<CtiLogger> doubt_guard(dout);
+            dout << RWTime() << " Could NOT allocate memory " << __FILE__ << " (" << __LINE__ << ") " << endl;
+
+            return MEMORY;
+        }
+
+        ReturnMsg->setUserMessageId(InMessage->Return.UserID);
+
+        pPoint = getDevicePointOffsetTypeEqual( 1, PulseAccumulatorPointType );
+
+        RecentValue = MAKEULONG(MAKEUSHORT(DSt->Message[2], DSt->Message[1]), (USHORT)(DSt->Message[0]));
+
+        // handle accumulator data here
+        if( pPoint != NULL)
+        {
+            // 24 bit pulse value
+            Value = ((CtiPointNumeric *)pPoint)->computeValueForUOM( RecentValue );
+            RWTime pointTime;
+
+            resultString = getName() + " / " + pPoint->getName() + " = " + CtiNumStr(Value,
+                                                                                     ((CtiPointNumeric *)pPoint)->getPointUnits().getDecimalPlaces());
+            pData = CTIDBG_new CtiPointDataMsg(pPoint->getPointID(), Value, NormalQuality, PulseAccumulatorPointType, resultString);
+
+            if(pData != NULL)
+            {
+                pointTime -= pointTime.seconds() % 300;
+                pData->setTime( pointTime );
+                ReturnMsg->PointData().insert(pData);
+                pData = NULL;  // We just put it on the list...
+            }
+        }
+        else
+        {
+            resultString = getName() + " / KYZ 1 = " + CtiNumStr(RecentValue) + "  --  POINT UNDEFINED IN DB";
+            ReturnMsg->setResultString(resultString);
+        }
+    }
+
+    retMsgHandler( InMessage->Return.CommandStr, status, ReturnMsg, vgList, retList );
+
+    return status;
+}
+
+
+INT CtiDeviceMCT410::decodeGetValueDemand(INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList)
+{
+    int       status = NORMAL;
+    double    Value;
+    RWCString resultString;
+
+    INT ErrReturn  = InMessage->EventCode & 0x3fff;
+    DSTRUCT *DSt   = &InMessage->Buffer.DSt;
+
+    CtiPointBase         *pPoint = NULL;
+    CtiReturnMsg         *ReturnMsg = NULL;    // Message sent to VanGogh, inherits from Multi
+    CtiPointDataMsg      *pData = NULL;
+
+    {
+        CtiLockGuard<CtiLogger> doubt_guard(dout);
+        dout << RWTime() << " **** Demand Decode for \"" << getName() << "\" **** " << __FILE__ << " (" << __LINE__ << ")" << endl;
+    }
+
+
+    resetScanPending();
+
+    if(!(status = decodeCheckErrorReturn(InMessage, retList, outList)))
+    {
+        // No error occured, we must do a real decode!
+
+        if((ReturnMsg = CTIDBG_new CtiReturnMsg(getID(), InMessage->Return.CommandStr)) == NULL)
+        {
+            CtiLockGuard<CtiLogger> doubt_guard(dout);
+            dout << RWTime() << " Could NOT allocate memory " << __FILE__ << " (" << __LINE__ << ") " << endl;
+
+            return MEMORY;
+        }
+
+        ReturnMsg->setUserMessageId(InMessage->Return.UserID);
+
+        // 2 byte demand value.  Upper 2 bits are error indicators.
+        Value = MAKEUSHORT(DSt->Message[2], (DSt->Message[1] & 0x3f) );
+
+        //  turn raw pulses into a demand reading
+        Value *= DOUBLE(3600 / getDemandInterval());
+
+        // look for first defined DEMAND accumulator
+        pPoint = getDevicePointOffsetTypeEqual( 1, DemandAccumulatorPointType );
+
+        if( pPoint != NULL)
+        {
+            RWRecursiveLock<RWMutexLock>::LockGuard pGuard( pPoint->getMux() );
+            RWTime pointTime;
+
+            Value = ((CtiPointNumeric*)pPoint)->computeValueForUOM(Value);
+
+            resultString = getName() + " / " + pPoint->getName() + " = " + CtiNumStr(Value,
+                                                                                     ((CtiPointNumeric *)pPoint)->getPointUnits().getDecimalPlaces());
+
+            pData = CTIDBG_new CtiPointDataMsg(pPoint->getPointID(), Value, NormalQuality, DemandAccumulatorPointType, resultString);
+
+            if(pData != NULL)
+            {
+                pointTime -= pointTime.seconds() % getDemandInterval();
+                pData->setTime( pointTime );
+                ReturnMsg->PointData().insert(pData);
+                pData = NULL;  // We just put it on the list...
+            }
+        }
+        else
+        {
+            resultString = getName() + " / Demand = " + CtiNumStr(Value) + "  --  POINT UNDEFINED IN DB";
+            ReturnMsg->setResultString(resultString);
+        }
+    }
+
+    retMsgHandler( InMessage->Return.CommandStr, status, ReturnMsg, vgList, retList );
+
+    return status;
+}
+
+
+INT CtiDeviceMCT410::decodeGetStatusInternal( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList )
+{
+    INT status = NORMAL;
+
+    INT ErrReturn  = InMessage->EventCode & 0x3fff;
+    unsigned char *geneBuf = InMessage->Buffer.DSt.Message;
+
+    if(!(status = decodeCheckErrorReturn(InMessage, retList, outList)))
+    {
+        // No error occured, we must do a real decode!
+
+        RWCString resultString;
+
+        CtiReturnMsg         *ReturnMsg = NULL;    // Message sent to VanGogh, inherits from Multi
+        CtiPointDataMsg      *pData = NULL;
+
+        if((ReturnMsg = CTIDBG_new CtiReturnMsg(getID(), InMessage->Return.CommandStr)) == NULL)
+        {
+            CtiLockGuard<CtiLogger> doubt_guard(dout);
+            dout << RWTime() << " Could NOT allocate memory " << __FILE__ << " (" << __LINE__ << ") " << endl;
+
+            return MEMORY;
+        }
+
+        ReturnMsg->setUserMessageId(InMessage->Return.UserID);
+
+        resultString  = getName() + " / Internal Status:\n";
+
+        resultString += "  Real Time Load A Status: " + (geneBuf[0] & 0x80)?"Closed":"Tripped" + RWCString("\n");
+        resultString += "  Real Time Load B Status: " + (geneBuf[0] & 0x40)?"Closed":"Tripped" + RWCString("\n");
+
+        resultString += "  Address mode: ";
+
+        switch( geneBuf[0] & 0x03 )
+        {
+            case 0:     resultString += "All valid addresses\n";    break;
+            case 1:     resultString += "Unique and FCT only\n";    break;
+            case 2:     resultString += "FCT only\n";               break;
+            default:    resultString += "Undefined\n";              break;
+        }
+
+        ReturnMsg->setResultString(resultString);
+
+        retMsgHandler( InMessage->Return.CommandStr, status, ReturnMsg, vgList, retList );
+    }
+
+    return status;
+}
+
+
+INT CtiDeviceMCT410::decodeGetConfigModel(INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList)
+{
+    INT status = NORMAL;
+
+    INT ErrReturn  = InMessage->EventCode & 0x3fff;
+    DSTRUCT *DSt   = &InMessage->Buffer.DSt;
+
+    if(!(status = decodeCheckErrorReturn(InMessage, retList, outList)))
+    {
+        // No error occured, we must do a real decode!
+
+        RWCString sspec;
+        RWCString options;
+        int  ssp;
+        char rev;
+        CtiReturnMsg *ReturnMsg = NULL;    // Message sent to VanGogh, inherits from Multi
+
+
+        ssp  = InMessage->Buffer.DSt.Message[0];
+        ssp |= InMessage->Buffer.DSt.Message[4] << 8;
+
+        rev  = 'A' + InMessage->Buffer.DSt.Message[1] - 1;
+
+        sspec = "\nSoftware Specification " + CtiNumStr(ssp) + "  Rom Revision " + RWCString(rev) + "\n";
+
+        if((ReturnMsg = CTIDBG_new CtiReturnMsg(getID(), InMessage->Return.CommandStr)) == NULL)
+        {
+            CtiLockGuard<CtiLogger> doubt_guard(dout);
+            dout << RWTime() << " Could NOT allocate memory " << __FILE__ << " (" << __LINE__ << ") " << endl;
+
+            return MEMORY;
+        }
+
+        ReturnMsg->setUserMessageId(InMessage->Return.UserID);
+        ReturnMsg->setResultString( sspec + options );
+
+        retMsgHandler( InMessage->Return.CommandStr, status, ReturnMsg, vgList, retList );
+    }
+
+    return status;
+}
+
