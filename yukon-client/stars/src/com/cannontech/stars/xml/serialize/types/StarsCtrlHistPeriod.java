e5aa99889d06d0da369cdcd90bfff19c37a531ca
Zhihong, Yao <>
2003-10-28 16:27:34 +0000

    *** empty log message ***

--- yukon-client/stars/src/com/cannontech/stars/xml/serialize/types/StarsCtrlHistPeriod.java
+++ yukon-client/stars/src/com/cannontech/stars/xml/serialize/types/StarsCtrlHistPeriod.java
@@ -2,7 +2,7 @@
  * This class was automatically generated with 
  * <a href="http://castor.exolab.org">Castor 0.9.3.9+</a>, using an
  * XML Schema.
- * $Id: StarsCtrlHistPeriod.java,v 1.54 2003/10/17 21:02:00 zyao Exp $
+ * $Id: StarsCtrlHistPeriod.java,v 1.55 2003/10/28 16:26:59 zyao Exp $
  */
 
 package com.cannontech.stars.xml.serialize.types;
@@ -19,7 +19,7 @@ import org.exolab.castor.xml.*;
 /**
  * 
  * 
- * @version $Revision: 1.54 $ $Date: 2003/10/17 21:02:00 $
+ * @version $Revision: 1.55 $ $Date: 2003/10/28 16:26:59 $
 **/
 public class StarsCtrlHistPeriod implements java.io.Serializable {
 
--- yukon-client/stars/src/com/cannontech/stars/xml/serialize/types/StarsSearchByType.java
+++ yukon-client/stars/src/com/cannontech/stars/xml/serialize/types/StarsSearchByType.java
@@ -2,7 +2,7 @@
  * This class was automatically generated with 
  * <a href="http://castor.exolab.org">Castor 0.9.3.9+</a>, using an
  * XML Schema.
- * $Id: StarsSearchByType.java,v 1.54 2003/10/17 21:02:00 zyao Exp $
+ * $Id: StarsSearchByType.java,v 1.55 2003/10/28 16:27:00 zyao Exp $
  */
 
 package com.cannontech.stars.xml.serialize.types;
@@ -19,7 +19,7 @@ import org.exolab.castor.xml.*;
 /**
  * 
  * 
- * @version $Revision: 1.54 $ $Date: 2003/10/17 21:02:00 $
+ * @version $Revision: 1.55 $ $Date: 2003/10/28 16:27:00 $
 **/
 public class StarsSearchByType implements java.io.Serializable {
 
