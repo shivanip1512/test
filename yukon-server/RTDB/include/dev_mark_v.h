9e93eda0aee39a80c25b3db5a6f87abeee11ba12
Schmit, Eric <>
2003-10-30 14:56:20 +0000

    transdata billing **works**

--- yukon-server/RTDB/include/dev_mark_v.h
+++ yukon-server/RTDB/include/dev_mark_v.h
@@ -14,11 +14,14 @@
 *
 * PVCS KEYWORDS:
 * ARCHIVE      :  $Archive$
-* REVISION     :  $Revision: 1.1 $
-* DATE         :  $Date: 2003/08/06 19:49:52 $
+* REVISION     :  $Revision: 1.2 $
+* DATE         :  $Date: 2003/10/30 14:56:20 $
 *
 * Copyright (c) 1999, 2000, 2001, 2002 Cannon Technologies Inc. All rights reserved.
 *----------------------------------------------------------------------------------*/
+
+#include <rw/rwtime.h>
+#include <rw/rwdate.h>
 
 #include "dev_meter.h"
 #include "dlldefs.h"
@@ -28,14 +31,46 @@
 #include "types.h"
 #include "mgr_point.h"
 #include "device.h"
+#include "pt_numeric.h"
 
 class IM_EX_DEVDB CtiDeviceMarkV : public CtiDeviceMeter
 {
+   enum
+   {
+      TOTAL_USAGE      = 1,
+      CURRENT_DEMAND,
+      PEAK_DEMAND,
+      PEAK_TIME,
+      PEAK_DATE,
+      RATEA_USAGE,
+      RATEB_USAGE,
+      RATEC_USAGE,
+      RATED_USAGE,
+      RATEA_PEAK_DEMAND,
+      RATEB_PEAK_DEMAND,
+      RATEC_PEAK_DEMAND,
+      RATED_PEAK_DEMAND,
+      RATEA_PEAK_TIME,
+      RATEB_PEAK_TIME,
+      RATEC_PEAK_TIME,
+      RATED_PEAK_TIME,
+      RATEA_PEAK_DATE,
+      RATEB_PEAK_DATE,
+      RATEC_PEAK_DATE,
+      RATED_PEAK_DATE,
+      PREVIOUS_DEMAND,
+      CH1_OFFSET       = 200,
+      CH2_OFFSET       = 250,
+      CH3_OFFSET       = 300,
+      CH4_OFFSET       = 350
+   };
+
 protected:
 
 private:
 
-   CtiProtocolTransdata    _transdataProtocol;
+   CtiProtocolTransdata          _transdataProtocol;
+   CtiTransdataData              *_converted;
 
 public:
 
@@ -76,10 +111,23 @@ public:
                            RWTPtrSlist< CtiMessage >  &retList,
                            RWTPtrSlist< OUTMESS >     &outList);
 
+   int decodeResultScan( INMESS                    *InMessage,
+                          RWTime                    &TimeNow,
+                          RWTPtrSlist< CtiMessage > &vgList,
+                          RWTPtrSlist< CtiMessage > &retList,
+                          vector<CtiTransdataData *> transVector );
+
    CtiProtocolTransdata & getProtocol( void );
+   RWTime getMsgTime( int timeID, int dateID, vector<CtiTransdataData *> transVector );
+   CtiPointDataMsg* fillPDMsg( vector<CtiTransdataData *> transVector,
+                               CtiPointBase *point, 
+                               int index,
+                               int timeID,
+                               int dateID );
+
 //   void DecodeDatabaseReader( RWDBReader &rdr );
 
 
 };
 
-#endif // #ifndef __DEV_MARK_V_H__
+#endif // #ifndef __DEV_H__
