c08c0c4b0cbac18883ea1e4ea06c89d4ddc8ea94
Neuharth, Ryan <>
2003-12-04 23:44:25 +0000

    initial

---
+++ yukon-client/notification/src/com/cannontech/notif/server/NotifMsgHandler.java
@@ -0,0 +1,130 @@
+package com.cannontech.notif.server;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import com.cannontech.clientutils.CTILogger;
+import com.cannontech.common.util.CtiUtilities;
+import com.cannontech.database.cache.functions.NotificationGroupFuncs;
+import com.cannontech.database.data.lite.LiteNotificationGroup;
+import com.cannontech.message.dispatch.message.Multi;
+import com.cannontech.message.util.MessageEvent;
+import com.cannontech.message.util.MessageListener;
+import com.cannontech.notif.message.NotifEmailMsg;
+import com.cannontech.notif.message.NotifResultMsg;
+import com.cannontech.tools.email.EmailMessage;
+
+/**
+ * @author rneuharth
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class NotifMsgHandler implements Observer, MessageListener
+{
+	public NotifMsgHandler()
+	{
+		super();
+	}
+	
+	public void messageReceived( MessageEvent e )
+	{		
+		//do the stuff here
+		handleMessage( e.getMessage() );
+	}	
+
+
+
+	private NotifResultMsg handleMessage( Object msg_ )
+	{
+		if( msg_ instanceof Multi )
+		{
+			for( int i = 0; i < ((Multi)msg_).getVector().size(); i++ )
+			{
+				handleMessage( ((Multi)msg_).getVector().get(i) );
+			}
+			
+			return new NotifResultMsg("SUCCESS: Multi message handled", NotifResultMsg.RESULT_SUCCESS);
+		}
+		else if( msg_ instanceof NotifEmailMsg )
+		{
+			CTILogger.debug(
+				" Notification server got an email message from: " + ((NotifEmailMsg)msg_).getSource() );
+
+			return doEmail( (NotifEmailMsg)msg_ );
+		}
+		else
+		{
+			CTILogger.debug(
+				" ERROR message received that is of an unknown type: " + msg_.getClass().getName() );
+
+			return new NotifResultMsg(
+				"ERROR: The message was an invalid notification mesage", NotifResultMsg.RESULT_FAILED);
+		}
+
+	}
+
+
+	private NotifResultMsg doEmail( NotifEmailMsg msg_ )
+	{
+		try
+		{
+			EmailMessage outMsg = new EmailMessage( msg_.getTo(), msg_.getSubject(), msg_.getBody() );
+			
+			//if we hav a NotificationGroupID, use its values instead
+			if( msg_.getNotifGroupID() != CtiUtilities.NONE_ID )
+			{
+				LiteNotificationGroup lGrp =
+						NotificationGroupFuncs.getLiteNotificationGroup(msg_.getNotifGroupID());
+						
+				if( lGrp == null )
+					return new NotifResultMsg(
+							"FAILURE: Email attempt to a a non-existant notification, GroupID= " + 
+							msg_.getNotifGroupID(), 
+							NotifResultMsg.RESULT_FAILED);
+
+				if( lGrp.isDisabled() )
+					return new NotifResultMsg(
+							"FAILURE: Email attempt to a DISABLED notification group has been blocked (" + 
+							lGrp.getNotificationGroupName() + ")", 
+							NotifResultMsg.RESULT_FAILED);
+
+				outMsg.setTo( NotificationGroupFuncs.getNotifEmailsByLiteGroup(lGrp) );
+				outMsg.setSubject( lGrp.getEmailSubject() );
+				outMsg.setFrom( lGrp.getEmailFrom() );
+				outMsg.setBody( lGrp.getEmailBody() );
+			}
+
+
+			outMsg.setTo_CC( msg_.getTo_CC() );
+			outMsg.setTo_BCC( msg_.getTo_BCC() );
+			
+			for( int i = 0; i < msg_.getAttachments().size(); i++ )
+			{
+				outMsg.getAttachmentNames().add( msg_.getAttachmentAt( i ).getFileName() );
+				outMsg.getAttachments().add( msg_.getAttachmentAt( i ).getFileContents() );
+			}
+			
+			outMsg.send();
+			
+			return new NotifResultMsg("SUCCESS: Email message sent", NotifResultMsg.RESULT_SUCCESS);
+		}
+		catch( Exception e )
+		{
+			CTILogger.error( e.getMessage(), e );
+
+			return new NotifResultMsg(
+				"FAILURE: Email message was not sent for the following reason: " + 
+				e.getMessage(), 
+				NotifResultMsg.RESULT_FAILED);
+		}
+		
+	}
+
+	public void update( Observable obs_, Object obj_ )
+	{
+		//do nothing
+
+	}
+
+}
---
+++ yukon-client/notification/src/com/cannontech/notif/server/NotificationServer.java
@@ -0,0 +1,230 @@
+package com.cannontech.notif.server;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import com.cannontech.clientutils.CTILogger;
+import com.cannontech.database.cache.DefaultDatabaseCache;
+import com.cannontech.database.cache.GenericDBCacheHandler;
+
+/**
+ * @author rneuharth
+ *
+ * The server used for accepting and creating notification messages.
+ *
+ */
+public class NotificationServer implements Runnable
+{
+	// The port the web server listens on
+	private int port = 1515;
+
+	// The interface to bind to
+	private InetAddress bindAddress;
+
+	// The serverSocket listen queue depth
+	private int backlog = 50;
+
+	// The servers listening socket
+	private ServerSocket server = null;
+
+	// The thread accept any incoming connections
+	private Thread acceptThread = null;
+	
+
+	//since this guy uses the cache, he better care about DBUpdates
+	private GenericDBCacheHandler dbCacheHandler = null;
+
+	// Total number of connections to the server made
+	private long connsMade = 0;
+
+
+	/**
+	 * 
+	 */
+	public NotificationServer()
+	{
+		super();
+		setBindAddress( "127.0.0.1" );		
+	}
+
+	/**
+	 * @return
+	 */
+	public int getBacklog()
+	{
+		return backlog;
+	}
+
+	/**
+	 * @return
+	 */
+	public int getPort()
+	{
+		return port;
+	}
+
+	/**
+	 * @param i
+	 */
+	public void setBacklog(int i)
+	{
+		if( i <= 0 )
+			i = 50;
+		backlog = i;
+	}
+
+	/**
+	 * @param i
+	 */
+	public void setPort(int i)
+	{
+		port = i;
+	}
+
+	public String getBindAddress()
+	{
+		String address = null;
+		if( bindAddress != null )
+			address = bindAddress.getHostAddress();
+
+		return address;
+	}
+
+	public void setBindAddress(String host)
+	{
+		try
+		{
+			if (host != null)
+				bindAddress = InetAddress.getByName(host);
+		}
+		catch(UnknownHostException e)
+		{
+			String msg = "Invalid host address specified: " + host;
+			CTILogger.error( msg, e );
+		}
+	}
+
+	/**
+	 * Start the notification server on port and begin listening for requests.
+	 */
+	public void start() throws IOException
+	{
+		try
+		{
+			server = new ServerSocket( 
+					getPort(), 
+					getBacklog(), 
+					bindAddress );
+
+			CTILogger.info("Started Notification server: " + server);
+
+
+			dbCacheHandler = new GenericDBCacheHandler("NotificationServer");
+			DefaultDatabaseCache.getInstance().addDBChangeListener( dbCacheHandler );
+
+			acceptThread = new Thread( this, "NotifListener" );
+			acceptThread.start();
+		}
+		catch (IOException e)
+		{
+			throw e;
+		}
+	}
+
+
+	/** 
+	 * Close the notification server listening socket
+	 */
+	public void stop()
+	{
+		try
+		{
+			dbCacheHandler.getClientConnection().disconnect();
+			DefaultDatabaseCache.getInstance().removeDBChangeListener( dbCacheHandler );
+			
+			ServerSocket srv = server;
+			server = null;
+			srv.close();
+		}
+		catch (Exception e)
+		{}
+	}
+
+	public boolean isRunning()
+	{
+		return server != null;
+	}
+
+	private long getConnsMade()
+	{
+		return connsMade;
+	}
+
+	/** 
+	 * Listen threads entry point. Here we accept a client connection
+	 */
+	public void run()
+	{
+		for( ;; )
+		{
+			// Return if the server has been stopped
+			if (server == null)
+				return;
+
+
+			// Accept a connection
+			Socket socket = null;
+			try 
+			{
+				socket = server.accept();
+				connsMade++;
+			}
+			catch (IOException e) 
+			{
+				// If the server is not null meaning we were not stopped report the error
+				if( server != null )
+					CTILogger.error("Failed to accept connection", e);
+
+				server = null;
+				return;
+			}
+
+	
+			//we have a connection, pass it off to another thread to process it
+			try 
+			{
+				//start handling the message here
+				NotifServerConnection conn = new NotifServerConnection( socket );
+
+				conn.connectWithoutWait(); //passes control to another thread
+			}
+			catch (Exception ex)
+			{
+				CTILogger.error( "error handling socket connection", ex );
+			}
+		}
+				 
+	}
+	
+	
+	
+	public static void main( String[] argsv )
+	{
+		System.setProperty("cti.app.name", "Notification-Server");
+		NotificationServer ns = new NotificationServer();
+		
+		try 
+		{
+			ns.start();
+		}
+		catch( Throwable t )
+		{
+			t.printStackTrace();
+		}
+
+	}
+
+}
