e8bad2fc8f36fd4e29282da70603b3a908d5751f
Zhihong, Yao <>
2004-01-05 19:46:41 +0000

    Made class LiteCICustomer derive from LiteCustomer

--- yukon-client/common/src/com/cannontech/database/data/lite/LiteCICustomer.java
+++ yukon-client/common/src/com/cannontech/database/data/lite/LiteCICustomer.java
@@ -1,26 +1,16 @@
 package com.cannontech.database.data.lite;
 
-import java.util.Vector;
-
-import com.cannontech.database.cache.functions.ContactFuncs;
 import com.cannontech.database.db.customer.CICustomerBase;
-import com.cannontech.database.db.customer.Customer;
 
 /**
  * @author alauinger
  */
-public class LiteCICustomer extends LiteBase 
+public class LiteCICustomer extends LiteCustomer 
 {
 	private int mainAddressID = 0;
 	private String companyName = null;
-	private int primaryContactID = 0;
-	private String timeZone = null;
 	private double demandLevel = 0.0;
 	private double curtailAmount = 0.0;
-	
-	//non-persistent data, 
-	//contains com.cannontech.database.data.lite.LiteContact
-	private Vector additionalContacts = null;
 	
 	
 	private LiteCICustomer() 
@@ -34,6 +24,7 @@ public class LiteCICustomer extends LiteBase
 	
 	public LiteCICustomer( int id, String companyName_ ) 
 	{
+		super( id );
 		setLiteType(LiteTypes.CUSTOMER_CI);
 		setCustomerID( id );
 		setCompanyName( companyName_ );
@@ -80,22 +71,6 @@ public class LiteCICustomer extends LiteBase
 	}
 
 	/**
-	 * Returns the primaryContactID.
-	 * @return int
-	 */
-	public int getPrimaryContactID() {
-		return primaryContactID;
-	}
-
-	/**
-	 * Returns the timeZone.
-	 * @return String
-	 */
-	public String getTimeZone() {
-		return timeZone;
-	}
-
-	/**
 	 * Sets the companyName.
 	 * @param companyName The companyName to set
 	 */
@@ -136,22 +111,6 @@ public class LiteCICustomer extends LiteBase
 	}
 
 	/**
-	 * Sets the primaryContactID.
-	 * @param primaryContactID The primaryContactID to set
-	 */
-	public void setPrimaryContactID(int primaryContactID) {
-		this.primaryContactID = primaryContactID;
-	}
-
-	/**
-	 * Sets the timeZone.
-	 * @param timeZone The timeZone to set
-	 */
-	public void setTimeZone(String timeZone) {
-		this.timeZone = timeZone;
-	}
-
-	/**
 	 * This method was created by Cannon Technologies Inc.
 	 */
 	public String toString() 
@@ -161,58 +120,29 @@ public class LiteCICustomer extends LiteBase
 
 	public void retrieve( String dbalias )
 	{
+		super.retrieve( dbalias );
 		
 		try
 		{
-	      com.cannontech.database.SqlStatement stat = 
-	            new com.cannontech.database.SqlStatement(
-	             "select i.MainAddressID, i.CompanyName, " +
-	             "c.PrimaryContactID, c.TimeZone, i.CustomerDemandLevel, " +
-	             "i.CurtailAmount " +
-	             "from " + CICustomerBase.TABLE_NAME + " i, " +
-	             Customer.TABLE_NAME + " c " + 
-	             "where i.CustomerID = " + getLiteID() +
-	             " and c.CustomerID = " + getLiteID(),
-	             dbalias );
-	
+			com.cannontech.database.SqlStatement stat = 
+				new com.cannontech.database.SqlStatement(
+					"SELECT MainAddressID, CompanyName, " +
+					"CustomerDemandLevel, CurtailAmount " +
+					"FROM " + CICustomerBase.TABLE_NAME +
+					"WHERE CustomerID = " + getLiteID(),
+					dbalias );
+			
 			stat.execute();
-	
-	      if( stat.getRowCount() <= 0 )
-	         throw new IllegalStateException("Unable to find the Customer with CustomerID = " + getLiteID() );
-	
-	      Object[] objs = stat.getRow(0);
-	      
+			
+			if( stat.getRowCount() <= 0 )
+				throw new IllegalStateException("Unable to find the Customer with CustomerID = " + getLiteID() );
+			
+			Object[] objs = stat.getRow(0);
+			
 			setMainAddressID( ((java.math.BigDecimal) objs[0]).intValue() ); 			
 			setCompanyName( objs[1].toString() );	
-			setPrimaryContactID( ((java.math.BigDecimal) objs[2]).intValue() );
-			setTimeZone( objs[3].toString() );
 			setDemandLevel( Double.valueOf(objs[4].toString()).doubleValue() );
 			setCurtailAmount( Double.valueOf(objs[5].toString()).doubleValue() );
-			
-			
-			
-	      stat = new com.cannontech.database.SqlStatement(
-						"SELECT ca.ContactID " + 
-						"FROM CustomerAdditionalContact ca, " + 
-						CICustomerBase.TABLE_NAME + " ci " +
-						"WHERE " +
-						"ci.CustomerID=" + getCustomerID() + " " +
-						"AND ci.CustomerID=ca.CustomerID " +
-						"ORDER BY ci.CustomerID",
-						dbalias );
-			
-			stat.execute();
-			
-			getAdditionalContacts().removeAllElements();
-			
-			for( int i = 0; i < stat.getRowCount(); i++ )
-			{
-				//add the LiteContact to this CICustomer
-				getAdditionalContacts().add(
-					ContactFuncs.getContact(((java.math.BigDecimal) stat.getRow(i)[0]).intValue()) );
-
-			}
-
 		}
 		catch( Exception e )
 		{
@@ -220,18 +150,5 @@ public class LiteCICustomer extends LiteBase
 		}
 	
 	}
-	
-	/**
-	 * Returns the additionalContacts.
-	 * @return Vector
-	 */
-	public Vector getAdditionalContacts() 
-	{
-		if( additionalContacts == null )
-			additionalContacts = new Vector(10);
-
-		return additionalContacts;
-	}
-
 
 }
