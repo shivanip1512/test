13a07064a4edc611af6600e19ffaed22006b3a39
Zhihong, Yao <>
2004-01-28 20:54:54 +0000

    Increased length of the notes field of customerresidence table Added pumpsize field to applianceheatpump table

--- yukon-database/DBUpdates/oracle/stars/3.00-stars.sql
+++ yukon-database/DBUpdates/oracle/stars/3.00-stars.sql
@@ -6,3 +6,10 @@
 alter table InventoryBase modify Notes Varchar(500);
 alter table AccountSite modify PropertyNotes Varchar(300);
 alter table ApplianceBase modify Notes Varchar(300);
+alter table CustomerResidence modify Notes Varchar(300);
+
+alter table ApplianceHeatPump add PumpSizeID number;
+
+alter table ApplianceHeatPump
+   add constraint FK_AppHtPm_YkLst3 foreign key (PumpSizeID)
+      references YukonListEntry (EntryID);
--- yukon-database/DBUpdates/sqlserver/stars/3.00-stars.sql
+++ yukon-database/DBUpdates/sqlserver/stars/3.00-stars.sql
@@ -6,3 +6,11 @@
 alter table InventoryBase alter column Notes Varchar(500);
 alter table AccountSite alter column PropertyNotes Varchar(300);
 alter table ApplianceBase alter column Notes Varchar(300);
+alter table CustomerResidence alter column Notes Varchar(300);
+
+alter table ApplianceHeatPump add PumpSizeID numeric;
+
+alter table ApplianceHeatPump
+   add constraint FK_AppHtPm_YkLst3 foreign key (PumpSizeID)
+      references YukonListEntry (EntryID)
+go
