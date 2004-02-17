6689d1b9e352e5840b526d32eb73d5dbcf4ba57c
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2004-02-17 21:19:53 +0000

    Added get all graphDataSeries functionality.

--- yukon-client/common/src/com/cannontech/database/cache/functions/GraphFuncs.java
+++ yukon-client/common/src/com/cannontech/database/cache/functions/GraphFuncs.java
@@ -1,11 +1,13 @@
 package com.cannontech.database.cache.functions;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import com.cannontech.database.cache.DefaultDatabaseCache;
 import com.cannontech.database.data.lite.LiteGraphDefinition;
-import com.cannontech.database.data.lite.LiteYukonPAObject;
 import com.cannontech.database.data.lite.LitePoint;
+import com.cannontech.database.data.lite.LiteYukonPAObject;
 import com.cannontech.database.db.graph.GraphDataSeries;
 
 /**
@@ -54,5 +56,48 @@ public final class GraphFuncs {
 			}
 		}
 		return liteYukonPaobjectsVector;
+	}
+	
+	public static java.util.List getAllGraphDataSeries()
+	{
+		List graphDataSeriesVector = new ArrayList();
+		
+		DefaultDatabaseCache cache = DefaultDatabaseCache.getInstance();
+		synchronized(cache)
+		{
+			Iterator iter = cache.getAllGraphDefinitions().iterator();
+			while(iter.hasNext())
+			{
+				LiteGraphDefinition liteGdef = (LiteGraphDefinition) iter.next();
+				GraphDataSeries [] allGDSSeries = GraphDataSeries.getAllGraphDataSeries(new Integer(liteGdef.getLiteID()));
+				for(int i = 0; i < allGDSSeries.length; i++)
+				{
+					graphDataSeriesVector.add(allGDSSeries[i]);
+				}
+			}
+		}
+		return graphDataSeriesVector;
+	}
+	
+	public static java.util.List getAllGraphDataSeries(int type_)
+	{
+		java.util.List graphDataSeriesVector = new java.util.Vector(10);
+		
+		DefaultDatabaseCache cache = DefaultDatabaseCache.getInstance();
+		synchronized(cache)
+		{
+			Iterator iter = cache.getAllGraphDefinitions().iterator();
+			while(iter.hasNext())
+			{
+				LiteGraphDefinition liteGdef = (LiteGraphDefinition) iter.next();
+				GraphDataSeries [] allGDSSeries = GraphDataSeries.getAllGraphDataSeries(new Integer(liteGdef.getLiteID()));
+				for(int i = 0; i < allGDSSeries.length; i++)
+				{
+					if( (allGDSSeries[i].getType().intValue() & type_) == type_)
+						graphDataSeriesVector.add(allGDSSeries[i]);
+				}
+			}
+		}
+		return graphDataSeriesVector;
 	}	
 }
