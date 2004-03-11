2ef4ead93deae5b95d54580592fbf2a67d776712
Dayton, Jon R <JonRDayton@Eaton.com>
2004-03-11 17:40:37 +0000

    LMControlScenario panels...

---
+++ yukon-client/dbeditor/src/com/cannontech/dbeditor/wizard/device/lmscenario/LMScenarioWizardPanel.java
@@ -0,0 +1,93 @@
+/*
+ * Created on Mar 8, 2004
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package com.cannontech.dbeditor.wizard.device.lmscenario;
+
+import com.cannontech.common.wizard.WizardPanel;
+
+/**
+ * @author jdayton
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class LMScenarioWizardPanel extends WizardPanel 
+{
+	
+	private LMControlScenarioPanel lmControlScenarioPanel;
+	/**
+	 * LMGroupWizardPanel constructor comment.
+	 */
+	public LMScenarioWizardPanel() {
+		super();
+	}
+	/**
+	 * Insert the method's description here.
+	 * Creation date: (3/2/2004 4:01:28 PM)
+	 * @return java.awt.Dimension
+	 */
+	public java.awt.Dimension getActualSize() 
+	{
+		setPreferredSize( new java.awt.Dimension(410, 500) );
+
+		return getPreferredSize();
+	}
+	/**
+	 * This method was created in VisualAge.
+	 * @return java.lang.String
+	 */
+	protected String getHeaderText() {
+		return "LM Control Scenario Setup";
+	}
+	/**
+	 * Insert the method's description here.
+	 * Creation date: (3/2/2004 4:01:28 PM)
+	 * @return com.cannontech.dbeditor.wizard.device.lmcontrolarea.LMControlScenarioPanel
+	 */
+	public LMControlScenarioPanel getLmControlScenarioPanel() 
+	{
+		if( lmControlScenarioPanel == null )
+			lmControlScenarioPanel = new LMControlScenarioPanel();
+		
+		return lmControlScenarioPanel;
+	}
+	
+	
+	/**
+	 * This method was created in VisualAge.
+	 * @return java.awt.Dimension
+	 */
+	public java.awt.Dimension getMinimumSize() {
+		return getPreferredSize();
+	}
+	/**
+	 * This method was created in VisualAge.
+	 * @return com.cannontech.common.gui.util.InputPanel
+	 * @param currentInputPanel com.cannontech.common.gui.util.InputPanel
+	 */
+	protected com.cannontech.common.gui.util.DataInputPanel getNextInputPanel(com.cannontech.common.gui.util.DataInputPanel currentInputPanel) 
+	{
+		if( currentInputPanel == null )
+		{
+			return getLmControlScenarioPanel();
+		}
+		
+		return null;
+	}
+	/**
+	 * This method was created in VisualAge.
+	 * @return boolean
+	 * @param currentPanel com.cannontech.common.gui.util.DataInputPanel
+	 */
+	protected boolean isLastInputPanel(com.cannontech.common.gui.util.DataInputPanel currentPanel) 
+	{
+		return (currentPanel == getLmControlScenarioPanel() );
+	}
+	
+
+}
+
+
