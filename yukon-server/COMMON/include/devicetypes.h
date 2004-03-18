5a5b67be5a32c2a60348c3956eff7326d300486f
Plender, Corey <>
2004-03-18 19:48:25 +0000

    Added defines for: RTC SERIESVRTU SERIESVLMIRTU GOLAY SADIGITAL SA105,205 & 305 RTM

--- yukon-server/COMMON/include/devicetypes.h
+++ yukon-server/COMMON/include/devicetypes.h
@@ -6,8 +6,8 @@
 *
 * PVCS KEYWORDS:
 * ARCHIVE      :  $Archive:   Z:/SOFTWAREARCHIVES/YUKON/common/INCLUDE/devicetypes.h-arc  $
-* REVISION     :  $Revision: 1.17 $
-* DATE         :  $Date: 2004/03/08 20:52:34 $
+* REVISION     :  $Revision: 1.18 $
+* DATE         :  $Date: 2004/03/18 19:48:25 $
 *
 * Copyright (c) 1999, 2000, 2001 Cannon Technologies Inc. All rights reserved.
 *-----------------------------------------------------------------------------*/
@@ -110,6 +110,11 @@
 #define TYPE_TAPTERM_EMAIL                   1902
 #define TYPE_WCTP                            1905
 
+#define TYPE_RTC                             1910
+#define TYPE_RTM                             1911
+#define TYPE_SERIESVRTU                      1915
+#define TYPE_SERIESVLMIRTU                   1920
+
 
 #define TYPE_LMGROUP_EMETCON                 2000  // Group type devices...
 #define TYPE_LMGROUP_VERSACOM                2001
@@ -118,9 +123,12 @@
 #define TYPE_LMGROUP_EXPRESSCOM              2015
 #define TYPE_LMGROUP_MCT                     2020
 #define TYPE_LMGROUP_ENERGYPRO               2025
-#define TYPE_LMGROUP_SA305                   2026  
-#define TYPE_LMGROUP_SA205OR105              2027
-#define TYPE_LMGROUP_SADIGITALORGOLAY        2028
+
+#define TYPE_LMGROUP_GOLAY                   2030
+#define TYPE_LMGROUP_SADIGITAL               2031
+#define TYPE_LMGROUP_SA105                   2032
+#define TYPE_LMGROUP_SA205                   2033
+#define TYPE_LMGROUP_SA305                   2034
 
 #define TYPEMCTBCAST                         2050
 
