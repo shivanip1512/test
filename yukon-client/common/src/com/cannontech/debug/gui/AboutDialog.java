f05faf695369e6606602514ef4440363207b8051
Lauinger, Aaron C <AaronCLauinger@Eaton.com>
2004-04-07 19:23:59 +0000

    cleaned up some fugly code setValue doesn't have to be called now setting the title works now

--- yukon-client/common/src/com/cannontech/debug/gui/AboutDialog.java
+++ yukon-client/common/src/com/cannontech/debug/gui/AboutDialog.java
@@ -1,151 +1,145 @@
 package com.cannontech.debug.gui;
 
+import java.awt.Dialog;
 import java.util.Collection;
+import java.util.Vector;
 
 import com.cannontech.common.util.CtiUtilities;
 
 /**
- * Insert the type's description here.
+ * A dialog that provides some environment information , and optionally, some 
+ * application specific information.
+ * 
  * Creation date: (3/5/2001 10:36:05 AM)
- * @author: 
+ * @author:   
  */
 public class AboutDialog extends com.cannontech.debug.gui.AbstractListDataViewer 
 {
+	/* The parent class doesn't honor the constructors title
+	 * which is why it is explicitly in all the constructors
+	 */
+	private Vector _listData;
+	private static final String DEFAULT_TITLE = "About";
+
+	{   // Initializer
+		init();
+	}
+	
 /**
  * ScheduleDebugViewer constructor comment.
  */
 public AboutDialog() {
-	super();
+	super((Dialog)null, DEFAULT_TITLE);
+	setTitle(DEFAULT_TITLE);
 }
 /**
- * ScheduleDebugViewer constructor comment.
- * @param owner java.awt.Dialog
+  * @param owner java.awt.Dialog
  */
 public AboutDialog(java.awt.Dialog owner) {
-	super(owner);
+	super(owner, DEFAULT_TITLE);
+	setTitle(DEFAULT_TITLE);
 }
 /**
- * ScheduleDebugViewer constructor comment.
  * @param owner java.awt.Dialog
  * @param title java.lang.String
  */
 public AboutDialog(java.awt.Dialog owner, String title) {
 	super(owner, title);
+	setTitle(title);
 }
 /**
- * ScheduleDebugViewer constructor comment.
  * @param owner java.awt.Dialog
  * @param title java.lang.String
  * @param modal boolean
  */
 public AboutDialog(java.awt.Dialog owner, String title, boolean modal) {
 	super(owner, title, modal);
+	setTitle(title);
 }
 /**
- * ScheduleDebugViewer constructor comment.
  * @param owner java.awt.Dialog
  * @param modal boolean
  */
 public AboutDialog(java.awt.Dialog owner, boolean modal) {
-	super(owner, modal);
+	super(owner, DEFAULT_TITLE, modal);
 }
 /**
- * ScheduleDebugViewer constructor comment.
  * @param owner java.awt.Frame
  */
 public AboutDialog(java.awt.Frame owner) {
-	super(owner);
+	super(owner, DEFAULT_TITLE);
 }
 /**
- * ScheduleDebugViewer constructor comment.
- * @param owner java.awt.Frame
+  * @param owner java.awt.Frame
  * @param title java.lang.String
  */
 public AboutDialog(java.awt.Frame owner, String title) {
 	super(owner, title);
+	setTitle(title);
 }
 /**
- * ScheduleDebugViewer constructor comment.
- * @param owner java.awt.Frame
+  * @param owner java.awt.Frame
  * @param title java.lang.String
  * @param modal boolean
  */
 public AboutDialog(java.awt.Frame owner, String title, boolean modal) {
 	super(owner, title, modal);
+	setTitle(title);
 }
 /**
- * ScheduleDebugViewer constructor comment.
- * @param owner java.awt.Frame
+  * @param owner java.awt.Frame
  * @param modal boolean
  */
 public AboutDialog(java.awt.Frame owner, boolean modal) {
-	super(owner, modal);
+	super(owner, DEFAULT_TITLE, modal);
+	setTitle(DEFAULT_TITLE);
 }
 /**
- * Insert the method's description here.
+ * Don't call this.
  * Creation date: (3/5/2001 10:36:05 AM)
  */
 public Object getValue() 
 {
-	return null;
+	throw new Error("Don't call this");
 }
 /**
- * Insert the method's description here.
+ * Adds a Collection of Objects to the list to be displayed.
  * Creation date: (3/5/2001 10:36:05 AM)
  */
 public void setValue(Object obj) 
 {
-	java.util.Vector data = new java.util.Vector(20);
-	setTitle("About");
+	if(obj instanceof Collection) 
+	{
+		_listData.addAll((Collection) obj);
+	}
+}
+
+private void init() {
+	_listData = new Vector();
 
 	try
 	{
-		data.addElement( CtiUtilities.COPYRIGHT );
-		data.addElement("Version      : " + com.cannontech.common.version.VersionTools.getYUKON_VERSION() );
-		//data.addElement("Dev ID    : " + pointValue.getDeviceID());
-
+		_listData.addElement( CtiUtilities.COPYRIGHT );
+		_listData.addElement("Version      : " + com.cannontech.common.version.VersionTools.getYUKON_VERSION() );
 	
 		String[] pools = com.cannontech.database.PoolManager.getInstance().getAllPoolsStrings();
 		for( int i = 0; i < pools.length; i++ )
-			data.addElement("DB Pool ("+i+")  : " + pools[i] );
+			_listData.addElement("DB Pool ("+i+")  : " + pools[i] );
 
-
-		
-		//Strange-- prints out 2 drivers for each one that is loaded??
-/*
-		int k = 0;
-		java.util.Enumeration enum = java.sql.DriverManager.getDrivers();
-		while( enum.hasMoreElements() )
-		{
-	      java.sql.Driver driver = (java.sql.Driver)enum.nextElement();
-	      data.addElement("DB Driver("+(k++)+") : " + driver.getClass().getName() + " : " +
-	         driver.getMajorVersion() + "." +
-	         driver.getMinorVersion() );
-		}
-*/
-
-		
-		
-      data.addElement("JRE Version  : " + System.getProperty("java.version") );
+	    _listData.addElement("JRE Version  : " + System.getProperty("java.version") );
 
 		/* ALWAYS leave this as the last thing */
 		com.cannontech.database.db.version.CTIDatabase db = com.cannontech.common.version.VersionTools.getDatabaseVersion();
 		com.cannontech.clientutils.commonutils.ModifiedDate md = new com.cannontech.clientutils.commonutils.ModifiedDate( db.getDateApplied().getTime() );		
-		data.addElement("DB Version   : " + db.getVersion() + "  on  " + md.getDateString() );
-		data.addElement("DB Alias     : " + CtiUtilities.getDatabaseAlias() );
+		_listData.addElement("DB Version   : " + db.getVersion() + "  on  " + md.getDateString() );
+		_listData.addElement("DB Alias     : " + CtiUtilities.getDatabaseAlias() );
 
 	}
 	catch(Throwable t)  // Catch ALL things and just print them out
 	{
 		com.cannontech.clientutils.CTILogger.info("*** Throwable caught in + " + this.getClass() + " : " + t.getMessage() );
-		data.addElement("(Exception occured while getting values from database)");
+		_listData.addElement("(Exception occured while getting values from database)");
 	}
-
-	//if we wanted to add more items, do it here
-	if( obj instanceof Collection )
-		data.addAll( (Collection)obj );
-
-
-	getJListInfo().setListData(data);
+	getJListInfo().setListData(_listData);
 }
 }
