5a294b47500f143afd60a4ad75121f6bc07caedc
Lauinger, Aaron C <AaronCLauinger@Eaton.com>
2004-04-08 21:23:55 +0000

    cleaned up activitylogger and added a new method to do logging

--- yukon-client/common/src/com/cannontech/clientutils/ActivityLogger.java
+++ yukon-client/common/src/com/cannontech/clientutils/ActivityLogger.java
@@ -1,8 +1,5 @@
 /*
  * Created on Feb 23, 2004
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
  */
 package com.cannontech.clientutils;
 
@@ -18,10 +15,13 @@ import com.cannontech.database.data.lite.LiteEnergyCompany;
 import com.cannontech.database.data.lite.LiteYukonUser;
 
 /**
+ * ActivityLogger provides a mechanism for applications to log interesting things that
+ * a yukon user might do along with other possibly interesting information about the user.
+ * i.e. account id, energy company id
+ * 
+ * It is intended to be useful to generate reports from.
  * @author yao
- *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
+ * @author aaron
  */
 public class ActivityLogger {
 	
@@ -32,6 +32,88 @@ public class ActivityLogger {
 	public static void logEvent(int userID, int accountID, int energyCompanyID, int customerID,
 		String action, String description)
 	{
+		logEvent(userID, accountID, energyCompanyID, customerID, -1, action, description);
+	}
+	
+	/**
+	 * The log method used by LoginController when the login attempt failed
+	 */
+	public static void logEvent(String action, String description) {
+		logEvent(-1, -1, -1, -1, -1, action, description);
+	}
+	
+	public static void logEvent(int userID, int paoID, String action, String description)
+	{
+		logEvent(userID, -1, -1, -1, paoID, action, description);	
+	}
+	
+	/**
+	 * The log method used by LoginController. The rest of the fields
+	 * will be filled as much as we can.
+	 */
+	public static void logEvent(int userID, String action, String description) {
+		logEvent(userID, -1, -1, -1, -1, action, description);
+	}
+	
+	/**
+	 * logEvent with all possible parameters.
+	 * Set any parameters that aren't relevant to -1 and attempts will be made to
+	 * fill them automagically.
+	 * @param userID
+	 * @param accountID
+	 * @param energyCompanyID
+	 * @param customerID
+	 * @param paoID
+	 * @param action
+	 * @param description
+	 */
+	public static void logEvent(int userID, int accountID, int energyCompanyID, 
+								int customerID, int paoID, String action, String description)
+	{
+		if(energyCompanyID == -1)
+		{	
+			LiteYukonUser liteUser = YukonUserFuncs.getLiteYukonUser( userID );
+		
+			LiteEnergyCompany liteComp = EnergyCompanyFuncs.getEnergyCompany( liteUser );
+			if (liteComp != null)
+			{ 			
+				energyCompanyID = liteComp.getEnergyCompanyID();
+			}
+		}
+		
+		if(customerID == -1 || energyCompanyID == -1 || accountID == -1)
+		{		
+			LiteContact liteContact = YukonUserFuncs.getLiteContact( userID );
+			if (liteContact != null) {
+			LiteCustomer liteCust = ContactFuncs.getCustomer( liteContact.getContactID() );
+			
+				if (liteCust != null) {
+					if(customerID == -1)
+					{								
+						customerID = liteCust.getCustomerID();
+					}
+				
+					if (!liteCust.isExtended())
+					{					
+						liteCust.retrieve( com.cannontech.common.util.CtiUtilities.getDatabaseAlias() );
+					}
+				
+					java.util.Vector acctIDs = liteCust.getAccountIDs();
+					if (acctIDs.size() > 0) {
+						// This customer has residential information
+						if(energyCompanyID == -1)
+						{					
+							energyCompanyID = liteCust.getEnergyCompanyID();
+						}
+						if(accountID == -1)
+						{					
+							accountID = ((Integer) acctIDs.get(0)).intValue();
+						}
+					}
+				}
+			}
+		}
+		
 		ActivityLog actLog = new ActivityLog();
 		actLog.setUserID( userID );
 		actLog.setAccountID( accountID );
@@ -47,48 +129,4 @@ public class ActivityLogger {
 			CTILogger.error( "Failed to write action '" + action + "' to activity log", e );
 		}
 	}
-	
-	/**
-	 * The log method used by LoginController when the login attempt failed
-	 */
-	public static void logEvent(String action, String description) {
-		logEvent(-1, -1, -1, -1, action, description);
-	}
-	
-	/**
-	 * The log method used by LoginController. The rest of the fields
-	 * will be filled as much as we can.
-	 */
-	public static void logEvent(int userID, String action, String description) {
-		int accountID = -1;
-		int energyCompanyID = -1;
-		int customerID = -1;
-		
-		LiteYukonUser liteUser = YukonUserFuncs.getLiteYukonUser( userID );
-		
-		LiteEnergyCompany liteComp = EnergyCompanyFuncs.getEnergyCompany( liteUser );
-		if (liteComp != null) energyCompanyID = liteComp.getEnergyCompanyID();
-		
-		LiteContact liteContact = YukonUserFuncs.getLiteContact( userID );
-		if (liteContact != null) {
-			LiteCustomer liteCust = ContactFuncs.getCustomer( liteContact.getContactID() );
-			
-			if (liteCust != null) {
-				customerID = liteCust.getCustomerID();
-				
-				if (!liteCust.isExtended())
-					liteCust.retrieve( com.cannontech.common.util.CtiUtilities.getDatabaseAlias() );
-				
-				java.util.Vector acctIDs = liteCust.getAccountIDs();
-				if (acctIDs.size() > 0) {
-					// This customer has residential information
-					energyCompanyID = liteCust.getEnergyCompanyID();
-					accountID = ((Integer) acctIDs.get(0)).intValue();
-				}
-			}
-		}
-		
-		logEvent(userID, accountID, energyCompanyID, customerID, action, description);
-	}
-
 }
