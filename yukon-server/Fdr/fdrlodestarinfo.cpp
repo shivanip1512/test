53d21efdd8061ec0ab08ec1f5979261720ec1f4c
Ganz, Julie <>
2004-04-12 16:51:46 +0000

    jrichter  FDR Lodestar changes to handle standard format and files are read in based on point parameters.
    Testing commit...

---
+++ yukon-server/Fdr/fdrlodestarinfo.cpp
@@ -0,0 +1,85 @@
+#pragma warning( disable : 4786 )  // No truncated debug name warnings please....
+
+/*****************************************************************************
+*
+*    FILE NAME: fdrlodestarinfo.cpp
+*
+*    DATE: 03/31/2004
+*
+*    AUTHOR: Julie Richter
+*
+*    PURPOSE: class header CtiFDRD_LodeStarInfoTable
+*
+*    DESCRIPTION: represents a FDR destination record
+*
+*
+*
+*    Copyright (C) 2000 Cannon Technologies, Inc.  All rights reserved.
+****************************************************************************
+*/
+
+
+/** include files **/
+#include "fdrlodestarinfo.h"
+
+CtiFDR_LodeStarInfoTable::CtiFDR_LodeStarInfoTable(RWCString &aDrivePath, RWCString &aFileName)
+: _lodeStarDrivePath(aDrivePath),
+  _lodeStarFileName(aFileName)
+{
+} 
+    
+CtiFDR_LodeStarInfoTable::~CtiFDR_LodeStarInfoTable()
+{
+}
+
+CtiFDR_LodeStarInfoTable &CtiFDR_LodeStarInfoTable::operator=( const CtiFDR_LodeStarInfoTable &other )
+{   
+    if(this != &other)
+    {
+        _lodeStarDrivePath = other.getLodeStarDrivePath();
+        _lodeStarFileName = other.getLodeStarFileName();
+    }
+    return *this;
+}
+
+RWCString & CtiFDR_LodeStarInfoTable::getLodeStarDrivePath(void)
+{
+    return _lodeStarDrivePath;
+}    
+RWCString CtiFDR_LodeStarInfoTable::getLodeStarDrivePath(void) const
+{
+    return _lodeStarDrivePath;
+}
+CtiFDR_LodeStarInfoTable& CtiFDR_LodeStarInfoTable::setLodeStarDrivePath(RWCString aDrivePath)
+{
+    _lodeStarDrivePath = aDrivePath;
+    return *this;
+}
+    
+RWCString & CtiFDR_LodeStarInfoTable::getLodeStarFileName(void)
+{
+    return _lodeStarFileName;
+}
+RWCString CtiFDR_LodeStarInfoTable::getLodeStarFileName(void) const
+{
+    return _lodeStarFileName;
+}
+    
+CtiFDR_LodeStarInfoTable& CtiFDR_LodeStarInfoTable::setLodeStarFileName(RWCString aFileName)
+{
+    _lodeStarFileName = aFileName; 
+    return *this;
+
+}
+
+/*int operator==(const CtiFDR_LodeStarInfoTable &lodeStarInfoList1, const CtiFDR_LodeStarInfoTable &lodeStarInfoList2)
+{
+    const char * ptr1;
+    const char * ptr2;
+
+    ptr1 = lodeStarInfoList1.getLodeStarFileName().data();
+    ptr2 = lodeStarInfoList2.getLodeStarFileName().data();
+
+    return !strcmp(ptr1, ptr2);
+}
+*/
---
+++ yukon-server/Fdr/include/fdrlodestarinfo.h
@@ -0,0 +1,51 @@
+#pragma warning( disable : 4786 )  // No truncated debug name warnings please....
+/*****************************************************************************
+*
+*    FILE NAME: fdrlodestarinfo.h
+*
+*    DATE: 03/31/2004
+*
+*    AUTHOR: Julie Richter
+*
+*    PURPOSE: CtiFDR_LodeStarInfoTable class header
+*
+*    DESCRIPTION: contains destination information for a point 
+*
+*
+*
+*    Copyright (C) 2000 Cannon Technologies, Inc.  All rights reserved.
+****************************************************************************
+*/
+
+#ifndef __FDRLODESTARINFO_H__
+#define __FDRLODESTARINFO_H__
+
+/** include files **/
+#include <windows.h>    //  NOTE:  if porting this to non-WIN32, make sure to replace this
+                        //         with ctitypes.h...  i only put this in here because
+                        //         the compiler was having fits with BOOL.
+
+#include <rw/cstring.h>
+#include "dlldefs.h"
+#include "fdr.h"
+
+
+class IM_EX_FDRBASE CtiFDR_LodeStarInfoTable 
+{
+public:
+    CtiFDR_LodeStarInfoTable(RWCString &aDrivePath, RWCString &aFileName);
+    ~CtiFDR_LodeStarInfoTable();
+    CtiFDR_LodeStarInfoTable& operator=( const CtiFDR_LodeStarInfoTable &other );
+    RWCString & getLodeStarDrivePath(void);
+    RWCString getLodeStarDrivePath(void) const;
+    RWCString & getLodeStarFileName(void);
+    RWCString getLodeStarFileName(void) const;
+    CtiFDR_LodeStarInfoTable& setLodeStarDrivePath(RWCString aDrivePath);
+    CtiFDR_LodeStarInfoTable& setLodeStarFileName(RWCString aFileName);
+private:
+    RWCString _lodeStarDrivePath;
+    RWCString _lodeStarFileName;
+
+};
+
+#endif
