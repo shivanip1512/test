ae759e9c5037d9e321089fb18cc173b1a1bcc3d7
Schmit, Eric <>
2004-04-14 18:16:32 +0000

    updated the shells

--- yukon-server/Protocol/include/prot_sasimple.h
+++ yukon-server/Protocol/include/prot_sasimple.h
@@ -14,10 +14,27 @@
 *
 * PVCS KEYWORDS:
 * ARCHIVE      :  $Archive$
-* REVISION     :  $Revision: 1.1 $
-* DATE         :  $Date: 2004/04/05 19:50:26 $
+* REVISION     :  $Revision: 1.2 $
+* DATE         :  $Date: 2004/04/14 18:16:32 $
 *
 * Copyright (c) 1999, 2000, 2001, 2002 Cannon Technologies Inc. All rights reserved.
 *----------------------------------------------------------------------------------*/
+
+#include "cmdparse.h"
+#include "dlldefs.h"
+#include "dsm2.h"
+#include "pointtypes.h"
+#include "prot_base.h"
+
+
+class IM_EX_PROT CtiProtocolSASimple : public CtiProtocolBase
+{
+private:
+protected:
+public:
+    CtiProtocolSASimple();
+    CtiProtocolSASimple( const CtiProtocolSASimple& aRef );
+    ~CtiProtocolSASimple();
+};
 
 #endif // #ifndef __PROT_SASIMPLE_H__
