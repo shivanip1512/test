333e95c6295434dd1de4339fa888eac234a00c66
Zhihong, Yao <>
2004-04-14 22:11:48 +0000

    Add ProgramOrder column to the LMProgramWebPublishing table

--- yukon-database/DBUpdates/oracle/stars/3.00-stars.sql
+++ yukon-database/DBUpdates/oracle/stars/3.00-stars.sql
@@ -16,4 +16,7 @@ alter table ApplianceHeatPump
 alter table CustomerAccount drop constraint FK_YkUs_CstAcc;
 alter table CustomerAccount drop column LoginID;
 
+alter table LMProgramWebPublishing add ProgramOrder numeric;
+update LMProgramWebPublishing set ProgramOrder = 0;
+
 update YukonUserRole ur set Value=(select GroupID from YukonGroup where GroupName=ur.Value or GroupName=ur.Value || ' Grp') where RolePropertyID in (-1105,-1106) and Value <> '(none)';
--- yukon-database/DBUpdates/sqlserver/stars/3.00-stars.sql
+++ yukon-database/DBUpdates/sqlserver/stars/3.00-stars.sql
@@ -4,17 +4,30 @@
 
 
 alter table InventoryBase alter column Notes Varchar(500);
+go
 alter table AccountSite alter column PropertyNotes Varchar(300);
+go
 alter table ApplianceBase alter column Notes Varchar(500);
+go
 alter table CustomerResidence alter column Notes Varchar(300);
+go
 
 alter table ApplianceHeatPump add PumpSizeID numeric;
+go
 alter table ApplianceHeatPump
    add constraint FK_AppHtPm_YkLst3 foreign key (PumpSizeID)
       references YukonListEntry (EntryID)
 go
 
 alter table CustomerAccount drop constraint FK_YkUs_CstAcc;
+go
 alter table CustomerAccount drop column LoginID;
+go
+
+alter table LMProgramWebPublishing add ProgramOrder numeric;
+go
+update LMProgramWebPublishing set ProgramOrder = 0;
+go
 
 update YukonUserRole ur set Value=(select GroupID from YukonGroup where GroupName=ur.Value or GroupName=ur.Value || ' Grp') where RolePropertyID in (-1105,-1106) and Value <> '(none)';
+go
