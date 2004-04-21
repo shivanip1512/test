0f884a5c6a4b1b6f5f8ca50f76e1a95ce7309b68
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2004-04-21 17:21:46 +0000

    Addition of action parameter to drive what the servlet should actually do. Changes to graphBean session declaration. Changes for graphBean extending Graph instead of having an instance of Graph. Changed to reference the ServletUtil.ATT_YUKON_USER static string

--- yukon-client/servlet/src/com/cannontech/servlet/GraphGenerator.java
+++ yukon-client/servlet/src/com/cannontech/servlet/GraphGenerator.java
@@ -27,16 +27,17 @@ package com.cannontech.servlet;
  * @author: Stacey Nebben
  */
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.cannontech.clientutils.CTILogger;
+import com.cannontech.util.ServletUtil;
+import com.cannontech.util.SessionAttribute;
 
 public class GraphGenerator extends javax.servlet.http.HttpServlet {
 		
-	// Maybe this could be moved to a location so that it can
-	// be shared by various clients (jsps/servlets) of the GraphBean
-	private static final String GRAPH_BEAN_SESSION_KEY = "graphBean";	
-	
 /**
  * 
  * Creation date: (12/9/99 3:24:30 PM)
@@ -45,73 +46,103 @@ public class GraphGenerator extends javax.servlet.http.HttpServlet {
  * @exception javax.servlet.ServletException The exception description.
  * @exception java.io.IOException The exception description.
  */
-public synchronized void  doGet(javax.servlet.http.HttpServletRequest req, javax.servlet.http.HttpServletResponse resp) throws javax.servlet.ServletException, java.io.IOException
+public synchronized void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, java.io.IOException
 {
 	try
 	{	 	
 		HttpSession session = req.getSession(false);
-				
-		com.cannontech.graph.GraphBean localBean = (com.cannontech.graph.GraphBean)session.getAttribute(GRAPH_BEAN_SESSION_KEY);
+		String destURL = req.getParameter( ServletUtil.ATT_REDIRECT );	//successsful action URL
+		String errorURL = req.getParameter( ServletUtil.ATT_REFERRER );	//failed action URL
+
+		com.cannontech.graph.GraphBean localBean = (com.cannontech.graph.GraphBean)session.getAttribute(ServletUtil.ATT_GRAPH_BEAN);
 		if(localBean == null)
 		{
-			session.setAttribute(GRAPH_BEAN_SESSION_KEY, new com.cannontech.graph.GraphBean());
-			localBean = (com.cannontech.graph.GraphBean)session.getAttribute(GRAPH_BEAN_SESSION_KEY);
+			session.setAttribute(ServletUtil.ATT_GRAPH_BEAN, new com.cannontech.graph.GraphBean());
+			localBean = (com.cannontech.graph.GraphBean)session.getAttribute(ServletUtil.ATT_GRAPH_BEAN);
 		}
 
-		{			
-			String param;
-			String param2;
+		
+		String param;
+		String param2;
+		
+		param = req.getParameter("gdefid");
+		if( param != null)
+			localBean.setGdefid(Integer.parseInt( param));
 			
-			param = req.getParameter("gdefid");
-			if( param != null)
-				localBean.setGdefid(Integer.parseInt( param));
-				
-			param = req.getParameter("start");
-			if( param != null) 								
-				localBean.setStart(param);
+		param = req.getParameter("start");
+		if( param != null) 								
+			localBean.setStart(param);
 
-			param = req.getParameter("period");
-			if( param != null)
-				localBean.setPeriod(param);
-	
-			param = req.getParameter("view");
-			if( param != null)
-				localBean.setViewType(Integer.parseInt(param));
-	
-			param = req.getParameter("option");
-			if( param != null)
-				localBean.setOption(Integer.parseInt(param));
-	
-			param = req.getParameter("format");
-			if( param != null)
-				localBean.setFormat(param);
+		param = req.getParameter("period");
+		if( param != null)
+			localBean.setPeriod(param);
 
-			param = req.getParameter("width");
-			param2 = req.getParameter("height");
-			if(param != null && param2 != null) {
-				localBean.setSize(Integer.parseInt(param),Integer.parseInt(param2));
-			}
-		}
+		param = req.getParameter("view");
+		if( param != null)
+			localBean.setViewType(Integer.parseInt(param));
 
-		localBean.updateCurrentPane();
-		javax.servlet.ServletOutputStream out = null;
-		try
-		{	
-			// Try to defeat caching
-			resp.setHeader("Cache-Control","no-store"); //HTTP 1.1
-			resp.setHeader("Pragma","no-cache"); 		//HTTP 1.0
-	 		resp.setDateHeader ("Expires", 0); 			//prevents caching at the proxy server
-	 	
-	    	out = resp.getOutputStream();
-	    	
-	    	localBean.encode(out);
-			out.flush();
-		}
-		catch( java.io.IOException ioe )
+		param = req.getParameter("option");
+		if( param != null)
+			localBean.setOption(Integer.parseInt(param));
+
+		param = req.getParameter("format");
+		if( param != null)
+			localBean.setFormat(param);
+
+		param = req.getParameter("width");
+		param2 = req.getParameter("height");
+		if(param != null && param2 != null) {
+			localBean.setSize(Integer.parseInt(param),Integer.parseInt(param2));
+		}	
+		
+		param = req.getParameter("action");
+		if (param != null )
 		{
-			ioe.printStackTrace();
-		}			
-
+			if(  param.equalsIgnoreCase("UpdateOptions"))
+			{
+				if (destURL == null)
+					destURL = req.getContextPath() + "/user/Metering/user_trending.jsp";
+			}
+			else if ( param.equalsIgnoreCase("EncodeGraph"))
+			{
+				localBean.updateCurrentPane();
+				javax.servlet.ServletOutputStream out = null;
+				try
+				{	
+					// Try to defeat caching
+					resp.setHeader("Cache-Control","no-store"); //HTTP 1.1
+					resp.setHeader("Pragma","no-cache"); 		//HTTP 1.0
+					resp.setDateHeader ("Expires", 0); 			//prevents caching at the proxy server
+	 	
+					out = resp.getOutputStream();
+	    	
+					localBean.encode(out);
+					out.flush();
+				}
+				catch( java.io.IOException ioe )
+				{
+					ioe.printStackTrace();
+				}			
+			}
+			else if ( param.equalsIgnoreCase("GetDataNow"))
+			{
+				com.cannontech.database.data.lite.LiteYukonUser liteYukonUser = (com.cannontech.database.data.lite.LiteYukonUser) session.getAttribute(ServletUtil.ATT_YUKON_USER);
+				if( liteYukonUser != null)
+				{
+					String[] custDevices = req.getParameterValues("custDevices");
+					if( custDevices != null)
+					{
+						SessionAttribute att = localBean.getDataNow(liteYukonUser, custDevices);
+						if( att != null)
+							session.setAttribute( att.getAttName(), att.getAttValue());	
+					}	
+				}
+				if (destURL == null)
+					destURL = req.getContextPath() + "/user/Metering/user_get_data_now.jsp";				
+			}		
+		}
+		if(destURL != null)
+			resp.sendRedirect( destURL );
 	}
 	catch( Throwable t )
 	{
@@ -120,16 +151,4 @@ public synchronized void  doGet(javax.servlet.http.HttpServletRequest req, javax
 	}
 }
 
-/**
- * POST method not supported
- * Creation date: (12/9/99 3:39:10 PM)
- * @param req javax.servlet.http.HttpServletRequest
- * @param resp javax.servlet.http.HttpServletResponse
- * @exception javax.servlet.ServletException The exception description.
- * @exception java.io.IOException The exception description.
- */
-public void doPost(javax.servlet.http.HttpServletRequest req, javax.servlet.http.HttpServletResponse resp) throws javax.servlet.ServletException, java.io.IOException 
-{
-	resp.sendError( javax.servlet.http.HttpServletResponse.SC_METHOD_NOT_ALLOWED);
-}
 }
