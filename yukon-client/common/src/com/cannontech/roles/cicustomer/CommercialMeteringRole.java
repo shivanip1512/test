a4b81b60a5bcf1d45f6a667259813aa5ffff35bf
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2004-04-21 12:29:33 +0000

    Addition of properities -30402 (CommercialMetering-minimum scan frequency) and -30403 (CommercialMetering-maximum daily scans allowed)

--- yukon-client/common/src/com/cannontech/roles/cicustomer/CommercialMeteringRole.java
+++ yukon-client/common/src/com/cannontech/roles/cicustomer/CommercialMeteringRole.java
@@ -9,5 +9,7 @@ public interface CommercialMeteringRole {
 	public static final int ROLEID = CICustomerRoleDefs.COMMERCIAL_METERING_ROLEID;
 	
 	public static final int TRENDING_DISCLAIMER = CICustomerRoleDefs.COMMERCIAL_METERING_PROPERTYID_BASE;
-	public static final int TRENDING_GET_DATA_NOW_BUTTON = CICustomerRoleDefs.COMMERCIAL_METERING_PROPERTYID_BASE - 1;
+	public static final int GET_DATA_NOW_ENABLED = CICustomerRoleDefs.COMMERCIAL_METERING_PROPERTYID_BASE - 1;
+	public static final int MINIMUM_SCAN_FREQUENCY = CICustomerRoleDefs.COMMERCIAL_METERING_PROPERTYID_BASE - 2;
+	public static final int MAXIMUM_DAILY_SCANS = CICustomerRoleDefs.COMMERCIAL_METERING_PROPERTYID_BASE - 3;
 }
