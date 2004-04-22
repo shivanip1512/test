09e35688c6d933c70dd45124595a5aee5891445f
Neuharth, Ryan <>
2004-04-22 17:55:35 +0000

    Added fdrinterface changes

--- yukon-database/DBUpdates/oracle/3.00.sql
+++ yukon-database/DBUpdates/oracle/3.00.sql
@@ -957,6 +957,19 @@ alter table PAOExclusion add FuncParams varchar2(200);
 update PAOExclusion set FuncParams = '(none)';
 alter table PAOExclusion alter column FuncParams varchar2(200) not null;
 
+delete from fdrinterface where InterfaceId = 14;
+insert into fdrinterface values (16,'LODESTAR_STD','Receive','f');
+insert into fdrinterface values (17,'LODESTAR_ENH','Receive','f');
+
+delete from fdrinterfaceoption where interfaceid = 14;
+insert into fdrinterfaceoption values (16,'Customer',1,'Text','(none)');
+insert into fdrinterfaceoption values (16,'Channel',2,'Text','(none)');
+insert into fdrinterfaceoption values (16,'DrivePath',3,'Text','(none)');
+insert into fdrinterfaceoption values (16,'Filename',4,'Text','(none)');
+insert into fdrinterfaceoption values (17,'Customer',1,'Text','(none)');
+insert into fdrinterfaceoption values (17,'Channel',2,'Text','(none)');
+insert into fdrinterfaceoption values (17,'DrivePath',3,'Text','(none)');
+insert into fdrinterfaceoption values (17,'Filename',4,'Text','(none)');
 
 
 /* Stars tables initialization script */
