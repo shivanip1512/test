96d44784cfa7c9433e94b7d8a030195f4d41741a
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2004-04-26 22:12:24 +0000

    Removed implements Pageable.  Didn' t have a need for it and it just cause problems. The extend JTextPanePrintable was also removed.  The instance is moved into the class declaration in order to control the vector of lines needed.

--- yukon-client/common/src/com/cannontech/common/gui/util/JTextPanePrintable.java
+++ yukon-client/common/src/com/cannontech/common/gui/util/JTextPanePrintable.java
@@ -7,13 +7,13 @@
 package com.cannontech.common.gui.util;
 
 import java.awt.Font;
-import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.print.PageFormat;
-import java.awt.print.Pageable;
 import java.awt.print.Printable;
 import java.awt.print.PrinterException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Vector;
 
 import javax.swing.JTextPane;
@@ -24,92 +24,83 @@ import javax.swing.JTextPane;
  * To change the template for this generated type comment go to
  * Window>Preferences>Java>Code Generation>Code and Comments
  */
-public class JTextPanePrintable extends JTextPane implements Printable, Pageable
+public class JTextPanePrintable implements Printable
 {
-	private int rowCount = 0;
-	private int pageCount  = 1;
+	// The JTextPane to print [the text area will be parsed for lines]
+	private JTextPane textPane = null;
+	
 	private static Font f = new Font("Arial", Font.PLAIN, 10);
 	
+	//Vector of strings for each line to be printed.
 	private Vector vectorOfLines = null;
+	//current index of vectorOfLines.
+	private int rowCount = 0;
+	
+	//boolean used to control printing of every other pageIndex, as the pageIndex comes through 2 times for each value.
 	private boolean skipping = true;
-	public JTextPanePrintable(){
+
+
+	public JTextPanePrintable(JTextPane textPane_)
+	{
 		super();
-		setDoubleBuffered(false);
+		textPane = textPane_;
+		textPane.setDoubleBuffered(false);
 	}
+
+	
 	/* (non-Javadoc)
 	 * @see java.awt.print.Printable#print(java.awt.Graphics, java.awt.print.PageFormat, int)
 	 */
 	public int print(Graphics graphics, PageFormat pageFormat, int pageIndex)
 		throws PrinterException
 	{
+		//setup a 2D graphic
+		Graphics2D g2 = (Graphics2D) graphics;
+		g2.setFont(f);
+		int fontHeight = g2.getFontMetrics().getHeight();
+		int fontDesent = g2.getFontMetrics().getDescent();
+	
+		int fontAscent = g2.getFontMetrics().getMaxAscent();
+	
+		//leave room for page number
+		double pageHeight = pageFormat.getImageableHeight() - fontHeight;
+		double pageWidth = pageFormat.getImageableWidth();
+
+		int imgX =(int)pageFormat.getImageableX();
+		int imgY =(int)pageFormat.getImageableY() + fontAscent;
+		 
+		g2.translate( imgX, imgY );
+		
+		int numRowsOnAPage = (int)((pageHeight - imgX) / fontHeight) - 1;
+		
+		int totalNumPages = (int)Math.ceil(( (double)getVectorOfLines().size()) / numRowsOnAPage);
+		
+		if( pageIndex >= totalNumPages)
+			return NO_SUCH_PAGE;
+	
+		//Only print rows on every other pageIndex, starting with the second one.
+		// For some reason, the pageIndex repeats itself and only prints every other time,
+		// therefore we skip the first occurance of each pageIndex value
 		if( !skipping )
-		{		
-			//getVectorOfLines();	//setup the vector of text lines.
-			Graphics2D g2 = (Graphics2D) graphics;
-			g2.setFont(f);
-						
-			FontMetrics fontMetrics = g2.getFontMetrics(f);
-			int fontAscent = fontMetrics.getMaxAscent();
-			int fontHeight = fontMetrics.getHeight();
-			int x =(int)pageFormat.getImageableX();
-			int y =(int)pageFormat.getImageableY() + fontAscent;
-			int linesPerPage = (int)(pageFormat.getImageableHeight()/fontHeight);
-			
-			int i;
-			for (i = rowCount; i < (linesPerPage*pageCount) && i < vectorOfLines.size(); i++)
+		{			
+			//top right, print the date and time
+			SimpleDateFormat format = new SimpleDateFormat("MMMMM dd, yyyy HH:mm");
+			g2.drawString(format.format(new Date()), (int)pageWidth-100, 0 );
+		
+			int i = 0;
+			for (i = rowCount; i < (numRowsOnAPage * (pageIndex + 1)) && i < getVectorOfLines().size(); i++)
 			{
-				g2.drawString(vectorOfLines.get(i).toString(), x, y);
-				y+= fontHeight;
+				g2.drawString(getVectorOfLines().get(i).toString(), imgX , imgY);
+				imgY+= fontHeight;
 			}
-			pageCount++;
 			rowCount = i;
+
+			// bottom center, print the page number
+			g2.drawString("Page: " + (pageIndex+1), (int)pageWidth/2 - 20, (int)(pageHeight - fontDesent));
 		}
 		skipping=!skipping;
-		return PAGE_EXISTS;
-			
-	}
-	
-	PageFormat pf = null;
-	
-	// READ ME!!!
-	//We are going to init all we can in this function cause I'm not
-	// really sure where else to do it at this time.
-	// Vector of Lines get's reset with text that is in the textPane at the time of print.
-	// PageCount is reset to 1 as we are starting a new print.
-	// RowCount is reset to 0 as we are starting our total row count with the new print process.
-	/** 
-	 * This function must be called in order for the row counting and page counting to be correct.
-	 * The text Vector of text lines is also populated in this function.
-	 * Not calling this function will cause a null pointer on the vector of lines.
-	 * */
-	public int getNumberOfPages()
-	{
-		getVectorOfLines();
-		pageCount = 1;
-		rowCount = 0;
-		return (int)(getBounds().height / pf.getImageableHeight()) + 1;
-	}
-	/* (non-Javadoc)
-	 * @see java.awt.print.Pageable#getPageFormat(int)
-	 */
-	public PageFormat getPageFormat(int pageIndex) throws IndexOutOfBoundsException
-	{
-		return pf;
-	}
-	
-	/**
-	 * @param pgFormat
-	 */
-	public void setPageFormat(PageFormat pgFormat)
-	{
-		pf = pgFormat;
-	}
-	/* (non-Javadoc)
-	 * @see java.awt.print.Pageable#getPrintable(int)
-	 */
-	public Printable getPrintable(int pageIndex) throws IndexOutOfBoundsException
-	{
-		return this;
+
+		return Printable.PAGE_EXISTS;
 	}
 
 	/**
@@ -119,19 +110,41 @@ public class JTextPanePrintable extends JTextPane implements Printable, Pageable
 	 */
 	public Vector getVectorOfLines()
 	{
-		vectorOfLines = new Vector();
-		int beginIndex = 0;
-		int i = 0;
-		for (i = 0; i < getText().length(); i++)
+		if (vectorOfLines == null)
 		{
-			if( getText().charAt(i) == '\n')
+			vectorOfLines = new Vector();
+			int beginIndex = 0;
+			int i = 0;
+			for (i = 0; i < textPane.getText().length(); i++)
 			{
-				vectorOfLines.add(getText().substring(beginIndex, i-1));
-				beginIndex = i+1;
+				//check if count between \n is greater than 85 chars, this will be too long
+				if( i - beginIndex > 85)
+				{
+					int endIndex = textPane.getText().lastIndexOf(' ', i);
+					if( endIndex < 0)	//no blank found
+					{
+						endIndex = textPane.getText().lastIndexOf('\t', i);
+						if( endIndex < 0)	//still not found
+							endIndex = i;	//truncate right where we are!
+					}
+					if( endIndex < beginIndex)	//found one too far back
+						endIndex = i;
+					vectorOfLines.add(textPane.getText().substring(beginIndex, endIndex));
+					i = endIndex;
+					beginIndex = i+1;
+				}
+				if (textPane.getText().charAt(i) == '\n')
+				{
+					if( beginIndex < i)
+						vectorOfLines.add( textPane.getText().substring(beginIndex, i));
+					else
+						vectorOfLines.add("");
+					beginIndex = i+1;
+				}
 			}
+			//MUST DO THE LAST ROW!!!
+			vectorOfLines.add(textPane.getText().substring(beginIndex));
 		}
-		//MUST DO THE LAST ROW!!!
-		vectorOfLines.add(getText().substring(beginIndex));
 		return vectorOfLines;
 	}
 }
