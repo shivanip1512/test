cff8266948e95f60f3f634e1fe0fa89e11393e13
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2004-05-06 20:02:30 +0000

    Change directory to C is REQUIRED!

--- yukon-client/billing/resource/billing.bat
+++ yukon-client/billing/resource/billing.bat
@@ -1,1 +1,3 @@
+c:
+cd \yukon\client\bin
 java -Djava.class.path=.;%YUKON_BASE%/server/web;billing.jar;common.jar;classes12.jar;jtds.jar;SqlServer.jar;yukonappserver.jar;rwav.jar;log4j-1.2.4.jar;j2ee.jar com.cannontech.billing.mainprograms.BillingFile outputfile=billing.csv collectiongroup=Default %1 %2 %3 %4 %5 %6 %7 %8
