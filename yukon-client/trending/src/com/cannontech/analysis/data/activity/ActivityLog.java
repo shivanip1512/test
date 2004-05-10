7b18e543821d617d7d5149af4d88075c94ad32e3
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2004-05-10 20:33:11 +0000

    Added username to class.

--- yukon-client/trending/src/com/cannontech/analysis/data/activity/ActivityLog.java
+++ yukon-client/trending/src/com/cannontech/analysis/data/activity/ActivityLog.java
@@ -6,8 +6,10 @@ import com.cannontech.analysis.ColumnProperties;
 import com.cannontech.analysis.Reportable;
 import com.cannontech.database.cache.functions.CustomerFuncs;
 import com.cannontech.database.cache.functions.EnergyCompanyFuncs;
+import com.cannontech.database.cache.functions.YukonUserFuncs;
 import com.cannontech.database.data.lite.LiteContact;
 import com.cannontech.database.data.lite.LiteEnergyCompany;
+import com.cannontech.database.data.lite.LiteYukonUser;
 
 
 /**
@@ -25,20 +27,22 @@ import com.cannontech.database.data.lite.LiteEnergyCompany;
 public class ActivityLog implements Reportable
 {	
 	/** Number of columns */
-	protected final int NUMBER_COLUMNS = 5;
+	protected final int NUMBER_COLUMNS = 6;
 	
 	/** Enum values for column representation */
 	public final static int ENERGY_COMPANY_COLUMN = 0;
 	public final static int CONTACT_COLUMN = 1;
-	public final static int ACCOUNT_NUMBER_COLUMN = 2;
-	public final static int ACTION_COLUMN = 3;
-	public final static int ACTION_COUNT_COLUMN = 4;
-//	public final static int DATE_TIME_COLUMN = 5;
-//	public final static int ACTION_COLUMN = 6;
-//	public final static int DESCRIPTION_COLUMN = 7;	
+	public final static int USERNAME_COLUMN = 2;	
+	public final static int ACCOUNT_NUMBER_COLUMN = 3;
+	public final static int ACTION_COLUMN = 4;
+	public final static int ACTION_COUNT_COLUMN = 5;
+//	public final static int DATE_TIME_COLUMN = 6;
+//	public final static int ACTION_COLUMN = 7;
+//	public final static int DESCRIPTION_COLUMN = 8;	
 	
 	/** String values for column representation */
 	public final static String ENERGY_COMPANY_STRING = "Energy Company";
+	public final static String USERNAME_STRING = "Username";
 	public final static String CONTACT_STRING = "Contact";
 	public final static String ACCOUNT_NUMBER_STRING = "Account Number";
 	public final static String ACTION_STRING = "Action";
@@ -57,6 +61,7 @@ public class ActivityLog implements Reportable
 	private static String title = "ENERGY COMPANY ACTIVITY LOG";
 
 	public Integer ecID = null;
+	public Integer userID = null;
 	public Integer custID = null;	//used to get the primary contact
 	public String acctNumber = null;
 	public Integer actionCount = new Integer(0);			
@@ -74,10 +79,11 @@ public class ActivityLog implements Reportable
 	/**
 	 * Default Constructor
 	 */
-	public ActivityLog(Integer ecID_, Integer custID_, String acctNum_, Integer actionCount_, String action_)
+	public ActivityLog(Integer ecID_, Integer userID_, Integer custID_, String acctNum_, Integer actionCount_, String action_)
 	{
 		super();
 		ecID = ecID_;
+		userID = userID_;
 		custID = custID_;
 		acctNumber = acctNum_;
 		actionCount = actionCount_;
@@ -86,11 +92,12 @@ public class ActivityLog implements Reportable
 	/**
 	 * Default Constructor
 	 */
-	public ActivityLog(Integer ecID_, Integer custID_, Integer acctID_, Integer actionCount_, 
+	public ActivityLog(Integer ecID_, Integer userID_, Integer custID_, Integer acctID_, Integer actionCount_, 
 						Date dateTime_, String action_, String description_)
 	{
 		super();
 		ecID = ecID_;
+		userID = userID_;
 		custID = custID_;
 		actionCount = actionCount_;
 //		dateTime = dateTime_;
@@ -107,6 +114,7 @@ public class ActivityLog implements Reportable
 			columnNames = new String[]{
 				ENERGY_COMPANY_STRING,
 				CONTACT_STRING,
+				USERNAME_STRING,
 				ACCOUNT_NUMBER_STRING,
 				ACTION_STRING,
 				ACTION_COUNT_STRING
@@ -127,6 +135,7 @@ public class ActivityLog implements Reportable
 		if( columnTypes == null)
 		{
 			columnTypes = new Class[]{
+				String.class,
 				String.class,
 				String.class,
 				String.class,
@@ -153,8 +162,9 @@ public class ActivityLog implements Reportable
 				new ColumnProperties(0, 1, 150, 18, null),
 				new ColumnProperties(0, 1, 150, 18, null),
 				new ColumnProperties(150, 1, 100, 18, null),
-				new ColumnProperties(250, 1, 200, 18, null),
-				new ColumnProperties(450, 1, 25, 18, null)
+				new ColumnProperties(250, 1, 100, 18, null),
+				new ColumnProperties(350, 1, 200, 18, null),
+				new ColumnProperties(550, 1, 25, 18, null)
 			};				
 		}
 		return columnProperties;
@@ -177,6 +187,14 @@ public class ActivityLog implements Reportable
 					if( lec == null)
 						return "(deleted)";
 					return lec.getName();
+				}
+				case USERNAME_COLUMN:
+				{
+					LiteYukonUser user = YukonUserFuncs.getLiteYukonUser(userID.intValue());
+					if (user == null)
+						return "(deleted)";
+					
+					return (user.getUsername());
 				}
 				case CONTACT_COLUMN:
 				{
