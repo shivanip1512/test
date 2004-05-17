4d304cf68be04a91b5b0c633a2a4225216be57dc
Lauinger, Aaron C <AaronCLauinger@Eaton.com>
2004-05-17 21:15:03 +0000

    adding boost 1_31_0

---
+++ yukon-server/boost_1_31_0/boost/mpl/list/aux_/include_preprocessed.hpp
@@ -0,0 +1,27 @@
+
+// Copyright (c) 2001-04 Aleksey Gurtovoy
+//
+// Use, modification and distribution are subject to the Boost Software 
+// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy 
+// at http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/mpl for documentation.
+
+// $Source: c:\subversion/sandbox/cvsfinal/cvs/yukon-server/boost_1_31_0/boost/mpl/list/aux_/include_preprocessed.hpp,v $
+// $Date: 2004/05/17 21:12:57 $
+// $Revision: 1.1 $
+
+// no include guards, the header is intended for multiple inclusion!
+
+#include "boost/preprocessor/cat.hpp"
+#include "boost/preprocessor/stringize.hpp"
+
+#   define AUX_PREPROCESSED_HEADER \
+    aux_/preprocessed/plain/BOOST_MPL_PREPROCESSED_HEADER \
+/**/
+
+#   include BOOST_PP_STRINGIZE(boost/mpl/list/AUX_PREPROCESSED_HEADER)
+
+#   undef AUX_PREPROCESSED_HEADER
+
+#undef BOOST_MPL_PREPROCESSED_HEADER
