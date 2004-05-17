159e08bf86f16fc2641dba6e8b721c22cf36c95d
Lauinger, Aaron C <AaronCLauinger@Eaton.com>
2004-05-17 21:23:23 +0000

    added some boost date conversion

--- yukon-server/COMMON/include/rwutil.h
+++ yukon-server/COMMON/include/rwutil.h
@@ -1,22 +1,41 @@
 #ifndef __RWUTIL_H__
 #define __RWUTIL_H__
 
+#include <set>
 #include <string>
 #include <rw/cstring.h>
 #include <rw/vstream.h>
 #include <rw/db/reader.h>
+#include <rw/rwdate.h>
+#include <rw/rwtime.h>
+#include <rw/db/datetime.h>
+
+#include "boost/date_time/posix_time/posix_time.hpp"
 
 #include "dlldefs.h"
+
+using std::set;
+using std::string;
+
+/*
+ * RW Date/Time - boost
+ */
+using namespace boost::posix_time;
+using namespace boost::gregorian; 
+
+IM_EX_CTIBASE date to_boost_date(const RWDate& rw_date);
+IM_EX_CTIBASE ptime to_boost_ptime(const RWTime& rw_time);
+IM_EX_CTIBASE ptime to_boost_ptime(const RWDBDateTime& rwdb_datetime);
 
 /*
  * RW virtual stream - std::string operators
  */
-IM_EX_CTIBASE RWvostream& operator<<(RWvostream& strm, const std::string& s);
-IM_EX_CTIBASE RWvistream&  operator>>(RWvistream& strm, std::string& s);
+IM_EX_CTIBASE RWvostream& operator<<(RWvostream& strm, const string& s);
+IM_EX_CTIBASE RWvistream&  operator>>(RWvistream& strm, string& s);
 
 /*
  * RW Database - std::string operators
  */
-IM_EX_CTIBASE RWDBReader&  operator>>(RWDBReader& rdr, std::string& s);
+IM_EX_CTIBASE RWDBReader&  operator>>(RWDBReader& rdr, string& s);
 
 #endif
--- yukon-server/COMMON/rwutil.cpp
+++ yukon-server/COMMON/rwutil.cpp
@@ -1,11 +1,42 @@
 #include "rwutil.h"
 
-RWvostream& operator<<(RWvostream& strm, const std::string& s)
+date to_boost_date(const RWDate& rw_date)
+{
+    return date(rw_date.year(), rw_date.month(), rw_date.dayOfMonth());
+}
+
+ptime to_boost_ptime(const RWTime& rw_time)
+{
+    struct tm tm;
+    rw_time.extract(&tm);
+    time_t tt = mktime(&tm);
+    return from_time_t(tt);
+}
+
+ptime to_boost_ptime(const RWDBDateTime& rwdb_datetime)
+{
+    struct tm tm;
+    rwdb_datetime.extract(&tm);
+    time_t tt = mktime(&tm);
+    return from_time_t(tt);
+}
+/*
+RWSet makeRWSet(const set<T>& std_set)
+{
+    RWSet rw_set;
+    for(set<T>::iterator iter = std_set.begin(); iter != std_set.end(); iter++)
+    {
+	rw_set.insert(*iter);
+    }
+    return rw_set;
+}*/
+
+RWvostream& operator<<(RWvostream& strm, const string& s)
 {
     return strm << RWCString((const char*) s.data());
 }
 
-RWvistream&  operator>>(RWvistream& strm, std::string& s)
+RWvistream&  operator>>(RWvistream& strm, string& s)
 {
     RWCString rw_str;
     strm >> rw_str;
@@ -13,7 +44,7 @@ RWvistream&  operator>>(RWvistream& strm, std::string& s)
     return strm;
 }
 
-RWDBReader& operator>>(RWDBReader& rdr, std::string& s)
+RWDBReader& operator>>(RWDBReader& rdr, string& s)
 {
     RWCString rw_str;
     rdr >> rw_str;
