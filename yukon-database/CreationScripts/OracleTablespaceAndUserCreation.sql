3babe08d2820294a7c5c1a2f44af7d8fba546f87
Neuharth, Ryan <>
2004-06-02 20:48:17 +0000

    Made the oracle user into a DBA

--- yukon-database/CreationScripts/OracleTablespaceAndUserCreation.sql
+++ yukon-database/CreationScripts/OracleTablespaceAndUserCreation.sql
@@ -2,7 +2,4 @@ create tablespace yukon datafile 'yukon.ora' size 10M autoextend on;
 create temporary tablespace yukontemp tempfile 'yukontemp.ora' size 5M autoextend on;
 
 create user yukon identified by yukon default tablespace yukon temporary tablespace yukontemp quota unlimited on yukon quota unlimited on yukontemp;
-grant create session to yukon;
-grant create table to yukon;
-grant create view to yukon;
-grant create trigger to yukon;
+grant dba to yukon;
