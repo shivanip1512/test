e8e7b55d6ac15c1fa647716e24a72b5d614afa3d
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2004-06-21 15:08:37 +0000

    New System Log Types tree model (used for reporting initially).

---
+++ yukon-client/common/src/com/cannontech/database/model/SystemLogTypeCheckBoxTreeModel.java
@@ -0,0 +1,61 @@
+/*
+ * Created on Jun 7, 2004
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package com.cannontech.database.model;
+
+import com.cannontech.common.gui.tree.CheckNode;
+import com.cannontech.database.db.point.SystemLog;
+
+/**
+ * @author snebben
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class SystemLogTypeCheckBoxTreeModel extends DBTreeModel
+{
+	public static String TITLE_STRING = "System Log Type";
+	/**
+	 * @param root
+	 */
+	public SystemLogTypeCheckBoxTreeModel()
+	{
+		super(new CheckNode(TITLE_STRING));
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.cannontech.database.model.LiteBaseTreeModel#isLiteTypeSupported(int)
+	 */
+	public boolean isLiteTypeSupported(int liteType)
+	{
+		//No liteType exists for SystemLog types.
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.cannontech.database.model.LiteBaseTreeModel#update()
+	 */
+	public void update()
+	{
+		String availableSystemLogTypesArray[] = SystemLog.LOG_TYPE_STRINGS;
+		
+		CheckNode rootNode = (CheckNode) getRoot();
+		rootNode.removeAllChildren();
+	
+		for (int i = 0; i <  availableSystemLogTypesArray.length; i++)
+		{
+			CheckNode cycleGroupNode = new CheckNode( availableSystemLogTypesArray[i] );
+			rootNode.add(cycleGroupNode);
+		}
+	
+		reload();
+	}
+	
+	public String toString()
+	{
+		return TITLE_STRING;
+	}
+}
