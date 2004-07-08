ff6e0495555b5f1c9677cce68732ddd84617f6af
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2004-07-08 18:03:57 +0000

    AMR Role types.

---
+++ yukon-client/common/src/com/cannontech/roles/AMRRoleDefs.java
@@ -0,0 +1,10 @@
+package com.cannontech.roles;
+
+/**
+ * @author snebben
+ */
+public interface AMRRoleDefs extends RoleDefs {
+	public static final int BILLING_ROLEID = AMR_ROLEID_BASE;
+		
+	static final int BILLING_PROPERTYID_BASE = AMR_PROPERTYID_BASE;
+}
