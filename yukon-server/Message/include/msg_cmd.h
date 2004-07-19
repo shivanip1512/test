60b92a89617daa69bd5043a0d308b73d7c054400
Plender, Corey <>
2004-07-19 20:43:36 +0000

    Adjusted the command operations enumeration to make all VALID commands divisible by 10.
    The intervalued commands could be used for test purposes.

--- yukon-server/Message/include/msg_cmd.h
+++ yukon-server/Message/include/msg_cmd.h
@@ -9,8 +9,8 @@
 *
 * PVCS KEYWORDS:
 * ARCHIVE      :  $Archive:   Z:/SOFTWAREARCHIVES/YUKON/MESSAGE/INCLUDE/msg_cmd.h-arc  $
-* REVISION     :  $Revision: 1.6 $
-* DATE         :  $Date: 2004/01/14 17:22:25 $
+* REVISION     :  $Revision: 1.7 $
+* DATE         :  $Date: 2004/07/19 20:43:36 $
 *
 * Copyright (c) 1999, 2000, 2001 Cannon Technologies Inc. All rights reserved.
 *-----------------------------------------------------------------------------*/
@@ -72,29 +72,31 @@ public:
    void What() const;
    virtual void dump() const;
 
-   // Command Operaitons I will support
+   // Command Operaitons I will support.
+   // Real codes to be sent from client to server must be divisible by 10.
    enum {
       NoOp = 0,                        // Does a LoopClient command
-      Shutdown,                        // Bye bye to VanGogh
-      ClientAppShutdown,               // Client App has gone away
-      NewClient,                       // Client App has been connected by Connection Handler
-      LoopClient,                      // Sends message back to the client
-      TraceRoute,                      // Prints a blurb out in each place it is encountered.
-      AreYouThere,                     // Used to find out if a client is still there. Receipt causes a response.
-      AcknowledgeAlarm,                //
-      ClearAlarm,                      //
-      TokenGrant,                      // Server is providing client with a security pass key.
-      ReregistrationRequest,           // Client must re-register with server application.
-      UpdateFailed,                    // scan completed with an error, vector contains (token, idtype, dev/pointid, scantype, failcode) This causes Quality to go nonUpdated on all affected points!
-      ControlRequest,                  // Vector contains token, deviceid (0 if not available), pointid/controloffset, raw state number, (optional) TRUE if cmd is to use control offset.
-      Ablement,                        // Vector contains token, idtype (0 = Device, 1 = Point), id, ablement bit (0 = dis, 1 = enable).
-      CommStatus,                      // Vector contains token, deviceid, status (communication result in porter, 0 = NORMAL).
-      AlternateScanRate,               // Vector contains token, deviceid, seconds since midnight start (may be negative - use receipt time for start), duration in seconds (may be zero)
-      ControlAblement,                 // Vector contains token, idtype (0 = Device, 1 = Point), id, ablement bit (0 = dis, 1 = enable).
-      PointTagAdjust,                  // Vector contains token, pointid, tag(s) to set, tag(s) to reset.
-      PorterConsoleInput,              // Vector contains token, operation (same as keyboard character. see PorterConsoleInput:porter.cpp)
-      ResetControlHours,               // By default resets the Seasonal control history, could add to the op arg list to allow for resets of other control histories; Vector contains token
-      LastCommand//this is to be the last entry in the enum, so add new entries above here, otay
+      Shutdown = 10,                   // Bye bye to VanGogh
+      ClientAppShutdown = 20,          // Client App has gone away
+      NewClient = 30,                  // Client App has been connected by Connection Handler
+      LoopClient = 40,                 // Sends message back to the client
+      TraceRoute = 50,                 // Prints a blurb out in each place it is encountered.
+      AreYouThere = 60,                // Used to find out if a client is still there. Receipt causes a response.
+      AcknowledgeAlarm = 70,           //
+      ClearAlarm = 80,                 //
+      TokenGrant = 90,                 // Server is providing client with a security pass key.
+      ReregistrationRequest = 100,     // Client must re-register with server application.
+      UpdateFailed = 110,              // scan completed with an error, vector contains (token, idtype, dev/pointid, scantype, failcode) This causes Quality to go nonUpdated on all affected points!
+      ControlRequest = 120,            // Vector contains token, deviceid (0 if not available), pointid/controloffset, raw state number, (optional) TRUE if cmd is to use control offset.
+      Ablement = 130,                  // Vector contains token, idtype (0 = Device, 1 = Point), id, ablement bit (0 = dis, 1 = enable).
+      CommStatus = 140,                // Vector contains token, deviceid, status (communication result in porter, 0 = NORMAL).
+      AlternateScanRate = 150,         // Vector contains token, deviceid, seconds since midnight start (may be negative - use receipt time for start), duration in seconds (may be zero)
+      ControlAblement = 160,           // Vector contains token, idtype (0 = Device, 1 = Point), id, ablement bit (0 = dis, 1 = enable).
+      PointTagAdjust = 170,            // Vector contains token, pointid, tag(s) to set, tag(s) to reset.
+      PorterConsoleInput = 180,        // Vector contains token, operation (same as keyboard character. see PorterConsoleInput:porter.cpp)
+      ResetControlHours = 190,         // By default resets the Seasonal control history, could add to the op arg list to allow for resets of other control histories; Vector contains token
+
+      LastCommand = 10000              //this is to be the last entry in the enum, so add new entries above here, otay
    };
 };
 
