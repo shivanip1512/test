f364de87a561ff75c55f824a79b5f6ecfc0783c3
Fisher, Matthew S <MatthewSFisher@Eaton.com>
2004-09-21 17:28:09 +0000

    brought in the whole ptime namespace

--- yukon-server/COMMON/include/boost_time.h
+++ yukon-server/COMMON/include/boost_time.h
@@ -4,17 +4,10 @@
 #include "boost/date_time/posix_time/posix_time.hpp"
 #include "boost/date_time/gregorian/greg_date.hpp"
 
-//  it may be prudent to bring the entire gregorian namespace in,
-//    and possibly the entire posix_time namespace...  but for now,
-//    these are really the only classes we need
-using boost::posix_time::ptime;
+using namespace boost::posix_time;
+
+//  perhaps we'll bring in the whole namespace someday, but right now, all we need is date
 using boost::gregorian::date;
-
-using boost::posix_time::second_clock;
-using boost::posix_time::hours;
-using boost::posix_time::minutes;
-using boost::posix_time::seconds;
-
 
 inline unsigned long ptime_to_utc_seconds(const ptime &p)
 {
