65fa2bdd62b170ef60276a63d6a4f48b2fdb1088
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2004-09-28 01:44:24 +0000

    moved loadPrompValue function to CommandFuncs class.

--- yukon-client/common/src/com/cannontech/common/util/KeysAndValuesFile.java
+++ yukon-client/common/src/com/cannontech/common/util/KeysAndValuesFile.java
@@ -281,58 +281,7 @@ public class KeysAndValuesFile extends java.io.File
 		this.comment = comment;
 	}
 	
-	/** 
-	 * Replace the Default value prompt with the prompted input text.
-	 * @param valueString
-	 * @param parent
-	 * @return
-	 */
-	public static String loadPromptValue(String valueString, java.awt.Component parent )
-	{
-		int promptIndex = valueString.indexOf(DEFAULT_VALUE_PROMPT);
-		while (promptIndex > -1)
-		{
-			int endIndex = -1;
-			String promptString = valueString.substring(promptIndex+1).trim();	//clear all blanks at the begining of the prompt text
-			String stringEnding = "";
-			if( promptString.charAt(0) == '\'' || promptString.charAt(0) == '\"')//quoted prompt string
-			{
-				promptString = promptString.substring(1);	//remove quote from begining of strng
-				endIndex = promptString.indexOf('\'');	//locate ending quote
-				if( endIndex < 0 )
-					endIndex = promptString.indexOf('\"');
-			}
-			else
-			{
-				endIndex = promptString.indexOf(" ");	//locate string end, space is separator
-				
-				if( promptString.indexOf('\'') > 0 && (promptString.indexOf('\'')< endIndex || endIndex < 0))
-					endIndex = promptString.indexOf('\'');
-				else if( promptString.indexOf('\"') > 0 && (promptString.indexOf('\"')< endIndex || endIndex < 0))
-					endIndex = promptString.indexOf('\"');
-				
-				if( endIndex > 0)
-					stringEnding +=promptString.charAt(endIndex);	//add the char back to the begining of the command.
-			}
-				
-			if( endIndex > 0)
-			{
-				stringEnding += promptString.substring(endIndex+1);	//Store the end of the command string.
-				promptString = promptString.substring(0, endIndex);	//truncate the end of the string to get just the prompt value.
-			}
-				
-			String value = javax.swing.JOptionPane.showInputDialog(parent, promptString + ": ", "Enter the parameter value", javax.swing.JOptionPane.QUESTION_MESSAGE );
-				
-			if( value != null)
-			{
-				valueString = (valueString.substring(0, promptIndex) + value + stringEnding).trim();
-				promptIndex = valueString.trim().indexOf(DEFAULT_VALUE_PROMPT);	//look for another prompt value
-			}
-			else
-				return null;	//CANCEL
-		}
-		return valueString;
-	}
+
 	/**
 	 * Insert the method's description here.
 	 * Creation date: (5/13/2002 9:23:11 AM)
