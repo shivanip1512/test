b01a6bcb4c8d7a11bb753a451a2f1425ea8627a1
Plender, Corey <>
2004-10-12 20:16:20 +0000

    Added memory dumping if DEBUG_MEMORY is TRUE

--- yukon-server/Dispatch/vangogh.cpp
+++ yukon-server/Dispatch/vangogh.cpp
@@ -7,8 +7,8 @@
 *
 * PVCS KEYWORDS:
 * ARCHIVE      :  $Archive:   Z:/SOFTWAREARCHIVES/YUKON/DISPATCH/vangogh.cpp-arc  $
-* REVISION     :  $Revision: 1.4 $
-* DATE         :  $Date: 2002/08/28 16:15:40 $
+* REVISION     :  $Revision: 1.5 $
+* DATE         :  $Date: 2004/10/12 20:16:20 $
 *
 * Copyright (c) 1999, 2000, 2001 Cannon Technologies Inc. All rights reserved.
 *-----------------------------------------------------------------------------*/
@@ -28,10 +28,11 @@ using namespace std;  // get the STL into our namespace for use.  Do NOT use ios
 #include <rw\rwerr.h>
 #include <rw\thr\mutex.h>
 
+#include "cparms.h"
+#include "ctivangogh.h"
 #include "dllbase.h"
 #include "dlldefs.h"
 #include "dllvg.h"
-#include "ctivangogh.h"
 #include "stdexcepthdlr.h"
 #include "logger.h"
 
@@ -40,6 +41,13 @@ DLLIMPORT extern CtiLogger dout;
 
 static RWWinSockInfo  winsock;
 
+static _CRT_ALLOC_HOOK pfnOldCrtAllocHook = NULL;
+
+static int MyAllocHook(int nAllocType, void *pvData,
+                       size_t nSize, int nBlockUse, long lRequest,
+                       const unsigned char * szFileName, int nLine );
+
+
 
 int DispatchMainFunction(int argc, char **argv)
 {
@@ -47,11 +55,19 @@ int DispatchMainFunction(int argc, char **argv)
    {
       CtiVanGogh VanGogh;
 
+      SET_CRT_OUTPUT_MODES;
+      if(gConfigParms.isOpt("DEBUG_MEMORY") && !gConfigParms.getValueAsString("DEBUG_MEMORY").compareTo("true", RWCString::ignoreCase) )
+          ENABLE_CRT_SHUTDOWN_CHECK;
+
+      pfnOldCrtAllocHook = _CrtSetAllocHook(MyAllocHook);
+
       VanGogh.CmdLine(argc, argv);      // Record the Command line options for the world to see.
 
       int i = VanGogh.execute();
 
       VanGogh.join();
+
+      _CrtSetAllocHook(pfnOldCrtAllocHook);
 
       return(i);
    }
@@ -64,3 +80,76 @@ int DispatchMainFunction(int argc, char **argv)
 }
 
 
+static int MyAllocHook(int nAllocType, void *pvData,
+                       size_t nSize, int nBlockUse, long lRequest,
+                       const unsigned char * szFileName, int nLine )
+{
+    static ULONG lastAlloc = 0;
+    static ULONG prevLastAlloc = 0;
+    static ULONG pprevLastAlloc = 0;
+
+    int twnetyfourcnt = 0;
+
+    if(lRequest > 1000000 && (nSize == 24 || nSize == 52 || nSize == 1316 || nSize == 68) )
+    {
+        if( (nSize == 24) )
+        {
+            if(lastAlloc == (lRequest - 1))
+            {
+                twnetyfourcnt++;
+
+                if(prevLastAlloc == (lRequest - 2))
+                {
+                    twnetyfourcnt++;
+
+                    if(pprevLastAlloc == (lRequest - 3))
+                    {
+                        twnetyfourcnt++;
+                    }
+                    pprevLastAlloc = prevLastAlloc;
+                }
+                prevLastAlloc = lastAlloc;
+            }
+
+            twnetyfourcnt++;
+
+            lastAlloc = lRequest;
+        }
+        else if((nSize == 19) )
+        {
+            twnetyfourcnt++;
+        }
+        else if( (nSize == 53) )
+        {
+            twnetyfourcnt++;
+        }
+        else if( (nSize == 63) )
+        {
+            twnetyfourcnt++;
+        }
+        else if( (nSize == 164) )
+        {
+            twnetyfourcnt++;
+        }
+    }
+
+#ifdef IGNORE_CRT_ALLOC
+    if(_BLOCK_TYPE(nBlockUse) == _CRT_BLOCK)  // Ignore internal C runtime library allocations
+        return TRUE;
+#endif
+    extern int _crtDbgFlag;
+
+    if( ((_CRTDBG_ALLOC_MEM_DF & _crtDbgFlag) == 0) && ( (nAllocType == _HOOK_ALLOC) || (nAllocType == _HOOK_REALLOC) ) )
+    {
+        // Someone has disabled that the runtime should log this allocation
+        // so we do not log this allocation
+        if(pfnOldCrtAllocHook != NULL)
+            pfnOldCrtAllocHook(nAllocType, pvData, nSize, nBlockUse, lRequest, szFileName, nLine);
+        return TRUE;
+    }
+
+    // call the previous alloc hook
+    if(pfnOldCrtAllocHook != NULL)
+        pfnOldCrtAllocHook(nAllocType, pvData, nSize, nBlockUse, lRequest, szFileName, nLine);
+    return TRUE; // allow the memory operation to proceed
+}
