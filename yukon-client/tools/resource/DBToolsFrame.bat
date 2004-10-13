0119067e258ccc830c1ee0fa2160cfa48cbeb4d1
Neuharth, Ryan <>
2004-10-13 17:44:51 +0000

    Added -Dsun.java2d.noddraw=true

--- yukon-client/tools/resource/DBToolsFrame.bat
+++ yukon-client/tools/resource/DBToolsFrame.bat
@@ -1,1 +1,1 @@
-java -Djava.class.path=.;%YUKON_BASE%/server/web;classes12.jar;common.jar;tools.jar;jtds.jar;SqlServer.jar;yukonappserver.jar;log4j-1.2.4.jar;j2ee.jar; com.cannontech.tools.gui.DBToolsFrame
+java -Dsun.java2d.noddraw=true -Djava.class.path=.;%YUKON_BASE%/server/web;classes12.jar;common.jar;tools.jar;jtds.jar;SqlServer.jar;yukonappserver.jar;log4j-1.2.4.jar;j2ee.jar; com.cannontech.tools.gui.DBToolsFrame
