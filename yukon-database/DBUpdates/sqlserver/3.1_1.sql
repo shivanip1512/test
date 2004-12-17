c4c8305251a9b693565bd642dd5752362cb1fbcf
Neuharth, Ryan <>
2004-12-17 15:59:19 +0000

    Added Template column to MacSchedule

--- yukon-database/DBUpdates/sqlserver/3.1_1.sql
+++ yukon-database/DBUpdates/sqlserver/3.1_1.sql
@@ -1568,8 +1568,18 @@ insert into FDRInterfaceOption values(19, 'Translation', 1, 'Text', '(none)' );
 
 update YukonRoleProperty set DefaultValue='yukon/DefaultHeader.gif' where RolePropertyID=-10805;
 
-alter TABLE MacSchedule add Template NUMERIC null DEFAULT null;
-update MacSchedule set template = 0 where commandfile is not null;
+alter table MacSchedule add Template NUMERIC;
+update MacSchedule set Template = 0;
+alter table MacSchedule alter column Template NUMERIC not null;
+
+
+
+
+
+
+
+
+
 
 
 /******************************************************************************/
