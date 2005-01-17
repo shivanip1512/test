158c21bbad2f13313ab5621cadc63026dc89221f
Lauinger, Aaron C <AaronCLauinger@Eaton.com>
2005-01-17 20:49:08 +0000

    Added reset peak point command

--- yukon-client/loadcontrol/src/com/cannontech/loadcontrol/messages/LMCommand.java
+++ yukon-client/loadcontrol/src/com/cannontech/loadcontrol/messages/LMCommand.java
@@ -36,7 +36,7 @@ public class LMCommand extends LMMessage
 	public static final int ENABLE_GROUP								= 14;
 	public static final int DISABLE_GROUP								= 15;
 	public static final int CONFIRM_GROUP								= 16;
-
+	public static final int RESET_PEAK_POINT_VALUE                      = 17;
           
 	public static final String[] CMD_STRS =
 	{
@@ -58,8 +58,8 @@ public class LMCommand extends LMMessage
 		"Restore",
 		"Enable Group",  //14
 		"Disable Group",
-		"Confirm"
-		
+		"Confirm",
+		"Reset Peak Point"		
 	};
 /**
  * ScheduleCommand constructor comment.
