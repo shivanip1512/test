9e09ad3d5b1419ebb337bbc9aad03850b9984b7c
Fisher, Matthew S <MatthewSFisher@Eaton.com>
2005-02-25 21:50:38 +0000

    enum changes, added lp peak, frozen, changes in privacy

--- yukon-server/RTDB/include/dev_mct410.h
+++ yukon-server/RTDB/include/dev_mct410.h
@@ -9,8 +9,8 @@
 *
 * PVCS KEYWORDS:
 * ARCHIVE      :  $Archive:   Z:/SOFTWAREARCHIVES/YUKON/RTDB/INCLUDE/dev_MCT410.h-arc  $
-* REVISION     :  $Revision: 1.9 $
-* DATE         :  $Date: 2005/02/21 21:49:23 $
+* REVISION     :  $Revision: 1.10 $
+* DATE         :  $Date: 2005/02/25 21:50:38 $
 *
 * Copyright (c) 1999, 2000 Cannon Technologies Inc. All rights reserved.
 *-----------------------------------------------------------------------------*/
@@ -26,71 +26,118 @@ class IM_EX_DEVDB CtiDeviceMCT410 : public CtiDeviceMCT
 {
 protected:
 
+    enum Commands
+    {
+        Command_Connect          = 0x4C,
+        Command_Disconnect       = 0x4D
+    };
+
+    enum MemoryLocations
+    {
+        Memory_StatusPos         = 0x05,
+        Memory_StatusLen         =    5,
+
+        Memory_PowerfailCountPos = 0x23,
+        Memory_PowerfailCountLen =    2,
+
+        Memory_AlarmsPos         = 0x15,
+        Memory_AlarmsLen         =    2,
+
+        Memory_IntervalsPos      = 0x1a,
+        Memory_IntervalsLen      =    4,
+
+        Memory_LastFreezePos     = 0x26,
+        Memory_LastFreezeLen     =   10,
+
+        Memory_RTCPos            = 0x40,
+        Memory_RTCLen            =    4,
+
+        Memory_LastTSyncPos      = 0x44,
+        Memory_LastTSyncLen      =    4
+    };
+
+    enum Functions
+    {
+        FuncWrite_IntervalsPos   = 0x03,
+        FuncWrite_IntervalsLen   =    4,
+
+        FuncRead_OutagePos       = 0x10,
+        FuncRead_OutageLen       =   13,
+
+        FuncRead_MReadPos        = 0x90,
+        FuncRead_MReadLen        =    3,  //  this is for the 410 KWH Only;  will need to be increased later
+
+        FuncRead_FrozenMReadPos  = 0x91,
+        FuncRead_FrozenMReadLen  =    4,
+
+        FuncRead_DemandPos       = 0x92,
+        FuncRead_DemandLen       =    6,  //  brings back recent demand, avg. voltage, and blink counter
+
+        FuncRead_PeakDemandPos   = 0x93,
+        FuncRead_PeakDemandLen   =    9,  //  peak demand, time of peak, current meter reading
+
+        FuncRead_FrozenPos       = 0x94,
+        FuncRead_FrozenLen       =   10,
+
+        FuncRead_VoltagePos      = 0x95,
+        FuncRead_VoltageLen      =   12,  //  max and min voltages
+
+        FuncRead_LPStatusPos     = 0x97,
+        FuncRead_LPStatusLen     =   12,
+
+        FuncRead_LLPPeakDailyPos      = 0xa0,
+        FuncRead_LLPPeakHourPos       = 0xa1,
+        FuncRead_LLPPeakIntervalPos   = 0xa2,
+        FuncRead_LLPPeakLen           =   13,
+
+        FuncWrite_DisconnectConfigPos = 0xfe,
+        FuncWrite_DisconnectConfigLen =    6,
+
+        FuncWrite_LLPInterestPos      = 0x05,
+        FuncWrite_LLPInterestLen      =    6,
+
+        FuncWrite_LLPPeakInterestPos  = 0x06,
+        FuncWrite_LLPPeakInterestLen  =    7,
+
+        FuncRead_DisconnectConfigPos  = 0xFE,
+        FuncRead_DisconnectConfigLen  =    9,
+
+        FuncRead_DisconnectStatusPos  = 0xFE,
+        FuncRead_DisconnectStatusLen  =    1
+    };
+
+    enum ValueType
+    {
+        ValueType_Voltage,
+        ValueType_KW,
+        ValueType_LoadProfile_Voltage,
+        ValueType_LoadProfile_KW,
+        ValueType_Accumulator,
+        ValueType_Raw
+    };
+
+    enum ErrorClasses
+    {
+        EC_MeterReading    = 0x0001,
+        EC_DemandReading   = 0x0002,
+        EC_TOUDemand       = 0x0004,
+        EC_TOUFrozenDemand = 0x0008,
+        EC_LoadProfile     = 0x0010
+    };
+
     enum
     {
-        MCT410_StatusPos         = 0x05,
-        MCT410_StatusLen         =    5,
+        MCT4XX_Command_FreezeVoltageOne   = 0x59,
+        MCT4XX_Command_FreezeVoltageTwo   = 0x5A,
 
-        MCT410_PowerfailCountPos = 0x23,
-        MCT410_PowerfailCountLen =    2,
+        MCT4XX_Command_PowerfailReset     = 0x89,
+        MCT4XX_Command_Reset              = 0x8A,
 
-        MCT410_AlarmsPos         = 0x15,
-        MCT410_AlarmsLen         =    2,
+        MCT410_PointOffset_Voltage       =    4,
+        MCT410_PointOffset_Analog_Outage =  100,
 
-        MCT410_IntervalsPos      = 0x1a,
-        MCT410_IntervalsLen      =    4,
-
-        MCT410_RTCPos            = 0x40,
-        MCT410_RTCLen            =    4,
-        MCT410_LastTSyncPos      = 0x44,
-        MCT410_LastTSyncLen      =    4,
-
-        MCT410_FuncWriteIntervalsPos = 0x03,
-        MCT410_FuncWriteIntervalsLen =    4,
-
-        MCT410_FuncReadOutagePos     = 0x10,
-        MCT410_FuncReadOutageLen     =   13,
-
-        MCT410_FuncWriteTSyncPos     = 0xf0,
-        MCT410_FuncWriteTSyncLen     =    6,
-
-        MCT410_FuncReadMReadPos      = 0x90,
-        MCT410_FuncReadMReadLen      =    3,  //  this is for the 410 KWH Only;  will need to be increased later
-
-        MCT410_FuncReadDemandPos     = 0x92,
-        MCT410_FuncReadDemandLen     =    6,  //  brings back recent demand, avg. voltage, and blink counter
-
-        MCT410_FuncReadPeakDemandPos = 0x93,
-        MCT410_FuncReadPeakDemandLen =    9,  //  peak demand, time of peak, current meter reading
-
-        MCT410_FuncReadVoltagePos    = 0x95,
-        MCT410_FuncReadVoltageLen    =   12,  //  max and min voltages
-
-        MCT410_FuncReadLPStatusPos   = 0x97,
-        MCT410_FuncReadLPStatusLen   =   12,
-
-        MCT410_FuncWriteDisconnectConfigPos = 0xFE,
-        MCT410_FuncWriteDisconnectConfigLen =    6,
-
-        MCT410_FuncReadDisconnectConfigPos = 0xFE,
-        MCT410_FuncReadDisconnectConfigLen =    9,
-
-        MCT410_FuncReadDisconnectStatusPos = 0xFE,
-        MCT410_FuncReadDisconnectStatusLen =    1,
-
-        MCT410_CommandConnect           = 0x4C,
-        MCT410_CommandDisconnect        = 0x4D,
-
-        MCT4XX_CommandFreezeOne         = 0x51,
-        MCT4XX_CommandFreezeTwo         = 0x52,
-
-        MCT4XX_CommandFreezeVoltageOne  = 0x59,
-        MCT4XX_CommandFreezeVoltageTwo  = 0x5A,
-
-        MCT4XX_CommandPowerfailReset    = 0x89,
-        MCT4XX_CommandReset             = 0x8A,
-
-        MCT410_VoltageOffset = 4
+        MCT4XX_PointOffset_PeakOffset   = 10,
+        MCT4XX_PointOffset_FrozenOffset = 10
     };
 
     enum
@@ -103,48 +150,53 @@ protected:
                                               //  if 81c99f60 is 1970
     };
 
-    enum ErrorClasses
-    {
-        EC_MeterReading    = 0x0001,
-        EC_DemandReading   = 0x0002,
-        EC_TOUDemand       = 0x0004,
-        EC_TOUFrozenDemand = 0x0008,
-        EC_LoadProfile     = 0x0010
-    };
+    typedef pair<double, PointQuality_t> data_pair;
+    typedef map<unsigned long, pair<PointQuality_t, int> > QualityMap;  //  the int will hold ErrorClasses OR'd together
 
-    enum ValueType
-    {
-        ValueType_Voltage,
-        ValueType_KW,
-        ValueType_Accumulator,
-        ValueType_Raw
-    };
+    data_pair getData(unsigned char *buf, int len, ValueType vt=ValueType_KW);
+    static  const QualityMap _errorQualities;
 
-    typedef map<unsigned long, pair<PointQuality_t, int> > QualityMap;
-    static  QualityMap _errorQualities;
+    bool _intervalsSent;
 
-    struct LPInfo
+    struct lp_info_t
     {
         unsigned long archived_reading;
         unsigned long current_request;
         unsigned long current_schedule;
     } _lp_info[MCT4XX_LPChannels];
 
-    struct LLPInterest
+    struct llp_interest_t
     {
         unsigned long time;
         int offset;
         int channel;
     } _llpInterest;
 
+    struct llp_peak_report_interest_t
+    {
+        unsigned long time;
+        int channel;
+        int period;
+        int command;
+    } _llpPeakInterest;
+
 private:
 
     static const DLCCommandSet _commandStore;
-    bool _intervalsSent;
 
-    static  QualityMap initErrorQualities( void );
+    static        QualityMap initErrorQualities( void );
+
+    typedef CtiDeviceMCT Inherited;
 
 public:
+
+    enum
+    {
+        UniversalAddress = 4194012,
+
+        FuncWrite_TSyncPos       = 0xf0,
+        FuncWrite_TSyncLen       =    6
+    };
 
     enum
     {
@@ -153,9 +205,6 @@ public:
         MCT410_StatusDisconnected           = 0x02,
         MCT410_StatusDisconnectedConfirmed  = 0x03
     };
-
-    typedef CtiDeviceMCT Inherited;
-    typedef pair<double, PointQuality_t> data_pair;
 
     CtiDeviceMCT410( );
     CtiDeviceMCT410( const CtiDeviceMCT410 &aRef );
@@ -174,24 +223,25 @@ public:
     virtual INT   calcAndInsertLPRequests( OUTMESS *&OutMessage, RWTPtrSlist< OUTMESS > &outList );
     virtual bool  calcLPRequestLocation( const CtiCommandParser &parse, OUTMESS *&OutMessage );
 
-    data_pair getData(unsigned char *buf, int len, ValueType vt=ValueType_KW);
-
     virtual INT executeGetValueLoadProfile(CtiRequestMsg *pReq, CtiCommandParser &parse, OUTMESS *&OutMessage, RWTPtrSlist<CtiMessage>&vgList, RWTPtrSlist<CtiMessage>&retList, RWTPtrSlist<OUTMESS>&outList);
 
     virtual INT ResultDecode( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist<OUTMESS> &outList );
 
-    INT decodeGetValueKWH         ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
-    INT decodeGetValueDemand      ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
-    INT decodeGetValuePeakDemand  ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
-    INT decodeGetValueVoltage     ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
-    INT decodeGetValueOutage      ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
-    INT decodeGetValueLoadProfile ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
-    INT decodeScanLoadProfile     ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
-    INT decodeGetStatusInternal   ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
-    INT decodeGetStatusLoadProfile( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
-    INT decodeGetConfigTime       ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
-    INT decodeGetConfigIntervals  ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
-    INT decodeGetConfigModel      ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
-    INT decodeGetConfigDisconnect ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
+    INT decodeGetValueKWH                   ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
+    INT decodeGetValueFrozenKWH             ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
+    INT decodeGetValueFrozen                ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
+    INT decodeGetValueDemand                ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
+    INT decodeGetValuePeakDemand            ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
+    INT decodeGetValueVoltage               ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
+    INT decodeGetValueOutage                ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
+    INT decodeGetValueLoadProfile           ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
+    INT decodeGetValueLoadProfilePeakReport ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
+    INT decodeScanLoadProfile               ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
+    INT decodeGetStatusInternal             ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
+    INT decodeGetStatusLoadProfile          ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
+    INT decodeGetConfigTime                 ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
+    INT decodeGetConfigIntervals            ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
+    INT decodeGetConfigModel                ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
+    INT decodeGetConfigDisconnect           ( INMESS *InMessage, RWTime &TimeNow, RWTPtrSlist< CtiMessage > &vgList, RWTPtrSlist< CtiMessage > &retList, RWTPtrSlist< OUTMESS > &outList );
 };
 #endif // #ifndef __DEV_MCT410_H__
