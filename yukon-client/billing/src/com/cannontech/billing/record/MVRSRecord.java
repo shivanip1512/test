4352722b9273c7d6c073d8a64c5cabd9b0f75f34
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2005-03-02 23:05:35 +0000

    Updated CUS, MTR, and RDG record processing per MVRS Host-Download Record Definitions Docment (and jeffW).

--- yukon-client/billing/src/com/cannontech/billing/record/MVRSRecord.java
+++ yukon-client/billing/src/com/cannontech/billing/record/MVRSRecord.java
@@ -1,9 +1,12 @@
 package com.cannontech.billing.record;
 
 import java.io.File;
+import java.sql.SQLException;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
+import java.util.Vector;
 
 import com.cannontech.billing.mvrsformatsegments.CustomerExtraRecord;
 import com.cannontech.billing.mvrsformatsegments.CustomerRecord;
@@ -66,7 +69,7 @@ public MVRSRecord()
  */
 public String dataToString()
 {
-	StringBuffer writeToFile = new StringBuffer();
+	Vector vectorOfLines = new Vector();
 	try
 	{
 	    File file = new File(getInputFile());
@@ -85,16 +88,29 @@ public String dataToString()
 			CTILogger.info("MISSING MVRS FILE HEADER - PROCESS TERMINATED");
 			return "";
 		}
-		writeToFile.append(processFileHeader());
+		vectorOfLines.add(processFileHeader());
+		
 		while (!(buffer.toString().substring(0,3).equalsIgnoreCase("FTR")))
 		{
 			inputLine++;
 			buffer.replace(0, buffer.length(), fromFile.readLine());
-			processMVRSField(buffer.toString(), writeToFile);			
+			vectorOfLines.add(processMVRSField(buffer.toString()));			
 		}
 		
 	} catch(Exception e){e.printStackTrace();}
 	
+	StringBuffer writeToFile = new StringBuffer();
+	for (int i = 0; i < vectorOfLines.size(); i++)
+	{
+		//Find the FHD record and update it!
+		String str = (String)vectorOfLines.get(i);
+		if( str.startsWith("FHD") )
+		{
+			vectorOfLines.set(i, processFileHeader());			
+		}
+		
+		writeToFile.append(vectorOfLines.get(i));	
+	}
 	return writeToFile.toString();
 }
 
@@ -253,11 +269,20 @@ public final HashMap getDeviceNameToRPHMap() throws java.sql.SQLException
  */
 public final String processCustomerRecord(String buffer) {
 	StringBuffer storage = new StringBuffer();
-	storage.append(buffer.substring(0,97));
-	storage.append(buffer.substring(117,118));
-	storage.append("N");
-	storage.append(buffer.substring(118,125));
-	storage.append(buffer.substring(97,117));
+	
+	storage.append("CUS");
+	storage.append(buffer.substring(3, 11));	//A/N8 RouteID
+	storage.append(buffer.substring(11, 14));	//N3 Number Meters
+	storage.append(buffer.substring(14, 34));	//A/N20 Account Number
+	storage.append(buffer.substring(34, 54));	//A 20 Account Name 
+	storage.append(buffer.substring(54, 94));	//A/N40 Address
+	storage.append("00" + buffer.substring(96, 97));	//N3 Group Code
+	storage.append(buffer.substring(117, 118));	//A1 Extra Customer Record
+	storage.append("N");	//A1 Exception Code ???Not sure if this should be "N" or " "
+	storage.append(buffer.substring(118, 122));	//A/N4 Segment Code
+	storage.append(buffer.substring(122, 124));	//A/N2 Utility ID
+	storage.append(buffer.substring(124, 125));	//A/N1 Customer Flag
+	storage.append(buffer.substring(97, 117));	//A/N20 Addition Customer information
 	storage.append("\r\n");
 	numberCustomers++;
 	return storage.toString();
@@ -270,9 +295,19 @@ public final String processCustomerRecord(String buffer) {
  */
 public final String processFileHeader() {
 	StringBuffer storage = new StringBuffer();
+	DecimalFormat countFormat = new DecimalFormat("000000");
 	storage.append("FHD");
-	for (int x=0; x<123; x++)
-		storage.append(" "); // Reserved/undefined spaces
+	storage.append(" ");	//A1 New Service Indicator
+	storage.append(" ");	//A1 Future Use
+	storage.append(" ");	//A1 Change Meter Indicator
+	storage.append(" ");	//A1 Future Use
+	storage.append("000");	//N3 Number Routes (N/A, Yukon doesn't process these records)
+	storage.append(countFormat.format(numberCustomers));	//N6 Number Customers
+	storage.append(countFormat.format(numberMeters));	//N6 Number Meters
+	storage.append(countFormat.format(numberReads));	//N6 Number Reads
+	
+	while (storage.length() < 128)
+		storage.append(" "); //Extended Route Header Flag, Pad, Reserved (Not Supported by Yukon)
 	storage.append("\r\n");
 	return storage.toString();
 }
@@ -311,18 +346,28 @@ public final String processFileTrailer() {
  */
 public final String processMeterRecord(String buffer) {
 	StringBuffer storage = new StringBuffer();
-	storage.append(buffer.substring(0,17));
-	storage.append("000000");
-	storage.append(buffer.substring(23,69));
-	storage.append(buffer.substring(61,69));
-	storage.append(buffer.substring(89,99));
-	storage.append(buffer.substring(100,101));
-	storage.append("     "); // data not read in
-	storage.append(buffer.substring(101,106));
-	storage.append("   ");
-	storage.append(buffer.substring(69,89));
-	storage.append("     ");
+	storage.append("MTR");
+	storage.append(buffer.substring(3, 11));	//A/N8 Route Number
+	storage.append(buffer.substring(11, 14));	//N3 Number Reads
+	storage.append("00" + buffer.substring(16, 17));	//N3 Group Code
+	storage.append("000" + "000");	//N3 Future Use, N3 Future use
+	storage.append("00" + buffer.substring(25, 26));	//N3 Survey Code 1
+	storage.append("00" + buffer.substring(28, 29));	//N3 Survey Code 2
+	storage.append(buffer.substring(29, 30));	//A1 Bill Code
+	storage.append(buffer.substring(30, 31));	//A1 Meter Status
+	storage.append(buffer.substring(31, 45));	//A/N14 Optical Probe Record ID
+	storage.append(buffer.substring(45, 57));	//A/N12 Meter Number
+	storage.append("00" + buffer.substring(59, 61));	//N4 Meter Type
+	storage.append(buffer.substring(61, 69));	//N8 Meter Read Sequence
+	storage.append(buffer.substring(61, 69));	//N8 New Read Sequence
+	storage.append("0" + buffer.substring(90, 92));	//N3 Location
+	storage.append("0" + buffer.substring(93, 95));	//N3 Meter Instruction 1
+	storage.append("0" + buffer.substring(96, 98));	//N3 Meter Instruction 2
+	storage.append(buffer.substring(98, 99));	//N1 Special message Display
+	storage.append(buffer.substring(100, 101));	//A1 Special Message indicator
+	storage.append(" ");	//A1 Trouble Message Indicator
 	storage.append("\r\n");
+	
 	meterRecord.meterNumber = null;
 	meterRecord.meterNumber = new StringBuffer(buffer.substring(45,57).trim());
 	numberMeters++;
@@ -333,23 +378,33 @@ public final String processMeterRecord(String buffer) {
  * Creation date: (6/1/00 10:22:36 AM)
  * @param indicator java.lang.String
  */
-public final void processMVRSField(String buffer, StringBuffer writeToFile) {
+public final String processMVRSField(String buffer){//, StringBuffer writeToFile) {
+	String record = "";
 	if (buffer.regionMatches(true, 0, "CHD", 0, 3) || buffer.regionMatches(true, 0, "CTR", 0, 3))
-		return; // not implemented
-	if (buffer.regionMatches(true, 0, "FHD", 0, 3))
-		writeToFile.append(processFileHeader());
+		record = ""; // not implemented
+		
+	else if (buffer.regionMatches(true, 0, "FHD", 0, 3))
+		record = processFileHeader();
+		
 	else if (buffer.regionMatches(true, 0, "FTR", 0, 3))
-		writeToFile.append(processFileTrailer());
+		record = processFileTrailer();
+		
 	else if (buffer.regionMatches(true, 0, "RHD", 0, 3) || buffer.regionMatches(true, 0, "RTR", 0, 3))
-		writeToFile.append(processRouteHeaderAndTrailer(buffer));
+		record = processRouteHeaderAndTrailer(buffer);
+		
 	else if (buffer.regionMatches(true, 0, "CUS", 0, 3))
-		writeToFile.append(processCustomerRecord(buffer));
+		record = processCustomerRecord(buffer);
+		
 	else if (buffer.regionMatches(true, 0, "MTR", 0, 3))
-		writeToFile.append(processMeterRecord(buffer));
+		record = processMeterRecord(buffer);
+		
 	else if (buffer.regionMatches(true, 0, "RDG", 0, 3))
-		writeToFile.append(processReadingRecord(buffer));
+		record = processReadingRecord(buffer);
+		
 	// others not implemented, as in original C code
 	else CTILogger.info("MISSING/UNSUPPORTED MVRS RECORD INDICATOR, LINE " + Integer.toString(inputLine) + " - LINE IGNORED");
+	
+	return record;
 }
 /**
  * Handles a reading record from the input file
@@ -362,40 +417,62 @@ public final void processMVRSField(String buffer, StringBuffer writeToFile) {
  */
 public final String processReadingRecord(String buffer) {
 	StringBuffer storage = new StringBuffer();
-	try {
-		storage.append(buffer.substring(0,15));
-		storage.append(buffer.substring(16,26));
-		storage.append(buffer.substring(30,31));
-		storage.append(buffer.substring(61,68));
-		RawPointHistory dummyRPH = (RawPointHistory)getDeviceNameToRPHMap().get(meterRecord.meterNumber);
-		String reading = "";
-		if( dummyRPH != null)
-		    reading = dummyRPH.getValue().toString();
-		for (int x=0; x<10-reading.length(); x++)
-			storage.append("0");
-		storage.append(reading);
+	
+	DecimalFormat readingFormat = new DecimalFormat("0000000000");
+	
+	storage.append("RDG");
+	storage.append(buffer.substring(3, 11));	//A/N8 RouteNumber
+	storage.append(buffer.substring(11, 15));	//A/N4 Text Prompt
+	storage.append(buffer.substring(16, 17));	//A1 Read Direction
+	storage.append(buffer.substring(17, 20));	//N3 Compare Code
+	storage.append(buffer.substring(20, 23));	//N3 Validation Code
+	storage.append("0" + buffer.substring(24, 26));	//N3 Channel Number
+	storage.append(buffer.substring(30, 31));	//A1 Read Method
+	storage.append(buffer.substring(61, 67));	//N6 Meter Constant
+	storage.append(buffer.substring(67, 68));	//N1 Constant Flag
+	
+	RawPointHistory dummyRPH = null;
+	try
+	{
+		dummyRPH = (RawPointHistory)getDeviceNameToRPHMap().get(meterRecord.meterNumber);
+	}
+	catch (SQLException e1)	{ }
+	if( dummyRPH != null)
+	{
+		storage.append(readingFormat.format(dummyRPH.getValue()));	//N10 Reading
+		storage.append("0000");	//N4 Read Order
+		storage.append(DATE_FORMAT.format(dummyRPH.getTimeStamp().getTime()));	//D8 Read Date
+		storage.append(TIME_FORMAT.format(dummyRPH.getTimeStamp().getTime()));	//T6 Read Time
+	}
+	else	//Defaults for the above fields
+	{
+		storage.append("0000000000");
 		storage.append("0000");
-		if( dummyRPH != null)
-		    storage.append(DATE_FORMAT.format(dummyRPH.getTimeStamp().getTime()));
-		else
-		    storage.append("        ");
-		if( dummyRPH != null)
-		    storage.append(TIME_FORMAT.format(dummyRPH.getTimeStamp().getTime()));
-		else
-		    storage.append("      ");
-		storage.append("N");
-		storage.append("000000000000");
-		storage.append("N");
-		storage.append("         "); // data not present
-		storage.append("00000"); // data not present
-		storage.append(buffer.substring(26,30));
-		storage.append("  "); // data not present
-		storage.append("0"); // data not present
-		storage.append(buffer.substring(61,67));
-		storage.append(buffer.substring(31,41));
-		storage.append("           "); // "Reserved" spaces
-		storage.append("\r\n");
-	} catch (java.sql.SQLException e) {e.printStackTrace();}
+		storage.append("        ");
+		storage.append("      ");
+	}
+	
+	storage.append("N");	//A1 Read code
+	storage.append("000");	//N3 Read Reason
+	storage.append("0");	//N1 Read Audit Result
+	storage.append("000");	//N3 Trouble Code 1
+	storage.append("000");	//N3 Trouble Code 2
+	storage.append("0");	//N1 Audit Counter
+	storage.append("0");	//N1 Clear Counter
+	storage.append("N");	//A1 Unporcessed Read Flag
+	storage.append("         ");	//A/N9 Meter ReaderID (N/A)
+	storage.append("00000");	//N5 Elapsed Time
+	storage.append(buffer.substring(26, 28));	//N2 New Number Dials
+	storage.append(buffer.substring(28, 30));	//N2 New Number Decimals
+	storage.append(" ");	//A1 New Read Method
+	storage.append(" ");	//A1 Meter Constant Verified
+	storage.append("0");	//N1 HHF Position
+	storage.append(buffer.substring(61, 67));	//N6 New Meter Constant
+	storage.append("0" + buffer.substring(76, 78));	//N3 Read Type
+	storage.append(buffer.substring(31, 41));	//N10 Previous Reading
+	storage.append("           ");	//A/N11	Future Use
+	storage.append("\r\n");
+	
 	numberReads++;
 	return storage.toString();
 }
