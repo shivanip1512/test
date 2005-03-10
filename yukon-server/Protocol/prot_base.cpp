3e68c70a4d7b654c2d1151838042b533aa2f0317
Fisher, Matthew S <MatthewSFisher@Eaton.com>
2005-03-10 21:15:17 +0000

    changed to namespaces, changed from an RW list to a vector

--- yukon-server/Protocol/prot_base.cpp
+++ yukon-server/Protocol/prot_base.cpp
@@ -8,8 +8,8 @@
 *
 * PVCS KEYWORDS:
 * ARCHIVE      :  $Archive$
-* REVISION     :  $Revision: 1.4 $
-* DATE         :  $Date: 2005/02/10 23:23:57 $
+* REVISION     :  $Revision: 1.5 $
+* DATE         :  $Date: 2005/03/10 21:15:17 $
 *
 * Copyright (c) 2002 Cannon Technologies Inc. All rights reserved.
 *-----------------------------------------------------------------------------*/
@@ -21,16 +21,20 @@
 #include "porter.h"  //  for the RESULT EventCode flag
 #include "prot_base.h"
 
-CtiProtocolBase::CtiProtocolBase()  {}
+namespace Cti       {
+namespace Protocol  {
 
-CtiProtocolBase::CtiProtocolBase(const CtiProtocolBase &aRef)
+
+Interface::Interface()  {}
+
+Interface::Interface(const Interface &aRef)
 {
     *this = aRef;
 }
 
-CtiProtocolBase::~CtiProtocolBase() {}
+Interface::~Interface() {}
 
-CtiProtocolBase &CtiProtocolBase::operator=(const CtiProtocolBase &aRef)
+Interface &Interface::operator=(const Interface &aRef)
 {
     if( this != &aRef )
     {
@@ -40,17 +44,23 @@ CtiProtocolBase &CtiProtocolBase::operator=(const CtiProtocolBase &aRef)
 }
 
 
-int CtiProtocolBase::generate( CtiXfer &xfer )              {   return 0;   }
-int CtiProtocolBase::decode( CtiXfer &xfer, int status )    {   return 0;   }
+int Interface::generate( CtiXfer &xfer )              {   return 0;   }
+int Interface::decode( CtiXfer &xfer, int status )    {   return 0;   }
 
-int CtiProtocolBase::commOut( OUTMESS *&OutMessage )   {   return NoMethod;    }
-int CtiProtocolBase::commIn ( INMESS   *InMessage  )   {   return NoMethod;    }
+int Interface::commOut( OUTMESS *&OutMessage )   {   return NoMethod;    }
+int Interface::commIn ( INMESS   *InMessage  )   {   return NoMethod;    }
 
-int CtiProtocolBase::sendCommRequest( OUTMESS *&OutMessage, RWTPtrSlist< OUTMESS > &outList )   {   return NoMethod;    }
-int CtiProtocolBase::recvCommResult ( INMESS *InMessage,    RWTPtrSlist< OUTMESS > &outList )   {   return NoMethod;    }
+int Interface::sendCommRequest( OUTMESS *&OutMessage, RWTPtrSlist< OUTMESS > &outList )   {   return NoMethod;    }
+int Interface::recvCommResult ( INMESS *InMessage,    RWTPtrSlist< OUTMESS > &outList )   {   return NoMethod;    }
 
-int CtiProtocolBase::recvCommRequest( OUTMESS  *OutMessage )   {   return NoMethod;    }
-int CtiProtocolBase::sendCommResult ( INMESS   *InMessage  )   {   return NoMethod;    }
+int Interface::recvCommRequest( OUTMESS  *OutMessage )   {   return NoMethod;    }
+int Interface::sendCommResult ( INMESS   *InMessage  )   {   return NoMethod;    }
 
-bool CtiProtocolBase::isTransactionComplete( void ) {   return true;    }
+bool Interface::isTransactionComplete( void ) {   return true;    }
 
+bool Interface::hasInboundPoints( void )      {   return false;   }
+void Interface::getInboundPoints( vector< CtiPointDataMsg > &pointList ) {   }
+
+
+}
+}
