f1fbf78bf160b112f514b17e016f1a21e601fc89
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2005-03-25 15:02:48 +0000

    Added "groupings" for activity log actions, used for reporting by "groups"

--- yukon-client/common/src/com/cannontech/database/data/activity/ActivityLogActions.java
+++ yukon-client/common/src/com/cannontech/database/data/activity/ActivityLogActions.java
@@ -15,6 +15,7 @@ package com.cannontech.database.data.activity;
 public interface ActivityLogActions
 {
 	/* Login/out Actions Strings to log */
+	/* Please add all new action Strings to appropriate "group" below */
 	public static final String LOGIN_WEB_ACTIVITY_ACTION = "LOG IN (WEB)";
 	public static final String LOGIN_CLIENT_ACTIVITY_ACTION = "LOG IN (CLIENT)";
 	public static final String LOGOUT_ACTIVITY_LOG = "LOG OUT";
@@ -47,4 +48,52 @@ public interface ActivityLogActions
 	public static final String THERMOSTAT_SCHEDULE_ACTION = "Thermostat Schedule";
 	
 	public static final String SCAN_DATA_NOW_ACTION = "Scan Meter Data Now";
+	
+	/* Action groups*/
+	public static final String[] LOGIN_ACTION_GROUP = new String[]{
+		LOGIN_CLIENT_ACTIVITY_ACTION, LOGIN_FAILED_ACTIVITY_LOG, LOGIN_WEB_ACTIVITY_ACTION
+	};
+	public static final String[] MANUAL_LMPROGRAM_ACTION_GROUP = new String[]{
+		MANUAL_LMPROGRAM_START_ACTION, MANUAL_LMPROGRAM_STOP_ACTION
+	};
+	public static final String[] PROGRAM_ACTION_GROUP = new String[]{
+		PROGRAM_CANCEL_SCHEDULED_ACTION, PROGRAM_ENROLLMENT_ACTION, 
+		PROGRAM_OPT_OUT_ACTION, PROGRAM_REENABLE_ACTION 
+	};
+	public static final String[] HARDWARE_ACTION_GROUP = new String[]{
+		HARDWARE_CONFIGURATION_ACTION, HARDWARE_DISABLE_ACTION, 
+		HARDWARE_ENABLE_ACTION, HARDWARE_SAVE_CONFIG_ONLY_ACTION,
+		HARDWARE_SAVE_TO_BATCH_ACTION, HARDWARE_SEND_BATCH_CONFIG_ACTION
+	};
+	public static final String[] INVENTORY_ACTION_GROUP = new String[]{
+		INVENTORY_ADD_RANGE, INVENTORY_CONFIG_RANGE, INVENTORY_DELETE_RANGE, INVENTORY_UPDATE_RANGE
+	};
+	public static final String[] THERMOSTAT_ACTION_GROUP = new String[]{
+		THERMOSTAT_MANUAL_ACTION, THERMOSTAT_SCHEDULE_ACTION
+	};
+	public static final String[] OTHER_ACTION_GROUP = new String[]{
+		SCAN_DATA_NOW_ACTION
+	};
+	
+	/* Enums for the action group string arrays*/
+	public static final int ALL_ACTIONS = 0;
+	public static final int HARDWARE_ACTIONS = 1;
+	public static final int INVENTORY_ACTIONS = 2;
+	public static final int LOGIN_ACTIONS = 3;
+	public static final int MANUAL_LMPROGRAM_ACTIONS = 4;
+	public static final int PROGRAM_ACTIONS = 5;
+	public static final int THERMOSTAT_ACTIONS = 6;
+	public static final int OTHER_ACTIONS = 7;
+	/* Array of String values for the Action Groups, indexed by the above enums */
+	public static final String[] ACTION_GROUPS_STRING = new String[]{
+		"All Actions",  "Hardware Actions", "Inventory Actions", "Login Actions", 
+		"Manual Program Actions", "Program Actions", "Thermostat Actions", "Other"
+	};
+
+	/* Array of String Arrays (Action groups), indexed by the above enums*/	
+	public static final String[][] ACTION_GROUPS = new String[][]{
+		null, HARDWARE_ACTION_GROUP, INVENTORY_ACTION_GROUP, LOGIN_ACTION_GROUP, 
+		MANUAL_LMPROGRAM_ACTION_GROUP, PROGRAM_ACTION_GROUP, THERMOSTAT_ACTION_GROUP, OTHER_ACTION_GROUP
+	};
 }
+
