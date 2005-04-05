0ec0c6f6e76e6e6ef3ecd3268b0803ea30ff18a4
Neuharth, Ryan <>
2005-04-05 15:54:11 +0000

    Force the yukon-database project to use the same ant version as yukon-client

--- yukon-database/ant.bat
+++ yukon-database/ant.bat
@@ -1,10 +1,10 @@
 @echo off
-REM $Id: ant.bat,v 1.1 2005/04/05 15:47:11 cplender Exp $
+REM
+REM This duplicates the ant.bat file in yukon-client, allowing this project to
+REM   use the same version of ant that yukon-client uses.
+REM
 set JAVA=%JAVA_HOME%\bin\java
 set cp=..\yukon-client\build\ant.jar;%JAVA_HOME%\lib\tools.jar;
 for %%i in (..\yukon-client\build\*.jar) do call ..\yukon-client\build\cp.bat %%i
-echo:
-echo %cp%
-echo:
 %JAVA% -Xmx256M -classpath %CP% -Dant.home=. org.apache.tools.ant.Main %1 %2 %3 %4 %5 %6 %7 %8 %9 -buildfile build.xml
 
