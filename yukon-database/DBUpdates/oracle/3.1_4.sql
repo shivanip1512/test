2fd77e7427af9605a441464956521df5175ec271
Neuharth, Ryan <>
2005-05-09 17:45:29 +0000

    Fixed modify column statement

--- yukon-database/DBUpdates/oracle/3.1_4.sql
+++ yukon-database/DBUpdates/oracle/3.1_4.sql
@@ -16,7 +16,7 @@ alter table DeviceTapPagingSettings modify POSTPath not null;
 
 insert into FDRInterface values (22, 'LIVEDATA','Receive', 'f' );
 
-alter table FDRINTERFACEOPTION modify column OPTIONVALUES varchar2(256);
+alter table FDRINTERFACEOPTION modify OPTIONVALUES varchar2(256);
 
 insert into FDRInterfaceOption values(22, 'Address', 1, 'Text', '(none)' );
 insert into FDRInterfaceOption values(22, 'Data Type', 2, 'Combo', 'Data_RealExtended,Data_DiscreteExtended,Data_StateExtended,Data_RealQ,Data_DiscreteQ,Data_State,Data_Discrete,Data_Real,Data_RealQTimeTag,Data_StateQTimeTag,Data_DiscreteQTimeTag' );
