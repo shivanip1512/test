b4019c05b382fd0b9ba586f352706262a047315e
Neuharth, Ryan <>
2005-05-10 14:20:41 +0000

    Added next dbupdate files

---
+++ yukon-database/DBUpdates/sqlserver/3.2_0.sql
@@ -0,0 +1,44 @@
+/******************************************/
+/**** SQLServer 2000 DBupdates         ****/
+/******************************************/
+
+delete from yukonuserrole where rolepropertyid = -10105 or rolepropertyid = -10106 or rolepropertyid = -10106 or rolepropertyid = -10112;
+go
+insert into YukonRoleProperty values(-70005,-700,'Mapping Interface','amfm','Optional interface to the AMFM mapping system');
+insert into YukonRoleProperty values(-70006,-700,'CBC Creation Name','CBC %PAOName%','What text will be added onto CBC names when they are created');
+insert into YukonRoleProperty values(-70007,-700,'PFactor Decimal Places','1','How many decimal places to show for real values for PowerFactor');
+insert into YukonRoleProperty values(-70008,-700,'CBC Allow OVUV','false','Allows users to toggle OV/UV usage on capbanks');
+go
+
+update yukongrouprole set rolepropertyid = -70005, roleid = -700 where rolepropertyid = -10105
+update yukongrouprole set rolepropertyid = -70006, roleid = -700 where rolepropertyid = -10106;
+update yukongrouprole set rolepropertyid = -70007, roleid = -700 where rolepropertyid = -10109;
+update yukongrouprole set rolepropertyid = -70008, roleid = -700 where rolepropertyid = -10112;
+go
+
+delete from yukonroleproperty where rolepropertyid = -10105 or rolepropertyid = -10106 or rolepropertyid = -10106 or rolepropertyid = -10112;
+go
+
+update state set foregroundcolor = 10 where stategroupid = 3 and rawstate = 2;
+
+insert into YukonRoleProperty values(-70009,-700,'CBC Refresh Rate','60','The rate, in seconds, all CBC clients reload data from the CBC server');
+go
+
+
+
+
+
+
+
+/******************************************************************************/
+/* Run the Stars Update if needed here */
+/* Note: DBUpdate application will ignore this if STARS is not present */
+/* @include StarsUpdate */
+/******************************************************************************/
+
+
+/**************************************************************/
+/* VERSION INFO                                               */
+/*   Automatically gets inserted from build script            */
+/**************************************************************/
+/* __YUKON_VERSION__ */
