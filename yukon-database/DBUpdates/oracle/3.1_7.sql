b43514e48b27d3d5bb112826657a8a8ae5f6c167
Neuharth, Ryan <>
2005-05-18 19:51:23 +0000

    Added new build scripts

---
+++ yukon-database/DBUpdates/oracle/3.1_7.sql
@@ -0,0 +1,26 @@
+/******************************************/
+/**** Oracle 9.2 DBupdates             ****/
+/******************************************/
+
+
+
+
+
+
+
+
+
+
+
+/******************************************************************************/
+/* Run the Stars Update if needed here */
+/* Note: DBUpdate application will ignore this if STARS is not present */
+/* @include StarsUpdate */
+/******************************************************************************/
+
+
+/**************************************************************/
+/* VERSION INFO                                               */
+/*   Automatically gets inserted from build script            */
+/**************************************************************/
+insert into CTIDatabase values('3.1', 'Ryan', '25-MAY-2005', 'Manual version insert done', 7);
---
+++ yukon-database/DBUpdates/sqlserver/3.1_7.sql
@@ -0,0 +1,25 @@
+/******************************************/
+/**** SQLServer 2000 DBupdates         ****/
+/******************************************/
+
+
+
+
+
+
+
+
+
+
+/******************************************************************************/
+/* Run the Stars Update if needed here */
+/* Note: DBUpdate application will ignore this if STARS is not present */
+/* @include StarsUpdate */
+/******************************************************************************/
+
+
+/**************************************************************/
+/* VERSION INFO                                               */
+/*   Automatically gets inserted from build script            */
+/**************************************************************/
+insert into CTIDatabase values('3.1', 'Ryan', '25-MAY-2005', 'Manual version insert done', 7);
