721de309ce6e042895c8d0c193e2ed79c54b5af3
Mack, Tom <>
2005-06-01 00:38:18 +0000

    Just so it builds...

--- yukon-client/notification/src/com/cannontech/notif/test/TestCaller.java
+++ yukon-client/notification/src/com/cannontech/notif/test/TestCaller.java
@@ -1,5 +1,9 @@
 package com.cannontech.notif.test;
 
+import java.io.File;
+
+import javax.swing.*;
+
 import com.cannontech.clientutils.CTILogger;
 import com.cannontech.notif.voice.*;
 
@@ -11,26 +15,44 @@ public class TestCaller {
         
         
         Dialer dialer = new TestDialer();
-//        Dialer dialer = new VocomoDialer();
+        //Dialer dialer = new VocomoDialer("10.100.2.100", "notification");
         dialer.setPhonePrefix("9");
-        CallPool callPool = new CallPool(dialer, 2, 20000);
-        NotificationQueue queue = new NotificationQueue();
+        CallPool callPool = new CallPool(dialer, 2, 180000);
+        NotificationQueue queue = new NotificationQueue(callPool);
         
-        WorkerThread worker = new WorkerThread(queue, callPool);
-        worker.start();
+        //WorkerThread worker = new WorkerThread(queue, callPool);
+        //worker.start();
+        
+        // select base directory
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        chooser.showOpenDialog(null);
+        File baseStlDirectory = chooser.getSelectedFile();
+        System.out.println("URI: " + baseStlDirectory.toURI());
+        try {
+            System.out.println("URL: " + baseStlDirectory.toURL());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         
         try {
             Thread.sleep(1500);
             queue.add(new SingleNotification(guy1, "mesg 1"));
             Thread.sleep(1500);
-            queue.add(new SingleNotification(guy3, "mesg 2"));
+            //queue.add(new SingleNotification(guy3, "mesg 2"));
             Thread.sleep(1500);
-            queue.add(new SingleNotification(guy3, "mesg 3"));
-//            System.in.read();
-            Thread.sleep(30000);
-            CTILogger.info("Shuting down worker");
-            worker.shutdown();
+            //queue.add(new SingleNotification(guy3, "mesg 3"));
+            System.in.read();
+            //Thread.sleep(30000);
+            //CTILogger.info("Shuting down worker");
+            //worker.shutdown();
             CTILogger.info("Shuting down pool");
+            queue.shutdown();
             callPool.shutdown();
             CTILogger.info("All shutdown!");
             
