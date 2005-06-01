705dc3292ec8ddf3449bc60e6c34242b9db8fb5e
Mack, Tom <>
2005-06-01 00:34:15 +0000

    Updates to handle new LM message.

--- yukon-client/notification/src/com/cannontech/notif/handler/LoadManagementMessageHandler.java
+++ yukon-client/notification/src/com/cannontech/notif/handler/LoadManagementMessageHandler.java
@@ -1,17 +1,13 @@
 package com.cannontech.notif.handler;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Iterator;
-import java.util.List;
 
-import com.cannontech.clientutils.CTILogger;
-import com.cannontech.database.Transaction;
-import com.cannontech.database.TransactionException;
 import com.cannontech.database.cache.functions.NotificationGroupFuncs;
 import com.cannontech.database.cache.functions.PAOFuncs;
-import com.cannontech.database.data.device.lm.LMProgramDirect;
-import com.cannontech.database.data.lite.LiteFactory;
 import com.cannontech.database.data.lite.LiteNotificationGroup;
-import com.cannontech.database.db.device.lm.LMDirectNotificationGroupList;
+import com.cannontech.database.data.lite.LiteYukonPAObject;
 import com.cannontech.message.util.Message;
 import com.cannontech.notif.message.NotifLMControlMsg;
 import com.cannontech.notif.outputs.Notification;
@@ -21,6 +17,9 @@ import com.cannontech.notif.outputs.OutputHandlerHelper;
  * 
  */
 public class LoadManagementMessageHandler extends NotifHandler {
+
+    private static final DateFormat _dateFormater = new SimpleDateFormat("EEEE, MMMM d"); // e.g. "Tuesday May 31"
+    private static final DateFormat _timeFormater = new SimpleDateFormat("h:m a"); // e.g. "3:45 PM"
 
     public LoadManagementMessageHandler(OutputHandlerHelper helper) {
         super(helper);
@@ -36,29 +35,20 @@ public class LoadManagementMessageHandler extends NotifHandler {
         // building the Notification object is the main work of
         // this function
         Notification notif = new Notification("loadmanagement");
+        
+        notif.put("starttime", _timeFormater.format(msg.startTime));
+        notif.put("startdate", _dateFormater.format(msg.startTime));
+        notif.put("stoptime", _timeFormater.format(msg.stopTime));
+        notif.put("stopdate", _dateFormater.format(msg.stopTime));
+        LiteYukonPAObject liteYukonPAO = PAOFuncs.getLiteYukonPAO(msg.programId);
+        notif.put("programname", liteYukonPAO.getPaoName());
 
-        int programID = msg.programId;
-
-        LMProgramDirect programObject = 
-            (LMProgramDirect) LiteFactory.createDBPersistent(PAOFuncs.getLiteYukonPAO(programID));
-        try {
-            Transaction t = Transaction.createTransaction(Transaction.RETRIEVE,
-                                                          programObject);
-            t.execute();
-
-            List notifyGroupVector = programObject.getLmProgramDirectNotifyGroupVector();
-            for (Iterator iter = notifyGroupVector.iterator(); iter.hasNext();) {
-                LMDirectNotificationGroupList notificationGroup = 
-                    (LMDirectNotificationGroupList) iter.next();
-                
-                int notificationGroupId = notificationGroup.getNotificationGroupID().intValue();
-                LiteNotificationGroup liteNotificationGroup = 
-                    NotificationGroupFuncs.getLiteNotificationGroup(notificationGroupId);
-
-                outputNotification(notif, liteNotificationGroup);
-            }
-        } catch (TransactionException e) {
-            CTILogger.error("Unable to handle message " + msg, e);
+        for (Iterator iter = msg.notifGroupIds.iterator(); iter.hasNext();) {
+            Integer notifGroupId = (Integer) iter.next();
+            LiteNotificationGroup notificationGroup = 
+                NotificationGroupFuncs.getLiteNotificationGroup(notifGroupId.intValue());
+            
+            outputNotification(notif, notificationGroup);
         }
     }
 
--- yukon-client/notification/src/com/cannontech/notif/message/NotifLMControlMsg.java
+++ yukon-client/notification/src/com/cannontech/notif/message/NotifLMControlMsg.java
@@ -1,7 +1,6 @@
 package com.cannontech.notif.message;
 
-import java.util.Date;
-import java.util.Vector;
+import java.util.*;
 
 import com.cannontech.message.util.Message;
 
@@ -13,7 +12,7 @@ public class NotifLMControlMsg extends Message {
 	public static final int FINISHING_CONTROL_NOTIFICATION = 3;
 	
 	// Vector<int> contains notification group ids
-	public Vector notifGroupIds = new Vector(2);
+	public List notifGroupIds = new Vector(2);
 	public int notifType;
     public int programId;
     public Date startTime;
