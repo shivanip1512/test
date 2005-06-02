93160014d0647748913430d4619981049fd9b22e
Neuharth, Ryan <>
2005-06-02 15:23:46 +0000

    initial add

---
+++ yukon-database/DBUpdates/sqlserver/3.1_8.sql
@@ -0,0 +1,22 @@
+/******************************************/
+/**** SQLServer 2000 DBupdates         ****/
+/******************************************/
+
+
+
+
+
+
+
+/******************************************************************************/
+/* Run the Stars Update if needed here */
+/* Note: DBUpdate application will ignore this if STARS is not present */
+/* @include StarsUpdate */
+/******************************************************************************/
+
+
+/**************************************************************/
+/* VERSION INFO                                               */
+/*   Automatically gets inserted from build script            */
+/**************************************************************/
+insert into CTIDatabase values('3.1', 'Ryan', '5-JUN-2005', 'Manual version insert done', 8);
