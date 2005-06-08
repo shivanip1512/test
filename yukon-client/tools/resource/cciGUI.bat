af4a69cf7bbdf93ac84895e772fc2433ceec1590
Solberg, Aaron <>
2005-06-08 20:27:58 +0000

    *** empty log message ***

---
+++ yukon-client/tools/resource/cciGUI.bat
@@ -0,0 +1,1 @@
+java -Dsun.java2d.noddraw=true -Djava.class.path=.;%YUKON_BASE%/SERVER/WEB;CapControlImporter.jar;common.jar;yukonappserver.jar;jtds.jar;classes12.jar;j2ee.jar;log4j-1.2.4.jar;SqlServer.jar; com.cannontech.capcontrol.CapControlImporter gui
