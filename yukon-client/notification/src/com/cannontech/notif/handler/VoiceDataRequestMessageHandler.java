f3981e5afd51eca70d016ddb9d90e0dfcd5b01ba
Mack, Tom <>
2005-06-08 22:55:15 +0000

    Added more of the implementation.

--- yukon-client/notification/src/com/cannontech/notif/handler/VoiceDataRequestMessageHandler.java
+++ yukon-client/notification/src/com/cannontech/notif/handler/VoiceDataRequestMessageHandler.java
@@ -1,15 +1,16 @@
 package com.cannontech.notif.handler;
 
-import java.io.IOException;
 import java.io.StringWriter;
 
 import org.jdom.Document;
 import org.jdom.output.Format;
 import org.jdom.output.XMLOutputter;
 
+import com.cannontech.clientutils.CTILogger;
 import com.cannontech.message.server.ServerRequestMsg;
 import com.cannontech.message.util.Message;
 import com.cannontech.notif.message.VoiceDataRequestMsg;
+import com.cannontech.notif.message.VoiceDataResponseMsg;
 import com.cannontech.notif.outputs.VoiceHandler;
 
 public class VoiceDataRequestMessageHandler extends MessageHandler {
@@ -31,29 +32,29 @@ public class VoiceDataRequestMessageHandler extends MessageHandler {
     }
 
     public void handleMessage(Message msg_) {
-        ServerRequestMsg req = (ServerRequestMsg) msg_;
-        VoiceDataRequestMsg msg = (VoiceDataRequestMsg) req.getPayload();
         
-        Object callData = _voiceHandler.getCallData(msg.token);
-        if (callData instanceof Document) {
-            Document xmlDoc = (Document) callData;
-            try {
-                //TODO if brent wants an XML fragment, this could just process the root node
-                
-                XMLOutputter out = new XMLOutputter(Format.getCompactFormat());
-                StringWriter stringWriter = new StringWriter();
-                out.output(xmlDoc, stringWriter);
-                
-                // send good response
-                
-                return;
-            } catch (IOException e) {
-                // log error
-            }
+        ServerRequestMsg req = (ServerRequestMsg) msg_;
+        VoiceDataRequestMsg reqMsg = (VoiceDataRequestMsg) req.getPayload();
+
+        try {
+            Document xmlDoc = _voiceHandler.getCallData(reqMsg.token);
+
+            XMLOutputter out = new XMLOutputter(Format.getCompactFormat());
+            StringWriter stringWriter = new StringWriter();
+            out.output(xmlDoc.getRootElement(), stringWriter);
+            
+            VoiceDataResponseMsg rspMsg = new VoiceDataResponseMsg();
+            rspMsg.xmlData = stringWriter.toString();
+            
+            req.createResponseMsg(rspMsg);
+            //TODO send the rspMsg
+
+            return;
+        } catch (Exception e) {
+            CTILogger.warn("Unable to return VoiceXML data for call (token=" + reqMsg.token + ")", e);
         }
         // send bad response
-        
-        return;
+
     }
 
 }
