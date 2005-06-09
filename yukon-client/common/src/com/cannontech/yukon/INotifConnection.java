3d975e71f9448af49e8692cddf98c906303dec96
Neuharth, Ryan <>
2005-06-09 15:49:53 +0000

    Added a NotifClientConnection into the connection Pool

---
+++ yukon-client/common/src/com/cannontech/yukon/INotifConnection.java
@@ -0,0 +1,14 @@
+package com.cannontech.yukon;
+
+import com.cannontech.yukon.conns.NotifRequestException;
+
+/**
+ * Interface for connections to the Notifcation Server
+ *
+ */
+public interface INotifConnection extends IServerConnection 
+{
+	
+	public String requestMessage( String token ) throws NotifRequestException;
+
+}
