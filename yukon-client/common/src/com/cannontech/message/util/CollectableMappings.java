318e0ad0e996c856fe59435bb8fedf2a40bfde7c
Mack, Tom <>
2005-06-10 20:24:07 +0000

    Added more default collectables.

--- yukon-client/common/src/com/cannontech/message/util/CollectableMappings.java
+++ yukon-client/common/src/com/cannontech/message/util/CollectableMappings.java
@@ -10,7 +10,10 @@ public class CollectableMappings {
 	private static DefineCollectable[] mappings =
 	{
 		new com.cannontech.message.util.DefineCollectableMessage(),
-		new com.cannontech.message.util.DefineCollectablePing()
+		new com.cannontech.message.util.DefineCollectablePing(),
+        new com.cannontech.message.server.DefineCollectableServerRequest(),
+        new com.cannontech.message.server.DefineCollectableServerResponse(),
+        new com.cannontech.message.dispatch.message.DefineCollectableMulti()
 	};	
 /**
  * This method was created in VisualAge.
