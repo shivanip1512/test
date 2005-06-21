4f2090f69d733e1b529e7e55701daba8c0762dee
Lauinger, Aaron C <AaronCLauinger@Eaton.com>
2005-06-21 20:48:23 +0000

    Added a starting never stop type

--- yukon-client/common/src/com/cannontech/message/notif/NotifLMControlMsg.java
+++ yukon-client/common/src/com/cannontech/message/notif/NotifLMControlMsg.java
@@ -8,8 +8,9 @@ public class NotifLMControlMsg extends Message {
 	
 	// types of control notifications 
 	public static final int STARTING_CONTROL_NOTIFICATION = 1;
-	public static final int ADJUSTING_CONTROL_NOTIFICATION = 2;
-	public static final int FINISHING_CONTROL_NOTIFICATION = 3;
+	public static final int STARTING_NEVER_STOP_CONTROL_NOTIFICATION = 2;	
+	public static final int ADJUSTING_CONTROL_NOTIFICATION = 3;
+	public static final int FINISHING_CONTROL_NOTIFICATION = 4;
 	
 	public int[] notifGroupIds = new int[0];
 	public int notifType;
