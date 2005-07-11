9faaa7c2d7fc10462e5682d113f98ae592ecdfe1
Lauinger, Aaron C <AaronCLauinger@Eaton.com>
2005-07-11 20:09:16 +0000

    Ports were defined in two places moved them to netports.h

--- yukon-server/COMMON/include/ctinexus.h
+++ yukon-server/COMMON/include/ctinexus.h
@@ -7,29 +7,7 @@
 #include <rw\thr\mutex.h>
 
 #include "dlldefs.h"
-
-/*
- *  Some define Nexus's and their socket numbers
- */
-
-#define  NEXUSBASE                                          1500
-#define  PORTCONTROLNEXUS                                   (NEXUSBASE + 0  )
-#define  PORTGUINEXUS                                       (NEXUSBASE + 1  )
-#define  VANGOGHNEXUS                                       (NEXUSBASE + 10 )
-#define  TELEGRAPHNEXUS                                     (NEXUSBASE + 15 )
-
-#define  PROCLOGNEXUS                                       (NEXUSBASE + 20)
-
-#define  SES92NEXUS                                         (NEXUSBASE + 120)
-#define  WELCONEXUS                                         (NEXUSBASE + 121)
-#define  CCU710NEXUS                                        (NEXUSBASE + 122)
-
-#define  CAPCONTROLNEXUS                                    (NEXUSBASE + 410)
-#define  LOADMANAGEMENTNEXUS                                (NEXUSBASE + 420)
-
-#define  PORTSHARENEXUS                                     (NEXUSBASE + 430)
-//  Next allocation should start at NEXUSBASE+530 to allow for 100 shared ports
-
+#include "netports.h"
 
 #define  ERR_CTINEXUS_BASE                                   100000
 #define  ERR_CTINEXUS_INVALID_HANDLE                         (ERR_CTINEXUS_BASE + 0)
--- yukon-server/COMMON/include/netports.h
+++ yukon-server/COMMON/include/netports.h
@@ -1,11 +1,14 @@
+#ifndef __CTINETPORTS_H__
+#define __CTINETPORTS_H__
 /*
  *  Some define Nexus's and their socket numbers
  */
 
 #define  NEXUSBASE                                          1500
 #define  PORTCONTROLNEXUS                                   (NEXUSBASE + 0  )
+#define  PORTGUINEXUS                                       (NEXUSBASE + 1  )
 #define  VANGOGHNEXUS                                       (NEXUSBASE + 10 )
-#define  NOTIFICATIONNEXUS                                  (NEXUSBASE + 15 )
+#define  NOTIFICATIONNEXUS                                     (NEXUSBASE + 15 )
 
 #define  PROCLOGNEXUS                                       (NEXUSBASE + 20)
 #define  PORTERINTERFACENEXUS                               (NEXUSBASE + 40)
@@ -14,5 +17,10 @@
 #define  WELCONEXUS                                         (NEXUSBASE + 121)
 #define  CCU710NEXUS                                        (NEXUSBASE + 122)
 
+#define  CAPCONTROLNEXUS                                    (NEXUSBASE + 410)
+#define  LOADMANAGEMENTNEXUS                                (NEXUSBASE + 420)
 
+#define  PORTSHARENEXUS                                     (NEXUSBASE + 430)
+//  Next allocation should start at NEXUSBASE+530 to allow for 100 shared ports
 
+#endif
