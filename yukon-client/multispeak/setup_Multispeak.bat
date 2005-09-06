d3b4b7af16e6b419c7280dca4c15f8c666e7f1db
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2005-09-06 15:36:31 +0000

    Fixed cb_cd (duplicate) to cd_cb.  Changed endpoint to use \soap\ instead of \services\

--- yukon-client/multispeak/setup_Multispeak.bat
+++ yukon-client/multispeak/setup_Multispeak.bat
@@ -1,4 +1,4 @@
-set endpoint=http://localhost:8080/head/services/AdminService
+set endpoint=http://localhost:8080/head/soap/AdminService
 set AXIS_LIB=C:\WebSphere\dev\workspace\third-party\
 set AXISCLASSPATH=%AXIS_LIB%\axis.jar;%AXIS_LIB%\commons-discovery.jar;%AXIS_LIB%\commons-logging.jar;%AXIS_LIB%\jaxrpc.jar;%AXIS_LIB%\saaj.jar;%AXIS_LIB%\log4j-1.2.8.jar;%AXIS_LIB%\xml-apis.jar;%AXIS_LIB%\xercesImpl.jar;%AXIS_LIB%\wsdl4j.jar;%AXIS_LIB%\mail.jar;%AXIS_LIB%\activation.jar
 
@@ -18,7 +18,7 @@ java -cp %AXISCLASSPATH% org.apache.axis.wsdl.WSDL2Java --server-side --skeleton
 copy /Y com\cannontech\multispeak\deploy.wsdd ..\deploy\cb_cd_deploy.wsdd
 
 java -cp %AXISCLASSPATH% org.apache.axis.wsdl.WSDL2Java --server-side --skeletonDeploy true ..\wsdl/2B_CD_CB.wsdl -pcom.cannontech.multispeak
-copy /Y com\cannontech\multispeak\deploy.wsdd ..\deploy\cb_cd_deploy.wsdd
+copy /Y com\cannontech\multispeak\deploy.wsdd ..\deploy\cd_cb_deploy.wsdd
 
 java -cp %AXISCLASSPATH% org.apache.axis.wsdl.WSDL2Java --server-side --skeletonDeploy true ..\wsdl/4_EA_MR.wsdl -pcom.cannontech.multispeak
 copy /Y com\cannontech\multispeak\deploy.wsdd ..\deploy\ea_mr_deploy.wsdd
