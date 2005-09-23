7a3754b820255ac93a92c391916d3ccbfa859054
Dayton, Jon R <JonRDayton@Eaton.com>
2005-09-23 16:47:46 +0000

    Putting back as it was...

--- yukon-client/notification/src/com/cannontech/notif/handler/CustomerEmailMessageHandler.java
+++ yukon-client/notification/src/com/cannontech/notif/handler/CustomerEmailMessageHandler.java
@@ -15,42 +15,42 @@ import com.cannontech.notif.server.NotifServerConnection;
 import com.cannontech.tools.email.SimpleEmailMessage;
 
 public class CustomerEmailMessageHandler extends MessageHandler {
-    public static final Set EMAIL_NOTIFICATION_TYPES = new HashSet(1);
+	public static final Set EMAIL_NOTIFICATION_TYPES = new HashSet(1);
     
-    static {
-        EMAIL_NOTIFICATION_TYPES.add(new Integer(YukonListEntryTypes.YUK_ENTRY_ID_EMAIL));
-    }
+	static {
+		EMAIL_NOTIFICATION_TYPES.add(new Integer(YukonListEntryTypes.YUK_ENTRY_ID_EMAIL));
+	}
 
-    public boolean canHandle(Message msg) {
-        return msg instanceof NotifCustomerEmailMsg;
-    }
+	public boolean canHandle(Message msg) {
+		return msg instanceof NotifCustomerEmailMsg;
+	}
 
-    public void handleMessage(NotifServerConnection connection, Message msg_) {
-        final NotifCustomerEmailMsg msg = (NotifCustomerEmailMsg) msg_;
+	public void handleMessage(NotifServerConnection connection, Message msg_) {
+		final NotifCustomerEmailMsg msg = (NotifCustomerEmailMsg) msg_;
         
-        ContactableCustomer customer = new ContactableCustomer(CustomerFuncs.getLiteCICustomer(msg.getCustomerID()));
+		ContactableCustomer customer = new ContactableCustomer(CustomerFuncs.getLiteCICustomer(msg.getCustomerID()));
         
-        try {
-            SimpleEmailMessage emailMsg = new SimpleEmailMessage();
+		try {
+			SimpleEmailMessage emailMsg = new SimpleEmailMessage();
 
-            emailMsg.setSubject(msg.getSubject());
-            emailMsg.setBody(msg.getBody());
+			emailMsg.setSubject(msg.getSubject());
+			emailMsg.setBody(msg.getBody());
             
-           /* for (Iterator iter = customer.getNotifications(EmailHandler.checker).iterator(); iter.hasNext();) {
-                LiteContactNotification addr = (LiteContactNotification) iter.next();
-                String emailTo = addr.getNotification();
-                try {
-                    emailMsg.setRecipient(emailTo);
-                    emailMsg.send();
-                } catch (MessagingException e) {
-                    CTILogger.warn("Unable to email message for " + customer + " to address " + emailTo + ".", e);
-                }
+			for (Iterator iter = customer.getNotifications(EmailHandler.checker).iterator(); iter.hasNext();) {
+				LiteContactNotification addr = (LiteContactNotification) iter.next();
+				String emailTo = addr.getNotification();
+				try {
+					emailMsg.setRecipient(emailTo);
+					emailMsg.send();
+				} catch (MessagingException e) {
+					CTILogger.warn("Unable to email message for " + customer + " to address " + emailTo + ".", e);
+				}
 
-            }*/
-        } catch (MessagingException e) {
-            CTILogger.error("Unable to email message '" + msg.getSubject() + "' to " + customer + ".", e );
-        }
+			}
+		} catch (MessagingException e) {
+			CTILogger.error("Unable to email message '" + msg.getSubject() + "' to " + customer + ".", e );
+		}
         
-    }
+	}
 
 }
