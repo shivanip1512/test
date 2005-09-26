2dd7ea5f29a8fd4d09762ee779a46eac3b73ed6c
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2005-09-26 16:34:55 +0000

    Changed Sort Order to Order Direction.

--- yukon-client/report/src/com/cannontech/analysis/tablemodel/MeterOutageModel.java
+++ yukon-client/report/src/com/cannontech/analysis/tablemodel/MeterOutageModel.java
@@ -378,7 +378,7 @@ public class MeterOutageModel extends ReportModelBase
 		html += "    <td valign='top'>" + LINE_SEPARATOR;
 		html += "      <table width='100%' border='0' cellspacing='0' cellpadding='0' class='TableCell'>" + LINE_SEPARATOR;		
 		html += "        <tr>" + LINE_SEPARATOR;
-		html += "          <td class='TitleHeader'>&nbsp;Sort Order</td>" +LINE_SEPARATOR;
+		html += "          <td class='TitleHeader'>&nbsp;Order Direction</td>" +LINE_SEPARATOR;
 		html += "        </tr>" + LINE_SEPARATOR;
 		for (int i = 0; i < getAllSortOrders().length; i++)
 		{
