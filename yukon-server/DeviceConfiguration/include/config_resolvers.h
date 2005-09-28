efcfae60f4dde1d4b0c01d275fe753d97e4f5bfb
Otteson, Jess M <JessMOtteson@Eaton.com>
2005-09-28 21:13:55 +0000

    Changed DLL naming so external access works appropriately.

--- yukon-server/DeviceConfiguration/include/config_resolvers.h
+++ yukon-server/DeviceConfiguration/include/config_resolvers.h
@@ -6,8 +6,8 @@
 *
 * PVCS KEYWORDS:
 * ARCHIVE      :  $Archive:   Z:/SOFTWAREARCHIVES/YUKON/DEVICECONFIGURATION/include/config_resolvers.h-arc  $
-* REVISION     :  $Revision: 1.2 $
-* DATE         :  $Date: 2005/09/28 14:32:09 $
+* REVISION     :  $Revision: 1.3 $
+* DATE         :  $Date: 2005/09/28 21:13:55 $
 *
 * Copyright (c) 2005 Cannon Technologies Inc. All rights reserved.
 *-----------------------------------------------------------------------------*/
@@ -34,6 +34,6 @@ enum CtiConfig_t
     ConfigTypeMax
 };
 
-IM_EX_PORT INT resolveConfigType(RWCString rwsTemp);
+IM_EX_CONFIG INT resolveConfigType(RWCString rwsTemp);
 
 #endif //__CONFIG_RESOLVERS_H__
