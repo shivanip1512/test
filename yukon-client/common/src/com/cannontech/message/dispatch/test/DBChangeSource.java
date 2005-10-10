33722204d34ed38c2797347fffc4215d11d608b8
Neuharth, Ryan <>
2005-10-10 14:56:52 +0000

    Removed loopback command

--- yukon-client/common/src/com/cannontech/message/dispatch/test/DBChangeSource.java
+++ yukon-client/common/src/com/cannontech/message/dispatch/test/DBChangeSource.java
@@ -3,6 +3,7 @@ package com.cannontech.message.dispatch.test;
 /**
  * This type was created in VisualAge.
  */
+import com.cannontech.clientutils.CTILogger;
 import com.cannontech.message.dispatch.ClientConnection;
 
 public class DBChangeSource {
@@ -14,9 +15,9 @@ public static void main(String[] args) {
 
 	if( args.length != 4 )
 	{
-		com.cannontech.clientutils.CTILogger.info("Usage:  DBChangeSource vangoghmachine port numberofchanges delay");
-		com.cannontech.clientutils.CTILogger.info("specify numberofchanges = -1 to keep sending changes forever");
-		com.cannontech.clientutils.CTILogger.info("note that port 1510 has been the default");
+		CTILogger.info("Usage:  DBChangeSource vangoghmachine port numberofchanges delay");
+		CTILogger.info("specify numberofchanges = -1 to keep sending changes forever");
+		CTILogger.info("note that port 1510 has been the default");
 		System.exit(0);
 	}
 
@@ -41,12 +42,12 @@ public static void main(String[] args) {
 	}
 	catch( java.io.IOException e )
 	{
-		com.cannontech.clientutils.CTILogger.error( e.getMessage(), e );
+		CTILogger.error( e.getMessage(), e );
 		System.exit(0);
 	}
 
 	//First do a registration
-	com.cannontech.clientutils.CTILogger.info("Registering client with vangogh");
+	CTILogger.info("Registering client with vangogh");
 	com.cannontech.message.dispatch.message.Registration reg = new com.cannontech.message.dispatch.message.Registration();
 	reg.setAppName("Datbase Change Source - Java" + (new java.util.Date()).getTime() );
 	reg.setAppIsUnique(0);
@@ -54,16 +55,6 @@ public static void main(String[] args) {
 	reg.setAppExpirationDelay( 1000000 );
 
 	conn.write( reg );
-
-	//Do a loopback
-	com.cannontech.clientutils.CTILogger.info("Attempting a loopback command");
-	com.cannontech.message.dispatch.message.Command cmd = new com.cannontech.message.dispatch.message.Command();
-	cmd.setOperation( com.cannontech.message.dispatch.message.Command.NO_OP );
-	conn.write( cmd );
-
-	//Expect the message back
-	Object response = conn.read();
-	com.cannontech.clientutils.CTILogger.info("Received loopback:  " + response );
 
 	//Send changes
 	int numSent = 0;
@@ -75,14 +66,14 @@ public static void main(String[] args) {
 		//dbChange.setDBType(com.cannontech.message.dispatch.message.DBChangeMsg.CHANGE_TYPE_ADD);
 		//conn.write( dbChange );
 		numSent++;
-		com.cannontech.clientutils.CTILogger.info("Sent change #" + numSent);
+		CTILogger.info("Sent change #" + numSent);
 		try
 		{
 			Thread.sleep(delay);
 		}
 		catch( InterruptedException e )
 		{
-			com.cannontech.clientutils.CTILogger.error( e.getMessage(), e );
+			CTILogger.error( e.getMessage(), e );
 		}
 	}
 
