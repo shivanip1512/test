5b7fe0fcdddbb94eed473392b78701b59659c248
Otteson, Jess M <JessMOtteson@Eaton.com>
2005-10-17 16:47:59 +0000

    Pointer names changed, moved includes to .cpp file.

--- yukon-server/RTDB/include/mgr_config.h
+++ yukon-server/RTDB/include/mgr_config.h
@@ -6,8 +6,8 @@
 *
 * PVCS KEYWORDS:
 * ARCHIVE      :  $Archive:   Z:/SOFTWAREARCHIVES/YUKON/DEVICECONFIGURATION/include/mgr_config.h-arc  $
-* REVISION     :  $Revision: 1.1 $
-* DATE         :  $Date: 2005/09/28 21:07:09 $
+* REVISION     :  $Revision: 1.2 $
+* DATE         :  $Date: 2005/10/17 16:47:59 $
 *
 * Copyright (c) 2005 Cannon Technologies Inc. All rights reserved.
 *-----------------------------------------------------------------------------*/
@@ -18,20 +18,17 @@
 #include "dllbase.h"
 
 #include "logger.h"
-#include "config_base.h"
-#include "config_device.h"
 #include "mgr_device.h"
+
 
 using namespace Config;
 class IM_EX_DEVDB CtiConfigManager
 {
-
-
 private:
     typedef CtiLockGuard<CtiMutex> LockGuard;
     mutable CtiMutex    _devMux;//For use with _deviceConfig map.
 
-    typedef map<int,CtiConfigBaseSPtr>    ConfigTypeMap;
+    typedef map<int,BaseSPtr>    ConfigTypeMap;
     typedef map<int,CtiConfigDeviceSPtr>  ConfigDeviceMap;
 
     CtiDeviceManager*   _devMgr;
@@ -42,8 +39,8 @@ private:
     RWCString getConfigValuesTableName();
     RWCString getConfigTypeTableName();
     RWCString getConfigDeviceTableName();
-    CtiConfigBaseSPtr createConfigByType(const int &type);
-    bool insertValueIntoConfigMap(const int &partID, const RWCString &value, const RWCString &key);
+    BaseSPtr createConfigByType(const int type);
+    bool insertValueIntoConfigMap(const int partID, const RWCString &value, const RWCString &valueid);
 
 public:
     CtiConfigManager();
