81bb974e41d19b2206e04af8b5321818dce9a12a
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2006-01-05 18:29:50 +0000

    New Settlement Report group added.

--- yukon-client/common/src/com/cannontech/roles/application/ReportingRole.java
+++ yukon-client/common/src/com/cannontech/roles/application/ReportingRole.java
@@ -24,6 +24,7 @@ public interface ReportingRole {
 	public static final int DATABASE_REPORTS_GROUP = ApplicationRoleDefs.REPORTING_PROPERTYID_BASE - 8;
 	public static final int STARS_REPORTS_GROUP = ApplicationRoleDefs.REPORTING_PROPERTYID_BASE - 9;
 	public static final int OTHER_REPORTS_GROUP = ApplicationRoleDefs.REPORTING_PROPERTYID_BASE - 10;
+	public static final int SETTLEMENT_REPORTS_GROUP = ApplicationRoleDefs.REPORTING_PROPERTYID_BASE - 11;
 	
 	public static final int ADMIN_REPORTS_GROUP_LABEL = ApplicationRoleDefs.REPORTING_PROPERTYID_BASE - 13;
 	public static final int AMR_REPORTS_GROUP_LABEL = ApplicationRoleDefs.REPORTING_PROPERTYID_BASE - 14;
@@ -33,4 +34,5 @@ public interface ReportingRole {
 	public static final int DATABASE_REPORTS_GROUP_LABEL = ApplicationRoleDefs.REPORTING_PROPERTYID_BASE - 18;
 	public static final int STARS_REPORTS_GROUP_LABEL = ApplicationRoleDefs.REPORTING_PROPERTYID_BASE - 19;
 	public static final int OTHER_REPORTS_GROUP_LABEL = ApplicationRoleDefs.REPORTING_PROPERTYID_BASE - 20;
+	public static final int SETTLEMENT_REPORTS_GROUP_LABEL = ApplicationRoleDefs.REPORTING_PROPERTYID_BASE - 21;
 }
