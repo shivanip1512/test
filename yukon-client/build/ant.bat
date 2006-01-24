91bcb455bc8e02eb26ff37093ccfad50c6424f8e
Mack, Tom <>
2006-01-24 16:27:39 +0000

    New ant script that runs ant from within the ant directory located under this directory.

--- yukon-client/build/ant.bat
+++ yukon-client/build/ant.bat
@@ -1,7 +1,4 @@
 @echo off
-REM $Id: ant.bat,v 1.2 2003/08/12 18:27:43 rneuharth Exp $
-set JAVA=%JAVA_HOME%\bin\java
-set cp=ant.jar;%JAVA_HOME%\lib\tools.jar;
-for %%i in (..\build\*.jar) do call cp.bat %%i
-%JAVA% -Xmx256M -classpath %CP% -Dant.home=. org.apache.tools.ant.Main %1 %2 %3 %4 %5 %6 %7 %8 %9 -buildfile build.xml
-
+REM $Id: ant.bat,v 1.3 2006/01/24 16:27:39 tmack Exp $
+REM set ANT_HOME=ant
+ant\bin\ant.bat -lib lib %1 %2 %3 %4 %5 %6 %7 %8 %9
