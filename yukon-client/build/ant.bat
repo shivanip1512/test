1db898156423a22877f7c50d5e5f09d843592f7e
Mack, Tom <>
2006-01-28 21:23:23 +0000

    Found a better way to specify "all arguments".

--- yukon-client/build/ant.bat
+++ yukon-client/build/ant.bat
@@ -1,4 +1,4 @@
 @echo off
-REM $Id: ant.bat,v 1.3 2006/01/24 16:27:39 tmack Exp $
+REM $Id: ant.bat,v 1.4 2006/01/28 21:23:23 tmack Exp $
 REM set ANT_HOME=ant
-ant\bin\ant.bat -lib lib %1 %2 %3 %4 %5 %6 %7 %8 %9
+ant\bin\ant.bat -lib lib %*
