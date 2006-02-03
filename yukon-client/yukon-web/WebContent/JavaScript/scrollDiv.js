8ffda30db5cd4ebdff431ac558e7b2033fc94332
Khazon,Elliot <>
2006-02-03 21:46:33 +0000

    Identation fix

---
+++ yukon-client/yukon-web/WebContent/JavaScript/scrollDiv.js
@@ -0,0 +1,48 @@
+function scrollPerNode(comp, div) {
+	//get the number of elements
+	var len = 0;
+	if (comp.tagName == "SPAN") {
+	   len = div.getElementsByTagName("span").length;
+    }
+	if (comp.tagName == "UL") {
+	   len = comp.getElementsByTagName("a").length;	
+	}
+	return div.scrollHeight / len;
+}
+
+function toggleDivs(){
+	var checked = document.getElementById("enableDualBus").checked;
+	var subBody = document.getElementById("subBody");
+	if (checked == true){
+	   subBody.style.visibility = "visible";
+	}
+	else {
+		subBody.style.visibility = "hidden";
+	}
+}
+
+function addSmartScrolling(hidden_id, div_id, indx_id, comp_id){
+	Event.observe(window, 'load', function() {	
+	//get the hidden and div
+	var hidden_el = document.getElementById(hidden_id);
+	var div_el =  document.getElementById(div_id);
+	//to remember the selected
+	var indx_el = document.getElementById(indx_id);
+	var comp_el = document.getElementById(comp_id);
+	
+	if(hidden_el.value != 0) {
+		div_el.scrollTop = hidden_el.value;	
+	}
+	else {
+		if (indx_el!=null && comp_el!=null)	{
+            div_el.scrollTop = indx_el.value * scrollPerNode(comp_el, div_el);
+	       }	    
+    }
+	div_el.onscroll = function () {hidden_el.value = div_el.scrollTop + 1;};
+	toggleDivs();
+	});
+}
+
+
+
+
