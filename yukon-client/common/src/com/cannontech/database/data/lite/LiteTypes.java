07701dcc2d47dd2289fca64559f2ae49c0355fef
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2006-02-06 15:17:12 +0000

    Added StarsserviceCompanyDesignationCode

--- yukon-client/common/src/com/cannontech/database/data/lite/LiteTypes.java
+++ yukon-client/common/src/com/cannontech/database/data/lite/LiteTypes.java
@@ -74,4 +74,5 @@ public final class LiteTypes
 	public final static int STARS_CUSTOMER_RESIDENCE = 125;
 	public final static int STARS_THERMOSTAT_SCHEDULE = 126;
 	public final static int STARS_LMCONFIGURATION = 127;
+	public final static int STARS_SERVICE_COMPANY_DESIGNATION_CODE = 128;
 }
