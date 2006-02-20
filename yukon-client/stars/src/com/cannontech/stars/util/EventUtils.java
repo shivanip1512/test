21e3714bdb8ebfd2037bcd1d38ec9740a9ee6a3f
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2006-02-20 06:36:38 +0000

    Added build EventWorkOrder util method.

--- yukon-client/stars/src/com/cannontech/stars/util/EventUtils.java
+++ yukon-client/stars/src/com/cannontech/stars/util/EventUtils.java
@@ -35,9 +35,7 @@ public class EventUtils
         }
         else if(sysCategory.compareTo(EVENT_CATEGORY_WORKORDER) == 0)
         {
-        	eventBase = new EventWorkOrder();
-            eventBase.getEventBase().setSystemCategoryID(new Integer(YukonListEntryTypes.EVENT_SYS_CAT_WORKORDER));
-            ((EventWorkOrder)eventBase).getEventWorkOrder().setWorkOrderID(new Integer(objectID));
+        	eventBase = buildEventWorkOrder(userID, actionID, objectID);
         }
         else	//TODO this is a bad catch all
         	eventBase = new EventBase();
@@ -56,4 +54,15 @@ public class EventUtils
         }
     }
 
+    public static EventWorkOrder buildEventWorkOrder(int userID, int actionID, int objectID)
+    {
+        EventWorkOrder  eventWorkOrder = new EventWorkOrder();
+        eventWorkOrder.getEventBase().setSystemCategoryID(new Integer(YukonListEntryTypes.EVENT_SYS_CAT_WORKORDER));
+        eventWorkOrder.getEventWorkOrder().setWorkOrderID(new Integer(objectID));
+        eventWorkOrder.getEventBase().setUserID(new Integer(userID));
+        eventWorkOrder.getEventBase().setActionID(new Integer(actionID));
+        eventWorkOrder.getEventBase().setEventTimestamp(new Date());
+        
+        return eventWorkOrder;
+    }
 }
