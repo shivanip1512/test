dbd08e5be5511ae8a556a270673eac02e49ec4ba
Khazon,Elliot <>
2006-02-24 22:45:08 +0000

    data model for the Point Editor

---
+++ yukon-client/yukon-web/src/com/cannontech/web/wizard/PointWizardModel.java
@@ -0,0 +1,68 @@
+package com.cannontech.web.wizard;
+
+import com.cannontech.database.data.point.PointTypes;
+
+public class PointWizardModel {
+
+    private String name = null;
+    private Boolean disabled = new Boolean(false);
+    private Integer pointType = new Integer (PointTypes.INVALID_POINT);
+    private int subType = PointTypes.INVALID_POINT;
+    private Integer parentId = new Integer (0);
+    public PointWizardModel() {
+        super();
+       
+    }
+
+
+    public Boolean getDisabled() {
+        return disabled;
+    }
+
+    public void setDisabled(Boolean disabled) {
+        this.disabled = disabled;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPointType() {
+        
+            return pointType;
+    }
+
+    public void setPointType(Integer wizPointType) {
+        
+        this.pointType = wizPointType;
+    }
+    
+    public boolean isSubtypeNeeded(){
+        if (getPointType() == PointTypes.CALCULATED_POINT)
+            return true;
+        else
+            return false;
+    }
+
+    public int getSubType() {
+        return subType;
+    }
+
+    public void setSubType(int subType) {
+        this.subType = subType;
+    }
+
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    
+}
