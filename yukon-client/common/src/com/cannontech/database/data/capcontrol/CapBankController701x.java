22d7e90d093de27a34fc93d9c01b842d4553de83
Khazon,Elliot <>
2006-02-24 22:26:37 +0000

    new classes that replace CapBankController7020 and 7010 accordingly

---
+++ yukon-client/common/src/com/cannontech/database/data/capcontrol/CapBankController701x.java
@@ -0,0 +1,13 @@
+package com.cannontech.database.data.capcontrol;
+
+/**
+ * This type was created in VisualAge.
+ */
+public class CapBankController701x extends CapBankController 
+{
+/**
+ */
+public CapBankController701x() {
+	super();
+}
+}
