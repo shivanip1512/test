382f7f07ac9b3057e764442ccb765aba935e8c3f
Lauinger, Aaron C <AaronCLauinger@Eaton.com>
2006-03-03 22:31:00 +0000

    Added porter work count point

--- yukon-database/DBUpdates/oracle/3.2_3.sql
+++ yukon-database/DBUpdates/oracle/3.2_3.sql
@@ -4668,6 +4668,11 @@ alter table YukonUserRole
       references YukonUser (UserID);
 
 
+insert into point select max(PointID)+1,'Analog','Porter Work Count',0,'Default',0,'N','N','R',1500,'None',0 from point;
+insert into pointalarming select max(pointID), '', 'NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN', 'N', 1, 0  from point;
+insert into pointanalog select max(pointID), 0, 'None', 1, 0 from point;
+insert into pointunit select max(pointID), 9, 1, 1.0E+30, -1.0E+30 from point;
+
 /* @error ignore-end */
 
 /******************************************************************************/
