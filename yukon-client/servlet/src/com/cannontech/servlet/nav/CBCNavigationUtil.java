a36a24c4da91baba90e48dc72f3dd9e60afcbd31
Khazon,Elliot <>
2006-03-23 16:55:09 +0000

    added parsing for the pages that are being put in histoty. some of the pages are intermediary links to the actual pages.

--- yukon-client/servlet/src/com/cannontech/servlet/nav/CBCNavigationUtil.java
+++ yukon-client/servlet/src/com/cannontech/servlet/nav/CBCNavigationUtil.java
@@ -19,9 +19,28 @@ public class CBCNavigationUtil {
     public static String goBack(HttpSession session) {        
         CtiNavObject navObject = (CtiNavObject) session.getAttribute("CtiNavObject");        
         if (navObject.getHistory().size() >= 1)
-            return (String) navObject.getHistory().pop();
+            return parseRedirect( (String) navObject.getHistory().pop());
         else
             return "";  
-    }  
+    }
+
+	/*
+	 * method that filters out the url strings that are bumped into the stack
+	 * every time the navigation is done from the 3-tier view.
+	 * From navigation point of view these pages are "intermediary"
+	 * therefore they need to be skipped 
+	 * */
+    private static String parseRedirect(String string) {
+		if (string.indexOf("capBankCmd") != -1) {
+			return string.replaceAll("capBankCmd", "feeders");
+		}
+		if (string.indexOf("feederCmd") != -1) {
+			return string.replaceAll("feederCmd", "feeders");
+		}
+		if (string.indexOf("subCmd") != -1) {
+			return string.replaceAll("subCmd", "feeders");
+		}		
+		return string;
+	}  
 
 }
