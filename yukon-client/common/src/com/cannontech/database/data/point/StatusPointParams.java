41599621127d54983961456d3aacb77c422e16aa
Khazon,Elliot <>
2006-03-23 16:48:06 +0000

    added new field for control type

--- yukon-client/common/src/com/cannontech/database/data/point/StatusPointParams.java
+++ yukon-client/common/src/com/cannontech/database/data/point/StatusPointParams.java
@@ -1,16 +1,29 @@
 package com.cannontech.database.data.point;
 
 public class StatusPointParams extends PointParams {
-
+    int controlType = 0;
     public StatusPointParams(int offset, String name) {
         super(offset, name);
 
+    }
+
+    public StatusPointParams(int offset, String name, int controltype) {
+        super(offset, name);
+        controlType = controltype;
     }
 
     @Override
     public int getType() {
 
         return PointTypes.STATUS_POINT;
+    }
+
+    public int getControlType() {
+        return controlType;
+    }
+
+    public void setControlType(int controlType) {
+        this.controlType = controlType;
     }
 
 }
