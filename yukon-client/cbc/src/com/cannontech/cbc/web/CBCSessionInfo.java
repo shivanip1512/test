2212141d6b31eb9e3225690ade4692d7d1180ca6
Khazon,Elliot <>
2006-05-04 14:45:25 +0000

    Added storage for the tree state across the session

--- yukon-client/cbc/src/com/cannontech/cbc/web/CBCSessionInfo.java
+++ yukon-client/cbc/src/com/cannontech/cbc/web/CBCSessionInfo.java
@@ -1,5 +1,7 @@
 package com.cannontech.cbc.web;
 
+import java.util.HashMap;
+
 import javax.servlet.http.HttpServletRequest;
 import com.cannontech.util.ParamUtil;
 
@@ -16,12 +18,14 @@ public class CBCSessionInfo
 	private int lastSubID = 0;
 	private int lastFeederID = 0;
 	private String lastSearchCriteria = "";
+	
+	private HashMap treeState = new HashMap(100);
 
 	public static final String STR_CBC_AREA = "cbc_lastArea";
 	public static final String STR_SUBID = "cbc_lastSubID";
 	public static final String STR_FEEDERID = "cbc_lastFeederID";
 	public static final String STR_LAST_SEARCH = "cbc_lastSearch";
-
+	
 	public static final String POPUP_EVENT = "pop_up_event";
 	public CBCSessionInfo()
 	{
@@ -96,6 +100,14 @@ public class CBCSessionInfo
 	 */
 	public void setLastSearchCriteria(String string) {
 		lastSearchCriteria = string;
+	}
+
+	public Object getTreeState(String key) {
+		return treeState.get(key);
+	}
+
+	public void setTreeState(String key, Object state) {
+		this.treeState.put(key, state);
 	}
 
 }
