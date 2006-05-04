5ec034c620fb4ef8ed553fa81257b263bb52535a
Mack, Tom <>
2006-05-04 17:52:06 +0000

    Added new methods for commercial curtailment.

--- yukon-client/common/src/com/cannontech/yukon/INotifConnection.java
+++ yukon-client/common/src/com/cannontech/yukon/INotifConnection.java
@@ -1,16 +1,24 @@
 package com.cannontech.yukon;
 
+import com.cannontech.enums.CurtailmentEventAction;
+import com.cannontech.enums.EconomicEventAction;
 import com.cannontech.yukon.conns.NotifRequestException;
 
 /**
  * Interface for connections to the Notifcation Server
  *
  */
-public interface INotifConnection extends IServerConnection 
+public interface INotifConnection
 {
-	
 	public String requestMessage( String token ) throws NotifRequestException;
 
 	public void sendConfirmation(String token, boolean success);
 
+    public void sendCurtailmentNotification(Integer curtailmentEventId, CurtailmentEventAction action);
+
+    public boolean attemptDeleteCurtailmentNotification(Integer curtailmentEventId);
+
+    public void sendEconomicNotification(Integer economicEventId, Integer revision, EconomicEventAction action);
+
+    public boolean attemptDeleteEconomic(Integer eventId);
 }
--- yukon-client/common/src/com/cannontech/yukon/conns/NotifClientConnection.java
+++ yukon-client/common/src/com/cannontech/yukon/conns/NotifClientConnection.java
@@ -2,10 +2,30 @@ package com.cannontech.yukon.conns;
 
 import com.cannontech.clientutils.CTILogger;
 import com.cannontech.database.cache.functions.RoleFuncs;
-import com.cannontech.message.notif.*;
+import com.cannontech.enums.CurtailmentEventAction;
+import com.cannontech.enums.EconomicEventAction;
+import com.cannontech.message.notif.CurtailmentEventDeleteMsg;
+import com.cannontech.message.notif.CurtailmentEventMsg;
+import com.cannontech.message.notif.DefColl_CurtailmentEventDeleteMsg;
+import com.cannontech.message.notif.DefColl_CurtailmentEventMsg;
+import com.cannontech.message.notif.DefColl_EconomicEventDeleteMsg;
+import com.cannontech.message.notif.DefColl_EconomicEventMsg;
+import com.cannontech.message.notif.DefColl_NotifAlarmMsg;
+import com.cannontech.message.notif.DefColl_NotifCompletedMsg;
+import com.cannontech.message.notif.DefColl_NotifEmailMsg;
+import com.cannontech.message.notif.DefColl_NotifLMControlMsg;
+import com.cannontech.message.notif.DefColl_VoiceDataRequestMsg;
+import com.cannontech.message.notif.DefColl_VoiceDataResponseMsg;
+import com.cannontech.message.notif.EconomicEventDeleteMsg;
+import com.cannontech.message.notif.EconomicEventMsg;
+import com.cannontech.message.notif.NotifCompletedMsg;
+import com.cannontech.message.notif.VoiceDataRequestMsg;
+import com.cannontech.message.notif.VoiceDataResponseMsg;
 import com.cannontech.message.server.ServerResponseMsg;
 import com.cannontech.message.util.ClientConnection;
+import com.cannontech.message.util.CollectableBoolean;
 import com.cannontech.message.util.ServerRequest;
+import com.cannontech.message.util.ServerRequestHelper;
 import com.cannontech.roles.yukon.SystemRole;
 import com.cannontech.yukon.INotifConnection;
 import com.roguewave.vsj.CollectableStreamer;
@@ -24,7 +44,11 @@ public class NotifClientConnection extends ClientConnection implements INotifCon
         new DefColl_VoiceDataRequestMsg(),
         new DefColl_VoiceDataResponseMsg(),
         new DefColl_NotifCompletedMsg(),
-        new DefColl_NotifEmailMsg()
+        new DefColl_NotifEmailMsg(),
+        new DefColl_CurtailmentEventMsg(),
+        new DefColl_CurtailmentEventDeleteMsg(),
+        new DefColl_EconomicEventMsg(),
+        new DefColl_EconomicEventDeleteMsg()
 	};
 	
 	public NotifClientConnection() 
@@ -115,12 +139,59 @@ public class NotifClientConnection extends ClientConnection implements INotifCon
      * @param token the call token
      * @param success true if the person called was likely to have heard the notification
      */
-    public synchronized void sendConfirmation(String token, boolean success) {
+    public void sendConfirmation(String token, boolean success) {
         NotifCompletedMsg msg = new NotifCompletedMsg();
         msg.token = token;
         msg.gotConfirmation = success;
         
         write(msg);
+    }
+    
+    /**
+     * Send a Curtailment Notification message to the notification
+     * server.
+     * @param the id of the event from the CCurtCurtailmentEvent table
+     * @param action the action that's affecting the event
+     */
+    public void sendCurtailmentNotification(Integer curtailmentEventId, CurtailmentEventAction action) {
+        CurtailmentEventMsg msg = new CurtailmentEventMsg();
+        msg.curtailmentEventId = curtailmentEventId;
+        switch (action) {
+        case ADJUSTING:
+            msg.action = CurtailmentEventMsg.ADJUSTING;
+            break;
+        case CANCELING:
+            msg.action = CurtailmentEventMsg.CANCELLING;
+            break;
+        case STARTING: 
+            msg.action = CurtailmentEventMsg.STARTING;
+            break;
+        default:
+            throw new IllegalArgumentException("Invalid action: " + action);
+        }
+        write(msg);
+    }
+    
+    public boolean attemptDeleteCurtailmentNotification(Integer curtailmentEventId) {
+        CurtailmentEventDeleteMsg deleteMsg = new CurtailmentEventDeleteMsg();
+        deleteMsg.curtailmentEventId = curtailmentEventId;
+        CollectableBoolean wasCancelled = (CollectableBoolean) ServerRequestHelper.makeServerRequest(this, deleteMsg);
+        return wasCancelled.getValue();
+    }
+    
+    public void sendEconomicNotification(Integer economicPricingRevisionId, Integer revision, EconomicEventAction action) {
+        EconomicEventMsg msg = new EconomicEventMsg();
+        msg.economicEventId = economicPricingRevisionId;
+        msg.revisionNumber = revision;
+        msg.action = action;
+        write(msg);
+    }
+
+    public boolean attemptDeleteEconomic(Integer eventId) {
+        EconomicEventDeleteMsg msg = new EconomicEventDeleteMsg();
+        msg.economicEventId = eventId;
+        CollectableBoolean wasCancelled = (CollectableBoolean) ServerRequestHelper.makeServerRequest(this, msg);
+        return wasCancelled.getValue();
     }
 
 }
