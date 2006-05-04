13915ac2b57793d069c964a76f2162d656bc7e74
Khazon,Elliot <>
2006-05-04 16:54:15 +0000

    added some message constants

--- yukon-client/yukon-web/src/com/cannontech/web/exceptions/CBCExceptionMessages.java
+++ yukon-client/yukon-web/src/com/cannontech/web/exceptions/CBCExceptionMessages.java
@@ -6,9 +6,16 @@ public class CBCExceptionMessages {
     public static final String[] MSG_MULTIPLE_DEVICES_ON_PORT = {"ERROR: Master/Slave addressing already in use in device: ", 
                                                             ". Please edit the addressing or choose a different port."};
 
-    public static final String MSG_SAME_MASTER_SLAVE_COMBINATION = "WARNING: Master/Slave addressing already in use in device: "; 
+    public static final String MSG_SAME_MASTER_SLAVE_COMBINATION = "WARNING: Master/Slave addressing already in use in device: ";
 
-    public CBCExceptionMessages() {
+	public static final String MSG_DUPLICATE_SERIAL_NUMBER = "The following devices have the same serial number: ";
+
+	public static final String MSG_ALTSUB_NEEDS_SWITCH_PT = "ERROR: alternative sub bus needs to have a switch point";
+
+	public static String DB_UPDATE_SUCCESS = "Database update was SUCCESSFUL"; 
+
+    
+	public CBCExceptionMessages() {
         super();
     }
 
