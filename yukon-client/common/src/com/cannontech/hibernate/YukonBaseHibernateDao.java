21ed55cd5b8371ca6969200f3e739c0c1d52b7ae
Mack, Tom <>
2006-05-04 17:50:33 +0000

    Moved some stuff around.

---
+++ yukon-client/common/src/com/cannontech/hibernate/YukonBaseHibernateDao.java
@@ -0,0 +1,7 @@
+package com.cannontech.hibernate;
+
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+public abstract class YukonBaseHibernateDao extends HibernateDaoSupport {
+    
+}
