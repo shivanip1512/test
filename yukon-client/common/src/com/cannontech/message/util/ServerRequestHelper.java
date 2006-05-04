e3a3f24db61e1a1e8aa07f231235fee4936c83b2
Mack, Tom <>
2006-05-04 03:15:26 +0000

    A class to simplify the creationof server request and response messages.

---
+++ yukon-client/common/src/com/cannontech/message/util/ServerRequestHelper.java
@@ -0,0 +1,72 @@
+package com.cannontech.message.util;
+
+import com.cannontech.clientutils.CTILogger;
+import com.cannontech.message.server.ServerRequestMsg;
+import com.cannontech.message.server.ServerResponseMsg;
+
+public class ServerRequestHelper {
+    
+    private ServerRequestHelper() {};
+    
+    /**
+     * @param conn the ClientConnection to send the request on
+     * @param msg the message to embed in the ServerRequestMsg
+     * @param timeoutMilliSeconds
+     * @return payload object from response
+     * @throws ServerRequestHelper.BadServerResponseException
+     */
+    public static Object makeServerRequest(ClientConnection conn, 
+                                           Message msg, 
+                                           int timeoutMilliSeconds) {
+        Object result = null;
+        try {
+            ServerRequest req = ServerRequest.makeServerRequest(conn, msg);
+            ServerResponseMsg responseMsg = req.execute(timeoutMilliSeconds); // could block up to 60 seconds
+            if(responseMsg.getStatus() == ServerResponseMsg.STATUS_OK) {
+                // good response
+                result = responseMsg.getPayload();
+            }
+        } catch (InterruptedException e) {
+            CTILogger.error("Was interrupted while waiting for server response", e);
+        }
+        if (result == null) {
+            throw new BadServerResponseException();
+        }
+        return result;
+    }
+    
+    public static void makeServerResponse(ClientConnection conn, 
+                                          ServerRequestMsg req,
+                                          Object payload) {
+        ServerResponseMsg responseMsg = req.createResponseMsg();
+        responseMsg.setPayload(payload);
+        responseMsg.setStatus(ServerResponseMsg.STATUS_OK);
+        conn.write(responseMsg);
+    }
+    
+    
+    /**
+     * Convenience method that calls makeServerRequest(conn, msg, 10000).
+     * @param conn
+     * @param msg
+     * @return
+     */
+    public static Object makeServerRequest(ClientConnection conn, 
+                                           Message msg) {
+        return makeServerRequest(conn, msg, 10000);
+    }
+
+    
+    public static class BadServerResponseException extends RuntimeException {
+    }
+    
+    public static boolean isPayloadInstanceOf(Message msg, Class clazz) {
+        if (msg instanceof ServerRequestMsg) {
+            ServerRequestMsg reqMsg = (ServerRequestMsg) msg;
+            Object payload = reqMsg.getPayload();
+            return clazz.isInstance(payload);
+        }
+        return false;
+    }
+    
+}
