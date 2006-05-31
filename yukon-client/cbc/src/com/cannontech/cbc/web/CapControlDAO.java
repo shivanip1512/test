aa80dc71e7ec495a68a30357e6276c8aa4eee17e
Khazon,Elliot <>
2006-05-31 15:57:41 +0000

    Interface and an implementation used primarily to restrict access to subs based on the user role

---
+++ yukon-client/cbc/src/com/cannontech/cbc/web/CapControlDAO.java
@@ -0,0 +1,147 @@
+package com.cannontech.cbc.web;
+
+import java.util.List;
+
+import com.cannontech.database.data.lite.LiteState;
+import com.cannontech.web.lite.LiteWrapper;
+import com.cannontech.yukon.cbc.CapBankDevice;
+import com.cannontech.yukon.cbc.Feeder;
+import com.cannontech.yukon.cbc.StreamableCapObject;
+import com.cannontech.yukon.cbc.SubBus;
+
+public interface CapControlDAO {
+
+	/**
+	 * 
+	 * @return SubBus
+	 */
+	public abstract SubBus getSubBus(Integer subID);
+
+	/**
+	 * Returs the base object type for a SubBus, Feeder or CapBankDevice
+	 * 
+	 */
+	public abstract StreamableCapObject getCapControlPAO(Integer paoID);
+
+	/**
+	 * 
+	 * @return Feeder
+	 */
+	public abstract Feeder getFeeder(Integer feederID);
+
+	/**
+	 * @return CapBankDevice
+	 */
+	public abstract CapBankDevice getCapBankDevice(Integer capBankDeviceID);
+
+	/**
+	 * @return Feeder[]
+	 */
+	public abstract Feeder[] getFeedersBySub(Integer subBusID);
+
+	/**
+	 * @return CapBankDevice[]
+	 */
+	public abstract CapBankDevice[] getCapBanksByFeeder(Integer feederID);
+
+	/**
+	 * Instant lookup to check if this paoID is used by a SubBus
+	 * 
+	 */
+	public abstract boolean isSubBus(int id);
+
+	/**
+	 * Instant lookup to check if this paoID is used by a Feeder
+	 * 
+	 */
+	public abstract boolean isFeeder(int id);
+
+	/**
+	 * Instant lookup to check if this paoID is used by a CapBankDevice
+	 * 
+	 */
+	public abstract boolean isCapBank(int id);
+
+	/**
+	 * @return CapBankDevice[]
+	 * @param subBusID long
+	 */
+	public abstract CapBankDevice[] getCapBanksBySub(Integer subBusID);
+
+	/**
+	 * Returns all SubBuses for a given Area
+	 * 
+	 */
+	public abstract SubBus[] getSubsByArea(String area);
+
+	/**
+	 * Returns all CapBanks for a given Area
+	 * 
+	 */
+	public abstract CapBankDevice[] getCapBanksByArea(String area);
+
+	/**
+	 * Returns all Feeders for a given Area
+	 * 
+	 */
+	public abstract Feeder[] getFeedersByArea(String area);
+
+	/**
+	 * Find all the orphaned CBC's in the system. This requires a database hit since
+	 * the CapControl server does not know about these.
+	 * 
+	 */
+	public abstract LiteWrapper[] getOrphanedCBCs();
+
+	/**
+	 * Find the orphaned CapBanks in the system. This requires a database hit since
+	 * the CapControl server does not know about these.
+	 * 
+	 */
+	public abstract LiteWrapper[] getOrphanedCapBanks();
+
+	/**
+	 * Find the orphaned Feeders in the system. This requires a database hit since
+	 * the CapControl server does not know about these.
+	 * 
+	 */
+	public abstract LiteWrapper[] getOrphanedFeeders();
+
+	/**
+	 * Create an array of SubBuses. Best usage is to store the results of this call
+	 * instead of repeatingly calling this method. Never returns null.
+	 * 
+	 * @return SubBus[]
+	 * @param
+	 */
+	public abstract SubBus[] getAllSubBuses();
+
+	/**
+	 * Distinct area Strings that are used by substations
+	 * 
+	 * @return List
+	 */
+	public abstract List getAreaNames();
+
+	/**
+	 * State group & states to use for CapBanks
+	 * 
+	 * @return LiteState
+	 */
+	public abstract LiteState getCapBankState(int rawState);
+
+	/**
+	 * Returns the Parent SubBus ID for the given child id
+	 */
+	public abstract int getParentSubBusID(int childID);
+
+	/**
+	 * 
+	 * @param id - Point Id
+	 * @return String containing parent names for Point Id
+	 * @comments the fact that it is a point is checked on the front end    
+	 */
+
+	public abstract String getParentNames(int id);
+
+}
---
+++ yukon-client/cbc/src/com/cannontech/cbc/web/CapControlUserOwnerDAO.java
@@ -0,0 +1,128 @@
+package com.cannontech.cbc.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.cannontech.database.cache.functions.AuthFuncs;
+import com.cannontech.database.data.lite.LiteState;
+import com.cannontech.database.data.lite.LiteYukonUser;
+import com.cannontech.web.lite.LiteWrapper;
+import com.cannontech.yukon.cbc.CapBankDevice;
+import com.cannontech.yukon.cbc.Feeder;
+import com.cannontech.yukon.cbc.StreamableCapObject;
+import com.cannontech.yukon.cbc.SubBus;
+
+public class CapControlUserOwnerDAO implements CapControlDAO {
+
+	private CapControlDAO _ccDao;
+	private LiteYukonUser _user;
+	
+	public CapControlUserOwnerDAO(CapControlDAO dao, LiteYukonUser user) {
+		super();
+		_ccDao = dao;
+		_user = user;
+	}
+
+	public CapControlUserOwnerDAO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public SubBus[] getAllSubBuses() {
+	//check 
+
+		return _ccDao.getAllSubBuses();
+	}
+
+	public List getAreaNames() {
+		List names = _ccDao.getAreaNames();
+		return names;
+	}
+
+	public CapBankDevice getCapBankDevice(Integer capBankDeviceID) {
+		return _ccDao.getCapBankDevice(capBankDeviceID);
+	}
+
+	public CapBankDevice[] getCapBanksByArea(String area) {
+		return _ccDao.getCapBanksByArea(area);
+	}
+
+	public CapBankDevice[] getCapBanksByFeeder(Integer feederID) {
+		return _ccDao.getCapBanksByFeeder(feederID);
+	}
+
+	public CapBankDevice[] getCapBanksBySub(Integer subBusID) {
+		return _ccDao.getCapBanksBySub(subBusID);
+	}
+
+	public LiteState getCapBankState(int rawState) {
+		return _ccDao.getCapBankState(rawState);
+	}
+
+	public StreamableCapObject getCapControlPAO(Integer paoID) {
+		return _ccDao.getCapControlPAO(paoID);
+	}
+
+	public Feeder getFeeder(Integer feederID) {
+		return _ccDao.getFeeder(feederID);
+	}
+
+	public Feeder[] getFeedersByArea(String area) {
+		return _ccDao.getFeedersByArea(area);
+	}
+
+	public Feeder[] getFeedersBySub(Integer subBusID) {
+		return _ccDao.getFeedersBySub(subBusID);
+	}
+
+	public LiteWrapper[] getOrphanedCapBanks() {
+		return _ccDao.getOrphanedCapBanks();
+	}
+
+	public LiteWrapper[] getOrphanedCBCs() {
+		return _ccDao.getOrphanedCBCs();
+	}
+
+	public LiteWrapper[] getOrphanedFeeders() {
+		return _ccDao.getOrphanedFeeders();
+	}
+
+	public String getParentNames(int id) {
+		return _ccDao.getParentNames(id);
+	}
+
+	public int getParentSubBusID(int childID) {
+		return _ccDao.getParentSubBusID(childID);
+	}
+
+	public SubBus getSubBus(Integer subID) {
+		return _ccDao.getSubBus(subID);
+	}
+
+	public SubBus[] getSubsByArea(String area) {
+		SubBus[] subs =  _ccDao.getSubsByArea(area);
+		SubBus[] retArray = new SubBus[subs.length];
+		List subsAllowedToView = new ArrayList(10);
+		for (int i=0; i < area.length(); i++) {
+			SubBus sub = subs[i];
+			if (AuthFuncs.userHasAccessPAO(_user, sub.getCcId().intValue()))
+				subsAllowedToView.add(sub);
+		}
+		return ((SubBus[])subsAllowedToView.toArray(retArray));
+	}
+
+	public boolean isCapBank(int id) {
+		return _ccDao.isCapBank(id);
+	}
+
+	public boolean isFeeder(int id) {
+		return _ccDao.isFeeder(id);
+	}
+
+	public boolean isSubBus(int id) {
+		return _ccDao.isSubBus(id);
+	}
+
+
+
+}
