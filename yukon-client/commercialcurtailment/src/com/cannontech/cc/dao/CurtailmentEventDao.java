70270d1a283fc0feb350cc4c2ec1ba955c613d90
Mack, Tom <>
2006-06-15 05:00:33 +0000

    Removed unused imports.

--- yukon-client/commercialcurtailment/src/com/cannontech/cc/dao/CurtailmentEventDao.java
+++ yukon-client/commercialcurtailment/src/com/cannontech/cc/dao/CurtailmentEventDao.java
@@ -2,10 +2,8 @@ package com.cannontech.cc.dao;
 
 import java.util.List;
 
-import com.cannontech.cc.model.BaseEvent;
 import com.cannontech.cc.model.CurtailmentEvent;
 import com.cannontech.cc.model.Program;
-import com.cannontech.database.data.lite.LiteEnergyCompany;
 
 public interface CurtailmentEventDao extends StandardDaoOperations<CurtailmentEvent>, CommonEventOperations {
 
