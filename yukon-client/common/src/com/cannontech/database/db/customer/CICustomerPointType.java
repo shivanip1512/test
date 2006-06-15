6821ac9e4e51c1dfde74911697b0391c5504069c
Mack, Tom <>
2006-06-15 05:03:36 +0000

    Added default labels to the constants.

--- yukon-client/common/src/com/cannontech/database/db/customer/CICustomerPointType.java
+++ yukon-client/common/src/com/cannontech/database/db/customer/CICustomerPointType.java
@@ -2,27 +2,33 @@ package com.cannontech.database.db.customer;
 
 public enum CICustomerPointType {
     // original types
-    Demand(""),
-    Settlement(""),
-    Baseline(""),
-    Curtailable(""),
+    Demand("","Demand"),
+    Settlement("","Settlement"),
+    Baseline("","Baseline"),
+    Curtailable("","Curtailable"),
     
     // isoc types
-    ContractFrmDmd("kW"),
-    AdvBuyThroughKw("kW"),
-    AdvBuyThrough$("$/kW"),
-    InterruptHours("hours"),
-    MinimumNotice("minutes"),
-    ContractIntLoad("kW"),
-    CurrentLoad("kW");
+    ContractFrmDmd("kW","Contract Firm Demand"),
+    AdvBuyThroughKw("kW", "Advanced Buy Through kW"),
+    AdvBuyThrough$("$/kW", "Advanced Buy Through Price"),
+    InterruptHours("hours", "Max Interrupt Hours"),
+    MinimumNotice("minutes", "Minimum Notice"),
+    ContractIntLoad("kW", "Contract Interruptible Load"),
+    CurrentLoad("kW", "Current Load");
     
     private final String unit;
+    private final String label;
 
-    private CICustomerPointType(String unit) {
+    private CICustomerPointType(String unit, String label) {
         this.unit = unit;
+        this.label = label;
     }
     
     public String getUnit() {
         return unit;
+    }
+
+    public String getLabel() {
+        return label;
     }
 }
