e2fa936c010061309fbb69ed71b16c7ec78bbc8b
Mack, Tom <>
2006-06-18 21:02:34 +0000

    Added new parameters to be used by the notification strategy.

--- yukon-client/commercialcurtailment/src/com/cannontech/cc/model/ProgramParameterKey.java
+++ yukon-client/commercialcurtailment/src/com/cannontech/cc/model/ProgramParameterKey.java
@@ -3,9 +3,10 @@ package com.cannontech.cc.model;
 public enum ProgramParameterKey {
     DEFAULT_EVENT_OFFSET_MINUTES("60","Event Time Offset Minutes"),
     DEFAULT_NOTIFICATION_OFFSET_MINUTES("60","Notification Time Offset Minutes"),
+    MINIMUM_NOTIFICATION_MINUTES("60","Minimum Notification Time Minutes"),
     DEFAULT_EVENT_DURATION_MINUTES("240","Default Event Duration"),
     MINIMUM_EVENT_DURATION_MINUTES("240","Minimum Event Duration"),
-    DEFAULT_ENERGY_PRICE("100","Energy Price");
+    DEFAULT_ENERGY_PRICE(".01","Energy Price");
     
     private final String defaultValue;
     private final String longDescription;
