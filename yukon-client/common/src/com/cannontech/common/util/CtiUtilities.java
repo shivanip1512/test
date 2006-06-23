4fb304b26f55eb7ad9787ec847b270c3ccee1a14
Mack, Tom <>
2006-06-23 15:51:06 +0000

    Updated copyright string (even though it is only referenced in one place).

--- yukon-client/common/src/com/cannontech/common/util/CtiUtilities.java
+++ yukon-client/common/src/com/cannontech/common/util/CtiUtilities.java
@@ -37,7 +37,7 @@ public final class CtiUtilities
     public static final String CELSIUS_CHARACTER = "C";
     public static final String FAHRENHEIT_CHARACTER = "F";
 	
-	public static final String COPYRIGHT = "Copyright (C)1999-2005 Cannon Technologies";
+	public static final String COPYRIGHT = "Copyright (C)1999-2006 Cannon Technologies";
 	
 	public static final String USER_DIR = System.getProperty("user.dir") + System.getProperty("file.separator");
 	
