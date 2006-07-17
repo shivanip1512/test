cac380bc7df935b69ba0eeae055618519ece35cb
Mack, Tom <>
2006-07-17 23:51:10 +0000

    Making compatible with Spring 2.0 (not as backwards compatible as they claim!).

--- yukon-client/common/src/com/cannontech/database/MaxRowCalbackHandlerRse.java
+++ yukon-client/common/src/com/cannontech/database/MaxRowCalbackHandlerRse.java
@@ -4,11 +4,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.ResultReader;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowCallbackHandler;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.RowMapperResultReader;
 
 /**
  * This class is for use with Spring's JdbcTemplate class. It can wrap
@@ -23,12 +20,6 @@ public class MaxRowCalbackHandlerRse implements ResultSetExtractor {
     private int loadedRows = 0;
     RowCallbackHandler rch;
 
-    public MaxRowCalbackHandlerRse(RowMapper rm, int maxRows) {
-        super();
-        this.maxRows = maxRows;
-        this.rch = new RowMapperResultReader(rm);
-    }
-    
     public MaxRowCalbackHandlerRse(RowCallbackHandler rch, int maxRows) {
         this.rch = rch;
         this.maxRows = maxRows;
@@ -38,12 +29,7 @@ public class MaxRowCalbackHandlerRse implements ResultSetExtractor {
         while (loadedRows++ < maxRows && rs.next()) {
             this.rch.processRow(rs);
         }
-        if (this.rch instanceof ResultReader) {
-            return ((ResultReader) this.rch).getResults();
-        }
-        else {
-            return null;
-        }
+        return null;
     }
 
     public int getLoadedRows() {
