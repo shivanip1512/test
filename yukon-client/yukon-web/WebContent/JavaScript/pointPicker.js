39e0e47d9e1cc6f141a6b13a4489eb7cd935a614
Mack, Tom <>
2006-07-31 19:52:48 +0000

    JavaScript for the point picker.

---
+++ yukon-client/yukon-web/WebContent/JavaScript/pointPicker.js
@@ -0,0 +1,74 @@
+var pointPicker_destPointIdFieldId;
+var pointPicker_currentSearch = '';
+var pointPicker_inSearch = false;
+function pointPicker_showPicker(destPointIdFieldId) {
+    // create div to store result
+    var bodyElem = document.documentElement.getElementsByTagName("body")[0];
+    var newDivElem = document.createElement("div");
+    newDivElem.setAttribute("id", "pointPickerContainer");
+    bodyElem.appendChild(newDivElem);
+    
+    var url = '/pointPicker/initial?';
+    url += 'destPointIdFieldId=' + destPointIdFieldId;
+    url += '&currentPointId=' + $(destPointIdFieldId).value;
+    new Ajax.Updater('pointPickerContainer', url, {'method': 'get'});
+    pointPicker_destPointIdFieldId = destPointIdFieldId;
+}
+
+var onComplete = function() {
+    var ss = escape($('query').value);
+    if (pointPicker_currentSearch != ss) {
+        //do another search
+        pointPicker_doPartialSearch();
+    } else {
+        pointPicker_inSearch = false;
+        Element.hide('pointPicker_indicator');
+    }
+}
+
+function pointPicker_doKeyUp() {
+    var timerFunction = function() {
+        if (!pointPicker_inSearch) {
+            pointPicker_doPartialSearch(0);
+        }
+    };
+    var quietDelay = 300;
+    setTimeout(timerFunction, quietDelay);
+    Element.show('pointPicker_indicator');
+}
+
+function pointPicker_doPartialSearch(start) {
+    pointPicker_inSearch = true;
+    var ss = escape($('query').value);
+    pointPicker_currentSearch = ss;
+    var url = '/pointPicker/search?';
+    url += 'ss=' + ss;
+    url += '&currentPointId=' + $(pointPicker_destPointIdFieldId).value;
+    url += '&start=' + start;
+    new Ajax.Updater('pointPicker_results', url, {'method': 'get', 'onComplete': onComplete});
+}
+
+function pointPicker_selectThisPoint(pointId) {
+    $(pointPicker_destPointIdFieldId).value = pointId;
+    $('pointPickerContainer').parentNode.removeChild($('pointPickerContainer'))
+}
+
+function pointPicker_cancel() {
+    $('pointPickerContainer').parentNode.removeChild($('pointPickerContainer'));
+}
+
+function pointPicker_previous(index) {
+    pointPicker_doPartialSearch(index);
+}
+
+function pointPicker_next(index) {
+    pointPicker_doPartialSearch(index);
+}
+
+function pointPicker_showAll(start) {
+    var url = '/pointPicker/showAll?';
+    url += '&currentPointId=' + $(pointPicker_destPointIdFieldId).value;
+    url += '$start=' + start;
+    new Ajax.Updater('pointPicker_results', url, {'method': 'get', 'onComplete': onComplete});
+    Element.show('pointPicker_indicator');
+}
