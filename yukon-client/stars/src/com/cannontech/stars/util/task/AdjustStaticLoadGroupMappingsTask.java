fcb74c9fba858aca6f148881baccfdaf81dc7e4f
Dayton, Jon R <JonRDayton@Eaton.com>
2006-08-10 21:32:50 +0000

    New task for Xcel PMSI Replacement Project that will cycle through and set the addressing group IDs (load group IDs) for all switches in SAM to their statically mapped db value...

---
+++ yukon-client/stars/src/com/cannontech/stars/util/task/AdjustStaticLoadGroupMappingsTask.java
@@ -0,0 +1,288 @@
+/*
+ * Created on Apr 28, 2004
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package com.cannontech.stars.util.task;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import com.cannontech.clientutils.ActivityLogger;
+import com.cannontech.clientutils.CTILogger;
+import com.cannontech.common.util.Pair;
+import com.cannontech.core.dao.DaoFactory;
+import com.cannontech.database.Transaction;
+import com.cannontech.database.data.activity.ActivityLogActions;
+import com.cannontech.database.data.lite.LiteCICustomer;
+import com.cannontech.database.data.lite.LiteCustomer;
+import com.cannontech.database.data.lite.stars.LiteAddress;
+import com.cannontech.database.data.lite.stars.LiteInventoryBase;
+import com.cannontech.database.data.lite.stars.LiteLMConfiguration;
+import com.cannontech.database.data.lite.stars.LiteLMHardwareEvent;
+import com.cannontech.database.data.lite.stars.LiteStarsCustAccountInformation;
+import com.cannontech.database.data.lite.stars.LiteStarsEnergyCompany;
+import com.cannontech.database.data.lite.stars.LiteStarsLMHardware;
+import com.cannontech.database.data.lite.stars.StarsLiteFactory;
+import com.cannontech.database.db.stars.appliance.ApplianceBase;
+import com.cannontech.database.db.stars.hardware.LMHardwareConfiguration;
+import com.cannontech.database.db.stars.hardware.StaticLoadGroupMapping;
+import com.cannontech.roles.operator.AdministratorRole;
+import com.cannontech.stars.util.ECUtils;
+import com.cannontech.stars.util.InventoryUtils;
+import com.cannontech.stars.util.ServletUtils;
+import com.cannontech.stars.util.SwitchCommandQueue;
+import com.cannontech.stars.util.WebClientException;
+import com.cannontech.stars.web.StarsYukonUser;
+import com.cannontech.stars.web.action.UpdateLMHardwareConfigAction;
+import com.cannontech.stars.web.action.YukonSwitchCommandAction;
+import com.cannontech.stars.web.util.InventoryManagerUtil;
+import com.cannontech.stars.xml.serialize.StarsCustAccountInformation;
+import com.cannontech.stars.xml.serialize.StarsInventory;
+import com.cannontech.stars.xml.serialize.StarsLMConfiguration;
+
+/**
+ * @author yao
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class AdjustStaticLoadGroupMappingsTask extends TimeConsumingTask {
+	
+	LiteStarsEnergyCompany energyCompany = null;
+	boolean fullReset = false;
+    boolean sendConfig = true;
+	HttpServletRequest request = null;
+	
+	List<StaticLoadGroupMapping> mappingsToAdjust = null;
+	ArrayList hwsToAdjust = null;
+	ArrayList configurationSet = new ArrayList();
+    List<String> failureInfo = new ArrayList<String>();
+    HashMap<Integer, LMHardwareConfiguration> existingsConfigs = new HashMap<Integer, LMHardwareConfiguration>();
+    //don't need since liteHw already has the config loaded....HashMap<Integer, com.cannontech.database.db.stars.hardware.LMHardwareConfiguration> existingConfigEntries;
+	int numSuccess = 0, numFailure = 0;
+	int numToBeConfigured = 0;
+	
+	public AdjustStaticLoadGroupMappingsTask(LiteStarsEnergyCompany energyCompany, boolean fullReset, boolean sendConfig, HttpServletRequest request)
+	{
+		this.energyCompany = energyCompany;
+		this.fullReset = fullReset;
+        this.sendConfig = sendConfig;
+		this.request = request;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.cannontech.stars.util.task.TimeConsumingTask#getProgressMsg()
+	 */
+	public String getProgressMsg() {
+		if (fullReset) {
+			if (status == STATUS_FINISHED && numFailure == 0) {
+				return numSuccess + " hardwares have been configured successfully.";
+			}
+			else
+				return numSuccess + " of " + numToBeConfigured + " hardwares have been configured.";
+		}
+		else {
+			if (status == STATUS_FINISHED && numFailure == 0) {
+			    return numSuccess + " hardware configuration saved to batch.";
+			}
+			else
+				return numSuccess + " of " + numToBeConfigured + " hardware configuration saved to batch.";
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	public void run() {
+		HttpSession session = request.getSession(false);
+		StarsYukonUser user = (StarsYukonUser) session.getAttribute( ServletUtils.ATT_STARS_YUKON_USER );
+		
+		mappingsToAdjust = StaticLoadGroupMapping.getAllStaticLoadGroups();
+		if (mappingsToAdjust == null || mappingsToAdjust.size() < 1) {
+			status = STATUS_ERROR;
+			errorMsg = "There are no static load group mappings available.";
+			return;
+		}
+		
+		status = STATUS_RUNNING;
+		
+		boolean searchMembers = DaoFactory.getAuthDao().checkRoleProperty( user.getYukonUser(), AdministratorRole.ADMIN_MANAGE_MEMBERS )
+				&& energyCompany.getChildren().size() > 0;
+		hwsToAdjust = new ArrayList();
+        
+		if (!searchMembers) {
+			List<LiteInventoryBase> hwsFromEC = energyCompany.loadAllInventory(true);
+            /*
+             * If not a full reset, we will want to only look for those with an addressing group ID of zero
+             */
+            if(!fullReset)
+                existingsConfigs = LMHardwareConfiguration.getAllLMHardwareConfigurationsWithoutLoadGroups(energyCompany.getEnergyCompanyID());
+			for (int j = 0; j < hwsToAdjust.size(); j++) {
+				if (!hwsToAdjust.contains( hwsFromEC.get(j) ) && hwsFromEC.get(j) instanceof LiteStarsLMHardware) {
+                    if(!fullReset)
+                    {
+                        LMHardwareConfiguration config = existingsConfigs.get(hwsFromEC.get(j).getInventoryID());
+                        if(config != null)
+                            hwsToAdjust.add( hwsFromEC.get(j) );
+                    }
+                    else
+                        hwsToAdjust.add( hwsFromEC.get(j) );
+                }
+			}
+		}
+		else {
+			ArrayList descendants = ECUtils.getAllDescendants( energyCompany );
+			for (int j = 0; j < descendants.size(); j++) {
+				LiteStarsEnergyCompany company = (LiteStarsEnergyCompany) descendants.get(j);
+                List<LiteInventoryBase> hwsFromEC = company.loadAllInventory(true);
+                /*
+                 * If not a full reset, we will want to only look for those with an addressing group ID of zero
+                 */
+                if(!fullReset)
+                    existingsConfigs = LMHardwareConfiguration.getAllLMHardwareConfigurationsWithoutLoadGroups(energyCompany.getEnergyCompanyID());
+                for (int j = 0; j < hwsToAdjust.size(); j++) {
+                    if (!hwsToAdjust.contains( hwsFromEC.get(j) ) && hwsFromEC.get(j) instanceof LiteStarsLMHardware) {
+                        if(!fullReset)
+                        {
+                            LMHardwareConfiguration config = existingsConfigs.get(hwsFromEC.get(j).getInventoryID());
+                            if(config != null)
+                                hwsToAdjust.add( hwsFromEC.get(j) );
+                        }
+                        else
+                            hwsToAdjust.add( hwsFromEC.get(j) );
+                    }
+                }
+			}
+		}
+		
+		numToBeConfigured = hwsToAdjust.size();
+		if (numToBeConfigured == 0) {
+			status = STATUS_ERROR;
+			errorMsg = "No inventory could be loaded.";
+			return;
+		}
+		
+		/*TODO: shouldn't need to support hardware addressing, but make sure 
+		 *User has specified a new configuration
+		 *StarsLMConfiguration hwConfig = null;
+         * if (request.getParameter("UseHardwareAddressing") != null) {
+			hwConfig = new StarsLMConfiguration();
+			try {
+				InventoryManagerUtil.setStarsLMConfiguration( hwConfig, request );
+			}
+			catch (WebClientException e) {
+				CTILogger.error( e.getMessage(), e );
+				status = STATUS_ERROR;
+				errorMsg = e.getMessage();
+				return;
+			}
+		}
+		else {*/
+        String options = null;
+        
+		for (int i = 0; i < hwsToAdjust.size(); i++) {
+			LiteStarsLMHardware liteHw = (LiteStarsLMHardware) hwsToAdjust.get(i);;
+			LiteStarsEnergyCompany company = energyCompany;
+						
+            //get the current Configuration
+            com.cannontech.database.db.stars.hardware.LMHardwareConfiguration configDB = com.cannontech.database.db.stars.hardware.LMHardwareConfiguration.getLMHardwareConfigurationFromInvenID(liteHw.getInventoryID());
+            
+            LiteStarsCustAccountInformation liteAcctInfo = 
+                energyCompany.getCustAccountInformation( liteHw.getAccountID(), true );
+            //get zipCode
+            LiteAddress address = energyCompany.getAddress(liteAcctInfo.getAccountSite().getStreetAddressID());
+            String zip = address.getZipCode();
+            //get ConsumptionType
+            LiteCustomer cust = liteAcctInfo.getCustomer();
+            Integer consumptionType = -1;
+            if(cust instanceof LiteCICustomer)
+                consumptionType = ((LiteCICustomer)cust).getCICustType();
+            //get ApplianceCategoryID
+            Integer applianceCatID = -1;
+            for(int j = 0; j < liteAcctInfo.getAppliances().size(); j++) {
+                if(liteAcctInfo.getAppliances().get(j).getApplianceID() == configdb.getApplianceID().intValue())
+                    applianceCatID = liteAcctInfo.getAppliances().get(j).getApplianceCategoryID();
+            }
+            //get SwitchTypeID 
+            Integer devType = liteHw.getLmHardwareTypeID();
+            StaticLoadGroupMapping groupMapping = StaticLoadGroupMapping.getAStaticLoadGroupMapping(applianceCatID, zip, consumptionType, devType);
+            if(groupMapping == null) {
+                configurationSet.add( hwsToAdjust.get(i) );
+                numFailure++;
+                failureInfo.add("A static mapping could not be appropriate for serial number " + liteHw.getManufacturerSerialNumber() 
+                                + ".  ApplianceCategoryID=" + applianceCatID + ", ZipCode=" + zip + ", ConsumptionTypeID=" 
+                                + consumptionType + ",SwitchTypeID=" + devType);
+                continue;
+            }
+            
+            try {
+                configDB.setAddressingGroupID(groupMapping.getLoadGroupID());
+                Transaction.createTransaction( Transaction.UPDATE, configDB ).execute();
+                options = "GroupID:" + groupMapping.getLoadGroupID(); 
+                
+    			/*TODO: Will I need this to make sure accounts/inventory get the change?
+                if (hwConfig != null)
+    				UpdateLMHardwareConfigAction.updateLMConfiguration( hwConfig, liteHw, company );
+    			*/
+    				
+                if (sendConfig) {
+    				YukonSwitchCommandAction.fileWriteConfigCommand(company, liteHw, searchMembers, options)(company, liteHw, true, options);
+    				
+    				if (liteHw.getAccountID() > 0) {
+    					StarsCustAccountInformation starsAcctInfo = company.getStarsCustAccountInformation( liteHw.getAccountID() );
+    					if (starsAcctInfo != null) {
+    						StarsInventory starsInv = StarsLiteFactory.createStarsInventory( liteHw, company );
+    						YukonSwitchCommandAction.parseResponse( starsAcctInfo, starsInv );
+    					}
+    				}
+    			}
+    			
+                /*
+                 * Jon Gill may wish to use the queue instead of going straight out to his batch files
+                 *else {
+    				SwitchCommandQueue.SwitchCommand cmd = new SwitchCommandQueue.SwitchCommand();
+    				cmd.setEnergyCompanyID( company.getLiteID() );
+    				cmd.setAccountID( liteHw.getAccountID() );
+    				cmd.setInventoryID( liteHw.getInventoryID() );
+    				cmd.setCommandType( SwitchCommandQueue.SWITCH_COMMAND_CONFIGURE );
+    				cmd.setInfoString( options );
+    				
+    				SwitchCommandQueue.getInstance().addCommand( cmd, false );
+    			}*/
+    			
+    			numSuccess++;
+			}
+			catch (WebClientException e) {
+				CTILogger.error( e.getMessage() , e );
+				if (errorMsg == null) errorMsg = e.getMessage();
+				configurationSet.add( hwsToAdjust.get(i) );
+				numFailure++;
+                failureInfo.add("A static mapping could not be sent out for serial number " + liteHw.getManufacturerSerialNumber() 
+                                    + ".  ApplianceCategoryID=" + applianceCatID + ", ZipCode=" + zip + ", ConsumptionTypeID=" 
+                                    + consumptionType + ",SwitchTypeID=" + devType);
+			}
+			
+			if (isCanceled) {
+				status = STATUS_CANCELED;
+				return;
+			}
+		}
+		
+		//if (!fullReset) SwitchCommandQueue.getInstance().addCommand( null, true );
+		status = STATUS_FINISHED;
+        session.removeAttribute( InventoryManagerUtil.INVENTORY_SET );
+		session.setAttribute(ServletUtils.ATT_CONFIRM_MESSAGE, "Addressing Groups were reset to static mapped values for " + numSuccess + " switches");
+        
+        if (numFailure > 0) {
+            session.setAttribute(ServletUtils.ATT_ERROR_MESSAGE, "Addressing Groups adjustment failed for " + numFailure + " switches");
+			session.setAttribute(InventoryManagerUtil.INVENTORY_SET, configurationSet);
+			session.setAttribute(ServletUtils.ATT_REDIRECT, request.getContextPath() + "/operator/Hardware/PowerUserStaticLoadGroupReset.jsp");
+		}
+	}
+}
--- yukon-client/yukon-web/WebContent/operator/Hardware/PowerUserStaticLoadGroupReset.jsp
+++ yukon-client/yukon-web/WebContent/operator/Hardware/PowerUserStaticLoadGroupReset.jsp
@@ -1,6 +1,17 @@
 <%@ taglib uri="http://java.sun.com/jstl/core" prefix="c" %>
 <%@ include file="../Consumer/include/StarsHeader.jsp" %>
 <jsp:useBean id="configBean" class="com.cannontech.stars.web.bean.ConfigBean" scope="page"/>
+<jsp:useBean id="resultSetBean" class="com.cannontech.stars.web.bean.InventoryBean" scope="session"/>
+<jsp:setProperty name="resultSetBean" property="energyCompanyID" value="<%= user.getEnergyCompanyID() %>"/>
+
+<% if (request.getParameter("page") == null) { %>
+    <%-- intialize bean properties --%>
+    <jsp:setProperty name="resultSetBean" property="sortBy" value="<%= YukonListEntryTypes.YUK_DEF_ID_INV_SORT_BY_SERIAL_NO %>"/>
+    <jsp:setProperty name="resultSetBean" property="htmlStyle" value="<%= InventoryBean.HTML_STYLE_INVENTORY_SET %>"/>
+    <jsp:setProperty name="resultSetBean" property="page" value="1"/>
+    <jsp:setProperty name="resultSetBean" property="referer" value="<%= session.getAttribute(ServletUtils.ATT_REFERRER) %>"/>
+    <% resultSetBean.resetInventoryList(); %>
+<% } %>
 
 <cti:standardPage title="Energy Services Operations Center" module="stars" htmlLevel="quirks">
 	
@@ -25,18 +36,49 @@
 		<form name="MForm" method="post" action="<%=request.getContextPath()%>/servlet/InventoryManager">
 	    	<input type="hidden" name="action" value="StaticLoadGroupMapMasterReset">
 	    	
+            <div align="center">
+                <c:choose>
+                    <c:when test="${configBean.writeToFileAllowed && configBean.hasResetPermission}"> 
+                        <td> 
+                            <span class="ConfirmMsg">This will set any address group mappings in the database that currently</span>
+                            <br>
+                            <span class="ConfirmMsg">have no addressing group assigned to the defaults specified in the static load group mapping table.</span>
+                        </td> 
+                    </c:when>
+                    <c:otherwise>   
+                        <span class="ErrorMsg"> You have reached this page as a result of incorrect navigation.  Please click Back on your browser.</span>
+                    </c:otherwise>
+                </c:choose>
+            </div>
+            <table width="80%" border="2" cellspacing="0" cellpadding="0" align="center" class="TableCell">
+                <tr>
+                    <c:choose>
+                        <c:when test="${configBean.writeToFileAllowed && configBean.hasResetPermission}"> 
+                            <td> 
+                                <div align="center"> 
+                                    <input type="button" name="DefaultAndConfig" value="Set Unassigned to Default Groups" onclick="setDefaults(this.form)">
+                                </div>
+                                <div align="center"> 
+                                    <input type="button" name="DefaultJustSave" value="Set Unassigned and Save Only (No Configs Sent)" onclick="setDefaultsNoConfig(this.form)">
+                                </div>
+                            </td> 
+                        </c:when>
+                        <c:otherwise>   
+                            
+                        </c:otherwise>
+                    </c:choose>
+                </tr>
+            </table>
+            <br>
+            <br>
+            <br>
 	    	<div align="center">
 				<c:choose>
-					<c:when test="${configBean.hasStaticLoadGroupMapping && configBean.hasResetPermission}"> 
+					<c:when test="${configBean.writeToFileAllowed && configBean.hasResetPermission}"> 
 					 	<td> 
 		                	<span class="ConfirmMsg">This will reset all address group mappings in this database to the defaults</span>
 		                	<br>
-		                	<span class="ConfirmMsg">specified in the static load group mapping table</span>
-		                	<br>  
-		                	<br>
-		                	<span class="ErrorMsg">**This action has the potential to seize maximum resources of this system until completion.</span>
-		                	<span class="ErrorMsg">  It may cause visible slowness or even lack of functionality for any users logged in.</span>
-	                    	<span class="ErrorMsg">  It is recommended that this action only be taken during non-business hours.</span>
+		                	<span class="ConfirmMsg">specified in the static load group mapping table.</span>
 	                    </td> 
                     </c:when>
                     <c:otherwise> 	
@@ -47,11 +89,19 @@
 	    	<table width="80%" border="2" cellspacing="0" cellpadding="0" align="center" class="TableCell">
 			  	<tr>
 					<c:choose>
-						<c:when test="${configBean.hasStaticLoadGroupMapping}"> 
+						<c:when test="${configBean.writeToFileAllowed && configBean.hasResetPermission}"> 
 						 	<td> 
-			                	<div align="center"> 
+			                  <div align="center"> 
 		                        	<input type="submit" name="Reset" value="Reset Now">
 		                      </div>
+                              <div align="center"> 
+                                    <input type="button" name="ResetJustSave" value="Reset and Save Only (No Config Sent)" onclick="resetAllNoConfig(this.form)">
+                              </div>
+                              <div align="center">
+                                  <span class="ErrorMsg">**This action has the potential to seize maximum resources of this system until completion.</span>
+                                  <span class="ErrorMsg">  It may cause visible slowness or even lack of functionality for any users logged in.</span>
+                                  <span class="ErrorMsg">  It is recommended that this action only be taken during non-business hours.</span>
+                              </div>
 		                    </td> 
 	                    </c:when>
 	                    <c:otherwise> 	
@@ -60,7 +110,32 @@
 			    	</c:choose>
 				</tr>
  			</table>
-        	
+        	<br>
+            <br>
+            <br>
+            <table width="80%" border="2" cellspacing="0" cellpadding="0" align="center" class="TableCell">
+                <tr>
+                    <c:choose>
+                        <c:when test="${configBean.writeToFileAllowed && configBean.hasResetPermission}"> 
+                            <td> 
+                                <span class="SubtitleHeader">STATUS MESSAGES</span> 
+                                <% if (confirmMsg != null) out.write("<span class=\"ConfirmMsg\">* " + confirmMsg + "</span><br>"); %>
+                                <% if (errorMsg != null) out.write("<span class=\"ErrorMsg\">* " + errorMsg + "</span><br>"); %>
+                                <%
+                                ArrayList inventorySet = (ArrayList) session.getAttribute(InventoryManagerUtil.INVENTORY_SET);
+                                if (inventorySet != null) {
+                                    resultSetBean.setInventorySet(inventorySet);
+                                %>
+                                    <%= resultSetBean.getHTML(request) %> 
+                                <% } %>
+                            </td> 
+                        </c:when>
+                        <c:otherwise>   
+                            
+                        </c:otherwise>
+                    </c:choose>
+                </tr>
+            </table>
         </form>
     </div>
     
@@ -68,6 +143,24 @@
 		function init()
 		{
 		}
+        
+        function setDefaults(form)
+        {
+            form.action.value="StaticLoadGroupMapSetDefaults";
+            form.submit();
+        }
+        
+        function setDefaultsNoConfig(form)
+        {
+            form.action.value="StaticLoadGroupMapSetDefaultsNoConfig";
+            form.submit();
+        }
+        
+        function resetAllNoConfig(form)
+        {
+            form.action.value="StaticLoadGroupMapMasterResetNoConfig";
+            form.submit();
+        }
 		
 	</script>
 </cti:standardPage>          
