c17afdad5bf71c3ff1b75ef87fdfb85994a93850
Khazon,Elliot <>
2006-08-17 18:47:17 +0000

    refactored cap bank editor to be more independent from cbc editor. added support for deleting from dynamic tables upon 2-way device/volt points being disabled

--- yukon-client/yukon-web/src/com/cannontech/web/editor/CapBankEditorForm.java
+++ yukon-client/yukon-web/src/com/cannontech/web/editor/CapBankEditorForm.java
@@ -1,5 +1,6 @@
 package com.cannontech.web.editor;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -10,14 +11,22 @@ import java.util.Map;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+import org.springframework.jdbc.core.JdbcOperations;
 
 import com.cannontech.clientutils.CTILogger;
 import com.cannontech.common.util.CtiUtilities;
 import com.cannontech.core.dao.DaoFactory;
+import com.cannontech.database.JdbcTemplateHelper;
 import com.cannontech.database.PoolManager;
 import com.cannontech.database.TransactionException;
 import com.cannontech.database.data.capcontrol.CapBank;
+import com.cannontech.database.data.capcontrol.CapBankController;
+import com.cannontech.database.data.capcontrol.CapBankController702x;
 import com.cannontech.database.data.capcontrol.CapControlFeeder;
+import com.cannontech.database.data.device.DeviceBase;
+import com.cannontech.database.data.device.TwoWayDevice;
 import com.cannontech.database.data.lite.LiteFactory;
 import com.cannontech.database.data.lite.LitePoint;
 import com.cannontech.database.data.lite.LiteYukonPAObject;
@@ -27,23 +36,40 @@ import com.cannontech.database.data.point.PointUnits;
 import com.cannontech.database.db.DBPersistent;
 import com.cannontech.database.db.capcontrol.CCMonitorBankList;
 import com.cannontech.database.db.capcontrol.CapControlStrategy;
+import com.cannontech.database.db.device.DeviceScanRate;
 import com.cannontech.web.util.JSFComparators;
 
-public class CapBankEditorForm extends DBEditorForm {
-
- private List unassignedPoints = null;
+public class CapBankEditorForm extends DBEditorForm{
+    
+ /**
+     * 
+     */
+//    private static final long serialVersionUID = 5491225604682004562L;
+private List unassignedPoints = null;
  private List assignedPoints = null;
  private MultiDBPersistent monitorPointsVector = null;
- private List columns;
- 
+ //this variable exists only to display relevant information about a cbc 
+ //in the cap bank editor. Right now it is not possible to
+ //edit cbc from cap bank editor (which is how it should be since there is a separate editor for CBC).
+ //This object should be decoupled from CBController class which is where editing occurs.
+ //TODO move the setting of the control device to this menu as well
+ private DeviceBase controller = null;
+ //this cap bank that we are editing
+ private CapBank capBank = null;
+ private String[] DYNAMIC_TABLE_NAMES = {"DynamicCCMonitorBankHistory","DynamicCCMonitorPointResponse"};
  
 	public CapBankEditorForm() {
 		super();
 	}
 
 	
-	protected void checkForErrors() throws Exception {
-		// TODO Auto-generated method stub
+	protected void checkForErrors() throws IllegalArgumentException {
+		List monitorPointList = ((CapBank)getDbPersistent()).getCcMonitorBankList();
+		for (Iterator iter = monitorPointList.iterator(); iter.hasNext();) {
+			CCMonitorBankList monitorPoint = (CCMonitorBankList) iter.next();
+			if (monitorPoint.getNINAvg().longValue() <= 0 )
+				throw new IllegalArgumentException ("Adaptive Count value should be greater then 0.");
+		}
 	}
 	
 	public void update() {
@@ -57,9 +83,18 @@ public class CapBankEditorForm extends DBEditorForm {
 			points.add(monitorPoint);
 		}
 		((CapBank)getDbPersistent()).setCcMonitorBankList(points);
+		
 		try {
-			updateDBObject(getDbPersistent(), facesMessage);			
+			checkForErrors();
+			updateDBObject(getDbPersistent(), facesMessage);
+			capBank = (CapBank)getDbPersistent();
+			handleMonitorPointsForController(capBank.getCapBank().getControlDeviceID().intValue());
 		} 		
+		catch(IllegalArgumentException eae) {
+            String errorString = eae.getMessage();
+            facesMessage.setDetail(errorString);
+            facesMessage.setSeverity(FacesMessage.SEVERITY_ERROR);
+		}
 		catch (TransactionException e) {
             String errorString = e.getMessage();
             facesMessage.setDetail(errorString);
@@ -69,6 +104,18 @@ public class CapBankEditorForm extends DBEditorForm {
 					facesMessage);
 		}
 
+	}
+
+
+	/**
+	 * 
+	 */
+	private void resetAdvancedTab() {
+		handleMonitorPointsForController(capBank.getCapBank().getControlDeviceID().intValue());
+		assignedPoints = DaoFactory.getPointDao().getCapBankMonitorPoints(capBank);
+		unassignedPoints = null;
+		getUnassignedPoints();
+		Collections.sort(assignedPoints, JSFComparators.monitorPointDisplayOrderComparator);
 	}
 
 	
@@ -86,10 +133,11 @@ public class CapBankEditorForm extends DBEditorForm {
 			CapBank capBank = ((CapBank) getDbPersistent());			
 			int controlDeviceId = capBank.getCapBank().getControlDeviceID().intValue();
 			if (controlDeviceId > 0) {
-                List<LitePoint> allPoints = 
-                    DaoFactory.getPointDao().getLitePointsByPaObjectId(controlDeviceId);
-                for (LitePoint point : allPoints) {
-    				if (point.getUofmID() == PointUnits.UOMID_VOLTS) {							
+				List allPoints = DaoFactory.getPointDao().getLitePointsByPaObjectId(controlDeviceId);
+			for (int i = 0; i < allPoints.size(); i++) {
+				LitePoint point = (LitePoint)allPoints.get(i);			
+				if (point.getUofmID() == PointUnits.UOMID_VOLTS)
+					{							
 						CapBankMonitorPointParams monitorPoint = new CapBankMonitorPointParams(point);
 						//set the cap bank id to replace the CBC if with Cap Bank Id
 						monitorPoint.setCapBankId(capBank.getCapBank().getDeviceID().intValue());
@@ -99,8 +147,8 @@ public class CapBankEditorForm extends DBEditorForm {
 						if(!isPointAssigned (monitorPoint)) {
 							unassignedPoints.add(monitorPoint);
 						}								
-    					}
-    				}				
+					}
+				}				
 			}
 					Collections.sort(unassignedPoints, JSFComparators.monitorPointComparator);
 			}
@@ -201,14 +249,108 @@ public class CapBankEditorForm extends DBEditorForm {
 	// hook into capcontrol form
 	public void init(DBPersistent dbPersistent) {
 		if (getDbPersistent() == null) {
-			if (dbPersistent instanceof CapBank) {
 			setDbPersistent(dbPersistent);
-			//set the assigned points
-			CapBank capBank = ((CapBank)getDbPersistent());
-			assignedPoints = DaoFactory.getPointDao().getCapBankMonitorPoints(capBank);
-			Collections.sort(assignedPoints, JSFComparators.monitorPointDisplayOrderComparator);
+			if (dbPersistent instanceof CapBank) {
+				capBank = (CapBank)getDbPersistent();		
+				initController(capBank);
 			}
 		}
+	}
+
+
+	/**
+	 * @param capBank
+	 */
+	private void initController(CapBank capBank) {
+		int controllerID = resetController(capBank);
+		//need to make sure that controller is synched with assigned points
+		//make sure that all the points that are defined in assigned and unassined lists
+		//are the points available for the given controller
+		if (controller instanceof CapBankController) {
+			handleMonitorPointsForController(controllerID);
+			}
+		else if (controller instanceof CapBankController702x){
+			assignedPoints = DaoFactory.getPointDao().getCapBankMonitorPoints(capBank);
+			unassignedPoints = null;
+			getUnassignedPoints();
+			Collections.sort(assignedPoints, JSFComparators.monitorPointDisplayOrderComparator);
+		}
+	}
+
+
+	/**
+	 * @param capBank
+	 * @return
+	 */
+	private int resetController(CapBank capBank) {
+		int controllerID = capBank.getCapBank().getControlDeviceID().intValue();
+		LiteYukonPAObject liteController = DaoFactory.getPaoDao().getLiteYukonPAO(controllerID);
+		if (liteController == null)
+			liteController = DaoFactory.getPaoDao().getLiteYukonPAO(0);				
+		DBPersistent temp = LiteFactory.convertLiteToDBPersAndRetrieve(liteController);		
+		if (temp instanceof DeviceBase) {
+			controller = (DeviceBase) temp;	
+			}
+		return controllerID;
+	}
+
+
+	/**
+	 * @param controllerID
+	 */
+	private void handleMonitorPointsForController(int controllerID) {
+		handleAllPointsOnList(getAssignedPoints(), controllerID, true);
+		if ( (getAssignedPoints() == null) || (getAssignedPoints().size() == 0) )
+				deleteAllDynamic (getUnassignedPoints());	
+		handleAllPointsOnList(getUnassignedPoints(), controllerID, false);			
+	}
+
+	private void deleteAllDynamic (List points) {
+		
+		for (int i = 0; i < DYNAMIC_TABLE_NAMES.length; i++) {
+			String table = DYNAMIC_TABLE_NAMES[i];
+			for (Iterator iter = points.iterator(); iter.hasNext();) {
+				CapBankMonitorPointParams point = (CapBankMonitorPointParams) iter.next();
+				deletePointFromTable (table, point.getPointId());
+			}
+		}
+	}
+	
+	
+	private void deletePointFromTable(String table, int pointId) {
+		String sqlStmt = "DELETE FROM " + table + " WHERE pointId = ?";       
+        JdbcOperations yukonTemplate = JdbcTemplateHelper.getYukonTemplate();                    
+        yukonTemplate.update(sqlStmt, new Integer[] {new Integer ( pointId ) });
+	}
+	
+
+	private void handleAllPointsOnList (List points, int controllerID, boolean delDynamic) {
+		List controllerPoints = DaoFactory.getPointDao().getLitePointsByPaObjectId(controllerID);
+		List pointsToRemove = new ArrayList (10);
+		if (controllerPoints != null && points != null) {
+			for (Iterator iter = points.iterator(); iter.hasNext();) {
+				CapBankMonitorPointParams point = (CapBankMonitorPointParams) iter.next();
+				if (!pointBelongsToController (controllerPoints, point.getPointId()))
+					pointsToRemove.add(point);
+			}
+			if (pointsToRemove != null)
+			{
+				if (delDynamic) 
+					deleteAllDynamic(pointsToRemove);										
+			points.removeAll(pointsToRemove);		
+			
+			}
+		}
+	}
+
+
+	private boolean pointBelongsToController(List controllerPoints, int pointID) {
+		for (int i = 0; i < controllerPoints.size(); i++) {
+			LitePoint point = (LitePoint) controllerPoints.get(i);
+			if (point.getLiteID() == pointID)
+				return true;
+		}
+		return false;
 	}
 
 
@@ -232,5 +374,92 @@ public class CapBankEditorForm extends DBEditorForm {
 		}				
 		return false;
 	}
+
+
+	public DeviceBase getController() {
+		return controller;
+	}
+	
+	   public boolean isTwoWayController() {
+	        if (controller != null)
+	        	return (controller instanceof TwoWayDevice); 
+	        return false;
+	    }
+	    
+	    public boolean isOneWayController() {
+	        if (controller != null) {
+	        	if ( !isTwoWayController() && (controller instanceof CapBankController))
+	        		
+	        		return true;
+	        }
+	        return false;
+	        
+	    }
+
+
+		public CapBank getCapBank() {
+			return capBank;
+		}
 		
+	    public boolean isEditingIntegrity() {
+
+	        return isTwoWayController() && ((TwoWayDevice) controller).getDeviceScanRateMap()
+	                                                         .containsKey(DeviceScanRate.TYPE_INTEGRITY);
+	    }
+
+	    /**
+	     * @return
+	     */
+	    public boolean isEditingException() {
+
+	        return isTwoWayController() && ((TwoWayDevice) controller).getDeviceScanRateMap()
+	                                                         .containsKey(DeviceScanRate.TYPE_EXCEPTION);
+	    }
+		
+
+		/**
+		 * Event fired when the the CapBank control point selection has changed
+		 * @throws IOException 
+		 */
+		public void capBankTeeClick(ActionEvent ae) throws IOException {
+
+			String val = (String) FacesContext.getCurrentInstance()
+					.getExternalContext().getRequestParameterMap().get("ptID");
+			if (val == null)
+				return;
+			if (getDbPersistent() instanceof CapBank) {				
+	            capBank.getCapBank().setControlPointID(new Integer(val));
+				int controlPointId = capBank.getCapBank().getControlPointID().intValue();
+	            LitePoint litePoint = DaoFactory.getPointDao().getLitePoint(controlPointId);
+				if (litePoint != null) {
+		            int paoId = litePoint.getPaobjectID();
+		            Integer ctlPointid = new Integer(paoId);	            
+		            capBank.getCapBank().setControlDeviceID(ctlPointid);
+				}				
+			}	
+			resetController(capBank);
+			if (controller instanceof CapBankController)
+				handleMonitorPointsForController(capBank.getCapBank().getControlDeviceID().intValue());
+			else
+				resetAdvancedTab();
+		}
+		
+        public String getCtlPaoName () {
+            Integer controlDeviceID = capBank.getCapBank().getControlDeviceID();
+            return DaoFactory.getPaoDao().getLiteYukonPAO (controlDeviceID).getPaoName();    
+        }
+		
+        public String getCtlPointName () {
+            int pointID = capBank.getCapBank().getControlPointID();
+            return DaoFactory.getPointDao().getLitePoint(pointID).getPointName();
+        }
+        
+        public Integer getCtlPaoID () {
+            return capBank.getCapBank().getControlDeviceID();
+        }
+
+        public Integer getCtlPointID () {
+            return capBank.getCapBank().getControlPointID();
+        }
+
 }
