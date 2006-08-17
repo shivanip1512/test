f55418ff13b2706e788c4071259f5ee0fb47fb5c
Lauinger, Aaron C <AaronCLauinger@Eaton.com>
2006-08-17 21:27:15 +0000

    Need to fix this thing up before readding it

--- yukon-client/common/test/com/cannontech/common/AllTests.java
+++ yukon-client/common/test/com/cannontech/common/AllTests.java
@@ -8,7 +8,6 @@ public class AllTests {
     public static Test suite() {
         TestSuite suite = new TestSuite("Test for com.cannontech.common");
         //$JUnit-BEGIN$
-        suite.addTestSuite(com.cannontech.dynamic.TestAsyncDynamicDataSource.class);
         //$JUnit-END$
         return suite;
     }
