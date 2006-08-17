b6867264c4a54c21e65fea0cc47534c02c993cc1
Lauinger, Aaron C <AaronCLauinger@Eaton.com>
2006-08-17 21:25:38 +0000

    Eliminated threading issues and Fixed up how id's are generated, not sure if random is any beter though

--- yukon-client/common/src/com/cannontech/message/util/ServerRequestImpl.java
+++ yukon-client/common/src/com/cannontech/message/util/ServerRequestImpl.java
@@ -43,28 +43,75 @@ import com.cannontech.yukon.IServerConnection;
  * @author aaron
  * TODO: Fix exception handling
  */
-public class ServerRequestImpl implements MessageListener, ServerRequest 
+public class ServerRequestImpl implements ServerRequest 
 {
 	private static int MIN_RESERVED_ID = 0;
     private static int MAX_RESERVED_ID = 10;
 	
 	// used to generate a request id
 	private static int _currentRequestID = Integer.MIN_VALUE;
+	private static Random randomGen = new Random(System.currentTimeMillis());
 
-	static { 
-		Random r = new Random(System.currentTimeMillis());
+    /**
+     * InnerServerRequest is stateful and should only be used for a single request.
+     * @author alauinger
+     *
+     */
+    private class InnerServerRequest implements MessageListener {
+        private IServerConnection _connection;
+        private ServerRequestMsg _requestMsg;  
+        private ServerResponseMsg _responseMsg;        
+        InnerServerRequest(IServerConnection conn, ServerRequestMsg reqMsg) {
+            _connection = conn;
+            _requestMsg = reqMsg;
+        }
+ 
+        /**
+         * Returns a ServerResponseMsg that matches the request.
+         * If no matching response is received a Timeout response will be returned 
+         * after the given timeout number of milliseconds.
+         * @param timeout
+         * @return
+         * @throws Exception
+         */
+        private synchronized ServerResponseMsg execute(long timeout) {
+            try { 
+                //Add this as a listener so we can look for a response
+                _connection.addMessageListener(this);
+                _connection.write(_requestMsg);
+                wait(timeout);
+            }
+            catch(InterruptedException ie) {
+            }
+            finally {
+                //Make sure to remove us or else there will be a leak!
+                _connection.removeMessageListener(this);
+            }
+            
+            // Did we get a response that matched our request id?
+            if(_responseMsg == null) {
+                _responseMsg = ServerResponseMsg.createTimeoutResp();
+                //throw new TimeoutException("Timed out waiting for response message with id: " + _requestID);
+            }
+            
+            return _responseMsg;
+        }
         
-        //values MIN_RESERVED_ID to MAX_RESERVED_ID inclusive are reserved
-        while( _currentRequestID >= MIN_RESERVED_ID && _currentRequestID <= MAX_RESERVED_ID )
-            _currentRequestID = r.nextInt();
-	}
+        /* (non-Javadoc)
+         * @see com.cannontech.message.util.ServerRequest#messageReceived(com.cannontech.message.util.MessageEvent)
+         */
+        public synchronized void messageReceived(MessageEvent e) {
+            Message msg = e.getMessage();
+            if(msg instanceof ServerResponseMsg) {
+                ServerResponseMsg responseMsg = (ServerResponseMsg) msg;
+                if(responseMsg.getId() == _requestMsg.getId() ) {
+                    _responseMsg = responseMsg;
+                    notify(); //score! we found matching response, let the blocked thread know
+                }
+            }
+        }
+    }
 
-	private IServerConnection _connection;
-	private ServerRequestMsg _requestMsg;
-	private ServerResponseMsg _responseMsg;
-	private int _requestID;
-
-	
     /* (non-Javadoc)
      * @see com.cannontech.message.util.ServerRequest#makeServerRequest(com.cannontech.yukon.IServerConnection, com.cannontech.message.util.Message)
      */
@@ -76,63 +123,18 @@ public class ServerRequestImpl implements MessageListener, ServerRequest
      * @see com.cannontech.message.util.ServerRequest#makeServerRequest(com.cannontech.yukon.IServerConnection, com.cannontech.message.util.Message, long)
      */
     public ServerResponseMsg makeServerRequest(IServerConnection conn, Message msg, long timeout) {
-        _connection = conn;        
-        _requestMsg = new ServerRequestMsg();
-        _requestMsg.setPayload(msg);
+        int reqId = nextClientMessageID();
+        ServerRequestMsg reqMsg = new ServerRequestMsg();
+        reqMsg.setPayload(msg);
+        reqMsg.setId(reqId);
         
-        _requestID = nextClientMessageID();
-        _requestMsg.setId(_requestID);
+        InnerServerRequest innerReq = new InnerServerRequest(conn, reqMsg);
         
-        return execute(timeout);
+        return innerReq.execute(timeout);
 	}
 	
-	/**
-	 * Returns a ServerResponseMsg that matches the request.
-	 * If no matching response is received a Timeout response will be returned 
-     * after the given timeout number of milliseconds.
-	 * @param timeout
-	 * @return
-	 * @throws Exception
-	 */
-	private synchronized ServerResponseMsg execute(long timeout) {
-		try { 
-			//Add this as a listener so we can look for a response
-			_connection.addMessageListener(this);
-			_connection.write(_requestMsg);
-			wait(timeout);
-		}
-        catch(InterruptedException ie) {
-        }
-		finally {
-			//Make sure to remove us or else there will be a leak!
-			_connection.removeMessageListener(this);
-		}
-		
-		// Did we get a response that matched our request id?
-		if(_responseMsg == null) {
-            _responseMsg = ServerResponseMsg.createTimeoutResp();
-			//throw new TimeoutException("Timed out waiting for response message with id: " + _requestID);
-		}
-		
-		return _responseMsg;
-	}
-
    public ServerRequestImpl() {
    } 
-	
-	/* (non-Javadoc)
-     * @see com.cannontech.message.util.ServerRequest#messageReceived(com.cannontech.message.util.MessageEvent)
-     */
-	public synchronized void messageReceived(MessageEvent e) {
-		Message msg = e.getMessage();
-		if(msg instanceof ServerResponseMsg) {
-			ServerResponseMsg responseMsg = (ServerResponseMsg) msg;
-			if(responseMsg.getId() == _requestID) {
-				_responseMsg = responseMsg;
-				notify(); //score! we found matching response, let the blocked thread know
-			}
-		}
-	}
 	
 	/**
 	 * Generate the next client message id
@@ -140,11 +142,12 @@ public class ServerRequestImpl implements MessageListener, ServerRequest
 	 */
 	private static synchronized int nextClientMessageID()
     {
-        _currentRequestID +=
-            (_currentRequestID >= MIN_RESERVED_ID && _currentRequestID <= MAX_RESERVED_ID
-                ? MAX_RESERVED_ID + 1
-                : 1 );
-        
+	    do {
+          _currentRequestID = randomGen.nextInt();  
+        }
+        while(_currentRequestID >= MIN_RESERVED_ID && 
+              _currentRequestID <= MAX_RESERVED_ID);
+     
         return _currentRequestID;
 	}
 }
