76c0f062f654aaee77d421d0c74a61f337a02811
Mack, Tom <>
2006-08-21 23:16:43 +0000

    Much has changed. The result table is now built in a JS function instead of being returned in whole. Not the prettiest solution, but it gives the JS code a lot more information which it can use when the point is selected (which is the other major change).

---
+++ yukon-client/yukon-web/WebContent/JavaScript/tableCreation.js
@@ -0,0 +1,59 @@
+// The following function creates a table given an array of row data
+// and an array that specifies the order of the columns. An example
+// dataArray may look like this:
+//    [{'firstname': 'Tom', 'lastname': 'Mack'},
+//     {'firstname': 'Jon', 'lastname': 'Dayton'},
+//     {'firstname': 'Aaron', 'lastname': 'Solberg'}]
+// An example outputCols might then look like this:
+//    [{'title': 'First Name', 'field': 'firstname'},
+//     {'title': 'Last Name', 'field': 'lastname'}]
+// Note that the keys and values from the dataArray are completely
+// arbitrary. On the other hand, the outputCols array must have 
+// 'title', 'field', and (optionally) 'link' keys for each entry
+// and the value of the 'field' key must match a key in the dataArray.
+// The rowCallback is a function of the following form:
+//    function(rowElement, data) {
+//        if (data.firstname == 'Tom') {
+//            rowElement.addClassName('awesome'); # causes <tr class="awesome"> ... </tr> to be output
+//        }
+//     }
+// An unattached table DOM element is returned from this function.
+function createHtmlTableFromJson(dataArray, outputCols, rowCallback) {
+    var resultTable = document.createElement("table");
+    var resultTableHead = document.createElement("thead");
+    resultTable.appendChild(resultTableHead);
+    var headRow = document.createElement("tr");
+    resultTableHead.appendChild(headRow);
+    for (col = 0; col < outputCols.length; col++) {
+        var headCell = document.createElement("th");
+        headCell.appendChild(document.createTextNode(outputCols[col].title));
+        headRow.appendChild(headCell);
+    }
+    var resultTableBody = document.createElement("tbody");
+    resultTable.appendChild(resultTableBody);
+    for (i=0; i < dataArray.length; i++) {
+      var tableRow = document.createElement("tr");
+      (rowCallback || Prototype.emptyFunction)(tableRow, dataArray[i]);
+      resultTableBody.appendChild(tableRow);
+      for (col = 0; col < outputCols.length; col++) {
+          var tableCell = document.createElement("td");
+          var node = tableCell;
+          tableRow.appendChild(tableCell);
+          if (outputCols[col].link) {
+              var link = document.createElement("a");
+              node = link;
+              tableCell.appendChild(link);
+              var linkFuncGenerate = outputCols[col].link;
+              link.setAttribute("href", "javascript:nothing()");
+              var linkFunc = linkFuncGenerate(dataArray[i]);
+              Event.observe(link,'click',linkFunc);
+          }
+          var text = document.createTextNode(dataArray[i][outputCols[col].field]);
+          node.appendChild(text);
+      }
+    }
+    return resultTable;
+}
+
+function nothing() {
+}
