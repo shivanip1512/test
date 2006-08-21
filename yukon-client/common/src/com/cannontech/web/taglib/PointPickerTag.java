c3de4cf40a37c9db6cf822cbbcb2cc2f9dd85dfb
Mack, Tom <>
2006-08-21 20:56:49 +0000

    Updated tag to build up the new "extra info" parameter. The extra info controls how extra information about the point is populated into the HTML page upon selection of a point.

--- yukon-client/common/src/com/cannontech/web/taglib/PointPickerTag.java
+++ yukon-client/common/src/com/cannontech/web/taglib/PointPickerTag.java
@@ -1,11 +1,14 @@
 package com.cannontech.web.taglib;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.TagSupport;
 
 import org.apache.commons.lang.RandomStringUtils;
+import org.apache.commons.lang.StringUtils;
 
 public class PointPickerTag extends TagSupport {
     private String pointIdField;
@@ -24,13 +27,23 @@ public class PointPickerTag extends TagSupport {
             StandardPageTag spTag = (StandardPageTag) TagSupport.findAncestorWithClass(this, StandardPageTag.class);
             if (spTag != null) {
                 spTag.addScriptFile("/JavaScript/pointPicker.js");
+                spTag.addScriptFile("/JavaScript/tableCreation.js");
                 spTag.addCSSFile("/WebConfig/yukon/styles/pointPicker.css");
             }
             
-            
-            String sufix = RandomStringUtils.randomNumeric(6);
+            // build up extraMapping string
+            List<String> extraMappings = new ArrayList<String>();
+            if (StringUtils.isNotBlank(pointNameElement)) {
+                extraMappings.add("pointName:" + pointNameElement);
+            }
+            if (StringUtils.isNotBlank(paoNameElement)) {
+                extraMappings.add("deviceName:" + paoNameElement);
+            }
+            String extraMappingString = StringUtils.join(extraMappings.iterator(), ";");
             pageContext.getOut().println(
-                " <a href=\"javascript:pointPicker_showPicker(\'"+ pointIdField + "\','" + constraint + "')\">");
+                " <a href=\"javascript:pointPicker_showPicker(\'"
+                    + pointIdField + "\','" + constraint + "', '" 
+                    + extraMappingString + "')\">");
             return super.doEndTag();
         } catch (IOException e) {
             throw new JspException("Can't output PointPickerTag", e);
