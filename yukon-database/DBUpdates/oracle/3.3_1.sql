3b6fbd95080f681eaf95e7bcc77426b0f9f3619d
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2006-08-23 22:58:13 +0000

    Changed DeviceMeterGroup.CollectionGroup to size 50. (orgin in 3.2.9)

---
+++ yukon-database/DBUpdates/oracle/3.3_1.sql
@@ -0,0 +1,17 @@
+/******************************************/
+/**** Oracle 9.2 DBupdates             ****/
+/******************************************/
+alter table DeviceMeterGroup modify CollectionGroup VARCHAR2(50) not null;
+
+/******************************************************************************/
+/* Run the Stars Update if needed here */
+/* Note: DBUpdate application will ignore this if STARS is not present */
+/* @include StarsUpdate */
+/******************************************************************************/
+
+
+/**************************************************************/
+/* VERSION INFO                                               */
+/*   Automatically gets inserted from build script            */
+/**************************************************************/
+/* __YUKON_VERSION__ */
---
+++ yukon-database/DBUpdates/sqlserver/3.3_1.sql
@@ -0,0 +1,17 @@
+/******************************************/
+/**** SQLServer 2000 DBupdates         ****/
+/******************************************/
+alter table DeviceMeterGroup alter column CollectionGroup varchar(50) not null;
+go
+/******************************************************************************/
+/* Run the Stars Update if needed here */
+/* Note: DBUpdate application will ignore this if STARS is not present */
+/* @include StarsUpdate */
+/******************************************************************************/
+
+
+/**************************************************************/
+/* VERSION INFO                                               */
+/*   Automatically gets inserted from build script            */
+/**************************************************************/
+/* __YUKON_VERSION__ */
