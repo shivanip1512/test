49d4c23c5543db1bf6c807581d456f85bd358f4d
Lauinger, Aaron C <AaronCLauinger@Eaton.com>
2006-08-25 18:37:35 +0000

    Adding alarm categories up to 100

--- yukon-database/DBUpdates/sqlserver/3.3_1.sql
+++ yukon-database/DBUpdates/sqlserver/3.3_1.sql
@@ -13,6 +13,77 @@ delete MSPInterface where vendorid = 1 and interface = 'EA_MR';
 delete MSPInterface where vendorid = 1 and interface = 'OA_OD';
 delete MSPInterface where vendorid = 1 and interface = 'CB_CD';
 go
+
+insert into AlarmCategory values(33,'Category 33',1);
+insert into AlarmCategory values(34,'Category 34',1);
+insert into AlarmCategory values(35,'Category 35',1);
+insert into AlarmCategory values(36,'Category 36',1);
+insert into AlarmCategory values(37,'Category 37',1);
+insert into AlarmCategory values(38,'Category 38',1);
+insert into AlarmCategory values(39,'Category 39',1);
+insert into AlarmCategory values(40,'Category 40',1);
+insert into AlarmCategory values(41,'Category 41',1);
+insert into AlarmCategory values(42,'Category 42',1);
+insert into AlarmCategory values(43,'Category 43',1);
+insert into AlarmCategory values(44,'Category 44',1);
+insert into AlarmCategory values(45,'Category 45',1);
+insert into AlarmCategory values(46,'Category 46',1);
+insert into AlarmCategory values(47,'Category 47',1);
+insert into AlarmCategory values(48,'Category 48',1);
+insert into AlarmCategory values(49,'Category 49',1);
+insert into AlarmCategory values(50,'Category 50',1);
+insert into AlarmCategory values(51,'Category 51',1);
+insert into AlarmCategory values(52,'Category 52',1);
+insert into AlarmCategory values(53,'Category 53',1);
+insert into AlarmCategory values(54,'Category 54',1);
+insert into AlarmCategory values(55,'Category 55',1);
+insert into AlarmCategory values(56,'Category 56',1);
+insert into AlarmCategory values(57,'Category 57',1);
+insert into AlarmCategory values(58,'Category 58',1);
+insert into AlarmCategory values(59,'Category 59',1);
+insert into AlarmCategory values(60,'Category 60',1);
+insert into AlarmCategory values(61,'Category 61',1);
+insert into AlarmCategory values(62,'Category 62',1);
+insert into AlarmCategory values(63,'Category 63',1);
+insert into AlarmCategory values(64,'Category 64',1);
+insert into AlarmCategory values(65,'Category 65',1);
+insert into AlarmCategory values(66,'Category 66',1);
+insert into AlarmCategory values(67,'Category 67',1);
+insert into AlarmCategory values(68,'Category 68',1);
+insert into AlarmCategory values(69,'Category 69',1);
+insert into AlarmCategory values(70,'Category 70',1);
+insert into AlarmCategory values(71,'Category 71',1);
+insert into AlarmCategory values(72,'Category 72',1);
+insert into AlarmCategory values(73,'Category 73',1);
+insert into AlarmCategory values(74,'Category 74',1);
+insert into AlarmCategory values(75,'Category 75',1);
+insert into AlarmCategory values(76,'Category 76',1);
+insert into AlarmCategory values(77,'Category 77',1);
+insert into AlarmCategory values(78,'Category 78',1);
+insert into AlarmCategory values(79,'Category 79',1);
+insert into AlarmCategory values(80,'Category 80',1);
+insert into AlarmCategory values(81,'Category 81',1);
+insert into AlarmCategory values(82,'Category 82',1);
+insert into AlarmCategory values(83,'Category 83',1);
+insert into AlarmCategory values(84,'Category 84',1);
+insert into AlarmCategory values(85,'Category 85',1);
+insert into AlarmCategory values(86,'Category 86',1);
+insert into AlarmCategory values(87,'Category 87',1);
+insert into AlarmCategory values(88,'Category 88',1);
+insert into AlarmCategory values(89,'Category 89',1);
+insert into AlarmCategory values(90,'Category 90',1);
+insert into AlarmCategory values(91,'Category 91',1);
+insert into AlarmCategory values(92,'Category 92',1);
+insert into AlarmCategory values(93,'Category 93',1);
+insert into AlarmCategory values(94,'Category 94',1);
+insert into AlarmCategory values(95,'Category 95',1);
+insert into AlarmCategory values(96,'Category 96',1);
+insert into AlarmCategory values(97,'Category 97',1);
+insert into AlarmCategory values(98,'Category 98',1);
+insert into AlarmCategory values(99,'Category 99',1);
+insert into AlarmCategory values(100,'Category 100',1);
+go
+
 /******************************************************************************/
 /* Run the Stars Update if needed here */
 /* Note: DBUpdate application will ignore this if STARS is not present */
