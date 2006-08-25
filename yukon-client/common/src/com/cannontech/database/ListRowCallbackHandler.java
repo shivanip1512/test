925820b41ea6f1e266468854baf5e7805d253ddb
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2006-08-25 19:47:05 +0000

    Call back handler that returns a list of objects for use with JdbcTemplate

---
+++ yukon-client/common/src/com/cannontech/database/ListRowCallbackHandler.java
@@ -0,0 +1,29 @@
+package com.cannontech.database;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class ListRowCallbackHandler extends AbstractRowCallbackHandler
+{
+    private List objectList;
+    private RowMapper rowMapper;
+    public ListRowCallbackHandler(List objectList, RowMapper rowMapper) {
+        super();
+        this.objectList = objectList;
+        this.rowMapper = rowMapper;
+    }
+
+    @Override
+    public void processRow(ResultSet rs, int rowNum) throws SQLException
+    {
+        objectList.add(rowMapper.mapRow(rs, rowNum));
+    }
+
+    public List getObjectList()
+    {
+        return objectList;
+    }
+}
