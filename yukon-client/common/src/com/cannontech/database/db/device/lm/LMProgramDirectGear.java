54077b94b9d46b45941404566dba3c7c0ef05cbc
Dayton, Jon R <JonRDayton@Eaton.com>
2006-09-13 18:16:06 +0000

    Oops, left in the pluses from the rej patch file...

--- yukon-client/common/src/com/cannontech/database/db/device/lm/LMProgramDirectGear.java
+++ yukon-client/common/src/com/cannontech/database/db/device/lm/LMProgramDirectGear.java
@@ -4,8 +4,8 @@ import java.sql.Statement;
 
 import com.cannontech.common.util.CtiUtilities;
 import com.cannontech.database.data.device.lm.NoControlGear;
-+import com.cannontech.database.data.device.lm.MagnitudeCycleGear;
-+import com.cannontech.database.data.device.lm.TargetCycleGear;
+import com.cannontech.database.data.device.lm.MagnitudeCycleGear;
+import com.cannontech.database.data.device.lm.TargetCycleGear;
 import com.cannontech.database.data.device.lm.ThermostatPreOperateGear;
 import com.cannontech.database.data.device.lm.ThermostatSetbackGear;
 import com.cannontech.database.db.NestedDBPersistent;
