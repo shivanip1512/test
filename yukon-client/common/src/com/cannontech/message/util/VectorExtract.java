08eb4bff545a05bc14f8559ce3f5dd92092ed7d2
Mack, Tom <>
2006-10-25 02:42:51 +0000

    Added helper methods to allow inserting/extracting Lists and integer arrays.

--- yukon-client/common/src/com/cannontech/message/util/VectorExtract.java
+++ yukon-client/common/src/com/cannontech/message/util/VectorExtract.java
@@ -1,12 +1,14 @@
 package com.cannontech.message.util;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Vector;
 
 
 public class VectorExtract 
 {
     
+    @SuppressWarnings("unchecked")
     public static Vector extractVector(com.roguewave.vsj.VirtualInputStream vstr, com.roguewave.vsj.CollectableStreamer polystr) throws IOException
     {
         
@@ -15,6 +17,25 @@ public class VectorExtract
 
         for (int i = 0; i < num; i++) {
             v.addElement(vstr.restoreObject(polystr));
+        }
+        return v;
+    }
+    
+    @SuppressWarnings("unchecked")
+    public static void extractVector(Collection c, com.roguewave.vsj.VirtualInputStream vstr, com.roguewave.vsj.CollectableStreamer polystr) throws IOException {
+        int num = (int) vstr.extractUnsignedInt();
+
+        for (int i = 0; i < num; i++) {
+            c.add(vstr.restoreObject(polystr));
+        }
+    }
+    
+    public static int[] extractIntArray(com.roguewave.vsj.VirtualInputStream vstr, com.roguewave.vsj.CollectableStreamer polystr) throws IOException {
+        int num = (int) vstr.extractUnsignedInt();
+        int[] v = new int[num];
+        
+        for (int i = 0; i < num; i++) {
+            v[i] = vstr.extractInt();
         }
         return v;
     }
--- yukon-client/common/src/com/cannontech/message/util/VectorInsert.java
+++ yukon-client/common/src/com/cannontech/message/util/VectorInsert.java
@@ -1,14 +1,14 @@
 package com.cannontech.message.util;
 
 import java.io.IOException;
-import java.util.Vector;
+import java.util.List;
 
 import com.roguewave.vsj.CollectableStreamer;
 import com.roguewave.vsj.VirtualOutputStream;
 
 public class VectorInsert 
 {
-    public static void insertVector(Vector v, VirtualOutputStream vstr, CollectableStreamer polystr) throws IOException
+    public static void insertVector(List v, VirtualOutputStream vstr, CollectableStreamer polystr) throws IOException
     {
         vstr.insertUnsignedInt( v.size() );
         for(int i=0;i<v.size();i++)
@@ -16,4 +16,14 @@ public class VectorInsert
             vstr.saveObject( v.get(i), polystr );
         }
     }
+    
+    public static void insertIntArray(int[] a, VirtualOutputStream vstr, CollectableStreamer polystr) throws IOException
+    {
+        vstr.insertUnsignedInt( a.length );
+        for(int i=0; i < a.length; i++)
+        {
+            vstr.insertInt(a[i]);
+        }
+    }
+    
 }
