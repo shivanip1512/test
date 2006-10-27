4e3634a25b55967357f1e866f949d1de5bafbfd9
Dayton, Jon R <JonRDayton@Eaton.com>
2006-10-27 19:49:23 +0000

    --Database changes 3.2.10 and up: New columns in CapControlStrategy

--- yukon-database/DBUpdates/oracle/3.2_10.sql
+++ yukon-database/DBUpdates/oracle/3.2_10.sql
@@ -9,6 +9,30 @@ insert into fdrinterfaceoption values(12,'Filename',3,'Text','(none)');
 
 alter table lmprogramdirectgear add KWReduction float default 0.0 not null;
 
+alter table capcontrolstrategy add peakVARlag float;
+update capcontrolstrategy set peakVARlag = 0;
+alter table capcontrolstrategy modify peakVARlag float not null;
+
+alter table capcontrolstrategy add peakVARlead float;
+update capcontrolstrategy set peakVARlead = 0;
+alter table capcontrolstrategy modify peakVARlead float not null;
+
+alter table capcontrolstrategy add offPkVARlag float;
+update capcontrolstrategy set offPkVARlag = 0;
+alter table capcontrolstrategy modify offPkVARlag float not null;
+
+alter table capcontrolstrategy add offPkVARlead float;
+update capcontrolstrategy set offPkVARlead = 0;
+alter table capcontrolstrategy modify offPkVARlead float not null;
+
+alter table capcontrolstrategy add peakPFSetPoint float;
+update capcontrolstrategy set peakPFSetPoint = 100;
+alter table capcontrolstrategy modify peakPFSetPoint float not null;
+
+alter table capcontrolstrategy add offPkPFSetPoint float;
+update capcontrolstrategy set offPkPFSetPoint = 100;
+alter table capcontrolstrategy modify offPkPFSetPoint float not null;
+
 /******************************************************************************/
 /* Run the Stars Update if needed here */
 /* Note: DBUpdate application will ignore this if STARS is not present */
--- yukon-database/DBUpdates/sqlserver/3.2_10.sql
+++ yukon-database/DBUpdates/sqlserver/3.2_10.sql
@@ -9,6 +9,48 @@ insert into fdrinterfaceoption values(12,'Filename',3,'Text','(none)');
 
 alter table lmprogramdirectgear add KWReduction float not null default 0.0;
 
+alter table capcontrolstrategy add peakVARlag float;
+go
+update capcontrolstrategy set peakVARlag = 0;
+go
+alter table capcontrolstrategy alter column peakVARlag float not null;
+go
+
+alter table capcontrolstrategy add peakVARlead float;
+go
+update capcontrolstrategy set peakVARlead = 0;
+go
+alter table capcontrolstrategy alter column peakVARlead float not null;
+go
+
+alter table capcontrolstrategy add offPkVARlag float;
+go
+update capcontrolstrategy set offPkVARlag = 0;
+go
+alter table capcontrolstrategy alter column offPkVARlag float not null;
+go
+
+alter table capcontrolstrategy add offPkVARlead float;
+go
+update capcontrolstrategy set offPkVARlead = 0;
+go
+alter table capcontrolstrategy alter column offPkVARlead float not null;
+go
+
+alter table capcontrolstrategy add peakPFSetPoint float;
+go
+update capcontrolstrategy set peakPFSetPoint = 100;
+go
+alter table capcontrolstrategy alter column peakPFSetPoint float not null;
+go
+
+alter table capcontrolstrategy add offPkPFSetPoint float;
+go
+update capcontrolstrategy set offPkPFSetPoint = 100;
+go
+alter table capcontrolstrategy alter column offPkPFSetPoint float not null;
+go
+
 /******************************************************************************/
 /* Run the Stars Update if needed here */
 /* Note: DBUpdate application will ignore this if STARS is not present */
