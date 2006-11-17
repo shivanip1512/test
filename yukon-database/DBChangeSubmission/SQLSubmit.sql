e0465a93841ef04ad42960ce69b124ee73cc1db5
Khazon,Elliot <>
2006-11-17 16:16:17 +0000

    Bug Id: 573

--- yukon-database/DBChangeSubmission/SQLSubmit.sql
+++ yukon-database/DBChangeSubmission/SQLSubmit.sql
@@ -2,6 +2,13 @@
 /*to split your new additions from those already there.  Thanks!
 */
 
+/*****added by Elliot ***********************/
+/****Bug Id: 573*****************************/
+/*****COS from P/Q is not supported anymore**/
+/*****however still exists in DB Editor******/
+/****start***********************************/
+delete from yukonlistentry where entryid = 125
+/****end*************************************/
 
 /***********************************************************************************/
 
