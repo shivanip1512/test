5f5f8865188a0dad4afcf4724defd8571303a9da
Mack, Tom <>
2006-11-30 00:22:04 +0000

    This has been replaced.

--- yukon-client/yukon-web/WebContent/install/sign.bat
+++
@@ -1,9 +0,0 @@
-if not"%1" == "" goto single
-
-for %%i in (*.jar) do jarsigner -keystore .keystore -storepass CTI123 %%i mykey
-goto done
-
-:single
-jarsigner -keystore .keystore -storepass CTI123 %1 mykey
-
-:done
