1516f886d7051bd5fd03c4fcc376924185991961
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2006-12-04 21:28:00 +0000

    Added String suffix for removed meter numbers (and paonames).

--- yukon-client/common/src/com/cannontech/database/db/device/DeviceMeterGroup.java
+++ yukon-client/common/src/com/cannontech/database/db/device/DeviceMeterGroup.java
@@ -18,6 +18,8 @@ public class DeviceMeterGroup extends com.cannontech.database.db.DBPersistent
     public static final String DISCONNECTED_GROUP_PREFIX = "@_DISC_";
     public static final String USAGE_MONITORING_GROUP_PREFIX = "@_UM_";
     public static final String INVENTORY_GROUP_PREFIX = "@_INV_";
+
+    public static final String REMOVED_METER_NUMBER_SUFFIX = "_REM";
     
 	//group types (used for billing mainly)
 	public static final int COLLECTION_GROUP = 0;
