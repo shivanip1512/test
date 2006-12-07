59cd1b4dade59d2ec98d73b319cdc8fbb82ecead
Khazon,Elliot <>
2006-12-07 16:19:34 +0000

    Template/Display TDC changes

--- yukon-database/DBChangeSubmission/SQLSubmit.sql
+++ yukon-database/DBChangeSubmission/SQLSubmit.sql
@@ -9,6 +9,31 @@
 /****start***********************************/
 delete from yukonlistentry where entryid = 125
 /****end*************************************/
+/***********added by Elliot******************/
+/***********DB changes related to Bugs 493, 494****/
+/*************need a table between template and display****/
+/****start***********************************/
+create table TemplateDisplay (
+DisplayNum numeric (18,0) not null,
+TemplateNum numeric (18,0) not null
+)
+
+go
+
+alter table TemplateDisplay
+   add constraint PK_TemplateDisplay primary key  (DisplayNum)
+go
+
+alter table TemplateDisplay
+   add constraint FK_TemplateDisplay_DISPLAY foreign key (DisplayNum)
+      references DISPLAY (DISPLAYNUM)
+go
+
+alter table TemplateDisplay
+   add constraint FK_TemplateDisplay_TEMPLATE foreign key (TemplateNum)
+      references TEMPLATE (TEMPLATENUM)
+go
+/****end*************************************/
 
 /***********************************************************************************/
 
