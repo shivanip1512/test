8f205bcf12de8c9ef5053b8df783958a3f5f5a42
Dayton, Jon R <JonRDayton@Eaton.com>
2006-12-08 21:59:01 +0000

    Db changes for the last month: for 3.2.11 and latest 3.4 --added template and display fix for TDC --multispeak and bulk importer changes --new role properties

--- yukon-database/DBChangeSubmission/SQLSubmit.sql
+++ yukon-database/DBChangeSubmission/SQLSubmit.sql
@@ -2,38 +2,7 @@
 /*to split your new additions from those already there.  Thanks!
 */
 
-/*****added by Elliot ***********************/
-/****Bug Id: 573*****************************/
-/*****COS from P/Q is not supported anymore**/
-/*****however still exists in DB Editor******/
-/****start***********************************/
-delete from yukonlistentry where entryid = 125
-/****end*************************************/
-/***********added by Elliot******************/
-/***********DB changes related to Bugs 493, 494****/
-/*************need a table between template and display****/
-/****start***********************************/
-create table TemplateDisplay (
-DisplayNum numeric (18,0) not null,
-TemplateNum numeric (18,0) not null
-)
 
-go
-
-alter table TemplateDisplay
-   add constraint PK_TemplateDisplay primary key  (DisplayNum)
-go
-
-alter table TemplateDisplay
-   add constraint FK_TemplateDisplay_DISPLAY foreign key (DisplayNum)
-      references DISPLAY (DISPLAYNUM)
-go
-
-alter table TemplateDisplay
-   add constraint FK_TemplateDisplay_TEMPLATE foreign key (TemplateNum)
-      references TEMPLATE (TEMPLATENUM)
-go
-/****end*************************************/
 
 /***********************************************************************************/
 
---
+++ yukon-database/DBUpdates/oracle/3.4_0.sql
@@ -0,0 +1,153 @@
+/******************************************/
+/**** SQLServer 2000 DBupdates         ****/
+/******************************************/
+
+/* @error ignore-begin */
+delete from yukonlistentry where entryid = 125;
+
+alter table MspVendor add AppName varchar2(64);
+update MspVendor set AppName = '(none)';
+alter table MspVendor modify AppName varchar2(64) not null;
+
+alter table MspVendor add OutUserName varchar2(64);
+update MspVendor set OutUserName = '(none)';
+alter table MspVendor modify OutUserName varchar2(64) not null;
+
+alter table MspVendor add OutPassword varchar2(64);
+update MspVendor set OutPassword = '(none)';
+alter table MspVendor modify OutPassword varchar2(64) not null;
+
+delete YukonGroupRole where Roleid = -7;
+delete YukonUserRole where RoleID = -7;
+delete yukonroleproperty where RoleID = -7;
+
+insert into YukonRoleProperty values(-1600,-7,'PaoName Alias','0','Defines a Yukon Pao (Device) Name field alias. Valid values(0-4): [0=Device Name, 1=Account Number, 2=Service Location, 3=Customer]');
+insert into YukonRoleProperty values(-1601,-7,'Primary CIS Vendor','0','Defines the primary CIS vendor for CB interfaces.');
+insert into YukonGroupRole values(-270,-1,-7,-1600,'0');
+insert into point values( -110, 'System', 'Multispeak' , 0, 'Default', 0, 'N', 'N', 'S', 110 ,'None', 0);
+
+create table Substation (
+   SubstationID         number              not null,
+   SubstationName       varchar2(50)          null,
+   LMRouteID              number              null
+);
+
+insert into Substation values (0,'(none)',0);
+alter table Substation
+   add constraint PK_SUBSTATION primary key  (SubstationID);
+
+alter table Substation
+   add constraint FK_Sub_Rt foreign key (RouteID)
+      references Route (RouteID);
+
+alter table Substation rename column RouteID to LMRouteID;
+
+/*==============================================================*/
+/* Table: SubstationToRouteMapping                              */
+/*==============================================================*/
+create table SubstationToRouteMapping (
+   SubstationID		number              not null,
+   RouteID              number              not null,
+   Ordering             number              not null
+);
+
+alter table SubstationToRouteMapping
+   add constraint PK_SUB_Rte_Map primary key  (SubstationID, RouteID);
+
+alter table SubstationToRouteMapping
+   add constraint FK_Sub_Rte_Map_SubID foreign key (SubstationID)
+      references Substation (SubstationID);
+
+alter table SubstationToRouteMapping
+   add constraint FK_Sub_Rte_Map_RteID foreign key (RouteID)
+      references Route (RouteID);
+
+alter table ImportData add BillGrp varchar2(64);
+update ImportData set BillGrp = '';
+alter table ImportData modify BillGrp varchar2(64) not null;
+
+alter table ImportData add SubstationName varchar2(50);
+update ImportData set SubstationName = '';
+alter table ImportData modify SubstationName varchar2(50) not null;
+
+alter table ImportFail add BillGrp varchar2(64);
+update ImportFail set BillGrp = '';
+alter table ImportFail modify BillGrp varchar2(64) not null;
+
+alter table ImportFail add SubstationName varchar2(50);
+update ImportFail set SubstationName = '';
+alter table ImportFail modify SubstationName varchar2(50) not null;
+
+alter table ImportFail add FailType varchar2(64);
+update ImportFail set FailType = '';
+alter table ImportFail modify FailType varchar2(64) not null;
+
+/*==============================================================*/
+/* Table: ImportPendingComm                                     */
+/*==============================================================*/
+create table ImportPendingComm (
+   DeviceID	       	   	number		    	 not null,
+   Address             	varchar2(64)          not null,
+   Name	               	varchar2(64)          not null,
+   RouteName           	varchar2(64)          not null,
+   MeterNumber	       	varchar2(64)	    	 not null,
+   CollectionGrp       	varchar2(64)          not null,
+   AltGrp	       		varchar2(64)          not null,
+   TemplateName	       	varchar2(64)          not null,
+   BillGrp             	varchar2(64)          not null, 
+   SubstationName      	varchar2(64)          not null
+)
+
+alter table ImportPendingComm
+   add constraint PK_IMPPENDINGCOMM primary key  (DeviceID);
+
+alter table ImportPendingComm
+   add constraint FK_ImpPC_PAO foreign key (DeviceID)
+      references YukonPAObject (PAObjectID);
+
+insert into YukonRoleProperty values(-20011,-200,'Multispeak Setup','false','Controls access to configure the Multispeak Interfaces.');
+
+/* @error ignore-end */
+
+insert into YukonRoleProperty values(-10922,-109,'C&I Curtailment Reports Group Label','Stars','Label (header) for C&I Curtailment group reports.');
+insert into YukonRoleProperty values(-10923,-109,'C&I Curtailment Reports Group','false','Access to C&I Curtailment group reports');
+insert into YukonRoleProperty values(-10812, -108,'Java Web Start Launcher Enabled', 'false', 'Allow access to the Java Web Start Launcher for client applications.');
+go
+
+/*==============================================================*/
+/* Table: TemplateDisplay                                       */
+/*==============================================================*/
+create table TemplateDisplay (
+   DisplayNum           number              not null,
+   TemplateNum          number              not null
+);
+go
+
+alter table TemplateDisplay
+   add constraint PK_TEMPLATEDISPLAY primary key  (DisplayNum);
+go
+
+
+alter table TemplateDisplay
+   add constraint FK_TemplateDisplay_DISPLAY foreign key (DisplayNum)
+      references DISPLAY (DISPLAYNUM);
+go
+
+
+alter table TemplateDisplay
+   add constraint FK_TemplateDisplay_TEMPLATE foreign key (TemplateNum)
+      references TEMPLATE (TEMPLATENUM);
+go
+
+/******************************************************************************/
+/* Run the Stars Update if needed here */
+/* Note: DBUpdate application will ignore this if STARS is not present */
+/* @include StarsUpdate */
+/******************************************************************************/
+
+
+/**************************************************************/
+/* VERSION INFO                                               */
+/*   Automatically gets inserted from build script            */
+/**************************************************************/
+/* __YUKON_VERSION__ */
