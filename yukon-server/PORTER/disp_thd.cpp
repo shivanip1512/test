e03ee26d689e472c44a11b827c20a51bbf34f84c
Fisher, Matthew S <MatthewSFisher@Eaton.com>
2007-01-08 16:49:08 +0000

    Bug Id: 421 now uses UDPInterfaceQueue instead of UDP_MessageQueue

--- yukon-server/PORTER/disp_thd.cpp
+++ yukon-server/PORTER/disp_thd.cpp
@@ -6,8 +6,8 @@
 *
 * PVCS KEYWORDS:
 * ARCHIVE      :  $Archive:   Z:/SOFTWAREARCHIVES/YUKON/PORTER/disp_thd.cpp-arc  $
-* REVISION     :  $Revision: 1.28 $
-* DATE         :  $Date: 2006/12/28 21:02:51 $
+* REVISION     :  $Revision: 1.29 $
+* DATE         :  $Date: 2007/01/08 16:49:08 $
 *
 * Copyright (c) 1999, 2000, 2001 Cannon Technologies Inc. All rights reserved.
 *-----------------------------------------------------------------------------*/
@@ -61,13 +61,15 @@
 #include "guard.h"
 #include "utility.h"
 
+#include "port_udp.h"
+
 using namespace std;  // get the STL into our namespace for use.  Do NOT use iostream.h anymore
 
 CtiConnection VanGoghConnection;
 
 namespace Cti { namespace Porter {
 
-    extern CtiFIFOQueue< CtiMessage > UDP_MessageQueue;
+    extern UDPMessenger UDPInterfaceQueue;
 }
 }
 
@@ -151,7 +153,7 @@ void DispatchMsgHandlerThread(VOID *Arg)
                         }
 
                         //  the UDP thread needs to reload each item individually, so we can't ever discard any
-                        Cti::Porter::UDP_MessageQueue.putQueue(MsgPtr->replicateMessage());
+                        Cti::Porter::UDPInterfaceQueue.push_back(MsgPtr->replicateMessage());
 
                         if(pChg)
                         {
