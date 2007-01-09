de7db6d141826be1052bab9a7ce817faa4606a73
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2007-01-09 14:40:03 +0000

    Set MSPVendor.appname for default entry to 'Yukon'

--- yukon-database/DBChangeSubmission/SQLSubmit.sql
+++ yukon-database/DBChangeSubmission/SQLSubmit.sql
@@ -3,7 +3,10 @@
 */
 /***********************************************************************************/
 /**SN - Changing default companyname from 'cannon' to 'Cannon' 
-Update statement needed for update scripts.
-Modify the mspVendor default insert statements for creation scripts */
+Update statement needed for update scripts.*/
 update mspVendor set CompanyName = 'Cannon' where vendorid = 1;
+update mspVendor set AppName = 'Yukon' where vendorid = 1;
+
+/*Modify the mspVendor default insert statement (as below) for creation scripts */
+insert into MSPVendor values (1, 'Cannon', '(none)', '(none)', 'meterNumber', 0, 'http://127.0.0.1:8080/soap/', 'Yukon', '(none)', '(none)');
 /**********************/
