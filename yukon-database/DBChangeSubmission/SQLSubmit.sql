77630cacfab66cac721ae4eafbb765405e8fdb75
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2007-01-09 14:36:33 +0000

    DB changed for default MSPVendor entry to have proper capitalization.

--- yukon-database/DBChangeSubmission/SQLSubmit.sql
+++ yukon-database/DBChangeSubmission/SQLSubmit.sql
@@ -1,8 +1,9 @@
 /*Please be kind and keep things cleanly organized.  Add new entries to the end and use /**********************/
 /*to split your new additions from those already there.  Thanks!
 */
-
-
-
 /***********************************************************************************/
-
+/**SN - Changing default companyname from 'cannon' to 'Cannon' 
+Update statement needed for update scripts.
+Modify the mspVendor default insert statements for creation scripts */
+update mspVendor set CompanyName = 'Cannon' where vendorid = 1;
+/**********************/
