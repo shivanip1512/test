080fd0ff2d2da83e343ca03968a224ddfdc22b0c
Otteson, Jess M <JessMOtteson@Eaton.com>
2007-01-25 21:03:28 +0000

    Initial Revision. Added for MACS logging of received data from porter. This contains information about every response from porter with error data.

---
+++ yukon-server/Database/include/tbl_meterreadlog.h
@@ -0,0 +1,89 @@
+
+/*-----------------------------------------------------------------------------*
+*
+* File:   tbl_meterreadlog
+*
+* Date:   1/24/2007
+*
+* PVCS KEYWORDS:
+* ARCHIVE      :  $Archive:   Z:/SOFTWAREARCHIVES/YUKON/Database/Include/tbl_meterreadlog.h-arc  $
+* REVISION     :  $Revision: 1.1 $
+* DATE         :  $Date: 2007/01/25 21:03:28 $
+*
+* Copyright (c) 2007 Cannon Technologies Inc. All rights reserved.
+*-----------------------------------------------------------------------------*/
+
+#pragma warning( disable : 4786)
+#ifndef __TBL_METERREADLOG_H__
+#define __TBL_METERREADLOG_H__
+
+#include <windows.h>
+#include "ctitime.h"
+#include <rw/db/datetime.h>
+
+#include "dlldefs.h"
+#include "pointdefs.h"
+#include "utility.h"
+#include "yukon.h"
+
+#include "boost/shared_ptr.hpp"
+using boost::shared_ptr;
+
+class IM_EX_CTIYUKONDB CtiTableMeterReadLog
+{
+protected:
+
+    long _logID;
+    long _deviceID;
+    long _requestID;
+    long _statusCode;
+    CtiTime _time;
+
+private:
+
+public:
+
+   CtiTableMeterReadLog(   LONG     logid          = 0L,
+                           LONG     devid          = 0L,
+                           LONG     reqid          = 0L,
+                           LONG     code           = 0L,
+                           const CtiTime    &tme    = CtiTime() ) :
+      _logID(logid),
+      _deviceID(devid),
+      _requestID(reqid),
+      _statusCode(code),
+      _time(tme)
+   {
+   }
+
+   CtiTableMeterReadLog(const CtiTableMeterReadLog& aRef)
+   {
+      *this = aRef;
+   }
+
+   virtual ~CtiTableMeterReadLog() {}
+
+   CtiTableMeterReadLog& operator=(const CtiTableMeterReadLog& aRef);
+   virtual RWBoolean operator<(const CtiTableMeterReadLog& aRef) const;
+
+   virtual void Insert();
+   virtual void Insert(RWDBConnection &conn);
+   virtual string getTableName() const;
+
+   virtual void DecodeDatabaseReader( RWDBReader& rdr );
+
+   LONG                    getLogID() const;
+   LONG                    getDeviceID() const;
+   LONG                    getRequestID() const;
+   LONG                    getStatusCode() const;
+   CtiTime                 getTime() const;
+
+   CtiTableMeterReadLog&   setLogID(LONG id);
+   CtiTableMeterReadLog&   setDeviceID(LONG id);
+   CtiTableMeterReadLog&   setRequestID(LONG id);
+   CtiTableMeterReadLog&   setStatusCode(LONG code);
+   CtiTableMeterReadLog&   setTime(const CtiTime &rwt);
+};
+
+typedef shared_ptr< CtiTableMeterReadLog > CtiTableMeterReadLogSPtr;
+#endif // #ifndef __TBL_METERREADLOG_H__
---
+++ yukon-server/Database/tbl_meterreadlog.cpp
@@ -0,0 +1,172 @@
+
+/*-----------------------------------------------------------------------------*
+*
+* File:   tbl_meterreadlog
+*
+* Date:   1/24/2007
+*
+* PVCS KEYWORDS:
+* ARCHIVE      :  $Archive:   Z:/SOFTWAREARCHIVES/YUKON/Database/tbl_meterreadlog.h-arc  $
+* REVISION     :  $Revision: 1.1 $
+* DATE         :  $Date: 2007/01/25 21:03:28 $
+*
+* Copyright (c) 2007 Cannon Technologies Inc. All rights reserved.
+*-----------------------------------------------------------------------------*/
+
+#include "yukon.h"
+#include "tbl_meterreadlog.h"
+#include "dbaccess.h"
+#include "logger.h"
+#include "ctitime.h"
+#include "rwutil.h"
+
+
+void CtiTableMeterReadLog::Insert()
+{
+    CtiLockGuard<CtiSemaphore> cg(gDBAccessSema);
+    RWDBConnection conn = getConnection();
+
+    Insert(conn);
+}
+
+void CtiTableMeterReadLog::Insert(RWDBConnection &conn)
+{
+    RWDBTable table = conn.database().table( getTableName().c_str() );
+    RWDBInserter inserter = table.inserter();
+
+    inserter <<
+    getLogID() <<
+    getDeviceID() <<
+    getRequestID() <<
+    getTime() <<
+    getStatusCode();
+
+    RWDBStatus stat = ExecuteInserter(conn,inserter,__FILE__,__LINE__);
+
+    if( stat.errorCode() != RWDBStatus::ok )
+    {
+        LONG newcid = SynchronizedIdGen("MeterReadLog", 1);
+
+        if(newcid != getLogID() && newcid != 0)
+        {
+            CtiTime Now;
+
+            {
+                CtiLockGuard<CtiLogger> doubt_guard(dout);
+                dout << Now << " Insert collision occurred in table " << getTableName() << "." << endl;
+                dout << Now << "   ChangeId has been re-initialized." << endl;
+            }
+
+            setLogID( newcid );
+            stat = ExecuteInserter(conn,inserter,__FILE__,__LINE__);
+
+            if( stat.errorCode() != RWDBStatus::ok )
+            {
+                CtiLockGuard<CtiLogger> doubt_guard(dout);
+                dout << CtiTime() << " Unable to insert log for device id " << getDeviceID() << ". " << __FILE__ << " (" << __LINE__ << ")" << endl;
+                dout << "   " << inserter.asString() << endl;
+            }
+        }
+    }
+}
+
+
+CtiTableMeterReadLog& CtiTableMeterReadLog::operator=(const CtiTableMeterReadLog& aRef)
+{
+    if(this != &aRef)
+    {
+        setLogID(aRef.getLogID());
+        setDeviceID(aRef.getDeviceID());
+        setRequestID(aRef.getRequestID());
+        setStatusCode(aRef.getStatusCode());
+        setTime(aRef.getTime()); 
+    }
+    return *this;
+}
+
+RWBoolean CtiTableMeterReadLog::operator<(const CtiTableMeterReadLog& aRef) const
+{
+    bool retVal;
+    if( getTime() < aRef.getTime() )
+    {
+        retVal = true;
+    }
+    else if( getTime() == aRef.getTime() && getDeviceID() < aRef.getDeviceID() )
+    {
+        retVal = true;
+    }
+    else
+    {
+        retVal = false;
+    }
+    return(retVal);
+}
+
+LONG CtiTableMeterReadLog::getLogID() const
+{
+    return _logID;
+}
+
+LONG CtiTableMeterReadLog::getDeviceID() const
+{
+    return _deviceID;
+}
+
+LONG CtiTableMeterReadLog::getRequestID() const
+{
+    return _requestID;
+}
+
+LONG CtiTableMeterReadLog::getStatusCode() const
+{
+    return _statusCode;
+}
+
+CtiTime CtiTableMeterReadLog::getTime() const
+{
+    return _time;
+}
+
+string CtiTableMeterReadLog::getTableName() const
+{
+    return string("meterreadlog");
+}
+
+CtiTableMeterReadLog&   CtiTableMeterReadLog::setLogID(LONG id)
+{
+    _logID = id;
+    return *this;
+}
+
+CtiTableMeterReadLog&   CtiTableMeterReadLog::setDeviceID(LONG id)
+{
+    _deviceID = id;
+    return *this;
+}
+
+CtiTableMeterReadLog&   CtiTableMeterReadLog::setRequestID(LONG id)
+{
+    _requestID = id;
+    return *this;
+}
+
+CtiTableMeterReadLog&   CtiTableMeterReadLog::setStatusCode(LONG code)
+{
+    _statusCode = code;
+    return *this;
+}
+
+CtiTableMeterReadLog& CtiTableMeterReadLog::setTime(const CtiTime &rwt)
+{
+    _time = rwt;
+    return *this;
+}
+
+void CtiTableMeterReadLog::DecodeDatabaseReader( RWDBReader& rdr )
+{
+    rdr["deviceid"]     >> _deviceID;
+    rdr["foreignid"]    >> _requestID;
+    rdr["statuscode"]   >> _statusCode;
+    rdr["timestamp"]    >> _time;
+}
+
