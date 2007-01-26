c072334917286c42ce253a5f7e036bb41a9cc5be
Spar, Thain <>
2007-01-26 21:22:25 +0000

    Bug Id: 607
    Added a FDR interface for Wabash, when load programs start and stop the interface will write out a line of code to a text file for another program to pick up.
    
    Format of output:
    11/08/06 14:00:19,AEP-AC-COOL-75%,STOP

--- yukon-database/DBChangeSubmission/SQLSubmit.sql
+++ yukon-database/DBChangeSubmission/SQLSubmit.sql
@@ -3,5 +3,9 @@
 */
 /*
 
+insert into FDRInterface values (24, 'WABASH', 'Send', 'f' );
+insert into FDRInterfaceOption values(24, 'SchedName', 1, 'Text', '(none)' );
+insert into FDRInterfaceOption values(24, 'Path', 2, 'Text', 'c:\\yukon\\server\\export\\' );
+insert into FDRInterfaceOption values(24, 'Filename', 3, 'Text', 'control.txt' );
 
-
+/**********************/
