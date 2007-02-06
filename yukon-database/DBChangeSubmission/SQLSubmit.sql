14c0ef8d71e5b6a53b127ee7bf977f878ff5e702
Ganz, Julie <>
2007-02-06 23:10:33 +0000

    added kvarBefore, kvarAfter, kvarChange, additionalinfo to cceventlog

--- yukon-database/DBChangeSubmission/SQLSubmit.sql
+++ yukon-database/DBChangeSubmission/SQLSubmit.sql
@@ -9,3 +9,34 @@ insert into FDRInterfaceOption values(24, 'Path', 2, 'Text', 'c:\\yukon\\server\
 insert into FDRInterfaceOption values(24, 'Filename', 3, 'Text', 'control.txt' );
 
 /**********************/
+
+
+/*********************** BEGIN JULIE: cceventlog column additions **********************/
+alter table cceventlog add kvarBefore float;
+go
+update cceventlog  set kvarBefore = 0;
+go
+alter table cceventlog alter column kvarBefore float not null;
+go
+
+alter table cceventlog add kvarAfter float;
+go
+update cceventlog  set kvarAfter = 0;
+go
+alter table cceventlog alter column kvarAfter float not null;
+go
+
+alter table cceventlog add kvarChange float;
+go
+update cceventlog  set kvarChange = 0;
+go
+alter table cceventlog alter column kvarChange float not null;
+go
+
+alter table cceventlog add additionalInfo varchar(20);
+go
+update cceventlog  set additionalInfo = '(none)';
+go
+alter table cceventlog alter column additionalInfo varchar(20) not null;
+go
+/*********************** END JULIE: cceventlog column additions **********************/
