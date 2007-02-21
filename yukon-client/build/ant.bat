af736f4c5ca3669a694d0450ef66ebd623857b07
softwarebuild <softwarebuild@Eaton.com>
2007-02-21 20:13:45 +0000

    forcing ANT_HOME to be our copy of ant so that we won't pick up some random version of ant from the systems ANT_HOME

--- yukon-client/build/ant.bat
+++ yukon-client/build/ant.bat
@@ -1,4 +1,5 @@
 @echo off
-REM $Id: ant.bat,v 1.5 2006/04/21 18:35:01 jkoponen Exp $
+REM $Id: ant.bat,v 1.6 2007/02/21 20:13:45 softwarebuild Exp $
 REM set ANT_HOME=ant
+set ANT_HOME=%~dp0ant
 ant\bin\ant.bat %*
