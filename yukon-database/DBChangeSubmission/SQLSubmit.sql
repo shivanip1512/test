4f8c613670556ad65672c475fac84af9cc8b4351
Ganz, Julie <>
2007-03-04 22:04:35 +0000

    Added storing of 2way cbc state information to dynamic capbank table.

--- yukon-database/DBChangeSubmission/SQLSubmit.sql
+++ yukon-database/DBChangeSubmission/SQLSubmit.sql
@@ -1,3 +1,11 @@
 /*Please be kind and keep things cleanly organized.  Add new entries to the end and use /**********************/
 /*to split your new additions from those already there.  Thanks!
 */
+
+/********** JULIE: added 2way cbc state information to dynamic cap table ******/
+alter table dynamiccccapbank add twowaycbcstate numeric;
+go
+update dynamiccccapbank set twowaycbcstate = -1;
+go
+alter table dynamiccccapbank alter column twowaycbcstate numeric not null;
+/********** END JULIE ********************************************************/
