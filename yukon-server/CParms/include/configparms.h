c2735233f933f1f054ff0dc069cc2be4c89075ef
softwarebuild <softwarebuild@Eaton.com>
2007-03-14 15:05:58 +0000

    Fixed up the signatures in the function pointer definitions - this would causing any code using these to blow up

--- yukon-server/CParms/include/configparms.h
+++ yukon-server/CParms/include/configparms.h
@@ -13,10 +13,10 @@ IM_EX_C_CPARM int             RefreshConfigParameters(const string& FileName = D
 IM_EX_C_CPARM void            DumpConfigParms();
 IM_EX_C_CPARM BOOL            getConfigValueAsString(const string& Key, char *targ, int len);
 
-typedef BOOL (*CPARM_ISCONFIG)(string);
-typedef int  (*CPARM_REFRESH)(string);
+typedef BOOL (*CPARM_ISCONFIG)(const string&);
+typedef int  (*CPARM_REFRESH)(const string&);
 typedef void (*CPARM_DUMPCONFIG)(void);
-typedef BOOL (*CPARM_GETCONFIGSTRING)(string, char*, int);
+typedef BOOL (*CPARM_GETCONFIGSTRING)(const string&, char*, int);
 
 #ifdef __cplusplus
 }
