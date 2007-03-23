8b6984c5a1fccd42d0795853886c43f76ad70616
Khazon,Elliot <>
2007-03-23 19:04:12 +0000

    Updated cache refresh rate

--- yukon-client/cbc/src/com/cannontech/cbc/web/CapControlCache.java
+++ yukon-client/cbc/src/com/cannontech/cbc/web/CapControlCache.java
@@ -49,7 +49,7 @@ import com.cannontech.yukon.conns.ConnPool;
 public class CapControlCache implements MessageListener, CapControlDAO
 {
 	private static final int STARTUP_REF_RATE = 15 * 1000;
-	private static final int NORMAL_REF_RATE = 5 * 1000; //5 minutes
+	private static final int NORMAL_REF_RATE = 5 * 60* 1000; //5 minutes
 	private ScheduledExecutorService refreshTimer = YukonSpringHook.getGlobalExecutor();
 	
 	// Map<suBusID(Integer), sub(SubBus)>
