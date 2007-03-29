7f74c1bd6888f36614f60cb881a5510aacf87c0d
Khazon,Elliot <>
2007-03-29 23:39:13 +0000

    UT for Substation creation

---
+++ yukon-client/yukon-web/src/com/cannontech/web/test/TestSubCreation.java
@@ -0,0 +1,25 @@
+package com.cannontech.web.test;
+
+import junit.framework.TestCase;
+
+import com.cannontech.web.editor.CapControlForm;
+
+/**
+ * example to to create cap control objects
+ * @author ekhazon
+ *
+ */
+public class TestSubCreation extends TestCase {
+
+    CapControlForm form = new CapControlForm();
+
+    public void testCreateSub () {
+        CapControlForm form = new CapControlForm();
+        form.getWizData().setName("Elliot's Sub 5");
+        form.initWizard (4000);
+        form.create();
+    
+}
+
+
+}
