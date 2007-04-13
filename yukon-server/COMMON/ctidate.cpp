0778ae5a29b8ee38f797eeb21c1a5d7380f9178b
Fisher, Matthew S <MatthewSFisher@Eaton.com>
2007-04-13 19:25:44 +0000

    added protection against date's habit of throwing exceptions for any out-of-bounds day/month/year parameters;  now sets bdate to not_a_date instead

--- yukon-server/COMMON/ctidate.cpp
+++ yukon-server/COMMON/ctidate.cpp
@@ -14,58 +14,84 @@ CtiDate::CtiDate() :
 
 
 CtiDate::CtiDate(unsigned int day, unsigned int month, unsigned int year) :
-  bdate(year, month, day)
+    bdate(boost::date_time::not_a_date_time)
 {
-    //bdate had a month range of 1-12 so you muct remember when using other time containers, 
-    //and creating a ctidate ( like  'tm' )  they may be on a 0-11 scale    'tm' also stores the year  -1900
-    if ( month ==0 || month > 12 ){
-		CtiLockGuard<CtiLogger> doubt_guard(dout);
-		dout << CtiTime() << " **** Checkpoint **** Bad Month Passed to CtiDate" << __FILE__ << " (" << __LINE__ << ")" << endl;
-	}
+    try
+    {
+        bdate = date(year, month, day);
+    }
+    catch( ... )
+    {
+        CtiLockGuard<CtiLogger> doubt_guard(dout);
+        dout << CtiTime() << " **** Checkpoint - exception in CtiDate(day,month,year) (" << day << "," << month << "," << year << ") " << __FILE__ << " (" << __LINE__ << ")" << endl;
+    }
 }
+
 CtiDate::CtiDate(unsigned int days, unsigned int year) :
-  bdate(year, 1, 1)
+    bdate(boost::date_time::not_a_date_time)
 {
-    date_duration dd(days-1);
-    bdate = bdate + dd;
+    try
+    {
+        bdate = date(year, 1, 1) + date_duration(days-1);
+    }
+    catch( ... )
+    {
+        CtiLockGuard<CtiLogger> doubt_guard(dout);
+        dout << CtiTime() << " **** Checkpoint - exception in CtiDate(days,year) (" << days << "," << year << ") " << __FILE__ << " (" << __LINE__ << ")" << endl;
+    }
 }
 
 
 CtiDate::CtiDate(const CtiTime& ct) :
-  bdate(boost::date_time::not_a_date_time)
+    bdate(boost::date_time::not_a_date_time)
 {
-    if(ct.isValid())
-    { 
-        if(ct.is_neg_infinity()){
+    if( ct.isValid() )
+    {
+        if( ct.is_neg_infinity() )
+        {
             bdate = date(boost::date_time::neg_infin);
-        } else if(ct.is_pos_infinity()){
+        }
+        else if(ct.is_pos_infinity())
+        {
             bdate = date(boost::date_time::pos_infin);
-        } else {
+        }
+        else
+        {
             struct tm ctime;
             ct.extract(&ctime);
-            bdate = date(ctime.tm_year + 1900, ctime.tm_mon + 1, ctime.tm_mday);
+
+            try
+            {
+                bdate = date(ctime.tm_year + 1900, ctime.tm_mon + 1, ctime.tm_mday);
+            }
+            catch( ... )
+            {
+                CtiLockGuard<CtiLogger> doubt_guard(dout);
+                dout << CtiTime() << " **** Checkpoint - exception in CtiDate(ct) (" << ct << ") " << __FILE__ << " (" << __LINE__ << ")" << endl;
+            }
         }
     }
 }
 
 CtiDate::CtiDate(const CtiDate& cd) :
-  bdate(cd.bdate)
-{}
+    bdate(cd.bdate)
+{
+}
 
 CtiDate::CtiDate(specialvalues sv) :
-  bdate(boost::date_time::not_a_date_time)
+    bdate(boost::date_time::not_a_date_time)
 {
-    if(sv == CtiDate::neg_infin)
+    switch( sv )
     {
-        bdate = date(boost::date_time::neg_infin);
-    }
-    else if(sv == CtiDate::pos_infin)
-    {
-        bdate= date(boost::date_time::pos_infin);
-    }
-    else if(sv == CtiDate::not_a_date)
-    {
-        bdate= date(boost::date_time::not_a_date_time);
+        case neg_infin:     bdate = date(boost::date_time::neg_infin);      break;
+
+        case pos_infin:     bdate = date(boost::date_time::pos_infin);      break;
+
+        //  we're already ::not_a_date_time, no need to reassign it
+        /*
+        default:
+        case not_a_date:    bdate = date(boost::date_time::not_a_date_time);    break;
+        */
     }
 }
 
@@ -111,30 +137,34 @@ bool CtiDate::is_pos_infinity() const
 }
 
 
-unsigned int CtiDate::day() const 
+unsigned int CtiDate::day() const
 {
     date_period dp(date(bdate.year(), 1, 1), bdate);
+
     return dp.length().days() + 1;
 }
+
 unsigned int CtiDate::dayOfMonth() const
 {
     return bdate.day();
 }
-unsigned int CtiDate::year() const 
+
+unsigned int CtiDate::year() const
 {
     return bdate.year();
 }
-unsigned int CtiDate::month() const 
+
+unsigned int CtiDate::month() const
 {
     return bdate.month();
 }
 
-string CtiDate::asString() const 
+string CtiDate::asString() const
 {
     return to_simple_string(bdate);
 }
 
-string CtiDate::weekDayName() const 
+string CtiDate::weekDayName() const
 {
     return bdate.day_of_week().as_long_string();
 }
@@ -142,12 +172,14 @@ string CtiDate::weekDayName() const
 unsigned int CtiDate::weekDay() const
 {
     date::ymd_type ymd = bdate.year_month_day();
+
     return gregorian_calendar::day_of_week(ymd);
 }
 
 unsigned int CtiDate::firstDayOfMonth() const
-{ 
+{
     date_period dp(date(bdate.year(), 1, 1), date(bdate.year(), bdate.month(), 1));
+
     return dp.length().days() + 1;
 }
 
@@ -161,13 +193,15 @@ CtiDate CtiDate::now()
 {
     return CtiDate();
 }
+
 CtiDate& CtiDate::toUTCdate()
 {
     struct tm ctime;
+
     CtiTime ct(*this);
     ct.toUTCtime();
     ct.extract(&ctime);
-    bdate = date(ctime.tm_mday, ctime.tm_mon, ctime.tm_year); 
+    bdate = date(ctime.tm_mday, ctime.tm_mon, ctime.tm_year);
 
     return *this;
 }
@@ -175,45 +209,47 @@ CtiDate& CtiDate::toUTCdate()
 CtiDate& CtiDate::operator++()
 {
     bdate = bdate + date_duration(1);
+
     return *this;
 }
 
 CtiDate& CtiDate::operator--()
 {
-    bdate = bdate - date_duration(1); 
+    bdate = bdate - date_duration(1);
+
     return *this;
 }
 
 
-bool operator < (const CtiDate& d1, const CtiDate& d2)
+bool CtiDate::operator < (const CtiDate& d2) const
 {
-    return d1.bdate < d2.bdate;
+    return bdate < d2.bdate;
 }
-bool operator <= (const CtiDate& d1, const CtiDate& d2)
+bool CtiDate::operator <= (const CtiDate& d2) const
 {
-    return d1.bdate <= d2.bdate;
+    return bdate <= d2.bdate;
 }
-bool operator > (const CtiDate& d1, const CtiDate& d2)
+bool CtiDate::operator > (const CtiDate& d2) const
 {
-    return d1.bdate > d2.bdate;
+    return bdate > d2.bdate;
 }
-bool operator >= (const CtiDate& d1, const CtiDate& d2)
+bool CtiDate::operator >= (const CtiDate& d2) const
 {
-    return d1.bdate >= d2.bdate;
+    return bdate >= d2.bdate;
 }
-bool operator == (const CtiDate& d1, const CtiDate& d2)
+bool CtiDate::operator == (const CtiDate& d2) const
 {
-    return d1.bdate == d2.bdate;
+    return bdate == d2.bdate;
 }
-bool operator != (const CtiDate& d1, const CtiDate& d2)
+bool CtiDate::operator != (const CtiDate& d2) const
 {
-    return d1.bdate != d2.bdate;
+    return bdate != d2.bdate;
 }
 
 
 
 CtiDate operator + (const CtiDate& d, const unsigned long s)
-{   
+{
     CtiDate _date;
     _date.bdate = d.bdate + date_duration(s);
     return _date;
