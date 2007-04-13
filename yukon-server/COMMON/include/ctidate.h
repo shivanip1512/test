87bcb68e789561091eb97ef5e40e20b0d27fc864
Fisher, Matthew S <MatthewSFisher@Eaton.com>
2007-04-13 19:23:54 +0000

    comparison operators are now class methods, not friends

--- yukon-server/COMMON/include/ctidate.h
+++ yukon-server/COMMON/include/ctidate.h
@@ -1,10 +1,10 @@
 /*
  * class CtiDate
- *  
- * Author: Jian Liu 
- * Date: 08/09/2005 11:36:03 
- * 
- * 
+ *
+ * Author: Jian Liu
+ * Date: 08/09/2005 11:36:03
+ *
+ *
  * This class is to replace the RWDate class.
  *
  * It uses boost date to store the current time.
@@ -15,8 +15,6 @@
  *
  * the whole class is based on boost date class.
  */
-
-
 
 #ifndef _CTIDATE_H_
 #define _CTIDATE_H_
@@ -31,12 +29,14 @@ using std::string;
 class CtiTime;
 
 
-class IM_EX_CTIBASE CtiDate{
+class IM_EX_CTIBASE CtiDate
+{
 private:
     boost::gregorian::date bdate;
+
 public:
-    enum specialvalues  {neg_infin, pos_infin, not_a_date};
-    
+    enum specialvalues  { neg_infin, pos_infin, not_a_date };
+
     CtiDate();
     CtiDate(unsigned int day, unsigned int month, unsigned int year);
     CtiDate(unsigned int days, unsigned int year);
@@ -46,46 +46,42 @@ public:
     CtiDate(const CtiDate&);
     CtiDate(specialvalues);
 
-    CtiDate& operator=(const CtiDate&);
-    CtiDate& operator+=(const int days);
-    CtiDate& operator-=(const int days);
+    static unsigned int daysInMonthYear(unsigned month, unsigned year);
+    static CtiDate now();
 
-    CtiDate& toUTCdate();
+    CtiDate & operator=(const CtiDate&);
+    CtiDate & operator+=(const int days);
+    CtiDate & operator-=(const int days);
+
+    CtiDate & operator++();
+    CtiDate & operator--();
+
+    bool operator <  (const CtiDate& d2) const;
+    bool operator <= (const CtiDate& d2) const;
+    bool operator >  (const CtiDate& d2) const;
+    bool operator >= (const CtiDate& d2) const;
+    bool operator == (const CtiDate& d2) const;
+    bool operator != (const CtiDate& d2) const;
+
+    CtiDate & toUTCdate();
 
     unsigned int day() const;
     unsigned int dayOfMonth() const;
     unsigned int year() const;
     unsigned int month() const;
+    unsigned int weekDay() const;
+    unsigned int firstDayOfMonth() const;
+
     bool isValid() const;
     bool is_special() const;
     bool is_neg_infinity() const;
     bool is_pos_infinity() const;
 
-
     string asString() const;
     string weekDayName() const;
-    unsigned int weekDay() const;
-    unsigned int firstDayOfMonth() const;
-
-    static unsigned int daysInMonthYear(unsigned month, unsigned year);
-    static CtiDate now();
-
-
-    CtiDate& operator++();
-    CtiDate& operator--();
 
     friend CtiDate IM_EX_CTIBASE operator + (const CtiDate& d, const unsigned long s);
-
     friend CtiDate IM_EX_CTIBASE operator - (const CtiDate& d, const unsigned long s);
-
-    friend bool IM_EX_CTIBASE operator < (const CtiDate& d1, const CtiDate& d2);
-    friend bool IM_EX_CTIBASE operator <= (const CtiDate& d1, const CtiDate& d2);
-    friend bool IM_EX_CTIBASE operator > (const CtiDate& d1, const CtiDate& d2);
-    friend bool IM_EX_CTIBASE operator >= (const CtiDate& d1, const CtiDate& d2);
-    friend bool IM_EX_CTIBASE operator == (const CtiDate& d1, const CtiDate& d2);
-    friend bool IM_EX_CTIBASE operator != (const CtiDate& d1, const CtiDate& d2);
-
-
 };
 
 
