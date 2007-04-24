7dabab866000952c1065e5f784abb88a50ab7257
Dayton, Jon R <JonRDayton@Eaton.com>
2007-04-24 21:54:07 +0000

    Some cap control role property restructuring...

--- yukon-database/DBUpdates/sqlserver/3.4_1.sql
+++ yukon-database/DBUpdates/sqlserver/3.4_1.sql
@@ -53,6 +53,32 @@ go
 alter table dynamicccfeeder alter column iWCount numeric not null;
 go
 
+
+insert into YukonRole values (-1000,'Substation Display','CBC Oneline','Change display settings for substation details.');
+insert into YukonRole values (-1001,'Feeder Display','CBC Oneline','Change display settings for feeder details.');
+insert into YukonRole values (-1002,'Cap Bank Display','CBC Oneline','Change display settings for cap bank details.');
+
+insert into YukonRoleProperty values(-100000, -1000, 'Target', 'true', 'display Target settings');
+insert into YukonRoleProperty values(-100001, -1000, 'kVAR', 'true', 'display kVAR');
+insert into YukonRoleProperty values(-100002, -1000, 'Estimated kVAR', 'true', 'display estimated kVAR');
+insert into YukonRoleProperty values(-100003, -1000, 'Power Factor', 'true', 'display Power Factor');
+insert into YukonRoleProperty values(-100004, -1000, 'Estimated Power Factor', 'true', 'display estimated Power Factor');
+insert into YukonRoleProperty values(-100005, -1000, 'Watts', 'true', 'display Watts');
+insert into YukonRoleProperty values(-100006, -1000, 'Volts', 'true', 'display Volts');
+insert into YukonRoleProperty values(-100007, -1000, 'Daily Op Count', 'true', 'display Daily Operation Count');
+insert into YukonRoleProperty values(-100008, -1000, 'Max Op Count', 'true', 'display Max Operation Count');
+
+insert into YukonRoleProperty values(-100100, -1001, 'kVAR', 'true', 'display kVAR');
+insert into YukonRoleProperty values(-100101, -1001, 'Power Factor', 'true', 'display Power Factor');
+insert into YukonRoleProperty values(-100102, -1001, 'Watts', 'true', 'display Watts');
+insert into YukonRoleProperty values(-100103, -1001, 'Daily Op Count', 'true', 'display Daily Operation Count');
+insert into YukonRoleProperty values(-100104, -1001, 'Volts', 'true', 'display Volts');
+
+insert into YukonRoleProperty values(-100200, -1002, 'Total Op Count', 'true', 'display Total Operation Count');
+insert into YukonRoleProperty values(-100201, -1002, 'Bank Size', 'true', 'display Bank Size');
+go
+
+
 /******************************************************************************/
 /* Run the Stars Update if needed here */
 /* Note: DBUpdate application will ignore this if STARS is not present */
