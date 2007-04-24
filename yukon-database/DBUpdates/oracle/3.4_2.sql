c13c9088754ebcd70cc53119805c4d962ea07fbe
Dayton, Jon R <JonRDayton@Eaton.com>
2007-04-24 22:03:42 +0000

    Some cap control role property restructuring...

---
+++ yukon-database/DBUpdates/oracle/3.4_2.sql
@@ -0,0 +1,19 @@
+/******************************************/
+/**** Oracle DBupdates   		       ****/
+/******************************************/
+update YukonRole set Category = 'Capacitor Control' where Category = 'CapBank Control';
+update YukonRole set RoleName = 'Administrator' where RoleName = 'CBC Control';
+update YukonRole set Category = 'Capacitor Control' where Category = 'CBC Oneline';
+
+/******************************************************************************/
+/* Run the Stars Update if needed here */
+/* Note: DBUpdate application will ignore this if STARS is not present */
+/* @include StarsUpdate */
+/******************************************************************************/
+
+
+/**************************************************************/
+/* VERSION INFO                                               */
+/*   Automatically gets inserted from build script            */
+/**************************************************************/
+/* __YUKON_VERSION__ */
