5d418405dd9c7543af47d86399a756353727328b
Khazon,Elliot <>
2007-04-26 18:49:08 +0000

    YUK-3089 Added Bookmark for CBC copy

--- yukon-client/yukon-web/src/com/cannontech/web/copy/DBCopyForm.java
+++ yukon-client/yukon-web/src/com/cannontech/web/copy/DBCopyForm.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
 
 import com.cannontech.clientutils.CTILogger;
 import com.cannontech.database.TransactionException;
@@ -12,195 +13,199 @@ import com.cannontech.database.data.capcontrol.CapBankController702x;
 import com.cannontech.database.data.pao.YukonPAObject;
 import com.cannontech.database.data.point.PointBase;
 import com.cannontech.database.db.DBPersistent;
+import com.cannontech.servlet.nav.CBCNavigationUtil;
 import com.cannontech.web.editor.DBEditorForm;
 import com.cannontech.web.exceptions.CBCExceptionMessages;
 import com.cannontech.web.util.CBCCopyUtils;
 
 public class DBCopyForm extends DBEditorForm {
 
-	private static final String NULL_COPY_OBJECT = "Copy object was null";
-	private static final String ERROR_COPY = "Could not copy object. ";
-	
-	private DBPersistent copyObject = null;
-	private DBPersistent origObject = null;
-	private boolean copyPoints = false;
-	private String paoName = "";
-	private boolean showCopyPoints = true;
-	
+    private static final String NULL_COPY_OBJECT = "Copy object was null";
+    private static final String ERROR_COPY = "Could not copy object. ";
 
-	public DBCopyForm() {
-		super();
+    private DBPersistent copyObject = null;
+    private DBPersistent origObject = null;
+    private boolean copyPoints = false;
+    private String paoName = "";
+    private boolean showCopyPoints = true;
 
-	}
+    public DBCopyForm() {
+        super();
 
-	public void init(int id) {
-		origObject = CBCCopyUtils.getDBPersistentByID( id );
-		if (CBCCopyUtils.isPoint(origObject)) {
-			setPaoName(((PointBase) origObject).getPoint().getPointName());
-			showCopyPoints = false;
-		}
-		else 
-			setPaoName(((YukonPAObject) origObject).getPAOName());
-			
-	}
+    }
 
-	protected void checkForErrors() throws Exception {
+    public void init(int id) {
+        origObject = CBCCopyUtils.getDBPersistentByID(id);
+        if (CBCCopyUtils.isPoint(origObject)) {
+            setPaoName(((PointBase) origObject).getPoint().getPointName());
+            showCopyPoints = false;
+        } else
+            setPaoName(((YukonPAObject) origObject).getPAOName());
 
-	}
+    }
 
-	public void copyDBObject() {
-		FacesMessage message = new FacesMessage();
-		message.setDetail(CBCExceptionMessages.DB_UPDATE_SUCCESS);
+    protected void checkForErrors() throws Exception {
 
-		if (origObject != null) {
-			try {
-				copyObject = CBCCopyUtils.copy(origObject);
-				if (CBCCopyUtils.isPoint(copyObject))
-				{
-					((PointBase)copyObject).getPoint().setPointName( getPaoName() );
-				}
-				else 
-				{
-					((YukonPAObject)copyObject).setPAOName( getPaoName() );
-				}
-				addDBObject(copyObject, message);
-				
-			
-			} catch (IllegalArgumentException e) {
-				message.setDetail(ERROR_COPY + e.getMessage());
-				message.setSeverity(FacesMessage.SEVERITY_ERROR);
-			} catch (TransactionException e) {
-				message.setDetail(ERROR_COPY + e.getMessage());
-				message.setSeverity(FacesMessage.SEVERITY_ERROR);
-			} finally {
-				FacesContext.getCurrentInstance().addMessage("copy_object",
-						message);
-			}
-			if (message.getSeverity() != FacesMessage.SEVERITY_ERROR) {
-				if (copyObject instanceof CapBankController) {
-					if (copyPoints)
-						CBCCopyUtils.copyAllPointsForPAO(
-								((CapBankController) origObject).getPAObjectID(),
-								((CapBankController) copyObject).getPAObjectID());
-						routeToEditor(((CapBankController) copyObject)
-							.getPAObjectID().intValue());
-				}
-				if (copyObject instanceof CapBankController702x) {
-					if (copyPoints)
-						CBCCopyUtils.copyAllPointsForPAO(
-								((CapBankController702x) origObject)
-										.getPAObjectID(),
-								((CapBankController702x) copyObject)
-										.getPAObjectID());
-					routeToEditor(((CapBankController702x) copyObject)
-							.getPAObjectID().intValue());
-				}
-				if (copyObject instanceof PointBase) {
-					routeToEditor(((PointBase) copyObject).getPoint()
-							.getPointID().intValue());
-				}
-			}
-		}
-		else { 
-			if (origObject != null) {
-				if (origObject instanceof YukonPAObject)
-					routeToEditor(((YukonPAObject) origObject).getPAObjectID()
-						.intValue());
-				else
-					routeToEditor(((PointBase) origObject).getPoint().getPointID()
-						.intValue());
-				}
-			else {
-					message.setDetail(NULL_COPY_OBJECT);
-					FacesContext.getCurrentInstance()
-							.addMessage("copy_object", message);
-					return;
-				}	
-		}	
-	}
+    }
 
+    public void copyDBObject() {
+        FacesMessage message = new FacesMessage();
+        message.setDetail(CBCExceptionMessages.DB_UPDATE_SUCCESS);
 
+        if (origObject != null) {
+            try {
+                copyObject = CBCCopyUtils.copy(origObject);
+                if (CBCCopyUtils.isPoint(copyObject)) {
+                    ((PointBase) copyObject).getPoint()
+                                            .setPointName(getPaoName());
+                } else {
+                    ((YukonPAObject) copyObject).setPAOName(getPaoName());
+                }
+                addDBObject(copyObject, message);
 
-	private void routeToEditor(int copyPaobjectID) {
-		FacesMessage facesMessage = new FacesMessage();
-		
-		if (copyObject != null) 
-		{
-			if (CBCCopyUtils.isPoint(copyObject) ) {
-		            routeToPointEditor(copyPaobjectID);
-				}
-			else {
-	            routeToCBCEditor(copyPaobjectID);			
-			}
-		}
-		else if (origObject != null) {
-			if (CBCCopyUtils.isPoint(origObject) ) {
-				routeToPointEditor(copyPaobjectID);
-			}
-			else {
-				routeToCBCEditor(copyPaobjectID);
-			}
-		}
-		else {
-			FacesMessage message = facesMessage;
-			message.setDetail("Could not route - object is null");
-			FacesContext.getCurrentInstance()
-					.addMessage("copy_object", message);
-			return;
-		}		
-	}
+            } catch (IllegalArgumentException e) {
+                message.setDetail(ERROR_COPY + e.getMessage());
+                message.setSeverity(FacesMessage.SEVERITY_ERROR);
+            } catch (TransactionException e) {
+                message.setDetail(ERROR_COPY + e.getMessage());
+                message.setSeverity(FacesMessage.SEVERITY_ERROR);
+            } finally {
+                FacesContext.getCurrentInstance().addMessage("copy_object",
+                                                             message);
+            }
+            if (message.getSeverity() != FacesMessage.SEVERITY_ERROR) {
+                if (copyObject instanceof CapBankController) {
+                    if (copyPoints)
+                        CBCCopyUtils.copyAllPointsForPAO(((CapBankController) origObject).getPAObjectID(),
+                                                         ((CapBankController) copyObject).getPAObjectID());
+                    routeToEditor(((CapBankController) copyObject).getPAObjectID()
+                                                                  .intValue());
+                }
+                if (copyObject instanceof CapBankController702x) {
+                    if (copyPoints)
+                        CBCCopyUtils.copyAllPointsForPAO(((CapBankController702x) origObject).getPAObjectID(),
+                                                         ((CapBankController702x) copyObject).getPAObjectID());
+                    routeToEditor(((CapBankController702x) copyObject).getPAObjectID()
+                                                                      .intValue());
+                }
+                if (copyObject instanceof PointBase) {
+                    routeToEditor(((PointBase) copyObject).getPoint()
+                                                          .getPointID()
+                                                          .intValue());
+                }
+            }
+        } else {
+            if (origObject != null) {
+                if (origObject instanceof YukonPAObject)
+                    routeToEditor(((YukonPAObject) origObject).getPAObjectID()
+                                                              .intValue());
+                else
+                    routeToEditor(((PointBase) origObject).getPoint()
+                                                          .getPointID()
+                                                          .intValue());
+            } else {
+                message.setDetail(NULL_COPY_OBJECT);
+                FacesContext.getCurrentInstance().addMessage("copy_object",
+                                                             message);
+                return;
+            }
+        }
 
-	/**
-	 * @param copyPaobjectID
-	 */
-	private void routeToCBCEditor(int copyPaobjectID) {
-		String red = "cbcBase.jsf?type=2&itemid=" + copyPaobjectID;
-		try {
-			FacesContext.getCurrentInstance().getExternalContext().redirect(red);
-		    FacesContext.getCurrentInstance().responseComplete();
-		} 
-		catch (Exception e) {
-			CTILogger.error("DBCopyForm.routeToEditor. ERROR - " + e.getMessage());     	
-		}
-	}
+    }
 
-	/**
-	 * @param copyPaobjectID
-	 */
-	private void routeToPointEditor(int copyPaobjectID) {
-		String red = "pointBase.jsf?itemid=" + copyPaobjectID;
-		try {
-			FacesContext.getCurrentInstance().getExternalContext().redirect(red);
-			FacesContext.getCurrentInstance().responseComplete();
-		} 
-		catch (IOException e) {
-			CTILogger.error("DBCopyForm.routeToEditor. ERROR - " + e.getMessage());
-		}
-	}
+    private void routeToEditor(int copyPaobjectID) {
+        FacesMessage facesMessage = new FacesMessage();
 
-	public String getPaoName() {
-		return paoName;
-	}
+        if (copyObject != null) {
+            if (CBCCopyUtils.isPoint(copyObject)) {
+                routeToPointEditor(copyPaobjectID);
+            } else {
+                routeToCBCEditor(copyPaobjectID);
+            }
+        } else if (origObject != null) {
+            if (CBCCopyUtils.isPoint(origObject)) {
+                routeToPointEditor(copyPaobjectID);
+            } else {
+                routeToCBCEditor(copyPaobjectID);
+            }
+        } else {
+            FacesMessage message = facesMessage;
+            message.setDetail("Could not route - object is null");
+            FacesContext.getCurrentInstance()
+                        .addMessage("copy_object", message);
+            return;
+        }
+    }
 
-	public void setPaoName(String paoName) {
-		this.paoName = paoName;
-	}
+    /**
+     * @param copyPaobjectID
+     */
+    private void routeToCBCEditor(int copyPaobjectID) {
+        String red = "cbcBase.jsf?type=2&itemid=" + copyPaobjectID;
+        try {
+            FacesContext.getCurrentInstance()
+                        .getExternalContext()
+                        .redirect(red);
+            FacesContext.getCurrentInstance().responseComplete();
+        } catch (Exception e) {
+            CTILogger.error("DBCopyForm.routeToEditor. ERROR - " + e.getMessage());
+        } finally {
+            bookmarkURL(red);
 
-	public boolean isCopyPoints() {
-		return copyPoints;
-	}
+        }
+    }
 
-	public void setCopyPoints(boolean copyPoints) {
-		this.copyPoints = copyPoints;
-	}
+    private void bookmarkURL(String red) {
+        if (FacesContext.getCurrentInstance() != null) {
+            HttpSession session = (HttpSession) FacesContext.getCurrentInstance()
+                                                            .getExternalContext()
+                                                            .getSession(false);
+            String url = "/editor/" + red;
+            CBCNavigationUtil.bookmarkLocation(url, session);
+        }
+    }
 
-	public boolean isShowCopyPoints() {
-		return showCopyPoints;
-	}
+    /**
+     * @param copyPaobjectID
+     */
+    private void routeToPointEditor(int copyPaobjectID) {
+        String red = "pointBase.jsf?itemid=" + copyPaobjectID;
+        try {
+            FacesContext.getCurrentInstance()
+                        .getExternalContext()
+                        .redirect(red);
+            FacesContext.getCurrentInstance().responseComplete();
+        } catch (IOException e) {
+            CTILogger.error("DBCopyForm.routeToEditor. ERROR - " + e.getMessage());
+        } finally {
+            bookmarkURL(red);
+
+        }
+    }
+
+    public String getPaoName() {
+        return paoName;
+    }
+
+    public void setPaoName(String paoName) {
+        this.paoName = paoName;
+    }
+
+    public boolean isCopyPoints() {
+        return copyPoints;
+    }
+
+    public void setCopyPoints(boolean copyPoints) {
+        this.copyPoints = copyPoints;
+    }
+
+    public boolean isShowCopyPoints() {
+        return showCopyPoints;
+    }
 
     @Override
     public void resetForm() {
-        
+
     }
 
 }
