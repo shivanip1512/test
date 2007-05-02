3188120c7cf1b5107aa6b9205b3c7f10e8fce5da
Dayton, Jon R <JonRDayton@Eaton.com>
2007-05-02 21:41:36 +0000

    YUK-3205 -3.2.15 only but 3.4 and HEAD at least need the 3.2.15 update file

--- yukon-database/DBUpdates/oracle/3.2_15.sql
+++ yukon-database/DBUpdates/oracle/3.2_15.sql
@@ -4,6 +4,8 @@
 
 insert into YukonRoleProperty values(-1114,-2,'Inherit Parent App Cats','true','If part of a member structure, should appliance categories be inherited from the parent.');
 
+insert into YukonListEntry values (1064,1005,-1,'ExpressStat Heat Pump',1313);
+
 /******************************************************************************/
 /* Run the Stars Update if needed here */
 /* Note: DBUpdate application will ignore this if STARS is not present */
--- yukon-database/DBUpdates/sqlserver/3.2_15.sql
+++ yukon-database/DBUpdates/sqlserver/3.2_15.sql
@@ -5,6 +5,9 @@
 insert into YukonRoleProperty values(-1114,-2,'Inherit Parent App Cats','true','If part of a member structure, should appliance categories be inherited from the parent.');
 go
 
+insert into YukonListEntry values (1064,1005,-1,'ExpressStat Heat Pump',1313);
+go
+
 /******************************************************************************/
 /* Run the Stars Update if needed here */
 /* Note: DBUpdate application will ignore this if STARS is not present */
