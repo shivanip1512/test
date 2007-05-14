2b6c96884ef24aac543e2556badfcf1b90e36cbd
Otteson, Jess M <JessMOtteson@Eaton.com>
2007-05-14 15:03:36 +0000

    Removed lines that were added for creation of CtiTime and have nothing to do with unit testing.

--- yukon-server/COMMON/test_ctitime.cpp
+++ yukon-server/COMMON/test_ctitime.cpp
@@ -35,13 +35,15 @@ using namespace boost::gregorian;
 using namespace boost::posix_time;
 using namespace std;
 
+/*
 BOOST_AUTO_UNIT_TEST(test_ptime)
 {
     ptime pt = from_time_t((time_t)0);
     std::cout << to_simple_string(pt) << std::endl;
-}
+}*/
 
-BOOST_AUTO_UNIT_TEST(test_rwtime_methods)
+/* I dont think this belongs in the unit test - Jess*/
+/*BOOST_AUTO_UNIT_TEST(test_rwtime_methods)
 {
     int timeduration = 60*60;
     RWTime rt1;
@@ -76,7 +78,7 @@ BOOST_AUTO_UNIT_TEST(test_rwtime_methods)
     cout << rt4.asString() << endl;
     cout << rt5.asString() << endl << endl; 
     
-}
+}*/
 
 
 
@@ -87,7 +89,7 @@ BOOST_AUTO_UNIT_TEST(test_ctitime_methods)
 
     RWTime rw_time = RWTime();
     CtiTime d = CtiTime(rw_time.hour(), rw_time.minute(), rw_time.second());
-    std::cout << "Current time: " << d.asString() << std::endl;
+    //std::cout << "Current time: " << d.asString() << std::endl;
     BOOST_CHECK_EQUAL( rw_time.hour(), d.hour() );
     BOOST_CHECK_EQUAL( rw_time.minute(), d.minute() );
     BOOST_CHECK_EQUAL( rw_time.second(), d.second() );
@@ -97,7 +99,7 @@ BOOST_AUTO_UNIT_TEST(test_ctitime_methods)
 
     d.addDays(1);
     rw_time = RWTime(d.toRwSeconds());
-    std::cout << "Current time: " << d.asString() << std::endl;
+    //std::cout << "Current time: " << d.asString() << std::endl;
     BOOST_CHECK_EQUAL( rw_time.hour(), d.hour() );
     BOOST_CHECK_EQUAL( rw_time.minute(), d.minute() );
     BOOST_CHECK_EQUAL( rw_time.second(), d.second() );
@@ -124,12 +126,12 @@ BOOST_AUTO_UNIT_TEST(test_ctitime_methods)
     BOOST_CHECK_EQUAL( d2.second(), rw2.second() );
 
     ct5 = d2;
-    std::cout << std::endl << "Time, " << ct5.asString() << ", to UTC time: " << d2.toUTCtime().asString() << std::endl;
+    //std::cout << std::endl << "Time, " << ct5.asString() << ", to UTC time: " << d2.toUTCtime().asString() << std::endl;
 
     //static methods
     BOOST_CHECK_EQUAL( RWTime::now().minute(), CtiTime::now().minute() );
-    std::cout << std::endl << "DST for 2005 in your local is: " << std::endl << CtiTime::beginDST(2005).asString();
-    std::cout << " to " << CtiTime::endDST(2005).asString() << std::endl;
+    /*std::cout << std::endl << "DST for 2005 in your local is: " << std::endl << CtiTime::beginDST(2005).asString();
+    std::cout << " to " << CtiTime::endDST(2005).asString() << std::endl;*/
 
     // check the constructor CtiTime(seconds), and the special case CtiTime(0)
     long timep = 5*24*60*60 + 8*60*60;
@@ -157,34 +159,37 @@ BOOST_AUTO_UNIT_TEST(test_ctitime_specials)
     //check boost special values
     std::cout << endl;
     ptime x(date(boost::date_time::neg_infin), boost::date_time::neg_infin);
-    std::cout << "neg_infin ptime hour: " << x.time_of_day().hours() << std::endl;
+    /*std::cout << "neg_infin ptime hour: " << x.time_of_day().hours() << std::endl;
     std::cout << "neg_infin ptime seconds: " << x.time_of_day().seconds() << std::endl;
-    std::cout << endl;
+    std::cout << endl;*/
 
     ptime y(date(boost::date_time::not_a_date_time), boost::date_time::not_a_date_time);
-    std::cout << "not_a_date_time ptime hour: " << y.time_of_day().hours() << std::endl;
+    /*std::cout << "not_a_date_time ptime hour: " << y.time_of_day().hours() << std::endl;
     std::cout << "not_a_date_time ptime seconds: " << y.time_of_day().seconds() << std::endl;
-    std::cout << endl;
+    std::cout << endl;*/
 
 
     // check the special time values
     CtiTime ct1 = CtiTime(CtiTime::neg_infin);
+    BOOST_CHECK_EQUAL( ct1.is_special(), true );
 
-    std::cout << "neg_infin time as string: " << ct1.asString() << std::endl;
+   /* std::cout << "neg_infin time as string: " << ct1.asString() << std::endl;
     std::cout << "neg_infin time hour: " << ct1.hour() << std::endl;
     std::cout << "neg_infin time seconds: " << ct1.second() << std::endl;
-    std::cout << endl;
+    std::cout << endl;*/
 
     ct1 = CtiTime(CtiTime::not_a_time);
 
-    std::cout << "not_a_time as string: " << ct1.asString() << std::endl;
+    BOOST_CHECK_EQUAL( ct1.is_special(), true );
+    /*std::cout << "not_a_time as string: " << ct1.asString() << std::endl;
     std::cout << "not_a_time hour: " << ct1.hour() << std::endl;
     std::cout << "not_a_time seconds: " << ct1.second() << std::endl;
-    std::cout << endl;
+    std::cout << endl;*/
 
-   
+    BOOST_CHECK_EQUAL( CtiTime((unsigned long)0).is_special(), true );
     BOOST_CHECK_EQUAL( CtiTime(CtiTime::neg_infin).isValid(), true );
     BOOST_CHECK_EQUAL( CtiTime(CtiTime::pos_infin).isValid(), true );
+    BOOST_CHECK_EQUAL( CtiTime((unsigned long)0).isValid(), false );
     BOOST_CHECK_EQUAL( CtiTime(ct1).isValid(), false );
     
 
@@ -262,9 +267,9 @@ BOOST_AUTO_UNIT_TEST(test_ctitime_DST)
 {
     //explicitly create a time 1:00, then add 1 hour to it, which results the boundary of DST
     CtiTime ict(CtiTime::beginDST(2005).date(), 1, 0 ,0 );
-    std::cout << std::endl << ict.asString() << std::endl;
+    //std::cout << std::endl << ict.asString() << std::endl;
     ict += 1*60*60;
-    std::cout << ict.asString() << std::endl;
+    //std::cout << ict.asString() << std::endl;
 
 
     CtiTime ct(CtiTime::beginDST(2005));
@@ -276,11 +281,11 @@ BOOST_AUTO_UNIT_TEST(test_ctitime_DST)
     CtiTime cta;
     cta = ct + timeduration;
     
-    std::cout << std::endl;
+    /*std::cout << std::endl;
     std::cout << "DST begin time:\t\t\t" << ct.asString() << std::endl;
     std::cout << "A not-exsit time (2:30am):\t" << CtiTime(ct.date(), 2, 30, 0).asString() << std::endl;
     std::cout << "DST begin time - 5hrs:\t\t" << ctb.asString() << std::endl;
-    std::cout << "DST begin time + 5hrs:\t\t" << cta.asString() << std::endl;
+    std::cout << "DST begin time + 5hrs:\t\t" << cta.asString() << std::endl;*/
 
     // check the time duration calculation acrossing the DST boundary
     BOOST_CHECK_EQUAL( 2 * timeduration, cta.seconds() - ctb.seconds() );
@@ -300,16 +305,16 @@ BOOST_AUTO_UNIT_TEST(test_ctitime_DST)
     ctt = ct - 30*60; // 0:30am
     ctt1 = ctt - 90*60; // 23:00pm
 
-    std::cout << std::endl << "DST end time - 1hr: \t\t" << ct.asString() << std::endl;
+    /*std::cout << std::endl << "DST end time - 1hr: \t\t" << ct.asString() << std::endl;
     std::cout << "DST end time - 1.5hr: \t\t" << ctt.asString() << std::endl;
-    std::cout << "DST end time - 3hr: \t\t" << ctt1.asString() << std::endl;
+    std::cout << "DST end time - 3hr: \t\t" << ctt1.asString() << std::endl;*/
     
     BOOST_CHECK_EQUAL( ctt1.seconds() + 2*timeduration, ct.seconds() );
     //asdf
     ct = ctt; // 0:30am
     ctt = ct + 6*60*60; //5.30am
     ctt1 = ct + 5*60*60; //4.30am
-    std::cout << "DST end time + 3.5hr: \t\t" << ctt1.asString() << std::endl;
+    //std::cout << "DST end time + 3.5hr: \t\t" << ctt1.asString() << std::endl;
     BOOST_CHECK_EQUAL( ctt1.seconds() + timeduration, ctt.seconds() );
 
     ctt = CtiTime(ct.date() - 1, 23, 30, 0);
@@ -324,7 +329,7 @@ BOOST_AUTO_UNIT_TEST(test_ctitime_DST)
 
 
 }
-
+/*
 BOOST_AUTO_UNIT_TEST(test_locale)
 {
     using namespace boost::gregorian;
@@ -337,7 +342,7 @@ BOOST_AUTO_UNIT_TEST(test_locale)
     ss.imbue(global); 
     ss << t1.asString();
     std::cout << "using default locale: " << ss.str() << std::endl;
-}
+}*/
 
 //test_suite*
 //init_unit_test_suite( int /*argc*/, char* /*argv*/[] ) {
