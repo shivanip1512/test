70355bc3ab45868975e2e6b180f916de5f9540e3
Ganz, Julie <>
2007-05-15 22:36:02 +0000

    YUK-3885 Able to create multiple schedules with the same name.

--- yukon-database/DBChangeSubmission/SQLSubmit.sql
+++ yukon-database/DBChangeSubmission/SQLSubmit.sql
@@ -2,8 +2,14 @@
 /*to split your new additions from those already there.  Thanks!
 */
 
-/** elliot **/
+/** elliot: to go into 3.4 and head **/
 /** CBC Name property for oneline **/
 insert into YukonRoleProperty values(-100202, -1002, 'CBC Name', 'true', 'display CBC Name');
 go
 /************/
+
+/*** JULIE: to go into 3.4 and head ****/
+create unique  index Indx_SchedNm on paoschedule (
+schedulename
+)
+/*** END JULIE: to go into 3.4 and head ****/
