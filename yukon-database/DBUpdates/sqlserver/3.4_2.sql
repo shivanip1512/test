8a59574187d8431af4485bdc3c1c7d33ee0be3e2
Dayton, Jon R <JonRDayton@Eaton.com>
2007-05-18 17:28:56 +0000

    Removes duplicate role property lines...

--- yukon-database/DBUpdates/sqlserver/3.4_2.sql
+++ yukon-database/DBUpdates/sqlserver/3.4_2.sql
@@ -24,13 +24,6 @@ insert into devicetypecommand values(-577, -128, 'VersacomSerial', 23, 'N', -1);
 insert into devicetypecommand values(-578, -129, 'VersacomSerial', 24, 'N', -1);
 go
 
-insert into YukonListEntry values (1064,1005,-1,'ExpressStat Heat Pump',1313);
-go
-
-insert into YukonRoleProperty values(-40198,-400,'Opt Out Today Only','false','Prevents residential side opt outs from being available for scheduling beyond the current day.');
-insert into YukonRoleProperty values(-20894,-201,'Opt Out Today Only','false','Prevents operator side opt outs from being available for scheduling beyond the current day.');
-go
-
 insert into YukonRoleProperty values(-1114,-2,'Inherit Parent App Cats','true','If part of a member structure, should appliance categories be inherited from the parent.');
 go
 
