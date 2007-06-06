c7a9e1b7548f5308ed87cd6ee3bb539818bf783f
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2007-06-06 18:18:45 +0000

    Implemented getServiceLocation for AccountInfoDao

--- yukon-client/multispeak/src/com/cannontech/multispeak/dao/impl/MspAccountInfoDaoImpl.java
+++ yukon-client/multispeak/src/com/cannontech/multispeak/dao/impl/MspAccountInfoDaoImpl.java
@@ -9,7 +9,7 @@ import com.cannontech.amr.account.model.AccountInfo;
 import com.cannontech.amr.account.model.Address;
 import com.cannontech.amr.account.model.ServiceLocation;
 import com.cannontech.clientutils.CTILogger;
-import com.cannontech.core.dao.DaoFactory;
+import com.cannontech.core.dao.DeviceDao;
 import com.cannontech.database.data.lite.LiteDeviceMeterNumber;
 import com.cannontech.multispeak.client.MultispeakFuncs;
 import com.cannontech.multispeak.client.MultispeakVendor;
@@ -21,6 +21,7 @@ public class MspAccountInfoDaoImpl implements AccountInfoDao {
 
     public MultispeakDao multispeakDao;
     public MultispeakFuncs multispeakFuncs;
+    public DeviceDao deviceDao;
     
     public void setMultispeakDao(MultispeakDao multispeakDao) {
         this.multispeakDao = multispeakDao;
@@ -29,13 +30,26 @@ public class MspAccountInfoDaoImpl implements AccountInfoDao {
     public void setMultispeakFuncs(MultispeakFuncs multispeakFuncs) {
         this.multispeakFuncs = multispeakFuncs;
     }
+    
+    public void setDeviceDao(DeviceDao deviceDao) {
+        this.deviceDao = deviceDao;
+    }
 
+    public AccountInfo getAccount(int deviceId) {        
+        com.cannontech.multispeak.service.Customer mspCustomer = getMspCustomer(deviceId);
+        return mapToAccountInfo(mspCustomer);
+    }
+    
+    public ServiceLocation getServiceLocation(int deviceId) {
+        com.cannontech.multispeak.service.ServiceLocation mspServLoc = getMspServiceLocation(deviceId);
+        return mapToServicLocation(mspServLoc);
+    }
+    
     public com.cannontech.multispeak.service.Customer getMspCustomer(int deviceId) {
         
-        MultispeakVendor mspVendor;
-        mspVendor = multispeakDao.getMultispeakVendor(multispeakFuncs.getPrimaryCIS());
+        MultispeakVendor mspVendor = multispeakDao.getMultispeakVendor(multispeakFuncs.getPrimaryCIS());
         
-        LiteDeviceMeterNumber liteDevMeterNum = DaoFactory.getDeviceDao().getLiteDeviceMeterNumber(deviceId);
+        LiteDeviceMeterNumber liteDevMeterNum = deviceDao.getLiteDeviceMeterNumber(deviceId);
         
         String meterNumber = liteDevMeterNum.getMeterNumber();
         com.cannontech.multispeak.service.Customer mspCustomer = null;
@@ -46,25 +60,39 @@ public class MspAccountInfoDaoImpl implements AccountInfoDao {
             
         } catch (ServiceException e) {
             CTILogger.error("CB_MR service is not defined for company(" + mspVendor.getCompanyName()+ ") - getCustomerByMeterNo failed.");
-            CTILogger.error("ServiceExceptionDetail: " + e.getMessage());
+            CTILogger.error("ServiceExceptionDetail: " + e);
 
         } catch (RemoteException e) {
             CTILogger.error("TargetService for company(" + mspVendor.getCompanyName()+ ") - getCustomerByMeterNo failed.");
-            CTILogger.error("RemoteExceptionDetail: "+e.getMessage());
+            CTILogger.error("RemoteExceptionDetail: " + e);
         }
         return mspCustomer;
     }
     
-    public AccountInfo getAccount(int deviceId) {        
-        com.cannontech.multispeak.service.Customer mspCustomer = getMspCustomer(deviceId);
-        return mapMspCustomerToCustomer(mspCustomer);
+    public com.cannontech.multispeak.service.ServiceLocation getMspServiceLocation(int deviceId) {
+        MultispeakVendor mspVendor = multispeakDao.getMultispeakVendor(multispeakFuncs.getPrimaryCIS());
+        
+        LiteDeviceMeterNumber liteDevMeterNum = deviceDao.getLiteDeviceMeterNumber(deviceId);
+        
+        String meterNumber = liteDevMeterNum.getMeterNumber();
+        com.cannontech.multispeak.service.ServiceLocation mspServLoc = null;
+            
+        try {
+            CB_MRSoap_BindingStub port = MultispeakPortFactory.getCB_MRPort(mspVendor);
+            mspServLoc = port.getServiceLocationByMeterNo(meterNumber);
+            
+        } catch (ServiceException e) {
+            CTILogger.error("CB_MR service is not defined for company(" + mspVendor.getCompanyName()+ ") - getServiceLocationByMeterNo failed.");
+            CTILogger.error("ServiceExceptionDetail: " + e);
+
+        } catch (RemoteException e) {
+            CTILogger.error("TargetService for company(" + mspVendor.getCompanyName()+ ") - getServiceLocationByMeterNo failed.");
+            CTILogger.error("RemoteExceptionDetail: " + e);
+        }
+        return mspServLoc;
     }
     
-    public ServiceLocation getServiceLocation(int deviceId) {
-        return null;
-    }
-    
-    public AccountInfo mapMspCustomerToCustomer(com.cannontech.multispeak.service.Customer mspCustomer) {
+    public AccountInfo mapToAccountInfo(com.cannontech.multispeak.service.Customer mspCustomer) {
         final AccountInfo accountInfo = new AccountInfo();
         accountInfo.setFirstName(mspCustomer.getFirstName());
         accountInfo.setLastName(mspCustomer.getLastName());
@@ -79,4 +107,22 @@ public class MspAccountInfoDaoImpl implements AccountInfoDao {
         
         return accountInfo;
     }
+    
+    public ServiceLocation mapToServicLocation(com.cannontech.multispeak.service.ServiceLocation mspServLoc) {
+        final ServiceLocation servLoc = new ServiceLocation();
+        servLoc.setAccountNumber(mspServLoc.getAccountNumber());
+        servLoc.setCustomerNumber(mspServLoc.getCustID());
+        servLoc.setSiteNumber(mspServLoc.getSiteID());
+        servLoc.setMapLocation(mspServLoc.getGridLocation());
+        
+        final Address address = new Address();
+        address.setCityName(mspServLoc.getServCity());
+        address.setStateCode(mspServLoc.getServState());
+        address.setZipCode(mspServLoc.getServZip());
+        address.setLocationAddress1(mspServLoc.getServAddr1());
+        address.setLocationAddress2(mspServLoc.getServAddr2());
+        servLoc.setAddress(address);
+        
+        return servLoc;
+    }    
 }
