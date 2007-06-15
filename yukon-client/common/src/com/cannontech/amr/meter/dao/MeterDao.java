c0debc3d0944e3168734a12e5392ad545a8fbf8c
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2007-06-15 00:49:44 +0000

    Added support for getting a meter by MeterNumber and by PaoName.

--- yukon-client/common/src/com/cannontech/amr/meter/dao/MeterDao.java
+++ yukon-client/common/src/com/cannontech/amr/meter/dao/MeterDao.java
@@ -1,8 +1,18 @@
 package com.cannontech.amr.meter.dao;
 
+import java.util.List;
+
 import com.cannontech.amr.meter.model.Meter;
 import com.cannontech.core.dao.support.StandardDaoOperations;
 
 public interface MeterDao extends StandardDaoOperations<Meter> {
     public String getFormattedDeviceName(Meter device);
+    
+    public Meter getForMeterNumber(String meterNumber);
+    
+    public Meter getForPaoName(String paoName);
+    
+    public List<Meter> getMetersByMeterNumber(String lastReceived, int maxRecordCount);
+    
+    public List<Meter> getMetersByPaoName(String lastReceived, int maxRecordCount);
 }
--- yukon-client/common/src/com/cannontech/amr/meter/dao/impl/MeterDaoImpl.java
+++ yukon-client/common/src/com/cannontech/amr/meter/dao/impl/MeterDaoImpl.java
@@ -1,9 +1,12 @@
 package com.cannontech.amr.meter.dao.impl;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang.Validate;
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcOperations;
 
@@ -12,32 +15,52 @@ import com.cannontech.amr.meter.model.Meter;
 import com.cannontech.common.util.SimpleTemplateProcessor;
 import com.cannontech.common.util.TemplateProcessor;
 import com.cannontech.core.dao.RoleDao;
+import com.cannontech.database.ListRowCallbackHandler;
+import com.cannontech.database.MaxRowCalbackHandlerRse;
 import com.cannontech.roles.yukon.ConfigurationRole;
 
 public class MeterDaoImpl implements MeterDao {
     private SimpleJdbcOperations simpleJdbcTemplate;
+    private JdbcOperations jdbcOps;
     private ParameterizedRowMapper<Meter> meterRowMapper;
     private RoleDao roleDao;
     
-    String retrieveOneSql = 
+    String retrieveMeterSql = 
         "select YukonPaObject.*, DeviceMeterGroup.*, DeviceCarrierSettings.*, DeviceRoutes.*, yporoute.paoName as route "
         + "from YukonPaObject "
         + "join Device on YukonPaObject.paObjectId = Device.deviceId "
-        + " join DeviceMeterGroup on Device.deviceId = DeviceMeterGroup.deviceId "
-        + " join DeviceCarrierSettings on Device.deviceId = DeviceCarrierSettings.deviceId "
-        + " join DeviceRoutes on Device.deviceId = DeviceRoutes.deviceId "
-        + " join YukonPaObject yporoute on DeviceRoutes.routeId = yporoute.paObjectId "
-        + "where YukonPaObject.paObjectId = ?";
+        + "join DeviceMeterGroup on Device.deviceId = DeviceMeterGroup.deviceId "
+        + "join DeviceCarrierSettings on Device.deviceId = DeviceCarrierSettings.deviceId "
+        + "join DeviceRoutes on Device.deviceId = DeviceRoutes.deviceId "
+        + "join YukonPaObject yporoute on DeviceRoutes.routeId = yporoute.paObjectId ";
 
+    String retrieveOneByIdSql = retrieveMeterSql + "where YukonPaObject.paObjectId = ? ";
+    String retrieveOneByMeterNumberSql = retrieveMeterSql + "where DeviceMeterGroup.MeterNumber = ? ";
+    String retrieveOneByPaoNameSql = retrieveMeterSql + "where YukonPaobject.PaoName = ? ";
+    
     public void delete(Meter object) {
         throw new UnsupportedOperationException("maybe someday...");
     }
 
     public Meter getForId(Integer id) {
-        Meter meter = simpleJdbcTemplate.queryForObject(retrieveOneSql, meterRowMapper, id);
+        Meter meter = simpleJdbcTemplate.queryForObject(retrieveOneByIdSql, meterRowMapper, id);
         return meter;
     }
-
+    
+    public Meter getForMeterNumber(String meterNumber) {
+        Meter meter = simpleJdbcTemplate.queryForObject(retrieveOneByMeterNumberSql, 
+                                                        meterRowMapper, 
+                                                        meterNumber);
+        return meter;
+    }
+    
+    public Meter getForPaoName(String paoName) {
+        Meter meter = simpleJdbcTemplate.queryForObject(retrieveOneByPaoNameSql, 
+                                                        meterRowMapper, 
+                                                        paoName);
+        return meter;
+    }
+    
     public void save(Meter object) {
         throw new UnsupportedOperationException("maybe someday...");
     }
@@ -60,6 +83,40 @@ public class MeterDaoImpl implements MeterDao {
         return result;
     }
     
+    /**
+     * Returns a list of Meter objects.  LastReceived is the minimum MeterNumber 
+     * (order ASC by MeterNumber) to return.  MaxRecordCount is the maximum number of records
+     * to return in the Meter list.
+     */
+    public List<Meter> getMetersByMeterNumber(String lastReceived, int maxRecordCount) {
+        if( lastReceived == null)
+            lastReceived = "";
+
+        String sql = retrieveMeterSql + " AND MeterNumber > ? ORDER BY MeterNumber";
+        
+        List<Meter> mspMeters = new ArrayList<Meter>();
+        ListRowCallbackHandler lrcHandler = new ListRowCallbackHandler(mspMeters, meterRowMapper);
+        jdbcOps.query(sql, new Object[]{lastReceived}, new MaxRowCalbackHandlerRse(lrcHandler, maxRecordCount));
+        return mspMeters;
+    }
+
+    /**
+     * Returns a list of Meter objects.  LastReceived is the minimum PaoName 
+     * (order ASC by PaoName) to return.  MaxRecordCount is the maximum number of records
+     * to return in the Meter list.
+     */
+    public List<Meter> getMetersByPaoName(String lastReceived, int maxRecordCount) {
+        if( lastReceived == null)
+            lastReceived = "";
+
+        String sql = retrieveMeterSql + " AND PaoName > ? ORDER BY PaoName";
+        
+        List<Meter> mspMeters = new ArrayList<Meter>();
+        ListRowCallbackHandler lrcHandler = new ListRowCallbackHandler(mspMeters, meterRowMapper);
+        jdbcOps.query(sql, new Object[]{lastReceived}, new MaxRowCalbackHandlerRse(lrcHandler, maxRecordCount));
+        return mspMeters;
+    }
+    
     public void setMeterRowMapper(ParameterizedRowMapper<Meter> meterRowMapper) {
         this.meterRowMapper = meterRowMapper;
     }
@@ -71,5 +128,8 @@ public class MeterDaoImpl implements MeterDao {
     public void setRoleDao(RoleDao roleDao) {
         this.roleDao = roleDao;
     }
-
+    
+    public void setJdbcOps(JdbcOperations jdbcOps) {
+        this.jdbcOps = jdbcOps;
+    }
 }
