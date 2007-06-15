31546df1cddf087368804951c6dd8d05d83c66d8
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2007-06-15 00:51:07 +0000

    New wrapper for retrieving dynamicDataSource data for Attribute.

---
+++ yukon-client/common/src/com/cannontech/common/device/attribute/service/AttributeDynamicDataSource.java
@@ -0,0 +1,32 @@
+package com.cannontech.common.device.attribute.service;
+
+import com.cannontech.amr.meter.model.Meter;
+import com.cannontech.common.device.attribute.model.Attribute;
+import com.cannontech.core.dao.PaoDao;
+import com.cannontech.core.dynamic.DynamicDataSource;
+import com.cannontech.core.dynamic.PointValueHolder;
+import com.cannontech.database.data.lite.LitePoint;
+
+public class AttributeDynamicDataSource {
+
+    public DynamicDataSource dynamicDataSource;
+    public PaoDao paoDao;
+    public AttributeService attributeService;
+    
+    public void setDynamicDataSource(DynamicDataSource dynamicDataSource) {
+        this.dynamicDataSource = dynamicDataSource;
+    }
+
+    public void setPaoDao(PaoDao paoDao) {
+        this.paoDao = paoDao;
+    }
+
+    public void setAttributeService(AttributeService attributeService) {
+        this.attributeService = attributeService;
+    }
+    
+    public PointValueHolder getPointValue(Meter meter, Attribute attribute) {
+        LitePoint litePoint = attributeService.getPointForAttribute(meter, attribute);
+        return dynamicDataSource.getPointValue(litePoint.getPointID());
+    }
+}
