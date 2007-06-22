04432cd7a89bccefa04b332552100a32fea20ac9
Mack, Tom <>
2007-06-22 17:02:09 +0000

    Added descriptions to the enums. This is a little lazy, but will work until they need to be customized.

--- yukon-client/common/src/com/cannontech/common/device/attribute/model/BuiltInAttribute.java
+++ yukon-client/common/src/com/cannontech/common/device/attribute/model/BuiltInAttribute.java
@@ -1,11 +1,18 @@
 package com.cannontech.common.device.attribute.model;
 
 public enum BuiltInAttribute implements Attribute {
-    LOAD_PROFILE, USAGE, DEMAND, PEAK_DEMAND, BLINK_COUNT, 
-    DISCONNECT_STATUS, VOLTAGE, VOLTAGE_PROFILE;
+    LOAD_PROFILE("Load Profile"), USAGE("Usage"), DEMAND("Demand"), PEAK_DEMAND("Peak Demand"), 
+    BLINK_COUNT("Blink Count"), DISCONNECT_STATUS("Disconnect Status"), VOLTAGE("Voltage"), 
+    VOLTAGE_PROFILE("Voltage Profile");
+    
+    private BuiltInAttribute(String description) {
+        this.description = description;
+    }
+    
+    private String description;
     
     public String getDescription() {
-        return this.toString(); // until we have something better here...
+        return description;
     }
     
     public String getKey() {
