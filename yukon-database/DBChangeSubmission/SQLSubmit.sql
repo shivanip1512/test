3c7457729783837d2237c9b60a1e154b698afa11
Dayton, Jon R <JonRDayton@Eaton.com>
2007-06-26 20:00:48 +0000

    Short 3.5 database change todo list...

--- yukon-database/DBChangeSubmission/SQLSubmit.sql
+++ yukon-database/DBChangeSubmission/SQLSubmit.sql
@@ -1,3 +1,9 @@
 /*Please be kind and keep things cleanly organized.  Add new entries to the end and use /**********************/
 /*to split your new additions from those already there.  Thanks!
 */
+
+/*Tom's DeviceMeterGroup changes*/
+
+/*Removal/cleanup of Commercial Metering roles*/
+
+/*PAObject constraint change*/
