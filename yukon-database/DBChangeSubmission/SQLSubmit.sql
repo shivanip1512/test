8c046e302ec13632f4236559db3ff94d85d964c2
Mack, Tom <>
2007-07-03 21:30:46 +0000

    YUK-3823: Develop device grouping system (AMR collection groups)

--- yukon-database/DBChangeSubmission/SQLSubmit.sql
+++ yukon-database/DBChangeSubmission/SQLSubmit.sql
@@ -1,5 +1,5 @@
 /*Please be kind and keep things cleanly organized.  Add new entries to the end and use /**********************/
-/*to split your new additions from those already there.  Thanks!
+/*to split your new additions from those already there.  Thanks!*/
 
 
-
+/* Jon, I would like DeviceGroupMember to have a cascade delete constraint to the device/yukonpaobject table */
