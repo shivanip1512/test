95d88be068cdf1ff31d7b9f6119f88725bc116ba
Fisher, Matthew S <MatthewSFisher@Eaton.com>
2007-07-09 21:50:43 +0000

    YUK-3823 Removed old group name columns

--- yukon-server/Database/tbl_metergrp.cpp
+++ yukon-server/Database/tbl_metergrp.cpp
@@ -8,8 +8,8 @@
 *
 * PVCS KEYWORDS:
 * ARCHIVE      :  $Archive:   Z:/SOFTWAREARCHIVES/YUKON/DATABASE/tbl_metergrp.cpp-arc  $
-* REVISION     :  $Revision: 1.10 $
-* DATE         :  $Date: 2005/12/20 17:16:06 $
+* REVISION     :  $Revision: 1.11 $
+* DATE         :  $Date: 2007/07/09 21:50:43 $
 *
 * Copyright (c) 1999, 2000, 2001 Cannon Technologies Inc. All rights reserved.
 *-----------------------------------------------------------------------------*/
@@ -36,63 +36,20 @@ CtiTableDeviceMeterGroup& CtiTableDeviceMeterGroup::operator=(const CtiTableDevi
     if(this != &aRef)
     {
         _deviceID            = aRef.getDeviceID();
-        _collectionGroup     = aRef.getCollectionGroup();
-        _testCollectionGroup = aRef.getTestCollectionGroup();
         _meterNumber         = aRef.getMeterNumber();
-        _billingGroup        = aRef.getBillingGroup();
     }
-    return *this;
-}
-
-string  CtiTableDeviceMeterGroup::getCollectionGroup() const
-{
-
-    return _collectionGroup;
-}
-
-CtiTableDeviceMeterGroup& CtiTableDeviceMeterGroup::setCollectionGroup( const string &aCycleGroup )
-{
-
-    _collectionGroup = aCycleGroup;
-    return *this;
-}
-
-string CtiTableDeviceMeterGroup::getBillingGroup() const
-{
-
-    return _billingGroup;
-}
-
-CtiTableDeviceMeterGroup& CtiTableDeviceMeterGroup::setBillingGroup( const string &billGroup )
-{
-
-    _billingGroup = billGroup;
     return *this;
 }
 
 string CtiTableDeviceMeterGroup::getMeterNumber() const
 {
-
     return _meterNumber;
 }
 
 CtiTableDeviceMeterGroup& CtiTableDeviceMeterGroup::setMeterNumber( const string &meterNumber )
 {
-
     _meterNumber = meterNumber;
-    return *this;
-}
 
-string  CtiTableDeviceMeterGroup::getTestCollectionGroup() const
-{
-
-    return _testCollectionGroup;
-}
-
-CtiTableDeviceMeterGroup& CtiTableDeviceMeterGroup::setTestCollectionGroup( const string &aAreaCodeGroup )
-{
-
-    _testCollectionGroup = aAreaCodeGroup;
     return *this;
 }
 
@@ -101,10 +58,7 @@ void CtiTableDeviceMeterGroup::getSQL(RWDBDatabase &db,  RWDBTable &keyTable, RW
     RWDBTable devTbl = db.table(getTableName().c_str());
 
     selector <<
-    devTbl["collectiongroup"] <<
-    devTbl["testcollectiongroup"] <<
-    devTbl["meternumber"] <<
-    devTbl["billinggroup"];
+    devTbl["meternumber"];
 
     selector.from(devTbl);
     selector.where( keyTable["paobjectid"] == devTbl["deviceid"] && selector.where() );  //later: == getDeviceID());
@@ -112,7 +66,6 @@ void CtiTableDeviceMeterGroup::getSQL(RWDBDatabase &db,  RWDBTable &keyTable, RW
 
 void CtiTableDeviceMeterGroup::DecodeDatabaseReader(RWDBReader &rdr)
 {
-
     if(getDebugLevel() & DEBUGLEVEL_DATABASE)
     {
         CtiLockGuard<CtiLogger> logger_guard(dout);
@@ -120,22 +73,18 @@ void CtiTableDeviceMeterGroup::DecodeDatabaseReader(RWDBReader &rdr)
     }
 
     rdr["deviceid"] >> _deviceID;
-    rdr["collectiongroup"] >> _collectionGroup;
-    rdr["testcollectiongroup"] >> _testCollectionGroup;
     rdr["meternumber"] >> _meterNumber;
-    rdr["billinggroup"] >> _billingGroup;
 }
 
 LONG CtiTableDeviceMeterGroup::getDeviceID() const
 {
-
     return _deviceID;
 }
 
 CtiTableDeviceMeterGroup& CtiTableDeviceMeterGroup::setDeviceID( const LONG deviceID )
 {
-
     _deviceID = deviceID;
+
     return *this;
 }
 
@@ -146,7 +95,6 @@ string CtiTableDeviceMeterGroup::getTableName()
 
 RWDBStatus CtiTableDeviceMeterGroup::Restore()
 {
-
     char temp[32];
 
     CtiLockGuard<CtiSemaphore> cg(gDBAccessSema);
@@ -157,10 +105,7 @@ RWDBStatus CtiTableDeviceMeterGroup::Restore()
 
     selector <<
     table["deviceid"] <<
-    table["collectiongroup"] <<
-    table["testcollectiongroup"] <<
-    table["meternumber"] <<
-    table["billinggroup"];
+    table["meternumber"];
 
     selector.where( table["deviceid"] == getDeviceID() );
 
@@ -175,13 +120,12 @@ RWDBStatus CtiTableDeviceMeterGroup::Restore()
     {
         setDirty( true );
     }
+
     return reader.status();
 }
 
 RWDBStatus CtiTableDeviceMeterGroup::Insert()
 {
-
-
     CtiLockGuard<CtiSemaphore> cg(gDBAccessSema);
     RWDBConnection conn = getConnection();
 
@@ -190,10 +134,7 @@ RWDBStatus CtiTableDeviceMeterGroup::Insert()
 
     inserter <<
     getDeviceID() <<
-    getCollectionGroup() <<
-    getTestCollectionGroup() <<
-    getMeterNumber() <<
-    getBillingGroup();
+    getMeterNumber();
 
     if( ExecuteInserter(conn,inserter,__FILE__,__LINE__).errorCode() == RWDBStatus::ok)
     {
@@ -207,8 +148,6 @@ RWDBStatus CtiTableDeviceMeterGroup::Update()
 {
     char temp[32];
 
-
-
     CtiLockGuard<CtiSemaphore> cg(gDBAccessSema);
     RWDBConnection conn = getConnection();
 
@@ -217,11 +156,7 @@ RWDBStatus CtiTableDeviceMeterGroup::Update()
 
     updater.where( table["deviceid"] == getDeviceID() );
 
-    updater <<
-    table["collectiongroup"].assign(getCollectionGroup().c_str() ) <<
-    table["testcollectiongroup"].assign(getTestCollectionGroup().c_str() ) <<
-    table["meternumber"].assign(getMeterNumber().c_str() ) <<
-    table["billinggroup"].assign(getBillingGroup().c_str() );
+    updater << table["meternumber"].assign(getMeterNumber().c_str() );
 
     if( ExecuteUpdater(conn,updater,__FILE__,__LINE__) == RWDBStatus::ok )
     {
@@ -233,8 +168,6 @@ RWDBStatus CtiTableDeviceMeterGroup::Update()
 
 RWDBStatus CtiTableDeviceMeterGroup::Delete()
 {
-
-
     CtiLockGuard<CtiSemaphore> cg(gDBAccessSema);
     RWDBConnection conn = getConnection();
 
