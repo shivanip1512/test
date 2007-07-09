ea43b64f4e697e07b3ccb92e7082df2a9d5c22d1
Mack, Tom <>
2007-07-09 16:53:17 +0000

    YUK-3823: Develop device grouping system (AMR collection groups)
    Tweaked a bit, added placeholder for David's script.

--- yukon-database/DBChangeSubmission/SQLSubmit.sql
+++ yukon-database/DBChangeSubmission/SQLSubmit.sql
@@ -7,6 +7,8 @@ insert into DeviceGroup values (9,'Inventory',8,'Y','STATIC');
 insert into DeviceGroup values (10,'DisconnectedStatus',8,'Y','STATIC');
 insert into DeviceGroup values (11,'UsageMonitoring',8,'Y','STATIC');
 
+/*David, but your prefix stripping script here */
+
 insert into DeviceGroup select distinct 100, CollectionGroup, 3, 'N', 'STATIC' from DeviceMeterGroup;
 insert into DeviceGroup select distinct 100, TestCollectionGroup, 4, 'N', 'STATIC' from DeviceMeterGroup;
 insert into DeviceGroup select distinct 100, BillingGroup, 2, 'N', 'STATIC' from DeviceMeterGroup;
