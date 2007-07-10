5db8423def69b9a3775392237b664768062f9263
Koponen, Jason <>
2007-07-10 18:36:41 +0000

    YUK-3814
    Added success list and point values to email.

--- yukon-client/common/src/com/cannontech/common/device/commands/impl/GroupCommandExecutorImpl.java
+++ yukon-client/common/src/com/cannontech/common/device/commands/impl/GroupCommandExecutorImpl.java
@@ -22,6 +22,8 @@ import com.cannontech.common.device.commands.CommandRequestExecutor;
 import com.cannontech.common.device.commands.GroupCommandExecutor;
 import com.cannontech.common.util.ScheduledExecutor;
 import com.cannontech.core.authorization.exception.PaoAuthorizationException;
+import com.cannontech.core.dynamic.PointValueHolder;
+import com.cannontech.core.service.PointFormattingService;
 import com.cannontech.database.data.lite.LiteYukonUser;
 import com.cannontech.tools.email.DefaultEmailMessage;
 import com.cannontech.tools.email.EmailService;
@@ -32,6 +34,7 @@ public class GroupCommandExecutorImpl implements GroupCommandExecutor {
     private CommandRequestExecutor commandRequestExecutor = null;
     private MeterDao meterDao = null;
     private ScheduledExecutor executor = null;
+    private PointFormattingService pointFormattingService = null;
     private Logger logger = YukonLogManager.getLogger(GroupCommandExecutorImpl.class);
     
     private static final SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
@@ -51,6 +54,10 @@ public class GroupCommandExecutorImpl implements GroupCommandExecutor {
     public void setExecutor(ScheduledExecutor executor) {
         this.executor = executor;
     }
+    
+    public void setPointFormattingService(PointFormattingService pointFormattingService) {
+        this.pointFormattingService = pointFormattingService;
+    }
 
     public void execute(final Set<Integer> deviceIds, final String command, 
                         final String emailAddresses, LiteYukonUser user)
@@ -60,6 +67,7 @@ public class GroupCommandExecutorImpl implements GroupCommandExecutor {
         
         final StringBuffer resultBuffer = new StringBuffer();
         final List<Integer> missedList = new ArrayList<Integer>();
+        final StringBuffer successBuffer = new StringBuffer();
 
         // Make a list of all of the device ids to execute command for
         final Set<Integer> pendingIdList = new HashSet<Integer>();
@@ -74,19 +82,19 @@ public class GroupCommandExecutorImpl implements GroupCommandExecutor {
             CollectingCommandCompletionCallback collectingCommandCompletionCallback = new CollectingCommandCompletionCallback() {
 
                 public void complete() {
-
+                    
                     synchronized (resultBuffer) {
 
                         Meter device = meterDao.getForId(id);
                         String formattedDeviceName = meterDao.getFormattedDeviceName(device);
                         resultBuffer.append("\n\n" + format.format(new Date()) + 
-                                            " - " + formattedDeviceName + "\n\n");
+                                            " - " + formattedDeviceName + "\n");
 
 
                         // Add any errors to the result string buffer
                         List<DeviceErrorDescription> errors = getErrors();
                         for (DeviceErrorDescription ded : errors) {
-                            resultBuffer.append(ded.toString() + "\n");
+                            resultBuffer.append("  " + ded.toString() + "\n");
 
                         }
 
@@ -95,10 +103,42 @@ public class GroupCommandExecutorImpl implements GroupCommandExecutor {
                             missedList.add(id);
                         }
 
+                        // Add any successful readings to the success buffer
+                        List<PointValueHolder> values = getValues();
+                        if(values.size() > 0){
+                            successBuffer.append("  " + formattedDeviceName + 
+                                                 " (" + device.getMeterNumber() + ") - ");
+                            boolean first = true;
+                            for(PointValueHolder value : values){
+                                
+                                String valueString = 
+                                    pointFormattingService.getValueString(value, 
+                                                                          PointFormattingService.Format.FULL);
+
+                                if(!first){
+                                    successBuffer.append("; ");
+                                }
+                                successBuffer.append(valueString);
+                                
+                                first = false;
+                            }
+                            successBuffer.append("\n");
+                        }
+                        
+                        // Add devices with no errors and no point data to the success list -
+                        // means that the reading was successful but no point exists to 
+                        // archive the data for
+                        if(errors.size() == 0 && values.size() == 0){
+                            successBuffer.append("  " + formattedDeviceName + 
+                                                 " (" + device.getMeterNumber() + 
+                                                 ") - No data archived. See log below.");
+                            successBuffer.append("\n");
+                        }
+
                         // Add any results strings to the result string buffer
                         List<String> resultStrings = getResultStrings();
                         for (String result : resultStrings) {
-                            resultBuffer.append(result + "\n");
+                            resultBuffer.append("  " + result + "\n");
                         }
 
                         pendingIdList.remove(id);
@@ -106,6 +146,7 @@ public class GroupCommandExecutorImpl implements GroupCommandExecutor {
                         if (pendingIdList.size() == 0) {
                             sendResultEmail(command,
                                             resultBuffer.toString(),
+                                            successBuffer.toString(),
                                             missedList,
                                             emailAddresses,
                                             startTime,
@@ -124,7 +165,7 @@ public class GroupCommandExecutorImpl implements GroupCommandExecutor {
 
     }
 
-    private void sendResultEmail(final String command, final String results, 
+    private void sendResultEmail(final String command, final String results, final String success, 
             final List<Integer> missedList, final String emailAddresses, final Date startTime, 
             final int numberOfDevices) {
         
@@ -142,27 +183,34 @@ public class GroupCommandExecutorImpl implements GroupCommandExecutor {
                 
                 Date now = new Date();
                 body.append("Started group processing: " + format.format(startTime));
-                body.append("\n\n");
+                body.append("  ");
                 body.append("Finished group processing: " + format.format(now) + "\n\n");
 
                 body.append("Total number of devices: " + numberOfDevices + "\n");
                 body.append("Successful: " + (numberOfDevices - missedList.size()) + "\n");
-                body.append("Missed: " + missedList.size() + "\n\n\n");
+                body.append("Missed: " + missedList.size() + "\n\n");
 
-                body.append("Results:");
-                
-                // Add porter results to email body
-                body.append(results);
-        
-                // Add missed list to email body
+                // Success list to email body
+                body.append("Success:");
                 body.append("\n\n");
+                body.append(success);
+                body.append("\n\n");
+                
+                
+                // Add missed list to email body
                 body.append("Missed:");
                 body.append("\n\n");
                 for (Integer id : missedList) {
                     String formattedDeviceName = meterDao.getFormattedDeviceName(meterDao.getForId(id));
-                    body.append(formattedDeviceName + "\n");
+                    body.append("  " + formattedDeviceName + "\n");
                 }
                 body.append("\n\n");
+
+                // Add porter results to email body
+                body.append("Results:");
+                body.append(results);
+                body.append("\n\n");
+        
                 body.append("You have received this notification because your email address was " +
                         "added to the notification list of a Yukon group processing event. ");
         
