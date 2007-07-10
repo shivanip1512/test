1c59abb7cdea15c892f3a4bdf7cd63367fce204b
Fisher, Matthew S <MatthewSFisher@Eaton.com>
2007-07-10 22:08:36 +0000

    changed project identification to use new build labels (compileinfo_t)

---
+++ yukon-server/Fdr/include/id_fdr.h
@@ -0,0 +1,20 @@
+/*-----------------------------------------------------------------------------*
+*
+* File:   id_fdr
+*
+* Date:   7/5/2007
+*
+* PVCS KEYWORDS:
+* ARCHIVE      :  $Archive:   Z:/SOFTWAREARCHIVES/YUKON/RTDB/INCLUDE/id_pntdll.h-arc  $
+* REVISION     :  $Revision: 1.1 $
+* DATE         :  $Date: 2007/07/10 22:08:36 $
+*
+* Copyright (c) 2007 Cannon Technologies. All rights reserved.
+*-----------------------------------------------------------------------------*/
+#pragma warning( disable : 4786)
+
+#include "utility.h"
+
+PROJECT_ID("CTI Foreign Data Router");
+
+
