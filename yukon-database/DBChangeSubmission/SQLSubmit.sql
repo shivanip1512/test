8bf516fa1cab661d98afcc410d98c53557be3505
Dayton, Jon R <JonRDayton@Eaton.com>
2007-08-01 21:57:19 +0000

    YUK-4109 An initial db entry in the YukonGroupRole table for the Yukon group for role properties -1019 and -1020 was missing...

--- yukon-database/DBChangeSubmission/SQLSubmit.sql
+++ yukon-database/DBChangeSubmission/SQLSubmit.sql
@@ -1,3 +1,6 @@
 /*Please be kind and keep things cleanly organized.  Add new entries to the end and use /**********************/
 /*to split your new additions from those already there.  Thanks!*/
 
+/*Fixes YUK-4109, needs to be in creation and update scripts for 3.5 and HEAD*/
+insert into YukonGroupRole values(-20,-1,-1,-1019,'(none)');
+insert into YukonGroupRole values(-21,-1,-1,-1020,'(none)');
