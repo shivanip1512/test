a0319190b113bf4c305523e2c1c05e86dcb404e3
Spar, Thain <>
2007-08-03 19:47:28 +0000

    YUK-4138
    DB updates

--- yukon-database/DBChangeSubmission/SQLSubmit.sql
+++ yukon-database/DBChangeSubmission/SQLSubmit.sql
@@ -4,3 +4,11 @@
 /*Fixes YUK-4109, needs to be in creation and update scripts for 3.5 and HEAD*/
 insert into YukonGroupRole values(-20,-1,-1,-1019,'(none)');
 insert into YukonGroupRole values(-21,-1,-1,-1020,'(none)');
+
+/* Needed in 3.4.6, 3.5, and Head*/
+insert into yukonroleproperty values (-100011,-1000, 'Daily/Max Operation Count', 'true', 'is Daily/Max Operation stat displayed');
+insert into yukonroleproperty values (-100012,-1000, 'Substation Last Update Timestamp', 'true', 'is last update timstamp shown for substations');
+insert into yukonroleproperty values (-100106,-1001, 'Feeder Last Update Timestamp', 'true', 'is last update timstamp shown for feeders');
+insert into yukonroleproperty values (-100203,-1002, 'CapBank Last Update Timestamp', 'true', 'is last update timstamp shown for capbanks');
+update yukonroleproperty set DefaultValue = 'false' where rolepropertyid = -100008;
+update yukonroleproperty set DefaultValue = 'false' where rolepropertyid = -100007;
