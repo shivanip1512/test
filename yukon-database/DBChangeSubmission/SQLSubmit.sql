b8c1ecefc133618578da41310dce15e7520564b2
Spar, Thain <>
2007-08-13 19:34:52 +0000

    YUK-4118
    Constraint prevented schedule from being deleted.
    
    run this sql command on databases to fix.
    ALTER TABLE DeviceReadJobLog DROP CONSTRAINT FK_DEVICERE_FK_DRJOBL_MACSCHED

--- yukon-database/DBChangeSubmission/SQLSubmit.sql
+++ yukon-database/DBChangeSubmission/SQLSubmit.sql
@@ -5,7 +5,7 @@
 insert into YukonGroupRole values(-20,-1,-1,-1019,'(none)');
 insert into YukonGroupRole values(-21,-1,-1,-1020,'(none)');
 
-/* Needed in 3.4.6, 3.5, and Head*/
+/* Needed in 3.4.6, 3.5, and Head  TS*/
 insert into yukonroleproperty values (-100011,-1000, 'Daily/Max Operation Count', 'true', 'is Daily/Max Operation stat displayed');
 insert into yukonroleproperty values (-100012,-1000, 'Substation Last Update Timestamp', 'true', 'is last update timstamp shown for substations');
 insert into yukonroleproperty values (-100106,-1001, 'Feeder Last Update Timestamp', 'true', 'is last update timstamp shown for feeders');
@@ -23,3 +23,9 @@ insert into YukonRole values(-212,'Scheduler','Operator','Operator access to Sch
 /* Scheduler Role properties */
 insert into YukonRoleProperty values(-21200,-212,'Enable/Disable Schedule','true','Right to enable or disable a schedule'); 
 /**********************************/
+
+
+
+/* MACS YUK-4118 fix 8-13-2007 in  3.4.6  3.5 and head*/
+ALTER TABLE DeviceReadJobLog DROP CONSTRAINT FK_DEVICERE_FK_DRJOBL_MACSCHED
+/************/
