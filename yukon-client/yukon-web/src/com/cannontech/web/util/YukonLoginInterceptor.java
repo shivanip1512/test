b9badadb0090054f7cffb9bc4ead2c2512c1e30b
Mack, Tom <>
2007-08-16 15:06:54 +0000

    YUK-4249: Session timeout needs to redirect to login screen.
    Changed the YukonLoginInterceptor to redirect to login.jsp isntead of just throwing an exception. This probably isn't a perfect solution for some of these entries (e.g. chart-servlet.xml), but will do for now.

--- yukon-client/yukon-web/src/com/cannontech/web/util/YukonLoginInterceptor.java
+++ yukon-client/yukon-web/src/com/cannontech/web/util/YukonLoginInterceptor.java
@@ -1,17 +1,49 @@
 package com.cannontech.web.util;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-import com.cannontech.web.taglib.CheckLoginTag;
+import com.cannontech.common.constants.LoginController;
+import com.cannontech.database.data.lite.LiteYukonUser;
+import com.cannontech.util.ServletUtil;
 
 public class YukonLoginInterceptor extends  HandlerInterceptorAdapter {
 
     public boolean preHandle(HttpServletRequest request,
             HttpServletResponse response, Object handler) throws Exception {
-        return CheckLoginTag.checkLogin(request, response);
+        
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            LiteYukonUser yukonUser = ServletUtil.getYukonUser(session);
+            if (yukonUser != null) {
+                // we have a valid login
+                return true;
+            }
+        }
+        
+
+        
+        // do a redirect (copied from CheckLoginTag)
+        String redirectURL = "/login.jsp";
+        
+        // I wish I know what the following was all about!
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null) {       
+            for(int i = 0; i < cookies.length; i++) {
+                Cookie c = cookies[i];
+                if(c.getName().equalsIgnoreCase(LoginController.LOGIN_URL_COOKIE)) {
+                    redirectURL = c.getValue();
+                    break;
+                }
+            }
+        }
+
+        response.sendRedirect(redirectURL);
+        return false;
     }
 
 }
