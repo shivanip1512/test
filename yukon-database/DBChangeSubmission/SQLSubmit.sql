483df9ed6fabac30846a688ff6e240dadb1840ed
Dayton, Jon R <JonRDayton@Eaton.com>
2007-08-31 13:08:21 +0000

    YUK-4334 Not a clear solution to this yet but some steps outlined to deal with it.  Issue should not occur by default; only in certain situations.  Some db maintenance may be necessary.

--- yukon-database/DBChangeSubmission/SQLSubmit.sql
+++ yukon-database/DBChangeSubmission/SQLSubmit.sql
@@ -1,4 +1,17 @@
 /*Please be kind and keep things cleanly organized.  Add new entries to the end and use /**********************/
 /*to split your new additions from those already there.  Thanks!*/
 
+/*New issue, check YUK-4334 for reference */
+NEED TO CHECK TO SEE WHETHER THE USERPAOWNER TO USERPAOPERMISSION TRANSITION IN THE 3.4_0.sql file
+is working correctly to move the default route group mapping for STARS energy companies.
+
+If a default route is not loading correctly in STARS, then run the following query:
+insert into UserPaoPermission (UserPaoPermissionID, UserID, PaoID, Permission) select 0, ec.userID, macro.OwnerID, 'allow LM visibility'
+from EnergyCompany ec, LMGroupExpressCom exc, GenericMacro macro 
+where macro.MacroType = 'GROUP' and macro.ChildID = exc.LMGroupID and exc.SerialNumber = '0' and ec.EnergyCompanyID = 0;
+
+If an ID is returned, then the mapping failed and the default route is not correctly saved.
+
+There are several questions we need to answer on this issue, so this can stay open for awhile.  
+/*I'm marking this in here for reference until we figure out the best way to deal with this.  Jon*/
 
