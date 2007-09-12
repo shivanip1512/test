4d421e2e71b44fb7ffd5df56153febf6be84b97d
Meverden, Nick <>
2007-09-12 21:01:24 +0000

    YUK-4270 refacted.
    - Removed special workorder case from StarsActionController.
    - Added parent beans to STARS Spring context files. http://static.springframework.org/spring/docs/2.0.x/reference/beans.html#beans-child-bean-definitions

--- yukon-client/yukon-web/src/com/cannontech/web/stars/StarsActionController.java
+++ yukon-client/yukon-web/src/com/cannontech/web/stars/StarsActionController.java
@@ -2,7 +2,6 @@ package com.cannontech.web.stars;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
@@ -12,16 +11,11 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
 import com.cannontech.stars.util.ServletUtils;
-import com.cannontech.stars.util.WebClientException;
-import com.cannontech.stars.web.StarsYukonUser;
-import com.cannontech.web.navigation.CtiNavObject;
-import com.cannontech.web.stars.service.SwitchContextService;
 
 public class StarsActionController implements Controller,BeanFactoryAware {
     private BeanFactory beanFactory;
     private String prefix;
     private String defaultBeanName;
-    private SwitchContextService switchContextService;
     
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         final boolean isMultiPart = ServletUtils.isMultiPartRequest(request);
@@ -31,23 +25,6 @@ public class StarsActionController implements Controller,BeanFactoryAware {
         
         final String beanName = (action != null) ? prefix + action : prefix + defaultBeanName;
 
-        if (prefix.equalsIgnoreCase("workorder")) {
-            Integer memberID = ServletRequestUtils.getIntParameter(request, "SwitchContext");
-            if (memberID != null) {
-                HttpSession session = request.getSession(false);
-                try {
-                    final StarsYukonUser user = ServletUtils.getStarsYukonUser(request);
-                    switchContextService.switchContext(user, request, session, memberID);
-                } catch (WebClientException e) {
-                    session.setAttribute( ServletUtils.ATT_ERROR_MESSAGE, e.getMessage() );
-                    String referer = ServletRequestUtils.getStringParameter(request, ServletUtils.ATT_REFERRER);
-                    if (referer == null) referer = ((CtiNavObject) session.getAttribute(ServletUtils.NAVIGATE)).getPreviousPage();
-                    response.sendRedirect(referer);
-                    return null;
-                }
-            }
-        }
-        
         Controller actionController = (Controller) beanFactory.getBean(beanName, Controller.class);
         actionController.handleRequest(request, response);
         return null;
@@ -63,10 +40,6 @@ public class StarsActionController implements Controller,BeanFactoryAware {
     
     public void setDefaultBeanName(String defaultBeanName) {
         this.defaultBeanName = defaultBeanName;
-    }
-
-    public void setSwitchContextService(SwitchContextService switchContextService) {
-        this.switchContextService = switchContextService;
     }
     
 }
