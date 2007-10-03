1cd85aa5896d8f0a96c464c7975dc37b6251d885
Mack, Tom <>
2007-10-03 22:44:10 +0000

    This is a drop in replacement of Spring's JdbcTemplate that times the execution of each query be overriding the three base queries that JdbcTemplate uses.
    To activate, edit mainContext.xml. This should be safe to do on shipping code, but I'll hold off on that for now.

---
+++ yukon-client/common/src/com/cannontech/spring/LoggingJdbcTemplate.java
@@ -0,0 +1,70 @@
+package com.cannontech.spring;
+
+import java.text.DecimalFormat;
+
+import org.apache.log4j.Logger;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.CallableStatementCallback;
+import org.springframework.jdbc.core.CallableStatementCreator;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCallback;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.SqlProvider;
+import org.springframework.jdbc.core.StatementCallback;
+
+import com.cannontech.clientutils.YukonLogManager;
+
+public class LoggingJdbcTemplate extends JdbcTemplate {
+    private Logger log = YukonLogManager.getLogger(LoggingJdbcTemplate.class);
+    @Override
+    public Object execute(StatementCallback action) throws DataAccessException {
+        long startTime = System.nanoTime();
+        Object object = super.execute(action);
+        long totalTime = System.nanoTime() - startTime;
+        if (log.isDebugEnabled()) {
+            String time = formatNanoTime(totalTime);
+            log.debug("SQL SC Execution (" + time + "): " + getSql(action));
+        }
+        return object;
+    }
+
+    @Override
+    public Object execute(PreparedStatementCreator psc, PreparedStatementCallback action) throws DataAccessException {
+        long startTime = System.nanoTime();
+        Object object = super.execute(psc,action);
+        long totalTime = System.nanoTime() - startTime;
+        if (log.isDebugEnabled()) {    
+            String time = formatNanoTime(totalTime);
+            log.debug("SQL PSC Execution (" + time + "): " + getSql(psc));
+        }
+        return object;
+    }
+
+    private String formatNanoTime(long totalTime) {
+        String time = DecimalFormat.getNumberInstance().format((totalTime / 1000000f));
+        return time + "ms";
+    }
+
+    @Override
+    public Object execute(CallableStatementCreator csc, CallableStatementCallback action) throws DataAccessException {
+        long startTime = System.nanoTime();
+        Object object = super.execute(csc,action);
+        long totalTime = System.nanoTime() - startTime;
+        if (log.isDebugEnabled()) {
+            String time = formatNanoTime(totalTime);
+            log.debug("SQL CSC Execution (" + time + "): " + getSql(csc));
+        }
+        return object;
+    }
+
+    private String getSql(Object o) {
+        if (o instanceof SqlProvider) {
+            SqlProvider sqlProvider = (SqlProvider) o;
+            String sql = sqlProvider.getSql();
+            sql = sql.replaceAll("\\s+", " ");
+            return sql;
+        } else {
+            return "unknown sql";
+        }
+    }
+}
