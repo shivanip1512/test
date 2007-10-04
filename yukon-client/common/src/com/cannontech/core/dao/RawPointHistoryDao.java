d8d45c0e673dd1b41517f8369a1b8cc49eadb573
Mack, Tom <>
2007-10-04 15:58:44 +0000

    YUK-4460: Meter Read Widget Loads slow
    Adding some JavaDoc for the new method.

--- yukon-client/common/src/com/cannontech/core/dao/RawPointHistoryDao.java
+++ yukon-client/common/src/com/cannontech/core/dao/RawPointHistoryDao.java
@@ -23,6 +23,24 @@ public interface RawPointHistoryDao {
      * @return List of values for the point
      */
     public List<PointValueHolder> getPointData(int pointId, Date startDate, Date stopDate);
+    
+
+    /**
+     * This method gets values from raw point history similarly to getPointData. But, it
+     * will not return all of the data between startDate and stopDate. Instead it will
+     * return at most one value per ChartInterval. The method for determining which value
+     * will be used is controlled by the Mode parameter.
+     * 
+     *   Mode.HIGHEST - The point data with the largest value for the period will be returned
+     *   Mode.LAST - The point data with the largest timestamp for the period will be returned
+     *   
+     * @param pointId
+     * @param startDate
+     * @param stopDate
+     * @param resolution
+     * @param mode
+     * @return
+     */
     public List<PointValueHolder> getIntervalPointData(int pointId, Date startDate, Date stopDate, ChartInterval resolution, Mode mode);
 
 }
