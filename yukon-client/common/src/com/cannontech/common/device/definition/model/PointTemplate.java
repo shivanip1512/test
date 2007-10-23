6c5ffd4604c75a1de368bcd35f80a2b97331cafd
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2007-10-23 19:46:40 +0000

    YUK-4327 The hashCode and equals additions cause inconsitent/incorrect results when using sets of PointTemplates and DevicePointIdentifier.  The isComparableTo method allows us to compare PointTemplates with DevicePointIdentifiers while the compareTo method can compare PointTemplates with other PointTemplates correctly.

--- yukon-client/common/src/com/cannontech/common/device/definition/model/PointTemplate.java
+++ yukon-client/common/src/com/cannontech/common/device/definition/model/PointTemplate.java
@@ -85,31 +85,6 @@ public class PointTemplate extends DevicePointIdentifier implements Comparable<P
 
     }
 
-	@Override
-	public int hashCode() {
-		final int PRIME = 31;
-		int result = super.hashCode();
-		result = PRIME * result + ((name == null) ? 0 : name.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		final PointTemplate other = (PointTemplate) obj;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		return true;
-	}
-
 	public boolean isComparableTo(DevicePointIdentifier identifier) {
     	
     	return super.isComparableTo(identifier);
