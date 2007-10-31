714be98ede678c4e42e744c9d3a315d83e705453
Solberg, Aaron <>
2007-10-31 16:12:55 +0000

    YUK-4385  	 capcontrol report ehnancements
    * altered capcontrol views to include substations

--- yukon-database/DBChangeSubmission/SQLSubmit.sql
+++ yukon-database/DBChangeSubmission/SQLSubmit.sql
@@ -93,6 +93,80 @@ insert into YukonRoleProperty values(-70016,-700,'Definition Closed','Close,Clos
 update YukonListEntry set EntryText = 'Postal Code' where EntryText = 'Zip Code' and ListID = 1056
 /* end YUK-4597*/
 
+/*HEAD   START:NEW CCOPERATIONS_VIEW*/
+
+SELECT 
+      yp3.PAOName AS cbcName, yp.PAOName AS capbankname, el.DateTime AS opTime, el.Text AS operation, 
+      el2.DateTime AS confTime, el2.Text AS confStatus, yp1.PAOName AS feederName, yp1.PAObjectID AS feederId, 
+        yp2.PAOName AS subName, yp2.PAObjectID AS subBusId, ssl.substationid AS substationid, yp4.paoname AS region, cb.BANKSIZE, 
+        cb.ControllerType AS protocol, p.Value AS ipAddress, cbc.SERIALNUMBER AS serialNum, da.SlaveAddress, 
+        el2.kvarAfter, el2.kvarChange, el2.kvarBefore
+FROM   
+      (SELECT op.LogID AS oid, MIN(aaa.confid) AS cid FROM
+              (SELECT LogID, PointID FROM CCEventLog 
+        WHERE Text LIKE '%Close sent%' OR Text LIKE '%Open sent%') op
+        LEFT OUTER JOIN 
+        (SELECT el.LogID AS opid, MIN(el2.LogID) AS confid 
+        FROM CCEventLog el INNER JOIN CCEventLog el2 ON el2.PointID = el.PointID AND el.LogID < el2.LogID 
+        LEFT OUTER JOIN
+        (SELECT a.LogID AS aid, MIN(b.LogID) AS next_aid FROM 
+        CCEventLog a INNER JOIN CCEventLog b ON a.PointID = b.PointID AND a.LogID < b.LogID 
+        WHERE (a.Text LIKE '%Close sent,%' OR a.Text LIKE '%Open sent,%') 
+        AND (b.Text LIKE '%Close sent,%' OR b.Text LIKE '%Open sent,%')
+        GROUP BY a.LogID) el3 ON el3.aid = el.LogID 
+      WHERE (el.Text LIKE '%Close sent,%' OR el.Text LIKE '%Open sent,%') 
+        AND (el2.Text LIKE 'Var: %') AND (el2.LogID < el3.next_aid) 
+      OR (el.Text LIKE '%Close sent,%' OR el.Text LIKE '%Open sent,%') 
+        AND (el2.Text LIKE 'Var: %') AND (el3.next_aid IS NULL)
+        GROUP BY el.LogID)  aaa ON op.LogID = aaa.opid
+GROUP BY op.LogID) OpConf INNER JOIN
+        CCEventLog el ON el.LogID = OpConf.oid LEFT OUTER JOIN
+        CCEventLog el2 ON el2.LogID = OpConf.cid INNER JOIN
+        POINT ON POINT.POINTID = el.PointID INNER JOIN
+        DynamicCCCapBank ON DynamicCCCapBank.CapBankID = POINT.PAObjectID INNER JOIN
+        YukonPAObject yp ON yp.PAObjectID = DynamicCCCapBank.CapBankID INNER JOIN
+        YukonPAObject yp1 ON yp1.PAObjectID = el.FeederID INNER JOIN
+        YukonPAObject yp2 ON yp2.PAObjectID = el.SubID INNER JOIN
+        CAPBANK cb ON cb.DEVICEID = DynamicCCCapBank.CapBankID LEFT OUTER JOIN
+        DeviceDirectCommSettings ddcs ON ddcs.DEVICEID = cb.CONTROLDEVICEID LEFT OUTER JOIN
+        DeviceAddress da ON da.DeviceID = cb.CONTROLDEVICEID INNER JOIN
+        YukonPAObject yp3 ON yp3.PAObjectID = cb.CONTROLDEVICEID LEFT OUTER JOIN
+        DeviceCBC cbc ON cbc.DEVICEID = cb.CONTROLDEVICEID LEFT OUTER JOIN
+        (SELECT EntryID, PAObjectID, Owner, InfoKey, Value, UpdateTime
+        FROM DynamicPAOInfo WHERE (InfoKey LIKE '%udp ip%')) 
+        p ON p.PAObjectID = cb.CONTROLDEVICEID LEFT OUTER JOIN
+        ccsubstationsubbuslist as ssl on ssl.substationbusid = el.subid  LEFT OUTER JOIN
+        ccsubareaassignment as csa on csa.substationbusid = ssl.substationid left outer join 
+        YukonPAObject AS yp4 ON yp4.paobjectid = csa.areaid;
+/*HEAD   END:NEW CCOPERATIONS_VIEW*/
+
+/*HEAD   START:NEW CCINVENTORY_VIEW*/
+
+SELECT yp4.paoname AS Region, yp3.PAOName AS SubName, yp2.PAOName AS FeederName, yp3.PAObjectID AS subId, ssl.substationid AS substationid, yp2.PAObjectID AS fdrId, 
+                      yp.PAOName AS CBCName, yp.PAObjectID AS cbcId, yp1.PAOName AS capBankName, yp1.PAObjectID AS bankId, cb.BANKSIZE AS CapBankSize, 
+                      fb.ControlOrder AS Sequence, dcb.ControlStatus, cb.SwitchManufacture AS SWMfgr, cb.TypeOfSwitch AS SWType, 
+                      cb.OPERATIONALSTATE AS ControlType, cb.ControllerType AS Protocol, pts.IPADDRESS, da.SlaveAddress, 
+                                capa.latitude AS LAT, capa.longitude AS LON, capa.drivedirections AS DriveDirection, 
+                                cb.maplocationid AS OpCenter, capa.maintenanceareaid AS TA
+FROM CAPBANK cb INNER JOIN
+                      YukonPAObject yp ON yp.PAObjectID = cb.CONTROLDEVICEID INNER JOIN
+                      YukonPAObject yp1 ON yp1.PAObjectID = cb.DEVICEID INNER JOIN
+                      DynamicCCCapBank dcb ON dcb.CapBankID = yp1.PAObjectID INNER JOIN
+                      STATE s ON s.STATEGROUPID = 3 AND dcb.ControlStatus = s.RAWSTATE INNER JOIN
+                      CCFeederBankList fb ON fb.DeviceID = cb.DEVICEID INNER JOIN
+                      YukonPAObject yp2 ON yp2.PAObjectID = fb.FeederID INNER JOIN
+                      CCFeederSubAssignment sf ON fb.FeederID = sf.FeederID INNER JOIN
+                      YukonPAObject yp3 ON yp3.PAObjectID = sf.SubStationBusID INNER JOIN
+                      ccsubstationsubbuslist ssl on ssl.substationbusid = yp3.paobjectid INNER JOIN
+                      ccsubareaassignment sa on sa.substationbusid = ssl.substationid INNER JOIN
+                      yukonpaobject yp4 on yp4.paobjectid = sa.areaid INNER JOIN
+                      DeviceDirectCommSettings ddcs ON ddcs.DEVICEID = cb.CONTROLDEVICEID INNER JOIN
+                      DeviceAddress da ON da.DeviceID = cb.CONTROLDEVICEID INNER JOIN
+                      PORTTERMINALSERVER pts ON pts.PORTID = ddcs.PORTID INNER JOIN
+                      DeviceCBC cbc ON cbc.DEVICEID = cb.CONTROLDEVICEID INNER JOIN
+                      capbankadditional capa on capa.deviceid = cb.deviceid;
+/*HEAD END: NEW CCINVENTORY_VIEW*/
+
 /* Head changes  Thain*/
 insert into YukonRoleProperty VALUES ( -100204, -1002, 'Daily/Total Operation Count', 'true', 'is Daily/Total Operation Count displayed');
 /*end*/
