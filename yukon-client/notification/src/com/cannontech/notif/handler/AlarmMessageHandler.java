f6fa77c0dd73389899c35a22bd6fab274cfbb8fe
Peterson, Michael <>
2007-11-06 20:10:23 +0000

    YUK-4505 Alarm notifications are not formatted with a timezone in the same way that other notifications are

--- yukon-client/notification/src/com/cannontech/notif/handler/AlarmMessageHandler.java
+++ yukon-client/notification/src/com/cannontech/notif/handler/AlarmMessageHandler.java
@@ -2,6 +2,7 @@ package com.cannontech.notif.handler;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 
 import org.apache.commons.lang.BooleanUtils;
 import org.apache.log4j.Logger;
@@ -42,56 +43,65 @@ public class AlarmMessageHandler extends NotifHandler {
         return true;
     }
 
-    private NotificationBuilder createNotificationBuilder(NotifAlarmMsg msg, 
-                                                          LiteNotificationGroup liteNotifGroup) {
-        final Notification notif = new Notification("alarm");
+    private NotificationBuilder createNotificationBuilder(NotifAlarmMsg _msg, 
+                                                          LiteNotificationGroup _liteNotifGroup) {
         
-        notif.addData("notificationgroup", liteNotifGroup.getNotificationGroupName());
-        
-        LitePoint point = (LitePoint)DaoFactory.getPointDao().getLitePoint(msg.pointId);
-        notif.addData("pointid", Integer.toString(point.getPointID()));
-        notif.addData("pointname", point.getPointName());
-        notif.addData("rawvalue", Double.toString(msg.value));
-        notif.addData("pointtype", PointTypes.getType(point.getPointType()));
-        int pAObjectId = point.getPaobjectID();
-        PaoDao paoDao = DaoFactory.getPaoDao();
-        LiteYukonPAObject liteYukonPAO = paoDao.getLiteYukonPAO(pAObjectId);
-        notif.addData("paoname", liteYukonPAO.getPaoName());
-        notif.addData("paodescription", liteYukonPAO.getPaoDescription());
-        
-        notif.addData("abnormal", BooleanUtils.toStringTrueFalse(msg.abnormal));
-        notif.addData("acknowledged", BooleanUtils.toStringTrueFalse(msg.acknowledged));
-        
-        
-        String conditionText = AlarmUtils.getAlarmConditionText(msg.condition, point);
-        notif.addData("condition", conditionText);
-        
-        String categoryText = DaoFactory.getAlarmCatDao().getAlarmCategoryName(msg.alarmCategoryId);
-        notif.addData("category", categoryText);
-        
-        DateFormat dateFormatter = new SimpleDateFormat("EEEE, MMMM d"); // e.g. "Tuesday, May 31"
-        DateFormat timeFormatter = new SimpleDateFormat("h:mm a"); // e.g. "3:45 PM"
-        
-        notif.addData("alarmtime", timeFormatter.format(msg.alarmTimestamp));
-        notif.addData("alarmdate", dateFormatter.format(msg.alarmTimestamp));
-        
-        String uofm = "";
-        if (point.getPointType() == PointTypes.STATUS_POINT) {
-            // handle as status
-            LiteState liteState = DaoFactory.getStateDao().getLiteState(point.getStateGroupID(), (int)msg.value);
-            notif.addData("value", liteState.getStateText());
-        } else {
-            // handle as analog
-            notif.addData("value", Double.toString(msg.value));
-            LiteUnitMeasure liteUnitMeasureByPointID = DaoFactory.getUnitMeasureDao().getLiteUnitMeasureByPointID(msg.pointId);
-            if (liteUnitMeasureByPointID != null) {
-                uofm = liteUnitMeasureByPointID.getUnitMeasureName();
-            }
-        }
-        notif.addData("unitofmeasure", uofm);
+        final NotifAlarmMsg msg = _msg;
+        final LiteNotificationGroup liteNotifGroup = _liteNotifGroup;
         
         NotificationBuilder notifFormatter = new NotificationBuilder() {
             public Notification buildNotification(Contactable contact) {
+                
+                Notification notif = new Notification("alarm");
+                
+                notif.addData("notificationgroup", liteNotifGroup.getNotificationGroupName());
+                
+                LitePoint point = (LitePoint)DaoFactory.getPointDao().getLitePoint(msg.pointId);
+                notif.addData("pointid", Integer.toString(point.getPointID()));
+                notif.addData("pointname", point.getPointName());
+                notif.addData("rawvalue", Double.toString(msg.value));
+                notif.addData("pointtype", PointTypes.getType(point.getPointType()));
+                int pAObjectId = point.getPaobjectID();
+                PaoDao paoDao = DaoFactory.getPaoDao();
+                LiteYukonPAObject liteYukonPAO = paoDao.getLiteYukonPAO(pAObjectId);
+                notif.addData("paoname", liteYukonPAO.getPaoName());
+                notif.addData("paodescription", liteYukonPAO.getPaoDescription());
+                
+                notif.addData("abnormal", BooleanUtils.toStringTrueFalse(msg.abnormal));
+                notif.addData("acknowledged", BooleanUtils.toStringTrueFalse(msg.acknowledged));
+                
+                
+                String conditionText = AlarmUtils.getAlarmConditionText(msg.condition, point);
+                notif.addData("condition", conditionText);
+                
+                String categoryText = DaoFactory.getAlarmCatDao().getAlarmCategoryName(msg.alarmCategoryId);
+                notif.addData("category", categoryText);
+                
+                DateFormat dateFormatter = new SimpleDateFormat("EEEE, MMMM d"); // e.g. "Tuesday, May 31"
+                DateFormat timeFormatter = new SimpleDateFormat("h:mm:ss a z"); // e.g. "3:45:13 CDT"
+                
+                TimeZone timeZone = contact.getTimeZone();
+                timeFormatter.setTimeZone(timeZone);
+                dateFormatter.setTimeZone(timeZone);
+                
+                notif.addData("alarmtime", timeFormatter.format(msg.alarmTimestamp));
+                notif.addData("alarmdate", dateFormatter.format(msg.alarmTimestamp));
+                
+                String uofm = "";
+                if (point.getPointType() == PointTypes.STATUS_POINT) {
+                    // handle as status
+                    LiteState liteState = DaoFactory.getStateDao().getLiteState(point.getStateGroupID(), (int)msg.value);
+                    notif.addData("value", liteState.getStateText());
+                } else {
+                    // handle as analog
+                    notif.addData("value", Double.toString(msg.value));
+                    LiteUnitMeasure liteUnitMeasureByPointID = DaoFactory.getUnitMeasureDao().getLiteUnitMeasureByPointID(msg.pointId);
+                    if (liteUnitMeasureByPointID != null) {
+                        uofm = liteUnitMeasureByPointID.getUnitMeasureName();
+                    }
+                }
+                notif.addData("unitofmeasure", uofm);
+                
                 return notif;
             }
             public void notificationComplete(Contactable contactable, NotifType notifType, boolean success) {
