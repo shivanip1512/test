3905d512f4fdc36fac877f3aa2f437078e89394b
Spar, Thain <>
2007-11-12 20:31:20 +0000

    YUK-4381 Provide alarm/anomaly screen to show cap banks with daily max operations...
    Change undone, redone

--- yukon-client/yukon-web/WebContent/capcontrol/js/cbc_funcs.js
+++ yukon-client/yukon-web/WebContent/capcontrol/js/cbc_funcs.js
@@ -864,7 +864,7 @@ function pause(numberMillis) {
     	for (var i=0; i < rows.length; i++) {
             var row = rows[i];
             var cells = row.getElementsByTagName('td');
-            var fdr = cells[7];
+            var fdr = cells[8];
             var spans = fdr.getElementsByTagName('span');
 	        var fdrName = new String (spans[0].innerHTML);
     		var index = feederNames.indexOf(trim(fdrName));
