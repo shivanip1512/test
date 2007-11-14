d9e8b1366657ed66be1703aca77469614f9a70fb
Duffy, David <>
2007-11-14 20:12:51 +0000

    YUK-4385 I mistakenly put 4.0 versions of CCOperations views in 3.5.

--- yukon-database/DBUpdates/sqlserver/3.5_0.sql
+++ yukon-database/DBUpdates/sqlserver/3.5_0.sql
@@ -29,17 +29,18 @@ insert into yukonlistentry values (138, 100, 0, 'Float From 16bit', 0);
 update point set pointname = 'MultiSpeak' where pointid = -110;
 update YukonRole set RoleName = 'MultiSpeak', RoleDescription='MultiSpeak web services interface.' where RoleID = -7;
 update YukonRoleProperty set KeyName = 'MultiSpeak Setup', Description='Controls access to configure the Multispeak Interfaces.' where RolePropertyID = -20011;
+/* @error ignore-end */
 
 drop view ccinventory_view;
 go
 
 create view CCINVENTORY_VIEW as
-SELECT yp4.paoname AS Region, yp3.PAOName AS SubName, yp2.PAOName AS FeederName, yp3.PAObjectID AS subId, ssl.substationid AS substationid, yp2.PAObjectID AS fdrId, 
-                      yp.PAOName AS CBCName, yp.PAObjectID AS cbcId, yp1.PAOName AS capBankName, yp1.PAObjectID AS bankId, cb.BANKSIZE AS CapBankSize, 
+SELECT yp4.paoname AS Region, yp3.PAOName AS SubName, yp2.PAOName AS FeederName, yp3.PAObjectID AS subId, yp2.PAObjectID AS fdrId, 
+                      yp.PAOName AS CBCName, yp.PAObjectID AS cbcId, yp1.PAOName AS capbankname, yp1.PAObjectID AS bankId, cb.BANKSIZE AS CapBankSize, 
                       fb.ControlOrder AS Sequence, dcb.ControlStatus, cb.SwitchManufacture AS SWMfgr, cb.TypeOfSwitch AS SWType, 
                       cb.OPERATIONALSTATE AS ControlType, cb.ControllerType AS Protocol, pts.IPADDRESS, da.SlaveAddress, 
-                                capa.latitude AS LAT, capa.longitude AS LON, capa.drivedirections AS DriveDirection, 
-                                cb.maplocationid AS OpCenter, capa.maintenanceareaid AS TA
+capa.latitude AS LAT, capa.longitude AS LON, capa.drivedirections AS DriveDirection, 
+cb.maplocationid AS OpCenter, capa.maintenanceareaid AS TA
 FROM CAPBANK cb INNER JOIN
                       YukonPAObject yp ON yp.PAObjectID = cb.CONTROLDEVICEID INNER JOIN
                       YukonPAObject yp1 ON yp1.PAObjectID = cb.DEVICEID INNER JOIN
@@ -49,14 +50,13 @@ FROM CAPBANK cb INNER JOIN
                       YukonPAObject yp2 ON yp2.PAObjectID = fb.FeederID INNER JOIN
                       CCFeederSubAssignment sf ON fb.FeederID = sf.FeederID INNER JOIN
                       YukonPAObject yp3 ON yp3.PAObjectID = sf.SubStationBusID INNER JOIN
-                      ccsubstationsubbuslist ssl on ssl.substationbusid = yp3.paobjectid INNER JOIN
-                      ccsubareaassignment sa on sa.substationbusid = ssl.substationid INNER JOIN
+                      ccsubareaassignment sa on sa.substationbusid = yp3.paobjectid INNER JOIN
                       yukonpaobject yp4 on yp4.paobjectid = sa.areaid INNER JOIN
                       DeviceDirectCommSettings ddcs ON ddcs.DEVICEID = cb.CONTROLDEVICEID INNER JOIN
                       DeviceAddress da ON da.DeviceID = cb.CONTROLDEVICEID INNER JOIN
                       PORTTERMINALSERVER pts ON pts.PORTID = ddcs.PORTID INNER JOIN
                       DeviceCBC cbc ON cbc.DEVICEID = cb.CONTROLDEVICEID INNER JOIN
-                      capbankadditional capa on capa.deviceid = cb.deviceid;
+                      capbankadditional capa on capa.deviceid = cb.deviceid
 go
 
 drop view ccoperations_view;
@@ -64,14 +64,14 @@ go
 
 create view CCOPERATIONS_VIEW as
 SELECT 
-      yp3.PAOName AS cbcName, yp.PAOName AS capbankname, el.DateTime AS opTime, el.Text AS operation, 
-      el2.DateTime AS confTime, el2.Text AS confStatus, yp1.PAOName AS feederName, yp1.PAObjectID AS feederId, 
-        yp2.PAOName AS subName, yp2.PAObjectID AS subBusId, ssl.substationid AS substationid, yp4.paoname AS region, cb.BANKSIZE, 
+	yp3.PAOName AS cbcName, yp.PAOName AS capbankname, el.DateTime AS opTime, el.Text AS operation, 
+	el2.DateTime AS confTime, el2.Text AS confStatus, yp1.PAOName AS feederName, yp1.PAObjectID AS feederId, 
+        yp2.PAOName AS subName, yp2.PAObjectID AS subBusId, yp4.paoname AS region, cb.BANKSIZE, 
         cb.ControllerType AS protocol, p.Value AS ipAddress, cbc.SERIALNUMBER AS serialNum, da.SlaveAddress, 
         el2.kvarAfter, el2.kvarChange, el2.kvarBefore
 FROM   
-      (SELECT op.LogID AS oid, MIN(aaa.confid) AS cid FROM
-              (SELECT LogID, PointID FROM CCEventLog 
+	(SELECT op.LogID AS oid, MIN(aaa.confid) AS cid FROM
+	        (SELECT LogID, PointID FROM CCEventLog 
         WHERE Text LIKE '%Close sent%' OR Text LIKE '%Open sent%') op
         LEFT OUTER JOIN 
         (SELECT el.LogID AS opid, MIN(el2.LogID) AS confid 
@@ -82,9 +82,9 @@ FROM
         WHERE (a.Text LIKE '%Close sent,%' OR a.Text LIKE '%Open sent,%') 
         AND (b.Text LIKE '%Close sent,%' OR b.Text LIKE '%Open sent,%')
         GROUP BY a.LogID) el3 ON el3.aid = el.LogID 
-      WHERE (el.Text LIKE '%Close sent,%' OR el.Text LIKE '%Open sent,%') 
+	WHERE (el.Text LIKE '%Close sent,%' OR el.Text LIKE '%Open sent,%') 
         AND (el2.Text LIKE 'Var: %') AND (el2.LogID < el3.next_aid) 
-      OR (el.Text LIKE '%Close sent,%' OR el.Text LIKE '%Open sent,%') 
+	OR (el.Text LIKE '%Close sent,%' OR el.Text LIKE '%Open sent,%') 
         AND (el2.Text LIKE 'Var: %') AND (el3.next_aid IS NULL)
         GROUP BY el.LogID)  aaa ON op.LogID = aaa.opid
 GROUP BY op.LogID) OpConf INNER JOIN
@@ -103,11 +103,9 @@ GROUP BY op.LogID) OpConf INNER JOIN
         (SELECT EntryID, PAObjectID, Owner, InfoKey, Value, UpdateTime
         FROM DynamicPAOInfo WHERE (InfoKey LIKE '%udp ip%')) 
         p ON p.PAObjectID = cb.CONTROLDEVICEID LEFT OUTER JOIN
-        ccsubstationsubbuslist as ssl on ssl.substationbusid = el.subid  LEFT OUTER JOIN
-        ccsubareaassignment as csa on csa.substationbusid = ssl.substationid left outer join 
-        YukonPAObject AS yp4 ON yp4.paobjectid = csa.areaid;
+        ccsubareaassignment as csa on csa.substationbusid = el.SubID left outer join 
+        YukonPAObject AS yp4 ON yp4.paobjectid = csa.areaid
 go
-/* @error ignore-end */
 
 insert into FDRInterface values (25, 'TRISTATESUB', 'Receive', 'f' );
 go
