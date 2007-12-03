ec84aac42348a3b877354d35350d9c077c32c4f7
Dayton, Jon R <JonRDayton@Eaton.com>
2007-12-03 04:51:32 +0000

    YUK-4116 -Adds initial classes for the STARS Control History Summary and STARS Control History Detail reports (Southwest reports) -Only adds empty model classes for now as the report implementation is not complete

---
+++ yukon-client/report/src/com/cannontech/analysis/tablemodel/StarsLMControlHistorySummaryModel.java
@@ -0,0 +1,31 @@
+package com.cannontech.analysis.tablemodel;
+
+import com.cannontech.analysis.ColumnProperties;
+
+public class StarsLMControlHistorySummaryModel extends ReportModelBase {
+
+    @Override
+    public void collectData() {
+    }
+
+    public Object getAttribute(int columnIndex, Object o) {
+        return null;
+    }
+
+    public String[] getColumnNames() {
+        return null;
+    }
+
+    public ColumnProperties[] getColumnProperties() {
+        return null;
+    }
+
+    public Class[] getColumnTypes() {
+        return null;
+    }
+
+    public String getTitleString() {
+        return null;
+    }
+	
+}
