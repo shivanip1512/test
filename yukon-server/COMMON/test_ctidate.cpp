f2e9da18a60970fd04a490127b0d1d5c034e8bfc
Fisher, Matthew S <MatthewSFisher@Eaton.com>
2007-12-26 21:31:01 +0000

    Changed to not rely on current date Fixed broken day() comparison that didn't work at the end of the year Added testing for beginning/end of month on all 12 months

--- yukon-server/COMMON/test_ctidate.cpp
+++ yukon-server/COMMON/test_ctidate.cpp
@@ -1,12 +1,12 @@
 /*
  * file test_cmdparse.cpp
- *  
- * Author: Jian Liu 
- * Date: 07/27/2005 14:05:51 
- * 
+ *
+ * Author: Jian Liu
+ * Date: 07/27/2005 14:05:51
+ *
  *
  * test CtiDate
- * 
+ *
  */
 
 
@@ -28,29 +28,52 @@ using boost::unit_test_framework::test_suite;
 BOOST_AUTO_UNIT_TEST(test_ctidate_methods)
 {
     // check the function match between CtiTime and RWTime
-    RWDate rw_date = RWDate();
-    CtiDate d(rw_date.dayOfMonth(), rw_date.month(), rw_date.year());
-    CtiDate d1 = CtiDate();
-    BOOST_CHECK_EQUAL( d.asString(), d1.asString() );
-    BOOST_CHECK_EQUAL( rw_date.dayOfMonth(), d.dayOfMonth() );
-    BOOST_CHECK_EQUAL( rw_date.month(), d.month() );
-    BOOST_CHECK_EQUAL( rw_date.year(), d.year() );
-    BOOST_CHECK_EQUAL( rw_date.weekDayName().data(), d.weekDayName() );
-    BOOST_CHECK_EQUAL( rw_date.weekDay() % 7, d.weekDay() );  //  CtiDate is 0-6 Sun-Sat, RWDate is 1-7 Mon-Sun
-    BOOST_CHECK_EQUAL( rw_date.day(), d.day() );
+    for( int i = 1; i < 12; i++ )
+    {
+        {
+            RWDate rw_date = RWDate(1, i, 2007);
+            CtiDate d(rw_date.dayOfMonth(), rw_date.month(), rw_date.year());
+            CtiDate d1 = CtiDate(1, i, 2007);
+            BOOST_CHECK_EQUAL( d.asString(), d1.asString() );
+            BOOST_CHECK_EQUAL( rw_date.dayOfMonth(), d.dayOfMonth() );
+            BOOST_CHECK_EQUAL( rw_date.month(), d.month() );
+            BOOST_CHECK_EQUAL( rw_date.year(), d.year() );
+            BOOST_CHECK_EQUAL( rw_date.weekDayName().data(), d.weekDayName() );
+            BOOST_CHECK_EQUAL( rw_date.weekDay() % 7, d.weekDay() );  //  CtiDate is 0-6 Sun-Sat, RWDate is 1-7 Mon-Sun
+            BOOST_CHECK_EQUAL( rw_date.day(), d.day() );
 
-    
+            // check constructing from CtiDate
+            //std::cout << d.asString() << std::endl;
+            CtiTime ct(d);
+            BOOST_CHECK_EQUAL( d.asString(), CtiDate(ct).asString() );
+            CtiDate cd4(ct);
+            BOOST_CHECK_EQUAL( cd4.asString(), CtiDate(ct).asString() );
+        }
+        {
+            RWDate rw_date = RWDate(RWDate::daysInMonthYear(i, 2007), i, 2007);
+            CtiDate d(rw_date.dayOfMonth(), rw_date.month(), rw_date.year());
+            CtiDate d1 = CtiDate(CtiDate::daysInMonthYear(i, 2007), i, 2007);
+            BOOST_CHECK_EQUAL( d.asString(), d1.asString() );
+            BOOST_CHECK_EQUAL( rw_date.dayOfMonth(), d.dayOfMonth() );
+            BOOST_CHECK_EQUAL( rw_date.month(), d.month() );
+            BOOST_CHECK_EQUAL( rw_date.year(), d.year() );
+            BOOST_CHECK_EQUAL( rw_date.weekDayName().data(), d.weekDayName() );
+            BOOST_CHECK_EQUAL( rw_date.weekDay() % 7, d.weekDay() );  //  CtiDate is 0-6 Sun-Sat, RWDate is 1-7 Mon-Sun
+            BOOST_CHECK_EQUAL( rw_date.day(), d.day() );
+
+            // check constructing from CtiDate
+            //std::cout << d.asString() << std::endl;
+            CtiTime ct(d);
+            BOOST_CHECK_EQUAL( d.asString(), CtiDate(ct).asString() );
+            CtiDate cd4(ct);
+            BOOST_CHECK_EQUAL( cd4.asString(), CtiDate(ct).asString() );
+        }
+    }
+
+
     //static methods
     BOOST_CHECK_EQUAL( RWDate::now().dayOfMonth(), CtiDate::now().dayOfMonth() );
     BOOST_CHECK_EQUAL( RWDate::daysInMonthYear(12, 1990), CtiDate::daysInMonthYear(12, 1990) );
-
-
-    // check constructing from CtiDate
-	//std::cout << d.asString() << std::endl;
-    CtiTime ct(d);
-    BOOST_CHECK_EQUAL( d.asString(), CtiDate(ct).asString() );
-    CtiDate cd4(ct);
-    BOOST_CHECK_EQUAL( cd4.asString(), CtiDate(ct).asString() );
 
 
     // check the special time values
@@ -95,47 +118,48 @@ BOOST_AUTO_UNIT_TEST(test_ctidate_operators)
     // check the operators
     CtiDate d = CtiDate();
     CtiDate d1 = d;
-    BOOST_CHECK_EQUAL( true, d1 == d );
+    BOOST_CHECK( d1 == d );
     BOOST_CHECK_EQUAL( d.asString(), d1.asString() );
-    
+
     d1 = d - 10;
-    BOOST_CHECK_EQUAL( d.day() - 10, d1.day() );
-    
+    BOOST_CHECK_EQUAL( d.daysFrom1970() - 10, d1.daysFrom1970() );
+
     d1 = d + 10;
-    BOOST_CHECK_EQUAL( d.day() + 10, d1.day() );
+    BOOST_CHECK_EQUAL( d.daysFrom1970() + 10, d1.daysFrom1970() );
+
     BOOST_CHECK_EQUAL( false, d1 <= d );
     BOOST_CHECK_EQUAL( false, d1 == d );
-    BOOST_CHECK_EQUAL( true, d1 >= d );
-    BOOST_CHECK_EQUAL( true, d1 != d );
-    BOOST_CHECK_EQUAL( false, d1 < d );
-    BOOST_CHECK_EQUAL( true, d1 > d );
-    BOOST_CHECK_EQUAL( true, CtiDate(CtiDate::neg_infin) < d );
-    BOOST_CHECK_EQUAL( false, CtiDate(CtiDate::pos_infin) < d );
-    BOOST_CHECK_EQUAL( true, CtiDate(CtiDate::not_a_date) > d ); //!!!
+    BOOST_CHECK_EQUAL( true,  d1 >= d );
+    BOOST_CHECK_EQUAL( true,  d1 != d );
+    BOOST_CHECK_EQUAL( false, d1 <  d );
+    BOOST_CHECK_EQUAL( true,  d1 >  d );
+    BOOST_CHECK_EQUAL( true,  CtiDate(CtiDate::neg_infin)  < d );
+    BOOST_CHECK_EQUAL( false, CtiDate(CtiDate::pos_infin)  < d );
+    BOOST_CHECK_EQUAL( true,  CtiDate(CtiDate::not_a_date) > d ); //!!!
 
     // check the operators with special time values
     CtiDate ct(CtiDate::neg_infin);
     CtiDate ct1 = ct + 4;
-    BOOST_CHECK_EQUAL( true, ct1 <= ct );
-    BOOST_CHECK_EQUAL( true, ct1 == ct );
-    BOOST_CHECK_EQUAL( true, ct1 >= ct );
+    BOOST_CHECK_EQUAL( true,  ct1 <= ct );
+    BOOST_CHECK_EQUAL( true,  ct1 == ct );
+    BOOST_CHECK_EQUAL( true,  ct1 >= ct );
     BOOST_CHECK_EQUAL( false, ct1 != ct );
     BOOST_CHECK_EQUAL( false, ct1 < ct );  //!!!!!!
     BOOST_CHECK_EQUAL( false, ct1 > ct );  //!!!!!! basically, this means ct1==ct
 
     BOOST_CHECK_EQUAL( false, ct > d );
-    BOOST_CHECK_EQUAL( true, ct < d );
-    
+    BOOST_CHECK_EQUAL( true,  ct < d );
+
     ct= CtiDate(CtiDate::not_a_date);
     ct1 = ct - 4;
-    BOOST_CHECK_EQUAL( true, ct1 <= ct );
-    BOOST_CHECK_EQUAL( true, ct1 == ct );
-    BOOST_CHECK_EQUAL( true, ct1 >= ct );
+    BOOST_CHECK_EQUAL( true,  ct1 <= ct );
+    BOOST_CHECK_EQUAL( true,  ct1 == ct );
+    BOOST_CHECK_EQUAL( true,  ct1 >= ct );
     BOOST_CHECK_EQUAL( false, ct1 != ct );
     BOOST_CHECK_EQUAL( false, ct1 < ct );
     BOOST_CHECK_EQUAL( false, ct1 > ct );  //!!!!!! this means ct == ct1
 
-    BOOST_CHECK_EQUAL( true, ct > d );
+    BOOST_CHECK_EQUAL( true,  ct > d );
     BOOST_CHECK_EQUAL( false, ct < d );  // means the not_a_date greater than current date, and any date!!!
 
 
@@ -144,11 +168,11 @@ BOOST_AUTO_UNIT_TEST(test_ctidate_operators)
 BOOST_AUTO_UNIT_TEST(test_ctidate_daysfrom1970)
 {
     CtiDate zeroTest(1, 1, 1970);
-    CtiDate oneTest(2, 1, 1970);
+    CtiDate oneTest (2, 1, 1970);
     CtiDate yearTest(1, 1, 1971);
-    CtiDate twoYearTest(1, 1, 1972);
+    CtiDate twoYearTest  (1, 1, 1972);
     CtiDate threeYearTest(1, 1, 1973);
-    CtiDate manyYearTest(1, 1, 2001); //2000 is a tricky year, and is a leap year. 8 leap years since 1970...
+    CtiDate manyYearTest (1, 1, 2001); //2000 is a tricky year, and is a leap year. 8 leap years since 1970...
 
     BOOST_CHECK_EQUAL( 0, zeroTest.daysFrom1970() );
     BOOST_CHECK_EQUAL( 1, oneTest.daysFrom1970() );
@@ -158,11 +182,11 @@ BOOST_AUTO_UNIT_TEST(test_ctidate_daysfrom1970)
     BOOST_CHECK_EQUAL( (2001-1970)*365 + 8, manyYearTest.daysFrom1970() );//leap year included!
 
     CtiDate dayBeforeTest(10, 3, 2007);
-    CtiDate dayOfTest(11, 3, 2007);
-    CtiDate dayAfterTest(12, 3, 2007);
+    CtiDate dayOfTest    (11, 3, 2007);
+    CtiDate dayAfterTest (12, 3, 2007);
     CtiDate endDayBeforeTest(3, 11, 2007);
-    CtiDate endDayOfTest(4, 11, 2007);
-    CtiDate endDayAfterTest(5, 11, 2007);
+    CtiDate endDayOfTest    (4, 11, 2007);
+    CtiDate endDayAfterTest (5, 11, 2007);
 
     BOOST_CHECK_EQUAL( dayBeforeTest.daysFrom1970() + 1, dayOfTest.daysFrom1970() );
     BOOST_CHECK_EQUAL( dayBeforeTest.daysFrom1970() + 2, dayAfterTest.daysFrom1970() );
@@ -176,8 +200,8 @@ BOOST_AUTO_UNIT_TEST(test_ctidate_daysfrom1970)
 //    test_suite* test= BOOST_TEST_SUITE( "Test CtiDate" );
 //    test->add( BOOST_TEST_CASE( &test_ctidate_methods )) ;
 //    test->add( BOOST_TEST_CASE( &test_ctidate_operators ));
-//    
-//    return test; 
+//
+//    return test;
 //}
 
 
