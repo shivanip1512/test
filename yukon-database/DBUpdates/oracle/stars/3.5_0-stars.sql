e8f4a7f832ef5d21c125fcb33d96938301f95354
Duffy, David <>
2008-01-04 15:15:35 +0000

    YUK-5001 Removed table creation code and an error block since the only part that was needed was a PK definition.  The table and FKs already exist. /* @error ignore-begin */ create table LMHardwareToMeterMapping  (    LMHardwareInventoryID numeric not null,    MeterInventoryID numeric not null );
    alter table LMHardwareToMeterMapping
       add constraint FK_LMMETMAP_LMHARDBASE foreign key (LMHardwareInventoryID)
          references LMHardwareBase (InventoryID);
    
    alter table LMHardwareToMeterMapping
       add constraint FK_LMMETMAP_METERHARDBASE foreign key (MeterInventoryID)
          references MeterHardwareBase (InventoryID);
    
    /* @error ignore-end */

--- yukon-database/DBUpdates/oracle/stars/3.5_0-stars.sql
+++ yukon-database/DBUpdates/oracle/stars/3.5_0-stars.sql
@@ -3,24 +3,8 @@ update Invoice set AuthorizedNumber = '';
 alter table Invoice modify AuthorizedNumber varchar2(60) not null;
 
 /* Start YUK-5001 */
-/* @error ignore-begin */
-create table LMHardwareToMeterMapping
- (
-   LMHardwareInventoryID numeric not null,
-   MeterInventoryID numeric not null
-);
-
-alter table LMHardwareToMeterMapping
-   add constraint FK_LMMETMAP_LMHARDBASE foreign key (LMHardwareInventoryID)
-      references LMHardwareBase (InventoryID);
-
-alter table LMHardwareToMeterMapping
-   add constraint FK_LMMETMAP_METERHARDBASE foreign key (MeterInventoryID)
-      references MeterHardwareBase (InventoryID);
-
 alter table LMHardwareToMeterMapping
    add constraint PK_LMHARDWARETOMETERMAPPING primary key (LMHardwareInventoryID, MeterInventoryID);
-/* @error ignore-end */
 
 delete from LMThermostatSeasonEntry where SeasonID in (select SeasonID from LMThermostatSeason where ScheduleID in (select ScheduleID from LMThermostatSchedule where ThermostatTypeID in (select EntryID from YukonListEntry where YukonDefinitionID = 3100)));
 delete from LMThermostatSeason where ScheduleID in (select ScheduleID from LMThermostatSchedule where ThermostatTypeID in (select EntryID from YukonListEntry where YukonDefinitionID = 3100));
