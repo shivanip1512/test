bc11635832972df7f812e8c2b73e10cbdf86f722
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2008-02-15 20:15:05 +0000

    YUK-5327 Fixed the updating of the tree view panel for Meter Numbers to convert the Device (YukonPaobject) to a DeviceMeterGroup object.  This conversion allows for the correct type of object to be displayed in the tree view.  Specifically, this provides for the Meter Number to display in the Tree view instead of the Device Name.

--- yukon-client/common/src/com/cannontech/database/model/DBTreeModel.java
+++ yukon-client/common/src/com/cannontech/database/model/DBTreeModel.java
@@ -5,6 +5,7 @@ package com.cannontech.database.model;
  */
 import javax.swing.tree.TreePath;
 
+import com.cannontech.clientutils.CTILogger;
 import com.cannontech.database.data.lite.LiteBase;
 
 public abstract class DBTreeModel extends javax.swing.tree.DefaultTreeModel implements LiteBaseTreeModel
@@ -13,191 +14,150 @@ public abstract class DBTreeModel extends javax.swing.tree.DefaultTreeModel impl
 	public static final int SORT_POINT_OFFSET = 1;
 	
 
-/**
- * DBEditorTreeModel constructor comment.
- * @param root javax.swing.tree.TreeNode
- */
-public DBTreeModel(javax.swing.tree.TreeNode root) {
-	super(root);
-}
+    public DBTreeModel(javax.swing.tree.TreeNode root) {
+    	super(root);
+    }
 
-
-/**
- * Insert the method's description here.
- * Creation date: (4/18/2002 2:20:48 PM)
- * @return DBTreeNode
- * @param lb com.cannontech.database.data.lite.LiteBase
- */
-protected DBTreeNode findLiteObject( TreePath path, LiteBase lb)
-{
-	if( path == null )
-		path = new TreePath( getRoot() );
-		
-	DBTreeNode node = (DBTreeNode) path.getLastPathComponent();
-	
-	if( lb == null || !isLiteTypeSupported(lb.getLiteType()) )
-	{
-		return null;
-	}
-	if( node.getUserObject().equals(lb) )
-	{
-		return node;
-	}
-	else
-	if( node.getChildCount() == 0 )
-	{
-		return null;
-	}
-	else
-	{
-		for( int i = 0; i < node.getChildCount(); i++ )
-		{
-			Object nextPathObjs[] = new Object[path.getPath().length +1];
-
-			System.arraycopy( path.getPath(), 0, nextPathObjs, 0, path.getPath().length );
-
-			nextPathObjs[path.getPath().length] = node.getChildAt(i);
-			
-			TreePath nextPath = new TreePath(nextPathObjs);
-
-			DBTreeNode tempNode = findLiteObject(nextPath,lb);
-			if( tempNode != null )
-				return tempNode;
-		}
-
-		//unable to find the node!!
-		return null;
-	}
-	
-		
-}
-/**
- * Insert the method's description here.
- * Creation date: (4/17/2002 1:58:45 PM)
- * @param lite com.cannontech.database.data.lite.LiteBase
- */
-public boolean insertTreeObject( LiteBase lb )
-{
-	if( lb == null || !isLiteTypeSupported(lb.getLiteType()) )
-		return false;
-
-	DBTreeNode rootNode = (DBTreeNode) getRoot();
-		
-	DBTreeNode node = new DBTreeNode(lb);
-
-	//add all new tree nodes to the top, for now
-	int[] ind = { 0 };
-	
-	rootNode.insert( node, ind[0] );
-	
-	nodesWereInserted(
-		rootNode,
-		ind );
-
-	return true;	
-}
-/**
- * Insert the method's description here.
- * Creation date: (4/17/2002 1:58:45 PM)
- * @param lite com.cannontech.database.data.lite.LiteBase
- */
-public boolean removeTreeObject(LiteBase lb) 
-{
-	if( lb == null || !isLiteTypeSupported(lb.getLiteType()) )
-		return false;
-
-java.util.Date s = new java.util.Date();
-
-	DBTreeNode node = findLiteObject( null, lb );
-	
-com.cannontech.clientutils.CTILogger.info("*** REMOVE Took " + 
-	(new java.util.Date().getTime() - s.getTime()) * .001 + 
-	" seconds to find node in DBtreeModel, node = " + node );
-
-	if( node != null )
-	{
-	
-		int[] ind = { node.getParent().getIndex(node) };
-		Object[] delArr = { node };
-			
-		nodesWereRemoved(
-			node.getParent(),
-			ind,
-			delArr );
-
-		//remove the nodes data
-		((DBTreeNode)node.getParent()).remove( node );
-
-		return true;
-	}
-
-	return false;
-}
-/**
- * Insert the method's description here.
- * Creation date: (4/22/2002 12:43:37 PM)
- * @param parentNode DBTreeNode
- * @param sortType int
- */
-public void sortChildNodes(DBTreeNode parentNode, int sortType) 
-{
-	//this method is not implemented so sub classes will not do anything.
-	//If a sub class wants to do sorting, override this method
-}
-/**
- * Insert the method's description here.
- * Creation date: (4/25/2002 12:35:32 PM)
- * @param path javax.swing.tree.TreePath
- */
-public void treePathWillExpand(javax.swing.tree.TreePath path)
-{
-	//do nothing for our default implementation
-}
-/**
- * Insert the method's description here.
- * Creation date: (4/17/2002 1:58:45 PM)
- * @param DefaultMutableTreeNode
- */
-protected void updateTreeNodeStructure(javax.swing.tree.TreeNode node) 
-{
-	if( node == null )
-		return;
-
-	nodeStructureChanged( node );
-}
-/**
- * Insert the method's description here.
- * Creation date: (4/17/2002 1:58:45 PM)
- * @param lite com.cannontech.database.data.lite.LiteBase
- */
-public boolean updateTreeObject(LiteBase lb) 
-{
-	if( lb == null || !isLiteTypeSupported(lb.getLiteType()) )
-		return false;
-
-java.util.Date s = new java.util.Date();
-	DBTreeNode node = findLiteObject( null, lb );
-
-com.cannontech.clientutils.CTILogger.info("*** UPDATE Took " + 
-	(new java.util.Date().getTime() - s.getTime()) * .001 + 
-	" seconds to find node in DBtreeModel, node = " + node);
-
-
-	if( node != null )
-	{
-        node.setUserObject(lb);
-		nodeChanged( node );
-		return true;			
-	}
-
-	return false;
-}
-
-/**
- * This method was created in VisualAge.
- * @return java.lang.String
- */
-public String toString() {
-    return ModelFactory.getModelString(this.getClass());
-}
+    protected DBTreeNode findLiteObject( TreePath path, LiteBase lb)
+    {
+    	if( path == null )
+    		path = new TreePath( getRoot() );
+    		
+    	DBTreeNode node = (DBTreeNode) path.getLastPathComponent();
+    	
+    	if( lb == null || !isLiteTypeSupported(lb.getLiteType()) ) {
+    		return null;
+    	}
+    	if( node.getUserObject().equals(lb) ) {
+    		return node;
+    	}
+    	else
+    	if( node.getChildCount() == 0 ) {
+    		return null;
+    	}
+    	else {
+    		for( int i = 0; i < node.getChildCount(); i++ ) {
+    			Object nextPathObjs[] = new Object[path.getPath().length +1];
+    
+    			System.arraycopy( path.getPath(), 0, nextPathObjs, 0, path.getPath().length );
+    
+    			nextPathObjs[path.getPath().length] = node.getChildAt(i);
+    			
+    			TreePath nextPath = new TreePath(nextPathObjs);
+    
+    			DBTreeNode tempNode = findLiteObject(nextPath,lb);
+    			if( tempNode != null )
+    				return tempNode;
+    		}
+    
+    		//unable to find the node!!
+    		return null;
+    	}
+    }
+    
+    public boolean insertTreeObject( LiteBase lb ) {
+    	if( lb == null || !isLiteTypeSupported(lb.getLiteType()) )
+    		return false;
+    
+    	DBTreeNode rootNode = (DBTreeNode) getRoot();
+    	LiteBase liteBase = convertLiteBase(lb);
+    	DBTreeNode node = new DBTreeNode(liteBase);
+    
+    	//add all new tree nodes to the top, for now
+    	int[] ind = { 0 };
+    	
+    	rootNode.insert( node, ind[0] );
+    	
+    	nodesWereInserted(
+    		rootNode,
+    		ind );
+    
+    	return true;	
+    }
+    
+    public boolean removeTreeObject(LiteBase lb) {
+    	if( lb == null || !isLiteTypeSupported(lb.getLiteType()) )
+    		return false;
+    
+    	java.util.Date s = new java.util.Date();
+    
+    	//Don't call convertLiteBase, it may return null if the device is already deleted.
+        //Just remove the liteBase object as it exists.
+    	DBTreeNode node = findLiteObject( null, lb);
+    	
+    	CTILogger.info("*** REMOVE Took " +
+    	               (new java.util.Date().getTime() - s.getTime()) * .001 +
+    	               " seconds to find node in DBtreeModel, node = " + node );
+    
+    	if( node != null ) {
+    	
+    		int[] ind = { node.getParent().getIndex(node) };
+    		Object[] delArr = { node };
+    			
+    		nodesWereRemoved(
+    			node.getParent(),
+    			ind,
+    			delArr );
+    
+    		//remove the nodes data
+    		((DBTreeNode)node.getParent()).remove( node );
+    
+    		return true;
+    	}
+    
+    	return false;
+    }
+    
+    public void sortChildNodes(DBTreeNode parentNode, int sortType) {
+    	//this method is not implemented so sub classes will not do anything.
+    	//If a sub class wants to do sorting, override this method
+    }
+    
+    public void treePathWillExpand(javax.swing.tree.TreePath path) {
+    	//do nothing for our default implementation
+    }
+    
+    protected void updateTreeNodeStructure(javax.swing.tree.TreeNode node) {
+    	if( node == null )
+    		return;
+    
+    	nodeStructureChanged( node );
+    }
+    
+    public boolean updateTreeObject(LiteBase lb) {
+    	
+        if( lb == null || !isLiteTypeSupported(lb.getLiteType()) )
+    		return false;
+    
+    	LiteBase liteBase = convertLiteBase(lb);
+    	java.util.Date s = new java.util.Date();
+    	DBTreeNode node = findLiteObject( null, liteBase);
+    
+    	CTILogger.info("*** UPDATE Took " +
+    	               (new java.util.Date().getTime() - s.getTime()) * .001 +
+    	               " seconds to find node in DBtreeModel, node = " + node);
+    
+    	if( node != null ) {
+            node.setUserObject(liteBase);
+    		nodeChanged( node );
+    		return true;			
+    	}
+    
+    	return false;
+    }
+    
+    /**
+     * Returns the converted liteBase object.  Override this method if the tree model
+     * is not based on LiteYukonPaobjects (such as the DeviceMeterGroupModel which uses
+     * LiteDeviceMeterNumber objects instead).
+     * @param lb
+     * @return
+     */
+    protected LiteBase convertLiteBase(LiteBase lb){
+        return lb;
+    }
+    
+    public String toString() {
+        return ModelFactory.getModelString(this.getClass());
+    }
 }
--- yukon-client/common/src/com/cannontech/database/model/DeviceMeterGroupModel.java
+++ yukon-client/common/src/com/cannontech/database/model/DeviceMeterGroupModel.java
@@ -1,137 +1,81 @@
 package com.cannontech.database.model;
 
+import com.cannontech.database.cache.DefaultDatabaseCache;
+import com.cannontech.database.data.device.DeviceTypesFuncs;
 import com.cannontech.database.data.lite.LiteBase;
+import com.cannontech.database.data.lite.LiteComparators;
 import com.cannontech.database.data.lite.LiteDeviceMeterNumber;
+import com.cannontech.database.data.lite.LiteTypes;
+import com.cannontech.database.data.lite.LiteYukonPAObject;
+import com.cannontech.database.data.pao.PAOGroups;
 import com.cannontech.yukon.IDatabaseCache;
-
-/**
- * This type was created in VisualAge.
- */
 
 public class DeviceMeterGroupModel extends DBTreeModel 
 {
-/**
- * DeviceTreeModel constructor comment.
- * @param root javax.swing.tree.TreeNode
- */
-public DeviceMeterGroupModel() {
-	super( new DBTreeNode("Meter Numbers") );
-}
 
-
-/**
- * Insert the method's description here.
- * Creation date: (4/17/2002 1:58:45 PM)
- * @param lite com.cannontech.database.data.lite.LiteBase
- */
-public boolean insertTreeObject( LiteBase lb ) 
-{
-	if( lb == null || !isLiteTypeSupported(lb.getLiteType()) )
-		return false;
-
-	DBTreeNode rootNode = (DBTreeNode) getRoot();
-		
-	if ( lb instanceof com.cannontech.database.data.lite.LiteYukonPAObject )
-	{
-		com.cannontech.database.data.lite.LiteYukonPAObject liteYuk =
-				(com.cannontech.database.data.lite.LiteYukonPAObject)lb;
-
-		if( isDeviceValid(liteYuk.getCategory(), liteYuk.getPaoClass(), liteYuk.getType() ) )
-		{
-			//this is the case that makes this model so special, we must map the current
-			//  PAOObject to its owner LiteDeviceMeterGroup
-			LiteBase lBase = findLiteDMG( liteYuk );
-			
-			
-			DBTreeNode node = new DBTreeNode(lBase);
-
-			//add all new tree nodes to the top, for now
-			int[] ind = { 0 };
-			
-			rootNode.insert( node, ind[0] );
-			
-			nodesWereInserted(
-				rootNode,
-				ind );
-
-			return true;
-		}
-
-	}
-
-	return false;
-}
-
-private LiteBase findLiteDMG( com.cannontech.database.data.lite.LiteYukonPAObject liteYuk )
-{
-	LiteBase lBase = null;
-
-	IDatabaseCache cache =
-					com.cannontech.database.cache.DefaultDatabaseCache.getInstance();
-	synchronized(cache)
-	{
-		java.util.List deviceMeterGroupsList = cache.getAllDeviceMeterGroups();
-		
-		int deviceDevID;
-		for( int i = 0; i < deviceMeterGroupsList.size(); i++ )
-		{
-			lBase = (LiteBase)deviceMeterGroupsList.get(i);
-			if( lBase.equals(liteYuk) )
-			{
-				lBase = (LiteDeviceMeterNumber)deviceMeterGroupsList.get(i);
-				break;
-			}
-			else
-				lBase = null;
-		}
-	}
-	
-	return lBase;
-}
-
-
-/**
- * Insert the method's description here.
- * Creation date: (4/22/2002 2:05:03 PM)
- * @return com.cannontech.database.data.lite.LiteBase[]
- */
-public boolean isLiteTypeSupported( int liteType )
-{
-	return( liteType == com.cannontech.database.data.lite.LiteTypes.YUKON_PAOBJECT
-			   || liteType == com.cannontech.database.data.lite.LiteTypes.DEVICE_METERNUMBER );	
-}
-
-public boolean isDeviceValid( int category_, int class_, int type_ )
-{
-	return ( (com.cannontech.database.data.device.DeviceTypesFuncs.isMCT(type_)
-					|| com.cannontech.database.data.device.DeviceTypesFuncs.isMeter(type_))
-			    && category_ == com.cannontech.database.data.pao.PAOGroups.CAT_DEVICE );
-}
-
-/**
- * This method was created in VisualAge.
- */
-public void update()
-{
-	IDatabaseCache cache =
-					com.cannontech.database.cache.DefaultDatabaseCache.getInstance();
-
-	synchronized(cache)
-	{
-		java.util.List deviceMeterGroupsList = cache.getAllDeviceMeterGroups();
-		java.util.Collections.sort( deviceMeterGroupsList, com.cannontech.database.data.lite.LiteComparators.liteStringComparator );
-		
-		DBTreeNode rootNode = (DBTreeNode) getRoot();
-		rootNode.removeAllChildren();
-		
-		int deviceDevID;
-		for( int i = 0; i < deviceMeterGroupsList.size(); i++ )
-		{
-			DBTreeNode deviceNode = new DBTreeNode( deviceMeterGroupsList.get(i));
-			rootNode.add( deviceNode );
-		}
-	}
-
-	reload();
-}
+    public DeviceMeterGroupModel() {
+    	super( new DBTreeNode("Meter Numbers") );
+    }
+    
+    @Override
+    protected LiteBase convertLiteBase(LiteBase lb) {
+        if (lb instanceof LiteYukonPAObject) {
+            return findLiteDMG((LiteYukonPAObject)lb);
+        }
+        return lb;
+    }
+    
+    private LiteBase findLiteDMG( LiteYukonPAObject liteYuk )
+    {
+    	LiteBase lBase = null;
+    
+    	IDatabaseCache cache = DefaultDatabaseCache.getInstance();
+    	synchronized(cache) {
+    		java.util.List<LiteDeviceMeterNumber> deviceMeterGroupsList = cache.getAllDeviceMeterGroups();
+    		
+    		for (LiteDeviceMeterNumber liteDeviceMeterNumber : deviceMeterGroupsList) {
+    			lBase = (LiteBase)liteDeviceMeterNumber;
+    			if( lBase.equals(liteYuk) ) {
+    				lBase = liteDeviceMeterNumber;
+    				break;
+    			}
+    			else
+    				lBase = null;
+    		}
+    	}
+    	
+    	return lBase;
+    }
+    
+    
+    public boolean isLiteTypeSupported( int liteType ) {
+    	return( liteType == LiteTypes.YUKON_PAOBJECT
+    			   || liteType == LiteTypes.DEVICE_METERNUMBER );	
+    }
+    
+    public boolean isDeviceValid( int category_, int class_, int type_ )
+    {
+    	return ( (DeviceTypesFuncs.isMCT(type_) || DeviceTypesFuncs.isMeter(type_))
+    			    && category_ == PAOGroups.CAT_DEVICE );
+    }
+    
+    public void update()
+    {
+    	IDatabaseCache cache = DefaultDatabaseCache.getInstance();
+    
+    	synchronized(cache) {
+    		java.util.List<LiteDeviceMeterNumber> deviceMeterGroupsList = cache.getAllDeviceMeterGroups();
+    		java.util.Collections.sort( deviceMeterGroupsList, LiteComparators.liteStringComparator );
+    		
+    		DBTreeNode rootNode = (DBTreeNode) getRoot();
+    		rootNode.removeAllChildren();
+    		
+    		for (LiteDeviceMeterNumber liteDeviceMeterNumber : deviceMeterGroupsList) {
+    			DBTreeNode deviceNode = new DBTreeNode( liteDeviceMeterNumber);
+    			rootNode.add( deviceNode );
+    		}
+    	}
+    
+    	reload();
+    }
 }
