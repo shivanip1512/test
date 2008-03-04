2d10495590b622bbe4d610db67a8b1a0ae33c534
Duffy, David <>
2008-03-04 16:45:37 +0000

    YUK-5431 INSERT for the DynamicCCSpecialArea table was missing a column.

--- yukon-database/DBUpdates/sqlserver/4.0_0.sql
+++ yukon-database/DBUpdates/sqlserver/4.0_0.sql
@@ -167,7 +167,7 @@ alter table DYNAMICCCAREA
    add constraint PK_DYNAMICCCAREA primary key  (AreaId);
 go
 
-insert into DynamicCCSpecialArea (AreaId, Additionalflags) select areaid, 'NNNNNNNNNNNNNNNNNNNN', 0 from CapControlSpecialArea;
+insert into DynamicCCSpecialArea (AreaId, Additionalflags, ControlValue) select areaid, 'NNNNNNNNNNNNNNNNNNNN', 0 from CapControlSpecialArea;
 go
 
 alter table DynamicCCFeeder add PhaseAValue float;
