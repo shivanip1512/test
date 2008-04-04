b26e9b9eecffe8e6cd74d0ec8fdbdb54795b653d
Meverden, Nick <>
2008-04-04 14:12:07 +0000

    YUK-5603 - Work order report extremely slow and times out completely for large submissions
    - Refactored WorkOrder to remove "EC" abbreviations.
    - New JUnit test for ChunkingSqlTemplate to ensure that List's are being chunked to correct size.

---
+++ yukon-client/common/test/unit/com/cannontech/common/util/ChunkingSqlTemplateTest.java
@@ -0,0 +1,70 @@
+package com.cannontech.common.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+
+import com.cannontech.common.mock.MockDataSource;
+
+public class ChunkingSqlTemplateTest {
+    private final int customChunkSize = 5;
+    private final List<Integer> chunkedList1 = Arrays.asList(new Integer[] {0,1,2,3,4});
+    private final List<Integer> chunkedList2 = Arrays.asList(new Integer[] {5,6,7,8,9});
+    private final List<Integer> chunkedList3 = Arrays.asList(new Integer[] {10});
+    
+    @Test
+    public void test_no_elements_missed_in_chunking() {
+        int initSize = chunkedList1.size() + chunkedList2.size() + chunkedList3.size();
+        List<Integer> fullSizeList = new ArrayList<Integer>(initSize);
+        fullSizeList.addAll(chunkedList1);
+        fullSizeList.addAll(chunkedList2);
+        fullSizeList.addAll(chunkedList3);
+        
+        ChunkingSqlTemplate<Integer> template = new ChunkingSqlTemplate<Integer>(new CustomerSimpleJdbcTemplate());
+        template.setChunkSize(customChunkSize);
+        
+        List<String> resultList = template.query(new SqlGenerator<Integer>() {
+            @Override
+            public String generate(List<Integer> subList) {
+                return Arrays.toString(subList.toArray());
+            }
+        
+        }, fullSizeList, null);
+        
+        int expectedChunkListSize = 3;
+        int resultChunkListSize = resultList.size();
+        Assert.assertEquals("incorrect sized chunk List", expectedChunkListSize, resultChunkListSize);
+        
+        String chunkedList1String = Arrays.toString(chunkedList1.toArray());
+        String resultString1 = resultList.get(0);
+        Assert.assertEquals("incorrect subList elements in first chunk", chunkedList1String, resultString1);
+        
+        String chunkedList2String = Arrays.toString(chunkedList2.toArray());
+        String resultString2 = resultList.get(1);
+        Assert.assertEquals("incorrect subList elements in second chunk", chunkedList2String, resultString2);
+
+        String chunkedList3String = Arrays.toString(chunkedList3.toArray());
+        String resultString3 = resultList.get(2);
+        Assert.assertEquals("incorrect subList elements in third chunk", chunkedList3String, resultString3);
+    }
+    
+    
+    private class CustomerSimpleJdbcTemplate extends SimpleJdbcTemplate {
+        public CustomerSimpleJdbcTemplate() {
+            super(new MockDataSource());
+        }
+        
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> List<T> query(String sql, ParameterizedRowMapper<T> rm, Object... args) throws DataAccessException {
+            return (List<T>) Arrays.asList(new String[]{sql});
+        }
+    }
+}
