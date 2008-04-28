1d24e6124d28dd286641e61cb3cd020302aba31c
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2008-04-28 17:08:49 +0000

    YUK-5740 PointCreate.bat to create frozen points Added new pointCreate command line option (mct410frozen) which will create the frozen peak demand, frozen min volts, and frozen max volts points for ALL MCT410 type meters that do not already have these points defined on them.

--- yukon-client/tools/resource/pointCreate.bat
+++ yukon-client/tools/resource/pointCreate.bat
@@ -7,7 +7,7 @@ goto done
 
 :usage
 echo:
-echo Usage:	pointcreate.bat [ p|b[owerfail|] o[utageslog] d[isconnect] mct410 lg[oadgroup] analog cbop] [(with analog only) devID count]
+echo Usage:	pointcreate.bat [ p[owerfail] o[utageslog] d[isconnect] mct410 mct410frozen lg[oadgroup] analog cbop] [(with analog only) devID count]
 echo:
 echo Ex1:  pointcreate.bat p d     - creates Blink Count and DISCONNECT points
 echo Ex1:  pointcreate.bat b       - creates Blink Count (same as powerfail) point
@@ -15,8 +15,9 @@ echo Ex1:  pointcreate.bat o       - creates OutageLog point
 echo Ex2:  pointcreate.bat analog devID count     - creates count ANALOG points for devID
 echo Ex3:  pointcreate.bat disc    - creates only DISCONNECT STATUS points
 echo Ex4:  pointcreate.bat power   - creates only POWER FAIL points
-echo Ex4:  pointcreate.bat mct410  - adds missing points (kw,kwh,volts,minVolts,maxVolts,peakKw,kwLP,voltLP,blink,outagelog to MCT410 devices
+echo Ex4:  pointcreate.bat mct410  - adds missing points (kw,kwh,volts,minVolts,maxVolts,peakKw,kwLP,voltLP,blink,outagelog) to MCT410 devices
 echo Ex5:  pointcreate.bat lg      - creates history and control points for every LoadGroup
 echo Ex6:  pointcreate.bat cbop    - creates operations count points for every Switched Cap Bank
+echo Ex7:  pointcreate.bat mct410frozen  - adds missing frozen points (frozen peak demand,frozen min volts,frozen max volts) to MCT410 devices
 echo:
 :done
---
+++ yukon-client/tools/src/com/cannontech/datagenerator/point/MCT410FrozenPointCreate.java
@@ -0,0 +1,194 @@
+package com.cannontech.datagenerator.point;
+
+import java.util.List;
+import java.util.Vector;
+
+import com.cannontech.clientutils.CTILogger;
+import com.cannontech.core.dao.DaoFactory;
+import com.cannontech.core.dao.PointDao;
+import com.cannontech.database.cache.DefaultDatabaseCache;
+import com.cannontech.database.data.device.DeviceTypesFuncs;
+import com.cannontech.database.data.lite.LitePoint;
+import com.cannontech.database.data.lite.LiteYukonPAObject;
+import com.cannontech.database.data.multi.SmartMultiDBPersistent;
+import com.cannontech.database.data.point.PointFactory;
+import com.cannontech.database.data.point.PointTypes;
+import com.cannontech.database.data.point.PointUnits;
+import com.cannontech.yukon.IDatabaseCache;
+/**
+ * Creates all Frozen MCT10 points (frozen peak demand, frozen min volts, frozen max volts for every MCT410 type in the database.
+ */
+public class MCT410FrozenPointCreate extends PointCreate
+{
+	java.util.Hashtable<Integer, CreatePointList> createPointHashtable = null;
+	private class CreatePointList
+	{
+		boolean frozenPeakKw = true;  //Demand Acc offset 21
+		boolean frozenMaxVolts = true;    //Demand Acc offset 24
+		boolean frozenMinVolts = true;    //Demand Acc offset 25
+	}
+
+	/**
+	 * Constructor for LoadGroup_ControlPointCreate.
+	 */
+	public MCT410FrozenPointCreate()
+	{
+		super();
+	}
+
+	/**
+	 * @see com.cannontech.datagenerator.point.PointCreate#create()
+	 */
+	public boolean create()
+	{
+		//Points are going to be added to every MCT410.
+		CTILogger.info("Starting MCT410 Frozen Point creation process...");
+
+		Vector<LiteYukonPAObject> devicesVector = getMCT410DeviceVector();
+
+		//create an object to hold all of our DBPersistant objects
+		SmartMultiDBPersistent multi = new SmartMultiDBPersistent();
+		
+		// if this is not set to false it will create its own PointIDs
+		multi.setCreateNewPAOIDs( false );
+	
+		double multiplier = 0.1;
+		int addCount = 0;
+		PointDao pointDao = DaoFactory.getPointDao();
+        
+        for (LiteYukonPAObject litePaobject : devicesVector) {
+
+			int paobjectID = litePaobject.getLiteID();
+			CreatePointList createPoint = createPointHashtable.get(new Integer(paobjectID));
+			
+			if( createPoint.frozenPeakKw) {
+			    int pointID = pointDao.getNextPointId();
+				multi.addDBPersistent(
+					PointFactory.createDmdAccumPoint(
+						"Frozen Peak Demand",
+						new Integer(paobjectID),
+						new Integer(pointID),
+						PointTypes.PT_OFFSET_FROZEN_PEAK_DEMAND,
+						PointUnits.UOMID_KW,
+						multiplier) );
+				CTILogger.info("Adding Frozen Peak Demand: PointId " + pointID  + " to Device: " + litePaobject.getPaoName());
+				addCount++;
+			}
+			if( createPoint.frozenMaxVolts) {
+			    int pointID = pointDao.getNextPointId();
+				multi.addDBPersistent(
+			        PointFactory.createDmdAccumPoint(
+						"Frozen Max Volts",
+						new Integer(paobjectID),
+						new Integer(pointID),
+						PointTypes.PT_OFFSET_FROZEN_MAX_VOLT,
+						PointUnits.UOMID_VOLTS,
+						multiplier) );
+				CTILogger.info("Adding Frozen Max Volts: PointId " + pointID + " to Device: " + litePaobject.getPaoName());
+				addCount++;
+			}
+			
+			if( createPoint.frozenMinVolts)
+			{
+			    int pointID = pointDao.getNextPointId();
+				multi.addDBPersistent(
+			        PointFactory.createDmdAccumPoint(
+						"Frozen Min Volts",
+						new Integer(paobjectID),
+						new Integer(pointID),
+						PointTypes.PT_OFFSET_FROZEN_MIN_VOLT,
+						PointUnits.UOMID_VOLTS,
+						multiplier) );
+		
+				CTILogger.info("Adding Frozen Min Volts: PointId " + pointID + " to Device: " + litePaobject.getPaoName());
+				addCount++;
+			}
+		}
+	
+		boolean success = writeToSQLDatabase(multi);
+	
+		if( success )
+			CTILogger.info(addCount + " MCT410 Frozen Points were processed and inserted Successfully");
+		else
+			CTILogger.info("MCT410 Frozen Points failed insertion");
+			
+		return success;
+	}
+
+	/**
+	 * Returns true if the Device is a valid container of LMGroup Control points.
+	 * A valid type_ is of type all groups except Macro Groups
+	 * Creation date: (4/22/2002 4:11:23 PM)
+	 * @param _type int
+	 * @return boolean
+	 */
+	public boolean isDeviceValid( LiteYukonPAObject litePaobject_ )
+	{
+		//All MCT410s 
+		return DeviceTypesFuncs.isMCT410(litePaobject_.getType());
+	}
+
+	/**
+	 * Returns true if the pointOffset and Pointype combination already exists.
+	 * Creation date: (4/22/2002 4:11:23 PM)
+	 * @param pointOffset_ int
+	 * @param pointType_ int
+	 * @return boolean
+	 */
+	public boolean isPointCreated( LitePoint lp)
+	{
+		if( lp.getPointOffset() == PointTypes.PT_OFFSET_FROZEN_PEAK_DEMAND && lp.getPointType() == PointTypes.DEMAND_ACCUMULATOR_POINT)
+			createPointHashtable.get(new Integer(lp.getPaobjectID())).frozenPeakKw = false;
+		else if( lp.getPointOffset() == PointTypes.PT_OFFSET_FROZEN_MAX_VOLT && lp.getPointType() == PointTypes.DEMAND_ACCUMULATOR_POINT)
+			createPointHashtable.get(new Integer(lp.getPaobjectID())).frozenMaxVolts = false;
+		else if( lp.getPointOffset() == PointTypes.PT_OFFSET_FROZEN_MIN_VOLT && lp.getPointType() == PointTypes.DEMAND_ACCUMULATOR_POINT)
+			createPointHashtable.get(new Integer(lp.getPaobjectID())).frozenMinVolts = false;
+		
+		return false;
+	}	
+	
+	/**
+	 * Returns a Vector of LiteYukonPaobjects that need a Power Fail point added to them.
+	 * Creation date: (4/22/2002 4:11:23 PM)
+	 * @return java.util.Vector
+	 */
+	protected Vector<LiteYukonPAObject> getMCT410DeviceVector()
+	{
+		Vector<LiteYukonPAObject> paobjects = new Vector<LiteYukonPAObject>();
+		
+		IDatabaseCache cache = DefaultDatabaseCache.getInstance();
+	
+		synchronized (cache)
+		{
+			List<LiteYukonPAObject> mcts = cache.getAllMCTs();
+            PointDao pointDao = DaoFactory.getPointDao();
+
+			createPointHashtable = new java.util.Hashtable<Integer, CreatePointList>(mcts.size());
+
+			for (LiteYukonPAObject litePaobject: mcts) {
+
+				if( isDeviceValid( litePaobject ) )
+				{
+					int paobjectID = litePaobject.getLiteID();
+										
+					CreatePointList item = new CreatePointList();
+					createPointHashtable.put(new Integer(paobjectID), item);
+					
+                    boolean foundPoint = false;
+                    List<LitePoint> devicePoints = pointDao.getLitePointsByPaObjectId(paobjectID);
+                    for (LitePoint point : devicePoints) {
+                        if(isPointCreated(point)) {
+                            foundPoint = true;
+                        }
+                    }
+                    //makes a list of points devices to add control history points to
+                    if(!foundPoint) {
+                        paobjects.add(litePaobject);
+                    }
+				}
+			}
+			CTILogger.info(paobjects.size() + " Total Devices needing points added.");
+		}
+		return paobjects;
+	}
+}
