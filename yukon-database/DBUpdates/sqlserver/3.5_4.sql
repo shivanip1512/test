89d466265ca110bb5eec9b18d22f1d80e2a1b30f
Kruse, Matt <>
2008-05-16 20:46:23 +0000

    YUK-5900, Adding Create Login For Account role property.
    Changed the the role property insert from the 3.5_4 version to the 3.5.5 version.

--- yukon-database/DBUpdates/sqlserver/3.5_4.sql
+++ yukon-database/DBUpdates/sqlserver/3.5_4.sql
@@ -2,12 +2,6 @@
 /**** SQLServer 2000 DBupdates         ****/
 /******************************************/
 
-/* Start Yuk-5900 */
-/* @error ignore-begin */
-INSERT INTO YukonRoleProperty VALUES(-40200,-400,'Create Login For Account','false','Allows a new login to be automatically created for each contact on a customer account.');
-/* @error ignore-end */
-/* End Yuk-5900 */
-
 /******************************************************************************/
 /* Run the Stars Update if needed here */
 /* Note: DBUpdate application will ignore this if STARS is not present */
--- yukon-database/DBUpdates/sqlserver/3.5_5.sql
+++ yukon-database/DBUpdates/sqlserver/3.5_5.sql
@@ -1,6 +1,12 @@
 /******************************************/
-/**** Oracle DBupdates   		       ****/
+/**** SQLServer 2000 DBupdates         ****/
 /******************************************/
+
+/* Start Yuk-5900 */
+/* @error ignore-begin */
+INSERT INTO YukonRoleProperty VALUES(-40200,-400,'Create Login For Account','false','Allows a new login to be automatically created for each contact on a customer account.');
+/* @error ignore-end */
+/* End Yuk-5900 */
 
 /******************************************************************************/
 /* Run the Stars Update if needed here */
@@ -14,4 +20,4 @@
 /*   Automatically gets inserted from build script            */
 /**************************************************************/
 
-insert into CTIDatabase values('3.5', 'Matt K', '29-Apr-2008', 'Latest Update', 4 );
+insert into CTIDatabase values('3.5', 'Matt K', '16-May-2008', 'Latest Update', 5 );
