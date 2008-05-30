b62e0257f40db4ec696a6c63e3f17243d35cf95c
Kruse, Matt <>
2008-05-30 15:13:02 +0000

    YUK-5872, Add a new system device group - the "Temporary" group - with permission of HIDDEN
    I moved the sql to the top of the 4.0 script instead of the bot.

--- yukon-database/DBUpdates/sqlserver/4.0_0.sql
+++ yukon-database/DBUpdates/sqlserver/4.0_0.sql
@@ -2,6 +2,95 @@
 /**** SQLServer 2000 DBupdates         ****/
 /******************************************/
 
+/* Start YUK-5961 */
+ALTER TABLE DeviceGroupMember DROP CONSTRAINT FK_DevGrpMember_DeviceGroup;
+go 
+
+ALTER TABLE DeviceGroup DROP CONSTRAINT FK_DeviceGroup_DeviceGroup;
+go
+
+CREATE TABLE TempDeviceGroupIdMap ( 
+   deviceGroupId NUMERIC(18,0), 
+   newDeviceGroupId NUMERIC(18,0) 
+); 
+go 
+
+INSERT INTO TempDeviceGroupIdMap (deviceGroupId, newDeviceGroupId) 
+(SELECT deviceGroupid, deviceGroupid 
+ FROM DeviceGroup 
+ WHERE deviceGroupId > 11 and deviceGroupId < 101); 
+go 
+
+UPDATE tempDeviceGroupIdMap 
+SET newDeviceGroupId = deviceGroupId + (SELECT CASE 
+                                        WHEN (MAX(deviceGroupId) is NULL) 
+                                            THEN 101 
+                                        ELSE MAX(deviceGroupId) + 1 
+                                        END 
+                                        FROM DeviceGroup 
+                                        WHERE deviceGroupId > 100);
+go 
+
+UPDATE DeviceGroup 
+SET parentDeviceGroupId = (SELECT newDeviceGroupId 
+                           FROM TempDeviceGroupIdMap DGMAP
+                           WHERE deviceGroup.parentDeviceGroupId = DGMAP.deviceGroupId) 
+WHERE deviceGroupId IN (SELECT DISTINCT deviceGroupId 
+                        FROM TempDeviceGroupIdMap
+                        WHERE parentDeviceGroupId > 11);
+go
+
+UPDATE DeviceGroup 
+SET deviceGroupId = (SELECT newDeviceGroupId 
+                     FROM TempDeviceGroupIdMap DGMAP 
+                     WHERE deviceGroup.deviceGroupId = DGMAP.deviceGroupId) 
+WHERE deviceGroupId IN (SELECT DISTINCT deviceGroupId 
+                        FROM TempDeviceGroupIdMap);
+go
+
+UPDATE DeviceGroupMember 
+SET deviceGroupId = (SELECT newDeviceGroupid 
+                     FROM TempDeviceGroupIdMap DGMAP 
+                     WHERE DeviceGroupMember.deviceGroupId = DGMAP.deviceGroupId) 
+WHERE deviceGroupId IN (SELECT DISTINCT deviceGroupId 
+                        FROM TempDeviceGroupIdMap);
+go 
+
+DROP TABLE TempDeviceGroupIdMap; 
+go 
+
+ALTER TABLE DEVICEGROUP
+    ADD CONSTRAINT FK_DeviceGroup_DeviceGroup FOREIGN KEY (parentDeviceGroupId)
+        REFERENCES DeviceGroup (deviceGroupId);
+go
+
+ALTER TABLE DEVICEGROUPMEMBER 
+   ADD CONSTRAINT FK_DevGrpMember_DeviceGroup foreign key (deviceGroupId) 
+      REFERENCES DEVICEGROUP (deviceGroupId); 
+go
+
+DELETE FROM SequenceNumber 
+WHERE SequenceName = 'DeviceGroup'; 
+
+/* @start-block */
+DECLARE @dGSequenceNumber INT; 
+SET @dGSequenceNumber = CAST((SELECT CASE  
+                                  WHEN (MAX(DeviceGroupId) IS NULL)
+                                      THEN 100 
+                                  ELSE 
+                                      MAX(deviceGroupId) 
+                                  END 
+                              FROM DeviceGroup 
+                              WHERE deviceGroupId > 100) as INT);
+
+
+INSERT INTO SequenceNumber 
+VALUES (@dGSequenceNumber, 'DeviceGroup');
+/* @end-block */
+go
+/* End YUK-5961 */
+
+
 /* Start YUK-5287 */
 alter table dynamicccarea add controlvalue numeric;
 go
@@ -2304,94 +2393,6 @@ UPDATE FDRInterface SET PossibleDirections = 'Receive,Send' WHERE InterfaceId = 
 INSERT INTO YukonRoleProperty VALUES(-40200,-400,'Create Login For Account','false','Allows a new login to be automatically created for each contact on a customer account.');
 /* @error ignore-end */
 /* End Yuk-5900 */
-
-/* Start YUK-5961 */
-ALTER TABLE DeviceGroupMember DROP CONSTRAINT FK_DevGrpMember_DeviceGroup;
-go 
-
-ALTER TABLE DeviceGroup DROP CONSTRAINT FK_DeviceGroup_DeviceGroup;
-go
-
-CREATE TABLE TempDeviceGroupIdMap ( 
-   deviceGroupId NUMERIC(18,0), 
-   newDeviceGroupId NUMERIC(18,0) 
-); 
-go 
-
-INSERT INTO TempDeviceGroupIdMap (deviceGroupId, newDeviceGroupId) 
-(SELECT deviceGroupid, deviceGroupid 
- FROM DeviceGroup 
- WHERE deviceGroupId > 14 and deviceGroupId < 101); 
-go 
-
-UPDATE tempDeviceGroupIdMap 
-SET newDeviceGroupId = deviceGroupId + (SELECT CASE 
-                                        WHEN (MAX(deviceGroupId) is NULL) 
-                                            THEN 101 
-                                        ELSE MAX(deviceGroupId) + 1 
-                                        END 
-                                        FROM DeviceGroup 
-                                        WHERE deviceGroupId > 100);
-go 
-
-UPDATE DeviceGroup 
-SET parentDeviceGroupId = (SELECT newDeviceGroupId 
-                           FROM TempDeviceGroupIdMap DGMAP
-                           WHERE deviceGroup.parentDeviceGroupId = DGMAP.deviceGroupId) 
-WHERE deviceGroupId IN (SELECT DISTINCT deviceGroupId 
-                        FROM TempDeviceGroupIdMap
-                        WHERE parentDeviceGroupId > 14);
-go
-
-UPDATE DeviceGroup 
-SET deviceGroupId = (SELECT newDeviceGroupId 
-                     FROM TempDeviceGroupIdMap DGMAP 
-                     WHERE deviceGroup.deviceGroupId = DGMAP.deviceGroupId) 
-WHERE deviceGroupId IN (SELECT DISTINCT deviceGroupId 
-                        FROM TempDeviceGroupIdMap);
-go
-
-UPDATE DeviceGroupMember 
-SET deviceGroupId = (SELECT newDeviceGroupid 
-                     FROM TempDeviceGroupIdMap DGMAP 
-                     WHERE DeviceGroupMember.deviceGroupId = DGMAP.deviceGroupId) 
-WHERE deviceGroupId IN (SELECT DISTINCT deviceGroupId 
-                        FROM TempDeviceGroupIdMap);
-go 
-
-DROP TABLE TempDeviceGroupIdMap; 
-go 
-
-ALTER TABLE DEVICEGROUP
-    ADD CONSTRAINT FK_DeviceGroup_DeviceGroup FOREIGN KEY (parentDeviceGroupId)
-        REFERENCES DeviceGroup (deviceGroupId);
-go
-
-ALTER TABLE DEVICEGROUPMEMBER 
-   ADD CONSTRAINT FK_DevGrpMember_DeviceGroup foreign key (deviceGroupId) 
-      REFERENCES DEVICEGROUP (deviceGroupId); 
-go
-
-DELETE FROM SequenceNumber 
-WHERE SequenceName = 'DeviceGroup'; 
-
-/* @start-block */
-DECLARE @dGSequenceNumber INT; 
-SET @dGSequenceNumber = CAST((SELECT CASE  
-                                  WHEN (MAX(DeviceGroupId) IS NULL)
-                                      THEN 100 
-                                  ELSE 
-                                      MAX(deviceGroupId) 
-                                  END 
-                              FROM DeviceGroup 
-                              WHERE deviceGroupId > 100) as INT);
-
-
-INSERT INTO SequenceNumber 
-VALUES (@dGSequenceNumber, 'DeviceGroup');
-/* @end-block */
-go
-/* End YUK-5961 */
 
 /* Start YUK-5963 */
 /* @start-block */
