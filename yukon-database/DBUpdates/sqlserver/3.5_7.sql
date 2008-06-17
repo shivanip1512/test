9bd21ebd9e21cb6a58973bd6bec8d299ea6efbf5
Kruse, Matt <>
2008-06-17 15:03:34 +0000

    Adding 3.5_7 update scripts

---
+++ yukon-database/DBUpdates/sqlserver/3.5_7.sql
@@ -0,0 +1,17 @@
+/******************************************/
+/**** SQLServer 2000 DBupdates         ****/
+/******************************************/
+
+/******************************************************************************/
+/* Run the Stars Update if needed here */
+/* Note: DBUpdate application will ignore this if STARS is not present */
+/* @include StarsUpdate */
+/******************************************************************************/
+
+
+/**************************************************************/
+/* VERSION INFO                                               */
+/*   Automatically gets inserted from build script            */
+/**************************************************************/
+
+insert into CTIDatabase values('3.5', 'Matt K', '17-Jun-2008', 'Latest Update', 7);
