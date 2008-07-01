d7455ba3e952dbd4606dff868a3aecbf9b0ac53a
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2008-07-01 20:38:48 +0000

    added meterAddNotification testing example.

--- yukon-client/multispeak/src/com/cannontech/multispeak/emulator/MR_CB_Test.java
+++ yukon-client/multispeak/src/com/cannontech/multispeak/emulator/MR_CB_Test.java
@@ -17,10 +17,13 @@ import org.apache.axis.message.SOAPHeaderElement;
 import com.cannontech.clientutils.CTILogger;
 import com.cannontech.multispeak.client.YukonMultispeakMsgHeader;
 import com.cannontech.multispeak.dao.impl.MspMeterDaoImpl;
+import com.cannontech.multispeak.deploy.service.EaLoc;
 import com.cannontech.multispeak.deploy.service.ErrorObject;
 import com.cannontech.multispeak.deploy.service.MR_CBSoap_BindingStub;
 import com.cannontech.multispeak.deploy.service.Meter;
 import com.cannontech.multispeak.deploy.service.MeterRead;
+import com.cannontech.multispeak.deploy.service.Nameplate;
+import com.cannontech.multispeak.deploy.service.UtilityInfo;
 import com.cannontech.spring.YukonSpringHook;
 
 /**
@@ -35,8 +38,9 @@ public class MR_CB_Test {
 	{
 		try {
 			String endpointURL = "http://localhost:8080/soap/MR_CBSoap";
-			endpointURL = "http://demo.cannontech.com/soap/MR_CBSoap";
+//			endpointURL = "http://demo.cannontech.com/soap/MR_CBSoap";
 //			endpointURL = "http://10.100.10.25:80/soap/MR_CBSoap";
+			endpointURL = "http://10.106.36.79:8080/soap/MR_CBSoap";  //Mike's computer
 		  	MR_CBSoap_BindingStub instance = new MR_CBSoap_BindingStub(new URL(endpointURL), new Service());
 			
             YukonMultispeakMsgHeader msgHeader =new YukonMultispeakMsgHeader();
@@ -45,53 +49,39 @@ public class MR_CB_Test {
 			SOAPHeaderElement header = new SOAPHeaderElement("http://www.multispeak.org/Version_3.0", "MultiSpeakMsgHeader", msgHeader);
 			instance.setHeader(header);
 
-			int todo = 2;	//0=meterRead, 1=getAMRSupportedMeters, 2=pingURL, 3=getReadingsByMeterNo
+			int todo = 2;	//0=meterRead, 1=getAMRSupportedMeters, 2=pingURL, 3=getReadingsByMeterNo, 4=meterAddNotification
 			
-			if (todo==0)
-			{
+			if (todo==0) {
 			    MeterRead mr = instance.getLatestReadingByMeterNo("10620108");	//1068048 whe, 1010156108 sn_head/amr_demo
-				if( mr != null)
-				{
+				if( mr != null) {
 				    CTILogger.info("MeterRead received: " + ( mr.getReadingDate() != null?mr.getReadingDate().getTime():null) + " : " +mr.getPosKWh());
 				    CTILogger.info("MeterRead Error String: " + mr.getErrorString());
-				}
-				else
-				{
+				} else {
 				    CTILogger.info("******   NULL METER READING  **********");
 				}
 			}
-			else if( todo == 1)
-			{
+			else if( todo == 1) {
                 List<Meter>meters = ((MspMeterDaoImpl)YukonSpringHook.getBean("mspMeterDao")).getAMRSupportedMeters("0", 10000);
-                System.out.println(meters.size());
-/*				ArrayOfMeter meters = new ArrayOfMeter();
-				meters = instance.getAMRSupportedMeters("10224712");//new String ("MCT - Annandale Broadcast"));
-				if (meters != null && meters.getMeter().length > 0)
-				{
-					CTILogger.info("METERS RETURNED: " + meters.getMeter().length);
-					for (int i = 0; i < meters.getMeter().length; i++)
-					{
-						Meter m = meters.getMeter(i);
-						CTILogger.info(m.getMeterNo());
-	//					String obj = strings.getString(i);
-	//					System.out.println("Method" + i + ": " + obj);
-					}
-				}*/
-			}
-			else if (todo == 2)
-			{
-			    ErrorObject[] objects = instance.pingURL();
-				if (objects != null && objects != null)
-				{
-					for (int i = 0; i < objects.length; i++)
-					{
-						ErrorObject obj = objects[i];
-						System.out.println("Ping" + i + ": " + obj.getErrorString());
+				if (meters != null && meters.size() > 0) {
+				    
+					CTILogger.info("METERS RETURNED: " + meters.size());
+					for (Meter meter : meters) {
+						CTILogger.info(meter.getMeterNo());
 					}
 				}
 			}
-			else if( todo == 3)
-			{
+			else if (todo == 2) {
+			    ErrorObject[] objects = instance.pingURL();
+				if (objects != null && objects != null) {
+					for (int i = 0; i < objects.length; i++) {
+						ErrorObject obj = objects[i];
+						CTILogger.info("Ping" + i + ": " + obj.getErrorString());
+					}
+				} else {
+				    CTILogger.info("PingURL Successful");
+	            }
+			}
+			else if( todo == 3) {
 				GregorianCalendar cal = new GregorianCalendar();
 				cal.set(Calendar.MONTH, Calendar.JULY);
 				cal.set(Calendar.YEAR, 2006);
@@ -104,11 +94,34 @@ public class MR_CB_Test {
 				GregorianCalendar endCal = (GregorianCalendar)cal.clone();
 				endCal.add(Calendar.MONTH, 2);
 				MeterRead[] amr = instance.getReadingsByMeterNo("01071861", cal, endCal);	//1068048 whe, 1010156108 sn_head/amr_demo
-				if( amr != null)
-				{
+				if( amr != null) {
 					CTILogger.info("MeterRead received: " + amr.length + " : " );
-//									CTILogger.info("MeterRead Error String: " + mr.getErrorString());
 				}
+			} else if( todo == 4) {
+			    Meter meter = new Meter();
+			    meter.setAMRDeviceType("*Default Template"); // this is the "Template" name
+			    meter.setMeterNo("2222223"); //MeterNumber, and value used for PaoName when PaoNameAlias is DeviceName (Default)
+			    UtilityInfo utilityInfo = new UtilityInfo();
+			    utilityInfo.setAccountNumber("1234567"); //Value used for PaoName when PaoNameAlias = AccountNumber
+			    utilityInfo.setCustID("1234567"); //Value used for PaoName when PaoNameAlias = CustomerNumber
+			    EaLoc eaLoc = new EaLoc();
+			    eaLoc.setName("1234567");    //Value used for PaoName when PaoNameAlias = EALocation
+			    utilityInfo.setEaLoc(eaLoc);
+			    utilityInfo.setSubstationName("SUBSTATION 1");   //SubstationName that maps to Route in SubstationToRouteMapping
+			    meter.setUtilityInfo(utilityInfo);
+			    Nameplate nameplate = new Nameplate();
+			    nameplate.setTransponderID("2222223");   //This is the Physical Address
+			    meter.setNameplate(nameplate);
+			    
+			    ErrorObject[] objects = instance.meterAddNotification(new Meter[]{meter});
+                if (objects != null && objects != null) {
+                    for (int i = 0; i < objects.length; i++) {
+                        ErrorObject obj = objects[i];
+                        CTILogger.info("Add Meter Error-" + i + ": " + obj.getErrorString());
+                    }
+                } else {
+                    CTILogger.info("MeterAddNotification Successful");
+                }
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
