ce4777a306eb91def0a6a2427bf080ecd030db12
Spar, Thain <>
2008-07-08 15:52:18 +0000

    YUK-5724 ccu simulator - support simultaneous queue and no-queue messages
    Code was not correctly calculating the ACK values in the response message.

--- yukon-server/Simulator/CCUSimulator.cpp
+++ yukon-server/Simulator/CCUSimulator.cpp
@@ -265,3 +265,23 @@ void CCUThread(const int portNumber, const int strategy)
     newSocket->CTINexusClose();
     return;
 }
+
+int uchar_parity( unsigned char x ) {
+  x = x ^ (x >> 4);
+  x = x ^ (x >> 2);
+  x = x ^ (x >> 1);
+
+  return x & 1;
+} 
+
+unsigned char makeAck(int ccuAddress)
+{
+    unsigned char ack = 0x40;
+    unsigned char adr = 0x03 & ccuAddress;
+    ack = adr | ack;
+    if (uchar_parity(ack)) {
+        ack = 0x80 | ack;
+    }
+    return ack;
+}
+
--- yukon-server/Simulator/include/SharedFunctions.h
+++ yukon-server/Simulator/include/SharedFunctions.h
@@ -1,23 +1,7 @@
 #ifndef  __SHAREDFUNCTIONS_H__
 #define  __SHAREDFUNCTIONS_H__
 
-int uchar_parity( unsigned char x ) {
-  x = x ^ (x >> 4);
-  x = x ^ (x >> 2);
-  x = x ^ (x >> 1);
-
-  return x & 1;
-} 
-
-unsigned char makeAck(int ccuAddress)
-{
-    unsigned char ack = 0x40;
-    unsigned char adr = 0x03 & ccuAddress;
-    ack = adr | ack;
-    if (uchar_parity(ack)) {
-        ack = 0x80 | ack;
-    }
-    return ack;
-}
+int uchar_parity( unsigned char x );
+unsigned char makeAck(int ccuAddress);
 
 #endif
