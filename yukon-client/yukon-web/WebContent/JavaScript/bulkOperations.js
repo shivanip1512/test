0da7bf49c35001770aba4ca8432a8117890c9a81
Peterson, Michael <>
2008-07-23 22:26:29 +0000

    YUK-4887  New bulk add,delete and update features for managing the deployment of an AMR system - make the "magnifier" safer, only allow one request for device details at a time - right now that img is dangerous when working with large device collection, there is no indication that something is happening, and multiple clicks will just put more stress on server - when clicked, disabled further clicks on the img and swap img or a more translucent "disabled" version, restore when request is complete

--- yukon-client/yukon-web/WebContent/JavaScript/bulkOperations.js
+++ yukon-client/yukon-web/WebContent/JavaScript/bulkOperations.js
@@ -3,8 +3,12 @@
 function showSelectedDevices(imgEl, divId, innerDivId, url, mag, magOverDisabled) {
 
     var savedOnClickValue = imgEl.attributes['onclick'].value;
+    var savedOnMouseOverValue = imgEl.attributes['onmouseover'].value;
+    var savedOnMouseOutValue = imgEl.attributes['onmouseout'].value;
     
     imgEl.attributes['onclick'].value = '';
+    imgEl.attributes['onmouseover'].value = '';
+    imgEl.attributes['onmouseout'].value = '';
     imgEl.src = magOverDisabled;
     
     new Ajax.Request(url, {
@@ -17,6 +21,8 @@ function showSelectedDevices(imgEl, divId, innerDivId, url, mag, magOverDisabled
             $(divId).show();
         
             imgEl.attributes['onclick'].value = savedOnClickValue;
+            imgEl.attributes['onmouseover'].value = savedOnMouseOverValue;
+            imgEl.attributes['onmouseout'].value = savedOnMouseOutValue;
             imgEl.src = mag;
         }
         
