75b8a137440c329060a074ddfc953bfd011b3142
Meverden, Nick <>
2008-07-30 17:05:33 +0000

    YUK-6187 - Exception when clicking on Billing Generation
    - MsgTag will now get the correct MessageSource if the YukonUserContext is attached to the Request.

--- yukon-client/yukon-web/src/com/cannontech/web/i18n/MsgTag.java
+++ yukon-client/yukon-web/src/com/cannontech/web/i18n/MsgTag.java
@@ -1,11 +1,33 @@
 package com.cannontech.web.i18n;
 
+import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
 import org.springframework.context.MessageSourceResolvable;
 import org.springframework.web.servlet.tags.ThemeTag;
 
+import com.cannontech.clientutils.YukonLogManager;
 import com.cannontech.common.i18n.DisplayableEnum;
+import com.cannontech.i18n.YukonUserContextMessageSourceResolver;
+import com.cannontech.servlet.YukonUserContextUtils;
+import com.cannontech.user.YukonUserContext;
 
 public class MsgTag extends ThemeTag {
+    private static final Logger log = YukonLogManager.getLogger(MsgTag.class);
+    
+    @Override
+    protected MessageSource getMessageSource() {
+        try {
+            YukonUserContextMessageSourceResolver messageSourceResolver = getMessageSourceResolver();
+            YukonUserContext yukonUserContext = YukonUserContextUtils.getYukonUserContext(super.pageContext);
+            MessageSource messageSource = messageSourceResolver.getMessageSource(yukonUserContext);
+            return messageSource;
+        } catch (RuntimeException e) {
+            log.debug("Unable to get YukonUserContext from the Request.", e);
+        }
+        
+        return super.getMessageSource();
+    }
     
     public void setKey(Object key) {
         if (key instanceof MessageSourceResolvable) {
@@ -25,7 +47,17 @@ public class MsgTag extends ThemeTag {
         }
     }
     
+    private YukonUserContextMessageSourceResolver getMessageSourceResolver() {
+        ApplicationContext context = super.getRequestContext().getWebApplicationContext();
+        
+        YukonUserContextMessageSourceResolver messageSourceResolver = 
+            (YukonUserContextMessageSourceResolver) context.getBean("yukonUserContextMessageSourceResolver");
+        
+        return messageSourceResolver;
+    }
+    
     public void setArgument(Object argument) {
         setArguments(new Object[]{argument});
     }
+    
 }
