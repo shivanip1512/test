248cec8c9d2f2daa61342aeafd7b321f58bcb01b
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2008-08-01 22:51:11 +0000

    YUK-6197 Reporting properties for Labels are no longer used. - Removed role properties for report group lables.  These labels have been i18n'd and are no longer needed.

--- yukon-client/common/src/com/cannontech/roles/application/ReportingRole.java
+++ yukon-client/common/src/com/cannontech/roles/application/ReportingRole.java
@@ -25,17 +25,5 @@ public interface ReportingRole {
 	public static final int STARS_REPORTS_GROUP = ApplicationRoleDefs.REPORTING_PROPERTYID_BASE - 9;
 	public static final int OTHER_REPORTS_GROUP = ApplicationRoleDefs.REPORTING_PROPERTYID_BASE - 10;
 	public static final int SETTLEMENT_REPORTS_GROUP = ApplicationRoleDefs.REPORTING_PROPERTYID_BASE - 11;
-	
-	public static final int ADMIN_REPORTS_GROUP_LABEL = ApplicationRoleDefs.REPORTING_PROPERTYID_BASE - 13;
-	public static final int AMR_REPORTS_GROUP_LABEL = ApplicationRoleDefs.REPORTING_PROPERTYID_BASE - 14;
-	public static final int STATISTICAL_REPORTS_GROUP_LABEL = ApplicationRoleDefs.REPORTING_PROPERTYID_BASE - 15;
-	public static final int LOAD_MANAGEMENT_REPORTS_GROUP_LABEL = ApplicationRoleDefs.REPORTING_PROPERTYID_BASE - 16;
-	public static final int CAP_CONTROL_REPORTS_GROUP_LABEL = ApplicationRoleDefs.REPORTING_PROPERTYID_BASE - 17;
-	public static final int DATABASE_REPORTS_GROUP_LABEL = ApplicationRoleDefs.REPORTING_PROPERTYID_BASE - 18;
-	public static final int STARS_REPORTS_GROUP_LABEL = ApplicationRoleDefs.REPORTING_PROPERTYID_BASE - 19;
-	public static final int OTHER_REPORTS_GROUP_LABEL = ApplicationRoleDefs.REPORTING_PROPERTYID_BASE - 20;
-	public static final int SETTLEMENT_REPORTS_GROUP_LABEL = ApplicationRoleDefs.REPORTING_PROPERTYID_BASE - 21;
-    
-	public static final int CI_CURTAILMENT_REPORTS_GROUP_LABEL = ApplicationRoleDefs.REPORTING_PROPERTYID_BASE - 22;
 	public static final int CI_CURTAILMENT_REPORTS_GROUP = ApplicationRoleDefs.REPORTING_PROPERTYID_BASE - 23;
 }
