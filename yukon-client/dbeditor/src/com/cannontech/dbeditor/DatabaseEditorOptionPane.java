bcd16e892776865bc9f323c9b75b12cb151f2632
Meverden, Nick <>
2008-08-06 18:59:57 +0000

    YUK-5210 - Mystery device list at Farmers MO when editing 210s and 240s
    - A better looking Dialog.

--- yukon-client/dbeditor/src/com/cannontech/dbeditor/DatabaseEditorOptionPane.java
+++ yukon-client/dbeditor/src/com/cannontech/dbeditor/DatabaseEditorOptionPane.java
@@ -2,8 +2,10 @@ package com.cannontech.dbeditor;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.Font;
 import java.util.List;
 
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -11,7 +13,8 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class DatabaseEditorOptionPane {
-
+    private static final Font textAreaFont = new JLabel().getFont();
+    
     private DatabaseEditorOptionPane() {
         
     }
@@ -31,15 +34,20 @@ public class DatabaseEditorOptionPane {
         final JPanel panel = new JPanel();
         
         final JList list = new JList(devices);
-        list.setVisibleRowCount(8);
-
+        list.setVisibleRowCount(4);
+        list.setBackground(panel.getBackground());
+        
         final JTextArea textArea = new JTextArea(message);
         textArea.setEditable(false);
         textArea.setBackground(panel.getBackground());
+        textArea.setFont(textAreaFont);
+        
+        final JScrollPane spane = new JScrollPane(list);
+        spane.setBorder(null);
         
         panel.setLayout(new BorderLayout());
         panel.add(textArea, BorderLayout.NORTH);
-        panel.add(new JScrollPane(list), BorderLayout.SOUTH);
+        panel.add(spane, BorderLayout.SOUTH);
         
         int res = JOptionPane.showConfirmDialog(parentComponent,
                                                 panel,
