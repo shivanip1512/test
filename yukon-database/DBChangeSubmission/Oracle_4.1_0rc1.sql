fa981257673502b6b0c86b86cfc6f9e96d0c2984
Kruse, Matt <>
2008-10-10 19:33:30 +0000

    YUK-6549, YukonGroupRole Entries in a 3.2.15 Database Updated to 4.1 do not Match a Clean 4.1 Database
    - changed the rc1 update scripts.

--- yukon-database/DBChangeSubmission/Oracle_4.1_0rc1.sql
+++ yukon-database/DBChangeSubmission/Oracle_4.1_0rc1.sql
@@ -3,130 +3,50 @@
 
 /* START SPECIAL BLOCK */
 
-Declare
-    temp Number;
+UPDATE YukonGroupRole 
+SET GroupRoleId = -2500 
+WHERE GroupRoleId = -1150 
+AND GroupId = -2 
+AND RolePropertyId = -21300;
 
-Begin
-    SELECT count(*) into temp
-    FROM YukonGroupRole 
-    WHERE GroupRoleId = -1150 
-    AND GroupId = -2 
-    AND RolePropertyId = -21300;
-    
-    IF (temp > 0) THEN
-        UPDATE YukonGroupRole Set GroupRoleId = -2500 WHERE GroupRoleId = -1150;
-    END IF;
-END;
-/
+UPDATE YukonGroupRole 
+SET GroupRoleId = -2501 
+WHERE GroupRoleId = -1151 
+AND GroupId = -2 
+AND RolePropertyId = -21301;
 
-Declare
-    temp Number;
+UPDATE YukonGroupRole 
+SET GroupRoleId = -2502 
+WHERE GroupRoleId = -1152 
+AND GroupId = -2 
+AND RolePropertyId = -21302;
 
-Begin
-    SELECT count(*) into temp
-    FROM YukonGroupRole 
-    WHERE GroupRoleId = -1151 
-    AND GroupId = -2 
-    AND RolePropertyId = -21301;
-    
-    IF (temp > 0) THEN
-        UPDATE YukonGroupRole Set GroupRoleId = -2501 WHERE GroupRoleId = -1151;
-    END IF;
-END;
-/
+UPDATE YukonGroupRole 
+SET GroupRoleId = -2503 
+WHERE GroupRoleId = -1153
+AND GroupId = -2 
+AND RolePropertyId = -21303;
 
-Declare
-    temp Number;
+UPDATE YukonGroupRole 
+SET GroupRoleId = -2504 
+WHERE GroupRoleId = -1154
+AND GroupId = -2 
+AND RolePropertyId = -21304;
 
-Begin
-    SELECT count(*) into temp
-    FROM YukonGroupRole 
-    WHERE GroupRoleId = -1152 
-    AND GroupId = -2 
-    AND RolePropertyId = -21302;
-    
-    IF (temp > 0) THEN
-        UPDATE YukonGroupRole Set GroupRoleId = -2502 WHERE GroupRoleId = -1152;
-    END IF;
-END;
-/
+UPDATE YukonGroupRole 
+SET GroupRoleId = -2505 
+WHERE GroupRoleId = -1155
+AND GroupId = -2 
+AND RolePropertyId = -21305;
 
-Declare
-    temp Number;
+UPDATE YukonGroupRole 
+SET GroupRoleId = -2506 
+WHERE GroupRoleId = -1156
+AND GroupId = -2 
+AND RolePropertyId = -21306;
 
-Begin
-    SELECT count(*) into temp
-    FROM YukonGroupRole 
-    WHERE GroupRoleId = -1153 
-    AND GroupId = -2 
-    AND RolePropertyId = -21303;
-    
-    IF (temp > 0) THEN
-        UPDATE YukonGroupRole Set GroupRoleId = -2503 WHERE GroupRoleId = -1153;
-    END IF;
-END;
-/
-
-Declare
-    temp Number;
-
-Begin
-    SELECT count(*) into temp
-    FROM YukonGroupRole 
-    WHERE GroupRoleId = -1154 
-    AND GroupId = -2 
-    AND RolePropertyId = -21304;
-    
-    IF (temp > 0) THEN
-        UPDATE YukonGroupRole Set GroupRoleId = -2504 WHERE GroupRoleId = -1154;
-    END IF;
-END;
-/
-
-Declare
-    temp Number;
-
-Begin
-    SELECT count(*) into temp
-    FROM YukonGroupRole 
-    WHERE GroupRoleId = -1155 
-    AND GroupId = -2 
-    AND RolePropertyId = -21305;
-    
-    IF (temp > 0) THEN
-        UPDATE YukonGroupRole Set GroupRoleId = -2505 WHERE GroupRoleId = -1155;
-    END IF;
-END;
-/
-
-Declare
-    temp Number;
-
-Begin
-    SELECT count(*) into temp
-    FROM YukonGroupRole 
-    WHERE GroupRoleId = -1156 
-    AND GroupId = -2 
-    AND RolePropertyId = -21306;
-    
-    IF (temp > 0) THEN
-        UPDATE YukonGroupRole Set GroupRoleId = -2506 WHERE GroupRoleId = -1156;
-    END IF;
-END;
-/
-
-Declare
-    temp Number;
-
-Begin
-    SELECT count(*) into temp
-    FROM YukonGroupRole 
-    WHERE GroupRoleId = -1157 
-    AND GroupId = -2 
-    AND RolePropertyId = -21307;
-    
-    IF (temp > 0) THEN
-        UPDATE YukonGroupRole Set GroupRoleId = -2507 WHERE GroupRoleId = -1157;
-    END IF;
-END;
-/
+UPDATE YukonGroupRole 
+SET GroupRoleId = -2507 
+WHERE GroupRoleId = -1157
+AND GroupId = -2 
+AND RolePropertyId = -21307;
--- yukon-database/DBChangeSubmission/SQL_4.1_0rc1.sql
+++ yukon-database/DBChangeSubmission/SQL_4.1_0rc1.sql
@@ -3,76 +3,50 @@
 
 /* START SPECIAL BLOCK */
 
-IF EXISTS (SELECT * 
-           FROM YukonGroupRole 
-           WHERE GroupRoleId = -1150 
-           AND GroupId = -2 
-           AND RolePropertyId = -21300)
-    BEGIN
-        UPDATE YukonGroupRole Set GroupRoleId = -2500 WHERE GroupRoleId = -1150;
-    END 
+UPDATE YukonGroupRole 
+SET GroupRoleId = -2500 
+WHERE GroupRoleId = -1150 
+AND GroupId = -2 
+AND RolePropertyId = -21300;
 
-IF EXISTS (SELECT * 
-           FROM YukonGroupRole 
-           WHERE GroupRoleId = -1151 
-           AND GroupId = -2 
-           AND RolePropertyId = -21301)
-    BEGIN
-        UPDATE YukonGroupRole Set GroupRoleId = -2501 WHERE GroupRoleId = -1151;
-    END 
+UPDATE YukonGroupRole 
+SET GroupRoleId = -2501 
+WHERE GroupRoleId = -1151 
+AND GroupId = -2 
+AND RolePropertyId = -21301;
 
-IF EXISTS (SELECT * 
-           FROM YukonGroupRole 
-           WHERE GroupRoleId = -1152 
-           AND GroupId = -2 
-           AND RolePropertyId = -21302)
-    BEGIN
-        UPDATE YukonGroupRole Set GroupRoleId = -2502 WHERE GroupRoleId = -1152;
-    END 
+UPDATE YukonGroupRole 
+SET GroupRoleId = -2502 
+WHERE GroupRoleId = -1152 
+AND GroupId = -2 
+AND RolePropertyId = -21302;
 
-IF EXISTS (SELECT * 
-           FROM YukonGroupRole 
-           WHERE GroupRoleId = -1153 
-           AND GroupId = -2 
-           AND RolePropertyId = -21303)
-    BEGIN
-        UPDATE YukonGroupRole Set GroupRoleId = -2503 WHERE GroupRoleId = -1153;
-    END 
+UPDATE YukonGroupRole 
+SET GroupRoleId = -2503 
+WHERE GroupRoleId = -1153
+AND GroupId = -2 
+AND RolePropertyId = -21303;
 
+UPDATE YukonGroupRole 
+SET GroupRoleId = -2504 
+WHERE GroupRoleId = -1154
+AND GroupId = -2 
+AND RolePropertyId = -21304;
 
-IF EXISTS (SELECT * 
-           FROM YukonGroupRole 
-           WHERE GroupRoleId = -1154 
-           AND GroupId = -2 
-           AND RolePropertyId = -21304)
-    BEGIN
-        UPDATE YukonGroupRole Set GroupRoleId = -2504 WHERE GroupRoleId = -1154;
-    END 
+UPDATE YukonGroupRole 
+SET GroupRoleId = -2505 
+WHERE GroupRoleId = -1155
+AND GroupId = -2 
+AND RolePropertyId = -21305;
 
+UPDATE YukonGroupRole 
+SET GroupRoleId = -2506 
+WHERE GroupRoleId = -1156
+AND GroupId = -2 
+AND RolePropertyId = -21306;
 
-IF EXISTS (SELECT * 
-           FROM YukonGroupRole 
-           WHERE GroupRoleId = -1155 
-           AND GroupId = -2 
-           AND RolePropertyId = -21305)
-    BEGIN
-        UPDATE YukonGroupRole Set GroupRoleId = -2505 WHERE GroupRoleId = -1155;
-    END 
-
-IF EXISTS (SELECT * 
-           FROM YukonGroupRole 
-           WHERE GroupRoleId = -1156 
-           AND GroupId = -2 
-           AND RolePropertyId = -21306)
-    BEGIN
-        UPDATE YukonGroupRole Set GroupRoleId = -2506 WHERE GroupRoleId = -1156;
-    END 
-
-IF EXISTS (SELECT * 
-           FROM YukonGroupRole 
-           WHERE GroupRoleId = -1157 
-           AND GroupId = -2 
-           AND RolePropertyId = -21307)
-    BEGIN
-        UPDATE YukonGroupRole Set GroupRoleId = -2507 WHERE GroupRoleId = -1157;
-    END 
+UPDATE YukonGroupRole 
+SET GroupRoleId = -2507 
+WHERE GroupRoleId = -1157
+AND GroupId = -2 
+AND RolePropertyId = -21307;
