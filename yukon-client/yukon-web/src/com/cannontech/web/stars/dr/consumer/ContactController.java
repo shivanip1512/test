6fae644a19582ee594e2f14b6578106232e926f8
Solberg, Aaron <>
2008-10-13 19:52:02 +0000

    YUKRV-405   YUK-5758 HECO: login not created for contact that resident/commercial customer creates.
    http://fisheye.cannontech.com/cru/YUKRV-405#c2267
    
    Removing old comment.

--- yukon-client/yukon-web/src/com/cannontech/web/stars/dr/consumer/ContactController.java
+++ yukon-client/yukon-web/src/com/cannontech/web/stars/dr/consumer/ContactController.java
@@ -176,7 +176,6 @@ public class ContactController extends AbstractConsumerController {
                     ServerUtils.handleDBChange(liteUser, DBChangeMsg.CHANGE_TYPE_ADD);
                     contact.setLoginID(login.getUserID().intValue());
                 }
-                // end hack
             }
             
             contactDao.saveContact(contact);
