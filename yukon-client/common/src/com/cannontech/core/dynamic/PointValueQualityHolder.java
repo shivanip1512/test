d7d19f484ee0f362d998b254122bc512447f3ea7
Mack, Tom <>
2008-11-03 20:30:37 +0000

    YUK-6281 BG&E 2009 Deliverable: Yukon Integration with TIBCO
    Sample API code checkin.

--- yukon-client/common/src/com/cannontech/core/dynamic/PointValueQualityHolder.java
+++ yukon-client/common/src/com/cannontech/core/dynamic/PointValueQualityHolder.java
@@ -1,6 +1,8 @@
 package com.cannontech.core.dynamic;
 
+import com.cannontech.common.point.PointQuality;
+
 
 public interface PointValueQualityHolder extends PointValueHolder {
-    public long getQuality();
+    public PointQuality getQualityEnum();
 }
