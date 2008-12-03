20508633a0ffb8307fd4fbfa7c52a309fe1e5317
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2008-12-03 23:26:53 +0000

    YUK-6634 Limit to 4 in 24 - Added type for Max interrupt hours in a 24 hour period. YUK-6636 Less than 4 hour control - Added type for Minimum event duration in minutes

--- yukon-client/common/src/com/cannontech/database/db/customer/CICustomerPointType.java
+++ yukon-client/common/src/com/cannontech/database/db/customer/CICustomerPointType.java
@@ -1,20 +1,27 @@
 package com.cannontech.database.db.customer;
 
 public enum CICustomerPointType {
+
+    // The UNIT values are restricted to 16 characters or less (CICustomerPointData.Type field)
+    // The LABEL values are restricted to 32 characters or less (CICustomerPointData.OptionalLabel field)
+    
     // original types
     Demand("","Demand"),
     Settlement("","Settlement"),
     Baseline("","Baseline"),
     Curtailable("","Curtailable"),
     
-    // isoc types
+    // isoc types 
     ContractFrmDmd("kW","Contract Firm Demand"),
     AdvBuyThroughKw("kW", "Advanced Buy Through kW"),
     AdvBuyThrough$("$/kW", "Advanced Buy Through Price"),
     InterruptHours("hours", "Max Interrupt Hours"),
     MinimumNotice("minutes", "Minimum Notice"),
     ContractIntLoad("kW", "Contract Interruptible Load"),
-    CurrentLoad("kW", "Current Load");
+    CurrentLoad("kW", "Current Load"),
+    MinEventDuration("minutes", "Minimum Event Duration Minutes"), //0 and 1440 both mean no limit
+    InterruptHrs24Hr("hours", "Max Interrupt Hours 24Hr Period"); //maximum number of hours controlled in 24 hour period
+    
     
     private final String unit;
     private final String label;
