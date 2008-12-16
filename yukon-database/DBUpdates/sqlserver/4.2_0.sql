f0b7e28fd66a3d944d815d06d00c0027cb0ab253
Kruse, Matt <>
2008-12-16 23:28:24 +0000

    YUK-6518, Add 'Manage Indexes' and 'View Logs' role properties to limit access to these "admin" type functions.
    - fixed yuk number.

--- yukon-database/DBUpdates/sqlserver/4.2_0.sql
+++ yukon-database/DBUpdates/sqlserver/4.2_0.sql
@@ -2,10 +2,10 @@
 /**** SQLServer 2000 DBupdates         ****/
 /******************************************/
 
-/* Start YUK-6240 */
+/* Start YUK-6518 */
 INSERT INTO YukonRoleProperty VALUES(-20015,-200,'Manage Indexes','false','Controls access to manually build Lucene indexes.'); 
 INSERT INTO YukonRoleProperty VALUES(-20016,-200,'View Logs','false','Controls access to view or download log files.');
-/* End YUK-6240 */
+/* End YUK-6518 */
 
 /**************************************************************/
 /* VERSION INFO                                               */
