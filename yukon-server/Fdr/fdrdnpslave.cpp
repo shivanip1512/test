cc2aa38be5cc4cb2c5632ffebc4caa458bbb1310
Ganz, Julie <>
2009-01-22 16:17:31 +0000

    YUK-6843 DNP Slave FDR Interface

---
+++ yukon-server/Fdr/fdrdnpslave.cpp
@@ -0,0 +1,546 @@
+
+/*
+ *
+ *    FILE NAME: fdrdnpslave.cpp
+ *
+ *    DATE: 03/07/2001
+ *
+ *    PVCS KEYWORDS:
+ *    ARCHIVE      :  $Archive:   Z:/SOFTWAREARCHIVES/YUKON/FDR/fdracs.cpp-arc  $
+ *    REVISION     :  $Revision$
+ *    DATE         :  $Date$
+ *
+ *
+ *    AUTHOR: David Sutton
+ *
+ *    PURPOSE: Interface to the Acs Foreign System
+ *
+ *
+ *    ---------------------------------------------------
+ *    History:
+ *      $Log$
+ *
+ *
+ *
+ *    Copyright (C) 2005 Cannon Technologies, Inc.  All rights reserved.
+ *
+ */
+#include "yukon.h"
+
+
+#include <windows.h>
+#include <iostream>
+
+#include <stdio.h>
+
+/** include files **/
+
+#include <rw/ctoken.h>
+#include "ctidate.h"
+#include "ctitime.h"
+#include "ctistring.h"
+#include <boost/tokenizer.hpp>
+
+#include "cparms.h"
+#include "msg_multi.h"
+#include "msg_ptreg.h"
+#include "msg_cmd.h"
+#include "message.h"
+#include "msg_reg.h"
+#include "msg_ptreg.h"
+#include "msg_pdata.h"
+#include "msg_signal.h"
+#include "connection.h"
+#include "pointtypes.h"
+#include "dllbase.h"
+#include "logger.h"
+#include "guard.h"
+#include "fdrpointlist.h"
+#include "fdrsocketinterface.h"
+#include "fdrscadaserver.h"
+#include "fdrdnphelper.h"
+#include "utility.h"
+// this class header
+#include "fdrdnpslave.h"
+#include "prot_dnp.h"
+#include "prot_base.h"
+#include "dnp_object_binaryinput.h"
+#include "dnp_object_counter.h"
+#include "dnp_objects.h"
+
+using namespace Cti::Protocol;
+/** local definitions **/
+
+/** global used to start the interface by c functions **/
+CtiFDRDnpSlave * dnpSlaveInterface;
+
+const CHAR * CtiFDRDnpSlave::KEY_LISTEN_PORT_NUMBER = "FDR_DNPSLAVE_PORT_NUMBER";
+const CHAR * CtiFDRDnpSlave::KEY_DB_RELOAD_RATE = "FDR_DNPSLAVE_DB_RELOAD_RATE";
+const CHAR * CtiFDRDnpSlave::KEY_DEBUG_MODE = "FDR_DNPSLAVE_DEBUG_MODE";
+const CHAR * CtiFDRDnpSlave::KEY_FDR_DNPSLAVE_SERVER_NAMES = "FDR_DNPSLAVE_SERVER_NAMES";
+const CHAR * CtiFDRDnpSlave::KEY_LINK_TIMEOUT = "FDR_DNPSLAVE_LINK_TIMEOUT_SECONDS";
+
+const string CtiFDRDnpSlave::dnpMasterId="MasterId";
+const string CtiFDRDnpSlave::dnpSlaveId="SlaveId";
+const string CtiFDRDnpSlave::dnpPointType="POINTTYPE";
+const string CtiFDRDnpSlave::dnpPointOffset="Offset";
+const string CtiFDRDnpSlave::dnpPointStatusString="Status";
+const string CtiFDRDnpSlave::dnpPointAnalogString="Analog";
+const string CtiFDRDnpSlave::dnpPointCounterString="PulseAccumulator";
+
+// Constructors, Destructor, and Operators
+CtiFDRDnpSlave::CtiFDRDnpSlave()
+: CtiFDRSocketServer(string("DNPSLAVE"))
+{}
+
+void CtiFDRDnpSlave::startup()
+{
+    init();
+    _helper = new CtiFDRDNPHelper<CtiDnpId>(this);
+}
+
+CtiFDRDnpSlave::~CtiFDRDnpSlave()
+{
+    delete _helper;
+}
+
+/*************************************************
+* Function Name: CtiFDRDnpSlave::config()
+*
+* Description: loads cparm config values
+*
+**************************************************
+*/
+int CtiFDRDnpSlave::readConfig()
+{
+    int         successful = TRUE;
+    string   tempStr;
+
+    setPortNumber(iConfigParameters.getValueAsInt( KEY_LISTEN_PORT_NUMBER, DNPSLAVE_PORTNUMBER));
+
+    setReloadRate(iConfigParameters.getValueAsInt(KEY_DB_RELOAD_RATE, 86400));
+
+    setLinkTimeout(iConfigParameters.getValueAsInt(KEY_LINK_TIMEOUT, 60));
+
+    
+    tempStr = iConfigParameters.getValueAsString(KEY_DEBUG_MODE);
+    if (tempStr.length() > 0)
+    {
+        setInterfaceDebugMode (true);
+    }
+    else
+    {
+        setInterfaceDebugMode (false);
+    }
+
+    tempStr = iConfigParameters.getValueAsString(KEY_FDR_DNPSLAVE_SERVER_NAMES);
+    std::string serverNames = tempStr;
+    typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
+    boost::char_separator<char> sep(",");
+    tokenizer pairTok(serverNames, sep);
+    for (tokenizer::iterator pairIter = pairTok.begin();
+         pairIter != pairTok.end();
+         ++pairIter)
+    {
+        boost::char_separator<char> innerSep("= ");
+        tokenizer innerTok(*pairIter, innerSep);
+        tokenizer::iterator first = innerTok.begin();
+        tokenizer::iterator end = innerTok.end();
+        if (first != end)
+        {
+            tokenizer::iterator second = first;
+            ++second;
+            if (second != end)
+            {
+                std::string serverName = *first;
+                std::string serverAddress = *second;
+                _serverNameLookup[serverAddress] = serverName;
+                if (getDebugLevel () & STARTUP_FDR_DEBUGLEVEL)
+                {
+                    CtiLockGuard<CtiLogger> doubt_guard(dout);
+                    logNow() << "Added server mapping: " << serverAddress
+                        << " -> " << serverName << endl;
+                }
+            }
+        }
+    }
+
+
+    if (getDebugLevel() & STARTUP_FDR_DEBUGLEVEL)
+    {
+        CtiLockGuard<CtiLogger> doubt_guard(dout);
+        dout << "----------------FDR-DNPSLAVE Configs------------------------------" << endl;
+        dout << "  " << KEY_LISTEN_PORT_NUMBER << ": "
+            << getPortNumber() << endl;
+
+        dout << "  " << KEY_DB_RELOAD_RATE << ": "
+            << getReloadRate() << endl;
+
+        dout << "  " << KEY_LINK_TIMEOUT << ": "
+            << getLinkTimeout() << "       second(s)" << endl;
+
+        dout << "  " << KEY_DEBUG_MODE << ": "
+            << (isInterfaceInDebugMode() ? "TRUE" : "FALSE") << endl;
+
+    }
+    return successful;
+}
+
+CtiFDRClientServerConnection* CtiFDRDnpSlave::createNewConnection(SOCKET newSocket)
+{
+    sockaddr_in peerAddr;
+    int peerAddrSize = sizeof(peerAddr);
+    getpeername(newSocket, (SOCKADDR*) &peerAddr, &peerAddrSize);
+    std::string ipString(inet_ntoa(peerAddr.sin_addr));
+    std::string connName;
+    ServerNameMap::const_iterator iter = _serverNameLookup.find(ipString);
+    if (iter == _serverNameLookup.end())
+    {
+        connName = ipString;
+    }
+    else
+    {
+        connName = _serverNameLookup[ipString];
+    }
+    CtiFDRClientServerConnection* newConnection;
+    newConnection = new CtiFDRClientServerConnection(connName.c_str(),
+                                                 newSocket,
+                                                 this);
+    newConnection->setRegistered(true); //DNPSLAVE doesn't have a separate registration message
+
+    // I'm not sure this is the best location for this
+    sendAllPoints(newConnection);
+
+    return newConnection;
+}
+
+
+
+bool CtiFDRDnpSlave::translateSinglePoint(CtiFDRPointSPtr & translationPoint, bool isSend)
+{
+    bool foundPoint = false;
+
+    for (int x = 0; x < translationPoint->getDestinationList().size(); x++)
+    {
+        foundPoint = true;
+        CtiFDRDestination pointDestination = translationPoint->getDestinationList()[x];
+        // translate and put the point id the list
+
+        CtiDnpId dnpId = ForeignToYukonId(pointDestination);
+        if (!dnpId.valid)
+            return foundPoint;
+
+        if (isSend)
+        {
+            _helper->addSendMapping(dnpId, pointDestination);
+        }
+    }
+    return foundPoint;
+}
+
+void CtiFDRDnpSlave::cleanupTranslationPoint(CtiFDRPointSPtr & translationPoint, bool recvList)
+{
+    if (recvList)
+        return;
+    for (int x = 0; x < translationPoint->getDestinationList().size(); x++)
+    {
+        CtiFDRDestination pointDestination = translationPoint->getDestinationList()[x];
+        // translate and put the point id the list
+
+              
+        CtiDnpId dnpId = ForeignToYukonId(pointDestination);
+        if (!dnpId.valid)
+            return;
+
+        if (!_helper->getIdForDestination(pointDestination, dnpId))
+        {
+            return;
+        }
+        
+        if (!recvList)
+        {
+            _helper->removeSendMapping(dnpId, pointDestination);
+        }
+    }
+}
+
+bool CtiFDRDnpSlave::buildForeignSystemMessage(const CtiFDRDestination& destination,
+                                               char** buffer,
+                                               unsigned int& bufferSize)
+{
+    bufferSize = 0;
+    return false;
+}
+
+int CtiFDRDnpSlave::processMessageFromForeignSystem (CtiFDRClientServerConnection& connection,
+                                         char* data, unsigned int size)
+{
+    BYTEUSHORT dest, src;
+    unsigned long function = getHeaderBytes(data, size);
+
+    switch (function)
+    {
+        case SINGLE_SOCKET_DNP_READ:
+        {
+            /******************************************************************************************************** 
+              here should add something to _dnpData.slaveDecode()  However, for now, this will process any read command
+              as a scan1230 request.
+              NOTE:  yukon doesn't send the correct scan integrity dnp message out anyway.  it is a time and date message
+                05 64 17 c4 1e 00 02 00 78 b5 c0 ca 01 32 01 06 3c 02 06 3c 03 06 3c 04 06 3c 9d f5 01 06 75 e1
+             ********************************************************************************************************/
+            if (size > FDR_DNP_HEADER_SIZE)
+            {
+                processScanSlaveRequest (connection, data,size);
+            }
+            break;
+        }
+        case SINGLE_SOCKET_DNP_CONFIRM:  
+        case SINGLE_SOCKET_DNP_WRITE:    
+        case SINGLE_SOCKET_DNP_DIRECT_OP:
+        default:
+        { 
+            if (getDebugLevel() & DETAIL_FDR_DEBUGLEVEL)
+            {
+                CtiLockGuard<CtiLogger> dout_guard(dout);
+                logNow() << " "<< getInterfaceName() <<" received an unsupported DNP message, response not generated. "<< endl;
+            }
+            break;
+        }
+    }
+    return 0;
+}
+
+int CtiFDRDnpSlave::processScanSlaveRequest (CtiFDRClientServerConnection& connection,
+                                         char* data, unsigned int size)
+{
+
+    int retVal = 0;
+    
+    CtiXfer xfer = CtiXfer(NULL, 0, (BYTE*)data, getMessageSize(data));
+    
+    BYTEUSHORT dest, src;
+    dest.ch[0] = data[4];
+    dest.ch[1] = data[5];
+    src.ch[0] = data[6];
+    src.ch[1] = data[7];
+
+
+    std::map<CtiFDRDestination, CtiDnpId> sendMap = _helper->getSendMappings();
+    for (std::map<CtiFDRDestination, CtiDnpId>::iterator iter = sendMap.begin(); iter != sendMap.end(); iter++)
+    {
+        CtiDnpId dnpId = iter->second;
+        if (dnpId.SlaveId == dest.sh && dnpId.MasterId == src.sh )
+        {
+            CtiFDRDestination dest = iter->first;
+            CtiFDRPoint* fdrPoint = dest.getParentPoint();
+
+            Cti::Protocol::DNPSlaveInterface::input_point iPoint;
+
+            iPoint.onLine = YukonToForeignQuality(fdrPoint->getQuality());
+            iPoint.control_offset = dnpId.Offset;
+
+            if (dnpId.PointType == StatusPointType )
+            {
+                if (fdrPoint->getValue() == 0)
+                    iPoint.din.trip_close = DNP::BinaryOutputControl::TripClose::Trip;
+                else
+                    iPoint.din.trip_close =  DNP::BinaryOutputControl::TripClose::Close;
+                iPoint.type = Cti::Protocol::DNPSlaveInterface::InputPointType::DigitalInput;
+            }
+            else if (dnpId.PointType == AnalogPointType )
+            {
+                iPoint.ain.value =  fdrPoint->getValue();
+                iPoint.type = Cti::Protocol::DNPSlaveInterface::InputPointType::AnalogInputType;
+            }
+            else if (dnpId.PointType == PulseAccumulatorPointType )
+            {
+                iPoint.counterin.value =  fdrPoint->getValue();
+                iPoint.type = Cti::Protocol::DNPSlaveInterface::InputPointType::Counters;
+            }
+            else 
+            {
+                continue;
+                    
+            }
+            _dnpData.getInputPointList()->push_back(iPoint);
+        }
+    }
+
+    _dnpData.setAddresses(src.sh, dest.sh);
+    _dnpData.setOptions(DNPSlaveInterface::Options_SlaveResponse);
+    _dnpData.setSlaveCommand(DNPSlaveInterface::Command_Class1230Read);
+
+    
+    if (_dnpData.slaveGenerate(xfer) == 0)
+    {
+        bool status = true;
+        
+        if (xfer.getOutBuffer() != NULL && xfer.getOutCount() > 0)
+        {
+            char* buffer = NULL;
+            unsigned int bufferSize = xfer.getOutCount();
+            buffer = new CHAR[bufferSize];
+            memcpy(buffer, xfer.getOutBuffer(), bufferSize);
+            
+            connection.queueMessage(buffer, xfer.getOutCount(), MAXPRIORITY - 1); 
+        }
+    }
+    else
+    {
+        if (getDebugLevel() & DETAIL_FDR_DEBUGLEVEL)
+        {
+            CtiLockGuard<CtiLogger> dout_guard(dout);
+            logNow() << " "<< getInterfaceName() <<" was not able to generate scan response. "<< endl;
+        }
+        
+    }
+
+    _dnpData.slaveTransactionComplete();
+
+    return retVal;
+
+}
+
+
+
+        
+
+CtiDnpId CtiFDRDnpSlave::ForeignToYukonId(CtiFDRDestination pointDestination)
+{
+    CtiDnpId dnpId;
+
+    string masterId  = pointDestination.getTranslationValue(dnpMasterId);
+    string slaveId   = pointDestination.getTranslationValue(dnpSlaveId);
+    string pointType = pointDestination.getTranslationValue(dnpPointType);
+    string dnpOffset = pointDestination.getTranslationValue(dnpPointOffset);
+
+    if (masterId.empty() || slaveId.empty() || pointType.empty() || dnpOffset.empty())
+    {
+        CtiLockGuard<CtiLogger> doubt_guard(dout);
+        logNow() << "Unable to add destination " << pointDestination
+            << " because one of the fields was blank" << endl;
+        dnpId.valid = false;
+        return dnpId;
+    }
+
+    dnpId.MasterId = atoi(masterId.c_str());
+    dnpId.SlaveId = atoi(slaveId.c_str());
+
+    if (!stringCompareIgnoreCase(pointType.c_str(), dnpPointStatusString)) 
+        dnpId.PointType = StatusPointType;
+    else if (!stringCompareIgnoreCase(pointType.c_str(), dnpPointAnalogString)) 
+        dnpId.PointType = AnalogPointType;
+    else if (!stringCompareIgnoreCase(pointType.c_str(), dnpPointCounterString)) 
+        dnpId.PointType = PulseAccumulatorPointType;
+    else dnpId.PointType = InvalidPointType;
+
+    dnpId.Offset = atoi(dnpOffset.c_str());
+    dnpId.MasterServerName = pointDestination.getDestination();
+    dnpId.valid = true;
+
+    return dnpId;
+}
+
+
+bool CtiFDRDnpSlave::YukonToForeignQuality(USHORT aQuality)
+{
+
+    bool goodQuality = false;
+    if (aQuality == ManualQuality ||
+        aQuality == NormalQuality)
+    {
+        goodQuality = true;
+    }
+    return goodQuality;
+}
+
+
+
+unsigned long CtiFDRDnpSlave::getHeaderBytes(const char* data, unsigned int size)
+{
+    unsigned long retVal = -1;
+    if (size >= FDR_DNP_HEADER_SIZE)
+    {
+        if (data[0] == FDR_DNP_HEADER_BYTE1 &&
+            data[1] == FDR_DNP_HEADER_BYTE2 )
+        { 
+            long function = (BYTE)* (data + FDR_DNP_HEADER_SIZE);
+            retVal = function;
+        }
+    }
+    return retVal;
+    
+}
+
+unsigned int CtiFDRDnpSlave::getMessageSize(const char* data)
+{
+    BYTE msgSize = 0;
+    if (data != NULL)
+    {
+        msgSize =  (BYTE)*(data + 2);
+
+        msgSize += ( (msgSize / 16) * 2);
+        msgSize += 7; //x05 x64 Len(1) headerCRC(2)  finalCRC(2)
+    }
+    return msgSize;
+}
+
+
+/****************************************************************************************
+*
+*      Here Starts some C functions that are used to Start the
+*      Interface and Stop it from the Main() of FDR.EXE.
+*
+*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/************************************************************************
+* Function Name: Extern C int RunInterface(void)
+*
+* Description: This is used to Start the Interface from the Main()
+*              of FDR.EXE. Each interface it Dynamicly loaded and
+*              this function creates a global FDRCygnet Object and then
+*              calls its run method to cank it up.
+*
+*************************************************************************
+*/
+
+    DLLEXPORT int RunInterface(void)
+    {
+
+        // make a point to the interface
+        dnpSlaveInterface = new CtiFDRDnpSlave();
+        dnpSlaveInterface->startup();
+        // now start it up
+        return dnpSlaveInterface->run();
+    }
+
+/************************************************************************
+* Function Name: Extern C int StopInterface(void)
+*
+* Description: This is used to Stop the Interface from the Main()
+*              of FDR.EXE. Each interface it Dynamicly loaded and
+*              this function stops a global FDRCygnet Object and then
+*              deletes it.
+*
+*************************************************************************
+*/
+    DLLEXPORT int StopInterface( void )
+    {
+
+        dnpSlaveInterface->stop();
+        delete dnpSlaveInterface;
+        dnpSlaveInterface = 0;
+
+        return 0;
+    }
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
