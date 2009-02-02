413dc198654bb512cc69a844138506f996494d4b
Kruse, Matt <>
2009-02-02 17:40:44 +0000

    YUK-6518, Add 'Manage Indexes' and 'View Logs' role properties to limit access to these "admin" type functions.
    - added a role property for Managing indexes and viewing logs to the 4.2_1 scripts with an error ignore.

--- yukon-database/DBUpdates/oracle/4.2_1.sql
+++ yukon-database/DBUpdates/oracle/4.2_1.sql
@@ -13,6 +13,13 @@ INSERT INTO YukonRoleProperty
 VALUES(-1603,-7,'Msp LM Interface Mapping Setup','false','Controls access to setup the MultiSpeak LM interface mappings.');
 /* End YUK-6878 */
 
+/* Start YUK-6518 */
+/* @error ignore-begin */
+INSERT INTO YukonRoleProperty VALUES(-20015,-200,'Manage Indexes','true','Controls access to manually build Lucene indexes.');
+INSERT INTO YukonRoleProperty VALUES(-20016,-200,'View Logs','true','Controls access to view or download log files.');
+/* @error ignore-end */
+/* End YUK-6518 */
+
 /**************************************************************/
 /* VERSION INFO                                               */
 /*   Automatically gets inserted from build script            */
