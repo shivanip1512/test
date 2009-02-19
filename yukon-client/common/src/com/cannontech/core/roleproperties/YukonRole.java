a5e94c73f44e292d1ab6cfb87a79ba1cad2f5ce3
Mack, Tom <>
2009-02-19 22:34:55 +0000

    YUK-6864 Check RoleProperty must require check on Role also
    Moved energy company role to the correct category. Fixed other roles as well.

--- yukon-client/common/src/com/cannontech/core/roleproperties/YukonRole.java
+++ yukon-client/common/src/com/cannontech/core/roleproperties/YukonRole.java
@@ -18,7 +18,6 @@ import static com.cannontech.core.roleproperties.YukonRoleCategory.*;
 
 public enum YukonRole {
     APPLICATION_BILLING(Application, ApplicationRoleDefs.BILLING_ROLEID),
-    CALC_HISTORICAL(Application, ApplicationRoleDefs.CALC_HISTORICAL_ROLEID),
     COMMANDER(Application, ApplicationRoleDefs.COMMANDER_ROLEID),
     DATABASE_EDITOR(Application, ApplicationRoleDefs.DATABASE_EDITOR_ROLEID),
     APPLICATION_ESUBSTATION_EDITOR(Application, ApplicationRoleDefs.ESUBSTATION_EDITOR_ROLEID),
@@ -26,34 +25,42 @@ public enum YukonRole {
     TABULAR_DISPLAY_CONSOLE(Application, ApplicationRoleDefs.TABULAR_DISPLAY_CONSOLE_ROLEID),
     TRENDING(Application, ApplicationRoleDefs.TRENDING_ROLEID),
     WEB_CLIENT(Application, ApplicationRoleDefs.WEB_CLIENT_ROLEID),
-    WEB_GRAPH(Application, ApplicationRoleDefs.WEB_GRAPH_ROLEID),
+    
     CI_ESUBSTATION_DRAWINGS(CiCustomer, CICustomerRoleDefs.ESUBSTATION_DRAWINGS_ROLEID),
     USER_CONTROL(CiCustomer, CICustomerRoleDefs.USER_CONTROL_ROLEID),
+    
     CBC_SETTINGS(CapControl, CapControlRoleDefs.CBC_SETTINGS_ROLEID),
     CBC_ONELINE_CAP_SETTINGS(CapControl, CBCOnelineSettingsRole.CAP_ROLEID),
     CBC_ONELINE_FEEDER_SETTINGS(CapControl, CBCOnelineSettingsRole.FDR_ROLEID),
     CBC_ONELINE_SUB_SETTINGS(CapControl, CBCOnelineSettingsRole.SUB_ROLEID),
+    
     RESIDENTIAL_CUSTOMER(Consumer, ConsumerRoleDefs.RESIDENTIAL_CUSTOMER_ROLEID),
+    
     LM_DIRECT_LOADCONTROL(LoadControl, LMRoleDefs.DIRECT_LOADCONTROL_ROLEID),
+    
     NOTIFICATION_CONFIGURATION(Notifications, NotificationsRoleDefs.CONFIGURATION_ROLEID),
     IVR(Notifications, NotificationsRoleDefs.IVR_ROLEID),
+    
     OPERATOR_ADMINISTRATOR(Operator, OperatorRoleDefs.ADMINISTRATOR_ROLEID),
     CI_CURTAILMENT(Operator, OperatorRoleDefs.CI_CURTAILMENT_ROLEID),
     CONSUMER_INFO(Operator, OperatorRoleDefs.CONSUMER_INFO_ROLEID),
     DEVICE_ACTIONS(Operator, OperatorRoleDefs.DEVICE_ACTIONS_ROLEID),
+    ENERGY_COMPANY(Operator, YukonRoleDefs.ENERGY_COMPANY_ROLEID),
     OPERATOR_ESUBSTATION_DRAWINGS(Operator, OperatorRoleDefs.ESUBSTATION_DRAWINGS_ROLEID),
     INVENTORY(Operator, OperatorRoleDefs.INVENTORY_ROLEID),
     METERING(Operator, OperatorRoleDefs.METERING_ROLEID),
     ODDS_FOR_CONTROL(Operator, OperatorRoleDefs.ODDS_FOR_CONTROL_ROLEID),
     SCHEDULER(Operator, OperatorRoleDefs.SCHEDULER_ROLEID),
     WORK_ORDER(Operator, OperatorRoleDefs.WORK_ORDER_ROLEID),
+    
     AUTHENTICATION(System, YukonRoleDefs.AUTHENTICATION_ROLEID),
     SYSTEM_BILLING(System, YukonRoleDefs.BILLING_ROLEID),
+    CALC_HISTORICAL(System, ApplicationRoleDefs.CALC_HISTORICAL_ROLEID),
     SYSTEM_CONFIGURATION(System, YukonRoleDefs.CONFIGURATION_ROLEID),
-    ENERGY_COMPANY(System, YukonRoleDefs.ENERGY_COMPANY_ROLEID),
     MULTISPEAK(System, YukonRoleDefs.MULTISPEAK_ROLEID),
-    SYSTEM(System, YukonRoleDefs.SYSTEM_ROLEID),
     VOICE_SERVER(System, YukonRoleDefs.VOICE_SERVER_ROLEID),
+    WEB_GRAPH(System, ApplicationRoleDefs.WEB_GRAPH_ROLEID),
+    SYSTEM(System, YukonRoleDefs.SYSTEM_ROLEID),
     ;
     
     private final YukonRoleCategory category;
