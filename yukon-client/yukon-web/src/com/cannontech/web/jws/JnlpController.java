1a4d254dd526e5d29acdf8910e9d8c6342301eb0
Peterson, Michael <>
2009-04-16 18:22:00 +0000

    YUK-7211 Windows Internet Explorer Error gets displayed when trying to launch the Java Editors from the Secure webpage - Both the of the headers 'Cache-Control' and 'Pragma' need to be set to something other than 'no-cache'. - If a value is not set in the response, they are defaulted to 'no-cache'. IE will respect that one of them is set to no-cache, and it will not store the .jnlp file.

--- yukon-client/yukon-web/src/com/cannontech/web/jws/JnlpController.java
+++ yukon-client/yukon-web/src/com/cannontech/web/jws/JnlpController.java
@@ -46,7 +46,8 @@ public class JnlpController extends AbstractController {
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
         response.setContentType("application/x-java-jnlp-file");
         response.setHeader("Cache-Control", "private");
-
+        response.setHeader("Pragma", "private");
+        
         Document doc = new Document();
         Element jnlpElem = new Element("jnlp");
         doc.addContent(jnlpElem);
