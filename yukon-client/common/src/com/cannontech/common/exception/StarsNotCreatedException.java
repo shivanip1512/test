72decf16e1d89e42549d4eb9a6f9001419988d2e
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2009-04-20 20:14:26 +0000

    YUK-7339  DBUpdateFrame - must warn when STARS tables do not exist. - Added check for STARS exists before running db updates.

--- yukon-client/common/src/com/cannontech/common/exception/StarsNotCreatedException.java
+++ yukon-client/common/src/com/cannontech/common/exception/StarsNotCreatedException.java
@@ -4,12 +4,9 @@ public class StarsNotCreatedException extends RuntimeException {
 
 	public StarsNotCreatedException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public StarsNotCreatedException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
-
 }
