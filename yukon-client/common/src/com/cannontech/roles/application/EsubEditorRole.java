026175663ff39e6652dca02392c9c98164a62d2f
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2009-04-20 16:55:44 +0000

    YUK-7236 ''unknown role property' message in Webserver log. clean up issue? - Added default property value for EsubEditorRole.  Required for Role to actually be added to YukonGroupRole.

--- yukon-client/common/src/com/cannontech/roles/application/EsubEditorRole.java
+++ yukon-client/common/src/com/cannontech/roles/application/EsubEditorRole.java
@@ -8,5 +8,6 @@ import com.cannontech.roles.ApplicationRoleDefs;
 public interface EsubEditorRole {
 	public static final int ROLEID = ApplicationRoleDefs.ESUBSTATION_EDITOR_ROLEID;
 	
-//	public static final int LOG_LEVEL = ApplicationRoleDefs.ESUBSTATION_EDITOR_PROPERTYID_BASE;
+	/** Default property required for existence of the Role itself **/
+	public static final int ESUB_EDITOR_ROLE_EXITS = ApplicationRoleDefs.ESUBSTATION_EDITOR_PROPERTYID_BASE;
 }
