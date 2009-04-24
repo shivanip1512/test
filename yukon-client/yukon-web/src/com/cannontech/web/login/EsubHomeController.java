3198550342139c9a0c60b33b601c3a546245e37b
Solberg, Aaron <>
2009-04-24 18:25:53 +0000

    YUK-6125 Link between Operations and eSubstation.
    - Added eSubstation module to Operations page and dropdown menu on standard page header driven by new role property esub home url.
    
    Missed these files on the first checkin.

---
+++ yukon-client/yukon-web/src/com/cannontech/web/login/EsubHomeController.java
@@ -0,0 +1,37 @@
+package com.cannontech.web.login;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.cannontech.core.roleproperties.YukonRoleProperty;
+import com.cannontech.core.roleproperties.dao.RolePropertyDao;
+import com.cannontech.database.data.lite.LiteYukonUser;
+import com.cannontech.util.ServletUtil;
+
+@Controller
+public class EsubHomeController {
+    private RolePropertyDao rolePropertyDao;
+    
+    @RequestMapping(value = "/esub/home", method = RequestMethod.GET)
+    public ModelAndView home(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        
+        final LiteYukonUser user = ServletUtil.getYukonUser(request);
+        String esubHomeUrl = ServletUtil.createSafeUrl(request, rolePropertyDao.getPropertyStringValue(YukonRoleProperty.ESUB_HOME_URL, user));
+        
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("redirect:" + esubHomeUrl);
+        return mav;
+    }
+    
+    @Autowired
+    public void setRolePropertyDao(RolePropertyDao rolePropertyDao) {
+        this.rolePropertyDao = rolePropertyDao;
+    }
+    
+}
