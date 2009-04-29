2bbbf03e12d6b90b08ac5f238d80718eda747d4d
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2009-04-29 14:15:29 +0000

    YUK-7263 At Forgot Password entry page, all info was entered but still received a 'Info does not match' message. - Added OPERATOR_ROTATION_DIGIT_LENGTH to exclusion list.

--- yukon-client/common/src/com/cannontech/core/roleproperties/dao/impl/RolePropertyDaoImpl.java
+++ yukon-client/common/src/com/cannontech/core/roleproperties/dao/impl/RolePropertyDaoImpl.java
@@ -200,6 +200,7 @@ public class RolePropertyDaoImpl implements RolePropertyDao {
         	propertyExceptions.add(YukonRoleProperty.TEMPLATE_ROOT);	//YUK-6977, YUK-7073
         	propertyExceptions.add(YukonRoleProperty.METER_MCT_BASE_DESIGNATION);   //YUK-6978
         	propertyExceptions.add(YukonRoleProperty.OPERATOR_ACCOUNT_NUMBER_LENGTH);   //YUK-7263, YUK-7327
+        	propertyExceptions.add(YukonRoleProperty.OPERATOR_ROTATION_DIGIT_LENGTH);	//YUK-7263, YUK-411
         	
             String[] exceptionArray = exceptionList.split("\\s*,\\s*");
             for (String propertyStr : exceptionArray) {
