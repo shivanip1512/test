e2da62d44142020e560de6c6a22a444a2e8c462c
Johnston, Samuel T <SamuelTJohnston@Eaton.com>
2009-04-29 18:17:29 +0000

    Lowered the max heap setting again - ant was still erroring out on some machines building yukon-client.

--- yukon-client/build/ant.bat
+++ yukon-client/build/ant.bat
@@ -1,6 +1,6 @@
 @echo off
 REM $Id: ant.bat,v 1.6 2007/02/21 20:13:45 softwarebuild Exp $
 REM set ANT_HOME=ant
-set ANT_OPTS=-Xmx1500m
+set ANT_OPTS=-Xmx800m
 set ANT_HOME=%~dp0ant
 "%ANT_HOME%"\bin\ant.bat %*
