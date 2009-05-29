1764f13f111f6736f584248653ec1e2848eeebdd
Peterson, Michael <>
2009-05-29 19:45:50 +0000

    YUK-7484 Bulk Import/Update needs to support setting the Device Group - initial check in

---
+++ yukon-client/yukon-web/WebContent/WEB-INF/pages/group/updater/upload.jsp
@@ -0,0 +1,101 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://cannontech.com/tags/cti" prefix="cti" %>
+<%@ taglib prefix="tags" tagdir="/WEB-INF/tags" %>
+
+<cti:standardPage title="Device Group Updater" module="amr">
+
+    <cti:standardMenu menuSelection="devicegroups|updater"/>
+
+   	<cti:breadCrumbs>
+	   <cti:crumbLink url="/operator/Operations.jsp" title="Operations Home" />
+	   &gt; File Upload
+  	</cti:breadCrumbs>
+        
+	<tags:boxContainer title="File Upload" id="updaterContainer" hideEnabled="false">
+	
+		<table>
+			<tr valign="top">
+				<td width="30%" style="padding-right:20px;">
+	
+					<form id="uploadForm" method="post" action="/spring/group/updater/parseUpload" enctype="multipart/form-data">
+					
+						<%-- file select --%>
+			            <div class="normalBoldLabel" style="display:inline;">Update File:</div>
+			            <input type="file" name="dataFile" size="30px">
+			            <tags:slowInput myFormId="uploadForm" label="Process" labelBusy="Processing" />
+					
+					</form>
+					
+					<br>
+					<c:choose>
+						<c:when test="${not empty error}">
+							<div class="errorRed">${error}</div>
+						</c:when>
+						<c:when test="${success}">
+							<div class="okGreen">Successfully updated device groups.</div>
+						</c:when>
+					</c:choose>
+				
+				</td>
+				
+				<%-- INSTRUCTIONS --%>
+                <td>
+                
+                    <div class="normalBoldLabel">Instructions:</div>
+                    <ul style="font-size:11px;">
+                        <li>Files must contain a header row with valid column identifiers as listed in the table below.</li><br>
+						<li>The first column is always the Identifier Column.<br>It will be used to determine which device is to be updated.</li><br>
+						<li>One of the following column identifiers must be used in the Identifier Column:<br><span style="font-weight:bold;">ADDRESS, METER_NUMBER, NAME, or DEVICE_ID.</span></li><br>
+						<li>All column headers (besides the Identifier column) should be of the form<br>
+							<b>DEVICE_GROUP_PREFIX:&lt;<i>Group Name</i>&gt;</b> or <b>DEVICE_GROUP_INCLUDE:&lt;<i>Group Name</i>&gt;</b><br>
+							Descriptions of the two column header types are listed in the table below.
+						</li><br>
+						<li>Rows are processed top to bottom, columns are processed left to right.</li><br>
+						<li>Groups will be automatically be created if they do not yet exist.</li>
+                    </ul>
+                    
+                    <%-- sample files --%>
+                    <div class="small">
+                        <div class="normalBoldLabel" style="display:inline;"><cti:msg key="yukon.common.device.bulk.importUpload.sampleFilesLabel"/>:</div>
+                        <a href="<cti:url value="/WebConfig/custom/sample_bulk_files/Sample_DeviceGroup_Update_File1.csv"/>">File 1</a>, 
+                        <a href="<cti:url value="/WebConfig/custom/sample_bulk_files/Sample_DeviceGroup_Update_File2.csv"/>">File 2</a>, 
+                        <a href="<cti:url value="/WebConfig/custom/sample_bulk_files/Sample_DeviceGroup_Update_File3.csv"/>">File 3</a>
+                    </div>
+                    
+                </td>
+              </tr>
+                
+            <tr>
+                <td></td>
+                <td style="padding-top:20px;">
+                
+                	<table class="miniResultsTable">
+                		<tr >
+                			<th style="width:20%;">Column Header</th>
+                			<th>Description</th>
+                		</tr>
+                		
+                		<tr>
+                			<td class="normalBoldLabel">DEVICE_GROUP_PREFIX</td>
+                			<td>The full path of the group to be prefixed to the group name value in each row.<br><br>
+                				Example: If the header is <b>DEVICE_GROUP_PREFIX:/Meter/Collection</b>, and the value in the row is <b>A</b>, then the device will be added to <b>/Meters/Collection/A</b>.<br><br>
+                				In addition, the device will be removed from all other groups under <b>/Meters/Collection<b>.<br><br>
+                			</td>
+                		</tr>
+                		
+                		<tr>
+                			<td class="normalBoldLabel">DEVICE_GROUP_INCLUDE</td>
+                			<td>The full path of a group to either add or remove the device from.<br><br>
+                				Example: If the header is <b>DEVICE_GROUP_INCLUDE:/Meters/Extra</b>, and the value in the row is <b>true</b>, then the device will be added to the group. If the value is <b>false</b>, it will be removed from the group.<br><br>
+                			</td>
+                		</tr>
+                	</table>
+                	
+                </td>
+            </tr>
+				
+		</table>
+	
+	</tags:boxContainer>
+	
+</cti:standardPage>
---
+++ yukon-client/yukon-web/src/com/cannontech/web/group/DeviceGroupUpdaterController.java
@@ -0,0 +1,238 @@
+package com.cannontech.web.group;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.ServletRequestUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.cannontech.common.bulk.field.BulkField;
+import com.cannontech.common.bulk.field.BulkFieldColumnHeader;
+import com.cannontech.common.bulk.field.BulkFieldService;
+import com.cannontech.common.bulk.field.impl.BulkYukonDeviceFieldFactory;
+import com.cannontech.common.bulk.field.processor.BulkFieldProcessor;
+import com.cannontech.common.bulk.mapper.ObjectMappingException;
+import com.cannontech.common.device.YukonDevice;
+import com.cannontech.common.device.groups.editor.dao.DeviceGroupEditorDao;
+import com.cannontech.common.device.groups.editor.dao.DeviceGroupMemberEditorDao;
+import com.cannontech.common.device.groups.editor.model.StoredDeviceGroup;
+import com.cannontech.database.data.lite.LiteYukonUser;
+import com.cannontech.tools.csv.CSVReader;
+
+@Controller
+@RequestMapping("/updater/*")
+public class DeviceGroupUpdaterController {
+	
+	private BulkYukonDeviceFieldFactory bulkYukonDeviceFieldFactory;
+	private BulkFieldService bulkFieldService;
+	private DeviceGroupEditorDao deviceGroupEditorDao;
+	private DeviceGroupMemberEditorDao deviceGroupMemberEditorDao;
+
+	@RequestMapping
+    public void upload(HttpServletRequest request, LiteYukonUser user, ModelMap model) throws ServletException {
+
+		String error = ServletRequestUtils.getStringParameter(request, "error", null);
+		boolean success = ServletRequestUtils.getBooleanParameter(request, "success", false);
+		
+		model.addAttribute("error", error);
+		model.addAttribute("success", success);
+    }
+	
+	@RequestMapping
+    public String parseUpload(HttpServletRequest request, LiteYukonUser user, ModelMap model) throws ServletException, IOException {
+
+		String error = null;
+		
+		MultipartHttpServletRequest mRequest = (MultipartHttpServletRequest)request;
+        MultipartFile dataFile = mRequest.getFile("dataFile");
+        
+        // get file from request
+        if (dataFile == null || StringUtils.isBlank(dataFile.getOriginalFilename())) {
+        	error = "No file selected.";
+        } else {
+        	
+            InputStream inputStream = dataFile.getInputStream();
+            if (inputStream.available() <= 0) {
+            	error = "File is empty.";
+            }
+            
+            // header row
+            InputStreamReader inputStreamReader = new InputStreamReader(dataFile.getInputStream());
+            CSVReader csvReader = new CSVReader(inputStreamReader);
+            String[] headerRow = csvReader.readNext();
+            
+            if (headerRow.length < 2) {
+            	error = "File header should contain an Identifier column and at least one action column.";
+            } else {
+            
+            	int currentLineNumber = 0;
+	            String currentIdentifier = "";
+	            String currentColumnValue = "";
+	            BulkField<?, YukonDevice> identifierBulkField = null;
+	            
+            	try {
+		            
+		            // identifier bulk field
+		            BulkFieldColumnHeader identifierColunHeader = BulkFieldColumnHeader.valueOf(headerRow[currentLineNumber++].trim());
+		            identifierBulkField = bulkYukonDeviceFieldFactory.getBulkField(identifierColunHeader.getFieldName());
+		            
+		            // processors
+		            DeviceGroupProcessorFactory deviceGroupProcessorFactory = new DeviceGroupProcessorFactory();
+		            
+		            List<BulkFieldProcessor<YukonDevice, String>> processors = new ArrayList<BulkFieldProcessor<YukonDevice, String>>();
+		            for (int columnIdx = 1; columnIdx < headerRow.length; columnIdx++) {
+		            	
+		            	String header = headerRow[columnIdx].trim();
+		            	
+	            		String[] parts = header.split(":");
+	            		String processorType = parts[0];
+	            		String groupName = parts[1];
+	            		
+	            		processors.add(deviceGroupProcessorFactory.getProcessor(processorType, groupName));
+		            }
+		            
+		            // process rows
+	            	String [] line = null;
+		            while((line = csvReader.readNext()) != null) {
+		            		
+		                currentIdentifier = StringUtils.trim(line[0]);
+		                YukonDevice device = bulkFieldService.getYukonDeviceForIdentifier(identifierBulkField, currentIdentifier);
+		                
+		                int idx = 1;
+		                for (BulkFieldProcessor<YukonDevice, String> processor : processors) {
+		                	
+		                	currentColumnValue = line[idx++].trim();
+	                		processor.updateField(device, currentColumnValue);
+		                }
+		                
+		                currentLineNumber++;
+		            }
+		            
+	            } catch (ObjectMappingException e) {
+	            	error = "Error (line " + currentLineNumber + "): No device with " + identifierBulkField.getInputSource().getDisplayName() + ": " + currentIdentifier + ".";
+	            } catch (IndexOutOfBoundsException e) {
+	            	error = "Error (line " + currentLineNumber + "): Incompete row, each row must have a value for each header column.";
+	            } catch (IllegalArgumentException e) {
+	            	error = "Error (line 1): Invalid header column. Should begin with DEVICE_GROUP_PREFIX:, or DEVICE_GROUP_INCLUDE:";
+	        		
+	        	} finally {
+	        		csvReader.close();
+	        	}
+            }
+        }
+        
+        model.addAttribute("error", error);
+        model.addAttribute("success", error == null);
+        return "redirect:upload";
+    }
+	
+	
+	
+	// PREFIX PROCESSOR
+	private class DeviceGroupPrefixProcessor implements BulkFieldProcessor<YukonDevice, String> {
+
+		private StoredDeviceGroup group;
+		
+		public DeviceGroupPrefixProcessor (String groupName) {
+			this.group = deviceGroupEditorDao.getStoredGroup(groupName, true);
+		}
+
+		@Override
+		public void updateField(YukonDevice device, String subgroup) {
+
+			// remove from all child groups
+			for (StoredDeviceGroup childGroup : deviceGroupEditorDao.getStaticGroups(this.group)) {
+				removeDeviceFromChildGroup(childGroup, device);
+			}
+			
+			// find subgroup, add device to it
+			StoredDeviceGroup subgroupGroup = deviceGroupEditorDao.getGroupByName(this.group, subgroup, true);
+			deviceGroupMemberEditorDao.addDevices(subgroupGroup, device);
+		}
+		
+		private void removeDeviceFromChildGroup(StoredDeviceGroup removeFromGroup, YukonDevice device) {
+			
+			deviceGroupMemberEditorDao.removeDevices(removeFromGroup, device);
+			
+			for (StoredDeviceGroup childGroup : deviceGroupEditorDao.getStaticGroups(removeFromGroup)) {
+				removeDeviceFromChildGroup(childGroup, device);
+			}
+		}
+		
+		@Override
+		public Set<BulkField<?, YukonDevice>> getUpdatableFields() {
+			return null;
+		}
+	}
+	
+	// GROUP PROCESSOR
+	private class DeviceGroupGroupProcessor implements BulkFieldProcessor<YukonDevice, String> {
+		
+		private StoredDeviceGroup group;
+		
+		public DeviceGroupGroupProcessor (String groupName) {
+			this.group = deviceGroupEditorDao.getStoredGroup(groupName, true);
+		}
+
+		@Override
+		public void updateField(YukonDevice device, String addStr) {
+
+			boolean add = Boolean.valueOf(addStr);
+			if (add) {
+				deviceGroupMemberEditorDao.addDevices(this.group, device);
+			} else {
+				deviceGroupMemberEditorDao.removeDevices(this.group, device);
+			}
+		}
+		
+		@Override
+		public Set<BulkField<?, YukonDevice>> getUpdatableFields() {
+			return null;
+		}
+	}
+	
+	private class DeviceGroupProcessorFactory {
+		
+		public BulkFieldProcessor<YukonDevice, String> getProcessor(String processorType, String groupName) throws IllegalArgumentException {
+			
+			if ("DEVICE_GROUP_PREFIX".equals(processorType)) {
+				return new DeviceGroupPrefixProcessor(groupName); 
+			} else if ("DEVICE_GROUP_INCLUDE".equals(processorType)) {
+				return new DeviceGroupGroupProcessor(groupName);
+			} else {
+				throw new IllegalArgumentException("Invalid processorType");
+			}
+		}
+	}
+	
+	
+	@Autowired
+	public void setBulkYukonDeviceFieldFactory(BulkYukonDeviceFieldFactory bulkYukonDeviceFieldFactory) {
+		this.bulkYukonDeviceFieldFactory = bulkYukonDeviceFieldFactory;
+	}
+	@Autowired
+	public void setBulkFieldService(BulkFieldService bulkFieldService) {
+		this.bulkFieldService = bulkFieldService;
+	}
+	@Autowired
+	public void setDeviceGroupEditorDao(DeviceGroupEditorDao deviceGroupEditorDao) {
+		this.deviceGroupEditorDao = deviceGroupEditorDao;
+	}
+	@Autowired
+	public void setDeviceGroupMemberEditorDao(DeviceGroupMemberEditorDao deviceGroupMemberEditorDao) {
+		this.deviceGroupMemberEditorDao = deviceGroupMemberEditorDao;
+	}
+}
