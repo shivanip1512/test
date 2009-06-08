010d5354375506fbeebb44aa1d64852dd39ea07f
Kruse, Matt <>
2009-06-08 23:12:41 +0000

    YUK-7529, Issues with 4.2.9 sqlServer update script
    - Moved the sql back to the 4.2_10 scripts from 4.2_11

--- yukon-database/DBUpdates/sqlserver/4.2_11.sql
+++ yukon-database/DBUpdates/sqlserver/4.2_11.sql
@@ -25,53 +25,6 @@ INSERT INTO YukonRoleProperty VALUES(-20894,-201,'Opt Out Today Only','false','P
 /* @error ignore-end */
 /* End YUK-7518 */
 
-/* Start YUK-7529 */
-DROP VIEW CCOperationsASent_View;
-DROP VIEW CCOperationsBConfirmed_View;
-/* @error ignore-begin */
-/* We want to use schema binding if possible.  This will give us a performance boost. */
-SET NUMERIC_ROUNDABORT OFF;
-SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT ON;
-SET QUOTED_IDENTIFIER, ANSI_NULLS ON;
-GO
-CREATE VIEW [dbo].[CCOperationsASent_View] with schemabinding
-AS
-SELECT LogId, PointId, ActionId, DateTime, Text, FeederId, SubId, AdditionalInfo
-FROM dbo.CCEventLog
-WHERE EventType = 1
-AND ActionId > -1;
-GO
-
-SET NUMERIC_ROUNDABORT OFF;
-SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT ON;
-SET QUOTED_IDENTIFIER, ANSI_NULLS ON;
-GO
-CREATE VIEW [dbo].[CCOperationsBConfirmed_View] with schemabinding
-AS
-SELECT LogId, PointId, ActionId, DateTime, Text, KvarBefore, KvarAfter, KvarChange, CapBankStateInfo
-FROM dbo.CCEventLog
-WHERE EventType = 0
-AND ActionId > -1;
-GO
-
-CREATE VIEW CCOperationsASent_View
-AS
-SELECT LogId, PointId, ActionId, DateTime, Text, FeederId, SubId, AdditionalInfo
-FROM CCEventLog
-WHERE EventType = 1
-AND ActionId > -1;
-GO
-
-CREATE VIEW CCOperationsBConfirmed_View
-AS
-SELECT LogId, PointId, ActionId, DateTime, Text, KvarBefore, KvarAfter, KvarChange, CapBankStateInfo
-FROM CCEventLog
-WHERE EventType = 0
-AND ActionId > -1;
-GO
-/* @error ignore-end */
-/* End YUK-7529 */
-
 /* Start YUK-7537 */
 DELETE FROM EnergyCompanyOperatorLoginList
 WHERE OperatorLoginId IN (SELECT DISTINCT ECOLL.OperatorLoginId 
