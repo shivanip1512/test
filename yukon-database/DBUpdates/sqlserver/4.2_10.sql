5c1bb81dc7e2ef20e823f1c981f9e44a39c73749
Kruse, Matt <>
2009-06-08 23:09:48 +0000

    Moved 4.2_10.sql scripts to 4.2_11.sql scripts.

--- yukon-database/DBUpdates/sqlserver/4.2_10.sql
+++ yukon-database/DBUpdates/sqlserver/4.2_10.sql
@@ -2,6 +2,53 @@
 /**** SQLServer 2000 DBupdates         ****/
 /******************************************/
 
+/* Start YUK-7529 */
+DROP VIEW CCOperationsASent_View;
+DROP VIEW CCOperationsBConfirmed_View;
+/* @error ignore-begin */
+/* We want to use schema binding if possible.  This will give us a performance boost. */
+SET NUMERIC_ROUNDABORT OFF;
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT ON;
+SET QUOTED_IDENTIFIER, ANSI_NULLS ON;
+GO
+CREATE VIEW [dbo].[CCOperationsASent_View] with schemabinding
+AS
+SELECT LogId, PointId, ActionId, DateTime, Text, FeederId, SubId, AdditionalInfo
+FROM dbo.CCEventLog
+WHERE EventType = 1
+AND ActionId > -1;
+GO
+
+SET NUMERIC_ROUNDABORT OFF;
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT ON;
+SET QUOTED_IDENTIFIER, ANSI_NULLS ON;
+GO
+CREATE VIEW [dbo].[CCOperationsBConfirmed_View] with schemabinding
+AS
+SELECT LogId, PointId, ActionId, DateTime, Text, KvarBefore, KvarAfter, KvarChange, CapBankStateInfo
+FROM dbo.CCEventLog
+WHERE EventType = 0
+AND ActionId > -1;
+GO
+
+CREATE VIEW CCOperationsASent_View
+AS
+SELECT LogId, PointId, ActionId, DateTime, Text, FeederId, SubId, AdditionalInfo
+FROM CCEventLog
+WHERE EventType = 1
+AND ActionId > -1;
+GO
+
+CREATE VIEW CCOperationsBConfirmed_View
+AS
+SELECT LogId, PointId, ActionId, DateTime, Text, KvarBefore, KvarAfter, KvarChange, CapBankStateInfo
+FROM CCEventLog
+WHERE EventType = 0
+AND ActionId > -1;
+GO
+/* @error ignore-end */
+/* End YUK-7529 */
+
 /**************************************************************/
 /* VERSION INFO                                               */
 /*   Automatically gets inserted from build script            */
