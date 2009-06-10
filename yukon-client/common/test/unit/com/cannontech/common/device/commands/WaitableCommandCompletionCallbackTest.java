32640eee51be28b06f477f6d7aa55484f382f325
Peterson, Michael <>
2009-06-10 16:03:44 +0000

    YUK-7571 MSP meter creation can cause PorterInThread to hang - new unit test to test WaitableCommandCompletionCallback timeouts

---
+++ yukon-client/common/test/unit/com/cannontech/common/device/commands/WaitableCommandCompletionCallbackTest.java
@@ -0,0 +1,77 @@
+package com.cannontech.common.device.commands;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.cannontech.common.device.commands.impl.WaitableCommandCompletionCallback;
+
+public class WaitableCommandCompletionCallbackTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testWaitForCompletion() {
+        final AtomicInteger timeoutCount = new AtomicInteger(0);
+        final AtomicInteger interruptCount = new AtomicInteger(0);
+        Runnable runnable = new Runnable() {
+            public void run() {
+                WaitableCommandCompletionCallback completionCallback = new WaitableCommandCompletionCallback();
+                try {
+                    completionCallback.waitForCompletion(10, 20);
+                    fail("completed normally");
+                } catch (InterruptedException e) {
+                    System.out.println("InterruptedException");
+                    interruptCount.incrementAndGet();
+                } catch (TimeoutException e) {
+                    System.out.println("TimeoutException");
+                    timeoutCount.incrementAndGet();
+                }
+            }
+        };
+        
+        int threadsToStart = 10;
+        List<Thread> threads = new ArrayList<Thread>();
+        for (int i = 0; i < threadsToStart; ++i) {
+            Thread thread = new Thread(runnable);
+            threads.add(thread);
+            System.out.println("Starting thread " + i);
+            thread.start();
+            try {
+                Thread.sleep(2060);
+            } catch (InterruptedException e) {
+                fail("this shouldn't happen 1");
+            }
+        }
+        
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e1) {
+            fail("this shouldn't happen 2");
+        }
+        
+        // clean up threads, everything should have timed out by now
+        for (Thread thread : threads) {
+            try {
+                thread.interrupt();
+                thread.join(100);
+                System.out.println("Thread joined");
+            } catch (InterruptedException e) {
+                fail("this shouldn't happen 3");
+            }
+        }
+        
+        assertEquals(threadsToStart, timeoutCount.get());
+        assertEquals(0, interruptCount.get());
+    }
+
+}
