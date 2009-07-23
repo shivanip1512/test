b5e7a8844ded0e0f8672052b73971299b8d39ea6
Kruse, Matt <>
2009-07-23 15:00:48 +0000

    Updated the 4.2_13.sql update script date

--- yukon-database/DBUpdates/sqlserver/4.2_13.sql
+++ yukon-database/DBUpdates/sqlserver/4.2_13.sql
@@ -6,4 +6,4 @@
 /* VERSION INFO                                               */
 /*   Automatically gets inserted from build script            */
 /**************************************************************/
-INSERT INTO CTIDatabase VALUES('4.2', 'Matt K', '16-JUL-2009', 'Latest Update', 13);
+INSERT INTO CTIDatabase VALUES('4.2', 'Matt K', '23-JUL-2009', 'Latest Update', 13);
