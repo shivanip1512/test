30b6ce70031fb609907f7b7033cb51801e768175
Kruse, Matt <>
2009-08-25 18:36:37 +0000

    YUK-7786, Latest versions of Oracle no longer support quotas on temporary tablespaces.
    - Removed 'quota unlimited on yukontemp' from OracleTablespaceAndUserCreation.sql

--- yukon-database/CreationScripts/OracleTablespaceAndUserCreation.sql
+++ yukon-database/CreationScripts/OracleTablespaceAndUserCreation.sql
@@ -1,5 +1,5 @@
 create tablespace yukon datafile 'yukon.ora' size 10M autoextend on;
 create temporary tablespace yukontemp tempfile 'yukontemp.ora' size 5M autoextend on;
 
-create user yukon identified by yukon default tablespace yukon temporary tablespace yukontemp quota unlimited on yukon quota unlimited on yukontemp;
+create user yukon identified by yukon default tablespace yukon temporary tablespace yukontemp quota unlimited on yukon;
 grant dba to yukon;
