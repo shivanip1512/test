20f875816c823d5e8e6e2c972d16cdc4881e3c43
Johnston, Samuel T <SamuelTJohnston@Eaton.com>
2009-09-14 20:17:16 +0000

    YUKRV-1772 YUK-7411 - At Forgot Password entry page, needs access to...
    Removed property exception list entries that I had only commented-out.

--- yukon-client/common/src/com/cannontech/core/roleproperties/dao/impl/RolePropertyDaoImpl.java
+++ yukon-client/common/src/com/cannontech/core/roleproperties/dao/impl/RolePropertyDaoImpl.java
@@ -199,8 +199,6 @@ public class RolePropertyDaoImpl implements RolePropertyDao {
         	// Add known issues to this list for 4.2
         	propertyExceptions.add(YukonRoleProperty.TEMPLATE_ROOT);	//YUK-6977, YUK-7073
         	propertyExceptions.add(YukonRoleProperty.METER_MCT_BASE_DESIGNATION);   //YUK-6978
-        	//propertyExceptions.add(YukonRoleProperty.OPERATOR_ACCOUNT_NUMBER_LENGTH);   //YUK-7263, YUK-7327
-        	//propertyExceptions.add(YukonRoleProperty.OPERATOR_ROTATION_DIGIT_LENGTH);	//YUK-7263, YUK-7411
         	
             String[] exceptionArray = exceptionList.split("\\s*,\\s*");
             for (String propertyStr : exceptionArray) {
