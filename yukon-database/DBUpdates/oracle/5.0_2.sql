ab4c5e215c352eca4db35ddf87a8268df389adf9
Kruse, Matt <>
2009-10-05 20:16:30 +0000

    YUK-7875, DBChange -Multispeak - populate group based on substation name from CIS vendor
    - Updated the 'Substation' device group, if it exists, to 'CIS Substation'

---
+++ yukon-database/DBUpdates/oracle/5.0_2.sql
@@ -0,0 +1,18 @@
+/******************************************/
+/**** Oracle DBupdates                 ****/
+/******************************************/
+
+/* Start YUK-7875 */ 
+UPDATE DeviceGroup 
+SET GroupName = 'CIS Substation' 
+WHERE DeviceGroupId = 25 
+AND GroupName = 'Substation' 
+AND ParentDeviceGroupId = 1 
+AND Permission = 'NOEDIT_MOD' 
+AND Type = 'STATIC'; 
+/* End YUK-7875 */
+
+/**************************************************************/
+/* VERSION INFO                                               */
+/*   Automatically gets inserted from build script            */
+/**************************************************************/
