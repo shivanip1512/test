7130a3378ca615246a7b6ecaa8a9e38c5c500fce
Kruse, Matt <>
2009-10-14 22:25:07 +0000

    YUK-7363, View Previous kWh Rate(A,B,C,D) kWh reads in the Metering Page
    - Overhauled the TOU Widget
    --- Created a previous readings popup for TOU values
    ----- Externalized the previous readings code from the meter reading widget
    --- Changed the tou elements to use the name values pairs
    --- Internationalized the TOU Widget
    --- Cleaned up the code
    - Cleaned up some style differences in account information widget and also changes the widget to use name value pairs.

--- yukon-client/yukon-web/src/com/cannontech/web/widget/MeterReadingsWidget.java
+++ yukon-client/yukon-web/src/com/cannontech/web/widget/MeterReadingsWidget.java
@@ -1,8 +1,5 @@
 package com.cannontech.web.widget;
 
-import java.util.Calendar;
-import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -10,24 +7,21 @@ import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.cannontech.amr.deviceread.dao.MeterReadService;
 import com.cannontech.amr.meter.dao.MeterDao;
 import com.cannontech.amr.meter.model.Meter;
-import com.cannontech.common.chart.model.ChartInterval;
 import com.cannontech.common.device.attribute.model.Attribute;
 import com.cannontech.common.device.attribute.model.BuiltInAttribute;
 import com.cannontech.common.device.attribute.service.AttributeService;
 import com.cannontech.common.device.commands.CommandRequestExecutionType;
 import com.cannontech.common.device.commands.CommandResultHolder;
+import com.cannontech.common.device.model.PreviousReadings;
+import com.cannontech.common.device.service.PointService;
 import com.cannontech.common.events.loggers.MeteringEventLogService;
-import com.cannontech.core.dao.RawPointHistoryDao;
-import com.cannontech.core.dynamic.PointValueHolder;
 import com.cannontech.database.data.lite.LitePoint;
 import com.cannontech.database.data.lite.LiteYukonUser;
 import com.cannontech.util.ServletUtil;
@@ -42,13 +36,9 @@ public class MeterReadingsWidget extends WidgetControllerBase {
     private MeterDao meterDao;
     private MeterReadService meterReadService;
     private AttributeService attributeService;
-    private RawPointHistoryDao rphDao;
     private MeteringEventLogService meteringEventLogService;
+    private PointService pointService;
     private List<? extends Attribute> attributesToShow;
-
-    public void setMeterReadService(MeterReadService meterReadService) {
-        this.meterReadService = meterReadService;
-    }
     
     public void setAttributesToShow(List<BuiltInAttribute> attributesToShow) {
         // this setter accepts the enum to make Spring happy
@@ -73,7 +63,13 @@ public class MeterReadingsWidget extends WidgetControllerBase {
         boolean usageAttributeExists = existingAttributes.containsKey(BuiltInAttribute.USAGE);
         if (usageAttributeExists) {
 	        LitePoint lp = attributeService.getPointForAttribute(meter, BuiltInAttribute.USAGE);
-	        fillInPreviousReadings(mav, lp, "VALUE");
+	        PreviousReadings previousReadings = pointService.fillInPreviousReadings(lp);
+	        
+	        mav.addObject("previousReadings_All", previousReadings.getPrevious36());
+	        mav.addObject("previousReadings_Daily", previousReadings.getPrevious3Months());
+	        mav.addObject("previousReadings_CutoffDate", previousReadings.getCutoffDate());
+	        mav.addObject("previousReadings_Cutoff", !previousReadings.getPrevious3Months().isEmpty());
+	        mav.addObject("previousReadings_OptionValue", "VALUE");
         }
         mav.addObject("usageAttributeExists", usageAttributeExists);
         
@@ -114,70 +110,29 @@ public class MeterReadingsWidget extends WidgetControllerBase {
         return meter;
     }
 
-    @Required
+    @Autowired
     public void setMeterDao(MeterDao meterDao) {
         this.meterDao = meterDao;
     }
     
-    @Required
+    @Autowired
+    public void setMeterReadService(MeterReadService meterReadService) {
+        this.meterReadService = meterReadService;
+    }
+
+    @Autowired
     public void setAttributeService(AttributeService attributeService) {
         this.attributeService = attributeService;
     }
-    
-	/**
-     * Sets up the mav object to support the previous readings include.
-     * 
-     * @param mav
-     * @param lp
-     */
-	public void fillInPreviousReadings(ModelAndView mav, LitePoint lp,
-			String optionValue) {
-
-		// ask for six months, with a max of 36 results
-		Date today = new Date();
-
-		// first 36 hours - all points
-		Date sixMonthsAgo = DateUtils.addMonths(today, -6);
-		List<PointValueHolder> previous36 = rphDao.getPointData(
-				lp.getPointID(), today, sixMonthsAgo, 36);
-
-		List<PointValueHolder> previous3Months = Collections.emptyList();
-		if (previous36.size() == 36) {
-			// great, let's go get some more
-			PointValueHolder lastPvhOfThe36 = previous36.get(36 - 1);
-			Date lastDateOfThe36 = lastPvhOfThe36.getPointDataTimeStamp();
-			Date beforeDate = DateUtils
-					.truncate(lastDateOfThe36, Calendar.DATE);
-			beforeDate = DateUtils.addSeconds(beforeDate, -1);
-			mav.addObject("previousReadings_CutoffDate", beforeDate);
-			// ask for daily readings from 93 days ago to "before"
-			Date today1 = new Date();
-
-			Date ninetyThreeDaysAgo = DateUtils.addDays(today1, -93);
-
-			if (!beforeDate.before(ninetyThreeDaysAgo)) {
-				previous3Months = rphDao.getIntervalPointData(lp.getPointID(),
-						beforeDate, ninetyThreeDaysAgo,
-						ChartInterval.DAY_MIDNIGHT,
-						RawPointHistoryDao.Mode.HIGHEST);
-			}
-		} else {
-			mav.addObject("previousReadings_CutoffDate", sixMonthsAgo);
-		}
-		mav.addObject("previousReadings_All", previous36);
-		mav.addObject("previousReadings_Daily", previous3Months);
-		mav.addObject("previousReadings_Cutoff", !previous3Months.isEmpty());
-		mav.addObject("previousReadings_OptionValue", optionValue);
-	}
-
-    @Required
-    public void setRphDao(RawPointHistoryDao rphDao) {
-        this.rphDao = rphDao;
-    }
-    
+	
     @Autowired
     public void setMeteringEventLogService(
             MeteringEventLogService meteringEventLogService) {
         this.meteringEventLogService = meteringEventLogService;
+    }
+    
+    @Autowired
+    public void setPointService(PointService pointService) {
+        this.pointService = pointService;
     }
 }
