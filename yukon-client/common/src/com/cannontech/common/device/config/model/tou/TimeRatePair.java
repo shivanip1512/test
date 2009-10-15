316c124f3808317c18376feee94c60b9779870e0
Solberg, Aaron <>
2009-10-15 17:08:00 +0000

    YUK-7925 TOU information does not get created for 470 Configs properly -  Default times and rates will be saved now, and times will be in proper 00:00 23:59 time.

--- yukon-client/common/src/com/cannontech/common/device/config/model/tou/TimeRatePair.java
+++ yukon-client/common/src/com/cannontech/common/device/config/model/tou/TimeRatePair.java
@@ -1,11 +1,20 @@
 package com.cannontech.common.device.config.model.tou;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class TimeRatePair {
     private Date time = null;
-    private String rate = null;
+    private String rate = "A";
 
+    public TimeRatePair(){
+        Calendar calendar = new GregorianCalendar();
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        this.time = calendar.getTime();
+    }
+    
     public String getRate() {
         return rate;
     }
