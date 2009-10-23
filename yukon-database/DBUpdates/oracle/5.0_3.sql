a302149561e8a49b4d9611c4336b50dde8d987ab
Kruse, Matt <>
2009-10-23 18:06:53 +0000

    YUK-7940, Mass Delete should be defaulted to 'false'
    - Updated the Mass Delete role property to default to 'false'.

---
+++ yukon-database/DBUpdates/oracle/5.0_3.sql
@@ -0,0 +1,14 @@
+/******************************************/ 
+/**** Oracle DBupdates                 ****/ 
+/******************************************/ 
+
+/* Start YUK-7940 */ 
+UPDATE YukonRoleProperty 
+SET DefaultValue = 'false' 
+WHERE RolePropertyId = -21307; 
+/* End YUK-7940 */ 
+
+/**************************************************************/ 
+/* VERSION INFO                                               */ 
+/*   Automatically gets inserted from build script            */ 
+/**************************************************************/ 
