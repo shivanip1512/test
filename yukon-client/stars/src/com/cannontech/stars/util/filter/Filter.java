3fb4fc76bafe9ed3f537beb58d29066c221ff85c
Malekar, Murthy <>
2009-11-18 17:00:44 +0000

    YUKRV-2071 YUK-7453 Inventory results overlap Review changes, Java doc updated.

--- yukon-client/stars/src/com/cannontech/stars/util/filter/Filter.java
+++ yukon-client/stars/src/com/cannontech/stars/util/filter/Filter.java
@@ -14,11 +14,37 @@ public interface Filter<E> {
     
     public int getFilterCount(List<Integer> energyCompanyIdList,
             List<FilterWrapper> filterWrapperList, Date startDate, Date stopDate) throws PersistenceException;
-    
+
+    /**
+     * Executes the SQL query with the given Filters and calls on Processor to
+     * process the results between the specified fromIndex inclusive, and
+     * toIndex exclusive.
+     * 
+     * @param fromIndex inclusive
+     * @param toIndex exclusive
+     * @param processor
+     * @param energyCompanyIdList
+     * @param filterWrapperList
+     * @param orderBy
+     * @throws PersistenceException
+     */
     public void filter(int fromIndex, int toIndex, Processor<E> processor,
             List<Integer> energyCompanyIdList, List<FilterWrapper> filterWrapperList,
                 DirectionAwareOrderBy orderBy) throws PersistenceException;
 
+    /**
+     * Executes the SQL query with the given Filters and calls on Processor to
+     * process the results between the specified fromIndex inclusive, and
+     * toIndex exclusive.
+     * 
+     * @param fromIndex inclusive
+     * @param toIndex exclusive
+     * @param processor
+     * @param energyCompanyIdList
+     * @param filterWrapperList
+     * @param orderBy
+     * @throws PersistenceException
+     */    
     public void filter(int fromIndex, int toIndex, Processor<E> processor, 
             List<Integer> energyCompanyIdList, List<FilterWrapper> filterWrapperList,
                 DirectionAwareOrderBy orderBy, Date startDate, Date stopDate) throws PersistenceException;
--- yukon-client/stars/src/com/cannontech/stars/web/collection/SimpleCollection.java
+++ yukon-client/stars/src/com/cannontech/stars/web/collection/SimpleCollection.java
@@ -7,7 +7,16 @@ import com.cannontech.core.dao.PersistenceException;
 public interface SimpleCollection<E> extends Iterable<E> {
 
     public List<E> getList() throws PersistenceException;
-    
+
+    /**
+     * Returns the results between the specified fromIndex inclusive, and
+     * toIndex exclusive.
+     * 
+     * @param fromIndex inclusive
+     * @param toIndex exclusive
+     * @return list of results
+     * @throws PersistenceException
+     */
     public List<E> getList(int fromIndex, int toIndex) throws PersistenceException;
     
     public int getCount() throws PersistenceException;
