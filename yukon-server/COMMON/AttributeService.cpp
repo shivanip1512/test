3a9e769a04ca5785baf40b677efa003944777d52
Spar, Thain <>
2010-01-14 23:42:33 +0000

    YUK-8089 IVVC: Add ability to load LTC device type.
    Missed files

---
+++ yukon-server/COMMON/AttributeService.cpp
@@ -0,0 +1,140 @@
+#include "yukon.h"
+
+#include "AttributeService.h"
+#include "resolvers.h"
+#include "numstr.h"
+#include "dbaccess.h"
+#include "rwutil.h"
+
+#include <rw/db/db.h>
+
+/**
+ * Returns all Points listed as extra paos.
+ *
+ *
+ * @param paoId
+ *
+ * @return std::list<LitePoint>
+ */
+std::list<LitePoint> AttributeService::getExtraPaoPoints(int paoId)
+{
+    std::list<int> pointIds;
+
+    {
+        string sql("SELECT pointId FROM ExtraPaoPointAssignment WHERE paoId = ");
+        sql += CtiNumStr(paoId);
+
+        CtiLockGuard<CtiSemaphore> cg(gDBAccessSema);
+        RWDBConnection conn = getConnection();
+
+        RWDBReader rdr = ExecuteQuery( conn, sql );
+
+        int pointId;
+        while(rdr.isValid() && rdr() )
+        {
+            rdr["pointId"] >> pointId;
+            pointIds.push_back(pointId);
+        }
+    }
+
+    //Call Point function
+    std::list<LitePoint> points = getLitePointsById(pointIds);
+
+    return points;
+}
+
+/**
+ * Returns the point associated with the ExtraPao and Attribute.
+ *
+ * @param paoId
+ * @param attribute
+ *
+ * @return LitePoint
+ */
+LitePoint AttributeService::getPointByPaoAndAttribute(int paoId, const string& attribute)
+{
+    int pointId;
+    string attributeName = attribute;
+
+    {
+        string sql("SELECT pointId FROM ExtraPaoPointAssignment WHERE paoId = ");
+        sql += CtiNumStr(paoId);
+        sql += " AND Attribute = '" + attributeName + "'";
+
+        CtiLockGuard<CtiSemaphore> cg(gDBAccessSema);
+        RWDBConnection conn = getConnection();
+
+        RWDBReader rdr = ExecuteQuery( conn, sql );
+
+        if(rdr.isValid() && rdr() )
+        {
+            rdr["pointId"] >> pointId;
+        }
+    }
+
+    LitePoint point = getLitePointsById(pointId);
+
+    return point;
+}
+
+LitePoint AttributeService::getLitePointsById(int pointId)
+{
+    std::list<int> pointIds;
+    pointIds.push_back(pointId);
+
+    std::list<LitePoint> points = getLitePointsById(pointIds);
+
+    if (points.size() != 1)
+    {
+        return LitePoint();
+    }
+
+    return points.front();
+}
+
+std::list<LitePoint> AttributeService::getLitePointsById(const std::list<int>& pointIds)
+{
+    std::list<LitePoint> points;
+
+    string sql("SELECT PointId,PointType,PointName,PAOBjectId,PointOffset FROM Point WHERE pointid in (");
+    for each(int pointId in pointIds)
+    {
+        sql += CtiNumStr(pointId);
+        sql += ",";
+    }
+    sql.replace(sql.find_last_of(","),1,")");
+
+    {
+        CtiLockGuard<CtiSemaphore> cg(gDBAccessSema);
+        RWDBConnection conn = getConnection();
+
+        RWDBReader rdr = ExecuteQuery( conn, sql );
+
+        while(rdr.isValid() && rdr() )
+        {
+            LitePoint point;
+            int temp;
+            string tempStr;
+
+            rdr["PointId"] >> temp;
+            point.setPointId(temp);
+
+            rdr["PointType"] >> tempStr;
+            CtiPointType_t type = resolvePointType(tempStr);
+            point.setPointType(type);
+
+            rdr["PointName"] >> tempStr;
+            point.setPointName(tempStr);
+
+            rdr["PAOBjectId"] >> temp;
+            point.setPaoId(temp);
+
+            rdr["PointOffset"] >> temp;
+            point.setPointOffset(temp);
+
+            points.push_back(point);
+        }
+    }
+
+    return points;
+}
--- yukon-server/COMMON/include/AttributeService.h
+++ yukon-server/COMMON/include/AttributeService.h
@@ -6,6 +6,19 @@
 
 #include "LitePoint.h"
 
+/**
+ * This Class is to hold the accessors for Attribute
+ * information.
+ *
+ * It is to mirror the Java class in functionality but currently
+ * only supports the using the Database tables to access the
+ * attributes.
+ *
+ * When implemented this file should also be used to access the
+ * deviceDefinition.xml file for Attribute information.
+ */
+
+
 class IM_EX_CTIBASE AttributeService
 {
     public:
