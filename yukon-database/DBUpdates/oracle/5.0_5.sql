18b4ed6a08cfa0e9535be8b11a1074c4583c67bb
Kruse, Matt <>
2010-02-23 21:25:20 +0000

    YUK-8414, DB Change: Capbank Editor needs to set control pointid in capbank table.
    - Added update to fix the ControlPointId for the CapBank table

--- yukon-database/DBUpdates/oracle/5.0_5.sql
+++ yukon-database/DBUpdates/oracle/5.0_5.sql
@@ -11,6 +11,17 @@ ALTER TABLE DynamicCCOriginalParent
             ON DELETE CASCADE;
 /* End YUK-8250 */
 
+/* Start YUK-8414 */
+UPDATE CapBank 
+SET ControlPointId = (SELECT P.PointId 
+                      FROM POINT P 
+                      WHERE CapBank.ControlDeviceId = P.PAObjectId 
+                      AND P.PointOffset = 1 
+                      AND P.PointType = 'Status' 
+                      AND CapBank.ControlDeviceId > 0)
+WHERE ControlDeviceId > 0;
+/* End YUK-8414 */
+
 /**************************************************************/ 
 /* VERSION INFO                                               */ 
 /*   Automatically gets inserted from build script            */ 
--- yukon-database/DBUpdates/oracle/5.1_3.sql
+++ yukon-database/DBUpdates/oracle/5.1_3.sql
@@ -8,6 +8,17 @@ UPDATE ApplianceCategory SET ConsumerSelectable = 'Y';
 ALTER TABLE ApplianceCategory MODIFY ConsumerSelectable CHAR(1) NOT NULL;
 /* End YUK-8403 */
 
+/* Start YUK-8414 */
+UPDATE CapBank 
+SET ControlPointId = (SELECT P.PointId 
+                      FROM POINT P 
+                      WHERE CapBank.ControlDeviceId = P.PAObjectId 
+                      AND P.PointOffset = 1 
+                      AND P.PointType = 'Status' 
+                      AND CapBank.ControlDeviceId > 0)
+WHERE ControlDeviceId > 0;
+/* End YUK-8414 */
+
 /**************************************************************/ 
 /* VERSION INFO                                               */ 
 /*   Automatically gets inserted from build script            */ 
