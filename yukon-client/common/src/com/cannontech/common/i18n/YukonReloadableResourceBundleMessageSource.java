73f7ad3ad1098cf0017493a0e4b9a797f3d74174
Peterson, Michael <>
2010-03-19 22:16:24 +0000

    Change DEVELOPER_MODE reloadable resource bundle message cache time from 1s to 10s - using a value of 10 instead of 1 dramatically increases performance, but you'll have to wait up to 10 sec to see xml/property changes.

--- yukon-client/common/src/com/cannontech/common/i18n/YukonReloadableResourceBundleMessageSource.java
+++ yukon-client/common/src/com/cannontech/common/i18n/YukonReloadableResourceBundleMessageSource.java
@@ -13,7 +13,7 @@ public class YukonReloadableResourceBundleMessageSource extends ReloadableResour
     public void afterPropertiesSet() throws Exception {
         boolean devMode = configurationSource.getBoolean("DEVELOPMENT_MODE", false);
         if(devMode) {
-            setCacheSeconds(1);
+            setCacheSeconds(10); // using a value of 10 instead of 1 dramatically increases performance, but you'll have to wait up to 10 sec to see xml/property changes.
         } else {
             setCacheSeconds(-1);
         }
