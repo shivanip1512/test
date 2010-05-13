bd218f1e9b6ff5e0825852e3e30f306c07ae108c
Kruse, Matt <>
2010-05-13 17:09:24 +0000

    5.1.6 CTIDatabase inserts

--- yukon-database/DBUpdates/sqlserver/5.1_6.sql
+++ yukon-database/DBUpdates/sqlserver/5.1_6.sql
@@ -12,3 +12,4 @@ DELETE FROM YukonRoleProperty WHERE RolePropertyId = -20101;
 /* VERSION INFO                                               */ 
 /*   Automatically gets inserted from build script            */ 
 /**************************************************************/ 
+INSERT INTO CTIDatabase VALUES ('5.1', 'Matt K', '13-MAY-2010', 'Latest Update', 6);
