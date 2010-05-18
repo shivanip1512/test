4f7328a4ce4c91f7b4a52a5e29a288812f4ab8da
Mack, Tom <>
2010-05-18 23:06:58 +0000

    YUK-8639 Process loop when trying to launch a client app from the Operations page
    Simplifies Java Web Start launcher so that no Java detection is performed.

--- yukon-client/yukon-web/WebContent/JavaScript/javaWebStartLauncher.js
+++ yukon-client/yukon-web/WebContent/JavaScript/javaWebStartLauncher.js
@@ -1,65 +1,16 @@
-var jwsCancelCheck = 0;
 var jwsCurrentUrl = "";
 
 function jwsLaunch(url) {
-  var javaCookieValue = readCookie("javainstalled");
-  if (javaCookieValue == 'true') {
-    window.location.replace(url);
-    return;
-  }
-  
-  jwsCurrentUrl = url;
-  jwsCancelCheck += 1;
   $('javaWebStartPopup').show();
-  
-  // add applet to page
-  // <APPLET CODE="JavaDetector.class"  codebase="/JavaScript/" NAME="myApplet" HEIGHT=0 WIDTH=0></APPLET>
-  var applet = document.createElement("applet");
-  applet.setAttribute("id", "javaDetectorApplet");
-  applet.setAttribute("code", "JavaDetector.class");
-  applet.setAttribute("codebase", "/JavaScript/");
-  applet.setAttribute("name", "myApplet");
-  applet.setAttribute("mayscript", "");
-  applet.setAttribute("height", "0");
-  applet.setAttribute("width", "0");
-  document.body.appendChild(applet);
-
-  var cancelCheckAtStart = jwsCancelCheck;
-
-  setTimeout(function() {
-    if (cancelCheckAtStart != jwsCancelCheck) return;
-    var thisApplet = document.getElementsByTagName("applet")[0];
-    
-    var versionFound = false;
-    var browserName=navigator.appName;
-    if (browserName=="Microsoft Internet Explorer") {
-    	versionFound = ((typeof thisApplet.getJavaVersion != 'undefined') && (thisApplet.getJavaVersion))
-    } else {
-    	versionFound = thisApplet.getJavaVersion
-    }
-    
-    if (versionFound) {
-        var javaVersion = thisApplet.getJavaVersion();
-        if (javaVersion.indexOf('.') != -1) {
-          createCookie("javainstalled", "true");
-          $('javaWebStartPopup').hide();
-          window.location.replace(url);
-          return;
-        }
-      }
-    $('javaWebStartWaiting').hide();
-    $('javaWebStartNoJava').show();
-  },5000);
+  jwsCurrentUrl = url;
 }
 
 function jwsClosePopup() {
-  jwsCancelCheck += 1;
   $('javaWebStartPopup').hide();
 }
 
 function jwsRelaunchCurrent() {
-  jwsCancelCheck += 1;
-  $('javaWebStartWaiting').hide();
+  $('javaWebStartPopup').hide();
   window.location.replace(jwsCurrentUrl);
 }
 
