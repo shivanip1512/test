6ca0050b8631bcaecb62fc49246fefae6d3a9b17
Kruse, Matt <>
2010-05-20 18:47:03 +0000

    YUK-8718, New table for eke meters (CRFAddress)
    - Moved this change to 5.2_0.

--- yukon-database/DBUpdates/oracle/5.1_7.sql
+++ yukon-database/DBUpdates/oracle/5.1_7.sql
@@ -11,27 +11,6 @@ INSERT INTO DeviceTypeCommand VALUES (-813, -54, 'Repeater 850', 4, 'Y', -1);
 /* @error ignore-end */
 /* End YUK-8710 */
 
-/* Start YUK-8718 */
-CREATE TABLE CRFAddress  (
-   DeviceId             NUMBER                          NOT NULL,
-   SerialNumber         VARCHAR2(30)                    NOT NULL,
-   Manufacturer         VARCHAR2(80)                    NOT NULL,
-   Model                VARCHAR2(80)                    NOT NULL,
-   CONSTRAINT PK_CRFAdd PRIMARY KEY (DeviceId)
-);
-
-ALTER TABLE CRFAddress
-    ADD CONSTRAINT FK_CRFAdd_Device FOREIGN KEY (DeviceId)
-        REFERENCES Device (DeviceId)
-            ON DELETE CASCADE;
-
-CREATE UNIQUE INDEX Indx_CRFAdd_SerNum_Man_Mod_UNQ ON CRFAddress(
-SerialNumber ASC,
-Manufacturer ASC,
-Model ASC
-);
-/* End YUK-8718 */
-
 /**************************************************************/ 
 /* VERSION INFO                                               */ 
 /*   Automatically gets inserted from build script            */ 
--- yukon-database/DBUpdates/sqlserver/5.2_0.sql
+++ yukon-database/DBUpdates/sqlserver/5.2_0.sql
@@ -2,6 +2,30 @@
 /**** SQL Server DBupdates             ****/ 
 /******************************************/ 
 
+/* Start YUK-8718 */
+CREATE TABLE CRFAddress (
+   DeviceId             NUMERIC              NOT NULL,
+   SerialNumber         VARCHAR(30)          NOT NULL,
+   Manufacturer         VARCHAR(80)          NOT NULL,
+   Model                VARCHAR(80)          NOT NULL,
+   CONSTRAINT PK_CRFAdd PRIMARY KEY (DeviceId)
+);
+GO
+
+ALTER TABLE CRFAddress
+    ADD CONSTRAINT FK_CRFAdd_Device FOREIGN KEY (DeviceId)
+        REFERENCES Device (DeviceId)
+            ON DELETE CASCADE;
+GO
+
+CREATE UNIQUE INDEX Indx_CRFAdd_SerNum_Man_Mod_UNQ ON CRFAddress (
+SerialNumber ASC,
+Manufacturer ASC,
+Model ASC
+);
+GO
+/* End YUK-8718 */
+
 /**************************************************************/ 
 /* VERSION INFO                                               */ 
 /*   Automatically gets inserted from build script            */ 
