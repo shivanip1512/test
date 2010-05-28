49aa1566fe1a2bee2d4213cacea17a3ca2a3fa0b
Mack, Tom <>
2010-05-28 13:21:49 +0000

    YUK-8715 Eka Integration Demo
    New ConnectionFactory that relies on Spring for the heavy thread-safety initialization stuff.

--- yukon-client/common/src/com/cannontech/services/jms/YukonJmsConnectionFactory.java
+++ yukon-client/common/src/com/cannontech/services/jms/YukonJmsConnectionFactory.java
@@ -1,88 +1,68 @@
 package com.cannontech.services.jms;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import javax.annotation.PostConstruct;
-import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
-import javax.jms.JMSException;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.connection.CachingConnectionFactory;
 
 import com.cannontech.clientutils.YukonLogManager;
 import com.cannontech.common.config.ConfigurationSource;
 import com.cannontech.common.util.CtiUtilities;
-import com.google.common.base.Supplier;
 
-public class YukonJmsConnectionFactory implements ConnectionFactory {
+public class YukonJmsConnectionFactory implements FactoryBean {
     private static final Logger log = YukonLogManager.getLogger(YukonJmsConnectionFactory.class);
-    
-    private ConfigurationSource configurationSource;
-    
-    private volatile ConnectionFactory delegate = null;
 
-    public ConnectionFactory getDelegate() {
-        return delegate;
-    }
-    
-    @PostConstruct
-    public void initialize() {
-        createConnectionFactory();
-        
-    }
-    
-    private void createConnectionFactory() {
-        // double-check now that we're in sync block
+    private ConfigurationSource configurationSource;
+
+    @Override
+    public Object getObject() throws Exception {
 
         final String applicationName = CtiUtilities.getApplicationName();
 
+        ConnectionFactory delegate;
         if (applicationName.equals("ServiceManager")) {
             String serverListenConnection = configurationSource.getString("JMS_SERVER_BROKER_LISTEN_CONNECTION", "tcp://localhost:61616");
             ServerEmbeddedBroker serverEmbeddedBroker = new ServerEmbeddedBroker(applicationName, serverListenConnection);
             serverEmbeddedBroker.start();
             delegate = serverEmbeddedBroker.getConnectionFactory();
+            log.debug("created ServiceManager ConnectionFactory at " + serverListenConnection);
         } else if (!CtiUtilities.isRunningAsClient()) {
-            delegate = new LazyConnectionFactory(new Supplier<ConnectionFactory>() {
-                public ConnectionFactory get() {
-                    String clientBrokerConnection = configurationSource.getString("JMS_CLIENT_BROKER_CONNECTION", "tcp://localhost:61616");
-                    ClientEmbeddedBroker clientEmbeddedBroker = new ClientEmbeddedBroker(applicationName, clientBrokerConnection);
-                    clientEmbeddedBroker.start();
-                    return clientEmbeddedBroker.getConnectionFactory();
-                }
-            });
+            String clientBrokerConnection = configurationSource.getString("JMS_CLIENT_BROKER_CONNECTION", "tcp://localhost:61616");
+            ClientEmbeddedBroker clientEmbeddedBroker = new ClientEmbeddedBroker(applicationName, clientBrokerConnection);
+            clientEmbeddedBroker.start();
+            delegate = clientEmbeddedBroker.getConnectionFactory();
+            log.debug("created Server ConnectionFactory at " + clientBrokerConnection);
         } else {
-            delegate = new LazyConnectionFactory(new Supplier<ConnectionFactory>() {
-                public ConnectionFactory get() {
-                    String clientConnection = configurationSource.getString("JMS_CLIENT_CONNECTION", "tcp://localhost:61616");
-                    // Create a ConnectionFactory
-                    ConnectionFactory factory = new ActiveMQConnectionFactory(clientConnection);
+            String clientConnection = configurationSource.getString("JMS_CLIENT_CONNECTION", "tcp://localhost:61616");
+            // Create a ConnectionFactory
+            ConnectionFactory factory = new ActiveMQConnectionFactory(clientConnection);
 
-                    // if using Spring, create a CachingConnectionFactory
-                    CachingConnectionFactory cachingFactory = new CachingConnectionFactory();
-                    cachingFactory.setTargetConnectionFactory(factory);
-                    return cachingFactory;
-                }
-            });
+            // if using Spring, create a CachingConnectionFactory
+            CachingConnectionFactory cachingFactory = new CachingConnectionFactory();
+            cachingFactory.setTargetConnectionFactory(factory);
+            delegate = cachingFactory;
+            log.debug("created Client ConnectionFactory at " + clientConnection);
         }
+
+        return delegate;
     }
 
     @Override
-    public Connection createConnection() throws JMSException {
-        return delegate.createConnection();
+    public Class<?> getObjectType() {
+        return ConnectionFactory.class;
     }
 
     @Override
-    public Connection createConnection(String userName, String password)
-            throws JMSException {
-        return delegate.createConnection(userName, password);
+    public boolean isSingleton() {
+        return true;
     }
 
     @Autowired
     public void setConfigurationSource(ConfigurationSource configurationSource) {
         this.configurationSource = configurationSource;
     }
-
+    
 }
