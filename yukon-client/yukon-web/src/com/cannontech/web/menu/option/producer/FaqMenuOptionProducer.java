805234975115406d51f9190a05153002a02a13b4
Gartner, David <>
2010-06-02 15:18:09 +0000

    YUK-8753: Account search fails when logged in as an operator and Link FAQ set to inherited
    -get energy company from the cache instead of from the customer tied to the login.

--- yukon-client/yukon-web/src/com/cannontech/web/menu/option/producer/FaqMenuOptionProducer.java
+++ yukon-client/yukon-web/src/com/cannontech/web/menu/option/producer/FaqMenuOptionProducer.java
@@ -1,23 +1,18 @@
 package com.cannontech.web.menu.option.producer;
 
-import java.util.List;
-
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.cannontech.core.roleproperties.dao.RolePropertyDao;
+import com.cannontech.database.cache.StarsDatabaseCache;
 import com.cannontech.database.data.lite.LiteYukonUser;
 import com.cannontech.database.data.lite.stars.LiteStarsEnergyCompany;
-import com.cannontech.stars.core.dao.ECMappingDao;
-import com.cannontech.stars.dr.account.dao.CustomerAccountDao;
-import com.cannontech.stars.dr.account.model.CustomerAccount;
 import com.cannontech.stars.util.ServletUtils;
 import com.cannontech.web.menu.option.SimpleMenuOptionLink;
 
 public abstract class FaqMenuOptionProducer extends DynamicMenuOptionProducer {
     protected RolePropertyDao rolePropertyDao;
-    protected CustomerAccountDao customerAccountDao;
-    protected ECMappingDao ecMappingDao;
+    protected StarsDatabaseCache starsDatabaseCache;
     
     protected boolean isInherited(String link) {
         boolean result = ServletUtils.INHERITED_FAQ.equals(link);
@@ -25,10 +20,7 @@ public abstract class FaqMenuOptionProducer extends DynamicMenuOptionProducer {
     }
     
     protected String getInheritedLink(LiteYukonUser user, String defaultLink) {
-        final List<CustomerAccount> customerAccountList = customerAccountDao.getByUser(user);
-        final CustomerAccount customerAccount = customerAccountList.get(0);
-
-        LiteStarsEnergyCompany energyCompany = ecMappingDao.getCustomerAccountEC(customerAccount);
+        LiteStarsEnergyCompany energyCompany = starsDatabaseCache.getEnergyCompanyByUser(user); 
 
         while (ServletUtils.isCustomerFAQInherited(energyCompany)) {
             energyCompany = energyCompany.getParent();
@@ -67,13 +59,7 @@ public abstract class FaqMenuOptionProducer extends DynamicMenuOptionProducer {
     }
     
     @Autowired
-    public void setCustomerAccountDao(CustomerAccountDao customerAccountDao) {
-        this.customerAccountDao = customerAccountDao;
+    public void setStarsDatabaseCache(StarsDatabaseCache starsDatabaseCache) {
+        this.starsDatabaseCache = starsDatabaseCache;
     }
-    
-    @Autowired
-    public void setEcMappingDao(ECMappingDao ecMappingDao) {
-        this.ecMappingDao = ecMappingDao;
-    }
-    
 }
