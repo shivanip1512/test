bb3227894b157c215ea1270c8d9bb541a42ced27
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2010-07-13 19:07:56 +0000

    YUK-6576 Remove the "Yukon Servers" view from TDC  - deleted all Yukon Servers display code.  (WebServicesPanel and JNT references).

--- yukon-client/tdc/resource/build.bat
+++ yukon-client/tdc/resource/build.bat
@@ -1,1 +1,1 @@
-cl /I d:\j2sdk1.4.1\include /I d:\j2sdk1.4.1\include\win32 -LD JNTServices.c advapi32.lib -FeJNTServices.dll
+cl /I d:\j2sdk1.4.1\include /I d:\j2sdk1.4.1\include\win32 -LD advapi32.lib
