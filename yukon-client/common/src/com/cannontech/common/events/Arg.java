f0b4b1980ce3099d3ff7e1673e27ea4456cd9cc0
Kruse, Matt <>
2010-07-26 18:06:13 +0000

    YUK-8670, More Detailed Transaction History (By Type)
    - Refactored the EventLogFactoryBean, EventLogDao, and EventLogService
    --- Restructured which items were found in which file
    --- Added @Arg annotation support
    --- Refactored the way we build up the event log method entries
    - Added a rejectValues method to the YukonValidationUtils that allows someone to mark multiple fields on a validation error.
    - Added a session based eventLogTypeBackingBean attribute to help cut down how much information we need to reload and pass around the request.

--- yukon-client/common/src/com/cannontech/common/events/Arg.java
+++ yukon-client/common/src/com/cannontech/common/events/Arg.java
@@ -5,10 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import com.cannontech.common.events.loggers.EventLogArgEnum;
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface Arg {
 
-    String value();
+    EventLogArgEnum value();
     
 }
