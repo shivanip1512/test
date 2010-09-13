329c5aa89be6012bae54aafdf912b66d0f70165a
Kruse, Matt <>
2010-09-13 04:56:55 +0000

    YUK-9022, DB updates for build 5.1.10 not being performed when updating 5.2.0 to 5.2.1
    - Added the 'Account Search' and 'Inventory Search' role properties to the 5.2 update scripts.

--- yukon-database/DBUpdates/oracle/5.2_2.sql
+++ yukon-database/DBUpdates/oracle/5.2_2.sql
@@ -2,10 +2,17 @@
 /**** Oracle DBupdates                 ****/ 
 /******************************************/ 
 
-/* Start YUK-9057*/
+/* Start YUK-9057 */
 ALTER TABLE SurveyResult
 MODIFY AccountId NUMBER NULL;
-/* End YUK-9057*/
+/* End YUK-9057 */
+
+/* Start YUK-9022 */
+/* @error ignore-begin */
+INSERT INTO YukonRoleProperty VALUES(-20165,-201,'Account Search','true','Enables you to use account searching.');
+INSERT INTO YukonRoleProperty VALUES(-20911,-209,'Inventory Search','true','Enables you to use inventory searching.');
+/* @error ignore-end */
+/* End YUK-9022 */
 
 /**************************************************************/ 
 /* VERSION INFO                                               */ 
