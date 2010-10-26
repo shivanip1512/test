4431bf16864ee18348f1e4afce03a99d83d4507d
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2010-10-26 16:29:58 +0000

    YUK-9194 Stars operator > Contacts, Create a new Contact, clicking Save caused an error occured page to display  - Removed DBChangeMsg int values and replaced with DbChangeType enum  - Created TransitionType enum, deprecates Transition.operation int field.  - Fixed incorrect int value used in the YukonUserDaoImpl; replaced with enum.

--- yukon-client/common/src/com/cannontech/database/db/CTIDbChange.java
+++ yukon-client/common/src/com/cannontech/database/db/CTIDbChange.java
@@ -1,19 +1,13 @@
 package com.cannontech.database.db;
 
 /**
- * Insert the type's description here.
- * Creation date: (9/12/2001 9:57:13 AM)
- * @author: 
+ *  Every object that has a DBChangeMsg type associated 
+ * with it should implement this interface.
  */
-// Every object that has a DBChangeMsg type associated 
-//  with it should implement this interface.
+
 import com.cannontech.message.dispatch.message.DBChangeMsg;
+import com.cannontech.message.dispatch.message.DbChangeType;
 
 public interface CTIDbChange {
-/**
- * Insert the method's description here.
- * Creation date: (9/12/2001 9:57:50 AM)
- * @return com.cannontech.message.dispatch.message.DBChangeMsg
- */
-DBChangeMsg[] getDBChangeMsgs( int typeOfChange );
+    public abstract DBChangeMsg[] getDBChangeMsgs(DbChangeType dbChangeType);
 }
