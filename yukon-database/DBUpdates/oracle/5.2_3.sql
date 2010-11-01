548db6c0463c8d3791d665189659257d513ce1d3
Kruse, Matt <>
2010-11-01 04:41:48 +0000

    YUK-9213, CapControl: Change strategy method and units in database to be new enum entries.
    - Cleaned up the enum values in the CapControlStrategy table.

---
+++ yukon-database/DBUpdates/oracle/5.2_3.sql
@@ -0,0 +1,48 @@
+/******************************************/ 
+/**** Oracle DBupdates                 ****/ 
+/******************************************/ 
+
+/* Start YUK-9213 */
+UPDATE CapControlStrategy 
+SET ControlMethod = 'INDIVIDUAL_FEEDER' 
+WHERE ControlMethod = 'IndividualFeeder'; 
+UPDATE CapControlStrategy 
+SET ControlMethod = 'SUBSTATION_BUS' 
+WHERE ControlMethod = 'SubstationBus'; 
+UPDATE CapControlStrategy 
+SET ControlMethod = 'BUSOPTIMIZED_FEEDER' 
+WHERE ControlMethod = 'BusOptimizedFeeder'; 
+UPDATE CapControlStrategy 
+SET ControlMethod = 'MANUAL_ONLY' 
+WHERE ControlMethod = 'ManualOnly'; 
+UPDATE CapControlStrategy 
+SET ControlMethod = 'TIME_OF_DAY' 
+WHERE ControlMethod = 'TimeOfDay'; 
+
+UPDATE CapControlStrategy 
+SET ControlUnits = 'NONE' 
+WHERE ControlUnits = '(none)'; 
+UPDATE CapControlStrategy 
+SET ControlUnits = 'MULTI_VOLT' 
+WHERE ControlUnits = 'Multi Volt'; 
+UPDATE CapControlStrategy 
+SET ControlUnits = 'KVAR' 
+WHERE ControlUnits = 'kVAr'; 
+UPDATE CapControlStrategy 
+SET ControlUnits = 'MULTI_VOLT_VAR' 
+WHERE ControlUnits = 'Multi Volt/VAR'; 
+UPDATE CapControlStrategy 
+SET ControlUnits = 'PFACTOR_KW_KVAR' 
+WHERE ControlUnits = 'P-Factor kW/kVAr'; 
+UPDATE CapControlStrategy 
+SET ControlUnits = 'TIME_OF_DAY' 
+WHERE ControlUnits = 'Time of Day'; 
+UPDATE CapControlStrategy 
+SET ControlUnits = 'INTEGRATED_VOLT_VAR' 
+WHERE ControlUnits = 'Integrated Volt/Var';
+/* End YUK-9213 */
+
+/**************************************************************/ 
+/* VERSION INFO                                               */ 
+/*   Automatically gets inserted from build script            */ 
+/**************************************************************/ 
