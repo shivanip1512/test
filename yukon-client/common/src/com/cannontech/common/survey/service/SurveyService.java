d87e745432f4464b7f2e684d04790dd6043c2ecd
Stenersen, Ross <>
2010-12-08 15:44:10 +0000

    Formatting cleanup.

--- yukon-client/common/src/com/cannontech/common/survey/service/SurveyService.java
+++ yukon-client/common/src/com/cannontech/common/survey/service/SurveyService.java
@@ -5,15 +5,14 @@ import java.util.List;
 import org.springframework.context.MessageSourceResolvable;
 
 import com.cannontech.common.search.SearchResult;
-import com.cannontech.common.survey.model.Question;
 import com.cannontech.common.survey.model.Survey;
 import com.cannontech.user.YukonUserContext;
 
 public interface SurveyService {
-    SearchResult<Survey> findSurveys(int energyCompanyId, int startIndex,
-                                     int count);
+    public SearchResult<Survey> findSurveys(int energyCompanyId, int startIndex, int count);
 
-    boolean areAllSurveyKeysForContextValid(int surveyId, YukonUserContext userContext);
+    public boolean areAllSurveyKeysForContextValid(int surveyId, YukonUserContext userContext);
 
-    List<MessageSourceResolvable> getKeyErrorsForQuestions(int surveyId, YukonUserContext userContext);
+    public List<MessageSourceResolvable> getKeyErrorsForQuestions(int surveyId,
+            YukonUserContext userContext);
 }
