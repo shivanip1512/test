4a60ab6eaa335e431519a5910be9b5b08abe8351
Spar, Thain <>
2011-04-19 22:55:46 +0000

    YUK-9583 Digi Webservices - Sending SEP Messages
    YUKRV-3690 review changes

--- yukon-client/common/src/com/cannontech/thirdparty/digi/DigiErrorHandler.java
+++ yukon-client/common/src/com/cannontech/thirdparty/digi/DigiErrorHandler.java
@@ -8,13 +8,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.ResponseErrorHandler;
 
+import com.cannontech.thirdparty.exception.DigiRESTException;
+
 public class DigiErrorHandler implements ResponseErrorHandler {
 	
 	private static final Log logger = LogFactory.getLog(DigiErrorHandler.class);
 	
 	public boolean hasError(ClientHttpResponse response) throws IOException {
 		HttpStatus status = response.getStatusCode();
-		if (status == HttpStatus.BAD_REQUEST || status == HttpStatus.INTERNAL_SERVER_ERROR) {
+		if (status == HttpStatus.BAD_REQUEST || 
+		    status == HttpStatus.INTERNAL_SERVER_ERROR ||
+		    status == HttpStatus.UNAUTHORIZED) {
 			return true;
 		}
 
@@ -23,5 +27,6 @@ public class DigiErrorHandler implements ResponseErrorHandler {
 
 	public void handleError(ClientHttpResponse response) throws IOException {
 		logger.error(response.getStatusCode() + " - " + response.getStatusText());
+		throw new DigiRESTException("Error in iDigi Webservices call: " + response.getStatusCode() + " - " + response.getStatusText());
 	}
 }
---
+++ yukon-client/common/src/com/cannontech/thirdparty/exception/DigiRESTException.java
@@ -0,0 +1,9 @@
+package com.cannontech.thirdparty.exception;
+
+import java.io.IOException;
+
+public class DigiRESTException extends IOException {
+    public DigiRESTException(String message) {
+        super(message);
+    }
+}
