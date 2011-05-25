fcbcd5e399b959ea996bc296493f5a9509126d24
Fisher, Matthew S <MatthewSFisher@Eaton.com>
2011-05-25 19:37:34 +0000

    Added multiset insert behavior test.

--- yukon-server/COMMON/test_compiler_behaviors.cpp
+++ yukon-server/COMMON/test_compiler_behaviors.cpp
@@ -1,5 +1,7 @@
 #define BOOST_AUTO_TEST_MAIN "Test compiler behaviors"
 
+#include <set>
+
 #include <boost/test/unit_test.hpp>
 using boost::unit_test_framework::test_suite;
 
@@ -10,3 +12,84 @@ BOOST_AUTO_TEST_CASE(test_modulus_sign)
     BOOST_CHECK_EQUAL(-12 %  10, -2);
     BOOST_CHECK_EQUAL(-12 % -10, -2);
 }
+
+struct ordering_test
+{
+    const int first;
+    const int second;
+
+    ordering_test(int x, int y) :
+        first(x),
+        second(y)
+    {
+    }
+
+    bool operator<(const ordering_test &rhs) const
+    {
+        return first < rhs.first;
+    }
+};
+
+BOOST_AUTO_TEST_CASE(test_multiset_insert_order)
+{
+    std::multiset<ordering_test> t;
+
+    t.insert(ordering_test(3,1));
+    t.insert(ordering_test(3,2));
+    t.insert(ordering_test(3,3));
+    t.insert(ordering_test(3,4));
+    t.insert(ordering_test(3,5));
+
+    {
+        std::multiset<ordering_test>::const_iterator itr = t.begin();
+
+        BOOST_CHECK_EQUAL(itr++->second, 1);
+        BOOST_CHECK_EQUAL(itr++->second, 2);
+        BOOST_CHECK_EQUAL(itr++->second, 3);
+        BOOST_CHECK_EQUAL(itr++->second, 4);
+        BOOST_CHECK_EQUAL(itr++->second, 5);
+    }
+
+    t.insert(ordering_test(1,6));
+
+    {
+        std::multiset<ordering_test>::const_iterator itr = t.begin();
+
+        BOOST_CHECK_EQUAL(itr++->second, 6);
+        BOOST_CHECK_EQUAL(itr++->second, 1);
+        BOOST_CHECK_EQUAL(itr++->second, 2);
+        BOOST_CHECK_EQUAL(itr++->second, 3);
+        BOOST_CHECK_EQUAL(itr++->second, 4);
+        BOOST_CHECK_EQUAL(itr++->second, 5);
+    }
+
+    t.insert(ordering_test(4,7));
+
+    {
+        std::multiset<ordering_test>::const_iterator itr = t.begin();
+
+        BOOST_CHECK_EQUAL(itr++->second, 6);
+        BOOST_CHECK_EQUAL(itr++->second, 1);
+        BOOST_CHECK_EQUAL(itr++->second, 2);
+        BOOST_CHECK_EQUAL(itr++->second, 3);
+        BOOST_CHECK_EQUAL(itr++->second, 4);
+        BOOST_CHECK_EQUAL(itr++->second, 5);
+        BOOST_CHECK_EQUAL(itr++->second, 7);
+    }
+
+    t.insert(ordering_test(4,8));
+
+    {
+        std::multiset<ordering_test>::const_iterator itr = t.begin();
+
+        BOOST_CHECK_EQUAL(itr++->second, 6);
+        BOOST_CHECK_EQUAL(itr++->second, 1);
+        BOOST_CHECK_EQUAL(itr++->second, 2);
+        BOOST_CHECK_EQUAL(itr++->second, 3);
+        BOOST_CHECK_EQUAL(itr++->second, 4);
+        BOOST_CHECK_EQUAL(itr++->second, 5);
+        BOOST_CHECK_EQUAL(itr++->second, 7);
+        BOOST_CHECK_EQUAL(itr++->second, 8);
+    }
+}
+
