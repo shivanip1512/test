f98a104e8b0a181a68c4e65cbc08b25ca8670fa6
Otteson, Jess M <JessMOtteson@Eaton.com>
2011-05-31 16:14:21 +0000

    YUKRV-3734 YUK-9553 Null pointer in WS call not handled Changed to fit standard of extending RuntimeException instead of Exception.

--- yukon-client/common/src/com/cannontech/message/util/BadServerResponseException.java
+++ yukon-client/common/src/com/cannontech/message/util/BadServerResponseException.java
@@ -1,5 +1,5 @@
 package com.cannontech.message.util;
 
-public class BadServerResponseException extends Exception {
+public class BadServerResponseException extends RuntimeException {
     private static final long serialVersionUID = -1945592074714506789L;
 }
