232172e8eecd56d06a0c6425542d7404f8d66325
Kruse, Matt <>
2011-06-08 21:22:46 +0000

    YUK-9692, Create a Water Meter Detail Page
    - Fixed the place of a comment in PaoTag that was accedentially moved down a line.
    - Fixed a missing table name in the UnitMeasureDaoImpl
    - Cleaned up the PaoTypeIconTag to use a dao call to get the deviceIconType class
    - Changed the meterHome.jsp and waterMeterHome.jsp to use the module config pages meterDetail.electric and meterDetail.water.

--- yukon-client/common/src/com/cannontech/common/pao/definition/model/PaoTag.java
+++ yukon-client/common/src/com/cannontech/common/pao/definition/model/PaoTag.java
@@ -41,7 +41,7 @@ public enum PaoTag {
     DIRECT_PROGRAM_ENROLLMENT("Direct Program Enrollment"),
     SEP_PROGRAM_ENROLLMENT("SEP Program Enrollment"),
     DLC_ADDRESS_RANGE_ENFORCE("DLC Address Range Enforced", stringType()),  // range used for validation, may be mfg address or other(field length, for example).
-    PLC_ADDRESS_RANGE("PLC Address Range", stringType()),   // range used for display info, should be limited to mfg addresses
+    DLC_ADDRESS_RANGE("DLC Address Range", stringType()),   // range used for display info, should be limited to mfg addresses
 	DUMMY_LONG_TAG("for internal testing", longType()),  // can be removed when we add a real tag that uses long
 	WATER_METER_DETAIL_DISPLAYABLE("Water Detail Displayable"),
 	;
