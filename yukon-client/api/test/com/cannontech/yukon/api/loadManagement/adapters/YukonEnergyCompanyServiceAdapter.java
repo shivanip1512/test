61093e505d38c7db36ff04badb801e509379d812
Stenersen, Ross <>
2011-06-17 14:45:31 +0000

    YUK-9786 Updates for new energy company pages based on meeting
    Update based on code review.

--- yukon-client/api/test/com/cannontech/yukon/api/loadManagement/adapters/YukonEnergyCompanyServiceAdapter.java
+++ yukon-client/api/test/com/cannontech/yukon/api/loadManagement/adapters/YukonEnergyCompanyServiceAdapter.java
@@ -2,7 +2,6 @@ package com.cannontech.yukon.api.loadManagement.adapters;
 
 import java.util.List;
 
-import com.cannontech.database.cache.StarsDatabaseCache;
 import com.cannontech.database.data.lite.LiteYukonUser;
 import com.cannontech.stars.core.service.YukonEnergyCompanyService;
 import com.cannontech.stars.energyCompany.model.YukonEnergyCompany;
@@ -31,6 +30,6 @@ public class YukonEnergyCompanyServiceAdapter implements YukonEnergyCompanyServi
 
     @Override
     public boolean isDefaultEnergyCompany(YukonEnergyCompany energyCompany) {
-        return energyCompany.getEnergyCompanyId() == StarsDatabaseCache.DEFAULT_ENERGY_COMPANY_ID;
+        throw new UnsupportedOperationException("not implemented");
     }
 }
