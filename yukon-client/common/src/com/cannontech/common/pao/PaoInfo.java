7f43276f4b290c4cf6e6e62e740dddc1df4e0717
Woodward, Daniel <>
2011-08-10 16:42:21 +0000

    YUK-10119 Add encryption key editing to web
    Created a new controller, EncryptionController which handles the encryption page to view and save encryption keys.

--- yukon-client/common/src/com/cannontech/common/pao/PaoInfo.java
+++ yukon-client/common/src/com/cannontech/common/pao/PaoInfo.java
@@ -11,8 +11,30 @@ public enum PaoInfo {
     RDS_TRANSMITTER_SITE_ADDRESS,
     RDS_TRANSMITTER_ENCODER_ADDRESS,
     RDS_TRANSMITTER_TRANSMIT_SPEED,
-    RDS_TRANSMITTER_GROUP_TYPE
+    RDS_TRANSMITTER_GROUP_TYPE,
     
     // Other....
+    CPS_ONE_WAY_ENCRYPTION_KEY(null);
     
+    private final String defaultValue;
+    private final boolean defaultable;
+    
+    PaoInfo() {
+        this.defaultValue = null;
+        this.defaultable = false;
+    }
+    
+    PaoInfo(String defaultValue) {
+        this.defaultable = true;
+        this.defaultValue = defaultValue;
+    }
+    
+    public String getDefaultValue() {
+        return defaultValue;
+    }
+
+    public boolean isDefaultable() {
+        return defaultable;
+    }
+
 }
