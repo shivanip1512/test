1c94bbf47f573d6e010e894e4c2e7cd1da51ddf7
DeZeeuw, Garrett <>
2011-09-09 18:54:10 +0000

    YUK-10170 Update RfnAdress table SerialNumber column to drop spaces
    Added note explaining when the script should and should not be run.

--- yukon-database/Custom Scripts/Oracle/RemoveSpacesFromRfnAddress.sql
+++ yukon-database/Custom Scripts/Oracle/RemoveSpacesFromRfnAddress.sql
@@ -1,2 +1,8 @@
+-- !!! This script MUST be run when upgrading from NM 5.0 to NM 5.1  !!!
+-- !!! ONLY run this script if running or upgrading to NM 5.1        !!!
+-- !!! as it will break an installation that uses NM 5.0.            !!!
+-- !!!                                                               !!!
+-- !!! DO NOT run this script if you are using Network Mangager 5.0  !!!
+
 UPDATE rfnAddress
 SET SerialNumber = TRIM(SerialNumber);
