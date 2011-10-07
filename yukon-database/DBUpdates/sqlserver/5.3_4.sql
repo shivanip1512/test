14a56807191c3ab10b9535d43fe51db4a2067237
DeZeeuw, Garrett <>
2011-10-07 15:30:53 +0000

    YUK-10174 DB Change to add cymDISTService
    Removed CymDIST service from 5.3.4 update scripts.

--- yukon-database/DBUpdates/sqlserver/5.3_4.sql
+++ yukon-database/DBUpdates/sqlserver/5.3_4.sql
@@ -1,16 +1,9 @@
 /******************************************/ 
 /**** SQL Server DBupdates             ****/ 
 /******************************************/ 
-
-/* Start YUK-10174 */ 
-INSERT INTO YukonServices 
-VALUES (18, 'CymDISTMessageListener', 
-        'classpath:com/cannontech/services/cymDISTService/cymDISTServiceContext.xml', 
-        'ServiceManager');
-/* End YUK-10174 */
-
+       
 /**************************************************************/ 
 /* VERSION INFO                                               */ 
 /*   Automatically gets inserted from build script            */ 
 /**************************************************************/ 
-INSERT INTO CTIDatabase VALUES ('5.3', 'Garrett D', '31-Aug-2011', 'Latest Update', 4 );
+INSERT INTO CTIDatabase VALUES ('5.3', 'Garrett D', '12-Sep-2011', 'Latest Update', 4 );
