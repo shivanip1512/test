38e421b267c377d85b644aab1c51a5779ea0dbc1
DeZeeuw, Garrett <>
2011-10-07 15:56:33 +0000

    YUK-10174 DB Change to add cymDISTService
    Fixed date on 5.3_4 updates.

--- yukon-database/DBUpdates/sqlserver/5.3_4.sql
+++ yukon-database/DBUpdates/sqlserver/5.3_4.sql
@@ -6,4 +6,4 @@
 /* VERSION INFO                                               */ 
 /*   Automatically gets inserted from build script            */ 
 /**************************************************************/ 
-INSERT INTO CTIDatabase VALUES ('5.3', 'Garrett D', '12-Sep-2011', 'Latest Update', 4 );
+INSERT INTO CTIDatabase VALUES ('5.3', 'Garrett D', '07-OCT-2011', 'Latest Update', 4 );
