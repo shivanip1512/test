7e813b444243ee04adcd8d0fdabd09ccd84abad7
DeZeeuw, Garrett <>
2011-10-11 22:32:52 +0000

    YUK-10174 DB Change to add cymDISTService
    Removed ctidatabase timestamp.

--- yukon-database/DBUpdates/oracle/5.3_5.sql
+++ yukon-database/DBUpdates/oracle/5.3_5.sql
@@ -11,4 +11,3 @@ DELETE FROM YukonServices
 /* VERSION INFO                                               */ 
 /*   Automatically gets inserted from build script            */ 
 /**************************************************************/ 
-INSERT INTO CTIDatabase VALUES ('5.3', 'Garrett D', '07-OCT-2011', 'Latest Update', 5 );
