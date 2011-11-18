4c9669094c8ece5e499142018252258ff8b55850
Stenersen, Ross <>
2011-11-18 16:57:53 +0000

    YUK-10334 use jQuery for dialogs in Yukon
    Fixed something I broke with the I18N demo page.

--- yukon-client/yukon-web/src/com/cannontech/web/support/development/UiDemosController.java
+++ yukon-client/yukon-web/src/com/cannontech/web/support/development/UiDemosController.java
@@ -1,17 +1,34 @@
 package com.cannontech.web.support.development;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.cannontech.web.security.annotation.CheckDevelopmentMode;
+import com.cannontech.web.taglib.MessageScopeHelper;
 
 @Controller
 @RequestMapping("/development/uiDemos/*")
 @CheckDevelopmentMode
 public class UiDemosController {
+    public static class ScopePeeker {
+        private HttpServletRequest request;
+
+        private ScopePeeker(HttpServletRequest request) {
+            this.request = request;
+        }
+
+        public String getScope() {
+            return MessageScopeHelper.forRequest(request).toString();
+        }
+    }
+
     @RequestMapping("main")
-    public void main(ModelMap model) {
+    public void main(ModelMap model, HttpServletRequest request) {
+        model.addAttribute("scopePeeker", new ScopePeeker(request));
+
         model.addAttribute("numberNegativeFive", -5);
         model.addAttribute("numberZero", 0);
         model.addAttribute("numberOne", 1);
