7f8e4d35d8102e3bf592712ec619c7c6f14c2ead
Kruse, Matt <>
2011-11-22 22:59:17 +0000

    YUK-10427, Out Out Limit Report Needs Per Account Overrides Used Total Filtering Based on the Opt Out Limit
    - Simplified the code for adding the dilimeter to the Opt Out Limit Report.

--- yukon-client/report/src/com/cannontech/analysis/tablemodel/OptOutLimitModel.java
+++ yukon-client/report/src/com/cannontech/analysis/tablemodel/OptOutLimitModel.java
@@ -326,12 +326,7 @@ public class OptOutLimitModel extends BareDatedReportModelBase<OptOutLimitModel.
         if (programNames != null && programNames.size() > 0) {
             programNamesStr = programNames.get(0);
             for (int i = 1; i < programNames.size(); i++) {
-                if (i < programNames.size()) {
-                    programNamesStr += ", ";
-                }
-                
-                String programName = programNames.get(i);
-                programNamesStr += programName;
+                programNamesStr += ", " + programNames.get(i);
             }
         }
         
