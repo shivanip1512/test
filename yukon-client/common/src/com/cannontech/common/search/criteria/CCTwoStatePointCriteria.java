ae322c8c19467121530c357cf6064c3fe9fb2e48
Delegard, Alex <>
2012-01-02 19:32:46 +0000

    YUK-10501 Add more stategroups to the switch point choice in the CBC Web Editor
    -Picker changed to get all points having a two-state state group

--- yukon-client/common/src/com/cannontech/common/search/criteria/CCTwoStatePointCriteria.java
+++ yukon-client/common/src/com/cannontech/common/search/criteria/CCTwoStatePointCriteria.java
@@ -1,21 +1,32 @@
 package com.cannontech.common.search.criteria;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.lucene.search.BooleanClause;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cannontech.core.dao.StateDao;
+import com.cannontech.database.data.lite.LiteStateGroup;
 
 public class CCTwoStatePointCriteria extends YukonObjectCriteriaHelper {
-    
-    public CCTwoStatePointCriteria() {
-        super();
+
+    @Autowired private StateDao stateDao;
+
+    @PostConstruct
+    public void init() {
         //create all the rules for this criteria
         //point for the dual bus should be
         //a. two-state state groups 
         //b. can't have name BANK STATUS
         
         //a
-        addCriteria("stategroupid", 1, BooleanClause.Occur.SHOULD);
-        addCriteria("stategroupid", 4, BooleanClause.Occur.SHOULD);
-        addCriteria("stategroupid", 5, BooleanClause.Occur.SHOULD);
-        addCriteria("stategroupid", -8, BooleanClause.Occur.SHOULD);
+        LiteStateGroup[] allStateGroups = stateDao.getAllStateGroups();
+        for (LiteStateGroup group : allStateGroups) {
+            if (group.getStatesList().size() == 2) {
+                addCriteria("stategroupid", group.getStateGroupID(), BooleanClause.Occur.SHOULD);
+            }
+        }
+
         //b
         addCriteria("pointName", "BANK STATUS", BooleanClause.Occur.MUST_NOT);
     }
