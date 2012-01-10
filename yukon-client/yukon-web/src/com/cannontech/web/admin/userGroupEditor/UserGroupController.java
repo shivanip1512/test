6dd79ad2b390e4e6e9d13266ec61fe6a52ee08a5
DeZeeuw, Garrett <>
2012-01-10 17:52:42 +0000

    YUK-10480 Web User/Group editor is accessable but not usable without Admin Super User role property
    Added annotation: @CheckRoleProperty(YukonRoleProperty.ADMIN_SUPER_USER) to UserGroupController.java.

--- yukon-client/yukon-web/src/com/cannontech/web/admin/userGroupEditor/UserGroupController.java
+++ yukon-client/yukon-web/src/com/cannontech/web/admin/userGroupEditor/UserGroupController.java
@@ -4,9 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.cannontech.core.roleproperties.YukonRoleProperty;
 import com.cannontech.user.YukonUserContext;
+import com.cannontech.web.security.annotation.CheckRoleProperty;
 
 @Controller
+@CheckRoleProperty(YukonRoleProperty.ADMIN_SUPER_USER)
 public class UserGroupController {
     
     /* User Group Editor Home Page */
