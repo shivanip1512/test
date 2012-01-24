2c79a27c6653745b103602facb50a8e0093e2272
Delegard, Alex <>
2012-01-24 20:00:04 +0000

    YUK-10169 Support RF meter alarms and events
    -adding serialVersionUID to RfnAlarm

--- yukon-client/common/src/com/cannontech/amr/rfn/message/alarm/RfnAlarm.java
+++ yukon-client/common/src/com/cannontech/amr/rfn/message/alarm/RfnAlarm.java
@@ -4,6 +4,8 @@ import com.cannontech.amr.rfn.message.event.RfnEvent;
 
 public class RfnAlarm extends RfnEvent {
 
+    private static final long serialVersionUID = 1L;
+
     @Override
     public String toString() {
         return String.format("RfnAlarm [type=%s, rfnMeterIdentifier=%s, timeStamp=%tc, alarmData=%s]",
