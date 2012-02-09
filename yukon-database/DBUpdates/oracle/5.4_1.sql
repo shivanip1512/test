89ff7d81df698480ae35f7a4bcc6781d7baeae4d
Kruse, Matt <>
2012-02-09 23:02:57 +0000

    Fixed the yuk number of YUK-10613 to be YUK-10618.

--- yukon-database/DBUpdates/oracle/5.4_1.sql
+++ yukon-database/DBUpdates/oracle/5.4_1.sql
@@ -97,10 +97,10 @@ DELETE FROM YukonRoleProperty
 WHERE RolePropertyId = -1020;
 /* End YUK-10610 */
 
-/* Start YUK-10613 */
+/* Start YUK-10618 */
 DELETE FROM FdrInterfaceOption WHERE InterfaceId = 29 AND Ordering = 2;
 UPDATE FdrInterfaceOption SET Ordering = 2 WHERE InterfaceId = 29 AND Ordering = 3;
-/* End YUK-10613 */
+/* End YUK-10618 */
 
 /* Start YUK-10255 */
 UPDATE UnitMeasure 
