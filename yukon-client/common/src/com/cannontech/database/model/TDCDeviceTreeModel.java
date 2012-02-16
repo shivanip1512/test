e713c7dadcc4d98fef30671edadce6c006533941
Woodward, Daniel <>
2012-02-16 16:31:11 +0000

    YUK-10655 System points are no longer available in TDC
    Refactored class to go along with java coding standards

--- yukon-client/common/src/com/cannontech/database/model/TDCDeviceTreeModel.java
+++ yukon-client/common/src/com/cannontech/database/model/TDCDeviceTreeModel.java
@@ -1,5 +1,7 @@
 package com.cannontech.database.model;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Vector;
 
 import com.cannontech.common.pao.PaoCategory;
@@ -8,7 +10,6 @@ import com.cannontech.common.pao.PaoType;
 import com.cannontech.database.cache.DefaultDatabaseCache;
 import com.cannontech.database.data.lite.LiteComparators;
 import com.cannontech.database.data.lite.LiteYukonPAObject;
-import com.cannontech.database.data.pao.PAOGroups;
 import com.cannontech.yukon.IDatabaseCache;
 
 /**
@@ -17,73 +18,68 @@ import com.cannontech.yukon.IDatabaseCache;
  * @author: 
  * @Version: <version>
  */
-public class TDCDeviceTreeModel extends AbstractDeviceTreeModel
-{
+public class TDCDeviceTreeModel extends AbstractDeviceTreeModel {
 
 	
-/**
- * NoEmptyDeviceTreeModel constructor comment.
- * @param root javax.swing.tree.TreeNode
- */
-public TDCDeviceTreeModel() 
-{
-	super( true, new DBTreeNode("Object Types") );
-}
-/**
- * Insert the method's description here.
- * Creation date: (4/22/2002 4:11:23 PM)
- * @param deviceType int
- */
-public boolean isDeviceValid( PaoCategory paoCategory, PaoClass paoClass, PaoType paoType )
-{
-	return true;
-}
+    /**
+     * NoEmptyDeviceTreeModel constructor comment.
+     * @param root javax.swing.tree.TreeNode
+     */
+    public TDCDeviceTreeModel() {
+    	super(true, new DBTreeNode("Object Types"));
+    }
+    /**
+     * Insert the method's description here.
+     * Creation date: (4/22/2002 4:11:23 PM)
+     * @param deviceType int
+     */
+    public boolean isDeviceValid(PaoCategory paoCategory, PaoClass paoClass, PaoType paoType) {
+    	return true;
+    }
 
-protected synchronized java.util.List getCacheList(IDatabaseCache cache ) 
-{
-    return cache.getAllYukonPAObjects();
-}
+    protected synchronized java.util.List<LiteYukonPAObject> getCacheList(IDatabaseCache cache) {
+        return cache.getAllYukonPAObjects();
+    }
 
-// Override me if you want a sub class to do something different.
-protected synchronized void runUpdate() 
-{
-	DBTreeNode rootNode = (DBTreeNode)getRoot();
-	Vector typeList = new Vector(32);
+    // Override me if you want a sub class to do something different.
+    protected synchronized void runUpdate() {
+    	DBTreeNode rootNode = (DBTreeNode)getRoot();
+    	Vector<DummyTreeNode> typeList = new Vector<DummyTreeNode>(32);
 
-	IDatabaseCache cache = DefaultDatabaseCache.getInstance();
-	synchronized (cache)
-	{
-		java.util.List paos = getCacheList(cache);
-		java.util.Collections.sort( paos, LiteComparators.litePaoTypeComparator );
+    	IDatabaseCache cache = DefaultDatabaseCache.getInstance();
+    	synchronized (cache) {
+    		List<LiteYukonPAObject> paos = getCacheList(cache);
+    		Collections.sort(paos, LiteComparators.litePaoTypeComparator);
 
-		rootNode.removeAllChildren();
-		
-		int currType = Integer.MIN_VALUE;
-		DummyTreeNode devTypeNode = null;
-		for( int i = 0; i < paos.size(); i++ )
-		{
-			LiteYukonPAObject litPAO = (LiteYukonPAObject)paos.get(i);
+    		rootNode.removeAllChildren();
+    		
+    		int currType = Integer.MIN_VALUE;
+    		DummyTreeNode devTypeNode = null;
+    		for(int i=0; i<paos.size(); i++) {
+    			LiteYukonPAObject litPao = paos.get(i);
 
-    		if( currType != litPAO.getPaoType().getDeviceTypeId() ) {
-    			devTypeNode = new DummyTreeNode(litPAO.getPaoType().getDbString());
-    			typeList.add( devTypeNode );
-    		}
+        		if(currType != litPao.getPaoType().getDeviceTypeId()) {
+        			devTypeNode = new DummyTreeNode(litPao.getPaoType().getDbString());
+        			typeList.add(devTypeNode);
+        		}
 
-    		DBTreeNode deviceNode = getNewNode(litPAO);
-    		devTypeNode.add(deviceNode);
-    		deviceNode.setWillHaveChildren(true);
+        		DBTreeNode deviceNode = getNewNode(litPao);
+        		devTypeNode.add(deviceNode);
+        		deviceNode.setWillHaveChildren(true);
 
-    		currType = litPAO.getPaoType().getDeviceTypeId();
+        		currType = litPao.getPaoType().getDeviceTypeId();
 
-		} //for loop		
-	} //synch
+    		} //for loop		
+    	} //synch
 
-	//this list will be a fixed size with a controlled max value
-	java.util.Collections.sort( typeList, DummyTreeNode.comparator);
-	for( int i = 0; i < typeList.size(); i++ )
-		rootNode.add( (DummyTreeNode)typeList.get(i) );
+    	//this list will be a fixed size with a controlled max value
+    	Collections.sort(typeList, DummyTreeNode.comparator);
 
-	reload();	
-}
+    	for( int i = 0; i < typeList.size(); i++ ) {
+    		rootNode.add(typeList.get(i));
+    	}
+
+    	reload();
+    }
 
 }
