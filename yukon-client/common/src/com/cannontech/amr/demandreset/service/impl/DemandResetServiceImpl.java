016a0df41a92652d17e96af172288b5f259237e2
Stenersen, Ross <>
2012-02-27 17:02:23 +0000

    YUK-10635 AMR - EIM Web Services - add RF support and verification to demand reset web service
    This is the matching check-in to YUK-10607.  Added 470 to supported devices and simplified command.

--- yukon-client/common/src/com/cannontech/amr/demandreset/service/impl/DemandResetServiceImpl.java
+++ yukon-client/common/src/com/cannontech/amr/demandreset/service/impl/DemandResetServiceImpl.java
@@ -3,6 +3,7 @@ package com.cannontech.amr.demandreset.service.impl;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSourceResolvable;
@@ -10,9 +11,6 @@ import org.springframework.context.MessageSourceResolvable;
 import com.cannontech.amr.demandreset.service.DemandResetService;
 import com.cannontech.common.bulk.collection.device.DeviceCollection;
 import com.cannontech.common.device.DeviceRequestType;
-import com.cannontech.common.device.commands.CommandCallback;
-import com.cannontech.common.device.commands.CommandRequestDevice;
-import com.cannontech.common.device.commands.CommandRequestExecutionParameterDto;
 import com.cannontech.common.device.commands.GroupCommandExecutor;
 import com.cannontech.common.device.commands.GroupCommandResult;
 import com.cannontech.common.device.model.SimpleDevice;
@@ -23,60 +21,28 @@ import com.cannontech.database.data.lite.LiteYukonUser;
 import com.google.common.base.Function;
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableMap.Builder;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 
 public class DemandResetServiceImpl implements DemandResetService {
-    // This isn't the complete command but the version of groupCommandExecutor.execute we're calling
-    // right now doesn't use it anyway.
-    // In 5.4.2, we'll switch to the version of groupCommandExecutor.execute that does.  When we do,
-    // this will be the correct command.  (We need an update in porter to support this.)
-    private final static String command = "putvalue ied reset";
-
-    private final static ImmutableMap<PaoType, CommandCallback> commandsByPaoType;
-    static {
-        Builder<PaoType, CommandCallback> builder = ImmutableMap.builder();
-
-        builder.put(PaoType.MCT430A, new CommandCallback() {
-            @Override
-            public String generateCommand(CommandRequestExecutionParameterDto parameterDto) {
-                return "putvalue ied reset alpha";
-            }
-        });
-        builder.put(PaoType.MCT430A3, new CommandCallback() {
-            @Override
-            public String generateCommand(CommandRequestExecutionParameterDto parameterDto) {
-                return "putvalue ied reset a3";
-            }
-        });
-        builder.put(PaoType.MCT430S4, new CommandCallback() {
-            @Override
-            public String generateCommand(CommandRequestExecutionParameterDto parameterDto) {
-                return "putvalue ied reset s4";
-            }
-        });
-        builder.put(PaoType.MCT430SL, new CommandCallback() {
-            @Override
-            public String generateCommand(CommandRequestExecutionParameterDto parameterDto) {
-                return "putvalue ied reset sentinal";
-            }
-        });
-
-        commandsByPaoType = builder.build();
-    }
+    private final static String COMMAND = "putvalue ied reset";
+    private final static Set<PaoType> validTypes =
+        ImmutableSet.of(PaoType.MCT430A,
+                        PaoType.MCT430A3,
+                        PaoType.MCT430S4,
+                        PaoType.MCT430SL,
+                        PaoType.MCT470);
 
     private final static Predicate<YukonPao> isValidDevice = new Predicate<YukonPao>() {
         @Override
         public boolean apply(YukonPao pao) {
-            return commandsByPaoType.containsKey(pao.getPaoIdentifier().getPaoType());
+            return validTypes.contains(pao.getPaoIdentifier().getPaoType());
         }
     };
     private final static Predicate<YukonPao> isInvalidDevice = Predicates.not(isValidDevice);
 
-    @Autowired
-    private GroupCommandExecutor groupCommandExecutor;
+    @Autowired private GroupCommandExecutor groupCommandExecutor;
 
     @Override
     public <T extends YukonPao> Iterable<T> validDevices(Iterable<T> paos) {
@@ -101,9 +67,6 @@ public class DemandResetServiceImpl implements DemandResetService {
                 }
             }));
 
-        // TODO:  We should split DeviceCollection into two interfaces--one for the basic collection
-        // of devices needed by the service calls that execute the commands (and this method)
-        // and another which contains the UI stuff (getDescription and getCollectionParameters).
         DeviceCollection deviceCollection = new DeviceCollection() {
             @Override
             public Iterator<SimpleDevice> iterator() {
@@ -135,23 +98,14 @@ public class DemandResetServiceImpl implements DemandResetService {
                 return null;
             }
         };
-        Function<SimpleDevice, CommandRequestDevice> commandForDevice =
-            new Function<SimpleDevice, CommandRequestDevice>() {
-                @Override
-                public CommandRequestDevice apply(SimpleDevice device) {
-                    CommandRequestDevice retVal = new CommandRequestDevice();
-                    retVal.setDevice(device);
-                    retVal.setCommandCallback(commandsByPaoType.get(device.getDeviceType()));
-                    return retVal;
-                }
-            };
-        List<CommandRequestDevice> requests = Lists.transform(devices, commandForDevice);
-        SimpleCallback<GroupCommandResult> ceCallback = new SimpleCallback<GroupCommandResult>() {
+
+        SimpleCallback<GroupCommandResult> gceCallback = new SimpleCallback<GroupCommandResult>() {
             @Override
             public void handle(GroupCommandResult item) throws Exception {
             }
         };
-        groupCommandExecutor.execute(deviceCollection, command, requests,
-                                     DeviceRequestType.GROUP_COMMAND, ceCallback, user);
+
+        groupCommandExecutor.execute(deviceCollection, COMMAND, DeviceRequestType.GROUP_COMMAND,
+                                     gceCallback, user);
     }
 }
