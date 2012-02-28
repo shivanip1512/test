b068b305dce937fa9cf159d51474367b14472939
Ganz, Julie <>
2012-02-28 23:24:55 +0000

    YUK-10657 Capcontrol Strategies are not being cascaded properly

---
+++ yukon-server/CapControl/ccAreaBase.cpp
@@ -0,0 +1,306 @@
+
+#include "precompiled.h"
+
+#include <rw/tpsrtvec.h>
+
+#include "dbaccess.h"
+#include "msg_signal.h"
+
+#include "ccareabase.h"
+#include "ccid.h"
+#include "cccapbank.h"
+#include "pointdefs.h"
+#include "pointtypes.h"
+#include "logger.h"
+#include "capcontroller.h"
+#include "resolvers.h"
+#include "utility.h"
+#include "ccOperationStats.h"
+#include "ccConfirmationStats.h"
+#include "database_writer.h"
+
+using std::endl;
+using namespace Cti::CapControl;
+
+extern ULONG _CC_DEBUG;
+
+RWDEFINE_COLLECTABLE( CtiCCAreaBase, CTICCAREABASE_ID )
+
+/*---------------------------------------------------------------------------
+    Constructors
+---------------------------------------------------------------------------*/
+CtiCCAreaBase::CtiCCAreaBase()
+    : Controllable(0),
+      _voltReductionControlPointId(0),
+      _voltReductionControlValue(0),
+      _pfactor(0),
+      _estPfactor(0),
+      _ovUvDisabledFlag(false),
+      _dirty(false)
+{
+}
+
+CtiCCAreaBase::CtiCCAreaBase(StrategyManager * strategyManager)
+    : Controllable(strategyManager),
+      _voltReductionControlPointId(0),
+      _voltReductionControlValue(0),
+      _pfactor(0),
+      _estPfactor(0),
+      _ovUvDisabledFlag(false),
+      _dirty(false)
+{
+}
+
+CtiCCAreaBase::CtiCCAreaBase(Cti::RowReader& rdr, StrategyManager * strategyManager)
+    : Controllable(rdr, strategyManager)
+{
+    restore(rdr);
+    _operationStats.setPAOId(getPaoId());
+    _confirmationStats.setPAOId(getPaoId());
+}
+
+CtiCCAreaBase::CtiCCAreaBase(const CtiCCAreaBase& base)
+    : Controllable(base)
+{
+    operator=(base);
+}
+
+/*---------------------------------------------------------------------------
+    Destructor
+---------------------------------------------------------------------------*/
+CtiCCAreaBase::~CtiCCAreaBase()
+{
+    if (!_subStationIds.empty())
+    {
+        _subStationIds.clear();
+    }
+}
+
+CtiCCOperationStats& CtiCCAreaBase::getOperationStats()
+{
+    return _operationStats;
+}
+
+CtiCCConfirmationStats& CtiCCAreaBase::getConfirmationStats()
+{
+    return _confirmationStats;
+}
+
+
+/*---------------------------------------------------------------------------
+    operator=
+---------------------------------------------------------------------------*/
+CtiCCAreaBase& CtiCCAreaBase::operator=(const CtiCCAreaBase& right)
+{
+    Controllable::operator=(right);
+
+    if( this != &right )
+    {
+        _ovUvDisabledFlag = right._ovUvDisabledFlag;
+        _pfactor = right._pfactor;
+        _estPfactor = right._estPfactor;
+
+        _voltReductionControlPointId = right._voltReductionControlPointId;
+        _voltReductionControlValue = right._voltReductionControlValue;
+        _additionalFlags = right._additionalFlags;
+
+        _subStationIds = right._subStationIds;
+
+        _operationStats = right._operationStats;
+        _confirmationStats = right._confirmationStats;
+        _dirty = right._dirty;
+
+    }
+    return *this;
+}
+
+/*---------------------------------------------------------------------------
+    replicate
+
+    Restores self's operation fields
+---------------------------------------------------------------------------*/
+CtiCCAreaBase* CtiCCAreaBase::replicate() const
+{
+    return(new CtiCCAreaBase(*this));
+}
+
+/*---------------------------------------------------------------------------
+    saveGuts
+
+    Save self's state onto the given stream
+---------------------------------------------------------------------------*/
+void CtiCCAreaBase::saveGuts(RWvostream& ostrm ) const
+{
+    RWCollectable::saveGuts( ostrm );
+    CapControlPao::saveGuts(ostrm);
+}
+
+/*---------------------------------------------------------------------------
+    restore
+
+    Restores given a Reader
+---------------------------------------------------------------------------*/
+void CtiCCAreaBase::restore(Cti::RowReader& rdr)
+{
+    rdr["voltreductionpointid"] >> _voltReductionControlPointId;
+    if (_voltReductionControlPointId <= 0)
+    {
+        setVoltReductionControlValue(FALSE);
+    }
+    setOvUvDisabledFlag(FALSE);
+    setPFactor(-1);
+    setEstPFactor(-1);
+    setDirty(false);
+}
+void CtiCCAreaBase::setDynamicData(Cti::RowReader& rdr)
+{
+    string tempBoolString;
+
+    rdr["additionalflags"] >> _additionalFlags;
+    std::transform(_additionalFlags.begin(), _additionalFlags.end(), _additionalFlags.begin(), tolower);
+
+    _ovUvDisabledFlag = (_additionalFlags[0]=='y'?TRUE:FALSE);
+}
+
+void CtiCCAreaBase::setDirty(bool flag)
+{
+    _dirty = flag;
+}
+
+string CtiCCAreaBase::getAdditionalFlags() const
+{
+    return _additionalFlags;
+}
+
+/*---------------------------------------------------------------------------
+    getControlPointId
+
+    Returns the controlPoint Id of the area
+---------------------------------------------------------------------------*/
+LONG CtiCCAreaBase::getVoltReductionControlPointId() const
+{
+    return _voltReductionControlPointId;
+}
+
+/*---------------------------------------------------------------------------
+    getControlValue
+
+    Returns the ControlValue flag of the area
+---------------------------------------------------------------------------*/
+BOOL CtiCCAreaBase::getVoltReductionControlValue() const
+{
+    return _voltReductionControlValue;
+}
+
+
+
+/*---------------------------------------------------------------------------
+    getOvUvDisabledFlag
+
+    Returns the ovuv disable flag of the area
+---------------------------------------------------------------------------*/
+BOOL CtiCCAreaBase::getOvUvDisabledFlag() const
+{
+    return _ovUvDisabledFlag;
+}
+
+/*---------------------------------------------------------------------------
+    getPFactor
+
+    Returns the getPFactor of the area
+---------------------------------------------------------------------------*/
+DOUBLE CtiCCAreaBase::getPFactor() const
+{
+    return _pfactor;
+}
+/*---------------------------------------------------------------------------
+    getEstPFactor
+
+    Returns the getEstPFactor of the area
+---------------------------------------------------------------------------*/
+DOUBLE CtiCCAreaBase::getEstPFactor() const
+{
+    return _estPfactor;
+}
+
+/*---------------------------------------------------------------------------
+    setControlPointId
+
+    Sets the ControlPointId of the area
+---------------------------------------------------------------------------*/
+CtiCCAreaBase& CtiCCAreaBase::setVoltReductionControlPointId(LONG pointId)
+{
+    _voltReductionControlPointId = pointId;
+    return *this;
+}
+/*---------------------------------------------------------------------------
+    setControlValue
+
+    Sets the ControlValue flag of the area
+---------------------------------------------------------------------------*/
+CtiCCAreaBase& CtiCCAreaBase::setVoltReductionControlValue(BOOL flag)
+{
+    if (_voltReductionControlValue != flag)
+        _dirty = true;
+    _voltReductionControlValue = flag;
+    return *this;
+}
+
+
+/*---------------------------------------------------------------------------
+    setOvUvDisabledFlag
+
+    Sets the ovuv disable flag of the area
+---------------------------------------------------------------------------*/
+CtiCCAreaBase& CtiCCAreaBase::setOvUvDisabledFlag(BOOL flag)
+{
+    if (_ovUvDisabledFlag != flag)
+        _dirty = true;;
+    _ovUvDisabledFlag = flag;
+    return *this;
+}
+
+/*---------------------------------------------------------------------------
+    setPFactor
+
+    Sets the PFactor of the area
+---------------------------------------------------------------------------*/
+CtiCCAreaBase& CtiCCAreaBase::setPFactor(DOUBLE pfactor)
+{
+    _pfactor = pfactor;
+    return *this;
+}
+/*---------------------------------------------------------------------------
+    setEstPFactor
+
+    Sets the estPFactor of the area
+---------------------------------------------------------------------------*/
+CtiCCAreaBase& CtiCCAreaBase::setEstPFactor(DOUBLE estpfactor)
+{
+    _estPfactor = estpfactor;
+    return *this;
+}
+
+void CtiCCAreaBase::addSubstationId(long subId)
+{
+    _subStationIds.push_back(subId);
+}
+PaoIdVector::iterator CtiCCAreaBase::removeSubstationId(PaoIdVector::iterator subIter)
+{
+    return _subStationIds.erase(subIter);
+}
+
+
+void CtiCCAreaBase::removeSubstationId(long subId)
+{
+    for (PaoIdVector::const_iterator subIter =_subStationIds.begin(); subIter != _subStationIds.end(); ++subIter)
+    {
+        if (*subIter == subId)
+        {
+            _subStationIds.erase(subIter);
+            return;
+        }
+
+    }
+}
+            
---
+++ yukon-server/CapControl/include/ccAreaBase.h
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <rw/collect.h>
+#include <rw/vstream.h>
+#include <rw/thr/mutex.h>
+#include <rw/thr/recursiv.h>
+
+#include "dbaccess.h"
+#include "connection.h"
+#include "types.h"
+#include "observe.h"
+#include "ccsubstationbus.h"
+#include "ccfeeder.h"
+#include "cccapbank.h"
+#include "msg_pcrequest.h"
+#include "msg_cmd.h"
+#include "StrategyManager.h"
+#include "ccmonitorpoint.h"
+#include "Controllable.h"
+
+class CtiCCAreaBase : public Controllable, public RWCollectable
+{
+
+public:
+    RWDECLARE_COLLECTABLE( CtiCCAreaBase )
+    CtiCCAreaBase();
+    CtiCCAreaBase(StrategyManager * strategyManager);
+    CtiCCAreaBase(Cti::RowReader& rdr, StrategyManager * strategyManager);
+    CtiCCAreaBase(const CtiCCAreaBase& area);
+
+    virtual ~CtiCCAreaBase();
+
+    LONG getVoltReductionControlPointId() const;
+    BOOL getVoltReductionControlValue() const;
+    BOOL getOvUvDisabledFlag() const;
+    DOUBLE getPFactor() const;
+    DOUBLE getEstPFactor() const;
+    string getAdditionalFlags() const;
+
+    Cti::CapControl::PaoIdVector getSubstationIds() const {return _subStationIds;};
+    void addSubstationId(long subId);
+    Cti::CapControl::PaoIdVector::iterator removeSubstationId(Cti::CapControl::PaoIdVector::iterator subIter);
+    void removeSubstationId(long subId);
+    CtiCCOperationStats& getOperationStats();
+    CtiCCConfirmationStats& getConfirmationStats();
+
+    CtiCCAreaBase& setVoltReductionControlPointId(LONG pointId);
+    CtiCCAreaBase& setVoltReductionControlValue(BOOL flag);
+    CtiCCAreaBase& setOvUvDisabledFlag(BOOL flag);
+    CtiCCAreaBase& setPFactor(DOUBLE pfactor);
+    CtiCCAreaBase& setEstPFactor(DOUBLE estPfactor);
+
+    void setDirty(bool flag);
+    bool isDirty() {return _dirty;};
+    virtual bool isSpecial() {return false;};
+
+
+    void setDynamicData(Cti::RowReader& rdr);
+    void restore(Cti::RowReader& rdr);
+
+    //Members inherited from RWCollectable
+    void saveGuts(RWvostream& ) const;
+
+    CtiCCAreaBase& operator=(const CtiCCAreaBase& right);
+
+    CtiCCAreaBase* replicate() const;
+
+private:
+
+    LONG _voltReductionControlPointId;
+    BOOL _voltReductionControlValue;
+    DOUBLE _pfactor;
+    DOUBLE _estPfactor;
+    string _additionalFlags;
+    BOOL _ovUvDisabledFlag;
+    bool _dirty;
+
+    Cti::CapControl::PaoIdVector _subStationIds;
+
+    CtiCCOperationStats _operationStats;
+    CtiCCConfirmationStats _confirmationStats;
+
+
+};
+
+typedef CtiCCAreaBase* CtiCCAreaBasePtr;
+
