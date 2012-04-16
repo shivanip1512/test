c94f68064ff58657e97a1ccd7c748f8d625548c5
Johnston, Samuel T <SamuelTJohnston@Eaton.com>
2012-04-16 20:05:52 +0000

    YUK-10808 FDR translation importer log locks up browser on very large imports Modified logging javascript to append new log statements much more efficiently.

--- yukon-client/yukon-web/WebContent/JavaScript/fdrImportLogger.js
+++ yukon-client/yukon-web/WebContent/JavaScript/fdrImportLogger.js
@@ -1,15 +1,17 @@
 function initiateLogUpdate(url, updateSecs) {
     var getNewLines = function() {
         jQuery.getJSON(url, function(data) {
+            //build new rows
             var items = [];
             jQuery.each(data, function(logString, quality) {
                 items.push('<span class="' + quality + '">' + logString + '</span><br>');
             });
-            var currentHtml = jQuery('#logDiv').html();
-            jQuery('#logDiv').html(currentHtml + items.join(''));
+            
+            //add new rows
+            var logDiv = jQuery('#logDiv');
+            logDiv.append(items.join(''));
         });
         setTimeout(getNewLines, updateSecs * 1000);
     };
     getNewLines();
 }
-
