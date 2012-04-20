7a6ed2e6c245fe0eca17db83786810b7bba93e79
DeZeeuw, Garrett <>
2012-04-20 15:01:28 +0000

    5.5.0 ctidatabase insert

--- yukon-database/DBUpdates/oracle/5.5_0.sql
+++ yukon-database/DBUpdates/oracle/5.5_0.sql
@@ -2,10 +2,10 @@
 /****     Oracle DBupdates             ****/ 
 /******************************************/ 
 
-/* Start YUK- */
-/* End YUK- */
+/* No Updates */
 
 /**************************************************************/ 
 /* VERSION INFO                                               */ 
 /*   Automatically gets inserted from build script            */ 
 /**************************************************************/ 
+INSERT INTO CTIDatabase VALUES ('5.5', 'Garrett D', '20-APR-2012', 'Latest Update', 0 );
