db985f617b55a197264e80a1eeafeb208ad27627
DeZeeuw, Garrett <>
2012-04-23 19:28:32 +0000

    SoapUI Automation Testing - Batch script to replace localhost with test machine IP

--- yukon-test/webservices/replaceText.bat
+++ yukon-test/webservices/replaceText.bat
@@ -15,6 +15,6 @@ for /f "tokens=1,* delims=]" %%A in ('"type %3|find /n /v """') do (
     set "line=%%B"
     if defined line (
         call set "line=echo.%%line:%~1=%~2%%"
-        for /f "delims=" %%X in ('"echo."%%line%%""') do %%~X
+        for /f "delims=" %%X in ('"echo."%%line%%""') do %%~X >> changedIP_%3
     ) ELSE echo.
 )
