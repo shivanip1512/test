8fbc718d42a96bc644455e81f6d04eae64d5bfe6
Johnston, Samuel T <SamuelTJohnston@Eaton.com>
2012-04-23 15:04:46 +0000

    Gave ant more memory.

--- yukon-build/go.bat
+++ yukon-build/go.bat
@@ -2,6 +2,6 @@ echo:
 echo Executing %~df0 %*
 echo:
 
-set ANT_OPTS=-Xmx1024m
+set ANT_OPTS=-Xmx2048m
 %~dp0..\yukon-client\build\ant\bin\ant -f %~dp0build.xml %*
 if not "%ERRORLEVEL%" == "0" exit %ERRORLEVEL%
