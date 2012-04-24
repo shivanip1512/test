4e422e0bd702edb790e8b3d9ebbf82429ab357a3
Kruse, Matt <>
2012-04-24 15:29:42 +0000

    Alphabetized the MasterConfigBooleanKeysEnum enums

--- yukon-client/common/src/com/cannontech/common/config/MasterConfigBooleanKeysEnum.java
+++ yukon-client/common/src/com/cannontech/common/config/MasterConfigBooleanKeysEnum.java
@@ -2,16 +2,16 @@ package com.cannontech.common.config;
 
 public enum MasterConfigBooleanKeysEnum {
 
+    BULK_IMPORTER_SINGLE_GROUP,	//option to limit membership to only 1 child group per parent group. 
+    DEFAULT_ENERGY_COMPANY_EDIT,
+    DEVELOPMENT_MODE,
+    DIGI_ENABLED,
+    DISABLE_ANALYTICS,
+    ENABLE_INVENTORY_SAVE_TO_FILE,
 	ENABLE_WEB_DEBUG_PAGES,
-	DEVELOPMENT_MODE,
 	I18N_DESIGN_MODE,
-	DEFAULT_ENERGY_COMPANY_EDIT,
+	SHOW_ONE_WAY_ENCRYPT,
 	USE_OLD_FORGOTTEN_PASSWORD_PAGE,
 	VIRTUAL_PROGRAMS,
-	SHOW_ONE_WAY_ENCRYPT,
-	BULK_IMPORTER_SINGLE_GROUP,	//option to limit membership to only 1 child group per parent group. 
-	DIGI_ENABLED,
-	ENABLE_INVENTORY_SAVE_TO_FILE,
-	DISABLE_ANALYTICS,
 	;
 }
