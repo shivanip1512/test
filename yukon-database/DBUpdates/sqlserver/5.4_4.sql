3e3ccfe0b1aebcfca0386c024d942e26d2825fac
DeZeeuw, Garrett <>
2012-04-24 16:02:35 +0000

    YUK-10919 DB Change Add Service to YukonServices for OPC

---
+++ yukon-database/DBUpdates/sqlserver/5.4_4.sql
@@ -0,0 +1,13 @@
+/******************************************/ 
+/**** SQL Server DBupdates             ****/ 
+/******************************************/ 
+
+/* Start YUK-10919 */
+INSERT INTO YukonServices
+VALUES (20,'OpcService','classpath:com/cannontech/services/opc/opcService.xml','ServiceManager');
+/* End YUK-10919 */
+
+/**************************************************************/ 
+/* VERSION INFO                                               */ 
+/*   Automatically gets inserted from build script            */ 
+/**************************************************************/ 
