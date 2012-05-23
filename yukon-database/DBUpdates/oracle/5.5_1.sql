b20c474fc4a81db75c448dcb34caaeffc48c9d42
DeZeeuw, Garrett <>
2012-05-23 18:26:43 +0000

    YUK-10978 DBChange - ArchiveValuesExportField.FieldType - change DLC_ADDRESS values to ADDRESS
    Added updates to 5.5.1 scripts.

---
+++ yukon-database/DBUpdates/oracle/5.5_1.sql
@@ -0,0 +1,20 @@
+/******************************************/ 
+/****     Oracle DBupdates             ****/ 
+/******************************************/ 
+
+/* Start YUK-10982 */
+UPDATE BillingFileFormats
+SET FormatType = 'CMEP-MDM1'
+WHERE FormatType = 'CMEP';
+/* End YUK-10982 */
+
+/* Start YUK-10978 */
+UPDATE ArchiveValuesExportField 
+SET FieldType = 'ADDRESS' 
+WHERE FieldType = 'DLC_ADDRESS';
+/* End YUK-10978 */
+
+/**************************************************************/ 
+/* VERSION INFO                                               */ 
+/*   Automatically gets inserted from build script            */ 
+/**************************************************************/ 
