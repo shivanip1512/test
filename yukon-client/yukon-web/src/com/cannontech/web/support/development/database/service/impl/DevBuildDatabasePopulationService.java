ba1a5d4705df435da135a7c586f06876182c1026
Woodward, Daniel <>
2012-06-05 22:39:59 +0000

    YUK-10955 Make nightly trunk install populate the database in a useful way and reboot itself if necessary

--- yukon-client/yukon-web/src/com/cannontech/web/support/development/database/service/impl/DevBuildDatabasePopulationService.java
+++ yukon-client/yukon-web/src/com/cannontech/web/support/development/database/service/impl/DevBuildDatabasePopulationService.java
@@ -1,17 +1,20 @@
 package com.cannontech.web.support.development.database.service.impl;
 
 
-import com.cannontech.spring.YukonSpringHook;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.cannontech.web.support.development.DevDbSetupTask;
 import com.cannontech.web.support.development.database.service.DevDatabasePopulationService;
 
 public class DevBuildDatabasePopulationService {
     
-    private static DevDatabasePopulationService devDatabasePopulationService;
+    @Autowired private static DevDatabasePopulationService devDatabasePopulationService;
     
     public static void main(String[] args) {
-        devDatabasePopulationService = YukonSpringHook.getBean(DevDatabasePopulationService.class);
+        //devDatabasePopulationService = YukonSpringHook.getBean(DevDatabasePopulationService.class);
         devDatabasePopulationService.executeFullDatabasePopulation(new DevDbSetupTask());
     }
 
+    
+    
 }
