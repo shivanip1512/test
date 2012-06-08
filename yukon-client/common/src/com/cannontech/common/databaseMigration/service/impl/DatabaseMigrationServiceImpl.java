954e722374d8ae41ff33d3a341d62cddcc2441f9
Stenersen, Ross <>
2012-06-08 17:32:10 +0000

    YUK-10363 SelectCom LCR support using native RF radio
    Merged baseapi context into common.

--- yukon-client/common/src/com/cannontech/common/databaseMigration/service/impl/DatabaseMigrationServiceImpl.java
+++ yukon-client/common/src/com/cannontech/common/databaseMigration/service/impl/DatabaseMigrationServiceImpl.java
@@ -28,6 +28,7 @@ import org.jdom.output.XMLOutputter;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ResourceLoaderAware;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
@@ -1527,6 +1528,7 @@ public class DatabaseMigrationServiceImpl implements DatabaseMigrationService, R
     }
     
     @Autowired
+    @Qualifier("main")
     public void setScheduledExecutor(ScheduledExecutor scheduledExecutor) {
 		this.scheduledExecutor = scheduledExecutor;
 	}
