b9945e3522ccfec2e527c37ccfc10ea0e9790558
Stenersen, Ross <>
2012-06-27 14:50:24 +0000

    Formatting cleanup.

--- yukon-client/common/src/com/cannontech/user/checker/AggregateAndUserChecker.java
+++ yukon-client/common/src/com/cannontech/user/checker/AggregateAndUserChecker.java
@@ -8,18 +8,17 @@ import com.cannontech.database.data.lite.LiteYukonUser;
 
 public class AggregateAndUserChecker extends UserCheckerBase {
     private Collection<UserChecker> checkerList = Collections.emptyList();
-    
-    
+
     public AggregateAndUserChecker(Collection<UserChecker> checkerList) {
         this.checkerList = checkerList;
     }
-    
+
     public AggregateAndUserChecker(UserChecker... checkers) {
         this.checkerList = Arrays.asList(checkers);
     }
 
-
     public boolean check(LiteYukonUser user) {
+        System.out.println(toString());
         for (UserChecker checker : checkerList) {
             if (!checker.check(user)) {
                 return false;
@@ -28,15 +27,8 @@ public class AggregateAndUserChecker extends UserCheckerBase {
         return true;
     }
 
-
-    public void setCheckerList(
-            Collection<UserChecker> checkerList) {
-        this.checkerList = checkerList;
-    }
-    
     @Override
     public String toString() {
         return "aggregate and(" + checkerList + ") checker";
     }
-
 }
