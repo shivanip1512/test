672ac1f9153f8e06306b0bcd4af8aa5d9991c091
Kruse, Matt <>
2012-06-29 19:33:42 +0000

    YUK-11093, The static lockout is not unlocking automatically.
    - Added a few junit tests to cover the StatitcAuthenticationThrottle functionality
    - Fixed the the StatitcAuthenticationThrottle unlocking mechanism to unlock after the duration is met.
    - Fixed the count in the StatitcAuthenticationThrottle, which was off by one.

--- yukon-client/common/test/unit/com/cannontech/core/authentication/service/impl/AuthenticationServiceImplTest.java
+++ yukon-client/common/test/unit/com/cannontech/core/authentication/service/impl/AuthenticationServiceImplTest.java
@@ -1,6 +1,12 @@
 package com.cannontech.core.authentication.service.impl;
 
+import org.junit.Test;
+
 public class AuthenticationServiceImplTest {
+    @Test
+    public void placeHolderTest() {
+        
+    }
     
     
 }
