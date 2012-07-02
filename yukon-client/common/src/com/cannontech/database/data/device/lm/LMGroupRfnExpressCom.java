1985a03bb55a70a2daf4bb378190b614a1fa40b6
Pipp, Joseph <>
2012-07-02 21:27:47 +0000

    YUK-11111 RFN: Create new RFN Expresscom Group and implement sending of expresscom to NM from Porter.
    Created new RFN Expresscom Group which is an Expresscom Group that does not have a route.
    
    Added missing file

---
+++ yukon-client/common/src/com/cannontech/database/data/device/lm/LMGroupRfnExpressCom.java
@@ -0,0 +1,16 @@
+package com.cannontech.database.data.device.lm;
+
+public class LMGroupRfnExpressCom extends com.cannontech.database.data.device.lm.LMGroupExpressCom implements IGroupRoute
+{
+    public LMGroupRfnExpressCom()
+    {
+        super();
+        lmGroupExpressComm = new com.cannontech.database.db.device.lm.LMGroupExpressCom();
+        getYukonPAObject().setType( com.cannontech.database.data.pao.PAOGroups.STRING_RFN_EXPRESSCOMM_GROUP[0] );
+        getLMGroupExpressComm().setRouteID( 0 );
+    }
+    
+    //Route ID is unused for RFN Expresscom group, so do nothing when change is attempted
+    public void setRouteID( Integer rtID_ ) {  
+    }
+}
