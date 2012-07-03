ec7ed19881452ce3d68ab9120dad6b962eeb7509
Otteson, Jess M <JessMOtteson@Eaton.com>
2012-07-03 17:50:15 +0000

    YUK-11111 Create new RFN Expresscom Group and implement sending of expresscom to NM from Porter Added code to deal with streamed messages from Porter.

--- yukon-client/common/src/com/cannontech/dr/rfn/message/broadcast/RfnExpressComBroadcastRequest.java
+++ yukon-client/common/src/com/cannontech/dr/rfn/message/broadcast/RfnExpressComBroadcastRequest.java
@@ -3,6 +3,9 @@ package com.cannontech.dr.rfn.message.broadcast;
 import java.io.Serializable;
 import java.util.Arrays;
 
+import javax.jms.JMSException;
+import javax.jms.StreamMessage;
+
 import com.cannontech.common.rfn.message.RfnMessageClass;
 
 /**
@@ -56,6 +59,32 @@ public class RfnExpressComBroadcastRequest implements Serializable {
 
     public void setPayload(byte[] payload) {
         this.payload = payload;
+    }
+    
+    // Created a RfnExpressComBroadcastRequest object based on a stream. This is required
+    // for the c++ code which currently cannot create ObjectMessages.
+    public static RfnExpressComBroadcastRequest create(StreamMessage message) {
+        try {
+            short messageId = message.readShort();
+            int messagePriority = message.readInt();
+            RfnMessageClass messageClass = RfnMessageClass.valueOf(message.readString());
+            long time = message.readLong();
+            int payloadSize = message.readInt();
+
+            byte[] bytes = new byte[payloadSize];
+            message.readBytes(bytes);
+
+            RfnExpressComBroadcastRequest result = new RfnExpressComBroadcastRequest();
+            result.setMessageId(messageId);
+            result.setMessagePriority(messagePriority);
+            result.setRfnMessageClass(messageClass);
+            result.setExpirationTime(time);
+            result.setPayload(bytes);
+
+            return result;
+        } catch (JMSException e) {
+            return null;
+        }
     }
 
     @Override
