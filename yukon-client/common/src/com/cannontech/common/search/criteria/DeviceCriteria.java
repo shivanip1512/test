cc0dc1eb3511b8cd7e353cfa59e5aab3de2d4c00
Feldman, Marina <MarinaFeldman@Eaton.com>
2012-07-06 22:28:29 +0000

    YUK-10880 [Select all ### matching items] link causes Yukon to display an error

--- yukon-client/common/src/com/cannontech/common/search/criteria/DeviceCriteria.java
+++ yukon-client/common/src/com/cannontech/common/search/criteria/DeviceCriteria.java
@@ -5,7 +5,6 @@ import org.apache.lucene.search.BooleanClause;
 public class DeviceCriteria extends YukonObjectCriteriaHelper {
 
     public DeviceCriteria() {
-        int i = 0;
         addCriteria("isDevice", "true", BooleanClause.Occur.SHOULD);
         // devices with paoid >= 1 (prevents picker from displaying system devices)
         addCriteria("paoid", "1", null, true, false, BooleanClause.Occur.MUST);
