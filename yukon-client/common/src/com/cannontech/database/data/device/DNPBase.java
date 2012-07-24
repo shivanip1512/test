18628a1443fec4795fe9647fd7cbe1b61bc1ef48
Gerads, Jordan <>
2012-07-24 14:31:46 +0000

    YUK-10944 Modify DNP master config parameters to be set individually on DNP devices - Added the dnp configuration loading for the DNPBase DBPersistent.

--- yukon-client/common/src/com/cannontech/database/data/device/DNPBase.java
+++ yukon-client/common/src/com/cannontech/database/data/device/DNPBase.java
@@ -1,8 +1,13 @@
 package com.cannontech.database.data.device;
 
+import com.cannontech.common.device.config.dao.DeviceConfigurationDao;
 import com.cannontech.common.device.config.model.DNPConfiguration;
+import com.cannontech.common.device.model.SimpleDevice;
+import com.cannontech.common.pao.PaoType;
+import com.cannontech.common.pao.YukonDevice;
 import com.cannontech.database.data.pao.DeviceClasses;
 import com.cannontech.database.db.DBCopiable;
+import com.cannontech.spring.YukonSpringHook;
 
 public class DNPBase extends AddressBase implements DBCopiable {
     private DNPConfiguration dnpConfiguration = null;
@@ -25,6 +30,12 @@ public class DNPBase extends AddressBase implements DBCopiable {
 
     public void retrieve() throws java.sql.SQLException {
         super.retrieve();
+        
+        DeviceConfigurationDao configurationDao = 
+                YukonSpringHook.getBean("deviceConfigurationDao", DeviceConfigurationDao.class);
+        YukonDevice device = new SimpleDevice(getPAObjectID(), PaoType.getForDbString(getPAOType()));
+        
+        dnpConfiguration = (DNPConfiguration) configurationDao.findConfigurationForDevice(device);
     }
 
     public void setDbConnection(java.sql.Connection conn) {
