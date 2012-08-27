984fc624bd0b758ae085e8f9ebada7a7122f5386
Stenersen, Ross <>
2012-08-27 23:32:46 +0000

    Removed some debugging added to help solve issue running this test on build machine.  Fixed unit test classpath.

--- yukon-client/common/test/unit/com/cannontech/amr/errors/DeviceErrorTranslatorDaoImplTest.java
+++ yukon-client/common/test/unit/com/cannontech/amr/errors/DeviceErrorTranslatorDaoImplTest.java
@@ -15,34 +15,24 @@ public class DeviceErrorTranslatorDaoImplTest {
 
     @Before
     public void setUp() throws Exception {
-        try {
-            translator = new DeviceErrorTranslatorDaoImpl();
-            ClassLoader classLoader = getClass().getClassLoader();
-            String path = "com/cannontech/amr/errors/dao/impl/error-code.xml";
-            InputStream resourceAsStream = classLoader.getResourceAsStream(path);
-            translator.setErrorDefinitions(resourceAsStream);
-            translator.initialize();
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            throw exception;
-        }
+        translator = new DeviceErrorTranslatorDaoImpl();
+        ClassLoader classLoader = getClass().getClassLoader();
+        String path = "com/cannontech/amr/errors/dao/impl/error-code.xml";
+        InputStream resourceAsStream = classLoader.getResourceAsStream(path);
+        translator.setErrorDefinitions(resourceAsStream);
+        translator.initialize();
     }
 
     @Test
     public void testTraslateErrorCode() {
-        try {
-            DeviceErrorDescription description = translator.translateErrorCode(15);
-            assertNotNull("returned null data", description);
-            assertNotNull("returned null description", description.getDescription());
-            assertNotNull("returned null trouble", description.getTroubleshooting());
+        DeviceErrorDescription description = translator.translateErrorCode(15);
+        assertNotNull("returned null data", description);
+        assertNotNull("returned null description", description.getDescription());
+        assertNotNull("returned null trouble", description.getTroubleshooting());
 
-            description = translator.translateErrorCode(23475845); // no way this is a real error!
-            assertNotNull("returned null data", description);
-            assertNotNull("returned null description", description.getDescription());
-            assertNotNull("returned null trouble", description.getTroubleshooting());
-        } catch (RuntimeException exception) {
-            exception.printStackTrace();
-            throw exception;
-        }
+        description = translator.translateErrorCode(23475845); // no way this is a real error!
+        assertNotNull("returned null data", description);
+        assertNotNull("returned null description", description.getDescription());
+        assertNotNull("returned null trouble", description.getTroubleshooting());
     }
 }
