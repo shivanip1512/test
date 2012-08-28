fbf48a4953538f592191d192ae76fc71aa9a2a37
Johnston, Samuel T <SamuelTJohnston@Eaton.com>
2012-08-28 14:42:29 +0000

    YUK-11323 Point Importer does not accept "KW" as a valid unit of measure.
    Unit of Measure "KW" will no longer cause an exception to be thrown.

--- yukon-client/common/src/com/cannontech/common/bulk/model/ImportUnitOfMeasure.java
+++ yukon-client/common/src/com/cannontech/common/bulk/model/ImportUnitOfMeasure.java
@@ -5,7 +5,7 @@ import com.cannontech.database.data.point.UnitOfMeasure;
 public class ImportUnitOfMeasure {
     public static int valueOf(String string) {
         int id = UnitOfMeasure.valueOf(string).getId();
-        if(id < 1) throw new IllegalArgumentException();
+        if(id < 0) throw new IllegalArgumentException();
         return id;
     }
 }
