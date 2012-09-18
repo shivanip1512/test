0df57dd5f65cbe311ca502dcb1d66d03fd40a01c
DeZeeuw, Garrett <>
2012-09-18 21:12:20 +0000

    CTIDatabase signature - 5.4.6

---
+++ yukon-database/DBUpdates/oracle/5.4_6.sql
@@ -0,0 +1,11 @@
+/******************************************/ 
+/****     Oracle DBupdates             ****/ 
+/******************************************/ 
+
+/* No Updates */
+
+/**************************************************************/ 
+/* VERSION INFO                                               */ 
+/*   Automatically gets inserted from build script            */ 
+/**************************************************************/ 
+INSERT INTO CTIDatabase VALUES ('5.4', 'Garrett D', '18-SEP-2012', 'Latest Update', 6 );
---
+++ yukon-database/DBUpdates/sqlserver/5.4_6.sql
@@ -0,0 +1,11 @@
+/******************************************/ 
+/**** SQL Server DBupdates             ****/ 
+/******************************************/ 
+
+/* No Updates */
+
+/**************************************************************/ 
+/* VERSION INFO                                               */ 
+/*   Automatically gets inserted from build script            */ 
+/**************************************************************/ 
+INSERT INTO CTIDatabase VALUES ('5.4', 'Garrett D', '18-SEP-2012', 'Latest Update', 6 );
