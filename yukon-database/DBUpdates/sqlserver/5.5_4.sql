a39039deaabc7ba65f876c7a558087d6a4d291f3
DeZeeuw, Garrett <>
2012-10-04 21:10:29 +0000

    YUK-11466 Populate CCStrategyTargetSettings table with Voltage Violation values
    Inserted values into CCStrategyTargetSettings for the following values of SettingType:
    BANDWIDTH
    COST
    EMERGENCY_COST
    
    Both Low Voltage Violation and High Voltage Violation levels are inserted.

--- yukon-database/DBUpdates/sqlserver/5.5_4.sql
+++ yukon-database/DBUpdates/sqlserver/5.5_4.sql
@@ -13,6 +13,38 @@ ALTER TABLE Regulator
 ALTER COLUMN vTapChange FLOAT NOT NULL;
 /* End YUK-11429 */
 
+/* Start YUK-11466 */
+INSERT INTO CCStrategyTargetSettings (StrategyId, SettingName, SettingValue, SettingType)
+SELECT StrategyId, 'Low Voltage Violation', '3.0', 'BANDWIDTH'
+FROM CapControlStrategy 
+WHERE ControlUnits = 'INTEGRATED_VOLT_VAR';
+
+INSERT INTO CCStrategyTargetSettings (StrategyId, SettingName, SettingValue, SettingType)
+SELECT StrategyId, 'High Voltage Violation', '1.0', 'BANDWIDTH'
+FROM CapControlStrategy 
+WHERE ControlUnits = 'INTEGRATED_VOLT_VAR';
+
+INSERT INTO CCStrategyTargetSettings (StrategyId, SettingName, SettingValue, SettingType)
+SELECT StrategyId, 'Low Voltage Violation', '-10.0', 'COST'
+FROM CapControlStrategy 
+WHERE ControlUnits = 'INTEGRATED_VOLT_VAR';
+
+INSERT INTO CCStrategyTargetSettings (StrategyId, SettingName, SettingValue, SettingType)
+SELECT StrategyId, 'High Voltage Violation', '70.0', 'COST'
+FROM CapControlStrategy 
+WHERE ControlUnits = 'INTEGRATED_VOLT_VAR';
+
+INSERT INTO CCStrategyTargetSettings (StrategyId, SettingName, SettingValue, SettingType)
+SELECT StrategyId, 'Low Voltage Violation', '-150.0', 'EMERGENCY_COST'
+FROM CapControlStrategy 
+WHERE ControlUnits = 'INTEGRATED_VOLT_VAR';
+
+INSERT INTO CCStrategyTargetSettings (StrategyId, SettingName, SettingValue, SettingType)
+SELECT StrategyId, 'High Voltage Violation', '300.0', 'EMERGENCY_COST'
+FROM CapControlStrategy 
+WHERE ControlUnits = 'INTEGRATED_VOLT_VAR';
+/* End YUK-11466 */
+
 /**************************************************************/ 
 /* VERSION INFO                                               */ 
 /*   Automatically gets inserted from build script            */ 
