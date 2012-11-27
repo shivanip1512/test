6f61f46711a57609f876e59b236ca7e173ea7d36
DeZeeuw, Garrett <>
2012-11-27 23:34:32 +0000

    YUK-11288 Add tracking of db insert/update run date
    Now uses DEFAULT values for the InstallDate column.  
    In Oracle the default is SYSDATE.
    In SQL Server the default is GETDATE().
    Altered the syntax of the insert statement for CTIDatabase. Column names are now explicitly supplied. InstallDate is not included so that the default value will be used.

--- yukon-database/DBUpdates/oracle/5.6_0.sql
+++ yukon-database/DBUpdates/oracle/5.6_0.sql
@@ -9,11 +9,12 @@ ALTER TABLE CTIDatabase
 DROP COLUMN CTIEmployeeName;
 
 ALTER TABLE CTIDatabase
-ADD InstallDate DATE;
+ADD InstallDate DATE DEFAULT SYSDATE;
 /* End YUK-11288 */
 
 /**************************************************************/
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
 /**************************************************************/
-/*INSERT INTO CTIDatabase VALUES ('5.6', '05-DEC-2012', 'Latest Update', 0, GETDATE());*/ 
+/*INSERT INTO CTIDatabase (Version, BuildDate, Notes, Build)
+VALUES ('5.6', '05-DEC-2012', 'Latest Update', 0);*/ 
--- yukon-database/DBUpdates/sqlserver/5.6_0.sql
+++ yukon-database/DBUpdates/sqlserver/5.6_0.sql
@@ -11,7 +11,7 @@ DROP COLUMN CTIEmployeeName;
 GO
 
 ALTER TABLE CTIDatabase
-ADD InstallDate DATETIME;
+ADD InstallDate DATETIME DEFAULT GETDATE();
 GO
 /* End YUK-11288 */
 
@@ -19,4 +19,5 @@ GO
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
 /**************************************************************/
-/*INSERT INTO CTIDatabase VALUES ('5.6', '05-DEC-2012', 'Latest Update', 0, GETDATE());*/
+/*INSERT INTO CTIDatabase (Version, BuildDate, Notes, Build) 
+VALUES ('5.6', '05-DEC-2012', 'Latest Update', 0);*/
