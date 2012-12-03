509eb056f8fff477f4184a7900bb94f4ba5d7bd3
Delegard, Alex <>
2012-12-03 17:00:24 +0000

    YUK-11628 Meter Events Report is not generated
    -removed old & slow methods used for validation & testing

--- yukon-client/yukon-web/src/com/cannontech/web/amr/meterEventsReport/MeterEventsReportController.java
+++ yukon-client/yukon-web/src/com/cannontech/web/amr/meterEventsReport/MeterEventsReportController.java
@@ -26,7 +26,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSourceResolvable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.util.CollectionUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -36,7 +35,6 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.cannontech.amr.meter.dao.MeterDao;
 import com.cannontech.amr.meter.model.Meter;
 import com.cannontech.amr.meter.service.impl.MeterEventLookupService;
 import com.cannontech.amr.meter.service.impl.MeterEventStatusTypeGroupings;
@@ -45,21 +43,14 @@ import com.cannontech.common.bulk.collection.device.DeviceCollection;
 import com.cannontech.common.bulk.collection.device.DeviceCollectionCreationException;
 import com.cannontech.common.bulk.collection.device.DeviceCollectionFactory;
 import com.cannontech.common.bulk.collection.device.DeviceGroupCollectionHelper;
-import com.cannontech.common.device.model.SimpleDevice;
 import com.cannontech.common.i18n.MessageSourceAccessor;
 import com.cannontech.common.i18n.ObjectFormattingService;
-import com.cannontech.common.pao.PaoIdentifier;
 import com.cannontech.common.pao.attribute.model.Attribute;
 import com.cannontech.common.pao.attribute.model.BuiltInAttribute;
 import com.cannontech.common.pao.attribute.service.AttributeService;
 import com.cannontech.common.search.SearchResult;
 import com.cannontech.common.validator.SimpleValidator;
 import com.cannontech.common.validator.YukonValidationUtils;
-import com.cannontech.core.dao.PointDao;
-import com.cannontech.core.dao.RawPointHistoryDao;
-import com.cannontech.core.dao.RawPointHistoryDao.Clusivity;
-import com.cannontech.core.dao.RawPointHistoryDao.Order;
-import com.cannontech.core.dynamic.PointValueQualityHolder;
 import com.cannontech.core.service.DateFormattingService;
 import com.cannontech.core.service.DateFormattingService.DateFormatEnum;
 import com.cannontech.core.service.DateFormattingService.DateOnlyMode;
@@ -78,7 +69,6 @@ import com.cannontech.web.input.DatePropertyEditorFactory.BlankMode;
 import com.cannontech.web.util.WebFileUtils;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMap.Builder;
-import com.google.common.collect.ListMultimap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
@@ -88,13 +78,10 @@ import com.google.common.collect.Sets;
 public class MeterEventsReportController {
 	
 	@Autowired private DatePropertyEditorFactory datePropertyEditorFactory;
-	@Autowired private RawPointHistoryDao rawPointHistoryDao;
 	@Autowired private PaoPointValueService paoPointValueService;
 	@Autowired private PointFormattingService pointFormattingService;
 	@Autowired private YukonUserContextMessageSourceResolver messageSourceResolver;
 	@Autowired private DeviceCollectionFactory deviceCollectionFactory;
-	@Autowired private MeterDao meterDao;
-	@Autowired private PointDao pointDao;
 	@Autowired private MeterEventLookupService meterEventLookupService;
 	@Autowired private DeviceGroupCollectionHelper deviceGroupCollectionHelper;
     @Autowired private DateFormattingService dateFormattingService;
@@ -218,16 +205,7 @@ public class MeterEventsReportController {
         headerRow[4] = messageSourceAccessor.getMessage("yukon.web.modules.amr.meterEventsReport.report.tableHeader.value.linkText");
         
         //data rows
-        List<MeterPointValue> events = getReportEvents(backingBean, userContext); // TEMPORARY - USE THE OLD METHOD FOR VERIFICATION (CSV ONLY)
-        if (backingBean.getSort() != null) {
-            if (backingBean.getDescending()) {
-                Collections.sort(events, Collections.reverseOrder(sorters.get(backingBean.getSort())));
-            } else {
-                Collections.sort(events, sorters.get(backingBean.getSort()));
-            }
-        } else {
-            Collections.sort(events, MeterPointValue.getDateMeterNameComparator());
-        }
+        List<MeterPointValue> events = getSortedMeterEvents(backingBean, userContext);
         
         List<String[]> dataRows = Lists.newArrayList();
         for(MeterPointValue event : events) {
@@ -288,22 +266,7 @@ public class MeterEventsReportController {
     private void setupReportFromFilter(MeterEventsReportFilterBackingBean backingBean,
                                      YukonUserContext userContext, ModelMap model) {
         
-        List<MeterPointValue> events = paoPointValueService.getMeterPointValues(Sets.newHashSet(backingBean.getDeviceCollection().getDeviceList()),
-                                                                                backingBean.getEnabledEventTypes(attributeService.getNameComparator(userContext)),
-                                                                                backingBean.getRange(),
-                                                                                backingBean.isOnlyLatestEvent() ? 1 : null,
-                                                                                backingBean.isIncludeDisabledPaos(),
-                                                                                backingBean.isOnlyAbnormalEvents() ? NON_ABNORMAL_VALUES : null,
-                                                                                userContext);
-        if (backingBean.getSort() != null) {
-            if (backingBean.getDescending()) {
-                Collections.sort(events, Collections.reverseOrder(sorters.get(backingBean.getSort())));
-            } else {
-                Collections.sort(events, sorters.get(backingBean.getSort()));
-            }
-        } else {
-            Collections.sort(events, Collections.reverseOrder(MeterPointValue.getDateMeterNameComparator()));
-        }
+        List<MeterPointValue> events = getSortedMeterEvents(backingBean, userContext);
 
         DeviceCollection collectionFromReportResults = getDeviceCollectionFromReportResults(events, userContext);
         model.addAttribute("collectionFromReportResults", collectionFromReportResults);
@@ -322,6 +285,27 @@ public class MeterEventsReportController {
         filterResult.setResultList(events);
         model.addAttribute("filterResult", filterResult);
         model.addAllAttributes(backingBean.getDeviceCollection().getCollectionParameters());
+    }
+
+    private List<MeterPointValue> getSortedMeterEvents(MeterEventsReportFilterBackingBean backingBean,
+                                            YukonUserContext userContext) {
+        List<MeterPointValue> events = paoPointValueService.getMeterPointValues(Sets.newHashSet(backingBean.getDeviceCollection().getDeviceList()),
+                                                                                backingBean.getEnabledEventTypes(attributeService.getNameComparator(userContext)),
+                                                                                backingBean.getRange(),
+                                                                                backingBean.isOnlyLatestEvent() ? 1 : null,
+                                                                                backingBean.isIncludeDisabledPaos(),
+                                                                                backingBean.isOnlyAbnormalEvents() ? NON_ABNORMAL_VALUES : null,
+                                                                                userContext);
+        if (backingBean.getSort() != null) {
+            if (backingBean.getDescending()) {
+                Collections.sort(events, Collections.reverseOrder(sorters.get(backingBean.getSort())));
+            } else {
+                Collections.sort(events, sorters.get(backingBean.getSort()));
+            }
+        } else {
+            Collections.sort(events, Collections.reverseOrder(MeterPointValue.getDateMeterNameComparator()));
+        }
+        return events;
     }
     
     private DeviceCollection getDeviceCollectionFromReportResults(List<MeterPointValue> events, YukonUserContext userContext) {
@@ -368,66 +352,6 @@ public class MeterEventsReportController {
         model.addAttribute("tamperEvents", getJSONArray(MeterEventStatusTypeGroupings.getTamper(), userContext));
         model.addAttribute("outageEvents", getJSONArray(MeterEventStatusTypeGroupings.getOutage(), userContext));
         model.addAttribute("meteringEvents", getJSONArray(MeterEventStatusTypeGroupings.getMetering(), userContext));
-    }
-    
-    private List<MeterPointValue> getReportEvents(MeterEventsReportFilterBackingBean backingBean, YukonUserContext userContext) {
-        
-        List<SimpleDevice> deviceList = backingBean.getDeviceCollection().getDeviceList();
-        List<Meter> meters = meterDao.getMetersForYukonPaos(deviceList);
-        
-        List<MeterPointValue> events = Lists.newArrayList();
-        if (CollectionUtils.isEmpty(deviceList)) {
-            return events;
-        }
-        
-        for (Entry<BuiltInAttribute, Boolean> type : backingBean.getMeterEventTypesMap().entrySet()) {
-            if (!type.getValue()) continue;
-
-            ListMultimap<PaoIdentifier, PointValueQualityHolder> attributeData;
-            if (backingBean.isOnlyLatestEvent()) {
-                attributeData = rawPointHistoryDao.getLimitedAttributeData(meters,
-                                                           type.getKey(),
-                                                           backingBean.getFromInstant().toDate(),
-                                                           backingBean.getToInstant().toDate(),
-                                                           1,
-                                                           !backingBean.isIncludeDisabledPaos(),
-                                                           Clusivity.INCLUSIVE_INCLUSIVE,
-                                                           Order.REVERSE);
-            } else {
-                attributeData = rawPointHistoryDao.getAttributeData(meters,
-                                                    type.getKey(),
-                                                    backingBean.getFromInstant().toDate(),
-                                                    backingBean.getToInstant().toDate(),
-                                                    !backingBean.isIncludeDisabledPaos(),
-                                                    Clusivity.INCLUSIVE_INCLUSIVE,
-                                                    Order.FORWARD);
-            }
-
-            for (Entry<PaoIdentifier, PointValueQualityHolder> entry : attributeData.entries()) {
-                PointValueQualityHolder pointValueHolder = entry.getValue();
-                String valueString = pointFormattingService.getValueString(pointValueHolder, Format.VALUE, userContext);
-                if (backingBean.isOnlyAbnormalEvents()) {
-                    // StateGroup: "Event Status" has states "cleared" and "active"
-                    // StateGroup: "Outage Status" has states "good", "questionable", and "bad"
-                    if ("cleared".equalsIgnoreCase(valueString) || "good".equalsIgnoreCase(valueString)) continue;
-                }
-                Meter meter = getMeterFromPaoIdentifier(meters, entry.getKey());
-                String pointName = pointDao.getPointName(pointValueHolder.getId());
-                MeterPointValue meterPointValue = new MeterPointValue(meter, null, type.getKey(), pointValueHolder, pointName, valueString);
-                
-                events.add(meterPointValue);
-            }
-        }
-        return events;
-    }
-    
-    private Meter getMeterFromPaoIdentifier(List<Meter> meters, PaoIdentifier paoIdentifier) {
-        for (Meter meter : meters) {
-            if (meter.getDeviceId() == paoIdentifier.getPaoId()) {
-                return meter;
-            }
-        }
-        return null;
     }
     
     private JSONArray getJSONArray(Set<BuiltInAttribute> originalSet, YukonUserContext context) {
