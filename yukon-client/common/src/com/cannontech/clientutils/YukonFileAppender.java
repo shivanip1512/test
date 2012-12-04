c35e3eac68b5bfd8f7a55ed1629cb99df2d53c34
Gerads, Jordan <>
2012-12-04 19:40:44 +0000

    YUK-9949 Increase log retention to 90 days - Formatting cleanup

--- yukon-client/common/src/com/cannontech/clientutils/YukonFileAppender.java
+++ yukon-client/common/src/com/cannontech/clientutils/YukonFileAppender.java
@@ -29,36 +29,36 @@ import com.cannontech.common.util.CtiUtilities;
  */
 
 public class YukonFileAppender extends AppenderSkeleton {
-    
+
     /**
      * maximum file size set at 1 gigabyte
-     */ 
-    private static long maxFileSize = 1073741824; 
+     */
+    private static long maxFileSize = 1073741824;
 
     /**
      * maximum file size as a string, defaults to 1 gigabyte
      * Accepts forms: "1G" or "1g", ".5M", etc.
-     */ 
-    private String maxFileSizeString; 
+     */
+    private String maxFileSizeString;
 
     private int maxFileOpenRetries = DatedFileAppender.MAX_FILE_OPEN_RETRIES;
     private int retryDelayInMs = DatedFileAppender.RETRY_DELAY_IN_MS;
     private final int logRetentionDays = DatedFileAppender.LOG_RETENTION_DAYS;
-    
+
     /**
      *  This appenders name
      */
-	private String name;
-    
+    private String name;
+
     /**
      * The conversion pattern sets the layout for the log4j logging
-     * See the log4j docs for specifics on formatting characters. The 
-     * default string uses: [date and time, thread that generated log event, 
-     * the log level left justified, the logger name which is usually the fq-class name, 
-     * log message and newline] 
+     * See the log4j docs for specifics on formatting characters. The
+     * default string uses: [date and time, thread that generated log event,
+     * the log level left justified, the logger name which is usually the fq-class name,
+     * log message and newline]
      */
     private String conversionPattern = "%d [%t] %-5p %c - %m%n";
-    
+
     /**
      *  The appender that YukonFileAppender delegates its work
      */
@@ -72,15 +72,15 @@ public class YukonFileAppender extends AppenderSkeleton {
      */
     public YukonFileAppender() {
     }
-    
+
     @Override
     public void activateOptions() {
         super.activateOptions();
 
-        //initialize dailyRollingFileAppender 
+        //initialize dailyRollingFileAppender
         //get the name of the application running this appender
 
-        String nameOfApp = CtiUtilities.getApplicationName(); 
+        String nameOfApp = CtiUtilities.getApplicationName();
         String directory = BootstrapUtils.getServerLogDir();
         String fileName = directory + nameOfApp + ".log";
 
@@ -98,7 +98,7 @@ public class YukonFileAppender extends AppenderSkeleton {
         //The layout for the log file:
         Layout layout = new PatternLayout(conversionPattern);
         dailyRollingFileAppender.setLayout(layout);
-        
+
         //Inherited from AppenderSkeleton. Calls once options are set
         dailyRollingFileAppender.activateOptions();
     }
@@ -124,23 +124,23 @@ public class YukonFileAppender extends AppenderSkeleton {
      */
     @Override
     public void close() {
-       dailyRollingFileAppender.close();
+        dailyRollingFileAppender.close();
     }
-	
+
     /**
      * @return name for this appender
      */
-	public final String getAppenderName() {
-		return this.name;
-	}
-    
-	/**
-	 * @param name is the name of this appender
-	 */
-	public final void setAppenderName(String name) {
-		this.name = name;
-	}
-    
+    public final String getAppenderName() {
+        return this.name;
+    }
+
+    /**
+     * @param name is the name of this appender
+     */
+    public final void setAppenderName(String name) {
+        this.name = name;
+    }
+
     /**
      * @param maxFileSizeString the maximum file size entered
      * as: 1G or .5M or 1000K or 100000000B or just 1000000000
@@ -165,21 +165,21 @@ public class YukonFileAppender extends AppenderSkeleton {
     public String getMaxFileSizeString(){
         return maxFileSizeString;
     }
-    
+
     /**
      * @return the maximum file size as a long
      */
     public static long getMaxFileSize(){
         return maxFileSize;
     }
-    
+
     /**
      * @return the maximum file size as a long
      */
     public static void setMaxFileSize(long maxFileSize){
         YukonFileAppender.maxFileSize = maxFileSize;
     }
-    
+
     /**
      * Takes a string that represents the maximum files size to be used for logging
      * and parses it into a number value and units. Examples: 1G, .5M  1000k etc.
@@ -191,18 +191,18 @@ public class YukonFileAppender extends AppenderSkeleton {
         long numberOfBytes = 0L;
         Double number = 1.0; //default is 1 gig
         Character units = 'G'; //dafault value is 1G
-        
+
         //the units as either G, M, K, or B (gigabyte, megabyte, etc.)
         Map<Character, Long> unitMap = new HashMap<Character, Long>();
         unitMap.put(Character.valueOf('G'), Long.valueOf(1073741824));
         unitMap.put(Character.valueOf('M'), Long.valueOf(1048576));
         unitMap.put(Character.valueOf('K'), Long.valueOf(1024));
         unitMap.put(Character.valueOf('B'), Long.valueOf(1));
-        
+
         Pattern p = Pattern.compile("^([\\d.]+)([A-Za-z]?)$");
         Matcher m = p.matcher(maxFileSizeString);
         if (m.matches()) {
-            
+
             //extract number from string and handle NaN
             try {
                 number = Double.parseDouble(m.group(1));
@@ -210,13 +210,15 @@ public class YukonFileAppender extends AppenderSkeleton {
                 errorHandler.error("Incorrect NaN value for file size, using default 1G file size ");
                 return numberOfBytes = unitMap.get('G');
             }
-         
+
             //handle case with no units given--default to bytes
-            if(m.group(2).length() > 0)
+            if(m.group(2).length() > 0) {
                 units = m.group(2).toUpperCase().charAt(0);
-            else
+            }
+            else {
                 units = 'B'; //no units so default to bytes
-                
+            }
+
             //do the conversion of string to bytes
             if (unitMap.containsKey(units)) {
                 // if units in map,
@@ -224,15 +226,15 @@ public class YukonFileAppender extends AppenderSkeleton {
                 numberOfBytes = Math.round(tempDouble);
             } else {
                 numberOfBytes = Math.round(number);
-            } 
+            }
         }
         //if failed to parse maxFileSizeString or file size is less than 1 byte
         //log an error and use the default 1G file size
         if(numberOfBytes <= 1) {
             errorHandler.error("Max log file string format was incorrect, use: .5G, 1000M, 30000K, or 25000000");
             numberOfBytes = unitMap.get('G');
-        }    
-        
+        }
+
         return numberOfBytes;
     }
 
