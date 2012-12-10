30ec40d66a463a117339884a9d825e34027c4710
Spar, Thain <>
2012-12-10 22:08:43 +0000

    YUK-11685 Unable to remove Schedule Assignments
    formatting change

--- yukon-client/common/src/com/cannontech/core/dao/PaoScheduleDao.java
+++ yukon-client/common/src/com/cannontech/core/dao/PaoScheduleDao.java
@@ -7,14 +7,22 @@ import com.cannontech.database.db.pao.PaoScheduleAssignment;
 
 public interface PaoScheduleDao {
     public PaoScheduleAssignment getScheduleAssignmentByEventId(Integer eventId);
+
     public List<PaoScheduleAssignment> getAllScheduleAssignments();
+
     public List<PAOSchedule> getAllPaoScheduleNames();
 
-	public boolean assignCommand  (List<PaoScheduleAssignment> param);
-	public boolean assignCommand  (PaoScheduleAssignment param);
+    public boolean assignCommand(List<PaoScheduleAssignment> param);
+
+    public boolean assignCommand(PaoScheduleAssignment param);
+
     public boolean unassignCommandByEventId(int eventId);
+
     public boolean updateAssignment(PaoScheduleAssignment assignment);
+
     public boolean delete(int scheduleId);
+
     public boolean deletePaoScheduleAssignmentsByScheduleId(int scheduleId);
+
     public int add(String name, boolean disabled);
 }
