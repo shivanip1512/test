f5677b8ab87e6f0219efa3bcb8dfcb7216dfb90e
Kruse, Matt <>
2013-01-18 21:56:14 +0000

    YUK-10645, EIM Web Services - ability to create a new thermostat schedule and edit an existing schedule. (XML and XSDs)
    Review Changes:
    - Cleaned up the enum values for ThermostatScheduleMode, SchedulableThermostatType, and TimeOfWeek to be more readable.
    - Updated YukonXPathTemplate evaluateAsEnum to accept more human readable formats.  Also included unit test for the new changes.
    - Added all of the DeleteThermostatSchedule, ThermostatSchedule, and UpdateThermostatSchedul xsds to the AllConsumer.xsd and moved them to the consumer directory.

--- yukon-client/common/src/com/cannontech/common/util/xml/YukonXPathTemplate.java
+++ yukon-client/common/src/com/cannontech/common/util/xml/YukonXPathTemplate.java
@@ -1,12 +1,53 @@
 package com.cannontech.common.util.xml;
 
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.joda.time.LocalTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.springframework.xml.xpath.XPathException;
+
 import com.cannontech.common.temperature.CelsiusTemperature;
 import com.cannontech.common.temperature.FahrenheitTemperature;
 import com.cannontech.common.temperature.Temperature;
 import com.cannontech.common.temperature.TemperatureUnit;
+import com.cannontech.common.util.DatabaseRepresentationSource;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
 
 public class YukonXPathTemplate extends SimpleXPathTemplate {
 
+    
+    private static final DateTimeFormatter PERIOD_START_TIME_FORMATTER = DateTimeFormat.forPattern("HH:mm");
+    
+    private static LoadingCache<Class<Enum<?>>, Map<String, Enum<?>>> enumValueLookup = 
+            CacheBuilder.newBuilder().concurrencyLevel(10).build(new CacheLoader<Class<Enum<?>>, Map<String, Enum<?>>>() {
+                @Override
+                public Map<String, Enum<?>> load(Class<Enum<?>> key) throws Exception {
+                    Enum<?>[] enumConstants = key.getEnumConstants();
+                    Builder<String, Enum<?>> builder = ImmutableMap.builder();
+                    for (Enum<?> enum1 : enumConstants) {
+                        DatabaseRepresentationSource drs = (DatabaseRepresentationSource) enum1;
+                        Object databaseRepresentation = drs.getDatabaseRepresentation();
+                        String stringRepresentation;
+                        if (databaseRepresentation instanceof Number) {
+                            long longValue = ((Number) databaseRepresentation).longValue();
+                            stringRepresentation = Long.toString(longValue);
+                        } else if (databaseRepresentation instanceof String){
+                            stringRepresentation = databaseRepresentation.toString();
+                        } else {
+                            throw new UnsupportedOperationException("DatabaseRepresentationSource for " + key + " returned a non-Number, non-String for " + enum1);
+                        }
+                        builder.put(stringRepresentation, enum1);
+                    }
+                    return builder.build();
+                }
+        });
+    
     public Temperature evaluateAsTemperature(String expression) {
         Temperature temperature = null;
         
@@ -22,5 +63,55 @@ public class YukonXPathTemplate extends SimpleXPathTemplate {
             temperature = new FahrenheitTemperature(temperatureValue);
         }
         return temperature;
+    }
+    
+    /**
+     * This method will attempt to return the Enum that may be stored in an xpath expression.  This method also handles
+     * upper casing the text in the XPath expression and also putting underscores in for spaces.  This way a user can put in a more 
+     * readable 'Weekday Weekend' for the enum 'WEEKDAY_WEEKEND'.
+     * 
+     * @throws XPathException
+     */
+    public <E extends Enum<E>> E evaluateAsEnum(String expression, Class<E> enumClass) throws XPathException {
+        
+        // Getting the string value from the expression and also converting it to an Enum format.
+        String stringValue = evaluateAsString(expression);
+        if (StringUtils.isBlank(stringValue)) {
+            return null;
+        }
+        stringValue = stringValue.toUpperCase().replaceAll(" ", "_");
+
+        // Converting the supplied string value to an Enum.
+        E result;
+        if (DatabaseRepresentationSource.class.isAssignableFrom(enumClass)) {
+            @SuppressWarnings("unchecked") // This cast is needed since the LoadingCache class uses k and not ? extends k.  When they fix that we can remove this suppress warning.
+            Map<String, Enum<?>> map = enumValueLookup.getUnchecked((Class<Enum<?>>) enumClass);
+            Enum<?> enum1 = map.get(stringValue);
+            result = enumClass.cast(enum1);
+        } else {
+            try {
+                result = Enum.valueOf(enumClass, stringValue);
+            } catch (IllegalArgumentException e) {
+                throw new XPathException(stringValue + " is not a legal representation of " + enumClass);
+            }
+        }
+        
+        return result;
+    }
+
+    /**
+     * This method takes the expression supplied and tries to return a LocalTime using the format hh:mm.
+     */
+    public LocalTime evaluateAsLocalTime(String expression) {
+        String stringValue = evaluateAsString(expression);
+        if (StringUtils.isBlank(stringValue)) {
+            return null;
+        }
+
+        try {
+            return PERIOD_START_TIME_FORMATTER.parseLocalTime(stringValue);
+        } catch (IllegalArgumentException e) {
+            throw new XPathException(stringValue + " is not a legal representation. [hh:mm]");
+        }
     }
 }
--- yukon-client/common/test/unit/com/cannontech/common/util/xml/YukonXPathTemplateTest.java
+++ yukon-client/common/test/unit/com/cannontech/common/util/xml/YukonXPathTemplateTest.java
@@ -3,10 +3,13 @@ package com.cannontech.common.util.xml;
 import junit.framework.Assert;
 
 import org.jdom.Element;
+import org.joda.time.LocalTime;
 import org.junit.Test;
+import org.springframework.xml.xpath.XPathException;
 
 import com.cannontech.common.temperature.Temperature;
 import com.cannontech.common.temperature.TemperatureUnit;
+import com.cannontech.stars.dr.thermostat.model.ThermostatScheduleMode;
 
 public class YukonXPathTemplateTest {
     
@@ -72,5 +75,180 @@ public class YukonXPathTemplateTest {
         Assert.assertEquals(TemperatureUnit.CELSIUS, temperatureValue.toCelsius().getUnit());
         Assert.assertEquals(26.0, temperatureValue.toCelsius().getValue());
     }
+
+    /**
+     * <testElement>
+     *      <enumElement thermostatScheduleMode="Weekday Weekend" />
+     * </testElement>
+     */
+    @Test
+    public void testEvaluateAsEnum_attribute() {
+        Element testElement = createEnumElement("Weekday Weekend", null);
+
+        YukonXPathTemplate yukonTemplate = YukonXml.getXPathTemplateForElement(testElement);
+        ThermostatScheduleMode thermostatScheduleMode = yukonTemplate.evaluateAsEnum("/testElement/enumElement/@thermostatScheduleMode", ThermostatScheduleMode.class);
+        
+        Assert.assertNotNull(thermostatScheduleMode);
+        Assert.assertEquals(ThermostatScheduleMode.WEEKDAY_WEEKEND, thermostatScheduleMode);
+    }
+
+    /**
+     * <testElement>
+     *      <enumElement>WeekDay_WeekEnd<enumElement>
+     * </testElement>
+     */
+    @Test
+    public void testEvaluateAsEnum_element() {
+        Element testElement = createEnumElement(null, "WeekDay_WeekEnd");
+
+        YukonXPathTemplate yukonTemplate = YukonXml.getXPathTemplateForElement(testElement);
+        ThermostatScheduleMode thermostatScheduleMode = yukonTemplate.evaluateAsEnum("/testElement/enumElement", ThermostatScheduleMode.class);
+        
+        Assert.assertNotNull(thermostatScheduleMode);
+        Assert.assertEquals(ThermostatScheduleMode.WEEKDAY_WEEKEND, thermostatScheduleMode);
+    }
+
+    /**
+     * <testElement>
+     *      <enumElement />
+     * </testElement>
+     */
+    @Test
+    public void testEvaluateAsEnum_nullElement() {
+        Element testElement = createEnumElement(null, null);
+
+        YukonXPathTemplate yukonTemplate = YukonXml.getXPathTemplateForElement(testElement);
+        ThermostatScheduleMode thermostatScheduleMode = yukonTemplate.evaluateAsEnum("/testElement/enumElement", ThermostatScheduleMode.class);
+        
+        Assert.assertNull(thermostatScheduleMode);
+    }
+
+    /**
+     * <testElement>
+     *      <enumElement>WeekDay_WeekEndddd<enumElement>
+     * </testElement>
+     */
+    @Test(expected=XPathException.class)
+    public void testEvaluateAsEnum_badEnumElement() {
+        Element testElement = createEnumElement(null, "WeekDay_WeekEndddd");
+
+        YukonXPathTemplate yukonTemplate = YukonXml.getXPathTemplateForElement(testElement);
+        ThermostatScheduleMode thermostatScheduleMode = yukonTemplate.evaluateAsEnum("/testElement/enumElement", ThermostatScheduleMode.class);
+        
+        Assert.assertNotNull(thermostatScheduleMode);
+        Assert.assertEquals(ThermostatScheduleMode.WEEKDAY_WEEKEND, thermostatScheduleMode);
+    }
+
+    /**
+     * <testElement>
+     *      <enumElement>19:24<enumElement>
+     * </testElement>
+     */
+    @Test
+    public void testEvaluateAsLocalTime() {
+        Element testElement = createLocalTimeElement("19:24");
+
+        YukonXPathTemplate yukonTemplate = YukonXml.getXPathTemplateForElement(testElement);
+        LocalTime localTime= yukonTemplate.evaluateAsLocalTime("/testElement/localTimeElement");
+        
+        Assert.assertNotNull(localTime);
+        Assert.assertEquals(new LocalTime(19, 24), localTime);
+    }
     
+    /**
+     * <testElement>
+     *      <enumElement>5:42<enumElement>
+     * </testElement>
+     */
+    @Test
+    public void testEvaluateAsLocalTime_singleHourDigit() {
+        Element testElement = createLocalTimeElement("5:42");
+
+        YukonXPathTemplate yukonTemplate = YukonXml.getXPathTemplateForElement(testElement);
+        LocalTime localTime= yukonTemplate.evaluateAsLocalTime("/testElement/localTimeElement");
+        
+        Assert.assertNotNull(localTime);
+        Assert.assertEquals(new LocalTime(5, 42), localTime);
+    }
+
+    /**
+     * <testElement>
+     *      <enumElement><enumElement>
+     * </testElement>
+     */
+    @Test
+    public void testEvaluateAsLocalTime_nullElement() {
+        Element testElement = createLocalTimeElement(null);
+
+        YukonXPathTemplate yukonTemplate = YukonXml.getXPathTemplateForElement(testElement);
+        LocalTime localTime= yukonTemplate.evaluateAsLocalTime("/testElement/localTimeElement");
+        
+        Assert.assertNull(localTime);
+    }
+
+    /**
+     * <testElement>
+     *      <enumElement>19:4<enumElement>
+     * </testElement>
+     */
+    @Test
+    public void testEvaluateAsLocalTime_singleMinutesDigit() {
+        Element testElement = createLocalTimeElement("19:4");
+
+        YukonXPathTemplate yukonTemplate = YukonXml.getXPathTemplateForElement(testElement);
+        LocalTime localTime= yukonTemplate.evaluateAsLocalTime("/testElement/localTimeElement");
+        
+        Assert.assertNotNull(localTime);
+        Assert.assertEquals(new LocalTime(19, 4), localTime);
+    }
+
+    /**
+     * <testElement>
+     *      <enumElement>42:4<enumElement>
+     * </testElement>
+     */
+    @Test(expected=XPathException.class)
+    public void testEvaluateAsLocalTime_invalidDate() {
+        Element testElement = createLocalTimeElement("42:4");
+
+        YukonXPathTemplate yukonTemplate = YukonXml.getXPathTemplateForElement(testElement);
+        LocalTime localTime= yukonTemplate.evaluateAsLocalTime("/testElement/localTimeElement");
+        
+        Assert.assertNotNull(localTime);
+        Assert.assertEquals(new LocalTime(), localTime);
+    }
+
+    /**
+     *  <testElement>
+     *      <enumElement thermostatScheduleMode="${attributeValue}">${elementValue}</enumElement>
+     * </testElement>
+     */
+    private Element createEnumElement(String attributeValue, String elementValue) {
+        Element testElement = new Element("testElement");
+
+        Element temperatureElement = new Element("enumElement");
+        if (attributeValue != null) {
+            temperatureElement.setAttribute("thermostatScheduleMode", attributeValue);
+        }
+
+        temperatureElement.setText(elementValue);
+        testElement.addContent(temperatureElement);
+        
+        return testElement;
+    }
+
+    /**
+     *  <testElement>
+     *      <localTimeElement>${elementValue}</localTimeElement>
+     *  </testElement>
+     */
+    private Element createLocalTimeElement(String elementValue) {
+        Element testElement = new Element("testElement");
+
+        Element temperatureElement = new Element("localTimeElement");
+        temperatureElement.setText(elementValue);
+        testElement.addContent(temperatureElement);
+        
+        return testElement;
+    }
 }
