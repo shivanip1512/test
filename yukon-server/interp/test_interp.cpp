673802d281fda5a802bd9defdb514d63003285e9
Fisher, Matthew S <MatthewSFisher@Eaton.com>
2013-02-14 19:35:39 +0000

    YUK-11377 MACS simple schedules do not parse quotes correctly Added multiline unit test cases to demonstrate broken script newline handling

--- yukon-server/interp/test_interp.cpp
+++ yukon-server/interp/test_interp.cpp
@@ -4,7 +4,7 @@
 #include "interp.h"
 
 struct test_CtiInterpreter : CtiInterpreter
-{  
+{
     test_CtiInterpreter(std::set<std::string> commandsToEscape) :
         CtiInterpreter(commandsToEscape)
     {
@@ -34,7 +34,7 @@ BOOST_AUTO_TEST_CASE( test_isEscapeCommand )
     BOOST_REQUIRE(!interp.isEscapeCommand("Kate is a thief"));        // case-sensitive
     BOOST_REQUIRE( interp.isEscapeCommand("hurley is a millionaire"));
     BOOST_REQUIRE(!interp.isEscapeCommand("Hurley is a millionaire"));// case-sensitive
-    BOOST_REQUIRE( interp.isEscapeCommand("sawyer is a con artist")); 
+    BOOST_REQUIRE( interp.isEscapeCommand("sawyer is a con artist"));
     BOOST_REQUIRE(!interp.isEscapeCommand("Sawyer is a con artist")); // case-sensitive
     BOOST_REQUIRE( interp.isEscapeCommand("sayid is a soldier"));
     BOOST_REQUIRE(!interp.isEscapeCommand("Sayid is a soldier"));     // case-sensitive
@@ -49,19 +49,27 @@ BOOST_AUTO_TEST_CASE( test_escapeQuotes )
 {
     test_CtiInterpreter interp(commandsToEscape);
 
-    std::vector<std::string> expected = boost::assign::list_of
+    const std::vector<std::string> expected = boost::assign::list_of
         ("jack said, \\\"Last week most of us were strangers.\\\"") // jack is okay, escape it
         ("locke said something about \\\\\"The Island.\\\\\"")      // locke is okay, even with already-escaped quotes, escape it
         ("Sawyer called her \"Freckles.\"")                         // wrong case, don't escape
         ("he caught kate reading his letter to \"Sawyer.\"")        // kate isn't at the start of the string, and sawyer is the wrong case, don't escape
-        ("ben said, \"You have thin doors.\"");                     // ben isn't a keyword, don't escape
+        ("ben said, \"You have thin doors.\"")                      // ben isn't a keyword, don't escape
+        ("ben wondered about multiline commands.\n"
+         "locke said, \\\"My command is to escape.\\\"")
+        ("sawyer ate a burrito and exclaimed, \\\"Ole!\\\"\r\n"
+         "Sayid ate macaroons and mumbled, \"Coconut.\"");
 
-    std::vector<std::string> output = boost::assign::list_of
+    const std::vector<std::string> output = boost::assign::list_of
         (interp.escapeQuotationMarks("jack said, \"Last week most of us were strangers.\""))
         (interp.escapeQuotationMarks("locke said something about \\\"The Island.\\\""))
         (interp.escapeQuotationMarks("Sawyer called her \"Freckles.\""))
         (interp.escapeQuotationMarks("he caught kate reading his letter to \"Sawyer.\""))
-        (interp.escapeQuotationMarks("ben said, \"You have thin doors.\""));
+        (interp.escapeQuotationMarks("ben said, \"You have thin doors.\""))
+        (interp.escapeQuotationMarks("ben wondered about multiline commands.\n"
+                                     "locke said, \"My command is to escape.\""))
+        (interp.escapeQuotationMarks("sawyer ate a burrito and exclaimed, \"Ole!\"\r\n"
+                                     "Sayid ate macaroons and mumbled, \"Coconut.\""));
 
     BOOST_CHECK_EQUAL_COLLECTIONS(expected.begin(), expected.end(), output.begin(), output.end());
 }
