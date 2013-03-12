85a479de9905ef0fed32e48625b038a21e249127
Gerads, Jordan <>
2013-03-12 16:40:18 +0000

    YUK-9956 Add the LCD Cycle Speed and Disconnect Status Enable/Disable to the MCT-420 device configurations - Client side config checkin

--- yukon-client/common/src/com/cannontech/common/device/config/model/MCT420Configuration.java
+++ yukon-client/common/src/com/cannontech/common/device/config/model/MCT420Configuration.java
@@ -8,6 +8,9 @@ import com.google.common.collect.Lists;
 
 public class MCT420Configuration extends ConfigurationBase {
     private List<Integer> displayItems;
+    private int lcdCycleTime = 8;
+    private boolean disconnectDisplayDisabled = false;
+    private int displayDigits = 5;
     private final int NUM_OF_DISPLAY_ITEMS = 26;
     
     public MCT420Configuration(){
@@ -21,7 +24,31 @@ public class MCT420Configuration extends ConfigurationBase {
     public void setDisplayItems(List<Integer> displayItems) {
         this.displayItems = displayItems;
     }
-
+    
+    public int getLcdCycleTime() {
+        return lcdCycleTime;
+    }
+    
+    public void setLcdCycleTime(int lcdCycleTime) {
+        this.lcdCycleTime = lcdCycleTime;
+    }
+    
+    public boolean isDisconnectDisplayDisabled() {
+        return disconnectDisplayDisabled;
+    }
+    
+    public void setDisconnectDisplayDisabled(boolean disconnectDisplayDisabled) {
+        this.disconnectDisplayDisabled = disconnectDisplayDisabled;
+    }
+    
+    public void setDisplayDigits(int displayDigits) {
+        this.displayDigits = displayDigits;
+    }
+    
+    public int getDisplayDigits() {
+        return displayDigits;
+    }
+    
     @Override
     public ConfigurationType getType() {
         return ConfigurationType.MCT420;
