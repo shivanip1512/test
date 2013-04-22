a10774d1f3eb175582a5b01ddca59a3e6064a2c5
DeZeeuw, Garrett <>
2013-04-22 20:36:14 +0000

    YUK-12116 MCT 420 Configurations created before 5.6.2 are missing fields in the database
    Inserted missing DeviceConfigurationItem records for MCT420 device configurations.  The items added and their default values are:
    
    'Display Digits', 5
    'LCD Cycle Time', 8
    'Disconnect Display Disabled', 'false'
    
    Note: No creation script changes for this issue.

--- yukon-database/DBUpdates/oracle/5.6_4.sql
+++ yukon-database/DBUpdates/oracle/5.6_4.sql
@@ -23,6 +23,103 @@ ALTER TABLE AcctThermostatSchedule
 MODIFY Archived CHAR(1) NOT NULL;
 /* End YUK-12012 */
 
+/* Start YUK-12116 */
+/* @start-block */
+DECLARE 
+    v_configId NUMBER;
+    v_itemId NUMBER;
+
+    /* Find all of the 420 configurations without a Display Digits field and add it with the default value of 5. */
+BEGIN 
+
+    SELECT MIN(DC.DeviceConfigurationId) INTO v_configId
+    FROM DeviceConfiguration DC
+    WHERE DC.Type = 'MCT420'
+    MINUS
+    SELECT DC.DeviceConfigurationId
+    FROM DeviceConfiguration DC, DeviceConfigurationItem DCI
+    WHERE DC.Type = 'MCT420' 
+      AND DCI.FieldName = 'Display Digits'
+      AND DCI.DeviceConfigurationId = DC.DeviceConfigurationId;
+
+    WHILE(v_configId IS NOT NULL)
+    LOOP
+        SELECT NVL(MAX(DeviceConfigurationItemId) + 1,0) INTO v_itemId FROM DeviceConfigurationItem;
+
+        INSERT INTO DeviceConfigurationItem VALUES (v_itemId, v_configId, 'Display Digits', '5');
+        
+        SELECT MIN(DC.DeviceConfigurationId) INTO v_configId
+        FROM DeviceConfiguration DC
+        WHERE DC.Type = 'MCT420'
+        MINUS
+        SELECT DC.DeviceConfigurationId
+        FROM DeviceConfiguration DC, DeviceConfigurationItem DCI
+        WHERE DC.Type = 'MCT420' 
+          AND DCI.FieldName = 'Display Digits'
+          AND DCI.DeviceConfigurationId = DC.DeviceConfigurationId ;
+
+    END LOOP;
+
+    /* Find all of the 420 configurations without an LCD Cycle Time field and add it with the default value of 8. */
+    SELECT MIN(DC.DeviceConfigurationId) INTO v_configId
+    FROM DeviceConfiguration DC
+    WHERE DC.Type = 'MCT420'
+    MINUS
+    SELECT DC.DeviceConfigurationId
+    FROM DeviceConfiguration DC, DeviceConfigurationItem DCI 
+    WHERE DC.Type = 'MCT420' 
+      AND DCI.FieldName = 'LCD Cycle Time'
+      AND DCI.DeviceConfigurationId = DC.DeviceConfigurationId;
+
+    WHILE(v_configId IS NOT NULL)
+    LOOP
+        SELECT NVL(MAX(DeviceConfigurationItemId) + 1,0) INTO v_itemId FROM DeviceConfigurationItem;
+
+        INSERT INTO DeviceConfigurationItem VALUES (v_itemId, v_configId, 'LCD Cycle Time', '8');
+
+        SELECT MIN(DC.DeviceConfigurationId) INTO v_configId
+        FROM DeviceConfiguration DC
+        WHERE DC.Type = 'MCT420'
+        MINUS
+        SELECT DC.DeviceConfigurationId
+        FROM DeviceConfiguration DC, DeviceConfigurationItem DCI 
+        WHERE DC.Type = 'MCT420' 
+          AND DCI.FieldName = 'LCD Cycle Time'
+          AND DCI.DeviceConfigurationId = DC.DeviceConfigurationId;
+    END LOOP;
+
+    /* Find all of the 420 configurations without a Disconnect Display Disabled field and add it with the default value of false. */
+    SELECT MIN(DC.DeviceConfigurationId) INTO v_configId
+        FROM DeviceConfiguration DC
+        WHERE DC.Type = 'MCT420'
+        MINUS
+        SELECT DC.DeviceConfigurationId
+        FROM DeviceConfiguration DC, DeviceConfigurationItem DCI 
+        WHERE DC.Type = 'MCT420' 
+          AND DCI.FieldName = 'Disconnect Display Disabled'
+          AND DCI.DeviceConfigurationId = DC.DeviceConfigurationId;
+
+    WHILE(v_configId IS NOT NULL)
+    LOOP
+        SELECT NVL(MAX(DeviceConfigurationItemId) + 1,0) INTO v_itemId FROM DeviceConfigurationItem;
+
+        INSERT INTO DeviceConfigurationItem VALUES (v_itemId, v_configId, 'Disconnect Display Disabled', 'false');
+        
+        SELECT MIN(DC.DeviceConfigurationId) INTO v_configId
+        FROM DeviceConfiguration DC
+        WHERE DC.Type = 'MCT420'
+        MINUS
+        SELECT DC.DeviceConfigurationId
+        FROM DeviceConfiguration DC, DeviceConfigurationItem DCI 
+        WHERE DC.Type = 'MCT420' 
+          AND DCI.FieldName = 'Disconnect Display Disabled'
+          AND DCI.DeviceConfigurationId = DC.DeviceConfigurationId;
+    END LOOP;
+END;
+/
+/* @end-block */
+/* End YUK-12116 */
+
 /**************************************************************/
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
--- yukon-database/DBUpdates/sqlserver/5.6_4.sql
+++ yukon-database/DBUpdates/sqlserver/5.6_4.sql
@@ -24,6 +24,99 @@ ALTER TABLE AcctThermostatSchedule
 ALTER COLUMN Archived CHAR(1) NOT NULL;
 /* End YUK-11880 */
 
+/* Start YUK-12116 */
+/* @start-block */
+BEGIN 
+    DECLARE @configId INT;
+    DECLARE @itemId INT;
+
+    /* Find all of the 420 configurations without a Display Digits field and add it with the default value of 5. */
+    SET @configId = 
+       (SELECT MIN(DC.DeviceConfigurationId)
+        FROM DeviceConfiguration DC
+        WHERE DC.Type = 'MCT420'
+        EXCEPT
+        SELECT DC.DeviceConfigurationId
+        FROM DeviceConfiguration DC
+            JOIN DeviceConfigurationItem DCI ON DCI.DeviceConfigurationId = DC.DeviceConfigurationId 
+        WHERE DC.Type = 'MCT420' AND DCI.FieldName = 'Display Digits');
+
+    WHILE(@configId IS NOT NULL)
+    BEGIN
+        SELECT @itemId = MAX(DeviceConfigurationItemId) + 1 FROM DeviceConfigurationItem;
+
+        INSERT INTO DeviceConfigurationItem VALUES (@itemId, @configId, 'Display Digits', '5');
+        
+        SET @configId = 
+           (SELECT MIN(DC.DeviceConfigurationId)
+            FROM DeviceConfiguration DC
+            WHERE DC.Type = 'MCT420'
+            EXCEPT
+            SELECT DC.DeviceConfigurationId
+            FROM DeviceConfiguration DC
+                JOIN DeviceConfigurationItem DCI ON DCI.DeviceConfigurationId = DC.DeviceConfigurationId 
+            WHERE DC.Type = 'MCT420' AND DCI.FieldName = 'Display Digits');
+    END;
+
+    /* Find all of the 420 configurations without an LCD Cycle Time field and add it with the default value of 8. */
+    SET @configId = 
+       (SELECT MIN(DC.DeviceConfigurationId)
+        FROM DeviceConfiguration DC
+        WHERE DC.Type = 'MCT420'
+        EXCEPT
+        SELECT DC.DeviceConfigurationId
+        FROM DeviceConfiguration DC
+            JOIN DeviceConfigurationItem DCI ON DCI.DeviceConfigurationId = DC.DeviceConfigurationId 
+        WHERE DC.Type = 'MCT420' AND DCI.FieldName = 'LCD Cycle Time');
+
+    WHILE(@configId IS NOT NULL)
+    BEGIN
+        SELECT @itemId = MAX(DeviceConfigurationItemId) + 1 FROM DeviceConfigurationItem;
+
+        INSERT INTO DeviceConfigurationItem VALUES (@itemId, @configId, 'LCD Cycle Time', '8');
+
+        SET @configId = 
+           (SELECT MIN(DC.DeviceConfigurationId)
+            FROM DeviceConfiguration DC
+            WHERE DC.Type = 'MCT420'
+            EXCEPT
+            SELECT DC.DeviceConfigurationId
+            FROM DeviceConfiguration DC
+                JOIN DeviceConfigurationItem DCI ON DCI.DeviceConfigurationId = DC.DeviceConfigurationId 
+            WHERE DC.Type = 'MCT420' AND DCI.FieldName = 'LCD Cycle Time');
+    END;
+
+    /* Find all of the 420 configurations without a Disconnect Display Disabled field and add it with the default value of false. */
+    SET @configId = 
+       (SELECT MIN(DC.DeviceConfigurationId)
+        FROM DeviceConfiguration DC
+        WHERE DC.Type = 'MCT420'
+        EXCEPT
+        SELECT DC.DeviceConfigurationId
+        FROM DeviceConfiguration DC
+            JOIN DeviceConfigurationItem DCI ON DCI.DeviceConfigurationId = DC.DeviceConfigurationId 
+        WHERE DC.Type = 'MCT420' AND DCI.FieldName = 'Disconnect Display Disabled');
+
+    WHILE(@configId IS NOT NULL)
+    BEGIN
+        SELECT @itemId = MAX(DeviceConfigurationItemId) + 1 FROM DeviceConfigurationItem;
+
+        INSERT INTO DeviceConfigurationItem VALUES (@itemId, @configId, 'Disconnect Display Disabled', 'false');
+        
+        SET @configId = 
+           (SELECT MIN(DC.DeviceConfigurationId)
+            FROM DeviceConfiguration DC
+            WHERE DC.Type = 'MCT420'
+            EXCEPT
+            SELECT DC.DeviceConfigurationId
+            FROM DeviceConfiguration DC
+                JOIN DeviceConfigurationItem DCI ON DCI.DeviceConfigurationId = DC.DeviceConfigurationId 
+            WHERE DC.Type = 'MCT420' AND DCI.FieldName = 'Disconnect Display Disabled');
+    END;
+END;
+/* @end-block */
+/* End YUK-12116 */
+
 /**************************************************************/
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
