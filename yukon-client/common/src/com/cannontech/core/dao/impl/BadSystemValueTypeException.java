b537e3cd0400acd09ede9e144044cbe5330f1bee
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2013-04-25 19:10:02 +0000

    Added missing closing quote in message.

--- yukon-client/common/src/com/cannontech/core/dao/impl/BadSystemValueTypeException.java
+++ yukon-client/common/src/com/cannontech/core/dao/impl/BadSystemValueTypeException.java
@@ -4,6 +4,6 @@ import com.cannontech.core.dao.PersistedSystemValueKey;
 
 public class BadSystemValueTypeException extends RuntimeException {
     public BadSystemValueTypeException(PersistedSystemValueKey property, String value, Throwable cause) {
-        super("Unable to convert value of \"" + value + " for " + property, cause);
+        super("Unable to convert value of \"" + value + "\" for " + property, cause);
     }
 }
