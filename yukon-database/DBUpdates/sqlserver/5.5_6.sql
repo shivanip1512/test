3c57439c6c71911ddfa793f3915e14675710328d
DeZeeuw, Garrett <>
2013-05-08 20:52:06 +0000

    CTIDatabase signature - 5.5.6

---
+++ yukon-database/DBUpdates/sqlserver/5.5_6.sql
@@ -0,0 +1,11 @@
+/******************************************/ 
+/**** SQL Server DBupdates             ****/ 
+/******************************************/ 
+
+/* No Updates */
+
+/**************************************************************/ 
+/* VERSION INFO                                               */ 
+/*   Automatically gets inserted from build script            */ 
+/**************************************************************/ 
+INSERT INTO CTIDatabase VALUES ('5.5', 'Garrett D', '10-MAY-2013', 'Latest Update', 6 );
