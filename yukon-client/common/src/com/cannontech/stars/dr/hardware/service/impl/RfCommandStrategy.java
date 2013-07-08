a00d406a6d2d66182dfb2e00a8235cad2b360eb7
Woodward, Daniel <>
2013-07-08 19:11:17 +0000

    YUK-12174 Yukon Config for RF LCR 6600 and 6200 Model Number Changes impacting Enrollment and Other Features
    Fixed rfn command to rfn devices to use devices manufacture, instead of templates

--- yukon-client/common/src/com/cannontech/stars/dr/hardware/service/impl/RfCommandStrategy.java
+++ yukon-client/common/src/com/cannontech/stars/dr/hardware/service/impl/RfCommandStrategy.java
@@ -6,20 +6,18 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSourceResolvable;
 
+import com.cannontech.amr.rfn.dao.RfnDeviceDao;
 import com.cannontech.clientutils.LogHelper;
 import com.cannontech.clientutils.YukonLogManager;
 import com.cannontech.common.config.ConfigurationSource;
-import com.cannontech.common.constants.YukonListEntry;
 import com.cannontech.common.device.DeviceRequestType;
 import com.cannontech.common.device.commands.exception.CommandCompletionException;
 import com.cannontech.common.device.commands.impl.CommandRequestExecutionDefaults;
 import com.cannontech.common.inventory.HardwareType;
 import com.cannontech.common.model.YukonCancelTextMessage;
 import com.cannontech.common.model.YukonTextMessage;
-import com.cannontech.common.rfn.message.RfnIdentifier;
 import com.cannontech.common.rfn.message.RfnMessageClass;
-import com.cannontech.common.rfn.model.RfnManufacturerModel;
-import com.cannontech.core.dao.YukonListDao;
+import com.cannontech.common.rfn.model.RfnDevice;
 import com.cannontech.database.data.lite.LiteYukonUser;
 import com.cannontech.dr.rfn.message.broadcast.RfnExpressComBroadcastRequest;
 import com.cannontech.dr.rfn.message.unicast.RfnExpressComUnicastReplyType;
@@ -27,7 +25,6 @@ import com.cannontech.dr.rfn.message.unicast.RfnExpressComUnicastRequest;
 import com.cannontech.dr.rfn.service.RawExpressComCommandBuilder;
 import com.cannontech.dr.rfn.service.RfnExpressComMessageService;
 import com.cannontech.dr.rfn.service.RfnUnicastCallback;
-import com.cannontech.stars.database.data.lite.LiteLmHardwareBase;
 import com.cannontech.stars.dr.account.model.CustomerAccount;
 import com.cannontech.stars.dr.hardware.model.LmCommand;
 import com.cannontech.stars.dr.hardware.model.LmHardwareCommand;
@@ -50,18 +47,14 @@ public class RfCommandStrategy implements LmHardwareCommandStrategy {
     @Autowired private RfnExpressComMessageService rfnExpressComMessageService;
     @Autowired private RawExpressComCommandBuilder commandBuilder;
     @Autowired private ConfigurationSource configurationSource;
-    @Autowired private YukonListDao yukonListDao;
+    @Autowired private RfnDeviceDao rfnDeviceDao;
 
     @Override
     public void sendCommand(final LmHardwareCommand parameters) throws CommandCompletionException {
         
-        LiteLmHardwareBase device = parameters.getDevice();
-        YukonListEntry typeEntry = yukonListDao.getYukonListEntry(device.getLmHardwareTypeID());
-        HardwareType type = HardwareType.valueOf(typeEntry.getYukonDefID());
-        RfnManufacturerModel template = RfnManufacturerModel.getForType(type.getForHardwareType()).get(0);
-        RfnIdentifier rfnIdentifier = new RfnIdentifier(device.getManufacturerSerialNumber(), template.getManufacturer(), template.getModel());
+        RfnDevice rfnDevice = rfnDeviceDao.getDeviceForId(parameters.getDevice().getDeviceID());
         
-        RfnExpressComUnicastRequest request = new RfnExpressComUnicastRequest(rfnIdentifier);
+        RfnExpressComUnicastRequest request = new RfnExpressComUnicastRequest(rfnDevice.getRfnIdentifier());
         request.setPayload(commandBuilder.getCommandAsHexStringByteArray(parameters));
         
         Long expirationDuration = parameters.findParam(LmHardwareCommandParam.EXPIRATION_DURATION, Long.class);
