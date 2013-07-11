3e2f7d22212e5f65f9c0239e48b2c1bc46ef7c86
Solberg, Aaron <>
2013-07-11 22:22:10 +0000

    YUK-12327 Grapical glitch on Meter Events Report tooltips
    - fixed field helper popup positioning

--- yukon-client/yukon-web/WebContent/JavaScript/yukon/ui/fieldHelper.js
+++ yukon-client/yukon-web/WebContent/JavaScript/yukon/ui/fieldHelper.js
@@ -109,7 +109,7 @@ if(typeof(Yukon.FieldHelper) === "undefined"){
              }
              
              var target = jQuery(event.target);
-             var popupLeft = target.offset().left + target.width() + 4;
+             var popupLeft = target.offset().left + target.width() + 12;
              var left = popupLeft + 'px';
              var top = (target.offset().top -20) + 'px';
              
