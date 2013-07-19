a019104a3f26bafa1d7a7121b84d7bbaea7d6e40
Woodward, Daniel <>
2013-07-19 14:04:29 +0000

    YUK-11619 convert to Maven
    Unused code elimination

--- yukon-client/yukon-web/src/com/cannontech/web/support/development/database/service/DevObjectCreationInterface.java
+++
@@ -1,9 +0,0 @@
-package com.cannontech.web.support.development.database.service;
-
-import com.cannontech.web.support.development.DevDbSetupTask;
-import com.cannontech.web.support.development.database.objects.DevObject;
-
-public interface DevObjectCreationInterface {
-    public void createAll(DevDbSetupTask devDbSetupTask);
-    public void logFinalExecutionDetails(DevObject devObj);
-}
