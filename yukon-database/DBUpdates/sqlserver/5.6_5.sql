9f6663ea21716110941f5130c700a66c6c5579ea
DeZeeuw, Garrett <>
2013-07-22 18:31:05 +0000

    YUK-12368 DB Change - CalcBase UpdateType storing enum toString instead of value
    Updated CalcBase.UpdateType field.  Fixed incorrect values that were the result of incorrect paoDefinition.xml definitions involved in RFN template device creation process.

--- yukon-database/DBUpdates/sqlserver/5.6_5.sql
+++ yukon-database/DBUpdates/sqlserver/5.6_5.sql
@@ -17,6 +17,15 @@ ALTER TABLE ArchiveValuesExportFormat
 ALTER COLUMN Delimiter VARCHAR(20) NULL;
 /* End YUK-12218 */
 
+/* Start YUK-12368 */
+UPDATE CALCBASE SET UPDATETYPE = 'On First Change' WHERE UPDATETYPE = 'ON_FIRST_CHANGE';
+UPDATE CALCBASE SET UPDATETYPE = 'On All Change' WHERE UPDATETYPE = 'ON_ALL_CHANGE';
+UPDATE CALCBASE SET UPDATETYPE = 'On Timer' WHERE UPDATETYPE = 'ON_TIMER';
+UPDATE CALCBASE SET UPDATETYPE = 'On Timer And Change' WHERE UPDATETYPE = 'ON_TIMER_CHANGE';
+UPDATE CALCBASE SET UPDATETYPE = 'Constant' WHERE UPDATETYPE = 'CONSTANT';
+UPDATE CALCBASE SET UPDATETYPE = 'Historical' WHERE UPDATETYPE = 'HISTORICAL';
+/* End YUK-12368 */
+
 /**************************************************************/
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
--- yukon-database/DBUpdates/sqlserver/6.0_1.sql
+++ yukon-database/DBUpdates/sqlserver/6.0_1.sql
@@ -1,20 +1,6 @@
-/******************************************/ 
-/****     Oracle DBupdates             ****/ 
-/******************************************/ 
-
-/* Start YUK-12218 */
-ALTER TABLE ArchiveValuesExportFormat
-ADD TimeZoneFormat VARCHAR2(20);
-
-UPDATE ArchiveValuesExportFormat
-SET TimeZoneFormat = 'LOCAL';
-
-ALTER TABLE ArchiveValuesExportFormat
-MODIFY TimeZoneFormat VARCHAR2(20) NOT NULL;
-
-ALTER TABLE ArchiveValuesExportFormat
-MODIFY Delimiter VARCHAR2(20) NULL;
-/* End YUK-12218 */
+/******************************************/
+/**** SQL Server DBupdates             ****/
+/******************************************/
 
 /* Start YUK-12368 */
 UPDATE CALCBASE SET UPDATETYPE = 'On First Change' WHERE UPDATETYPE = 'ON_FIRST_CHANGE';
@@ -29,4 +15,4 @@ UPDATE CALCBASE SET UPDATETYPE = 'Historical' WHERE UPDATETYPE = 'HISTORICAL';
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
 /**************************************************************/
-INSERT INTO CTIDatabase VALUES ('5.6', '25-JUL-2013', 'Latest Update', 5, SYSDATE);
+/*INSERT INTO CTIDatabase VALUES ('6.0', '15-AUG-2013', 'Latest Update', 1, GETDATE());*/
