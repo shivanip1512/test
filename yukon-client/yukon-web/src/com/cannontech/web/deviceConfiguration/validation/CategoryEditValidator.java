4698acde45ea56457ff078a0326f8467dab71071
Gerads, Jordan <>
2013-08-12 14:33:15 +0000

    YUK-11856 MCT TOU schedule config and parsing allows invalid TOU schedules - Added service layer validation of TOU times and Display items. - Added i18n entries for the error messages.

--- yukon-client/yukon-web/src/com/cannontech/web/deviceConfiguration/validation/CategoryEditValidator.java
+++ yukon-client/yukon-web/src/com/cannontech/web/deviceConfiguration/validation/CategoryEditValidator.java
@@ -1,19 +1,23 @@
 package com.cannontech.web.deviceConfiguration.validation;
 
 import java.util.List;
+import java.util.Map.Entry;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.validation.Errors;
 
 import com.cannontech.clientutils.YukonLogManager;
+import com.cannontech.common.device.config.model.jaxb.CategoryType;
 import com.cannontech.common.validator.SimpleValidator;
 import com.cannontech.common.validator.YukonValidationUtils;
 import com.cannontech.web.deviceConfiguration.model.CategoryEditBean;
+import com.cannontech.web.deviceConfiguration.model.CategoryEditBean.RateBackingBean;
 import com.cannontech.web.deviceConfiguration.model.CategoryTemplate;
 import com.cannontech.web.deviceConfiguration.model.Field;
 import com.cannontech.web.deviceConfiguration.model.FloatField;
 import com.cannontech.web.deviceConfiguration.model.IntegerField;
+import com.cannontech.web.deviceConfiguration.model.RateInput;
 import com.cannontech.web.input.validate.InputValidator;
 
 public class CategoryEditValidator extends SimpleValidator<CategoryEditBean> {
@@ -34,37 +38,80 @@ public class CategoryEditValidator extends SimpleValidator<CategoryEditBean> {
     
         List<Field<?>> fields = categoryTemplate.getFields();
         
-        for (Field<?> field : fields) {
-            if (field.getInputType() != null) {
-                String value = target.getCategoryInputs().get(field.getFieldName());
-    
-                final String path = "categoryInputs[" + field.getFieldName() + "]";
-                
-                if (StringUtils.isBlank(value)) {
-                    errors.rejectValue(path, baseKey + ".emptyValue");
-                    continue;
+        if (CategoryType.DISPLAY_ITEMS.value().equals(categoryTemplate.getCategoryType())) {
+            // Handle this guy differently.
+            boolean slotDisabledHit = false;
+            for (Field<?> field : fields) {
+                if (Integer.valueOf(target.getCategoryInputs().get(field.getFieldName())) == 0) {
+                    slotDisabledHit = true;
+                } else if (slotDisabledHit) {
+                    // Error
+                    errors.rejectValue("categoryInputs[" + field.getFieldName() + "]", baseKey + ".postDisabled");
                 }
-                
-                if (field.getValidator() != null) {
-                    try {
-                        if (field.getClass() == IntegerField.class) {
-                            IntegerField intField = (IntegerField) field;
-                            InputValidator<Integer> validator = intField.getValidator();
-                            validator.validate(path, field.getDisplayName(), Integer.valueOf(value), errors);
-                        } else if (field.getClass() == FloatField.class) {
-                            FloatField floatField = (FloatField) field;
-                            InputValidator<Float> validator = floatField.getValidator();
-                            validator.validate(path, field.getDisplayName(), Float.valueOf(value), errors);
-                        } else {
-                            log.error("Received a validator for an unsupoorted type: " + field.getClass().getSimpleName());
+            }
+        } else {
+            for (Field<?> field : fields) {
+                if (field.getInputType() != null) {
+                    String value = target.getCategoryInputs().get(field.getFieldName());
+        
+                    final String path = "categoryInputs[" + field.getFieldName() + "]";
+                    
+                    if (StringUtils.isBlank(value)) {
+                        errors.rejectValue(path, baseKey + ".emptyValue");
+                        continue;
+                    }
+                    
+                    if (field.getValidator() != null) {
+                        try {
+                            if (field.getClass() == IntegerField.class) {
+                                IntegerField intField = (IntegerField) field;
+                                InputValidator<Integer> validator = intField.getValidator();
+                                validator.validate(path, field.getDisplayName(), Integer.valueOf(value), errors);
+                            } else if (field.getClass() == FloatField.class) {
+                                FloatField floatField = (FloatField) field;
+                                InputValidator<Float> validator = floatField.getValidator();
+                                validator.validate(path, field.getDisplayName(), Float.valueOf(value), errors);
+                            } else {
+                                log.error("Received a validator for an unsupported type: " + field.getClass().getSimpleName());
+                            }
+                        } catch (NumberFormatException nfe) {
+                            // The value wasn't a number
+                            errors.rejectValue(path, baseKey + ".nonNumber");
                         }
-                    } catch (NumberFormatException nfe) {
-                        // The value wasn't a number
-                        errors.rejectValue(path, baseKey + ".nonNumber");
+                    }
+                } else {
+                    RateBackingBean rateBackingBean = target.getScheduleInputs().get(field.getFieldName());
+                    
+                    int prevTime = 0; // Start at midnight.
+                    boolean midnightHit = false; // Keep track of if we've encountered a midnight entry.
+                    
+                    for (Entry<String, RateInput> entry : rateBackingBean.getRateInputs().entrySet()) {
+                        if (!"time0".equals(entry.getKey())) {
+                            Integer thisTime = Integer.valueOf(entry.getValue().getTime().replace(":", ""));
+                            
+                            if (thisTime <= prevTime) {
+                                if (thisTime == 0) {
+                                    // As long as the rest are midnights, this is okay. 
+                                    midnightHit = true;
+                                    prevTime = thisTime;
+                                } else {
+                                    // Problem. This is never okay 
+                                    String path = "scheduleInputs[" + field.getFieldName() + "].rateInputs[" + entry.getKey() + "].time";
+                                    errors.rejectValue(path, baseKey + ".invalidTime");
+                                }
+                            } else {
+                                if (midnightHit) {
+                                    // Problem. We've already hit midnight, the list should be over.
+                                    String path = "scheduleInputs[" + field.getFieldName() + "].rateInputs[" + entry.getKey() + "].time";
+                                    errors.rejectValue(path, baseKey + ".invalidTime");
+                                } else {
+                                    prevTime = thisTime;
+                                }
+                            }
+                        }
                     }
                 }
             }
-            // TODO: Else case is a map. There should be some thought put into how validating of the maps will look.
         }
     }
 }
