ce3fb3f9d08811f2178d801c6ae8ad7d262daa64
Pipp, Joseph <>
2013-09-03 16:21:38 +0000

    YUK-12064 Dashboard -Added Javadoc

--- yukon-client/common/src/com/cannontech/common/userpage/dao/UserPageDao.java
+++ yukon-client/common/src/com/cannontech/common/userpage/dao/UserPageDao.java
@@ -10,11 +10,29 @@ public interface UserPageDao {
 
     public static final int MAX_HISTORY = 10;
 
+    /**
+     * @return true IFF a page with that path exists in database for the user and is favorited
+     */
     public boolean isFavorite(UserPage page);
+
+    /**
+     * Toggles favorite boolean in database for page with path and userId.
+     * If the page is not in the database, it is added with favorite=true
+     */
     public boolean toggleFavorite(UserPage page);
+
+    /**
+     * Adds page to history. Removes all but the MAX_HISTORY most recently viewed non-favorite pages for that page's userId.
+     */
     public void updateHistory(UserPage page);
 
+    /**
+     * @return All UserPages that have the userId of the user
+     */
     public List<UserPage> getPagesForUser(LiteYukonUser user);
 
+    /**
+     * Delete all references to pao in page history/favorites for all users. To be called after a Pao is deleted.
+     */
     public void deletePagesForPao(PaoIdentifier paoIdentifier);
 }
