516e964fb7d3ecdfffff0604f8eb8c2114a3859e
Feldman, Marina <MarinaFeldman@Eaton.com>
2013-10-03 18:01:03 +0000

    YUK-12219 EC Operator Logins - missing ability to add (existing) user to Operator List/ review changes

--- yukon-client/yukon-web/src/com/cannontech/web/picker/v2/ECOperatorCandidatePicker.java
+++ yukon-client/yukon-web/src/com/cannontech/web/picker/v2/ECOperatorCandidatePicker.java
@@ -7,13 +7,19 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.cannontech.common.bulk.filter.AbstractRowMapperWithBaseQuery;
+import com.cannontech.common.bulk.filter.PostProcessingFilter;
+import com.cannontech.common.bulk.filter.SqlFilter;
+import com.cannontech.common.survey.dao.impl.EnergyCompanyFilter;
+import com.cannontech.common.survey.model.Survey;
 import com.cannontech.common.util.SqlFragmentSource;
 import com.cannontech.common.util.SqlStatementBuilder;
 import com.cannontech.core.dao.YukonUserDao;
 import com.cannontech.database.YukonResultSet;
+import com.cannontech.database.data.lite.LiteEnergyCompany;
 import com.cannontech.database.db.user.UltraLightYukonUser;
 import com.cannontech.stars.core.service.YukonEnergyCompanyService;
 import com.cannontech.stars.energyCompany.dao.EnergyCompanyDao;
+import com.cannontech.user.YukonUserContext;
 import com.google.common.collect.Lists;
 
 public class ECOperatorCandidatePicker extends DatabasePicker<UltraLightYukonUser> {
@@ -53,7 +59,16 @@ public class ECOperatorCandidatePicker extends DatabasePicker<UltraLightYukonUse
         return outputColumns;
     }
 
-    public static class ECOperatorCandidateRowMapper extends
+    @Override
+    protected void updateFilters(List<SqlFilter> sqlFilters,
+            List<PostProcessingFilter<UltraLightYukonUser>> postProcessingFilters,
+            String extraArgs, YukonUserContext userContext) {
+        LiteEnergyCompany energyCompany =
+            energyCompanyDao.getEnergyCompany(userContext.getYukonUser());
+        sqlFilters.add(new OperatorCandidateFilter(energyCompany.getEnergyCompanyID()));
+    }
+    
+    private static class ECOperatorCandidateRowMapper extends
             AbstractRowMapperWithBaseQuery<UltraLightYukonUser> {
 
         @Override
@@ -61,11 +76,6 @@ public class ECOperatorCandidatePicker extends DatabasePicker<UltraLightYukonUse
             SqlStatementBuilder sql = new SqlStatementBuilder();
             sql.append("SELECT UserId, Username");
             sql.append("FROM YukonUser YU");
-            sql.append("WHERE (YU.UserGroupId is null OR YU.UserGroupId not in");
-            sql.append("      (SELECT UG.UserGroupId FROM ECToResidentialGroupMapping ECTGM JOIN UserGroup UG");
-            sql.append("              ON UG.UserGroupId = ECTGM.UserGroupId))");
-            sql.append("       AND YU.UserId not in(SELECT OperatorLoginID from EnergyCompanyOperatorLoginList)");
-            sql.append("              AND YU.UserId not in(SELECT loginid from contact)");
             return sql;
         }
 
@@ -75,6 +85,36 @@ public class ECOperatorCandidatePicker extends DatabasePicker<UltraLightYukonUse
             user.setUserId(rs.getInt("UserId"));
             user.setUsername(rs.getString("Username"));
             return user;
+        }
+
+        @Override
+        public boolean needsWhere() {
+            return true;
+        }
+    }
+    
+    private class OperatorCandidateFilter implements SqlFilter {
+        private int energyCompanyId;
+
+        public OperatorCandidateFilter(int energyCompanyId) {
+            this.energyCompanyId = energyCompanyId;
+        }
+
+        @Override
+        public SqlFragmentSource getWhereClauseFragment() {
+            SqlStatementBuilder sql = new SqlStatementBuilder();
+            sql.append("YU.UserId NOT IN(SELECT OperatorLoginID from EnergyCompanyOperatorLoginList)");
+            sql.append("AND (YU.UserGroupId IS NOT null AND YU.UserGroupId IN");
+            sql.append("    (SELECT UserGroupId FROM ECToOperatorGroupMapping ECTRM WHERE ECTRM.EnergyCompanyId").eq(energyCompanyId).append("))");
+            sql.append("            OR ((YU.UserGroupId IS NULL AND YU.UserId NOT IN");
+            sql.append("                   (SELECT C.LoginId FROM CustomerAccount CA");
+            sql.append("                         JOIN Customer CU ON  CU.CustomerId=CA.CustomerId");
+            sql.append("                         JOIN Contact C ON  C.ContactID=CU.PrimaryContactID))");
+            sql.append("            AND ((YU.UserGroupId IS NULL AND YU.UserId NOT IN");
+            sql.append("                   (SELECT C.LoginId FROM CustomerAccount CA");
+            sql.append("                         JOIN CustomerAdditionalContact CAC ON CA.CustomerId = CAC.CustomerId");
+            sql.append("                         JOIN Contact C ON CA.CustomerId = C.ContactID))))");
+            return sql;
         }
     }
 }
