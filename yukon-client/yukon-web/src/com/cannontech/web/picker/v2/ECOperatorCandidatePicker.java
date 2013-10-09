d35228d32d65a8f78858170db62fd8d2499d7daf
Feldman, Marina <MarinaFeldman@Eaton.com>
2013-10-09 19:50:22 +0000

    YUK-12219 EC Operator Logins - missing ability to add (existing) user to Operator List/ review changes

--- yukon-client/yukon-web/src/com/cannontech/web/picker/v2/ECOperatorCandidatePicker.java
+++ yukon-client/yukon-web/src/com/cannontech/web/picker/v2/ECOperatorCandidatePicker.java
@@ -9,33 +9,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.cannontech.common.bulk.filter.AbstractRowMapperWithBaseQuery;
 import com.cannontech.common.bulk.filter.PostProcessingFilter;
 import com.cannontech.common.bulk.filter.SqlFilter;
-import com.cannontech.common.survey.dao.impl.EnergyCompanyFilter;
-import com.cannontech.common.survey.model.Survey;
+import com.cannontech.common.search.result.UltraLightYukonUser;
 import com.cannontech.common.util.SqlFragmentSource;
 import com.cannontech.common.util.SqlStatementBuilder;
-import com.cannontech.core.dao.YukonUserDao;
 import com.cannontech.database.YukonResultSet;
 import com.cannontech.database.data.lite.LiteEnergyCompany;
-import com.cannontech.database.db.user.UltraLightYukonUser;
-import com.cannontech.stars.core.service.YukonEnergyCompanyService;
 import com.cannontech.stars.energyCompany.dao.EnergyCompanyDao;
 import com.cannontech.user.YukonUserContext;
 import com.google.common.collect.Lists;
 
 public class ECOperatorCandidatePicker extends DatabasePicker<UltraLightYukonUser> {
 
-    @Autowired private YukonEnergyCompanyService yukonEnergyCompanyService;
     @Autowired private EnergyCompanyDao energyCompanyDao;
-    @Autowired private YukonUserDao yukonUserDao;
-
 
     private final static String[] searchColumnNames = new String[] {
-        "UserId", "Username"};
+        "UserId", "UserName"};
     
     private static List<OutputColumn> outputColumns;
     static {
         List<OutputColumn> columns = Lists.newArrayList();
-        columns.add(new OutputColumn("username", "yukon.web.picker.user.name"));        
+        columns.add(new OutputColumn("userName", "yukon.web.picker.user.name"));        
         columns.add(new OutputColumn("userId", "yukon.web.picker.user.userId"));
         outputColumns = Collections.unmodifiableList(columns);
     }
@@ -74,16 +67,31 @@ public class ECOperatorCandidatePicker extends DatabasePicker<UltraLightYukonUse
         @Override
         public SqlFragmentSource getBaseQuery() {
             SqlStatementBuilder sql = new SqlStatementBuilder();
-            sql.append("SELECT UserId, Username");
+            sql.append("SELECT UserId, UserName");
             sql.append("FROM YukonUser YU");
             return sql;
         }
 
         @Override
         public UltraLightYukonUser mapRow(YukonResultSet rs) throws SQLException {
-            UltraLightYukonUser user = new UltraLightYukonUser();
-            user.setUserId(rs.getInt("UserId"));
-            user.setUsername(rs.getString("Username"));
+            final String username = rs.getString("UserName");
+            final int userId = rs.getInt("UserId");
+            final UltraLightYukonUser user = new UltraLightYukonUser() {
+                @Override
+                public String getUserName() {
+                    return username;
+                }
+
+                @Override
+                public String getGroupName() {
+                    return "";
+                }
+
+                @Override
+                public int getUserId() {
+                    return userId;
+                }
+            };
             return user;
         }
 
