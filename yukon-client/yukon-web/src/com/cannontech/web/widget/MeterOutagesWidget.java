e1150d937001d4315f8023b05e3fdbcd3e46e4e7
Stenersen, Ross <>
2013-10-16 23:08:35 +0000

    YUK-12428 MultiSpeak - limit meter read requests to enabled meters only
    Temporarily backed out partial check-in.

--- yukon-client/yukon-web/src/com/cannontech/web/widget/MeterOutagesWidget.java
+++ yukon-client/yukon-web/src/com/cannontech/web/widget/MeterOutagesWidget.java
@@ -201,8 +201,8 @@ public class MeterOutagesWidget extends WidgetControllerBase {
         List<OutageData> outageData = new ArrayList<OutageData>(values.size());
         for (PointValueHolder holder : values) {
             if( holder.getId() == litePoint.getPointID()) {
-//                String duration = TimeUtil.convertSecondsToTimeString(holder.getValue());
-                String duration = TimeUtil.convertSecondsToNormalizedStandard(holder.getValue());
+                String duration = TimeUtil.convertSecondsToTimeString(holder.getValue());
+//                String duration = TimeUtil.convertSecondsToNormalizedStandard(holder.getValue());
                 PointValueHolder timestamp = holder;
 
                 // based on the string returned from porter, parse out the outage index
