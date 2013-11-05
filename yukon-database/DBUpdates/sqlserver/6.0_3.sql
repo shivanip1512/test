312ad6e740d302bbf3cd74b4719df55cf3c0e369
DeZeeuw, Garrett <>
2013-11-05 19:43:49 +0000

    YUK-12709 DB Changes for Theming visited links and primary buttons
    Added inserts statements to the update scripts for the new theme properties.  This is intended to help support internal processes (QA, nightly builds, etc) that have already been running 6.0.2.

---
+++ yukon-database/DBUpdates/sqlserver/6.0_3.sql
@@ -0,0 +1,23 @@
+/******************************************/
+/**** SQL Server DBupdates             ****/
+/******************************************/
+
+/* Start YUK-12709 */
+INSERT INTO ThemeProperty (ThemeId, Property, Value) 
+(SELECT DISTINCT ThemeId, 'VISITED_COLOR', '#1c49a6' FROM Theme);
+
+INSERT INTO ThemeProperty (ThemeId, Property, Value) 
+(SELECT DISTINCT ThemeId, 'BUTTON_COLOR', '#777' FROM Theme);
+
+INSERT INTO ThemeProperty (ThemeId, Property, Value) 
+(SELECT DISTINCT ThemeId, 'BUTTON_COLOR_BORDER', '#666' FROM Theme);
+
+INSERT INTO ThemeProperty (ThemeId, Property, Value) 
+(SELECT DISTINCT ThemeId, 'BUTTON_COLOR_HOVER', '#888' FROM Theme);
+/* End YUK-12709 */
+
+/**************************************************************/
+/* VERSION INFO                                               */
+/* Inserted when update script is run                         */
+/**************************************************************/
+INSERT INTO CTIDatabase VALUES ('6.0', '05-NOV-2013', 'Latest Update', 3, GETDATE());
