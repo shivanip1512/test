afddc386e7e007009aa499331fa17b3c65e26538
DeZeeuw, Garrett <>
2013-11-25 17:53:34 +0000

    YUK-12765 set CLIENT_APPLICATION_REMEMBER_ME global setting in update scripts +review @b1afc @f84b5 @a87f4
    Added an insert into GlobalSetting to specify that the setting 'CLIENT_APPLICATION_REMEMBER_ME' is set to 'USERNAME_AND_PASSWORD' from all existing systems.
    No change has been made to the creation scripts for this issue.

---
+++ yukon-database/DBUpdates/oracle/6.0_5.sql
@@ -0,0 +1,15 @@
+/******************************************/ 
+/****     Oracle DBupdates             ****/ 
+/******************************************/ 
+
+/* Start YUK-12765 */
+INSERT INTO GlobalSetting (GlobalSettingId, Name, Value, Comments, LastChangedDate)
+VALUES ((SELECT NVL(MAX(GlobalSettingId)+1,1) FROM GlobalSetting),  
+        'CLIENT_APPLICATION_REMEMBER_ME', 'USERNAME_AND_PASSWORD', null, null);
+/* End YUK-12765 */
+
+/**************************************************************/
+/* VERSION INFO                                               */
+/* Inserted when update script is run                         */
+/**************************************************************/
+/*INSERT INTO CTIDatabase VALUES ('6.0', '9-DEC-2013', 'Latest Update', 5, SYSDATE);*/
