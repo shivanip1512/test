62721d9df0114d7ba1b06ba92d4e25d15ae27ea3
Stenersen, Ross <>
2013-12-02 20:33:34 +0000

    YUK-12366 Add support for finer grained control over TimerFilter logging
    Added two new loggers for finer grained control over logging.

--- yukon-client/yukon-web/src/com/cannontech/servlet/filter/TimerFilter.java
+++ yukon-client/yukon-web/src/com/cannontech/servlet/filter/TimerFilter.java
@@ -1,6 +1,8 @@
 package com.cannontech.servlet.filter;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Set;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -14,12 +16,18 @@ import org.apache.log4j.Logger;
 
 import com.cannontech.clientutils.YukonLogManager;
 import com.cannontech.util.ServletUtil;
+import com.google.common.collect.ImmutableSet;
 
 /**
  * Filter that times how long a request takes.
  */
 public class TimerFilter implements Filter {
-    private Logger log = YukonLogManager.getLogger(TimerFilter.class);
+    private final static Logger log = YukonLogManager.getLogger(TimerFilter.class);
+    private final static Logger ajaxLog = YukonLogManager.getLogger(TimerFilter.class.getName() + ".ajax");
+    private final static Logger staticLog = YukonLogManager.getLogger(TimerFilter.class.getName() + ".static");
+
+    private final static Set<String> staticRequestExtensions =
+            ImmutableSet.of("png", "jpg", "jpeg", "gif", "css", "js");
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse response, FilterChain chain) throws IOException,
@@ -33,10 +41,29 @@ public class TimerFilter implements Filter {
             name = ((HttpServletRequest) servletRequest).getRequestURI();
         }
         if (ServletUtil.isAjaxRequest(servletRequest)) {
-            log.debug(name + ": " + (after - before) + "ms");
+            ajaxLog.debug(name + ": " + (after - before) + "ms");
+        } else if (isStaticPage(name, response.getContentType())) {
+            staticLog.debug(name + ": " + (after - before) + "ms");
         } else {
             log.info(name + ": " + (after - before) + "ms");
         }
+    }
+
+    /**
+     * Calling these "static" pages is a bit of a lie but close enough for logging.  (Images, CSS, etc. can be
+     * generated dynamically too.)
+     */
+    private static boolean isStaticPage(String name, String contentType) {
+        String extension = null;
+        int lastPeriod = name.lastIndexOf('.');
+        if (lastPeriod != -1 && lastPeriod < name.length() - 1) {
+            extension = name.substring(lastPeriod + 1).toLowerCase(Locale.US);
+        }
+        if (log.isTraceEnabled()) {
+            log.trace("isStaticPage(" + name + ", " + contentType + "); extension = " + extension);
+        }
+        return staticRequestExtensions.contains(extension)
+                || contentType != null && (contentType.startsWith("image/") || contentType.equals("text/css"));
     }
 
     @Override
@@ -44,6 +71,6 @@ public class TimerFilter implements Filter {
     }
 
     @Override
-    public void init(FilterConfig arg0) throws ServletException {
+    public void init(FilterConfig filterConfig) throws ServletException {
     }
 }
