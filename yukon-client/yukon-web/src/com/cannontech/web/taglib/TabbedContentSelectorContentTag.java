afad60a6290dd30d78f156c2a880354eaff14804
Solberg, Aaron <>
2013-12-04 19:06:24 +0000

    YUK-12040 Yukon User Interface Update
    - fixed class being null

--- yukon-client/yukon-web/src/com/cannontech/web/taglib/TabbedContentSelectorContentTag.java
+++ yukon-client/yukon-web/src/com/cannontech/web/taglib/TabbedContentSelectorContentTag.java
@@ -6,29 +6,29 @@ import javax.servlet.jsp.JspException;
 
 public class TabbedContentSelectorContentTag extends YukonTagSupport {
     
-	private String selectorName = "";
-	private String cssClass;
-	private String tabId;
-	private boolean initiallySelected = false;
-	
-	@Override
+    private String selectorName = "";
+    private String cssClass = "";
+    private String tabId;
+    private boolean initiallySelected = false;
+    
+    @Override
     public void doTag() throws JspException, IOException {
-		String id = tabId != null ? tabId : UniqueIdentifierTag.generateIdentifier(getJspContext(), "tabbedContentSelectorContent_");
-		
-		// tell container tag about ourself
+        String id = tabId != null ? tabId : UniqueIdentifierTag.generateIdentifier(getJspContext(), "tabbedContentSelectorContent_");
+        
+        // tell container tag about ourself
         TabbedContentSelectorTag parent = getParent(TabbedContentSelectorTag.class);
         parent.addTab(selectorName, id, initiallySelected);
         
         // tab content
-		getJspContext().getOut().println("<div id=\"" + id + "\" class=\"" + cssClass + "\">");
+        getJspContext().getOut().println("<div id=\"" + id + "\" class=\"" + cssClass + "\">");
         getJspBody().invoke(getJspContext().getOut());
-		getJspContext().getOut().println("</div>");
+        getJspContext().getOut().println("</div>");
         
     }
-	
-	public void setSelectorName(String selectorName) {
-		this.selectorName = selectorName;
-	}
+    
+    public void setSelectorName(String selectorName) {
+        this.selectorName = selectorName;
+    }
 
     public void setCssClass(String cssClass) {
         this.cssClass = cssClass;
@@ -38,8 +38,8 @@ public class TabbedContentSelectorContentTag extends YukonTagSupport {
         this.tabId = tabId;
     }
     
-	public void setInitiallySelected(boolean initiallySelected) {
-		this.initiallySelected = initiallySelected;
-	}
-	
+    public void setInitiallySelected(boolean initiallySelected) {
+        this.initiallySelected = initiallySelected;
+    }
+    
 }
