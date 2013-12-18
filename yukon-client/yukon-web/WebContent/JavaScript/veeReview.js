2b27722074f6e95a018668c2986c108b329ebea8
Dyhr, Mark <>
2013-12-18 17:25:06 +0000

    YUK-12849 Convert prototypejs code to jQuery - whitespace-only changes

--- yukon-client/yukon-web/WebContent/JavaScript/veeReview.js
+++ yukon-client/yukon-web/WebContent/JavaScript/veeReview.js
@@ -1,70 +1,70 @@
-jQuery(function() {
+jQuery(function () {
 
-	$$('td.ACTION_TD').each(function(el) {
-		
-		Event.observe(el, 'click', function() {
+    $$('td.ACTION_TD').each(function (el) {
 
-			var h = getActionTdHash(el);
-        	var action = h.get('action');
-        	var valueEl = h.get('valueEl');
+        Event.observe(el, 'click', function () {
 
-        	if (valueEl.value == action) {
-        		resetDeleteAccept(action, h.get('deleteImgEl'), h.get('acceptImgEl'));
-		        valueEl.value = '';
-        	} else {
-        		toggleDeleteAccept(action, h.get('deleteImgEl'), h.get('acceptImgEl'));
-        		valueEl.value = action;
-        	}
+            var h = getActionTdHash(el);
+            var action = h.get('action');
+            var valueEl = h.get('valueEl');
+
+            if (valueEl.value == action) {
+                resetDeleteAccept(action, h.get('deleteImgEl'), h.get('acceptImgEl'));
+                valueEl.value = '';
+            } else {
+                toggleDeleteAccept(action, h.get('deleteImgEl'), h.get('acceptImgEl'));
+                valueEl.value = action;
+            }
         });
-	});
+    });
 });
 
-function checkUncheckAll(action) {
-	
-	var checkAllState = $('checkAllState');
+function checkUncheckAll (action) {
 
-	$$('td.ACTION_TD').each(function(el) {
+    var checkAllState = $('checkAllState');
 
-		var h = getActionTdHash(el);
-    	var tdAction = h.get('action');
-    	var valueEl = h.get('valueEl');
+    $$('td.ACTION_TD').each(function (el) {
 
-		if (checkAllState.value == action) {
-			resetDeleteAccept(tdAction, h.get('deleteImgEl'), h.get('acceptImgEl'));
-			valueEl.value = '';
-		} else {
-        	toggleDeleteAccept(action, h.get('deleteImgEl'), h.get('acceptImgEl'));
-    		valueEl.value = action;
-		}
-	});
+        var h = getActionTdHash(el);
+        var tdAction = h.get('action');
+        var valueEl = h.get('valueEl');
 
-	if (checkAllState.value == action) {
-		checkAllState.value = '';
-	} else {
-		checkAllState.value = action;
-	}
+        if (checkAllState.value == action) {
+            resetDeleteAccept(tdAction, h.get('deleteImgEl'), h.get('acceptImgEl'));
+            valueEl.value = '';
+        } else {
+            toggleDeleteAccept(action, h.get('deleteImgEl'), h.get('acceptImgEl'));
+            valueEl.value = action;
+        }
+    });
+
+    if (checkAllState.value == action) {
+        checkAllState.value = '';
+    } else {
+        checkAllState.value = action;
+    }
 }
 
-function resetDeleteAccept(action, deleteImgEl, acceptImgEl) {
+function resetDeleteAccept (action, deleteImgEl, acceptImgEl) {
 
-	if (action == 'DELETE') {
-	    jQuery(deleteImgEl).attr('disabled', 'disabled');
-	}
-	if (action == 'ACCEPT') {
-	    jQuery(acceptImgEl).attr('disabled', 'disabled');
-	}
-}
-
-function toggleDeleteAccept(action, deleteImgEl, acceptImgEl) {
-	
-	if (action == 'DELETE') {
-	    jQuery(deleteImgEl).removeAttr('disabled');
-	    jQuery(acceptImgEl).attr('disabled', 'disabled');
-	}
-	if (action == 'ACCEPT') {
-	    jQuery(acceptImgEl).removeAttr('disabled');
+    if (action == 'DELETE') {
         jQuery(deleteImgEl).attr('disabled', 'disabled');
-	}
+    }
+    if (action == 'ACCEPT') {
+        jQuery(acceptImgEl).attr('disabled', 'disabled');
+    }
+}
+
+function toggleDeleteAccept (action, deleteImgEl, acceptImgEl) {
+
+    if (action == 'DELETE') {
+        jQuery(deleteImgEl).removeAttr('disabled');
+        jQuery(acceptImgEl).attr('disabled', 'disabled');
+    }
+    if (action == 'ACCEPT') {
+        jQuery(acceptImgEl).removeAttr('disabled');
+        jQuery(deleteImgEl).attr('disabled', 'disabled');
+    }
 }
 
 /**
@@ -72,17 +72,17 @@ function toggleDeleteAccept(action, deleteImgEl, acceptImgEl) {
  * @param el    {Element}
  * @returns     {Hash} 
  */
-function getActionTdHash(el) {
+function getActionTdHash (el) {
 
-	var descendants = el.descendants();
-	var idParts = descendants[0].id.split('_');
-	var action = idParts[1];
-	var changeId = idParts[3];
+    var descendants = el.descendants();
+    var idParts = descendants[0].id.split('_');
+    var action = idParts[1];
+    var changeId = idParts[3];
 
-	return $H({
-	    'action': action,
-	    'deleteImgEl': $('ACTION_DELETE_IMG_' + changeId),
-        'acceptImgEl': $('ACTION_ACCEPT_IMG_' + changeId),
-        'valueEl': $('ACTION_' + changeId)
-	});
+    return $H({
+        'action' : action,
+        'deleteImgEl' : $('ACTION_DELETE_IMG_' + changeId),
+        'acceptImgEl' : $('ACTION_ACCEPT_IMG_' + changeId),
+        'valueEl' : $('ACTION_' + changeId)
+    });
 }
