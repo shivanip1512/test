ec141b001cf4a0fc8a0f7ba199522d21ba12f132
Woodward, Daniel <>
2014-02-28 18:51:32 +0000

    YUK-12926 Stars Oper, RF LCR - clicking on Control History caused Error Occurred page
    Fix an issue with paging

--- yukon-client/common/src/com/cannontech/common/model/PagingParameters.java
+++ yukon-client/common/src/com/cannontech/common/model/PagingParameters.java
@@ -7,10 +7,14 @@ public final class PagingParameters {
     private final int numberPerPage;
     private final int startIndex;
     private final int endIndex;
-    
+
+    /**
+     * @param numberPerPage must be greater than 0
+     * @param pageNumber is 1-based (i.e. starts at 1, not 0)
+     */
     public PagingParameters(int numberPerPage, int pageNumber) {
-        checkArgument(numberPerPage != 0);
-        checkArgument(pageNumber != 0);
+        checkArgument(numberPerPage > 0);
+        checkArgument(pageNumber > 0);
 
         this.numberPerPage = numberPerPage;
         this.pageNumber = pageNumber;
