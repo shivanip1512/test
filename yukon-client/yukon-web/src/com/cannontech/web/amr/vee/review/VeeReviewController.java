c9e03d2eb3eb443c4967d19540e404f036165b34
Ogaard, Jason <>
2014-03-13 16:48:36 +0000

    YUK-12681 Opt-Outs Administration Page is Old and Needs to be Redone -removed unused import

--- yukon-client/yukon-web/src/com/cannontech/web/amr/vee/review/VeeReviewController.java
+++ yukon-client/yukon-web/src/com/cannontech/web/amr/vee/review/VeeReviewController.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.cannontech.common.model.PagingParameters;
 import com.cannontech.common.search.result.SearchResults;
