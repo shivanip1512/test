f8865a53a77d2888210f475fec689efbf22addce
Gerads, Jordan <>
2014-03-13 21:16:25 +0000

    YUK-13023 DNP Default Assignment Not Working - Added a config assignment db change message to be sent upon the creation of a DNP device.

--- yukon-client/common/src/com/cannontech/database/data/device/DNPBase.java
+++ yukon-client/common/src/com/cannontech/database/data/device/DNPBase.java
@@ -1,5 +1,7 @@
 package com.cannontech.database.data.device;
 
+import java.util.List;
+
 import com.cannontech.common.device.config.dao.DeviceConfigurationDao;
 import com.cannontech.common.device.config.model.DNPConfiguration;
 import com.cannontech.common.device.config.model.DeviceConfiguration;
@@ -8,11 +10,15 @@ import com.cannontech.common.device.model.SimpleDevice;
 import com.cannontech.common.pao.PaoType;
 import com.cannontech.common.pao.YukonDevice;
 import com.cannontech.database.db.DBCopiable;
+import com.cannontech.message.dispatch.message.DBChangeMsg;
+import com.cannontech.message.dispatch.message.DbChangeType;
 import com.cannontech.spring.YukonSpringHook;
+import com.google.common.collect.Lists;
 
 public class DNPBase extends AddressBase implements DBCopiable {
     private DNPConfiguration dnpConfiguration = null;
 
+    @Override
     public void add() throws java.sql.SQLException {
         super.add();
 
@@ -20,6 +26,7 @@ public class DNPBase extends AddressBase implements DBCopiable {
         add("DeviceConfigurationDeviceMap", configValues);
     }
     
+    @Override
     public void retrieve() throws java.sql.SQLException {
         super.retrieve();
         
@@ -36,6 +43,25 @@ public class DNPBase extends AddressBase implements DBCopiable {
         } else {
             dnpConfiguration = null;
         }
+    }
+    
+    @Override
+    public DBChangeMsg[] getDBChangeMsgs(DbChangeType dbChangeType) {
+        if (dbChangeType == DbChangeType.ADD) {
+            // We have to alert the config manager that a configuration-to-device mapping exists!
+            List<DBChangeMsg> dbChangeMsgs = Lists.newArrayList(super.getDBChangeMsgs(dbChangeType));
+    
+            dbChangeMsgs.add(new DBChangeMsg(getPAObjectID(), 
+                                             DBChangeMsg.CHANGE_CONFIG_DB, 
+                                             DBChangeMsg.CAT_DEVICE_CONFIG, 
+                                             "device", 
+                                             dbChangeType));
+            
+            DBChangeMsg[] dbChange = new DBChangeMsg[dbChangeMsgs.size()];
+            return dbChangeMsgs.toArray(dbChange);
+        }
+        
+        return super.getDBChangeMsgs(dbChangeType);
     }
 
     public void setDnpConfiguration(DNPConfiguration dnpConfiguration) {
