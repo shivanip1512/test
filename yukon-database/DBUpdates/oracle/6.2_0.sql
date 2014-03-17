9fbd57e6e18e4e13f01348a12721092a9417a958
DeZeeuw, Garrett <>
2014-03-17 15:49:16 +0000

    YUK-13120 Delete "Temp Out of Service Duration" Point
    Changed WHERE clause criteria from "PointName = 'Temp Out of Service Duration'" to "PointType = 'Analog' AND PointOffset = 14".

--- yukon-database/DBUpdates/oracle/6.2_0.sql
+++ yukon-database/DBUpdates/oracle/6.2_0.sql
@@ -45,195 +45,195 @@ WHERE ItemName = 'reconnectButton';
 /* Start YUK-13120 */
 DELETE FROM CALCCOMPONENT WHERE PointID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM CalcPointBaseline WHERE PointID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM CustomerBaseLinePoint WHERE PointID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM DynamicCalcHistorical WHERE PointID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM CALCBASE WHERE POINTID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM CALCCOMPONENT WHERE COMPONENTPOINTID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM CAPBANK WHERE CONTROLPOINTID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM CAPCONTROLSUBSTATIONBUS WHERE SwitchPointID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM CAPCONTROLSUBSTATIONBUS WHERE CurrentVoltLoadPointID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM CAPCONTROLSUBSTATIONBUS WHERE CurrentWattLoadPointID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM CAPCONTROLSUBSTATIONBUS WHERE CurrentVarLoadPointID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM CCEventLog WHERE PointID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM CCMonitorBankList WHERE PointId IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM CICUSTOMERPOINTDATA WHERE PointID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM CapControlFeeder WHERE CurrentVarLoadPointID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM CapControlFeeder WHERE CurrentVoltLoadPointID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM CapControlFeeder WHERE CurrentWattLoadPointID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM DISPLAY2WAYDATA WHERE POINTID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM DYNAMICACCUMULATOR WHERE POINTID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM DYNAMICPOINTDISPATCH WHERE POINTID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM DynamicCCMonitorBankHistory WHERE PointID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM DynamicCCMonitorPointResponse WHERE PointID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM DynamicPointAlarming WHERE PointID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM DynamicTags WHERE PointID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM EstimatedLoadFunction WHERE InputPointId IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM EstimatedLoadLookupTable WHERE InputPointId IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM ExtraPaoPointAssignment WHERE PointId IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM FDRTranslation WHERE PointId IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM GRAPHDATASERIES WHERE POINTID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM LMControlAreaTrigger WHERE PointId IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM LMControlAreaTrigger WHERE PeakPointId IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM LMGroupPoint WHERE PointIDUsage IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM PAOExclusion WHERE PointID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM POINTACCUMULATOR WHERE POINTID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM POINTANALOG WHERE POINTID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM POINTLIMITS WHERE POINTID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM POINTPROPERTYVALUE WHERE PointID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM POINTSTATUS WHERE POINTID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM POINTTRIGGER WHERE TriggerID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM POINTTRIGGER WHERE VerificationID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM POINTTRIGGER WHERE PointID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM POINTUNIT WHERE POINTID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM PointAlarming WHERE PointID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM PointStatusControl WHERE PointId IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM PointControl WHERE PointId IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM PointToZoneMapping WHERE PointId IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM TagLog WHERE PointID IN ( 
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 
 DELETE FROM Point WHERE PointId IN (
     SELECT PointId FROM Point P JOIN YukonPAObject YP ON YP.PAObjectID = P.PAObjectID
-    WHERE PointName = 'Temp Out of Service Duration' AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
+    WHERE PointType = 'Analog' AND PointOffset = 14 AND YP.Type IN ('LCR-6200 RFN', 'LCR-6600 RFN'));
 /* End YUK-13120 */
 
 /**************************************************************/
