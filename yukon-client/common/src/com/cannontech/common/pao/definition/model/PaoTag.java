e88004ecfbe8d40c408dd6d50c12dd69044d1e02
Solberg, Aaron <>
2014-03-19 20:28:34 +0000

    formatting

--- yukon-client/common/src/com/cannontech/common/pao/definition/model/PaoTag.java
+++ yukon-client/common/src/com/cannontech/common/pao/definition/model/PaoTag.java
@@ -7,34 +7,34 @@ import com.cannontech.web.input.type.InputType;
 
 public enum PaoTag {
 
-	TOU("TOU"),
-	HIGH_BILL("High Bill"),
-	OUTAGE("Outage"),
-	MOVE_SUPPORTED("Move Supported"),
-	LOAD_PROFILE("Load Profile"),
-	PEAK_REPORT("Peak Report"),
-	VOLTAGE("Voltage"),
-	VOLTAGE_PROFILE("Voltage Profile"),
-	DEVICE_CONFIGURATION("Device Configuration"),
-	DEVICE_ICON_TYPE("Device Icon Type", stringType()),
-	DISCONNECT_COLLAR_COMPATIBLE("Disconnect Collar Compatible"),
-	DISCONNECT_RFN("RFN Disconnect"),
-	DISCONNECT_410("410 Disconnect"),
-	DISCONNECT_310("310 Disconnect"),
-	DISCONNECT_213("213 Disconnect"),
-	METER_DETAIL_DISPLAYABLE("Meter Detail Displayable"),
-	PHASE_DETECT("Phase Detect"),
-	STARS_ACCOUNT_ATTACHABLE_METER("Stars Account Attachable Meter"),
-	LOCATE_ROUTE("Locate Route"),
-	PORTER_COMMAND_REQUESTS("Porter Command Requests"),
-	NETWORK_MANAGER_ATTRIBUTE_READS("Network Manager Attribute Reads"),
-	COMMANDER_REQUESTS("Commander Requests"),
-	MCT_200_SERIES("MCT 200 Series"),
-	MCT_300_SERIES("MCT 300 Series"),
-	USES_METER_NUMBER_FOR_MSP("Uses meter number for Msp"),
-	VOLTAGE_REGULATOR("Voltage Regulator"),
-	LM_GROUP("LM Group"),
-	LM_PROGRAM("LM Program"),
+    TOU("TOU"),
+    HIGH_BILL("High Bill"),
+    OUTAGE("Outage"),
+    MOVE_SUPPORTED("Move Supported"),
+    LOAD_PROFILE("Load Profile"),
+    PEAK_REPORT("Peak Report"),
+    VOLTAGE("Voltage"),
+    VOLTAGE_PROFILE("Voltage Profile"),
+    DEVICE_CONFIGURATION("Device Configuration"),
+    DEVICE_ICON_TYPE("Device Icon Type", stringType()),
+    DISCONNECT_COLLAR_COMPATIBLE("Disconnect Collar Compatible"),
+    DISCONNECT_RFN("RFN Disconnect"),
+    DISCONNECT_410("410 Disconnect"),
+    DISCONNECT_310("310 Disconnect"),
+    DISCONNECT_213("213 Disconnect"),
+    METER_DETAIL_DISPLAYABLE("Meter Detail Displayable"),
+    PHASE_DETECT("Phase Detect"),
+    STARS_ACCOUNT_ATTACHABLE_METER("Stars Account Attachable Meter"),
+    LOCATE_ROUTE("Locate Route"),
+    PORTER_COMMAND_REQUESTS("Porter Command Requests"),
+    NETWORK_MANAGER_ATTRIBUTE_READS("Network Manager Attribute Reads"),
+    COMMANDER_REQUESTS("Commander Requests"),
+    MCT_200_SERIES("MCT 200 Series"),
+    MCT_300_SERIES("MCT 300 Series"),
+    USES_METER_NUMBER_FOR_MSP("Uses meter number for Msp"),
+    VOLTAGE_REGULATOR("Voltage Regulator"),
+    LM_GROUP("LM Group"),
+    LM_PROGRAM("LM Program"),
     LM_CONTROL_AREA("LM Control Area"),
     LM_SCENARIO("LM Scenario"),
     LM_SCHEDULE("LM Schedule"), //used to identify script/schedule that LM permissions can be given to
@@ -42,46 +42,47 @@ public enum PaoTag {
     SEP_PROGRAM_ENROLLMENT("SEP Program Enrollment"),
     DLC_ADDRESS_RANGE_ENFORCE("DLC Address Range Enforced", stringType()),  // range used for validation, may be mfg address or other(field length, for example).
     DLC_ADDRESS_RANGE("DLC Address Range", stringType()),   // range used for display info, should be limited to mfg addresses
-	DUMMY_LONG_TAG("for internal testing", longType()),  // can be removed when we add a real tag that uses long
-	WATER_METER_DETAIL_DISPLAYABLE("Water Detail Displayable"),
-	DB_EDITOR_INCOMPATIBLE("Not Displayable in Database Editor"),   // Excludes devices from DeviceTreeModel.java, hiding them in DB Editor and Commander
-	ROUTE_ENCRYPTION("Supports Route Encryption"),
-	THREE_PHASE_VOLTAGE("Supports Three Phase Voltage"),
-	THREE_PHASE_CURRENT("Supports Three Phase Current"),
-	RFN_EVENTS("Supports Rfn Events"),
-	ONE_WAY_DEVICE("Supports One-Way Communication"),
-	TWO_WAY_DEVICE("Supports Two-Way Communication"),
-	IPC_METER("IPC Meter"),
-	RFN_DEMAND_RESET("RFN Device Supporting Demand Reset"),
-	PLC_DEMAND_RESET("PLC Device Supporting Demand Reset"),
-	RFN_POINT_CALCULATION("RFN Point Calculation"), // This may need to be split into one tag per point and/or used for plc devices later
-	;
+    DUMMY_LONG_TAG("for internal testing", longType()),  // can be removed when we add a real tag that uses long
+    WATER_METER_DETAIL_DISPLAYABLE("Water Detail Displayable"),
+    DB_EDITOR_INCOMPATIBLE("Not Displayable in Database Editor"),   // Excludes devices from DeviceTreeModel.java, hiding them in DB Editor and Commander
+    ROUTE_ENCRYPTION("Supports Route Encryption"),
+    THREE_PHASE_VOLTAGE("Supports Three Phase Voltage"),
+    THREE_PHASE_CURRENT("Supports Three Phase Current"),
+    RFN_EVENTS("Supports Rfn Events"),
+    ONE_WAY_DEVICE("Supports One-Way Communication"),
+    TWO_WAY_DEVICE("Supports Two-Way Communication"),
+    IPC_METER("IPC Meter"),
+    RFN_DEMAND_RESET("RFN Device Supporting Demand Reset"),
+    PLC_DEMAND_RESET("PLC Device Supporting Demand Reset"),
+    RFN_POINT_CALCULATION("RFN Point Calculation"), // This may need to be split into one tag per point and/or used for plc devices later
+    ;
 
-	private final String description;
+    private final String description;
     private final InputType<?> valueType;
-	
-	PaoTag(String description) {
-		this(description, null);
-	}
-	
-	PaoTag(String description, InputType<?> valueType) {
-	    this.description = description;
+    
+    PaoTag(String description) {
+        this(description, null);
+    }
+    
+    PaoTag(String description, InputType<?> valueType) {
+        this.description = description;
         this.valueType = valueType;
-	}
-	
-	public String getDescription() {
-		return description;
-	}
-	
-	public String getName() {
-		return this.name();
-	}
-	
-	public boolean isTagHasValue() {
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public String getName() {
+        return this.name();
+    }
+    
+    public boolean isTagHasValue() {
         return valueType != null;
     }
-	
-	public InputType<?> getValueType() {
+    
+    public InputType<?> getValueType() {
         return valueType;
     }
+    
 }
