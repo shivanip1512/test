97f3bae3fe9e7054efbdeaed5542b65973a642be
Ogaard, Jason <>
2014-03-26 15:43:10 +0000

    YUK-13160 Add support for RF DA nodes in Service Manager, DB Editor, and Cap Control -new classes to handle request and response

---
+++ yukon-client/common/src/com/cannontech/dr/rfn/message/archive/RfDaArchiveResponse.java
@@ -0,0 +1,53 @@
+package com.cannontech.dr.rfn.message.archive;
+
+import java.io.Serializable;
+
+/**
+ * JMS Queue name: yukon.qr.obj.dr.rfn.LcrReadingArchiveResponse
+ */
+public class RfDaArchiveResponse implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private long sensorId;
+
+    public long getSensorId() {
+        return sensorId;
+    }
+
+    public void setSensorId(long sensorId) {
+        this.sensorId = sensorId;
+    }
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (sensorId ^ (sensorId >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        RfDaArchiveResponse other = (RfDaArchiveResponse) obj;
+        if (sensorId != other.sensorId)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("RfDaArchiveResponse [sensorId=%s]", sensorId);
+    }
+
+}
