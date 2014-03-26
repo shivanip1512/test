06115622a63f98ed1e38bd69550b3760a9ee2441
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2014-03-26 16:07:22 +0000

    YUK-13189 RFN Broadcast emails are not being sent to all notifications within a notificationGroup (short term solution)  - Fixed the query that returns email addresses for RFNBroadcast report to more closely follow how notification server handles notification group.  - Now includes Customers, direct Notifications, and additional contacts.

--- yukon-client/common/src/com/cannontech/core/dao/NotificationGroupDao.java
+++ yukon-client/common/src/com/cannontech/core/dao/NotificationGroupDao.java
@@ -6,6 +6,15 @@ import java.util.Set;
 import com.cannontech.database.data.lite.LiteNotificationGroup;
 
 public interface NotificationGroupDao {
+
+    /**
+     * Returns a list of email addresses to be sent to for notificationGroup.
+     * Must be active (enabled) email type notifications.
+     * This method should be deleted when YUK-13186 is fully corrected.
+     * And callers should utilize the Notification Server messaging instead.
+     * @deprecated
+     */
+    @Deprecated
     List<String> getContactNoficationEmails(LiteNotificationGroup notificationGroup);
 
     LiteNotificationGroup getLiteNotificationGroup(int groupId);
--- yukon-client/common/src/com/cannontech/core/dao/impl/NotificationGroupDaoImpl.java
+++ yukon-client/common/src/com/cannontech/core/dao/impl/NotificationGroupDaoImpl.java
@@ -7,9 +7,11 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.cannontech.common.model.ContactNotificationType;
+import com.cannontech.common.util.CtiUtilities;
 import com.cannontech.common.util.SqlStatementBuilder;
 import com.cannontech.core.dao.NotificationGroupDao;
 import com.cannontech.database.RowMapper;
+import com.cannontech.database.YNBoolean;
 import com.cannontech.database.YukonJdbcTemplate;
 import com.cannontech.database.data.lite.LiteNotificationGroup;
 import com.cannontech.yukon.IDatabaseCache;
@@ -21,13 +23,40 @@ public final class NotificationGroupDaoImpl implements NotificationGroupDao {
     @Override
     public List<String> getContactNoficationEmails(LiteNotificationGroup notificationGroup) {
         SqlStatementBuilder sql = new SqlStatementBuilder();
-        sql.append("SELECT CN.Notification");
-        sql.append("FROM NotificationGroup NG");
-        sql.append("   JOIN ContactNotifGroupMap GM ON NG.NotificationGroupID = GM.NotificationGroupID");
-        sql.append("   JOIN ContactNotification CN ON GM.ContactID = CN.ContactID");
-        sql.append("WHERE NG.NotificationGroupID").eq(notificationGroup.getNotificationGroupID());
-        sql.append("   AND CN.NotificationCategoryID").eq_k(ContactNotificationType.EMAIL);
-        
+        sql.append("SELECT Notification FROM (");
+        sql.append(    "(SELECT ContactNotifId, ContactId, Notification, Attribs");
+        sql.append(        "FROM NotificationDestination ND");
+        sql.append(        "JOIN ContactNotification CN ON ND.RecipientID = CN.ContactNotifID");
+        sql.append(        "WHERE ND.NotificationGroupID").eq(notificationGroup.getNotificationGroupID());
+        sql.append(        "AND CN.NotificationCategoryID").eq_k(ContactNotificationType.EMAIL);
+        sql.append(        "AND DisableFlag").eq_k(YNBoolean.NO);
+        sql.append(    ")");
+        sql.append("UNION");
+        sql.append(    "(SELECT CN.ContactNotifID, CN.ContactID, Notification, Attribs FROM (");
+        sql.append(        "(SELECT ContNGM.ContactID, ContNGM.NotificationGroupID, Attribs");
+        sql.append(            "FROM ContactNotifGroupMap ContNGM");
+        sql.append(            "WHERE ContNGM.NotificationGroupID").eq(notificationGroup.getNotificationGroupID());
+        sql.append(            ")");
+        sql.append(        "UNION (SELECT C.PrimaryContactID, CustNGM.NotificationGroupID, Attribs");
+        sql.append(            "FROM CustomerNotifGroupMap CustNGM");
+        sql.append(            "JOIN Customer C ON C.customerid = CustNGM.CustomerID");
+        sql.append(            "WHERE PrimaryContactID").gt(CtiUtilities.NONE_ZERO_ID);
+        sql.append(            "AND CustNGM.NotificationGroupID").eq(notificationGroup.getNotificationGroupID());
+        sql.append(            ")");
+        sql.append(        "UNION (SELECT CAC.ContactID, CustNGM.NotificationGroupID, Attribs");
+        sql.append(            "FROM CustomerNotifGroupMap CustNGM");
+        sql.append(            "JOIN Customer C ON C.customerid = CustNGM.CustomerID");
+        sql.append(            "JOIN CustomerAdditionalContact cac on cac.CustomerID = c.CustomerID");
+        sql.append(            "WHERE CustNGM.NotificationGroupID").eq(notificationGroup.getNotificationGroupID());
+        sql.append(            ")");
+        sql.append(        ") AllC");
+        sql.append(    "JOIN ContactNotification CN on AllC.ContactID = CN.ContactID");
+        sql.append(    "WHERE DisableFlag").eq_k(YNBoolean.NO);
+        sql.append(    "AND NotificationCategoryID").eq_k(ContactNotificationType.EMAIL);
+        sql.append(    ")");
+        sql.append(") results");
+        sql.append("WHERE Attribs LIKE 'Y%'");   //the first char in Attribs represents sending emails.
+
         return jdbcTemplate.query(sql, RowMapper.STRING);
     }
     
