d43c9ac3e2a4d998a3e70ffd778bdb007973e10c
Woodward, Daniel <>
2014-03-27 12:02:06 +0000

    whitespace and formatting

--- yukon-client/common/src/com/cannontech/stars/energyCompany/dao/EnergyCompanyDao.java
+++ yukon-client/common/src/com/cannontech/stars/energyCompany/dao/EnergyCompanyDao.java
@@ -10,68 +10,67 @@ import com.cannontech.stars.energyCompany.dao.impl.EnergyCompanyDaoImpl.Displaya
 
 public interface EnergyCompanyDao {
 
-	/**
-	 * Duplicate of StarsDatabaseCache.DEFAULT_ENERGY_COMPANY_ID
-	 */
-    public static final int DEFAULT_ENERGY_COMPANY_ID = -1;
-    
+    /**
+     * Duplicate of StarsDatabaseCache.DEFAULT_ENERGY_COMPANY_ID
+     */
+    static final int DEFAULT_ENERGY_COMPANY_ID = -1;
+
     /**
      * Returns the LiteEnergyCompany with the given energy company id.
      */
-    public LiteEnergyCompany getEnergyCompany(int energyCompanyID);
+    LiteEnergyCompany getEnergyCompany(int energyCompanyID);
 
     /**
      * Return the LiteEnergyCompany this user belongs to.
      */
-    public LiteEnergyCompany getEnergyCompany(LiteYukonUser user);
+    LiteEnergyCompany getEnergyCompany(LiteYukonUser user);
 
     /**
      * Return the LiteEnergyCompany looking up by energy company name.
-     * 
+     *
      * @throws NotFoundException
      */
-    public LiteEnergyCompany getEnergyCompanyByName(String energyCompanyName);
-    
+    LiteEnergyCompany getEnergyCompanyByName(String energyCompanyName);
+
     /**
      * Return the LiteEnergyCompany looking up by energy company name.
-     * 
+     *
      * @return LiteEnergyCompany or null if not found
      */
-    public LiteEnergyCompany findEnergyCompanyByName(String energyCompanyName);
+    LiteEnergyCompany findEnergyCompanyByName(String energyCompanyName);
 
     /**
      * Returns all the LiteEnergyCompany's that have customerID_ in it.
      */
-    public LiteEnergyCompany[] getEnergyCompaniesByCustomer(int customerID_);
+    LiteEnergyCompany[] getEnergyCompaniesByCustomer(int customerID_);
 
     /**
      * Returns the LiteYukonUser associated with this energy company.
      */
-    public LiteYukonUser getEnergyCompanyUser(LiteEnergyCompany company);
+    LiteYukonUser getEnergyCompanyUser(LiteEnergyCompany company);
 
     /**
      * Returns the LiteYukonUser associated with this energy company.
      */
-    public LiteYukonUser getEnergyCompanyUser(int energyCompanyID);
+    LiteYukonUser getEnergyCompanyUser(int energyCompanyID);
 
     /**
      * Adds a customer and energy company to EnergyCompanyCustomerList
      */
-    public void addEnergyCompanyCustomerListEntry(int customerId, int ecId);
+    void addEnergyCompanyCustomerListEntry(int customerId, int ecId);
 
-    public List<DisplayableServiceCompany> getAllInheritedServiceCompanies(int energyCompanyId);
+    List<DisplayableServiceCompany> getAllInheritedServiceCompanies(int energyCompanyId);
 
-    public List<Integer> getParentEnergyCompanyIds(int energyCompanyId);
+    List<Integer> getParentEnergyCompanyIds(int energyCompanyId);
 
-    public List<LiteEnergyCompany> getAllEnergyCompanies();
+    List<LiteEnergyCompany> getAllEnergyCompanies();
 
-    public void updateCompanyName(String name, int energyCompanyId);
-    
+    void updateCompanyName(String name, int energyCompanyId);
+
     /**
      * Creates or updates energy company.
      */
-    public void save(EnergyCompany energyCompany);
+    void save(EnergyCompany energyCompany);
 
-    public String retrieveCompanyName(int energyCompanyId);
-    
+    String retrieveCompanyName(int energyCompanyId);
 }
