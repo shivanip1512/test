4c713043830a043af0287f7539bf716c7db2535c
Stenersen, Ross <>
2014-03-27 12:59:29 +0000

    formatting

--- yukon-client/yukon-web/src/com/cannontech/web/taglib/RoleProperty.java
+++ yukon-client/yukon-web/src/com/cannontech/web/taglib/RoleProperty.java
@@ -1,6 +1,5 @@
 package com.cannontech.web.taglib;
 
-
 import javax.servlet.jsp.JspException;
 
 import org.springframework.web.util.ExpressionEvaluationUtils;
@@ -11,151 +10,112 @@ import com.cannontech.core.roleproperties.dao.RolePropertyDao;
 import com.cannontech.database.data.lite.LiteYukonUser;
 import com.cannontech.spring.YukonSpringHook;
 import com.cannontech.util.ServletUtil;
+
 /**
  * Writes out the value of a role property for the current user.
- * Creation date: (11/13/2001 4:30:35 PM)
- * @author: alauinger
  */
 public class RoleProperty extends javax.servlet.jsp.tagext.BodyTagSupport {
-
-	private int propertyid;
+    private int propertyid;
     private boolean useId = true;
     private String property;
     private String defaultvalue = "";
-	public String format = null;
-	public String var = null;
-	
-/**
- * TextTag constructor comment.
- */
-public RoleProperty() {
-	super();
-}
-/**
- * Creation date: (11/13/2001 4:36:50 PM)
- * @return int
- * @exception javax.servlet.jsp.JspException The exception description.
- */
-public int doEndTag() throws javax.servlet.jsp.JspException {
-    property = "";
-    propertyid = 0;
-    defaultvalue = "";
-    format = "";
-    return EVAL_PAGE;
-}
-/**
- * Creation date: (11/13/2001 4:35:26 PM)
- * @return int
- * @exception javax.servlet.jsp.JspException The exception description.
- */
-public int doStartTag() throws JspException {
-	try {
-		LiteYukonUser user = (LiteYukonUser) pageContext.getSession().getAttribute("YUKON_USER");
-		if(user != null) {
-            int propId;
-            if (useId) {
-                propId = propertyid;
-            } else {
-                propId = YukonRoleProperty.valueOf(property).getPropertyId();
+    public String format = null;
+    public String var = null;
+
+    @Override
+    public int doEndTag() throws JspException {
+        property = "";
+        propertyid = 0;
+        defaultvalue = "";
+        format = "";
+        return EVAL_PAGE;
+    }
+
+    @Override
+    public int doStartTag() throws JspException {
+        try {
+            LiteYukonUser user = (LiteYukonUser) pageContext.getSession().getAttribute("YUKON_USER");
+            if (user != null) {
+                int propId;
+                if (useId) {
+                    propId = propertyid;
+                } else {
+                    propId = YukonRoleProperty.valueOf(property).getPropertyId();
+                }
+                String text =
+                    YukonSpringHook.getBean(RolePropertyDao.class).getPropertyStringValue(
+                        YukonRoleProperty.getForId(propId), user);
+                String fmat = getFormat();
+                if (fmat != null) {
+                    if (fmat.equalsIgnoreCase(ServletUtil.FORMAT_UPPER)) {
+                        text = text.toUpperCase();
+                    } else if (fmat.equalsIgnoreCase(ServletUtil.FORMAT_LOWER)) {
+                        text = text.toLowerCase();
+                    } else if (fmat.equalsIgnoreCase(ServletUtil.FORMAT_CAPITAL)) {
+                        text = ServletUtil.capitalize(text);
+                    } else if (fmat.equalsIgnoreCase(ServletUtil.FORMAT_ALL_CAPITAL)) {
+                        text = ServletUtil.capitalizeAll(text);
+                    } else if (fmat.equalsIgnoreCase(ServletUtil.FORMAT_ADD_ARTICLE)) {
+                        text = ServletUtil.addArticle(text);
+                    }
+                }
+
+                // Expose as variable, if demanded, else write to the page.
+                String resolvedVar = ExpressionEvaluationUtils.evaluateString("var", this.var, pageContext);
+                if (resolvedVar != null) {
+                    pageContext.setAttribute(resolvedVar, text);
+                } else {
+                    pageContext.getOut().write(text);
+                }
             }
-			String text = YukonSpringHook.getBean(RolePropertyDao.class).getPropertyStringValue(YukonRoleProperty.getForId(propId), user);
-			String fmat = getFormat();
-			if (fmat != null) {
-				if (fmat.equalsIgnoreCase( ServletUtil.FORMAT_UPPER ))
-					text = text.toUpperCase();
-				else if (fmat.equalsIgnoreCase( ServletUtil.FORMAT_LOWER ))
-					text = text.toLowerCase();
-				else if (fmat.equalsIgnoreCase( ServletUtil.FORMAT_CAPITAL ))
-					text = ServletUtil.capitalize( text );
-				else if (fmat.equalsIgnoreCase( ServletUtil.FORMAT_ALL_CAPITAL ))
-					text = ServletUtil.capitalizeAll( text );
-				else if (fmat.equalsIgnoreCase( ServletUtil.FORMAT_ADD_ARTICLE ))
-					text = ServletUtil.addArticle( text );
-			}
-			
-			// Expose as variable, if demanded, else write to the page.
-			String resolvedVar = ExpressionEvaluationUtils.evaluateString("var", this.var, pageContext);
-			if (resolvedVar != null) {
-				pageContext.setAttribute(resolvedVar, text);
-			} else {
-				pageContext.getOut().write(text);
-			}
-		}
-	}
-	catch(java.io.IOException e )
-	{
-		throw new JspException(e.getMessage());
-	}
-	catch (UserNotInRoleException e)
-	{
-	    throw new JspException(e.getMessage());
-	}
+        } catch (java.io.IOException e) {
+            throw new JspException(e.getMessage());
+        } catch (UserNotInRoleException e) {
+            throw new JspException(e.getMessage());
+        }
 
-	return SKIP_BODY;
-}
-	
-	/**
-	 * Returns the propertyid.
-	 * @return int
-	 */
-	public int getPropertyid() {
-		return propertyid;
-	}
+        return SKIP_BODY;
+    }
 
-	/**
-	 * Sets the propertyid.
-	 * @param propertyid The propertyid to set
-	 */
-	public void setPropertyid(int propertyid) {
-		this.propertyid = propertyid;
+    public int getPropertyid() {
+        return propertyid;
+    }
+
+    public void setPropertyid(int propertyid) {
+        this.propertyid = propertyid;
         useId = true;
-	}
+    }
 
-	/**
-	 * Returns the format.
-	 * @return String
-	 */
-	public String getFormat() {
-		return format;
-	}
+    public String getFormat() {
+        return format;
+    }
 
-	/**
-	 * Sets the format.
-	 * @param format The format to set
-	 */
-	public void setFormat(String format) {
-		this.format = format;
-	}
+    public void setFormat(String format) {
+        this.format = format;
+    }
 
-	/**
-	 * @return
-	 */
-	public String getDefaultvalue() {
-		return defaultvalue;
-	}
+    public String getDefaultvalue() {
+        return defaultvalue;
+    }
 
-	/**
-	 * @param string
-	 */
-	public void setDefaultvalue(String string) {
-		defaultvalue = string;
-	}
-    
+    public void setDefaultvalue(String string) {
+        defaultvalue = string;
+    }
+
     public String getProperty() {
         return property;
     }
-    
+
     public void setProperty(String property) {
         this.property = property;
         useId = false;
     }
-    
-    public void setVar(String var) {
-		this.var = var;
-	}
-    
-    public String getVar() {
-		return var;
-	}
 
+    public void setVar(String var) {
+        this.var = var;
+    }
+
+    public String getVar() {
+        return var;
+    }
 }
