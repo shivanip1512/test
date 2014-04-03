1251c0ac2e26d95913457ba1c227ae7f1f063487
Woodward, Daniel <>
2014-04-03 19:41:12 +0000

    YUK-11813 Move StarsDatabaseCache from common to yukon-web
    fix mmissing  '?' in INVALID_URI

--- yukon-client/common/src/com/cannontech/common/constants/LoginController.java
+++ yukon-client/common/src/com/cannontech/common/constants/LoginController.java
@@ -5,33 +5,33 @@ package com.cannontech.common.constants;
 
 import com.cannontech.util.ServletUtil;
 
-/**	
+/**
  * Constants for the parameters passed to com.cannontech.servlet.LoginController
- * 
+ *
  * LoginController takes a username/password pair and attempts to find a matching
- * Yukon user in the database.  What it does after that depends on the ACTION 
+ * Yukon user in the database.  What it does after that depends on the ACTION
  * passed to it.
- * 
+ *
  * required parameters:
- * USERNAME 	
+ * USERNAME
  * PASSWORD
  * ACTION
- * 
+ *
  * One of the following actions is required:
  * LOGIN
  * LOGOUT
  * RETRIEVE_PROPERTIES
- * 
+ *
  * Odd Ducks:
  * REDIRECT - When used with a LOGIN, specifies where to redirect the brower
  * if login failed. When used with a LOGOUT, indicates where to redirect the
  * browser after logging out.
- * 
+ *
  * SAVE_CURRENT_USER - Only has an effect on a LOGIN, when specified, the
  * old session context will be saved under the SAVED_YUKON_USERS attribute
  * of the new session context. When the new user logs off, the old session
  * context will be restored.
- * 
+ *
  * @author aaron
   */
 public interface LoginController {
@@ -40,18 +40,18 @@ public interface LoginController {
 	public static final String PASSWORD = "PASSWORD";
 	public static final String REDIRECT = ServletUtil.ATT_REDIRECT;
 	public static final String TOKEN = "TOKEN";
-	
+
 	public static final String YUKON_USER = ServletUtil.ATT_YUKON_USER;
 	public static final String SAVED_YUKON_USERS = "SAVED_YUKON_USERS";
-	
+
 	public static final String FAILED_LOGIN_PARAM = "failed=true";
 	public static final String INVALID_URL_ACCESS_PARAM = "invalid=true";
     public static final String AUTH_FAILED_PARAM = "failed";
     public static final String AUTH_RETRY_SECONDS_PARAM = "retrySeconds";
-	
+
 	public static final String LOGIN_URL = "/login.jsp";
-    public static final String INVALID_URI = LOGIN_URL + FAILED_LOGIN_PARAM;
+    public static final String INVALID_URI = "/login.jsp?" + FAILED_LOGIN_PARAM;
 	public static final String REMEMBER_ME_COOKIE = "REMEMBER_ME_COOKIE";
     public static final String REDIRECTED_FROM = "REDIRECTED_FROM";
-    
+
 }
