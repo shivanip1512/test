c3f5ed1c0455a4ca7e2dcef0ac7723dc1599d0b6
Stenersen, Ross <>
2014-04-04 13:11:31 +0000

    YUK-11813 Move StarsDatabaseCache from common to yukon-web
    Added new parent and children objects to EnergyCompany class.

--- yukon-client/common/src/com/cannontech/stars/energyCompany/model/EnergyCompany.java
+++ yukon-client/common/src/com/cannontech/stars/energyCompany/model/EnergyCompany.java
@@ -1,22 +1,84 @@
 package com.cannontech.stars.energyCompany.model;
 
+import static com.google.common.base.Preconditions.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.cannontech.database.data.lite.LiteYukonUser;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 
-
+/**
+ * An immutable class representing an energy company.  This class can only be built using the builder and all
+ * energy companies must be created at the same time.
+ */
 public final class EnergyCompany implements YukonEnergyCompany {
-
     private final int ecId;
     private final String name;
     private final LiteYukonUser user;
     private final int contactId;
-    
+
+    private EnergyCompany parent;
+    private List<EnergyCompany> children;
+
+    /**
+     * Dan:  Make this private and remove the @Deprecated marker.
+     */
+    @Deprecated
     public EnergyCompany(int ecId, String name, LiteYukonUser user, int contactId) {
         this.ecId = ecId;
         this.name = name;
         this.user = user;
         this.contactId = contactId;
+
+        // Create a temporary mutable list for children.  The builder will make this immutable before making it
+        // available outside this class.
+        children = new ArrayList<>();
     }
-    
+
+    /**
+     * A builder class to create a hierarchy of EnergyCompany instances.  This class is the ONLY way to create
+     * energy companies and they should all be created at the same time.  (This should of course be only done in
+     * a single DAO that caches them.)
+     */
+    public static class Builder {
+        private final Map<Integer, EnergyCompany> temporaryMutableMap = new HashMap<>();
+        private final Map<Integer, Integer> parentIdsById = new HashMap<>();
+
+        private boolean built = false;
+
+        public void addEnergyCompany(int ecId, String name, LiteYukonUser user, int contactId, Integer parentEcId) {
+            checkState(!built, "Cannot add more energy companies.");
+            temporaryMutableMap.put(ecId, new EnergyCompany(ecId, name, user, contactId));
+            if (parentEcId != null) {
+                parentIdsById.put(ecId, parentEcId);
+            }
+        }
+
+        public Map<Integer, EnergyCompany> build() {
+            checkState(!built, "Cannot add build more than once.");
+            built = true;
+
+            // Build parent/child relationships.
+            for (Map.Entry<Integer, Integer> entry : parentIdsById.entrySet()) {
+                EnergyCompany ec = temporaryMutableMap.get(entry.getKey());
+                EnergyCompany parent = temporaryMutableMap.get(entry.getValue());
+                ec.parent = parent;
+                parent.children.add(ec);
+            }
+
+            // Make the children lists immutable.
+            for (EnergyCompany ec : temporaryMutableMap.values()) {
+                ec.children = ImmutableList.copyOf(ec.children);
+            }
+
+            return ImmutableMap.copyOf(temporaryMutableMap);
+        }
+    }
+
     @Override
     public int getEnergyCompanyId() {
         return ecId;
@@ -34,5 +96,26 @@ public final class EnergyCompany implements YukonEnergyCompany {
 
     public int getContactId() {
         return contactId;
+    }
+
+    public EnergyCompany getParent() {
+        return parent;
+    }
+
+    public List<EnergyCompany> getChildren() {
+        return children;
+    }
+
+    /**
+     * Get all descendants of this energy company (children, children of children, etc.).  This list
+     * does NOT include the energy company itself.
+     */
+    public List<EnergyCompany> getDescendants() {
+        ImmutableList.Builder<EnergyCompany> builder = ImmutableList.builder();
+        for (EnergyCompany child : children) {
+            builder.add(child);
+            builder.addAll(child.getChildren());
+        }
+        return builder.build();
     }
 }
---
+++ yukon-client/common/test/unit/com/cannontech/stars/energyCompany/model/EnergyCompanyTest.java
@@ -0,0 +1,120 @@
+package com.cannontech.stars.energyCompany.model;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.cannontech.database.data.lite.LiteYukonUser;
+import com.cannontech.stars.energyCompany.model.EnergyCompany.Builder;
+import com.google.common.collect.ImmutableList;
+
+public class EnergyCompanyTest {
+    private final static Object[][] sampleEnergyCompaniesInfo = new Object[][] {
+        // ecId, name, user, contactId, parentEcId
+        {1, "Middle Earth Power & Light", "mop", 100, null},
+        {2, "Moria Gas & Electric", "mgop", 101, 1},
+        {3, "Shire Power", "spop", 102, 1},
+        {4, "Western Regions Electric", "wreop", 103, 1},
+        {5, "North Shire Loop", "nsop", 104, 3},
+        {6, "Western Territories", "wtop", 105, 3},
+        {7, "Durin Took Gardens", "top", 106, 3},
+        {8, "Some Random Gas & Electric", "srgeop", 200, null},
+        {9, "Another Random Power Co.", "arpop", 200, null},
+        {100, "We Pretend Power & Light", "wpop", 300, null},
+        {101, "Don't You Dare Pizza!", "pizza", 301, 100},
+    };
+
+    private void basicTests(EnergyCompany ec, String name, int numberOfChildrenExpected, EnergyCompany parent) {
+        assertEquals("Wrong name for ecId " + ec.getEnergyCompanyId() + ".", name, ec.getName());
+        assertEquals("Incorrect number of children of " + name + " EnergyCompany.",
+            numberOfChildrenExpected, ec.getChildren().size());
+
+        // The parent should be the exact same object as the one in the map (not just equivalent).
+        assertTrue(parent == ec.getParent());
+    }
+
+    private void verifyCorrectChildren(EnergyCompany ec, EnergyCompany... children) {
+        assertTrue(ec.getChildren().containsAll(ImmutableList.copyOf(children)));
+    }
+
+    private void verifyCorrectDescendants(EnergyCompany ec, EnergyCompany... descendants) {
+        assertTrue(ec.getDescendants().containsAll(ImmutableList.copyOf(descendants)));
+    }
+
+    @Test
+    public void test() {
+        Builder builder = new EnergyCompany.Builder();
+        int userId = 7000;
+        for (Object[] sampleEcInfo : sampleEnergyCompaniesInfo) {
+            int ecId = (Integer) sampleEcInfo[0];
+            String name = (String) sampleEcInfo[1];
+            String userName = (String) sampleEcInfo[2];
+            int contactId = (Integer) sampleEcInfo[3];
+            Integer parentEcId = (Integer) sampleEcInfo[4];
+
+            builder.addEnergyCompany(ecId, name, new LiteYukonUser(++userId, userName), contactId, parentEcId);
+        }
+        Map<Integer, EnergyCompany> energyCompanies = builder.build();
+
+        assertEquals("Incorrect number of EnergyCompany objects in map.",
+            sampleEnergyCompaniesInfo.length, energyCompanies.size());
+
+        EnergyCompany mepl = energyCompanies.get(1);
+        basicTests(mepl, "Middle Earth Power & Light", 3, null);
+
+        EnergyCompany moria = energyCompanies.get(2);
+        basicTests(moria, "Moria Gas & Electric", 0, mepl);
+
+        EnergyCompany shire = energyCompanies.get(3);
+        basicTests(shire, "Shire Power", 3, mepl);
+
+        EnergyCompany westernRegionsElectric = energyCompanies.get(4);
+        basicTests(westernRegionsElectric, "Western Regions Electric", 0, mepl);
+
+        EnergyCompany northShireLoop = energyCompanies.get(5);
+        basicTests(northShireLoop, "North Shire Loop", 0, shire);
+
+        EnergyCompany westernTerritoriesOfShire = energyCompanies.get(6);
+        basicTests(westernTerritoriesOfShire, "Western Territories", 0, shire);
+
+        EnergyCompany durinTookGardens = energyCompanies.get(7);
+        basicTests(durinTookGardens, "Durin Took Gardens", 0, shire);
+
+        EnergyCompany someRandom = energyCompanies.get(8);
+        basicTests(someRandom, "Some Random Gas & Electric", 0, null);
+
+        EnergyCompany anotherRandom = energyCompanies.get(9);
+        basicTests(anotherRandom, "Another Random Power Co.", 0, null);
+
+        EnergyCompany wePretend = energyCompanies.get(100);
+        basicTests(wePretend, "We Pretend Power & Light", 1, null);
+
+        EnergyCompany pizza = energyCompanies.get(101);
+        basicTests(pizza, "Don't You Dare Pizza!", 0, wePretend);
+
+        verifyCorrectChildren(mepl, moria, shire, westernRegionsElectric);
+        verifyCorrectChildren(shire, northShireLoop, westernTerritoriesOfShire, durinTookGardens);
+        verifyCorrectChildren(wePretend, pizza);
+
+        verifyCorrectDescendants(mepl,
+            moria, shire, westernRegionsElectric, northShireLoop, westernTerritoriesOfShire, durinTookGardens);
+        verifyCorrectDescendants(shire, northShireLoop, westernTerritoriesOfShire, durinTookGardens);
+        verifyCorrectDescendants(wePretend, pizza);
+
+        try {
+            mepl.getChildren().add(pizza);
+            fail("should throw exception");
+        } catch (UnsupportedOperationException e) {
+            // Cool beans...we aren't allowed to mess with the children list after the fact.
+        }
+
+        try {
+            mepl.getDescendants().add(pizza);
+            fail("should throw exception");
+        } catch (UnsupportedOperationException e) {
+            // Awesome...we can't mess with the descendant list after the fact.
+        }
+    }
+}
