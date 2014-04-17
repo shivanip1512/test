187806bc7602ea0e16f3e9f2fca2d1c2a03924fa
Solberg, Aaron <>
2014-04-17 20:06:58 +0000

    formatting

--- yukon-client/common/src/com/cannontech/common/pao/definition/model/PaoTypeIcon.java
+++ yukon-client/common/src/com/cannontech/common/pao/definition/model/PaoTypeIcon.java
@@ -1,23 +1,25 @@
 package com.cannontech.common.pao.definition.model;
 
 public enum PaoTypeIcon {
-	ELECTRIC_METER("icon-electric-meter"),
-	WATER_METER("icon-water-meter"),
-	GAS_METER("icon-gas-meter");
-	
-	private String icon;
-	private PaoTypeIcon(String icon) {
-		this.icon = icon;
-	}
-	
-	public String getIcon() {
-		return icon;
-	}
-	
-	public PaoTypeIcon getForName(String name) {
-		for (PaoTypeIcon icon : values()) {
-			if (name.equalsIgnoreCase(icon.getIcon())) return icon;
-		}
-		throw new IllegalArgumentException("No value found for name: " + name);
-	}
+    
+    ELECTRIC_METER("icon-electric-meter"),
+    WATER_METER("icon-water-meter"),
+    GAS_METER("icon-gas-meter");
+    
+    private String icon;
+    private PaoTypeIcon(String icon) {
+        this.icon = icon;
+    }
+    
+    public String getIcon() {
+        return icon;
+    }
+    
+    public PaoTypeIcon getForName(String name) {
+        for (PaoTypeIcon icon : values()) {
+            if (name.equalsIgnoreCase(icon.getIcon())) return icon;
+        }
+        throw new IllegalArgumentException("No value found for name: " + name);
+    }
+    
 }
