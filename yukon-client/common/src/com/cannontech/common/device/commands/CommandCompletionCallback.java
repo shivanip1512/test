be173d34508225ae9dcb8592e74b6d598b70ce3e
Stenersen, Ross <>
2014-04-23 13:11:47 +0000

    formatting

--- yukon-client/common/src/com/cannontech/common/device/commands/CommandCompletionCallback.java
+++ yukon-client/common/src/com/cannontech/common/device/commands/CommandCompletionCallback.java
@@ -4,39 +4,33 @@ import com.cannontech.amr.errors.model.SpecificDeviceErrorDescription;
 import com.cannontech.core.dynamic.PointValueHolder;
 
 public interface CommandCompletionCallback<T> {
-	
-	/**
-	 * Methods to keep track of received values and errors.
-	 */
-    public void receivedValue(T command, PointValueHolder value);
-    
-    public void receivedIntermediateError(T command, SpecificDeviceErrorDescription error);
-    
-    public void receivedLastError(T command, SpecificDeviceErrorDescription error);
-    
-    public void receivedIntermediateResultString(T command, String value);
-    
+    // Methods to keep track of received values and errors.
+    void receivedValue(T command, PointValueHolder value);
+
+    void receivedIntermediateError(T command, SpecificDeviceErrorDescription error);
+
+    void receivedLastError(T command, SpecificDeviceErrorDescription error);
+
+    void receivedIntermediateResultString(T command, String value);
+
     /**
-     * Either this or receivedLastError will get called for each command exactly once, 
+     * Either this or receivedLastError will get called for each command exactly once,
      * unless a processingExceptionOccured happens, in which case it may not get called at all.
-     * @param command
-     * @param value
      */
-    public void receivedLastResultString(T command, String value);
-    
+    void receivedLastResultString(T command, String value);
+
     /**
      * Called when after the process has completed. Guaranteed to be called exactly once.
      */
-    public void complete();
-    
+    void complete();
+
     /**
      * Called when a request is made to cancel the command(s).
      */
-    public void cancel();
-    
+    void cancel();
+
     /**
      * Called when an exception occurs during the processing. May get called multiple times before complete().
-     * @param reason
      */
-    public void processingExceptionOccured(String reason);
+    void processingExceptionOccured(String reason);
 }
