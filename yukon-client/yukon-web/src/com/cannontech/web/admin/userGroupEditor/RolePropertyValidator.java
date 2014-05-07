7911c1b8a7523fe3d44c0dc3db9505f00bf1f5e1
More, Shweta <>
2014-05-07 04:54:48 +0000

    YUK-12975:Review comments

--- yukon-client/yukon-web/src/com/cannontech/web/admin/userGroupEditor/RolePropertyValidator.java
+++ yukon-client/yukon-web/src/com/cannontech/web/admin/userGroupEditor/RolePropertyValidator.java
@@ -7,56 +7,42 @@ import com.cannontech.common.exception.BadConfigurationException;
 import com.cannontech.common.util.CtiUtilities;
 import com.cannontech.common.validator.SimpleValidator;
 import com.cannontech.common.validator.YukonValidationUtils;
-import com.cannontech.core.roleproperties.*;
+import com.cannontech.core.roleproperties.YukonRoleProperty;
 import com.cannontech.web.admin.userGroupEditor.RolePropertyController.GroupRolePropertyEditorBean;
 
-public class RolePropertyValidator extends
-		SimpleValidator<GroupRolePropertyEditorBean> {
-	private final static String baseKey = "yukon.web.modules.adminSetup.config.error.";
+public class RolePropertyValidator extends SimpleValidator<GroupRolePropertyEditorBean> {
+    private final static String baseKey = "yukon.web.modules.adminSetup.config.error.";
 
-	public RolePropertyValidator() {
-		super(GroupRolePropertyEditorBean.class);
-	}
+    public RolePropertyValidator() {
+        super(GroupRolePropertyEditorBean.class);
+    }
 
-	@Override
-	protected void doValidation(GroupRolePropertyEditorBean groupRolePropertyEditorBean, Errors errors) {
+    @Override
+    protected void doValidation(GroupRolePropertyEditorBean groupRolePropertyEditorBean, Errors errors) {
+        Map<YukonRoleProperty, Object> rolePropertyValuesMap = groupRolePropertyEditorBean.getValues();
 
-		Map<YukonRoleProperty, Object> rolePropertyValuesMap = groupRolePropertyEditorBean.getValues();
-
-		if (rolePropertyValuesMap.containsKey(YukonRoleProperty.DEFAULT_TIMEZONE)) {
-			String timeZoneId = (String) rolePropertyValuesMap.get(YukonRoleProperty.DEFAULT_TIMEZONE);
-			YukonValidationUtils.checkExceedsMaxLength(errors, "values[DEFAULT_TIMEZONE]", timeZoneId, 1000);
-			if (StringUtils.isNotBlank(timeZoneId)) {
-				try {
-					 CtiUtilities.getValidTimeZone(timeZoneId);
-				} catch (BadConfigurationException e) {
-					 errors.rejectValue("values[DEFAULT_TIMEZONE]", baseKey + "invalidTimeZone");
-				}
-			}
-		} 
-		if(rolePropertyValuesMap.containsKey(YukonRoleProperty.HOME_URL)){
-			String homeURl = (String) rolePropertyValuesMap.get(YukonRoleProperty.HOME_URL);
-			Boolean isValid = YukonValidationUtils.isUrlPath(homeURl);
-			if(!isValid)
-			{
-				errors.rejectValue("values[HOME_URL]", baseKey + "invalidURL");
-			}
-		}
-		if(rolePropertyValuesMap.containsKey(YukonRoleProperty.LOG_IN_URL)){
-			String loginURl = (String) rolePropertyValuesMap.get(YukonRoleProperty.LOG_IN_URL);
-			Boolean isValid = YukonValidationUtils.isUrlPath(loginURl);
-			if(!isValid)
-			{
-				errors.rejectValue("values[LOG_IN_URL]", baseKey + "invalidURL");
-			}
-		}
-	}
-	
-	  
-	}
-
-	
-		
-		
-	
-
+        if (rolePropertyValuesMap.containsKey(YukonRoleProperty.DEFAULT_TIMEZONE)) {
+            String timeZoneId = (String) rolePropertyValuesMap.get(YukonRoleProperty.DEFAULT_TIMEZONE);
+            YukonValidationUtils.checkExceedsMaxLength(errors, "values[DEFAULT_TIMEZONE]", timeZoneId, 1000);
+            if (StringUtils.isNotBlank(timeZoneId)) {
+                try {
+                    CtiUtilities.getValidTimeZone(timeZoneId);
+                } catch (BadConfigurationException e) {
+                    errors.rejectValue("values[DEFAULT_TIMEZONE]", baseKey + "invalidTimeZone");
+                }
+            }
+        }
+        if (rolePropertyValuesMap.containsKey(YukonRoleProperty.HOME_URL)) {
+            String homeUrl = (String) rolePropertyValuesMap.get(YukonRoleProperty.HOME_URL);
+            if (!YukonValidationUtils.isUrlPath(homeUrl)) {
+                errors.rejectValue("values[HOME_URL]", baseKey + "invalidURL");
+            }
+        }
+        if (rolePropertyValuesMap.containsKey(YukonRoleProperty.LOG_IN_URL)) {
+            String loginUrl = (String) rolePropertyValuesMap.get(YukonRoleProperty.LOG_IN_URL);
+            if (!YukonValidationUtils.isUrlPath(loginUrl)) {
+                errors.rejectValue("values[LOG_IN_URL]", baseKey + "invalidURL");
+            }
+        }
+    }
+}
