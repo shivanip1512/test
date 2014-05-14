9e2f7a773b9acea82c7575e3040ccf80ef040494
Stenersen, Ross <>
2014-05-14 20:34:28 +0000

    Reformatting/rearranging.

--- yukon-client/yukon-web/src/com/cannontech/web/taglib/StandardPageInfo.java
+++ yukon-client/yukon-web/src/com/cannontech/web/taglib/StandardPageInfo.java
@@ -1,13 +1,12 @@
 package com.cannontech.web.taglib;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import com.cannontech.web.PageEditMode;
-import com.google.common.collect.Maps;
 
 public class StandardPageInfo {
-
     private String breadCrumbs;
     private List<String> cssFiles;
     private String htmlLevel;
@@ -19,92 +18,92 @@ public class StandardPageInfo {
     private String title;
     private String pageName;
     private String servletPath;
-    private Map<String, String> layoutParts = Maps.newHashMap();
+    private Map<String, String> layoutParts = new HashMap<>();
 
     public String getBreadCrumbs() {
         return breadCrumbs;
-    }
-
-    public List<String> getCssFiles() {
-        return cssFiles;
-    }
-
-    public String getHtmlLevel() {
-        return htmlLevel;
-    }
-
-    public String getMenuSelection() {
-        return menuSelection;
-    }
-
-    public String getModuleName() {
-        return moduleName;
-    }
-
-    public PageEditMode getPageEditMode() {
-		return pageEditMode;
-	}
-    
-    public List<String> getScriptFiles() {
-        return scriptFiles;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public boolean isShowMenu() {
-        return showMenu;
-    }
-    
-    public String getPageName() {
-        return pageName;
-    }
-
-    public String getServletPath() {
-        return servletPath;
     }
 
     public void setBreadCrumbs(String breadCrumbs) {
         this.breadCrumbs = breadCrumbs;
     }
 
+    public List<String> getCssFiles() {
+        return cssFiles;
+    }
+
     public void setCssFiles(List<String> cssFiles) {
         this.cssFiles = cssFiles;
+    }
+
+    public String getHtmlLevel() {
+        return htmlLevel;
     }
 
     public void setHtmlLevel(String htmlLevel) {
         this.htmlLevel = htmlLevel;
     }
 
+    public String getMenuSelection() {
+        return menuSelection;
+    }
+
     public void setMenuSelection(String menuSelection) {
         this.menuSelection = menuSelection;
+    }
+
+    public String getModuleName() {
+        return moduleName;
     }
 
     public void setModuleName(String moduleName) {
         this.moduleName = moduleName;
     }
 
+    public PageEditMode getPageEditMode() {
+        return pageEditMode;
+    }
+
     public void setPageEditMode(PageEditMode pageEditMode) {
-		this.pageEditMode = pageEditMode;
-	}
-    
+        this.pageEditMode = pageEditMode;
+    }
+
+    public List<String> getScriptFiles() {
+        return scriptFiles;
+    }
+
     public void setScriptFiles(List<String> scriptFiles) {
         this.scriptFiles = scriptFiles;
+    }
+
+    public boolean isShowMenu() {
+        return showMenu;
     }
 
     public void setShowMenu(boolean showMenu) {
         this.showMenu = showMenu;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getPageName() {
+        return pageName;
     }
 
     public void setPageName(String pageName) {
         this.pageName = pageName;
     }
-    
+
+    public String getServletPath() {
+        return servletPath;
+    }
+
     public void setServletPath(String servletPath) {
         this.servletPath = servletPath;
     }
@@ -112,7 +111,7 @@ public class StandardPageInfo {
     public Map<String, String> getLayoutParts() {
         return layoutParts;
     }
-    
+
     public void addLayoutPart(String name, String contents) {
         layoutParts.put(name, contents);
     }
