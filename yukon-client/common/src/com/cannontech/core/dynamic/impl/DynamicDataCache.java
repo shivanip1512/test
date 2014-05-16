b0f83d258fefdb9e59cf735691f298f65a925300
Stenersen, Ross <>
2014-05-16 17:00:50 +0000

    YUK-13338 Yukon has multiple threads named "RfnConverter: 1" (and "RfnConverter: 2", etc.)
    Formatting cleanup.

--- yukon-client/common/src/com/cannontech/core/dynamic/impl/DynamicDataCache.java
+++ yukon-client/common/src/com/cannontech/core/dynamic/impl/DynamicDataCache.java
@@ -26,34 +26,30 @@ import com.cannontech.yukon.IServerConnection;
  * holding onto stale data.
  */
 /* package */ class DynamicDataCache implements MessageListener {
+    // Stores current PointData messages by PointID
+    private Map<Integer, PointData> pointData = new ConcurrentHashMap<Integer, PointData>();
+
+    // Stores current Signal messages by PointID
+    // Only signals with a category > 1 will be stored
+    private Map<Integer, Set<Signal>> pointSignals = new ConcurrentHashMap<Integer, Set<Signal>>();
+
+    // Stores the current Signal message by Alarm Category ID
+    private Map<Integer, Set<Signal>> categorySignalsMap = new ConcurrentHashMap<Integer, Set<Signal>>();
 
     @Autowired
     public DynamicDataCache(@Qualifier("dispatch") IServerConnection dispatchConnection) {
         dispatchConnection.addMessageListener(this);
     }
 
-    //  Stores current PointData messages by PointID
-    private Map<Integer, PointData> pointData = 
-        new ConcurrentHashMap<Integer, PointData>();
-    
-    // Stores current Signal messages by PointID
-    // Only signals with a category > 1 will be stored
-    private Map<Integer, Set<Signal>> pointSignals = 
-        new ConcurrentHashMap<Integer, Set<Signal>>();
-    
-    // Stores the current Signal message by Alarm Category ID
-    private Map<Integer, Set<Signal>> categorySignalsMap = 
-        new ConcurrentHashMap<Integer, Set<Signal>>();
-    
     PointData getPointData(int pointId) {
         return pointData.get(pointId);
     }
-    
+
     Set<Signal> getSignals(int pointId) {
         Set<Signal> ret = pointSignals.get(pointId);
         return ret;
     }
-    
+
     Set<Signal> getSignalForCategory(int categoryId) {
         return categorySignalsMap.get(categoryId);
     }
@@ -63,77 +59,74 @@ import com.cannontech.yukon.IServerConnection;
         handleIncoming(e.getMessage());
     }
 
-    void handleIncoming(Object msg) {        
-        if(msg instanceof PointData) {
-            handlePointData((PointData)msg);
-        }
-        else if(msg instanceof Signal) {
-            handleSignal((Signal)msg);
-        }
-        else if(msg instanceof ServerResponseMsg) {
-            handleIncoming(((ServerResponseMsg)msg).getPayload());
-        }
-        else if(msg instanceof Multi) {
+    void handleIncoming(Object msg) {
+        if (msg instanceof PointData) {
+            handlePointData((PointData) msg);
+        } else if (msg instanceof Signal) {
+            handleSignal((Signal) msg);
+        } else if (msg instanceof ServerResponseMsg) {
+            handleIncoming(((ServerResponseMsg) msg).getPayload());
+        } else if (msg instanceof Multi) {
             for (Object obj : ((Multi<?>) msg).getVector()) {
                 handleIncoming(obj);
             }
-        }
-        else if(msg instanceof ConnStateChange) {
+        } else if (msg instanceof ConnStateChange) {
             ConnStateChange csc = (ConnStateChange) msg;
-            if(!csc.isConnected()) {
+            if (!csc.isConnected()) {
                 releaseCache();
             }
         }
     }
-    
+
     private void handlePointData(PointData pd) {
         pointData.put(pd.getId(), pd);
     }
-    
+
     public void handleSignals(Set<Signal> signals, int pointId) {
-        if(signals.isEmpty()) {
+        if (signals.isEmpty()) {
             Set<Signal> pSignals = new HashSet<Signal>();
             pointSignals.put(pointId, pSignals);
-        }else {
-            for(Signal signal : signals) {
+        } else {
+            for (Signal signal : signals) {
                 handleSignal(signal);
             }
         }
     }
-    
+
     private void handleSignal(Signal signal) {
         PointData pointData = getPointData(signal.getPointID());
-        if(pointData != null){
-            pointData.setTags((signal.getTags() & ~Signal.MASK_ANY_ALARM) | (pointData.getTags() & Signal.MASK_ANY_ALARM));
+        if (pointData != null) {
+            pointData.setTags((signal.getTags() & ~Signal.MASK_ANY_ALARM)
+                | (pointData.getTags() & Signal.MASK_ANY_ALARM));
         }
         int pointId = signal.getPointID();
-        int categoryId = (int)signal.getCategoryID();
-        
+        int categoryId = (int) signal.getCategoryID();
+
         Set<Signal> pSignals = pointSignals.get(pointId);
-        if(pSignals == null) {
+        if (pSignals == null) {
             pSignals = new HashSet<Signal>();
             pointSignals.put(pointId, pSignals);
         }
         Set<Signal> cSignals = categorySignalsMap.get(categoryId);
-        if(cSignals == null) {
+        if (cSignals == null) {
             cSignals = new HashSet<Signal>();
             categorySignalsMap.put(categoryId, cSignals);
         }
-        
+
         pSignals.remove(signal);
         cSignals.remove(signal);
-        
+
         // Only store the signal if the top two bits indicate alarm activity or a conidition is active
-        if((signal.getTags() & Signal.MASK_ANY_ALARM) != 0 ||
-                (signal.getTags() & Signal.MASK_ANY_ACTIVE_CONDITION) != 0) {
+        if ((signal.getTags() & Signal.MASK_ANY_ALARM) != 0
+            || (signal.getTags() & Signal.MASK_ANY_ACTIVE_CONDITION) != 0) {
             pSignals.add(signal);
             cSignals.add(signal);
         }
     }
-    
+
     private void releaseCache() {
         pointData.clear();
         pointSignals.clear();
-        categorySignalsMap.clear();        
+        categorySignalsMap.clear();
     }
 }
