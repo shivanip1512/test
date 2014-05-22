2224e12bd6aeed7434c5ab2e4aec2e65a226bd61
Solberg, Aaron <>
2014-05-22 21:49:10 +0000

    YUKRV-7085 YUK-13062 Redo the web Trending feature - revamped web trending page to use highstock js lib
    - review changes

--- yukon-client/common/src/com/cannontech/core/dao/GraphDao.java
+++ yukon-client/common/src/com/cannontech/core/dao/GraphDao.java
@@ -8,28 +8,20 @@ import com.cannontech.database.db.graph.GraphDataSeries;
 
 public interface GraphDao {
 
-    /** Get all paos for a graph */
-    public List<LiteYukonPAObject> getLiteYukonPaobjects(int graphDefinitionId);
+    List<LiteYukonPAObject> getLiteYukonPaobjects(int graphDefinitionId);
 
-    /** Get all data series */
-    public List<GraphDataSeries> getAllGraphDataSeries();
+    List<GraphDataSeries> getAllGraphDataSeries();
 
-    /** Get data series by type */
-    public List<GraphDataSeries> getAllGraphDataSeriesByType(int type);
+    List<GraphDataSeries> getAllGraphDataSeriesByType(int type);
 
-    /** Get a graph */
-    public LiteGraphDefinition getLiteGraphDefinition(int graphDefinitionId);
+    LiteGraphDefinition getLiteGraphDefinition(int graphDefinitionId);
 
-    /** Get all graphs */
-    public List<LiteGraphDefinition> getGraphDefinitions();
+    List<LiteGraphDefinition> getGraphDefinitions();
 
-    /** Get all graphs for a user*/
-    public List<LiteGraphDefinition> getGraphDefinitionsForUser(int userId);
+    List<LiteGraphDefinition> getGraphDefinitionsForUser(int userId);
     
-    /** Get all graphs for an energy company */
-    public List<LiteGraphDefinition> getGraphDefinitionsForEnergyCompany(int ecId);
+    List<LiteGraphDefinition> getGraphDefinitionsForEnergyCompany(int ecId);
 
-    /** Get all the data series for a graph */
-    public List<GraphDataSeries> getGraphDataSeries(int graphDefinitionId);
+    List<GraphDataSeries> getGraphDataSeries(int graphDefinitionId);
 
 }
