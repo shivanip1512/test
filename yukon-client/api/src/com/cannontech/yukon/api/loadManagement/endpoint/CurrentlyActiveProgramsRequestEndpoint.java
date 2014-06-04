25d32d48c968fb9ab9f095b6bf99438af95d444f
Woodward, Daniel <>
2014-06-04 15:57:29 +0000

    formatting

--- yukon-client/api/src/com/cannontech/yukon/api/loadManagement/endpoint/CurrentlyActiveProgramsRequestEndpoint.java
+++ yukon-client/api/src/com/cannontech/yukon/api/loadManagement/endpoint/CurrentlyActiveProgramsRequestEndpoint.java
@@ -29,81 +29,86 @@ public class CurrentlyActiveProgramsRequestEndpoint {
 
     private LoadControlService loadControlService;
     private RolePropertyDao rolePropertyDao;
-    
+
     private Namespace ns = YukonXml.getYukonNamespace();
-    
+
     @PostConstruct
     public void initialize() throws JDOMException {
     }
-    
-    @PayloadRoot(namespace="http://yukon.cannontech.com/api", localPart="currentlyActiveProgramsRequest")
+
+    @PayloadRoot(namespace = "http://yukon.cannontech.com/api", localPart = "currentlyActiveProgramsRequest")
     public Element invoke(Element currentlyActiveProgramsRequest, LiteYukonUser user) throws Exception {
-        
-    	XmlVersionUtils.verifyYukonMessageVersion(currentlyActiveProgramsRequest, XmlVersionUtils.YUKON_MSG_VERSION_1_0);
-    	
-    	// init response
+
+        XmlVersionUtils.verifyYukonMessageVersion(currentlyActiveProgramsRequest, XmlVersionUtils.YUKON_MSG_VERSION_1_0);
+
+        // init response
         Element resp = new Element("currentlyActiveProgramsResponse", ns);
         XmlVersionUtils.addVersionAttribute(resp, XmlVersionUtils.YUKON_MSG_VERSION_1_0);
-        
+
         // build response
         Element programStatuses = new Element("programStatuses", ns);
 
         // run service
         try {
-        
+
             // Check authorization
             rolePropertyDao.verifyProperty(YukonRoleProperty.OPERATOR_CONSUMER_INFO_WS_LM_DATA_ACCESS, user);
-            
+
             List<ProgramStatus> allCurrentlyActivePrograms = loadControlService.getAllCurrentlyActivePrograms(user);
-            
-            
+
             for (ProgramStatus programStatus : allCurrentlyActivePrograms) {
-                
+
                 Element programStatusElement = new Element("programStatus", ns);
-                programStatusElement.addContent(XmlUtils.createStringElement("programName", ns, programStatus.getProgramName()));
+                programStatusElement.addContent(XmlUtils.createStringElement("programName", ns,
+                    programStatus.getProgramName()));
                 if (programStatus.isActive()) {
                     programStatusElement.addContent(XmlUtils.createStringElement("currentStatus", ns, "Active"));
                 } else {
                     programStatusElement.addContent(XmlUtils.createStringElement("currentStatus", ns, "Inactive"));
                 }
-                programStatusElement.addContent(XmlUtils.createDateElement("startDateTime", ns, programStatus.getStartTime()));
+                programStatusElement.addContent(XmlUtils.createDateElement("startDateTime", ns,
+                    programStatus.getStartTime()));
                 if (programStatus.getStopTime() != null) {
-                    programStatusElement.addContent(XmlUtils.createDateElement("stopDateTime", ns, programStatus.getStopTime()));
+                    programStatusElement.addContent(XmlUtils.createDateElement("stopDateTime", ns,
+                        programStatus.getStopTime()));
                 }
-                programStatusElement.addContent(XmlUtils.createStringElement("gearName", ns, programStatus.getGearName()));
-                
+                programStatusElement.addContent(XmlUtils.createStringElement("gearName", ns,
+                    programStatus.getGearName()));
+
                 programStatuses.addContent(programStatusElement);
             }
-        
+
         } catch (NotAuthorizedException e) {
-            programStatuses = XMLFailureGenerator.generateFailure(currentlyActiveProgramsRequest,
-                                                                e,
-                                                                "UserNotAuthorized",
-                                                                "The user is not authorized to request currently active programs.");
+            programStatuses =
+                XMLFailureGenerator.generateFailure(currentlyActiveProgramsRequest, e, "UserNotAuthorized",
+                    "The user is not authorized to request currently active programs.");
         } catch (BadServerResponseException e) {
-            Element fe = XMLFailureGenerator.generateFailure(currentlyActiveProgramsRequest, e, "ServerCommunicationError", e.getMessage());
+            Element fe =
+                XMLFailureGenerator.generateFailure(currentlyActiveProgramsRequest, e, "ServerCommunicationError",
+                    e.getMessage());
             resp.addContent(fe);
             return resp;
         } catch (ConnectionException e) {
-            Element fe = XMLFailureGenerator.generateFailure(currentlyActiveProgramsRequest, e, "ServerCommunicationError", e.getMessage());
+            Element fe =
+                XMLFailureGenerator.generateFailure(currentlyActiveProgramsRequest, e, "ServerCommunicationError",
+                    e.getMessage());
             resp.addContent(fe);
             return resp;
         }
-        
+
         resp.addContent(programStatuses);
-        
+
         return resp;
     }
-    
-    
+
     @Autowired
     public void setLoadControlService(LoadControlService loadControlService) {
         this.loadControlService = loadControlService;
     }
-    
+
     @Autowired
     public void setRolePropertyDao(RolePropertyDao rolePropertyDao) {
         this.rolePropertyDao = rolePropertyDao;
     }
-    
+
 }
--- yukon-client/api/test/com/cannontech/yukon/api/loadManagement/CurrentlyActiveProgramsRequestEndpointTest.java
+++ yukon-client/api/test/com/cannontech/yukon/api/loadManagement/CurrentlyActiveProgramsRequestEndpointTest.java
@@ -27,107 +27,134 @@ import com.cannontech.yukon.api.utils.TestUtils;
 public class CurrentlyActiveProgramsRequestEndpointTest {
 
     private static final LiteYukonUser AUTH_USER = MockRolePropertyDao.getAuthorizedUser();
-    
+
     private CurrentlyActiveProgramsRequestEndpoint impl;
     private MockLoadControlService mockService;
-    
+
     private Namespace ns = YukonXml.getYukonNamespace();
-    
+
     @Before
     public void setUp() throws Exception {
-        
+
         mockService = new MockLoadControlService();
-        
+
         impl = new CurrentlyActiveProgramsRequestEndpoint();
         impl.setLoadControlService(mockService);
         impl.setRolePropertyDao(new MockRolePropertyDao());
         impl.initialize();
     }
-    
+
     private class MockLoadControlService extends LoadControlServiceAdapter {
-        
-    	private boolean returnEmptyList = true;
+
+        private boolean returnEmptyList = true;
+
         @Override
         public List<ProgramStatus> getAllCurrentlyActivePrograms(LiteYukonUser user) {
-            
+
             List<ProgramStatus> programStatuses = new ArrayList<ProgramStatus>();
             if (returnEmptyList) {
-            	return programStatuses;
+                return programStatuses;
             } else {
-            	MockProgramStatus program1 = new MockProgramStatus("Program1", LMProgramBase.STATUS_ACTIVE, "2008-10-13T12:30:00Z", "2008-10-13T21:40:01Z", "Gear1");
-                MockProgramStatus program2 = new MockProgramStatus("Program2", LMProgramBase.STATUS_INACTIVE, "2008-10-14T13:45:01Z", null, "Gear2");
+                MockProgramStatus program1 =
+                    new MockProgramStatus("Program1", LMProgramBase.STATUS_ACTIVE, "2008-10-13T12:30:00Z",
+                        "2008-10-13T21:40:01Z", "Gear1");
+                MockProgramStatus program2 =
+                    new MockProgramStatus("Program2", LMProgramBase.STATUS_INACTIVE, "2008-10-14T13:45:01Z", null,
+                        "Gear2");
                 programStatuses.add(program1);
                 programStatuses.add(program2);
                 return programStatuses;
             }
         }
-        
+
         public void setReturnEmptyList(boolean isEmpty) {
-        	returnEmptyList = isEmpty;
+            returnEmptyList = isEmpty;
         }
     }
-    
+
     @Test
     @Ignore("YUK-11816")
     public void testInvoke() throws Exception {
-        
+
         // init
         Element requestElement = null;
         Attribute versionAttribute = null;
         Element responseElement = null;
         SimpleXPathTemplate outputTemplate = null;
-        Resource requestSchemaResource = new ClassPathResource("/com/cannontech/yukon/api/loadManagement/schemas/CurrentlyActiveProgramsRequest.xsd", this.getClass());
-        Resource responseSchemaResource = new ClassPathResource("/com/cannontech/yukon/api/loadManagement/schemas/CurrentlyActiveProgramsResponse.xsd", this.getClass());
+        Resource requestSchemaResource =
+            new ClassPathResource("/com/cannontech/yukon/api/loadManagement/schemas/CurrentlyActiveProgramsRequest.xsd",
+                this.getClass());
+        Resource responseSchemaResource =
+            new ClassPathResource("/com/cannontech/yukon/api/loadManagement/schemas/CurrentlyActiveProgramsResponse.xsd",
+                this.getClass());
 
         // no programs, emtpy list
-        //==========================================================================================
+        // ==========================================================================================
         requestElement = new Element("currentlyActiveProgramsRequest", ns);
         versionAttribute = new Attribute("version", "1.0");
         requestElement.setAttribute(versionAttribute);
         TestUtils.validateAgainstSchema(requestElement, requestSchemaResource);
-        
+
         // run and validate response against xsd
         mockService.setReturnEmptyList(true);
         responseElement = impl.invoke(requestElement, AUTH_USER);
         TestUtils.validateAgainstSchema(responseElement, responseSchemaResource);
-        
+
         outputTemplate = YukonXml.getXPathTemplateForElement(responseElement);
-        
+
         // outputs
-        Assert.assertNotNull("No programStatuses node present.", outputTemplate.evaluateAsNode("/y:currentlyActiveProgramsResponse/y:programStatuses"));
-        Assert.assertEquals("Incorrect number of programStatuses nodes.", 1, outputTemplate.evaluateAsLong("count(/y:currentlyActiveProgramsResponse/y:programStatuses)").longValue());
-        Assert.assertEquals("Incorrect number of programStatus nodes.", 0, outputTemplate.evaluateAsLong("count(/y:currentlyActiveProgramsResponse/y:programStatuses/y:programStatus)").longValue());
-        
+        Assert.assertNotNull("No programStatuses node present.",
+            outputTemplate.evaluateAsNode("/y:currentlyActiveProgramsResponse/y:programStatuses"));
+        Assert.assertEquals("Incorrect number of programStatuses nodes.", 1,
+            outputTemplate.evaluateAsLong("count(/y:currentlyActiveProgramsResponse/y:programStatuses)").longValue());
+        Assert.assertEquals(
+            "Incorrect number of programStatus nodes.",
+            0,
+            outputTemplate.evaluateAsLong("count(/y:currentlyActiveProgramsResponse/y:programStatuses/y:programStatus)").longValue());
+
         // 2 programs, one with no stop datetime
-        //==========================================================================================
+        // ==========================================================================================
         requestElement = new Element("currentlyActiveProgramsRequest", ns);
         versionAttribute = new Attribute("version", "1.0");
         requestElement.setAttribute(versionAttribute);
         TestUtils.validateAgainstSchema(requestElement, requestSchemaResource);
-        
+
         // run and validate response against xsd
         mockService.setReturnEmptyList(false);
         responseElement = impl.invoke(requestElement, AUTH_USER);
         TestUtils.validateAgainstSchema(responseElement, responseSchemaResource);
-        
+
         outputTemplate = YukonXml.getXPathTemplateForElement(responseElement);
-        
+
         // outputs
-        Assert.assertNotNull("No programStatuses node present.", outputTemplate.evaluateAsNode("/y:currentlyActiveProgramsResponse/y:programStatuses"));
-        Assert.assertEquals("Incorrect number of programStatuses nodes.", 1, outputTemplate.evaluateAsLong("count(/y:currentlyActiveProgramsResponse/y:programStatuses)").longValue());
-        Assert.assertEquals("Incorrect number of programStatus nodes.", 2, outputTemplate.evaluateAsLong("count(/y:currentlyActiveProgramsResponse/y:programStatuses/y:programStatus)").longValue());
-        
-        Assert.assertEquals("Incorrect programName.", "Program1", outputTemplate.evaluateAsString("/y:currentlyActiveProgramsResponse/y:programStatuses/y:programStatus[1]/y:programName"));
-        Assert.assertEquals("Incorrect currentStatus.", "Active", outputTemplate.evaluateAsString("/y:currentlyActiveProgramsResponse/y:programStatuses/y:programStatus[1]/y:currentStatus"));
-        Assert.assertEquals("Incorrect startDateTime.", "2008-10-13T12:30:00Z", outputTemplate.evaluateAsString("/y:currentlyActiveProgramsResponse/y:programStatuses/y:programStatus[1]/y:startDateTime"));
-        Assert.assertEquals("Incorrect stopDateTime.", "2008-10-13T21:40:01Z", outputTemplate.evaluateAsString("/y:currentlyActiveProgramsResponse/y:programStatuses/y:programStatus[1]/y:stopDateTime"));
-        Assert.assertEquals("Incorrect gearName.", "Gear1", outputTemplate.evaluateAsString("/y:currentlyActiveProgramsResponse/y:programStatuses/y:programStatus[1]/y:gearName"));
-        
-        Assert.assertEquals("Incorrect programName.", "Program2", outputTemplate.evaluateAsString("/y:currentlyActiveProgramsResponse/y:programStatuses/y:programStatus[2]/y:programName"));
-        Assert.assertEquals("Incorrect currentStatus.", "Inactive", outputTemplate.evaluateAsString("/y:currentlyActiveProgramsResponse/y:programStatuses/y:programStatus[2]/y:currentStatus"));
-        Assert.assertEquals("Incorrect startDateTime.", "2008-10-14T13:45:01Z", outputTemplate.evaluateAsString("/y:currentlyActiveProgramsResponse/y:programStatuses/y:programStatus[2]/y:startDateTime"));
-        Assert.assertNull("Should not contain stopDateTime node.", outputTemplate.evaluateAsNode("/y:currentlyActiveProgramsResponse/y:programStatuses/y:programStatus[2]/y:stopDateTime"));
-        Assert.assertEquals("Incorrect gearName.", "Gear2", outputTemplate.evaluateAsString("/y:currentlyActiveProgramsResponse/y:programStatuses/y:programStatus[2]/y:gearName"));
+        Assert.assertNotNull("No programStatuses node present.",
+            outputTemplate.evaluateAsNode("/y:currentlyActiveProgramsResponse/y:programStatuses"));
+        Assert.assertEquals("Incorrect number of programStatuses nodes.", 1,
+            outputTemplate.evaluateAsLong("count(/y:currentlyActiveProgramsResponse/y:programStatuses)").longValue());
+        Assert.assertEquals("Incorrect number of programStatus nodes.", 2,
+            outputTemplate.evaluateAsLong("count(/y:currentlyActiveProgramsResponse/y:programStatuses/y:programStatus)").longValue());
+
+        Assert.assertEquals("Incorrect programName.", "Program1",
+            outputTemplate.evaluateAsString("/y:currentlyActiveProgramsResponse/y:programStatuses/y:programStatus[1]/y:programName"));
+        Assert.assertEquals("Incorrect currentStatus.", "Active",
+            outputTemplate.evaluateAsString("/y:currentlyActiveProgramsResponse/y:programStatuses/y:programStatus[1]/y:currentStatus"));
+        Assert.assertEquals("Incorrect startDateTime.", "2008-10-13T12:30:00Z",
+            outputTemplate.evaluateAsString("/y:currentlyActiveProgramsResponse/y:programStatuses/y:programStatus[1]/y:startDateTime"));
+        Assert.assertEquals("Incorrect stopDateTime.", "2008-10-13T21:40:01Z",
+            outputTemplate.evaluateAsString("/y:currentlyActiveProgramsResponse/y:programStatuses/y:programStatus[1]/y:stopDateTime"));
+        Assert.assertEquals("Incorrect gearName.", "Gear1",
+            outputTemplate.evaluateAsString("/y:currentlyActiveProgramsResponse/y:programStatuses/y:programStatus[1]/y:gearName"));
+
+        Assert.assertEquals("Incorrect programName.", "Program2",
+            outputTemplate.evaluateAsString("/y:currentlyActiveProgramsResponse/y:programStatuses/y:programStatus[2]/y:programName"));
+        Assert.assertEquals("Incorrect currentStatus.", "Inactive",
+            outputTemplate.evaluateAsString("/y:currentlyActiveProgramsResponse/y:programStatuses/y:programStatus[2]/y:currentStatus"));
+        Assert.assertEquals("Incorrect startDateTime.", "2008-10-14T13:45:01Z",
+            outputTemplate.evaluateAsString("/y:currentlyActiveProgramsResponse/y:programStatuses/y:programStatus[2]/y:startDateTime"));
+        Assert.assertNull("Should not contain stopDateTime node.",
+            outputTemplate.evaluateAsNode("/y:currentlyActiveProgramsResponse/y:programStatuses/y:programStatus[2]/y:stopDateTime"));
+        Assert.assertEquals("Incorrect gearName.", "Gear2",
+            outputTemplate.evaluateAsString("/y:currentlyActiveProgramsResponse/y:programStatuses/y:programStatus[2]/y:gearName"));
     }
 
 }
