173da00e3e12d12981169c32c6fdaead94e09584
Fisher, Matthew S <MatthewSFisher@Eaton.com>
2014-06-09 17:17:25 +0000

    YUK-12131 Add point configuration to RFN device configs Added JSON parsing for MetricIdLookup Updated device config XML parsing to  allow Indexed items

--- yukon-server/Resource/include/resource_ids.h
+++ yukon-server/Resource/include/resource_ids.h
@@ -2,10 +2,12 @@
     Resource IDs for:
         paoDefinition.xml
         paoDefinition.xsd
- 
+
         configurationCategoryDefinition.xml
         configurationCategoryDefinition.xsd
         deviceConfigurationCategory.xsd
+
+        MetricIdLookupMapping.json
 */
 
 #define PAODEFINITION_XML_ID        101
@@ -14,4 +16,6 @@
 #define CONFIGCATEGORYDEFINITION_XML_ID     103
 #define CONFIGCATEGORYDEFINITION_XSD_ID     104
 #define DEVICECONFIGCATEGORY_XSD_ID         105
+
+#define METRIC_ID_TO_ATTRIBUTE_MAPPING_ID   106
 
