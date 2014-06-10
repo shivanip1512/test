473cf8cd2de2accda74df12f6c7965a986e3caa3
Woodward, Daniel <>
2014-06-10 16:29:36 +0000

    YUK-13264 Add Ecobee section to global settings
    Added globalSettingTypeTest

---
+++ yukon-client/common/test/unit/com/cannontech/system/GlobalSettingTypeTest.java
@@ -0,0 +1,24 @@
+package com.cannontech.system;
+
+import static com.cannontech.core.roleproperties.InputTypeFactory.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GlobalSettingTypeTest {
+
+    @Test
+    public void test_isSensitiveInformation() {
+        for (GlobalSettingType settingType : GlobalSettingType.values()) {
+            if (settingType.isSensitiveInformation()) {
+                // If this test fails it is because an encrypted global setting type was changed (e.g. from string to int)
+                // or a new global setting was added to isSensitiveInformation to be encrypted and isn't a string type
+                // If we need to encrypt non string types we need to modify GlobalSettingDao, GlobalSettingEditorDao
+                // and GlobalSettingUpdateDao to handle non string types.
+                boolean isStringType = settingType.getType().equals(stringType());
+                Assert.assertTrue(settingType + " is not a stringType and doesn't support encryption", isStringType);
+            }
+        }
+    }
+
+}
