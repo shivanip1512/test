a1c61a62b6a956298d072fc80338bed9c2f9fd17
DeZeeuw, Garrett <>
2014-06-13 21:53:43 +0000

    CTI Database signature - trunk - 6.2.1

--- yukon-database/DBUpdates/oracle/6.2_1.sql
+++ yukon-database/DBUpdates/oracle/6.2_1.sql
@@ -211,4 +211,4 @@ WHERE EventType = 'system.mantenance.systemLogWeatherDataUpdate';
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
 /**************************************************************/
-/*INSERT INTO CTIDatabase VALUES ('6.2', '31-MAY-2014', 'Latest Update', 1, SYSDATE);*/
+INSERT INTO CTIDatabase VALUES ('6.2', '13-JUN-2014', 'Latest Update', 1, SYSDATE);
--- yukon-database/DBUpdates/sqlserver/6.2_1.sql
+++ yukon-database/DBUpdates/sqlserver/6.2_1.sql
@@ -198,4 +198,4 @@ WHERE EventType = 'system.mantenance.systemLogWeatherDataUpdate';
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
 /**************************************************************/
-/*INSERT INTO CTIDatabase VALUES ('6.2', '31-MAY-2014', 'Latest Update', 1, GETDATE());*/
+INSERT INTO CTIDatabase VALUES ('6.2', '13-JUN-2014', 'Latest Update', 1, GETDATE());
