3b82049edd7d76b4be380aeb5e2066dadc00db15
Stenersen, Ross <>
2014-06-17 15:34:22 +0000

    Added documentation.

--- yukon-client/common/src/com/cannontech/common/i18n/Displayable.java
+++ yukon-client/common/src/com/cannontech/common/i18n/Displayable.java
@@ -2,6 +2,13 @@ package com.cannontech.common.i18n;
 
 import org.springframework.context.MessageSourceResolvable;
 
+/**
+ * Implement this class to make instances usable in cti:msg2 and i:inline directly.  If you don't need the flexibility
+ * to use a default you can use {@link DisplayableEnum} instead.
+ */
 public interface Displayable {
+    /**
+     * Get the message that will be used when displaying this instance.
+     */
     MessageSourceResolvable getMessage();
 }
--- yukon-client/common/src/com/cannontech/common/i18n/DisplayableEnum.java
+++ yukon-client/common/src/com/cannontech/common/i18n/DisplayableEnum.java
@@ -1,5 +1,12 @@
 package com.cannontech.common.i18n;
 
+/**
+ * Implement this class to make instances usable in cti:msg2 and i:inline directly.  If you need the flexibility
+ * to use a default you can use {@link Displayable} instead.
+ */
 public interface DisplayableEnum {
-    public String getFormatKey();
+    /**
+     * Get the key for the message that will be used when displaying this instance.
+     */
+    String getFormatKey();
 }
