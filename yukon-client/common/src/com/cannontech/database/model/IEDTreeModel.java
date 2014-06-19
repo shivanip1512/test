1991c56c62b5a9bef9c52ff5f5cda25f7983946e
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2014-06-19 14:17:41 +0000

    YUK-13375 Cleanup String and int usage of PaoType, PaoClass, and PaoCategory within DBPersistents  - formatting

--- yukon-client/common/src/com/cannontech/database/model/IEDTreeModel.java
+++ yukon-client/common/src/com/cannontech/database/model/IEDTreeModel.java
@@ -1,33 +1,21 @@
 package com.cannontech.database.model;
 
-/**
- * This type was created in VisualAge.
- */
 import com.cannontech.common.pao.PaoCategory;
 import com.cannontech.common.pao.PaoClass;
 import com.cannontech.common.pao.PaoType;
 import com.cannontech.database.data.device.DeviceTypesFuncs;
 
-public class IEDTreeModel extends AbstractDeviceTreeModel 
-{
-/**
- * IEDTreeModel constructor comment.
- * @param root javax.swing.tree.TreeNode
- */
-public IEDTreeModel() 
-{
-	super( new DBTreeNode("IEDs") );
-}
+public class IEDTreeModel extends AbstractDeviceTreeModel {
 
-/**
- * Insert the method's description here.
- * Creation date: (4/22/2002 4:11:23 PM)
- * @param deviceType int
- */
-public boolean isDeviceValid( PaoCategory paoCategory, PaoClass paoClass, PaoType paoType )
-{
-	return( DeviceTypesFuncs.isMeter(paoType.getDeviceTypeId()) 
-		 	  || paoType == PaoType.DAVISWEATHER
-			  && paoCategory == PaoCategory.DEVICE );
-}
+    /**
+     * IEDTreeModel constructor comment.
+     */
+    public IEDTreeModel() {
+        super(new DBTreeNode("IEDs"));
+    }
+
+    @Override
+    public boolean isDeviceValid(PaoCategory paoCategory, PaoClass paoClass, PaoType paoType) {
+        return (DeviceTypesFuncs.isMeter(paoType.getDeviceTypeId()) || paoType == PaoType.DAVISWEATHER && paoCategory == PaoCategory.DEVICE);
+    }
 }
--- yukon-client/common/src/com/cannontech/database/model/RTUTreeModel.java
+++ yukon-client/common/src/com/cannontech/database/model/RTUTreeModel.java
@@ -1,41 +1,21 @@
 package com.cannontech.database.model;
 
-/**
- * This type was created in VisualAge.
- */
 import com.cannontech.common.pao.PaoCategory;
 import com.cannontech.common.pao.PaoClass;
 import com.cannontech.common.pao.PaoType;
 import com.cannontech.database.data.device.DeviceTypesFuncs;
 
-public class RTUTreeModel extends AbstractDeviceTreeModel 
-{
-/**
- * RtuTreeModel constructor comment.
- * @param root javax.swing.tree.TreeNode
- */
-public RTUTreeModel() {
-	super( new DBTreeNode("RTUs") );
-}
-/**
- * Insert the method's description here.
- * Creation date: (4/22/2002 4:11:23 PM)
- * @param deviceType int
- */
-public boolean isDeviceValid( PaoCategory paoCategory, PaoClass paoClass, PaoType paoType )
-{
-	return( paoClass == PaoClass.LOADMANAGEMENT
-				|| paoClass == PaoClass.IED
-				|| paoClass == PaoClass.METER
-				|| paoClass == PaoClass.RTU
-				|| paoClass == PaoClass.TRANSMITTER
-				|| paoClass == PaoClass.VIRTUAL
-				|| paoClass == PaoClass.RTU
-				|| paoClass == PaoClass.SYSTEM)
-				&&
-				(DeviceTypesFuncs.isRTU(paoType.getDeviceTypeId()) 
-					|| paoType == PaoType.DAVISWEATHER)
-				&& paoCategory == PaoCategory.DEVICE
-				&& !DeviceTypesFuncs.isIon(paoType.getDeviceTypeId());
-}
+public class RTUTreeModel extends AbstractDeviceTreeModel {
+
+    /**
+     * RtuTreeModel constructor comment.
+     */
+    public RTUTreeModel() {
+        super(new DBTreeNode("RTUs"));
+    }
+
+    @Override
+    public boolean isDeviceValid(PaoCategory paoCategory, PaoClass paoClass, PaoType paoType) {
+        return (paoClass == PaoClass.LOADMANAGEMENT || paoClass == PaoClass.IED || paoClass == PaoClass.METER || paoClass == PaoClass.RTU || paoClass == PaoClass.TRANSMITTER || paoClass == PaoClass.VIRTUAL || paoClass == PaoClass.RTU || paoClass == PaoClass.SYSTEM) && (DeviceTypesFuncs.isRTU(paoType.getDeviceTypeId()) || paoType == PaoType.DAVISWEATHER) && paoCategory == PaoCategory.DEVICE && !DeviceTypesFuncs.isIon(paoType.getDeviceTypeId());
+    }
 }
