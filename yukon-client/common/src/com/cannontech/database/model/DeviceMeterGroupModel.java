c20da65152ba45ffff230fa5f57635660f03ab1a
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2014-06-19 15:50:47 +0000

    YUK-13375 Cleanup String and int usage of PaoType, PaoClass, and PaoCategory within DBPersistents  - formatting

--- yukon-client/common/src/com/cannontech/database/model/DeviceMeterGroupModel.java
+++ yukon-client/common/src/com/cannontech/database/model/DeviceMeterGroupModel.java
@@ -13,85 +13,80 @@ import com.cannontech.database.data.lite.LiteYukonPAObject;
 import com.cannontech.spring.YukonSpringHook;
 import com.cannontech.yukon.IDatabaseCache;
 
-public class DeviceMeterGroupModel extends DBTreeModel 
-{
+public class DeviceMeterGroupModel extends DBTreeModel {
     private static final PaoDefinitionDao paoDefinitionDao = YukonSpringHook.getBean("paoDefinitionDao", PaoDefinitionDao.class);
-    
+
     public DeviceMeterGroupModel() {
-    	super( new DBTreeNode("Meter Numbers") );
+        super(new DBTreeNode("Meter Numbers"));
     }
-    
+
     @Override
     protected LiteBase convertLiteBase(LiteBase lb) {
         if (lb instanceof LiteYukonPAObject) {
-            return findLiteDMG((LiteYukonPAObject)lb);
+            return findLiteDMG((LiteYukonPAObject) lb);
         }
         return lb;
     }
-    
-    private LiteBase findLiteDMG( LiteYukonPAObject liteYuk )
-    {
-    	LiteBase lBase = null;
-    
-    	IDatabaseCache cache = DefaultDatabaseCache.getInstance();
-    	synchronized(cache) {
-    		java.util.List<LiteDeviceMeterNumber> deviceMeterGroupsList = cache.getAllDeviceMeterGroups();
-    		
-    		for (LiteDeviceMeterNumber liteDeviceMeterNumber : deviceMeterGroupsList) {
-    			lBase = liteDeviceMeterNumber;
-    			if( lBase.equals(liteYuk) ) {
-    				lBase = liteDeviceMeterNumber;
-    				break;
-    			}
-    			else
-    				lBase = null;
-    		}
-    	}
-    	
-    	return lBase;
+
+    private LiteBase findLiteDMG(LiteYukonPAObject liteYuk) {
+        LiteBase lBase = null;
+
+        IDatabaseCache cache = DefaultDatabaseCache.getInstance();
+        synchronized (cache) {
+            java.util.List<LiteDeviceMeterNumber> deviceMeterGroupsList = cache.getAllDeviceMeterGroups();
+
+            for (LiteDeviceMeterNumber liteDeviceMeterNumber : deviceMeterGroupsList) {
+                lBase = liteDeviceMeterNumber;
+                if (lBase.equals(liteYuk)) {
+                    lBase = liteDeviceMeterNumber;
+                    break;
+                } else {
+                    lBase = null;
+                }
+            }
+        }
+
+        return lBase;
     }
-    
-    
-    public boolean isLiteTypeSupported( int liteType ) {
-    	return( liteType == LiteTypes.YUKON_PAOBJECT
-    			   || liteType == LiteTypes.DEVICE_METERNUMBER );	
+
+    @Override
+    public boolean isLiteTypeSupported(int liteType) {
+        return (liteType == LiteTypes.YUKON_PAOBJECT || liteType == LiteTypes.DEVICE_METERNUMBER);
     }
-    
+
     @Override
     public boolean isTreePrimaryForObject(LiteBase lb) {
         return false;
     }
-    
-    private boolean isDeviceValid(PaoType paoType)
-    {
+
+    private boolean isDeviceValid(PaoType paoType) {
         if (isCommanderDevicesOnly()) {
             if (!paoDefinitionDao.isTagSupported(paoType, PaoTag.COMMANDER_REQUESTS)) {
                 // only devices read through porter
                 return false;
             }
         }
-    	return DeviceTypesFuncs.usesDeviceMeterGroup(paoType.getDeviceTypeId());
+        return DeviceTypesFuncs.usesDeviceMeterGroup(paoType.getDeviceTypeId());
     }
-    
-    public void update()
-    {
-    	IDatabaseCache cache = DefaultDatabaseCache.getInstance();
-    
-    	synchronized(cache) {
-    		java.util.List<LiteDeviceMeterNumber> deviceMeterGroupsList = cache.getAllDeviceMeterGroups();
-    		java.util.Collections.sort( deviceMeterGroupsList, LiteComparators.liteStringComparator );
-    		
-    		DBTreeNode rootNode = (DBTreeNode) getRoot();
-    		rootNode.removeAllChildren();
-    		
-    		for (LiteDeviceMeterNumber liteDeviceMeterNumber : deviceMeterGroupsList) {
-    			DBTreeNode deviceNode = new DBTreeNode( liteDeviceMeterNumber);
-    			if (isDeviceValid(liteDeviceMeterNumber.getPaoType())) {
-    			    rootNode.add( deviceNode );
-    			}
-    		}
-    	}
-    
-    	reload();
+
+    @Override
+    public void update() {
+        IDatabaseCache cache = DefaultDatabaseCache.getInstance();
+
+        synchronized (cache) {
+            java.util.List<LiteDeviceMeterNumber> deviceMeterGroupsList = cache.getAllDeviceMeterGroups();
+            java.util.Collections.sort(deviceMeterGroupsList, LiteComparators.liteStringComparator);
+
+            DBTreeNode rootNode = (DBTreeNode) getRoot();
+            rootNode.removeAllChildren();
+
+            for (LiteDeviceMeterNumber liteDeviceMeterNumber : deviceMeterGroupsList) {
+                DBTreeNode deviceNode = new DBTreeNode(liteDeviceMeterNumber);
+                if (isDeviceValid(liteDeviceMeterNumber.getPaoType())) {
+                    rootNode.add(deviceNode);
+                }
+            }
+        }
+        reload();
     }
 }
