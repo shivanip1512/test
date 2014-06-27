1a01eef15806ad5ce07f1f5d8bb8b75b4c5169ee
DeZeeuw, Garrett <>
2014-06-27 18:48:52 +0000

    CTI Database signature - trunk - 6.2.2

--- yukon-database/DBUpdates/sqlserver/6.2_2.sql
+++ yukon-database/DBUpdates/sqlserver/6.2_2.sql
@@ -33,4 +33,4 @@ WHERE RolePropertyId = -10800
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
 /**************************************************************/
-/*INSERT INTO CTIDatabase VALUES ('6.2', '27-JUN-2014', 'Latest Update', 2, GETDATE());*/
+INSERT INTO CTIDatabase VALUES ('6.2', '27-JUN-2014', 'Latest Update', 2, GETDATE());
