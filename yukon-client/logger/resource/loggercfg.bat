27cca21be3019e02c0e5e87d75a84d3b5617bcb7
Woodward, Daniel <>
2014-07-01 14:47:37 +0000

    YUK-13452 pre-maven task: Clean up packages and files
    Remove unused packages

--- yukon-client/logger/resource/loggercfg.bat
+++
@@ -1,2 +0,0 @@
-call setjavapath.bat
-java -Djava.class.path=.;%YUKON_BASE%/server/web;database.jar;common.jar;tdc.jar;dbeditor.jar;yukonappserver.jar;jcpagelayout450k.jar;logger.jar com.cannontech.logger.config.LoggerMainFrame
