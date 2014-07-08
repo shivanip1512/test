17798d6f68a84a73297cae35145280d264ef77ba
Khole, Sneha <>
2014-07-08 11:39:32 +0000

    Edited test cases for modified ecobee pojo's.

--- yukon-client/common/src/com/cannontech/dr/ecobee/message/CreateSetRequest.java
+++ yukon-client/common/src/com/cannontech/dr/ecobee/message/CreateSetRequest.java
@@ -23,4 +23,49 @@ public final class CreateSetRequest extends SetRequest{
     public String getParentPath() {
         return parentPath;
     }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((setName == null) ? 0 : setName.hashCode());
+        result = prime * result + ((operation == null) ? 0 : operation.hashCode());
+        result = prime * result + ((parentPath == null) ? 0 : parentPath.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        CreateSetRequest other = (CreateSetRequest) obj;
+        if (setName == null) {
+            if (other.setName != null) {
+                return false;
+            }
+        } else if (!setName.equals(other.setName)) {
+            return false;
+        }
+        if (operation == null) {
+            if (other.operation != null) {
+                return false;
+            }
+        } else if (!operation.equals(other.operation)) {
+            return false;
+        }
+        if (parentPath == null) {
+            if (other.parentPath != null) {
+                return false;
+            }
+        } else if (!parentPath.equals(other.parentPath)) {
+            return false;
+        }
+        return true;
+    }
 }
