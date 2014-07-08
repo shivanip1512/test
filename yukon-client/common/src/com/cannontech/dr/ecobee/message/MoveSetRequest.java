f11aa69c5b057512c9659302bf1b09d200d9dfb6
Khole, Sneha <>
2014-07-08 11:33:42 +0000

    Edited test cases for modified ecobee pojo's.

--- yukon-client/common/src/com/cannontech/dr/ecobee/message/MoveSetRequest.java
+++ yukon-client/common/src/com/cannontech/dr/ecobee/message/MoveSetRequest.java
@@ -26,4 +26,49 @@ public final class MoveSetRequest extends SetRequest{
     public String getToPath() {
         return toPath;
     }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((setPath == null) ? 0 : setPath.hashCode());
+        result = prime * result + ((operation == null) ? 0 : operation.hashCode());
+        result = prime * result + ((toPath == null) ? 0 : toPath.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        MoveSetRequest other = (MoveSetRequest) obj;
+        if (setPath == null) {
+            if (other.setPath != null) {
+                return false;
+            }
+        } else if (!setPath.equals(other.setPath)) {
+            return false;
+        }
+        if (operation == null) {
+            if (other.operation != null) {
+                return false;
+            }
+        } else if (!operation.equals(other.operation)) {
+            return false;
+        }
+        if (toPath == null) {
+            if (other.toPath != null) {
+                return false;
+            }
+        } else if (!toPath.equals(other.toPath)) {
+            return false;
+        }
+        return true;
+    }
 }
