f57349f8f7640e3cccc2d642baa3bb3ed6521020
DeZeeuw, Garrett <>
2014-08-19 17:55:07 +0000

    YUK-13459 Set up Oracle 12c and get Yukon to run against it
    Updated comments in PoolManager.java.
    Pulled common code out into a new method, buildOracleJdbcUrl().
    Updated example master.cfg file for clarity.

--- yukon-client/common/src/com/cannontech/database/PoolManager.java
+++ yukon-client/common/src/com/cannontech/database/PoolManager.java
@@ -114,36 +114,23 @@ public class PoolManager {
             }
             log.debug("Found MSSQL");
             return new ConnectionDescription(url.toString(), dbType);
-        }
-        else if (dbType == DatabaseVendor.ORACLE_DATABASE) {
+        } else if (dbType == DatabaseVendor.ORACLE_DATABASE) {
             try {
-                // configure as Oracle using SID, which has been deprecated as of Oracle 12c.
-                // example: jdbc:oracle:thin:@mn1db02:1521:xcel
-                StringBuilder url = new StringBuilder();
-                url.append("jdbc:oracle:thin:@");
-                String host = configSource.getRequiredString(DB_SQLSERVER_HOST);
-                url.append(host);
-                url.append(":1521:");
-                String tnsName = configSource.getRequiredString(DB_SQLSERVER);
-                url.append(tnsName);
+                // Configure using SID, which is used by Oracle 9, 10, and 11. 
+                // Note: As of Oracle version 12c, use of SID has been deprecated in favor of using service name.
+                // format: jdbc:oracle:thin:@<host>:<port>:<SID>
+                StringBuilder url = buildOracleJdbcUrl(false);
                 
-                log.debug("Found oracle");
+                log.debug("Found Oracle");
                 return new ConnectionDescription(url.toString(), dbType);
             } catch (UnknownKeyException e) {
                 throw new BadConfigurationException("Cannot connect to Oracle without DB_SQLSERVER_HOST and DB_SQLSERVER being specified.", e);
             }
-        }
-        else if (dbType == DatabaseVendor.ORACLE12_DATABASE) {
+        } else if (dbType == DatabaseVendor.ORACLE12_DATABASE) {
             try {
-                // configure as Oracle 12c using service name, not SID.
-                // example: jdbc:oracle:thin:@mn1db02:1521/xcel
-                StringBuilder url = new StringBuilder();
-                url.append("jdbc:oracle:thin:@");
-                String host = configSource.getRequiredString(DB_SQLSERVER_HOST);
-                url.append(host);
-                url.append(":1521/");
-                String tnsName = configSource.getRequiredString(DB_SQLSERVER);
-                url.append(tnsName);
+                // Configure using service name, not SID.  Required by Oracle 12c and later.
+                // format: jdbc:oracle:thin:@<host>:<port>/<serviceName>
+                StringBuilder url = buildOracleJdbcUrl(true);
                 
                 log.debug("Found Oracle 12C");
                 return new ConnectionDescription(url.toString(), dbType);
@@ -154,6 +141,22 @@ public class PoolManager {
         
         //unreachable
         throw new BadConfigurationException("Unable to generate connection URL");
+    }
+
+    private StringBuilder buildOracleJdbcUrl(boolean is12c) {
+        StringBuilder url = new StringBuilder();
+        url.append("jdbc:oracle:thin:@");
+        String host = configSource.getRequiredString(DB_SQLSERVER_HOST);
+        url.append(host);
+        url.append(":1521");
+        if (is12c) {
+            url.append("/");
+        } else {
+            url.append(":");
+        }
+        String tnsName = configSource.getRequiredString(DB_SQLSERVER);
+        url.append(tnsName);
+        return url;
     }
 
     private void createPools() {
