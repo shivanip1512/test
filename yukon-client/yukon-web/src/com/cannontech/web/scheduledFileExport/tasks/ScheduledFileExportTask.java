c8315deb709ca9d5716a356ea612991c349b54ff
Woodward, Daniel <>
2014-08-28 20:53:41 +0000

    YUK-13653 File Export History does not contain any results
    Fixed file export history page to display file exports

--- yukon-client/yukon-web/src/com/cannontech/web/scheduledFileExport/tasks/ScheduledFileExportTask.java
+++ yukon-client/yukon-web/src/com/cannontech/web/scheduledFileExport/tasks/ScheduledFileExportTask.java
@@ -258,8 +258,9 @@ public abstract class ScheduledFileExportTask extends YukonTaskBase {
             // strip characters not allowed in file names.
             finalName += dateString.replaceAll("[\\/:*?\"><|]", "");
         }
-        finalName += "_" + CtiUtilities.getUuidString();
+        // Yeah it's weird we put the extension before the random string but it's intentional
         finalName += overrideFileExtension ? exportFileExtension : defaultFileExtension;
+        finalName += "_" + CtiUtilities.getUuidString();
         return new File(CtiUtilities.getArchiveDirPath(), finalName);
     }
 	
