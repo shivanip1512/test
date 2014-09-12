02047e8765cab51db67f91ca5903b7d60b5e113d
Johnston, Samuel T <SamuelTJohnston@Eaton.com>
2014-09-12 20:31:37 +0000

    YUK-13685 Gateway and Gateway Data Listeners  - Added GatewayArchiveRequestListener to handle unsolicited GatewayArchiveRequest messages from Network Manager.  - Added GatewayDataResponseListener to handle unsolicited GatewayDataResponse messages from Network Manager.

---
+++ yukon-client/services/src/com/cannontech/services/rfn/endpoint/GatewayArchiveRequestListener.java
@@ -0,0 +1,105 @@
+package com.cannontech.services.rfn.endpoint;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jmx.export.annotation.ManagedResource;
+
+import com.cannontech.clientutils.YukonLogManager;
+import com.cannontech.common.pao.PaoIdentifier;
+import com.cannontech.common.rfn.message.RfnIdentifier;
+import com.cannontech.common.rfn.message.gateway.GatewayArchiveRequest;
+import com.cannontech.common.rfn.model.RfnDevice;
+import com.cannontech.common.rfn.service.RfnGatewayDataCache;
+import com.google.common.collect.ImmutableList;
+
+//TODO: logging
+@ManagedResource
+public class GatewayArchiveRequestListener extends ArchiveRequestListenerBase<GatewayArchiveRequest> {
+    
+    private static final Logger log = YukonLogManager.getLogger(GatewayArchiveRequestListener.class);
+    
+    @Autowired RfnGatewayDataCache rfnGatewayDataCache;
+    
+    private List<Worker> workers;
+    
+    private class Worker extends ConverterBase {
+        public Worker(int workerNumber, int queueSize) {
+            super("GatewayArchive", workerNumber, queueSize);
+        }
+        
+        @Override
+        protected RfnDevice processCreation(GatewayArchiveRequest request, RfnIdentifier identifier) {
+            try {
+                RfnDevice device = rfnDeviceCreationService.createGateway(request.getName(), request.getRfnIdentifier());
+                rfnDeviceCreationService.incrementNewDeviceCreated();
+                log.debug("Created new gateway: " + device);
+                loadGatewayData(device.getPaoIdentifier());
+                incrementProcessedArchiveRequest();
+                return device;
+            } catch (Exception e) {
+                log.warn("Creation failed for gateway: " + request.getRfnIdentifier(), e);
+                throw new RuntimeException("Creation failed for " + request.getRfnIdentifier(), e);
+            }
+        }
+        
+        private void loadGatewayData(PaoIdentifier paoIdentifier) {
+            try {
+                //requesting data not in the cache causes it to fetch the data from NM
+                rfnGatewayDataCache.get(paoIdentifier);
+                log.debug("Successfully cached data for gateway: " + paoIdentifier);
+            } catch (Exception e) {
+                log.warn("Exception updating gateway data cache for new gateway: " + paoIdentifier, e);
+            }
+        }
+        
+        @Override
+        public void processData(RfnDevice rfnDevice, GatewayArchiveRequest archiveRequest) {
+            //no data to archive on this queue, just device creation requests that have no other payload
+        }
+    }
+    
+    @Override
+    @PostConstruct
+    public void init() {
+        // setup as many workers as requested
+        ImmutableList.Builder<Worker> workerBuilder = ImmutableList.builder();
+        int workerCount = getWorkerCount();
+        int queueSize = getQueueSize();
+        for (int i = 0; i < workerCount; ++i) {
+            Worker worker = new Worker(i, queueSize);
+            workerBuilder.add(worker);
+            worker.start();
+        }
+        workers = workerBuilder.build();
+    }
+    
+    @PreDestroy
+    @Override
+    protected void shutdown() {
+        for (Worker worker : workers) {
+            worker.shutdown();
+        }
+    }
+    
+    @Override
+    protected List<Worker> getConverters() {
+        return workers;
+    }
+    
+    //Not needed, no response is sent for this message
+    @Override
+    protected Object getRfnArchiveResponse(GatewayArchiveRequest archiveRequest) {
+        return null;
+    }
+    
+    //Not needed, no response is sent for this message
+    @Override
+    protected String getRfnArchiveResponseQueueName() {
+        return null;
+    }
+}
---
+++ yukon-client/services/src/com/cannontech/services/rfn/endpoint/GatewayDataResponseListener.java
@@ -0,0 +1,100 @@
+package com.cannontech.services.rfn.endpoint;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cannontech.clientutils.YukonLogManager;
+import com.cannontech.common.rfn.message.RfnIdentifier;
+import com.cannontech.common.rfn.message.gateway.GatewayDataResponse;
+import com.cannontech.common.rfn.model.RfnDevice;
+import com.cannontech.common.rfn.model.RfnGatewayData;
+import com.cannontech.common.rfn.service.RfnGatewayDataCache;
+import com.google.common.collect.ImmutableList;
+
+//TODO: logging
+public class GatewayDataResponseListener extends ArchiveRequestListenerBase<GatewayDataResponse> {
+    
+    private static final Logger log = YukonLogManager.getLogger(GatewayDataResponseListener.class);
+    
+    @Autowired RfnGatewayDataCache rfnGatewayDataCache;
+    
+    private List<Worker> workers;
+    
+    private class Worker extends ConverterBase {
+        public Worker(int workerNumber, int queueSize) {
+            super("GatewayDataArchive", workerNumber, queueSize);
+        }
+        
+        @Override
+        protected RfnDevice processCreation(GatewayDataResponse message, RfnIdentifier identifier) {
+            //Somehow, we got data for a gateway that is not in the database.
+            log.warn("Received data for a gateway that is not in the database. Creating " + identifier);
+            try {
+                RfnDevice device = rfnDeviceCreationService.createGateway(identifier.getSensorSerialNumber(), identifier);
+                rfnDeviceCreationService.incrementNewDeviceCreated();
+                log.debug("Created new gateway: " + device);
+                return device;
+            } catch (Exception e) {
+                log.warn("Creation failed for " + identifier, e);
+                throw new RuntimeException("Creation failed for " + identifier, e);
+            }
+        }
+        
+        @Override
+        public void processData(RfnDevice rfnDevice, GatewayDataResponse message) {
+            try {
+                RfnGatewayData data = new RfnGatewayData(message);
+                rfnGatewayDataCache.put(rfnDevice.getPaoIdentifier(), data);
+            } catch (Exception e) {
+                log.warn("Data processing failed for " + rfnDevice, e);
+                log.warn("Gateway data: " + message);
+                throw new RuntimeException("Data processing failed for " + rfnDevice, e);
+            }
+        }
+    }
+    
+    @Override
+    @PostConstruct
+    public void init() {
+        // setup as many workers as requested
+        ImmutableList.Builder<Worker> workerBuilder = ImmutableList.builder();
+        int workerCount = getWorkerCount();
+        int queueSize = getQueueSize();
+        for (int i = 0; i < workerCount; ++i) {
+            Worker worker = new Worker(i, queueSize);
+            workerBuilder.add(worker);
+            worker.start();
+        }
+        workers = workerBuilder.build();
+    }
+    
+    @PreDestroy
+    @Override
+    protected void shutdown() {
+        for (Worker worker : workers) {
+            worker.shutdown();
+        }
+    }
+    
+    @Override
+    protected List<Worker> getConverters() {
+        return workers;
+    }
+    
+    //Not needed, no response is sent for this message
+    @Override
+    protected Object getRfnArchiveResponse(GatewayDataResponse archiveRequest) {
+        return null;
+    }
+    
+    //Not needed, no response is sent for this message
+    @Override
+    protected String getRfnArchiveResponseQueueName() {
+        return null;
+    }
+}
