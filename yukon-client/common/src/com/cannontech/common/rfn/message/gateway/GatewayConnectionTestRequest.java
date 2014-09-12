19594b0f446c03060e001f46ffa84abac223ce45
Johnston, Samuel T <SamuelTJohnston@Eaton.com>
2014-09-12 18:14:27 +0000

    YUK-13103 RF Gateway support in Yukon - Added infrastructure for creating gateway paos.  - Changed GatewayConnectionTestRequest to include authentication information.

--- yukon-client/common/src/com/cannontech/common/rfn/message/gateway/GatewayConnectionTestRequest.java
+++ yukon-client/common/src/com/cannontech/common/rfn/message/gateway/GatewayConnectionTestRequest.java
@@ -7,6 +7,9 @@ public class GatewayConnectionTestRequest implements Serializable {
     private static final long serialVersionUID = 1L;
     
     private String ipAddress;
+    private Authentication user;
+    private Authentication admin;
+    private Authentication superAdmin;
 
     public String getIpAddress() {
         return ipAddress;
@@ -16,11 +19,38 @@ public class GatewayConnectionTestRequest implements Serializable {
         this.ipAddress = ipAddress;
     }
 
+    public Authentication getUser() {
+        return user;
+    }
+
+    public void setUser(Authentication user) {
+        this.user = user;
+    }
+
+    public Authentication getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Authentication admin) {
+        this.admin = admin;
+    }
+
+    public Authentication getSuperAdmin() {
+        return superAdmin;
+    }
+
+    public void setSuperAdmin(Authentication superAdmin) {
+        this.superAdmin = superAdmin;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
+        result = prime * result + ((admin == null) ? 0 : admin.hashCode());
         result = prime * result + ((ipAddress == null) ? 0 : ipAddress.hashCode());
+        result = prime * result + ((superAdmin == null) ? 0 : superAdmin.hashCode());
+        result = prime * result + ((user == null) ? 0 : user.hashCode());
         return result;
     }
 
@@ -33,16 +63,36 @@ public class GatewayConnectionTestRequest implements Serializable {
         if (getClass() != obj.getClass())
             return false;
         GatewayConnectionTestRequest other = (GatewayConnectionTestRequest) obj;
+        if (admin == null) {
+            if (other.admin != null)
+                return false;
+        } else if (!admin.equals(other.admin))
+            return false;
         if (ipAddress == null) {
             if (other.ipAddress != null)
                 return false;
         } else if (!ipAddress.equals(other.ipAddress))
+            return false;
+        if (superAdmin == null) {
+            if (other.superAdmin != null)
+                return false;
+        } else if (!superAdmin.equals(other.superAdmin))
+            return false;
+        if (user == null) {
+            if (other.user != null)
+                return false;
+        } else if (!user.equals(other.user))
             return false;
         return true;
     }
 
     @Override
     public String toString() {
-        return String.format("GatewayConnectionTestRequest [ipAddress=%s]", ipAddress);
+        return String.format("GatewayConnectionTestRequest [ipAddress=%s, user=%s, admin=%s, superAdmin=%s]",
+                ipAddress,
+                user,
+                admin,
+                superAdmin);
     }
+    
 }
