cf3abd0918f1e0b16e4b9fda3655d9ab8e847724
More, Shweta <>
2014-09-26 06:29:57 +0000

    YUK_13041 : Meter Readings Show All Sort by Attribute exception :REWORK

--- yukon-client/yukon-web/src/com/cannontech/web/common/pao/service/YukonPointHelper.java
+++ yukon-client/yukon-web/src/com/cannontech/web/common/pao/service/YukonPointHelper.java
@@ -8,20 +8,20 @@ import com.cannontech.common.pao.YukonPao;
 
 public interface YukonPointHelper {
 
-	 /**
-     *  Builds a list of {@link LiteYukonPoint} objects for a device and sorts them.
+    /**
+     * Builds a list of {@link LiteYukonPoint} objects for a device and sorts them.
      * @param pao the yukon PAO whose points are to be sorted.
-     * @param sorting {sort : field on which sorting is to be done, direction: direction for sorting}. 
-     * @param accessor the accessor is required to getMessage for internalization. 
+     * @param sorting {sort : field on which sorting is to be done,direction:direction for sorting}.
+     * @param accessor the accessor is required to getMessage for internalization.
      * @return sorted list of yukon points based on field.
      */
     public List<LiteYukonPoint> getYukonPoints(YukonPao pao, SortingParameters sorting, MessageSourceAccessor accessor);
 
-	/**
-	 * Builds a list of {@link LiteYukonPoint} objects for a device 
-	 * @param pao the yukon PAO whose points are to be sorted.
-	 * @return the List of yukon points
-	 */
+    /**
+     * Builds a list of {@link LiteYukonPoint} objects for a device
+     * @param pao the yukon PAO whose points are to be sorted.
+     * @return the List of yukon points
+     */
     public List<LiteYukonPoint> getYukonPoints(YukonPao pao);
 
 }
