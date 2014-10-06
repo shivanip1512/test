9f08b1a6f12211c5103ecedba2ecace1029c4082
Solberg, Aaron <>
2014-10-06 15:21:20 +0000

    YUKRV-7695 YUK-12661 Web Commander Redo
    - cleanup of unused or unnecessary cache objects

--- yukon-client/common/test/unit/com/cannontech/common/pao/definition/dao/PaoDefinitionDaoImplTest.java
+++ yukon-client/common/test/unit/com/cannontech/common/pao/definition/dao/PaoDefinitionDaoImplTest.java
@@ -1,6 +1,10 @@
 package com.cannontech.common.pao.definition.dao;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -29,12 +33,12 @@ import com.cannontech.common.pao.definition.model.PaoPointTemplate;
 import com.cannontech.common.pao.definition.model.PaoTag;
 import com.cannontech.common.pao.definition.model.PointIdentifier;
 import com.cannontech.common.pao.definition.model.PointTemplate;
-import com.cannontech.core.dao.StateDao;
+import com.cannontech.core.dao.StateGroupDao;
 import com.cannontech.core.dao.UnitMeasureDao;
-import com.cannontech.database.data.lite.LiteState;
 import com.cannontech.database.data.lite.LiteStateGroup;
 import com.cannontech.database.data.lite.LiteUnitMeasure;
 import com.cannontech.database.data.point.PointType;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSet.Builder;
 import com.google.common.collect.Multimap;
@@ -59,7 +63,7 @@ public class PaoDefinitionDaoImplTest {
         URL schemaResource = classLoader.getResource("com/cannontech/common/pao/definition/dao/paoDefinition.xsd");
         dao.setSchemaFile(new UrlResource(schemaResource));
 
-        ReflectionTestUtils.setField(dao, "stateDao", new MockStateDao());
+        ReflectionTestUtils.setField(dao, "stateGroupDao", new MockStateGroupDao());
         ReflectionTestUtils.setField(dao, "unitMeasureDao", new MockUnitMeasureDao());
         dao.initialize();
 
@@ -443,7 +447,7 @@ public class PaoDefinitionDaoImplTest {
         URL schemaResource = classLoader.getResource("com/cannontech/common/pao/definition/dao/paoDefinition.xsd");
         dao.setSchemaFile(new UrlResource(schemaResource));
 
-        ReflectionTestUtils.setField(dao, "stateDao", new MockStateDao());
+        ReflectionTestUtils.setField(dao, "stateGroupDao", new MockStateGroupDao());
         ReflectionTestUtils.setField(dao, "unitMeasureDao", new MockUnitMeasureDao());
         final URL customFileUrl =
                 classLoader.getResource("com/cannontech/common/pao/definition/dao/testCustomPaoDefinition.xml");
@@ -630,36 +634,18 @@ public class PaoDefinitionDaoImplTest {
             }
             throw new IllegalArgumentException("Unit of measure doesn't exist: " + uomName);
         }
-
+        
     }
+    
+    private static class MockStateGroupDao implements StateGroupDao {
+        
+        @Override
+        public List<LiteStateGroup> getAllStateGroups() {
+            return ImmutableList.of(new LiteStateGroup(0, "state0"));
+        }
 
-    private static class MockStateDao implements StateDao {
         @Override
-        public LiteState findLiteState(int stateGroupID, int rawState) {
-            return null;
-        }
-        
-        @Override
-        public LiteStateGroup getLiteStateGroup(int stateGroupID) {
-            return null;
-        }
-        
-        @Override
-        public LiteStateGroup getLiteStateGroup(String stateGroupName) {
-            
-            if (stateGroupName.equals("state0")) {
-                return new LiteStateGroup(0);
-            }
-            throw new IllegalArgumentException("State group doesn't exist: " + stateGroupName);
-        }
-        
-        @Override
-        public LiteState[] getLiteStates(int stateGroupID) {
-            return null;
-        }
-        
-        @Override
-        public LiteStateGroup[] getAllStateGroups() {
+        public LiteStateGroup getStateGroup(int stateGroupId) {
             return null;
         }
         
