92a85d6eb1b4b09baba5557eee6d9791de2f9e99
Rawat, Payal <PayalRawat@Eaton.com>
2014-10-08 07:46:37 +0000

    YUK-13726 - Since Last ChangeId can possibly start out with huge range if there is a bad timestamp in rawpointhistory

--- yukon-client/common/src/com/cannontech/common/scheduledFileExport/dao/impl/ScheduledFileExportDaoImpl.java
+++ yukon-client/common/src/com/cannontech/common/scheduledFileExport/dao/impl/ScheduledFileExportDaoImpl.java
@@ -15,88 +15,89 @@ import com.cannontech.database.SqlParameterSink;
 import com.cannontech.database.YukonJdbcTemplate;
 
 public class ScheduledFileExportDaoImpl implements ScheduledFileExportDao {
-	@Autowired private YukonJdbcTemplate yukonJdbcTemplate;
-	@Autowired private RawPointHistoryDao rawPointHistoryDao;
-	@Autowired private ConfigurationSource configurationSource;
-	
-	private int initDays;
-	
-	@PostConstruct
-	public void init() {
-		initDays = configurationSource.getInteger("SINCE_LAST_RUN_INIT_DAYS", 7);
-	}
-	
-	@Override
-	public void setRphIdForJob(int jobId, long rphId) {
-		if(hasEntry(jobId)) {
-			updateRphIdForJob(jobId, rphId);
-		} else {
-			insertRphIdForJob(jobId, rphId);
-		}
-	}
-	
-	private void insertRphIdForJob(int jobId, long rphId) {
-		SqlStatementBuilder sql = new SqlStatementBuilder();
-		SqlParameterSink sink = sql.insertInto("RawPointHistoryDependentJob");
-		
-		sink.addValue("JobId", jobId);
-		sink.addValue("RawPointHistoryId", rphId);
-		
-		yukonJdbcTemplate.update(sql);
-	}
-	
-	private void updateRphIdForJob(int jobId, long rphId) {
-		SqlStatementBuilder sql = new SqlStatementBuilder();
-		SqlParameterSink sink = sql.update("RawPointHistoryDependentJob");
-		
-		sink.addValue("JobId", jobId);
-		sink.addValue("RawPointHistoryId", rphId);
-		sql.append("WHERE JobId").eq(jobId);
-		
-		yukonJdbcTemplate.update(sql);
-	}
-	
-	private boolean hasEntry(int jobId) {
-		SqlStatementBuilder sql = new SqlStatementBuilder();
-		sql.append("SELECT COUNT(*)");
-		sql.append("FROM RawPointHistoryDependentJob");
-		sql.append("WHERE JobId").eq(jobId);
-		
-		int result = yukonJdbcTemplate.queryForInt(sql);
-		return result > 0;
-	}
-	
-	@Override
-	public long getLastRphIdByJobId(int jobId) {
-		SqlStatementBuilder sql = new SqlStatementBuilder();
-		sql.append("SELECT RawPointHistoryId");
-		sql.append("FROM RawPointHistoryDependentJob");
-		sql.append("WHERE JobId").eq(jobId);
-		
-		long result;
-		try {
-			result = yukonJdbcTemplate.queryForLong(sql);
-		} catch(DataAccessException e) {
-			result = getOldestRphIdInDaysPrevious();
-		}
-		return result;
-	}
-	
-	private long getOldestRphIdInDaysPrevious() {
-		Instant xDaysAgo = Instant.now().minus(Duration.standardDays(initDays));
-		Instant now = Instant.now();
-		
-		SqlStatementBuilder sql = new SqlStatementBuilder();
-		sql.append("SELECT MIN(ChangeId)");
-		sql.append("FROM RawPointHistory");
-		sql.append("WHERE Timestamp").gte(xDaysAgo);
+    @Autowired private YukonJdbcTemplate yukonJdbcTemplate;
+    @Autowired private RawPointHistoryDao rawPointHistoryDao;
+    @Autowired private ConfigurationSource configurationSource;
+
+    private int initDays;
+
+    @PostConstruct
+    public void init() {
+        initDays = configurationSource.getInteger("SINCE_LAST_RUN_INIT_DAYS", 7);
+    }
+
+    @Override
+    public void setRphIdForJob(int jobId, long rphId) {
+        if (hasEntry(jobId)) {
+            updateRphIdForJob(jobId, rphId);
+        } else {
+            insertRphIdForJob(jobId, rphId);
+        }
+    }
+
+    private void insertRphIdForJob(int jobId, long rphId) {
+        SqlStatementBuilder sql = new SqlStatementBuilder();
+        SqlParameterSink sink = sql.insertInto("RawPointHistoryDependentJob");
+
+        sink.addValue("JobId", jobId);
+        sink.addValue("RawPointHistoryId", rphId);
+
+        yukonJdbcTemplate.update(sql);
+    }
+
+    private void updateRphIdForJob(int jobId, long rphId) {
+        SqlStatementBuilder sql = new SqlStatementBuilder();
+        SqlParameterSink sink = sql.update("RawPointHistoryDependentJob");
+
+        sink.addValue("JobId", jobId);
+        sink.addValue("RawPointHistoryId", rphId);
+        sql.append("WHERE JobId").eq(jobId);
+
+        yukonJdbcTemplate.update(sql);
+    }
+
+    private boolean hasEntry(int jobId) {
+        SqlStatementBuilder sql = new SqlStatementBuilder();
+        sql.append("SELECT COUNT(*)");
+        sql.append("FROM RawPointHistoryDependentJob");
+        sql.append("WHERE JobId").eq(jobId);
+
+        int result = yukonJdbcTemplate.queryForInt(sql);
+        return result > 0;
+    }
+
+    @Override
+    public long getLastRphIdByJobId(int jobId) {
+        SqlStatementBuilder sql = new SqlStatementBuilder();
+        sql.append("SELECT RawPointHistoryId");
+        sql.append("FROM RawPointHistoryDependentJob");
+        sql.append("WHERE JobId").eq(jobId);
+
+        long result;
+        try {
+            result = yukonJdbcTemplate.queryForLong(sql);
+        } catch (DataAccessException e) {
+            result = getOldestRphIdInDaysPrevious();
+        }
+        return result;
+    }
+
+    private long getOldestRphIdInDaysPrevious() {
+        Instant xDaysAgo = Instant.now().minus(Duration.standardDays(initDays));
+        Instant now = Instant.now();
+
+        SqlStatementBuilder sql = new SqlStatementBuilder();
+        sql.append("SELECT MIN(ChangeId)");
+        sql.append("FROM RawPointHistory");
+        sql.append("WHERE Timestamp").gte(xDaysAgo);
         sql.append(  "and Timestamp").lt(now);
-        
-		long changeId = yukonJdbcTemplate.queryForLong(sql);
-		if(changeId == 0) {
-			//No changeIds from the past X days. Just return the most recent one.
-			return rawPointHistoryDao.getMaxChangeId();
-		}
-		return changeId;
-	}
+
+        long changeId = yukonJdbcTemplate.queryForLong(sql);
+        if (changeId == 0) {
+            // No changeIds from the past X days. Just return the most recent
+            // one.
+            return rawPointHistoryDao.getMaxChangeId();
+        }
+        return changeId;
+    }
 }
