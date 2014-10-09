ca3b68eb7d45d7ea0ea6589bcf468abcfa88aec9
Woodward, Daniel <>
2014-10-09 16:47:37 +0000

    YUK-13724 Exception that can kill DispatchClientConnection
    Added dev tool to submit dbChange messages
    Added unit test

--- yukon-client/yukon-web/test/com/cannontech/web/dev/DeveloperControllerTest.java
+++ yukon-client/yukon-web/test/com/cannontech/web/dev/DeveloperControllerTest.java
@@ -3,10 +3,10 @@ package com.cannontech.web.dev;
 import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.replay;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Map;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.ui.ModelMap;
 
@@ -15,32 +15,35 @@ import com.cannontech.common.config.MasterConfigBooleanKeysEnum;
 
 public class DeveloperControllerTest {
     /**
-     * DeveloperController uses reflection to populate two maps which it uses for the DbChange message feature. Since
-     * we cannot predict when or if DBChangeMsg.java gets refactored we can use this test to detect when it does.
+     * DeveloperController uses reflection to populate two maps which it uses for the DbChange message
+     * feature. Since we cannot predict when or if DBChangeMsg.java gets refactored we can use this test to
+     * detect when it does.
      * 
-     * This test will fail for one of two reasons. 1. DeveloperController.dbChangePage() method was changed or the
-     * attributes 'databaseFields', 'categoryFields' were changed. 2. DBChangeMsg was finally refactored and we need
-     * to fix the initialization of the two maps.
+     * This test will fail for one of two reasons. 1. DeveloperController.dbChangePage() method was changed or
+     * the attributes 'databaseFields', 'categoryFields' were changed. 2. DBChangeMsg was finally refactored
+     * and we need to fix the initialization of the two maps.
      */
     @Test
     public void test_dbChangeMaps() {
-        
+
         ConfigurationSource configSource = createMock(ConfigurationSource.class);
         expect(configSource.getBoolean(MasterConfigBooleanKeysEnum.DEVELOPMENT_MODE)).andReturn(true);
         replay(configSource);
 
         DeveloperController controller = new DeveloperController(configSource);
-        
+
         ModelMap model = new ModelMap();
         controller.dbChangePage(model);
-        @SuppressWarnings("unchecked")
-        Map<String, Integer> databaseFields = (Map<String, Integer>) model.get("databaseFields");
-        @SuppressWarnings("unchecked")
-        Map<String, String>  categoryFields = (Map<String, String> ) model.get("categoryFields");
-        
-        Assert.assertTrue("Controller did not properly initialize databseFields" , databaseFields != null);
-        Assert.assertTrue("'databaseFields' is empty. Was DBChangeMsg refactored?" , databaseFields.size() > 10);
-        Assert.assertTrue("Controller did not properly initialize categoryFields" , categoryFields != null);
-        Assert.assertTrue("'categoryFields' is empty. Was DBChangeMsg refactored?" , categoryFields.size() > 10);
+        Object databaseFields = model.get("databaseFields");
+        Object categoryFields = model.get("categoryFields");
+
+        assertTrue("Controller did not properly initialize databseFields", databaseFields != null);
+        assertTrue("'databaseFields' is no longer a map. This test probably needs to be updated",
+            databaseFields instanceof Map);
+        assertTrue("'databaseFields' is empty. Was DBChangeMsg refactored?", ((Map<?, ?>) databaseFields).size() > 10);
+        assertTrue("Controller did not properly initialize categoryFields", categoryFields != null);
+        assertTrue("'categoryFields' is no longer a map. This test probably needs to be updated",
+            categoryFields instanceof Map);
+        assertTrue("'categoryFields' is empty. Was DBChangeMsg refactored?", ((Map<?, ?>) categoryFields).size() > 10);
     }
 }
