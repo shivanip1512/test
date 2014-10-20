75c20e2c390f97aa77d5f7ded8d9cd53a7e14547
DeZeeuw, Garrett <>
2014-10-20 18:07:43 +0000

    Fix for Oracle DB snapshot error
    This invokes a scheduled job on the Oracle database server which cleans up any leftover temp*.ora files that should have been deleted when the tablespace was dropped (but didn't get deleted).

--- yukon-database/Custom Scripts/Oracle/DropDatabase.sql
+++ yukon-database/Custom Scripts/Oracle/DropDatabase.sql
@@ -1,3 +1,7 @@
 drop tablespace tempDatabase including contents and datafiles;
 drop tablespace tempDatabase_temp including contents and datafiles;
 drop user tempDatabase cascade;
+BEGIN
+    DBMS_SCHEDULER.RUN_JOB('CLEANTEMPFILES');
+END;
+/
