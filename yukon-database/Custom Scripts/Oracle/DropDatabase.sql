2531eabf7933c13d60370d68a10207fbfcb6405f
DeZeeuw, Garrett <>
2014-10-20 19:16:42 +0000

    Fix for Oracle DB snapshot error
    Prepended 'SYS.' onto the DBMS scheduler command.

--- yukon-database/Custom Scripts/Oracle/DropDatabase.sql
+++ yukon-database/Custom Scripts/Oracle/DropDatabase.sql
@@ -2,5 +2,5 @@ drop tablespace tempDatabase including contents and datafiles;
 drop tablespace tempDatabase_temp including contents and datafiles;
 drop user tempDatabase cascade;
 BEGIN
-    DBMS_SCHEDULER.RUN_JOB('CLEANTEMPFILES');
+    SYS.DBMS_SCHEDULER.RUN_JOB('CLEANTEMPFILES');
 END;
