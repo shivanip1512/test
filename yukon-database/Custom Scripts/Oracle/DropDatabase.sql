2a52223b50b20ee06b2308bac8ded4912e72d5c2
DeZeeuw, Garrett <>
2014-10-22 17:21:30 +0000

    Fix for Oracle DB snapshot error
    I surrender.

--- yukon-database/Custom Scripts/Oracle/DropDatabase.sql
+++ yukon-database/Custom Scripts/Oracle/DropDatabase.sql
@@ -1,9 +1,3 @@
-create table tempA (id NUMERIC);
 drop tablespace tempDatabase including contents and datafiles;
 drop tablespace tempDatabase_temp including contents and datafiles;
 drop user tempDatabase cascade;
-BEGIN
-    SYS.DBMS_SCHEDULER.RUN_JOB('CLEANTEMPFILES');
-END;
-/
-drop table tempA;
