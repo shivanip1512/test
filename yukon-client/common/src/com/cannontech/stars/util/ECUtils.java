5753fdc68a107670e10966d46d49d6996ca84e6f
Woodward, Daniel <>
2014-10-22 15:50:29 +0000

    formatting

--- yukon-client/common/src/com/cannontech/stars/util/ECUtils.java
+++ yukon-client/common/src/com/cannontech/stars/util/ECUtils.java
@@ -1,9 +1,3 @@
-/*
- * Created on Nov 5, 2003
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 package com.cannontech.stars.util;
 
 import java.util.ArrayList;
@@ -12,30 +6,24 @@ import java.util.List;
 import com.cannontech.stars.database.cache.StarsDatabaseCache;
 import com.cannontech.stars.database.data.lite.LiteStarsEnergyCompany;
 
-/**
- * @author yao
- *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 public class ECUtils {
 
-	public static final int RIGHT_SHOW_ADDTL_PROTOCOLS = 0x20000000;
+    public static final int RIGHT_SHOW_ADDTL_PROTOCOLS = 0x20000000;
 
-	/**
-	 * Get all the energy companies that belongs to (directly or indirectly)
-	 * this energy company, including itself
-	 */
-	public static List<LiteStarsEnergyCompany> getAllDescendants(final LiteStarsEnergyCompany parent) {
-		List<LiteStarsEnergyCompany> descendants = new ArrayList<LiteStarsEnergyCompany>();
-		descendants.add( parent );
+    /**
+     * Get all the energy companies that belongs to (directly or indirectly)
+     * this energy company, including itself
+     */
+    public static List<LiteStarsEnergyCompany> getAllDescendants(final LiteStarsEnergyCompany parent) {
+        List<LiteStarsEnergyCompany> descendants = new ArrayList<>();
+        descendants.add(parent);
 
         for (final LiteStarsEnergyCompany child : parent.getChildren()) {
-            descendants.addAll( getAllDescendants(child) );
+            descendants.addAll(getAllDescendants(child));
         }
 
-		return descendants;
-	}
+        return descendants;
+    }
 
     /**
      * Get all the energy companies that the energy company
@@ -48,10 +36,10 @@ public class ECUtils {
      * @return
      */
     public static List<LiteStarsEnergyCompany> getAllAscendants(LiteStarsEnergyCompany childEC) {
-        List<LiteStarsEnergyCompany> ascendants = new ArrayList<LiteStarsEnergyCompany>();
-        ascendants.add( childEC );
+        List<LiteStarsEnergyCompany> ascendants = new ArrayList<>();
+        ascendants.add(childEC);
 
-        while(childEC.getParent() != null) {
+        while (childEC.getParent() != null) {
             ascendants.add(childEC.getParent());
             childEC = childEC.getParent();
         }
@@ -59,7 +47,7 @@ public class ECUtils {
         return ascendants;
     }
 
-	public static boolean isDefaultEnergyCompany(LiteStarsEnergyCompany company) {
-		return (company == null) ? false : company.getLiteID() == StarsDatabaseCache.DEFAULT_ENERGY_COMPANY_ID;
-	}
+    public static boolean isDefaultEnergyCompany(LiteStarsEnergyCompany company) {
+        return (company == null) ? false : company.getLiteID() == StarsDatabaseCache.DEFAULT_ENERGY_COMPANY_ID;
+    }
 }
