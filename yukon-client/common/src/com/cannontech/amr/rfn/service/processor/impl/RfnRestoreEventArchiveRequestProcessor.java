c9761268f790f9959fc33a6dceed487bcee8c8ca
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2014-10-30 18:22:37 +0000

    YUK-13645 Add support for estimating time for Restore alarm/event from RFN meters  - Added handling of 0 (undefined) event timestamp  - No Outage Log point will be archived for undefined event timestamp  - Restore Event and Restore Count will be archived with "now" event times and Estimated quality.

--- yukon-client/common/src/com/cannontech/amr/rfn/service/processor/impl/RfnRestoreEventArchiveRequestProcessor.java
+++ yukon-client/common/src/com/cannontech/amr/rfn/service/processor/impl/RfnRestoreEventArchiveRequestProcessor.java
@@ -2,6 +2,8 @@ package com.cannontech.amr.rfn.service.processor.impl;
 
 import java.util.List;
 
+import org.joda.time.Instant;
+
 import com.cannontech.amr.rfn.message.event.RfnConditionDataType;
 import com.cannontech.amr.rfn.message.event.RfnConditionType;
 import com.cannontech.amr.rfn.message.event.RfnEvent;
@@ -20,22 +22,35 @@ public class RfnRestoreEventArchiveRequestProcessor extends RfnEventConditionDat
     
     @Override
     public <T extends RfnEvent> void process(RfnDevice device, T event, List<? super PointData> pointDatas) {
-        rfnMeterEventService.processAttributePointData(device, pointDatas, BuiltInAttribute.OUTAGE_STATUS, event.getTimeStamp(), OutageStatus.GOOD.getRawState());
         
-        Long durationInSeconds = RfnInvalidValues.OUTAGE_DURATION.getValue();
-        PointQuality pointQuality = PointQuality.Unknown;
-        try {
-            Long start = (Long) getEventDataWithType(event, RfnConditionDataType.EVENT_START_TIME);
-            Long end = event.getTimeStamp();
-            durationInSeconds = (end - start) / 1000;
-            pointQuality = PointQuality.Normal;
-        } catch (InvalidEventMessageException e) {
-            // Old firmware doesn't include the EVENT_START_TIME meta-data, so just use the invalid value set above for the duration if we get here
+        long eventTimestamp = event.getTimeStamp();
+        PointQuality pointQuality = PointQuality.Normal;
+
+        if (event.getTimeStamp() == 0) {  //outage was too long, and firmware unable to know what time really is
+            // adjust the eventTimestamp to "now" and estimated quality
+            eventTimestamp = Instant.now().getMillis();
+            pointQuality = PointQuality.Estimated;
         }
-        rfnMeterEventService.processAttributePointData(device, pointDatas, BuiltInAttribute.OUTAGE_LOG, event.getTimeStamp(), durationInSeconds, pointQuality);
+
+        rfnMeterEventService.processAttributePointData(device, pointDatas, BuiltInAttribute.OUTAGE_STATUS, eventTimestamp,
+                                                       OutageStatus.GOOD.getRawState(), pointQuality);
+        
+        if (event.getTimeStamp() != 0) { // do not process Outage Log when actual eventTimestamp unknown.
+            Long durationInSeconds = RfnInvalidValues.OUTAGE_DURATION.getValue();
+            PointQuality outageLogPointQuality = PointQuality.Unknown;
+            try {
+                Long start = (Long) getEventDataWithType(event, RfnConditionDataType.EVENT_START_TIME);
+                Long end = eventTimestamp;
+                durationInSeconds = (end - start) / 1000;
+                outageLogPointQuality = PointQuality.Normal;
+            } catch (InvalidEventMessageException e) {
+                // Old firmware doesn't include the EVENT_START_TIME meta-data, so just use the invalid value set above for the duration if we get here
+            }
+            rfnMeterEventService.processAttributePointData(device, pointDatas, BuiltInAttribute.OUTAGE_LOG, eventTimestamp, durationInSeconds, outageLogPointQuality);
+        }
 
         Long count = (Long) getEventDataWithType(event, RfnConditionDataType.COUNT);
-        rfnMeterEventService.processAttributePointData(device, pointDatas, BuiltInAttribute.RFN_OUTAGE_RESTORE_COUNT, event.getTimeStamp(), count);
+        rfnMeterEventService.processAttributePointData(device, pointDatas, BuiltInAttribute.RFN_OUTAGE_RESTORE_COUNT, eventTimestamp, count, pointQuality);
     }
     
     @Override
