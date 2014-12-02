fd00cce19f70bb78e22e88c74a9b293bfffa25b8
Zhu, Li <LiZhu2@Eaton.com>
2014-12-02 21:26:09 +0000

    Reverted changes from revision 57113.

--- yukon-client/common/src/com/cannontech/common/rfn/message/gateway/RfnGatewayUpgradeResponse.java
+++ yukon-client/common/src/com/cannontech/common/rfn/message/gateway/RfnGatewayUpgradeResponse.java
@@ -11,16 +11,12 @@ import com.cannontech.common.rfn.message.RfnIdentifyingMessage;
 public class RfnGatewayUpgradeResponse implements RfnIdentifyingMessage, Serializable {
     private static final long serialVersionUID = 1L;
 
-    public static final short gatewayMessageTypeInitialValue = -1;
-    public static final byte gatewayMessageStatusInitialValue = (byte)0x80;
-    public static final int fragmentIdInitialValue = -1;
-
     private String upgradeId;
     private RfnIdentifier rfnIdentifier;
     private RfnGatewayUpgradeResponseType responseType = RfnGatewayUpgradeResponseType.UNKNOWN;
-    private short gatewayMessageType = gatewayMessageTypeInitialValue;
-    private byte gatewayMessageStatus = gatewayMessageStatusInitialValue;
-    private int fragmentId = fragmentIdInitialValue;
+    private short gatewayMessageType = -1;
+    private byte gatewayMessageStatus = -1;
+    private int fragmentId = -1;
     
     public String getUpgradeId() {
         return upgradeId;
