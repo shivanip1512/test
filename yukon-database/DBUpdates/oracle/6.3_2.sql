25cddd8dc4e8b475f61102da715d4f99df5c30a7
DeZeeuw, Garrett <>
2014-12-09 17:07:36 +0000

    YUK-13905 Split 'demandProfile' into 'demand' and 'profile' for DeviceConfigCategory table
    No changes were made to creation scripts.
    In update scripts, any existing 'demandProfile' device config categories are split into two new categories, 'demand' and 'profile'.  The corresponding config items are then assigned to the correct category.

--- yukon-database/DBUpdates/oracle/6.3_2.sql
+++ yukon-database/DBUpdates/oracle/6.3_2.sql
@@ -28,6 +28,56 @@ DELETE FROM YukonRoleProperty
 WHERE RolePropertyId = -10807;
 /* End YUK-13917 */
 
+/* Start YUK-13905 */
+DECLARE 
+    v_NextDeviceConfigCategoryId NUMBER;
+    v_Name VARCHAR2(100);
+    v_Description VARCHAR2(1024);
+ 
+BEGIN
+    FOR CurrentCategory IN (SELECT DeviceConfigCategoryId
+                            FROM DeviceConfigCategory
+                            WHERE CategoryType = 'demandProfile'
+                            ORDER BY DeviceConfigCategoryId ASC)
+    LOOP
+       /* Create new row (demand) */
+       SELECT
+          (SELECT MAX(DeviceConfigCategoryID) + 1 from DeviceConfigCategory) AS NextDeviceConfigCategoryId,
+           Name, 
+           Description 
+       INTO v_NextDeviceConfigCategoryId, v_Name, v_Description 
+       FROM DeviceConfigCategory
+       WHERE DeviceConfigCategoryId = CurrentCategory.DeviceConfigCategoryId;
+
+       INSERT INTO DeviceConfigCategory VALUES (
+           v_NextDeviceConfigCategoryId,
+           'demand',
+           SUBSTR(v_Name, 1, 84) || ' (Demand Split)',
+           SUBSTR(v_Description, 1, 984) || ' (Demand Split)'
+       );
+ 
+       UPDATE DeviceConfigCategoryItem 
+       SET DeviceConfigCategoryId = v_NextDeviceConfigCategoryId
+       WHERE DeviceConfigCategoryId = CurrentCategory.DeviceConfigCategoryId
+       AND ItemName = 'demandInterval';
+ 
+       INSERT INTO DeviceConfigCategoryMap
+           SELECT DeviceConfigurationId, v_NextDeviceConfigCategoryId
+           FROM DeviceConfigCategoryMap
+           WHERE DeviceConfigCategoryId = CurrentCategory.DeviceConfigCategoryId;
+ 
+       /* Fix current row (profile) */
+       UPDATE DeviceConfigCategory 
+       SET CategoryType = 'profile',
+           Name = SUBSTR(Name, 1, 84) || ' (Profile Split)',
+           Description = SUBSTR(Description, 1, 984) || ' (Profile Split)'
+       WHERE DeviceConfigCategoryId = CurrentCategory.DeviceConfigCategoryId;
+
+    END LOOP;
+END;
+/
+/* End YUK-13905 */
+
 /**************************************************************/
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
--- yukon-database/DBUpdates/sqlserver/6.3_2.sql
+++ yukon-database/DBUpdates/sqlserver/6.3_2.sql
@@ -28,6 +28,66 @@ DELETE FROM YukonRoleProperty
 WHERE RolePropertyId = -10807;
 /* End YUK-13917 */
 
+/* Start YUK-13905 */
+SELECT * INTO #DemandProfileCategories
+FROM DeviceConfigCategory
+WHERE CategoryType = 'demandProfile'
+
+
+/* @start-block */
+DECLARE @DeviceConfigCategoryId INT
+DECLARE @NextDeviceConfigCategoryId INT
+DECLARE @Name VARCHAR(100)
+DECLARE @Description VARCHAR(1024)
+ 
+WHILE EXISTS (SELECT * FROM #DemandProfileCategories)
+BEGIN
+    SET @DeviceConfigCategoryId = 
+       (SELECT TOP 1 DeviceConfigCategoryId
+        FROM #DemandProfileCategories
+        ORDER BY DeviceConfigCategoryId ASC);
+ 
+    /* Create new row (demand) */
+    SELECT
+        @NextDeviceConfigCategoryId = (SELECT MAX(DeviceConfigCategoryID) + 1 FROM DeviceConfigCategory),
+        @Name = Name, 
+        @Description = Description 
+    FROM DeviceConfigCategory
+    WHERE DeviceConfigCategoryId = @DeviceConfigCategoryId;
+ 
+    INSERT INTO DeviceConfigCategory VALUES (
+        @NextDeviceConfigCategoryId,
+        'demand',
+        LEFT(@Name, 84) + ' (Demand Split)',
+        LEFT(@Description, 984) + ' (Demand Split)'
+    );
+ 
+    UPDATE DeviceConfigCategoryItem 
+    SET DeviceConfigCategoryId = @NextDeviceConfigCategoryId
+    WHERE DeviceConfigCategoryId = @DeviceConfigCategoryId
+    AND ItemName = 'demandInterval';
+ 
+    INSERT INTO DeviceConfigCategoryMap
+        SELECT DeviceConfigurationId, @NextDeviceConfigCategoryId
+        FROM DeviceConfigCategoryMap
+        WHERE DeviceConfigCategoryId = @DeviceConfigCategoryId;
+ 
+    /* Fix current row (profile) */
+    UPDATE DeviceConfigCategory SET
+        CategoryType = 'profile',
+        Name = LEFT(Name, 84) + ' (Profile Split)',
+        Description = LEFT(Description, 984) + ' (Profile Split)'
+    WHERE DeviceConfigCategoryId = @DeviceConfigCategoryId;
+ 
+    DELETE #DemandProfileCategories
+    WHERE DeviceConfigCategoryId = @DeviceConfigCategoryId;
+END
+/* @end-block */
+ 
+DROP TABLE #DemandProfileCategories;
+/* End YUK-13905 */
+
+
 /**************************************************************/
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
