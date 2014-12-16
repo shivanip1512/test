5298c0bfe2ef7d336e2c6fbaca9bed8a1c0ea59d
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2014-12-16 20:55:01 +0000

    YUK-12221 Remove database code from model objects (e.g. LiteCustomer)  - deleted unused retrieve methods and reformatted code.

--- yukon-client/common/src/com/cannontech/database/data/lite/LiteContact.java
+++ yukon-client/common/src/com/cannontech/database/data/lite/LiteContact.java
@@ -1,230 +1,113 @@
 package com.cannontech.database.data.lite;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.List;
 
-import com.cannontech.clientutils.CTILogger;
+import org.apache.commons.lang3.StringUtils;
+
 import com.cannontech.common.util.CtiUtilities;
 import com.cannontech.common.util.LazyList;
-import com.cannontech.database.PoolManager;
-import com.cannontech.database.db.contact.Contact;
 import com.cannontech.user.UserUtils;
 
-/*
- */
-public class LiteContact extends LiteBase
-{
-	private String contFirstName = null;
-	private String contLastName = null;
-	private int loginID = UserUtils.USER_YUKON_ID;
-	private int addressID = CtiUtilities.NONE_ZERO_ID;
+public class LiteContact extends LiteBase {
+    private String contFirstName = null;
+    private String contLastName = null;
+    private int loginID = UserUtils.USER_YUKON_ID;
+    private int addressID = CtiUtilities.NONE_ZERO_ID;
 
-    public static final LiteContact NONE_LITE_CONTACT =
-            new LiteContact( CtiUtilities.NONE_ZERO_ID, 
-                    null, CtiUtilities.STRING_NONE );
+    private List<LiteContactNotification> liteContactNotifications = LazyList.ofInstance(LiteContactNotification.class);
 
-	//contains instances of com.cannontech.database.data.lite.LiteContactNotification
-	private List<LiteContactNotification> liteContactNotifications = LazyList.ofInstance(LiteContactNotification.class);
-	
-	public LiteContact() {
-	    super();
-	    setLiteType(LiteTypes.CONTACT);
-	}
+    public LiteContact(int contactId) {
+        super();
+        setContactID(contactId);
+        setLiteType(LiteTypes.CONTACT);
+    }
 
-	/**
-	 * LiteContact
-	 */
-	public LiteContact( int contID_ ) {
-		super();
-		setContactID(contID_);
-		setLiteType(LiteTypes.CONTACT);
-	}
+    public LiteContact(int contactId, String firstName, String lastName) {
+        this(contactId);
+        setContFirstName(firstName);
+        setContLastName(lastName);
+    }
 
-	/**
-	 * LiteContact
-	 */
-	public LiteContact( int contID_, String fName_, String lName_ ) {
-		this(contID_);
-		setContFirstName( fName_ );
-		setContLastName( lName_ );
-	}
-
-	/**
-     * LiteContact
-     */
-    public LiteContact( int contID_, String fName_, String lName_, int loginId ) {
-        this(contID_);
-        setContFirstName( fName_ );
-        setContLastName( lName_ );
+    public LiteContact(int contactId, String firstName, String lastName, int loginId) {
+        this(contactId);
+        setContFirstName(firstName);
+        setContLastName(lastName);
         setLoginID(loginId);
     }
-    
-	/**
-	 * LiteContact
-	 */
-	public LiteContact( int contID_, String contactFirstName_, 
-			String contactLastName_, int loginID_, int addressID_ ) 
-	{
-		this( contID_ );
-		setContFirstName( contactFirstName_ );
-		setContLastName( contactLastName_ );
-		setLoginID( loginID_ );
-		setAddressID( addressID_ );
-	}
-	/**
-	 * Insert the method's description here.
-	 * Creation date: (3/27/2001 9:29:21 AM)
-	 * @return int
-	 */
-	public int getContactID() {
-		return getLiteID();
-	}
-	/**
-	 * Insert the method's description here.
-	 * Creation date: (3/27/2001 9:29:21 AM)
-	 * @return java.lang.String
-	 */
-	public java.lang.String getContFirstName() {
-		return contFirstName;
-	}
-	/**
-	 * Insert the method's description here.
-	 * Creation date: (3/27/2001 9:29:21 AM)
-	 * @return java.lang.String
-	 */
-	public java.lang.String getContLastName() {
-		return contLastName;
-	}
-	
-	/**
-	 * Insert the method's description here.
-	 * Creation date: (3/27/2001 9:29:21 AM)
-	 * @param newContactID int
-	 */
-	public void setContactID(int newContactID) 
-	{
-		setLiteID(newContactID);
-	}
-	/**
-	 * Insert the method's description here.
-	 * Creation date: (3/27/2001 9:29:21 AM)
-	 * @param newContFirstName java.lang.String
-	 */
-	public void setContFirstName(java.lang.String newContFirstName) {
-		contFirstName = newContFirstName;
-	}
-	/**
-	 * Insert the method's description here.
-	 * Creation date: (3/27/2001 9:29:21 AM)
-	 * @param newContLastName java.lang.String
-	 */
-	public void setContLastName(java.lang.String newContLastName) {
-		contLastName = newContLastName;
-	}
-	
-	/**
-	 * This method was created by Cannon Technologies Inc.
-	 */
-	public String toString() {
-	    if( (getContLastName() == null && getContFirstName() == null) || (getContLastName().equalsIgnoreCase("") && getContFirstName().equalsIgnoreCase("")) ) {
-	        return CtiUtilities.STRING_NONE + "," + CtiUtilities.STRING_NONE;
-	    }
-		
-	    if( getContLastName() == null ) {
-				return getContFirstName();
-		} else {
-			if( getContFirstName() == null ) {
-				return getContLastName();
-			}else {
-				return getContLastName() + ", " + getContFirstName();
-			}
-		}
-	}
 
-	/**
-	 * Returns the userID.
-	 * @return int
-	 */
-	public int getLoginID() {
-		return loginID;
-	}
+    public LiteContact(int contactId, String firstName, String lastName, int loginId, int addressId) {
+        this(contactId);
+        setContFirstName(firstName);
+        setContLastName(lastName);
+        setLoginID(loginId);
+        setAddressID(addressId);
+    }
 
-	/**
-	 * Sets the userID.
-	 * @param userID The userID to set
-	 */
-	public void setLoginID(int loginID_) {
-		this.loginID = loginID_;
-	}
+    public int getContactID() {
+        return getLiteID();
+    }
 
-	/**
-	 * retrieve method comment.
-	 */
-	public void retrieve(String databaseAlias) 
-	{
-        PreparedStatement pstmt = null;
-        java.sql.Connection conn = null;
-        ResultSet rset = null;
-        try {
-            conn = PoolManager.getInstance().getConnection( CtiUtilities.getDatabaseAlias());
-            
-            String sql = "SELECT c.ContFirstName, c.ContLastName, c.LoginID, c.AddressID " + 
-                        " FROM " + Contact.TABLE_NAME + " c " +
-                        " WHERE c.ContactID = ? ";
-            
-            pstmt = conn.prepareStatement( sql );
-            pstmt.setInt( 1, getContactID());
-            rset = pstmt.executeQuery();                
+    public java.lang.String getContFirstName() {
+        return contFirstName;
+    }
 
-            if (rset.next())
-            {
-                setContFirstName( rset.getString(1));
-                setContLastName( rset.getString(2));
-                setLoginID( rset.getInt(3));
-                setAddressID( rset.getInt(4));
+    public java.lang.String getContLastName() {
+        return contLastName;
+    }
+
+    public void setContactID(int newContactID) {
+        setLiteID(newContactID);
+    }
+
+    public void setContFirstName(java.lang.String newContFirstName) {
+        contFirstName = newContFirstName;
+    }
+
+    public void setContLastName(java.lang.String newContLastName) {
+        contLastName = newContLastName;
+    }
+
+    @Override
+    public String toString() {
+        if (StringUtils.isBlank(getContLastName()) && StringUtils.isBlank(getContFirstName())) {
+            return CtiUtilities.STRING_NONE + "," + CtiUtilities.STRING_NONE;
+        }
+
+        if (StringUtils.isBlank(getContLastName())) {
+            return getContFirstName();
+        } else {
+            if (StringUtils.isBlank(getContFirstName())) {
+                return getContLastName();
+            } else {
+                return getContLastName() + ", " + getContFirstName();
             }
         }
-        catch (Exception e) {
-            CTILogger.error( e.getMessage(), e );
-        }
-        finally {
-            try {
-                if (rset != null) rset.close();
-                if (pstmt != null) pstmt.close();
-                if (conn != null) conn.close();
-            }
-            catch (java.sql.SQLException e) {
-                com.cannontech.clientutils.CTILogger.error( "Unable to load Contact", e );
-            }
-        }
-	}
+    }
 
-	/**
-	 * Returns the addressID.
-	 * @return int
-	 */
-	public int getAddressID() {
-		return addressID;
-	}
+    public int getLoginID() {
+        return loginID;
+    }
 
-	/**
-	 * Sets the addressID.
-	 * @param addressID The addressID to set
-	 */
-	public void setAddressID(int addressID) {
-		this.addressID = addressID;
-	}
+    public void setLoginID(int loginID_) {
+        this.loginID = loginID_;
+    }
 
-	/**
-	 * Returns the liteContactNotifications.
-	 * @return List<LiteContactNotifications>
-	 */
+    public int getAddressID() {
+        return addressID;
+    }
+
+    public void setAddressID(int addressID) {
+        this.addressID = addressID;
+    }
+
+    /**
+     * Returns the liteContactNotifications.
+     */
     public List<LiteContactNotification> getLiteContactNotifications() {
         return liteContactNotifications;
     }
+
     public void setNotifications(List<LiteContactNotification> notificationList) {
         liteContactNotifications = notificationList;
     }
-	
 }
--- yukon-client/common/src/com/cannontech/database/data/lite/LiteContactNotification.java
+++ yukon-client/common/src/com/cannontech/database/data/lite/LiteContactNotification.java
@@ -1,184 +1,93 @@
 package com.cannontech.database.data.lite;
 
 import com.cannontech.common.model.ContactNotificationType;
-import com.cannontech.database.db.contact.ContactNotification;
 
-/*
- */
-public class LiteContactNotification extends LiteBase
-{
-	private int contactID = 0;
-	private int notificationCategoryID = ContactNotificationType.HOME_PHONE.getDefinitionId(); /* Home Phone category */
-	private String disableFlag = null;
-	private String notification = null;
-	
-	private int order = 0;
-	
-	public LiteContactNotification() {
-		super();
-		setLiteType(LiteTypes.CONTACT_NOTIFICATION);
-	}
+public class LiteContactNotification extends LiteBase {
+    private int contactID = 0;
+    private int notificationCategoryID = ContactNotificationType.HOME_PHONE.getDefinitionId(); /* Home Phone category */
+    private String disableFlag = null;
+    private String notification = null;
 
-	/**
-	 * LiteContactNotification
-	 */
-	public LiteContactNotification( int contNotifID_ ) 
-	{
-		super();
-		setContactNotifID( contNotifID_ );
-		setLiteType( LiteTypes.CONTACT_NOTIFICATION );
-	}
-	/**
-	 * LiteContactNotification
-	 */
-	public LiteContactNotification( int contNotifID_, int contID_, 
-			int notifCatID_, String disableFlag_, String notification_ ) 
-	{
-		this( contNotifID_ );
-		setContactID( contID_ );
-		setNotificationCategoryID( notifCatID_ );
-		setDisableFlag( disableFlag_ );
-		setNotification( notification_ );
-	}
+    private int order = 0;
 
-	/**
-	 * Insert the method's description here.
-	 * Creation date: (3/27/2001 9:29:21 AM)
-	 * @return int
-	 */
-	public int getContactNotifID() {
-		return getLiteID();
-	}
+    public LiteContactNotification() {
+        super();
+        setLiteType(LiteTypes.CONTACT_NOTIFICATION);
+    }
 
-	/**
-	 * Insert the method's description here.
-	 * Creation date: (3/27/2001 9:29:21 AM)
-	 * @return int
-	 */
-	public int getContactID() {
-		return contactID;
-	}
-	
-	/**
-	 * Insert the method's description here.
-	 * Creation date: (3/27/2001 9:29:21 AM)
-	 * @param newContactID int
-	 */
-	public void setContactID(int newContactID) 
-	{
-		contactID = newContactID;
-	}
-	
-	/**
-	 * Insert the method's description here.
-	 * Creation date: (3/27/2001 9:29:21 AM)
-	 * @param newContactID int
-	 */
-	public void setContactNotifID(int newID) 
-	{
-		setLiteID(newID);
-	}
-		
-	/**
-	 * This method was created by Cannon Technologies Inc.
-	 */
-	@Override
+    public LiteContactNotification(int contactNotificationId) {
+        super();
+        setContactNotifID(contactNotificationId);
+        setLiteType(LiteTypes.CONTACT_NOTIFICATION);
+    }
+
+    public LiteContactNotification(int contactNotificationId, int contactId, int notificationCategoryId, 
+            String disableFlag, String notification) {
+        this(contactNotificationId);
+        setContactID(contactId);
+        setNotificationCategoryID(notificationCategoryId);
+        setDisableFlag(disableFlag);
+        setNotification(notification);
+    }
+
+    public int getContactNotifID() {
+        return getLiteID();
+    }
+
+    public int getContactID() {
+        return contactID;
+    }
+
+    public void setContactID(int newContactID) {
+        contactID = newContactID;
+    }
+
+    public void setContactNotifID(int newID) {
+        setLiteID(newID);
+    }
+
+    @Override
     public String toString() {
-		return getNotification();
-	}
+        return getNotification();
+    }
 
-	/**
-	 * retrieve method comment.
-	 */
-	public void retrieve(String databaseAlias) 
-	{
-	 	com.cannontech.database.SqlStatement stmt =
-	 		new com.cannontech.database.SqlStatement(
-				"SELECT c.NotificationCategoryID, c.DisableFlag, c.Notification " + 
-		 		"FROM " + ContactNotification.TABLE_NAME + " c " +
-		 		" WHERE c.ContactID = " + getContactID(),
-		 		databaseAlias);
-	
-	 	try
-	 	{
-	 		stmt.execute();
+    public String getDisableFlag() {
+        return disableFlag;
+    }
 
-			setNotificationCategoryID( ((java.math.BigDecimal) stmt.getRow(0)[0]).intValue() );
-			setDisableFlag( stmt.getRow(0)[1].toString() );
-			setNotification( stmt.getRow(0)[2].toString() );
-	 	}
-	 	catch( Exception e )
-	 	{
-	 		com.cannontech.clientutils.CTILogger.error( e.getMessage(), e );
-	 	}
-	}
-
-	/**
-	 * Returns the disableFlag.
-	 * @return String
-	 */
-	public String getDisableFlag() {
-		return disableFlag;
-	}
-    
-    /**
-     * Returns the disableFlag.
-     * @return boolean
-     */
     public boolean isDisabled() {
         return "y".equalsIgnoreCase(getDisableFlag());
-    }    
+    }
 
-	/**
-	 * Returns the notification.
-	 * @return String
-	 */
-	public String getNotification() {
-		return notification;
-	}
+    public String getNotification() {
+        return notification;
+    }
 
-	/**
-	 * Returns the notificationCategoryID.
-	 * @return int
-	 */
-	public int getNotificationCategoryID() {
-		return notificationCategoryID;
-	}
+    public int getNotificationCategoryID() {
+        return notificationCategoryID;
+    }
 
-	/**
-	 * Sets the disableFlag.
-	 * @param disableFlag The disableFlag to set
-	 */
-	public void setDisableFlag(String disableFlag) {
-		this.disableFlag = disableFlag;
-	}
+    public void setDisableFlag(String disableFlag) {
+        this.disableFlag = disableFlag;
+    }
 
-	/**
-	 * Sets the notification.
-	 * @param notification The notification to set
-	 */
-	public void setNotification(String notification) {
-		this.notification = notification;
-	}
+    public void setNotification(String notification) {
+        this.notification = notification;
+    }
 
-	/**
-	 * Sets the notificationCategoryID.
-	 * @param notificationCategoryID The notificationCategoryID to set
-	 */
-	public void setNotificationCategoryID(int notificationCategoryID) {
-		this.notificationCategoryID = notificationCategoryID;
-	}
+    public void setNotificationCategoryID(int notificationCategoryID) {
+        this.notificationCategoryID = notificationCategoryID;
+    }
 
-	public int getOrder() {
+    public int getOrder() {
         return order;
     }
-	
-	public void setOrder(int order) {
+
+    public void setOrder(int order) {
         this.order = order;
     }
-	
-	
-	public ContactNotificationType getContactNotificationType() {
-		return ContactNotificationType.getTypeForNotificationCategoryId(this.getNotificationCategoryID());
-	}
+
+    public ContactNotificationType getContactNotificationType() {
+        return ContactNotificationType.getTypeForNotificationCategoryId(this.getNotificationCategoryID());
+    }
 }
