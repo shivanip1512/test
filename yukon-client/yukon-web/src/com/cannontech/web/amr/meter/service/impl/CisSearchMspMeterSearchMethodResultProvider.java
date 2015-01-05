efc422c17df89c06e2c24dd77ae686c3004238f5
Khole, Sneha <>
2015-01-05 07:37:23 +0000

    YUK-13442 Add support for generic searching for meters using MultiSpeak Adding new file for CIS Search

---
+++ yukon-client/yukon-web/src/com/cannontech/web/amr/meter/service/impl/CisSearchMspMeterSearchMethodResultProvider.java
@@ -0,0 +1,30 @@
+package com.cannontech.web.amr.meter.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.cannontech.amr.meter.search.model.MspSearchField;
+import com.cannontech.multispeak.client.MultispeakVendor;
+import com.cannontech.multispeak.deploy.service.Meter;
+
+public class CisSearchMspMeterSearchMethodResultProvider extends MspMeterSearchMethodResultProviderBase {
+
+	@Override
+	public MspSearchField getSearchField() {
+		return MspSearchField.CIS_SEARCH;
+	}
+	
+	@Override
+	public List<String> getMeterNumbers(String filterValue) {
+		
+		List<String> meterNumbers = new ArrayList<String>();
+		
+		MultispeakVendor mspVendor = multispeakDao.getMultispeakVendor(multispeakFuncs.getPrimaryCIS());
+		List<Meter> meters = mspObjectDao.getMspMetersByServiceLocation(filterValue, mspVendor);
+		for (Meter meter : meters) {
+			meterNumbers.add(meter.getMeterNo());
+		}
+		
+		return meterNumbers;
+	}
+}
