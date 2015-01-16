b7cdb0f0448294a28b9afc0fb9be2726afa12f68
DeZeeuw, Garrett <>
2015-01-16 16:39:07 +0000

    CTI Database signature - updating 6.2.8 build date

--- yukon-database/DBUpdates/oracle/6.2_8.sql
+++ yukon-database/DBUpdates/oracle/6.2_8.sql
@@ -8,4 +8,4 @@
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
 /**************************************************************/
-INSERT INTO CTIDatabase VALUES ('6.2', '19-DEC-2014', 'Latest Update', 8, SYSDATE);
+INSERT INTO CTIDatabase VALUES ('6.2', '16-JAN-2015', 'Latest Update', 8, SYSDATE);
