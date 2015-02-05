a1d1141bfc0fbcfd63df912d51fbb0da4b9e745b
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2015-02-05 23:32:37 +0000

    YUK-14020 STARS Import taking a long time  - formatting

--- yukon-client/common/src/com/cannontech/stars/service/impl/UpdatableAccountConverterImpl.java
+++ yukon-client/common/src/com/cannontech/stars/service/impl/UpdatableAccountConverterImpl.java
@@ -17,48 +17,48 @@ import com.cannontech.stars.web.util.ImportFields;
 
 public class UpdatableAccountConverterImpl implements UpdatableAccountConverter {
 
-	private AccountService accountService;
-	private SiteInformationDao siteInformationDao;
-	
-	@Override
-	public UpdatableAccount createNewUpdatableAccount(String[] custFields, LiteStarsEnergyCompany ec) {
-		
-		// create new UpdatableAccount with acct number from custFields
-		UpdatableAccount acct = new UpdatableAccount();
-		acct.setAccountNumber(custFields[ImportFields.IDX_ACCOUNT_NO]);
-		
-		// new acct dto
-		AccountDto acctDto = new AccountDto();
-		
-		// update dto with cust fields
-		setCustFieldsOnDto(acctDto, custFields);
-		acct.setAccountDto(acctDto);
-		
-		return acct;
-		
-	}
-	
-	@Override
-	public UpdatableAccount getUpdatedUpdatableAccount(LiteAccountInfo starsCustAcctInfo, String[] custFields, LiteStarsEnergyCompany ec) {
-		
-		String accountNumber = starsCustAcctInfo.getCustomerAccount().getAccountNumber();
-		
-		// create new UpdatableAccount based on existing cust acct number
-		UpdatableAccount acct = new UpdatableAccount();
-		acct.setAccountNumber(accountNumber);
-		
-		// get dto based on existing cust acct
-		AccountDto acctDto = accountService.getAccountDto(accountNumber, ec);
-		
-		// update dto with cust fields
-		setCustFieldsOnDto(acctDto, custFields);
-		acct.setAccountDto(acctDto);
-		
-		return acct;
-	}
-	
-	private void setCustFieldsOnDto(AccountDto acctDto, String[] custFields) {
-		
+    private AccountService accountService;
+    private SiteInformationDao siteInformationDao;
+
+    @Override
+    public UpdatableAccount createNewUpdatableAccount(String[] custFields, LiteStarsEnergyCompany ec) {
+
+        // create new UpdatableAccount with acct number from custFields
+        UpdatableAccount acct = new UpdatableAccount();
+        acct.setAccountNumber(custFields[ImportFields.IDX_ACCOUNT_NO]);
+
+        // new acct dto
+        AccountDto acctDto = new AccountDto();
+
+        // update dto with cust fields
+        setCustFieldsOnDto(acctDto, custFields);
+        acct.setAccountDto(acctDto);
+
+        return acct;
+    }
+
+    @Override
+    public UpdatableAccount getUpdatedUpdatableAccount(LiteAccountInfo starsCustAcctInfo, String[] custFields,
+            LiteStarsEnergyCompany ec) {
+
+        String accountNumber = starsCustAcctInfo.getCustomerAccount().getAccountNumber();
+
+        // create new UpdatableAccount based on existing cust acct number
+        UpdatableAccount acct = new UpdatableAccount();
+        acct.setAccountNumber(accountNumber);
+
+        // get dto based on existing cust acct
+        AccountDto acctDto = accountService.getAccountDto(accountNumber, ec);
+
+        // update dto with cust fields
+        setCustFieldsOnDto(acctDto, custFields);
+        acct.setAccountDto(acctDto);
+
+        return acct;
+    }
+
+    private void setCustFieldsOnDto(AccountDto acctDto, String[] custFields) {
+
         if (!StringUtils.isEmpty(custFields[ImportFields.IDX_FIRST_NAME])) {
             acctDto.setFirstName(custFields[ImportFields.IDX_FIRST_NAME]);
         }
@@ -68,13 +68,13 @@ public class UpdatableAccountConverterImpl implements UpdatableAccountConverter 
         if (!StringUtils.isEmpty(custFields[ImportFields.IDX_EMAIL])) {
             acctDto.setEmailAddress(custFields[ImportFields.IDX_EMAIL]);
         }
-		
+
         if (!StringUtils.isEmpty(custFields[ImportFields.IDX_HOME_PHONE])) {
             acctDto.setHomePhone(custFields[ImportFields.IDX_HOME_PHONE]);
-        }		
+        }
         if (!StringUtils.isEmpty(custFields[ImportFields.IDX_WORK_PHONE])) {
             acctDto.setWorkPhone(custFields[ImportFields.IDX_WORK_PHONE]);
-        }		
+        }
 
         if (!StringUtils.isEmpty(custFields[ImportFields.IDX_IVR_USERNAME])) {
             acctDto.setIvrLogin(custFields[ImportFields.IDX_IVR_USERNAME]);
@@ -83,7 +83,7 @@ public class UpdatableAccountConverterImpl implements UpdatableAccountConverter 
         if (!StringUtils.isEmpty(custFields[ImportFields.IDX_IVR_PIN])) {
             acctDto.setVoicePIN(custFields[ImportFields.IDX_IVR_PIN]);
         }
-        
+
         if (!StringUtils.isEmpty(custFields[ImportFields.IDX_USERNAME])) {
             acctDto.setUserName(custFields[ImportFields.IDX_USERNAME]);
         }
@@ -92,56 +92,55 @@ public class UpdatableAccountConverterImpl implements UpdatableAccountConverter 
         }
         if (!StringUtils.isEmpty(custFields[ImportFields.IDX_LOGIN_GROUP])) {
             acctDto.setUserGroup(custFields[ImportFields.IDX_LOGIN_GROUP]);
-        }		
-		
+        }
+
         if (!StringUtils.isEmpty(custFields[ImportFields.IDX_COMPANY_NAME])) {
             acctDto.setCompanyName(custFields[ImportFields.IDX_COMPANY_NAME]);
         }
-		
-		if(custFields[ImportFields.IDX_CUSTOMER_TYPE].equalsIgnoreCase("COM") || custFields[ImportFields.IDX_COMPANY_NAME].length() > 0) {
-			acctDto.setIsCommercial(true);
-		} else {
-			acctDto.setIsCommercial(false);
-		}
+
+        if (custFields[ImportFields.IDX_CUSTOMER_TYPE].equalsIgnoreCase("COM") || custFields[ImportFields.IDX_COMPANY_NAME].length() > 0) {
+            acctDto.setIsCommercial(true);
+        } else {
+            acctDto.setIsCommercial(false);
+        }
         if (!StringUtils.isEmpty(custFields[ImportFields.IDX_MAP_NO])) {
             acctDto.setMapNumber(custFields[ImportFields.IDX_MAP_NO]);
         }
-		
-		
-		// street address
-		Address streetAddress = acctDto.getStreetAddress();
-		String cityName = custFields[ImportFields.IDX_CITY];
-		if (!StringUtils.isEmpty(cityName)) {
-		    streetAddress.setCityName(cityName);
-		}
 
-		String county = custFields[ImportFields.IDX_COUNTY];
-		if (!StringUtils.isEmpty(county)) {
-		    streetAddress.setCounty(county);
-		}
-		
-		String locationAddress1 = custFields[ImportFields.IDX_STREET_ADDR1];
-		if (!StringUtils.isEmpty(locationAddress1)) {
-		    streetAddress.setLocationAddress1(locationAddress1);
-		}
-		    
-		String locationAddress2 = custFields[ImportFields.IDX_STREET_ADDR2];
-		if (!StringUtils.isEmpty(locationAddress2)) {
-		    streetAddress.setLocationAddress2(locationAddress2);
-		}
-		
-		String stateCode = custFields[ImportFields.IDX_STATE];
-		if (!StringUtils.isEmpty(stateCode)) {
-		    streetAddress.setStateCode(stateCode);
-		}
-		
-		String zipCode = custFields[ImportFields.IDX_ZIP_CODE];
-		if (!StringUtils.isEmpty(zipCode)) {
-		    streetAddress.setZipCode(zipCode);
-		}
- 		
-		// site
-		SiteInformation siteInfo = acctDto.getSiteInfo();
+        // street address
+        Address streetAddress = acctDto.getStreetAddress();
+        String cityName = custFields[ImportFields.IDX_CITY];
+        if (!StringUtils.isEmpty(cityName)) {
+            streetAddress.setCityName(cityName);
+        }
+
+        String county = custFields[ImportFields.IDX_COUNTY];
+        if (!StringUtils.isEmpty(county)) {
+            streetAddress.setCounty(county);
+        }
+
+        String locationAddress1 = custFields[ImportFields.IDX_STREET_ADDR1];
+        if (!StringUtils.isEmpty(locationAddress1)) {
+            streetAddress.setLocationAddress1(locationAddress1);
+        }
+
+        String locationAddress2 = custFields[ImportFields.IDX_STREET_ADDR2];
+        if (!StringUtils.isEmpty(locationAddress2)) {
+            streetAddress.setLocationAddress2(locationAddress2);
+        }
+
+        String stateCode = custFields[ImportFields.IDX_STATE];
+        if (!StringUtils.isEmpty(stateCode)) {
+            streetAddress.setStateCode(stateCode);
+        }
+
+        String zipCode = custFields[ImportFields.IDX_ZIP_CODE];
+        if (!StringUtils.isEmpty(zipCode)) {
+            streetAddress.setZipCode(zipCode);
+        }
+
+        // site
+        SiteInformation siteInfo = acctDto.getSiteInfo();
         if (!StringUtils.isEmpty(custFields[ImportFields.IDX_FEEDER])) {
             siteInfo.setFeeder(custFields[ImportFields.IDX_FEEDER]);
         }
@@ -156,30 +155,30 @@ public class UpdatableAccountConverterImpl implements UpdatableAccountConverter 
         }
         if (!StringUtils.isEmpty(custFields[ImportFields.IDX_TRFM_SIZE])) {
             siteInfo.setTransformerSize(custFields[ImportFields.IDX_TRFM_SIZE]);
-        }		
-		
-		if (custFields[ImportFields.IDX_SUBSTATION].length() > 0) {
-			try {
-				// might be id
-				int subId = Integer.parseInt(custFields[ImportFields.IDX_SUBSTATION]);
-				String subNameFromId = siteInformationDao.getSubstationNameById(subId);
-				siteInfo.setSubstationName(subNameFromId);
-			} catch (NumberFormatException e) {
-				siteInfo.setSubstationName(custFields[ImportFields.IDX_SUBSTATION]);
-			} catch (NotFoundException e) {
-				// don't set
-			}
-		}
-		
-	}
-	
-	@Autowired
-	public void setAccountService(AccountService accountService) {
-		this.accountService = accountService;
-	}
-	
-	@Autowired
-	public void setSiteInformationDao(SiteInformationDao siteInformationDao) {
-		this.siteInformationDao = siteInformationDao;
-	}
+        }
+
+        if (custFields[ImportFields.IDX_SUBSTATION].length() > 0) {
+            try {
+                // might be id
+                int subId = Integer.parseInt(custFields[ImportFields.IDX_SUBSTATION]);
+                String subNameFromId = siteInformationDao.getSubstationNameById(subId);
+                siteInfo.setSubstationName(subNameFromId);
+            } catch (NumberFormatException e) {
+                siteInfo.setSubstationName(custFields[ImportFields.IDX_SUBSTATION]);
+            } catch (NotFoundException e) {
+                // don't set
+            }
+        }
+
+    }
+
+    @Autowired
+    public void setAccountService(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @Autowired
+    public void setSiteInformationDao(SiteInformationDao siteInformationDao) {
+        this.siteInformationDao = siteInformationDao;
+    }
 }
