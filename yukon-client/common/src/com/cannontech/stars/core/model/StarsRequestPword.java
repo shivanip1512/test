d7f85b3a0aa233815966af90ef58f15a32e3bb43
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2015-02-05 20:54:54 +0000

    YUK-14020 STARS Import taking a long time  - formatting

--- yukon-client/common/src/com/cannontech/stars/core/model/StarsRequestPword.java
+++ yukon-client/common/src/com/cannontech/stars/core/model/StarsRequestPword.java
@@ -20,140 +20,124 @@ import com.cannontech.stars.database.data.lite.LiteStarsEnergyCompany;
 import com.cannontech.stars.energyCompany.model.EnergyCompany;
 
 /**
- * @author rneuharth
- *
- * Used to handle requests for passwords and send the requests to 
- * the email address of the Energy Company that owns this Contact, YukonUser
- * or Customer. If we can not determine which Energy Company to use, we send
- * the request to the MasterMail, supplied in:
- * 
+ * Used to handle requests for passwords and send the requests
+ * to the email address of the Energy Company that owns this Contact,
+ * YukonUser or Customer. If we can not determine which Energy Company
+ * to use, we send the request to the MasterMail, supplied in:
  */
 public class StarsRequestPword extends RequestPword {
-	private String accNum = null;
-	private StarsCustAccountInformationDao starsCustAccountInformationDao;
-	private StarsSearchService starsSearchService;
-	
-	public StarsRequestPword( String userName, String email, String fName, String lName, String accNum, 
-	                          StarsCustAccountInformationDao starsCustAccountInformationDao,
-	                          StarsSearchService starsSearchService) {
-		super(
-			userName,
-			email,
-			fName,
-			lName );	
+    private String accNum = null;
+    private StarsCustAccountInformationDao starsCustAccountInformationDao;
+    private StarsSearchService starsSearchService;
 
-		this.accNum = accNum;
+    public StarsRequestPword(String userName, String email, String fName,
+            String lName, String accNum,
+            StarsCustAccountInformationDao starsCustAccountInformationDao,
+            StarsSearchService starsSearchService) {
+        super(userName, email, fName, lName);
 
-		String[] tParams = new String[ allParams.length + 1 ];
-		tParams[tParams.length-1] = accNum;
+        this.accNum = accNum;
 
-		System.arraycopy( allParams, 0, tParams, 0, allParams.length );
-		allParams = tParams;
-		
-		this.starsCustAccountInformationDao = starsCustAccountInformationDao;
-		this.starsSearchService = starsSearchService;
-	}
-	
-	@Override
+        String[] tParams = new String[allParams.length + 1];
+        tParams[tParams.length - 1] = accNum;
+
+        System.arraycopy(allParams, 0, tParams, 0, allParams.length);
+        allParams = tParams;
+
+        this.starsCustAccountInformationDao = starsCustAccountInformationDao;
+        this.starsSearchService = starsSearchService;
+    }
+
+    @Override
     public void doRequest() {
-		try {
-			//uses STARS functionality
-			if( accNum != null ) {
-				//we may continue after this, remove all the stored data
-				foundData.clear();
-				
-				List engrComps = StarsDatabaseCache.getInstance().getAllEnergyCompanies();
-				LiteStarsEnergyCompany eComp = null;
-				ArrayList allCustAccts = new ArrayList(8);
+        try {
+            // uses STARS functionality
+            if (accNum != null) {
+                // we may continue after this, remove all the stored data
+                foundData.clear();
 
-				for( int i = 0; i < engrComps.size(); i++ ) {
-					LiteStarsEnergyCompany lsec = (LiteStarsEnergyCompany)engrComps.get(i);
+                List<LiteStarsEnergyCompany> engrComps = StarsDatabaseCache.getInstance().getAllEnergyCompanies();
+                LiteStarsEnergyCompany eComp = null;
+                ArrayList<LiteAccountInfo> allCustAccts = new ArrayList<LiteAccountInfo>(8);
 
-					LiteAccountInfo lCustInfo = null;
-					List<Integer> accounts = starsSearchService.searchAccountByAccountNumber(lsec, accNum, false, true);
-					
-					lCustInfo = searchForMatchingAccount(accounts, lsec);
-					
-					if( lCustInfo != null ) {
-						allCustAccts.add( lCustInfo );
-						eComp = lsec;
-					}
-				}
-					
-				if( allCustAccts.size() == 1 ) {
-					//only 1 found, this is good
-					LiteAccountInfo lCustInf = 
-						(LiteAccountInfo)allCustAccts.get(0);
+                for (LiteStarsEnergyCompany lsec : engrComps) {
 
-					LiteContact lc = YukonSpringHook.getBean(ContactDao.class).getContact( lCustInf.getCustomer().getPrimaryContactID() );
-					if (lc != null) {
-    					LiteYukonUser user = YukonSpringHook.getBean(YukonUserDao.class).getLiteYukonUser( lc.getLoginID() );
-    					
-    					foundData.add( " Username: " + user.getUsername() );					
-    					foundData.add( " Contact Name: " + lc.getContFirstName() + " " + lc.getContLastName() );
-					}
-					
-					//we must get the Yukon lite energy company for the stars lite energy company
-					EnergyCompany energyCompany =
-						YukonSpringHook.getBean(EnergyCompanyDao.class).getEnergyCompany(eComp.getEnergyCompanyId());
+                    LiteAccountInfo lCustInfo = null;
+                    List<Integer> accounts = starsSearchService.searchAccountByAccountNumber(lsec, accNum, false, true);
 
-					processEnergyCompanies(Collections.singletonList(energyCompany));
-				}
-				else if( allCustAccts.size() < 1 ) {
-					setState( RET_FAILED, "NO_ACCOUNT" );					
-				}
-				else {
-					setState( RET_FAILED, "MULTIPLE_ACCOUNTS" );
-					subject = "WebMaster: " + subject;
-					foundData.add( " " + getResultString() );
-					foundData.add( " Number of Account Numbers for this Account: " + allCustAccts.size() );
-					for( int i = 0; i < allCustAccts.size(); i++ ) {
-						LiteAccountInfo lCstInfo =
-							(LiteAccountInfo)allCustAccts.get(i);
-						
-						foundData.add( "   Account # " + i + ": " + lCstInfo.getCustomerAccount().getAccountNumber() );
-					}
+                    lCustInfo = searchForMatchingAccount(accounts, lsec);
 
-					sendEmails( new String[] { masterMail }, genBody() );
-				}
-			}
-			
-			//try the parents functionality
-			if( getState() != RET_SUCCESS ) {
-				super.doRequest();
-			}
-		}
-        //TODO: This whole servlet should be overhauled to improve exception handling.  For now, will just
-        //change the text of the error message.
-		catch( Exception e ) {
-			//send this request with all its data to CTI
-			setState( RET_FAILED, "DOES_NOT_MATCH" );
-			subject = "WebMaster: " + subject;
+                    if (lCustInfo != null) {
+                        allCustAccts.add(lCustInfo);
+                        eComp = lsec;
+                    }
+                }
 
-			CTILogger.error( e.getMessage(), e );
-		}
-	}
+                if (allCustAccts.size() == 1) {
+                    // only 1 found, this is good
+                    LiteAccountInfo lCustInf = (LiteAccountInfo) allCustAccts.get(0);
 
-	private LiteAccountInfo searchForMatchingAccount(List<Integer> accounts, LiteStarsEnergyCompany lsec) {
-	    LiteAccountInfo lCustInfo = null;
-	    for(Integer accountId : accounts) {
+                    LiteContact lc = YukonSpringHook.getBean(ContactDao.class).getContact(lCustInf.getCustomer().getPrimaryContactID());
+                    if (lc != null) {
+                        LiteYukonUser user = YukonSpringHook.getBean(YukonUserDao.class).getLiteYukonUser(lc.getLoginID());
+
+                        foundData.add(" Username: " + user.getUsername());
+                        foundData.add(" Contact Name: " + lc.getContFirstName() + " " + lc.getContLastName());
+                    }
+
+                    // we must get the Yukon lite energy company for the stars lite energy company
+                    EnergyCompany energyCompany = YukonSpringHook.getBean(EnergyCompanyDao.class).getEnergyCompany(eComp.getEnergyCompanyId());
+
+                    processEnergyCompanies(Collections.singletonList(energyCompany));
+                } else if (allCustAccts.size() < 1) {
+                    setState(RET_FAILED, "NO_ACCOUNT");
+                } else {
+                    setState(RET_FAILED, "MULTIPLE_ACCOUNTS");
+                    subject = "WebMaster: " + subject;
+                    foundData.add(" " + getResultString());
+                    foundData.add(" Number of Account Numbers for this Account: " + allCustAccts.size());
+
+                    for (LiteAccountInfo lCstInfo : allCustAccts) {
+                        foundData.add("   Account #: " + lCstInfo.getCustomerAccount().getAccountNumber());
+                    }
+                    sendEmails(new String[] { masterMail }, genBody());
+                }
+            }
+
+            // try the parents functionality
+            if (getState() != RET_SUCCESS) {
+                super.doRequest();
+            }
+        }
+        // TODO: This whole servlet should be overhauled to improve exception
+        // handling. For now, will just change the text of the error message.
+        catch (Exception e) {
+            // send this request with all its data to CTI
+            setState(RET_FAILED, "DOES_NOT_MATCH");
+            subject = "WebMaster: " + subject;
+            CTILogger.error(e.getMessage(), e);
+        }
+    }
+
+    private LiteAccountInfo searchForMatchingAccount(List<Integer> accounts, LiteStarsEnergyCompany lsec) {
+        LiteAccountInfo lCustInfo = null;
+        for (Integer accountId : accounts) {
             lCustInfo = starsCustAccountInformationDao.getById(accountId, lsec.getEnergyCompanyId());
             return lCustInfo;
         }
-	    return null;
-	}
-	
-	@Override
+        return null;
+    }
+
+    @Override
     protected List<EnergyCompany> processContact(LiteContact lCont_) {
-		LiteCustomer liteCust = YukonSpringHook.getBean(ContactDao.class).getCustomer( lCont_.getContactID() );
-		
-		if (liteCust.getEnergyCompanyID() != -1) {
-			EnergyCompany energyCompany = 
-			        YukonSpringHook.getBean(EnergyCompanyDao.class).getEnergyCompany(liteCust.getEnergyCompanyID());
-			return Collections.singletonList(energyCompany);
-		}
-		
-		// Try the parent's functionality
-		return super.processContact( lCont_ );
-	}
+        LiteCustomer liteCust = YukonSpringHook.getBean(ContactDao.class).getCustomer(lCont_.getContactID());
+
+        if (liteCust.getEnergyCompanyID() != -1) {
+            EnergyCompany energyCompany = YukonSpringHook.getBean(EnergyCompanyDao.class).getEnergyCompany(liteCust.getEnergyCompanyID());
+            return Collections.singletonList(energyCompany);
+        }
+
+        // Try the parent's functionality
+        return super.processContact(lCont_);
+    }
 }
