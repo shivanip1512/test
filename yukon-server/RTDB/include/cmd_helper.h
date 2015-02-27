758972a3d6b3840453d8f02f2591d9ae62e9a1e1
Fisher, Matthew S <MatthewSFisher@Eaton.com>
2015-02-27 14:03:34 +0000

    YUK-12997 Add 'getvalue lp channel x' default command Moved validate(Condition()) helpers into cmd_helper.h

---
+++ yukon-server/RTDB/include/cmd_helper.h
@@ -0,0 +1,110 @@
+#pragma once
+
+#include "cmd_device.h"
+
+#include <string>
+#include <sstream>
+
+#include <boost/scoped_ptr.hpp>
+
+namespace Cti {
+namespace Devices {
+namespace Commands {
+
+
+/**
+ * Verify condition and save error description if condition is false
+ */
+class Condition
+{
+public:
+
+    Condition( bool valid_, YukonError_t errorCode_  )
+        :   valid(valid_),
+            errorCode(errorCode_)
+    {
+        if( ! valid )
+        {
+            description.reset( new std::ostringstream );
+        }
+    }
+
+    template <typename T>
+    Condition& operator<<(T & item)
+    {
+        return insert<T &>(item);
+    }
+
+    template <typename T>
+    Condition& operator<<(const T & item)
+    {
+        return insert<const T &>(item);
+    }
+
+    // insert unsigned char as unsigned
+    Condition& operator<<(unsigned char val)
+    {
+        return insert<unsigned>(val);
+    }
+
+    operator bool() const
+    {
+        return valid;
+    }
+
+    YukonError_t getErrorCode() const
+    {
+        return errorCode;
+    }
+
+    std::string getErrorDescription() const
+    {
+        if( ! description )
+        {
+            return std::string();
+        }
+
+        return description->str();
+    }
+
+private:
+
+    const bool valid;
+    const YukonError_t errorCode;
+
+    boost::scoped_ptr<std::ostringstream> description;
+
+    template <typename T>
+    inline Condition& insert( T item )
+    {
+        if( description )
+        {
+            *description << item;
+        }
+
+        return *this;
+    }
+};
+
+/**
+ * Throw Command::CommandException if the condition is false
+ *
+ * Usage Example :
+ *
+ * validate( Condition( parameter == validParameter, BADPARAM )
+ *           << "parameter is invalid (" << parameter << ")" );
+ */
+inline void validate( const Condition & condition )
+{
+    if( ! condition )
+    {
+        throw DeviceCommand::CommandException( condition.getErrorCode(),
+                                               condition.getErrorDescription() );
+    }
+}
+
+
+}
+}
+}
+
--- yukon-server/RTDB/include/cmd_rfn_helper.h
+++ yukon-server/RTDB/include/cmd_rfn_helper.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "cmd_helper.h"
 #include "cmd_rfn.h"
 #include <string>
 #include <sstream>
@@ -12,98 +13,6 @@ namespace Commands {
 
 
 boost::optional<std::string> findDescriptionForAscAsq( const unsigned char asc, const unsigned char asq );
-
-
-/**
- * Verify condition and save error description if condition is false
- */
-class Condition
-{
-public:
-
-    Condition( bool valid_, YukonError_t errorCode_  )
-        :   valid(valid_),
-            errorCode(errorCode_)
-    {
-        if( ! valid )
-        {
-            description.reset( new std::ostringstream );
-        }
-    }
-
-    template <typename T>
-    Condition& operator<<(T & item)
-    {
-        return insert<T &>(item);
-    }
-
-    template <typename T>
-    Condition& operator<<(const T & item)
-    {
-        return insert<const T &>(item);
-    }
-
-    // insert unsigned char as unsigned
-    Condition& operator<<(unsigned char val)
-    {
-        return insert<unsigned>(val);
-    }
-
-    operator bool() const
-    {
-        return valid;
-    }
-
-    YukonError_t getErrorCode() const
-    {
-        return errorCode;
-    }
-
-    std::string getErrorDescription() const
-    {
-        if( ! description )
-        {
-            return std::string();
-        }
-
-        return description->str();
-    }
-
-private:
-
-    const bool valid;
-    const YukonError_t errorCode;
-
-    boost::scoped_ptr<std::ostringstream> description;
-
-    template <typename T>
-    inline Condition& insert( T item )
-    {
-        if( description )
-        {
-            *description << item;
-        }
-
-        return *this;
-    }
-};
-
-/**
- * Throw RfnCommand::CommandException if the condition is false
- *
- * Usage Example :
- *
- * validate( Condition( parameter == validParameter, BADPARAM )
- *           << "parameter is invalid (" << parameter << ")" );
- */
-inline void validate( const Condition & condition )
-{
-    if( ! condition )
-    {
-        throw RfnCommand::CommandException( condition.getErrorCode(),
-                                            condition.getErrorDescription() );
-    }
-}
 
 
 }
