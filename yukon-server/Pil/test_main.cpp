c1e8d1a4010d7c78256586d7f4640df7d57bb4eb
Fisher, Matthew S <MatthewSFisher@Eaton.com>
2015-04-01 19:49:14 +0000

    Replicated OverrideActiveMQConnectionManager from CapControl\test_main.cpp

--- yukon-server/Pil/test_main.cpp
+++ yukon-server/Pil/test_main.cpp
@@ -2,6 +2,8 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include "amq_connection.h"
+
 //  hack to get BOOST_CHECK_EQUAL_COLLECTIONS to print unsigned char as integer
 namespace std {
 
@@ -11,3 +13,41 @@ ostream &operator<<( ostream &os, const unsigned char &uc ) {
 
 }
 
+// Close all yukon messaging connections when this object is destroyed
+Cti::Messaging::AutoCloseAllConnections g_autoCloseAllConnections;
+
+namespace Cti {
+namespace Messaging {
+
+extern IM_EX_MSG std::auto_ptr<ActiveMQConnectionManager> gActiveMQConnection;
+
+}
+}
+
+struct test_ActiveMQConnectionManager : Cti::Messaging::ActiveMQConnectionManager
+{
+    test_ActiveMQConnectionManager(std::string uri) :
+        ActiveMQConnectionManager(uri)
+    {
+    }
+
+    void enqueueOutgoingMessage(const Cti::Messaging::ActiveMQ::Queues::OutboundQueue &queueId, Cti::Messaging::StreamableMessage::auto_type message, boost::optional<SerializedMessageCallback> callback) override
+    {
+        //  delete message
+    }
+    void enqueueOutgoingMessage(const Cti::Messaging::ActiveMQ::Queues::OutboundQueue &queueId, const Cti::Messaging::ActiveMQConnectionManager::SerializedMessage &message, boost::optional<SerializedMessageCallback> callback) override
+    {
+        //  delete message
+    }
+};
+
+struct OverrideActiveMQConnectionManager
+{
+    OverrideActiveMQConnectionManager()
+    {
+        Cti::Messaging::gActiveMQConnection.reset(
+           new test_ActiveMQConnectionManager("0.0.0.0:0"));
+    }
+};
+
+BOOST_GLOBAL_FIXTURE( OverrideActiveMQConnectionManager );
