14d9a36406e5e0b137409a26fd602070f2a85b6d
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2015-05-06 18:20:10 +0000

    YUK-14309  Copying Point Type in dbEditor Causes Uncaught Exception  - added checks to ignore the hot key strokes for disabled menus.

--- yukon-client/dbeditor/src/com/cannontech/dbeditor/menu/EditMenu.java
+++ yukon-client/dbeditor/src/com/cannontech/dbeditor/menu/EditMenu.java
@@ -3,128 +3,114 @@ package com.cannontech.dbeditor.menu;
 /**
  * This type was created in VisualAge.
  */
+import java.awt.Event;
 import java.awt.KeyboardFocusManager;
 import java.awt.event.KeyEvent;
 
 import javax.swing.JMenuItem;
 import javax.swing.JSeparator;
+import javax.swing.KeyStroke;
 
 import com.cannontech.common.gui.util.CTIKeyEventDispatcher;
 
-public class EditMenu extends javax.swing.JMenu 
-{
-	public JMenuItem editMenuItem;
-	public JMenuItem copyMenuItem;
-	public JMenuItem changeTypeMenuItem;
-	public JMenuItem deleteMenuItem;
-	public JMenuItem searchMenuItem;
+public class EditMenu extends javax.swing.JMenu {
+    public JMenuItem editMenuItem;
+    public JMenuItem copyMenuItem;
+    public JMenuItem changeTypeMenuItem;
+    public JMenuItem deleteMenuItem;
+    public JMenuItem searchMenuItem;
 
-	private JSeparator separator1;
-/**
- * EditMenu constructor comment.
- */
-public EditMenu() {
-	super();
-	initialize();
-}
-/**
- * This method was created in VisualAge.
- */
-private void initialize() 
-{
-	java.awt.Font font = new java.awt.Font("dialog", 0, 14);
+    private JSeparator separator1;
 
-	editMenuItem = new JMenuItem("Edit...");
-	editMenuItem.setFont( font );
-	editMenuItem.setMnemonic('E');
-	editMenuItem.setAccelerator(javax.swing.KeyStroke.getKeyStroke( 
-													java.awt.event.KeyEvent.VK_E,
-													java.awt.Event.CTRL_MASK));
+    public EditMenu() {
+        super();
+        initialize();
+    }
 
-	copyMenuItem = new JMenuItem("Copy...");
-	copyMenuItem.setFont( font );
-	copyMenuItem.setMnemonic('C');
-	copyMenuItem.setEnabled(true);
-	copyMenuItem.setAccelerator(javax.swing.KeyStroke.getKeyStroke( 
-													java.awt.event.KeyEvent.VK_C,
-													java.awt.Event.CTRL_MASK));
+    private void initialize() {
+        java.awt.Font font = new java.awt.Font("dialog", 0, 14);
 
-	changeTypeMenuItem = new JMenuItem("Change Type...");
-	changeTypeMenuItem.setFont( font );
-	changeTypeMenuItem.setMnemonic('T');
-	changeTypeMenuItem.setEnabled(true);
-	changeTypeMenuItem.setAccelerator(javax.swing.KeyStroke.getKeyStroke( 
-													java.awt.event.KeyEvent.VK_T,
-													java.awt.Event.CTRL_MASK));
+        editMenuItem = new JMenuItem("Edit...");
+        editMenuItem.setFont(font);
+        editMenuItem.setMnemonic('E');
+        editMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, Event.CTRL_MASK));
 
-	deleteMenuItem = new JMenuItem("Delete");
-	deleteMenuItem.setFont( font );
-	deleteMenuItem.setMnemonic('d');
-	deleteMenuItem.setAccelerator(javax.swing.KeyStroke.getKeyStroke( 
-													java.awt.event.KeyEvent.VK_DELETE,
-													java.awt.Event.CTRL_MASK));
+        copyMenuItem = new JMenuItem("Copy...");
+        copyMenuItem.setFont(font);
+        copyMenuItem.setMnemonic('C');
+        copyMenuItem.setEnabled(true);
+        copyMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, Event.CTRL_MASK));
 
-	separator1 = new JSeparator();
+        changeTypeMenuItem = new JMenuItem("Change Type...");
+        changeTypeMenuItem.setFont(font);
+        changeTypeMenuItem.setMnemonic('T');
+        changeTypeMenuItem.setEnabled(true);
+        changeTypeMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_T, Event.CTRL_MASK));
 
-	searchMenuItem = new JMenuItem("Find...");
-	searchMenuItem.setFont( font );
-	searchMenuItem.setMnemonic('f');
-	searchMenuItem.setEnabled(true);
-	searchMenuItem.setAccelerator(javax.swing.KeyStroke.getKeyStroke( 
-													java.awt.event.KeyEvent.VK_F,
-													java.awt.Event.CTRL_MASK));
-		
-	setText("Edit");
-	setFont( font );
-	setMnemonic('e');
+        deleteMenuItem = new JMenuItem("Delete");
+        deleteMenuItem.setFont(font);
+        deleteMenuItem.setMnemonic('d');
+        deleteMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_DELETE, Event.CTRL_MASK));
 
-	add( editMenuItem );
-	add( copyMenuItem );
-	add( changeTypeMenuItem );
-	add( deleteMenuItem );
-	add( separator1 );
-	add( searchMenuItem );
-	
-	/* 
-	 * This way to handle accelerators was changed to work with JRE 1.4. The accelerator
-	 * event would always get consumed by the component focus was in. This ensures that
-	 * accelerator fires the correct event ONLY (that is why true is returned on after
-	 * each click). We keep the above accelerators set for display purposes.
-	 */
-	KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(
-		new CTIKeyEventDispatcher()
-		{
-			public boolean handleKeyEvent(KeyEvent e)
-			{
-				//do the checks of the keystrokes here
-				if( e.getKeyCode() == KeyEvent.VK_C && e.isControlDown() )
-				{
-					copyMenuItem.doClick();
-					return true;
-				}
-				else if( e.getKeyCode() == KeyEvent.VK_E && e.isControlDown() )
-				{
-					editMenuItem.doClick();
-					return true;
-				}
-				else if( e.getKeyCode() == KeyEvent.VK_F && e.isControlDown() )
-				{
-					searchMenuItem.doClick();
-					return true;
-				}
-				else if( e.getKeyCode() == KeyEvent.VK_DELETE && e.isControlDown() )
-				{
-					deleteMenuItem.doClick();
-					return true;
-				}
-				else if( e.getKeyCode() == KeyEvent.VK_T && e.isControlDown() )
-				{
-					changeTypeMenuItem.doClick();
-					return true;
-				}
-				//its this the last handling of the KeyEvent in this KeyboardFocusManager?
-				return false;
-			}
-		});	
-}
+        separator1 = new JSeparator();
+
+        searchMenuItem = new JMenuItem("Find...");
+        searchMenuItem.setFont(font);
+        searchMenuItem.setMnemonic('f');
+        searchMenuItem.setEnabled(true);
+        searchMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F, Event.CTRL_MASK));
+
+        setText("Edit");
+        setFont(font);
+        setMnemonic('e');
+
+        add(editMenuItem);
+        add(copyMenuItem);
+        add(changeTypeMenuItem);
+        add(deleteMenuItem);
+        add(separator1);
+        add(searchMenuItem);
+
+        /*
+         * This way to handle accelerators was changed to work with JRE 1.4. The
+         * accelerator event would always get consumed by the component focus
+         * was in. This ensures that accelerator fires the correct event ONLY
+         * (that is why true is returned on after each click). We keep the above
+         * accelerators set for display purposes.
+         */
+        KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new CTIKeyEventDispatcher() {
+            @Override
+            public boolean handleKeyEvent(KeyEvent e) {
+                // do the checks of the keystrokes here
+                if (e.getKeyCode() == KeyEvent.VK_C && e.isControlDown()) {
+                    if (copyMenuItem.isEnabled()) {
+                        copyMenuItem.doClick();
+                        return true;
+                    }
+                } else if (e.getKeyCode() == KeyEvent.VK_E && e.isControlDown()) {
+                    if (editMenuItem.isEnabled()) {
+                        editMenuItem.doClick();
+                        return true;
+                    }
+                } else if (e.getKeyCode() == KeyEvent.VK_F && e.isControlDown()) {
+                    if (searchMenuItem.isEnabled()) {
+                        searchMenuItem.doClick();
+                        return true;
+                    }
+                } else if (e.getKeyCode() == KeyEvent.VK_DELETE && e.isControlDown()) {
+                    if (deleteMenuItem.isEnabled()) {
+                        deleteMenuItem.doClick();
+                        return true;
+                    }
+                } else if (e.getKeyCode() == KeyEvent.VK_T && e.isControlDown()) {
+                    if (changeTypeMenuItem.isEnabled()) {
+                        changeTypeMenuItem.doClick();
+                        return true;
+                    }
+                }
+                // its this the last handling of the KeyEvent in this KeyboardFocusManager?
+                return false;
+            }
+        });
+    }
 }
