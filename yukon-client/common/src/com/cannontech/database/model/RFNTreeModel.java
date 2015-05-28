ea666fa41464d3681858c70f16bcd1eca30d31db
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2015-05-28 18:36:13 +0000

    YUK-14361 Creating or Editing a Gateway in dbEditor Causes Error  - added check for paoTag DB_EDITOR_INCOMPATIBLE for RF Mesh tree view.

--- yukon-client/common/src/com/cannontech/database/model/RFNTreeModel.java
+++ yukon-client/common/src/com/cannontech/database/model/RFNTreeModel.java
@@ -3,8 +3,13 @@ package com.cannontech.database.model;
 import com.cannontech.common.pao.PaoCategory;
 import com.cannontech.common.pao.PaoClass;
 import com.cannontech.common.pao.PaoType;
+import com.cannontech.common.pao.definition.dao.PaoDefinitionDao;
+import com.cannontech.common.pao.definition.model.PaoTag;
+import com.cannontech.spring.YukonSpringHook;
 
 public class RFNTreeModel extends AbstractDeviceTreeModel {
+
+    private static final PaoDefinitionDao paoDefinitionDao = YukonSpringHook.getBean("paoDefinitionDao", PaoDefinitionDao.class);
 
     public RFNTreeModel() {
         super(new DBTreeNode("RF Mesh"));
@@ -15,6 +20,9 @@ public class RFNTreeModel extends AbstractDeviceTreeModel {
      */
     @Override
     public boolean isDeviceValid(PaoCategory paoCategory, PaoClass paoClass, PaoType paoType) {
+        if (paoDefinitionDao.isTagSupported(paoType, PaoTag.DB_EDITOR_INCOMPATIBLE)) {
+            return false;
+        }
         return (paoClass == PaoClass.RFMESH);
     }
 }
