fe66625b8a9314c7dc1b5afbe7600e8db2fdf127
DeZeeuw, Garrett <>
2015-06-10 19:30:55 +0000

    CTI Database signature - trunk - 6.4.3

--- yukon-database/DBUpdates/sqlserver/6.4_3.sql
+++ yukon-database/DBUpdates/sqlserver/6.4_3.sql
@@ -56,4 +56,4 @@ WHERE Type = 'RF Gateway 2';
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
 /**************************************************************/
-/*INSERT INTO CTIDatabase VALUES ('6.4', '31-MAY-2015', 'Latest Update', 3, GETDATE());*/
+INSERT INTO CTIDatabase VALUES ('6.4', '31-MAY-2015', 'Latest Update', 3, GETDATE());
