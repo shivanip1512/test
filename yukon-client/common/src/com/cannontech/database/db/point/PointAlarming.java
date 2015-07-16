d26b194e0ec243955a4ad96e949b5dc944456734
Pipp, Joseph <>
2015-07-16 17:58:09 +0000

    YUK-14360 Replace Cap Control Point Editor (JSF) -Formatting

--- yukon-client/common/src/com/cannontech/database/db/point/PointAlarming.java
+++ yukon-client/common/src/com/cannontech/database/db/point/PointAlarming.java
@@ -1,276 +1,201 @@
 package com.cannontech.database.db.point;
 
+import java.sql.SQLException;
 
+import com.cannontech.database.db.DBPersistent;
 
-/**
- * This type was created in VisualAge.
- */
-public class PointAlarming extends com.cannontech.database.db.DBPersistent 
-{
-	public static final int NONE_NOTIFICATIONID = 1;
-//	public static final int NONE_LOCATIONID = 0;
-	public static final String NONE_VALUE_STRING = "(none)";
-	public static final String EXCLUDE_NOTIFY_VALUE_STRING = "Exclude Notify";
-	public static final String AUTO_ACK_VALUE_STRING = "Auto Ack";
-	public static final String BOTH_OPTIONS_VALUE_STRING = "Exclude Notify & Auto Ack";
-	public static final String DEFAULT_EXCLUDE_NOTIFY = "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN";
-	private static String tmp = new String();
+public class PointAlarming extends DBPersistent {
+    
+    public static final int NONE_NOTIFICATIONID = 1;
+    public static final String NONE_VALUE_STRING = "(none)";
+    public static final String EXCLUDE_NOTIFY_VALUE_STRING = "Exclude Notify";
+    public static final String AUTO_ACK_VALUE_STRING = "Auto Ack";
+    public static final String BOTH_OPTIONS_VALUE_STRING = "Exclude Notify & Auto Ack";
+    public static final String DEFAULT_EXCLUDE_NOTIFY = "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN";
+    public static final String DEFAULT_ALARM_STATES;
 
-   
-   public static final int ALARM_STATE_COUNT = 32;
-		
-	// we need to initialize our char mask	
-	static 
-	{
-		for( int i = 0; i < ALARM_STATE_COUNT; i++ )
-			tmp += '\u0001';  // Do not have nulls!! This is the Second char in a ASCII list, int value of 1
-	};	
-	public static final String DEFAULT_ALARM_STATES = tmp;
-	
-	
-	private Integer pointID = null;
-	private String alarmStates = DEFAULT_ALARM_STATES;
-	private String excludeNotifyStates = DEFAULT_EXCLUDE_NOTIFY;
-	private String notifyOnAcknowledge = "N";
-	private Integer notificationGroupID = new Integer(PointAlarming.NONE_NOTIFICATIONID);
+    public static final int ALARM_STATE_COUNT = 32;
 
-	public static final String CONSTRAINT_COLUMNS[] = { "POINTID" };
-	
-	public static final String SETTER_COLUMNS[] = { 
-			"ALARMSTATES", "EXCLUDENOTIFYSTATES", "NOTIFYONACKNOWLEDGE",
-			"NOTIFICATIONGROUPID" };
-	
+    // we need to initialize our char mask
+    static {
+        String tmp = "";
+        for (int i = 0; i < ALARM_STATE_COUNT; i++)
+            tmp += '\u0001'; // Do not have nulls!! This is the Second char in a ASCII list, int value of 1
+        DEFAULT_ALARM_STATES = tmp;
+    }
+    
+    private Integer pointID = null;
+    private String alarmStates = DEFAULT_ALARM_STATES;
+    private String excludeNotifyStates = DEFAULT_EXCLUDE_NOTIFY;
+    private String notifyOnAcknowledge = "N";
+    private Integer notificationGroupID = new Integer(PointAlarming.NONE_NOTIFICATIONID);
 
-	public final static String TABLE_NAME = "PointAlarming";
-/**
- * PointUnit constructor comment.
- */
-public PointAlarming() {
-	super();
-}
-/**
- * PointUnit constructor comment.
- */
-public PointAlarming(Integer pointID, String alarmStates, String excludeNotifyStates, 
-		String notifyOnAcknowledge, Integer notificationGroupID )
-{
-	super();
-	initialize(pointID, alarmStates, excludeNotifyStates, notifyOnAcknowledge, notificationGroupID );
-}
-/**
- * add method comment.
- */
-public void add() throws java.sql.SQLException 
-{
-	Object addValues[] = { getPointID(), getAlarmStates(), getExcludeNotifyStates(), 
-		getNotifyOnAcknowledge(), getNotificationGroupID()};
+    public static final String CONSTRAINT_COLUMNS[] = { "POINTID" };
 
-	add( TABLE_NAME, addValues );
-}
-/**
- * delete method comment.
- */
-public void delete() throws java.sql.SQLException {
+    public static final String SETTER_COLUMNS[] = { "ALARMSTATES", "EXCLUDENOTIFYSTATES", "NOTIFYONACKNOWLEDGE",
+        "NOTIFICATIONGROUPID" };
 
-	delete( TABLE_NAME, CONSTRAINT_COLUMNS[0], getPointID() );
-}
-/**
- * Insert the method's description here.
- * Creation date: (11/9/00 11:45:06 AM)
- * @return java.lang.String
- */
-public java.lang.String getAlarmStates() {
-	return alarmStates;
-}
-/**
- * Insert the method's description here.
- * Creation date: (11/9/00 11:45:06 AM)
- * @return java.lang.String
- */
-public java.lang.String getExcludeNotifyStates() {
-	return excludeNotifyStates;
-}
-/**
- * Insert the method's description here.
- * Creation date: (11/9/00 11:45:06 AM)
- * @return java.lang.Integer
- */
-public java.lang.Integer getNotificationGroupID() {
-	return notificationGroupID;
-}
-/**
- * Insert the method's description here.
- * Creation date: (11/9/00 11:45:06 AM)
- * @return java.lang.String
- */
-public java.lang.String getNotifyOnAcknowledge() {
-	return notifyOnAcknowledge;
-}
+    public final static String TABLE_NAME = "PointAlarming";
 
-public void setNotifyOnAck( boolean notify ) {
-	
-	if( notify ) {
-		if( isNotifyOnClear() ) setNotifyOnAcknowledge( "B" );
-		else setNotifyOnAcknowledge( "A" );
-	}
-	else {
-		if( isNotifyOnClear() ) setNotifyOnAcknowledge( "C" );
-		else setNotifyOnAcknowledge( "N" );
-	}
-}
+    public PointAlarming() {
+        super();
+    }
 
-public void setNotifyOnClear( boolean notify ) {
-	
-	if( notify ) {
-		if( isNotifyOnAck() ) setNotifyOnAcknowledge( "B" );
-		else setNotifyOnAcknowledge( "C" );
-	}
-	else {
-		if( isNotifyOnAck() ) setNotifyOnAcknowledge( "A" );
-		else setNotifyOnAcknowledge( "N" );
-	}
-}
+    public PointAlarming(Integer pointID, String alarmStates, String excludeNotifyStates, String notifyOnAcknowledge,
+            Integer notificationGroupID) {
+        super();
+        setPointID(pointID);
+        setAlarmStates(alarmStates);
+        setExcludeNotifyStates(excludeNotifyStates);
+        setNotifyOnAcknowledge(notifyOnAcknowledge);
+        setNotificationGroupID(notificationGroupID);
+    }
 
-/**
- * Possible values: A, C, B, N
- * A: notify on ACK only
- * C: notify on CLEAR only
- * B: notify on ACK or CLEAR (both)
- * N: notify on none
- */
-public boolean isNotifyOnClear() {	
-	return
-		getNotifyOnAcknowledge().charAt(0) == 'C'
-		|| getNotifyOnAcknowledge().charAt(0) == 'B';
-}
+    public void add() throws SQLException {
+        Object addValues[] =
+            { getPointID(), getAlarmStates(), getExcludeNotifyStates(), getNotifyOnAcknowledge(), getNotificationGroupID() };
 
-public boolean isNotifyOnAck() {	
-	return
-		getNotifyOnAcknowledge().charAt(0) == 'A'
-		|| getNotifyOnAcknowledge().charAt(0) == 'B';
-}
+        add(TABLE_NAME, addValues);
+    }
 
-/**
- * Returns the single char representation for the ExcludeNotify state.
- */
-public static char getExcludeNotifyChar( String notify ) {
-	if( EXCLUDE_NOTIFY_VALUE_STRING.equals(notify) )
-		return 'E';
-	else if( AUTO_ACK_VALUE_STRING.equals(notify) )
-		return  'A';
-	else if( BOTH_OPTIONS_VALUE_STRING.equals(notify) )
-		return 'B';
-	else
-		return 'N';
-}
+    public void delete() throws SQLException {
 
-/**
- * Returns the string representation for the ExcludeNotify char state.
- */
-public static String getExcludeNotifyString( char val ) {
-	if( val == 'E' || val == 'Y' )
-		return EXCLUDE_NOTIFY_VALUE_STRING;
-	else if( val == 'A' )
-		return AUTO_ACK_VALUE_STRING;
-	else if( val == 'B' )
-		return BOTH_OPTIONS_VALUE_STRING;
-	else
-		return NONE_VALUE_STRING;
-}
+        delete(TABLE_NAME, CONSTRAINT_COLUMNS[0], getPointID());
+    }
 
+    public String getAlarmStates() {
+        return alarmStates;
+    }
 
-/**
- * This method was created in VisualAge.
- * @return java.lang.Integer
- */
-public Integer getPointID() {
-	return pointID;
-}
-/**
- * This method was created in VisualAge.
- * @param pointID java.lang.Integer
- * @param units java.lang.String
- * @param logFrequency java.lang.Integer
- * @param defaultValue java.lang.Double
- */
-private void initialize(Integer pointID, String alarmStates, String excludeNotifyStates, 
-		String notifyOnAcknowledge, Integer notificationGroupID )
-{
-	setPointID( pointID );
-	setAlarmStates( alarmStates ) ;
-	setExcludeNotifyStates( excludeNotifyStates );
-	setNotifyOnAcknowledge( notifyOnAcknowledge );
-	setNotificationGroupID( notificationGroupID );
-}
-/**
- * retrieve method comment.
- */
-public void retrieve() throws java.sql.SQLException 
-{
-	Object constraintValues[] = { getPointID() };
+    public String getExcludeNotifyStates() {
+        return excludeNotifyStates;
+    }
 
-	Object results[] = retrieve( SETTER_COLUMNS, TABLE_NAME, CONSTRAINT_COLUMNS, constraintValues );
+    public Integer getNotificationGroupID() {
+        return notificationGroupID;
+    }
 
-	if( results.length == SETTER_COLUMNS.length )
-	{
-		setAlarmStates( (String) results[0] );
-		setExcludeNotifyStates( (String) results[1] );
-		setNotifyOnAcknowledge( (String) results[2] );
-		setNotificationGroupID( (Integer) results[3] );
-	
-	}
-	//else
-		//throw new Error(getClass() + " - Incorrect Number of results retrieved");	
-}
-/**
- * Insert the method's description here.
- * Creation date: (11/9/00 11:45:06 AM)
- * @param newAlarmStates java.lang.String
- */
-public void setAlarmStates(java.lang.String newAlarmStates) {
-	alarmStates = newAlarmStates;
-}
-/**
- * Insert the method's description here.
- * Creation date: (11/9/00 11:45:06 AM)
- * @param newExcludeNotifyStates java.lang.String
- */
-public void setExcludeNotifyStates(java.lang.String newExcludeNotifyStates) {
-	excludeNotifyStates = newExcludeNotifyStates;
-}
-/**
- * Insert the method's description here.
- * Creation date: (11/9/00 11:45:06 AM)
- * @param newNotificationGroupID java.lang.Integer
- */
-public void setNotificationGroupID(java.lang.Integer newNotificationGroupID) {
-	notificationGroupID = newNotificationGroupID;
-}
-/**
- * Insert the method's description here.
- * Creation date: (11/9/00 11:45:06 AM)
- * @param newNotifyOnAcknowledge java.lang.String
- */
-public void setNotifyOnAcknowledge(java.lang.String newNotifyOnAcknowledge) {
-	notifyOnAcknowledge = newNotifyOnAcknowledge;
-}
-/**
- * This method was created in VisualAge.
- * @param newValue java.lang.Integer
- */
-public void setPointID(Integer newValue) {
-	this.pointID = newValue;
-}
-/**
- * update method comment.
- */
-public void update() throws java.sql.SQLException 
-{
-	Object setValues[] = { getAlarmStates(), getExcludeNotifyStates(), 
-				getNotifyOnAcknowledge(), getNotificationGroupID()};
+    public String getNotifyOnAcknowledge() {
+        return notifyOnAcknowledge;
+    }
 
-	Object constraintValues[] = { getPointID() };
+    public void setNotifyOnAck(boolean notify) {
 
-	update( TABLE_NAME, SETTER_COLUMNS, setValues, CONSTRAINT_COLUMNS, constraintValues );
-}
+        if (notify) {
+            if (isNotifyOnClear())
+                setNotifyOnAcknowledge("B");
+            else
+                setNotifyOnAcknowledge("A");
+        } else {
+            if (isNotifyOnClear())
+                setNotifyOnAcknowledge("C");
+            else
+                setNotifyOnAcknowledge("N");
+        }
+    }
+
+    public void setNotifyOnClear(boolean notify) {
+
+        if (notify) {
+            if (isNotifyOnAck())
+                setNotifyOnAcknowledge("B");
+            else
+                setNotifyOnAcknowledge("C");
+        } else {
+            if (isNotifyOnAck())
+                setNotifyOnAcknowledge("A");
+            else
+                setNotifyOnAcknowledge("N");
+        }
+    }
+
+    /**
+     * Possible values: A, C, B, N
+     * A: notify on ACK only
+     * C: notify on CLEAR only
+     * B: notify on ACK or CLEAR (both)
+     * N: notify on none
+     */
+    public boolean isNotifyOnClear() {
+        return getNotifyOnAcknowledge().charAt(0) == 'C' || getNotifyOnAcknowledge().charAt(0) == 'B';
+    }
+
+    public boolean isNotifyOnAck() {
+        return getNotifyOnAcknowledge().charAt(0) == 'A' || getNotifyOnAcknowledge().charAt(0) == 'B';
+    }
+
+    /**
+     * Returns the single char representation for the ExcludeNotify state.
+     */
+    public static char getExcludeNotifyChar(String notify) {
+        if (EXCLUDE_NOTIFY_VALUE_STRING.equals(notify))
+            return 'E';
+        else if (AUTO_ACK_VALUE_STRING.equals(notify))
+            return 'A';
+        else if (BOTH_OPTIONS_VALUE_STRING.equals(notify))
+            return 'B';
+        else
+            return 'N';
+    }
+
+    /**
+     * Returns the string representation for the ExcludeNotify char state.
+     */
+    public static String getExcludeNotifyString(char val) {
+        if (val == 'E' || val == 'Y')
+            return EXCLUDE_NOTIFY_VALUE_STRING;
+        else if (val == 'A')
+            return AUTO_ACK_VALUE_STRING;
+        else if (val == 'B')
+            return BOTH_OPTIONS_VALUE_STRING;
+        else
+            return NONE_VALUE_STRING;
+    }
+
+    public Integer getPointID() {
+        return pointID;
+    }
+
+    public void retrieve() throws SQLException {
+        Object constraintValues[] = { getPointID() };
+
+        Object results[] = retrieve(SETTER_COLUMNS, TABLE_NAME, CONSTRAINT_COLUMNS, constraintValues);
+
+        if (results.length == SETTER_COLUMNS.length) {
+            setAlarmStates((String) results[0]);
+            setExcludeNotifyStates((String) results[1]);
+            setNotifyOnAcknowledge((String) results[2]);
+            setNotificationGroupID((Integer) results[3]);
+
+        }
+    }
+
+    public void setAlarmStates(String newAlarmStates) {
+        alarmStates = newAlarmStates;
+    }
+
+    public void setExcludeNotifyStates(String newExcludeNotifyStates) {
+        excludeNotifyStates = newExcludeNotifyStates;
+    }
+
+    public void setNotificationGroupID(Integer newNotificationGroupID) {
+        notificationGroupID = newNotificationGroupID;
+    }
+
+    public void setNotifyOnAcknowledge(String newNotifyOnAcknowledge) {
+        notifyOnAcknowledge = newNotifyOnAcknowledge;
+    }
+
+    public void setPointID(Integer newValue) {
+        this.pointID = newValue;
+    }
+
+    public void update() throws SQLException {
+        Object setValues[] =
+            { getAlarmStates(), getExcludeNotifyStates(), getNotifyOnAcknowledge(), getNotificationGroupID() };
+
+        Object constraintValues[] = { getPointID() };
+
+        update(TABLE_NAME, SETTER_COLUMNS, setValues, CONSTRAINT_COLUMNS, constraintValues);
+    }
 }
