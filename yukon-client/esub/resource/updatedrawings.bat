11f8a8fac11fa307c8e743fb4146cc16165a5eed
PSPL-Software Build Account <>
2015-08-12 04:56:40 -0400

    YUK-13333 - asmitav - removed esub project

--- yukon-client/esub/resource/updatedrawings.bat
+++
@@ -1,5 +0,0 @@
-rem updatedrawings.bat
-rem Starting in a given directory it recursively loads and saves all the jlx files it finds.
-rem Run this file from the client bin directory
-call setjavapath.bat
-java -classpath .;%YUKON_BASE%/server/web;esub-editor.jar;common.jar;yukonappserver.jar;jloox2.0.2.jar com.cannontech.esub.tool.UpdateDrawings %1 %2
--- yukon-client/esub/src/com/cannontech/esub/util/DrawingUpdater.java
+++
@@ -1,9 +0,0 @@
-package com.cannontech.esub.util;
-
-public interface DrawingUpdater {
-
-    public  void updateDrawing();
-
-    public  void setUpdateGraphs(boolean b);
-
-}
