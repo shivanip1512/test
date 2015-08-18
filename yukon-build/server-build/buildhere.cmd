3c056794edc268a667997f8bc1a0a212cf730595
Johnson, Wayne <>
2015-08-18 14:01:07 +0000

    Fix issue with setting version numbers when BUILD_ID and/or BUILD_NUMBER are set.

--- yukon-build/server-build/buildhere.cmd
+++ yukon-build/server-build/buildhere.cmd
@@ -188,10 +188,12 @@ for /f "tokens=4 delims=_" %%p in ("%my_version%") do (
         set my_version_build=%%p
 )
 
-rem Get the SVN global revison number
-FOR /F "tokens=* USEBACKQ" %%F IN (`svnversion %~dp0`) DO (
-SET my_version_svn=%%F
-)
+if not "%BUILD_NUMBER%" == "" goto haveBuildNumber
+  rem Get BUILD_NUMBER to the SVN global revison number if it's not already set.
+  FOR /F "tokens=* USEBACKQ" %%F IN (`svnversion %~dp0`) DO (
+  SET BUILD_NUMBER=%%F
+  )
+:haveBuildNumber
 
 echo +---------------------------------------
 echo ^|
@@ -225,17 +227,17 @@ if "%updateVersion%" == "0" (
   for /f "tokens=2* delims== " %%p in (%versionFileName%) do (
       if "%%p" == "D_FILE_VERSION_STR" (
           call :trim %%q rev
-          if not "!rev!" == "%my_version_maj%.%my_version_min%.%my_version_rev%.%my_version_svn%" set updateVersion=1
+          if not "!rev!" == "%my_version_maj%.%my_version_min%.%my_version_rev%.%BUILD_NUMBER%" set updateVersion=1
       )
   )
 )
 
 if "%updateVersion%" == "1" (
   echo Updating %versionFileName%
-  echo #define D_FILE_VERSION %my_version_maj%,%my_version_min%,%my_version_rev%,%my_version_svn% >%versionFileName%
-  echo #define D_FILE_VERSION_STR %my_version_maj%.%my_version_min%.%my_version_rev%.%my_version_svn% >>%versionFileName%
-  echo #define D_PRODUCT_VERSION %my_version_maj%,%my_version_min%,%my_version_rev%,%my_version_svn% >>%versionFileName%
-  echo #define D_PRODUCT_VERSION_STR %my_version_maj%.%my_version_min%.%my_version_rev%.%my_version_svn% >>%versionFileName%
+  echo #define D_FILE_VERSION %my_version_maj%,%my_version_min%,%my_version_rev%,%BUILD_NUMBER% >%versionFileName%
+  echo #define D_FILE_VERSION_STR %my_version_maj%.%my_version_min%.%my_version_rev%.%BUILD_NUMBER% >>%versionFileName%
+  echo #define D_PRODUCT_VERSION %my_version_maj%,%my_version_min%,%my_version_rev%,%BUILD_NUMBER% >>%versionFileName%
+  echo #define D_PRODUCT_VERSION_STR %my_version_maj%.%my_version_min%.%my_version_rev%.%BUILD_NUMBER% >>%versionFileName%
   echo.>>%versionFileName%
 
   echo #define BUILD_VERSION %my_version_maj%.%my_version_min% ^(build %my_version_build%^) >>%versionFileName%
