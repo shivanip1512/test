5e80709263d976bdd164d2266face478443e5615
Fisher, Matthew S <MatthewSFisher@Eaton.com>
2015-09-24 12:26:15 +0000

    YUK-14545 C++ Porter service for retrieving dynamic device info YUKRV-8889 Whitespace fix

--- yukon-shared/src/main/resources/messages/thrift/PorterDynamicPaoInfo.thrift
+++ yukon-shared/src/main/resources/messages/thrift/PorterDynamicPaoInfo.thrift
@@ -2,7 +2,7 @@ namespace cpp Cti.Messaging.Serialization.Thrift
 namespace java com.cannontech.messaging.serialization.thrift.generated
 
 enum DynamicPaoInfoKeys {
-	RFN_VOLTAGE_PROFILE_ENABLED_UNTIL,
+    RFN_VOLTAGE_PROFILE_ENABLED_UNTIL,
     RFN_VOLTAGE_PROFILE_INTERVAL
 }
 
@@ -12,9 +12,9 @@ struct PorterDynamicPaoInfoRequest {
 }
 
 union DynamicPaoInfoTypes {
-	1: i64    _integer;
-	2: i64    _time;
-	3: string _string;
+    1: i64    _integer;
+    2: i64    _time;
+    3: string _string;
 }
 
 struct PorterDynamicPaoInfoResponse {
