4e1e59a9e1c7edf7dd2f5ba8c98a405f54038794
Fisher, Matthew S <MatthewSFisher@Eaton.com>
2015-09-24 12:13:20 +0000

    YUK-14545 C++ Porter service for retrieving dynamic device info Added PorterDynamicPaoInfoRequest/Response messages, including DynamicPaoInfoKeys enum listing supported entries

---
+++ yukon-shared/src/main/resources/messages/thrift/PorterDynamicPaoInfo.thrift
@@ -0,0 +1,23 @@
+namespace cpp Cti.Messaging.Serialization.Thrift
+namespace java com.cannontech.messaging.serialization.thrift.generated
+
+enum DynamicPaoInfoKeys {
+	RFN_VOLTAGE_PROFILE_ENABLED_UNTIL,
+    RFN_VOLTAGE_PROFILE_INTERVAL
+}
+
+struct PorterDynamicPaoInfoRequest {
+    1: required  i32                     _deviceId;
+    2: required  set<DynamicPaoInfoKeys> _keys;
+}
+
+union DynamicPaoInfoTypes {
+	1: i64    _integer;
+	2: i64    _time;
+	3: string _string;
+}
+
+struct PorterDynamicPaoInfoResponse {
+    1: required  i32                 _deviceId;
+    2: required  map<DynamicPaoInfoKeys, DynamicPaoInfoTypes> _values;
+}
