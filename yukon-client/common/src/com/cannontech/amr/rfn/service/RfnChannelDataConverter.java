74ca8e4e72999cc432711a85d1bd128492708508
Fisher, Matthew S <MatthewSFisher@Eaton.com>
2015-09-30 12:58:37 +0000

    YUK-14526 OV/UV alarms and events – save analog voltage reading in the database Refactored RfnChannelDataConverter.convert to functional streams, exposing a new convertSingleChannelData Replaced incomplete attribute decode in RfnVoltageEventArchiveRequestProcessor with full RfnChannelDataConverter.convertSingleChannelData call

--- yukon-client/common/src/com/cannontech/amr/rfn/service/RfnChannelDataConverter.java
+++ yukon-client/common/src/com/cannontech/amr/rfn/service/RfnChannelDataConverter.java
@@ -2,6 +2,10 @@ package com.cannontech.amr.rfn.service;
 
 import java.util.List;
 import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
 
@@ -55,9 +59,26 @@ public class RfnChannelDataConverter {
     private ImmutableSet<PaoTypePointIdentifier> calculationContributors;
     private static final Logger log = YukonLogManager.getLogger(RfnChannelDataConverter.class);
     
-    public List<CalculationData> convert(RfnMeterPlusReadingData reading, List<? super PointData> toArchive) {
+    public class PointDataWithIdentifier {
+        private PointData pointData;
+        private PaoPointIdentifier ppi;
         
-        List<CalculationData> calculationData = Lists.newArrayList();
+        PointDataWithIdentifier(PointData pointData, PaoPointIdentifier ppi) {
+            this.pointData = pointData;
+            this.ppi = ppi;
+        }
+        public PointData getPointData() {
+            return pointData;
+        }
+        public PaoPointIdentifier getPaoPointIdentifier() {
+            return ppi;
+        }
+        public PaoPointValue asPaoPointValue() {
+            return PaoPointValue.of(ppi, pointData);
+        }
+    }
+    
+    public List<CalculationData> convert(RfnMeterPlusReadingData reading, List<? super PointData> toArchive) {
         
         List<ChannelData> nonDatedChannelData = reading.getRfnMeterReadingData().getChannelDataList();
         List<? extends ChannelData> datedChannelData = reading.getRfnMeterReadingData().getDatedChannelDataList();
@@ -73,87 +94,132 @@ public class RfnChannelDataConverter {
         }
         
         Instant readingInstant = new Instant(reading.getRfnMeterReadingData().getTimeStamp());
-        RfnDevice rfnDevice = reading.getRfnDevice();
         
-        for (ChannelData channelData : allChannelData) {
-            if (log.isDebugEnabled()) {
-                log.debug("Processing " + channelData + " for " + rfnDevice);
-            }
-            ChannelDataStatus status = channelData.getStatus();
-            if (status == null) {
-                if (log.isDebugEnabled()) {
-                    log.debug("Received null status for channelData, skipping");
-                }
-                continue;
-            }
-            if (!status.isOk()) {
-                if (log.isDebugEnabled()) {
-                    log.debug("Received status of " + status + " for channelData, skipping");
-                }
-                continue;
-            }
-            
-            PointValueHandler pointValueHandler = unitOfMeasureToPointMapper.findMatch(rfnDevice, channelData);
-            if (pointValueHandler == null) {
-                log.debug("No PointValueHandler for this channelData");
-                continue;
-            }
-            if (log.isDebugEnabled()) {
-                log.debug("Got PointValueHandler " + pointValueHandler);
-            }
-            
-            PaoPointIdentifier ppi = pointValueHandler.getPaoPointIdentifier();
-            LitePoint point;
-            try {
-                point = pointDao.getLitePoint(ppi);
-            } catch (NotFoundException e) {
-                if (log.isDebugEnabled()) {
-                    log.debug("Unable to find point for channelData: " + channelData);
-                }
-                continue;
-            }
-            
-            PointData pointData = new PointData();
-            pointData.setId(point.getPointID());
-            pointData.setPointQuality(PointQuality.Normal);
-            double value = pointValueHandler.convert(channelData.getValue());
+        //TODO JAVA 8
+        
+        List<PointDataWithIdentifier> pointData =
+                allChannelData.stream()
+                    //.map(channelData -> convertSingleChannelData(reading.getRfnDevice(), channelData, readingInstant))
+                    .map(new Function<ChannelData, PointDataWithIdentifier>() {
+                            @Override
+                            public PointDataWithIdentifier apply(ChannelData channelData) {
+                                return convertSingleChannelData(reading.getRfnDevice(), channelData, readingInstant);
+                            }
+                        })
+                    //.filter(Objects::nonNull)
+                    .filter(new Predicate<Object>() {
+                        @Override
+                        public boolean test(Object t) {
+                            return t != null;
+                        }
+                    })
+                    .collect(Collectors.toList());
+        
+        pointData.stream()
+                //.map(PointDataWithIdentifier::getPointData)
+                .map(new Function<PointDataWithIdentifier, PointData>() {
+                    @Override
+                    public PointData apply(PointDataWithIdentifier pdwi) {
+                        return pdwi.getPointData();
+                    }
+                })
+                //.forEachOrdered(toArchive::add)
+                .forEachOrdered(new Consumer<PointData>(){
+                    @Override
+                    public void accept(PointData pointData) {
+                        toArchive.add(pointData);
+                    }
+                });
+        
+        /* If this point is used to calculate other points, return it. */
+        return pointData.stream()
+                //.filter(pdwi -> calculationContributors.contains(pdwi.getPaoPointIdentifier().getPaoTypePointIdentifier()))
+                .filter(new Predicate<PointDataWithIdentifier>() {
+                    @Override
+                    public boolean test(PointDataWithIdentifier pdwi) {
+                        return calculationContributors.contains(pdwi.getPaoPointIdentifier().getPaoTypePointIdentifier());
+                    }
+                })
+                //.map(pdwi -> return CalculationData.of(pdwi.asPaoPointValue(), reading.getRfnMeterReadingData().getRecordInterval())) 
+                .map(new Function<PointDataWithIdentifier, CalculationData>() {
+                    @Override
+                    public CalculationData apply(PointDataWithIdentifier pdwi) {
+                        return CalculationData.of(pdwi.asPaoPointValue(), reading.getRfnMeterReadingData().getRecordInterval());
+                    }
+                })
+                .collect(Collectors.toList());
+    }
 
-            Double multiplier = point.getMultiplier();
-            if (multiplier != null) {
-                value *= point.getMultiplier();
-            }
-            Double dataOffset = point.getDataOffset();
-            if (dataOffset != null) {
-                value += point.getDataOffset();
-            }
-            int meterDigits = point.getDecimalDigits();
-            if (meterDigits > 0) {
-                value %= Math.pow(10, meterDigits);
-            }
-            pointData.setValue(value);
-            if (channelData instanceof DatedChannelData) {
-                DatedChannelData dated = (DatedChannelData)channelData;
-                pointData.setTime(new Instant(dated.getTimeStamp()).toDate());
-            } else {
-                pointData.setTime(readingInstant.toDate());
-            }
-            pointData.setType(ppi.getPointIdentifier().getPointType().getPointTypeId());
-            pointData.setTagsPointMustArchive(true); // temporary solution
-            
+    public PointDataWithIdentifier convertSingleChannelData(RfnDevice rfnDevice, ChannelData channelData, Instant readingInstant) {
+        if (log.isDebugEnabled()) {
+            log.debug("Processing " + channelData + " for " + rfnDevice);
+        }
+        ChannelDataStatus status = channelData.getStatus();
+        if (status == null) {
             if (log.isDebugEnabled()) {
-                log.debug("PointData converted: " + pointData);
+                log.debug("Received null status for channelData, skipping");
             }
-            
-            toArchive.add(pointData);
-            
-            /* If this point is used to calculate other points, return it. */
-            PaoTypePointIdentifier ptpi = PaoTypePointIdentifier.of(ppi.getPaoIdentifier().getPaoType(), ppi.getPointIdentifier());
-            if (calculationContributors.contains(ptpi)) {
-                calculationData.add(CalculationData.of(PaoPointValue.of(ppi, pointData), reading.getRfnMeterReadingData().getRecordInterval()));
+            return null;
+        }
+        if (!status.isOk()) {
+            if (log.isDebugEnabled()) {
+                log.debug("Received status of " + status + " for channelData, skipping");
             }
+            return null;
         }
         
-        return calculationData;
+        PointValueHandler pointValueHandler = unitOfMeasureToPointMapper.findMatch(rfnDevice, channelData);
+        if (pointValueHandler == null) {
+            log.debug("No PointValueHandler for this channelData");
+            return null;
+        }
+        if (log.isDebugEnabled()) {
+            log.debug("Got PointValueHandler " + pointValueHandler);
+        }
+        
+        PaoPointIdentifier ppi = pointValueHandler.getPaoPointIdentifier();
+        LitePoint point;
+        try {
+            point = pointDao.getLitePoint(ppi);
+        } catch (NotFoundException e) {
+            if (log.isDebugEnabled()) {
+                log.debug("Unable to find point for channelData: " + channelData);
+            }
+            return null;
+        }
+        
+        PointData pointData = new PointData();
+        pointData.setId(point.getPointID());
+        pointData.setPointQuality(PointQuality.Normal);
+        double value = pointValueHandler.convert(channelData.getValue());
+
+        Double multiplier = point.getMultiplier();
+        if (multiplier != null) {
+            value *= point.getMultiplier();
+        }
+        Double dataOffset = point.getDataOffset();
+        if (dataOffset != null) {
+            value += point.getDataOffset();
+        }
+        int meterDigits = point.getDecimalDigits();
+        if (meterDigits > 0) {
+            value %= Math.pow(10, meterDigits);
+        }
+        pointData.setValue(value);
+        if (channelData instanceof DatedChannelData) {
+            DatedChannelData dated = (DatedChannelData)channelData;
+            pointData.setTime(new Instant(dated.getTimeStamp()).toDate());
+        } else {
+            pointData.setTime(readingInstant.toDate());
+        }
+        pointData.setType(ppi.getPointIdentifier().getPointType().getPointTypeId());
+        pointData.setTagsPointMustArchive(true); // temporary solution
+        
+        if (log.isDebugEnabled()) {
+            log.debug("PointData converted: " + pointData);
+        }
+        
+        return new PointDataWithIdentifier(pointData, ppi);
     }
     
     @PostConstruct
--- yukon-client/common/src/com/cannontech/amr/rfn/service/processor/impl/RfnVoltageEventArchiveRequestProcessor.java
+++ yukon-client/common/src/com/cannontech/amr/rfn/service/processor/impl/RfnVoltageEventArchiveRequestProcessor.java
@@ -2,26 +2,47 @@ package com.cannontech.amr.rfn.service.processor.impl;
 
 import java.util.List;
 
+import org.joda.time.Instant;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.cannontech.amr.rfn.message.event.RfnConditionDataType;
 import com.cannontech.amr.rfn.message.event.RfnEvent;
+import com.cannontech.amr.rfn.message.event.RfnUomModifierSet;
+import com.cannontech.amr.rfn.message.read.ChannelData;
+import com.cannontech.amr.rfn.message.read.ChannelDataStatus;
+import com.cannontech.amr.rfn.service.RfnChannelDataConverter;
 import com.cannontech.amr.rfn.service.processor.RfnArchiveRequestProcessor;
 import com.cannontech.amr.rfn.service.processor.RfnEventConditionDataProcessorHelper;
-import com.cannontech.common.pao.attribute.model.BuiltInAttribute;
 import com.cannontech.common.rfn.model.RfnDevice;
 import com.cannontech.message.dispatch.message.PointData;
 
 public abstract class RfnVoltageEventArchiveRequestProcessor extends RfnEventConditionDataProcessorHelper
         implements RfnArchiveRequestProcessor {
 
+    @Autowired private RfnChannelDataConverter converter;
+
     @Override
     public <T extends RfnEvent> void process(RfnDevice device, T event,
                                              List<? super PointData> pointDatas) {
+
+        //  Manually create a ChannelData, since NM doesn't provide one yet (see YUK-14688).
+        //    This code should eventually move to RfnMeterEventService::handleRfnEventStatusEvents to allow all events to handle ChannelData. 
+        ChannelData channelData = new ChannelData();
         
-        Double voltage = (Double) getEventDataWithType(event, RfnConditionDataType.MEASURED_VALUE);
-        rfnMeterEventService.processAttributePointData(device,
-                                                       pointDatas,
-                                                       BuiltInAttribute.VOLTAGE,
-                                                       event.getTimeStamp(),
-                                                       voltage);
+        channelData.setUnitOfMeasure(
+                (String) getEventDataWithType(event, RfnConditionDataType.UOM));
+        
+        channelData.setUnitOfMeasureModifiers(
+                ((RfnUomModifierSet) getEventDataWithType(event, RfnConditionDataType.UOM_MODIFIERS)).getUomModifiers());
+        
+        channelData.setValue(
+                (Double) getEventDataWithType(event, RfnConditionDataType.MEASURED_VALUE));
+        
+        channelData.setStatus(
+                ChannelDataStatus.OK);
+                
+        pointDatas.add(
+                converter.convertSingleChannelData(device, channelData, new Instant(event.getTimeStamp()))
+                         .getPointData());
     }
 }
