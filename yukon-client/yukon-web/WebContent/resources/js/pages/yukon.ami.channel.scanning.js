7cd1ff784b9779e507a4066af4e73bc3aaf05e56
Rawat, Payal <PayalRawat@Eaton.com>
2015-10-30 22:39:50 +0000

    YUK-14527 - Add future temporary profile collection portion of profile widget for RF meters

--- yukon-client/yukon-web/WebContent/resources/js/pages/yukon.ami.channel.scanning.js
+++ yukon-client/yukon-web/WebContent/resources/js/pages/yukon.ami.channel.scanning.js
@@ -6,24 +6,29 @@
 
 yukon.namespace('yukon.ami.channel.scanning');
 
-yukon.ami.channel.scanning = {
-init: function() {
-    $(document).on('yukon.toggle.click', '#toggle-state', function (ev) {
-    $('#confirm-popup').dialog('close');
-    doToggleScanning(0,newToggleVal);
-});
+yukon.ami.channel.scanning = (function (){
+    var newToggleVal = false,
 
-$(document).on('click', ".js-toggle-profile-on", function(e){
-    newToggleVal = true;
-    yukon.ui.dialog('#confirm-popup');
-});
+    mod = {
+            init: function() {
+             $(document).on('yukon.toggle.click', '#toggle-state', function (ev) {
+             $('#confirm-popup').dialog('close');
+             doToggleScanning(0,newToggleVal);
+            });
 
-$(document).on('click', ".js-toggle-profile-off", function(e){
-    newToggleVal = false;
-    doToggleScanning(0,newToggleVal);
-});
-}
-};
+            $(document).on('click', ".js-toggle-profile-on", function(e){
+            newToggleVal = true;
+            yukon.ui.dialog('#confirm-popup');
+            });
+
+           $(document).on('click', ".js-toggle-profile-off", function(e){
+           newToggleVal = false;
+           doToggleScanning(0,newToggleVal);
+           });	
+         }
+       };
+    return mod;
+})();
 
 $(function() {
 	yukon.ami.channel.scanning.init();
