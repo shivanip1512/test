25d0d87ec40c650ca69e8388bdee13c495094934
Vagyani, Asmita <>
2015-11-17 09:36:51 +0000

    YUK-14803 - Added the check for the inventory and program ids to authorize user to see only the data for which he is authorized

--- yukon-client/common/src/com/cannontech/stars/core/service/impl/AccountCheckerServiceImpl.java
+++ yukon-client/common/src/com/cannontech/stars/core/service/impl/AccountCheckerServiceImpl.java
@@ -44,10 +44,10 @@ public class AccountCheckerServiceImpl implements AccountCheckerService {
     public void checkInventory(final LiteYukonUser user, final Integer... inventoryIds)
             throws NotAuthorizedException {
         
-        if (isOperator(user)) return;
-        
-        List<Integer> actualInventoryIds = getInventoryIdsByUser(user);
-        doCheck(user, actualInventoryIds, inventoryIds, "Inventory");
+        if (isOperator(user)) {
+	        List<Integer> actualInventoryIds = getInventoryIdsByUser(user);
+	        doCheck(user, actualInventoryIds, inventoryIds, "Inventory");
+        }
     }
     
     @Override
@@ -98,10 +98,10 @@ public class AccountCheckerServiceImpl implements AccountCheckerService {
     public void checkProgram(final LiteYukonUser user, final Integer... programIds)
             throws NotAuthorizedException {
         
-        if (isOperator(user)) return;
-        
-        List<Integer> actualProgramIds = getProgramIdsByUser(user);
-        doCheck(user, actualProgramIds, programIds, "Program");
+        if (isOperator(user)) {        
+        	List<Integer> actualProgramIds = getProgramIdsByUser(user);
+        	doCheck(user, actualProgramIds, programIds, "Program");
+        }
     }
     
 	@Override
