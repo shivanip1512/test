d37c12f61443da092e36c98462c98c174eb47d26
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2015-12-22 21:42:43 +0000

    YUK-14940 DB Change - Configuration Shows Centron C2SX Meter Parameters  - update CategoryType for lcdConfiguration.  - updated build properties for 6.5.1

---
+++ yukon-database/DBUpdates/oracle/6.5_1.sql
@@ -0,0 +1,15 @@
+/******************************************/ 
+/****     Oracle DBupdates             ****/ 
+/******************************************/ 
+
+/* Start YUK-14940 */
+UPDATE DeviceConfigCategory
+SET CategoryType= 'lcdConfiguration'
+WHERE CategoryType = 'meterParameters';
+/* End YUK-14940 */
+
+/**************************************************************/
+/* VERSION INFO                                               */
+/* Inserted when update script is run                         */
+/**************************************************************/
+/*INSERT INTO CTIDatabase VALUES ('6.5', '31-JAN-2015', 'Latest Update', 1, SYSDATE);*/
