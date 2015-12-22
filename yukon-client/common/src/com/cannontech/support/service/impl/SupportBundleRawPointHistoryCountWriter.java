d0f2daaad555deef541c7e35c0f188ebe45b9612
Joshi, Rohit <RohitJoshi2@Eaton.com>
2015-12-22 05:42:38 +0000

    YUK-14893-Support Bundle count of rows in RPH table is slow

--- yukon-client/common/src/com/cannontech/support/service/impl/SupportBundleRawPointHistoryCountWriter.java
+++ yukon-client/common/src/com/cannontech/support/service/impl/SupportBundleRawPointHistoryCountWriter.java
@@ -1,14 +1,34 @@
 package com.cannontech.support.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.cannontech.common.util.SqlFragmentSource;
 import com.cannontech.common.util.SqlStatementBuilder;
+import com.cannontech.database.vendor.DatabaseVendorResolver;
 
 public class SupportBundleRawPointHistoryCountWriter extends SupportBundleSqlWriter {
+
+    @Autowired
+    private DatabaseVendorResolver dbVendorResolver;
+
     @Override
     protected SqlFragmentSource getSqlFragmentSource() {
+
         SqlStatementBuilder sql = new SqlStatementBuilder();
-        sql.append("SELECT COUNT(*)");
-        sql.append("FROM RawPointHistory");
+        if (dbVendorResolver.getDatabaseVendor().isSqlServer()) {
+            sql.append("DECLARE @TableName sysname");
+            sql.append("SET @TableName = 'RawPointHistory'");
+            sql.append("SELECT SUM(PART.rows) AS rows");
+            sql.append("FROM sys.tables TBL");
+            sql.append("INNER JOIN sys.partitions PART ON TBL.object_id = PART.object_id");
+            sql.append("INNER JOIN sys.indexes IDX ON PART.object_id = IDX.object_id");
+            sql.append("AND PART.index_id = IDX.index_id");
+            sql.append("WHERE TBL.name = @TableName");
+            sql.append("AND IDX.index_id < 2");
+        } else if (dbVendorResolver.getDatabaseVendor().isOracle()) {
+            sql.append("SELECT COUNT(*)");
+            sql.append("FROM RawPointHistory");
+        }
         return sql;
     }
 
