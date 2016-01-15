cd45bccf6806a39d3021b7b3ac716e63024c751c
DeZeeuw, Garrett <>
2016-01-15 22:07:19 +0000

    YUK-14987: Create IgnoredControlReason state group
    Removed entry for state 14, 'Remote', which should not be in the state group.  Also re-colored the remaining states to avoid green being used to represent a failure state.

--- yukon-database/CreationScripts/OracleTableCreation.sql
+++ yukon-database/CreationScripts/OracleTableCreation.sql
@@ -1,7 +1,7 @@
 /*==============================================================*/
 /* Database name:  YukonDatabase                                */
 /* DBMS name:      ORACLE Version 9i                            */
-/* Created on:     1/13/2016 11:09:46 AM                        */
+/* Created on:     1/15/2016 11:42:29 AM                        */
 /*==============================================================*/
 
 
@@ -7902,26 +7902,25 @@ create table State  (
    constraint PK_STATE primary key (StateGroupId, RawState)
 );
 
-INSERT INTO State VALUES(-20, 0, 'Manual', 0, 6, 0);
-INSERT INTO State VALUES(-20, 1, 'SCADA Override', 1, 6, 0);
-INSERT INTO State VALUES(-20, 2, 'Fault Current', 2, 6, 0);
-INSERT INTO State VALUES(-20, 3, 'Emergency Voltage', 3, 6, 0);
-INSERT INTO State VALUES(-20, 4, 'Time ONOFF', 4, 6, 0);
-INSERT INTO State VALUES(-20, 5, 'OVUV Control', 5, 6, 0);
-INSERT INTO State VALUES(-20, 6, 'VAR', 7, 6, 0);
-INSERT INTO State VALUES(-20, 7, 'Va', 8, 6, 0);
-INSERT INTO State VALUES(-20, 8, 'Vb', 1, 6, 0);
-INSERT INTO State VALUES(-20, 9, 'Vc', 2, 6, 0);
-INSERT INTO State VALUES(-20, 10, 'Ia', 3, 6, 0);
-INSERT INTO State VALUES(-20, 11, 'Ib', 4, 6, 0);
-INSERT INTO State VALUES(-20, 12, 'Ic', 5, 6, 0);
-INSERT INTO State VALUES(-20, 13, 'Temp', 7, 6, 0);
-INSERT INTO State VALUES(-20, 14, 'Remote', 8, 6, 0);
-INSERT INTO State VALUES(-20, 15, 'Time', 1, 6, 0);
-INSERT INTO State VALUES(-20, 17, 'Bad Active Relay', 2, 6, 0);
-INSERT INTO State VALUES(-20, 18, 'NC Lockout', 3, 6, 0);
-INSERT INTO State VALUES(-20, 20, 'Auto Mode', 4, 6, 0);
-INSERT INTO State VALUES(-20, 21, 'Reclose Block', 5, 6, 0);
+INSERT INTO State VALUES(-20, 0, 'Manual', 1, 6, 0);
+INSERT INTO State VALUES(-20, 1, 'SCADA Override', 2, 6, 0);
+INSERT INTO State VALUES(-20, 2, 'Fault Current', 3, 6, 0);
+INSERT INTO State VALUES(-20, 3, 'Emergency Voltage', 4, 6, 0);
+INSERT INTO State VALUES(-20, 4, 'Time ONOFF', 5, 6, 0);
+INSERT INTO State VALUES(-20, 5, 'OVUV Control', 7, 6, 0);
+INSERT INTO State VALUES(-20, 6, 'VAR', 8, 6, 0);
+INSERT INTO State VALUES(-20, 7, 'Va', 9, 6, 0);
+INSERT INTO State VALUES(-20, 8, 'Vb', 10, 6, 0);
+INSERT INTO State VALUES(-20, 9, 'Vc', 1, 6, 0);
+INSERT INTO State VALUES(-20, 10, 'Ia', 2, 6, 0);
+INSERT INTO State VALUES(-20, 11, 'Ib', 3, 6, 0);
+INSERT INTO State VALUES(-20, 12, 'Ic', 4, 6, 0);
+INSERT INTO State VALUES(-20, 13, 'Temp', 5, 6, 0);
+INSERT INTO State VALUES(-20, 15, 'Time', 7, 6, 0);
+INSERT INTO State VALUES(-20, 17, 'Bad Active Relay', 8, 6, 0);
+INSERT INTO State VALUES(-20, 18, 'NC Lockout', 9, 6, 0);
+INSERT INTO State VALUES(-20, 20, 'Auto Mode', 10, 6, 0);
+INSERT INTO State VALUES(-20, 21, 'Reclose Block', 1, 6, 0);
 INSERT INTO State VALUES(-19, 0, 'Success', 0, 6, 0);
 INSERT INTO State VALUES(-19, 1, 'Not Applicable', 9, 6, 0);
 INSERT INTO State VALUES(-19, 2, 'Failure', 1, 6, 0);
--- yukon-database/DBUpdates/sqlserver/6.5_1.sql
+++ yukon-database/DBUpdates/sqlserver/6.5_1.sql
@@ -12,26 +12,25 @@ WHERE CategoryType = 'meterParameters';
 /* @error ignore-begin */
 INSERT INTO StateGroup VALUES(-20, 'Ignored Control', 'Status');
 
-INSERT INTO State VALUES(-20, 0, 'Manual', 0, 6, 0);
-INSERT INTO State VALUES(-20, 1, 'SCADA Override', 1, 6, 0);
-INSERT INTO State VALUES(-20, 2, 'Fault Current', 2, 6, 0);
-INSERT INTO State VALUES(-20, 3, 'Emergency Voltage', 3, 6, 0);
-INSERT INTO State VALUES(-20, 4, 'Time ONOFF', 4, 6, 0);
-INSERT INTO State VALUES(-20, 5, 'OVUV Control', 5, 6, 0);
-INSERT INTO State VALUES(-20, 6, 'VAR', 7, 6, 0);
-INSERT INTO State VALUES(-20, 7, 'Va', 8, 6, 0);
-INSERT INTO State VALUES(-20, 8, 'Vb', 1, 6, 0);
-INSERT INTO State VALUES(-20, 9, 'Vc', 2, 6, 0);
-INSERT INTO State VALUES(-20, 10, 'Ia', 3, 6, 0);
-INSERT INTO State VALUES(-20, 11, 'Ib', 4, 6, 0);
-INSERT INTO State VALUES(-20, 12, 'Ic', 5, 6, 0);
-INSERT INTO State VALUES(-20, 13, 'Temp', 7, 6, 0);
-INSERT INTO State VALUES(-20, 14, 'Remote', 8, 6, 0);
-INSERT INTO State VALUES(-20, 15, 'Time', 1, 6, 0);
-INSERT INTO State VALUES(-20, 17, 'Bad Active Relay', 2, 6, 0);
-INSERT INTO State VALUES(-20, 18, 'NC Lockout', 3, 6, 0);
-INSERT INTO State VALUES(-20, 20, 'Auto Mode', 4, 6, 0);
-INSERT INTO State VALUES(-20, 21, 'Reclose Block', 5, 6, 0);
+INSERT INTO State VALUES(-20, 0, 'Manual', 1, 6, 0);
+INSERT INTO State VALUES(-20, 1, 'SCADA Override', 2, 6, 0);
+INSERT INTO State VALUES(-20, 2, 'Fault Current', 3, 6, 0);
+INSERT INTO State VALUES(-20, 3, 'Emergency Voltage', 4, 6, 0);
+INSERT INTO State VALUES(-20, 4, 'Time ONOFF', 5, 6, 0);
+INSERT INTO State VALUES(-20, 5, 'OVUV Control', 7, 6, 0);
+INSERT INTO State VALUES(-20, 6, 'VAR', 8, 6, 0);
+INSERT INTO State VALUES(-20, 7, 'Va', 9, 6, 0);
+INSERT INTO State VALUES(-20, 8, 'Vb', 10, 6, 0);
+INSERT INTO State VALUES(-20, 9, 'Vc', 1, 6, 0);
+INSERT INTO State VALUES(-20, 10, 'Ia', 2, 6, 0);
+INSERT INTO State VALUES(-20, 11, 'Ib', 3, 6, 0);
+INSERT INTO State VALUES(-20, 12, 'Ic', 4, 6, 0);
+INSERT INTO State VALUES(-20, 13, 'Temp', 5, 6, 0);
+INSERT INTO State VALUES(-20, 15, 'Time', 7, 6, 0);
+INSERT INTO State VALUES(-20, 17, 'Bad Active Relay', 8, 6, 0);
+INSERT INTO State VALUES(-20, 18, 'NC Lockout', 9, 6, 0);
+INSERT INTO State VALUES(-20, 20, 'Auto Mode', 10, 6, 0);
+INSERT INTO State VALUES(-20, 21, 'Reclose Block', 1, 6, 0);
 /* @error ignore-end */
 /* End YUK-14987 */
 
