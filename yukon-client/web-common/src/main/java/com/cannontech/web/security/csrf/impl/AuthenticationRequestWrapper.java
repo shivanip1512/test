871d9aa3bf902023d9abd89f8968dec88a8dc3e1
Modi, Preeti <>
2016-01-22 06:32:29 +0000

    YUK-14997 : Unable to Open the Java Clients Changed encoding to "UTF-8" to "ISO-8859-1"

--- yukon-client/web-common/src/main/java/com/cannontech/web/security/csrf/impl/AuthenticationRequestWrapper.java
+++ yukon-client/web-common/src/main/java/com/cannontech/web/security/csrf/impl/AuthenticationRequestWrapper.java
@@ -3,10 +3,8 @@
  */
 package com.cannontech.web.security.csrf.impl;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 import javax.servlet.ServletInputStream;
