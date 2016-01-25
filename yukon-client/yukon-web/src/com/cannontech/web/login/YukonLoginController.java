c46314ff1ddf745e253b9d89b6f523f8f1a382af
Vant, Carrie <carrieVant@Eaton.com>
2016-01-25 20:28:46 +0000

    YUK-14873 - Cross Site Request Forgery   - space in redirect was causing issues

--- yukon-client/yukon-web/src/com/cannontech/web/login/YukonLoginController.java
+++ yukon-client/yukon-web/src/com/cannontech/web/login/YukonLoginController.java
@@ -40,7 +40,7 @@ public class YukonLoginController extends MultiActionController {
     public ModelAndView view(HttpServletRequest request, HttpServletResponse response) throws Exception {
         ModelAndView mav = new ModelAndView();
         if (request.getSession() != null && request.getSession().getAttribute(LoginController.YUKON_USER) != null) {
-            return new ModelAndView("redirect: /dashboard");
+            return new ModelAndView("redirect:/dashboard");
         }
 
         mav.setViewName("login.jsp");
