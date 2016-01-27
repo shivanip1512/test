80d92f3d891f71f2401e2104cfbffce2af49dcd3
Khole, Sneha <>
2016-01-27 04:43:49 +0000

    YUKRV-9229 YUK-15022 NullPointerException in OutageJMSMessageListener
    Fixed review comments.

---
+++ yukon-client/multispeak/src/com/cannontech/multispeak/util/YukonJmsErrorHandler.java
@@ -0,0 +1,17 @@
+package com.cannontech.multispeak.util;
+
+import org.apache.log4j.Logger;
+import org.springframework.util.ErrorHandler;
+
+import com.cannontech.clientutils.YukonLogManager;
+
+public class YukonJmsErrorHandler implements ErrorHandler {
+    private static final Logger log = YukonLogManager.getLogger(YukonJmsErrorHandler.class);
+
+    @Override
+    public void handleError(Throwable t) {
+        log.error("Message Listerner encountered an error: " + t.getMessage());
+        t.printStackTrace();
+    }
+
+}
