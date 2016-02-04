0692861737d9f70244f0fd6d04693b40aca678f8
Feldman, Marina <MarinaFeldman@Eaton.com>
2016-02-04 20:19:19 +0000

    YUK-14923 Set up database and system to have 250k RFN electric meters/ changed getAllBins method to get paoTypes from cache

--- yukon-client/common/src/com/cannontech/common/device/groups/dao/impl/providers/DeviceTypeGroupProvider.java
+++ yukon-client/common/src/com/cannontech/common/device/groups/dao/impl/providers/DeviceTypeGroupProvider.java
@@ -1,42 +1,47 @@
 package com.cannontech.common.device.groups.dao.impl.providers;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
 
+import com.cannontech.common.pao.PaoCategory;
 import com.cannontech.common.pao.YukonDevice;
 import com.cannontech.common.util.SqlFragmentSource;
 import com.cannontech.common.util.SqlStatementBuilder;
 import com.cannontech.core.dao.PaoDao;
 import com.cannontech.database.data.lite.LiteYukonPAObject;
 import com.cannontech.database.db.device.Device;
+import com.cannontech.yukon.IDatabaseCache;
+
 
 public class DeviceTypeGroupProvider extends CompleteBinningDeviceGroupProviderBase<String> {
-    private PaoDao paoDao;
+    
+    @Autowired private IDatabaseCache dbCache;
+    @Autowired private PaoDao paoDao;
     
     @Override
     protected List<String> getAllBins() {
-        SqlStatementBuilder sql = new SqlStatementBuilder();
-        sql.append("SELECT DISTINCT ypo.type");
-        sql.append("FROM Device d");
-        sql.append("JOIN YukonPaObject ypo ON (d.deviceid = ypo.paobjectid)");
-        sql.append("WHERE ypo.paobjectid").neq(Device.SYSTEM_DEVICE_ID);
-        sql.append("ORDER BY ypo.type");
-
-        ParameterizedRowMapper<String> mapper = new ParameterizedRowMapper<String>() {
-            public String mapRow(ResultSet rs, int rowNum) throws SQLException {
-                return rs.getString("type");
+        Set<String> paoTypes = dbCache.getAllYukonPAObjects().stream().filter(new Predicate<LiteYukonPAObject>() {
+            @Override
+            public boolean test(LiteYukonPAObject pao) {
+                return pao.getPaoType().getPaoCategory() == PaoCategory.DEVICE
+                    && pao.getLiteID() != Device.SYSTEM_DEVICE_ID;
             }
-        };
-        List<String> resultList = getJdbcTemplate().query(sql.getSql(), mapper, sql.getArguments());
-        return resultList;
+        }).map(new Function<LiteYukonPAObject, String>() {
+            @Override
+            public String apply(LiteYukonPAObject pao) {
+                return pao.getPaoType().getDbString();
+            }
+        }).collect(Collectors.toSet());
+        return new ArrayList<String>(paoTypes);
     }
-    
+
     @Override
     protected SqlFragmentSource getChildSqlSelectForBin(String bin) {
         SqlStatementBuilder sql = new SqlStatementBuilder();
@@ -54,10 +59,4 @@ public class DeviceTypeGroupProvider extends CompleteBinningDeviceGroupProviderB
         String type = devicePao.getPaoType().getDbString();
         return Collections.singleton(type);
     }
-    
-    @Autowired
-    public void setPaoDao(PaoDao paoDao) {
-        this.paoDao = paoDao;
-    }
-
 }
