7b19dbe60e5dee14760efba98739f92da4af9ccc
DeZeeuw, Garrett <>
2016-02-09 23:26:20 +0000

    CTI Database signature - trunk - 6.5.1 update
    Also added update stubs for 6.2.10 to trunk.

--- yukon-database/DBUpdates/sqlserver/6.5_1.sql
+++ yukon-database/DBUpdates/sqlserver/6.5_1.sql
@@ -130,4 +130,4 @@ WHERE Type = 'WEATHER LOCATION'
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
 /**************************************************************/
-/*INSERT INTO CTIDatabase VALUES ('6.5', '31-JAN-2016', 'Latest Update', 1, GETDATE());*/
+INSERT INTO CTIDatabase VALUES ('6.5', '31-JAN-2016', 'Latest Update', 1, GETDATE());
