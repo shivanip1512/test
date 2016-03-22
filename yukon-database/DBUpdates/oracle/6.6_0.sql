e9936733f83ad45914fe90cdff68002f516d90c3
DeZeeuw, Garrett <>
2016-03-22 22:11:42 +0000

    YUK-15182 Database Editor Does Not Display the Ignored Control State Group Correctly
    Added issue to 6.6.0 update scripts.

---
+++ yukon-database/DBUpdates/oracle/6.6_0.sql
@@ -0,0 +1,17 @@
+/******************************************/ 
+/****     Oracle DBupdates             ****/ 
+/******************************************/ 
+
+/* Start YUK-15182 */
+/* @error ignore-begin */
+INSERT INTO State VALUES(-20, 14, 'N/A', 9, 6, 0);
+INSERT INTO State VALUES(-20, 15, 'N/A', 9, 6, 0);
+INSERT INTO State VALUES(-20, 16, 'N/A', 9, 6, 0);
+/* @error ignore-end */
+/* End YUK-15182 */
+
+/**************************************************************/
+/* VERSION INFO                                               */
+/* Inserted when update script is run                         */
+/**************************************************************/
+/*INSERT INTO CTIDatabase VALUES ('6.6', '31-MAR-2015', 'Latest Update', 0, SYSDATE);*/
