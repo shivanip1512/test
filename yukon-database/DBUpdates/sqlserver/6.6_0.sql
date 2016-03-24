fe7e1fc313259fea499ba197d952dfba7c4e2806
DeZeeuw, Garrett <>
2016-03-24 20:30:12 +0000

    YUK-15180 CI Curtailment Database Cleanup
    Updated any 'home_url' role property values that had been set to the old CI curtailment pages.  They were replaced with the urls of the new pages.
    
    Cleared any entries in the UserPage table that had pointed to the 'commercialcurtailment' module.

--- yukon-database/DBUpdates/sqlserver/6.6_0.sql
+++ yukon-database/DBUpdates/sqlserver/6.6_0.sql
@@ -10,6 +10,18 @@ INSERT INTO State VALUES(-20, 16, 'N/A', 9, 6, 0);
 /* @error ignore-end */
 /* End YUK-15182 */
 
+/* Start YUK-15180 */
+DELETE FROM UserPage WHERE Module = 'commercialcurtailment';
+
+UPDATE YukonGroupRole SET Value = '/dr/cc/user/overview' 
+WHERE RolePropertyId = -10800 
+AND Value = '/cc/user/overview.jsf';
+
+UPDATE YukonGroupRole SET Value = '/dr/cc/home' 
+WHERE RolePropertyId = -10800 
+AND Value = '/cc/ciSetup.jsf';
+/* End YUK-15180 */
+
 /**************************************************************/
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
