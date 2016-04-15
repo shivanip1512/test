ad60fc086b0703d6933e9ea58ed6e619b05fb2f5
Moc, Jeremy P <JeremyPMoc@Eaton.com>
2016-04-15 19:29:35 +0000

    YUK-15260 Continue C++ substation refactoring without breaking anything YUKRV-9545
    Added name to season manager and a method to determine if a particular named season is active within a particular schedule.

--- yukon-server/RTDB/include/mgr_season.h
+++ yukon-server/RTDB/include/mgr_season.h
@@ -1,20 +1,19 @@
 #pragma once
 
-#include <map>
-
 #include "ctidate.h"
-#include "ctitime.h"
-
 #include "dlldefs.h"
 #include "mutex.h"
-#include "guard.h"
-#include "logger.h"
+
 
 class IM_EX_SEASONDB CtiSeasonManager
 {
 public:
+
     bool isInAnySeason(const CtiDate& date = CtiDate(), long season_sched_id=0);
     bool isInAnySeason(long season_sched_id, const CtiDate& date = CtiDate());
+
+    bool isInNamedSeason( const long scheduleID, const std::string & name, const CtiDate & date );
+
     void refresh();
 
     static CtiSeasonManager& getInstance();
@@ -23,10 +22,10 @@ private:
 
     struct date_of_season
     {
-    	unsigned start_month;
-    	unsigned start_day;
-    	unsigned end_month;
-    	unsigned end_day;
+        unsigned start_month;
+        unsigned start_day;
+        unsigned end_month;
+        unsigned end_day;
         std::string name;
     };
 
@@ -39,6 +38,5 @@ private:
 
     CtiSeasonManager();
     ~CtiSeasonManager() { };
-
-    static const std::string _season_sql;
 };
+
--- yukon-server/RTDB/mgr_season.cpp
+++ yukon-server/RTDB/mgr_season.cpp
@@ -2,19 +2,14 @@
 
 #include "ctidbgmem.h"  // CTIDBG_new
 #include "mgr_season.h"
-#include "dbaccess.h"
 #include "database_reader.h"
-#include "database_connection.h"
 #include "ctidate.h"
+#include "guard.h"
+#include "logger.h"
 
-using std::multimap;
-using std::make_pair;
-using std::string;
-using std::endl;
 using Cti::Database::DatabaseConnection;
 using Cti::Database::DatabaseReader;
 
-const string CtiSeasonManager::_season_sql = "select seasonscheduleid, seasonstartmonth, seasonstartday, seasonendmonth, seasonendday from dateofseason";
 
 CtiSeasonManager::CtiSeasonManager()
 {
@@ -60,8 +55,50 @@ bool CtiSeasonManager::isInAnySeason(long season_sched_id, const CtiDate& date)
     return isInAnySeason(date, season_sched_id);
 }
 
+bool CtiSeasonManager::isInNamedSeason( const long scheduleID, const std::string & name, const CtiDate & date )
+{
+    auto range = _season_map.equal_range( scheduleID );
+
+    if ( range.first != range.second ) 
+    {
+        const unsigned month_of_year = date.month(),
+                       day_of_month  = date.dayOfMonth();
+
+        for ( ; range.first != range.second; ++range.first )
+        {
+            const date_of_season & cur_dos = range.first->second;
+
+            if ( cur_dos.name == name &&
+                 ( month_of_year > cur_dos.start_month ||
+                    ( month_of_year == cur_dos.start_month && day_of_month >= cur_dos.start_day ) ) &&
+                 ( month_of_year < cur_dos.end_month ||
+                   ( month_of_year == cur_dos.end_month && day_of_month <= cur_dos.end_day ) ) )
+            {
+                return true;
+            }
+        }
+    }
+    else
+    {
+        CTILOG_ERROR( dout, "Couldn't locate Season Schedule ID: " << scheduleID );
+    }
+
+    return false;
+}
+
 void CtiSeasonManager::refresh()
 {
+    static const std::string _season_sql =
+        "SELECT "
+            "SeasonScheduleID, "
+            "SeasonName, "
+            "SeasonStartMonth, "
+            "SeasonStartDay, "
+            "SeasonEndMonth, "
+            "SeasonEndDay "
+        "FROM "
+            "DateOfSeason";
+
     long id;
     struct date_of_season dos_temp;
 
@@ -77,13 +114,14 @@ void CtiSeasonManager::refresh()
 
             while( rdr() )
             {
-                rdr["seasonscheduleid"]      >> id;
-                rdr["seasonstartmonth"] >> dos_temp.start_month;
-                rdr["seasonstartday"] >> dos_temp.start_day;
-                rdr["seasonendmonth"] >> dos_temp.end_month;
-                rdr["seasonendday"] >> dos_temp.end_day;
+                rdr["SeasonScheduleID"] >> id;
+                rdr["SeasonName"]       >> dos_temp.name; 
+                rdr["SeasonStartMonth"] >> dos_temp.start_month;
+                rdr["SeasonStartDay"]   >> dos_temp.start_day;
+                rdr["SeasonEndMonth"]   >> dos_temp.end_month;
+                rdr["SeasonEndDay"]     >> dos_temp.end_day;
 
-                _season_map.insert( make_pair(id, dos_temp) );
+                _season_map.emplace( id, dos_temp );
             }
         }
     }
@@ -91,20 +129,21 @@ void CtiSeasonManager::refresh()
     {
         CTILOG_UNKNOWN_EXCEPTION_ERROR(dout);
     }
-
-    return;
 }
 
 //STATIC
 CtiMutex CtiSeasonManager::_mux;
-CtiSeasonManager* CtiSeasonManager::_instance = NULL;
+CtiSeasonManager* CtiSeasonManager::_instance = nullptr;
 
 CtiSeasonManager& CtiSeasonManager::getInstance()
 {
     CtiLockGuard<CtiMutex> g(_mux);
 
-    if( _instance == NULL )
+    if ( ! _instance )
+    {
         _instance = CTIDBG_new CtiSeasonManager;
+    }
 
     return *_instance;
 }
+
