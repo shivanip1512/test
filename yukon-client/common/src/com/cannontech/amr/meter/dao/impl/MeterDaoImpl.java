ad2b04aa1901f95c1912cdcf409a618708020ad6
Vagyani, Asmita <>
2016-04-26 10:54:03 +0000

    YUK-13783 - Add support for adding a disconnect collar to a meter from the web, review comments fixed

--- yukon-client/common/src/com/cannontech/amr/meter/dao/impl/MeterDaoImpl.java
+++ yukon-client/common/src/com/cannontech/amr/meter/dao/impl/MeterDaoImpl.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
