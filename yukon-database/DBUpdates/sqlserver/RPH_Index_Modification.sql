06d98509454783c3c73ec34dfc61e43c28cf9bf4
DeZeeuw, Garrett <>
2016-05-10 20:30:02 +0000

    YUK-15173 Modify RawPointHistory constraints and indexes
    Returned primary key sort order to ASC.
    Wrapped the RPH_Index_Modification.sql statements within an IF check that checks to see if the statements have already been executed.

--- yukon-database/DBUpdates/sqlserver/RPH_Index_Modification.sql
+++ yukon-database/DBUpdates/sqlserver/RPH_Index_Modification.sql
@@ -13,7 +13,7 @@ BEGIN
     /* Creating this will take a while!!!! Slightly longer than the below one */
     ALTER TABLE RawPointHistory ADD CONSTRAINT PKC_RawPointHistory PRIMARY KEY CLUSTERED 
     (
-           ChangeId DESC
+           ChangeId ASC
     ) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, 
             ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON);
     
