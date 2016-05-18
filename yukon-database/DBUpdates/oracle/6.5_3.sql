8535442b6fd39f60597bce26ef3813d2501060b9
DeZeeuw, Garrett <>
2016-05-18 16:54:47 +0000

    CTI Database signature - trunk - 6.5.3
    Fixed insert statement.

--- yukon-database/DBUpdates/oracle/6.5_3.sql
+++ yukon-database/DBUpdates/oracle/6.5_3.sql
@@ -33,4 +33,4 @@ END;
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
 /**************************************************************/
-INSERT INTO CTIDatabase VALUES ('6.5', 11-MAY-2015', 'Latest Update', 3, SYSDATE);
+INSERT INTO CTIDatabase VALUES ('6.5', '11-MAY-2015', 'Latest Update', 3, SYSDATE);
