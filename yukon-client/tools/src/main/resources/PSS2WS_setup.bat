98241a4761f2e6a6c19612175b8b2610ceea1d6d
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2016-06-09 19:09:37 +0000

    YUK-15427 Remove unused Price Server project

--- yukon-client/tools/src/main/resources/PSS2WS_setup.bat
+++
@@ -1,8 +0,0 @@
-set endpoint=http://localhost:8080/head/services/AdminService
-set AXIS_LIB=C:\WebSphere\dev\workspace\third-party\
-set AXISCLASSPATH=%AXIS_LIB%\axis.jar;%AXIS_LIB%\commons-discovery.jar;%AXIS_LIB%\commons-logging.jar;%AXIS_LIB%\jaxrpc.jar;%AXIS_LIB%\saaj.jar;%AXIS_LIB%\log4j-1.2.8.jar;%AXIS_LIB%\xercesImpl-2.11.0.jar;%AXIS_LIB%\wsdl4j-1.6.3.jar;%AXIS_LIB%\mailapi-1.5.1.jar;%AXIS_LIB%\activation.jar
-
-java -cp %AXISCLASSPATH% org.apache.axis.wsdl.WSDL2Java --skeletonDeploy true ..\wsdl/PSS2WS.wsdl -pcom.cannontech.custom.pss2ws
-copy /Y com\cannontech\custom\pss2ws\deploy.wsdd ..\deploy\pss2ws_deploy.wsdd
-
-java -cp %AXISCLASSPATH% org.apache.axis.client.AdminClient -l%endpoint% ..\deploy\pss2ws_deploy.wsdd
