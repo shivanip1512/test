3efabfcf7e53360bb018661b6d7d3f3d977a85d0
Otteson, Jess M <JessMOtteson@Eaton.com>
2016-06-20 20:16:07 +0000

    6.5.4 build lines added

--- yukon-database/DBUpdates/oracle/6.5_4.sql
+++ yukon-database/DBUpdates/oracle/6.5_4.sql
@@ -37,4 +37,4 @@ WHERE p.PAObjectID = 0
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
 /**************************************************************/
-/*INSERT INTO CTIDatabase VALUES ('6.5', '01-JUN-2016', 'Latest Update', 4, SYSDATE);*/
+INSERT INTO CTIDatabase VALUES ('6.5', '20-JUN-2016', 'Latest Update', 4, SYSDATE);
--- yukon-database/DBUpdates/sqlserver/6.5_4.sql
+++ yukon-database/DBUpdates/sqlserver/6.5_4.sql
@@ -34,4 +34,4 @@ WHERE p.PAObjectID = 0
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
 /**************************************************************/
-/*INSERT INTO CTIDatabase VALUES ('6.5', '01-JUN-2016', 'Latest Update', 4, GETDATE());*/
+INSERT INTO CTIDatabase VALUES ('6.5', '20-JUN-2016', 'Latest Update', 4, GETDATE());
