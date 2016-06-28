c1aaa1e3044e6e4eb95a245a2d95258f15aca2a4
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2016-06-28 12:09:44 +0000

    YUK-14245 Enable calc-logic historical if any calc points have historical update type (vs relying on cparm)  - added cparm to deprecated list

--- yukon-client/common/src/com/cannontech/common/config/MasterConfigDeprecatedKey.java
+++ yukon-client/common/src/com/cannontech/common/config/MasterConfigDeprecatedKey.java
@@ -25,6 +25,7 @@ public enum MasterConfigDeprecatedKey {
     SCANNER_QUEUE, // see yuk-8552
     SCANNER_QUEUE_SCANS, // see yuk-8552
     YUKON_SMTP_SERVER, // used when dispatch sent notifications and it no longer does
+    CALC_LOGIC_RUN_HISTORICAL, //see YUK-14245
 
     /* The following YUKON_DNP_* keys were intended to be deprecated but have been commented out of this enum.
      * This was done to prevent the 5.5.2 update script from throwing an exception when
