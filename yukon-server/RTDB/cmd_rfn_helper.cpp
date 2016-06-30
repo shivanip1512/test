b046addc067fdff23afc4e99cad887e36d0ba772
Fisher, Matthew S <MatthewSFisher@Eaton.com>
2016-06-30 19:50:56 +0000

    YUK-15449 Porter support for data streaming configuration Trimmed out unnecessary make_pair

--- yukon-server/RTDB/cmd_rfn_helper.cpp
+++ yukon-server/RTDB/cmd_rfn_helper.cpp
@@ -11,24 +11,24 @@ namespace Commands {
 namespace   {
 
 const std::map< std::pair<unsigned char, unsigned char>, std::string>  ascAscqResolver {
-    { std::make_pair( 0x00, 0x00 ), "NO ADDITIONAL STATUS" },
-    { std::make_pair( 0x00, 0x01 ), "REJECTED, SERVICE NOT SUPPORTED" },
-    { std::make_pair( 0x00, 0x02 ), "REJECTED, INVALID FIELD IN COMMAND" },
-    { std::make_pair( 0x00, 0x03 ), "REJECTED, INAPPROPRIATE ACTION REQUESTED" },
-    { std::make_pair( 0x00, 0x04 ), "REJECTED, LOAD VOLTAGE HIGHER THAN THRESHOLD" },
-    { std::make_pair( 0x00, 0x05 ), "REJECTED, SWITCH IS OPEN" },
-    { std::make_pair( 0x00, 0x06 ), "REJECTED, TEST MODE ENABLED" },
-    { std::make_pair( 0x00, 0x07 ), "REJECTED, SERVICE DISCONNECT BUTTON PRESSED BUT METER NOT ARMED" },
-    { std::make_pair( 0x00, 0x08 ), "REJECTED, SERVICE DISCONNECT NOT ENABLED" },
-    { std::make_pair( 0x00, 0x09 ), "REJECTED, SERVICE DISCONNECT IS CURRENTLY CHARGING" },
-    { std::make_pair( 0x00, 0x0a ), "REJECTED, SERVICE DISCONNECT IN OPERATION" },
-    { std::make_pair( 0x01, 0x00 ), "ACCESS DENIED, INSUFFICIENT SECURITY CLEARANCE" },
-    { std::make_pair( 0x01, 0x01 ), "ACCESS DENIED, DATA LOCKED" },
-    { std::make_pair( 0x01, 0x02 ), "ACCESS DENIED, INVALID SERVICE SEQUENCE STATE" },
-    { std::make_pair( 0x01, 0x03 ), "ACCESS DENIED, RENEGOTIATE REQUEST" },
-    { std::make_pair( 0x02, 0x00 ), "DATA NOT READY" },
-    { std::make_pair( 0x03, 0x00 ), "DEVICE BUSY" },
-    { std::make_pair( 0x04, 0x00 ), "SCHEDULED FOR NEXT RECORD INTERVAL" }
+    { { 0x00, 0x00 }, "NO ADDITIONAL STATUS" },
+    { { 0x00, 0x01 }, "REJECTED, SERVICE NOT SUPPORTED" },
+    { { 0x00, 0x02 }, "REJECTED, INVALID FIELD IN COMMAND" },
+    { { 0x00, 0x03 }, "REJECTED, INAPPROPRIATE ACTION REQUESTED" },
+    { { 0x00, 0x04 }, "REJECTED, LOAD VOLTAGE HIGHER THAN THRESHOLD" },
+    { { 0x00, 0x05 }, "REJECTED, SWITCH IS OPEN" },
+    { { 0x00, 0x06 }, "REJECTED, TEST MODE ENABLED" },
+    { { 0x00, 0x07 }, "REJECTED, SERVICE DISCONNECT BUTTON PRESSED BUT METER NOT ARMED" },
+    { { 0x00, 0x08 }, "REJECTED, SERVICE DISCONNECT NOT ENABLED" },
+    { { 0x00, 0x09 }, "REJECTED, SERVICE DISCONNECT IS CURRENTLY CHARGING" },
+    { { 0x00, 0x0a }, "REJECTED, SERVICE DISCONNECT IN OPERATION" },
+    { { 0x01, 0x00 }, "ACCESS DENIED, INSUFFICIENT SECURITY CLEARANCE" },
+    { { 0x01, 0x01 }, "ACCESS DENIED, DATA LOCKED" },
+    { { 0x01, 0x02 }, "ACCESS DENIED, INVALID SERVICE SEQUENCE STATE" },
+    { { 0x01, 0x03 }, "ACCESS DENIED, RENEGOTIATE REQUEST" },
+    { { 0x02, 0x00 }, "DATA NOT READY" },
+    { { 0x03, 0x00 }, "DEVICE BUSY" },
+    { { 0x04, 0x00 }, "SCHEDULED FOR NEXT RECORD INTERVAL" }
 };
 
 }
