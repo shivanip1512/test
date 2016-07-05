6ddc50e31ee06b3cf62d536c7a545b6a24953236
Fisher, Matthew S <MatthewSFisher@Eaton.com>
2016-07-05 21:07:16 +0000

    YUK-15451 Porter support for data streaming messaging with Web Added operator<< for std::chrono::minutes and Attribute

--- yukon-server/DeviceConfiguration/test_main.cpp
+++ yukon-server/DeviceConfiguration/test_main.cpp
@@ -2,3 +2,17 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include "PointAttribute.h"
+#include <chrono>
+
+namespace std {
+    ostream &operator<<(ostream& os, const chrono::minutes& m)
+    {
+        return os << m.count() << "minutes";
+    }
+}
+
+std::ostream &operator<<(std::ostream& os, const Attribute &a)
+{
+    return os << a.getName();
+}
