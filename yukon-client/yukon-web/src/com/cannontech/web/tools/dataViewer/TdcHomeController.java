464faa232b6a8a68d488a2ba842135bc6ca9b743
Feldman, Marina <MarinaFeldman@Eaton.com>
2016-07-11 16:14:33 +0000

    YUK-15442 Implement DeviceBehaviorService/ checked in by mistake - reverting

--- yukon-client/yukon-web/src/com/cannontech/web/tools/dataViewer/TdcHomeController.java
+++ yukon-client/yukon-web/src/com/cannontech/web/tools/dataViewer/TdcHomeController.java
@@ -1,7 +1,9 @@
 package com.cannontech.web.tools.dataViewer;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,23 +14,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.cannontech.common.device.streaming.dao.DeviceBehaviorDao;
 import com.cannontech.common.i18n.MessageSourceAccessor;
-import com.cannontech.common.pao.attribute.model.BuiltInAttribute;
 import com.cannontech.common.tdc.dao.DisplayDao;
+import com.cannontech.common.tdc.model.Display;
 import com.cannontech.common.tdc.model.DisplayData;
+import com.cannontech.common.tdc.model.DisplayType;
 import com.cannontech.common.tdc.model.IDisplay;
 import com.cannontech.common.tdc.service.TdcService;
 import com.cannontech.core.roleproperties.YukonRole;
 import com.cannontech.i18n.YukonMessageSourceResolvable;
 import com.cannontech.i18n.YukonUserContextMessageSourceResolver;
 import com.cannontech.user.YukonUserContext;
-import com.cannontech.web.rfn.dataStreaming.model.DataStreamingAttribute;
-import com.cannontech.web.rfn.dataStreaming.model.DataStreamingConfig;
-import com.cannontech.web.rfn.dataStreaming.service.DataStreamingService;
 import com.cannontech.web.security.annotation.CheckRole;
 import com.google.common.base.Function;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Maps;
 
 @Controller
@@ -38,45 +36,28 @@ public class TdcHomeController {
     @Autowired private DisplayDao displayDao;
     @Autowired private TdcService tdcService;
     @Autowired private YukonUserContextMessageSourceResolver resolver;
-    @Autowired private DeviceBehaviorDao deviceBehaviorDao;
-    @Autowired private DataStreamingService dataStreamingService;
-    @Autowired protected YukonUserContextMessageSourceResolver messageSourceResolver;
     
     @RequestMapping("data-viewer")
-    public String home(ModelMap model, YukonUserContext userContext) {
-             
-      MessageSourceAccessor accessor = messageSourceResolver.getMessageSourceAccessor(userContext);
-      List<BuiltInAttribute> attributes = ImmutableList.of(BuiltInAttribute.KVAR,
-          BuiltInAttribute.DEMAND, BuiltInAttribute.DELIVERED_KWH);
-      DataStreamingConfig newConfig = new DataStreamingConfig(accessor);
-      int i = 0;
-      attributes.forEach(a -> {
-          DataStreamingAttribute attribute = new DataStreamingAttribute();
-          attribute.setAttribute(a);
-          attribute.setInterval(7);
-          attribute.setAttributeOn(Boolean.TRUE);
-          newConfig.addAttribute(attribute);
-      });
-      System.out.println(newConfig.getName());
-
-      
-      //int id = dataStreamingService.saveConfig(newConfig);
-      
-      DataStreamingConfig config = dataStreamingService.findDataStreamingConfiguration(154);
-      System.out.println();
-  
-     
-  
-     // List<LiteBehavior> b  = deviceBehaviorDao.getLiteBehaviorsByType(BehaviorType.DATA_STREAMING);
-      
-    
-    /*  BehaviorReport report = new BehaviorReport();
-      report.setDeviceId(6);
-      report.setStatus(BehaviorReportStatus.PENDING);
-      report.setType(BehaviorType.DATA_STREAMING);
-      report.setTimestamp(new Instant());
-      report.setValues(values);
-      int id = deviceBehaviorDao.saveBehaviorReport(report);*/
+    public String home(ModelMap model) {
+        
+       List<Display> custom = displayDao.getDisplayByType(DisplayType.CUSTOM_DISPLAYS);
+       List<Display> events = displayDao.getDisplayByType(DisplayType.ALARMS_AND_EVENTS);
+       List<Display> topEvents = new ArrayList<>();
+       ListIterator<Display> it =  events.listIterator();
+        while (it.hasNext()) {
+            Display display = it.next();
+            switch (display.getDisplayId()) {
+            case IDisplay.SOE_LOG_DISPLAY_NUMBER:
+            case IDisplay.TAG_LOG_DISPLAY_NUMBER:
+            case IDisplay.EVENT_VIEWER_DISPLAY_NUMBER:
+            case IDisplay.GLOBAL_ALARM_DISPLAY:
+                topEvents.add(display);
+                it.remove();
+            }
+        }
+        model.addAttribute("customEvents", custom);
+        model.addAttribute("events", events);
+        model.addAttribute("topEvents", topEvents);
         return "data-viewer/home.jsp";
     }
     
@@ -103,7 +84,7 @@ public class TdcHomeController {
     }
      
     
-    @RequestMapping(value="data-viewer/acknowledge-all", method=RequestMethod.POST)
+    @RequestMapping(value="data-viewer/acknowledgeAll", method=RequestMethod.POST)
     public @ResponseBody Map<String, String> acknowledgeAll(YukonUserContext context) {
         
         int alarms = tdcService.acknowledgeAllAlarms(context.getYukonUser());
@@ -113,7 +94,7 @@ public class TdcHomeController {
         return Collections.singletonMap("success", accessor.getMessage(successMsg));
     }
     
-    @RequestMapping(value="data-viewer/acknowledge", method=RequestMethod.POST)
+    @RequestMapping(value="/acknowledge", method=RequestMethod.POST)
     public @ResponseBody Map<String, String> acknowledge(YukonUserContext context, int pointId, int condition) {
         
         tdcService.acknowledgeAlarm(pointId, condition, context.getYukonUser());
