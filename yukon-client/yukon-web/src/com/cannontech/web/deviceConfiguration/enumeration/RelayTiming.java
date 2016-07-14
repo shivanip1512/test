7070edad37aa4d83669af3abf0cdf29ea45e907e
Johnson, Wayne <>
2016-07-14 21:00:14 +0000

    YUK-13440 - DeviceConfig Enumerations have inconsistent DB values (SQL)

--- yukon-client/yukon-web/src/com/cannontech/web/deviceConfiguration/enumeration/RelayTiming.java
+++ yukon-client/yukon-web/src/com/cannontech/web/deviceConfiguration/enumeration/RelayTiming.java
@@ -20,26 +20,26 @@ public final class RelayTiming implements DeviceConfigurationInputEnumeration {
 
     @Autowired private YukonUserContextMessageSourceResolver messageResolver;
 
-    private YukonMessageSourceResolvable formatDbValue(int dbValue) {
+    private YukonMessageSourceResolvable formatTimer(double timer) {
         /*
          * The integer value is written to the database, and represents a relay timing in the following fashion:
-         *    1 = 250 ms
-         *    2 = 500 ms
-         *    4 = 1 s
-         *    8 = 2 s 
+         *    .250 = 250 ms
+         *    .500 = 500 ms
+         *    1.000 = 1 s
+         *    2.000 = 2 s 
          * etc.
          */
-        if (dbValue % 4 == 0) {
-            // If dbValue mod 4 is zero, this value is only seconds with no millis.
-            return new YukonMessageSourceResolvable(baseKey + ".seconds", dbValue / 4);
-        } else if (dbValue < 4) {
-            // There aren't any seconds if the dbValue is less than 4, just millis.
-            return new YukonMessageSourceResolvable(baseKey + ".millis", dbValue * 250);
+        if (timer == (int)timer) {
+            // This timer is only seconds with no millis.
+            return new YukonMessageSourceResolvable(baseKey + ".seconds", (int)timer );
+        } else if (timer < 1.0) {
+            // There aren't any seconds if the dbValue is less than 1000, just millis.
+            return new YukonMessageSourceResolvable(baseKey + ".millis", timer * 1000 );
         } else {
             // We have a dbValue representing both seconds and millis.
-            int seconds = dbValue / 4;
-            int millis = (dbValue % 4) * 250;
-            return new YukonMessageSourceResolvable(baseKey + ".both", seconds, millis);
+            double millis = timer%1;
+            double seconds = timer-millis;
+            return new YukonMessageSourceResolvable(baseKey + ".both", seconds, millis*1000);
         }
     }
 
@@ -49,9 +49,9 @@ public final class RelayTiming implements DeviceConfigurationInputEnumeration {
         final DecimalFormat decimalFormatter = new DecimalFormat("0.00");
         List<InputOption> validTimings = new ArrayList<>();
 
-        for (int dbValue = 1; dbValue <= 50; dbValue++) {
-            validTimings.add(new InputOption(decimalFormatter.format((double) dbValue / 4),
-                                             messageAccessor.getMessage(formatDbValue(dbValue))));
+        for (double timer = .250; timer <= 63.750; timer += .250) {
+            validTimings.add(new InputOption(decimalFormatter.format(timer),
+                                             messageAccessor.getMessage(formatTimer(timer))));
         }
         return validTimings;
     }
