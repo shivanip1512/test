2c1d5f0173827047db4b9519db8583026c01e3ec
Vant, Carrie <carrieVant@Eaton.com>
2016-07-29 15:05:16 +0000

    YUK-15552 - Data Streaming Verification UI   - changed Devices Not Supported messages to be internationalized

--- yukon-client/yukon-web/src/com/cannontech/web/bulk/DataStreamingController.java
+++ yukon-client/yukon-web/src/com/cannontech/web/bulk/DataStreamingController.java
@@ -102,7 +102,10 @@ public class DataStreamingController {
 
         VerificationInformation verifyInfo = dataStreamingService.verifyConfiguration(configId, deviceIds);
         verifyInfo.setConfiguration(modelConfig);
-        verifyInfo.getDeviceUnsupported().forEach(device -> device.setDeviceCollection(dcProducer.createDeviceCollection(device.getDeviceIds(), null)));
+        verifyInfo.getDeviceUnsupported().forEach(device -> {
+            device.setDeviceCollection(dcProducer.createDeviceCollection(device.getDeviceIds(), null));
+            device.setAccessor(accessor);
+        });
         verifyInfo.getGatewayLoadingInfo().forEach(gateway -> gateway.setAccessor(accessor));
         
         model.addAttribute("verificationInfo", verifyInfo);
--- yukon-client/yukon-web/src/com/cannontech/web/rfn/dataStreaming/model/DeviceUnsupported.java
+++ yukon-client/yukon-web/src/com/cannontech/web/rfn/dataStreaming/model/DeviceUnsupported.java
@@ -4,15 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.cannontech.common.bulk.collection.device.model.DeviceCollection;
+import com.cannontech.common.i18n.MessageSourceAccessor;
 import com.cannontech.common.pao.attribute.model.BuiltInAttribute;
 
 public class DeviceUnsupported {
+    
+    private static final String nameKey= "yukon.web.modules.tools.bulk.dataStreaming.verification.devicesUnsupported";
 
     private List<Integer> deviceIds = new ArrayList<Integer>();
     private List<BuiltInAttribute> attributes = new ArrayList<BuiltInAttribute>();
     private String detail;
     private DeviceCollection deviceCollection;
-    
+    private MessageSourceAccessor accessor;
     
     public List<Integer> getDeviceIds() {
         return deviceIds;
@@ -26,11 +29,13 @@ public class DeviceUnsupported {
     public void setAttributes(List<BuiltInAttribute> attributes) {
         this.attributes = attributes;
     }
+    public void setAccessor(MessageSourceAccessor accessor) {
+        this.accessor = accessor;
+    }
     public String getDetail() {
-        String devices = deviceIds.size() > 1 ? " devices do" : " device does";
         List<String> attList = new ArrayList<String>();
         getAttributes().forEach(attribute -> attList.add(attribute.getDescription()));
-        detail = deviceIds.size() + devices + " not support " + String.join(", ",  attList);
+        detail = accessor.getMessage(nameKey, deviceIds.size(), String.join(", ", attList));
         return detail;
     }
     public void setDetail(String detail) {
