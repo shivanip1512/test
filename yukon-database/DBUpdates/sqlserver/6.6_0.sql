63b73bfdce35ff79e0ee350fc2979c96bbf20a92
Modi, Preeti <>
2016-08-08 11:47:51 +0000

    YUK-15502 : Common Commands Missing for the RFN-500

--- yukon-database/DBUpdates/sqlserver/6.6_0.sql
+++ yukon-database/DBUpdates/sqlserver/6.6_0.sql
@@ -567,6 +567,94 @@ ADD CONSTRAINT PK_FDRTrans PRIMARY KEY (PointId, DirectionType, InterfaceType, T
 GO
 /* End YUK-15548 */
 
+/* Start YUK-15502 */
+/* @error ignore-begin */
+INSERT INTO DeviceTypeCommand VALUES (-1157, -193, 'RFN-510fL', 1, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1158, -194, 'RFN-510fL', 2, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1159, -195, 'RFN-510fL', 3, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1160, -196, 'RFN-510fL', 4, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1161, -197, 'RFN-510fL', 5, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1162, -198, 'RFN-510fL', 6, 'Y', -1);
+
+INSERT INTO DeviceTypeCommand VALUES (-1163, -193, 'RFN-520fAX', 1, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1164, -194, 'RFN-520fAX', 2, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1165, -195, 'RFN-520fAX', 3, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1166, -196, 'RFN-520fAX', 4, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1167, -197, 'RFN-520fAX', 5, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1168, -198, 'RFN-520fAX', 6, 'Y', -1);
+
+INSERT INTO DeviceTypeCommand VALUES (-1169, -193, 'RFN-520fRX', 1, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1170, -194, 'RFN-520fRX', 2, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1171, -195, 'RFN-520fRX', 3, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1172, -196, 'RFN-520fRX', 4, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1173, -197, 'RFN-520fRX', 5, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1174, -198, 'RFN-520fRX', 6, 'Y', -1);
+
+INSERT INTO DeviceTypeCommand VALUES (-1175, -193, 'RFN-520fAXD', 1, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1176, -194, 'RFN-520fAXD', 2, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1177, -195, 'RFN-520fAXD', 3, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1178, -196, 'RFN-520fAXD', 4, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1179, -197, 'RFN-520fAXD', 5, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1180, -198, 'RFN-520fAXD', 6, 'Y', -1);
+
+INSERT INTO DeviceTypeCommand VALUES (-1181, -193, 'RFN-520fRXD', 1, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1182, -194, 'RFN-520fRXD', 2, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1183, -195, 'RFN-520fRXD', 3, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1184, -196, 'RFN-520fRXD', 4, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1185, -197, 'RFN-520fRXD', 5, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1186, -198, 'RFN-520fRXD', 6, 'Y', -1);
+
+INSERT INTO DeviceTypeCommand VALUES (-1187, -193, 'RFN-530fAX', 1, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1188, -194, 'RFN-530fAX', 2, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1189, -195, 'RFN-530fAX', 3, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1190, -196, 'RFN-530fAX', 4, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1191, -197, 'RFN-530fAX', 5, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1192, -198, 'RFN-530fAX', 6, 'Y', -1);
+
+INSERT INTO DeviceTypeCommand VALUES (-1193, -193, 'RFN-530fRX', 1, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1194, -194, 'RFN-530fRX', 2, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1195, -195, 'RFN-530fRX', 3, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1196, -196, 'RFN-530fRX', 4, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1197, -197, 'RFN-530fRX', 5, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1198, -198, 'RFN-530fRX', 6, 'Y', -1);
+
+INSERT INTO DeviceTypeCommand VALUES (-1199, -193, 'RFN-530S4x', 1, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1200, -194, 'RFN-530S4x', 2, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1201, -195, 'RFN-530S4x', 3, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1202, -196, 'RFN-530S4x', 4, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1203, -197, 'RFN-530S4x', 5, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1204, -198, 'RFN-530S4x', 6, 'Y', -1);
+
+INSERT INTO DeviceTypeCommand VALUES (-1205, -193, 'RFN-530S4eAD', 1, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1206, -194, 'RFN-530S4eAD', 2, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1207, -195, 'RFN-530S4eAD', 3, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1208, -196, 'RFN-530S4eAD', 4, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1209, -197, 'RFN-530S4eAD', 5, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1210, -198, 'RFN-530S4eAD', 6, 'Y', -1);
+
+INSERT INTO DeviceTypeCommand VALUES (-1211, -193, 'RFN-530S4eAT', 1, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1212, -194, 'RFN-530S4eAT', 2, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1213, -195, 'RFN-530S4eAT', 3, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1214, -196, 'RFN-530S4eAT', 4, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1215, -197, 'RFN-530S4eAT', 5, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1216, -198, 'RFN-530S4eAT', 6, 'Y', -1);
+
+INSERT INTO DeviceTypeCommand VALUES (-1217, -193, 'RFN-530S4eRD', 1, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1218, -194, 'RFN-530S4eRD', 2, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1219, -195, 'RFN-530S4eRD', 3, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1220, -196, 'RFN-530S4eRD', 4, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1221, -197, 'RFN-530S4eRD', 5, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1222, -198, 'RFN-530S4eRD', 6, 'Y', -1);
+
+INSERT INTO DeviceTypeCommand VALUES (-1223, -193, 'RFN-530S4eRT', 1, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1224, -194, 'RFN-530S4eRT', 2, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1225, -195, 'RFN-530S4eRT', 3, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1226, -196, 'RFN-530S4eRT', 4, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1227, -197, 'RFN-530S4eRT', 5, 'Y', -1);
+INSERT INTO DeviceTypeCommand VALUES (-1228, -198, 'RFN-530S4eRT', 6, 'Y', -1);
+/* @error ignore-end */
+/* End YUK-15502 */
+
 /**************************************************************/
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
