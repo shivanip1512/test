a34604c0a6f9c7a9566513cd6fae8490004ae018
Vagyani, Asmita <>
2016-08-22 09:03:41 +0000

    YUK-15472 - "Content-Security-Policy" header review comments fixed

--- yukon-client/yukon-web/src/com/cannontech/web/filter/ContentSecurityPolicyFilterType.java
+++ yukon-client/yukon-web/src/com/cannontech/web/filter/ContentSecurityPolicyFilterType.java
@@ -1,9 +1,7 @@
 package com.cannontech.web.filter;
 
 public enum ContentSecurityPolicyFilterType {
-    DEFAULT_SRC("default-src "
-        + "'self' "
-        + "'unsafe-inline' " 
+    DEFAULT_SRC("default-src 'self' 'unsafe-inline' " 
         + "http://java.sun.com " 
         + "http://cannontech.com "
         + "http://docs.spring.io " 
@@ -15,37 +13,27 @@ public enum ContentSecurityPolicyFilterType {
         + "http://api.recaptcha.net " 
         + "http://www.google-analytics.com " 
         + "https://www.google.com/recaptcha/; "),
-    SCRIPT_SRC("script-src "
-        + "'self' "
+    SCRIPT_SRC("script-src 'self' 'unsafe-inline' 'unsafe-eval' "
         + "http://www.google.com/js " 
         + "https://api.tiles.mapbox.com " 
-        + "'unsafe-inline' "
-        + "'unsafe-eval' " 
         + "http://www.google-analytics.com " 
         + "https://www.google.com/recaptcha/; "),
-    CONNECT_SRC("connect-src " 
-        + "'self' " 
-        + "'unsafe-inline' " 
+    CONNECT_SRC("connect-src 'self' 'unsafe-inline' " 
         + "https://api.mapbox.com "
         + "https://*.tiles.mapbox.com " 
         + "https://www.google.com/recaptcha/; "),
-    IMG_SRC("img-src "
-        + "'self' " 
+    IMG_SRC("img-src 'self' data: Access-Control-Allow-Origin: *" // Access-Control-Allow-Origin: * used for cross origin resource sharing for map images
         + "https://www.google.com/recaptcha/ " 
-        + "data: " + "http://www.google-analytics.com "
-        + "http://api.recaptcha.net " 
-        + "Access-Control-Allow-Origin: *;"), // Access-Control-Allow-Origin: * used for cross origin resource sharing for map images
-    STYLE_SRC("style-src "
-        + "'self' " 
-        + "'unsafe-inline' " 
+        + "http://www.google-analytics.com "
+        + "http://api.recaptcha.net;"),
+    STYLE_SRC("style-src 'self' 'unsafe-inline' " 
         + "https://api.tiles.mapbox.com "
         + "https://www.google.com/recaptcha; "),
     MEDIA_SRC("media-src 'self';"),
     CHILD_SRC("child-src 'self';"),
     OBJECT_SRC("object-src 'self';"),
     FONT_SRC("font-src 'self';"),
-    FRAME_SRC("frame-src "
-        + "'self' " 
+    FRAME_SRC("frame-src 'self' " 
         + "https://www.google.com/recaptcha"),
     FRAME_ANCESTORS("'self'"),
     FORM_ACTION("'self'"),
