0e27141fac86f6f64eb7b6109ca41650a2f1ae53
Rawat, Payal <PayalRawat@Eaton.com>
2016-09-06 09:19:23 +0000

    YUK-15674 - Create new service for hosting the AMQ broker

--- yukon-client/common/src/com/cannontech/services/jms/ClientEmbeddedBroker.java
+++ yukon-client/common/src/com/cannontech/services/jms/ClientEmbeddedBroker.java
@@ -61,7 +61,7 @@ public class ClientEmbeddedBroker {
             //  network manager -> remote proxy -> local proxy -> service manager -> web
             networkConnector.setNetworkTTL(5);
             broker.setUseJmx(true);
-            if(name.equals("ServiceManager")) {
+            if (name.equals("ServiceManager")) {
                 broker.getManagementContext().setConnectorPort(1099);
             } else {
                 broker.getManagementContext().setConnectorPort(1098);
