61634ea1444cbc6c83b95fe8d852787207931520
Beckius, Aaron T <AaronTBeckius@Eaton.com>
2016-09-29 14:34:30 +0000

    YUK-15718 Send LM messages from C++ to Java to initiate and cancel Honeywell control
    Minor formatting work and a bit of code modernization.

--- yukon-server/LoadManagement/include/honeywellControlInterface.h
+++ yukon-server/LoadManagement/include/honeywellControlInterface.h
@@ -6,14 +6,14 @@
 namespace Cti {
 namespace LoadManagement {
 
-    struct HoneywellControlInterface
-    {
-        virtual bool sendCycleControl(long dutyCycle,
-                                      long controlDurationSeconds,
-                                      bool rampInOutOption) = 0;
-    };
+struct HoneywellControlInterface
+{
+    virtual bool sendCycleControl(long dutyCycle,
+                                    long controlDurationSeconds,
+                                    bool rampInOutOption) = 0;
+};
 
-    typedef boost::shared_ptr<HoneywellControlInterface> HoneywellControlInterfacePtr;
+typedef boost::shared_ptr<HoneywellControlInterface> HoneywellControlInterfacePtr;
 
 }
 }
--- yukon-server/LoadManagement/test_lmobjects.cpp
+++ yukon-server/LoadManagement/test_lmobjects.cpp
@@ -297,84 +297,57 @@ BOOST_AUTO_TEST_CASE(test_lmobjects_honeywell_group)
 
     reader();
 
-    const std::size_t   object_size = sizeof LMGroupHoneywell;
-    char                object_buffer[object_size];
-
-    std::fill(object_buffer, object_buffer + object_size, 0xAA);
-
-    // custom deleter to call only the destructor for the honeywell group allocated by placement new
-    struct HoneywellGroupDeleter
-    {
-        void operator()(LMGroupHoneywell * group)
-        {
-            group->~LMGroupHoneywell();
-        }
-    };
-
-    boost::shared_ptr<LMGroupHoneywell>
-        group(new (object_buffer) LMGroupHoneywell(reader), HoneywellGroupDeleter());
+    LMGroupHoneywell group{ reader };
 
     // Document the initial state of the object
 
-    BOOST_CHECK_EQUAL(group->getPAOId(), 1234);
+    BOOST_CHECK_EQUAL(group.getPAOId(), 1234);
 
-    BOOST_CHECK_EQUAL(group->getPAOCategory(), "DEVICE");
-    BOOST_CHECK_EQUAL(group->getPAOClass(), "GROUP");
-    BOOST_CHECK_EQUAL(group->getPAOName(), "Test Group");
-    BOOST_CHECK_EQUAL(group->getPAOType(), TYPE_LMGROUP_HONEYWELL);
-    BOOST_CHECK_EQUAL(group->getPAOTypeString(), "HONEYWELL GROUP");
-    BOOST_CHECK_EQUAL(group->getPAODescription(), "(none)");
+    BOOST_CHECK_EQUAL(group.getPAOCategory(), "DEVICE");
+    BOOST_CHECK_EQUAL(group.getPAOClass(), "GROUP");
+    BOOST_CHECK_EQUAL(group.getPAOName(), "Test Group");
+    BOOST_CHECK_EQUAL(group.getPAOType(), TYPE_LMGROUP_HONEYWELL);
+    BOOST_CHECK_EQUAL(group.getPAOTypeString(), "HONEYWELL GROUP");
+    BOOST_CHECK_EQUAL(group.getPAODescription(), "(none)");
 
-    BOOST_CHECK_EQUAL(group->getDisableFlag(), FALSE);
-    BOOST_CHECK_EQUAL(group->getAlarmInhibit(), FALSE);
-    BOOST_CHECK_EQUAL(group->getControlInhibit(), FALSE);
+    BOOST_CHECK_EQUAL(group.getDisableFlag(), FALSE);
+    BOOST_CHECK_EQUAL(group.getAlarmInhibit(), FALSE);
+    BOOST_CHECK_EQUAL(group.getControlInhibit(), FALSE);
 
-    BOOST_CHECK_EQUAL(group->getIsRampingIn(), false);
-    BOOST_CHECK_EQUAL(group->getIsRampingOut(), false);
+    BOOST_CHECK_EQUAL(group.getIsRampingIn(), false);
+    BOOST_CHECK_EQUAL(group.getIsRampingOut(), false);
 
-    BOOST_CHECK_EQUAL(group->getCurrentHoursDaily(), 0);
-    BOOST_CHECK_EQUAL(group->getCurrentHoursMonthly(), 0);
-    BOOST_CHECK_EQUAL(group->getCurrentHoursSeasonal(), 0);
-    BOOST_CHECK_EQUAL(group->getCurrentHoursAnnually(), 0);
+    BOOST_CHECK_EQUAL(group.getCurrentHoursDaily(), 0);
+    BOOST_CHECK_EQUAL(group.getCurrentHoursMonthly(), 0);
+    BOOST_CHECK_EQUAL(group.getCurrentHoursSeasonal(), 0);
+    BOOST_CHECK_EQUAL(group.getCurrentHoursAnnually(), 0);
 
-    BOOST_CHECK_EQUAL(group->getHoursDailyPointId(), 0);
-    BOOST_CHECK_EQUAL(group->getHoursMonthlyPointId(), 0);
-    BOOST_CHECK_EQUAL(group->getHoursSeasonalPointId(), 0);
-    BOOST_CHECK_EQUAL(group->getHoursAnnuallyPointId(), 0);
+    BOOST_CHECK_EQUAL(group.getHoursDailyPointId(), 0);
+    BOOST_CHECK_EQUAL(group.getHoursMonthlyPointId(), 0);
+    BOOST_CHECK_EQUAL(group.getHoursSeasonalPointId(), 0);
+    BOOST_CHECK_EQUAL(group.getHoursAnnuallyPointId(), 0);
 
-    BOOST_CHECK_EQUAL(group->getLastControlSent(), gInvalidCtiTime);
-    BOOST_CHECK_EQUAL(group->getControlStartTime(), gInvalidCtiTime);
-    BOOST_CHECK_EQUAL(group->getControlCompleteTime(), gInvalidCtiTime);
-    BOOST_CHECK_EQUAL(group->getNextControlTime(), gInvalidCtiTime);
-    BOOST_CHECK_EQUAL(group->getLastStopTimeSent(), gInvalidCtiTime);
+    BOOST_CHECK_EQUAL(group.getLastControlSent(), gInvalidCtiTime);
+    BOOST_CHECK_EQUAL(group.getControlStartTime(), gInvalidCtiTime);
+    BOOST_CHECK_EQUAL(group.getControlCompleteTime(), gInvalidCtiTime);
+    BOOST_CHECK_EQUAL(group.getNextControlTime(), gInvalidCtiTime);
+    BOOST_CHECK_EQUAL(group.getLastStopTimeSent(), gInvalidCtiTime);
 
-    BOOST_CHECK_EQUAL(group->getGroupControlState(), CtiLMGroupBase::InactiveState);
+    BOOST_CHECK_EQUAL(group.getGroupControlState(), CtiLMGroupBase::InactiveState);
 
-    BOOST_CHECK_EQUAL(group->getLastControlString(), "");
+    BOOST_CHECK_EQUAL(group.getLastControlString(), "");
 
-    BOOST_CHECK_EQUAL(group->getDailyOps(), 0);
-    BOOST_CHECK_CLOSE(group->getKWCapacity(), 3.1415, 1e-4);
-
-    // The following are not initialized by the current reader - they are set later:
-
-    // when points are loaded for the groups
-    BOOST_CHECK_EQUAL(group->getControlStatusPointId(), 0xAAAAAAAA);
-    // when the group is attached to the program
-    BOOST_CHECK_EQUAL(group->getGroupOrder(), 0xAAAAAAAA);
-    // never used...
-    BOOST_CHECK_EQUAL(group->getChildOrder(), 0xAAAAAAAA);
-
-    // This guy is initialized to the creation time of the object
-    //BOOST_CHECK_EQUAL( group->getDynamicTimestamp(), 0 );
+    BOOST_CHECK_EQUAL(group.getDailyOps(), 0);
+    BOOST_CHECK_CLOSE(group.getKWCapacity(), 3.1415, 1e-4);
 
     // clone to test operators
-    boost::scoped_ptr<CtiLMGroupBase>   clone(group->replicate());
+    auto clone = group.replicate();
 
-    BOOST_CHECK(*group == *clone);
+    BOOST_CHECK(group == *clone);
 
     clone->setPAOId(clone->getPAOId() + 1);
 
-    BOOST_CHECK(*group != *clone);
+    BOOST_CHECK(group != *clone);
 }
 
 
@@ -454,45 +427,45 @@ BOOST_AUTO_TEST_CASE(test_lmobjects_honeywell_cycle_gear)
 
     reader();
 
-    boost::scoped_ptr<HoneywellCycleGear>  gear(new HoneywellCycleGear(reader));
+    HoneywellCycleGear gear{ reader };
 
-    BOOST_CHECK_EQUAL(gear->getProgramPAOId(), 101);
-    BOOST_CHECK_EQUAL(gear->getGearName(), "Test Honeywell Cycle Gear");
-    BOOST_CHECK_EQUAL(gear->getGearNumber(), 2);
-    BOOST_CHECK_EQUAL(gear->getControlMethod(), "HoneywellCycle");
-    BOOST_CHECK_EQUAL(gear->getMethodRate(), 5);
-    BOOST_CHECK_EQUAL(gear->getMethodPeriod(), 6);
-    BOOST_CHECK_EQUAL(gear->getMethodRateCount(), 7);
-    BOOST_CHECK_EQUAL(gear->getCycleRefreshRate(), 8);
-    BOOST_CHECK_EQUAL(gear->getMethodStopType(), "Restore");
-    BOOST_CHECK_EQUAL(gear->getChangeCondition(), "None");
-    BOOST_CHECK_EQUAL(gear->getChangeDuration(), 9);
-    BOOST_CHECK_EQUAL(gear->getChangePriority(), 10);
-    BOOST_CHECK_EQUAL(gear->getChangeTriggerNumber(), 11);
-    BOOST_CHECK_CLOSE(gear->getChangeTriggerOffset(), 1.2, 1e-1);
-    BOOST_CHECK_EQUAL(gear->getPercentReduction(), 10);
-    BOOST_CHECK_EQUAL(gear->getGroupSelectionMethod(), "LastControlled");
-    BOOST_CHECK_EQUAL(gear->getMethodOptionType(), "FixedCount");
-    BOOST_CHECK_EQUAL(gear->getMethodOptionMax(), 13);
-    BOOST_CHECK_EQUAL(gear->getUniqueID(), 14);
-    BOOST_CHECK_EQUAL(gear->getRampInInterval(), 15);
-    BOOST_CHECK_EQUAL(gear->getRampInPercent(), 16);
-    BOOST_CHECK_EQUAL(gear->getRampOutInterval(), 17);
-    BOOST_CHECK_EQUAL(gear->getRampOutPercent(), 18);
-    BOOST_CHECK_EQUAL(gear->getFrontRampOption(), "(none)");
-    BOOST_CHECK_EQUAL(gear->getBackRampOption(), "(none)");
-    BOOST_CHECK_CLOSE(gear->getKWReduction(), 3.4, 1e-1);
+    BOOST_CHECK_EQUAL(gear.getProgramPAOId(), 101);
+    BOOST_CHECK_EQUAL(gear.getGearName(), "Test Honeywell Cycle Gear");
+    BOOST_CHECK_EQUAL(gear.getGearNumber(), 2);
+    BOOST_CHECK_EQUAL(gear.getControlMethod(), "HoneywellCycle");
+    BOOST_CHECK_EQUAL(gear.getMethodRate(), 5);
+    BOOST_CHECK_EQUAL(gear.getMethodPeriod(), 6);
+    BOOST_CHECK_EQUAL(gear.getMethodRateCount(), 7);
+    BOOST_CHECK_EQUAL(gear.getCycleRefreshRate(), 8);
+    BOOST_CHECK_EQUAL(gear.getMethodStopType(), "Restore");
+    BOOST_CHECK_EQUAL(gear.getChangeCondition(), "None");
+    BOOST_CHECK_EQUAL(gear.getChangeDuration(), 9);
+    BOOST_CHECK_EQUAL(gear.getChangePriority(), 10);
+    BOOST_CHECK_EQUAL(gear.getChangeTriggerNumber(), 11);
+    BOOST_CHECK_CLOSE(gear.getChangeTriggerOffset(), 1.2, 1e-1);
+    BOOST_CHECK_EQUAL(gear.getPercentReduction(), 10);
+    BOOST_CHECK_EQUAL(gear.getGroupSelectionMethod(), "LastControlled");
+    BOOST_CHECK_EQUAL(gear.getMethodOptionType(), "FixedCount");
+    BOOST_CHECK_EQUAL(gear.getMethodOptionMax(), 13);
+    BOOST_CHECK_EQUAL(gear.getUniqueID(), 14);
+    BOOST_CHECK_EQUAL(gear.getRampInInterval(), 15);
+    BOOST_CHECK_EQUAL(gear.getRampInPercent(), 16);
+    BOOST_CHECK_EQUAL(gear.getRampOutInterval(), 17);
+    BOOST_CHECK_EQUAL(gear.getRampOutPercent(), 18);
+    BOOST_CHECK_EQUAL(gear.getFrontRampOption(), "(none)");
+    BOOST_CHECK_EQUAL(gear.getBackRampOption(), "(none)");
+    BOOST_CHECK_CLOSE(gear.getKWReduction(), 3.4, 1e-1);
 
     // No public access to 'FrontRampTime' or 'BackRampTime' from the reader in the gear object
 
     // clone to test operators
-    boost::scoped_ptr<CtiLMProgramDirectGear>   clone(gear->replicate());
+    auto clone = gear.replicate();
 
-    BOOST_CHECK(*gear == *clone);
+    BOOST_CHECK(gear == *clone);
 
     clone->setProgramPAOId(clone->getProgramPAOId() + 1);
 
-    BOOST_CHECK(*gear != *clone);
+    BOOST_CHECK(gear != *clone);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
