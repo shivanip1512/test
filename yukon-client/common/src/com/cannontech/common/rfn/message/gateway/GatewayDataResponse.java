fae241a710a908a5d1974927ecc4b12f0a8251a8
Johnston, Samuel T <SamuelTJohnston@Eaton.com>
2016-10-27 19:23:16 +0000

    YUK-15826 Make sure RF Data Streaming messaging still works with NM release in October  - Fixed serialVersionUID so this object should deserialize from old versions successfully.

--- yukon-client/common/src/com/cannontech/common/rfn/message/gateway/GatewayDataResponse.java
+++ yukon-client/common/src/com/cannontech/common/rfn/message/gateway/GatewayDataResponse.java
@@ -13,7 +13,7 @@ import com.cannontech.common.rfn.message.RfnIdentifyingMessage;
  */
 public class GatewayDataResponse implements RfnIdentifyingMessage, Serializable {
     
-    private static final long serialVersionUID = 2L;
+    private static final long serialVersionUID = 1L;
     
     private RfnIdentifier rfnIdentifier;
     
