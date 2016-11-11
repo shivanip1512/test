c70d32db3156352867facba61590c6f9aba82f1e
Vagyani, Asmita <>
2016-11-11 02:56:22 +0000

    YUK-15910 - Proxy changes added for forgot Password recaptcha functionality with CSP policies 1) Reverted changes in .bat file 2) Added proxy settings in Captcha service to lookup from Global settings.

--- yukon-client/yukon-web/src/com/cannontech/web/common/captcha/service/impl/CaptchaServiceImpl.java
+++ yukon-client/yukon-web/src/com/cannontech/web/common/captcha/service/impl/CaptchaServiceImpl.java
@@ -5,8 +5,10 @@ import javax.annotation.PostConstruct;
 import net.tanesha.recaptcha.ReCaptchaImpl;
 import net.tanesha.recaptcha.ReCaptchaResponse;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.cannontech.clientutils.YukonLogManager;
 import com.cannontech.common.config.ConfigurationSource;
 import com.cannontech.common.config.MasterConfigString;
 import com.cannontech.system.GlobalSettingType;
@@ -18,6 +20,7 @@ import com.cannontech.web.common.captcha.service.CaptchaService;
 
 public class CaptchaServiceImpl implements CaptchaService{
     
+    private Logger log = YukonLogManager.getLogger(CaptchaServiceImpl.class);
     private String RECAPTCHA_PUBLIC_KEY;
     private String RECAPTCHA_PRIVATE_KEY;
     
@@ -26,8 +29,25 @@ public class CaptchaServiceImpl implements CaptchaService{
     
     @PostConstruct 
     public void init() {
-        RECAPTCHA_PUBLIC_KEY = configurationSource.getString(MasterConfigString.RECAPTCHA_PUBLIC_KEY, "6LcLps0SAAAAAH-sva1H7pRvCWiDddeA0yO_CbHB");
-        RECAPTCHA_PRIVATE_KEY = configurationSource.getString(MasterConfigString.RECAPTCHA_PRIVATE_KEY, "6LcLps0SAAAAAM40wM_-kRx-FCYeEA72XVpQwGl8");
+        RECAPTCHA_PUBLIC_KEY =
+            configurationSource.getString(MasterConfigString.RECAPTCHA_PUBLIC_KEY,
+                "6LcLps0SAAAAAH-sva1H7pRvCWiDddeA0yO_CbHB");
+        RECAPTCHA_PRIVATE_KEY =
+            configurationSource.getString(MasterConfigString.RECAPTCHA_PRIVATE_KEY,
+                "6LcLps0SAAAAAM40wM_-kRx-FCYeEA72XVpQwGl8");
+ 
+        String httpProxy = globalSettingDao.getString(GlobalSettingType.HTTP_PROXY);
+        boolean isCaptchasEnabled = globalSettingDao.getBoolean(GlobalSettingType.ENABLE_CAPTCHAS);
+        if (isCaptchasEnabled && !httpProxy.equals("none")) {
+            String[] hostAndPort = httpProxy.split(":");
+            if (hostAndPort.length != 2) {
+                log.error("GlobalSettingType = HTTP_PROXY has an invalid value: " + httpProxy
+                    + ". Unable to setup proxy settings for Captcha service");
+            } else {
+                System.setProperty("http.proxyHost", hostAndPort[0]);
+                System.setProperty("http.proxyPort", hostAndPort[1]);
+            }
+        }
     }
     
     @Override
@@ -41,7 +61,6 @@ public class CaptchaServiceImpl implements CaptchaService{
         
         ReCaptchaImpl reCaptcha = new ReCaptchaImpl();
         reCaptcha.setPrivateKey(RECAPTCHA_PRIVATE_KEY);
-
         ReCaptchaResponse reCaptchaResponse = 
                 reCaptcha.checkAnswer(captcha.getRemoteAddr(), captcha.getChallenge(), captcha.getResponse());
         
