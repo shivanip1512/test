1afdc675f8339b2c5d89d1f54fdca7c954373538
Thonglyvong, Ricky <RickyThonglyvong@Eaton.com>
2016-12-01 22:57:06 +0000

    YUK-15883 Add role property control access for working with water meters (water leak report, specifically)... -Formatting

--- yukon-client/yukon-web/src/com/cannontech/web/support/SiteMapHelper.java
+++ yukon-client/yukon-web/src/com/cannontech/web/support/SiteMapHelper.java
@@ -93,12 +93,12 @@ public class SiteMapHelper {
             if ( permission instanceof YukonRole) {
                 permissions += ((YukonRole) permission).name() + " ";
             } else if (permission instanceof YukonRoleProperty) {
-                if(permission != YukonRoleProperty.WATER_LEAK_REPORT){
+                if (permission != YukonRoleProperty.WATER_LEAK_REPORT) {
                     permissions += ((YukonRoleProperty) permission).name() + " ";
                 }
                 else {
                     Set<PaoType> waterMeters = Sets.intersection(serverDatabaseCache.getAllPaoTypes(), PaoType.getWaterMeterTypes());
-                    if(!waterMeters.isEmpty()){
+                    if (!waterMeters.isEmpty()) {
                         permissions += ((YukonRoleProperty) permission).name() + " ";
                     }
                 }
