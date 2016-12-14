70aab423f629f548f57d7df4a21921eb23713063
Modi, Preeti <>
2016-12-14 10:24:26 +0000

    YUK-15811 : Honeywell wifi thermostats - enrollment Fixed review comments

--- yukon-client/common/src/com/cannontech/stars/dr/hardware/service/impl/HoneywellCommandStrategy.java
+++ yukon-client/common/src/com/cannontech/stars/dr/hardware/service/impl/HoneywellCommandStrategy.java
@@ -1,10 +1,7 @@
 package com.cannontech.stars.dr.hardware.service.impl;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
