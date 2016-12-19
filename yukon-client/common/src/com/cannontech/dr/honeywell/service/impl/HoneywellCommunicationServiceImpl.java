cf35a0318f5e742583402b53c7894f01ea591799
Vagyani, Asmita <>
2016-12-19 09:41:20 +0000

    YUK-15533 - Honeywell Opt out support 1) Added method to get honeywell DR events 2) Added implementation in commandStrategy to cancel optout and to start optout

--- yukon-client/common/src/com/cannontech/dr/honeywell/service/impl/HoneywellCommunicationServiceImpl.java
+++ yukon-client/common/src/com/cannontech/dr/honeywell/service/impl/HoneywellCommunicationServiceImpl.java
@@ -23,8 +23,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.TimeZone;
 
-import javax.xml.bind.DatatypeConverter;
-
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
