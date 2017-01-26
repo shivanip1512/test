c9ba4a7b12e3d72eb9ed6a1e56b4e96ed9b0fe69
Thonglyvong, Ricky <RickyThonglyvong@Eaton.com>
2017-01-26 17:00:49 +0000

    YUK-16170 Refactor readNowPushedForReadingsWidget event log to include more useful information (devicename and/or meter number vs paoId) -refactored method readNowPushedForReadingsWidget

--- yukon-client/yukon-web/src/com/cannontech/web/dev/database/service/impl/DevEventLogCreationService.java
+++ yukon-client/yukon-web/src/com/cannontech/web/dev/database/service/impl/DevEventLogCreationService.java
@@ -504,7 +504,7 @@ public class DevEventLogCreationService {
                 String scheduleName = devEventLog.getIndicatorString() + "ScheduleName";
                 int paoId = 123;
                 
-                String deviceName = devEventLog.getIndicatorString() + "aDeviceName";
+                String meterNumber = devEventLog.getIndicatorString() + "meterNumber";
                 String deviceRequestType = DeviceRequestType.GROUP_ATTRIBUTE_READ.getShortName();
                 String deviceGroup = "/Meters/Collection/Cycle 1";
                 Instant start = Instant.now().minus(Duration.standardDays(1));
@@ -517,7 +517,7 @@ public class DevEventLogCreationService {
                 int contextId = 56789;
                 int commands = 2;
                 
-                meteringEventLogService.readNowPushedForReadingsWidget(user, deviceName);
+                meteringEventLogService.readNowPushedForReadingsWidget(user, meterNumber);
                 meteringEventLogService.scheduleDeleted(user, scheduleName);
                 meteringEventLogService.jobStarted(deviceRequestType, scheduleName, deviceGroup, retry, user, jobId);
                 meteringEventLogService.jobCompleted(deviceRequestType, scheduleName, jobId, contextId);
