0d87288489598966823407c9039ba9c1714f1f68
Beckius, Aaron T <AaronTBeckius@Eaton.com>
2017-02-06 20:43:03 +0000

    YUK-16246 DynamicPAOInfo RFN Config Update  - Moving update scripts in Trunk from 6.7_0 to 6.6_3

--- yukon-database/DBUpdates/oracle/6.6_3.sql
+++ yukon-database/DBUpdates/oracle/6.6_3.sql
@@ -7,6 +7,22 @@ ALTER TABLE HoneywellWifiThermostat
    ADD CONSTRAINT AK_HONEYWELLWIFITHERMOSTAT_MAC UNIQUE (MacAddress);
 /* End YUK-16225 */
 
+/* Start YUK-16246 */
+UPDATE DynamicPAOInfo
+SET Value = REPLACE(Value, 'Schedule ', 'SCHEDULE_')
+WHERE Value LIKE 'Schedule %'
+AND InfoKey IN (
+    'rfn monday schedule',
+    'rfn tuesday schedule',
+    'rfn wednesday schedule',
+    'rfn thursday schedule',
+    'rfn friday schedule',
+    'rfn saturday schedule',
+    'rfn sunday schedule',
+    'rfn holiday schedule'
+);
+/* End YUK-16246 */
+
 /**************************************************************/
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
