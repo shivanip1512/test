55be7c68ce68dc036b4ddc15f9c7dbe61e0f4bce
Beckius, Aaron T <AaronTBeckius@Eaton.com>
2017-02-10 16:36:53 +0000

    (NO YUK ISSUE) 6.5.8 build  - Updating 6.5.8 update scripts for 2/10/2017 build

--- yukon-database/DBUpdates/sqlserver/6.5_8.sql
+++ yukon-database/DBUpdates/sqlserver/6.5_8.sql
@@ -8,4 +8,4 @@
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
 /**************************************************************/
-/*INSERT INTO CTIDatabase VALUES ('6.5', '08-DEC-2016', 'Latest Update', 8, GETDATE());*/
+INSERT INTO CTIDatabase VALUES ('6.5', '10-FEB-2016', 'Latest Update', 8, GETDATE());
