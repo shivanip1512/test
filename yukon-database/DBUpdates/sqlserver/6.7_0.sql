a01f7cb3d4ef1bf26ceb22d1ec6c75e4de89a8d7
Beckius, Aaron T <AaronTBeckius@Eaton.com>
2017-03-07 19:36:09 +0000

    YUK-16205 DB Change - Clean up paoDefinition.xml  - Adding DB updates for the paoDefinition clean-up effort (YUK-16025)

--- yukon-database/DBUpdates/sqlserver/6.7_0.sql
+++ yukon-database/DBUpdates/sqlserver/6.7_0.sql
@@ -65,6 +65,371 @@ ALTER TABLE DEVICEMETERGROUP
 GO
 /* End YUK-16164 */
 
+/* Start YUK-16205 */
+UPDATE Point 
+SET PointName = 'Reverse Power Alarm'
+WHERE PointType = 'Status' 
+AND PointOffset = 47
+AND PointName = 'Reverse Power'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('MCT-410cL', 'MCT-410gL', 'MCT-410iL')
+);
+
+UPDATE Point 
+SET PointName = 'Outages', PointOffset = 100
+WHERE PointType = 'Analog' 
+AND PointOffset = 24
+AND PointName = 'Outage Log'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-440-2131TD', 'RFN-440-2132TD', 'RFN-440-2132TD')
+);
+
+UPDATE Point 
+SET PointName = 'Delivered kVAh'
+WHERE PointType = 'Analog' 
+AND PointOffset = 150
+AND PointName = 'kVAh Delivered'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430KV','RFN-420cL','RFN-430A3K','RFN-420cD','RFN-430A3R')
+);
+
+UPDATE Point 
+SET PointName = 'Received kVAh'
+WHERE PointType = 'Analog' 
+AND PointOffset = 151
+AND PointName = 'kVAh Received'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430KV','RFN-420cL','RFN-430A3K','RFN-420cD','RFN-430A3R')
+);
+
+UPDATE Point 
+SET PointName = 'Delivered kVArh (Rate A kVArh)'
+WHERE PointType = 'Analog' 
+AND PointOffset = 161
+AND PointName = 'Delivered Rate A kVArh'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430KV','RFN-430SL4','RFN-430SL3','RFN-430SL2','RFN-430A3R')
+);
+
+UPDATE Point 
+SET PointName = 'Delivered kVArh (Rate B kVArh)'
+WHERE PointType = 'Analog' 
+AND PointOffset = 162
+AND PointName = 'Delivered Rate B kVArh'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430KV','RFN-430SL4','RFN-430SL3','RFN-430SL2','RFN-430A3R')
+);
+
+UPDATE Point 
+SET PointName = 'Delivered kVArh (Rate C kVArh)'
+WHERE PointType = 'Analog' 
+AND PointOffset = 163
+AND PointName = 'Delivered Rate C kVArh'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430KV','RFN-430SL4','RFN-430SL3','RFN-430SL2','RFN-430A3R')
+);
+
+UPDATE Point 
+SET PointName = 'Delivered kVArh (Rate D kVArh)'
+WHERE PointType = 'Analog' 
+AND PointOffset = 164
+AND PointName = 'Delivered Rate D kVArh'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430KV','RFN-430SL4','RFN-430SL3','RFN-430SL2','RFN-430A3R')
+);
+
+UPDATE Point 
+SET PointName = 'Received kVArh (Rate A kVArh)'
+WHERE PointType = 'Analog' 
+AND PointOffset = 202
+AND PointName = 'Received Rate A kVArh'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430KV','RFN-430SL4','RFN-430SL3','RFN-430SL2','RFN-430A3R')
+);
+
+UPDATE Point 
+SET PointName = 'Received kVArh (Rate B kVArh)'
+WHERE PointType = 'Analog' 
+AND PointOffset = 203
+AND PointName = 'Received Rate B kVArh'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430KV','RFN-430SL4','RFN-430SL3','RFN-430SL2','RFN-430A3R')
+);
+
+UPDATE Point 
+SET PointName = 'Received kVArh (Rate C kVArh)'
+WHERE PointType = 'Analog' 
+AND PointOffset = 204
+AND PointName = 'Received Rate C kVArh'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430KV','RFN-430SL4','RFN-430SL3','RFN-430SL2','RFN-430A3R')
+);
+
+UPDATE Point 
+SET PointName = 'Received kVArh (Rate D kVArh)'
+WHERE PointType = 'Analog' 
+AND PointOffset = 205
+AND PointName = 'Received Rate D kVArh'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430KV','RFN-430SL4','RFN-430SL3','RFN-430SL2','RFN-430A3R')
+);
+
+UPDATE Point 
+SET PointName = 'Sum kVArh'
+WHERE PointType = 'Analog' 
+AND PointOffset = 215
+AND PointName = 'Total kVArh'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-420fRX','RFN-420fRD')
+);
+
+UPDATE Point 
+SET PointOffset = 349
+WHERE PointType = 'Analog' 
+AND PointOffset = 226
+AND PointName = 'Current Angle'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-520fAX','RFN-520fAXD', 'RFN-520fRX','RFN-520fRXD')
+);
+
+UPDATE Point 
+SET PointOffset = 350, PointName = 'Net Delivered kVArh (Rate A kVArh)'
+WHERE PointType = 'Analog' 
+AND PointOffset = 230
+AND PointName = 'Rate A Net Delivered kVArh'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430SL4')
+);
+
+UPDATE Point 
+SET PointOffset = 351, PointName = 'Net Delivered kVArh (Rate B kVArh)'
+WHERE PointType = 'Analog' 
+AND PointOffset = 231
+AND PointName = 'Rate B Net Delivered kVArh'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430SL4')
+);
+
+UPDATE Point 
+SET PointOffset = 352, PointName = 'Net Delivered kVArh (Rate C kVArh)'
+WHERE PointType = 'Analog' 
+AND PointOffset = 232
+AND PointName = 'Rate C Net Delivered kVArh'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430SL4')
+);
+
+UPDATE Point 
+SET PointOffset = 353, PointName = 'Net Delivered kVArh (Rate D kVArh)'
+WHERE PointType = 'Analog' 
+AND PointOffset = 233
+AND PointName = 'Rate D Net Delivered kVArh'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430SL4')
+);
+
+UPDATE Point 
+SET PointName = 'Delivered kVAh (Rate A kVAh)'
+WHERE PointType = 'Analog' 
+AND PointOffset = 241
+AND PointName = 'kVAh Rate A'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430SL4', 'RFN-430SL3', 'RFN-430SL2', 'RFN-430SL1')
+);
+
+UPDATE Point 
+SET PointName = 'Delivered kVAh (Rate B kVAh)'
+WHERE PointType = 'Analog' 
+AND PointOffset = 242
+AND PointName = 'kVAh Rate B'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430SL4', 'RFN-430SL3', 'RFN-430SL2', 'RFN-430SL1')
+);
+
+UPDATE Point 
+SET PointName = 'Delivered kVAh (Rate C kVAh)'
+WHERE PointType = 'Analog' 
+AND PointOffset = 243
+AND PointName = 'kVAh Rate C'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430SL4', 'RFN-430SL3', 'RFN-430SL2', 'RFN-430SL1')
+);
+
+UPDATE Point 
+SET PointName = 'Delivered kVAh (Rate D kVAh)'
+WHERE PointType = 'Analog' 
+AND PointOffset = 244
+AND PointName = 'kVAh Rate D'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430SL4', 'RFN-430SL3', 'RFN-430SL2', 'RFN-430SL1')
+);
+
+UPDATE Point 
+SET PointName = 'Delivered Peak kVA (Rate A kVA)'
+WHERE PointType = 'Analog' 
+AND PointOffset = 251
+AND PointName = 'Peak kVA Rate A'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430SL4', 'RFN-430SL3', 'RFN-430SL2', 'RFN-430SL1')
+);
+
+UPDATE Point 
+SET PointName = 'Delivered Peak kVA (Rate B kVA)'
+WHERE PointType = 'Analog' 
+AND PointOffset = 252
+AND PointName = 'Peak kVA Rate B'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430SL4', 'RFN-430SL3', 'RFN-430SL2', 'RFN-430SL1')
+);
+
+UPDATE Point 
+SET PointName = 'Delivered Peak kVA (Rate C kVA)'
+WHERE PointType = 'Analog' 
+AND PointOffset = 253
+AND PointName = 'Peak kVA Rate C'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430SL4', 'RFN-430SL3', 'RFN-430SL2', 'RFN-430SL1')
+);
+
+UPDATE Point 
+SET PointName = 'Delivered Peak kVA (Rate D kVA)'
+WHERE PointType = 'Analog' 
+AND PointOffset = 254
+AND PointName = 'Peak kVA Rate D'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430SL4', 'RFN-430SL3', 'RFN-430SL2', 'RFN-430SL1')
+);
+
+UPDATE Point 
+SET PointOffset = 250
+WHERE PointType = 'Analog' 
+AND PointOffset = 149
+AND PointName = 'Peak kVA'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430KV', 'RFN-430A3K', 'RFN-430A3R')
+);
+
+UPDATE Point 
+SET PointOffset = 215
+WHERE PointType = 'Analog' 
+AND PointOffset = 165
+AND PointName = 'Sum kVArh'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430KV', 'RFN-430A3R')
+);
+
+UPDATE Point 
+SET PointOffset = 354
+WHERE PointType = 'Analog' 
+AND PointOffset = 327
+AND PointName IN ('Voltage (Single Phase)', 'Voltage', 'Voltage Phase A')
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-520fAX', 'RFN-520fAXD', 'RFN-520fRX', 'RFN-520fRXD')
+);
+
+UPDATE Point 
+SET PointOffset = 355
+WHERE PointType = 'Analog' 
+AND PointOffset = 330
+AND PointName = 'Min Voltage'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-520fAX', 'RFN-520fAXD', 'RFN-520fRX', 'RFN-520fRXD')
+);
+
+UPDATE Point 
+SET PointOffset = 356
+WHERE PointType = 'Analog' 
+AND PointOffset = 333
+AND PointName = 'Max Voltage'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-520fAX', 'RFN-520fAXD', 'RFN-520fRX', 'RFN-520fRXD')
+);
+
+UPDATE Point 
+SET PointOffset = 357
+WHERE PointType = 'Analog' 
+AND PointOffset = 336
+AND PointName = 'Current'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-520fAX', 'RFN-520fAXD', 'RFN-520fRX', 'RFN-520fRXD')
+);
+
+UPDATE Point 
+SET PointOffset = 244
+WHERE PointType = 'Analog' 
+AND PointOffset = 308
+AND PointName = ' Delivered kVAh (Rate D kVAh)'
+AND PaobjectId IN (
+    SELECT DISTINCT PaobjectId 
+    FROM YukonPaobject
+    WHERE Type IN ('RFN-430S4x')
+);
+/* End YUK-16205 */
+
 /**************************************************************/
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
