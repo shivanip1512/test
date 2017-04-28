110a67327db182dc93d4b18e968e070664ff3e9d
Vant, Carrie <carrieVant@Eaton.com>
2017-04-28 15:30:01 +0000

    YUK-16516 - Design new widget to show current state of data collection for all installed devices (MUC-1)   - create new package for Data Collection Service

---
+++ yukon-client/yukon-web/src/com/cannontech/web/common/dataCollection/service/DataCollectionService.java
@@ -0,0 +1,7 @@
+
+package com.cannontech.web.common.dataCollection.service;
+
+public interface DataCollectionService {
+
+}
+
---
+++ yukon-client/yukon-web/src/com/cannontech/web/common/dataCollection/service/impl/DataCollectionServiceImpl.java
@@ -0,0 +1,9 @@
+package com.cannontech.web.common.dataCollection.service.impl;
+
+import org.springframework.stereotype.Service;
+import com.cannontech.web.common.dataCollection.service.DataCollectionService;
+
+@Service
+public class DataCollectionServiceImpl implements DataCollectionService {
+
+}
