89ebaf348ea111466ba31e34ab37a2b17daf96c5
Beckius, Aaron T <AaronTBeckius@Eaton.com>
2017-05-03 18:10:57 +0000

    NO YUK ISSUE Trunk 6.6_4.sql CTIDatabase insert fix  - Un-commenting the CTIDatabase insert line in the trunk version of 6.6_4.sql

--- yukon-database/DBUpdates/sqlserver/6.6_4.sql
+++ yukon-database/DBUpdates/sqlserver/6.6_4.sql
@@ -69,4 +69,4 @@ DELETE FROM Point WHERE PointId IN (
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
 /**************************************************************/
-/*INSERT INTO CTIDatabase VALUES ('6.6', '15-FEB-2017', 'Latest Update', 4, GETDATE());*/
+INSERT INTO CTIDatabase VALUES ('6.6', '15-FEB-2017', 'Latest Update', 4, GETDATE());
