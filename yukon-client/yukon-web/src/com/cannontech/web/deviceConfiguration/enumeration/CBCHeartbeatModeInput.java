3810f7f797c106aad343583fe7e1544ac014bbb4
Beckius, Aaron T <AaronTBeckius@Eaton.com>
2017-05-10 17:42:09 +0000

    YUK-16592 Create device config for CBC heartbeat  - I missed adding the new file: CBCHeartbeatModeInput.java

---
+++ yukon-client/yukon-web/src/com/cannontech/web/deviceConfiguration/enumeration/CBCHeartbeatModeInput.java
@@ -0,0 +1,54 @@
+package com.cannontech.web.deviceConfiguration.enumeration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.cannontech.common.i18n.DisplayableEnum;
+import com.cannontech.common.i18n.MessageSourceAccessor;
+import com.cannontech.i18n.YukonUserContextMessageSourceResolver;
+import com.cannontech.user.YukonUserContext;
+import com.cannontech.web.input.type.InputOption;
+
+@Component
+public class CBCHeartbeatModeInput implements DeviceConfigurationInputEnumeration {
+
+    private static final String baseKey = "yukon.web.modules.tools.configs.enum.cbcHeartbeatMode.";
+    @Autowired private YukonUserContextMessageSourceResolver messageResolver;
+
+    public enum CBCHeartbeatMode implements DisplayableEnum {
+        DISABLED,
+        ANALOG,
+        PULSED
+        ;
+
+        @Override
+        public String getFormatKey() {
+            return baseKey + name();
+        }
+    }
+
+    @Override
+    public List<InputOption> getDisplayableValues(YukonUserContext userContext) {
+        MessageSourceAccessor messageAccessor = messageResolver.getMessageSourceAccessor(userContext);
+
+        List<InputOption> modes = new ArrayList<>();
+
+        for (CBCHeartbeatMode mode : CBCHeartbeatMode.values()) {
+            modes.add( new InputOption( mode.name(), messageAccessor.getMessage(mode)));
+        }
+        return modes;
+    }
+
+    @Override
+    public String getEnumOptionName() {
+        return "CBCHeartbeatMode";
+    }
+
+    @Override
+    public SelectionType getSelectionType() {
+        return SelectionType.SWITCH;
+    }
+}
