91fc14dfd7ff89a99a61ce0fd0634271b9479c0c
Mehta, Neha <>
2017-05-24 09:18:13 +0000

    YUK-16461: Create Gateway Popup Blank if Missing Operator Inventory Permission

--- yukon-client/yukon-web/src/com/cannontech/web/stars/gateway/GatewayFirmwareUpdateController.java
+++ yukon-client/yukon-web/src/com/cannontech/web/stars/gateway/GatewayFirmwareUpdateController.java
@@ -19,11 +19,8 @@ import com.cannontech.common.rfn.model.RfnGatewayFirmwareUpdateResult;
 import com.cannontech.common.rfn.model.RfnGatewayFirmwareUpdateSummary;
 import com.cannontech.common.rfn.service.RfnGatewayFirmwareUpgradeService;
 import com.cannontech.common.rfn.service.RfnGatewayService;
-import com.cannontech.core.roleproperties.YukonRole;
-import com.cannontech.web.security.annotation.CheckRole;
 
 @Controller
-@CheckRole(YukonRole.INVENTORY)
 public class GatewayFirmwareUpdateController {
     
     @Autowired private RfnGatewayFirmwareUpgradeService firmwareUpgradeService;
