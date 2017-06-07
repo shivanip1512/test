ce54d3cfb8470ae7b2340e56deb8db050ec38e17
Feldman, Marina <MarinaFeldman@Eaton.com>
2017-06-07 16:43:23 +0000

    YUK-16674 Usage Threshold Report service (MUC-2, 3)

--- yukon-client/yukon-web/src/com/cannontech/web/amr/usageThresholdReport/model/ThresholdReportFilter.java
+++ yukon-client/yukon-web/src/com/cannontech/web/amr/usageThresholdReport/model/ThresholdReportFilter.java
@@ -1,29 +1,33 @@
 package com.cannontech.web.amr.usageThresholdReport.model;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
 import com.cannontech.common.device.groups.model.DeviceGroup;
+import com.google.common.base.Joiner;
 
 public class ThresholdReportFilter {
 
-    private DeviceGroup group;
+    private List<DeviceGroup> groups;
     private double threshold;
     private ThresholdDescriptor thresholdDescriptor;
-    private DataAvailability availability;
+    private List<DataAvailability> availability;
     private boolean includeDisabled;
 
-    public ThresholdReportFilter(DeviceGroup group, double threshold, ThresholdDescriptor thresholdDescriptor,
-            DataAvailability availability, boolean includeDisabled) {
-        this.group = group;
+    public ThresholdReportFilter(List<DeviceGroup> groups, double threshold, ThresholdDescriptor thresholdDescriptor,
+            List<DataAvailability> availability, boolean includeDisabled) {
+        this.groups = groups;
         this.threshold = threshold;
         this.thresholdDescriptor = thresholdDescriptor;
         this.availability = availability;
         this.includeDisabled = includeDisabled;
     }
 
-    public DeviceGroup getGroup() {
-        return group;
+    public List<DeviceGroup> getGroups() {
+        return groups;
     }
 
     public double getThreshold() {
@@ -34,7 +38,7 @@ public class ThresholdReportFilter {
         return thresholdDescriptor;
     }
 
-    public DataAvailability getAvailability() {
+    public List<DataAvailability> getAvailability() {
         return availability;
     }
 
@@ -45,9 +49,9 @@ public class ThresholdReportFilter {
     @Override
     public String toString() {
         ToStringBuilder tsb = new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE);
-        tsb.append("group", group);
+        tsb.append("groups", Joiner.on(",").join(groups.stream().map(g -> g.getFullName()).collect(Collectors.toList())));
         tsb.append("threshold", thresholdDescriptor.getValue() + " " + threshold);
-        tsb.append("availability", availability);
+        tsb.append("availability", Joiner.on(",").join(availability));
         tsb.append("includeDisabled", includeDisabled);
         return tsb.toString();
     }
