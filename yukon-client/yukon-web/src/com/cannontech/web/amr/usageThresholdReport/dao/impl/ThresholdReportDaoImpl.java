59e01b0e240b27ca257f16db5ad4d15e21bfdf90
Feldman, Marina <MarinaFeldman@Eaton.com>
2017-06-13 18:36:00 +0000

    YUK-16674 Usage Threshold Report service (MUC-2, 3)

--- yukon-client/yukon-web/src/com/cannontech/web/amr/usageThresholdReport/dao/impl/ThresholdReportDaoImpl.java
+++ yukon-client/yukon-web/src/com/cannontech/web/amr/usageThresholdReport/dao/impl/ThresholdReportDaoImpl.java
@@ -1,18 +1,15 @@
 package com.cannontech.web.amr.usageThresholdReport.dao.impl;
 
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.sql.Types;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.log4j.Logger;
 import org.joda.time.Instant;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 
 import com.cannontech.clientutils.YukonLogManager;
 import com.cannontech.common.device.groups.service.DeviceGroupService;
@@ -21,7 +18,6 @@ import com.cannontech.common.model.PagingParameters;
 import com.cannontech.common.pao.attribute.model.BuiltInAttribute;
 import com.cannontech.common.point.PointQuality;
 import com.cannontech.common.search.result.SearchResults;
-import com.cannontech.common.util.ChunkingSqlTemplate;
 import com.cannontech.common.util.InstantRangeLogHelper;
 import com.cannontech.common.util.Range;
 import com.cannontech.common.util.SqlStatementBuilder;
@@ -290,55 +286,37 @@ public class ThresholdReportDaoImpl implements ThresholdReportDao {
     @Override
     public void createReportDetail(int reportId, List<ThresholdReportDetail> details) {
         log.debug("Inserting values into UsageThresholdReportRow:" + details.size());
-             
-        List<List<ThresholdReportDetail>> part = Lists.partition(details, ChunkingSqlTemplate.DEFAULT_SIZE);
-        part.forEach(batch -> {
-            //insert 1000 rows
-            SqlStatementBuilder insertSql = new SqlStatementBuilder();
-            insertSql.append("INSERT INTO UsageThresholdReportRow");
-            insertSql.append("(UsageThresholdReportId, PaoId, PointId, FirstTimestamp, FirstValue, LastTimestamp, LastValue, Delta)");
-            insertSql.append("values");
-            insertSql.append("(?, ?, ?, ?, ?, ?, ?, ?)");
+        
+        List<List<Object>> values = details.stream()
+                .map(detail -> {
+                    Date firstDate = null;
+                    Double firstValue = null;
+                    Date lastDate = null;
+                    Double lastValue = null;
+                    if(detail.getEarliestReading() != null){
+                        firstDate = detail.getEarliestReading().getPointDataTimeStamp();
+                        firstValue = detail.getEarliestReading().getValue();
+                        lastDate = detail.getLatestReading().getPointDataTimeStamp();
+                        lastValue = detail.getLatestReading().getValue();
+                    }
 
-            jdbcTemplate.batchUpdate(insertSql.toString(), new BatchPreparedStatementSetter() {
-                @Override
-                public void setValues(PreparedStatement ps, int i) throws SQLException {
-                    ThresholdReportDetail detail = batch.get(i);
-                    ps.setInt(1, reportId);
-                    ps.setInt(2, detail.getPaoIdentifier().getPaoId());
-                    if (detail.getPointId() == null) {
-                        ps.setNull(3, Types.NULL);
-                    } else {
-                        ps.setInt(3, detail.getPointId());
-                    }
-                    if (detail.getEarliestReading() == null) {
-                        ps.setNull(4, Types.NULL);
-                        ps.setNull(5, Types.NULL);
-                    } else {
-                        ps.setTimestamp(4, new Timestamp(detail.getEarliestReading().getPointDataTimeStamp().getTime()));
-                        ps.setDouble(5, detail.getEarliestReading().getValue());
-                    }
-                    if (detail.getLatestReading() == null) {
-                        ps.setNull(6, Types.NULL);
-                        ps.setNull(7, Types.NULL);
-                    } else {
-                        ps.setTimestamp(6, new Timestamp(detail.getLatestReading().getPointDataTimeStamp().getTime()));
-                        ps.setDouble(7, detail.getLatestReading().getValue());
-                    }
-                    if (detail.getDelta() == null) {
-                        ps.setNull(8, Types.NULL);
-                    } else {
-                        ps.setDouble(8, detail.getDelta());
-                    }
-                }
-
-                @Override
-                public int getBatchSize() {
-                    return batch.size();
-                }
-            });
-            log.debug("Size=" + batch.size());
-        });
+                    List<Object> row = Lists.newArrayList(reportId,
+                                                          detail.getPaoIdentifier().getPaoId(),
+                                                          detail.getPointId(),
+                                                          firstDate,
+                                                          firstValue,
+                                                          lastDate,
+                                                          lastValue,
+                                                          detail.getDelta());
+                    return row;
+                }).collect(Collectors.toList());
+        
+        SqlStatementBuilder sql = new SqlStatementBuilder();
+        sql.batchInsertInto("UsageThresholdReportRow")
+           .columns("UsageThresholdReportId", "PaoId", "PointId", "FirstTimestamp", "FirstValue", "LastTimestamp", "LastValue", "Delta")
+           .values(values);
+        
+        jdbcTemplate.yukonBatchUpdate(sql);
         log.debug("Done inserting into UsageThresholdReportRow");      
     }
     
