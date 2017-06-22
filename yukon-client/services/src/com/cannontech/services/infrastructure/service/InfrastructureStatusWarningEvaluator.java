2e5c835a39203fb57a24cf47a4360227886ca48c
Feldman, Marina <MarinaFeldman@Eaton.com>
2017-06-22 18:53:10 +0000

    YUK-16712 Infrastructure Warnings Widget Service/ CCU comm status/ Relay outage status/ Repeater comm status evaluators

---
+++ yukon-client/services/src/com/cannontech/services/infrastructure/service/InfrastructureStatusWarningEvaluator.java
@@ -0,0 +1,43 @@
+package com.cannontech.services.infrastructure.service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cannontech.amr.rfn.dao.RfnDeviceDao;
+import com.cannontech.clientutils.YukonLogManager;
+import com.cannontech.common.pao.PaoIdentifier;
+import com.cannontech.common.pao.attribute.model.BuiltInAttribute;
+import com.cannontech.common.rfn.model.RfnDevice;
+import com.cannontech.core.dao.RawPointHistoryDao;
+import com.cannontech.core.dynamic.PointValueQualityHolder;
+import com.cannontech.infrastructure.model.InfrastructureWarning;
+import com.cannontech.infrastructure.model.InfrastructureWarningType;
+
+public abstract class InfrastructureStatusWarningEvaluator implements InfrastructureWarningEvaluator {
+    
+    private static final Logger log = YukonLogManager.getLogger(InfrastructureStatusWarningEvaluator.class);
+    @Autowired private RawPointHistoryDao rphDao;
+    @Autowired private RfnDeviceDao rfnDeviceDao;
+    
+    @Override
+    public List<InfrastructureWarning> evaluate() {
+        log.debug("Running "+  getAttribute()+ " evaluator on "+ getSupportedTypes());
+        List<RfnDevice> devices = rfnDeviceDao.getDevicesByPaoTypes(getSupportedTypes());
+        Map<PaoIdentifier, PointValueQualityHolder> pao = rphDao.getSingleAttributeData(devices, getAttribute(), false, null, null);
+
+        return pao.keySet().stream().filter(device -> pao.get(device).getValue() == getBadState()).
+                map(device -> {
+                    return new InfrastructureWarning(device, getWarningType());
+                }).collect(Collectors.toList());
+    }
+    
+    public abstract InfrastructureWarningType getWarningType();
+    
+    public abstract BuiltInAttribute getAttribute();
+    
+    public abstract int getBadState();
+}
---
+++ yukon-client/services/src/com/cannontech/services/infrastructure/service/impl/CCUCommStatusEvaluator.java
@@ -0,0 +1,33 @@
+package com.cannontech.services.infrastructure.service.impl;
+
+import java.util.Set;
+
+import com.cannontech.common.pao.PaoType;
+import com.cannontech.common.pao.attribute.model.BuiltInAttribute;
+import com.cannontech.infrastructure.model.InfrastructureWarningType;
+import com.cannontech.services.infrastructure.service.InfrastructureStatusWarningEvaluator;
+
+public class CCUCommStatusEvaluator extends InfrastructureStatusWarningEvaluator {
+    
+    private static int DISCONNECTED = 1;
+
+    @Override
+    public Set<PaoType> getSupportedTypes() {
+        return PaoType.getCcuTypes();
+    }
+
+    @Override
+    public InfrastructureWarningType getWarningType() {
+        return InfrastructureWarningType.CCU_COMM_STATUS;
+    }
+
+    @Override
+    public BuiltInAttribute getAttribute() {
+        return BuiltInAttribute.COMM_STATUS;
+    }
+
+    @Override
+    public int getBadState() {
+        return DISCONNECTED;
+    }
+}
