9c0b52003715ced1a9b072c3ad6075d1521bed04
Mehta, Neha <>
2017-06-27 09:17:56 +0000

    YUK-16785: Category Changed To Unassigned When Removing Device From Config

--- yukon-client/common/src/com/cannontech/common/pao/definition/loader/DefinitionLoaderServiceImpl.java
+++ yukon-client/common/src/com/cannontech/common/pao/definition/loader/DefinitionLoaderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.cannontech.common.pao.definition.loader;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -67,6 +68,7 @@ public class DefinitionLoaderServiceImpl implements DefinitionLoaderService{
     @Autowired private StateGroupDao stateGroupDao;
     @Autowired private PointDao pointDao;
     private FileLoader fileLoader;
+    private Map<String, Category> categories = new HashMap<>();
     
     @PostConstruct
     public void initialize() {
@@ -151,19 +153,35 @@ public class DefinitionLoaderServiceImpl implements DefinitionLoaderService{
     @Override
     public SetMultimap<PaoType, Category> getPaoCategoryMap() {
         SetMultimap<PaoType, Category> paoCategoryMap = HashMultimap.create();
-        
-        for(Pao pao: fileLoader.getPaos()){
-           PaoType paoType = PaoType.valueOf(pao.getPaoType());
-           if(pao.getConfiguration() != null){
-               for(Category category: pao.getConfiguration().getCategory()){
-                   paoCategoryMap.put(paoType, category);
-               }
-           }
+        for (Pao pao : fileLoader.getPaos()) {
+            PaoType paoType = PaoType.valueOf(pao.getPaoType());
+            if (pao.getConfiguration() != null) {
+                for (Category category : getFilteredCategories(pao)) {
+                    paoCategoryMap.put(paoType, category);
+                }
+            }
         }
         
         return paoCategoryMap;
     }
-    
+
+    private List<Category> getFilteredCategories(Pao pao) {
+        List<Category> filteredCategories = new ArrayList<>();
+
+        for (Category category : pao.getConfiguration().getCategory()) {
+            String id = category.getType().name();
+            Category existingCategory = categories.get(id);
+            if (existingCategory != null) {
+                filteredCategories.add(existingCategory);
+            } else {
+                categories.put(id, category);
+                filteredCategories.add(category);
+            }
+        }
+
+        return filteredCategories;
+    }
+
     @Override
     public SetMultimap<PaoType, CommandDefinition> getPaoCommandMap() {
         SetMultimap<PaoType, CommandDefinition> paoCommandMap = HashMultimap.create();
