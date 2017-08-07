44aff36cc5c550fa96327c7477bd5e7b046038a8
Panday, Rahul <RahulPanday@Eaton.com>
2017-08-07 08:56:56 +0000

    YUK-16855 IVVC db population needs to add setpoint regulators as well as what it does now

--- yukon-client/common/src/com/cannontech/simulators/RegulatorVoltageControlMode.java
+++ yukon-client/common/src/com/cannontech/simulators/RegulatorVoltageControlMode.java
@@ -4,8 +4,7 @@ package com.cannontech.simulators;
  * Regulator Voltage Control Modes - SET_POINT or DIRECT_TAP
  */
 public enum RegulatorVoltageControlMode {
-
-    SET_POINT,
     DIRECT_TAP,
+    SET_POINT,
     BOTH;
 }
