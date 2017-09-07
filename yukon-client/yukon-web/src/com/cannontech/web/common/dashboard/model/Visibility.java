ef4657c4b0bd254e3e75a5ce9f1d212f11f15e52
Mehta, Neha <>
2017-09-07 05:09:08 +0000

    YUK-16993 - Sorting for “Visibility” is not working at Dashboard Administration and Manage Dashboard page

--- yukon-client/yukon-web/src/com/cannontech/web/common/dashboard/model/Visibility.java
+++ yukon-client/yukon-web/src/com/cannontech/web/common/dashboard/model/Visibility.java
@@ -1,11 +1,19 @@
 package com.cannontech.web.common.dashboard.model;
 
+import com.cannontech.common.i18n.DisplayableEnum;
+
 /**
  * Describes which users can see a dashboard.
  */
-public enum Visibility {
+public enum Visibility implements  DisplayableEnum {
     SYSTEM, //Built-in, cannot be deleted
-    PRIVATE, //Only visible to owner
-    PUBLIC, //Visible to everyone
+    PUBLIC, // Visible to everyone
+    PRIVATE, // Only visible to owner
     ;
+
+    @Override
+    public String getFormatKey() {
+        return "yukon.web.modules.dashboard.visibility." + name();
+    }
+
 }
