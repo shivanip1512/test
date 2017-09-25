54d2119ec08ffa7b347a9f4db9901c1adbab7df9
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2017-09-25 13:40:22 +0000

    YUK-17167 Existing Min/Max Volts Point Names Not Updated  - updated point names to include "Daily". Daily Min Volts/Daily Max Volts

--- yukon-database/DBUpdates/oracle/6.7_2.sql
+++ yukon-database/DBUpdates/oracle/6.7_2.sql
@@ -8,6 +8,16 @@ CREATE INDEX Indx_YukonUser_UserGroupId ON YukonUser (
 );
 /* End YUK-17248 */
 
+/* Start YUK-17167 */
+UPDATE Point SET PointName = 'Daily Max Volts'
+WHERE PointName = 'Max Volts' AND PointType = 'Analog' AND PointOffset = '14'
+AND PaobjectId IN (SELECT Distinct PaobjectId FROM YukonPaobject WHERE Type LIKE 'RFN%');
+
+UPDATE Point SET PointName = 'Daily Min Volts'
+WHERE PointName = 'Min Volts' AND PointType = 'Analog' AND PointOffset = '15'
+AND PaobjectId IN (SELECT Distinct PaobjectId FROM YukonPaobject WHERE Type LIKE 'RFN%');
+/* End YUK-17167 */
+
 /**************************************************************/
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
--- yukon-database/DBUpdates/sqlserver/6.7_2.sql
+++ yukon-database/DBUpdates/sqlserver/6.7_2.sql
@@ -9,6 +9,16 @@ UserGroupId ASC
 GO
 /* End YUK-17248 */
 
+/* Start YUK-17167 */
+UPDATE Point SET PointName = 'Daily Max Volts'
+WHERE PointName = 'Max Volts' AND PointType = 'Analog' AND PointOffset = '14'
+AND PaobjectId IN (SELECT Distinct PaobjectId FROM YukonPaobject WHERE Type LIKE 'RFN%');
+
+UPDATE Point SET PointName = 'Daily Min Volts'
+WHERE PointName = 'Min Volts' AND PointType = 'Analog' AND PointOffset = '15'
+AND PaobjectId IN (SELECT Distinct PaobjectId FROM YukonPaobject WHERE Type LIKE 'RFN%');
+/* End YUK-17167 */
+
 /**************************************************************/
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
