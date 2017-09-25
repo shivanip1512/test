f1e6d7cef85191f65ea4418f717df8476336842c
Rawat, Payal <PayalRawat@Eaton.com>
2017-09-25 06:04:13 +0000

    YUK-17175 - Dashboards load too slow

---
+++ yukon-client/yukon-web/src/com/cannontech/web/picker/NonResidentailUserPicker.java
@@ -0,0 +1,41 @@
+package com.cannontech.web.picker;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import org.apache.lucene.search.BooleanClause.Occur;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cannontech.core.dao.YukonUserDao;
+import com.cannontech.user.YukonUserContext;
+import com.cannontech.web.search.lucene.criteria.YukonObjectCriteria;
+
+public class NonResidentailUserPicker extends UserPicker {
+
+    @Autowired private YukonUserDao userDao;
+
+    @Override
+    public YukonObjectCriteria combineCriteria(YukonObjectCriteria criteria, YukonUserContext userContext,
+            String extraArgs) {
+        final BooleanQuery.Builder query = new BooleanQuery.Builder().setDisableCoord(false);
+
+        if (criteria != null) {
+            query.add(criteria.getCriteria(), Occur.MUST);
+        }
+        List<String> userGroupNames = new ArrayList<>();
+        userGroupNames = userDao.getNonResidentialUserGroups();
+
+        LuceneQueryHelper.buildQueryWithUserGroups(query, new HashSet<String>(userGroupNames));
+
+        return new YukonObjectCriteria() {
+            @Override
+            public Query getCriteria() {
+                return query.build();
+            }
+        };
+    }
+
+}
