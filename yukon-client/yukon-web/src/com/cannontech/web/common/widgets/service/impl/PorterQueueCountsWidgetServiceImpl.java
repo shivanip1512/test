71155dba40a5f97f618bd57acc79f391a7b41c6a
Major, Michael <>
2017-11-10 22:08:52 +0000

    YUKRV-11472 YUK-16653 Create a dashboard widget to show Porter queue counts (UI)
    Review change to use the GlobalSetting dao instead of accessing the enum directly.

--- yukon-client/yukon-web/src/com/cannontech/web/common/widgets/service/impl/PorterQueueCountsWidgetServiceImpl.java
+++ yukon-client/yukon-web/src/com/cannontech/web/common/widgets/service/impl/PorterQueueCountsWidgetServiceImpl.java
@@ -34,7 +34,7 @@ public class PorterQueueCountsWidgetServiceImpl implements PorterQueueCountsWidg
     @Autowired private PaoDao paoDao;
     @Autowired private AttributeService attributeService;
     @Autowired private GlobalSettingDao globalSettingDao;
-    private static final Duration MINUTES_TO_WAIT_BEFORE_NEXT_REFRESH = Duration.standardMinutes((Integer) GlobalSettingType.PORTER_QUEUE_COUNTS_MINUTES_TO_WAIT_BEFORE_REFRESH.getDefaultValue());
+    private final Duration MINUTES_TO_WAIT_BEFORE_NEXT_REFRESH = Duration.standardMinutes(globalSettingDao.getInteger(GlobalSettingType.PORTER_QUEUE_COUNTS_MINUTES_TO_WAIT_BEFORE_REFRESH));
     private static final Logger log = YukonLogManager.getLogger(PorterQueueCountsWidgetServiceImpl.class);
 
     @Override
