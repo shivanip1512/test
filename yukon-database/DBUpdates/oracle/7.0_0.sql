d5bcb1bb8f94f4ed3f35d8b236cd8b8d8480aa37
Beckius, Aaron T <AaronTBeckius@Eaton.com>
2017-12-14 19:31:13 +0000

    YUK-17578 Update RFN-430SL "DEMAND" references to "INSTANTANEOUS_KW"  - Moving the trunk script from 7.0_0.sql to 6.7_4.sql

--- yukon-database/DBUpdates/oracle/7.0_0.sql
+++ yukon-database/DBUpdates/oracle/7.0_0.sql
@@ -409,54 +409,6 @@ ALTER TABLE LmProgramDirectGear
     MODIFY StopCommandRepeat NUMBER NOT NULL;
 /* End YUK-17588 */
 
-/* Start YUK-17578 */
-/* Small update to BehaviorReportValue */
-UPDATE BehaviorReportValue BRV
-SET BRV.Value = 'INSTANTANEOUS_KW'
-WHERE BRV.BehaviorReportId IN (
-    SELECT BR.BehaviorReportId 
-    FROM 
-        BehaviorReport BR 
-    JOIN 
-        YukonPAObject Y ON BR.DeviceID = Y.PAObjectID
-    WHERE Y.Type IN ('RFN-430SL1', 'RFN-430SL2', 'RFN-430SL3', 'RFN-430SL4') )
-AND BRV.Value='DEMAND';
-
-/* Update all RFN Data Streaming Device Behaviors with RFN-430SL1+ to use INSTANTANEOUS_KW instead of DEMAND */
-UPDATE BehaviorValue BV
-SET 
-    BV.Value='INSTANTANEOUS_KW'
-WHERE
-    BV.BehaviorId IN (
-        SELECT DISTINCT
-            BV.BehaviorId
-        FROM BehaviorValue BV
-            JOIN Behavior B ON BV.BehaviorId=B.BehaviorId
-            JOIN DeviceBehaviorMap DBM ON B.BehaviorId=DBM.BehaviorId
-            JOIN YukonPAObject Y ON DBM.DeviceId=Y.PAObjectID
-        WHERE 
-            B.BehaviorType='DATA_STREAMING'
-            AND Y.Type IN ('RFN-430SL1','RFN-430SL2','RFN-430SL3','RFN-430SL4') )
-AND
-    BV.Value='DEMAND';
-
-/* Update all rfnChannelConfiguration categories assigned to RFN-430SL1+ to use INSTANTANEOUS_KW instead of DEMAND */
-UPDATE DeviceConfigCategoryItem DCCI
-SET ItemValue='INSTANTANEOUS_KW'
-WHERE DCCI.DeviceConfigCategoryId IN (
-        SELECT DISTINCT DCC.DeviceConfigCategoryId 
-        FROM
-            DeviceConfigCategory DCC
-            JOIN DeviceConfigCategoryMap DCCM ON DCC.DeviceConfigCategoryId=DCCM.DeviceConfigCategoryId
-            JOIN DeviceConfiguration DC ON DCCM.DeviceConfigurationId=DC.DeviceConfigurationID
-            JOIN DeviceConfigDeviceTypes DCDT ON DC.DeviceConfigurationID=DCDT.DeviceConfigurationId
-        WHERE
-            DCC.CategoryType='rfnChannelConfiguration'
-            AND DCDT.PaoType IN ('RFN-430SL1','RFN-430SL2','RFN-430SL3','RFN-430SL4') )
-    AND DCCI.ItemName LIKE 'enabledChannels%attribute'
-    AND DCCI.ItemValue='DEMAND';
-/* End YUK-17578 */
-
 /**************************************************************/
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
--- yukon-database/DBUpdates/sqlserver/7.0_0.sql
+++ yukon-database/DBUpdates/sqlserver/7.0_0.sql
@@ -410,61 +410,6 @@ ALTER TABLE LmProgramDirectGear
 GO
 /* End YUK-17588 */
 
-/* Start YUK-17578 */
-/* Small update to BehaviorReportValue */
-UPDATE BehaviorReportValue
-SET Value = 'INSTANTANEOUS_KW'
-FROM 
-    BehaviorReportValue BRV
-JOIN 
-    BehaviorReport BR ON BRV.BehaviorReportId=BR.BehaviorReportId
-JOIN 
-    YukonPAObject Y ON BR.DeviceId=Y.PAObjectID
-WHERE 
-    Y.Type IN ('RFN-430SL1', 'RFN-430SL2', 'RFN-430SL3', 'RFN-430SL4')
-AND 
-    BRV.Value='DEMAND';
-
-/* Update all RFN Data Streaming Device Behaviors with RFN-430SL1+ to use INSTANTANEOUS_KW instead of DEMAND */
-UPDATE BehaviorValue
-SET 
-    Value='INSTANTANEOUS_KW'
-FROM 
-    BehaviorValue BV 
-    JOIN (
-        SELECT DISTINCT
-            BV.BehaviorId
-        FROM BehaviorValue BV
-            JOIN Behavior B ON BV.BehaviorId=B.BehaviorId
-            JOIN DeviceBehaviorMap DBM ON B.BehaviorId=DBM.BehaviorId
-            JOIN YukonPAObject Y ON DBM.DeviceId=Y.PAObjectID
-        WHERE 
-            B.BehaviorType='DATA_STREAMING'
-            AND Y.Type IN ('RFN-430SL1','RFN-430SL2','RFN-430SL3','RFN-430SL4')
-        ) SentinelDS ON BV.BehaviorId=SentinelDS.BehaviorId
-WHERE 
-    BV.Value='DEMAND';
-
-/* Update all rfnChannelConfiguration categories assigned to RFN-430SL1+ to use INSTANTANEOUS_KW instead of DEMAND */
-UPDATE DCCI
-SET ItemValue='INSTANTANEOUS_KW'
-FROM 
-    DeviceConfigCategoryItem DCCI
-    JOIN (
-        SELECT DISTINCT DCC.DeviceConfigCategoryId 
-        FROM
-            DeviceConfigCategory DCC
-            JOIN DeviceConfigCategoryMap DCCM ON DCC.DeviceConfigCategoryId=DCCM.DeviceConfigCategoryId
-            JOIN DeviceConfiguration DC ON DCCM.DeviceConfigurationId=DC.DeviceConfigurationID
-            JOIN DeviceConfigDeviceTypes DCDT ON DC.DeviceConfigurationID=DCDT.DeviceConfigurationId
-        WHERE
-            DCC.CategoryType='rfnChannelConfiguration'
-            AND DCDT.PaoType IN ('RFN-430SL1','RFN-430SL2','RFN-430SL3','RFN-430SL4')) SentinelDC ON DCCI.DeviceConfigCategoryId=SentinelDC.DeviceConfigCategoryId
-WHERE
-    DCCI.ItemName LIKE 'enabledChannels%attribute'
-    AND DCCI.ItemValue='DEMAND';
-/* End YUK-17578 */
-
 /**************************************************************/
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
