115321f83fea14a9cb1be05cff9bbc3c3d3adfbe
Moc, Jeremy P <JeremyPMoc@Eaton.com>
2018-01-30 19:17:36 +0000

    YUK-17851 Enable DMV Test scheduling - C++
    Dead code removal and some very light refactoring around the CapControl PaoEvents.

--- yukon-server/CapControl/include/pao_event.h
+++ yukon-server/CapControl/include/pao_event.h
@@ -1,14 +1,12 @@
 #pragma once
 
-#include "dbmemobject.h"
-                       
 namespace Cti
 {
 class RowReader;
 }
                        
                        
-class CtiPAOEvent  : public CtiMemDBObject
+class CtiPAOEvent
 {
 public:
 
@@ -23,27 +21,24 @@ public:
     const std::string& getEventCommand() const;
     bool getDisableOvUvFlag() const;
 
-    void setEventId(long eventId);
-    void setScheduleId(long schedId);
-    void setPAOId(long paoId);
-    void setEventCommand(const std::string& eventCommand);
-    void setDisableOvUvFlag(bool flag);
-
     CtiPAOEvent& operator=(const CtiPAOEvent& right);
-    int operator==(const CtiPAOEvent& right) const;
-    int operator!=(const CtiPAOEvent& right) const;
-
-    bool isDirty();
-    void setDirty(bool flag);
 
 private:
     
-    bool _dirty;
-
     long         _eventId;
     long         _scheduleId;
     long         _paoId;
     std::string  _eventCommand;
     bool         _disableOvUvFlag;
-    
 };
+
+inline bool operator==( const CtiPAOEvent & lhs, const CtiPAOEvent & rhs )
+{
+    return lhs.getEventId() == rhs.getEventId();
+}
+
+inline bool operator!=( const CtiPAOEvent & lhs, const CtiPAOEvent & rhs )
+{
+    return ! ( lhs == rhs );
+}
+
--- yukon-server/CapControl/pao_event.cpp
+++ yukon-server/CapControl/pao_event.cpp
@@ -4,14 +4,13 @@
 #include "row_reader.h"
 #include "ccutil.h"
 
-using Cti::CapControl::setVariableIfDifferent;
-using std::string;
+using Cti::CapControl::deserializeFlag;
+
 
 /*---------------------------------------------------------------------------
     Constructor
 ---------------------------------------------------------------------------*/
 CtiPAOEvent::CtiPAOEvent() :
-_dirty(false),
 _eventId(0),
 _scheduleId(0),
 _paoId(0),
@@ -20,31 +19,25 @@ _disableOvUvFlag(false)
 }
 
 
-CtiPAOEvent::CtiPAOEvent(long eventId, long schedId, long paoId, const string& command, bool disableOvUv) :
-_dirty(false)
+CtiPAOEvent::CtiPAOEvent(long eventId, long schedId, long paoId, const std::string& command, bool disableOvUv)
+    :   _eventId( eventId ),
+        _scheduleId( schedId ),
+        _paoId( paoId ),
+        _eventCommand( command ),
+        _disableOvUvFlag( disableOvUv )
 {
-    _eventId = eventId;
-    _scheduleId = schedId;
-    _paoId = paoId;
-    _eventCommand = command;
-    _disableOvUvFlag = disableOvUv;
+    // empty
 }
 
 
 CtiPAOEvent::CtiPAOEvent(Cti::RowReader& rdr)
 {
-    string tempBoolString;
-
     rdr["eventid"] >> _eventId;
     rdr["scheduleid"] >> _scheduleId;
     rdr["paoid"] >> _paoId;
     rdr["command"] >> _eventCommand;
-    rdr["disableovuv"] >> tempBoolString;
-    std::transform(tempBoolString.begin(), tempBoolString.end(), tempBoolString.begin(), tolower);
-    setDisableOvUvFlag(tempBoolString=="y");
 
-
-    _dirty = false;
+    _disableOvUvFlag = deserializeFlag( rdr[ "disableovuv" ].as<std::string>() );
 }
 /*---------------------------------------------------------------------------
     Destructor
@@ -62,17 +55,7 @@ CtiPAOEvent& CtiPAOEvent::operator=(const CtiPAOEvent& right)
     _eventCommand = right._eventCommand;
     _disableOvUvFlag = right._disableOvUvFlag;
 
-    _dirty = right._dirty;
-
     return *this;
-}
-int CtiPAOEvent::operator==(const CtiPAOEvent& right) const
-{
-    return _eventId == right._eventId;
-}
-int CtiPAOEvent::operator!=(const CtiPAOEvent& right) const
-{
-    return _eventId != right._eventId;
 }
 
 long CtiPAOEvent::getEventId() const
@@ -90,7 +73,7 @@ long CtiPAOEvent::getPAOId() const
     return _paoId;
 }
 
-const string& CtiPAOEvent::getEventCommand() const
+const std::string& CtiPAOEvent::getEventCommand() const
 {
     return _eventCommand;
 }
@@ -100,32 +83,3 @@ bool CtiPAOEvent::getDisableOvUvFlag() const
     return _disableOvUvFlag;
 }
 
-void CtiPAOEvent::setEventId(long eventId)
-{
-    _dirty |= setVariableIfDifferent(_eventId, eventId);
-}
-void CtiPAOEvent::setScheduleId(long schedId)
-{
-    _dirty |= setVariableIfDifferent(_scheduleId, schedId);
-}
-void CtiPAOEvent::setPAOId(long paoId)
-{
-    _dirty |= setVariableIfDifferent(_paoId, paoId);
-}
-void CtiPAOEvent::setEventCommand(const string& eventCommand)
-{
-    _dirty |= setVariableIfDifferent(_eventCommand, eventCommand);
-}
-void CtiPAOEvent::setDisableOvUvFlag(bool flag)
-{
-    _dirty |= setVariableIfDifferent(_disableOvUvFlag, flag);
-}
-
-bool CtiPAOEvent::isDirty()
-{
-    return _dirty;
-}
-void CtiPAOEvent::setDirty(bool flag)
-{
-    _dirty = flag;
-}
