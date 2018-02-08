3f1e96864cb76fa5a87809249062d1f011c891f4
Singh, Manender <ManenderSingh@Eaton.com>
2018-02-08 06:10:03 +0000

    YUK-17743 - Gateway Test Connection Causes Exception in Webserver Log - Formatting changes

--- yukon-client/web-common/src/main/java/com/cannontech/web/util/ErrorHelperFilter.java
+++ yukon-client/web-common/src/main/java/com/cannontech/web/util/ErrorHelperFilter.java
@@ -36,14 +36,14 @@ import com.cannontech.common.util.FriendlyExceptionResolver;
 import com.cannontech.database.data.lite.LiteYukonUser;
 import com.cannontech.util.ServletUtil;
 
-public class ErrorHelperFilter  implements Filter {
+public class ErrorHelperFilter implements Filter {
 
     private Logger log = YukonLogManager.getLogger(ErrorHelperFilter.class);
-	private static final String SERVLET_STARTUP_ERROR = "comcannontechSERVLET_STARTUP_ERROR";
+    private static final String SERVLET_STARTUP_ERROR = "comcannontechSERVLET_STARTUP_ERROR";
     private static final String SERVLET_STARTUP_ERROR_ROOT_MESSAGE = "comcannontechSERVLET_STARTUP_ERROR_ROOT_MESSAGE";
     public static final String LOG_KEY = "comcannontechSERVLET_ERROR_LOG_KEY";
     private ServletContext servletContext;
-    
+
     private Set<Class<? extends Throwable>> exceptionToIgnore = new HashSet<Class<? extends Throwable>>();
     {
         exceptionToIgnore.add(HttpSessionRequiredException.class);
@@ -51,70 +51,67 @@ public class ErrorHelperFilter  implements Filter {
 
     public void init(FilterConfig arg0) throws ServletException {
         servletContext = arg0.getServletContext();
-	}
-	
-	private String getRequestInfo(ServletRequest req) {
-		req.getParameterMap();
-		if (req instanceof HttpServletRequest) {
-			HttpServletRequest httpReq = (HttpServletRequest) req;
-			String pathInfo = httpReq.getPathInfo();
-			String contextPath = httpReq.getContextPath();
-			StringBuffer requestURL = httpReq.getRequestURL();
-			List<String> parameterList = new ArrayList<String>(httpReq.getParameterMap().size());
-			Set parameterNames = httpReq.getParameterMap().keySet();
-			for (Iterator iter = parameterNames.iterator(); iter.hasNext();) {
-				String key = (String) iter.next();
-				String values;
-				//avoid printing password values to the log
-                if(key.equalsIgnoreCase("PASSWORD")) {
+    }
+
+    private String getRequestInfo(ServletRequest req) {
+        req.getParameterMap();
+        if (req instanceof HttpServletRequest) {
+            HttpServletRequest httpReq = (HttpServletRequest) req;
+            String pathInfo = httpReq.getPathInfo();
+            String contextPath = httpReq.getContextPath();
+            StringBuffer requestURL = httpReq.getRequestURL();
+            List<String> parameterList = new ArrayList<String>(httpReq.getParameterMap().size());
+            Set parameterNames = httpReq.getParameterMap().keySet();
+            for (Iterator iter = parameterNames.iterator(); iter.hasNext();) {
+                String key = (String) iter.next();
+                String values;
+                // avoid printing password values to the log
+                if (key.equalsIgnoreCase("PASSWORD")) {
                     values = "password removed";
                 } else {
                     values = StringUtils.join(httpReq.getParameterValues(key), ",");
                 }
                 parameterList.add(key + "={" + values + "}");
-			}
-			
-			String parameters = StringUtils.join(parameterList.iterator(), ",");
-			    
-			String remoteAddr = httpReq.getRemoteAddr();
-			String queryString = httpReq.getQueryString();
-			//avoid printing password values to the log
-            if(queryString != null) {
-			    queryString = queryString.replaceAll("password=[^&]*&?", "password=<password removed>&");
-			}
-			    
-			return "contextPath=" + contextPath +
-			    "; pathInfo=" + pathInfo + 
-				"; parameters=" + parameters + 
-				"; remoteAddr=" + remoteAddr + 
-				"; requestURL=" + requestURL +
-				"; queryString=" + queryString;
-		}
-		return "[non http request]";
-	}
-	
-	private void handleAjaxErrorResponse(ServletResponse resp, Throwable t) throws IOException {
-		if (resp instanceof HttpServletResponse) {
-			HttpServletResponse httpResp = (HttpServletResponse)resp;
-			if (httpResp.isCommitted()) {
-				throw new RuntimeException("Can't output AJAX exception, already committed",t);
-			}
-			httpResp.reset();
-			httpResp.resetBuffer();
-			httpResp.setStatus(500);
-			try {
-				httpResp.getWriter().print(t.getMessage());
-			} catch (IllegalStateException e) {
-				throw new RuntimeException("Can't output AJAX exception, OutputStream already opened",t);
-			}
-			return;
-		}
-		throw new RuntimeException("Can't output AJAX exception, not HttpServletResponse",t);
-	}
-	
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		CTILogger.debug("Starting request handling: " + getRequestInfo(request));
-        
+            }
+
+            String parameters = StringUtils.join(parameterList.iterator(), ",");
+
+            String remoteAddr = httpReq.getRemoteAddr();
+            String queryString = httpReq.getQueryString();
+            // avoid printing password values to the log
+            if (queryString != null) {
+                queryString = queryString.replaceAll("password=[^&]*&?", "password=<password removed>&");
+            }
+
+            return "contextPath=" + contextPath + "; pathInfo=" + pathInfo + "; parameters=" + parameters
+                + "; remoteAddr=" + remoteAddr + "; requestURL=" + requestURL + "; queryString=" + queryString;
+        }
+        return "[non http request]";
+    }
+
+    private void handleAjaxErrorResponse(ServletResponse resp, Throwable t) throws IOException {
+        if (resp instanceof HttpServletResponse) {
+            HttpServletResponse httpResp = (HttpServletResponse) resp;
+            if (httpResp.isCommitted()) {
+                throw new RuntimeException("Can't output AJAX exception, already committed", t);
+            }
+            httpResp.reset();
+            httpResp.resetBuffer();
+            httpResp.setStatus(500);
+            try {
+                httpResp.getWriter().print(t.getMessage());
+            } catch (IllegalStateException e) {
+                throw new RuntimeException("Can't output AJAX exception, OutputStream already opened", t);
+            }
+            return;
+        }
+        throw new RuntimeException("Can't output AJAX exception, not HttpServletResponse", t);
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        CTILogger.debug("Starting request handling: " + getRequestInfo(request));
+
         // first check if our server came up okay
         Object attribute = servletContext.getAttribute(SERVLET_STARTUP_ERROR);
         if (attribute instanceof Throwable) {
@@ -126,8 +123,8 @@ public class ErrorHelperFilter  implements Filter {
             requestDispatcher.forward(request, response);
             return;
         }
-		try {
-            HttpServletRequest hReq = (HttpServletRequest)request;
+        try {
+            HttpServletRequest hReq = (HttpServletRequest) request;
             HttpSession session = hReq.getSession(false);
             if (session != null) {
                 LiteYukonUser yukonUser = ServletUtil.getYukonUser(session);
@@ -136,47 +133,49 @@ public class ErrorHelperFilter  implements Filter {
                 }
             }
             MDC.put("path", hReq.getRequestURI());
-			chain.doFilter(request, response);
-		} catch (Error e) {
-			Throwable rc = CtiUtilities.getRootCause(e);
+            chain.doFilter(request, response);
+        } catch (Error e) {
+            Throwable rc = CtiUtilities.getRootCause(e);
             String key = setupUniqueLogKey(request);
-            CTILogger.error("Servlet error filter caught an Error processing {" + key + "}: " + getRequestInfo(request), e);
-			if (ServletUtil.isAjaxRequest(request)) {
-				handleAjaxErrorResponse(response, rc);
-			} else {
-				throw e;
-			}
-		} catch (RuntimeException re) {
-		    Throwable rc = CtiUtilities.getRootCause(re);
-		    String key = setupUniqueLogKey(request);
-			handleException(request, re, rc, key);
-			if (ServletUtil.isAjaxRequest(request)) {
-				handleAjaxErrorResponse(response, rc);
-			} else {
-				throw re;
-			}
-		} catch (Throwable t) {
-		    Throwable rc = CtiUtilities.getRootCause(t);
-		    String key = setupUniqueLogKey(request);
-		    handleException(request, t, rc, key);
-			if (ServletUtil.isAjaxRequest(request)) {
-				handleAjaxErrorResponse(response, rc);
-			} else {
-				throw new ServletException(t);
-			}
-		} finally {
-		    MDC.remove("yukonuser");
+            CTILogger.error("Servlet error filter caught an Error processing {" + key + "}: " + getRequestInfo(request),
+                e);
+            if (ServletUtil.isAjaxRequest(request)) {
+                handleAjaxErrorResponse(response, rc);
+            } else {
+                throw e;
+            }
+        } catch (RuntimeException re) {
+            Throwable rc = CtiUtilities.getRootCause(re);
+            String key = setupUniqueLogKey(request);
+            handleException(request, re, rc, key);
+            if (ServletUtil.isAjaxRequest(request)) {
+                handleAjaxErrorResponse(response, rc);
+            } else {
+                throw re;
+            }
+        } catch (Throwable t) {
+            Throwable rc = CtiUtilities.getRootCause(t);
+            String key = setupUniqueLogKey(request);
+            handleException(request, t, rc, key);
+            if (ServletUtil.isAjaxRequest(request)) {
+                handleAjaxErrorResponse(response, rc);
+            } else {
+                throw new ServletException(t);
+            }
+        } finally {
+            MDC.remove("yukonuser");
             MDC.remove("path");
         }
-		
-	}
-	
+
+    }
+
     private void handleException(ServletRequest request, Throwable t, Throwable rc, String key) {
         Level level = Level.ERROR;
         if (ignoreException(t)) {
             level = Level.DEBUG;
         }
-        log.log(level, "Servlet error filter caught an exception processing {" + key + "}: " + getRequestInfo(request), t);
+        log.log(level, "Servlet error filter caught an exception processing {" + key + "}: " + getRequestInfo(request),
+            t);
         log.log(level, "Root cause was: ", rc);
     }
 
@@ -190,16 +189,16 @@ public class ErrorHelperFilter  implements Filter {
         }
         return false;
     }
-    
+
     private String setupUniqueLogKey(ServletRequest request) {
         String uniqueKey = "YK" + RandomStringUtils.randomNumeric(10);
         request.setAttribute(LOG_KEY, uniqueKey);
         return uniqueKey;
     }
 
-	public void destroy() {
-	}
-    
+    public void destroy() {
+    }
+
     public static void recordStartupError(ServletContext servletContext, Throwable t) {
         servletContext.setAttribute(SERVLET_STARTUP_ERROR, t);
     }
@@ -207,15 +206,16 @@ public class ErrorHelperFilter  implements Filter {
     public static boolean doesStartupErrorExist(ServletContext servletContext) {
         return servletContext.getAttribute(SERVLET_STARTUP_ERROR) != null;
     }
-    
+
     public static String getFriendlyExceptionMessage(ServletContext servletContext, Throwable throwable) {
-        
+
         WebApplicationContext webContext = WebApplicationContextUtils.getRequiredWebApplicationContext(servletContext);
-        FriendlyExceptionResolver friendlyExceptionResolver = (FriendlyExceptionResolver)webContext.getBean("friendlyExceptionResolver");
-        
+        FriendlyExceptionResolver friendlyExceptionResolver =
+            (FriendlyExceptionResolver) webContext.getBean("friendlyExceptionResolver");
+
         String friendlyExceptionMessage = friendlyExceptionResolver.getFriendlyExceptionMessage(throwable);
-        
+
         return friendlyExceptionMessage;
     }
-    
+
 }
