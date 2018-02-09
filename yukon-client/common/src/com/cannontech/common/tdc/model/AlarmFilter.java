143b12acecbb955791cd9dc98b0be78ef3e8b397
Thonglyvong, Ricky <RickyThonglyvong@Eaton.com>
2018-02-09 16:26:39 +0000

    YUK-17442 "View Date" date picker should be enabled for All Alarms page on TDC -Review Change

--- yukon-client/common/src/com/cannontech/common/tdc/model/AlarmFilter.java
+++ yukon-client/common/src/com/cannontech/common/tdc/model/AlarmFilter.java
@@ -3,29 +3,15 @@ package com.cannontech.common.tdc.model;
 import com.cannontech.common.i18n.DisplayableEnum;
 
 public enum AlarmFilter implements DisplayableEnum {
-    NO_FILTER(""),
-    UNACKNOWLEDGED_ALARMS(""),
-    ACTIVE_ALARMS(""),
-    ALARM_HISTORY(""),
+    NO_FILTER,
+    UNACKNOWLEDGED_ALARMS,
+    ACTIVE_ALARMS,
+    ALARM_HISTORY,
     ;
-    
-    private String key;
-    
-    AlarmFilter(String key) {
-        this.setKey(key);
-    }
     
     @Override
     public String getFormatKey() {
         return "yukon.web.modules.tools.tdc.alarmFilters." + name();
-    }
-
-    public String getKey() {
-        return key;
-    }
-
-    public void setKey(String key) {
-        this.key = key;
     }
     
 }
