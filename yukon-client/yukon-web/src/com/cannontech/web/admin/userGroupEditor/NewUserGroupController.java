6b5c5489d8617bcb6a0b2638f080d980d5b8ce57
Modi, Preeti <>
2018-02-10 06:45:32 +0000

    YUK-17646 : Don't allow objects to be saved with leading or trailing spaces - SimpleTableAccessTemplate Changes Fixed review comments

--- yukon-client/yukon-web/src/com/cannontech/web/admin/userGroupEditor/NewUserGroupController.java
+++ yukon-client/yukon-web/src/com/cannontech/web/admin/userGroupEditor/NewUserGroupController.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
