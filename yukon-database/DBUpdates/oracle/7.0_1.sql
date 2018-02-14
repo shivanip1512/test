dbe4c124eeacaae87726a4644f03a1eddcbde320
Beckius, Aaron T <AaronTBeckius@Eaton.com>
2018-02-14 19:51:52 +0000

    YUK-17709 Remove Centron C1SX "Last Interval Voltage" entries from existing device configs  - Added an update query that will look for LAST_INTERVAL_VOLTAGE config category items from centron410DisplayItems config categories, then perform maintenance on the remaining entries to ensure that nothing breaks as a result.

--- yukon-database/DBUpdates/oracle/7.0_1.sql
+++ yukon-database/DBUpdates/oracle/7.0_1.sql
@@ -13,6 +13,98 @@ CREATE TABLE MaintenanceTaskSettings  (
 );
 /* End YUK-17899 */
 
+/* Start YUK-17709 */
+/* @error ignore-begin */
+/* This will almost definitely error, but it's a necessary safety check */
+TRUNCATE TABLE t_ConfigCategoryItemTemp;
+DROP TABLE t_ConfigCategoryItemTemp;
+/* @error ignore-end */
+
+/* Create a temp table */
+CREATE GLOBAL TEMPORARY TABLE t_ConfigCategoryItemTemp
+(
+    DeviceConfigCategoryIdTemp NUMBER,
+    ItemValueTemp VARCHAR2(60),
+    Ordering NUMBER
+) ON COMMIT PRESERVE ROWS;
+
+/* @start-block */
+BEGIN
+    
+    /* Fill the temp table with any items that include 'LAST_INTERVAL_VOLTAGE' and something else that's not 'SLOT_DISABLED' */
+    INSERT INTO t_ConfigCategoryItemTemp
+    SELECT 
+        I.DeviceConfigCategoryId AS DeviceConfigCategoryIdTemp, 
+        I.ItemValue AS ItemValueTemp, 
+        ROW_NUMBER() OVER ( PARTITION BY I.DeviceConfigCategoryId ORDER BY CAST( SUBSTR( I.ItemName, 12, 2 ) AS NUMBER ) ) AS Ordering
+    FROM DeviceConfigCategoryItem I
+    WHERE I.DeviceConfigCategoryId IN (
+        SELECT C.DeviceConfigCategoryId
+        FROM DeviceConfigCategory C
+        JOIN DeviceConfigCategoryItem CI
+            ON C.DeviceConfigCategoryId = CI.DeviceConfigCategoryId
+        WHERE C.CategoryType = 'centron410DisplayItems'
+        AND CI.ItemValue = 'LAST_INTERVAL_VOLTAGE'
+    )
+    AND I.ItemValue NOT IN( 'SLOT_DISABLED', 'LAST_INTERVAL_VOLTAGE' )
+    ORDER BY CAST( SUBSTR( I.ItemName, 12, 2 ) AS INT );
+    
+    /* Fill in the edge cases where the category items only includes 'LAST_INTERVAL_VOLTAGE' and 'SLOT_DISABLED' */
+    /* Any such edge cases will end up only having 'SLOT_DISABLED' remaining */
+    INSERT INTO t_ConfigCategoryItemTemp
+    SELECT 
+        I.DeviceConfigCategoryId AS DeviceConfigCategoryIdTemp, 
+        'DELETE_THIS' AS ItemValueTemp, 
+        -1 AS Ordering
+    FROM DeviceConfigCategoryItem I
+    WHERE I.DeviceConfigCategoryId IN (
+        SELECT C.DeviceConfigCategoryId
+        FROM DeviceConfigCategory C
+        JOIN DeviceConfigCategoryItem CI
+            ON C.DeviceConfigCategoryId = CI.DeviceConfigCategoryId
+        WHERE C.CategoryType = 'centron410DisplayItems'
+        AND CI.ItemValue = 'LAST_INTERVAL_VOLTAGE'
+    )
+    AND I.DeviceConfigCategoryId NOT IN (SELECT DeviceConfigCategoryIdTemp FROM t_ConfigCategoryItemTemp)
+    AND I.ItemValue = 'LAST_INTERVAL_VOLTAGE'
+    GROUP BY DeviceConfigCategoryId, ItemValue;
+    
+    /* Now that the temp table is complete, wipe clean all DeviceConfigs that include the 'LAST_INTERVAL_VOLTAGE' item */
+    UPDATE DeviceConfigCategoryItem
+    SET ItemValue = 'SLOT_DISABLED'
+    WHERE DeviceConfigCategoryId IN (
+        SELECT DeviceConfigCategoryIdTemp
+        FROM t_ConfigCategoryItemTemp
+    );
+    
+    /* Remove the edge case... cases */
+    DELETE FROM t_ConfigCategoryItemTemp
+    WHERE Ordering = -1;
+    
+    /* Update the table to match the now sorted temp table, where ordering matches the number after 'displayItemXX' */
+    UPDATE DeviceConfigCategoryItem
+    SET ItemValue = (
+        SELECT T.ItemValueTemp
+        FROM t_ConfigCategoryItemTemp T
+        WHERE DeviceConfigCategoryId = T.DeviceConfigCategoryIdTemp
+        AND CAST( SUBSTR( ItemName, 12, 2 ) AS INT ) = Ordering
+    )
+    WHERE DeviceConfigCategoryId = (
+        SELECT T.DeviceConfigCategoryIdTemp
+        FROM t_ConfigCategoryItemTemp T
+        WHERE DeviceConfigCategoryId = T.DeviceConfigCategoryIdTemp
+        AND CAST( SUBSTR( ItemName, 12, 2 ) AS INT ) = Ordering
+    );
+
+END;
+/
+/* @end-block */
+
+/* Clean up */
+TRUNCATE TABLE t_ConfigCategoryItemTemp;
+DROP TABLE t_ConfigCategoryItemTemp;
+/* End YUK-17709 */
+
 /**************************************************************/
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
--- yukon-database/DBUpdates/sqlserver/7.0_1.sql
+++ yukon-database/DBUpdates/sqlserver/7.0_1.sql
@@ -13,8 +13,86 @@ CREATE TABLE MaintenanceTaskSettings (
    CONSTRAINT PK_MaintenanceTaskSettings PRIMARY KEY (SettingType)
 );
 GO
-
 /* End YUK-17899 */
+
+/* Start YUK-17709 */
+/* @start-block */
+BEGIN
+
+    /* Create a temp table with any items that include 'LAST_INTERVAL_VOLTAGE' and something else that's not 'SLOT_DISABLED' */
+    SELECT 
+        I.DeviceConfigCategoryId AS DeviceConfigCategoryIdTemp, 
+        I.ItemValue AS ItemValueTemp, 
+        ROW_NUMBER() OVER ( PARTITION BY I.DeviceConfigCategoryId ORDER BY CAST( SUBSTRING( I.ItemName, 12, 2 ) AS INT ) ) AS Ordering
+    INTO #ConfigCategoryItemTemp
+    FROM DeviceConfigCategoryItem I
+    WHERE I.DeviceConfigCategoryId IN (
+        SELECT C.DeviceConfigCategoryId
+        FROM DeviceConfigCategory C
+        JOIN DeviceConfigCategoryItem CI
+            ON C.DeviceConfigCategoryId = CI.DeviceConfigCategoryId
+        WHERE C.CategoryType = 'centron410DisplayItems'
+        AND CI.ItemValue = 'LAST_INTERVAL_VOLTAGE'
+    )
+    AND I.ItemValue NOT IN( 'SLOT_DISABLED', 'LAST_INTERVAL_VOLTAGE' )
+    AND ItemName LIKE 'displayItem%'
+    ORDER BY CAST( SUBSTRING( I.ItemName, 12, 2 ) AS INT );
+    
+    /* Fill in the edge cases where the category items only includes 'LAST_INTERVAL_VOLTAGE' and 'SLOT_DISABLED' */
+    /* Any such edge cases will end up only having 'SLOT_DISABLED' remaining */
+    INSERT INTO #ConfigCategoryItemTemp
+    SELECT 
+        I.DeviceConfigCategoryId AS DeviceConfigCategoryIdTemp, 
+        'DELETE_THIS' AS ItemValueTemp, 
+        -1 AS Ordering
+    FROM DeviceConfigCategoryItem I
+    WHERE I.DeviceConfigCategoryId IN (
+        SELECT C.DeviceConfigCategoryId
+        FROM DeviceConfigCategory C
+        JOIN DeviceConfigCategoryItem CI
+            ON C.DeviceConfigCategoryId = CI.DeviceConfigCategoryId
+        WHERE C.CategoryType = 'centron410DisplayItems'
+        AND CI.ItemValue = 'LAST_INTERVAL_VOLTAGE'
+    )
+    AND I.DeviceConfigCategoryId NOT IN (SELECT DeviceConfigCategoryIdTemp FROM #ConfigCategoryItemTemp)
+    AND I.ItemValue = 'LAST_INTERVAL_VOLTAGE'
+    GROUP BY DeviceConfigCategoryId, ItemValue;
+
+    /* Now that the temp table is complete, wipe clean all DeviceConfigs that include the 'LAST_INTERVAL_VOLTAGE' item */
+    UPDATE DeviceConfigCategoryItem
+    SET ItemValue = 'SLOT_DISABLED'
+    WHERE DeviceConfigCategoryId IN (
+        SELECT DeviceConfigCategoryIdTemp
+        FROM #ConfigCategoryItemTemp
+    );
+    
+    /* Remove the edge cases from the temp table */
+    DELETE FROM #ConfigCategoryItemTemp
+    WHERE Ordering = -1;
+    
+    /* Update the table to match the now sorted temp table, where ordering matches the number after 'displayItemXX' */
+    UPDATE DeviceConfigCategoryItem
+    SET ItemValue = (
+        SELECT T.ItemValueTemp
+        FROM #ConfigCategoryItemTemp T
+        WHERE DeviceConfigCategoryId = T.DeviceConfigCategoryIdTemp
+        AND ItemName LIKE 'displayItem%'
+        AND CAST( SUBSTRING( ItemName, 12, 2 ) AS INT ) = Ordering
+    )
+    WHERE DeviceConfigCategoryId = (
+        SELECT T.DeviceConfigCategoryIdTemp
+        FROM #ConfigCategoryItemTemp T
+        WHERE DeviceConfigCategoryId = T.DeviceConfigCategoryIdTemp
+        AND ItemName LIKE 'displayItem%'
+        AND CAST( SUBSTRING( ItemName, 12, 2 ) AS INT ) = Ordering
+    );
+    
+    /* Clean up */
+    DROP TABLE #ConfigCategoryItemTemp;
+
+END;
+/* @end-block */
+/* End YUK-17709 */
 
 /**************************************************************/
 /* VERSION INFO                                               */
