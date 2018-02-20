a4cc333d7725b07d17328306ab28cbcfe906f3be
Shikha, Shikha <ShikhaShikha@Eaton.com>
2018-02-20 04:29:33 +0000

    YUK-17907 Schedule Detail Report Results Incomplete

--- yukon-client/common/src/com/cannontech/core/schedule/dao/impl/PaoScheduleDaoImpl.java
+++ yukon-client/common/src/com/cannontech/core/schedule/dao/impl/PaoScheduleDaoImpl.java
@@ -70,7 +70,7 @@ public class PaoScheduleDaoImpl implements PaoScheduleDao {
             sink.addValue("LastRunTime", schedule.getLastRunTime());
             sink.addValue("IntervalRate", schedule.getRepeatSeconds());
             sink.addValue("ScheduleName", schedule.getName());
-            sink.addValue("Disabled", schedule.isDisabled() ? YNBoolean.YES : YNBoolean.NO);
+            sink.addValue("Disabled", YNBoolean.valueOf(schedule.isDisabled()));
         }
 
         @Override
