6e1ca363428f9b2616fe9d87faa0a30b4492e665
Beckius, Aaron T <AaronTBeckius@Eaton.com>
2018-03-02 16:53:35 +0000

    YUK-18001 DB Update - Scanner Log Error - Scan Window Open and Close Times Equal  - Added 7.0_2.sql in 7.0 and 7.1 branches  - Removing DB occurrences where the scan window open and close times are 0

---
+++ yukon-database/DBUpdates/sqlserver/7.0_2.sql
@@ -0,0 +1,18 @@
+/******************************************/
+/**** SQL Server DBupdates             ****/
+/******************************************/
+
+/* Start YUK-18001 */
+DELETE FROM DeviceWindow
+WHERE Type = 'SCAN'
+AND WinOpen = 0
+AND WinClose = 0 
+AND AlternateOpen = 0
+AND AlternateClose = 0;
+/* End YUK-18001 */
+
+/**************************************************************/
+/* VERSION INFO                                               */
+/* Inserted when update script is run                         */
+/**************************************************************/
+/*INSERT INTO CTIDatabase VALUES ('7.0', '26-FEB-2018', 'Latest Update', 1, GETDATE());*/
