d770d054b8254afda20b02c19836caa82e58ca69
Otteson, Jess M <JessMOtteson@Eaton.com>
2018-03-23 20:23:06 +0000

    YUK-17355 Update DB tools frame and scripts to use new DBUpdates table Adding start and dbupdate commit to 7.0.2.

--- yukon-database/DBUpdates/oracle/7.0_2.sql
+++ yukon-database/DBUpdates/oracle/7.0_2.sql
@@ -2,14 +2,16 @@
 /****     Oracle DBupdates             ****/ 
 /******************************************/ 
 
-/* Start YUK-18001 */
+/* @start YUK-18001 */
 DELETE FROM DeviceWindow
 WHERE Type = 'SCAN'
 AND WinOpen = 0
 AND WinClose = 0 
 AND AlternateOpen = 0
 AND AlternateClose = 0;
-/* End YUK-18001 */
+
+INSERT INTO DBUpdates VALUES ('YUK-18001', '7.0.2', SYSDATE);
+/* @end YUK-18001 */
     
 /**************************************************************/
 /* VERSION INFO                                               */
