43778c841ab19e59085b05a98dbf84b6e13bec9a
Otteson, Jess M <JessMOtteson@Eaton.com>
2018-04-10 22:24:37 +0000

    YUK-18050 Install links to start and stop Yukon services
    Added batch files and restart powershell to installer.

---
+++ yukon-install/resources/scripts/StartYukon.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+SET ThisScriptsDirectory=%~dp0
+SET PowerShellScriptPath=%ThisScriptsDirectory%RestartYukonServices.ps1
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File """"%PowerShellScriptPath%"""" -Operation """"start"""" ' -Verb RunAs}";
