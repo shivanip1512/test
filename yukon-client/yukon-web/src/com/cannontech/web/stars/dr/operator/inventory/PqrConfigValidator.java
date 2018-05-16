01c9748aebcde499ca219ca0d0e09c8bf87c756f
Johnston, Samuel T <SamuelTJohnston@Eaton.com>
2018-05-16 15:34:00 +0000

    YUK-18296 GridBallast - PQR Config page, field value error message not descriptive of issue  - Added a more descriptive error message for too-large values in short fields.  - Modified the PqrConfigValidator to not display additional errors on any short field that already has an error.
    YUK-18295 GridBallast - OV / OF Min Event Duration can be > Max Event Duration
     - Added validation to check that the min event durations are less than their max event duration counterparts.

--- yukon-client/yukon-web/src/com/cannontech/web/stars/dr/operator/inventory/PqrConfigValidator.java
+++ yukon-client/yukon-web/src/com/cannontech/web/stars/dr/operator/inventory/PqrConfigValidator.java
@@ -36,22 +36,22 @@ public class PqrConfigValidator extends SimpleValidator<PqrConfig> {
         if (anyFieldNotNull(lovParameterFields)) {
             if (config.getLovTrigger() == null) {
                 errors.rejectValue("lovTrigger", keyBase + "valueEmpty");
-            } else {
+            } else if (config.getLovTrigger() != null) {
                 YukonValidationUtils.checkIsPositiveDouble(errors, "lovTrigger", config.getLovTrigger());
             }
             if (config.getLovRestore() == null) {
                 errors.rejectValue("lovRestore", keyBase + "valueEmpty");
-            } else {
+            } else if (config.getLovRestore() != null) {
                 YukonValidationUtils.checkIsPositiveDouble(errors, "lovRestore", config.getLovRestore());
             }
-            if (config.getLovTriggerTime() == null) {
+            if (config.getLovTriggerTime() == null && !errors.hasFieldErrors("lovTriggerTime")) {
                 errors.rejectValue("lovTriggerTime", keyBase + "valueEmpty");
-            } else {
+            } else if (config.getLovTriggerTime() != null) {
                 YukonValidationUtils.checkIsPositiveShort(errors, "lovTriggerTime", config.getLovTriggerTime());
             }
-            if (config.getLovRestoreTime() == null) {
+            if (config.getLovRestoreTime() == null && !errors.hasFieldErrors("lovRestoreTime")) {
                 errors.rejectValue("lovRestoreTime", keyBase + "valueEmpty");
-            } else {
+            } else if (config.getLovRestoreTime() != null) {
                 YukonValidationUtils.checkIsPositiveShort(errors, "lovRestoreTime", config.getLovRestoreTime());
             }
         }
@@ -67,16 +67,23 @@ public class PqrConfigValidator extends SimpleValidator<PqrConfig> {
         // All fields must be filled, or none
         // Values must be positive
         if (anyFieldNotNull(config.getLovMinEventDuration(), config.getLovMaxEventDuration())) {
-            if (config.getLovMinEventDuration() == null) {
+            if (config.getLovMinEventDuration() == null && !errors.hasFieldErrors("lovMinEventDuration")) {
                 errors.rejectValue("lovMinEventDuration", keyBase + "valueEmpty");
-            } else {
+            } else if (config.getLovMinEventDuration() != null) {
                 YukonValidationUtils.checkIsPositiveShort(errors, "lovMinEventDuration", config.getLovMinEventDuration());
             }
-            if (config.getLovMaxEventDuration() == null) {
+            if (config.getLovMaxEventDuration() == null && !errors.hasFieldErrors("lovMaxEventDuration")) {
                 errors.rejectValue("lovMaxEventDuration", keyBase + "valueEmpty");
-            } else {
+            } else if (config.getLovMaxEventDuration() != null) {
                 YukonValidationUtils.checkIsPositiveShort(errors, "lovMaxEventDuration", config.getLovMaxEventDuration());
             }
+        }
+        
+        //Min must be < Max
+        if (config.getLovMinEventDuration() != null && config.getLovMaxEventDuration() != null
+                && config.getLovMinEventDuration() >= config.getLovMaxEventDuration()) {
+            
+            errors.rejectValue("lovMinEventDuration", keyBase + "minLtMax");
         }
     }
     
@@ -84,14 +91,14 @@ public class PqrConfigValidator extends SimpleValidator<PqrConfig> {
         // All fields must be filled, or none
         // Values must be positive
         if (anyFieldNotNull(config.getLovStartRandomTime(), config.getLovEndRandomTime())) {
-            if (config.getLovStartRandomTime() == null) {
+            if (config.getLovStartRandomTime() == null && !errors.hasFieldErrors("lovStartRandomTime")) {
                 errors.rejectValue("lovStartRandomTime", keyBase + "valueEmpty");
-            } else {
+            } else if (config.getLovStartRandomTime() != null) {
                 YukonValidationUtils.checkIsPositiveShort(errors, "lovStartRandomTime", config.getLovStartRandomTime());
             }
-            if (config.getLovEndRandomTime() == null) {
+            if (config.getLovEndRandomTime() == null && !errors.hasFieldErrors("lovEndRandomTime")) {
                 errors.rejectValue("lovEndRandomTime", keyBase + "valueEmpty");
-            } else {
+            } else if (config.getLovEndRandomTime() != null) {
                 YukonValidationUtils.checkIsPositiveShort(errors, "lovEndRandomTime", config.getLovEndRandomTime());
             }
         }
@@ -104,24 +111,24 @@ public class PqrConfigValidator extends SimpleValidator<PqrConfig> {
         // All fields must be filled, or none
         // Values must be positive
         if (anyFieldNotNull(lofParameterFields)) {
-            if (config.getLofTrigger() == null) {
+            if (config.getLofTrigger() == null && !errors.hasFieldErrors("lofTrigger")) {
                 errors.rejectValue("lofTrigger", keyBase + "valueEmpty");
-            } else {
+            } else if (config.getLofTrigger() != null) {
                 YukonValidationUtils.checkIsPositiveShort(errors, "lofTrigger", config.getLofTrigger());
             }
-            if (config.getLofRestore() == null) {
+            if (config.getLofRestore() == null && !errors.hasFieldErrors("lofRestore")) {
                 errors.rejectValue("lofRestore", keyBase + "valueEmpty");
-            } else {
+            } else if (config.getLofRestore() != null){
                 YukonValidationUtils.checkIsPositiveShort(errors, "lofRestore", config.getLofRestore());
             }
-            if (config.getLofTriggerTime() == null) {
+            if (config.getLofTriggerTime() == null && !errors.hasFieldErrors("lofTriggerTime")) {
                 errors.rejectValue("lofTriggerTime", keyBase + "valueEmpty");
-            } else {
+            } else if (config.getLofTriggerTime() != null) {
                 YukonValidationUtils.checkIsPositiveShort(errors, "lofTriggerTime", config.getLofTriggerTime());
             }
-            if (config.getLofRestoreTime() == null) {
+            if (config.getLofRestoreTime() == null && !errors.hasFieldErrors("lofRestoreTime")) {
                 errors.rejectValue("lofRestoreTime", keyBase + "valueEmpty");
-            } else {
+            } else if (config.getLofRestoreTime() != null) {
                 YukonValidationUtils.checkIsPositiveShort(errors, "lofRestoreTime", config.getLofRestoreTime());
             }
         }
@@ -138,16 +145,23 @@ public class PqrConfigValidator extends SimpleValidator<PqrConfig> {
         // All fields must be filled, or none
         // Values must be positive
         if (anyFieldNotNull(config.getLofMinEventDuration(), config.getLofMaxEventDuration())) {
-            if (config.getLofMinEventDuration() == null) {
+            if (config.getLofMinEventDuration() == null && !errors.hasFieldErrors("lofMinEventDuration")) {
                 errors.rejectValue("lofMinEventDuration", keyBase + "valueEmpty");
-            } else {
+            } else if (config.getLofMinEventDuration() != null) {
                 YukonValidationUtils.checkIsPositiveShort(errors, "lofMinEventDuration", config.getLofMinEventDuration());
             }
-            if (config.getLofMaxEventDuration() == null) {
+            if (config.getLofMaxEventDuration() == null && !errors.hasFieldErrors("lofMaxEventDuration")) {
                 errors.rejectValue("lofMaxEventDuration", keyBase + "valueEmpty");
-            } else {
+            } else if (config.getLofMaxEventDuration() != null) {
                 YukonValidationUtils.checkIsPositiveShort(errors, "lofMaxEventDuration", config.getLofMaxEventDuration());
             }
+        }
+        
+        //Min must be < Max
+        if (config.getLofMinEventDuration() != null && config.getLofMaxEventDuration() != null
+                && config.getLofMinEventDuration() >= config.getLofMaxEventDuration()) {
+            
+            errors.rejectValue("lofMinEventDuration", keyBase + "minLtMax");
         }
     }
     
@@ -155,14 +169,14 @@ public class PqrConfigValidator extends SimpleValidator<PqrConfig> {
         // All fields must be filled, or none
         // Values must be positive
         if (anyFieldNotNull(config.getLofStartRandomTime(), config.getLofEndRandomTime())) {
-            if (config.getLofStartRandomTime() == null) {
+            if (config.getLofStartRandomTime() == null && !errors.hasFieldErrors("lofStartRandomTime")) {
                 errors.rejectValue("lofStartRandomTime", keyBase + "valueEmpty");
-            } else {
+            } else if (config.getLofStartRandomTime() != null) {
                 YukonValidationUtils.checkIsPositiveShort(errors, "lofStartRandomTime", config.getLofStartRandomTime());
             }
-            if (config.getLofEndRandomTime() == null) {
+            if (config.getLofEndRandomTime() == null && !errors.hasFieldErrors("lofEndRandomTime")) {
                 errors.rejectValue("lofEndRandomTime", keyBase + "valueEmpty");
-            } else {
+            } else if (config.getLofEndRandomTime() != null) {
                 YukonValidationUtils.checkIsPositiveShort(errors, "lofEndRandomTime", config.getLofEndRandomTime());
             }
         }
