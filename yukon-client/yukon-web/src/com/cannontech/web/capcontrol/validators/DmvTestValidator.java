70e41a1fd9787d50d507e29e2edd332d01a8aa94
Rutkowski, Daniel T <DanielTRutkowski@Eaton.com>
2018-05-30 15:23:35 +0000

    YUK-18127 DMV Test Doesn't Log Data At the 1 Second Rate YUKRV-12275 Changed the minimum polling Interval requirement to 15 seconds

--- yukon-client/yukon-web/src/com/cannontech/web/capcontrol/validators/DmvTestValidator.java
+++ yukon-client/yukon-web/src/com/cannontech/web/capcontrol/validators/DmvTestValidator.java
@@ -24,7 +24,7 @@ public class DmvTestValidator extends SimpleValidator<DmvTest>{
         
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "yukon.web.error.isBlank");   
         YukonValidationUtils.checkExceedsMaxLength(errors, "name", target.getName(), 100);
-        YukonValidationUtils.checkRange(errors, "pollingInterval", target.getPollingInterval(), 1, 300, true);
+        YukonValidationUtils.checkRange(errors, "pollingInterval", target.getPollingInterval(), 15, 300, true);
         YukonValidationUtils.checkRange(errors, "dataGatheringDuration", target.getDataGatheringDuration(), 1, 30, true);
         YukonValidationUtils.checkRange(errors, "stepSize", target.getStepSize(), 0.75, 5.0, true);
         YukonValidationUtils.checkRange(errors, "commSuccPercentage", target.getCommSuccPercentage(), 0, 100, true);
