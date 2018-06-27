47aaf7c1bc6cefd17ff609fc391e569e614ca9e5
Rawat, Payal <PayalRawat@Eaton.com>
2018-06-27 06:38:17 +0000

    YUK-18422: Check status of Network Manager Fixed review comment

--- yukon-client/yukon-watchdog-service/src/com/cannontech/watchdog/WatchdogService.java
+++ yukon-client/yukon-watchdog-service/src/com/cannontech/watchdog/WatchdogService.java
@@ -6,8 +6,6 @@ import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.logging.log4j.Logger;
-import org.joda.time.DateTime;
-import org.joda.time.Days;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
@@ -19,16 +17,12 @@ import com.cannontech.spring.YukonSpringHook;
 import com.cannontech.watchdog.base.Watchdog;
 
 public class WatchdogService {
-
-    private static Logger log = YukonLogManager.getLogger(WatchdogService.class);
-    
     private List<Watchdog> watchdog;
     private static ScheduledFuture<?> schdfuture;
-    private int noOfDaysToAttemptStart = 5;
     @Autowired private @Qualifier("main") ThreadCachingScheduledExecutorService executor;
     
     public static void main(String args[]) {
-
+        Logger log = YukonLogManager.getLogger(WatchdogService.class);
         CtiUtilities.setClientAppName(ApplicationId.WATCHDOG);
         YukonSpringHook.setDefaultContext(YukonSpringHook.WATCHDOG_BEAN_FACTORY_KEY);
         try {
@@ -44,18 +38,13 @@ public class WatchdogService {
     
     /*
      * This method checks and starts watchdogs, if the watchdog should to be started.
-     * It attempts to start watchdog daily once for noOfDaysToAttemptStart days.
-     * If it cannot be started in noOfDaysToAttemptStart day then it will not be started until watchdog service starts again.
+     * It attempts to start watchdog daily until all watchdogs have started.
      */
     private synchronized void start() throws Exception {
-        DateTime startTime = DateTime.now();
-
         schdfuture = executor.scheduleAtFixedRate(() -> {
             List<Watchdog> startedWatchdogs = new ArrayList<Watchdog>();
             // Cancel the daily scheduler if there are no more watchdog service to start 
-            // or it has tried  for noOfDaysToAttemptStart days to start a watchdog
-            if (watchdog.isEmpty() || (Days.daysBetween(startTime, DateTime.now()).getDays() >= noOfDaysToAttemptStart)) {
-                log.debug("Have started all service or have crossed limit of number of days to start service. Stopping scheduler");
+            if (watchdog.isEmpty()) {
                 schdfuture.cancel(true);
             }
             watchdog.forEach(watchdog -> {
