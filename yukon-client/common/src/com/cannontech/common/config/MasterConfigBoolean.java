9f1cfb16f32e979bdc63c6a37b5696638a19a4d5
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2018-08-21 20:32:40 +0000

    YUK-18496 Spelling Errors in the Logs  - reverted unintended commit

--- yukon-client/common/src/com/cannontech/common/config/MasterConfigBoolean.java
+++ yukon-client/common/src/com/cannontech/common/config/MasterConfigBoolean.java
@@ -39,7 +39,6 @@ public enum MasterConfigBoolean {
     USE_OLD_FORGOTTEN_PASSWORD_PAGE,
     USER_FEEDBACK_ENABLED,
     VIRTUAL_PROGRAMS,
-//    JOB_MANAGER_DISABLED, // need to move out of JobManager class
     WATCHDOG_WEB_SERVER_HTTP_CHECK_ENABLED
     ;
     
