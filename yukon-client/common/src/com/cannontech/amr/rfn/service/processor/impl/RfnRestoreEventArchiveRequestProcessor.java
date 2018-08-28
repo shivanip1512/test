d114454792ff714048e3194fee96949e53e7e490
Fisher, Matthew S <MatthewSFisher@Eaton.com>
2018-08-28 19:42:06 +0000

    YUK-18706 Outage Missing Count event Made COUNT optional for outage/restore alarms, similar to EVENT_START_TIME

--- yukon-client/common/src/com/cannontech/amr/rfn/service/processor/impl/RfnRestoreEventArchiveRequestProcessor.java
+++ yukon-client/common/src/com/cannontech/amr/rfn/service/processor/impl/RfnRestoreEventArchiveRequestProcessor.java
@@ -2,13 +2,16 @@ package com.cannontech.amr.rfn.service.processor.impl;
 
 import java.util.List;
 
+import org.apache.logging.log4j.Logger;
 import org.joda.time.Instant;
 
+import com.cannontech.amr.rfn.message.alarm.RfnAlarm;
 import com.cannontech.amr.rfn.message.event.RfnConditionDataType;
 import com.cannontech.amr.rfn.message.event.RfnConditionType;
 import com.cannontech.amr.rfn.message.event.RfnEvent;
 import com.cannontech.amr.rfn.service.processor.RfnArchiveRequestProcessor;
 import com.cannontech.amr.rfn.service.processor.RfnEventConditionDataProcessorHelper;
+import com.cannontech.clientutils.YukonLogManager;
 import com.cannontech.common.pao.attribute.model.BuiltInAttribute;
 import com.cannontech.common.point.PointQuality;
 import com.cannontech.common.rfn.model.InvalidEventMessageException;
@@ -19,6 +22,8 @@ import com.cannontech.message.dispatch.message.PointData;
 public class RfnRestoreEventArchiveRequestProcessor extends RfnEventConditionDataProcessorHelper
         implements RfnArchiveRequestProcessor {
     
+    private final static Logger log = YukonLogManager.getLogger(RfnRestoreEventArchiveRequestProcessor.class);
+
     @Override
     public void process(RfnDevice device, RfnEvent event, List<? super PointData> pointDatas, Instant now) {
         
@@ -42,18 +47,27 @@ public class RfnRestoreEventArchiveRequestProcessor extends RfnEventConditionDat
             } catch (InvalidEventMessageException e) {
                 // Old firmware and "compact aggregated restoration alarms" don't include the EVENT_START_TIME 
                 // meta-data, so don't create the outage log if we get here
+                log.trace(device + " restore received with no EVENT_START_TIME, not creating OUTAGE_LOG entry");
             }
         }
         
         rfnMeterEventService.processAttributePointData(device, pointDatas, BuiltInAttribute.OUTAGE_STATUS, eventInstant, OutageStatus.GOOD.getRawState(), pointQuality, now);
         
-        rfnMeterEventService.processAttributePointData(device, 
-                                                       pointDatas, 
-                                                       BuiltInAttribute.RFN_OUTAGE_RESTORE_COUNT, 
-                                                       eventInstant, 
-                                                       getLongEventData(event, RfnConditionDataType.COUNT), 
-                                                       pointQuality, 
-                                                       now);
+        try {
+            rfnMeterEventService.processAttributePointData(device, 
+                                                           pointDatas, 
+                                                           BuiltInAttribute.RFN_OUTAGE_RESTORE_COUNT, 
+                                                           eventInstant, 
+                                                           getLongEventData(event, RfnConditionDataType.COUNT), 
+                                                           pointQuality, 
+                                                           now);
+        } catch (InvalidEventMessageException ex) {
+            if (event instanceof RfnAlarm) {
+                log.trace(device + " restore alarm received with no COUNT, not sending RFN_OUTAGE_RESTORE_COUNT update");
+            } else {
+                throw ex;
+            }
+        }
     }
     
     @Override
