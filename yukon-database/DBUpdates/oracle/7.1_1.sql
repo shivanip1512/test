7ad033999b2345689cc137c25cf3cca3e66d6f58
Shikha, Shikha <ShikhaShikha@Eaton.com>
2018-08-29 11:48:56 +0000

    YUK-18744 : Update old enum values to the new one inside SmartNotificationEventParam for watchdog events

--- yukon-database/DBUpdates/oracle/7.1_1.sql
+++ yukon-database/DBUpdates/oracle/7.1_1.sql
@@ -2,19 +2,6 @@
 /****     Oracle DBupdates             ****/ 
 /******************************************/ 
 
-/* @start YUK-18039 */
-ALTER TABLE InfrastructureWarnings
-ADD Timestamp DATE;
-
-UPDATE InfrastructureWarnings
-SET Timestamp = '01-JAN-1990';
-
-ALTER TABLE InfrastructureWarnings
-MODIFY (Timestamp DATE NOT NULL);
-
-INSERT INTO DBUpdates VALUES ('YUK-18039', '7.2.0', SYSDATE);
-/* @end YUK-18039 */
-
 /* @start YUK-18744 */
 UPDATE SmartNotificationEventParam
 SET Name = 'Status', Value = 'STOPPED'
@@ -85,11 +72,11 @@ SET Value = 'YUKON_NETWORK_MANAGER'
 WHERE Value = 'NETWORK_MANAGER_STATUS'
 AND Name = 'WarningType';
 
-INSERT INTO DBUpdates VALUES ('YUK-18744', '7.2.0', SYSDATE);
+INSERT INTO DBUpdates VALUES ('YUK-18744', '7.1.1', SYSDATE);
 /* @end YUK-18744 */
 
 /**************************************************************/
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
 /**************************************************************/
-/*INSERT INTO CTIDatabase VALUES ('7.2', '13-AUG-2018', 'Latest Update', 0, SYSDATE);*/
+/*INSERT INTO CTIDatabase VALUES ('7.1', '29-AUG-2018', 'Latest Update', 1, SYSDATE);*/
