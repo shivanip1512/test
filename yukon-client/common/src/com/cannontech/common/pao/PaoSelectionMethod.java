9a7026b97fc5a88a46736d6bbfcf856226bfd9cd
Tongaonkar, Prachi S <PrachiSTongaonkar@Eaton.com>
2018-08-31 12:51:33 +0000

    YUK-18723 - Add "All" option to Notes Search device selection Removed the use of DisplableEnum

--- yukon-client/common/src/com/cannontech/common/pao/PaoSelectionMethod.java
+++ yukon-client/common/src/com/cannontech/common/pao/PaoSelectionMethod.java
@@ -1,15 +1,9 @@
 package com.cannontech.common.pao;
 
-import com.cannontech.common.i18n.DisplayableEnum;
-
-public enum PaoSelectionMethod implements DisplayableEnum {
+public enum PaoSelectionMethod {
 
     allDevices,
     selectIndividually,
     byDeviceGroups;
 
-    @Override
-    public String getFormatKey() {
-        return "yukon.web.common.paoNoteSearch." + name();
-    }
 }
