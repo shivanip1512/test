20e8585d6eb417d278b1efde33db8f19e5f243c2
Rawat, Payal <PayalRawat@Eaton.com>
2018-09-06 08:00:12 +0000

    YUK-18765: DBUpdater will not exit causing deploys to time out

--- yukon-client/common/src/com/cannontech/clientutils/YukonRollingFileAppender.java
+++ yukon-client/common/src/com/cannontech/clientutils/YukonRollingFileAppender.java
@@ -12,6 +12,9 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.zip.Deflater;
 
 import org.apache.logging.log4j.LogManager;
@@ -113,7 +116,7 @@ public class YukonRollingFileAppender extends AbstractOutputStreamAppender<Rolli
      * Name of the current logging file.
      */
     private String fileName;
-  //  private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    private ScheduledExecutorService scheduler;
 
     public YukonRollingFileAppender(final String name, final Filter filter, final Layout<? extends Serializable> layout,
             String fileName, String pattern, TriggeringPolicy policy, RolloverStrategy strategy, String applicationName,
@@ -269,26 +272,23 @@ public class YukonRollingFileAppender extends AbstractOutputStreamAppender<Rolli
      * Rename .log.zip files to .zip format in the log directory after 5 minutes. 
      */
     private void renameZippedFiles() {
-        Thread renameThread = new Thread("renameThread") {
-            @Override
-            public void run() {
-                try {
-                    // Sleep for 5 min
-                    Thread.sleep(300000);
-                    File currentDirectory = new File(directory);
-                    File[] filesForRename = currentDirectory.listFiles(new LogFilesToRenameFilter());
-                    for (File file : filesForRename) {
-                        String[] output = file.getAbsolutePath().split("\\.");
-                        file.renameTo(new File(output[0] + ".zip"));
-                    }
-                } catch (InterruptedException e) {
-                    return;
-                }
+        scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.schedule(() -> {
+            File currentDirectory = new File(directory);
+            File[] filesForRename = currentDirectory.listFiles(new LogFilesToRenameFilter());
+            for (File file : filesForRename) {
+                String[] output = file.getAbsolutePath().split("\\.");
+                file.renameTo(new File(output[0] + ".zip"));
             }
-        };
-        renameThread.start();
+            stopScheduler(); 
+        }, 5, TimeUnit.MINUTES);
+          
     }
 
+    private void stopScheduler() {
+        scheduler.shutdownNow();
+    }
+    
     private void setStartDate() {
         Date currentDate = new Date();
         SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
