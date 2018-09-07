a2b4d677018aa6853e1397d96940f8e6ea3592fc
Beckius, Aaron T <AaronTBeckius@Eaton.com>
2018-09-07 14:38:57 +0000

    NO YUK ISSUE 7.1.1 pre-build updates.  - Readying DB update files for 7.1.1

---
+++ yukon-database/DBUpdates/oracle/7.1_1.sql
@@ -0,0 +1,82 @@
+/******************************************/ 
+/****     Oracle DBupdates             ****/ 
+/******************************************/ 
+
+/* @start YUK-18744 */
+UPDATE SmartNotificationEventParam
+SET Name = 'Status', Value = 'STOPPED'
+WHERE Name = 'Argument0';
+
+UPDATE SmartNotificationEventParam
+SET Value = 'YUKON_WEB_APPLICATION_SERVICE'
+WHERE Value = 'WEB_SERVER_SERVICE_STATUS'
+AND Name = 'WarningType';
+
+UPDATE SmartNotificationEventParam
+SET Value = 'YUKON_SERVICE_MANAGER'
+WHERE Value = 'SERVICE_MANAGER_SERVICE_STATUS'
+AND Name = 'WarningType';
+
+UPDATE SmartNotificationEventParam
+SET Value = 'YUKON_NOTIFICATION_SERVER'
+WHERE Value = 'NOTIFICATION_SERVER_SERVICE_STATUS'
+AND Name = 'WarningType';
+
+UPDATE SmartNotificationEventParam
+SET Value = 'YUKON_MESSAGE_BROKER'
+WHERE Value = 'YUKON_MESSAGE_BROKER_SERVICE_STATUS'
+AND Name = 'WarningType';
+
+UPDATE SmartNotificationEventParam
+SET Value = 'YUKON_DISPATCH_SERVICE'
+WHERE Value = 'DISPATCH_SERVICE_STATUS'
+AND Name = 'WarningType';
+
+UPDATE SmartNotificationEventParam
+SET Value = 'YUKON_CAP_CONTROL_SERVICE'
+WHERE Value = 'CAPCONTROL_SERVICE_STATUS'
+AND Name = 'WarningType';
+
+UPDATE SmartNotificationEventParam
+SET Value = 'YUKON_LOAD_MANAGEMENT_SERVICE'
+WHERE Value = 'LOADMANAGEMENT_SERVICE_STATUS'
+AND Name = 'WarningType';
+
+UPDATE SmartNotificationEventParam
+SET Value = 'YUKON_PORT_CONTROL_SERVICE'
+WHERE Value = 'PORTER_SERVICE_STATUS'
+AND Name = 'WarningType';
+
+UPDATE SmartNotificationEventParam
+SET Value = 'YUKON_MAC_SCHEDULER_SERVICE'
+WHERE Value = 'MACS_SERVICE_STATUS'
+AND Name = 'WarningType';
+
+UPDATE SmartNotificationEventParam
+SET Value = 'YUKON_FOREIGN_DATA_SERVICE'
+WHERE Value = 'FDR_SERVICE_STATUS'
+AND Name = 'WarningType';
+
+UPDATE SmartNotificationEventParam
+SET Value = 'YUKON_CALC_LOGIC_SERVICE'
+WHERE Value = 'CALC_SERVICE_STATUS'
+AND Name = 'WarningType';
+
+UPDATE SmartNotificationEventParam
+SET Value = 'YUKON_REAL_TIME_SCAN_SERVICE'
+WHERE Value = 'SCANNER_SERVICE_STATUS'
+AND Name = 'WarningType';
+
+UPDATE SmartNotificationEventParam
+SET Value = 'YUKON_NETWORK_MANAGER'
+WHERE Value = 'NETWORK_MANAGER_STATUS'
+AND Name = 'WarningType';
+
+INSERT INTO DBUpdates VALUES ('YUK-18744', '7.1.1', SYSDATE);
+/* @end YUK-18744 */
+
+/**************************************************************/
+/* VERSION INFO                                               */
+/* Inserted when update script is run                         */
+/**************************************************************/
+INSERT INTO CTIDatabase VALUES ('7.1', '07-SEP-2018', 'Latest Update', 1, SYSDATE);
