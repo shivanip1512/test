c914c3885ae2d23a34b7f4bfdfbf448c37b99db9
Major, Michael <>
2018-09-28 13:21:36 +0000

    YUK-18785 Hide notes icon after deleting all notes in notes popup YUKRV-12640
    Updated javascript for the notes popup to use !exists() instead of checking length manually for determining whether all notes were deleted or not.

--- yukon-client/yukon-web/WebContent/resources/js/pages/yukon.tools.paonotespopup.js
+++ yukon-client/yukon-web/WebContent/resources/js/pages/yukon.tools.paonotespopup.js
@@ -153,7 +153,7 @@ yukon.tools.paonotespopup = (function () {
                 
                 // If all notes were deleted for this device, hide all of the note icons for that device
                 var paoId = $(this).find('#create-popup-note-form #paoId').val();
-                if ($(this).find('tr[id^="js-popup-note-row-"]').length < 1) {
+                if (!$(this).find('tr[id^="js-popup-note-row-"]').exists()) {
                     $('.js-view-all-notes').each(function(index, noteIcon) {
                         if($(noteIcon).data('pao-id') == paoId) {
                             $(noteIcon).hide();
