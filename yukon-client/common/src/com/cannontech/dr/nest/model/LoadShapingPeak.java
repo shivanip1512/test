9e61d95b4471528484ba2b5404a68f8f0fc2e06e
Rawat, Payal <PayalRawat@Eaton.com>
2018-10-05 15:21:25 +0000

    YUK-18900: Nest Simulator - send control request to Nest and cancel control

--- yukon-client/common/src/com/cannontech/dr/nest/model/LoadShapingPeak.java
+++ yukon-client/common/src/com/cannontech/dr/nest/model/LoadShapingPeak.java
@@ -1,16 +1,24 @@
 package com.cannontech.dr.nest.model;
 
+import com.cannontech.common.i18n.DisplayableEnum;
 import com.cannontech.common.util.DatabaseRepresentationSource;
 
-public enum LoadShapingPeak implements DatabaseRepresentationSource {
+public enum LoadShapingPeak implements DatabaseRepresentationSource, DisplayableEnum {
    //The peak period load shaping.
     STANDARD, 
     UNIFORM, 
     SYMMETRIC;
     
+    private static String baseKey = "yukon.web.modules.dr.nest.loadShapingType.";
+    
     @Override
     public Object getDatabaseRepresentation() {
         return name();
+    }
+    
+    @Override
+    public String getFormatKey() {
+        return baseKey + name();
     }
     
     static public LoadShapingPeak getLoadShapingPeak(Object value) {
--- yukon-client/yukon-web/src/com/cannontech/web/dev/NestTestController.java
+++ yukon-client/yukon-web/src/com/cannontech/web/dev/NestTestController.java
@@ -1,14 +1,23 @@
 package com.cannontech.web.dev;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.logging.log4j.Logger;
+import org.joda.time.Duration;
+import org.joda.time.Instant;
+import org.joda.time.Period;
 import org.jsoup.helper.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,7 +26,17 @@ import com.cannontech.clientutils.YukonLogManager;
 import com.cannontech.common.config.MasterConfigBoolean;
 import com.cannontech.common.i18n.MessageSourceAccessor;
 import com.cannontech.common.util.StringUtils;
+import com.cannontech.database.db.device.lm.GearControlMethod;
+import com.cannontech.development.model.NestControlEventSimulatorParameters;
+import com.cannontech.dr.nest.model.CriticalEvent;
+import com.cannontech.dr.nest.model.LoadShaping;
+import com.cannontech.dr.nest.model.LoadShapingPeak;
+import com.cannontech.dr.nest.model.LoadShapingPost;
+import com.cannontech.dr.nest.model.LoadShapingPreparation;
+import com.cannontech.dr.nest.model.NestError;
+import com.cannontech.dr.nest.model.NestException;
 import com.cannontech.dr.nest.model.NestExisting;
+import com.cannontech.dr.nest.model.StandardEvent;
 import com.cannontech.dr.nest.service.NestCommunicationService;
 import com.cannontech.dr.nest.service.NestSimulatorService;
 import com.cannontech.dr.nest.service.impl.NestSimulatorServiceImpl;
@@ -28,6 +47,8 @@ import com.cannontech.simulators.dao.YukonSimulatorSettingsKey;
 import com.cannontech.user.YukonUserContext;
 import com.cannontech.web.common.flashScope.FlashScope;
 import com.cannontech.web.dr.model.NestFileGenerationSetting;
+import com.cannontech.web.input.DatePropertyEditorFactory;
+import com.cannontech.web.input.DatePropertyEditorFactory.BlankMode;
 import com.cannontech.web.security.annotation.CheckCparm;
 
 @Controller
@@ -40,35 +61,59 @@ public class NestTestController {
     @Autowired NestCommunicationService nestComm;
     @Autowired NestSyncServiceImpl nestSync; 
     @Autowired private YukonUserContextMessageSourceResolver messageResolver;
+    @Autowired private DatePropertyEditorFactory datePropertyEditorFactory;
+    
+    @InitBinder
+    public void initBinder(WebDataBinder binder, final YukonUserContext userContext) {
+        datePropertyEditorFactory.setupDateTimePropertyEditor(binder, userContext, BlankMode.CURRENT);
+    }
 
     @RequestMapping(value = "home", method = RequestMethod.GET)
     public String home(ModelMap model, YukonUserContext userContext) {
-        String defaultFileName = nestService.getFileName(YukonSimulatorSettingsKey.NEST_FILE_NAME);
-        model.addAttribute("defaultFileName", defaultFileName);
-        
-        MessageSourceAccessor accessor = messageResolver.getMessageSourceAccessor(userContext);
-        model.addAttribute("helpTextForExistingFile", accessor.getMessage("yukon.web.modules.dev.nest.useNestFile.helpText",
-            NestSimulatorServiceImpl.SIMULATED_FILE_PATH));
-        
         return "nest/home.jsp";
     }
+    
+    @RequestMapping(value = "viewNestFileSetting", method = RequestMethod.GET)
+    public String fileSettings(ModelMap model, YukonUserContext userContext) {
+        String defaultFileName = nestService.getFileName(YukonSimulatorSettingsKey.NEST_FILE_NAME);
+        model.addAttribute("defaultFileName", defaultFileName);
 
+        MessageSourceAccessor accessor = messageResolver.getMessageSourceAccessor(userContext);
+        model.addAttribute("helpTextForExistingFile", accessor.getMessage(
+            "yukon.web.modules.dev.nest.useNestFile.helpText", NestSimulatorServiceImpl.SIMULATED_FILE_PATH));
+        return "nest/nestFileSettings.jsp";
+    }
+    
+    
+    @RequestMapping(value = "viewNestSync", method = RequestMethod.GET)
+    public String nestSync() {
+        return "nest/nestSync.jsp";
+    }
+
+    @RequestMapping(value = "viewControlEvents", method = RequestMethod.GET)
+    public String viewControlEvents(ModelMap model, YukonUserContext userContext) {
+        setupModelMap(model);
+        NestControlEventSimulatorParameters nestParameters = new NestControlEventSimulatorParameters();
+        model.addAttribute("nestParameters", nestParameters);
+        return "nest/nestControlEvents.jsp";
+    }
+    
     @RequestMapping(value = "/useAsNestFile", method = RequestMethod.POST)
     public String useAsNestFile(String fileName, FlashScope flash) {
         if (StringUtil.isBlank(fileName)) {
             flash.setError(new YukonMessageSourceResolvable("yukon.web.modules.dev.nest.useNestFile.blankFileName"));
-            return "redirect:home";
+            return "redirect:viewNestFileSetting";
         } else {
             File file = new File(NestSimulatorServiceImpl.SIMULATED_FILE_PATH, fileName);
             if (!file.exists()) {
                 flash.setError(new YukonMessageSourceResolvable("yukon.web.modules.dev.nest.useNestFile.fileNotExists",
                     NestSimulatorServiceImpl.SIMULATED_FILE_PATH));
-                return "redirect:home";
+                return "redirect:viewNestFileSetting";
             }
         }
         nestService.saveFileName(fileName);
         flash.setConfirm(new YukonMessageSourceResolvable("yukon.web.modules.dev.nest.useNestFile.savedSuccessfully"));
-        return "redirect:home";
+        return "redirect:viewNestFileSetting";
     }
 
     @RequestMapping(value = "/generateFile", method = RequestMethod.POST)
@@ -79,13 +124,13 @@ public class NestTestController {
         if (groupNames.size() > 3 || groupNames.size() < 1) {
             flash.setError(
                 new YukonMessageSourceResolvable("yukon.web.modules.dev.nest.nestFileGenerator.groupSizeError"));
-            return "redirect:home";
+            return "redirect:viewNestFileSetting";
         }
         
         if (settings.getNoOfRows() == null || settings.getNoOfThermostats() == null) {
             flash.setError(
                 new YukonMessageSourceResolvable("yukon.web.modules.dev.nest.nestFileGenerator.rowsThermostatError"));
-            return "redirect:home";
+            return "redirect:viewNestFileSetting";
         }
         
         String generatedFileName = nestService.generateExistingFile(groupNames, settings.getNoOfRows(),
@@ -97,14 +142,14 @@ public class NestTestController {
 
         flash.setConfirm(
             new YukonMessageSourceResolvable("yukon.web.modules.dev.nest.nestFileGenerator.fileGenerationSuccessful"));
-        return "redirect:home";
+        return "redirect:fileSettings";
     }
     
     @RequestMapping(value = "/syncYukonAndNest", method = RequestMethod.GET)
     public String sync(FlashScope flash) {
         nestSync.sync(true);
         flash.setConfirm(new YukonMessageSourceResolvable("yukon.web.modules.dev.nest.sync.success"));
-        return "redirect:home";
+        return "redirect:viewNestSync";
     }
 
     @RequestMapping(value = "/downloadExisting", method = RequestMethod.GET)
@@ -113,6 +158,73 @@ public class NestTestController {
         List<NestExisting> nestExisting = nestComm.downloadExisting();
         log.info("Nest Existing " + nestExisting);
         flash.setConfirm(new YukonMessageSourceResolvable("yukon.web.modules.dev.nest.downloadExisting.success"));
-        return "redirect:home";
+        return "redirect:viewNestSync";
+    }
+    
+    @RequestMapping(value = "/sendEvent", method = RequestMethod.POST)
+    public String sendEvent(@ModelAttribute("nestParameters") NestControlEventSimulatorParameters nestParameters,
+            ModelMap model, BindingResult result, HttpServletResponse resp, FlashScope flash) {
+        if (nestParameters.getGroupName().isEmpty()) {
+            flash.setError(new YukonMessageSourceResolvable("yukon.web.modules.dev.nest.controlEvent.emptyGroup"));
+            return "redirect:viewControlEvents";
+        }
+        if ((nestParameters.getStartTime()).isAfter(nestParameters.getStopTime())) {
+            flash.setError(new YukonMessageSourceResolvable("yukon.web.modules.dev.nest.controlEvent.incorrectDate"));
+            return "redirect:viewControlEvents";
+        }
+        
+        setupModelMap(model);
+        if (result.hasErrors()) {
+            resp.setStatus(HttpStatus.BAD_REQUEST.value());
+            return "redirect:viewControlEvents";
+        }
+        String message = sendEvent(nestParameters);
+        model.addAttribute("message", message);
+        return "nest/nestControlEvents.jsp";
+    }
+    
+    private void setupModelMap(ModelMap model) {
+        model.addAttribute("startTime", Instant.now());
+        model.addAttribute("stopTime", Instant.now().plus(Duration.standardHours(4)));
+        model.addAttribute("loadShapingPreparations", LoadShapingPreparation.values());
+        model.addAttribute("loadShapingPeaks", LoadShapingPeak.values());
+        model.addAttribute("loadShapingPosts", LoadShapingPost.values());
+        
+        List<GearControlMethod> controlMethods = new ArrayList<>();
+        controlMethods.add(GearControlMethod.NestCriticalCycle);
+        controlMethods.add(GearControlMethod.NestStandardCycle);
+        model.addAttribute("controlMethods", controlMethods);
+    }
+
+    // Sends control event to nest
+    private String sendEvent(NestControlEventSimulatorParameters nestParameters) {
+        List<String> groupNames = StringUtils.parseStringsForList(nestParameters.getGroupName(), ",");
+        String period = new Period(nestParameters.getStartTime(), nestParameters.getStopTime()).toString();
+
+        String message = org.apache.commons.lang3.StringUtils.EMPTY;
+        NestError nestError;
+        if (nestParameters.getControlMethod() == GearControlMethod.NestCriticalCycle) {
+            CriticalEvent criticalEvent =
+                new CriticalEvent(nestParameters.getStartTime().toString(), period, groupNames);
+            try {
+                nestError = nestComm.sendCriticalEvent(criticalEvent);
+                message = nestError.getMessage();
+            } catch (NestException e) {
+                message = e.getMessage();
+            }
+
+        } else if (nestParameters.getControlMethod() == GearControlMethod.NestStandardCycle) {
+            LoadShaping loadShaping = new LoadShaping(nestParameters.getLoadShapingPreparation(),
+                nestParameters.getLoadShapingPeak(), nestParameters.getLoadShapingPost());
+            StandardEvent standardEvent =
+                new StandardEvent(nestParameters.getStartTime().toString(), period, groupNames, loadShaping);
+            try {
+                nestError = nestComm.sendStandardEvent(standardEvent);
+                message = nestError.getMessage();
+            } catch (NestException e) {
+                message = e.getMessage();
+            }
+        }
+        return message;
     }
 }
