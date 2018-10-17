a7e13f0c7a6f441a66ebe6e1192f158e2ee04913
Otteson, Jess M <JessMOtteson@Eaton.com>
2018-10-17 21:44:51 +0000

    YUK-18948 Recreate nightly upgrade deploy Adding VM Upgrade Jenkinsfile.

---
+++ yukon-deploy/VMUpgrade.groovy
@@ -0,0 +1,56 @@
+#!groovy
+
+pipeline {
+    agent none
+    options {
+        preserveStashes()
+    }
+    parameters {
+        string(name: 'VM_GUEST_USERID', defaultValue: 'Administrator', description: 'The username. The scripts will automatically try VM_GUEST_ADDRESS+VM_GUEST_USERID as well. In this case the case of VM_GUEST_ADDRESS may matter')
+        string(name: 'VM_GUEST_PASSWORD', defaultValue: 'cti123', description: 'Who should I say hello to?')
+        string(name: 'VM_GUEST_ADDRESS', defaultValue: '', description: 'The name of the server, such as PSPL-SW-NIGHT.EATONEASENG.NET. Case may matter here if the guest userID needs this to be appended to it. See VM_GUEST_USERID.')
+        string(name: 'DATABASE_TYPE', defaultValue: 'sqlserver', description: 'For Oracle, set to oracle or oracle12, all other values use sqlserver')
+    }
+    
+    environment {
+        VM_GUEST_USERID = "${VM_GUEST_USERID}"
+        VM_GUEST_PASSWORD = "${VM_GUEST_PASSWORD}"
+        VM_GUEST_ADDRESS = "${VM_GUEST_ADDRESS}"
+        DATABASE_TYPE = "${DATABASE_TYPE}"
+    }
+
+    stages {
+        stage('upgrade') {
+            agent {
+                label '!master'
+            }
+
+            steps {
+                dir('yukon-deploy') {
+                    deleteDir()
+                }
+                cleanWs()
+                copyArtifacts(projectName: 'Yukon_Trunk');
+                checkout([$class: 'SubversionSCM',
+                    additionalCredentials: [],
+                    excludedCommitMessages: '',
+                    excludedRegions: '',
+                    excludedRevprop: '',
+                    excludedUsers: '',
+                    filterChangelog: false,
+                    ignoreDirPropChanges: false,
+                    includedRegions: '',
+                    locations: [[cancelProcessOnExternalsFail: true,
+                            credentialsId: '705036f1-44aa-43f0-8a78-4949f8bcc072',
+                            depthOption: 'infinity',
+                            ignoreExternalsOption: true,
+                            local: 'yukon-deploy',
+                            remote: 'https://svn.cooperpowereas.net/software/yukon/trunk/yukon-deploy']],
+                    quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
+
+                bat './yukon-deploy/vm-install/go.bat copy_support_files remote_upgrade_powershell verify_webserver verify_eim'
+                
+            }
+        }
+    }
+}
