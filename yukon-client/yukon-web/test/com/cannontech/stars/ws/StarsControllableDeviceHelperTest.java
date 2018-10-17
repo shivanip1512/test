b06dd05cf532238f49cc2c3f15b3808700faf11a
Singh, Manender <ManenderSingh@Eaton.com>
2018-10-17 05:32:12 +0000

    Commenting the Junit test case to build Successfully. isOperationAllowedForDevice method is no more inside StarsControllableDeviceHelperImpl class.

--- yukon-client/yukon-web/test/com/cannontech/stars/ws/StarsControllableDeviceHelperTest.java
+++ yukon-client/yukon-web/test/com/cannontech/stars/ws/StarsControllableDeviceHelperTest.java
@@ -1,26 +1,22 @@
 package com.cannontech.stars.ws;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
-import org.springframework.test.util.ReflectionTestUtils;
 
-import com.cannontech.common.inventory.HardwareType;
 
 public class StarsControllableDeviceHelperTest {
     StarsControllableDeviceHelperImpl deviceHelper = new StarsControllableDeviceHelperImpl();
 
     @Test
     public void testIsOperationAllowedForDeviceNest() {
-        boolean isOperationAllowed =
+/*        boolean isOperationAllowed =
             ReflectionTestUtils.invokeMethod(deviceHelper, "isOperationAllowedForDevice", HardwareType.NEST_THERMOSTAT);
-        assertTrue("Is operation allowed", !isOperationAllowed);
+        assertTrue("Is operation allowed", !isOperationAllowed);*/
     }
 
     @Test
     public void testIsOperationAllowedForDeviceNonNest() {
-        boolean isOperationAllowed =
+/*        boolean isOperationAllowed =
             ReflectionTestUtils.invokeMethod(deviceHelper, "isOperationAllowedForDevice", HardwareType.HONEYWELL_9000);
-        assertTrue("Is operation allowed", isOperationAllowed);
+        assertTrue("Is operation allowed", isOperationAllowed);*/
     }
 }
