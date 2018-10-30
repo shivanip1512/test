11e2dd972c3e5c78efa42dc66cb390d2b9386959
Otteson, Jess M <JessMOtteson@Eaton.com>
2018-10-30 20:45:17 +0000

    YUK-18948 Recreate nightly upgrade deploy Removed old attempts for deploy upgrade.

--- yukon-deploy/jenkins/jobs/nightlyDeployTrunk.groovy
+++
@@ -1,5 +0,0 @@
-#!groovy
-upgradeDeploy( vmUser: 'PSPL-SW-NIGHT+Administrator', vmPassword: 'cti123',
-               vmAddress: 'PSPL-SW-NIGHT.eatoneaseng.net', databaseType: 'sqlserver',
-               installerProject: 'Yukon_Trunk',
-               installerSelector: 'lastSuccessful')
