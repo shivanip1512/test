ca262fbdc5ac4526b883147f0a2f6e1507813375
Beckius, Aaron T <AaronTBeckius@Eaton.com>
2018-11-02 18:13:52 +0000

    NO YUK ISSUE 7.1.2 pre-build DB commits  - Setting up 7.1_2.sql for build

--- yukon-database/DBUpdates/sqlserver/7.1_2.sql
+++ yukon-database/DBUpdates/sqlserver/7.1_2.sql
@@ -8,4 +8,4 @@
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
 /**************************************************************/
-/*INSERT INTO CTIDatabase VALUES ('7.1', '07-SEP-2018', 'Latest Update', 2, GETDATE());*/
+INSERT INTO CTIDatabase VALUES ('7.1', '02-NOV-2018', 'Latest Update', 2, GETDATE());
