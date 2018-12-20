e9a4938a096466af0d038fda9241f33a1bd45120
Rawat, Payal <PayalRawat@Eaton.com>
2018-12-20 06:37:37 +0000

    YUK-18966: RFN Performance Broadcast Event Details Page - Backend Changes Pojo for UI

---
+++ yukon-client/common/src/com/cannontech/dr/rfn/model/BroadcastEventDeviceDetails.java
@@ -0,0 +1,55 @@
+package com.cannontech.dr.rfn.model;
+
+import org.joda.time.Instant;
+
+import com.cannontech.dr.model.PerformanceVerificationMessageStatus;
+import com.cannontech.stars.model.LiteLmHardware;
+import com.cannontech.dr.rfn.model.UnknownStatus;
+
+public class BroadcastEventDeviceDetails {
+
+    private PerformanceVerificationMessageStatus messageStatus;
+    private LiteLmHardware hardware;
+    private Instant lastComm;
+    private UnknownStatus deviceStatus;
+    
+    public BroadcastEventDeviceDetails(PerformanceVerificationMessageStatus messageStatus, LiteLmHardware hardware,
+            Instant lastComm, UnknownStatus deviceStatus) {
+        this.messageStatus = messageStatus;
+        this.hardware = hardware;
+        this.lastComm = lastComm;
+        this.deviceStatus = deviceStatus;
+    }
+
+    public PerformanceVerificationMessageStatus getMessageStatus() {
+        return messageStatus;
+    }
+
+    public void setMessageStatus(PerformanceVerificationMessageStatus messageStatus) {
+        this.messageStatus = messageStatus;
+    }
+
+    public LiteLmHardware getHardware() {
+        return hardware;
+    }
+
+    public void setHardware(LiteLmHardware hardware) {
+        this.hardware = hardware;
+    }
+
+    public Instant getLastComm() {
+        return lastComm;
+    }
+
+    public void setLastComm(Instant lastComm) {
+        this.lastComm = lastComm;
+    }
+
+    public DeviceStatus getDeviceStatus() {
+        return deviceStatus;
+    }
+
+    public void setDeviceStatus(DeviceStatus deviceStatus) {
+        this.deviceStatus = deviceStatus;
+    }
+}
