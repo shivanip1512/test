3de33ae23e43f3df19ce86bc1032f515896512fc
Fisher, Matthew S <MatthewSFisher@Eaton.com>
2018-12-20 18:00:18 +0000

    YUK-19212 MACS Script Doesn't Generate Billing File Removed incorrect space after -Xmx parameter

--- yukon-client/billing/src/main/resources/billing.bat
+++ yukon-client/billing/src/main/resources/billing.bat
@@ -1,3 +1,3 @@
 cd /d %YUKON_BASE%\client\bin
 call setjavapath.bat
-java -Xmx 256m -cp %YUKON_BASE%/Client/bin/billing.jar com.cannontech.billing.mainprograms.BillingFile outputfile=billing.csv collectiongroup=Default %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -Xmx256m -cp %YUKON_BASE%/Client/bin/billing.jar com.cannontech.billing.mainprograms.BillingFile outputfile=billing.csv collectiongroup=Default %1 %2 %3 %4 %5 %6 %7 %8 %9
