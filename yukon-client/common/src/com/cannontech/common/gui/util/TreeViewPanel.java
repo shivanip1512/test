91a0b7a4dd1a5000fc28038598c298057ce98703
Satpathy, Samir K <SamirKSatpathy@Eaton.com>
2019-01-04 10:04:24 +0000

    YUK-19301: Java Clients search is broken

--- yukon-client/common/src/com/cannontech/common/gui/util/TreeViewPanel.java
+++ yukon-client/common/src/com/cannontech/common/gui/util/TreeViewPanel.java
@@ -27,7 +27,7 @@ import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreeNode;
 import javax.swing.tree.TreePath;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import com.cannontech.clientutils.CTILogger;
 import com.cannontech.common.device.groups.service.DeviceGroupRenderer;
