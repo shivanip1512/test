a9923777db131b916e183266547c67f8800f6e6a
Shakya, Harsha <HarshaShakya@Eaton.com>
2019-01-08 12:14:37 +0000

    YUK-19239 : Asset Availability detail page - Last Run should be Last Runtime

--- yukon-client/yukon-web/src/com/cannontech/web/dr/assetavailability/AssetAvailabilityController.java
+++ yukon-client/yukon-web/src/com/cannontech/web/dr/assetavailability/AssetAvailabilityController.java
@@ -326,7 +326,7 @@ public class AssetAvailabilityController {
         headerRow[0] = messageSourceAccessor.getMessage(AssetAvailabilitySortBy.SERIAL_NUM);
         headerRow[1] = messageSourceAccessor.getMessage(AssetAvailabilitySortBy.TYPE);
         headerRow[2] = messageSourceAccessor.getMessage(AssetAvailabilitySortBy.LAST_COMM);
-        headerRow[3] = messageSourceAccessor.getMessage(AssetAvailabilitySortBy.LAST_RUN);
+        headerRow[3] = messageSourceAccessor.getMessage(AssetAvailabilitySortBy.LAST_RUNTIME);
         headerRow[4] = messageSourceAccessor.getMessage(baseKey + "AVAILABILITY");
 
         return headerRow;
@@ -369,7 +369,7 @@ public class AssetAvailabilityController {
         SERIAL_NUM, 
         TYPE, 
         LAST_COMM, 
-        LAST_RUN;
+        LAST_RUNTIME;
 
         @Override
         public String getFormatKey() {
