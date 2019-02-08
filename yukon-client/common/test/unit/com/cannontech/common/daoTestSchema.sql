32c64da63b8edde81a7592989abd47b26d7f9556
Shikha, Shikha <ShikhaShikha@Eaton.com>
2019-02-08 07:08:52 +0000

    YUK-19349 : DEA Itineris - UI - Scheduled Data File Import History Pagination & DateTime filter Junit Test cases

--- yukon-client/common/test/unit/com/cannontech/common/daoTestSchema.sql
+++ yukon-client/common/test/unit/com/cannontech/common/daoTestSchema.sql
@@ -225,3 +225,22 @@ create table LMHardwareControlGroup (
    ProgramId            int                  not null,
    constraint PK_LMHARDWARECONTROLGROUP primary key (ControlEntryID)
 );
+
+/*==============================================================*/
+/* Table: ScheduledDataImportHistory                            */
+/*==============================================================*/
+create table ScheduledDataImportHistory (
+   EntryId              numeric              not null,
+   FileName             varchar(100)         not null,
+   FileImportType       varchar(50)          not null,
+   ImportDate           datetime             not null,
+   ArchiveFileName      varchar(100)         not null,
+   ArchiveFilePath      varchar(300)         not null,
+   ArchiveFileExists    char(1)              not null,
+   FailedFileName       varchar(100)         null,
+   FailedFilePath       varchar(300)         null,
+   SuccessCount         numeric              not null,
+   FailureCount         numeric              not null,
+   JobGroupId           int                  not null,
+   constraint PK_ScheduledDataImportHistory primary key (EntryId)
+);
