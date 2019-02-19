9f8346c2feb5e7c63646f3c8ea8bbca3d0b6a7d3
Singh, Manender <ManenderSingh@Eaton.com>
2019-02-19 06:50:45 +0000

    YUK-19061 - DEA Itineris - Create Scheduled Data Import task - fix for large number of files

--- yukon-client/yukon-web/src/com/cannontech/web/dataImport/DataImportHelper.java
+++ yukon-client/yukon-web/src/com/cannontech/web/dataImport/DataImportHelper.java
@@ -1,12 +1,18 @@
 package com.cannontech.web.dataImport;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.Logger;
+
+import com.cannontech.clientutils.YukonLogManager;
 import com.cannontech.common.scheduledFileImport.DataImportWarning;
 
 public class DataImportHelper {
 
+    private static final Logger log = YukonLogManager.getLogger(DataImportHelper.class);
     /**
      * This method converts the import result object to dataimport warning.
      */
@@ -16,10 +22,34 @@ public class DataImportHelper {
         dataImportWarning.setJobGroupId(jobGroupId);
         dataImportWarning.setJobName(jobName);
         dataImportWarning.setImportType(importType);
-        dataImportWarning.setFilesWithError(String.join(",", errorFiles));
+        String fileWithError = String.join(",", errorFiles);
+        if (StringUtils.isNotBlank(fileWithError) && fileWithError.length() > 500) {
+            fileWithError = reduceFileWithError(fileWithError);
+        }
+        dataImportWarning.setFilesWithError(fileWithError);
         dataImportWarning.setSuccessFileCount(successFileCount);
         warning.add(dataImportWarning);
         return warning;
     
     }
+
+    /**
+     * This method is used to reduce the fileWithError string in case we have
+     * large number of files. For fileWithError with more than 500 char
+     * we will reduce it to small size to fit into database column and 
+     * print the additional info inside log file.
+     */
+    private static String reduceFileWithError(String fileWithError) {
+        String limitedString = fileWithError.substring(0, 469);
+        int lastIndex = limitedString.lastIndexOf(",");
+        String reducedFileWithError = fileWithError.substring(0, lastIndex);
+        String additionalFileWithError = fileWithError.substring(lastIndex + 1);
+        List<String> errorFiles = new ArrayList<>(Arrays.asList(additionalFileWithError.split(",")));
+        if (!errorFiles.isEmpty()) {
+            reducedFileWithError = reducedFileWithError.concat(" And " + errorFiles.size() + " additional file.");
+            log.debug("Additional files failed with error " + errorFiles);
+        }
+        return reducedFileWithError;
+    }
+
 }
