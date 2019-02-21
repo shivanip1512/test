a4d1e6030546908d323984196de9013121cda1d7
Singh, Manender <ManenderSingh@Eaton.com>
2019-02-21 05:24:20 +0000

    YUK-19196 - Fix SSL issues caused by Tomcat 9 - Reverting Changes

--- yukon-client/tools/src/main/resources/modifyTomcatConfig.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-SET ThisScriptsDirectory=%~dp0
-SET PowerShellScriptPath=%ThisScriptsDirectory%modifyTomcatConfig.ps1
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File """"%PowerShellScriptPath%"""" -Operation """"restart"""" ' -Verb RunAs -WindowStyle Hidden}";
