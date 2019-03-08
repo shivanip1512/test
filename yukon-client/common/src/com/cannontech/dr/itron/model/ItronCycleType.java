cad3cf7733f891ab5b07bff1fa1707de83839969
Beckius, Aaron T <AaronTBeckius@Eaton.com>
2019-03-08 18:28:40 +0000

    YUK-19514 DEA Itron - Java - Add TrueCycle Itron gear(s)  - Added a missing enum class

---
+++ yukon-client/common/src/com/cannontech/dr/itron/model/ItronCycleType.java
@@ -0,0 +1,27 @@
+package com.cannontech.dr.itron.model;
+
+public enum ItronCycleType {
+    STANDARD("Standard Cycle"),
+    TRUE_CYCLE("True Cycle"),
+    SMART_CYCLE("Smart Cycle");
+    
+    private String name;
+    
+    private ItronCycleType(String name) {
+        this.name = name;
+    }
+    
+    @Override
+    public String toString() {
+        return name;
+    }
+    
+    public static ItronCycleType of(String name) {
+        for(ItronCycleType type : values()) {
+            if (type.name == name) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Name is not a valid ItronCycleType");
+    }
+}
