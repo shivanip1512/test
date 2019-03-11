e910df6742e7780fbe7037add5f0b338acbe8c17
Joshi, Rohit <RohitJoshi2@Eaton.com>
2019-03-11 11:49:22 +0000

    YUK-19570-update ecobee simulator - Data file requests

--- yukon-client/yukon-web/src/com/cannontech/web/dev/EcobeeMockApiController.java
+++ yukon-client/yukon-web/src/com/cannontech/web/dev/EcobeeMockApiController.java
@@ -1,24 +1,36 @@
 package com.cannontech.web.dev;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cannontech.common.config.MasterConfigBoolean;
+import com.cannontech.common.util.JsonUtils;
 import com.cannontech.dr.ecobee.message.AuthenticationRequest;
 import com.cannontech.dr.ecobee.message.AuthenticationResponse;
 import com.cannontech.dr.ecobee.message.BaseResponse;
 import com.cannontech.dr.ecobee.message.DrRequest;
 import com.cannontech.dr.ecobee.message.DrResponse;
+import com.cannontech.dr.ecobee.message.EcobeeJobStatus;
 import com.cannontech.dr.ecobee.message.HierarchyResponse;
 import com.cannontech.dr.ecobee.message.RegisterDeviceRequest;
+import com.cannontech.dr.ecobee.message.RuntimeReportJobRequest;
+import com.cannontech.dr.ecobee.message.RuntimeReportJobResponse;
+import com.cannontech.dr.ecobee.message.RuntimeReportJobStatusRequest;
+import com.cannontech.dr.ecobee.message.RuntimeReportJobStatusResponse;
 import com.cannontech.dr.ecobee.message.SetRequest;
 import com.cannontech.dr.ecobee.message.StandardResponse;
 import com.cannontech.dr.ecobee.message.partial.Status;
+import com.cannontech.dr.ecobee.service.EcobeeStatusCode;
 import com.cannontech.web.security.annotation.CheckCparm;
 import com.cannontech.web.security.annotation.IgnoreCsrfCheck;
 
@@ -81,6 +93,28 @@ public class EcobeeMockApiController {
     public @ResponseBody AuthenticationResponse register(@RequestBody AuthenticationRequest request) {
         Status status = new Status(ecobeeDataConfiguration.getAuthenticate(), "Authenticated!");
         AuthenticationResponse response = new AuthenticationResponse("TK1", status);
+        return response;
+    }
+    
+    @IgnoreCsrfCheck
+    @PostMapping("runtimeReportJob/create")
+    public @ResponseBody RuntimeReportJobResponse createRuntimeReportJob(@RequestBody RuntimeReportJobRequest request) {
+        int code = ecobeeDataConfiguration.getRuntimeReport();
+        RuntimeReportJobResponse response = null;
+        if (code == EcobeeStatusCode.SUCCESS.getCode()) {
+            response = ecobeeMockApiService.createRuntimeReportJob(request);
+        } else {
+            Status status = new Status(code, "Some error has occurred");
+            response = new RuntimeReportJobResponse(null, EcobeeJobStatus.ERROR, status);
+        }
+        return response;
+    }
+    
+    @IgnoreCsrfCheck
+    @GetMapping("runtimeReportJob/status")
+    public @ResponseBody RuntimeReportJobStatusResponse getRuntimeJobStatus(@RequestParam("body") String bodyJson) throws IOException {
+        RuntimeReportJobStatusRequest request = JsonUtils.fromJson(bodyJson, RuntimeReportJobStatusRequest.class);
+        RuntimeReportJobStatusResponse response = ecobeeMockApiService.getRuntimeJobStatus(request.getJobId());
         return response;
     }
 
