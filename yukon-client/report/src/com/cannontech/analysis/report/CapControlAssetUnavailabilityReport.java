581d0a09964f3677d571701c7fb53f7064915d2b
Beckius, Aaron T <AaronTBeckius@Eaton.com>
2019-03-28 19:38:53 +0000

    YUK-19668 Updates to Cap Control Asset Unavailability Report  - Made requested updates to the Cap Control Asset Unavailability Report

--- yukon-client/report/src/com/cannontech/analysis/report/CapControlAssetUnavailabilityReport.java
+++ yukon-client/report/src/com/cannontech/analysis/report/CapControlAssetUnavailabilityReport.java
@@ -9,12 +9,14 @@ import com.cannontech.analysis.tablemodel.CapControlAssetUnavailabilityModel;
 public class CapControlAssetUnavailabilityReport extends SimpleYukonReportBase {
     
     private static final ColumnLayoutData bodyColumns[] = new ColumnLayoutData[] {
-        new ColumnLayoutData("Area", "area", 100),
-        new ColumnLayoutData("Substation", "substation", 100),
-        new ColumnLayoutData("Substation Bus", "subbus", 100),
-        new ColumnLayoutData("Feeder", "feeder", 100),
-        new ColumnLayoutData("Substation Bus Count", "subcount", 100),
-        new ColumnLayoutData("Feeder Count", "feedcount", 100)
+        new ColumnLayoutData("Area", "Area", 100),
+        new ColumnLayoutData("Substation", "Substation", 100),
+        new ColumnLayoutData("Bus", "Bus", 100),
+        new ColumnLayoutData("Feeder", "Feeder", 100),
+        new ColumnLayoutData("Bus Unable \nto Close", "BusUTC", 75),
+        new ColumnLayoutData("Feeder Unable \nto Close", "FeederUTC", 75),
+        new ColumnLayoutData("Bus Unable \nto Open", "BusUTO", 75),
+        new ColumnLayoutData("Feeder Unable \nto Open", "FeederUTO", 75)
     };
 
     public CapControlAssetUnavailabilityReport(BareReportModel bareModel) {
