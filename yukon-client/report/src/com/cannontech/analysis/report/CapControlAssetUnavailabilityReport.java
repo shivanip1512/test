b3be1db42faedc3ef8a8b0b9073690f1365559a7
Beckius, Aaron T <AaronTBeckius@Eaton.com>
2019-04-02 15:58:00 +0000

    YUK-19668 Updates to Cap Control Asset Unavailability Report  - Small error fix

--- yukon-client/report/src/com/cannontech/analysis/report/CapControlAssetUnavailabilityReport.java
+++ yukon-client/report/src/com/cannontech/analysis/report/CapControlAssetUnavailabilityReport.java
@@ -9,14 +9,14 @@ import com.cannontech.analysis.tablemodel.CapControlAssetUnavailabilityModel;
 public class CapControlAssetUnavailabilityReport extends SimpleYukonReportBase {
     
     private static final ColumnLayoutData bodyColumns[] = new ColumnLayoutData[] {
-        new ColumnLayoutData("Area", "Area", 100),
-        new ColumnLayoutData("Substation", "Substation", 100),
-        new ColumnLayoutData("Bus", "Bus", 100),
-        new ColumnLayoutData("Feeder", "Feeder", 100),
-        new ColumnLayoutData("Bus Unable \nto Close", "BusUTC", 75),
-        new ColumnLayoutData("Feeder Unable \nto Close", "FeederUTC", 75),
-        new ColumnLayoutData("Bus Unable \nto Open", "BusUTO", 75),
-        new ColumnLayoutData("Feeder Unable \nto Open", "FeederUTO", 75)
+        new ColumnLayoutData("Area", "area", 100),
+        new ColumnLayoutData("Substation", "substation", 100),
+        new ColumnLayoutData("Bus", "bus", 100),
+        new ColumnLayoutData("Feeder", "feeder", 100),
+        new ColumnLayoutData("Bus Unable \nto Close", "busUtc", 75),
+        new ColumnLayoutData("Feeder Unable \nto Close", "feederUtc", 75),
+        new ColumnLayoutData("Bus Unable \nto Open", "busUto", 75),
+        new ColumnLayoutData("Feeder Unable \nto Open", "feederUto", 75)
     };
 
     public CapControlAssetUnavailabilityReport(BareReportModel bareModel) {
