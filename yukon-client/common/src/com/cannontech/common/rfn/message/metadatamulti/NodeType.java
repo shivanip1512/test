446767743fd3c3b269df9305074fafd2b41ac73e
Feldman, Marina <MarinaFeldman@Eaton.com>
2019-04-15 15:37:31 +0000

    YUK-19793 NM RfnMultiMetadataRequest changes

--- yukon-client/common/src/com/cannontech/common/rfn/message/metadatamulti/NodeType.java
+++ yukon-client/common/src/com/cannontech/common/rfn/message/metadatamulti/NodeType.java
@@ -1,32 +1,12 @@
 package com.cannontech.common.rfn.message.metadatamulti;
 
 public enum NodeType {
-    
-    GATEWAY("Gateway"),
-    
-    ELECTRIC_NODE("Electric node"),
-    
-    GAS_NODE("Gas node"),
-    
-    WATER_NODE("Water node"),
-    
-    LCR_NODE("LCR node"),
-    
-    DA_NODE("DA node"),
-    
-    EKANET_RELAY("EkaNet relay"),
-    
-    UNKNOWN("Unknown node type"),
-    
-    ;
-    
-    private final String nodeTypeString;
-    
-    private NodeType(String nodeTypeString) {
-        this.nodeTypeString = nodeTypeString;
-    }
-
-    public String getNodeTypeString() {
-        return nodeTypeString;
-    }
+    GATEWAY,
+    ELECTRIC_NODE,
+    GAS_NODE,
+    WATER_NODE,
+    LCR_NODE,
+    DA_NODE,
+    EKANET_RELAY,
+    UNKNOWN    
 }
