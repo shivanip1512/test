08af7bf3783a91b4eb2d33f1c3899a22971241a5
Vant, Carrie <carrieVant@Eaton.com>
2019-04-22 15:06:20 +0000

    YUK-19750 - Comprehensive Map UI - initial map and gateway filter   - initial Map Filter object

---
+++ yukon-client/yukon-web/src/com/cannontech/web/tools/mapping/model/ComprehensiveMapFilter.java
@@ -0,0 +1,17 @@
+package com.cannontech.web.tools.mapping.model;
+
+import java.util.List;
+
+public class ComprehensiveMapFilter {
+    
+    private List<Integer> selectedGatewayIds;
+
+    public List<Integer> getSelectedGatewayIds() {
+        return selectedGatewayIds;
+    }
+
+    public void setSelectedGatewayIds(List<Integer> selectedGatewayIds) {
+        this.selectedGatewayIds = selectedGatewayIds;
+    }
+
+}
