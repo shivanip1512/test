309d5cf9e548d6bc9f8c3fab658d165033682b09
Otteson, Jess M <JessMOtteson@Eaton.com>
2019-05-10 15:30:36 +0000

    YUK-19945 LCR-6700s with no load addressing throw an exception when load addressing is reported Added unit tests and refactored for testability. No functional change should have come from this commit.

--- yukon-client/common/test/unit/com/cannontech/dr/rfn/service/RfnLcrTlvDataMappingServiceTest.java
+++ yukon-client/common/test/unit/com/cannontech/dr/rfn/service/RfnLcrTlvDataMappingServiceTest.java
@@ -1,22 +1,29 @@
 package com.cannontech.dr.rfn.service;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.nio.ByteBuffer;
+import java.util.HashSet;
 
 import org.joda.time.DateTime;
+import org.joda.time.Instant;
 import org.junit.Test;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import com.cannontech.dr.dao.ExpressComReportedAddress;
+import com.cannontech.dr.dao.ExpressComReportedAddressRelay;
 import com.cannontech.dr.rfn.model.RfnLcrTlvPointDataType;
 import com.cannontech.dr.rfn.service.impl.RfnLcrTlvDataMappingServiceImpl;
 import com.cannontech.dr.rfn.tlv.FieldType;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.LinkedListMultimap;
 import com.google.common.collect.ListMultimap;
+import com.google.common.collect.Sets;
 
 public class RfnLcrTlvDataMappingServiceTest {
-    private RfnLcrTlvDataMappingServiceImpl service = new RfnLcrTlvDataMappingServiceImpl();;
+    private RfnLcrTlvDataMappingServiceImpl service = new RfnLcrTlvDataMappingServiceImpl();
     
     @Test
     public void test_isValidTimeOfReading_withValidDate() {
@@ -121,6 +128,172 @@ public class RfnLcrTlvDataMappingServiceTest {
         Object result = ReflectionTestUtils.invokeMethod(service, "evaluateArchiveReadValue", data, dataType);
         assertTrue("evaluateArchiveReadValue did not return a double", result instanceof Double);
         assertEquals(new Double(value), result);
+    }
+    
+    /* This tests all of the incoming addressing with and without an existing address.
+     * 
+     * 1. We change every one of the "geo" address levels from a fixed 9 to a new value and verify that value
+     * 2. We add a new relay, and change only part of an existing relay (change splinter and not program)
+     * 3. Test with no existing addressing with and without new relays - YUK-19945
+     * 4. If the reported timestamp is older, nothing should change.
+     */
+    @Test 
+    public void test_generateUpdatedAddressingFromMessage() {
+        // Fixed "now" for the test with milliseconds removed for comparison with device that doesn't support ms
+        Instant now;
+        {
+            Instant instant = Instant.now();
+            now = new Instant(instant.getMillis()-instant.getMillis()%1000);
+        }
+        
+        final int devicePaoId = 1234;
+        
+        ListMultimap<FieldType, byte[]> data  = ArrayListMultimap.create();
+        data.put(FieldType.SPID, getBytes(1));
+        data.put(FieldType.GEO_ADDRESS, getBytes(2));
+        data.put(FieldType.SUBSTATION_ADDRESS, getBytes(3));
+        data.put(FieldType.FEEDER_ADDRESS, getBytes(4));
+        data.put(FieldType.ZIP_ADDRESS, getBytes(5));
+        data.put(FieldType.UDA_ADDRESS, getBytes(6));
+        data.put(FieldType.REQUIRED_ADDRESS, getBytes(7));
+        data.put(FieldType.UTC, getBytes(now.getMillis()/1000)); // Seconds
+        
+        HashSet<ExpressComReportedAddressRelay> relays = Sets.newHashSet();
+        ExpressComReportedAddressRelay newRelay = new ExpressComReportedAddressRelay(1);
+        newRelay.setProgram(1);
+        relays.add(newRelay);
+        newRelay = new ExpressComReportedAddressRelay(2);
+        newRelay.setProgram(2);
+        relays.add(newRelay);
+        newRelay = new ExpressComReportedAddressRelay(3);
+        newRelay.setSplinter(3);
+        relays.add(newRelay);
+        
+        ExpressComReportedAddress currentAddress = new ExpressComReportedAddress();
+        currentAddress.setChangeId(2);
+        currentAddress.setDeviceId(devicePaoId);
+        currentAddress.setFeeder(9);
+        currentAddress.setGeo(9);
+        currentAddress.setRelays(relays);
+        currentAddress.setRequired(9);
+        currentAddress.setSpid(9);
+        currentAddress.setSubstation(9);
+        currentAddress.setTimestamp(now.minus(3000));
+        currentAddress.setUda(9);
+        currentAddress.setZip(9);
+        
+        {
+            // Tests "new" addressing with no currentAddress and no relays
+            Object result = ReflectionTestUtils.invokeMethod(service, "generateUpdatedAddressingFromMessage", data, devicePaoId, null);
+            ExpressComReportedAddress resultAddress = (ExpressComReportedAddress)result;
+            assertEquals(resultAddress.getDeviceId(), devicePaoId);
+            assertEquals(resultAddress.getSpid(), 1);
+            assertEquals(resultAddress.getGeo(), 2);
+            assertEquals(resultAddress.getSubstation(), 3);
+            assertEquals(resultAddress.getFeeder(), 4);
+            assertEquals(resultAddress.getZip(), 5);
+            assertEquals(resultAddress.getUda(), 6);
+            assertEquals(resultAddress.getRequired(), 7);
+            assertEquals(resultAddress.getRelays(), null);
+            assertEquals(resultAddress.getTimestamp(), now);
+        }
+        
+        {
+            // Tests "new" addressing with currentAddress and no reported relays
+            Object result = ReflectionTestUtils.invokeMethod(service, "generateUpdatedAddressingFromMessage", data, devicePaoId, currentAddress);
+            ExpressComReportedAddress resultAddress = (ExpressComReportedAddress)result;
+            assertEquals(resultAddress.getDeviceId(), devicePaoId);
+            assertEquals(resultAddress.getSpid(), 1);
+            assertEquals(resultAddress.getGeo(), 2);
+            assertEquals(resultAddress.getSubstation(), 3);
+            assertEquals(resultAddress.getFeeder(), 4);
+            assertEquals(resultAddress.getZip(), 5);
+            assertEquals(resultAddress.getUda(), 6);
+            assertEquals(resultAddress.getRequired(), 7);
+            assertEquals(resultAddress.getRelays().size(), 3);
+            assertEquals(resultAddress.getTimestamp(), now);
+        }
+        
+        // Adding relays to the incoming data now for the rest of the tests
+        // Relay addresses are 2 bytes, first byte is relay, second is address
+        data.put(FieldType.RELAY_N_SPLINTER_ADDRESS, getBytes((short)0x0101)); // relay 1 splinter 1 
+        data.put(FieldType.RELAY_N_SPLINTER_ADDRESS, getBytes((short)0x0202)); // relay 2 splinter 2 program 3
+        data.put(FieldType.RELAY_N_PROGRAM_ADDRESS, getBytes((short)0x0203)); // relay 2 splinter 2 program 3
+        data.put(FieldType.RELAY_N_PROGRAM_ADDRESS, getBytes((short)0x0303)); // relay 3 program 3
+        data.put(FieldType.RELAY_N_PROGRAM_ADDRESS, getBytes((short)0x0404)); // relay 4 program 4
+        
+/*        {
+            // Tests "new" addressing with relays and no currentAddress (yuk-19945)
+            Object result = ReflectionTestUtils.invokeMethod(service, "generateUpdatedAddressingFromMessage", data, devicePaoId, null);
+            ExpressComReportedAddress resultAddress = (ExpressComReportedAddress)result;
+            System.out.println(resultAddress.getTimestamp());
+            assertEquals(resultAddress.getDeviceId(), devicePaoId);
+            assertEquals(resultAddress.getSpid(), 1);
+            assertEquals(resultAddress.getGeo(), 2);
+            assertEquals(resultAddress.getSubstation(), 3);
+            assertEquals(resultAddress.getFeeder(), 4);
+            assertEquals(resultAddress.getZip(), 5);
+            assertEquals(resultAddress.getUda(), 6);
+            assertEquals(resultAddress.getRequired(), 7);
+            assertEquals(resultAddress.getRelays().size(), 3);
+            assertEquals(resultAddress.getTimestamp(), now);
+        }*/
+
+        
+        {
+            Object result = ReflectionTestUtils.invokeMethod(service, "generateUpdatedAddressingFromMessage", data, devicePaoId, currentAddress);
+            ExpressComReportedAddress resultAddress = (ExpressComReportedAddress)result;
+            assertEquals(resultAddress.getDeviceId(), devicePaoId);
+            assertEquals(resultAddress.getSpid(), 1);
+            assertEquals(resultAddress.getGeo(), 2);
+            assertEquals(resultAddress.getSubstation(), 3);
+            assertEquals(resultAddress.getFeeder(), 4);
+            assertEquals(resultAddress.getZip(), 5);
+            assertEquals(resultAddress.getUda(), 6);
+            assertEquals(resultAddress.getRequired(), 7);
+            assertEquals(resultAddress.getRelays().size(), 4); // We added a relay so the count is 4 now
+            assertEquals(resultAddress.getTimestamp(), now);
+            
+            // We had relay 1 program 1, we reported relay 1 splinter 1, so we should now record relay1 p1 s1
+            ExpressComReportedAddressRelay testRelay = new ExpressComReportedAddressRelay(1);
+            testRelay.setProgram(1);
+            assertFalse(resultAddress.getRelays().contains(testRelay));
+            testRelay.setSplinter(1);
+            assertTrue(resultAddress.getRelays().contains(testRelay));
+            
+            // We switched from current of R2P2 to R2P3S2
+            testRelay.setRelayNumber(2);
+            testRelay.setProgram(3);
+            testRelay.setSplinter(2);
+            assertTrue(resultAddress.getRelays().contains(testRelay));
+            
+            // We switched from current of R3S3 to R3P3S3
+            testRelay.setRelayNumber(3);
+            testRelay.setProgram(3);
+            testRelay.setSplinter(3);
+            assertTrue(resultAddress.getRelays().contains(testRelay));
+            
+            // We added relay 4
+            testRelay.setRelayNumber(4);
+            testRelay.setProgram(4);
+            testRelay.setSplinter(0);
+            assertTrue(resultAddress.getRelays().contains(testRelay));
+        }
+        
+        
+        currentAddress.setTimestamp(now.plus(6000));
+        
+        {
+            // moved currentAddress to the future, so we should get null
+            Object result = ReflectionTestUtils.invokeMethod(service, "generateUpdatedAddressingFromMessage", data, devicePaoId, currentAddress);
+            assertEquals(result, null);
+        }
+    }
+    
+    private byte[] getBytes(short value) {
+        ByteBuffer buffer = ByteBuffer.allocate(2);
+        buffer.putShort(value);
+        return buffer.array();
     }
     
     private byte[] getBytes(int value) {
