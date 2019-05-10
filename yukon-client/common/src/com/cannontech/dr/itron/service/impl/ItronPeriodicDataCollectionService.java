5dcfde706c103d35faa8724b937f6687164501a3
Johnston, Samuel T <SamuelTJohnston@Eaton.com>
2019-05-10 20:08:01 +0000

    YUK-19897 - Itron data collection task doesn't run when devices are created
    Changed the service so that the task always runs periodically, but checks for Itron devices before doing any work. The task still won't run if the data collection interval is set to zero, but should pick up any changes and resume running if the interval is updated to a value >0. No restarts should be necessary.

--- yukon-client/common/src/com/cannontech/dr/itron/service/impl/ItronPeriodicDataCollectionService.java
+++ yukon-client/common/src/com/cannontech/dr/itron/service/impl/ItronPeriodicDataCollectionService.java
@@ -33,46 +33,47 @@ public class ItronPeriodicDataCollectionService {
     @Autowired private GlobalSettingDao settingsDao;
     @Autowired private AsyncDynamicDataSource asyncDynamicDataSource;
     @Autowired private ItronDataReadService itronDataReadService;
-    @Autowired PaoDao paoDao;
+    @Autowired private PaoDao paoDao;
     @Autowired private ConfigurationSource configSource;
     
     private Duration interval = Duration.ZERO;
     private ScheduledFuture<?> scheduledTask;
-    public static int maxRows = 1000;
-
+    private boolean itronDevicesFound;
+    public static final int maxRows = 1000;
 
     @PostConstruct
     public void init() {
         
-        //no Itron devices on the system
-        if(!paoDao.getExistingPaoTypes().stream()
-                .filter(PaoType::isItron)
-                .findAny().isPresent()) {
-            return;
-        }
-        
-        //no simulator, enabling this will produce errors
+        // No simulator, enabling this on a dev system will produce errors
         boolean isDevelopmentMode = configSource.getBoolean(MasterConfigBoolean.DEVELOPMENT_MODE, false);
         if(isDevelopmentMode) {
+            log.info("Disabling Itron periodic data collection - development mode is active.");
             return;
         }
         
         scheduleDataCollection(getDataCollectionInterval());
         
+        // Set up a listener for global setting changes, to process any updates to the data collection interval
         asyncDynamicDataSource.addDatabaseChangeEventListener(DbChangeCategory.GLOBAL_SETTING, this::databaseChangeEvent);
         
         log.info("Initialized ItronPeriodicDataCollectionService");
     }
     
+    /**
+     * Called when any global setting is updated
+     */
     private void databaseChangeEvent(DatabaseChangeEvent event) {
         var newInterval = getDataCollectionInterval();
-        if (newInterval.equals(interval)) {
-            //  interval matches, nothing to do
-            return;
+        if (!newInterval.equals(interval)) {
+            // Interval changed, schedule data collection with the updated interval.
+            scheduleDataCollection(newInterval);
         }
-        scheduleDataCollection(newInterval);
     }
     
+    /**
+     * Schedule the data collection task. If the new interval is 0, data collection stopped. Otherwise the
+     * currently scheduled task will be cancelled, and a new task will be scheduled based on the new interval.
+     */
     private void scheduleDataCollection(Duration newInterval) {
         if (newInterval.isZero()) {
             log.warn("Data collection rate set to zero.  Pausing data collection until this is changed.");
@@ -109,25 +110,57 @@ public class ItronPeriodicDataCollectionService {
         
         log.info("Collection set to " + format(interval) + ", starting in " + format(delay) + ".");
         
-        scheduledTask = scheduledExecutor.scheduleAtFixedRate(() -> itronDataReadService.collectData(),
+        scheduledTask = scheduledExecutor.scheduleAtFixedRate(this::collectData,
             delay.toSeconds(), interval.toSeconds(), TimeUnit.SECONDS);
     }
     
-    private static String format(Duration d) {
-        return DurationFormatUtils.formatDurationWords(d.toMillis(), true, true);
+    /**
+     * Start the data collection, so long as there are Itron devices present in the system.
+     */
+    private void collectData() {
+        if(!itronDevicesExist()) {
+            log.debug("Skipping Itron periodic data collection - no devices present that communicate over Itron network.");
+            return;
+        }
+        
+        log.info("Starting Itron data collection");
+        itronDataReadService.collectData();
+        log.info("Itron data collection complete");
     }
     
+    /**
+     * Checks for any devices in the system that communicate over the Itron network (a.k.a. SilverSprings)
+     */
+    private boolean itronDevicesExist() {
+        // If we already determined that there are Itron devices, don't bother checking again
+        if (itronDevicesFound) {
+            return true;
+        }
+        
+        // Otherwise, check for Itron devices in the system
+        itronDevicesFound = paoDao.getExistingPaoTypes()
+                                  .stream()
+                                  .anyMatch(PaoType::isItron);
+        
+        return itronDevicesFound;
+    }
+    
+    /**
+     * Get the data collection interval from the global setting and make sure it is valid.
+     */
     private Duration getDataCollectionInterval() {
         int hours = settingsDao.getInteger(GlobalSettingType.ITRON_HCM_DATA_COLLECTION_HOURS);
         
         if (hours > 0) {
             return Duration.ofHours(hours);
-        }
-        
-        if (hours < 0) {
+        } else if (hours < 0) {
             log.warn("Invalid data collection interval: " + hours + ", setting to zero");
         }
 
         return Duration.ZERO;
+    }
+    
+    private static String format(Duration d) {
+        return DurationFormatUtils.formatDurationWords(d.toMillis(), true, true);
     }
 }
