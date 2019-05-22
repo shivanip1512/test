de81b4c4c4e2828c8433f252d55b3959790b2999
Moc, Jeremy P <JeremyPMoc@Eaton.com>
2019-05-22 18:40:45 +0000

    YUK-19850 Add meter disconnect control to LM Server
    Minor refactor of the group creation factory.

--- yukon-server/LoadManagement/lmfactory.cpp
+++ yukon-server/LoadManagement/lmfactory.cpp
@@ -2,11 +2,12 @@
 
 #include "lmfactory.h"
 
-
 #include "guard.h"
 #include "logger.h"
 #include "resolvers.h"
 #include "devicetypes.h"
+
+#include "std_helper.h"
 
 #include "lmgroupversacom.h"
 #include "lmgroupdigisep.h"
@@ -27,84 +28,63 @@
 #include "lmgroupitron.h"
 #include "lmgroupmeterdisconnect.h"
 
-using std::string;
-using std::endl;
 
-CtiLMGroupPtr CtiLMGroupFactory::createLMGroup(Cti::RowReader &rdr)
+namespace
 {
-    CtiLMGroupBase* lm_group = 0;
-    string category;
-    string paotype;
 
-    if(rdr["category"].isNull() || rdr["type"].isNull())
-    {
-        CTILOG_INFO(dout, "No paotype available in the given Reader:");
-        return CtiLMGroupPtr();
-    }
-
-    rdr["category"] >> category;
-    rdr["type"] >> paotype;
-
-    switch(resolvePAOType(category, paotype))
-    {
-    case TYPE_LMGROUP_VERSACOM:
-        lm_group = CTIDBG_new CtiLMGroupVersacom(rdr);
-        break;
-    case TYPE_LMGROUP_EMETCON:
-        lm_group = CTIDBG_new CtiLMGroupEmetcon(rdr);
-        break;
-    case TYPE_LMGROUP_RIPPLE:
-        lm_group = CTIDBG_new CtiLMGroupRipple(rdr);
-        break;
-    case TYPE_LMGROUP_POINT:
-        lm_group = CTIDBG_new CtiLMGroupPoint(rdr);
-        break;
-    case TYPE_LMGROUP_RFN_EXPRESSCOM:
-    case TYPE_LMGROUP_EXPRESSCOM:
-        lm_group = CTIDBG_new CtiLMGroupExpresscom(rdr);
-        break;
-    case TYPE_LMGROUP_DIGI_SEP:
-        lm_group = CTIDBG_new LMGroupDigiSEP(rdr);
-        break;
-    case TYPE_LMGROUP_ECOBEE:
-        lm_group = CTIDBG_new LMGroupEcobee(rdr);
-        break;
-    case TYPE_LMGROUP_HONEYWELL:
-        lm_group = CTIDBG_new LMGroupHoneywell(rdr);
-        break;
-    case TYPE_LMGROUP_NEST:
-        lm_group = CTIDBG_new LMGroupNest(rdr);
-        break;
-    case TYPE_LMGROUP_ITRON:
-        lm_group = CTIDBG_new LMGroupItron(rdr);
-        break;
-    case TYPE_LMGROUP_METER_DISCONNECT:
-        lm_group = CTIDBG_new LMGroupMeterDisconnect(rdr);
-        break;
-    case TYPE_LMGROUP_MCT:
-        lm_group = CTIDBG_new CtiLMGroupMCT(rdr);
-        break;
-    case TYPE_LMGROUP_SA105:
-        lm_group = CTIDBG_new CtiLMGroupSA105(rdr);
-        break;
-    case TYPE_LMGROUP_SA205:
-        lm_group = CTIDBG_new CtiLMGroupSA205(rdr);
-        break;
-    case TYPE_LMGROUP_SA305:
-        lm_group = CTIDBG_new CtiLMGroupSA305(rdr);
-        break;
-    case TYPE_LMGROUP_SADIGITAL:
-        lm_group = CTIDBG_new CtiLMGroupSADigital(rdr);
-        break;
-    case TYPE_LMGROUP_GOLAY:
-        lm_group = CTIDBG_new CtiLMGroupGolay(rdr);
-        break;
-    case TYPE_MACRO:
-        lm_group = CTIDBG_new CtiLMGroupMacro(rdr);
-        break;
-    default:
-    CTILOG_ERROR(dout, "Invalid paotype: " << paotype);
-    break;
-    }
-    return CtiLMGroupPtr(lm_group);
+template< typename GROUP >
+CtiLMGroupBase * makeGroup( Cti::RowReader & rdr )
+{
+    return new GROUP( rdr );
 }
+
+static const std::map< DeviceTypes, std::function< CtiLMGroupBase * ( Cti::RowReader & ) > >    groupCreator
+{
+    { TYPE_LMGROUP_VERSACOM,         makeGroup< CtiLMGroupVersacom >        },
+    { TYPE_LMGROUP_EMETCON,          makeGroup< CtiLMGroupEmetcon >         },
+    { TYPE_LMGROUP_RIPPLE,           makeGroup< CtiLMGroupRipple >          },
+    { TYPE_LMGROUP_POINT,            makeGroup< CtiLMGroupPoint >           },
+    { TYPE_LMGROUP_RFN_EXPRESSCOM,   makeGroup< CtiLMGroupExpresscom >      },
+    { TYPE_LMGROUP_EXPRESSCOM,       makeGroup< CtiLMGroupExpresscom >      },
+    { TYPE_LMGROUP_DIGI_SEP,         makeGroup< LMGroupDigiSEP >            },
+    { TYPE_LMGROUP_ECOBEE,           makeGroup< LMGroupEcobee >             },
+    { TYPE_LMGROUP_HONEYWELL,        makeGroup< LMGroupHoneywell >          },
+    { TYPE_LMGROUP_NEST,             makeGroup< LMGroupNest >               },
+    { TYPE_LMGROUP_ITRON,            makeGroup< LMGroupItron >              },
+    { TYPE_LMGROUP_METER_DISCONNECT, makeGroup< LMGroupMeterDisconnect >    },
+    { TYPE_LMGROUP_MCT,              makeGroup< CtiLMGroupMCT >             },
+    { TYPE_LMGROUP_SA105,            makeGroup< CtiLMGroupSA105 >           },
+    { TYPE_LMGROUP_SA205,            makeGroup< CtiLMGroupSA205 >           },
+    { TYPE_LMGROUP_SA305,            makeGroup< CtiLMGroupSA305 >           },
+    { TYPE_LMGROUP_SADIGITAL,        makeGroup< CtiLMGroupSADigital >       },
+    { TYPE_LMGROUP_GOLAY,            makeGroup< CtiLMGroupGolay >           },
+    { TYPE_MACRO,                    makeGroup< CtiLMGroupMacro >           }
+};
+
+}
+
+CtiLMGroupPtr CtiLMGroupFactory::createLMGroup( Cti::RowReader & rdr )
+{
+    if ( rdr["Category"].isNull() || rdr["Type"].isNull() )
+    {
+        CTILOG_INFO( dout, "No paotype available in the given Reader." );
+
+        return nullptr;
+    }
+
+    std::string category,
+                paotype;
+
+    rdr["Category"] >> category;
+    rdr["Type"]     >> paotype;
+
+    if ( auto x = Cti::mapFind( groupCreator, static_cast< DeviceTypes >( resolvePAOType( category, paotype ) ) ) )
+    {
+        return CtiLMGroupPtr( ( *x )( rdr ) );
+    }
+
+    CTILOG_ERROR( dout, "Invalid paotype: " << paotype );
+
+    return nullptr;
+}
+
