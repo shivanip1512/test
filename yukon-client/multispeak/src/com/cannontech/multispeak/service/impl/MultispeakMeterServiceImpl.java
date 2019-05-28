15592c9ae359147d3ba81d2e4c98dd236f738517
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2019-05-28 19:36:05 +0000

    YUK-19971 NullPointerException in InitiateMeterReadByMeterNoAndType/InitiateMeterReadByMeterNumber  - added null check on response object

--- yukon-client/multispeak/src/com/cannontech/multispeak/service/impl/MultispeakMeterServiceImpl.java
+++ yukon-client/multispeak/src/com/cannontech/multispeak/service/impl/MultispeakMeterServiceImpl.java
@@ -819,7 +819,7 @@ public class MultispeakMeterServiceImpl extends MultispeakMeterServiceBase imple
                                                                                                                                     responseUrl,
                                                                                                                                     readingChangedNotification);
                         List<ErrorObject> errObjects = null;
-                        if (readingChangedNotificationResponse != null) {
+                        if (readingChangedNotificationResponse != null && readingChangedNotificationResponse.getReadingChangedNotificationResult() != null) {
                             List<ErrorObject> responseErrorObjects = readingChangedNotificationResponse.getReadingChangedNotificationResult().getErrorObject();
                             errObjects = responseErrorObjects;
                         }
@@ -920,7 +920,7 @@ public class MultispeakMeterServiceImpl extends MultispeakMeterServiceBase imple
                                                                                                                                 responseUrl,
                                                                                                                                 formattedBlockNotification);
                     List<ErrorObject> errObjects = null;
-                    if (formattedBlockNotificationResponse != null) {
+                    if (formattedBlockNotificationResponse != null && formattedBlockNotificationResponse.getFormattedBlockNotificationResult() != null) {
                         List<ErrorObject> responseErrorObjects = formattedBlockNotificationResponse.getFormattedBlockNotificationResult().getErrorObject();
                         errObjects = responseErrorObjects;
                     }
