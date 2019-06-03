84fdd86399421d3053db255da7e0b2d00d0e297f
Beckius, Aaron T <AaronTBeckius@Eaton.com>
2019-06-03 20:18:00 +0000

    YUK-19963 Dbchange add index - Improve query for finding latest CRE for JobId  - Committing back to version 7.1.4  - Updating 7.2.2 and 7.3.0 scripts to account for potential duplication errors (which are properly ignored)

--- yukon-database/DBUpdates/sqlserver/7.2_2.sql
+++ yukon-database/DBUpdates/sqlserver/7.2_2.sql
@@ -10,10 +10,13 @@ INSERT INTO DBUpdates VALUES ('YUK-19773', '7.2.2', GETDATE());
 /* @end YUK-19773 */
 
 /* @start YUK-19963 */
+/* Will error via. already existing if 7.1.4 or later creation scripts were ran. */
+/* @error ignore-begin */
 CREATE INDEX INDX_CRE_StartDesc_ExecContId ON CommandRequestExec (
     StartTime DESC,
     CommandRequestExecContextId ASC
 );
+/* @error ignore-end */
 
 INSERT INTO DBUpdates VALUES ('YUK-19963', '7.2.2', GETDATE());
 /* @end YUK-19963 */
