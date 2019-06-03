807a955c2abe51374c4536dd6ce7e94d767e1555
Verma, Dhwaj <DhwajVerma@Eaton.com>
2019-06-03 11:30:12 +0000

    YUK-19877 : For both Webserver and EIM, when invalid(alphanumeric) longitude/latitude is entered giving NumberFormatException.

--- yukon-client/common/test/unit/com/cannontech/common/pao/service/impl/LocationServiceImplTest.java
+++ yukon-client/common/test/unit/com/cannontech/common/pao/service/impl/LocationServiceImplTest.java
@@ -20,60 +20,60 @@ public class LocationServiceImplTest {
         assertTrue("Longitude valid : valid", gps.getLongitude() == 160.00);
     }
 
-    @Test(expected = StarsInvalidArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void test_invalidLatitude_forString() {
         ReflectionTestUtils.invokeMethod(impl, "getValidLocationFormat", "xyz", Double.toString(160.00));
     }
 
-    @Test(expected = StarsInvalidArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void test_invalidLatitude_forNaN() {
         ReflectionTestUtils.invokeMethod(impl, "getValidLocationFormat", Double.toString(Double.NaN),
             Double.toString(160.00));
     }
 
-    @Test(expected = StarsInvalidArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void test_invalidLatitude_forNull() {
         ReflectionTestUtils.invokeMethod(impl, "getValidLocationFormat", null, Double.toString(160.00));
     }
 
-    @Test(expected = StarsInvalidArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void test_invalidLongitude_forString() {
         ReflectionTestUtils.invokeMethod(impl, "getValidLocationFormat", Double.toString(85.00), "xyz");
     }
 
-    @Test(expected = StarsInvalidArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void test_invalidLongitude_forNaN() {
         ReflectionTestUtils.invokeMethod(impl, "getValidLocationFormat", Double.toString(85.00),
             Double.toString(Double.NaN));
     }
 
-    @Test(expected = StarsInvalidArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void test_invalidLongitude_forNull() {
         ReflectionTestUtils.invokeMethod(impl, "getValidLocationFormat", Double.toString(85.00), null);
     }
 
-    @Test(expected = StarsInvalidArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void test_invalidLatitudeandLongitude_forString() {
         ReflectionTestUtils.invokeMethod(impl, "getValidLocationFormat", "xyz", "xyz");
     }
 
-    @Test(expected = StarsInvalidArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void test_invalidLatitudeandLongitude_forNaN() {
         ReflectionTestUtils.invokeMethod(impl, "getValidLocationFormat", Double.toString(Double.NaN),
             Double.toString(Double.NaN));
     }
     
-    @Test(expected = StarsInvalidArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void test_invalidLatitudeandLongitude_forNull() {
         ReflectionTestUtils.invokeMethod(impl, "getValidLocationFormat", null, null);
     }
     
-    @Test(expected = StarsInvalidArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void test_invalidLatitudeandLongitude_forNaNandNull() {
         ReflectionTestUtils.invokeMethod(impl, "getValidLocationFormat", Double.toString(Double.NaN), null);
     }
     
-    @Test(expected = StarsInvalidArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void test_invalidLatitudeandLongitude_forNullandNaN() {
         ReflectionTestUtils.invokeMethod(impl, "getValidLocationFormat",null, Double.toString(Double.NaN));
     }
