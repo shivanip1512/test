e5aff5090a56195ea49d0ce740aa6d6feeb1e4bc
Shinde, Nitin S <NitinSShinde@Eaton.com>
2019-06-19 04:59:59 +0000

    YUK-20007 : DBE To Web Rest API - Digi Sep Load Group

--- yukon-client/common/src/com/cannontech/common/dr/setup/LMModelFactory.java
+++ yukon-client/common/src/com/cannontech/common/dr/setup/LMModelFactory.java
@@ -15,14 +15,14 @@ public class LMModelFactory {
         case LM_GROUP_HONEYWELL:
         case LM_GROUP_ECOBEE:
         case LM_GROUP_NEST:
-             loadGroup = new LoadGroupBase();
-             break;
+            loadGroup = new LoadGroupBase();
+            break;
         case LM_GROUP_EXPRESSCOMM:
         case LM_GROUP_RFN_EXPRESSCOMM:
             loadGroup = new LoadGroupExpresscom();
             break;
-        case LM_GROUP_EMETCON:
-            loadGroup = new LoadGroupEmetcon();
+        case LM_GROUP_DIGI_SEP:
+            loadGroup = new LoadGroupDigiSep();
             break;
         case LM_GROUP_ITRON:
             loadGroup = new LoadGroupItron();
