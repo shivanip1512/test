ba1a22917a6f9b14a70197bc62afdf376a9f2176
Shinde, Nitin S <NitinSShinde@Eaton.com>
2019-06-26 05:52:42 +0000

    YUK-20007 : DBE To Web Rest API - Digi Sep Load Group.

--- yukon-client/yukon-web/src/com/cannontech/web/api/dr/setup/LoadGroupDigiSepValidator.java
+++ yukon-client/yukon-web/src/com/cannontech/web/api/dr/setup/LoadGroupDigiSepValidator.java
@@ -22,26 +22,39 @@ public class LoadGroupDigiSepValidator extends LoadGroupSetupValidator<LoadGroup
 
     @Override
     protected void doValidation(LoadGroupDigiSep loadGroup, Errors errors) {
-        YukonValidationUtils.rejectIfEmptyOrWhitespace(errors, "deviceClassSet", key + "required", new Object[] { "Device Class set" });
+        YukonValidationUtils.rejectIfEmptyOrWhitespace(errors, "deviceClassSet", key + "required",
+            new Object[] { "Device Class set" });
 
         // Utility Enrollment Group
-        YukonValidationUtils.rejectIfEmptyOrWhitespace(errors, "utilityEnrollmentGroup", key + "required", new Object[] { "Utility Enrollment Group" });
-        YukonValidationUtils.checkIsPositiveInt(errors, "utilityEnrollmentGroup", loadGroup.getUtilityEnrollmentGroup());
+        YukonValidationUtils.rejectIfEmptyOrWhitespace(errors, "utilityEnrollmentGroup", key + "required",
+            new Object[] { "Utility Enrollment Group" });
+        if (!errors.hasFieldErrors("utilityEnrollmentGroup")) {
+            YukonValidationUtils.checkIsPositiveInt(errors, "utilityEnrollmentGroup",
+                loadGroup.getUtilityEnrollmentGroup());
+        }
 
         if (loadGroup.getId() != null && loadGroup.getUtilityEnrollmentGroup() == 0) {
             errors.rejectValue("utilityEnrollmentGroup", key + "utilityEnrollmentGroup.rangeCheck");
         }
+
         if (!errors.hasFieldErrors("utilityEnrollmentGroup")) {
             YukonValidationUtils.checkRange(errors, "utilityEnrollmentGroup", loadGroup.getUtilityEnrollmentGroup(), 1,
                 255, true);
         }
 
         // Ramp In Time
-        YukonValidationUtils.rejectIfEmptyOrWhitespace(errors, "rampInMinutes", key + "required", new Object[] { "Ramp In Minutes" });
-        YukonValidationUtils.checkRange(errors, "rampInMinutes", loadGroup.getRampInMinutes(), -99999, 99999, true);
-        // Ramp Out Time
-        YukonValidationUtils.rejectIfEmptyOrWhitespace(errors, "rampOutMinutes", key + "required", new Object[] { "Ramp Out Minutes." });
-        YukonValidationUtils.checkRange(errors, "rampOutMinutes", loadGroup.getRampOutMinutes(), -99999, 99999, true);
+        YukonValidationUtils.rejectIfEmptyOrWhitespace(errors, "rampInMinutes", key + "required",
+            new Object[] { "Ramp In Minutes" });
+        if (!errors.hasFieldErrors("rampInMinutes")) {
+            YukonValidationUtils.checkRange(errors, "rampInMinutes", loadGroup.getRampInMinutes(), -99999, 99999, true);
+        }
 
+        // Ramp Out Time
+        YukonValidationUtils.rejectIfEmptyOrWhitespace(errors, "rampOutMinutes", key + "required",
+            new Object[] { "Ramp Out Minutes" });
+        if (!errors.hasFieldErrors("rampOutMinutes")) {
+            YukonValidationUtils.checkRange(errors, "rampOutMinutes", loadGroup.getRampOutMinutes(), -99999, 99999,
+                true);
+        }
     }
 }
