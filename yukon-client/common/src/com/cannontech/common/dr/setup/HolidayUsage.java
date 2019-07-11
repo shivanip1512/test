9114b24fb34032ac0e399d31510a9bffdf9de3d4
Satpathy, Samir K <SamirKSatpathy@Eaton.com>
2019-07-11 14:04:44 +0000

    YUK-20138:DBE To Web Rest API - Program Constraint : Review Comments

--- yukon-client/common/src/com/cannontech/common/dr/setup/HolidayUsage.java
+++ yukon-client/common/src/com/cannontech/common/dr/setup/HolidayUsage.java
@@ -1,6 +1,10 @@
 package com.cannontech.common.dr.setup;
 
 import static com.google.common.base.Preconditions.checkArgument;
+
+import org.apache.logging.log4j.Logger;
+
+import com.cannontech.clientutils.YukonLogManager;
 import com.google.common.collect.ImmutableMap;
 
 public enum HolidayUsage {
@@ -8,11 +12,21 @@ public enum HolidayUsage {
     FORCE('F'),
     NONE('N');
     
-    private final static ImmutableMap<Character, HolidayUsage> lookupByHoliday =
-        ImmutableMap.<Character, HolidayUsage> builder().put('E', EXCLUDE)
-                                                        .put('F', FORCE)
-                                                        .put('N', NONE)
-                                                        .build();
+    private final static Logger log = YukonLogManager.getLogger(HolidayUsage.class);
+    private final static ImmutableMap<Character, HolidayUsage> lookupByHoliday;
+
+    static {
+        try {
+            ImmutableMap.Builder<Character, HolidayUsage> holidayBuilder = ImmutableMap.builder();
+            for (HolidayUsage holidayUsage : values()) {
+                holidayBuilder.put(holidayUsage.holidayUsage, holidayUsage);
+            }
+            lookupByHoliday = holidayBuilder.build();
+        } catch (IllegalArgumentException e) {
+            log.warn("Caught exception while building lookup maps, look for a duplicate holiday usage.", e);
+            throw e;
+        }
+    }
 
     private final Character holidayUsage;
 
