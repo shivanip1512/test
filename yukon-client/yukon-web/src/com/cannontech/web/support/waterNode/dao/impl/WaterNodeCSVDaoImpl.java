d4b811002f95bac4bee445d38f9eec450cf2da0d
Winter, Jackson T <JacksonTWinter@eaton.com>
2019-07-11 09:57:23 -0400

    YUK-20221 Create csv data input mechanism Added basic validation of data file

--- yukon-client/yukon-web/src/com/cannontech/web/support/waterNode/dao/impl/WaterNodeCSVDaoImpl.java
+++ yukon-client/yukon-web/src/com/cannontech/web/support/waterNode/dao/impl/WaterNodeCSVDaoImpl.java
@@ -7,11 +7,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.joda.time.Instant;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.Logger;
 
-import java.time.Instant;
-
 import com.cannontech.clientutils.YukonLogManager;
+import com.cannontech.common.util.CtiUtilities;
 import com.cannontech.dr.assetavailability.service.impl.AssetAvailabilityServiceImpl;
 import com.cannontech.web.support.waterNode.dao.WaterNodeDao;
 import com.cannontech.web.support.waterNode.details.WaterNodeDetails;
@@ -23,7 +24,7 @@ public class WaterNodeCSVDaoImpl implements WaterNodeDao {
     @Override
     public List<WaterNodeDetails> getWaterNodeDetails(Instant startTime, Instant stopTime) {
 
-        File file = new File("C:\\Users\\e0535626\\Desktop", "data.csv");
+        File file = new File(CtiUtilities.getImportArchiveDirPath(), "data.csv");
         CSVReader reader;
         ArrayList<WaterNodeDetails> resultsList = new ArrayList<WaterNodeDetails>();
         WaterNodeDetails waterNodeDetails = new WaterNodeDetails();
@@ -32,36 +33,43 @@ public class WaterNodeCSVDaoImpl implements WaterNodeDao {
             String[] row;
             // headers
             row = reader.readNext();
-            String oldSN = "";
+            String oldSN = StringUtils.EMPTY;
+            int rowLengthErrors = 0;
 
             while ((row = reader.readNext()) != null) {
-                Instant currentTimestamp = Instant.ofEpochMilli(Long.valueOf(row[4]));
-                if (currentTimestamp.isAfter(startTime) && currentTimestamp.isBefore(stopTime)) {
-                    // Set a meter's serial number and details once for each set of voltage readings.
-                    if (!row[0].equals(oldSN)) { 
+                if (row.length == 6) {
+                    Instant currentTimestamp = new Instant(Long.valueOf(row[4]));
+                    if (currentTimestamp.isAfter(startTime) && currentTimestamp.isBefore(stopTime)) {
+                        // Set a meter's serial number and details once for each set of voltage readings.
+                        if (!row[0].equals(oldSN)) {
 
-                        // Serial number is only null for the empty placeholder needed to initialize
-                        // the loop. This prevents an empty item at the beginning of resultsList
-                        if (waterNodeDetails.getSerialNumber() != null) {
-                            resultsList.add(waterNodeDetails);
+                            // Serial number is only null for the empty placeholder needed to initialize
+                            // the loop. This prevents an empty item at the beginning of resultsList
+                            if (waterNodeDetails.getSerialNumber() != null) {
+                                resultsList.add(waterNodeDetails);
+                            }
+
+                            waterNodeDetails = new WaterNodeDetails();
+                            waterNodeDetails.setSerialNumber(row[0]);
+                            waterNodeDetails.setMeterNumber(Integer.valueOf(row[1]));
+                            waterNodeDetails.setName(row[2]);
+                            waterNodeDetails.setType(row[3]);
+                            waterNodeDetails.setHighSleepingCurrentIndicator(false);
+                            waterNodeDetails.setBatteryLevel(null);
                         }
-
-                        waterNodeDetails = new WaterNodeDetails();
-                        waterNodeDetails.setSerialNumber(row[0]);
-                        waterNodeDetails.setMeterNumber(Integer.valueOf(row[1]));
-                        waterNodeDetails.setName(row[2]);
-                        waterNodeDetails.setType(row[3]);
-                        waterNodeDetails.setHighSleepingCurrentIndicator(false);
-                        waterNodeDetails.setBatteryLevel(null);
+                        waterNodeDetails.addTimestamp(new Instant(Long.valueOf(row[4])));
+                        waterNodeDetails.addVoltage(Double.valueOf(row[5]));
+                        oldSN = row[0];
                     }
-                    waterNodeDetails.addTimestamp(Instant.ofEpochMilli(Long.valueOf(row[4])));
-                    waterNodeDetails.addVoltage(Integer.valueOf(row[5]));
-                    oldSN = row[0];
+                } else {
+                    rowLengthErrors++;
                 }
+            }
+            if (rowLengthErrors > 0) {
+                log.error("Error in file parsing: " + rowLengthErrors + " unexpected added or missing columns present");
             }
             resultsList.add(waterNodeDetails);// add final meter to resultsList
             reader.close();
-
         } catch (IOException e) {
             log.error("Unable to read CSV file input", e);
         }
