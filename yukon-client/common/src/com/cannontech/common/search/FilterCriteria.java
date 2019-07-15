635a5970e69b4cab1d49110226e88444e142a2de
Tongaonkar, Prachi S <PrachiSTongaonkar@Eaton.com>
2019-07-15 11:07:49 +0000

    YUK-20214 - DBE To Web UI- DR setup page (Load Group) Code changes to Load Group setup page.

--- yukon-client/common/src/com/cannontech/common/search/FilterCriteria.java
+++ yukon-client/common/src/com/cannontech/common/search/FilterCriteria.java
@@ -5,7 +5,6 @@ import org.apache.commons.lang3.Validate;
 import com.cannontech.common.model.PagingParameters;
 import com.cannontech.common.model.SortingParameters;
 
-//TODO check if this class is correct.
 public class FilterCriteria<T> {
     
     private PagingParameters pagingParameters;
@@ -23,11 +22,14 @@ public class FilterCriteria<T> {
     public FilterCriteria (T filteringParameters, SortingParameters sortingParameters, PagingParameters pagingParameters) {
         Validate.notNull(filteringParameters, "filteringParameters must not be null");
         Validate.notNull(sortingParameters, "SortingParameters must not be null");
-        //TODO: check if this not null check should be added
-        Validate.notNull(pagingParameters, "pagingParameters must not be null");
         this.filteringParameter = filteringParameters;
         this.sortingParameters = sortingParameters;
-        this.pagingParameters = pagingParameters;
+        if (pagingParameters != null) {
+            this.pagingParameters = pagingParameters;
+        } else {
+            this.pagingParameters = PagingParameters.EVERYTHING;
+        }
+        
     }
 
     public PagingParameters getPagingParameters() {
