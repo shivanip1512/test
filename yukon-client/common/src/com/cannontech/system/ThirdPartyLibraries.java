a9bba1201a7a1db46aab4c15e08d3030ba6bda74
Vant, Carrie <carrieVant@Eaton.com>
2019-07-26 19:20:51 +0000

    YUK-18789 - Add support page for displaying third party libraries and license access   - changes after review

--- yukon-client/common/src/com/cannontech/system/ThirdPartyLibraries.java
+++ yukon-client/common/src/com/cannontech/system/ThirdPartyLibraries.java
@@ -1,10 +1,11 @@
 package com.cannontech.system;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.cannontech.common.stream.StreamUtils;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.Iterables;
 
 public class ThirdPartyLibraries {
 
@@ -17,28 +18,12 @@ public class ThirdPartyLibraries {
     @JsonProperty("Icons")
     public List<ThirdPartyIconLibrary> iconLibraries;
     
-    public List<ThirdPartyProject> getCppProjects() {
-        return getUniqueProjects(cppLibraries);
-    }
-    public List<ThirdPartyProject> getJavaProjects() {
-        return getUniqueProjects(javaLibraries);
-    }
-    public List<ThirdPartyProject> getJsProjects() {
-        return getUniqueProjects(jsLibraries);
-    }
-    public List<ThirdPartyProject> getIconProjects() {
-        return getUniqueProjects(iconLibraries);
-    }
     public List<ThirdPartyProject> getAllProjects() {
-        List<ThirdPartyLibrary> allLibraries = new ArrayList<>();
-        allLibraries.addAll(cppLibraries);
-        allLibraries.addAll(javaLibraries);
-        allLibraries.addAll(jsLibraries);
-        allLibraries.addAll(iconLibraries);
+        Iterable<? extends ThirdPartyLibrary> allLibraries = Iterables.concat(cppLibraries, javaLibraries, jsLibraries, iconLibraries);
         return getUniqueProjects(allLibraries);
     }
-    private static <T extends ThirdPartyProject> List<ThirdPartyProject> getUniqueProjects(List<T> libraries) {
-        return libraries.stream()
+    private static <T extends ThirdPartyProject> List<ThirdPartyProject> getUniqueProjects(Iterable<? extends ThirdPartyLibrary> allLibraries) {
+        return StreamUtils.stream(allLibraries)
                         .map(library -> (ThirdPartyProject)library)
                         .distinct()
                         .sorted()
