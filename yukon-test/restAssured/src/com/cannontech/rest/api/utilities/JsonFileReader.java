3c02e586a7a528ff17012af16938e1a8f0dd6202
Shrivastava, Rahul <RahulShrivastava@eaton.com>
2019-07-30 09:15:09 -0400

    QA-3821 added the generic function updateJsonFile which can be used for updating any json file object value

--- yukon-test/restAssured/src/com/cannontech/rest/api/utilities/JsonFileReader.java
+++ yukon-test/restAssured/src/com/cannontech/rest/api/utilities/JsonFileReader.java
@@ -93,4 +93,34 @@ public class JsonFileReader {
         return obj;
 
     }
+
+    /**
+     * This method reads any Json file, Iterates through all the keys and updates the value and returns as
+     * JSONObject
+     * You can also provide the nested keys separated by a dot like key1.key2 if they are nested in hierarchy
+     * 
+     * @param payloadFile - relative path ( w.r.t to payload directory)
+     * @param key - attribute whose value to be update
+     * @param replaceValueWith - value to update
+     * @return JSONObject
+     */
+    public static JSONObject updateJsonFile(String payloadFile, String key, String replaceValueWith) {
+        JSONObject jsonObj = JsonFileReader.readJsonFileAsJSONObject(payloadFile);
+        // when the keys are nested and separated by dot(.)
+        if (key.contains(".")) {
+            String keyArray[] = key.split("\\.");
+            // traverse till the last key and set the key value
+            JSONObject newObject = null;
+            for (int i = 0; i < keyArray.length - 1; i++) {
+                // iterate till the parentKey matches with required key name in keySet
+                newObject = (JSONObject) jsonObj.get(keyArray[i]);
+            }
+            newObject.replace(keyArray[keyArray.length - 1], replaceValueWith);
+            return jsonObj;
+
+        } else {
+            jsonObj.replace(key, replaceValueWith);
+            return jsonObj;
+        }
+    }
 }
