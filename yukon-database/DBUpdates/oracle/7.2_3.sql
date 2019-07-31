3218c659e00ad04ed452e1cd8e7d9366917da15e
Beckius, Aaron T <AaronTBeckius@Eaton.com>
2019-07-31 14:43:35 +0000

    NO YUK-ISSUE 7.2.3 post-build updates  - Updating build_version.properties for 7.2.3 post-build  - Copying 7.2_3.sql to 7.3 branch

---
+++ yukon-database/DBUpdates/oracle/7.2_3.sql
@@ -0,0 +1,65 @@
+/******************************************/ 
+/****     Oracle DBupdates             ****/ 
+/******************************************/ 
+
+/* @start YUK-20294 */
+UPDATE GlobalSetting 
+SET Value = 'NONE'
+WHERE Name IN ('LDAP_SSL_ENABLED', 'AD_SSL_ENABLED') 
+AND Value = '0';
+
+UPDATE GlobalSetting 
+SET Value = 'TLS'
+WHERE Name IN ('LDAP_SSL_ENABLED', 'AD_SSL_ENABLED') 
+AND Value = '1';
+
+INSERT INTO DBUpdates VALUES ('YUK-20294', '7.2.3', SYSDATE);
+/* @end YUK-20294 */
+
+/* @start YUK-20257 */
+UPDATE DeviceConfigCategoryItem DCCI
+SET ItemValue = 'DELIVERED_DEMAND'
+WHERE DCCI.DeviceConfigCategoryId IN (
+    SELECT DISTINCT DCC.DeviceConfigCategoryId 
+    FROM DeviceConfigCategory DCC
+    JOIN DeviceConfigCategoryMap DCCM ON DCC.DeviceConfigCategoryId = DCCM.DeviceConfigCategoryId
+    JOIN DeviceConfiguration DC ON DCCM.DeviceConfigurationId = DC.DeviceConfigurationID
+    JOIN DeviceConfigDeviceTypes DCDT ON DC.DeviceConfigurationID = DCDT.DeviceConfigurationId
+    WHERE DCC.CategoryType = 'rfnChannelConfiguration'
+    AND DCDT.PaoType IN ('RFN-410CL', 'RFN-420CL') )
+AND DCCI.ItemName LIKE 'enabledChannels%attribute'
+AND DCCI.ItemValue = 'DEMAND';
+
+INSERT INTO DBUpdates VALUES ('YUK-20257', '7.2.3', SYSDATE);
+/* @end YUK-20257 */
+
+/* @start YUK-20314 */
+INSERT INTO CCStrategyTargetSettings (StrategyId, SettingName, SettingValue, SettingType)
+SELECT
+    TS.StrategyId,
+    'Maximum Delta Voltage' AS SettingName,
+    '10' AS SettingValue,
+    'MAX_DELTA' AS SettingType
+FROM CCStrategyTargetSettings TS
+JOIN CapControlStrategy CS ON TS.StrategyId = CS.StrategyID
+WHERE CS.ControlUnits IN ('MULTI_VOLT', 'MULTI_VOLT_VAR', 'INTEGRATED_VOLT_VAR')
+AND NOT EXISTS (
+    SELECT 1
+    FROM CCStrategyTargetSettings 
+    WHERE StrategyId = TS.StrategyId 
+    AND SettingType = 'MAX_DELTA' )
+GROUP BY TS.StrategyId;
+
+UPDATE CCStrategyTargetSettings
+SET SettingValue = '10'
+WHERE SettingType = 'MAX_DELTA'
+AND SettingValue = '0';
+
+INSERT INTO DBUpdates VALUES ('YUK-20314', '7.2.3', SYSDATE);
+/* @end YUK-20314 */
+
+/**************************************************************/
+/* VERSION INFO                                               */
+/* Inserted when update script is run                         */
+/**************************************************************/
+INSERT INTO CTIDatabase VALUES ('7.2', '26-JUN-2019', 'Latest Update', 3, SYSDATE);
