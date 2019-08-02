0488c0f7c51d24cc4934f06bd00fdfbedba41dec
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2019-08-02 20:22:53 +0000

    YUK-20207 Unable to import the Regulators from Import file when file char set is ANSI or Windows-1252 format  - added null check before trying to close reader.

--- yukon-client/common/src/com/cannontech/common/util/FileUploadUtils.java
+++ yukon-client/common/src/com/cannontech/common/util/FileUploadUtils.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -143,7 +142,9 @@ public class FileUploadUtils {
         } finally {
             reader.close();
             csvReader.close();
-            csvReaderIso.close();
+            if (csvReaderIso != null) {
+                csvReaderIso.close();
+            }
         }
     }
 
