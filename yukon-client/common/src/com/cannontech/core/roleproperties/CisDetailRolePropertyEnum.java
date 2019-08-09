49f7a2a49aef5d402b119ed99bdd8eb20aac8903
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2019-08-09 21:11:15 +0000

    YUK-16605 Update PaoLocation via MultiSpeak  - added ability to update PaoLocation coordinates based of MultiSpeak Meter or Service Location data.  - will only update if lat/long are actually different than previous values.

--- yukon-client/common/src/com/cannontech/core/roleproperties/CisDetailRolePropertyEnum.java
+++ yukon-client/common/src/com/cannontech/core/roleproperties/CisDetailRolePropertyEnum.java
@@ -1,18 +1,25 @@
 package com.cannontech.core.roleproperties;
 
-public enum CisDetailRolePropertyEnum {
+import com.cannontech.common.i18n.DisplayableEnum;
 
-	NONE(null),
-	MULTISPEAK("accountInformationWidget"),
-	CAYENTA("cayentaAccountInformationWidget");
-	
-	private String widgetName;
-	
-	CisDetailRolePropertyEnum(String widgetName) {
-		this.widgetName = widgetName;
-	}
-	
-	public String getWidgetName() {
-		return widgetName;
-	}
+public enum CisDetailRolePropertyEnum implements DisplayableEnum {
+
+    NONE(null), 
+    MULTISPEAK("accountInformationWidget"), 
+    CAYENTA("cayentaAccountInformationWidget");
+
+    private String widgetName;
+
+    CisDetailRolePropertyEnum(String widgetName) {
+        this.widgetName = widgetName;
+    }
+
+    public String getWidgetName() {
+        return widgetName;
+    }
+    
+    @Override
+    public String getFormatKey() {
+        return "yukon.common.multispeakCisDetail." + name();
+    }
 }
