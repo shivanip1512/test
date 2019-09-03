8a060b50e2a167e7818252c8e19a329f832b59e2
Feldman, Marina <MarinaFeldman@Eaton.com>
2019-09-03 19:13:32 +0000

    YUK-20525 Remote CI Meter Config Event Handler

--- yukon-client/common/src/com/cannontech/amr/rfn/service/processor/impl/RfnRemoteMeterConfigurationFailureEventArchiveRequestProcessor.java
+++ yukon-client/common/src/com/cannontech/amr/rfn/service/processor/impl/RfnRemoteMeterConfigurationFailureEventArchiveRequestProcessor.java
@@ -1,10 +1,18 @@
 package com.cannontech.amr.rfn.service.processor.impl;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.jms.ConnectionFactory;
 
 import org.apache.logging.log4j.Logger;
 import org.joda.time.Instant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 
+import com.cannontech.amr.errors.dao.DeviceError;
+import com.cannontech.amr.rfn.message.event.DetailedConfigurationStatusCode.Status;
 import com.cannontech.amr.rfn.message.event.MeterConfigurationStatus;
 import com.cannontech.amr.rfn.message.event.RfnConditionDataType;
 import com.cannontech.amr.rfn.message.event.RfnConditionType;
@@ -12,8 +20,12 @@ import com.cannontech.amr.rfn.message.event.RfnEvent;
 import com.cannontech.amr.rfn.service.processor.RfnArchiveRequestProcessor;
 import com.cannontech.amr.rfn.service.processor.RfnEventConditionDataProcessorHelper;
 import com.cannontech.clientutils.YukonLogManager;
+import com.cannontech.common.device.programming.message.MeterProgramStatusArchiveRequest;
+import com.cannontech.common.device.programming.message.MeterProgramStatusArchiveRequest.Source;
+import com.cannontech.common.device.programming.model.ProgramStatus;
 import com.cannontech.common.pao.attribute.model.BuiltInAttribute;
 import com.cannontech.common.rfn.model.RfnDevice;
+import com.cannontech.common.util.jms.api.JmsApiDirectory;
 import com.cannontech.database.db.point.stategroup.EventStatus;
 import com.cannontech.message.dispatch.message.PointData;
 
@@ -21,6 +33,32 @@ public class RfnRemoteMeterConfigurationFailureEventArchiveRequestProcessor exte
         implements RfnArchiveRequestProcessor {
     
     private static final Logger log = YukonLogManager.getLogger(RfnRemoteMeterConfigurationFailureEventArchiveRequestProcessor.class);
+    private JmsTemplate jmsTemplate;
+    
+    private Map<Status, DeviceError> statusCodesToErrors = new HashMap<>();
+    {
+    	//statusCodesToErrors.put(Status.SUCCESS, DeviceError.UNKNOWN);
+    	statusCodesToErrors.put(Status.REASON_UNKNOWN, DeviceError.UNKNOWN);
+    	statusCodesToErrors.put(Status.SERVICE_NOT_SUPPORTED, DeviceError.UNKNOWN);
+    	statusCodesToErrors.put(Status.INSUFFICIENT_SECURITY_CLEARANCE, DeviceError.UNKNOWN);
+    	statusCodesToErrors.put(Status.OPERATION_NOT_POSSIBLE, DeviceError.UNKNOWN);
+    	statusCodesToErrors.put(Status.INAPPROPRIATE_ACTION_REQUESTED, DeviceError.UNKNOWN);
+    	statusCodesToErrors.put(Status.DEVICE_BUSY, DeviceError.UNKNOWN);
+    	statusCodesToErrors.put(Status.DATA_NOT_READY, DeviceError.UNKNOWN);
+    	statusCodesToErrors.put(Status.RENEGOTIATE_REQUEST, DeviceError.UNKNOWN);
+    	statusCodesToErrors.put(Status.INVALID_SERVICE_SEQUENCE_STATE, DeviceError.UNKNOWN);
+    	statusCodesToErrors.put(Status.DOWNLOAD_ABORTED, DeviceError.UNKNOWN);
+    	statusCodesToErrors.put(Status.FILE_TOO_LARGE, DeviceError.UNKNOWN);
+    	statusCodesToErrors.put(Status.INSUFFICIENT_SECURITY_CLEARANCE, DeviceError.UNKNOWN);
+    	statusCodesToErrors.put(Status.CONFIGURATION_IN_PROGRESS, DeviceError.UNKNOWN);
+    	statusCodesToErrors.put(Status.UNABLE_TO_GET_FILE, DeviceError.UNKNOWN);
+    	statusCodesToErrors.put(Status.FILE_EXPIRED, DeviceError.UNKNOWN);
+    	statusCodesToErrors.put(Status.FAILED_REQUIREMENTS, DeviceError.UNKNOWN);
+    	statusCodesToErrors.put(Status.MALFORMED_RECORD_IN_CONFIGURATION_FILE, DeviceError.UNKNOWN);
+    	statusCodesToErrors.put(Status.VERIFICATION_FAILED, DeviceError.UNKNOWN);
+    	statusCodesToErrors.put(Status.WRITE_KEY_FAILED, DeviceError.UNKNOWN);
+    	statusCodesToErrors.put(Status.CATASTROPHIC_FAILURE_FULL_REPROGRAM_REQUIRED, DeviceError.UNKNOWN);
+    }
     
     @Override
     public void process(RfnDevice device, RfnEvent event, List<? super PointData> pointDatas, Instant now) {
@@ -29,8 +67,9 @@ public class RfnRemoteMeterConfigurationFailureEventArchiveRequestProcessor exte
         var meterConfigurationId = (String) getEventDataWithType(event, RfnConditionDataType.METER_CONFIGURATION_ID);
         var meterConfigurationStatus = (MeterConfigurationStatus) getEventDataWithType(event, RfnConditionDataType.METER_CONFIGURATION_STATUS); 
         
-        // TODO - this will need to update the Remote Configuration status table
-        log.info(" Remote Meter Configuration failed for device=" + device + ", meterConfigurationId=" + meterConfigurationId + ", status = " + meterConfigurationStatus);
+        log.info("Remote Meter Configuration failed for device={}, meterConfigurationId={}, status={}", device, meterConfigurationId, meterConfigurationStatus);
+        
+		archiveProgramStatus(device, meterConfigurationId, meterConfigurationStatus);
         
         rfnMeterEventService.processAttributePointData(device, 
                                                        pointDatas, 
@@ -39,9 +78,43 @@ public class RfnRemoteMeterConfigurationFailureEventArchiveRequestProcessor exte
                                                        EventStatus.ACTIVE.getRawState(),
                                                        now);
     }
+
+    /**
+     * Sends status update message to SM to update MeterProgramStatus table
+     */
+	private void archiveProgramStatus(RfnDevice device, String meterConfigurationId,
+			MeterConfigurationStatus meterConfigurationStatus) {
+		if (meterConfigurationStatus.getDetailedConfigurationStatusCode() != null
+				&& meterConfigurationStatus.getDetailedConfigurationStatusCode().getStatus() != null
+				&& meterConfigurationId != null) {
+			Status status = meterConfigurationStatus.getDetailedConfigurationStatusCode().getStatus();
+			MeterProgramStatusArchiveRequest request = new MeterProgramStatusArchiveRequest();
+			request.setSource(Source.SM_CONFIG_FAILURE);
+			request.setRfnIdentifier(device.getRfnIdentifier());
+			request.setConfigurationId(meterConfigurationId);
+			if (status == Status.SUCCESS) {
+				request.setStatus(ProgramStatus.IDLE);
+			} else {
+				request.setStatus(ProgramStatus.FAILED);
+				request.setError(statusCodesToErrors.get(status));
+			}
+			request.setTimeStamp(System.currentTimeMillis());
+			log.debug("Sending {} on queue {}", request, JmsApiDirectory.METER_PROGRAM_STATUS_ARCHIVE.getQueue().getName());
+			jmsTemplate.convertAndSend(JmsApiDirectory.METER_PROGRAM_STATUS_ARCHIVE.getQueue().getName(), request);
+		} else {
+			 log.info("Failed to update program status, insufficient information for the update for device={}, meterConfigurationId={}, status={}", device, meterConfigurationId, meterConfigurationStatus);
+		}
+	}
     
     @Override
     public RfnConditionType getRfnConditionType() {
         return RfnConditionType.REMOTE_METER_CONFIGURATION_FAILURE;
+    }
+    
+    @Autowired
+    public void setConnectionFactory(ConnectionFactory connectionFactory) {
+        jmsTemplate = new JmsTemplate(connectionFactory);
+        jmsTemplate.setExplicitQosEnabled(true);
+        jmsTemplate.setDeliveryPersistent(false);
     }
 }
--- yukon-client/common/src/com/cannontech/common/device/programming/model/MeterProgramStatus.java
+++ yukon-client/common/src/com/cannontech/common/device/programming/model/MeterProgramStatus.java
@@ -1,8 +1,60 @@
 package com.cannontech.common.device.programming.model;
 
-public enum MeterProgramStatus {
-	IDLE, 
-	UPLOADING, 
-	CONFIRMING, 
-	FAILED
+import java.util.UUID;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.joda.time.Instant;
+
+import com.cannontech.amr.errors.dao.DeviceError;
+
+public class MeterProgramStatus {
+	private int deviceId;
+	private UUID reportedGuid;
+	private ProgramStatus status;
+	private MeterProgramSource source;
+	private Instant lastUpdate;
+	private DeviceError error;
+	public int getDeviceId() {
+		return deviceId;
+	}
+	public void setDeviceId(int deviceId) {
+		this.deviceId = deviceId;
+	}
+	public ProgramStatus getStatus() {
+		return status;
+	}
+	public void setStatus(ProgramStatus status) {
+		this.status = status;
+	}
+	public UUID getReportedGuid() {
+		return reportedGuid;
+	}
+	public void setReportedGuid(UUID reportedGuid) {
+		this.reportedGuid = reportedGuid;
+	}
+	public Instant getLastUpdate() {
+		return lastUpdate;
+	}
+	public void setLastUpdate(Instant lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+	public MeterProgramSource getSource() {
+		return source;
+	}
+	public void setSource(MeterProgramSource source) {
+		this.source = source;
+	}
+	public DeviceError getError() {
+		return error;
+	}
+	public void setError(DeviceError error) {
+		this.error = error;
+	}
+	
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE)
+				+ System.getProperty("line.separator");
+	}
 }
