2ac2667bc2fa062f80cbb46f24081941f5eb031f
Satpathy, Samir K <SamirKSatpathy@Eaton.com>
2019-09-03 07:22:35 +0000

    YUK-20390:Create Rest API test Jenkins job to run automatically

---
+++ yukon-deploy/vm-install/copy-to-vm/YukonSetupRestApi.ps1
@@ -0,0 +1,30 @@
+# To execut this PowerShell Script, we need to execute this administrator only
+# Execute following command if we have ExecutionPolicy set to Restricted
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+
+# Valid operations are start, stop, restart
+param([switch]$Elevated)
+
+function Test-Admin {
+  $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+  $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) 
+    {
+        # tried to elevate, did not work, aborting
+    } 
+    else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+}
+
+exit
+}
+
+Import-Module $PSScriptRoot\YukonDeploy
+ 
+Uninstall-Yukon
+Expand-Installer
+Install-Yukon
+Start-YukonServices
