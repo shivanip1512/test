c002cdadae859ce56b0875256519523d28a88908
Feldman, Marina <MarinaFeldman@Eaton.com>
2019-09-04 14:58:13 +0000

    YUK-20523 Remote CI Meter Config Summary back-end/UI object definition

---
+++ yukon-client/yukon-web/src/com/cannontech/web/tools/device/programming/model/MeterProgramStatistics.java
@@ -0,0 +1,44 @@
+package com.cannontech.web.tools.device.programming.model;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public class MeterProgramStatistics {
+	private MeterProgramInfo programInfo;
+	private int deviceTotal;
+	private int inProgressTotal;
+
+	public MeterProgramInfo getProgramInfo() {
+		return programInfo;
+	}
+
+	public void setProgramInfo(MeterProgramInfo programInfo) {
+		this.programInfo = programInfo;
+	}
+
+	public int getDeviceTotal() {
+		return deviceTotal;
+	}
+
+	public void setDeviceTotal(int deviceTotal) {
+		this.deviceTotal = deviceTotal;
+	}
+
+	public int getInProgressTotal() {
+		return inProgressTotal;
+	}
+
+	public void setInProgressTotal(int inProgressTotal) {
+		this.inProgressTotal = inProgressTotal;
+	}
+
+	public boolean displayDelete() {
+		return programInfo.getGuid() != null && deviceTotal == 0;
+	}
+	
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE)
+            + System.getProperty("line.separator");
+    }
+}
---
+++ yukon-client/yukon-web/src/com/cannontech/web/tools/device/programming/model/MeterProgramSummaryDetail.java
@@ -0,0 +1,72 @@
+package com.cannontech.web.tools.device.programming.model;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.joda.time.Instant;
+
+import com.cannontech.amr.errors.dao.DeviceError;
+import com.cannontech.common.device.model.DisplayableDevice;
+import com.cannontech.web.tools.device.programming.model.MeterProgrammingSummaryFilter.DisplayableStatus;
+
+public class MeterProgramSummaryDetail {
+	private DisplayableDevice device;
+	private String meterNumber;
+	private MeterProgramInfo programInfo;
+	private DisplayableStatus status;
+	private DeviceError error;
+	private Instant lastUpdate;
+	public DisplayableDevice getDevice() {
+		return device;
+	}
+	public void setDevice(DisplayableDevice device) {
+		this.device = device;
+	}
+	public String getMeterNumber() {
+		return meterNumber;
+	}
+	public void setMeterNumber(String meterNumber) {
+		this.meterNumber = meterNumber;
+	}
+	public MeterProgramInfo getProgramInfo() {
+		return programInfo;
+	}
+	public void setProgramInfo(MeterProgramInfo programInfo) {
+		this.programInfo = programInfo;
+	}
+	public Instant getLastUpdate() {
+		return lastUpdate;
+	}
+	public void setLastUpdate(Instant lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+	public DisplayableStatus getStatus() {
+		return status;
+	}
+	public void setStatus(DisplayableStatus status) {
+		this.status = status;
+	}
+	
+	public DeviceError getError() {
+		return error;
+	}
+	public void setError(DeviceError error) {
+		this.error = error;
+	}
+	public boolean displayCancel() {
+		return true;
+	}
+	
+	public boolean displayRead() {
+		return true;
+	}
+	
+    public boolean displaySend() {
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE)
+				+ System.getProperty("line.separator");
+	}
+}
