e70d9383406ad640ef09bc39a68f8e42447b089c
Joshi, Rohit <RohitJoshi2@Eaton.com>
2019-09-09 11:20:09 +0000

    YUK-20470-Program widget - control setup for 'tomorrow' is not being displayed in widget(4)

---
+++ yukon-client/common/src/com/cannontech/common/program/widget/model/ProgramWidgetData.java
@@ -0,0 +1,23 @@
+package com.cannontech.common.program.widget.model;
+
+import java.util.List;
+import java.util.Map;
+
+public class ProgramWidgetData {
+
+    private Map<String, List<ProgramData>> data;
+    private int programDataCount;
+
+    public ProgramWidgetData(Map<String, List<ProgramData>> data, int programDataCount) {
+        this.data = data;
+        this.programDataCount = programDataCount;
+    }
+
+	public Map<String, List<ProgramData>> getData() {
+		return data;
+	}
+
+	public int getProgramDataCount() {
+		return programDataCount;
+	}
+}
