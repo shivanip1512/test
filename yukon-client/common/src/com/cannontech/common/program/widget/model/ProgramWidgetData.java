c1594a2815973a4ae6315c044d442482e5fd4117
Joshi, Rohit <RohitJoshi2@Eaton.com>
2019-09-10 11:29:41 +0000

    YUK-20470-Program widget - control setup for 'tomorrow' is not being displayed in widget(5) Added Java docs.

--- yukon-client/common/src/com/cannontech/common/program/widget/model/ProgramWidgetData.java
+++ yukon-client/common/src/com/cannontech/common/program/widget/model/ProgramWidgetData.java
@@ -3,6 +3,13 @@ package com.cannontech.common.program.widget.model;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This class will hold program data and programDataCount for todays and future scheduled program.
+ * data : This holds maximum 10 program data records for today, scheduled and past date.
+ * programDataCount : This holds exact count for todays and future scheduled program data records.
+ *                    This will be used to display a message on program widget in case of todays and future 
+ *                    scheduled program count is greater than 10 (Max limit of showing program data in Program Widget).
+ * */
 public class ProgramWidgetData {
 
     private Map<String, List<ProgramData>> data;
