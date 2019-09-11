6ae3b18b3b1beeba030ad1db5d2236cf77d9b1b4
Satpathy, Samir K <SamirKSatpathy@Eaton.com>
2019-09-11 09:24:03 +0000

    YUK-20483: Issue while adding trigger in Control Area alongwith XSS issue.

--- yukon-client/yukon-web/src/com/cannontech/web/search/lucene/criteria/DevicePointCriteria.java
+++ yukon-client/yukon-web/src/com/cannontech/web/search/lucene/criteria/DevicePointCriteria.java
@@ -2,10 +2,12 @@ package com.cannontech.web.search.lucene.criteria;
 
 import org.apache.lucene.search.BooleanClause;
 
+import com.cannontech.common.pao.PaoCategory;
+
 public class DevicePointCriteria extends YukonObjectCriteriaHelper {
 
     public DevicePointCriteria() {
         super();
-        addCriteria("category", "DEVICE", BooleanClause.Occur.SHOULD);
+        addCriteria("category", PaoCategory.DEVICE.getDbString(), BooleanClause.Occur.SHOULD);
     }
 }
