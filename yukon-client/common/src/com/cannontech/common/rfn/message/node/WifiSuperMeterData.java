dfc0e4e24ae6c97e7ff3c31f198625068faf27a5
Feldman, Marina <MarinaFeldman@Eaton.com>
2019-09-13 13:59:55 +0000

    YUK-20582 Remove RSSI metadata

--- yukon-client/common/src/com/cannontech/common/rfn/message/node/WifiSuperMeterData.java
+++ yukon-client/common/src/com/cannontech/common/rfn/message/node/WifiSuperMeterData.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 public class WifiSuperMeterData implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    
+
     private String apSsid;
     
     private String configuredApBssid;
