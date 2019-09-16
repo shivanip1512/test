a71be81605a8248c8f4636562a85c5f95bfde198
Verma, Dhwaj <DhwajVerma@Eaton.com>
2019-09-16 10:41:10 +0000

    YUK-20408 : Issue in Creating Macro Load group using Select all matching items Link

--- yukon-client/yukon-web/src/com/cannontech/web/api/dr/setup/LMValidatorHelper.java
+++ yukon-client/yukon-web/src/com/cannontech/web/api/dr/setup/LMValidatorHelper.java
@@ -1,6 +1,6 @@
 package com.cannontech.web.api.dr.setup;
 
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -104,7 +104,6 @@ public class LMValidatorHelper {
      * Find duplicate entries from list and returns set of entries which are duplicate.
      */
     public Set<Integer> findDuplicates(List<Integer> list) {
-        Set<Integer> uniques = new HashSet<>();
-        return list.stream().filter(e -> !uniques.add(e)).collect(Collectors.toSet());
+        return list.stream().filter(e -> Collections.frequency(list, e) >1).collect(Collectors.toSet());
     }
 }
