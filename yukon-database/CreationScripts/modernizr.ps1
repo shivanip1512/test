3d16e5f724d8b9aa1db8a3ce3ee93f0457f09826
Fisher, Matthew S <MatthewSFisher@Eaton.com>
2019-09-18 20:05:35 +0000

    YUK-19063 Remote CI Meter Config tables Major overhaul: - Factored common replacement code out to a function - Changed to allow multiple replacements via hashmap - Fixed file replacement/delete logic - Added progress updates

--- yukon-database/CreationScripts/modernizr.ps1
+++ yukon-database/CreationScripts/modernizr.ps1
@@ -1,9 +1,49 @@
-cat .\OracleTableCreation.sql | % {$_ -replace "BINMax","BLOB  "} | out-file -Encoding utf8 .\OracleTableCreation.processed.sql
-Remove-Item .\OracleTableCreation.orig.sql
-Move-Item .\OracleTableCreation.sql .\OracleTableCreation.orig.sql
-Move-Item .\OracleTableCreation.processed.sql .\OracleTableCreation.sql
+function update-sql-keywords {
+    param ( 
+        [string] $filename, 
+        [hashtable] $replacements 
+    )
+    $rewritten = $filename + '.rewritten'
+    $filesize = (Get-Item $filename).Length
+    # use script scope so the callback can increment it
+    $script:changes = 0  
+    # join all keys into a single regex
+    $regex = [regex]($replacements.Keys -join "|") 
+    # callback used with Regex.Replace uses the found value to index into the replacement hashmap
+    $replacement_callback = { 
+        $script:changes++; 
+        $replacements[$args[0].Value] 
+    } 
+    
+    # Pipe to a file so we don't load all 700+kB of the file in at once
+    cat $filename | % { 
+        $processed += ([string]$_).Length + 2 # +2 for CRLF
+        $new_percent = $processed / $filesize * 100
+        if ($new_percent -ge $percent + 1) {
+            $percent = $new_percent
+            Write-Progress -Activity "Updating $filename" -PercentComplete $percent
+        }
+        $regex.Replace($_, $replacement_callback)  # replace all regexes at once using the callback
+    } | out-file -Encoding utf8 $rewritten
 
-cat .\SqlServerTableCreation.sql | % {$_ -replace "BINMax        ","varbinary(Max)"} | out-file -Encoding utf8 .\SqlServerTableCreation.processed.sql
-Remove-Item .\SqlServerTableCreation.orig.sql
-Move-Item .\SqlServerTableCreation.sql .\SqlServerTableCreation.orig.sql
-Move-Item .\SqlServerTableCreation.processed.sql .\SqlServerTableCreation.sql
+    if (!$changes) {
+        Write-Output "$filename had no changes"
+        Remove-Item $rewritten
+    } elseif (!(Test-Path -PathType Leaf $rewritten)) {
+        Write-Error "$filename could not be rewritten to $rewritten"
+    } else {
+        $pluralized = @('replacement','replacements')[$changes -ne 1]  # use false/true to index into the array for outputting the correct plural
+        Write-Output "$filename had $changes $pluralized"
+        Remove-Item $filename
+        Move-Item $rewritten $filename
+    }
+}
+
+$sql_filename = ".\SqlServerTableCreation.sql"
+$sql_replacements = @{ "BINMax        " = 
+                       "varbinary(Max)" }
+update-sql-keywords $sql_filename $sql_replacements
+
+$ora_filename = ".\OracleTableCreation.sql"
+$ora_replacements = @{ "BINMax" = "BLOB  " }
+update-sql-keywords $ora_filename $ora_replacements
