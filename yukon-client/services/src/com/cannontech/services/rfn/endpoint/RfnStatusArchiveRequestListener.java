e9b433278e80b4a06618c140062b90d41bf13985
Feldman, Marina <MarinaFeldman@Eaton.com>
2019-09-19 14:25:29 +0000

    YUK-20453 Auto Add Points for RFN Status Data

--- yukon-client/services/src/com/cannontech/services/rfn/endpoint/RfnStatusArchiveRequestListener.java
+++ yukon-client/services/src/com/cannontech/services/rfn/endpoint/RfnStatusArchiveRequestListener.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jmx.export.annotation.ManagedResource;
 
-import com.cannontech.amr.rfn.dao.RfnDeviceDao;
 import com.cannontech.amr.rfn.message.disconnect.RfnMeterDisconnectState;
 import com.cannontech.amr.rfn.message.status.RfnStatusArchiveRequest;
 import com.cannontech.amr.rfn.message.status.RfnStatusArchiveResponse;
@@ -30,23 +29,22 @@ import com.cannontech.common.pao.attribute.service.AttributeService;
 import com.cannontech.common.pao.attribute.service.IllegalUseOfAttribute;
 import com.cannontech.common.point.PointQuality;
 import com.cannontech.common.rfn.message.RfnIdentifier;
+import com.cannontech.common.rfn.model.RfnDevice;
+import com.cannontech.common.rfn.service.RfnDeviceLookupService;
 import com.cannontech.common.util.jms.api.JmsApiDirectory;
 import com.cannontech.core.dynamic.AsyncDynamicDataSource;
 import com.cannontech.database.data.lite.LitePoint;
-import com.cannontech.database.data.lite.LiteYukonPAObject;
 import com.cannontech.message.dispatch.message.PointData;
 import com.cannontech.services.rfn.RfnArchiveProcessor;
 import com.cannontech.services.rfn.RfnArchiveQueueHandler;
-import com.cannontech.yukon.IDatabaseCache;
 
 @ManagedResource
 public class RfnStatusArchiveRequestListener implements RfnArchiveProcessor {
     private static final Logger log = YukonLogManager.getLogger(RfnStatusArchiveRequestListener.class);
     @Autowired private RfnArchiveQueueHandler queueHandler;
     @Autowired private AttributeService attributeService;
-    @Autowired private RfnDeviceDao rfnDeviceDao;
+    @Autowired private RfnDeviceLookupService rfnDeviceLookupService;
     @Autowired private AsyncDynamicDataSource asyncDynamicDataSource;
-    @Autowired private IDatabaseCache dbCache;
     private JmsTemplate jmsTemplate;
     private Logger rfnCommsLog = YukonLogManager.getRfnLogger();
     /**
@@ -177,9 +175,8 @@ public class RfnStatusArchiveRequestListener implements RfnArchiveProcessor {
             String processor) {
         PointData pointData = null;
         try {
-            Integer id = rfnDeviceDao.getDeviceIdForRfnIdentifier(rfnIdentifier);
-            LiteYukonPAObject device = dbCache.getAllPaosMap().get(id);
-            LitePoint point = attributeService.getPointForAttribute(device, attribute);
+            RfnDevice rfnDevice = rfnDeviceLookupService.getDevice(rfnIdentifier);
+            LitePoint point = attributeService.createAndFindPointForAttribute(rfnDevice, attribute);
             pointData = new PointData();
             pointData.setId(point.getLiteID());
             pointData.setPointQuality(PointQuality.Normal);
