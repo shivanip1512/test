38450fab7354fd7758e10528d289c92b7ba27d79
E0521622 <nitinsshinde@eaton.com>
2019-10-16 15:06:18 +0530

    YUK-20748 : Yukon Rest API doesn't work by default on SSL Configurations that use an IP Address.

--- yukon-client/yukon-web/src/com/cannontech/web/api/ApiTestConnectionController.java
+++ yukon-client/yukon-web/src/com/cannontech/web/api/ApiTestConnectionController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ApiTestConnectionController {
 
-    @GetMapping("/test")
+    @GetMapping("/testyukon")
     public ResponseEntity<Object> testConnection() {
         return new ResponseEntity<>(HttpStatus.OK);
     }
