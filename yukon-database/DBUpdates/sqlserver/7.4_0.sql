03faf1afb4edd5cff9f6b158751c21d16a19a48a
E0534908 <JohnMJustin@Eaton.com>
2019-11-06 13:33:06 -0600

    YUK-20788 Add new Role Property for Custom Commands in Web Commander
    Undo merger - didnt work

--- yukon-database/DBUpdates/sqlserver/7.4_0.sql
+++ yukon-database/DBUpdates/sqlserver/7.4_0.sql
@@ -171,36 +171,6 @@ GO
 INSERT INTO DBUpdates VALUES ('YUK-20819', '7.3.2', GETDATE());
 /* @end YUK-20819 */
 
-/* @start YUK-20780 */
-UPDATE CommandRequestUnsupported
-SET Type = 'ALREADY_CONFIGURED'
-WHERE CommandRequestExecId in 
-    (SELECT DISTINCT CommandRequestExecId 
-    FROM CollectionActionCommandRequest
-    WHERE CollectionActionId in 
-        (SELECT CollectionActionId
-        FROM CollectionAction
-        WHERE Action = 'REMOVE_DATA_STREAMING'))
-AND Type = 'NOT_CONFIGURED';
-
-INSERT INTO DBUpdates VALUES ('YUK-20780', '7.4.0', GETDATE());
-/* @end YUK-20780 */
-
-/* @start YUK-20801 */
-ALTER TABLE MeterProgram
-ADD Password VARCHAR(200);
-GO
-
-UPDATE MeterProgram
-SET Password = '(none)';
-
-ALTER TABLE MeterProgram
-ALTER COLUMN Password VARCHAR(200) NOT NULL;
-GO
-
-INSERT INTO DBUpdates VALUES ('YUK-20801', '7.4.0', GETDATE());
-/* @end YUK-20801 */
-
 /* @start YUK-20788 */
 INSERT into YukonRoleProperty
 VALUES (-10320, -103, 'Manage Custom Commands', 'VIEW', 'Controls access to the ability to manage custom commands in web commander');
