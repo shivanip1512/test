737b76c9218f7e6bc895f6a86b35c9864e30be87
carrieVant <44090659+carrieVant@users.noreply.github.com>
2019-11-12 09:15:27 -0600

    YUK-20814 - Add color coded pins to Asset Availability and Broadcast Performance mapping   - added color coded map pins to Asset Availability and Broadcaset Performance

--- yukon-client/common/src/com/cannontech/dr/model/PerformanceVerificationMessageStatus.java
+++ yukon-client/common/src/com/cannontech/dr/model/PerformanceVerificationMessageStatus.java
@@ -1,9 +1,26 @@
 package com.cannontech.dr.model;
 
+import com.cannontech.common.i18n.DisplayableEnum;
 
-public enum PerformanceVerificationMessageStatus {
-    SUCCESS,
-    SUCCESS_UNENROLLED,
-    FAILURE,
-    UNKNOWN,
+public enum PerformanceVerificationMessageStatus implements DisplayableEnum {
+    SUCCESS("#093"),
+    SUCCESS_UNENROLLED("#093"),
+    FAILURE("#ec971f"),
+    UNKNOWN("#888");
+
+    private String color;
+    
+    private PerformanceVerificationMessageStatus(String color) {
+        this.color = color;
+    }
+    
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String getFormatKey() {
+        return "yukon.web.modules.dr.rf.broadcast.eventDetail.status." + name();
+    }
+
 }
