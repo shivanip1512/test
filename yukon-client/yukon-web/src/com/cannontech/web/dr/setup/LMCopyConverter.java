61668d75a5c676b28d533814e9adab4bc2e2bd9b
E0425883 <PrachiSTongaonkar@Eaton.com>
2019-11-15 17:34:54 +0530

    YUK-20976 - DBE to Web Group UI - Copy MCT Load Group functionality Added code to support copy functionality UI for MCT and Ripple load group.

--- yukon-client/yukon-web/src/com/cannontech/web/dr/setup/LMCopyConverter.java
+++ yukon-client/yukon-web/src/com/cannontech/web/dr/setup/LMCopyConverter.java
@@ -4,6 +4,7 @@ import org.springframework.core.convert.converter.Converter;
 
 import com.cannontech.common.dr.setup.LMCopy;
 import com.cannontech.common.dr.setup.LoadGroupCopy;
+import com.cannontech.common.pao.PaoType;
 
 /**
  * Class to convert appropriate Load Group copy object based upon the group type
@@ -13,11 +14,15 @@ public class LMCopyConverter implements Converter<String, LMCopy> {
     @Override
     public LMCopy convert(String groupType) {
         LMCopy loadGroup = null;
+        
+        PaoType paoType = PaoType.valueOf(groupType);
 
-        switch (groupType) {
-        case "LM_GROUP_EXPRESSCOMM":
-        case "LM_GROUP_EMETCON":
-        case "LM_GROUP_VERSACOM":
+        switch (paoType) {
+        case LM_GROUP_EXPRESSCOMM:
+        case LM_GROUP_EMETCON:
+        case LM_GROUP_VERSACOM:
+        case LM_GROUP_MCT:
+        case LM_GROUP_RIPPLE:
             loadGroup = new LoadGroupCopy();
             break;
         default:
