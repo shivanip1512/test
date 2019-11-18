aba0053c4f71c3a5688b7983e9f1e21624146460
E0425883 <PrachiSTongaonkar@Eaton.com>
2019-11-18 15:26:05 +0530

    YUK-20976 - DBE to Web Group UI - Copy MCT Load Group functionality Review comment fixes.

--- yukon-client/yukon-web/src/com/cannontech/web/dr/setup/LMCopyConverter.java
+++ yukon-client/yukon-web/src/com/cannontech/web/dr/setup/LMCopyConverter.java
@@ -1,7 +1,9 @@
 package com.cannontech.web.dr.setup;
 
+import org.apache.logging.log4j.Logger;
 import org.springframework.core.convert.converter.Converter;
 
+import com.cannontech.clientutils.YukonLogManager;
 import com.cannontech.common.dr.setup.LMCopy;
 import com.cannontech.common.dr.setup.LoadGroupCopy;
 import com.cannontech.common.pao.PaoType;
@@ -10,12 +12,19 @@ import com.cannontech.common.pao.PaoType;
  * Class to convert appropriate Load Group copy object based upon the group type
  */
 public class LMCopyConverter implements Converter<String, LMCopy> {
+    
+    private static final Logger log = YukonLogManager.getLogger(LMCopyConverter.class);
 
     @Override
     public LMCopy convert(String groupType) {
         LMCopy loadGroup = null;
+        PaoType paoType = null;
         
-        PaoType paoType = PaoType.valueOf(groupType);
+        try {
+            paoType = PaoType.valueOf(groupType);
+        } catch (IllegalArgumentException e) {
+            log.error(groupType + " pao type doesn't match with existing pao types", e);
+        }
 
         switch (paoType) {
         case LM_GROUP_EXPRESSCOMM:
