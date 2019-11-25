1e009b4004311784cb3cf986e1d9396ec59ad4b6
E0521622 <nitinsshinde@eaton.com>
2019-11-25 14:39:21 +0530

    YUK-20890 : DBE To Web Rest API - Ripple Group. Test cases for Ripple load group.

---
+++ yukon-test/restAssured/src/main/java/com/cannontech/rest/api/loadgroup/request/MockRippleAreaCode.java
@@ -0,0 +1,19 @@
+package com.cannontech.rest.api.loadgroup.request;
+
+public enum MockRippleAreaCode {
+    UNIVERSAL,
+    MINNKOTA,
+    BELTRAMI,
+    CASS_COUNTY,
+    CAVALIER_RURAL,
+    CLEARWATER_POLK,
+    NODAK_RURAL,
+    NORTH_STAR,
+    PKM_ELECTRIC,
+    RED_LAKE,
+    RED_RIVER_VALLEY,
+    ROSEAU_ELECTRIC,
+    SHEYENNE_VALLEY,
+    WILD_RICE,
+    NMPA;
+}
