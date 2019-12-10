455c783aebec0182878b37a2a95e791ce2db4266
E9816181 <MatthewSFisher@Eaton.com>
2019-12-10 13:37:29 -0800

    Added check to ensure command is targeted to EventManager.

--- yukon-server/RTDB/test_cmd_rfn_Wifi.cpp
+++ yukon-server/RTDB/test_cmd_rfn_Wifi.cpp
@@ -4,16 +4,28 @@
 #include "cmd_rfn_Wifi.h"
 #include "boost_test_helpers.h"
 
+namespace Cti::Messaging::Rfn {
+
+static std::ostream& operator<<(std::ostream& o, const ApplicationServiceIdentifiers asid)
+{
+    return o << "ASID:" << static_cast<uint8_t>(asid);
+}
+
+}
+
 BOOST_AUTO_TEST_SUITE( test_cmd_rfn_WifiCommunicationStatus )
 
 const CtiTime execute_time( CtiDate( 29, 7, 2013 ) , 11 );
 
 BOOST_AUTO_TEST_CASE( test_getStatusCommand )
 {
+    using ASID = Cti::Messaging::Rfn::ApplicationServiceIdentifiers;
+
     Cti::Devices::Commands::RfnWifiGetCommunicationStatusUpdateCommand command;
 
     BOOST_CHECK( ! command.isPost());
     BOOST_CHECK(command.isOneWay());
+    BOOST_CHECK_EQUAL(command.getApplicationServiceId(), ASID::EventManager);
 
     // execute
     {
