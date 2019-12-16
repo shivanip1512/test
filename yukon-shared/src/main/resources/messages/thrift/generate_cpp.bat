d98f567c10bd70424cd988535f6f256747b7504e
Shikha, Shikha <ShikhaShikha@eaton.com>
2019-12-16 18:47:23 +0530

    YUK-21105 : Upgrade libthrift-0.12.0.jar to libthrift 0.13.0

--- yukon-shared/src/main/resources/messages/thrift/generate_cpp.bat
+++ yukon-shared/src/main/resources/messages/thrift/generate_cpp.bat
@@ -1,4 +1,4 @@
-set THRIFT_VERSION=0.12.0
+set THRIFT_VERSION=0.13.0
 set CPP_OUTPUT_DIR=..\..\..\..\..\..\yukon-server\Message\Serialization\Thrift
 set THRIFT_EXE=%DEV_ENVIRONMENT%\libraries\apache\thrift\%THRIFT_VERSION%\bin\thrift-%THRIFT_VERSION%.exe
 if [%1] == [] (set FILES=*.thrift) ELSE (set FILES=%*)
