7b29aaa0ca415d0edd396737bd6deba60f5724fc
E0552673 <saurabhtiwari@eaton.com>
2019-12-27 14:19:06 +0530

    YUK-21161: Create Global Setting for IOT hub connection string:Conflict Resolved

--- yukon-client/common/src/com/cannontech/system/GlobalSettingType.java
+++ yukon-client/common/src/com/cannontech/system/GlobalSettingType.java
@@ -176,6 +176,9 @@ public enum GlobalSettingType implements DisplayableEnum {
     ERROR_REPORTING(GlobalSettingSubCategory.MISC, booleanType(), false),
     MAX_LOG_FILE_SIZE(GlobalSettingSubCategory.MISC, integerType(), 1),
     LOG_RETENTION_DAYS(GlobalSettingSubCategory.MISC, integerType(), 90),
+    NETWORK_MANAGER_DB_PASSWORD(GlobalSettingSubCategory.MISC, stringType(), null),
+    NETWORK_MANAGER_DB_USER(GlobalSettingSubCategory.MISC, stringType(), null),
+    NETWORK_MANAGER_DB_HOSTNAME(GlobalSettingSubCategory.MISC, stringType(), null),
     CLOUD_DATA_SENDING_FREQUENCY(GlobalSettingSubCategory.MISC, 6, Range.inclusive(1, Integer.MAX_VALUE)),
     CLOUD_IOT_HUB_CONNECTION_STRING(GlobalSettingSubCategory.MISC, stringType(), null),
 
