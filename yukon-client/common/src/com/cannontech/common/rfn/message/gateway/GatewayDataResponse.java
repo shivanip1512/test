bb84d01936a265bb102e034492bcb321287a36bf
E0534908 <JohnMJustin@Eaton.com>
2020-01-07 12:49:47 -0600

    YUK-21143 Process gateway connect/disconnect time
    added Null timestamp handling and fixed some formatting in the equals
    method

--- yukon-client/common/src/com/cannontech/common/rfn/message/gateway/GatewayDataResponse.java
+++ yukon-client/common/src/com/cannontech/common/rfn/message/gateway/GatewayDataResponse.java
@@ -385,138 +385,194 @@ public class GatewayDataResponse implements RfnIdentifyingMessage, Serializable 
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
+        if (this == obj) {
             return true;
-        if (obj == null)
+        }
+        if (obj == null) {
             return false;
-        if (getClass() != obj.getClass())
+        }
+        if (getClass() != obj.getClass()) {
             return false;
+        }
         GatewayDataResponse other = (GatewayDataResponse) obj;
         if (admin == null) {
-            if (other.admin != null)
+            if (other.admin != null) {
                 return false;
-        } else if (!admin.equals(other.admin))
+            }
+        } else if (!admin.equals(other.admin)) {
             return false;
+        }
         if (collectionSchedule == null) {
-            if (other.collectionSchedule != null)
+            if (other.collectionSchedule != null) {
                 return false;
-        } else if (!collectionSchedule.equals(other.collectionSchedule))
+            }
+        } else if (!collectionSchedule.equals(other.collectionSchedule)) {
             return false;
-        if (connectionStatus != other.connectionStatus)
+        }
+        if (connectionStatus != other.connectionStatus) {
             return false;
-        if (connectionStatusTimestamp != other.connectionStatusTimestamp)
+        }
+        if (connectionStatusTimestamp != other.connectionStatusTimestamp) {
             return false;
-        if (connectionType != other.connectionType)
+        }
+        if (connectionType != other.connectionType) {
             return false;
-        if (Double.doubleToLongBits(currentDataStreamingLoading) != Double.doubleToLongBits(other.currentDataStreamingLoading))
+        }
+        if (Double.doubleToLongBits(currentDataStreamingLoading) != Double.doubleToLongBits(other.currentDataStreamingLoading)) {
             return false;
-        if (gwTotalNodes != other.gwTotalNodes)
+        }
+        if (gwTotalNodes != other.gwTotalNodes) {
             return false;
-        if (gwTotalNodesNoInfo != other.gwTotalNodesNoInfo)
+        }
+        if (gwTotalNodesNoInfo != other.gwTotalNodesNoInfo) {
             return false;
-        if (gwTotalNodesWithInfo != other.gwTotalNodesWithInfo)
+        }
+        if (gwTotalNodesWithInfo != other.gwTotalNodesWithInfo) {
             return false;
-        if (gwTotalNodesWithSN != other.gwTotalNodesWithSN)
+        }
+        if (gwTotalNodesWithSN != other.gwTotalNodesWithSN) {
             return false;
-        if (gwTotalNotReadyNodes != other.gwTotalNotReadyNodes)
+        }
+        if (gwTotalNotReadyNodes != other.gwTotalNotReadyNodes) {
             return false;
-        if (gwTotalReadyNodes != other.gwTotalReadyNodes)
+        }
+        if (gwTotalReadyNodes != other.gwTotalReadyNodes) {
             return false;
+        }
         if (hardwareVersion == null) {
-            if (other.hardwareVersion != null)
+            if (other.hardwareVersion != null) {
                 return false;
-        } else if (!hardwareVersion.equals(other.hardwareVersion))
+            }
+        } else if (!hardwareVersion.equals(other.hardwareVersion)) {
             return false;
+        }
         if (ipAddress == null) {
-            if (other.ipAddress != null)
+            if (other.ipAddress != null) {
                 return false;
-        } else if (!ipAddress.equals(other.ipAddress))
+            }
+        } else if (!ipAddress.equals(other.ipAddress)) {
             return false;
+        }
         if (ipv6Prefix == null) {
-            if (other.ipv6Prefix != null)
+            if (other.ipv6Prefix != null) {
                 return false;
-        } else if (!ipv6Prefix.equals(other.ipv6Prefix))
+            }
+        } else if (!ipv6Prefix.equals(other.ipv6Prefix)) {
             return false;
+        }
         if (ipv6PrefixSuggested == null) {
-            if (other.ipv6PrefixSuggested != null)
+            if (other.ipv6PrefixSuggested != null) {
                 return false;
-        } else if (!ipv6PrefixSuggested.equals(other.ipv6PrefixSuggested))
+            }
+        } else if (!ipv6PrefixSuggested.equals(other.ipv6PrefixSuggested)) {
             return false;
-        if (lastCommStatus != other.lastCommStatus)
+        }
+        if (lastCommStatus != other.lastCommStatus) {
             return false;
-        if (lastCommStatusTimestamp != other.lastCommStatusTimestamp)
+        }
+        if (lastCommStatusTimestamp != other.lastCommStatusTimestamp) {
             return false;
-        if (Double.doubleToLongBits(maxDataStreamingCapacity) != Double.doubleToLongBits(other.maxDataStreamingCapacity))
+        }
+        if (Double.doubleToLongBits(maxDataStreamingCapacity) != Double.doubleToLongBits(other.maxDataStreamingCapacity)) {
             return false;
-        if (mode != other.mode)
+        }
+        if (mode != other.mode) {
             return false;
+        }
         if (name == null) {
-            if (other.name != null)
+            if (other.name != null) {
                 return false;
-        } else if (!name.equals(other.name))
+            }
+        } else if (!name.equals(other.name)) {
             return false;
+        }
         if (port == null) {
-            if (other.port != null)
+            if (other.port != null) {
                 return false;
-        } else if (!port.equals(other.port))
+            }
+        } else if (!port.equals(other.port)) {
             return false;
+        }
         if (radioVersion == null) {
-            if (other.radioVersion != null)
+            if (other.radioVersion != null) {
                 return false;
-        } else if (!radioVersion.equals(other.radioVersion))
+            }
+        } else if (!radioVersion.equals(other.radioVersion)) {
             return false;
+        }
         if (radios == null) {
-            if (other.radios != null)
+            if (other.radios != null) {
                 return false;
-        } else if (!radios.equals(other.radios))
+            }
+        } else if (!radios.equals(other.radios)) {
             return false;
+        }
         if (releaseVersion == null) {
-            if (other.releaseVersion != null)
+            if (other.releaseVersion != null) {
                 return false;
-        } else if (!releaseVersion.equals(other.releaseVersion))
+            }
+        } else if (!releaseVersion.equals(other.releaseVersion)) {
             return false;
+        }
         if (rfnIdentifier == null) {
-            if (other.rfnIdentifier != null)
+            if (other.rfnIdentifier != null) {
                 return false;
-        } else if (!rfnIdentifier.equals(other.rfnIdentifier))
+            }
+        } else if (!rfnIdentifier.equals(other.rfnIdentifier)) {
             return false;
-        if (routeColor != other.routeColor)
+        }
+        if (routeColor != other.routeColor) {
             return false;
+        }
         if (sequences == null) {
-            if (other.sequences != null)
+            if (other.sequences != null) {
                 return false;
-        } else if (!sequences.equals(other.sequences))
+            }
+        } else if (!sequences.equals(other.sequences)) {
             return false;
+        }
         if (softwareVersion == null) {
-            if (other.softwareVersion != null)
+            if (other.softwareVersion != null) {
                 return false;
-        } else if (!softwareVersion.equals(other.softwareVersion))
+            }
+        } else if (!softwareVersion.equals(other.softwareVersion)) {
             return false;
+        }
         if (superAdmin == null) {
-            if (other.superAdmin != null)
+            if (other.superAdmin != null) {
                 return false;
-        } else if (!superAdmin.equals(other.superAdmin))
+            }
+        } else if (!superAdmin.equals(other.superAdmin)) {
             return false;
+        }
         if (updateServerLogin == null) {
-            if (other.updateServerLogin != null)
+            if (other.updateServerLogin != null) {
                 return false;
-        } else if (!updateServerLogin.equals(other.updateServerLogin))
+            }
+        } else if (!updateServerLogin.equals(other.updateServerLogin)) {
             return false;
+        }
         if (updateServerUrl == null) {
-            if (other.updateServerUrl != null)
+            if (other.updateServerUrl != null) {
                 return false;
-        } else if (!updateServerUrl.equals(other.updateServerUrl))
+            }
+        } else if (!updateServerUrl.equals(other.updateServerUrl)) {
             return false;
+        }
         if (upperStackVersion == null) {
-            if (other.upperStackVersion != null)
+            if (other.upperStackVersion != null) {
                 return false;
-        } else if (!upperStackVersion.equals(other.upperStackVersion))
+            }
+        } else if (!upperStackVersion.equals(other.upperStackVersion)) {
             return false;
+        }
         if (versionConflicts == null) {
-            if (other.versionConflicts != null)
+            if (other.versionConflicts != null) {
                 return false;
-        } else if (!versionConflicts.equals(other.versionConflicts))
+            }
+        } else if (!versionConflicts.equals(other.versionConflicts)) {
             return false;
+        }
         return true;
     }
 
