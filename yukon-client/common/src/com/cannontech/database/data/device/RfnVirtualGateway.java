f0d70fbe3257dee151fe401913bb9e501042abd8
E9816170 <StaceyLNelson@Eaton.com>
2020-01-17 14:30:20 -0600

    YUK-21326 Gateway deletion fails due to FK_DynRfnDevData_RfnAddr_GatId
     - Added deletion of DynamicRfnDeviceData to dbPersistent deletion handling.

--- yukon-client/common/src/com/cannontech/database/data/device/RfnVirtualGateway.java
+++ yukon-client/common/src/com/cannontech/database/data/device/RfnVirtualGateway.java
@@ -6,7 +6,7 @@ import com.cannontech.common.pao.PaoType;
  * Object representation of the WI-FI Super Meter Virtual Gateway device stored in Yukon DB. For most purposes, 
  * com.cannontech.common.rfn.model.RfnVirtualGateway will be preferable.
  */
-public class RfnVirtualGateway extends RfnBase {
+public class RfnVirtualGateway extends RfnGatewayBase {
     
     public RfnVirtualGateway() {
         super(PaoType.VIRTUAL_GATEWAY);
