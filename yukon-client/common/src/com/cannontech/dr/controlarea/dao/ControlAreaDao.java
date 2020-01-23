07e6fde3805283bd4d2d25baef5b06f96a176935
E0471478 <ManenderSingh@eaton.com>
2020-01-23 13:10:57 +0530

    YUK-21342 - Backend changes - Add more fields in Control Area filter to initial DR Setup page

--- yukon-client/common/src/com/cannontech/dr/controlarea/dao/ControlAreaDao.java
+++ yukon-client/common/src/com/cannontech/dr/controlarea/dao/ControlAreaDao.java
@@ -1,11 +1,16 @@
 package com.cannontech.dr.controlarea.dao;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import com.cannontech.dr.controlarea.model.ControlArea;
+import com.cannontech.dr.controlarea.model.ControlAreaTrigger;
 
 public interface ControlAreaDao {
     public ControlArea getControlArea(int controlAreaId);
     
     public Set<Integer> getProgramIdsForControlArea(int controlAreaId);
+    
+    Map<Integer, List<ControlAreaTrigger>> getControlAreaTriggers(int controlAreaId);
 }
