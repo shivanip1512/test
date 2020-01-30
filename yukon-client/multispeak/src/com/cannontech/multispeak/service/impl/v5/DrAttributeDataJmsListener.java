2e580e70622e48fae3678e9cce6671708eabc291
E0552673 <saurabhtiwari@eaton.com>
2020-01-30 14:10:11 +0530

    YUK-21360 MDM message should not be send for Meters:

--- yukon-client/multispeak/src/com/cannontech/multispeak/service/impl/v5/DrAttributeDataJmsListener.java
+++ yukon-client/multispeak/src/com/cannontech/multispeak/service/impl/v5/DrAttributeDataJmsListener.java
@@ -8,6 +8,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.cannontech.clientutils.YukonLogManager;
+import com.cannontech.common.pao.PaoType;
 import com.cannontech.common.pao.attribute.model.BuiltInAttribute;
 import com.cannontech.common.pao.attribute.service.AttributeService;
 import com.cannontech.core.dynamic.RichPointData;
@@ -24,46 +25,50 @@ public class DrAttributeDataJmsListener implements RichPointDataListener {
     private static Set<BuiltInAttribute> attributes = Sets.union(
             Sets.union(BuiltInAttribute.getVoltageAttributes(), BuiltInAttribute.getRelayDataAttributes()),
             BuiltInAttribute.getItronLcrAttributes());
+    private static Set<PaoType> supportedPaoTypes = Sets.union(PaoType.getItronTypes(), PaoType.getTwoWayLcrTypes());
 
     private static final Logger log = YukonLogManager.getLogger(DrAttributeDataJmsListener.class);
 
     @Override
     public void pointDataReceived(RichPointData richPointData) {
-        Set<BuiltInAttribute> supportedAttributes = attributeService.findAttributesForPoint(
-                richPointData.getPaoPointIdentifier().getPaoTypePointIdentifier(),
-                attributes);
-        if (!supportedAttributes.isEmpty()) {
-            DrAttributeDataJmsMessage attributeDataJmsMessage = new DrAttributeDataJmsMessage();
-            attributeDataJmsMessage.setPaoPointIdentifier(richPointData.getPaoPointIdentifier());
+        if (supportedPaoTypes.contains(richPointData.getPaoPointIdentifier().getPaoIdentifier().getPaoType())) {
+            Set<BuiltInAttribute> supportedAttributes = attributeService.findAttributesForPoint(
+                    richPointData.getPaoPointIdentifier().getPaoTypePointIdentifier(),
+                    attributes);
+            if (!supportedAttributes.isEmpty()) {
+                DrAttributeDataJmsMessage attributeDataJmsMessage = new DrAttributeDataJmsMessage();
+                attributeDataJmsMessage.setPaoPointIdentifier(richPointData.getPaoPointIdentifier());
 
-            List<DrAttributeData> attributeDataList = new ArrayList<>();
-            for (BuiltInAttribute attribute : supportedAttributes) {
-                DrAttributeData attributeData = new DrAttributeData();
+                List<DrAttributeData> attributeDataList = new ArrayList<>();
+                for (BuiltInAttribute attribute : supportedAttributes) {
+                    DrAttributeData attributeData = new DrAttributeData();
 
-                attributeData.setAttribute(attribute);
-                attributeData.setTimeStamp(richPointData.getPointValue().getPointDataTimeStamp());
-                attributeData.setValue(richPointData.getPointValue().getValue());
+                    attributeData.setAttribute(attribute);
+                    attributeData.setTimeStamp(richPointData.getPointValue().getPointDataTimeStamp());
+                    attributeData.setValue(richPointData.getPointValue().getValue());
 
-                attributeDataList.add(attributeData);
+                    attributeDataList.add(attributeData);
 
-            }
-            attributeDataJmsMessage.setAttributeDataList(attributeDataList);
+                }
+                attributeDataJmsMessage.setAttributeDataList(attributeDataList);
 
-            setMessageTypeForAttribute(supportedAttributes, attributeDataJmsMessage);
+                setMessageTypeForAttribute(supportedAttributes, attributeDataJmsMessage);
 
-            switch (attributeDataJmsMessage.getMessageType()) {
-            case RELAYDATA:
-                drJmsMessageService.intervalDataNotification(attributeDataJmsMessage);
-                break;
-            case VOLTAGEDATA:
-                drJmsMessageService.voltageMeterReadingsNotification(attributeDataJmsMessage);
-                break;
-            case ALARMANDEVENT:
-                drJmsMessageService.alarmAndEventNotification(attributeDataJmsMessage);
-                break;
-            default:
-                log.debug("Unable to find proper multispeak Dr message type i.e: " + attributeDataJmsMessage.getMessageType());
-                break;
+                switch (attributeDataJmsMessage.getMessageType()) {
+                case RELAYDATA:
+                    drJmsMessageService.intervalDataNotification(attributeDataJmsMessage);
+                    break;
+                case VOLTAGEDATA:
+                    drJmsMessageService.voltageMeterReadingsNotification(attributeDataJmsMessage);
+                    break;
+                case ALARMANDEVENT:
+                    drJmsMessageService.alarmAndEventNotification(attributeDataJmsMessage);
+                    break;
+                default:
+                    log.debug("Unable to find proper multispeak Dr message type i.e: "
+                            + attributeDataJmsMessage.getMessageType());
+                    break;
+                }
             }
         }
     }
