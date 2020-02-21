3471d1a6eaef1937454f98b6f0497d96805dc779
E0507716 <DhwajVerma@eaton.com>
2020-02-21 18:03:15 +0530

    YUK-21245 : In Load group Copy functionality, Invalid routeId validation for non applicable load groups

--- yukon-client/yukon-web/src/com/cannontech/web/api/dr/setup/LMCopyValidator.java
+++ yukon-client/yukon-web/src/com/cannontech/web/api/dr/setup/LMCopyValidator.java
@@ -4,15 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.Errors;
 
 import com.cannontech.common.dr.setup.LMCopy;
-import com.cannontech.common.dr.setup.LoadGroupCopy;
-import com.cannontech.common.pao.PaoType;
 import com.cannontech.common.validator.SimpleValidator;
-import com.cannontech.core.dao.PaoDao;
-import com.cannontech.stars.util.ServletUtils;
 
 public class LMCopyValidator extends SimpleValidator<LMCopy> {
     @Autowired private LMValidatorHelper lmValidatorHelper;
-    @Autowired private PaoDao paoDao;
 
     public LMCopyValidator() {
         super(LMCopy.class);
@@ -23,6 +18,7 @@ public class LMCopyValidator extends SimpleValidator<LMCopy> {
 
         // Group Name
         lmValidatorHelper.validateCopyPaoName(lmCopy.getName(), errors, "Name");
+
         // Validate routeId if present.
         lmValidatorHelper.validateRouteId(lmCopy, lmCopy.getName(), errors, "RouteId");
     }
