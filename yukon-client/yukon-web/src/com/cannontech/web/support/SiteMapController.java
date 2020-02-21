c7be6a9c999df0a3109a1020430a0a71644c6231
E0569837 <BradyWAnderson@eaton.com>
2020-02-21 09:50:24 -0600

    YUK-21496 Consumer user should not have access to
    Removed permission check from sitemap, as it is now handled by the same
    interceptor as support

--- yukon-client/yukon-web/src/com/cannontech/web/support/SiteMapController.java
+++ yukon-client/yukon-web/src/com/cannontech/web/support/SiteMapController.java
@@ -5,29 +5,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.cannontech.common.exception.NotAuthorizedException;
-import com.cannontech.core.roleproperties.YukonRole;
-import com.cannontech.core.roleproperties.YukonRoleProperty;
-import com.cannontech.core.roleproperties.dao.RolePropertyDao;
-import com.cannontech.database.data.lite.LiteYukonUser;
 import com.cannontech.user.YukonUserContext;
-import com.cannontech.web.security.annotation.CheckRoleProperty;
 
 @Controller
 public class SiteMapController {
     
     @Autowired private SiteMapHelper siteMapHelper;
-    @Autowired private RolePropertyDao rolePropertyDao;
 
     @RequestMapping("/sitemap")
     public String view(ModelMap model, YukonUserContext context) {
-        LiteYukonUser user = context.getYukonUser();
-        boolean hasCIRole = rolePropertyDao.checkRole(YukonRole.CI_CURTAILMENT, user);
-        boolean isCIOperator = rolePropertyDao.checkProperty(YukonRoleProperty.CURTAILMENT_IS_OPERATOR, user);
-        if (hasCIRole && !isCIOperator) {
-            throw new NotAuthorizedException("User " + user + " is not authorized to access this page.");
-        }
-
         model.addAttribute("pageMap", siteMapHelper.getSiteMap(context));
         return "siteMap.jsp";
     }
