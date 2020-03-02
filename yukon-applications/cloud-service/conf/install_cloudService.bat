b60aeaa9b83bf4c959c1fe08b6694fedd3cdaff8
E0552673 <saurabhtiwari@eaton.com>
2020-03-02 18:18:12 +0530

    YUK-21583:Set YUKON_BASE environment variable

--- yukon-applications/cloud-service/conf/install_cloudService.bat
+++ yukon-applications/cloud-service/conf/install_cloudService.bat
@@ -1,2 +1,9 @@
-..\..\Runtime\bin\java -jar wrapper.jar -i cloudService.conf
+IF NOT "%YUKON_BASE%"=="" GOTO USAGE
+cd ..
+cd ..
+mkdir Yukon
+setx YUKON_BASE "C:\Yukon" /M
+set path=%YUKON_BASE%;%PATH%
+:USAGE
+%YUKON_BASE%\Runtime\bin\java -jar %YUKON_BASE%\Client\bin\wrapper.jar -i %YUKON_BASE%\Client\bin\cloudService.conf
 timeout 10
