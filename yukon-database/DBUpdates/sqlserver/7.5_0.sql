f3c1a2d0bc1736418c756859064d2cc7b42bb0bf
E0534908 <JohnMJustin@Eaton.com>
2020-03-04 16:31:55 -0600

    YUK-21642 DB Change - add DescendantCount field to DynamicRfnDeviceData
    Updated creation and 7.5.0 update scripts

--- yukon-database/DBUpdates/sqlserver/7.5_0.sql
+++ yukon-database/DBUpdates/sqlserver/7.5_0.sql
@@ -2,6 +2,40 @@
 /**** SQL Server DBupdates             ****/
 /******************************************/
 
+/* @start YUK-21642 */
+ALTER TABLE DynamicRfnDeviceData
+ALTER COLUMN GatewayId NUMERIC NOT NULL;
+GO
+
+ALTER TABLE DynamicRfnDeviceData
+DROP COLUMN LastTransferTime;
+GO
+
+ALTER TABLE DynamicRfnDeviceData
+ADD DescendantCount NUMERIC NULL;
+GO
+
+ALTER TABLE DynamicRfnDeviceData
+ADD LastTransferTime datetime NULL;
+GO
+
+UPDATE DynamicRfnDeviceData
+SET DescendantCount = -1;
+
+UPDATE DynamicRfnDeviceData
+SET LastTransferTime = GETDATE();
+go
+
+ALTER TABLE DynamicRfnDeviceData
+ALTER COLUMN DescendantCount NUMERIC NOT NULL;
+
+ALTER TABLE DynamicRfnDeviceData
+ALTER COLUMN LastTransferTime datetime NOT NULL;
+GO
+
+INSERT INTO DBUpdates VALUES ('YUK-21642', '7.5.0', GETDATE());
+/* @end YUK-21642 */
+
 /**************************************************************/
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
