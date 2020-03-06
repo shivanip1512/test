b89d4f178be61c25c2cebcaaf048d3f2f190347a
E9816147 <JessMOtteson@Eaton.com>
2020-03-06 15:15:59 -0600

    Preparing for 7.3.3 build

--- yukon-database/DBUpdates/sqlserver/7.3_3.sql
+++ yukon-database/DBUpdates/sqlserver/7.3_3.sql
@@ -6,4 +6,4 @@
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
 /**************************************************************/
-/*INSERT INTO CTIDatabase VALUES ('7.3', '15-NOV-2019', 'Latest Update', 3, GETDATE());*/
+INSERT INTO CTIDatabase VALUES ('7.3', '06-MAR-2020', 'Latest Update', 3, GETDATE());
