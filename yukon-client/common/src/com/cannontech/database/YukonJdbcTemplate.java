b0723ca79278976d9ebe1c36f91654e8cc4bb586
E0534908 <JohnMJustin@Eaton.com>
2020-03-30 10:13:40 -0500

    YUK-21714 Batch update - possibly increase the batch size
    Added MasterConfig Option for easier testing

--- yukon-client/common/src/com/cannontech/database/YukonJdbcTemplate.java
+++ yukon-client/common/src/com/cannontech/database/YukonJdbcTemplate.java
@@ -23,6 +23,8 @@ import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.jdbc.core.RowMapper;
 
 import com.cannontech.clientutils.YukonLogManager;
+import com.cannontech.common.config.ConfigurationSource;
+import com.cannontech.common.config.MasterConfigInteger;
 import com.cannontech.common.util.DatabaseRepresentationSource;
 import com.cannontech.common.util.SqlFragmentSource;
 import com.cannontech.common.util.SqlStatementBuilder;
@@ -33,7 +35,10 @@ import com.google.common.collect.Lists;
 public class YukonJdbcTemplate extends JdbcTemplate {
     private static final Logger log = YukonLogManager.getLogger(YukonJdbcTemplate.class);
     
-    public static final int DEFAULT_BATCH_SIZE = 50000;
+    private static final int defaultBatchSize = 1000;
+    
+    private static ConfigurationSource configSource = null;
+    private int masterConfigBatchSize = configSource.getInteger(MasterConfigInteger.DATABASE_BATCH_SIZE, defaultBatchSize);
 
     public YukonJdbcTemplate(DataSource dataSource) {
         super(dataSource);
@@ -126,7 +131,7 @@ public class YukonJdbcTemplate extends JdbcTemplate {
      * @param sql An SqlStatementBuilder configured for batch update via the batchInsertInto method.
      */
     public void yukonBatchUpdate(SqlStatementBuilder sql) {
-        yukonBatchUpdate(sql, DEFAULT_BATCH_SIZE);
+        yukonBatchUpdate(sql, masterConfigBatchSize);
     }
 
     /**
