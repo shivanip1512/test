cb5959e17a40f4aa01bda9ad6961b7fcd2393d89
E0534908 <JohnMJustin@Eaton.com>
2020-04-08 15:34:34 -0500

    YUK-21914 Disable test connection feature for Virtual Gateways
    Updated logic for adding the attribute to the model

--- yukon-client/yukon-web/src/com/cannontech/web/widget/gatewayInfo/GatewayInformationWidget.java
+++ yukon-client/yukon-web/src/com/cannontech/web/widget/gatewayInfo/GatewayInformationWidget.java
@@ -104,11 +104,7 @@ public class GatewayInformationWidget extends AdvancedWidgetControllerBase {
         try {
             
             RfnGateway gateway = rfnGatewayService.getGatewayByPaoIdWithData(deviceId);
-            if (gateway.getPaoIdentifier().getPaoType() == PaoType.VIRTUAL_GATEWAY) {
-                model.addAttribute("isVirtualGateway", true);
-            } else {
-                model.addAttribute("isVirtualGateway", false);
-            }
+            model.addAttribute("isVirtualGateway", gateway.getPaoIdentifier().getPaoType() == PaoType.VIRTUAL_GATEWAY);
             GatewaySettings settings = rfnGatewayService.gatewayAsSettings(gateway);
             model.addAttribute("settings", settings);
             
