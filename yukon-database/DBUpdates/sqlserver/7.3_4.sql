22c3953db3dc8df7f6945951004a3b483e7b6222
E9816170 <StaceyLNelson@Eaton.com>
2020-04-10 13:16:13 -0500

    Added 7.3.4 update scripts

--- yukon-database/DBUpdates/sqlserver/7.3_4.sql
+++ yukon-database/DBUpdates/sqlserver/7.3_4.sql
@@ -6,4 +6,4 @@
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
 /**************************************************************/
-/*INSERT INTO CTIDatabase VALUES ('7.3', '06-MAR-2020', 'Latest Update', 4, GETDATE());*/
+INSERT INTO CTIDatabase VALUES ('7.3', '10-APR-2020', 'Latest Update', 4, GETDATE());
