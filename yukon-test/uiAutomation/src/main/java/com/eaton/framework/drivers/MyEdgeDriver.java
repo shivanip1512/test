64fc58b03bd0b4d016b9060236ef064640f570ed
E0566392 <ElyshaAnderson@Eaton.com>
2020-04-13 11:03:11 -0500

    removed driver path from config file since using webdriver manager.
    Edge driver not working with webdriver manager so for now hard coded
    driver location.

--- yukon-test/uiAutomation/src/main/java/com/eaton/framework/drivers/MyEdgeDriver.java
+++ yukon-test/uiAutomation/src/main/java/com/eaton/framework/drivers/MyEdgeDriver.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 public final class MyEdgeDriver {
 
     private MyEdgeDriver() {
@@ -17,23 +15,27 @@ public final class MyEdgeDriver {
 
     public static WebDriver getNewEdgeDriver(boolean useRemoteDriver) {
 
-        WebDriver driver;
+        WebDriver driver;  
+                
+        //Hard coding edge driver for now since WebDriverManager is not working correctly
+        //Change the location to point to where your local version of the edge driver is located
+        System.setProperty("webdriver.edge.driver", "C:\\dev\\workspaces\\yukon\\yukon\\yukon-test\\uiAutomation\\SeleniumDrivers\\" + "msedgedriver.exe");
         
-        WebDriverManager.edgedriver().setup();
-
-        EdgeOptions options = new EdgeOptions();
+        //WebDriverManager.edgedriver().version("80.0.361.111").setup();
+        
+        EdgeOptions options = new EdgeOptions();                   
 
         if (useRemoteDriver) {
             driver = new RemoteWebDriver(options);
         } else
             driver = new EdgeDriver(options);
-
+        
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        EventFiringWebDriver firingDriver = new EventFiringWebDriver(driver);
+        EventFiringWebDriver firingDriver = new EventFiringWebDriver(driver);                
 
         driver = firingDriver;
 
