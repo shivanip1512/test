349016b78094d457ac06e01b0d83df95b650c0ba
Shikha <ShikhaShikha@eaton.com>
2020-04-16 20:10:07 +0530

    YUK-20981 : MultiSpeak Setup UI - endpoint (interface) level authentication

--- yukon-client/multispeak/src/com/cannontech/multispeak/dao/impl/MultispeakDaoImpl.java
+++ yukon-client/multispeak/src/com/cannontech/multispeak/dao/impl/MultispeakDaoImpl.java
@@ -206,10 +206,10 @@ public final class MultispeakDaoImpl implements MultispeakDao {
             sink.addValue("Interface", mspInterface.getMspInterface().trim());
             sink.addValue("Endpoint", mspInterface.getMspEndpoint().trim());
             sink.addValue("Version", mspInterface.getVersion());
-            sink.addValue("InUserName", mspInterface.getInUserName());
-            sink.addValue("InPassword", mspInterface.getInPassword());
-            sink.addValue("OutUserName", mspInterface.getOutUserName());
-            sink.addValue("OutPassword", mspInterface.getOutPassword());
+            sink.addValueSafe("InUserName", mspInterface.getInUserName());
+            sink.addValueSafe("InPassword", mspInterface.getInPassword());
+            sink.addValueSafe("OutUserName", mspInterface.getOutUserName());
+            sink.addValueSafe("OutPassword", mspInterface.getOutPassword());
             sink.addValue("UseVendorAuth", mspInterface.getUseVendorAuth());
             sink.addValue("ValidateCertificate", mspInterface.getValidateCertificate());
             
