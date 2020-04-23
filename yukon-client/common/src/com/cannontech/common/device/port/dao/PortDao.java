06795429e4250acc605c35e2a65630cd988fe3f0
E0507716 <DhwajVerma@eaton.com>
2020-04-23 13:08:41 +0530

    YUK-22009 : Add Dao layer to retrieve IPAddress and Port number from PortTerminalServer

--- yukon-client/common/src/com/cannontech/common/device/port/dao/PortDao.java
+++ yukon-client/common/src/com/cannontech/common/device/port/dao/PortDao.java
@@ -1,11 +1,9 @@
 package com.cannontech.common.device.port.dao;
 
-import com.cannontech.database.db.port.PortTerminalServer;
-
 public interface PortDao {
 
     /**
-     * Find PortTerminalServer object based on IP Address and Port number.
+     * Find Port Id based on IP Address and Port number.
      */
-    public PortTerminalServer findPortTerminalServer(String ipAddress, Integer port);
+    public Integer findUniquePortTerminalServer(String ipAddress, Integer port);
 }
--- yukon-client/common/src/com/cannontech/common/device/port/dao/impl/PortDaoImpl.java
+++ yukon-client/common/src/com/cannontech/common/device/port/dao/impl/PortDaoImpl.java
@@ -6,32 +6,19 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import com.cannontech.common.device.port.dao.PortDao;
 import com.cannontech.common.util.SqlStatementBuilder;
 import com.cannontech.database.YukonJdbcTemplate;
-import com.cannontech.database.YukonRowMapper;
-import com.cannontech.database.db.port.PortTerminalServer;
-import com.cannontech.database.db.port.PortTerminalServer.EncodingType;
 
 public class PortDaoImpl implements PortDao {
     @Autowired private YukonJdbcTemplate jdbcTemplate;
 
-    private static final YukonRowMapper<PortTerminalServer> portTerminalServerRowMapper = rs -> {
-        PortTerminalServer row = new PortTerminalServer();
-        row.setPortID(rs.getInt("PortId"));
-        row.setIpAddress(rs.getString("IpAddress"));
-        row.setSocketPortNumber(rs.getInt("SocketPortNumber"));
-        row.setEncodingKey(rs.getString("EncodingKey"));
-        row.setEncodingType(rs.getEnum("EncodingType", EncodingType.class));
-        return row;
-    };
-
     @Override
-    public PortTerminalServer findPortTerminalServer(String ipAddress, Integer port) {
+    public Integer findUniquePortTerminalServer(String ipAddress, Integer port) {
         SqlStatementBuilder sql = new SqlStatementBuilder();
-        sql.append("SELECT * ");
+        sql.append("SELECT PortId ");
         sql.append("FROM PortTerminalServer");
         sql.append("WHERE IpAddress").eq(ipAddress);
         sql.append("  AND SocketPortNumber").eq(port);
         try {
-            return jdbcTemplate.queryForObject(sql, portTerminalServerRowMapper);
+            return jdbcTemplate.queryForInt(sql);
         } catch (EmptyResultDataAccessException e) {
             return null;
         }
