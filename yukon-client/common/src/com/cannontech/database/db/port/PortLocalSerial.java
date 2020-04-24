c1fb40e3e083041ea38e402c29e7c3f0bdca7228
E0521622 <nitinsshinde@eaton.com>
2020-04-24 11:51:16 +0530

    YUK-21898 : API implementation for Local Serial Port (Dedicated). Fixed review comment.

--- yukon-client/common/src/com/cannontech/database/db/port/PortLocalSerial.java
+++ yukon-client/common/src/com/cannontech/database/db/port/PortLocalSerial.java
@@ -8,35 +8,23 @@ package com.cannontech.database.db.port;
  
 public class PortLocalSerial extends DBPersistent {
 	
-	private String physicalPort = null;
+	private String physicalPort = "";
 	private Integer portID = null;
+	
+	
+    public void initialize(Integer portID, String physicalPort) {
+        setPortID(portID);
+        setPhysicalPort((physicalPort == null) ? "" : physicalPort);
+    }
 /**
- * SerialPortSettings constructor comment.
+ * SerialPortSettings constructor comment.public PortLocalSerial( 
+ * 
+ * 
  */
-public PortLocalSerial() {
-	super();
-	initialize( null, "" );
-}
-/**
- * This method was created in VisualAge.
- * @param portNumber java.lang.Integer
- */
-public PortLocalSerial( Integer portNumber ) {
-	super();
-	initialize( portNumber, null );
-}
-/**
- * This method was created in VisualAge.
- * @param portNumber java.lang.Integer
- * @param physicalPort java.lang.String
- * @param baudRate java.lang.Integer
- * @param cdWait java.lang.Integer
- */
-public PortLocalSerial( Integer portNumber, String physicalPort ) {
-	super();
+    public PortLocalSerial() {
+        super();
+    }
 
-	initialize( portNumber, physicalPort );
-}
 /**
  * add method comment.
  */
@@ -67,18 +55,7 @@ public String getPhysicalPort() {
 public Integer getPortID() {
 	return portID;
 }
-/**
- * This method was created in VisualAge.
- * @param portNumber java.lang.Integer
- * @param physicalPort java.lang.String
- * @param baudRate java.lang.Integer
- * @param cdWait java.lang.Integer
- */
-public void initialize( Integer portID, String physicalPort ) {
 
-	setPortID( portID );
-	setPhysicalPort( physicalPort );
-}
 /**
  * retrieve method comment.
  */
