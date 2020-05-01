fa381d9e6e9d75531a1e9957e8f3eb53a18c1640
E9816170 <StaceyLNelson@Eaton.com>
2020-05-01 10:18:10 -0500

    YUK-22079 Fix discrepancy between DBEditor and web for program field in load Group.
    - fixed program and splinter range to 1-254

--- yukon-database/DBUpdates/oracle/7.4_1.sql
+++ yukon-database/DBUpdates/oracle/7.4_1.sql
@@ -2,6 +2,44 @@
 /****     Oracle DBupdates             ****/ 
 /******************************************/ 
 
+/* @start YUK-21642 */
+DROP INDEX INDX_DynRfnDevData_GatewayId;
+
+ALTER TABLE DynamicRfnDeviceData
+MODIFY GatewayId NUMBER NOT NULL;
+
+CREATE INDEX INDX_DynRfnDevData_GatewayId ON DynamicRfnDeviceData (
+GatewayId ASC
+);
+
+ALTER TABLE DynamicRfnDeviceData
+ADD DescendantCount NUMBER NULL;
+
+UPDATE DynamicRfnDeviceData
+SET DescendantCount = -1;
+
+ALTER TABLE DynamicRfnDeviceData
+MODIFY DescendantCount NUMBER NOT NULL;
+
+ALTER TABLE DynamicRfnDeviceData
+ADD LastTransferTimeNew DATE NULL;
+
+UPDATE DynamicRfnDeviceData
+SET LastTransferTimeNew = LastTransferTime;
+
+ALTER TABLE DynamicRfnDeviceData
+MODIFY LastTransferTimeNew DATE NOT NULL;
+
+ALTER TABLE DynamicRfnDeviceData
+DROP COLUMN LastTransferTime;
+
+ALTER TABLE DynamicRfnDeviceData
+RENAME COLUMN LastTransferTimeNew
+TO LastTransferTime;
+
+INSERT INTO DBUpdates VALUES ('YUK-21642', '7.4.1', SYSDATE);
+/* @end YUK-21642 */
+
 /* @start YUK-21621 */
 INSERT INTO StateGroup VALUES(-29, 'Meter Programming', 'Status');
 INSERT INTO State VALUES(-29, 0, 'Success', 0, 6, 0);
@@ -63,4 +101,4 @@ INSERT INTO DBUpdates VALUES ('YUK-21777', '7.4.1', SYSDATE);
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
 /**************************************************************/
-/*INSERT INTO CTIDatabase VALUES ('7.4', '10-FEB-2020', 'Latest Update', 1, SYSDATE);*/
+INSERT INTO CTIDatabase VALUES ('7.4', '01-MAY-2020', 'Latest Update', 1, SYSDATE);
