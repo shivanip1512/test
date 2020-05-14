7b345186f7f19691d1427d3e8dd47abe91f1425b
Rawat, Payal <payalrawat@eaton.com>
2020-05-14 11:45:42 +0530

    YUK-22073: Add processors for Electric Read Rate and Water Read Rate Fixed review comments

--- yukon-client/common/src/com/cannontech/common/device/data/collection/service/DataCollectionHelper.java
+++ yukon-client/common/src/com/cannontech/common/device/data/collection/service/DataCollectionHelper.java
@@ -7,11 +7,8 @@ import java.util.TreeMap;
 
 import org.apache.logging.log4j.Logger;
 import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
 import org.joda.time.Duration;
 import org.joda.time.Instant;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 
 import com.cannontech.clientutils.YukonLogManager;
 import com.cannontech.common.device.data.collection.dao.RecentPointValueDao.RangeType;
@@ -29,7 +26,6 @@ public class DataCollectionHelper {
      * Creates time ranges for each range type.
      */
     public static Map<RangeType, Range<Instant>> getRanges(Duration windowInDays) {
-        // Duration days = Duration.standardDays(globalSettingDao.getInteger(GlobalSettingType.DATA_AVAILABILITY_WINDOW_IN_DAYS));
         Map<RangeType, Range<Instant>> ranges = new TreeMap<>();
         Instant startOfTheDay = new Instant(new DateTime().withTimeAtStartOfDay());
         Range<Instant> currentRange = buildRange(RangeType.AVAILABLE, ranges, startOfTheDay.minus(windowInDays), now());
@@ -53,17 +49,6 @@ public class DataCollectionHelper {
      * Builds a string message that describes the range. Used for logging.
      */
     public static String getLogString(RangeType type, Range<Instant> range) {
-        final DateTimeFormatter df = DateTimeFormat.forPattern("MMM dd YYYY HH:mm:ss");
-        String min = range.getMin() == null ? "" : range.getMin().toString(df.withZone(DateTimeZone.getDefault()));
-        String max = range.getMax() == null ? "" : range.getMax().toString(df.withZone(DateTimeZone.getDefault()));
-        String includesMin = " [exclusive] ";
-        String includesMax = " [exclusive] ";
-        if (range.isIncludesMinValue()) {
-            includesMin = " [inclusive] ";
-        }
-        if (range.isIncludesMaxValue()) {
-            includesMax = " [inclusive] ";
-        }
-        return type + " : " + includesMin + min + " - " + includesMax + max + " ";
+        return type + " : " + range.toString();
     }
 }
