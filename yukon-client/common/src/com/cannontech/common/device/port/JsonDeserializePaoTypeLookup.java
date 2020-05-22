9f872e2e3237ddebc7ca2d58e67da699ee9f6dce
E0507716 <DhwajVerma@eaton.com>
2020-05-22 19:31:08 +0530

    TypeNotSupportedExcpetion class added

--- yukon-client/common/src/com/cannontech/common/device/port/JsonDeserializePaoTypeLookup.java
+++ yukon-client/common/src/com/cannontech/common/device/port/JsonDeserializePaoTypeLookup.java
@@ -3,6 +3,7 @@ package com.cannontech.common.device.port;
 import java.io.IOException;
 
 import com.cannontech.common.device.model.PaoModelFactory;
+import com.cannontech.common.exception.TypeNotSupportedExcpetion;
 import com.cannontech.common.pao.PaoType;
 import com.cannontech.common.pao.YukonPao;
 import com.cannontech.core.dao.NotFoundException;
@@ -26,36 +27,63 @@ public class JsonDeserializePaoTypeLookup extends StdDeserializer<YukonPao> {
     private IDatabaseCache serverDatabaseCache = YukonSpringHook.getBean(IDatabaseCache.class);
 
     @Override
-    public YukonPao deserialize(JsonParser parser, DeserializationContext ctxt)
-            throws IOException, JsonProcessingException {
+    public YukonPao deserialize(JsonParser parser, DeserializationContext ctxt) throws IOException, JsonProcessingException {
         TreeNode node = parser.readValueAsTree();
         // Catch the update case here.
         String id = ServletUtils.getPathVariable("id");
-        PaoType paoType = null;
-        TreeNode type = node.get("type");
+        PaoType paoType;
+
         if (id == null) {
             // Create Case
-            if (type != null) {
-                try {
-                    paoType = PaoType.valueOf(type.toString().replace("\"", ""));
-                } catch (IllegalArgumentException e) {
-                    throw new NotFoundException("type is not valid.");
-                }
-            } else {
-                throw new NotFoundException("type is not found in the request.");
-            }
-
+            paoType = getPaoTypeFromJson(node);
         } else {
-            // Update case.
-            LiteYukonPAObject pao = serverDatabaseCache.getAllPaosMap().get(Integer.valueOf(id));
-            if (pao != null) {
-                paoType = pao.getPaoType();
-            } else {
-                throw new NotFoundException("id not found.");
+            // Update case
+            paoType = getPaoTypeFromCache(id);
+        }
+        return (YukonPao) parser.getCodec().treeToValue(node, getYukonPaoFromModelFactory(paoType).getClass());
+    }
+
+    /**
+     * Retrieves PaoType from type field provided JSON.
+     */
+    private PaoType getPaoTypeFromJson(TreeNode node) {
+        TreeNode type = node.get("type");
+        if (type != null) {
+            try {
+                return PaoType.valueOf(type.toString().replace("\"", ""));
+            } catch (IllegalArgumentException e) {
+                // throw exception for invalid paoType
+                throw new TypeNotSupportedExcpetion("type is not valid.");
             }
+        } else {
+            throw new NotFoundException("type is not found in the request.");
         }
 
-        Class<?> clazz = PaoModelFactory.getModel(paoType).getClass();
-        return (YukonPao) parser.getCodec().treeToValue(node, clazz);
+    }
+
+    /**
+     * Retrieves PaoType from Cache based on id provided.
+     */
+    private PaoType getPaoTypeFromCache(String id) {
+        LiteYukonPAObject pao = serverDatabaseCache.getAllPaosMap().get(Integer.valueOf(id));
+        if (pao == null) {
+            throw new NotFoundException("id not found.");
+        }
+        return pao.getPaoType();
+
+    }
+
+    /**
+     * Retrieves valid Yukon pao from model factory.
+     */
+    private YukonPao getYukonPaoFromModelFactory(PaoType paoType) {
+        YukonPao pao = PaoModelFactory.getModel(paoType);
+        if (pao != null) {
+            return pao;
+        } else {
+            // throw exception for not supported paoType
+            throw new TypeNotSupportedExcpetion("type is not valid.");
+           
+        }
     }
 }
