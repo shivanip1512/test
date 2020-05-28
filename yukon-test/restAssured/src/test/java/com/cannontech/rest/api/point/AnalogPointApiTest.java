3251ab0f0fc954563fbcea880b87c44f17ae9b46
E0507716 <DhwajVerma@eaton.com>
2020-05-28 15:50:14 +0530

    negtaive test cases added

--- yukon-test/restAssured/src/test/java/com/cannontech/rest/api/point/AnalogPointApiTest.java
+++ yukon-test/restAssured/src/test/java/com/cannontech/rest/api/point/AnalogPointApiTest.java
@@ -1,6 +1,7 @@
 package com.cannontech.rest.api.point;
 
 import static org.junit.Assert.assertTrue;
+import static org.testng.Assert.assertTrue;
 
 import org.testng.ITestContext;
 import org.testng.annotations.BeforeClass;
@@ -11,7 +12,9 @@ import com.cannontech.rest.api.common.model.MockPointType;
 import com.cannontech.rest.api.point.helper.PointHelper;
 import com.cannontech.rest.api.point.request.MockAnalogPoint;
 import com.cannontech.rest.api.point.request.MockPointBase;
+import com.cannontech.rest.api.point.request.MockPointUnit;
 import com.cannontech.rest.api.utilities.Log;
+import com.cannontech.rest.api.utilities.ValidationHelper;
 
 import io.restassured.response.ExtractableResponse;
 
@@ -51,4 +54,126 @@ public class AnalogPointApiTest {
         assertTrue("Point Offset Should be : " + analogPoint.getPointOffset(), analogPoint.getPointOffset().equals(analogPointDetail.getPointOffset()));
     }
 
+    /**
+     * Test case to validate Analog Point cannot be created as name with null and gets valid error message
+     * in response
+     */
+    @Test
+    public void analogPoint_03_NameCannotBeNull() {
+        MockPointBase mockPointBase = (MockPointBase) PointHelper.buildPoint(MockPointType.Analog);
+
+        mockPointBase.setPointName(null);
+        ExtractableResponse<?> createResponse = ApiCallHelper.post("createPoint", mockPointBase);
+        assertTrue(createResponse.statusCode() == 422, "Status code should be 422");
+        assertTrue(ValidationHelper.validateErrorMessage(createResponse, "Validation error"),
+                "Expected message should be - Validation error");
+        assertTrue(ValidationHelper.validateFieldError(createResponse, "pointName", "pointName is required."),
+                "Expected code in response is not correct");
+    }
+
+    /**
+     * Test case to validate Analog Point cannot be created with name having more than 60 characters and
+     * validates valid error message in response
+     */
+    @Test
+    public void analogPoint_04_NameGreaterThanMaxLength() {
+        MockPointBase mockPointBase = (MockPointBase) PointHelper.buildPoint(MockPointType.Analog);
+
+        mockPointBase.setPointName("Poinmdmdldslldlkdlkdddddddddddddddddddddddddddddddddddddddddddddddddddddd");
+        ExtractableResponse<?> createResponse = ApiCallHelper.post("createPoint", mockPointBase);
+        assertTrue(createResponse.statusCode() == 422, "Status code should be 422");
+        assertTrue(ValidationHelper.validateErrorMessage(createResponse, "Validation error"),
+                "Expected message should be - Validation error");
+        assertTrue(ValidationHelper.validateFieldError(createResponse, "pointName", "Exceeds maximum length of 60."),
+                "Expected code in response is not correct");
+    }
+
+    /**
+     * Test case to validate Analog Point cannot be created as paoId with null and gets valid error message
+     * in response
+     */
+    @Test
+    public void analogPoint_05_NullPaoId() {
+        MockPointBase mockPointBase = (MockPointBase) PointHelper.buildPoint(MockPointType.Analog);
+
+        mockPointBase.setPaoId(null);
+        ExtractableResponse<?> createResponse = ApiCallHelper.post("createPoint", mockPointBase);
+        assertTrue(createResponse.statusCode() == 422, "Status code should be 422");
+        assertTrue(ValidationHelper.validateErrorMessage(createResponse, "Validation error"),
+                "Expected message should be - Validation error");
+        assertTrue(ValidationHelper.validateFieldError(createResponse, "paoId", "paoId is required."),
+                "Expected code in response is not correct");
+    }
+
+    /**
+     * Test case to validate Analog Point cannot be created with Invalid Pao Id and gets valid error message
+     * in response
+     */
+    @Test
+    public void analogPoint_05_InvalidPaoId() {
+        MockPointBase mockPointBase = (MockPointBase) PointHelper.buildPoint(MockPointType.Analog);
+
+        mockPointBase.setPaoId(99999999);
+        ExtractableResponse<?> createResponse = ApiCallHelper.post("createPoint", mockPointBase);
+        assertTrue(createResponse.statusCode() == 422, "Status code should be 422");
+        assertTrue(ValidationHelper.validateErrorMessage(createResponse, "Validation error"),
+                "Expected message should be - Validation error");
+        assertTrue(ValidationHelper.validateFieldError(createResponse, "paoId", "Pao Id does not exist."),
+                "Expected code in response is not correct");
+    }
+
+    /**
+     * Test case to validate Analog Point cannot be created with Invalid Uom Id and gets valid error message
+     * in response
+     */
+    @Test
+    public void analogPoint_06_InvalidUomId() {
+        MockPointBase mockPointBase = (MockPointBase) PointHelper.buildPoint(MockPointType.Analog);
+
+        MockPointUnit pointUnit = MockPointUnit.builder().uomId(100).build();
+        mockPointBase.setPointUnit(pointUnit);
+        ExtractableResponse<?> createResponse = ApiCallHelper.post("createPoint", mockPointBase);
+        assertTrue(createResponse.statusCode() == 422, "Status code should be 422");
+        assertTrue(ValidationHelper.validateErrorMessage(createResponse, "Validation error"),
+                "Expected message should be - Validation error");
+        assertTrue(ValidationHelper.validateFieldError(createResponse, "pointUnit.uomID", "Uom Id does not exist."),
+                "Expected code in response is not correct");
+    }
+
+    /**
+     * Test case to validate Analog Point cannot be cannot be created with name having special characters and validates
+     * valid error message in response
+     */
+    @Test
+    public void analogPoint_07_NameWithSpecialChars() {
+        MockPointBase mockPointBase = (MockPointBase) PointHelper.buildPoint(MockPointType.Analog);
+
+        mockPointBase.setPointName("Test,//Test");
+        ExtractableResponse<?> createResponse = ApiCallHelper.post("createPoint", mockPointBase);
+        assertTrue(createResponse.statusCode() == 422, "Status code should be 422");
+        assertTrue(ValidationHelper.validateErrorMessage(createResponse, "Validation error"),
+                "Expected message should be - Validation error");
+        assertTrue(
+                ValidationHelper.validateFieldError(createResponse, "pointName",
+                        "Cannot be blank or include any of the following characters: / \\ , ' \" |"),
+                "Expected code in response is not correct");
+    }
+
+    /**
+     * Test case to validate Analog Point cannot be cannot be created with name having special characters and validates
+     * valid error message in response
+     */
+    @Test
+    public void analogPoint_07_NullPointOffset() {
+        MockPointBase mockPointBase = (MockPointBase) PointHelper.buildPoint(MockPointType.Analog);
+
+        mockPointBase.setPointOffset(null);
+        ExtractableResponse<?> createResponse = ApiCallHelper.post("createPoint", mockPointBase);
+        assertTrue(createResponse.statusCode() == 422, "Status code should be 422");
+        assertTrue(ValidationHelper.validateErrorMessage(createResponse, "Validation error"),
+                "Expected message should be - Validation error");
+        assertTrue(
+                ValidationHelper.validateFieldError(createResponse, "pointOffset", "pointOffset is required."),
+                "Expected code in response is not correct");
+    }
 }
