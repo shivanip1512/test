3125c822106fda9428ab61d0d84cae1c5b974464
Rawat, Payal <payalrawat@eaton.com>
2020-06-02 20:45:17 +0530

    YUK-22221: Design POJO for web trends

---
+++ yukon-client/common/src/com/cannontech/common/trend/model/TrendSeriesDefinition.java
@@ -0,0 +1,79 @@
+package com.cannontech.common.trend.model;
+
+import org.joda.time.LocalDate;
+
+public class TrendSeriesDefinition {
+    private TrendType.GraphType type;
+    private Integer pointId;
+    private String label;
+    private String color;
+    private TrendAxis axis;
+    private Double multiplier;
+    private RenderType style;
+    private LocalDate date;
+
+    public TrendType.GraphType getType() {
+        return type;
+    }
+
+    public void setType(TrendType.GraphType type) {
+        this.type = type;
+    }
+
+    public Integer getPointId() {
+        return pointId;
+    }
+
+    public void setPointId(Integer pointId) {
+        this.pointId = pointId;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public TrendAxis getAxis() {
+        return axis;
+    }
+
+    public void setAxis(TrendAxis axis) {
+        this.axis = axis;
+    }
+
+    public Double getMultiplier() {
+        return multiplier;
+    }
+
+    public void setMultiplier(Double multiplier) {
+        this.multiplier = multiplier;
+    }
+
+    public RenderType getStyle() {
+        return style;
+    }
+
+    public void setStyle(RenderType style) {
+        this.style = style;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+}
