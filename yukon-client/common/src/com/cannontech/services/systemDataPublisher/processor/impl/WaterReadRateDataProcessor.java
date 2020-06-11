a964d3b307fc77e45bd2930bda54c78828c4838c
Satpathy, Samir K <SamirKSatpathy@eaton.com>
2020-06-11 10:02:39 +0530

    YUK-22235 Make change in Processors to point this newly created device group and YUK-22234 missing i18n keys.

--- yukon-client/common/src/com/cannontech/services/systemDataPublisher/processor/impl/WaterReadRateDataProcessor.java
+++ yukon-client/common/src/com/cannontech/services/systemDataPublisher/processor/impl/WaterReadRateDataProcessor.java
@@ -1,15 +1,19 @@
 package com.cannontech.services.systemDataPublisher.processor.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cannontech.common.device.groups.editor.dao.DeviceGroupEditorDao;
+import com.cannontech.common.device.groups.editor.dao.SystemGroupEnum;
 import com.cannontech.services.systemDataPublisher.service.model.SystemDataFieldType.FieldType;
 
 @Service
 public class WaterReadRateDataProcessor extends ReadRateDataProcessor {
+    @Autowired DeviceGroupEditorDao deviceGroupEditorDao;
 
     @Override
     public String getDeviceGroupName() {
-        return "/Service/Active RFW Meters";
+        return deviceGroupEditorDao.getFullPath(SystemGroupEnum.SERVICE_ACTIVE_RFW_METERS);
     }
 
     @Override
