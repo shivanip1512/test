356649958fab022ee836dd80dc0e173179085a56
E0569837 <BradyWAnderson@eaton.com>
2020-06-12 10:07:45 -0500

    YUK-22129 Virtual Devices REST API Create/Update/R
    Added javadoc comments for interface

--- yukon-client/common/src/com/cannontech/common/device/virtualDevice/service/VirtualDeviceService.java
+++ yukon-client/common/src/com/cannontech/common/device/virtualDevice/service/VirtualDeviceService.java
@@ -4,7 +4,18 @@ import com.cannontech.common.device.virtualDevice.VirtualDeviceModel;
 
 public interface VirtualDeviceService {
 
+    /*
+     * Create a new virtual device
+     */
     VirtualDeviceModel create(VirtualDeviceModel virtualDevice);
+
+    /*
+     * Retrieve an existing virtual device
+     */
     VirtualDeviceModel retrieve(int virtualDeviceId);
+
+    /*
+     * Update an existing virtual device
+     */
     VirtualDeviceModel update(int virtualDeviceId, VirtualDeviceModel virtualDevice);
 }
