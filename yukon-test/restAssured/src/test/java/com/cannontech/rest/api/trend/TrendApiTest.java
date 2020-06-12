0d48de7065bd37499b0052e9f959ac5e0a70dbe7
Satpathy, Samir K <SamirKSatpathy@eaton.com>
2020-06-12 20:14:17 +0530

    YUK-22205 api documentation for create trends- Resolving conflict

--- yukon-test/restAssured/src/test/java/com/cannontech/rest/api/trend/TrendApiTest.java
+++ yukon-test/restAssured/src/test/java/com/cannontech/rest/api/trend/TrendApiTest.java
@@ -1,30 +1,36 @@
 package com.cannontech.rest.api.trend;
 
-import static org.junit.Assert.assertTrue;
-
 import org.testng.ITestContext;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import com.cannontech.rest.api.common.ApiCallHelper;
-import com.cannontech.rest.api.trend.helper.TrendHelper;
-import com.cannontech.rest.api.trend.request.MockTrendModel;
+import com.cannontech.rest.api.utilities.Log;
 
 import io.restassured.response.ExtractableResponse;
 
 public class TrendApiTest {
-    MockTrendModel trendModel = null;
 
-    @BeforeClass
-    public void setUp() {
-        trendModel = TrendHelper.buildTrend();
-    }
     @Test
-    public void trend_01_Create(ITestContext context) {
-        ExtractableResponse<?> createResponse = ApiCallHelper.post("createTrend", trendModel);
-        String trendId = createResponse.asString();
-        context.setAttribute(TrendHelper.CONTEXT_TREND_ID, trendId);
-        assertTrue("Status code should be 200", createResponse.statusCode() == 200);
-        assertTrue("Trend Id should not be Null", trendId != null);
+    public void trend_Delete(ITestContext context) {
+
+        SoftAssert softAssert = new SoftAssert();
+        Log.startTestCase("trend_Delete");
+        // TODO
+        /**
+         * I have hard coded trendId as create API is not available.
+         * Once YUK-22204 is done we will be use the created trend for deletion.If YUK-22204 is not done in this sprint i will call
+         * create API to create a trend and will delete the same.
+         **/
+        context.setAttribute("trendId", 25);
+        ExtractableResponse<?> deleteTrendResponse = ApiCallHelper.delete("deleteTrend",
+                context.getAttribute("trendId").toString());
+        softAssert.assertTrue(deleteTrendResponse.statusCode() == 200, "Status code should be 200");
+        // TODO
+        /**
+         * We need to make sure the above trend is deleted .For this we need to make a get call and verify that above trend is
+         * deleted or not once get API is available.
+         **/
+        Log.endTestCase("trend_Delete");
     }
 }
