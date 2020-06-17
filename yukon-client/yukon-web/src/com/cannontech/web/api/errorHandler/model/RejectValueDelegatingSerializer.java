d077ab9c655aba1667d74d867393112e90ce0a45
Rawat, Payal <payalrawat@eaton.com>
2020-06-17 19:36:30 +0530

    YUK-22343: Date field values changes to millis in error response when validation fails.

---
+++ yukon-client/yukon-web/src/com/cannontech/web/api/errorHandler/model/RejectValueDelegatingSerializer.java
@@ -0,0 +1,25 @@
+package com.cannontech.web.api.errorHandler.model;
+
+import java.io.IOException;
+
+import org.joda.time.DateTime;
+
+import com.cannontech.common.trend.model.DateSerializer;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+/**
+ * This serializer delegator choose the correct serializer based on passed value
+ */
+public class RejectValueDelegatingSerializer extends JsonSerializer<Object> {
+
+    @Override
+    public void serialize(Object type, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        if (type instanceof DateTime) {
+            new DateSerializer().serialize((DateTime) type, gen, provider);
+        }
+
+        // If no match found default serializer is used.
+    }
+}
