89c8fef30f213d1b0ab6e74aeaa007aa95609755
Feldman, Marina <MarinaFeldman@Eaton.com>
2020-07-01 15:02:11 -0400

    Merge pull request #1207 in EASD_SW/yukon from bugfix/Marina/YUK-22319-Device-Group-Cache-has-stale-data to release/7.4
    Squashed commit of the following:
    
    commit fed0601281dded4337621a55690a85a90f976d8f
    Author: E9812163 <MarinaFeldman@Eaton.com>
    Date:   Mon Jun 29 11:23:45 2020 -0500
    
        YUK-22319 Device Group Cache has stale data and Device Data monitor
        processing data out of order/ review changes
    
    commit 085f93eb7b81cbb203300f37f9bec165c92ebf24
    Author: E9812163 <MarinaFeldman@Eaton.com>
    Date:   Thu Jun 25 14:36:33 2020 -0500
    
        YUK-22319 Device Group Cache has stale data and Device Data monitor
        processing data out of order
    
    commit f27f427307e241ee77f5e2a2f5bcfddb3b85505b
    Author: E9812163 <MarinaFeldman@Eaton.com>
    Date:   Fri Jun 19 16:46:27 2020 -0500
    
        YUK-22319 Device Group Cache has stale data and Device Data monitor
        processing data out of order
        7.4
    
    commit fb49646f7438ccdbcf5993c590c38c725f0845f9
    Author: E9812163 <MarinaFeldman@Eaton.com>
    Date:   Thu Jun 18 14:46:38 2020 -0500
    
        YUK-22319
        Device Group Cache has stale data and Device Data monitor processing
        data out of order
    
    commit 3ddfc5e722f9a09eab04ff25ab92df0b1c8096e4
    Author: E9812163 <MarinaFeldman@Eaton.com>
    Date:   Thu Jun 18 13:03:17 2020 -0500
    
        YUK-22319
        Device Group Cache has stale data and Device Data monitor processing
        data out of order
    
    commit 643a05e6b5e702308eec2095b591fe421b9e35ea
    Author: E9812163 <MarinaFeldman@Eaton.com>
    Date:   Tue Jun 16 11:11:37 2020 -0500
    
        YUK-22319 Device Group Cache has stale data and Device Data monitor
        processing data out of order
        7.4
    
    commit 248cadc55780cd6e53cd66c1049eca229cff2166
    Author: E9812163 <MarinaFeldman@Eaton.com>
    Date:   Tue Jun 16 11:07:03 2020 -0500
    
        YUK-22319
        Device Group Cache has stale data and Device Data monitor processing
        data out of order
        7.4

--- yukon-client/common/src/com/cannontech/amr/deviceDataMonitor/service/impl/DeviceDataMonitorCalculationServiceImpl.java
+++ yukon-client/common/src/com/cannontech/amr/deviceDataMonitor/service/impl/DeviceDataMonitorCalculationServiceImpl.java
@@ -91,7 +91,7 @@ public class DeviceDataMonitorCalculationServiceImpl implements DeviceDataMonito
     private Map<Integer, DeviceDataMonitor> pending = Collections.synchronizedMap(new HashMap<>());
     // monitorId / violation count
     private Map<Integer, Integer> monitorIdToViolationCount = Collections.synchronizedMap(new HashMap<>());
-
+    
     static final Logger log = YukonLogManager.getLogger(DeviceDataMonitorCalculationServiceImpl.class);
 
     @PostConstruct
@@ -342,22 +342,27 @@ public class DeviceDataMonitorCalculationServiceImpl implements DeviceDataMonito
     }
 
     @Override
-    public void updateViolationsGroupBasedOnNewPointData(DeviceDataMonitor monitor, RichPointData richPointData) {
-
-        BuiltInAttribute attribute = Iterables.getFirst(
-            attributeService.findAttributesForPoint(richPointData.getPaoPointIdentifier().getPaoTypePointIdentifier(),
-                Sets.newHashSet(monitor.getAttributes())), null);
-        if (attribute == null) {
-            log.debug("{} recalculation of violation for device {} is skipped. The processor for point id {} is not found.",
-                monitor, richPointData.getPaoPointIdentifier().getPaoIdentifier(), richPointData.getPointValue().getId());
-            return;
-        }
+    public void updateViolationsGroupBasedOnNewPointData(DeviceDataMonitor monitor, RichPointData richPointData,
+            BuiltInAttribute attribute) {
         SimpleDevice device = new SimpleDevice(richPointData.getPaoPointIdentifier().getPaoIdentifier());
-        Boolean addRemoveFromGroup = recalculateViolation(monitor, richPointData, attribute);
-        if (addRemoveFromGroup != null) {
-            addRemoveFromViolationGroup(monitor, device, addRemoveFromGroup);
+        try {
+            log.debug("Started recalculation monitor:{} device:{} point:{} attribute:{}",
+                    monitor.getName(), richPointData.getPaoPointIdentifier().getPaoIdentifier().getPaoId(),
+                    richPointData.getPointValue(),
+                    attribute);
+            Boolean addRemoveFromGroup = recalculateViolation(monitor, richPointData, attribute);
+            if (addRemoveFromGroup != null) {
+                addRemoveFromViolationGroup(monitor, device, addRemoveFromGroup, richPointData.getPointValue());
+            }
+            updateViolationCacheCount(monitor);
+            log.debug("Finished recalculation monitor:{} device:{} point:{} attribute:{}",
+                    monitor.getName(), richPointData.getPaoPointIdentifier().getPaoIdentifier().getPaoId(),
+                    richPointData.getPointValue(),
+                    attribute);
+        } catch (Exception e) {
+            log.error("Error while updating violation for monitor:" + monitor.getName() + "device:" + device.getDeviceId()
+                    + "pointId:" + richPointData.getPointValue().getId(), e);
         }
-        updateViolationCacheCount(monitor);
     }
 
     private Boolean recalculateViolation(DeviceDataMonitor monitor, RichPointData richPointData, BuiltInAttribute attribute){
@@ -373,33 +378,41 @@ public class DeviceDataMonitorCalculationServiceImpl implements DeviceDataMonito
             // if other violations found, keep the device in the group
             foundViolation = isOtherAttributeViolating(monitor, attribute, device);
         }
-        log.debug("{} recalculation of violation for {} is complete, violation found:{}",  monitor, device, foundViolation);
-        return ViolationHelper.shouldTheGroupBeModified(inViolationsGroup, foundViolation);
+        Boolean shouldTheGroupBeModified = ViolationHelper.shouldTheGroupBeModified(inViolationsGroup, foundViolation);
+        log.debug("monitor:{} device:{} point data:{} inViolationsGroup:{} violation found:{} shouldTheGroupBeModified:{}",
+                monitor.getName(), device.getDeviceId(),
+                richPointData.getPointValue(), inViolationsGroup, foundViolation, shouldTheGroupBeModified);
+        return shouldTheGroupBeModified;
     }
 
     /**
      * Adds or removes device from violation group and sends notification
      */
-    private void addRemoveFromViolationGroup(DeviceDataMonitor monitor, SimpleDevice device, boolean addToGroup) {
-        //found violation and device is not in violation group
-        if(addToGroup) {
-            //add device to group
+    private void addRemoveFromViolationGroup(DeviceDataMonitor monitor, SimpleDevice device, boolean addToGroup,
+            PointValueQualityHolder point) {
+        // found violation and device is not in violation group
+        if (addToGroup) {
+            // add device to group
             int rowsAdded = deviceGroupMemberEditorDao.addDevice(monitor.getViolationGroup(), device);
             if (rowsAdded > 0) {
                 sendSmartNotificationEvent(monitor, device.getDeviceId(), MonitorState.IN_VIOLATION);
-                log.debug("{} adding {} to violation group {}", monitor, device, monitor.getViolationGroup());
+                log.debug("{} adding {} ({}) to violation group {}", monitor.getName(), device.getDeviceId(),
+                        point, monitor.getViolationGroup());
             } else {
-                log.debug("{} adding {} to violation group {}. Device already in group, no notification.", monitor, device, monitor.getViolationGroup());
+                log.debug("{} adding {} ({}) to violation group {}. Device already in group, no notification.", monitor.getName(),
+                        device.getDeviceId(), monitor.getViolationGroup());
             }
-        }
-        else {
+        } else {
             // remove device from group
-            int rowsDeleted = deviceGroupMemberEditorDao.removeDevicesById(monitor.getViolationGroup(), Collections.singleton(device.getDeviceId()));
+            int rowsDeleted = deviceGroupMemberEditorDao.removeDevices(monitor.getViolationGroup(),
+                    Collections.singleton(device));
             if (rowsDeleted > 0) {
                 sendSmartNotificationEvent(monitor, device.getDeviceId(), MonitorState.OUT_OF_VIOLATION);
-                log.debug("{} removing {} from violation group {}", monitor, device, monitor.getViolationGroup());
+                log.debug("{} removing {} ({}) from violation group {}", monitor.getName(), device.getDeviceId(), point,
+                        monitor.getViolationGroup());
             } else {
-                log.debug("{} removing {} from violation group {}. Device already removed from group, no notification.", monitor, device, monitor.getViolationGroup());
+                log.debug("{} removing {} ({}) from violation group {}. Device already removed from group, no notification.",
+                        monitor.getName(), device.getDeviceId(), point, monitor.getViolationGroup());
             }
         }
     }
