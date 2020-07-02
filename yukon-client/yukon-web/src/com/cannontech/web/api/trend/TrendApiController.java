e72693ad9b51c4e74182089833adbb37460288eb
Satpathy, Samir K <SamirKSatpathy@eaton.com>
2020-07-02 19:00:54 +0530

    YUK-22217 Rest API for reset peak and YUK-22218 API Validation for reset peak

--- yukon-client/yukon-web/src/com/cannontech/web/api/trend/TrendApiController.java
+++ yukon-client/yukon-web/src/com/cannontech/web/api/trend/TrendApiController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cannontech.common.trend.model.ResetPeakModel;
 import com.cannontech.common.trend.model.TrendModel;
 import com.cannontech.common.trend.service.TrendService;
 import com.cannontech.stars.util.ServletUtils;
@@ -28,6 +30,7 @@ public class TrendApiController {
     @Autowired private TrendService trendService;
     @Autowired private TrendCreateValidator trendCreateValidator;
     @Autowired private TrendValidator trendValidator;
+    @Autowired private ResetPeakValidator resetPeakValidator;
 
     @PostMapping
     public ResponseEntity<Object> create(@Valid @RequestBody TrendModel trendModel) {
@@ -45,14 +48,20 @@ public class TrendApiController {
 
     @PutMapping("/{id}")
     public ResponseEntity<TrendModel> update(@Valid @RequestBody TrendModel trendModel, @PathVariable int id) {
-        TrendModel createdTrend = trendService.update(id, trendModel);
-        return new ResponseEntity<>(createdTrend, HttpStatus.OK);
+        TrendModel trend = trendService.update(id, trendModel);
+        return new ResponseEntity<>(trend, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<TrendModel> retrieve(@PathVariable int id) {
         TrendModel trend = trendService.retrieve(id);
         return new ResponseEntity<>(trend, HttpStatus.OK);
+    }
+    
+    @PatchMapping("{id}/resetPeak")
+    public ResponseEntity<Integer> resetPeak(@PathVariable int id, @Valid @RequestBody ResetPeakModel resetPeakModel){
+        Integer trendId = trendService.resetPeak(id, resetPeakModel);
+        return new ResponseEntity<>(trendId, HttpStatus.OK);
     }
 
     @InitBinder("trendModel")
@@ -63,5 +72,10 @@ public class TrendApiController {
         if (trendId == null) {
             binder.addValidators(trendCreateValidator);
         }
+    }
+
+    @InitBinder("resetPeakModel")
+    public void setupResetPeakBinder(WebDataBinder binder) {
+        binder.addValidators(resetPeakValidator);
     }
 }
