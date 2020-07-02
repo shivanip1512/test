e6a67c9c083cdddc9cb71420caf44f12fa1e2350
Chetna Gupta <ChetnaGupta@Eaton.com>
2020-07-02 19:28:30 +0530

    QA-6066 Restored master files of data creation via API

--- yukon-test/uiAutomation/src/main/java/com/eaton/rest/api/common/ApiCallHelper.java
+++ yukon-test/uiAutomation/src/main/java/com/eaton/rest/api/common/ApiCallHelper.java
@@ -6,11 +6,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.Properties;
 
-<<<<<<< HEAD
-import org.json.simple.JSONObject;
-
-=======
->>>>>>> master
 import com.eaton.framework.ConfigFileReader;
 
 import io.restassured.response.ExtractableResponse;
@@ -18,12 +13,6 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class ApiCallHelper {
-<<<<<<< HEAD
-        
-        static String payloadBaseDir = System.getProperty("user.dir")+"\\resources\\payload\\";
-        
-=======
->>>>>>> master
     //private static String userDirectory = System.getProperty("user.dir");
     public static String authToken = new ApiTokenAuthentication().getAuthToken();
 
@@ -40,18 +29,6 @@ public class ApiCallHelper {
         return getHeader().body(body).when().post(pathParam).then().log().all().extract();
     }
 
-<<<<<<< HEAD
-    
-    public static ExtractableResponse<?> post(String pathParam, Object body, String id) {
-        return getHeader().body(body).when().post(pathParam + id).then().log().all().extract();
-    }
-    
-    public static ExtractableResponse<?> delete(String pathParam, String id){
-                return getHeader().delete(pathParam + id).then().log().all().extract();
-    }
-    
-=======
->>>>>>> master
     private static RequestSpecification getHeader() {
         return given().accept("application/json").contentType("application/json").header("Authorization",
             "Bearer " + authToken).log().all();
