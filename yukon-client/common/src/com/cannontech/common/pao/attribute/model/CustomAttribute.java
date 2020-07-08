62618f4642bf980dc5088f69687a56e6b972dc96
E9812163 <MarinaFeldman@Eaton.com>
2020-07-08 14:17:29 -0500

    YUK-21837 Custom Attributes - Data Export - Fixed Attribute

--- yukon-client/common/src/com/cannontech/common/pao/attribute/model/CustomAttribute.java
+++ yukon-client/common/src/com/cannontech/common/pao/attribute/model/CustomAttribute.java
@@ -8,14 +8,14 @@ import com.cannontech.i18n.YukonMessageSourceResolvable;
 
 public class CustomAttribute implements Attribute {
     public static String i18Key = "yukon.common.attribute.customAttribute.";
-    private int id;
+    private Integer id;
     private String name;
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -39,6 +39,37 @@ public class CustomAttribute implements Attribute {
     @Override
     public MessageSourceResolvable getMessage() {
         return YukonMessageSourceResolvable.createDefault(getI18Key(), name);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CustomAttribute other = (CustomAttribute) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
     }
 
     @Override
