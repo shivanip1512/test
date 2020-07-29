b2bdd58cfae77477e2c125492ec65fddbd9faf60
E0511441 <neerajrawat@eaton.com>
2020-07-29 12:56:19 +0530

    QA-6149 added trend Url and AddMarkerModal

---
+++ yukon-test/uiAutomation/src/main/java/com/eaton/elements/modals/trend/AddMarkerModal.java
@@ -0,0 +1,29 @@
+package com.eaton.elements.modals.trend;
+
+import java.util.Optional;
+
+import com.eaton.elements.DropDownElement;
+import com.eaton.elements.PickerElement;
+import com.eaton.elements.RadioButtonElement;
+import com.eaton.elements.TextEditElement;
+import com.eaton.elements.modals.BaseModal;
+import com.eaton.framework.DriverExtensions;
+
+public class AddMarkerModal extends BaseModal{
+    
+    public AddMarkerModal(DriverExtensions driverExt, Optional<String> modalTitle, Optional<String> describedBy) {
+        super(driverExt, modalTitle, describedBy);
+    }
+    
+    public TextEditElement getValue() {
+        return new TextEditElement(driverExt, "value", getModal());
+    }
+    
+    public TextEditElement getLabel() {
+        return new TextEditElement(driverExt, "label", getModal());
+    }
+    
+    public RadioButtonElement getAxis() {
+        return new RadioButtonElement(driverExt, "axis", getModal());
+    }
+}
