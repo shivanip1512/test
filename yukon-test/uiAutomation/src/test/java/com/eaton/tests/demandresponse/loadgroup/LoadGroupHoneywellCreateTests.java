b810041d6b030a0d68242a8774ead8f9cccca308
E0566392 <ElyshaAnderson@Eaton.com>
2020-08-06 14:04:54 -0500

    minor updates

--- yukon-test/uiAutomation/src/test/java/com/eaton/tests/demandresponse/loadgroup/LoadGroupHoneywellCreateTests.java
+++ yukon-test/uiAutomation/src/test/java/com/eaton/tests/demandresponse/loadgroup/LoadGroupHoneywellCreateTests.java
@@ -6,10 +6,8 @@ import java.text.SimpleDateFormat;
 import java.util.Optional;
 import java.util.Random;
 
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.eaton.framework.DriverExtensions;
@@ -19,9 +17,8 @@ import com.eaton.framework.Urls;
 import com.eaton.pages.demandresponse.LoadGroupDetailPage;
 import com.eaton.pages.demandresponse.LoadGroupCreatePage;
 
-public class LoadGroupHoneywellCreateTests extends SeleniumTestSetup{
-	private LoadGroupCreatePage createPage;
-    WebDriver driver;
+public class LoadGroupHoneywellCreateTests extends SeleniumTestSetup {
+    private LoadGroupCreatePage createPage;
     private DriverExtensions driverExt;
     private Random randomNum;
 
@@ -29,75 +26,68 @@ public class LoadGroupHoneywellCreateTests extends SeleniumTestSetup{
     public void beforeClass() {
         driverExt = getDriverExt();
         randomNum = getRandomNum();
-    }
 
-    @BeforeMethod(alwaysRun = true)
-    public void beforeTest() {
         navigate(Urls.DemandResponse.LOAD_GROUP_CREATE);
         createPage = new LoadGroupCreatePage(driverExt);
     }
 
-    @Test(groups = { TestConstants.Priority.CRITICAL, TestConstants.DemandResponse.DEMAND_RESPONSE})
-    public void loadGroupCreateHoneywell_AllFieldsDisableFalseSuccess() {
-    	 String timeStamp = new SimpleDateFormat(TestConstants.DATE_FORMAT).format(System.currentTimeMillis());        
-         String name = "Honeywell Group " + timeStamp;
-         double randomDouble = randomNum.nextDouble();
-         int randomInt = randomNum.nextInt(9999);
-         double capacity = randomDouble + randomInt;       
-
-         final String EXPECTED_MSG = name + " saved successfully.";
-         
-         createPage.getType().selectItemByText("Honeywell Group");
-
-         waitForLoadingSpinner();
-         createPage.getName().setInputValue(name);      
-         createPage.getkWCapacity().setInputValue(String.valueOf(capacity));
-         createPage.getSaveBtn().click();
-
-         waitForPageToLoad("Load Group: " + name, Optional.empty());
-
-         LoadGroupDetailPage detailsPage = new LoadGroupDetailPage(driverExt);
-         String userMsg = detailsPage.getUserMessage();
-         
-         assertThat(userMsg).isEqualTo(EXPECTED_MSG);
-
-    }
-    
-    @Test(groups = { TestConstants.Priority.CRITICAL, TestConstants.DemandResponse.DEMAND_RESPONSE})
-    public void loadGroupCreateHoneywell_AllFieldsDisableTrueSuccess() {
-    	 String timeStamp = new SimpleDateFormat(TestConstants.DATE_FORMAT).format(System.currentTimeMillis());        
-         String name = "Honeywell Group " + timeStamp;
-         double randomDouble = randomNum.nextDouble();
-         int randomInt = randomNum.nextInt(9999);
-         double capacity = randomDouble + randomInt;       
-
-         final String EXPECTED_MSG = name + " saved successfully.";
-         
-         createPage.getType().selectItemByText("Honeywell Group");
-
-         waitForLoadingSpinner();
-         createPage.getName().setInputValue(name);      
-         createPage.getkWCapacity().setInputValue(String.valueOf(capacity));
-
-         createPage.getDisableGroup().setValue(true);
-         createPage.getDisableControl().setValue(true);
-
-         createPage.getSaveBtn().click();
-
-         waitForPageToLoad("Load Group: " + name, Optional.empty());
-
-         LoadGroupDetailPage detailsPage = new LoadGroupDetailPage(driverExt);
-         String userMsg = detailsPage.getUserMessage();
-         
-         assertThat(userMsg).isEqualTo(EXPECTED_MSG);
-
-    }
-    
     @AfterMethod(alwaysRun = true)
     public void afterTest() {
         refreshPage(createPage);
-        createPage = new LoadGroupCreatePage(driverExt);
     }
-	
 
+    @Test(groups = { TestConstants.Priority.CRITICAL, TestConstants.DemandResponse.DEMAND_RESPONSE })
+    public void loadGroupCreateHoneywell_AllFieldsDisableFalseSuccess() {
+        String timeStamp = new SimpleDateFormat(TestConstants.DATE_FORMAT).format(System.currentTimeMillis());
+        String name = "Honeywell Group " + timeStamp;
+        double randomDouble = randomNum.nextDouble();
+        int randomInt = randomNum.nextInt(9999);
+        double capacity = randomDouble + randomInt;
+
+        final String EXPECTED_MSG = name + " saved successfully.";
+
+        createPage.getType().selectItemByText("Honeywell Group");
+        waitForLoadingSpinner();
+        
+        createPage.getName().setInputValue(name);
+        createPage.getkWCapacity().setInputValue(String.valueOf(capacity));
+        createPage.getSaveBtn().click();
+
+        waitForPageToLoad("Load Group: " + name, Optional.empty());
+
+        LoadGroupDetailPage detailsPage = new LoadGroupDetailPage(driverExt);
+        String userMsg = detailsPage.getUserMessage();
+
+        assertThat(userMsg).isEqualTo(EXPECTED_MSG);
+    }
+
+    @Test(groups = { TestConstants.Priority.CRITICAL, TestConstants.DemandResponse.DEMAND_RESPONSE })
+    public void loadGroupCreateHoneywell_AllFieldsDisableTrueSuccess() {
+        String timeStamp = new SimpleDateFormat(TestConstants.DATE_FORMAT).format(System.currentTimeMillis());
+        String name = "Honeywell Group " + timeStamp;
+        double randomDouble = randomNum.nextDouble();
+        int randomInt = randomNum.nextInt(9999);
+        double capacity = randomDouble + randomInt;
+
+        final String EXPECTED_MSG = name + " saved successfully.";
+
+        createPage.getType().selectItemByText("Honeywell Group");
+
+        waitForLoadingSpinner();
+        createPage.getName().setInputValue(name);
+        createPage.getkWCapacity().setInputValue(String.valueOf(capacity));
+
+        createPage.getDisableGroup().setValue(true);
+        createPage.getDisableControl().setValue(true);
+
+        createPage.getSaveBtn().click();
+
+        waitForPageToLoad("Load Group: " + name, Optional.empty());
+
+        LoadGroupDetailPage detailsPage = new LoadGroupDetailPage(driverExt);
+        String userMsg = detailsPage.getUserMessage();
+
+        assertThat(userMsg).isEqualTo(EXPECTED_MSG);
+
+    }
 }
