1b212f8dc684ed08760c40f6499263bbfcbb573e
E0566392 <ElyshaAnderson@Eaton.com>
2020-08-06 12:55:26 -0500

    Updated LoadGroupEditPage to only have fields on all load group types

--- yukon-test/uiAutomation/src/main/java/com/eaton/pages/demandresponse/LoadGroupEditPage.java
+++ yukon-test/uiAutomation/src/main/java/com/eaton/pages/demandresponse/LoadGroupEditPage.java
@@ -1,10 +1,6 @@
 package com.eaton.pages.demandresponse;
 
-import java.util.List;
-
 import com.eaton.elements.Button;
-import com.eaton.elements.DropDownElement;
-import com.eaton.elements.RadioButtonElement;
 import com.eaton.elements.Section;
 import com.eaton.elements.SwitchBtnYesNoElement;
 import com.eaton.elements.TextEditElement;
@@ -14,152 +10,50 @@ import com.eaton.pages.PageBase;
 
 public class LoadGroupEditPage extends PageBase {
 
-	private TextEditElement name;
+    private TextEditElement name;
+    private TextEditElement kWCapacity;
+    private Button saveBtn;
+    private Button cancelBtn;
 
-	public LoadGroupEditPage(DriverExtensions driverExt, int id) {
-		super(driverExt);
+    public LoadGroupEditPage(DriverExtensions driverExt, int id) {
+        super(driverExt);
 
-		requiresLogin = true;
-		pageUrl = Urls.DemandResponse.LOAD_PROGRAM_EDIT + id + Urls.EDIT;
+        requiresLogin = true;
+        pageUrl = Urls.DemandResponse.LOAD_PROGRAM_EDIT + id + Urls.EDIT;
 
-		name = new TextEditElement(this.driverExt, "name");
-	}
+        name = new TextEditElement(this.driverExt, "name");
+        kWCapacity = new TextEditElement(this.driverExt, "kWCapacity");
+        saveBtn = new Button(this.driverExt, "Save");
+        cancelBtn = new Button(this.driverExt, "Cancel");
+    }
 
-	// General
-	public TextEditElement getName() {
-		return name;
-	}
+    // General
+    public TextEditElement getName() {
+        return name;
+    }
 
-	public Section getPageSection(String sectionName) {
-		return new Section(this.driverExt, sectionName);
-	}
+    //Optional Attributes
+    public TextEditElement getkWCapacity() {
+        return kWCapacity;
+    }
 
-	public DropDownElement getCommunicationRoute() {
-		return new DropDownElement(this.driverExt, "routeId");
-	}
+    public SwitchBtnYesNoElement getDisableControl() {
+        return new SwitchBtnYesNoElement(this.driverExt, "disableControl", getPageSection("Optional Attributes").getSection());
+    }
 
-	// Device Class
-	public DropDownElement getDeviceClass() {
-		return new DropDownElement(this.driverExt, "deviceClassSet");
-	}
+    public SwitchBtnYesNoElement getDisableGroup() {
+        return new SwitchBtnYesNoElement(this.driverExt, "disableGroup", getPageSection("Optional Attributes").getSection());
+    }
 
-	// Enrollment
-	public TextEditElement getUtilityEnrollmentGroup() {
-		return new TextEditElement(this.driverExt, "utilityEnrollmentGroup");
-	}
+    public Button getSaveBtn() {
+        return saveBtn;
+    }
 
-	// Timing
-	public TextEditElement getRampInTime() {
-		return new TextEditElement(this.driverExt, "rampInMinutes");
-	}
+    public Button getCancelBtn() {
+        return cancelBtn;
+    }
 
-	public TextEditElement getRampOutTime() {
-		return new TextEditElement(this.driverExt, "rampOutMinutes");
-	}
-
-	// Geographical Address
-	public RadioButtonElement getAddressUsage() {
-		// addressUsage is used for 2 different radio buttons on this page
-		return new RadioButtonElement(this.driverExt, "addressUsage");
-	}
-
-	// Geographical Addressing
-	public TextEditElement getSpid() {
-		return new TextEditElement(this.driverExt, "serviceProvider");
-	}
-
-	public TextEditElement getGeo() {
-		return new TextEditElement(this.driverExt, "geo");
-	}
-
-	public TextEditElement getSubstation() {
-		return new TextEditElement(this.driverExt, "substation");
-	}
-
-	public RadioButtonElement getFeeder() {
-		return new RadioButtonElement(this.driverExt, "feeder");
-	}
-
-	public TextEditElement getZip() {
-		return new TextEditElement(this.driverExt, "zip");
-	}
-
-	public TextEditElement getUser() {
-		return new TextEditElement(this.driverExt, "user");
-	}
-
-	public TextEditElement getSerial() {
-		return new TextEditElement(this.driverExt, "serialNumber");
-	}
-
-	// Addressing
-	public TextEditElement getGoldAddress() {
-		return new TextEditElement(this.driverExt, "goldAddress");
-	}
-
-	public TextEditElement getSilverAddress() {
-		return new TextEditElement(this.driverExt, "silverAddress");
-	}
-
-	public RadioButtonElement getAddressToUse() {
-		return new RadioButtonElement(this.driverExt, "addressUsage");
-	}
-
-	public RadioButtonElement getRelayToUse() {
-		return new RadioButtonElement(this.driverExt, "relayUsage");
-	}
-
-	public List<String> getRelayToUseValues() {
-		return getRelayToUse().getValues();
-	}
-
-	public List<String> getAddressToUseValues() {
-		return getAddressToUse().getValues();
-	}
-
-	// LoadAddress
-	public RadioButtonElement getUsage() {
-		// addressUsage is used for 2 different radio buttons on this page
-		return new RadioButtonElement(this.driverExt, "addressUsage");
-	}
-
-	// LoadAddressing
-	public RadioButtonElement getLoads() {
-		return new RadioButtonElement(this.driverExt, "relayUsage");
-	}
-
-	public TextEditElement getProgram() {
-		return new TextEditElement(this.driverExt, "program");
-	}
-
-	public TextEditElement getSplinter() {
-		return new TextEditElement(this.driverExt, "splinter");
-	}
-
-	// Optional Attributes
-	public DropDownElement getControlPriority() {
-		return new DropDownElement(this.driverExt, "protocolPriority");
-	}
-
-	public TextEditElement getkWCapacity() {
-		return new TextEditElement(this.driverExt, "kWCapacity");
-	}
-
-	public SwitchBtnYesNoElement getDisableControl() {
-		return new SwitchBtnYesNoElement(this.driverExt, "disableControl",
-				getPageSection("Optional Attributes").getSection());
-	}
-
-	public SwitchBtnYesNoElement getDisableGroup() {
-		return new SwitchBtnYesNoElement(this.driverExt, "disableGroup",
-				getPageSection("Optional Attributes").getSection());
-	}
-
-	public Button getSaveBtn() {
-		return new Button(this.driverExt, "Save");
-	}
-
-	public Button getCancelBtn() {
-		return new Button(this.driverExt, "Cancel");
-	}
+    public Section getPageSection(String sectionName) {
+        return new Section(this.driverExt, sectionName);
+    }
 }
