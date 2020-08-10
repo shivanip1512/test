f23d662948ec765a8e9134c9eb2e72b96fbbffd2
E9812163 <MarinaFeldman@Eaton.com>
2020-08-10 09:31:54 -0500

    YUK-18034 Display all attributes associated with a point/ added unit test back

---
+++ yukon-client/yukon-web/test/com/cannontech/web/common/pao/service/YukonPointHelperImplTest.java
@@ -0,0 +1,167 @@
+package com.cannontech.web.common.pao.service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.MessageSource;
+import org.springframework.context.MessageSourceAware;
+import org.springframework.context.support.StaticMessageSource;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.cannontech.common.i18n.MessageSourceAccessor;
+import com.cannontech.common.pao.PaoType;
+import com.cannontech.common.pao.attribute.model.Attribute;
+import com.cannontech.common.pao.attribute.model.BuiltInAttribute;
+import com.cannontech.common.pao.attribute.model.CustomAttribute;
+import com.cannontech.common.pao.definition.dao.PaoDefinitionDao;
+import com.cannontech.common.pao.definition.dao.PaoDefinitionDaoImplTest;
+import com.cannontech.i18n.YukonUserContextMessageSourceResolver;
+import com.cannontech.user.YukonUserContext;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+
+public class YukonPointHelperImplTest {
+
+    private YukonPointHelperImpl yukonPointHelperImpl;
+    private final CustomAttribute ca1 = new CustomAttribute(1, "foo");
+    private final CustomAttribute ca2 = new CustomAttribute(2, "alpha");
+    private final CustomAttribute ca3 = new CustomAttribute(3, "beta");
+    
+    private final StaticMessageSource messageSource = new StaticMessageSource();
+    {
+        messageSource.addMessage("yukon.common.attribute.customAttribute.1", Locale.US, "Z Foo");
+        messageSource.addMessage("yukon.common.attribute.customAttribute.2", Locale.US, "Y Alpha");
+        messageSource.addMessage("yukon.common.attribute.customAttribute.3", Locale.US, "X Beta");
+    }
+    
+    // sorted by i18n: ca3, ca2, ca1
+    // sorted by default (name): ca2, ca3, ca1
+
+    private class YukonUserContextMessageSourceResolverMock implements YukonUserContextMessageSourceResolver, MessageSourceAware {
+        
+        private MessageSource messageSource;
+
+        @Override
+        public MessageSource getMessageSource(YukonUserContext userContext) {
+            return messageSource;
+        }
+
+        @Override
+        public MessageSourceAccessor getMessageSourceAccessor(YukonUserContext userContext) {
+            return new MessageSourceAccessor(messageSource, Locale.US);
+        }
+
+        @Override
+        public void setMessageSource(MessageSource messageSource) {
+            this.messageSource = messageSource;
+        }
+    };
+    
+    @Before
+    public void setUp() {
+        yukonPointHelperImpl = new YukonPointHelperImpl();
+        
+        PaoDefinitionDao paoDefinitionDao = PaoDefinitionDaoImplTest.getTestPaoDefinitionDao();
+        ReflectionTestUtils.setField(yukonPointHelperImpl, "paoDefinitionDao", paoDefinitionDao);
+    }
+    
+    @Test
+    public void test_firstAttribute_null() throws Exception {
+        Set<BuiltInAttribute> builtInAttributes = Collections.emptySet();
+        List<CustomAttribute> customAttributes = Collections.emptyList();
+
+        YukonUserContextMessageSourceResolverMock resolver = new YukonUserContextMessageSourceResolverMock();
+        resolver.setMessageSource(new StaticMessageSource());
+        Attribute attribute = executeTest(builtInAttributes, customAttributes, resolver);
+        Assert.assertEquals("Objects not equal.", null, attribute);
+    }
+
+    @Test
+    public void test_firstAttribute_onlyOneCustomAttribute_one() throws Exception {
+        Set<BuiltInAttribute> builtInAttributes = Collections.emptySet();
+        List<CustomAttribute> customAttributes = Lists.newArrayList(ca3);
+        
+        YukonUserContextMessageSourceResolverMock resolver = new YukonUserContextMessageSourceResolverMock();
+        resolver.setMessageSource(new StaticMessageSource());
+        Attribute attribute = executeTest(builtInAttributes, customAttributes, resolver);
+        Assert.assertEquals("Objects not equal.", ca3, attribute);
+    }
+
+    @Test
+    public void test_firstAttribute_onlyOneCustomAttribute_many() throws Exception {
+        Set<BuiltInAttribute> builtInAttributes = Collections.emptySet();
+        List<CustomAttribute> customAttributes = Lists.newArrayList(ca3, ca2);
+        
+        YukonUserContextMessageSourceResolverMock resolver = new YukonUserContextMessageSourceResolverMock();
+        resolver.setMessageSource(new StaticMessageSource());
+        Attribute attribute = executeTest(builtInAttributes, customAttributes, resolver);
+        Assert.assertEquals("Objects not equal.", ca2, attribute);
+    }
+
+    @Test
+    public void test_firstAttribute_onlyBuiltInAttribute_one() throws Exception {
+        Set<BuiltInAttribute> builtInAttributes = Sets.newHashSet(BuiltInAttribute.DELIVERED_KWH);
+        List<CustomAttribute> customAttributes = Collections.emptyList();
+        
+        YukonUserContextMessageSourceResolverMock resolver = new YukonUserContextMessageSourceResolverMock();
+        resolver.setMessageSource(new StaticMessageSource());
+        Attribute attribute = executeTest(builtInAttributes, customAttributes, resolver);
+        Assert.assertEquals("Objects not equal.", BuiltInAttribute.DELIVERED_KWH, attribute);
+    }
+
+    @Test
+    public void test_firstAttribute_onlyBuiltInAttribute_many() throws Exception {
+        Set<BuiltInAttribute> builtInAttributes = Sets.newHashSet(BuiltInAttribute.DELIVERED_KWH, BuiltInAttribute.USAGE);
+        List<CustomAttribute> customAttributes = Collections.emptyList();
+        
+        YukonUserContextMessageSourceResolverMock resolver = new YukonUserContextMessageSourceResolverMock();
+        resolver.setMessageSource(new StaticMessageSource());
+        Attribute attribute = executeTest(builtInAttributes, customAttributes, resolver);
+        // PaoType.RFN420CD = <pointInfo name="Delivered kWh" init="true" attributes="USAGE,DELIVERED_KWH"/>
+        Assert.assertEquals("Objects not equal.", BuiltInAttribute.USAGE, attribute);
+    }
+
+    @Test
+    public void test_firstAttribute_bothTypesOfAttributes() throws Exception {
+        Set<BuiltInAttribute> builtInAttributes = Sets.newHashSet(BuiltInAttribute.USAGE, BuiltInAttribute.DELIVERED_KWH);
+        List<CustomAttribute> customAttributes = Lists.newArrayList(ca1, ca2, ca3);
+        
+        YukonUserContextMessageSourceResolverMock resolver = new YukonUserContextMessageSourceResolverMock();
+        resolver.setMessageSource(new StaticMessageSource());
+        Attribute attribute = executeTest(builtInAttributes, customAttributes, resolver);
+        Assert.assertEquals("Objects not equal.", ca2, attribute);
+    }
+    
+    @Test
+    public void test_firstAttribute_bothTypesOfAttributes_i18n() throws Exception {
+        Set<BuiltInAttribute> builtInAttributes = Sets.newHashSet(BuiltInAttribute.USAGE, BuiltInAttribute.DELIVERED_KWH);
+        List<CustomAttribute> customAttributes = Lists.newArrayList(ca1, ca2, ca3);
+        
+        YukonUserContextMessageSourceResolverMock resolver = new YukonUserContextMessageSourceResolverMock();
+        resolver.setMessageSource(messageSource);
+        Attribute attribute = executeTest(builtInAttributes, customAttributes, resolver);
+        Assert.assertEquals("Objects not equal.", ca3, attribute);
+    }
+    
+    private Attribute executeTest(Set<BuiltInAttribute> builtInAttributes, List<CustomAttribute> customAttributes, 
+            YukonUserContextMessageSourceResolverMock resolver) {
+        MessageSourceAccessor accessor = resolver.getMessageSourceAccessor(YukonUserContext.system);
+        List<Attribute> attributes = new ArrayList<>();
+        attributes.addAll(builtInAttributes);
+        attributes.addAll(customAttributes);
+        Attribute attribute = ReflectionTestUtils.invokeMethod(yukonPointHelperImpl,
+                                                                               "getFirstAttribute",
+                                                                               PaoType.RFN420CD,
+                                                                               accessor,
+                                                                               builtInAttributes,
+                                                                               customAttributes,
+                                                                               attributes);
+        return attribute;
+    }
+}
