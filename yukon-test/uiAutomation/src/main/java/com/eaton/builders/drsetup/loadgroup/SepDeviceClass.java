737d232d63d3804e57066bf22adb00b2e50f79e5
E0511441 <neerajrawat@eaton.com>
2020-08-19 17:53:28 +0530

    QA-6181 LoadGroup DigiSep create builder

---
+++ yukon-test/uiAutomation/src/main/java/com/eaton/builders/drsetup/loadgroup/SepDeviceClass.java
@@ -0,0 +1,35 @@
+package com.eaton.builders.drsetup.loadgroup;
+
+import java.util.Random;
+
+public enum SepDeviceClass {
+
+    HVAC_COMPRESSOR_FURNACE("HVAC_COMPRESSOR_FURNACE"),
+    BASEBOARD_HEAT("BASEBOARD_HEAT"),
+    WATER_HEATER("WATER_HEATER"),
+    POOL_PUMP("POOL_PUMP"),
+    SMART_APPLIANCE("SMART_APPLIANCE"),
+    IRRIGATION_PUMP("IRRIGATION_PUMP"),
+    MANAGED_COMMERCIAL_INDUSTRIAL("IRRIGATION_PUMP"),
+    SIMPLE_RESIDENTIAL_ON_OFF("SIMPLE_RESIDENTIAL_ON_OFF"),
+    EXTERIOR_LIGHTING("EXTERIOR_LIGHTING"),
+    INTERIOR_LIGHTING("INTERIOR_LIGHTING"),
+    ELECTRIC_VEHICLE("ELECTRIC_VEHICLE"),
+    GENERATION_SYSTEMS("GENERATION_SYSTEMS");
+    
+
+    private final String deviceClassSet;
+
+    SepDeviceClass(String deviceClassSet) {
+        this.deviceClassSet = deviceClassSet;
+    }
+    
+    public String getDeviceClassSet() {
+        return this.deviceClassSet;
+    }
+    
+    public static SepDeviceClass getRandomDeviceClassSet() {
+        Random random = new Random();
+        return values()[random.nextInt(values().length)];
+}
+}
