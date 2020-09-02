a5dfd5e387c3c47c778813da0f8f4578685306f9
E0534908 <JohnMJustin@Eaton.com>
2020-09-02 16:21:30 -0500

    YUK-22044 PointDataTrackingLogger - ConcurrentModificationException
    Added changes to make the code threadsafe

--- yukon-client/common/src/com/cannontech/amr/monitors/DeviceDataMonitorProcessorFactory.java
+++ yukon-client/common/src/com/cannontech/amr/monitors/DeviceDataMonitorProcessorFactory.java
@@ -1,8 +1,9 @@
 package com.cannontech.amr.monitors;
 
 import com.cannontech.amr.deviceDataMonitor.model.DeviceDataMonitor;
+import com.cannontech.amr.monitors.impl.PointDataTrackingLogger;
 import com.cannontech.core.dynamic.RichPointData;
 
 public interface DeviceDataMonitorProcessorFactory {
-    void handlePointDataReceived(final DeviceDataMonitor monitor, RichPointData richPointData);
+    void handlePointDataReceived(final DeviceDataMonitor monitor, RichPointData richPointData, PointDataTrackingLogger trackingLogger);
 }
