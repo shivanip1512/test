d02aba01facc318ca6161c963074d4afa1eedde3
E0589229 <saurabhdubey@Eaton.com>
2020-09-03 18:37:09 +0530

    added detail page test for load groups

---
+++ yukon-test/uiAutomation/src/test/java/com/eaton/tests/demandresponse/loadgroup/LoadGroupItronDetailTest.java
@@ -0,0 +1,55 @@
+package com.eaton.tests.demandresponse.loadgroup;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
+import org.javatuples.Pair;
+import org.json.JSONObject;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.eaton.builders.drsetup.loadgroup.LoadGroupItronCreateBuilder;
+import com.eaton.elements.modals.ConfirmModal;
+import com.eaton.framework.DriverExtensions;
+import com.eaton.framework.SeleniumTestSetup;
+import com.eaton.framework.TestConstants;
+import com.eaton.framework.Urls;
+import com.eaton.pages.demandresponse.DemandResponseSetupPage;
+import com.eaton.pages.demandresponse.LoadGroupDetailPage;
+
+public class LoadGroupItronDetailTest  extends SeleniumTestSetup {
+	private DriverExtensions driverExt;
+	 private Integer id;
+	 private String name;
+	 private LoadGroupDetailPage detailPage;
+	
+	 @BeforeClass(alwaysRun=true)
+	 public void beforeClass() {
+		 driverExt = getDriverExt();   
+	 }
+	 
+	 @Test(groups = { TestConstants.Priority.CRITICAL, TestConstants.DemandResponse.DEMAND_RESPONSE })
+	 public void ldGrpItronDetail_DeleteLoadGroup_Success() {
+		 Pair<JSONObject, JSONObject> pair = new LoadGroupItronCreateBuilder.Builder(Optional.empty())
+													.withKwCapacity(Optional.empty())
+													.withRelay(Optional.empty())
+													.create();
+	     JSONObject response = pair.getValue1();
+	     id = response.getInt("id");
+	     name = response.getString("name");
+	     final String expected_msg = name + " deleted successfully.";
+	     navigate(Urls.DemandResponse.LOAD_GROUP_DETAIL + id);
+	     
+	     detailPage = new LoadGroupDetailPage(driverExt, id);
+	     ConfirmModal  confirmModal = detailPage.showDeleteLoadGroupModal(); 
+	     confirmModal.clickOkAndWaitForModalToClose();
+	     
+	     waitForPageToLoad("Setup", Optional.empty());
+	     DemandResponseSetupPage setupPage = new DemandResponseSetupPage(driverExt, Urls.Filters.LOAD_GROUP);
+	     String userMsg = setupPage.getUserMessage();
+	     
+	     assertThat(userMsg).isEqualTo(expected_msg);
+	}
+
+}
