7d91f8c9e443be51708076bfe2ecf14a981a739a
Satpathy, Samir K <SamirKSatpathy@eaton.com>
2020-09-08 21:40:52 +0530

    YUK-22624:Support data export file copy for external path which is not in Yukon folder - Review comment fix

--- yukon-database/DBUpdates/oracle/7.5_0.sql
+++ yukon-database/DBUpdates/oracle/7.5_0.sql
@@ -716,7 +716,6 @@ INSERT INTO DBUpdates VALUES ('YUK-22624', '7.5.0', SYSDATE);
 /* @start-warning checkDirectoryAccess Yukon requires write access to Import and Export directories. Please grant Local Service user access to following directories. Press Start to resume execution. */
 SELECT Value FROM GlobalSetting WHERE Name 
     IN ('SCHEDULE_PARAMETERS_EXPORT_PATH', 'SCHEDULE_PARAMETERS_IMPORT_PATH');
-/* @end-warning checkDirectoryAccess */
 
 /* @end YUK-22624 */
 
