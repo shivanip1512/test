6377ce03f47e999a6d6f66206809e1022ea1e610
E0566392 <ElyshaAnderson@Eaton.com>
2020-09-10 14:57:53 -0500

    fixed error in adminmenutests

--- yukon-test/uiAutomation/src/test/java/com/eaton/tests/admin/AdminMenuTests.java
+++ yukon-test/uiAutomation/src/test/java/com/eaton/tests/admin/AdminMenuTests.java
@@ -1,7 +1,6 @@
 package com.eaton.tests.admin;
 
 import static org.assertj.core.api.Assertions.*;
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -15,17 +14,10 @@ public class AdminMenuTests extends SeleniumTestSetup {
 
     private HomePage page;
     private static final int ADMIN_INDEX = 5;
-    private String baseUrl;
 
     @BeforeClass(alwaysRun = true)
     public void beforeClass() {
-        WebDriver driver = getDriver();
         DriverExtensions driverExt = getDriverExt();
-
-        baseUrl = getBaseUrl();
-
-        driver.get(baseUrl + Urls.HOME);
-
         navigate(Urls.HOME);
         page = new HomePage(driverExt);
     }
@@ -37,3 +29,52 @@ public class AdminMenuTests extends SeleniumTestSetup {
 
         assertThat(url).contains(Urls.Admin.CONFIGURATION);
     }
+
+    @Test(groups = { TestConstants.Priority.HIGH, TestConstants.Admin.ADMIN })
+    public void adminMenu_EnergyCompanyUrlCorrect() {
+
+        String url = page.getMenu().getMenuOptionUrl(ADMIN_INDEX, 1);
+
+        assertThat(url).contains(Urls.Admin.ENERGY_COMPANY);
+    }
+
+    @Test(groups = { TestConstants.Priority.HIGH, TestConstants.Admin.ADMIN })
+    public void adminMenu_MaintenanceUrlCorrect() {
+
+        String url = page.getMenu().getMenuOptionUrl(ADMIN_INDEX, 2);
+
+        assertThat(url).contains(Urls.Admin.MAINTENANCE);
+    }
+
+    @Test(groups = { TestConstants.Priority.HIGH, TestConstants.Admin.ADMIN })
+    public void adminMenu_MultiSpeakUrlCorrect() {
+
+        String url = page.getMenu().getMenuOptionUrl(ADMIN_INDEX, 3);
+
+        assertThat(url).contains(Urls.Admin.MULTI_SPEAK);
+    }
+
+    @Test(groups = { TestConstants.Priority.HIGH, TestConstants.Admin.ADMIN })
+    public void adminMenu_SubstationsUrlCorrect() {
+
+        String url = page.getMenu().getMenuOptionUrl(ADMIN_INDEX, 4);
+
+        assertThat(url).contains(Urls.Admin.SUBSTATIONS);
+    }
+
+    @Test(groups = { TestConstants.Priority.HIGH, TestConstants.Admin.ADMIN })
+    public void adminMenu_UsersAndGroupsUrlCorrect() {
+
+        String url = page.getMenu().getMenuOptionUrl(ADMIN_INDEX, 5);
+
+        assertThat(url).contains(Urls.Admin.USERS_AND_GROUPS);
+    }
+
+    @Test(groups = { TestConstants.Priority.HIGH, TestConstants.Admin.ADMIN })
+    public void adminMenu_ReportsUrlCorrect() {
+
+        String url = page.getMenu().getMenuOptionUrl(ADMIN_INDEX, 6);
+
+        assertThat(url).contains(Urls.Admin.REPORTS);
+    }
+}
