d74539c569ec3408991d178bdaf45f19f519dd63
Chetna Gupta <ChetnaGupta@Eaton.com>
2020-09-15 20:56:52 +0530

    QA-6239
    Added TCs covering Virtual Device creation.

---
+++ yukon-test/uiAutomation/src/test/java/com/eaton/tests/assets/virtualdevices/VirtualDevicesCreateTests.java
@@ -0,0 +1,106 @@
+package com.eaton.tests.assets.virtualdevices;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.assertj.core.api.SoftAssertions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.eaton.elements.modals.virtualdevices.CreateVirtualDeviceModal;
+import com.eaton.framework.DriverExtensions;
+import com.eaton.framework.SeleniumTestSetup;
+import com.eaton.framework.TestConstants;
+import com.eaton.framework.Urls;
+import com.eaton.pages.assets.virtualdevices.VirtualDevicesListPage;
+import com.eaton.pages.tools.trends.TrendsListPage;
+import com.github.javafaker.Faker;
+
+public class VirtualDevicesCreateTests extends SeleniumTestSetup {
+    
+    private VirtualDevicesListPage listPage;
+    private DriverExtensions driverExt;
+    private Faker faker;
+    
+    @BeforeClass(alwaysRun = true)
+    public void beforeClass() {
+        driverExt = getDriverExt();
+        navigate(Urls.Assets.VIRTUAL_DEVICES);
+        listPage = new VirtualDevicesListPage(driverExt);
+    }
+    
+    @AfterMethod(alwaysRun = true)
+    public void afterMethod() {
+        refreshPage(listPage);
+    }
+    
+    @Test(groups = { TestConstants.Priority.LOW, TestConstants.Assets.VIRTUAL_DEVICES, TestConstants.Assets.ASSETS })
+    public void virtualDevices_Create_FieldValidation() {
+        SoftAssertions softly = new SoftAssertions();
+        CreateVirtualDeviceModal createModal = listPage.showAndWaitCreateVirtualDeviceModal();
+        List<String> labels = createModal.getVirtualDeviceModelLabels();
+
+        softly.assertThat(labels.size()).isEqualTo(2);
+        softly.assertThat(labels.get(0)).isEqualTo("Name:");
+        softly.assertThat(labels.get(1)).contains("Status:");
+        softly.assertAll();
+    }
+    
+//    To Do.... For TC "virtualDevicesCreate_Name_AlreadyExistsValidation" we need to create a Virtual Device via builder and it is not yet ready. So kept this in To Do
+    
+//    @Test(groups = { TestConstants.Priority.LOW, TestConstants.Assets.VIRTUAL_DEVICES, TestConstants.Assets.ASSETS })
+//    public void virtualDevicesCreate_Name_AlreadyExistsValidation() {
+//    }
+
+    @Test(groups = { TestConstants.Priority.LOW, TestConstants.Assets.VIRTUAL_DEVICES, TestConstants.Assets.ASSETS })
+    public void virtualDevicesCreate_Name_InvalidCharsValidation() {
+        String name = "AT Virtual Devices " + "/ \\ , ' \" |";
+
+        final String EXPECTED_MSG = "Name must not contain any of the following characters: / \\ , ' \" |.";
+        CreateVirtualDeviceModal createModal = listPage.showAndWaitCreateVirtualDeviceModal();
+        createModal.getName().setInputValue(name);
+        createModal.clickOk();
+
+        String errorMsg = createModal.getName().getValidationError();
+
+        assertThat(errorMsg).isEqualTo(EXPECTED_MSG);
+    }
+
+    @Test(groups = { TestConstants.Priority.LOW, TestConstants.Tools.TRENDS })
+    public void virtualDevicesCreate_Name_MaxLength60Chars() {
+        String name = "AT Virtual Devices " + faker.number().digits(60);
+
+        final String EXPECTED_MSG = name + " saved successfully.";
+
+        CreateVirtualDeviceModal createModal = listPage.showAndWaitCreateVirtualDeviceModal();
+        createModal.getName().setInputValue(name);
+        createModal.clickOk();
+
+        TrendsListPage listPage = new TrendsListPage(driverExt);
+
+        String userMsg = listPage.getUserMessage();
+
+        assertThat(userMsg).isEqualTo(EXPECTED_MSG);
+    }
+    
+    @Test(groups = { TestConstants.Priority.LOW, TestConstants.Tools.TRENDS })
+    public void trendCreate_Name_RequiredValidation() {
+        final String EXPECTED_MSG = "Name is required.";
+
+        CreateVirtualDeviceModal createModal = listPage.showAndWaitCreateVirtualDeviceModal();
+        createModal.clickOk();
+
+        String errorMsg = createModal.getName().getValidationError();
+
+        assertThat(errorMsg).isEqualTo(EXPECTED_MSG);
+    }
+    
+//    virtualDevices_Create_FieldValidation
+//    virtualDevices_Create_VirtualDeviceEnabled
+//    virtualDevices_Create_VirtualDeviceDisabled
+
+
+
+}
