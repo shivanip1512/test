ca407bc624301a02a3829739e1a648485ca1ab5e
Chetna Gupta <ChetnaGupta@Eaton.com>
2020-09-16 13:30:01 +0530

    QA-6132
    Corrected error in Rfn Expresscom builder class.

--- yukon-test/uiAutomation/src/main/java/com/eaton/builders/drsetup/loadgroup/LoadGroupRfnExpresscomCreateBuilder.java
+++ yukon-test/uiAutomation/src/main/java/com/eaton/builders/drsetup/loadgroup/LoadGroupRfnExpresscomCreateBuilder.java
@@ -8,7 +8,6 @@ import java.util.Optional;
 import org.javatuples.Pair;
 import org.json.JSONObject;
 
-import com.eaton.builders.drsetup.loadgroup.LoadGroupEnums.RelayUsageExpresscom;
 import com.eaton.rest.api.drsetup.DrSetupCreateRequest;
 import com.eaton.rest.api.drsetup.DrSetupGetRequest;
 import com.github.javafaker.Faker;
@@ -32,7 +31,7 @@ public class LoadGroupRfnExpresscomCreateBuilder {
         private double kwCapacity;
         private boolean disableGroup;
         private boolean disableControl;
-        private List<RelayUsageExpresscom> relayUsage;
+        private List<String> relayUsage;
         private String protocolPriority;
         List<LoadGroupEnums.AddressUsageExpresscom> addressUsageList = new ArrayList<>();
 
@@ -91,8 +90,8 @@ public class LoadGroupRfnExpresscomCreateBuilder {
             return this;
         }
         
-        public Builder withRelayUsage(Optional<List<LoadGroupEnums.RelayUsageExpresscom>> relayUsage) {
-            List<LoadGroupEnums.RelayUsageExpresscom> relays = new ArrayList<>();
+        public Builder withRelayUsage(Optional<List<String>> relayUsage) {
+            List<String> relays = new ArrayList<>();
             relays.add(LoadGroupEnums.RelayUsageExpresscom.getRandomRelayUsage());
             this.relayUsage = relayUsage.orElse(relays);
             addressUsageList.add(LoadGroupEnums.AddressUsageExpresscom.LOAD);
