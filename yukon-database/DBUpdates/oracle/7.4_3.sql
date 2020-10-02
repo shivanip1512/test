6febf52177f44f7ad38501f09361fc68fe6ddb3f
E9816170 <StaceyLNelson@Eaton.com>
2020-10-02 09:12:15 -0500

    YUK-23031 Prepare db update scripts for 7.4.3 build  - updated ctidatabase insert for update scripts

--- yukon-database/DBUpdates/oracle/7.4_3.sql
+++ yukon-database/DBUpdates/oracle/7.4_3.sql
@@ -40,4 +40,4 @@ INSERT INTO DBUpdates VALUES ('YUK-22622', '7.4.3', SYSDATE);
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
 /**************************************************************/
-/*INSERT INTO CTIDatabase VALUES ('7.4', '2-SEP-2020', 'Latest Update', 3, SYSDATE);*/
+INSERT INTO CTIDatabase VALUES ('7.4', '2-OCT-2020', 'Latest Update', 3, SYSDATE);
