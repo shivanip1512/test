e0bfcb2f3e76510d677f2578c90c18f905da02e4
E0566392 <ElyshaAnderson@Eaton.com>
2020-10-05 15:36:38 -0500

    removed old files and refactored test code

--- yukon-test/uiAutomation/src/test/java/com/eaton/tests/demandresponse/loadgroup/LoadGroupItronDetailTests.java
+++ yukon-test/uiAutomation/src/test/java/com/eaton/tests/demandresponse/loadgroup/LoadGroupItronDetailTests.java
@@ -23,7 +23,7 @@ import com.eaton.pages.demandresponse.DemandResponseSetupPage;
 import com.eaton.pages.demandresponse.loadgroup.LoadGroupDetailPage;
 
 public class LoadGroupItronDetailTests extends SeleniumTestSetup {
-	private DriverExtensions driverExt;
+    private DriverExtensions driverExt;
     private LoadGroupDetailPage detailPage;
     private JSONObject response;
 
@@ -31,30 +31,30 @@ public class LoadGroupItronDetailTests extends SeleniumTestSetup {
     public void beforeClass() {
         driverExt = getDriverExt();
         setRefreshPage(false);
-        
+
         Pair<JSONObject, JSONObject> pair = new LoadGroupItronCreateBuilder.Builder(Optional.empty())
-									              .withKwCapacity(Optional.empty())
-									              .withRelay(Optional.empty())
-									              .create();
-        
+                .withKwCapacity(Optional.empty())
+                .withRelay(Optional.empty())
+                .create();
+
         response = pair.getValue1();
         int id = response.getInt("id");
-        
+
         navigate(Urls.DemandResponse.LOAD_GROUP_DETAIL + id);
         detailPage = new LoadGroupDetailPage(driverExt, id);
     }
-    
+
     @AfterMethod
     public void afterMethod() {
-        if(getRefreshPage()) {
-            refreshPage(detailPage);    
+        if (getRefreshPage()) {
+            refreshPage(detailPage);
         }
         setRefreshPage(false);
     }
-    
-    @Test(groups = { TestConstants.Priority.HIGH, TestConstants.DemandResponse.DEMAND_RESPONSE })
+
+    @Test(groups = { TestConstants.Priority.HIGH, TestConstants.Features.DEMAND_RESPONSE })
     public void ldGrpItronDetail_Delete_Success() {
-    	setRefreshPage(true);
+        setRefreshPage(true);
         Pair<JSONObject, JSONObject> pair = new LoadGroupItronCreateBuilder.Builder(Optional.empty())
                 .withKwCapacity(Optional.empty())
                 .withRelay(Optional.empty())
@@ -70,25 +70,18 @@ public class LoadGroupItronDetailTests extends SeleniumTestSetup {
 
         waitForPageToLoad("Setup", Optional.empty());
         DemandResponseSetupPage setupPage = new DemandResponseSetupPage(driverExt, Urls.Filters.LOAD_GROUP);
+        
         String userMsg = setupPage.getUserMessage();
 
         assertThat(userMsg).isEqualTo(expected_msg);
     }
 
-    @Test(groups = { TestConstants.Priority.HIGH, TestConstants.DemandResponse.DEMAND_RESPONSE })
+    @Test(groups = { TestConstants.Priority.HIGH, TestConstants.Features.DEMAND_RESPONSE })
     public void ldGrpItronDetail_Copy_Success() {
         setRefreshPage(true);
-    	Pair<JSONObject, JSONObject> pair = new LoadGroupItronCreateBuilder.Builder(Optional.empty())
-                .withKwCapacity(Optional.empty())
-                .withRelay(Optional.empty())
-                .create();
-        JSONObject response = pair.getValue1();
-        int id = response.getInt("id");
         String name = response.getString("name");
         final String copyName = "Copy of " + name;
         final String expected_msg = copyName + " copied successfully.";
-
-        navigate(Urls.DemandResponse.LOAD_GROUP_DETAIL + id);
 
         CopyLoadGroupModal modal = detailPage.showCopyLoadGroupModal();
         modal.getName().setInputValue(copyName);
@@ -99,24 +92,24 @@ public class LoadGroupItronDetailTests extends SeleniumTestSetup {
 
         assertThat(userMsg).isEqualTo(expected_msg);
     }
-    
-    @Test(groups = { TestConstants.Priority.HIGH, TestConstants.DemandResponse.DEMAND_RESPONSE })
+
+    @Test(groups = { TestConstants.Priority.HIGH, TestConstants.Features.DEMAND_RESPONSE })
     public void ldGrpItronDetail_GeneralSection_LabelsCorrect() {
-    	SoftAssertions softly = new SoftAssertions();
+        SoftAssertions softly = new SoftAssertions();
         List<String> labels = detailPage.getGeneralSection().getSectionLabels();
-        
-    	softly.assertThat(3).isEqualTo(labels.size());
+
+        softly.assertThat(3).isEqualTo(labels.size());
         softly.assertThat("Name:").isEqualTo(labels.get(0));
         softly.assertThat("Type:").isEqualTo(labels.get(1));
         softly.assertThat("Relay:").isEqualTo(labels.get(2));
         softly.assertAll();
     }
- 
-    @Test(groups = { TestConstants.Priority.HIGH, TestConstants.DemandResponse.DEMAND_RESPONSE })
+
+    @Test(groups = { TestConstants.Priority.HIGH, TestConstants.Features.DEMAND_RESPONSE })
     public void ldGrpItronDetail_GeneralSection_ValuesCorrect() {
-    	SoftAssertions softly = new SoftAssertions();
-    	List<String> values = detailPage.getGeneralSection().getSectionValues();
-        
+        SoftAssertions softly = new SoftAssertions();
+        List<String> values = detailPage.getGeneralSection().getSectionValues();
+
         softly.assertThat(3).isEqualTo(values.size());
         softly.assertThat(response.get("virtualRelayId").toString()).isEqualTo(values.get(2));
         softly.assertAll();
