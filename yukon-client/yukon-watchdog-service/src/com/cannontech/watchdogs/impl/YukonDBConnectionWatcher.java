f42cd0187a8eb7a8a1dce3fe6c004a1fd969897e
Satpathy, Samir K <SamirKSatpathy@eaton.com>
2020-10-14 12:46:16 +0530

    YUK-22935:Create a file for maintaining Subscribers Email IDs

--- yukon-client/yukon-watchdog-service/src/com/cannontech/watchdogs/impl/YukonDBConnectionWatcher.java
+++ yukon-client/yukon-watchdog-service/src/com/cannontech/watchdogs/impl/YukonDBConnectionWatcher.java
@@ -2,26 +2,30 @@ package com.cannontech.watchdogs.impl;
 
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cannontech.clientutils.YukonLogManager;
+import com.cannontech.common.config.ConfigurationSource;
 import com.cannontech.common.smartNotification.dao.SmartNotificationSubscriptionDao;
 import com.cannontech.common.smartNotification.model.SmartNotificationEventType;
+import com.cannontech.watchdog.base.YukonServices;
 import com.cannontech.watchdog.model.WatchdogWarningType;
 import com.cannontech.watchdog.model.WatchdogWarnings;
-import com.cannontech.watchdogs.impl.ServiceStatusWatchdogImpl.ServiceStatus;
-import com.cannontech.watchdogs.util.WatchdogFileUtil;
+import com.cannontech.watchdog.model.Watchdogs;
+import com.cannontech.watchdogs.util.DBConnectionUtil;
 
 @Service
-public class YukonDBConnectionWatcher extends DBConnectionWatchdogImpl {
+public class YukonDBConnectionWatcher extends ServiceStatusWatchdogImpl {
+
+    public YukonDBConnectionWatcher(ConfigurationSource configSource) {
+        super(configSource);
+    }
 
     Logger log = YukonLogManager.getLogger(YukonDBConnectionWatcher.class);
 
     @Autowired private SmartNotificationSubscriptionDao subscriptionDao;
-    @Autowired private WatchdogFileUtil watchdogFileUtil;
 
     @Override
     public List<WatchdogWarnings> watch() {
@@ -31,12 +35,27 @@ public class YukonDBConnectionWatcher extends DBConnectionWatchdogImpl {
         if (serviceStatus == ServiceStatus.RUNNING) {
             try {
                 List<String> subscriberEmailIds = subscriptionDao.getSubscribedEmails(SmartNotificationEventType.YUKON_WATCHDOG);
-                watchdogFileUtil.writeToFile(StringUtils.join(subscriberEmailIds, ","));
+                // In YUK-22935, we will keep these IDs in file system.
             } catch (RuntimeException e) {
                 serviceStatus = ServiceStatus.STOPPED;
             }
         }
         log.info("Status of Database " + serviceStatus);
         return generateWarning(WatchdogWarningType.YUKON_DATABASE, serviceStatus);
+    }
+
+    @Override
+    public YukonServices getServiceName() {
+        return YukonServices.DATABASE;
+    }
+
+    @Override
+    public Watchdogs getName() {
+        return Watchdogs.DB_CONNECTION;
+    }
+
+    @Override
+    public boolean isServiceRunning() {
+        return DBConnectionUtil.isDBConnected(DBName.YUKON);
     }
 }
