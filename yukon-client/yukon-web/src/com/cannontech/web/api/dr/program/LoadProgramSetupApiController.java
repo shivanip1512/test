d4b88f71bdbae7ba614dd391b81ea5489600f06e
E0552673 <E0552673@INPU1WHP6096232.eaton.ad.etn.com>
2020-10-27 13:05:57 +0530

    YUK-23147: Update API URLs for Load Programs:Resolving conflict

--- yukon-client/yukon-web/src/com/cannontech/web/api/dr/program/LoadProgramSetupApiController.java
+++ yukon-client/yukon-web/src/com/cannontech/web/api/dr/program/LoadProgramSetupApiController.java
@@ -25,12 +25,11 @@ import com.cannontech.common.dr.program.setup.model.LoadProgramCopy;
 import com.cannontech.common.dr.program.setup.model.NotificationGroup;
 import com.cannontech.common.dr.program.setup.model.ProgramDirectMemberControl;
 import com.cannontech.common.dr.program.setup.model.ProgramGroup;
-import com.cannontech.common.dr.setup.ProgramDetails;
-import com.cannontech.common.pao.PaoType;
 import com.cannontech.core.roleproperties.HierarchyPermissionLevel;
 import com.cannontech.core.roleproperties.YukonRoleProperty;
 import com.cannontech.database.data.lite.LiteGear;
 import com.cannontech.dr.loadprogram.service.LoadProgramSetupService;
+import com.cannontech.user.YukonUserContext;
 import com.cannontech.web.security.annotation.CheckPermissionLevel;
 import com.cannontech.web.security.annotation.CheckRoleProperty;
 
@@ -50,15 +49,15 @@ public class LoadProgramSetupApiController {
 
     @PostMapping
     @CheckPermissionLevel(property = YukonRoleProperty.DR_SETUP_PERMISSION, level = HierarchyPermissionLevel.CREATE)
-    public ResponseEntity<Object> create(@Valid @RequestBody LoadProgram loadProgram) {
-        LoadProgram createLoadProgram = loadProgramService.create(loadProgram);
+    public ResponseEntity<Object> create(@Valid @RequestBody LoadProgram loadProgram, YukonUserContext userContext) {
+        LoadProgram createLoadProgram = loadProgramService.create(loadProgram, userContext.getYukonUser());
         return new ResponseEntity<>(createLoadProgram, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
     @CheckPermissionLevel(property = YukonRoleProperty.DR_SETUP_PERMISSION, level = HierarchyPermissionLevel.OWNER)
-    public ResponseEntity<Object> delete(@PathVariable int id) {
-        int paoId = loadProgramService.delete(id);
+    public ResponseEntity<Object> delete(@PathVariable int id, YukonUserContext userContext) {
+        int paoId = loadProgramService.delete(id, userContext.getYukonUser());
         HashMap<String, Integer> paoIdMap = new HashMap<>();
         paoIdMap.put("id", paoId);
         return new ResponseEntity<>(paoIdMap, HttpStatus.OK);
@@ -66,8 +65,9 @@ public class LoadProgramSetupApiController {
 
     @PostMapping("/{id}/copy")
     @CheckPermissionLevel(property = YukonRoleProperty.DR_SETUP_PERMISSION, level = HierarchyPermissionLevel.CREATE)
-    public ResponseEntity<Object> copy(@Valid @RequestBody LoadProgramCopy loadProgramCopy, @PathVariable int id) {
-        int paoId = loadProgramService.copy(id, loadProgramCopy);
+    public ResponseEntity<Object> copy(@Valid @RequestBody LoadProgramCopy loadProgramCopy, @PathVariable int id,
+            YukonUserContext userContext) {
+        int paoId = loadProgramService.copy(id, loadProgramCopy, userContext.getYukonUser());
         HashMap<String, Integer> paoIdMap = new HashMap<>();
         paoIdMap.put("programId", paoId);
         return new ResponseEntity<>(paoIdMap, HttpStatus.OK);
@@ -75,44 +75,25 @@ public class LoadProgramSetupApiController {
 
     @PutMapping("/{id}")
     @CheckPermissionLevel(property = YukonRoleProperty.DR_SETUP_PERMISSION, level = HierarchyPermissionLevel.UPDATE)
-    public ResponseEntity<Object> update(@Valid @RequestBody LoadProgram loadProgram, @PathVariable int id) {
-        LoadProgram updateLoadProgram = loadProgramService.update(id, loadProgram);
+    public ResponseEntity<Object> update(@Valid @RequestBody LoadProgram loadProgram, @PathVariable int id,
+            YukonUserContext userContext) {
+        LoadProgram updateLoadProgram = loadProgramService.update(id, loadProgram, userContext.getYukonUser());
         return new ResponseEntity<>(updateLoadProgram, HttpStatus.OK);
     }
-
-    @GetMapping("/allAvailableNotificationGroups")
-    public ResponseEntity<Object> getAllAvailableProgramNotificationGroups() {
-        List<NotificationGroup> notificationGroups = loadProgramService.getAllAvailableProgramNotificationGroups();
-        return new ResponseEntity<>(notificationGroups, HttpStatus.OK);
-    }
     
-    @GetMapping("/gears/{id}")
-    public ResponseEntity<Object> getProgramGear(@PathVariable Integer id) {
-        ProgramGear programGear = loadProgramService.getProgramGear(id);
+    @GetMapping("/gear/{gearId}")
+    public ResponseEntity<Object> getProgramGear(@PathVariable Integer gearId) {
+        ProgramGear programGear = loadProgramService.getProgramGear(gearId);
         return new ResponseEntity<>(programGear, HttpStatus.OK);
     }
 
-    @GetMapping("/allAvailableDirectMemberControls")
-    @CheckRoleProperty(YukonRoleProperty.ALLOW_MEMBER_PROGRAMS)
-    public ResponseEntity<Object> getAllAvailableDirectMemberControls() {
-        List<ProgramDirectMemberControl> directMemberControls =
-            loadProgramService.getAllAvailableDirectMemberControls();
-        return new ResponseEntity<>(directMemberControls, HttpStatus.OK);
-    }
-
-    @GetMapping("/allAvailableLoadGroups/{programType}")
-    public ResponseEntity<Object> getAllAvailableProgramLoadGroups(@PathVariable PaoType programType) {
-        List<ProgramGroup> programGroups = loadProgramService.getAllAvailableProgramLoadGroups(programType);
-        return new ResponseEntity<>(programGroups, HttpStatus.OK);
-    }
-
-    @GetMapping("/{id}/loadGroups/available")
+    @GetMapping("/availableLoadGroups/{id}")
     public ResponseEntity<Object> getAvailableProgramLoadGroups(@PathVariable int id) {
         List<ProgramGroup> programGroups = loadProgramService.getAvailableProgramLoadGroups(id);
         return new ResponseEntity<>(programGroups, HttpStatus.OK);
     }
 
-    @GetMapping("/{id}/notificationGroups/available")
+    @GetMapping("/availableNotificationGroups/{id}")
     public ResponseEntity<Object> getAvailableProgramNotificationGroups(@PathVariable int id) {
         List<NotificationGroup> notificationGroups =
             loadProgramService.getAvailableProgramNotificationGroups(id);
@@ -120,7 +101,7 @@ public class LoadProgramSetupApiController {
     }
 
     
-    @GetMapping("/{id}/directMemberControls/available")
+    @GetMapping("/availableDirectMemberControls/{id}")
     @CheckRoleProperty(YukonRoleProperty.ALLOW_MEMBER_PROGRAMS)
     public ResponseEntity<Object> getAvailableDirectMemberControls(@PathVariable int id) {
         List<ProgramDirectMemberControl> directMemberControls =
@@ -128,14 +109,9 @@ public class LoadProgramSetupApiController {
         return new ResponseEntity<>(directMemberControls, HttpStatus.OK);
     }
 
-    @GetMapping("/availablePrograms")
-    public ResponseEntity<List<ProgramDetails>> getAvailablePrograms() {
-        return new ResponseEntity<>(loadProgramService.getAvailablePrograms(), HttpStatus.OK);
-    }
-
-    @GetMapping("/{id}/gears")
-    public ResponseEntity<List<LiteGear>> getGearsForProgram(@PathVariable int id) {
-        return new ResponseEntity<>(loadProgramService.getGearsForProgram(id), HttpStatus.OK);
+    @GetMapping("/getGearsForProgram/{programId}")
+    public ResponseEntity<List<LiteGear>> getGearsForProgram(@PathVariable int programId) {
+        return new ResponseEntity<>(loadProgramService.getGearsForProgram(programId), HttpStatus.OK);
     }
 
     @InitBinder("loadProgramCopy")
