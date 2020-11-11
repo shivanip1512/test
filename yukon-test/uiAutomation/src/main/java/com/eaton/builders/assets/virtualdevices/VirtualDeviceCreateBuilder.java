ab1547d01c81870c7a4f84c71dadc1cd1ccc4444
E9987765 <AnandKanekar@Eaton.com>
2020-11-11 10:55:15 +0530

    QA-6318 Addeed API test for create,edit,retrive for virtual device

--- yukon-test/uiAutomation/src/main/java/com/eaton/builders/assets/virtualdevices/VirtualDeviceCreateBuilder.java
+++ yukon-test/uiAutomation/src/main/java/com/eaton/builders/assets/virtualdevices/VirtualDeviceCreateBuilder.java
@@ -11,43 +11,47 @@ import com.eaton.rest.api.virtualdevice.VirtualDeviceRequest;
 import io.restassured.response.ExtractableResponse;
 
 public class VirtualDeviceCreateBuilder {
-    public static class Builder {
-        private String name;
-        private boolean enable;
+	public static class Builder {
+		private String name;
+		private boolean enable;
 
-        public Builder(Optional<String> name) {
-            String u = UUID.randomUUID().toString();
-            String uuid = u.replace("-", "");
+		public Builder(Optional<String> name) {
+			String u = UUID.randomUUID().toString();
+			String uuid = u.replace("-", "");
 
-            this.name = name.orElse("AT Virtual Device " + uuid);
-        }
+			this.name = name.orElse("AT Virtual Device " + uuid);
+		}
 
-        public Builder withName(String name) {
-            this.name = name;
-            return this;
-        }
+		public Builder withName(String name) {
+			this.name = name;
+			return this;
+		}
 
-        public Builder withEnable(Optional<Boolean> enable) {
-            this.enable = enable.orElse(true);
-            return this;
-        }
+		public Builder withEnable(Optional<Boolean> enable) {
+			this.enable = enable.orElse(true);
+			return this;
+		}
 
-        public JSONObject build() {
-            JSONObject j = new JSONObject();
-            j.put("name", this.name);
-            j.put("enable", this.enable);
+		public JSONObject build() {
+			JSONObject j = new JSONObject();
+			j.put("name", this.name);
+			j.put("enable", this.enable);
 
-            return j;
-        }
+			return j;
+		}
 
-        public Pair<JSONObject, JSONObject> create() {
-            JSONObject request = build();
-            ExtractableResponse<?> createResponse = VirtualDeviceRequest.createVirtualDevice(request.toString());
+		public Pair<JSONObject, JSONObject> create() {
+			JSONObject request = build();
+			ExtractableResponse<?> createResponse = VirtualDeviceRequest.createVirtualDevice(request.toString());
 
-            String res = createResponse.asString();
-            JSONObject response = new JSONObject(res);
+			String res = createResponse.asString();
+			JSONObject response = new JSONObject(res);
 
-            return new Pair<>(request, response);
-        }
-    }
+			return new Pair<>(request, response);
+		}
+	}
+
+	public static Builder buildDefaultVirtualDevice() {
+		return new VirtualDeviceCreateBuilder.Builder(Optional.empty()).withEnable(Optional.empty());
+	}
 }
