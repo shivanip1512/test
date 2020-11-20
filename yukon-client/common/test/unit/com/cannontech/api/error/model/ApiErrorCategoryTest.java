e1d290d3eac52891245e1379af57dfab28a7f414
Rawat, Payal <PayalRawat@Eaton.com>
2020-11-19 23:21:25 -0500

    yukon-client/common/test/unit/com/cannontech/api/error/model/ApiErrorCategoryTest.java edited online with Bitbucket
    Fixed spelling mistake

--- yukon-client/common/test/unit/com/cannontech/api/error/model/ApiErrorCategoryTest.java
+++ yukon-client/common/test/unit/com/cannontech/api/error/model/ApiErrorCategoryTest.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 public class ApiErrorCategoryTest {
     
     @Test
-    public void findDupicateCode() {
+    public void findDuplicateCode() {
         List<ApiErrorCategory> enumValues = Arrays.asList(ApiErrorCategory.values());
         List<Integer> codes = enumValues.stream()
                 .map(e -> e.getCode())
