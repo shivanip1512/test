8b59a0b345c841444ef415dd81a647f06d1a669c
Rahul Shrivastava <rahulshrivastava@eaton.com>
2020-11-24 23:58:44 +0530

    QA-6428 updated powershell changed server to localhost

--- yukon-test/uiAutomation/db_restore.ps1
+++ yukon-test/uiAutomation/db_restore.ps1
@@ -7,31 +7,14 @@ param(
 	 [string]$dbUser
  )
  
-function Test-Admin {
-  $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-  $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-}
-
-if ((Test-Admin) -eq $false)  {
-    if ($elevated) 
-    {
-        # tried to elevate, did not work, aborting
-    } 
-    else {
-        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
-}
-
-exit
-}
-
 Import-Module $PSScriptRoot\..\..\yukon-deploy\vm-install\copy-to-vm\YukonDeploy
-Stop-Services
+#Stop-Services
  
 if ($dbType -eq "oracle"){
 	echo "Oracle DB restore is not implemented yet"
 }else{
 		echo "Restoring the Database using command : RESTORE DATABASE $dbName FROM DISK='$PSScriptRoot\TestDBs\$releaseFolderName\Sql\$dbBackupFileName' WITH REPLACE"
-		sqlcmd -s $env:computername -Q "RESTORE DATABASE $dbName FROM DISK='$PSScriptRoot\TestDBs\$releaseFolderName\Sql\$dbBackupFileName' WITH REPLACE"
+		sqlcmd -s localhost -Q "RESTORE DATABASE $dbName FROM DISK='$PSScriptRoot\TestDBs\$releaseFolderName\Sql\$dbBackupFileName' WITH REPLACE"
 		#Below sql command will reset the user login , this is required when you restore a database using a backup file
-		sqlcmd -s $env:computername -Q "USE $dbName ;ALTER USER $dbUser WITH LOGIN = $dbUser;"
+		sqlcmd -s localhost -Q "USE $dbName ;ALTER USER $dbUser WITH LOGIN = $dbUser;"
 }
