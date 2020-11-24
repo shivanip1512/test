8a13bf60047f70ec3383606729bcbc404918cb53
Rahul Shrivastava <rahulshrivastava@eaton.com>
2020-11-24 22:27:48 +0530

    QA-6428 added the powershell to restore the db for smoke and regression

---
+++ yukon-test/uiAutomation/db_restore.ps1
@@ -0,0 +1,19 @@
+param(
+     [Parameter()]
+	 [string]$dbName,
+	 [string]$dbBackupFileName,
+	 [string]$dbType,
+	 [string]$releaseFolderName,
+	 [string]$dbUser
+ )
+Import-Module $PSScriptRoot\..\..\yukon-deploy\vm-install\copy-to-vm\YukonDeploy
+Stop-Services
+ 
+if ($dbType -eq "oracle"){
+	echo "Oracle DB restore is not implemented yet"
+}else{
+		echo "Restoring the Database using command : RESTORE DATABASE $dbName FROM DISK='$PSScriptRoot\TestDBs\$releaseFolderName\Sql\$dbBackupFileName' WITH REPLACE"
+		sqlcmd -s $env:computername -Q "RESTORE DATABASE $dbName FROM DISK='$PSScriptRoot\TestDBs\$releaseFolderName\Sql\$dbBackupFileName' WITH REPLACE"
+		#Below sql command will reset the user login , this is required when you restore a database using a backup file
+		sqlcmd -s $env:computername -Q "USE $dbName ;ALTER USER $dbUser WITH LOGIN = $dbUser;"
+}
