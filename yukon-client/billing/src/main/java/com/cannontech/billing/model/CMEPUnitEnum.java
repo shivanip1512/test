ed54ae97a61957d5c710c0793efaa98bcadbee3f
Shakya, Hasha <HarshaShakya@eaton.com>
2020-12-08 17:51:44 +0530

    YUK-23278 : Update CEMP units to map to correct BuiltInAttribute

--- yukon-client/billing/src/main/java/com/cannontech/billing/model/CMEPUnitEnum.java
+++ yukon-client/billing/src/main/java/com/cannontech/billing/model/CMEPUnitEnum.java
@@ -5,19 +5,19 @@ import com.cannontech.common.pao.attribute.model.BuiltInAttribute;
 import com.cannontech.common.pao.attribute.service.IllegalUseOfAttribute;
 
 public enum CMEPUnitEnum {
-    KWHREG(BuiltInAttribute.USAGE), // Meter dial or register readings for printing on monthly bill. 
-    KVAHREG(null), // Meter dial or register readings for printing on monthly bill. 
-    KVARHREG(null), // Meter dial or register readings for printing on monthly bill. 
-    GASREG(null), // Meter dial or register readings for printing on monthly bill. 
+    KWHREG(BuiltInAttribute.DELIVERED_KWH), // Energy used by the end customer and delivered by the utility; UOMs ending in 'REG' indicate register or faceplate readings.
+    KVAHREG(BuiltInAttribute.KVAH), // Meter dial or register readings for printing on monthly bill. 
+    KVARHREG(BuiltInAttribute.KVARH), // Meter dial or register readings for printing on monthly bill. 
+    GASREG(BuiltInAttribute.USAGE_GAS), // Meter dial or register readings for printing on monthly bill. 
     PULSE(null), // Direct meter pulse readings. 
     KW(BuiltInAttribute.PEAK_DEMAND), // Kilowatt demand, usually expressed as peak value in time interval. 
-    KVA(null), // Kilovolt-ampere demand, usually expressed as peak value in time interval. 
+    KVA(BuiltInAttribute.PEAK_KVA), // Kilovolt-ampere demand, usually expressed as peak value in time interval. 
     KVAR(BuiltInAttribute.PEAK_KVAR), // Kilovolt-Ampere-Reactive demand, usually expressed as peak value in time interval. Values may be positive or negative depending upon power factor. Values are positive for VARs produced by customer or negative for VARs consumed by customer. Induction motors consume watts and VARs. A condenser bank produces VARs. An over-excited generator produces watts and VARs. An under-excited generator produces watts and consumes VARs. 
-    KWH(BuiltInAttribute.LOAD_PROFILE), // Kilowatt hours used.  // Using LOAD_PROFILE for this value is a little unusual, 
+    KWH(BuiltInAttribute.DELIVERED_KWH_PER_INTERVAL), // Kilowatt hours used.  // Using LOAD_PROFILE for this value is a little unusual, 
                                                              // but since we are using this in correlation with the calculation constraint value it creates a KWH value.
-    KVAH(null), // Kilovolt-ampere hours. 
-    KVARH(BuiltInAttribute.KVARH), // Kilovolt-Ampere-Reactive hours. Values may be positive or negative depending upon power factor. See KVAR above for further notes. 
-    GKW(null), // Kilowatt generation, received from customer, usually expressed as peak value in time interval. Used when customer is generating power. 
+    KVAH(BuiltInAttribute.KVAH), // Kilovolt-ampere hours. 
+    KVARH(BuiltInAttribute.DELIVERED_KVARH_PER_INTERVAL), // Kilovolt-Ampere-Reactive hours. Values may be positive or negative depending upon power factor. See KVAR above for further notes. 
+    GKW(BuiltInAttribute.RECEIVED_DEMAND), // Kilowatt generation, received from customer, usually expressed as peak value in time interval. Used when customer is generating power. 
     GKVA(null), // Kilovolt-Ampere generation received from customer, usually expressed as peak value in time interval. Used when customer is generating power. 
     GKVAR(null), // Kilovolt-Ampere Reactive generation received from customer, usually expressed as peak value in time interval. Used when customer is generating power. Values may be positive or negative depending upon power  factor. See KVAR above for further notes. 
     GKWH(BuiltInAttribute.RECEIVED_KWH), // Kilowatt hours received from customer. Used when customer is generating power. 
