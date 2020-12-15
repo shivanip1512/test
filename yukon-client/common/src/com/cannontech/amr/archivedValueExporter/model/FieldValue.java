c73b8c2160dfdd6273c98e987accc8d3a8f1d3d3
Shakya, Hasha <HarshaShakya@eaton.com>
2020-12-15 16:00:00 +0530

    YUK-23281 : Add Attribute Name format option - UI (fixed review comment)

--- yukon-client/common/src/com/cannontech/amr/archivedValueExporter/model/FieldValue.java
+++ yukon-client/common/src/com/cannontech/amr/archivedValueExporter/model/FieldValue.java
@@ -3,10 +3,20 @@ package com.cannontech.amr.archivedValueExporter.model;
 import com.cannontech.common.i18n.DisplayableEnum;
 
 public enum FieldValue implements DisplayableEnum {
-    DEFAULT,
-    CMEP;
+    DEFAULT(FieldType.ATTRIBUTE_NAME),
+    CMEP(FieldType.ATTRIBUTE_NAME);
 
     private final static String keyPrefix = "yukon.web.modules.tools.bulk.archivedValueExporter.";
+
+    private FieldType[] fieldTypes;
+
+    FieldValue(FieldType... fieldTypes) {
+        this.fieldTypes = fieldTypes;
+    }
+
+    public FieldType[] getFieldTypes() {
+        return fieldTypes;
+    }
 
     @Override
     public String getFormatKey() {
