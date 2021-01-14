a94643e39949dd0e853e447c2c7be1e893f26fc6
Nelson, Stacey L <StaceyLNelson@Eaton.com>
2021-01-14 14:42:40 -0500

    7.5_1.sql - Oracle DBupdates for 7.5.1.

--- yukon-database/DBUpdates/oracle/7.5_1.sql
+++ yukon-database/DBUpdates/oracle/7.5_1.sql
@@ -6,4 +6,4 @@
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
 /**************************************************************/
-/* INSERT INTO CTIDatabase VALUES ('7.5', '30-SEP-2020', 'Latest Update', 1, SYSDATE); */
+INSERT INTO CTIDatabase VALUES ('7.5', '14-JAN-2021', 'Latest Update', 1, SYSDATE);
