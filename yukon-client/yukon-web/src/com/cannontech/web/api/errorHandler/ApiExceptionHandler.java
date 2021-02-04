58db9dde7fde30125a5b028a898678086b01331c
Satpathy, Samir K <SamirKSatpathy@eaton.com>
2021-02-04 10:16:47 +0530

    YUK-23464- Conflicts

--- yukon-client/yukon-web/src/com/cannontech/web/api/errorHandler/ApiExceptionHandler.java
+++ yukon-client/yukon-web/src/com/cannontech/web/api/errorHandler/ApiExceptionHandler.java
@@ -100,7 +100,8 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         notSupportingUris.add(ApiURL.drControlAreaUrl);
         notSupportingUris.add(ApiURL.drControlScenarioUrl);
         notSupportingUris.add(ApiURL.commChannelUrl);
-        notSupportingUris.add(ApiURL.virtualDeviceUrl);
+        notSupportingUris.add(ApiURL.attributeUrl);
+        notSupportingUris.add(ApiURL.attributeAssignmentsUrl);
         notSupportingUris.add(ApiURL.aggregateDataReportUrl);
     }
 
