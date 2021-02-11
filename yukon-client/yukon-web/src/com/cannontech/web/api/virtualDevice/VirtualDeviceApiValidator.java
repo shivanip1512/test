1d8895cf9a4460e62091b945f0321adce79b4fa1
carrieVant <44090659+carrieVant@users.noreply.github.com>
2021-02-11 08:00:59 -0600

    YUK-23579 - Virtual Meter - Meter Number Required   - made change so that when editing a Virtual Meter through the UI, a meter number cannot be blank

--- yukon-client/yukon-web/src/com/cannontech/web/api/virtualDevice/VirtualDeviceApiValidator.java
+++ yukon-client/yukon-web/src/com/cannontech/web/api/virtualDevice/VirtualDeviceApiValidator.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.validation.Errors;
 
 import com.cannontech.common.device.virtualDevice.VirtualDeviceBaseModel;
+import com.cannontech.common.device.virtualDevice.VirtualMeterModel;
 import com.cannontech.common.validator.SimpleValidator;
 import com.cannontech.common.validator.YukonApiValidationHelper;
+import com.cannontech.common.validator.YukonApiValidationUtils;
 import com.cannontech.stars.util.ServletUtils;
 
 @Service
@@ -23,6 +25,11 @@ public class VirtualDeviceApiValidator <T extends VirtualDeviceBaseModel> extend
         String virtualDeviceId = ServletUtils.getPathVariable("id");
         if (virtualDevice.getName() != null) {
             yukonApiValidationHelper.validatePaoName(virtualDevice.getName(), virtualDevice.getType(), errors, "Name", virtualDeviceId);
+        }
+        if (virtualDevice instanceof VirtualMeterModel)
+        {
+            VirtualMeterModel meter = (VirtualMeterModel) virtualDevice;
+            YukonApiValidationUtils.checkIsBlank(errors, "meterNumber", meter.getMeterNumber(), "Meter Number", true);
         }
     }
 }
