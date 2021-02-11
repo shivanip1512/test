0429bd43771ba7731d34edd76c2e694513c11e34
Chung, Richard <E0620670@USROKWHP6119074.eaton.ad.etn.com>
2021-02-11 13:16:10 -0500

    NM-1591
    In Yukon repo, used Eclipse to generate the hashcode() and equals()
    method. Added null check for rssi. NodeWiFiComm matches file in NM repo.

--- yukon-client/common/src/com/cannontech/common/rfn/message/node/NodeWiFiComm.java
+++ yukon-client/common/src/com/cannontech/common/rfn/message/node/NodeWiFiComm.java
@@ -59,46 +59,49 @@ public class NodeWiFiComm implements Serializable{
     }
 
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((deviceRfnIdentifier == null) ? 0 : deviceRfnIdentifier.hashCode());
-        result = prime * result + ((gatewayRfnIdentifier == null) ? 0 : gatewayRfnIdentifier.hashCode());
-        result = prime * result + ((nodeWiFiCommStatus == null) ? 0 : nodeWiFiCommStatus.hashCode());
-        result = prime * result + rssi;
-        result = prime * result + (int) (wiFiCommStatusTimestamp ^ (wiFiCommStatusTimestamp >>> 32));
-        return result;
-    }
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((deviceRfnIdentifier == null) ? 0 : deviceRfnIdentifier.hashCode());
+		result = prime * result + ((gatewayRfnIdentifier == null) ? 0 : gatewayRfnIdentifier.hashCode());
+		result = prime * result + ((nodeWiFiCommStatus == null) ? 0 : nodeWiFiCommStatus.hashCode());
+		result = prime * result + ((rssi == null) ? 0 : rssi.hashCode());
+		result = prime * result + (int) (wiFiCommStatusTimestamp ^ (wiFiCommStatusTimestamp >>> 32));
+		return result;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        NodeWiFiComm other = (NodeWiFiComm) obj;
-        if (deviceRfnIdentifier == null) {
-            if (other.deviceRfnIdentifier != null)
-                return false;
-        } else if (!deviceRfnIdentifier.equals(other.deviceRfnIdentifier))
-            return false;
-        if (gatewayRfnIdentifier == null) {
-            if (other.gatewayRfnIdentifier != null)
-                return false;
-        } else if (!gatewayRfnIdentifier.equals(other.gatewayRfnIdentifier))
-            return false;
-        if (nodeWiFiCommStatus != other.nodeWiFiCommStatus)
-            return false;
-        if (rssi.equals(other.rssi))
-            return false;
-        if (wiFiCommStatusTimestamp != other.wiFiCommStatusTimestamp)
-            return false;
-        return true;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NodeWiFiComm other = (NodeWiFiComm) obj;
+		if (deviceRfnIdentifier == null) {
+			if (other.deviceRfnIdentifier != null)
+				return false;
+		} else if (!deviceRfnIdentifier.equals(other.deviceRfnIdentifier))
+			return false;
+		if (gatewayRfnIdentifier == null) {
+			if (other.gatewayRfnIdentifier != null)
+				return false;
+		} else if (!gatewayRfnIdentifier.equals(other.gatewayRfnIdentifier))
+			return false;
+		if (nodeWiFiCommStatus != other.nodeWiFiCommStatus)
+			return false;
+		if (rssi == null) {
+			if (other.rssi != null)
+				return false;
+		} else if (!rssi.equals(other.rssi))
+			return false;
+		if (wiFiCommStatusTimestamp != other.wiFiCommStatusTimestamp)
+			return false;
+		return true;
+	}
 
-    @Override
+	@Override
     public String toString() {
         return String
                 .format("NodeWiFiComm [deviceRfnIdentifier=%s, gatewayRfnIdentifier=%s, nodeWiFiCommStatus=%s, wiFiCommStatusTimestamp=%s, rssi=%s]",
