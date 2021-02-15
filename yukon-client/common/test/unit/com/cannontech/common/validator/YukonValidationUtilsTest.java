7b887e95150327366824974dc8e069f0987d1ae6
E0552673 <E0552673@INPU1WHP6096232.eaton.ad.etn.com>
2021-02-15 13:06:12 +0530

    YUK-23605: Add unit tests for YukonValidationUtils and YukonValidationUtilsCommon:Review Comment

--- yukon-client/common/test/unit/com/cannontech/common/validator/YukonValidationUtilsTest.java
+++ yukon-client/common/test/unit/com/cannontech/common/validator/YukonValidationUtilsTest.java
@@ -5,64 +5,116 @@ import org.junit.Test;
 import org.springframework.validation.BindException;
 
 public class YukonValidationUtilsTest {
-    BindException error = new BindException(Object.class, "testObject");
+    BindException error = null;
 
     @Test
     public void checkExceedsMaxLength_Test() {
+        error = new BindException(Object.class, "testObject");
         Assert.assertFalse(YukonValidationUtils.checkExceedsMaxLength(error, "name", null, 3));
         Assert.assertFalse(YukonValidationUtils.checkExceedsMaxLength(error, "name", "", 3));
         Assert.assertFalse(YukonValidationUtils.checkExceedsMaxLength(error, "name", " ", 3));
-        Assert.assertTrue(YukonValidationUtils.checkExceedsMaxLength(error, "name", "    ", 3));
         Assert.assertFalse(YukonValidationUtils.checkExceedsMaxLength(error, "name", "fun", 3));
+        Assert.assertFalse(error.hasFieldErrors("name"));
+
+        error = new BindException(Object.class, "testObject");
+        Assert.assertTrue(YukonValidationUtils.checkExceedsMaxLength(error, "name", "    ", 3));
+        Assert.assertTrue(error.hasFieldErrors("name"));
+
+        error = new BindException(Object.class, "testObject");
         Assert.assertTrue(YukonValidationUtils.checkExceedsMaxLength(error, "name", "funny", 3));
+        Assert.assertTrue(error.hasFieldErrors("name"));
     }
 
     @Test
     public void checkBlacklistedCharacter_Test() {
+        error = new BindException(Object.class, "testObject");
         Assert.assertTrue(YukonValidationUtils.checkBlacklistedCharacter(error, "name", "\""));
-        Assert.assertTrue(YukonValidationUtils.checkBlacklistedCharacter(error, "name", "*"));
-        Assert.assertTrue(YukonValidationUtils.checkBlacklistedCharacter(error, "name", ","));
+        Assert.assertTrue(error.hasFieldErrors("name"));
 
+        error = new BindException(Object.class, "testObject");
+        Assert.assertTrue(YukonValidationUtils.checkBlacklistedCharacter(error, "name", "*"));
+        Assert.assertTrue(error.hasFieldErrors("name"));
+
+        error = new BindException(Object.class, "testObject");
+        Assert.assertTrue(YukonValidationUtils.checkBlacklistedCharacter(error, "name", ","));
+        Assert.assertTrue(error.hasFieldErrors("name"));
+
+        error = new BindException(Object.class, "testObject");
         Assert.assertFalse(YukonValidationUtils.checkBlacklistedCharacter(error, "name", null));
         Assert.assertFalse(YukonValidationUtils.checkBlacklistedCharacter(error, "name", ""));
         Assert.assertFalse(YukonValidationUtils.checkBlacklistedCharacter(error, "name", "   "));
         Assert.assertFalse(YukonValidationUtils.checkBlacklistedCharacter(error, "name", "fun"));
+        Assert.assertFalse(error.hasFieldErrors("name"));
     }
 
     @Test
     public void checkIllegalCharacter_Test() {
-
+        error = new BindException(Object.class, "testObject");
         Assert.assertTrue(YukonValidationUtils.checkIllegalCharacter(error, "name", "\""));
-        Assert.assertFalse(YukonValidationUtils.checkIllegalCharacter(error, "name", "*"));
-        Assert.assertTrue(YukonValidationUtils.checkIllegalCharacter(error, "name", ","));
+        Assert.assertTrue(error.hasFieldErrors("name"));
 
+        error = new BindException(Object.class, "testObject");
+        Assert.assertTrue(YukonValidationUtils.checkIllegalCharacter(error, "name", ","));
+        Assert.assertTrue(error.hasFieldErrors("name"));
+
+        error = new BindException(Object.class, "testObject");
+        Assert.assertFalse(YukonValidationUtils.checkIllegalCharacter(error, "name", "*"));
         Assert.assertFalse(YukonValidationUtils.checkIllegalCharacter(error, "name", null));
         Assert.assertFalse(YukonValidationUtils.checkIllegalCharacter(error, "name", ""));
         Assert.assertFalse(YukonValidationUtils.checkIllegalCharacter(error, "name", "fun"));
+        Assert.assertFalse(error.hasFieldErrors("name"));
     }
 
     @Test
     public void checkIsBlank_Test() {
+        error = new BindException(Object.class, "testObject");
         Assert.assertFalse(YukonValidationUtils.checkIsBlank(error, "name", null, true));
-        Assert.assertTrue(YukonValidationUtils.checkIsBlank(error, "name", null, false));
-
-        Assert.assertTrue(YukonValidationUtils.checkIsBlank(error, "name", "", true));
-        Assert.assertTrue(YukonValidationUtils.checkIsBlank(error, "name", "", false));
-
-        Assert.assertTrue(YukonValidationUtils.checkIsBlank(error, "name", "   ", true));
-        Assert.assertTrue(YukonValidationUtils.checkIsBlank(error, "name", "   ", false));
-
         Assert.assertFalse(YukonValidationUtils.checkIsBlank(error, "name", "fun", true));
         Assert.assertFalse(YukonValidationUtils.checkIsBlank(error, "name", "fun", false));
+        Assert.assertFalse(error.hasFieldErrors("name"));
+
+        error = new BindException(Object.class, "testObject");
+        Assert.assertTrue(YukonValidationUtils.checkIsBlank(error, "name", null, false));
+        Assert.assertTrue(error.hasFieldErrors("name"));
+
+        error = new BindException(Object.class, "testObject");
+        Assert.assertTrue(YukonValidationUtils.checkIsBlank(error, "name", "", true));
+        Assert.assertTrue(error.hasFieldErrors("name"));
+
+        error = new BindException(Object.class, "testObject");
+        Assert.assertTrue(YukonValidationUtils.checkIsBlank(error, "name", "", false));
+        Assert.assertTrue(error.hasFieldErrors("name"));
+
+        error = new BindException(Object.class, "testObject");
+        Assert.assertTrue(YukonValidationUtils.checkIsBlank(error, "name", "   ", true));
+        Assert.assertTrue(error.hasFieldErrors("name"));
+
+        error = new BindException(Object.class, "testObject");
+        Assert.assertTrue(YukonValidationUtils.checkIsBlank(error, "name", "   ", false));
+        Assert.assertTrue(error.hasFieldErrors("name"));
     }
 
     @Test
     public void checkIsValidDouble_Test() {
+        error = new BindException(Object.class, "testObject");
         Assert.assertFalse(YukonValidationUtils.checkIsValidDouble(error, "name", null));
+        Assert.assertTrue(error.hasFieldErrors("name"));
+
+        error = new BindException(Object.class, "testObject");
         Assert.assertFalse(YukonValidationUtils.checkIsValidDouble(error, "name", Double.NaN));
+        Assert.assertTrue(error.hasFieldErrors("name"));
+
+        error = new BindException(Object.class, "testObject");
         Assert.assertFalse(YukonValidationUtils.checkIsValidDouble(error, "name", Double.POSITIVE_INFINITY));
+        Assert.assertTrue(error.hasFieldErrors("name"));
+
+        error = new BindException(Object.class, "testObject");
         Assert.assertFalse(YukonValidationUtils.checkIsValidDouble(error, "name", Double.NEGATIVE_INFINITY));
+        Assert.assertTrue(error.hasFieldErrors("name"));
+
+        error = new BindException(Object.class, "testObject");
         Assert.assertTrue(YukonValidationUtils.checkIsValidDouble(error, "name", (double) 78));
+        Assert.assertFalse(error.hasFieldErrors("name"));
     }
 
     @Test
