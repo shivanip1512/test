52c626c1a3983e3461cccd84e918dbb05d89b965
E0534908 <JohnMJustin@Eaton.com>
2021-03-08 12:28:26 -0600

    YUK-23661 Oracle Database Connection Lost
    Changed it back to initial db connection creation
    added connection.close() to prevent connections staying open

--- yukon-client/yukon-watchdog-service/src/com/cannontech/watchdogs/util/DBConnectionUtil.java
+++ yukon-client/yukon-watchdog-service/src/com/cannontech/watchdogs/util/DBConnectionUtil.java
@@ -6,7 +6,8 @@ import java.sql.SQLException;
 import org.apache.logging.log4j.Logger;
 
 import com.cannontech.clientutils.YukonLogManager;
-import com.cannontech.database.PoolManager;
+import com.cannontech.database.YukonJdbcTemplate;
+import com.cannontech.spring.YukonSpringHook;
 import com.cannontech.watchdogs.impl.DBName;
 
 public class DBConnectionUtil {
@@ -15,10 +16,12 @@ public class DBConnectionUtil {
 
     public static boolean isDBConnected(DBName dbName) {
         try {
-            Connection connection = PoolManager.getYukonConnection();
+            YukonJdbcTemplate jdbcTemplate = YukonSpringHook.getBean(YukonJdbcTemplate.class);
+            Connection connection = jdbcTemplate.getDataSource().getConnection();
             if (connection == null || connection.isClosed()) {
                 return false;
             }
+            connection.close();
         } catch (SQLException | RuntimeException e) {
             log.error("Error Connecting " + dbName + " database.", e);
             return false;
