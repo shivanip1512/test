08afcff4764b14e719cfa5383e7b4590d03644c3
Shakya, Hasha <HarshaShakya@eaton.com>
2021-03-15 16:51:26 +0530

    YUK-23856 : Create new Validation classes for REST APIs for DR(deleted redundant code)

--- yukon-client/yukon-web/src/com/cannontech/web/api/dr/setup/LMApiValidatorHelper.java
+++ yukon-client/yukon-web/src/com/cannontech/web/api/dr/setup/LMApiValidatorHelper.java
@@ -12,10 +12,7 @@ import com.cannontech.api.error.model.ApiErrorDetails;
 import com.cannontech.common.dr.setup.LMCopy;
 import com.cannontech.common.dr.setup.LoadGroupCopy;
 import com.cannontech.common.pao.PaoType;
-import com.cannontech.common.pao.PaoUtils;
 import com.cannontech.common.validator.YukonApiValidationUtils;
-import com.cannontech.common.validator.YukonValidationUtilsCommon;
-import com.cannontech.core.dao.PaoDao;
 import com.cannontech.database.data.lite.LiteYukonPAObject;
 import com.cannontech.stars.util.ServletUtils;
 import com.cannontech.yukon.IDatabaseCache;
@@ -24,59 +21,13 @@ import com.cannontech.yukon.IDatabaseCache;
  * Helper class for LM API validation
  */
 public class LMApiValidatorHelper {
-    @Autowired private PaoDao paoDao;
     @Autowired private IDatabaseCache serverDatabaseCache;
-
-    public void checkIfFieldRequired(String field, Errors errors, Object fieldValue, String fieldName) {
-        if (YukonValidationUtilsCommon.checkIfFieldRequired(fieldValue)) {
-            errors.rejectValue(field, ApiErrorDetails.FIELD_REQUIRED.getCodeString(), new Object[] { fieldName }, "");
-        }
-    }
-
-    public void validateName(String paoName, Errors errors, String fieldName) {
-        checkIfFieldRequired("name", errors, paoName, fieldName);
-        if (!errors.hasFieldErrors("name")) {
-            YukonApiValidationUtils.checkExceedsMaxLength(errors, "name", paoName, 60);
-            if (!PaoUtils.isValidPaoName(paoName)) {
-                errors.rejectValue("name", ApiErrorDetails.ILLEGAL_CHARACTERS.getCodeString(), new Object[] { fieldName }, "");
-            }
-        }
-    }
-
     /**
      * Checks whether the Pao name is unique or not
      */
-    private void validateUniquePaoName(String paoName, PaoType type, Errors errors, String fieldName) {
-        LiteYukonPAObject unique = paoDao.findUnique(paoName, type);
-        if (unique != null) {
-            errors.rejectValue("name", ApiErrorDetails.ALREADY_EXISTS.getCodeString(), new Object[] { fieldName },
-                    "");}
-    }
-
-    public void validateNewPaoName(String paoName, PaoType type, Errors errors, String fieldName) {
-        validateName(paoName, errors, fieldName);
-        if (!errors.hasFieldErrors("name")) {
-            String paoId = ServletUtils.getPathVariable("id");
-            // Check if pao name already exists
-            if (type != null && (paoId == null || !(paoDao.getYukonPAOName(Integer.valueOf(paoId)).equalsIgnoreCase(paoName)))) {
-                validateUniquePaoName(paoName, type, errors, fieldName);
-            }
-        }
-    }
-
-    public void validateCopyPaoName(String paoName, Errors errors, String fieldName) {
-        validateName(paoName, errors, fieldName);
-
-        if (!errors.hasFieldErrors("name")) {
-            Integer paoId = Integer.valueOf(ServletUtils.getPathVariable("id"));
-            if (paoId != null) {
-                validateUniquePaoName(paoName, paoDao.getLiteYukonPAO(paoId).getPaoType(), errors, fieldName);
-            }
-        }
-    }
 
     public void validateRoute(Errors errors, Integer routeId) {
-        checkIfFieldRequired("routeId", errors, routeId, "Route Id");
+        YukonApiValidationUtils.checkIfFieldRequired("routeId", errors, routeId, "Route Id");
         if (!errors.hasFieldErrors("routeId")) {
             LiteYukonPAObject liteRoute = serverDatabaseCache.getAllRoutesMap().get(routeId);
             if (liteRoute == null) {
--- yukon-client/yukon-web/src/com/cannontech/web/api/dr/setup/LMCopyApiValidator.java
+++ yukon-client/yukon-web/src/com/cannontech/web/api/dr/setup/LMCopyApiValidator.java
@@ -5,9 +5,12 @@ import org.springframework.validation.Errors;
 
 import com.cannontech.common.dr.setup.LMCopy;
 import com.cannontech.common.validator.SimpleValidator;
+import com.cannontech.common.validator.YukonApiValidationHelper;
+import com.cannontech.stars.util.ServletUtils;
 
 public class LMCopyApiValidator extends SimpleValidator<LMCopy> {
     @Autowired private LMApiValidatorHelper lmApiValidatorHelper;
+    @Autowired private YukonApiValidationHelper yukonApiValidationHelper;
 
     public LMCopyApiValidator() {
         super(LMCopy.class);
@@ -17,7 +20,10 @@ public class LMCopyApiValidator extends SimpleValidator<LMCopy> {
     protected void doValidation(LMCopy lmCopy, Errors errors) {
 
         // Group Name
-        lmApiValidatorHelper.validateCopyPaoName(lmCopy.getName(), errors, "Name");
+        String paoId = ServletUtils.getPathVariable("id");
+        yukonApiValidationHelper.validatePaoName(lmCopy.getName(), null, errors, "Name", paoId);
+        
+        //lmApiValidatorHelper.validateCopyPaoName(lmCopy.getName(), errors, "Name");
 
         // Validate routeId if present.
         lmApiValidatorHelper.validateRouteId(lmCopy, errors, "RouteId");
