5eff4c1fa75a407a151c3860dc89419e325b5963
Shinde, Nitin S <NitinSShinde@eaton.com>
2021-03-20 12:18:26 -0400

    Merge pull request #2468 in EASD_SW/yukon from feature/Nitin/YUK-23967-SonarDBSetup to master
    Squashed commit of the following:
    
    commit 3455693a34bd78f3fbbce5a0bd7a48d46a2e1c1d
    Author: Shinde, Nitin S <nitinsshinde@eaton.com>
    Date:   Mon Mar 15 11:37:01 2021 +0530
    
        YUK-23967 : Setup Database for currently running Sonar Server.

--- yukon-deploy/vm-install/CodeCoverage/SeleniumCoverage.bat
+++ yukon-deploy/vm-install/CodeCoverage/SeleniumCoverage.bat
@@ -1,3 +1,3 @@
 cd C:\Yukon\Runtime\bin
-java -jar C:\CodeCoverage\jacococli.jar dump --address 127.0.0.1 --port 6300 --destfile C:\CodeCoverage\jacoco-CodeCoverageAPI.exec
-java -jar C:\CodeCoverage\jacococli.jar report C:\CodeCoverage\jacoco-CodeCoverageAPI.exec --xml C:\CodeCoverage\apiCoverage.xml --classfiles C:\CodeCoverage\com\cannontech
+java -jar C:\CodeCoverage\jacococli.jar dump --address 127.0.0.1 --port 6300 --destfile C:\CodeCoverage\jacoco-CodeCoverageSelenium.exec
+java -jar C:\CodeCoverage\jacococli.jar report C:\CodeCoverage\jacoco-CodeCoverageSelenium.exec --xml C:\CodeCoverage\SeleniumCoverage.xml --classfiles C:\CodeCoverage\com\cannontech
