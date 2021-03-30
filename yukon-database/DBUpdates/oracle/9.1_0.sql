edb7e7dbfe1d062e6b5c81869697b6ac133ab0c0
E9816181 <MatthewSFisher@Eaton.com>
2021-03-30 10:50:21 -0700

    YUK-23949 Add Install Orientation to existing regulator device configurations Added installOrientation=FORWARD to all regulator configurations Updated default regulator category in creation scripts Updated physical models

--- yukon-database/DBUpdates/oracle/9.1_0.sql
+++ yukon-database/DBUpdates/oracle/9.1_0.sql
@@ -15,6 +15,19 @@ WHERE ProgramId IN
 INSERT INTO DBUpdates VALUES ('YUK-23591', '9.1.0', SYSDATE);
 /* @end YUK-23591 */
 
+/* Start YUK-23949 */
+INSERT INTO DeviceConfigCategoryItem
+SELECT ROW_NUMBER() OVER (ORDER BY DeviceConfigCategoryID) 
+           + (SELECT NVL(MAX(DeviceConfigCategoryItemID), 1) FROM DeviceConfigCategoryItem),
+       DeviceConfigCategoryID,
+       'installOrientation',
+       'FORWARD'
+FROM DeviceConfigCategory 
+WHERE CategoryType = 'regulatorCategory';
+
+INSERT INTO DBUpdates VALUES ('YUK-23949', '9.1.0', SYSDATE);
+/* End YUK-23949 */
+
 /**************************************************************/
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
