696680876a114752309a482b27ca833ed768162b
Shinde, Nitin S <nitinsshinde@eaton.com>
2021-05-26 22:14:29 +0530

    YUK-24401 : sharedKeyFile - event log when decrypting global setting fails

---
+++ yukon-client/common/src/com/cannontech/common/events/helper/EventLogHelper.java
@@ -0,0 +1,27 @@
+package com.cannontech.common.events.helper;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.apache.logging.log4j.Logger;
+import org.joda.time.Instant;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cannontech.clientutils.YukonLogManager;
+import com.cannontech.common.events.loggers.SystemEventLogService;
+import com.cannontech.common.util.ApplicationId;
+
+public class EventLogHelper {
+
+    @Autowired private SystemEventLogService systemEventLogService;
+
+    private static final Logger log = YukonLogManager.getLogger(EventLogHelper.class);
+
+    public void decryptionFailedEventLog(String yukonService, String type) {
+        try {
+            systemEventLogService.decryptionFailed(ApplicationId.SERVICE_MANAGER.getApplicationName(), InetAddress.getLocalHost().getHostName(), type, Instant.now());
+        } catch (UnknownHostException ex) {
+            log.error("Unknown host exception : \"" + ex.getMessage() + "\".");
+        }
+    }
+}
