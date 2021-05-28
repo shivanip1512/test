d33933e84146b48516e54a937e1b381bfffe9704
Shinde, Nitin S <nitinsshinde@eaton.com>
2021-05-28 14:35:35 +0530

    YUK-24401 : sharedKeyFile - event log when decrypting global setting fails.

--- yukon-client/common/test/unit/com/cannontech/encryption/impl/EcobeeSecurityServiceImplTest.java
+++ yukon-client/common/test/unit/com/cannontech/encryption/impl/EcobeeSecurityServiceImplTest.java
@@ -8,12 +8,14 @@ import java.io.InputStream;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
+import org.easymock.Capture;
 import org.easymock.EasyMock;
 import org.joda.time.Instant;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import com.cannontech.common.events.helper.EventLogHelper;
 import com.cannontech.common.exception.EcobeePGPException;
 import com.cannontech.database.db.security.EncryptionKey;
 import com.cannontech.encryption.EncryptedRouteDao;
@@ -23,12 +25,15 @@ public class EcobeeSecurityServiceImplTest {
 
     private EcobeeSecurityServiceImpl mockEcobeeSecurityServiceImpl;
     private EncryptedRouteDao mockEncryptedRouteDao;
+    private EventLogHelper mockEventLogHelper;
 
     @Before
     public void setup() {
         mockEncryptedRouteDao = EasyMock.createMock(EncryptedRouteDao.class);
+        mockEventLogHelper = EasyMock.createMock(EventLogHelper.class);
         mockEcobeeSecurityServiceImpl = new EcobeeSecurityServiceImpl();
         ReflectionTestUtils.setField(mockEcobeeSecurityServiceImpl, "encryptedRouteDao", mockEncryptedRouteDao);
+        ReflectionTestUtils.setField(mockEcobeeSecurityServiceImpl, "eventLogHelper", mockEventLogHelper);
     }
     
     @Test
@@ -64,11 +69,16 @@ public class EcobeeSecurityServiceImplTest {
 
     @Test(expected = EcobeePGPException.class)
     public void test_decryptEcobeeFile_Fail() {
+        Capture<String> yukonService = new Capture<>();
+        Capture<String> type = new Capture<>();
         EncryptionKey encryptionKey = new EncryptionKey();
         encryptionKey.setPrivateKey("de87aaa5e01fd1af0f0dd265a65a65b851510ac3557f44d553727c4d6d257f");
         InputStream stubInputStream = new ByteArrayInputStream( "Dummy data".getBytes() );
         EasyMock.expect(mockEncryptedRouteDao.getEncryptionKey(EncryptionKeyType.Ecobee)).andReturn(Optional.of(encryptionKey));
         EasyMock.replay(mockEncryptedRouteDao);
+        mockEventLogHelper.decryptionFailedEventLog(EasyMock.capture(yukonService),EasyMock.capture(type));
+        EasyMock.expectLastCall().once();
+        EasyMock.replay(mockEventLogHelper);
         mockEcobeeSecurityServiceImpl.decryptEcobeeFile(stubInputStream);
     }
 }
