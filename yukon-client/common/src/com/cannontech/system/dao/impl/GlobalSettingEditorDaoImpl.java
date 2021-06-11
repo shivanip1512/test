a9b1386b71161721a831d4a6eb08008c2d5a242e
E0360047 <NiveditaKumari@Eaton.com>
2021-06-11 16:56:14 +0530

    YUK-24402

--- yukon-client/common/src/com/cannontech/system/dao/impl/GlobalSettingEditorDaoImpl.java
+++ yukon-client/common/src/com/cannontech/system/dao/impl/GlobalSettingEditorDaoImpl.java
@@ -64,7 +64,7 @@ public class GlobalSettingEditorDaoImpl implements GlobalSettingEditorDao {
                         }
                     } catch (CryptoException | IOException | JDOMException | DecoderException e) {
                         value = type.getDefaultValue();
-                        eventLogHelper.decryptionFailedEventLog(BootstrapUtils.getApplicationName(), type.getDescriptionKey());
+                        eventLogHelper.decryptionFailedEventLog(BootstrapUtils.getApplicationName(), type.name());
                         decryptValueFailed = true;
                         log.error("Unable to decrypt value for setting " + type + ". Using the default value. ", e);
                     }
