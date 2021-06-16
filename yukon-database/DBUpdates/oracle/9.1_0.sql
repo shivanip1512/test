5579de78d7798285e9af916d3f2ebbbf8ff738c3
E0534908 <JohnMJustin@Eaton.com>
2021-06-16 08:28:52 -0500

    YUK-23668 RFN-1200 Web Edit - DB Change
    Fixed some typos in the Oracle script

--- yukon-database/DBUpdates/oracle/9.1_0.sql
+++ yukon-database/DBUpdates/oracle/9.1_0.sql
@@ -83,16 +83,16 @@ INSERT INTO StateGroup VALUES(-33, 'OnOff', 'Status');
 INSERT INTO State VALUES(-33, 0, 'Off', 1, 6, 0);
 INSERT INTO State VALUES(-33, 1, 'On', 0, 6, 0);
 
-INSERT INTO DBUpdates VALUES ('YUK-24286', '9.0.0', SYSDATE);
+INSERT INTO DBUpdates VALUES ('YUK-24286', '9.1.0', SYSDATE);
 /* @end YUK-24286 */
 
 /* @start YUK-23668 */
 INSERT INTO PortTiming (PORTID, PRETXWAIT, RTSTOTXWAIT, POSTTXWAIT, RECEIVEDATAWAIT, EXTRATIMEOUT, PostCommWait)
-SELECT 218648, 0, 0, 0, 0, 0, 0 
+SELECT PAObjectID, 0, 0, 0, 0, 0, 0 
 FROM YukonPAObject
 WHERE Type = 'RFN-1200'
 
-INSERT INTO DBUpdates VALUES ('YUK-23668', '9.0.0', SYSDATE);
+INSERT INTO DBUpdates VALUES ('YUK-23668', '9.1.0', SYSDATE);
 /* @end YUK-23668 */
 
 /**************************************************************/
