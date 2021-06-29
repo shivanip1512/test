724b781ca0d49326c5a706550104580fc8c50f50
Satpathy, Samir K <SamirKSatpathy@eaton.com>
2021-06-29 12:55:42 +0530

    YUK-24529: Dev Task- Support Multiple enrolment for Zeus- Updated Import

--- yukon-client/common/test/unit/com/cannontech/stars/dr/program/service/impl/ProgramEnrollmentServiceImplTest.java
+++ yukon-client/common/test/unit/com/cannontech/stars/dr/program/service/impl/ProgramEnrollmentServiceImplTest.java
@@ -1,7 +1,7 @@
 package com.cannontech.stars.dr.program.service.impl;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
