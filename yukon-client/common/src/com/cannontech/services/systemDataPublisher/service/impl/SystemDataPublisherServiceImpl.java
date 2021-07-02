db0b65689ae7bba591a400ba833075b7edbceafd
E0552673 <E0552673@INPU1WHP6096232.eaton.ad.etn.com>
2021-07-02 15:46:00 +0530

    YUK-24554: Upgrade Azure Service Bus from 3.5.1 to new-messageingversionDev task

--- yukon-client/common/src/com/cannontech/services/systemDataPublisher/service/impl/SystemDataPublisherServiceImpl.java
+++ yukon-client/common/src/com/cannontech/services/systemDataPublisher/service/impl/SystemDataPublisherServiceImpl.java
@@ -14,7 +14,7 @@ import com.cannontech.services.systemDataPublisher.service.SystemDataPublisherSe
 import com.cannontech.services.systemDataPublisher.service.model.SystemData;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
 
 @Service
 public class SystemDataPublisherServiceImpl implements SystemDataPublisherService {
@@ -32,7 +32,7 @@ public class SystemDataPublisherServiceImpl implements SystemDataPublisherServic
     public void initialize() {
         converter = new MappingJackson2MessageConverter();
         ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JSR310Module());
+        mapper.registerModule(new JodaModule());
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         converter.setObjectMapper(mapper);
 
