c3ea69877f810b8aa9ea2f302ea0e034c5c96173
Satpathy, Samir K <SamirKSatpathy@eaton.com>
2021-07-19 20:03:13 +0530

    YUK-24491-backend-changes-for-log4j2-migration: Resolving Conflicts

--- yukon-client/yukon-web/src/com/cannontech/web/admin/YukonLoggersController.java
+++ yukon-client/yukon-web/src/com/cannontech/web/admin/YukonLoggersController.java
@@ -1,19 +1,15 @@
 package com.cannontech.web.admin;
 
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+import javax.annotation.PostConstruct;
 
 import org.apache.commons.lang3.BooleanUtils;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -21,9 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.client.RestClientException;
 
-import com.cannontech.clientutils.YukonLogManager;
 import com.cannontech.common.i18n.DisplayableEnum;
 import com.cannontech.common.i18n.MessageSourceAccessor;
 import com.cannontech.common.log.model.LoggerLevel;
@@ -35,31 +29,34 @@ import com.cannontech.common.model.SortingParameters;
 import com.cannontech.i18n.YukonMessageSourceResolvable;
 import com.cannontech.i18n.YukonUserContextMessageSourceResolver;
 import com.cannontech.user.YukonUserContext;
-import com.cannontech.web.api.ApiRequestHelper;
-import com.cannontech.web.api.ApiURL;
-import com.cannontech.web.api.validation.ApiCommunicationException;
-import com.cannontech.web.api.validation.ApiControllerHelper;
 import com.cannontech.web.common.flashScope.FlashScope;
 import com.cannontech.web.common.sort.SortableColumn;
 
 @Controller
 public class YukonLoggersController {
 
-    private static final Logger log = YukonLogManager.getLogger(YukonLoggersController.class);
-
     @Autowired private YukonUserContextMessageSourceResolver messageResolver;
-    @Autowired private ApiRequestHelper apiRequestHelper;
-    @Autowired private ApiControllerHelper apiControllerHelper;
 
-    private static final String communicationKey = "yukon.exception.apiCommunicationException.communicationError";
-    private static final String baseKey = "yukon.web.modules.adminSetup.config.loggers.";
     private static final String redirectLink = "redirect:/admin/config/loggers/allLoggers";
+    private ConcurrentHashMap<Integer, YukonLogger> cache = new ConcurrentHashMap<Integer, YukonLogger>();
+
+    @PostConstruct
+    public void init() {
+        SystemLogger loggers[] = SystemLogger.values();
+        for (int id = 0; id < loggers.length; id++) {
+            YukonLogger logger = new YukonLogger();
+            logger.setLoggerId(id+1);
+            logger.setLoggerName(loggers[id].getLoggerName());
+            logger.setLevel(loggers[id].getLevel());
+            cache.put(id+1, logger);
+        }
+    }
 
     @GetMapping("/config/loggers/allLoggers")
     public String getAllLoggers(@DefaultSort(dir = Direction.asc, sort = "loggerName") SortingParameters sorting,
-            String loggerName, LoggerLevel[] loggerLevels, ModelMap model, YukonUserContext userContext,
-            HttpServletRequest request, FlashScope flashScope) {
-        retrieveLoggers(sorting, loggerName, loggerLevels, model, userContext, request, flashScope);
+            String loggerName, LoggerLevel[] loggerLevels, ModelMap model, YukonUserContext userContext) {
+
+        retrieveLoggers(sorting, loggerName, loggerLevels, model, userContext);
         model.addAttribute("loggerLevels", LoggerLevel.values());
         return "config/loggers.jsp";
     }
@@ -78,79 +75,47 @@ public class YukonLoggersController {
     }
 
     @PostMapping("/config/loggers")
-    public String saveLogger(@ModelAttribute YukonLogger logger, Boolean specifiedDateTime, HttpServletRequest request,
-            YukonUserContext userContext, FlashScope flashScope) {
-        try {
-            ResponseEntity<? extends Object> response = save(logger, specifiedDateTime, request, userContext, flashScope);
-            if (response.getStatusCode() == HttpStatus.OK || response.getStatusCode() == HttpStatus.CREATED) {
-                flashScope.setConfirm(new YukonMessageSourceResolvable("yukon.common.save.success", logger.getLoggerName()));
-            }
-        } catch (ApiCommunicationException e) {
-            log.error(e);
-            flashScope.setError(new YukonMessageSourceResolvable(communicationKey));
-        } catch (RestClientException ex) {
-            log.error("Error saving logger. Error: {}", ex.getMessage());
-            flashScope.setError(
-                    new YukonMessageSourceResolvable("yukon.web.api.save.error", logger.getLoggerName(), ex.getMessage()));
-        }
+    public String saveLogger(@ModelAttribute YukonLogger logger, Boolean specifiedDateTime) {
+        save(logger, specifiedDateTime);
         return redirectLink;
     }
 
     @GetMapping("/config/loggers/{loggerId}")
-    public String editLogger(@ModelAttribute YukonLogger logger, @PathVariable Integer loggerId, ModelMap model,
-            HttpServletRequest request, YukonUserContext userContext, FlashScope flashScope) {
-        try {
-            String getUrl = apiControllerHelper.findWebServerUrl(request, userContext, ApiURL.loggerUrl + "/" + loggerId);
+    public String editLogger(@PathVariable Integer loggerId, ModelMap model) {
+        YukonLogger logger = cache.get(loggerId);
+        model.addAttribute("logger", logger);
+        model.addAttribute("loggerLevels", LoggerLevel.values());
+        model.addAttribute("isEditMode", true);
+        Date expirationDate = new Date();
+        model.addAttribute("now", expirationDate);
+        model.addAttribute("specifiedDateTime", logger.getExpirationDate() != null);
 
-            ResponseEntity<? extends Object> loggerResponse = apiRequestHelper.callAPIForObject(userContext, request, getUrl,
-                    HttpMethod.GET, YukonLogger.class, Integer.class);
+        boolean allowDateTimeSelection = !SystemLogger.isSystemLogger(logger.getLoggerName());
+        model.addAttribute("allowDateTimeSelection", allowDateTimeSelection);
 
-            if (loggerResponse.getStatusCode() == HttpStatus.OK) {
-                logger = (YukonLogger) loggerResponse.getBody();
-                model.addAttribute("logger", logger);
-                model.addAttribute("loggerLevels", LoggerLevel.values());
-                model.addAttribute("isEditMode", true);
-                model.addAttribute("allowDateTimeSelection", true);
-            }
-        } catch (ApiCommunicationException e) {
-            log.error(e);
-            flashScope.setError(new YukonMessageSourceResolvable(communicationKey));
-        } catch (RestClientException ex) {
-            log.error("Error retreiving logger. Error: {}", ex.getMessage());
-            flashScope.setError(
-                    new YukonMessageSourceResolvable("yukon.web.api.retrieve.error", logger.getLoggerName(), ex.getMessage()));
-        }
         return "config/addLoggerPopup.jsp";
     }
 
-    private ResponseEntity<? extends Object> save(YukonLogger logger, Boolean specifiedDateTime, HttpServletRequest request,
-            YukonUserContext userContext,
-            FlashScope flashScope) {
-        if (BooleanUtils.isNotTrue(specifiedDateTime)) {
+    private void save(YukonLogger logger, Boolean specifiedDateTime) {
+        if(BooleanUtils.isNotTrue(specifiedDateTime)) {
             logger.setExpirationDate(null);
         }
         if (logger.getLoggerId() == 0) {
-            String url = apiControllerHelper.findWebServerUrl(request, userContext, ApiURL.loggerUrl);
-            return apiRequestHelper.callAPIForObject(userContext, request, url, HttpMethod.POST, YukonLogger.class,
-                    logger);
+            List<Integer> sortedKeys = cache.keySet().stream().sorted().collect(Collectors.toList());
+            logger.setLoggerId(sortedKeys.size() + 1);
+            cache.put(logger.getLoggerId(), logger);
         } else {
-            String url = apiControllerHelper.findWebServerUrl(request, userContext,
-                    ApiURL.loggerUrl + "/" + logger.getLoggerId());
-            return apiRequestHelper.callAPIForObject(userContext, request, url, HttpMethod.PUT, YukonLogger.class,
-                    logger);
+            cache.remove(logger.getLoggerId());
+            cache.put(logger.getLoggerId(), logger);
         }
     }
 
     @DeleteMapping("/config/loggers/{loggerId}")
-    public String deleteLogger(@PathVariable Integer loggerId, ModelMap model, FlashScope flashScope, HttpServletRequest request,
-            YukonUserContext userContext) {
-
-        String deleteUrl = apiControllerHelper.findWebServerUrl(request, userContext, ApiURL.loggerUrl + "/" + loggerId);
-        ResponseEntity<? extends Object> deleteResponse = apiRequestHelper.callAPIForObject(userContext, request, deleteUrl,
-                HttpMethod.DELETE, Object.class, Integer.class);
-
-        if (deleteResponse.getStatusCode() == HttpStatus.OK) {
-            flashScope.setConfirm(new YukonMessageSourceResolvable("yukon.common.delete.success", /* loggerName */""));
+    public String deleteLogger(@PathVariable Integer loggerId, ModelMap model, FlashScope flashScope) {
+        if (cache.get(loggerId) != null) {
+            String loggerName = cache.get(loggerId).getLoggerName();
+            cache.remove(loggerId);
+            flashScope.setConfirm(new YukonMessageSourceResolvable("yukon.common.delete.success", loggerName));
         } else {
             flashScope.setError(new YukonMessageSourceResolvable("yukon.web.api.delete.error", "Logger", "Invalid logger Id"));
         }
@@ -160,19 +125,34 @@ public class YukonLoggersController {
     // TODO: The filter and sorting remaining part will be covered in YUK-24684
     @GetMapping("/config/loggers/filter")
     public String filter(@DefaultSort(dir = Direction.asc, sort = "loggerName") SortingParameters sorting, String loggerName,
-            LoggerLevel[] loggerLevels, ModelMap model, YukonUserContext userContext, HttpServletRequest request,
-            FlashScope flashScope) {
-        retrieveLoggers(sorting, loggerName, loggerLevels, model, userContext, request, flashScope);
+            LoggerLevel[] loggerLevels, ModelMap model, YukonUserContext userContext) {
+        retrieveLoggers(sorting, loggerName, loggerLevels, model, userContext);
 
         return "config/userLoggersTable.jsp";
     }
+    
+    @GetMapping("/config/loggers/getSystemLoggers")
+    public String getSystemLoggers(@DefaultSort(dir = Direction.asc, sort = "loggerName") SortingParameters sorting, String loggerName,
+            LoggerLevel[] loggerLevels, ModelMap model, YukonUserContext userContext) {
+        retrieveLoggers(sorting, loggerName, loggerLevels, model, userContext);
 
+        return "config/systemLoggersTable.jsp";
+    }
+    
     private void retrieveLoggers(SortingParameters sorting, String loggerName, LoggerLevel[] loggerLevels,
-            ModelMap model, YukonUserContext userContext, HttpServletRequest request, FlashScope flashScope) {
+            ModelMap model, YukonUserContext userContext) {
         MessageSourceAccessor accessor = messageResolver.getMessageSourceAccessor(userContext);
         List<YukonLogger> userLoggers = new ArrayList<YukonLogger>();
         List<YukonLogger> systemLoggers = new ArrayList<YukonLogger>();
-        List<YukonLogger> loggers = new ArrayList<YukonLogger>();
+
+        for (Entry<Integer, YukonLogger> loggerEntry : cache.entrySet()) {
+            YukonLogger logger = loggerEntry.getValue();
+            if (SystemLogger.isSystemLogger(logger.getLoggerName())) {
+                systemLoggers.add(logger);
+            } else {
+                userLoggers.add(logger);
+            }
+        }
 
         FilterSortBy sortBy = FilterSortBy.valueOf(sorting.getSort());
         Direction dir = sorting.getDirection();
@@ -184,45 +164,6 @@ public class YukonLoggersController {
             model.addAttribute(column.name(), col);
         }
 
-        try {
-            String url = apiControllerHelper.findWebServerUrl(request, userContext, ApiURL.loggerUrl);
-            URIBuilder ub = new URIBuilder(url);
-            ub.addParameter("sort", sortBy.getValue().name());
-            ub.addParameter("dir", dir.name());
-            if (loggerLevels != null) {
-                for (LoggerLevel level : loggerLevels) {
-                    ub.addParameter("loggerLevels", level.name());
-                }
-            }
-            if (loggerName != null) {
-                ub.addParameter("loggerName", loggerName);
-            }
-            ResponseEntity<List<? extends Object>> loggersResponse = apiRequestHelper.callAPIForList(userContext, request,
-                    ub.toString(), YukonLogger.class, HttpMethod.GET, YukonLogger.class);
-            if (loggersResponse.getStatusCode() == HttpStatus.OK) {
-                loggers = (List<YukonLogger>) loggersResponse.getBody();
-            }
-
-        } catch (ApiCommunicationException e) {
-            log.error(e);
-            flashScope.setError(new YukonMessageSourceResolvable(communicationKey));
-        } catch (RestClientException ex) {
-            log.error("Error retrieving loggers. Error: {}", ex.getMessage());
-            String loggersLabel = accessor.getMessage(baseKey + "loggersLabel");
-            flashScope.setError(new YukonMessageSourceResolvable("yukon.web.api.retrieve.error", loggersLabel, ex.getMessage()));
-        } catch (URISyntaxException e) {
-            log.error("URI syntax error while creating builder for retrieving loggers", e);
-            String loggersLabel = accessor.getMessage(baseKey + "loggersLabel");
-            model.addAttribute("errorMessage", accessor.getMessage("yukon.web.api.retrieve.error", loggersLabel, e.getMessage()));
-        }
-
-        loggers.stream().forEach(logger -> {
-            if (SystemLogger.isSystemLogger(logger.getLoggerName())) {
-                systemLoggers.add(logger);
-            } else {
-                userLoggers.add(logger);
-            }
-        });
         model.addAttribute("userLoggers", userLoggers);
         model.addAttribute("systemLoggers", systemLoggers);
     }
