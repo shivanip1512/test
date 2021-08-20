af2f4c68b15342f2afc5582e5dc3583c0ca9d4e1
Tejaswee Khare <TejasweeSKhare@eaton.com>
2021-08-20 18:37:56 +0530

    YUK-24864 : Metric Id naming update

--- yukon-server/RTDB/cmd_rfn_ChannelConfiguration.cpp
+++ yukon-server/RTDB/cmd_rfn_ChannelConfiguration.cpp
@@ -76,7 +76,7 @@ const MetricInfo metricInfos[] = {
         {  53,  TouRates,  "VA delivered, peak demand (Frozen)"    },
         {  54,  TouRates,  "VA received, peak demand (Frozen)"     },
         {  55,  TouRates,  "VA lagging, peak demand"               },
-        {  56,  TouRates,  "Peak kVA (Quadrants 1 2 4) Frozen"     },
+        {  56,  TouRates,  "VA Q1 + Q2 + Q4, peak (Frozen)"        },
         {  57,  None,      "kVA (Quadrants 1 2 4)"                 },
 
         {  61,  TouRates,  "Q hour delivered"                      },
