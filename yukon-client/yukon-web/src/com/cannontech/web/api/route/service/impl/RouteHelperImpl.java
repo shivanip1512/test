3e51bcd3d008de3bf553cbc6a644733038084dc4
Mada Mahesh Goud <mahesh.goud@ltts.com>
2021-09-01 10:41:00 +0530

    YUK-24925 Resolved the comment.

--- yukon-client/yukon-web/src/com/cannontech/web/api/route/service/impl/RouteHelperImpl.java
+++ yukon-client/yukon-web/src/com/cannontech/web/api/route/service/impl/RouteHelperImpl.java
@@ -43,7 +43,7 @@ public class RouteHelperImpl implements RouteHelper {
             } else if (paoType.isCcu()) {
                 routePaoType = PaoType.ROUTE_CCU;
             } else {
-                throw new NotFoundException(paoType+" is not a SignalTransmitter");
+                throw new NotFoundException(paoType+" is not a Transmitter Type");
             }
         }
         return routePaoType;
