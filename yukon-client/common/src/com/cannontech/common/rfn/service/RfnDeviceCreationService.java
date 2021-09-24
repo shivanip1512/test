3ae8ccab5da8dc17680c3f74599b29ffb5086367
E0360047 <NiveditaKumari@Eaton.com>
2021-09-24 17:21:36 +0530

    YUK-25087 download Network info

--- yukon-client/common/src/com/cannontech/common/rfn/service/RfnDeviceCreationService.java
+++ yukon-client/common/src/com/cannontech/common/rfn/service/RfnDeviceCreationService.java
@@ -67,10 +67,4 @@ public interface RfnDeviceCreationService {
     RfnDevice create(RfnIdentifier rfnIdentifier, Hardware hardware, LiteYukonUser user);
     
     public String getUnknownTemplates();
-    
-    /**
-     * This method is used to find devices based on passed newDeviceIdentifier
-     */
-    RfnDevice findDevices(RfnIdentifier newDeviceIdentifier, Instant dataTimestamp);
-
 }
