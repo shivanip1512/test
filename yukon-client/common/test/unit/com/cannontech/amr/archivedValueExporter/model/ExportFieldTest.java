73b5754d6cac75384fdb90d9c9ccbe6ae7365459
E0650179 <E0650179@INPU1WHP6132289.eaton.ad.etn.com>
2021-10-06 18:56:05 +0530

    Correction in method name. Added more test cases.

--- yukon-client/common/test/unit/com/cannontech/amr/archivedValueExporter/model/ExportFieldTest.java
+++ yukon-client/common/test/unit/com/cannontech/amr/archivedValueExporter/model/ExportFieldTest.java
@@ -1,5 +1,6 @@
 package com.cannontech.amr.archivedValueExporter.model;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -7,12 +8,36 @@ import org.junit.jupiter.api.Test;
 public class ExportFieldTest {
 
     @Test
-    public void test_isEncrypted() {
+    public void test_isCustom() {
         ExportField expField = new ExportField();
         expField.setPattern("ddmmyy");
         Field field = new Field();
         field.setType(FieldType.POINT_TIMESTAMP);
         expField.setField(field);
-        assertTrue(expField.isCustomPattern(), "True");
+        assertTrue(expField.isCustomPattern(), "Must be true");
+        
+        expField = new ExportField();
+        expField.setPattern("###.###");
+        field = new Field();
+        field.setType(FieldType.POINT_VALUE);
+        expField.setField(field);
+        assertFalse(expField.isCustomPattern(), "Must be false");
+        
+        
+        expField = new ExportField();
+        expField.setPattern("MM/dd/yyyy");
+        field = new Field();
+        field.setType(FieldType.RUNTIME);
+        expField.setField(field);
+        assertFalse(expField.isCustomPattern(), "Must be false");
+        
+        
+        expField = new ExportField();
+        expField.setPattern("##");
+        field = new Field();
+        field.setType(FieldType.POINT_VALUE);
+        expField.setField(field);
+        assertTrue(expField.isCustomPattern(), "Must be true");
+        
     }
 }
