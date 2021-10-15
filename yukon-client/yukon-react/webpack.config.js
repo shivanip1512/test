4b63962680ac5544b46d1d72f2647a74e58750ea
carrieVant <44090659+carrieVant@users.noreply.github.com>
2021-10-15 08:00:53 -0500

    YUK-25224 - Investigate making menu render smoothly   - make menu render more smoothly   - add typescript to webpack

--- yukon-client/yukon-react/webpack.config.js
+++ yukon-client/yukon-react/webpack.config.js
@@ -6,6 +6,9 @@ module.exports = {
         filename: 'yukon.react.navigationContainer.js',
         publicPath: 'dist/'
     },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+    },
     optimization: {
       minimizer: [new UglifyJsPlugin()]
     },
@@ -15,14 +18,15 @@ module.exports = {
     module: {
         rules: [
         {
-            test: /\.js$/,
+            test: /\.(js|jsx|ts|tsx)$/,
             exclude: /node_modules/,
             use: {
                 loader: 'babel-loader',
                 options: {
                     presets: [
                         ['@babel/preset-env'], 
-                        ['@babel/preset-react']
+                        ['@babel/preset-react'],
+                        ['@babel/preset-typescript']
                     ]
                 }
             }
