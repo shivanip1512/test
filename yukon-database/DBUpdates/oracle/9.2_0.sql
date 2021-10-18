fd8ad7fa3381c94d36f575008af3298be1cef337
E0534908 <JohnMJustin@Eaton.com>
2021-10-18 14:29:21 -0500

    YUK-25311 Remove "9.2" entry from CTIDatabase table
    Updated 9.2 scripts to not enter CTIDatabase value
    Created 9.2.1 script to remove CTIDatabase 9.2 entry
    Improved Version Info Message

--- yukon-database/DBUpdates/oracle/9.2_0.sql
+++ yukon-database/DBUpdates/oracle/9.2_0.sql
@@ -66,8 +66,10 @@ AND Property = 'LOGIN_FONT_SHADOW';
 INSERT INTO DBUpdates VALUES ('YUK-25103', '9.2.0', SYSDATE);
 /* @end YUK-25103 */
 
-/**************************************************************/
-/* VERSION INFO                                               */
-/* Inserted when update script is run                         */
-/**************************************************************/
-INSERT INTO CTIDatabase VALUES ('9.2', '27-SEP-2021', 'Latest Update', 0, SYSDATE);
+/***********************************************************************************/
+/* VERSION INFO                                                                    */
+/* Inserted when update script is run, stays commented out until the release build */
+/* https://confluence-prod.tcc.etn.com/display/EEST/Writing+Update+Scripts         */
+/* https://confluence-prod.tcc.etn.com/display/EEST/Yukon+Build+Database+Tasks     */
+/***********************************************************************************/
+/* INSERT INTO CTIDatabase VALUES ('9.2', '18-OCT-2021', 'Latest Update', 0, SYSDATE); */
---
+++ yukon-database/DBUpdates/sqlserver/9.2_1.sql
@@ -0,0 +1,19 @@
+/******************************************/
+/**** SQL Server DBupdates             ****/
+/******************************************/
+
+/* @start YUK-25311 */
+DELETE FROM CTIDatabase
+WHERE Version = 9.2
+AND Build = 0;
+
+INSERT INTO DBUpdates VALUES ('YUK-25311', '9.2.0', GETDATE());
+/* @end YUK-25311 */
+
+/***********************************************************************************/
+/* VERSION INFO                                                                    */
+/* Inserted when update script is run, stays commented out until the release build */
+/* https://confluence-prod.tcc.etn.com/display/EEST/Writing+Update+Scripts         */
+/* https://confluence-prod.tcc.etn.com/display/EEST/Yukon+Build+Database+Tasks     */
+/***********************************************************************************/
+/* INSERT INTO CTIDatabase VALUES ('9.2', '18-OCT-2021', 'Latest Update', 1, GETDATE()); */
