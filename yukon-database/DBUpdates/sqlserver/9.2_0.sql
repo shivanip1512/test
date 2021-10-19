191a38585ec6cccaf6a648b257166c9b2d02aa9e
E0534908 <JohnMJustin@Eaton.com>
2021-10-19 07:58:54 -0500

    YUK-25311 Remove "9.2" entry from CTIDatabase table
    Cleaned up help text

--- yukon-database/DBUpdates/sqlserver/9.2_0.sql
+++ yukon-database/DBUpdates/sqlserver/9.2_0.sql
@@ -69,7 +69,5 @@ INSERT INTO DBUpdates VALUES ('YUK-25103', '9.2.0', GETDATE());
 /***********************************************************************************/
 /* VERSION INFO                                                                    */
 /* Inserted when update script is run, stays commented out until the release build */
-/* https://confluence-prod.tcc.etn.com/display/EEST/Writing+Update+Scripts         */
-/* https://confluence-prod.tcc.etn.com/display/EEST/Yukon+Build+Database+Tasks     */
 /***********************************************************************************/
 /* INSERT INTO CTIDatabase VALUES ('9.2', '18-OCT-2021', 'Latest Update', 0, GETDATE()); */
