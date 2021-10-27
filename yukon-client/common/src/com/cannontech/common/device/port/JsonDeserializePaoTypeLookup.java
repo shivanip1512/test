c04fbfaf288ba8816bf2a038da20fa9d68fff27d
E0552673 <E0552673@INPU1WHP6096232.eaton.ad.etn.com>
2021-10-27 17:38:35 +0530

    YUK-25346: Remove json property in Device base model and update field in swagger documentation accordingly:Review comment

--- yukon-client/common/src/com/cannontech/common/device/port/JsonDeserializePaoTypeLookup.java
+++ yukon-client/common/src/com/cannontech/common/device/port/JsonDeserializePaoTypeLookup.java
@@ -35,7 +35,7 @@ public class JsonDeserializePaoTypeLookup extends StdDeserializer<YukonPao> {
         }
         String idStr = ServletUtils.getPathVariable("id");
         if (idStr == null) {
-            TreeNode id = node.get("id");
+            TreeNode id = node.get("deviceId");
             if (id != null) {
                 idStr = id.toString();
             }
