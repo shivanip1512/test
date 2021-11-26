80f1a6c6a37c719f01dc9153560b95f83e9a8401
Tejaswee Khare <TejasweeSKhare@eaton.com>
2021-11-26 22:43:34 +0530

    YUK-25377 : Comment Updates

--- yukon-client/report/src/com/cannontech/analysis/pentahoreport/ItemColumnQuotientExpression.java
+++ yukon-client/report/src/com/cannontech/analysis/pentahoreport/ItemColumnQuotientExpression.java
@@ -1,8 +1,5 @@
 /*
  * Created on Jul 18, 2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 package com.cannontech.analysis.pentahoreport;
 
