b94f571c12420ffec2fd04d5ff0b2fc2a5b013b3
E0650179 <E0650179@INPU1WHP6132289.eaton.ad.etn.com>
2021-11-29 15:27:35 +0530

    YUK: 25507 Updating Command Priority for Existing ExpressCom Load groups.

--- yukon-database/DBUpdates/oracle/9.2_0.sql
+++ yukon-database/DBUpdates/oracle/9.2_0.sql
@@ -66,6 +66,14 @@ AND Property = 'LOGIN_FONT_SHADOW';
 INSERT INTO DBUpdates VALUES ('YUK-25103', '9.2.0', SYSDATE);
 /* @end YUK-25103 */
 
+/* @start YUK-25507 */
+UPDATE LMGroupExpressCom 
+SET ProtocolPriority = 0 
+WHERE ProtocolPriority = 4;
+
+INSERT INTO DBUpdates VALUES ('YUK-25507', '9.2.0', SYSDATE);
+/* @end YUK-25507 */
+
 /***********************************************************************************/
 /* VERSION INFO                                                                    */
 /* Inserted when update script is run, stays commented out until the release build */
