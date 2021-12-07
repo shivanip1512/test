cfeef7bc0168cd3b67d23e5176ed7da25a5350dc
Satpathy, Samir K <SamirKSatpathy@eaton.com>
2021-12-07 18:56:33 +0530

    YUK-25515: Design - Yukon Metrics and Performance

--- yukon-client/common/src/com/cannontech/yukon/system/metrics/listener/YukonMetricListener.java
+++ yukon-client/common/src/com/cannontech/yukon/system/metrics/listener/YukonMetricListener.java
@@ -13,6 +13,7 @@ import com.cannontech.clientutils.YukonLogManager;
 import com.cannontech.common.pao.PaoUtils;
 import com.cannontech.common.pao.definition.model.PaoPointIdentifier;
 import com.cannontech.common.pao.definition.model.PointIdentifier;
+import com.cannontech.common.util.JsonUtils;
 import com.cannontech.core.dao.PointDao;
 import com.cannontech.core.dao.SimplePointAccessDao;
 import com.cannontech.database.data.lite.LitePoint;
@@ -43,14 +44,15 @@ public class YukonMetricListener implements MessageListener {
             try {
                 textMessage = ((TextMessage) message).getText();
                 YukonMetric yukonMetric = gson.fromJson(textMessage, YukonMetric.class);
+                log.info("Received Yukon Metric data: {}", JsonUtils.toJson(yukonMetric));
                 if (shouldGeneratePointData(yukonMetric.getPointInfo())) {
                     PointIdentifier pointIdentifier = new PointIdentifier(yukonMetric.getPointInfo().getType(),
                             yukonMetric.getPointInfo().getOffset());
                     PaoPointIdentifier paoPointIdentifier = new PaoPointIdentifier(PaoUtils.SYSTEM_PAOIDENTIFIER,
                             pointIdentifier);
-                    LitePoint litePoint = pointDao.getLitePoint(paoPointIdentifier);
-                    pointAccessDao.setPointValue(litePoint, yukonMetric.getTimestamp().toInstant(),
-                            Double.valueOf(yukonMetric.getValue().toString()));
+                    //Commented these lines as DB changes are not yet committed to master. 
+                    //LitePoint litePoint = pointDao.getLitePoint(paoPointIdentifier);
+                    //pointAccessDao.setPointValue(litePoint, yukonMetric.getTimestamp().toInstant(), Double.valueOf(yukonMetric.getValue().toString()));
                 }
             } catch (Exception e) {
                 log.error("Error occurred while generating point data.", e);
