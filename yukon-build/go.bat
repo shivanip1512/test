0e1a415b831587f454ce1358c60d6be970e760cc
E0471478 <ManenderSingh@eaton.com>
2021-12-14 16:13:29 +0530

    YUK-25602-Prepare for a 7.1/7.2+ build

--- yukon-build/go.bat
+++ yukon-build/go.bat
@@ -3,5 +3,7 @@ echo Executing %~df0 %*
 echo:
 
 set ANT_OPTS=-Xmx2048m
+set JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-8.0.312.7-hotspot
+set PATH=C:\Program Files\Eclipse Adoptium\jdk-8.0.312.7-hotspot\bin;%PATH%
 %~dp0..\yukon-client\build\ant\bin\ant -f %~dp0build.xml %*
 if not "%ERRORLEVEL%" == "0" exit %ERRORLEVEL%
