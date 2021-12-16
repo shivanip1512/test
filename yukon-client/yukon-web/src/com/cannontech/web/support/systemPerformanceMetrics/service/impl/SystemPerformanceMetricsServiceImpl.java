06571a58a859f20c45265f49e4caccbd39ed9ae8
E0650179 <E0650179@INPU1WHP6132289.eaton.ad.etn.com>
2021-12-16 15:03:25 +0530

    YUK: Backend changes to get list of System points

--- yukon-client/yukon-web/src/com/cannontech/web/support/systemPerformanceMetrics/service/impl/SystemPerformanceMetricsServiceImpl.java
+++ yukon-client/yukon-web/src/com/cannontech/web/support/systemPerformanceMetrics/service/impl/SystemPerformanceMetricsServiceImpl.java
@@ -1,18 +1,36 @@
 package com.cannontech.web.support.systemPerformanceMetrics.service.impl;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.logging.log4j.core.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cannontech.clientutils.YukonLogManager;
 import com.cannontech.common.chart.model.ChartValue;
+import com.cannontech.core.dao.NotFoundException;
+import com.cannontech.core.dao.PointDao;
 import com.cannontech.database.data.lite.LitePoint;
+import com.cannontech.database.data.point.PointType;
 import com.cannontech.web.support.systemPerformanceMetrics.service.SystemPerformanceMetricsService;
 
 public class SystemPerformanceMetricsServiceImpl implements SystemPerformanceMetricsService {
-
+    private static Logger log = YukonLogManager.getLogger(SystemPerformanceMetricsServiceImpl.class);
+    @Autowired PointDao pointDao;
+    
     @Override
     public List<LitePoint> getAllSystemPoints() {
-        // TODO Auto-generated method stub
-        return null;
+        List<LitePoint> systemPoints = new ArrayList<LitePoint>();
+        try {
+            //For System Points, Device Id is 0 and Point Type is System.
+            systemPoints = pointDao.getLitePointIdByDeviceId_PointType(0, PointType.System);
+            systemPoints.stream().forEach(litePoint -> log.debug("Point Name {}, Point type {}, PAObject ID {} ",
+                    litePoint.getPointName(), PointType.getForId(litePoint.getLiteType()), litePoint.getPaobjectID()));
+        } catch (NotFoundException e) {
+            log.error("No System points found  in the database.");
+        }
+        return systemPoints;
     }
 
     @Override
