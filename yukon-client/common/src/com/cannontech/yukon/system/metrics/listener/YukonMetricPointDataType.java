08ca2581543f9c4720b37789661179cc53707cfc
E0552673 <E0552673@INPU1WHP6096232.eaton.ad.etn.com>
2021-12-20 14:43:18 +0530

    YUK-25640:Dev Task - YukonDataProcessor to generate YukonMetric instead of SystemData

--- yukon-client/common/src/com/cannontech/yukon/system/metrics/listener/YukonMetricPointDataType.java
+++ yukon-client/common/src/com/cannontech/yukon/system/metrics/listener/YukonMetricPointDataType.java
@@ -5,7 +5,19 @@ import com.cannontech.database.data.point.PointType;
 public enum YukonMetricPointDataType {
     RFN_METER_READING_ARCHIVERE_REQUEST_RECEIVED(1032, PointType.Analog),
     RFN_METER_READING_ARCHIVERE_REQUEST_PUSHED(1033, PointType.Analog),
-    GAS_METER_COUNT(1034, PointType.Analog);
+    GAS_METER_COUNT(1034, PointType.Analog),
+    DATA_COMPLETENESS_ELECTRIC(1035, PointType.Analog),
+    DATA_COMPLETENESS_WATER(1036, PointType.Analog),
+    ELECTRIC_METER_COUNT(1037, PointType.Analog),
+    RFN_LCR_COUNT(1038, PointType.Analog),
+    RFN_RELAYS(1039, PointType.Analog),
+    WATER_METER_COUNT(1040, PointType.Analog),
+    ELECTRIC_READ_RATE(1041, PointType.Analog),
+    WATER_READ_RATE(1042, PointType.Analog),
+    HIGHEST_LCR_DESCEDANT_COUNT_DATA(1043, PointType.Analog),
+    HIGHEST_METER_DESCEDANT_COUNT_DATA(1044, PointType.Analog),
+    HIGHEST_RELAY_DESCEDANT_COUNT_DATA(1045, PointType.Analog),
+    YUKON_VERSION(1046, PointType.Analog);
 
     private Integer offset;
     private PointType type;
