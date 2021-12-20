3985d3abcd6c33841596c2f07fe610557f49e8aa
Satpathy, Samir K <SamirKSatpathy@eaton.com>
2021-12-20 19:08:37 +0530

    YUK-25648: Auto create points if not available in DB dynamically

--- yukon-client/common/src/com/cannontech/yukon/system/metrics/listener/YukonMetricListener.java
+++ yukon-client/common/src/com/cannontech/yukon/system/metrics/listener/YukonMetricListener.java
@@ -1,5 +1,8 @@
 package com.cannontech.yukon.system.metrics.listener;
 
+import java.sql.SQLException;
+import java.util.Map;
+
 import javax.annotation.PostConstruct;
 import javax.jms.Message;
 import javax.jms.MessageListener;
@@ -11,10 +14,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.cannontech.clientutils.YukonLogManager;
 import com.cannontech.common.pao.PaoUtils;
+import com.cannontech.common.pao.definition.dao.PaoDefinitionDao;
+import com.cannontech.common.pao.definition.loader.jaxb.Point;
 import com.cannontech.common.pao.definition.model.PaoPointIdentifier;
 import com.cannontech.common.pao.definition.model.PointIdentifier;
+import com.cannontech.common.pao.service.PointCreationService;
+import com.cannontech.core.dao.DBPersistentDao;
+import com.cannontech.core.dao.NotFoundException;
 import com.cannontech.core.dao.PointDao;
 import com.cannontech.core.dao.SimplePointAccessDao;
+import com.cannontech.database.TransactionType;
+import com.cannontech.database.data.lite.LitePoint;
+import com.cannontech.database.data.point.PointArchiveInterval;
+import com.cannontech.database.data.point.PointArchiveType;
+import com.cannontech.database.data.point.PointBase;
+import com.cannontech.database.data.point.PointType;
+import com.cannontech.database.data.point.StatusControlType;
+import com.cannontech.database.data.point.UnitOfMeasure;
+import com.cannontech.database.db.state.StateGroupUtils;
 import com.cannontech.yukon.system.metrics.message.YukonMetric;
 import com.cannontech.yukon.system.metrics.message.YukonMetricPointInfo;
 import com.google.gson.Gson;
@@ -25,6 +42,9 @@ public class YukonMetricListener implements MessageListener {
 
     @Autowired private PointDao pointDao;
     @Autowired private SimplePointAccessDao pointAccessDao;
+    @Autowired private PointCreationService pointCreationService;
+    @Autowired private PaoDefinitionDao definitionDao;
+    @Autowired private DBPersistentDao dbPersistentDao;
 
     private Gson gson;
 
@@ -48,15 +68,46 @@ public class YukonMetricListener implements MessageListener {
                     PointIdentifier pointIdentifier = new PointIdentifier(pointDataype.getType(), pointDataype.getOffset());
                     PaoPointIdentifier paoPointIdentifier = new PaoPointIdentifier(PaoUtils.SYSTEM_PAOIDENTIFIER,
                             pointIdentifier);
-                    // Commented these lines as DB changes are not yet committed to master.
-                    // LitePoint litePoint = pointDao.getLitePoint(paoPointIdentifier);
-                    // pointAccessDao.setPointValue(litePoint, yukonMetric.getTimestamp().toInstant(),
-                    // Double.valueOf(yukonMetric.getValue().toString()));
+                    LitePoint litePoint = null;
+                    try {
+                        litePoint = pointDao.getLitePoint(paoPointIdentifier);
+                    } catch (NotFoundException e) {
+                        Map<String, Point> systemDevicePoints = definitionDao.getSystemDevicePoints();
+                        if (systemDevicePoints.containsKey(pointDataype.getName())) {
+                            litePoint = createPoint(systemDevicePoints.get(pointDataype.getName()));
+                        } else {
+                            throw new NotFoundException(
+                                    "Point definition not found for " + pointDataype.getName() + " in SYSTEM>xml file");
+                        }
+                    }
+                    pointAccessDao.setPointValue(litePoint, yukonMetric.getTimestamp().toInstant(),
+                            Double.valueOf(yukonMetric.getValue().toString()));
                 }
             } catch (Exception e) {
                 log.error("Error occurred while generating point data.", e);
             }
         }
+    }
+
+    private LitePoint createPoint(Point point) throws SQLException {
+        StatusControlType controlType = point.getControlType() == null ? StatusControlType.NONE : StatusControlType
+                .valueOf(point.getControlType().getValue().name());
+        PointArchiveType pointArchiveType = point.getArchive() == null
+                || point.getArchive().getType() == null ? PointArchiveType.NONE : PointArchiveType
+                        .valueOf(point.getArchive().getType());
+        PointArchiveInterval pointArchiveInterval = point.getArchive() == null
+                || point.getArchive().getInterval() == null ? PointArchiveInterval.ZERO : PointArchiveInterval
+                        .valueOf(point.getArchive().getInterval());
+        PointBase pointBase = pointCreationService.createPoint(PointType.Analog.getPointTypeId(),
+                point.getName(), PaoUtils.SYSTEM_PAOIDENTIFIER,
+                point.getOffset(), point.getMultiplier().getValue().doubleValue(),
+                UnitOfMeasure.valueOf(point.getUnitofmeasure().getValue().name()).getId(), StateGroupUtils.DEFAULT_STATE, 0,
+                point.getDecimalplaces().getValue(), controlType,
+                pointArchiveType, pointArchiveInterval);
+        dbPersistentDao.performDBChange(pointBase, TransactionType.INSERT);
+        LitePoint litePoint = pointDao.getLitePoint(pointBase.getPoint().getPointID());
+        return litePoint;
+
     }
 
     private boolean shouldGeneratePointData(YukonMetricPointInfo pointInfo) {
--- yukon-client/common/src/com/cannontech/yukon/system/metrics/listener/YukonMetricPointDataType.java
+++ yukon-client/common/src/com/cannontech/yukon/system/metrics/listener/YukonMetricPointDataType.java
@@ -3,16 +3,17 @@ package com.cannontech.yukon.system.metrics.listener;
 import com.cannontech.database.data.point.PointType;
 
 public enum YukonMetricPointDataType {
-    RFN_METER_READING_ARCHIVERE_REQUEST_RECEIVED(1032, PointType.Analog),
-    RFN_METER_READING_ARCHIVERE_REQUEST_PUSHED(1033, PointType.Analog),
-    GAS_METER_COUNT(1034, PointType.Analog);
+    RFN_METER_READING_ARCHIVE_REQUEST_RECEIVED(1032, PointType.Analog, "Rfn Meter Reading Archive Requests Received"),
+    RFN_METER_READING_ARCHIVE_REQUEST_PUSHED(1033, PointType.Analog, "Rfn Meter Reading Archive Requests Pushed");
 
     private Integer offset;
     private PointType type;
+    private String name;
 
-    YukonMetricPointDataType(Integer offset, PointType type) {
+    YukonMetricPointDataType(Integer offset, PointType type, String name) {
         this.offset = offset;
         this.type = type;
+        this.name = name;
     }
 
     public Integer getOffset() {
@@ -21,5 +22,9 @@ public enum YukonMetricPointDataType {
 
     public PointType getType() {
         return type;
+    }
+
+    public String getName() {
+        return name;
     }
 }
