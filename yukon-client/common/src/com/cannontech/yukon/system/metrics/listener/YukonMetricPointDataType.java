dd4fc63a99f2805909722e4dd52bf49d85e972ef
E0552673 <E0552673@INPU1WHP6096232.eaton.ad.etn.com>
2021-12-21 14:47:41 +0530

    YUK-25640:Dev Task - YukonDataProcessor to generate YukonMetric instead of SystemData: Resolve conflict

--- yukon-client/common/src/com/cannontech/yukon/system/metrics/listener/YukonMetricPointDataType.java
+++ yukon-client/common/src/com/cannontech/yukon/system/metrics/listener/YukonMetricPointDataType.java
@@ -1,30 +1,47 @@
 package com.cannontech.yukon.system.metrics.listener;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 import com.cannontech.database.data.point.PointType;
+import com.cannontech.yukon.system.metrics.message.YukonMetricPointInfo;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 
 public enum YukonMetricPointDataType {
-    RFN_METER_READING_ARCHIVERE_REQUEST_RECEIVED(1032, PointType.Analog),
-    RFN_METER_READING_ARCHIVERE_REQUEST_PUSHED(1033, PointType.Analog),
-    GAS_METER_COUNT(1034, PointType.Analog),
-    DATA_COMPLETENESS_ELECTRIC(1035, PointType.Analog),
-    DATA_COMPLETENESS_WATER(1036, PointType.Analog),
-    ELECTRIC_METER_COUNT(1037, PointType.Analog),
-    RFN_LCR_COUNT(1038, PointType.Analog),
-    RFN_RELAYS(1039, PointType.Analog),
-    WATER_METER_COUNT(1040, PointType.Analog),
-    ELECTRIC_READ_RATE(1041, PointType.Analog),
-    WATER_READ_RATE(1042, PointType.Analog),
-    HIGHEST_LCR_DESCEDANT_COUNT_DATA(1043, PointType.Analog),
-    HIGHEST_METER_DESCEDANT_COUNT_DATA(1044, PointType.Analog),
-    HIGHEST_RELAY_DESCEDANT_COUNT_DATA(1045, PointType.Analog),
-    YUKON_VERSION(1046, PointType.Analog);
+    RFN_METER_READING_ARCHIVE_REQUEST_RECEIVED(YukonMetricPointInfo.RFN_METER_READING_ARCHIVE_REQUEST_RECEIVED, 1032,
+            PointType.Analog, "Rfn Meter Reading Archive Requests Received"),
+    RFN_METER_READING_ARCHIVE_REQUEST_PUSHED(YukonMetricPointInfo.RFN_METER_READING_ARCHIVE_REQUEST_PUSHED, 1033,
+            PointType.Analog, "Rfn Meter Reading Archive Requests Pushed");
 
+    private final static ImmutableSet<YukonMetricPointInfo> lookupByYukonMetricPointInfo;
+    private final static ImmutableMap<YukonMetricPointInfo, YukonMetricPointDataType> lookupByPointInfo;
+
+    static {
+        ImmutableSet.Builder<YukonMetricPointInfo> pointInfoBuilder = ImmutableSet.builder();
+        ImmutableMap.Builder<YukonMetricPointInfo, YukonMetricPointDataType> lookupByPointInfoBuilder = ImmutableMap.builder();
+
+        for (YukonMetricPointDataType type : values()) {
+            pointInfoBuilder.add(type.getPointInfo());
+            lookupByPointInfoBuilder.put(type.getPointInfo(), type);
+        }
+        lookupByYukonMetricPointInfo = pointInfoBuilder.build();
+        lookupByPointInfo = lookupByPointInfoBuilder.build();
+    }
+
+    private YukonMetricPointInfo pointInfo;
     private Integer offset;
     private PointType type;
+    private String name;
 
-    YukonMetricPointDataType(Integer offset, PointType type) {
+    YukonMetricPointDataType(YukonMetricPointInfo pointInfo, Integer offset, PointType type, String name) {
+        this.pointInfo = pointInfo;
         this.offset = offset;
         this.type = type;
+        this.name = name;
+    }
+
+    public YukonMetricPointInfo getPointInfo() {
+        return pointInfo;
     }
 
     public Integer getOffset() {
@@ -33,5 +50,19 @@ public enum YukonMetricPointDataType {
 
     public PointType getType() {
         return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static boolean isPointData(YukonMetricPointInfo pointInfo) {
+        checkArgument(pointInfo != null);
+        return lookupByYukonMetricPointInfo.contains(pointInfo);
+    }
+
+    public static YukonMetricPointDataType getForPointInfo(YukonMetricPointInfo pointInfo) {
+        checkArgument(pointInfo != null);
+        return lookupByPointInfo.get(pointInfo);
     }
 }
