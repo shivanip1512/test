a9e14f9086e8add898c0141ac39de6c2a9a7fec2
Brady Anderson <BradyWAnderson@eaton.com>
2021-12-21 09:45:00 -0600

    YUK-25601 Update log4j to 2.17
    Added SQL update files

---
+++ yukon-database/DBUpdates/sqlserver/9.1_2.sql
@@ -0,0 +1,9 @@
+/******************************************/
+/**** SQL Server DBupdates             ****/
+/******************************************/
+
+/***********************************************************************************/
+/* VERSION INFO                                                                    */
+/* Inserted when update script is run, stays commented out until the release build */
+/***********************************************************************************/
+INSERT INTO CTIDatabase VALUES ('9.1', '21-DEC-2021', 'Latest Update', 2, GETDATE());
