8fbea1d5acd3b59467da9a367b030901717e743b
E0650179 <E0650179@INPU1WHP6132289.eaton.ad.etn.com>
2021-12-21 14:48:23 +0530

    YUK-25593: Backend changes to get point data for a point. Resolving conflicts.

--- yukon-client/yukon-web/src/com/cannontech/web/support/systemPerformanceMetrics/service/SystemPerformanceMetricsService.java
+++ yukon-client/yukon-web/src/com/cannontech/web/support/systemPerformanceMetrics/service/SystemPerformanceMetricsService.java
@@ -3,11 +3,8 @@ package com.cannontech.web.support.systemPerformanceMetrics.service;
 import java.util.Date;
 import java.util.List;
 
-import com.cannontech.common.chart.model.ChartInterval;
 import com.cannontech.common.chart.model.ChartValue;
-import com.cannontech.common.chart.model.ConverterType;
 import com.cannontech.database.data.lite.LitePoint;
-import com.cannontech.user.YukonUserContext;
 
 public interface SystemPerformanceMetricsService {
 
@@ -19,7 +16,6 @@ public interface SystemPerformanceMetricsService {
     /*
      * Get point data for the passed pointId and date range and convert it to ChartValue
      */
-    List<ChartValue<Double>> getPointData(int pointId, Date startDate, Date stopDate, YukonUserContext userContext,
-            ChartInterval interval, ConverterType converterType);
+    List<ChartValue<Double>> getPointData(int pointId, Date startDate, Date stopDate);
 
 }
