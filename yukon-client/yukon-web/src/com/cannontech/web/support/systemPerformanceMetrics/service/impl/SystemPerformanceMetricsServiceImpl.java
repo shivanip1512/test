c674c8d4bce8133ca310554c5250c40457af7a91
E0650179 <E0650179@INPU1WHP6132289.eaton.ad.etn.com>
2021-12-21 14:57:50 +0530

    YUK-25593: Backend changes to get point data for a point. Resolved Conflicts.

--- yukon-client/yukon-web/src/com/cannontech/web/support/systemPerformanceMetrics/service/impl/SystemPerformanceMetricsServiceImpl.java
+++ yukon-client/yukon-web/src/com/cannontech/web/support/systemPerformanceMetrics/service/impl/SystemPerformanceMetricsServiceImpl.java
@@ -1,5 +1,7 @@
 package com.cannontech.web.support.systemPerformanceMetrics.service.impl;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -8,17 +10,28 @@ import org.apache.logging.log4j.core.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.cannontech.clientutils.YukonLogManager;
+import com.cannontech.common.chart.model.ChartInterval;
 import com.cannontech.common.chart.model.ChartValue;
+import com.cannontech.common.chart.model.ConverterType;
+import com.cannontech.common.i18n.MessageSourceAccessor;
 import com.cannontech.core.dao.NotFoundException;
 import com.cannontech.core.dao.PointDao;
+import com.cannontech.core.dao.RawPointHistoryDao;
+import com.cannontech.core.dynamic.PointValueHolder;
 import com.cannontech.database.data.lite.LitePoint;
+import com.cannontech.database.data.lite.LitePointUnit;
 import com.cannontech.database.data.point.PointType;
+import com.cannontech.database.data.point.UnitOfMeasure;
 import com.cannontech.database.db.device.Device;
+import com.cannontech.i18n.YukonUserContextMessageSourceResolver;
+import com.cannontech.user.YukonUserContext;
 import com.cannontech.web.support.systemPerformanceMetrics.service.SystemPerformanceMetricsService;
 
 public class SystemPerformanceMetricsServiceImpl implements SystemPerformanceMetricsService {
     private static final Logger log = YukonLogManager.getLogger(SystemPerformanceMetricsServiceImpl.class);
+    @Autowired private RawPointHistoryDao rawPointHistoryDao;
     @Autowired private PointDao pointDao;
+    @Autowired private YukonUserContextMessageSourceResolver messageSourceResolver;
     
     @Override
     public List<LitePoint> getAllSystemPoints() {
@@ -37,8 +50,40 @@ public class SystemPerformanceMetricsServiceImpl implements SystemPerformanceMet
     }
 
     @Override
-    public List<ChartValue<Double>> getPointData(int pointId, Date startDate, Date stopDate) {
-        // TODO Auto-generated method stub
-        return null;
+    public List<ChartValue<Double>> getPointData(int pointId, Date startDate, Date stopDate, YukonUserContext userContext,
+            ChartInterval interval, ConverterType converterType) {
+        
+        List<PointValueHolder> pointData = rawPointHistoryDao.getPointData(pointId, startDate, stopDate);
+
+        log.debug("Point ID {}, Point Data Count {} ",
+                pointId, pointData.size());
+
+        String pointName = pointDao.getPointName(pointId);
+        
+        LitePointUnit pointUnit = pointDao.getPointUnit(pointId);
+        NumberFormat pointValueFormat = new DecimalFormat();
+        pointValueFormat.setMaximumFractionDigits(pointUnit.getDecimalPlaces());
+        pointValueFormat.setMinimumFractionDigits(pointUnit.getDecimalPlaces());
+        pointValueFormat.setGroupingUsed(false);
+
+        MessageSourceAccessor messageSourceAccessor = messageSourceResolver.getMessageSourceAccessor(userContext);
+        UnitOfMeasure unitMeasure = UnitOfMeasure.getForId(pointUnit.getUomID());
+        String chartIntervalString = messageSourceAccessor.getMessage(interval.getIntervalString());
+        String units = messageSourceAccessor
+                .getMessage(converterType.getFormattedUnits(unitMeasure, chartIntervalString));
+
+        List<ChartValue<Double>> chartData = new ArrayList<>();
+        for (PointValueHolder point : pointData) {
+            ChartValue<Double> chartValue = new ChartValue<>();
+            long timeStamp = point.getPointDataTimeStamp().getTime();
+            chartValue.setId(point.getId());
+            chartValue.setTime(timeStamp);
+            chartValue.setValue(point.getValue());
+            chartValue.setPointName(pointName);
+            chartValue.setUnits(units);
+            chartValue.setFormattedValue(pointValueFormat.format(point.getValue()));
+            chartData.add(chartValue);
+        }
+        return chartData;
     }
 }
