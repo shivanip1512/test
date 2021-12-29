044852c4198da9a98c8988a18956e6f0f7f397fc
E0650179 <E0650179@INPU1WHP6132289.eaton.ad.etn.com>
2021-12-29 13:59:18 +0530

    YUK-25664: Generate Data - RfnLcrArchiveRequest Received.

--- yukon-client/common/src/com/cannontech/yukon/system/metrics/listener/YukonMetricPointDataType.java
+++ yukon-client/common/src/com/cannontech/yukon/system/metrics/listener/YukonMetricPointDataType.java
@@ -14,7 +14,9 @@ public enum YukonMetricPointDataType {
             PointType.Analog, "Rfn Meter Reading Archive Requests Pushed"),
     RFN_METER_READING_ARCHIVE_REQUESTS_POINT_DATA_GENERATED_COUNT(
             YukonMetricPointInfo.RFN_METER_READING_ARCHIVE_REQUESTS_POINT_DATA_GENERATED_COUNT, 1034, PointType.Analog,
-            "Rfn Meter Reading Archive Requests Pt Data Generated Count");
+            "Rfn Meter Reading Archive Requests Pt Data Generated Count"),
+    RFN_LCR_ARCHIVE_REQUESTS_RECEIVED(YukonMetricPointInfo.RFN_LCR_ARCHIVE_REQUESTS_RECEIVED, 1035, PointType.Analog,
+            "RFN LCR Archive Requests Received");
 
     private final static ImmutableSet<YukonMetricPointInfo> lookupByYukonMetricPointInfo;
     private final static ImmutableMap<YukonMetricPointInfo, YukonMetricPointDataType> lookupByPointInfo;
