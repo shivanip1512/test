f4f35a5fb0d3f77c50b1748ad4a6e57fde474ecc
carrieVant <44090659+carrieVant@users.noreply.github.com>
2022-01-05 15:05:57 -0600

    YUK-25619 - Dev Task: Display graphs with dummy data   - get sparkline charts to work with json

--- yukon-client/yukon-web/WebContent/resources/js/pages/yukon.support.systemPerformanceMetrics.js
+++ yukon-client/yukon-web/WebContent/resources/js/pages/yukon.support.systemPerformanceMetrics.js
@@ -12,6 +12,84 @@ yukon.support.systemPerformanceMetrics = (function() {
     
     var
     _initialized = false,
+    
+    _sparklineDefaultOptions = {
+            chart: {
+                backgroundColor: null,
+                borderWidth: 0,
+                type: 'area',
+                width: 400,
+                height: 100,
+                style: {
+                    overflow: 'visible'
+                },
+                // small optimalization, saves 1-2 ms each sparkline
+                skipClone: true,
+            },
+            title: {
+                text: ''
+            },
+            credits: {
+                enabled: false
+            },
+            xAxis: {
+            	type: 'datetime',
+                labels: {
+                    enabled: false
+                },
+                title: {
+                    text: null
+                },
+                startOnTick: false,
+                endOnTick: false,
+                tickPositions: []
+            },
+            yAxis: {
+                endOnTick: false,
+                startOnTick: false,
+                labels: {
+                    enabled: false
+                },
+                title: {
+                    text: null
+                },
+                tickPositions: [0]
+            },
+            legend: {
+                enabled: false
+            },
+            tooltip: {
+                hideDelay: 0,
+                outside: true,
+                shared: true
+            },
+            plotOptions: {
+                series: {
+                    animation: false,
+                    lineWidth: 1,
+                    shadow: false,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    marker: {
+                        radius: 1,
+                        states: {
+                            hover: {
+                                radius: 2
+                            }
+                        }
+                    },
+                    fillOpacity: 0.25,
+                },
+                column: {
+                    negativeColor: '#910000',
+                    borderColor: 'silver'
+                }
+            }
+        },
+    
 
     mod = {
         
@@ -19,12 +97,18 @@ yukon.support.systemPerformanceMetrics = (function() {
         init: function () {
             
             if (_initialized) return;
+            
             var startDate = $(".js-dateStart").val(),
                 endDate = $(".js-dateEnd").val();
             $.get(yukon.url("/support/systemPerformanceMetrics/getChartJson?startDate=" + startDate + "&endDate=" + endDate)).done(function(data){
                 var count = 0;
                 $.each(data, function (key, value) {
-                    debugger;
+                    var data = [];
+                    	if (value.pointData != null) {
+                    		value.pointData.forEach(function (item, index) {
+                    			data.push([item.time, item.value]);
+                    		});
+                    	}
                     var row = $('<tr></tr>'),
                         pointNameTableCell = $('<td></td>').attr({'data-point-id': value.pointId})
                                                            .css({'width': '20%'})
@@ -38,143 +122,18 @@ yukon.support.systemPerformanceMetrics = (function() {
                     row.appendTo(appendToTableCssClass);
                     count++;
                     
+                    var sparklineOptions = {
+                    		chart: {
+                    			renderTo: chartTableCell
+                    		},
+                    		series: [{
+                    			data: data
+                    		}]
+                    };
                     
+                    var options = Highcharts.merge(_sparklineDefaultOptions, sparklineOptions);
                     
-                    /*************spark line start**************/ 
-                        Highcharts.SparkLine = function (a, b, c) {
-                            const hasRenderToArg = typeof a === 'string' || a.nodeName;
-                            let options = arguments[hasRenderToArg ? 1 : 0];
-                            const defaultOptions = {
-                                chart: {
-                                    renderTo: (options.chart && options.chart.renderTo) || (hasRenderToArg && a),
-                                    backgroundColor: null,
-                                    borderWidth: 0,
-                                    type: 'area',
-                                    width: 400,
-                                    height: 100,
-                                    style: {
-                                        overflow: 'visible'
-                                    },
-                                    // small optimalization, saves 1-2 ms each sparkline
-                                    skipClone: true,
-                                },
-                                title: {
-                                    text: ''
-                                },
-                                credits: {
-                                    enabled: false
-                                },
-                                xAxis: {
-                                    labels: {
-                                        enabled: false
-                                    },
-                                    title: {
-                                        text: null
-                                    },
-                                    startOnTick: false,
-                                    endOnTick: false,
-                                    tickPositions: []
-                                },
-                                yAxis: {
-                                    endOnTick: false,
-                                    startOnTick: false,
-                                    labels: {
-                                        enabled: false
-                                    },
-                                    title: {
-                                        text: null
-                                    },
-                                    tickPositions: [0]
-                                },
-                                legend: {
-                                    enabled: false
-                                },
-                                tooltip: {
-                                    hideDelay: 0,
-                                    outside: true,
-                                    shared: true
-                                },
-                                plotOptions: {
-                                    series: {
-                                        animation: false,
-                                        lineWidth: 1,
-                                        shadow: false,
-                                        states: {
-                                            hover: {
-                                                lineWidth: 1
-                                            }
-                                        },
-                                        marker: {
-                                            radius: 1,
-                                            states: {
-                                                hover: {
-                                                    radius: 2
-                                                }
-                                            }
-                                        },
-                                        fillOpacity: 0.25
-                                    },
-                                    column: {
-                                        negativeColor: '#910000',
-                                        borderColor: 'silver'
-                                    }
-                                }
-                            };
-
-                            options = Highcharts.merge(defaultOptions, options);
-
-                            return hasRenderToArg ?
-                                new Highcharts.Chart(a, options, c) :
-                                new Highcharts.Chart(options, b);
-                        };
-
-                        const start = +new Date(),
-                            tds = Array.from(document.querySelectorAll('td[data-point-data]')),
-                            fullLen = tds.length;
-
-                        let n = 0;
-
-                        
-                        function doChunk() {
-                            const time = +new Date(),
-                                len = tds.length;
-
-                            for (let i = 0; i < len; i += 1) {
-                                const td = tds[i];
-                                const stringdata = td.dataset.pointData;
-                                const arr = stringdata.split('; ');
-                                const data = arr[0].split(', ').map(parseFloat);
-                                const chart = {};
-
-                                if (arr[1]) {
-                                    chart.type = arr[1];
-                                }
-
-                                Highcharts.SparkLine(td, {
-                                    series: [{
-                                        data: data,
-                                        pointStart: 1
-                                    }],
-                                    tooltip: {
-                                        headerFormat: '<span style="font-size: 10px">' + td.parentElement.querySelector('td').innerText + ', {point.x}:</span><br/>',
-                                        pointFormat: '<b>{point.y}.000</b>'
-                                    },
-                                    chart: chart
-                                });
-
-                                n += 1;
-
-                                // If the process takes too much time, run a timeout to allow interaction with the browser
-                                if (new Date() - time > 500) {
-                                    tds.splice(0, i + 1);
-                                    setTimeout(doChunk, 0);
-                                    break;
-                                }
-                            }
-                        }
-                        doChunk();
-
-                   /***********spark line end****************/ 
+                    chartTableCell.highcharts(options);
                     
                 });
             });
