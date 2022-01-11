9d9a258c3700561cbcbcffe913ec6dac57a8a5fc
Satpathy, Samir K <SamirKSatpathy@eaton.com>
2022-01-11 10:44:45 +0530

    YUK-25744: Dev Task - Newly created system point should be archived by default

--- yukon-database/DBUpdates/oracle/9.2_0.sql
+++ yukon-database/DBUpdates/oracle/9.2_0.sql
@@ -181,18 +181,6 @@ WHERE (SELECT COUNT(*) FROM LMGroupExpressCom) = (SELECT COUNT(*) FROM LMGroupEx
 INSERT INTO DBUpdates VALUES ('YUK-25509', '9.2.0', SYSDATE);
 /* @end YUK-25509 */
 
-/* @start YUK-25744 */
-UPDATE POINT SET ARCHIVETYPE = 'On Change'
-    WHERE POINTNAME IN('RFN Meter Reading Archive Requests Received',
-                       'RFN Meter Reading Archive Requests Pt Data Generated Count',
-                       'RFN LCR Reading Archive Requests Received',
-                       'RFN LCR Reading Archive Requests Queue Size',
-                       'RFN Meter Reading Archive Requests Queue Size',
-                       'RFN LCR Reading Archive Point Data Generated Count');
-
-INSERT INTO DBUpdates VALUES ('YUK-25744', '9.2.0', SYSDATE);
-/* @end YUK-25744 */
-
 /***********************************************************************************/
 /* VERSION INFO                                                                    */
 /* Inserted when update script is run, stays commented out until the release build */
