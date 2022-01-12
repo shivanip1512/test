003134cf8244aaffaba83f37f8e91249e172f730
Shakya, Hasha <HarshaShakya@eaton.com>
2022-01-12 17:27:19 +0530

    YUK-25619 : Dev Task: Display graphs with dummy data(fixed blank date and invalid format issue)

--- yukon-client/yukon-web/src/com/cannontech/web/support/systemPerformanceMetrics/SystemPerformanceMetricsController.java
+++ yukon-client/yukon-web/src/com/cannontech/web/support/systemPerformanceMetrics/SystemPerformanceMetricsController.java
@@ -1,5 +1,8 @@
 package com.cannontech.web.support.systemPerformanceMetrics;
 
+import java.beans.PropertyEditorSupport;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -8,7 +11,9 @@ import org.joda.time.Months;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -71,5 +76,30 @@ public class SystemPerformanceMetricsController {
             graphAsJSON.put(systemPoint.getLiteID(), graphJSON);
         }
         return graphAsJSON;
+    }
+
+    @InitBinder
+    protected void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(Date.class, new PropertyEditorSupport() {
+            SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+            @Override
+            public void setAsText(String attr) throws IllegalArgumentException {
+                Date date = null;
+                try {
+                    date = format.parse(attr);
+                } catch (ParseException e) {
+                }
+                setValue(date);
+            }
+
+            @Override
+            public String getAsText() {
+                Date date = (Date) getValue();
+                if (date == null) {
+                    return null;
+                }
+                return format.format(date);
+            }
+        });
     }
 }
