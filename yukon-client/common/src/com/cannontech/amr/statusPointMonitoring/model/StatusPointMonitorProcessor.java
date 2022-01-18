40beed5e3a7aacf8db3ab2f4e8c2430335554aba
E9815779 <SamuelTJohnston@Eaton.com>
2022-01-18 16:10:45 -0600

    YUK-25753 UI - "ignore events" functionality
     - Added "Notify On Alarms Only" field on Outage Notification Monitor processor view page and select on edit page.
     - Added "Notify On Alarms Only" field to StatusPointMonitorProcessor objects.
     - Added help text for "Notify On Alarms Only" to Outage Notification Monitor help popup.
     - Note that "Notify On Alarms Only" value will not actually be saved or used until other subtasks are completed.

--- yukon-client/common/src/com/cannontech/amr/statusPointMonitoring/model/StatusPointMonitorProcessor.java
+++ yukon-client/common/src/com/cannontech/amr/statusPointMonitoring/model/StatusPointMonitorProcessor.java
@@ -6,6 +6,7 @@ public class StatusPointMonitorProcessor {
     private String prevState;
     private String nextState;
     private OutageActionType actionType;
+    private boolean notifyOnAlarmOnly = false;
     
     public void setStatusPointMonitorProcessorId(Integer statusPointMonitorProcessorId) {
         this.statusPointMonitorProcessorId = statusPointMonitorProcessorId;
@@ -13,6 +14,14 @@ public class StatusPointMonitorProcessor {
     
     public Integer getStatusPointMonitorProcessorId() {
         return statusPointMonitorProcessorId;
+    }
+    
+    public boolean isNotifyOnAlarmOnly() {
+        return notifyOnAlarmOnly;
+    }
+    
+    public void setNotifyOnAlarmOnly(boolean enabled) {
+        notifyOnAlarmOnly = enabled;
     }
     
     public void setPrevState(String prevState) {
@@ -105,28 +114,37 @@ public class StatusPointMonitorProcessor {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
+        if (this == obj) {
             return true;
-        if (obj == null)
+        }
+        if (obj == null) {
             return false;
-        if (getClass() != obj.getClass())
+        }
+        if (getClass() != obj.getClass()) {
             return false;
+        }
         StatusPointMonitorProcessor other = (StatusPointMonitorProcessor) obj;
         if (actionType == null) {
-            if (other.actionType != null)
+            if (other.actionType != null) {
                 return false;
-        } else if (!actionType.equals(other.actionType))
+            }
+        } else if (!actionType.equals(other.actionType)) {
             return false;
+        }
         if (nextState == null) {
-            if (other.nextState != null)
+            if (other.nextState != null) {
                 return false;
-        } else if (!nextState.equals(other.nextState))
+            }
+        } else if (!nextState.equals(other.nextState)) {
             return false;
+        }
         if (prevState == null) {
-            if (other.prevState != null)
+            if (other.prevState != null) {
                 return false;
-        } else if (!prevState.equals(other.prevState))
+            }
+        } else if (!prevState.equals(other.prevState)) {
             return false;
+        }
         return true;
     }
 
