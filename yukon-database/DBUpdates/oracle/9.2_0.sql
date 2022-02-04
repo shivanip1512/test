dfe86655f837dfeae20ef443978bb2e8e393ccb4
Brady Anderson <BradyWAnderson@eaton.com>
2022-02-04 15:26:05 -0600

    YUK-25003 SQL Creation Script Missing Unique Constraint
    Updated creation scripts and dropped + re-added the constraint in update
    scripts

--- yukon-database/DBUpdates/oracle/9.2_0.sql
+++ yukon-database/DBUpdates/oracle/9.2_0.sql
@@ -1,6 +1,6 @@
-/******************************************/
-/**** SQL Server DBupdates             ****/
-/******************************************/
+/******************************************/ 
+/****     Oracle DBupdates             ****/ 
+/******************************************/ 
 
 /* @start YUK-24891 */
 
@@ -203,6 +203,41 @@ CREATE INDEX INDX_EventLog_EvntTime_EvntLogID ON EventLog (
 
 INSERT INTO DBUpdates VALUES ('YUK-25792', '9.2.0', SYSDATE);
 /* @end YUK-25792 */
+
+/* @start YUK-25003 */
+DECLARE
+    v_count NUMBER := 0;
+BEGIN
+    SELECT COUNT(*) INTO v_count FROM (SELECT CONSTRAINT_NAME FROM USER_CONSTRAINTS WHERE CONSTRAINT_NAME = 'AK_ZONENAME');
+    IF v_count > 0 THEN
+        EXECUTE IMMEDIATE 'ALTER TABLE Zone DROP CONSTRAINT AK_ZoneName';
+    END IF;
+END;
+/* @error warn-once */
+/* @start-block */
+DECLARE
+    v_count NUMBER := 0;
+    v_newLine VARCHAR2(2);
+    v_errorText VARCHAR2(512);
+BEGIN
+    SELECT COUNT(*) INTO v_count FROM (SELECT ZoneName, COUNT(*) AS temp_count FROM Zone GROUP BY ZoneName HAVING COUNT(*) > 1);
+    IF v_count > 0 THEN
+        v_newLine := CHR(13) || CHR(10);
+        v_errorText := 'IVVC Zone Names are now required to be unique.' || v_newLine
+            || 'Setup has detected that IVVC Zones with duplicate names are present in the system.' || v_newLine
+            || 'In order to proceed with the update this must be manually resolved.' || v_newLine
+            || 'More information can be found in YUK-22834.' || v_newLine
+            || 'To locate Zones that have duplicated names you can use the query below:' || v_newLine
+            || 'SELECT ZoneName, COUNT(*) AS NumberOfOccurences FROM Zone GROUP BY ZoneName HAVING COUNT(*) > 1';
+        RAISE_APPLICATION_ERROR(-20001, v_errorText);
+    END IF;
+END;
+/
+/* @end-block */
+ALTER TABLE Zone ADD CONSTRAINT AK_ZONENAME_ZONE UNIQUE (ZoneName);
+
+INSERT INTO DBUpdates VALUES ('YUK-25003', '9.2.0', SYSDATE);
+/* @end YUK-25003 */
 
 /***********************************************************************************/
 /* VERSION INFO                                                                    */
--- yukon-database/DBUpdates/sqlserver/9.2_0.sql
+++ yukon-database/DBUpdates/sqlserver/9.2_0.sql
@@ -207,6 +207,41 @@ GO
 INSERT INTO DBUpdates VALUES ('YUK-25792', '9.2.0', GETDATE());
 /* @end YUK-25792 */
 
+/* @start YUK-25003 */
+BEGIN
+DECLARE @constraintExists NUMERIC = (SELECT COUNT(*) FROM sys.key_constraints WHERE name = 'AK_ZoneName');
+
+    IF @constraintExists > 0
+        BEGIN
+            ALTER TABLE Zone DROP CONSTRAINT AK_ZoneName;
+        END;
+END;
+GO
+
+/* @error warn-once */
+/* @start-block */
+BEGIN
+    DECLARE @DuplicateZones NUMERIC = (SELECT COUNT(*) FROM (SELECT ZoneName, COUNT(*) AS temp_count FROM Zone GROUP BY ZoneName HAVING COUNT(*) > 1) AS temp_table)
+
+IF @DuplicateZones > 0
+    BEGIN
+        DECLARE @NewLine CHAR(2) = CHAR(13) + CHAR(10);
+        DECLARE @ErrorText VARCHAR(1024) = 'IVVC Zone Names are now required to be unique.' + @NewLine
+            + 'Setup has detected that IVVC Zones with duplicate names are present in the system.' + @NewLine
+            + 'In order to proceed with the update this must be manually resolved.' + @NewLine
+            + 'More information can be found in YUK-22834.' + @NewLine
+            + 'To locate Zones that have duplicated names you can use the query below:' + @NewLine
+            + 'SELECT ZoneName, COUNT(*) AS NumberOfOccurences FROM Zone GROUP BY ZoneName HAVING COUNT(*) > 1';
+        RAISERROR(@ErrorText, 16, 1);
+    END;
+END;
+GO
+/* @end-block */
+ALTER TABLE Zone ADD CONSTRAINT Ak_ZoneName UNIQUE (ZoneName);
+
+INSERT INTO DBUpdates VALUES ('YUK-25003', '9.2.0', GETDATE());
+/* @end YUK-25003 */
+
 /***********************************************************************************/
 /* VERSION INFO                                                                    */
 /* Inserted when update script is run, stays commented out until the release build */
