3a9927f290a14366fe7a9674b2a3f4cfe9d06176
Satpathy, Samir K <SamirKSatpathy@eaton.com>
2022-02-10 14:14:12 +0530

    YUK-25786: Update Producers to Generate data before and after the Threshold value - Bug fix

--- yukon-client/common/src/com/cannontech/yukon/system/metrics/helper/YukonMetricHelper.java
+++ yukon-client/common/src/com/cannontech/yukon/system/metrics/helper/YukonMetricHelper.java
@@ -72,6 +72,6 @@ public class YukonMetricHelper {
                 shouldWatchAfterEscapevalve = true;
             }
         }
-        return !escapeValveAcheived || shouldWatchAfterEscapevalve;
+        return shouldWatchAfterEscapevalve;
     }
 }
