0629d3af966bf26284541c19310992f9d8d2b21b
E9816181 <MatthewSFisher@Eaton.com>
2022-02-15 15:39:57 -0800

    YUK-25721 Generate Data - Dispatch RPH Insert Time Taken per 10k Added tracking/sending of millis per row inserted

--- yukon-server/Service/YukonMetricTracker.cpp
+++ yukon-server/Service/YukonMetricTracker.cpp
@@ -16,7 +16,10 @@ const std::string YukonMetricTracker::RPH_INSERT_MILLIS_PER_ROW =
 const std::string YukonMetricTracker::RPH_QUEUE_SIZE = 
                                      "RPH_QUEUE_SIZE";
 
-void YukonMetricTracker::sendYukonMetricMessage(const std::string pointInfo, int64_t value, std::chrono::system_clock::time_point timestamp)
+namespace {
+
+template<class T>
+void sendImpl(const std::string pointInfo, T value, std::chrono::system_clock::time_point timestamp)
 {
     using Cti::Messaging::ActiveMQ::Topics::OutboundTopic;
 
@@ -34,6 +37,18 @@ void YukonMetricTracker::sendYukonMetricMessage(const std::string pointInfo, int
         yukonMetricMessage);
 
     CTILOG_DEBUG(dout, "Message sent on the Yukon Metric topic is:" + yukonMetricMessage);
+}
+
+}
+
+void YukonMetricTracker::sendIntegralMetric(const std::string pointInfo, int64_t value, std::chrono::system_clock::time_point timestamp)
+{
+    sendImpl(pointInfo, value, timestamp);
+}
+
+void YukonMetricTracker::sendFloatingMetric(const std::string pointInfo, double value, std::chrono::system_clock::time_point timestamp)
+{
+    sendImpl(pointInfo, value, timestamp);
 }
 
 }
--- yukon-server/Service/include/YukonMetricTracker.h
+++ yukon-server/Service/include/YukonMetricTracker.h
@@ -9,7 +9,8 @@ namespace Cti {
 
 struct IM_EX_SERVICE YukonMetricTracker
 {
-	void sendYukonMetricMessage(const std::string pointInfo, int64_t value, std::chrono::system_clock::time_point timestamp);
+	void sendIntegralMetric(const std::string pointInfo, int64_t value, std::chrono::system_clock::time_point timestamp);
+	void sendFloatingMetric(const std::string pointInfo,  double value, std::chrono::system_clock::time_point timestamp);
 
 	static const std::string RPH_INSERTS;
 	static const std::string RPH_INSERT_MILLIS_PER_ROW;
