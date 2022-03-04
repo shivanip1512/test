f7925f893092ae25763980ae5189d07adae11c4a
Satpathy, Samir K <SamirKSatpathy@eaton.com>
2022-03-04 17:41:34 +0530

    YUK-26009: Add i18n support for API global error response - Review Comment fix

--- yukon-client/common/src/com/cannontech/api/error/model/ApiErrorDetails.java
+++ yukon-client/common/src/com/cannontech/api/error/model/ApiErrorDetails.java
@@ -13,7 +13,7 @@ public enum ApiErrorDetails {
     OBJECT_ALREADY_EXISTS(101, "Object already exists", "An identical object already exists."), // code : 100101
     AUTHENTICATION_INVALID(102, "Authentication Invalid", "Username or password not valid."), // code : 100102
     NOT_AUTHORIZED(103, "Not authorized", "User not authorized to perform this transaction."), // code : 100103
-    DATABASE_ERROR(104, "Database error", "Database error occured while performing this transaction."), // code : 100104
+    DATABASE_ERROR(104, "Database error", "Database error occurred while performing this transaction."), // code : 100104
     METHOD_ARGUMENT_MISMATCH(105, "Method argument mismatch", "Method arguments are not matching."), // code : 100105
     BAD_REQUEST(106, "Bad request", "Bad request."), // code : 100106
     AUTHENTICATION_REQUIRED(107, "Authentication required", "Expired or invalid token"), // code : 100107
