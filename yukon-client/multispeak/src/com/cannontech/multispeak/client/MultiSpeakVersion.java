0313d554a55e68992f45a2a3a92904ddbe7e85c6
Tejaswee Khare <TejasweeSKhare@eaton.com>
2022-03-29 17:42:48 +0530

    YUK-26162: Backend, UI, DB updates

--- yukon-client/multispeak/src/com/cannontech/multispeak/client/MultiSpeakVersion.java
+++ yukon-client/multispeak/src/com/cannontech/multispeak/client/MultiSpeakVersion.java
@@ -11,11 +11,12 @@ import com.google.common.collect.ImmutableSet;
 public enum MultiSpeakVersion implements DatabaseRepresentationSource {
 
     V3("3.0", "http://www.multispeak.org/Version_3.0"), 
-    V5("5.0", "http://www.multispeak.org/V5.0"),;
+    V5("5.0", "http://www.multispeak.org/V5.0"),
+    V4("4.1", "http://www.multispeak.org/Version_4.1_Release");
 
     String version;
     String namespace;
-    private final static ImmutableSet<String> supportedMspVerions = ImmutableSet.of(V3.version, V5.version);
+    private final static ImmutableSet<String> supportedMspVerions = ImmutableSet.of(V3.version, V5.version, V4.version);
 
     private MultiSpeakVersion(String version, String namespace) {
         this.version = version;
