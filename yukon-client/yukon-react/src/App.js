f63592a4d08d892aa69a9c77cd78569df217e387
Palash <palashpandey@eaton.com>
2022-04-29 18:11:05 +0530

    removing code from AppJs

--- yukon-client/yukon-react/src/App.js
+++ yukon-client/yukon-react/src/App.js
@@ -19,12 +19,6 @@ import DashboardPage from "./components/YukonPage/Dashboards/Dashboard";
 import { useIdleTimer } from "react-idle-timer";
 import axios from "../src/axiosConfig";
 
-const onIdle = () => {
-    axios.post("/api/logout", {}).catch((error) => {
-        console.warn("error in logout while user is idle");
-    });
-};
-
 const ScrollToTop = () => {
     const { pathname } = useLocation();
 
@@ -34,10 +28,6 @@ const ScrollToTop = () => {
 
     return null;
 };
-
-export const App = () => {
-    // 2hr is default user idle time in yukon, which can be customizable as well
-    useIdleTimer({ onIdle, timeout: 1000 * 60 * 60 * 2 });
 
     return (
         <SecurityContextProvider>
