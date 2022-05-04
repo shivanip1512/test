2905adbc0fb25f8114e35d67cafb519d1229cb29
Brady Anderson <BradyWAnderson@eaton.com>
2022-05-04 11:49:31 -0500

    Update to move this branch to 9.2.2

--- yukon-database/DBUpdates/oracle/9.2_1.sql
+++ yukon-database/DBUpdates/oracle/9.2_1.sql
@@ -6,4 +6,4 @@
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
 /**************************************************************/
-/*INSERT INTO CTIDatabase VALUES ('9.2', '04-Apr-2022', 'Latest Update', 1, SYSDATE);*/
+INSERT INTO CTIDatabase VALUES ('9.2', '4-May-2022', 'Latest Update', 1, SYSDATE);
--- yukon-database/DBUpdates/sqlserver/9.2_1.sql
+++ yukon-database/DBUpdates/sqlserver/9.2_1.sql
@@ -6,4 +6,4 @@
 /* VERSION INFO                                               */
 /* Inserted when update script is run                         */
 /**************************************************************/
-/*INSERT INTO CTIDatabase VALUES ('9.2', '4-APR-2022', 'Latest Update', 1, GETDATE());*/
+INSERT INTO CTIDatabase VALUES ('9.2', '4-May-2022', 'Latest Update', 1, GETDATE());
