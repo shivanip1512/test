0513cd0df452b17edda3e71969c43939c975f8ff
E0360047 <Niveditakumari@Eaton.com>
2022-05-16 16:13:11 +0530

    YUK-26440 fixed porter issue

--- yukon-client/multispeak/src/com/cannontech/multispeak/service/impl/v4/MultispeakMeterServiceImpl.java
+++ yukon-client/multispeak/src/com/cannontech/multispeak/service/impl/v4/MultispeakMeterServiceImpl.java
@@ -14,6 +14,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.Logger;
 import org.joda.time.Instant;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.transaction.TransactionStatus;
@@ -96,7 +97,7 @@ public class MultispeakMeterServiceImpl extends MultispeakMeterServiceBase imple
 
     private static final Logger log = YukonLogManager.getLogger(MultispeakMeterServiceImpl.class);
 
-    private BasicServerConnection porterConnection;
+    @Autowired private @Qualifier("porter") BasicServerConnection porterConnection;
 
     @Autowired private MspObjectDao mspObjectDao;
     @Autowired private DeviceUpdateService deviceUpdateService;
