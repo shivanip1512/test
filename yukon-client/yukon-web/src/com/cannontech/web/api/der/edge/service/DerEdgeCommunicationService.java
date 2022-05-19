1c2817914ca2208c3d4b417a07f9ff3055d7c583
E0534908 <JohnMJustin@Eaton.com>
2022-05-19 16:30:14 -0500

    YUK-26194 SETO - Broadcast REST API endpoint
    Implemented the SETO Broadcast REST API Endpoint
    Cleanup some API validation

--- yukon-client/yukon-web/src/com/cannontech/web/api/der/edge/service/DerEdgeCommunicationService.java
+++ yukon-client/yukon-web/src/com/cannontech/web/api/der/edge/service/DerEdgeCommunicationService.java
@@ -2,9 +2,12 @@ package com.cannontech.web.api.der.edge.service;
 
 import com.cannontech.common.pao.YukonPao;
 import com.cannontech.user.YukonUserContext;
+import com.cannontech.web.api.der.edge.EdgeBroadcastRequest.Priority;
 
 public interface DerEdgeCommunicationService {
 
     public short sendUnicastRequest(YukonPao pao, byte[] payload, YukonUserContext userContext);
-    
+
+    public void sendBroadcastRequest(byte[] payload, Priority priority, YukonUserContext userContext);
+
 }
