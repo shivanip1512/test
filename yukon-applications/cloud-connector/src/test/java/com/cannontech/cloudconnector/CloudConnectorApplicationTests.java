1284e059aa00ba1d1e1ab66177a60a3dcc7419d9
Brady Anderson <BradyWAnderson@eaton.com>
2022-05-27 16:39:05 -0500

    YUK-26498 Created new project for Cloud Connector
    The cloud connector is will live here, however it does not come with any
    eclipse project files. However when you import it into your eclipse
    workspace it will derrive it's configuration from the gradle build files

---
+++ yukon-applications/cloud-connector/src/test/java/com/cannontech/cloudconnector/CloudConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cannontech.cloudconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
