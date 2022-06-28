51a3feb55114d7ad0996b80ed7f19ce54bfaa9d2
E0425883 <E0425883@INPU1WHP6099158.eaton.ad.etn.com>
2022-06-28 13:39:22 +0530

    YUK-26679 - UI Implementation - Notification Groups Create
    Initial commit.

---
+++ yukon-client/yukon-web/src/com/cannontech/web/tools/notificationGroup/NotificationGroupController.java
@@ -0,0 +1,23 @@
+package com.cannontech.web.tools.notificationGroup;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cannontech.user.YukonUserContext;
+import com.cannontech.web.PageEditMode;
+
+@Controller
+@RequestMapping("/notificationGroup/*")
+public class NotificationGroupController {
+    
+    @GetMapping("create")
+    public String create(ModelMap model, YukonUserContext userContext, HttpServletRequest request) {
+        model.addAttribute("mode", PageEditMode.CREATE);
+        
+        return "/notificationGroup/view.jsp";
+    }
+}
