d5ff718d3596d7d4552e2ae722052509400e996a
carrieVant <carrieVant@eaton.com>
2022-07-15 09:22:57 -0500

    YUK-26837 - Update infrastructure warning UI components to support meters   - changes based on review

--- yukon-client/yukon-web/WebContent/WEB-INF/pages/stars/infrastructureWarnings/deviceTypeInfrastructureWarningsWidget.jsp
+++ yukon-client/yukon-web/WebContent/WEB-INF/pages/stars/infrastructureWarnings/deviceTypeInfrastructureWarningsWidget.jsp
@@ -1,6 +1,5 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ taglib prefix="cti" uri="http://cannontech.com/tags/cti" %>
-<%@ taglib prefix="i" tagdir="/WEB-INF/tags/i18n"%>
 <%@ taglib prefix="tags" tagdir="/WEB-INF/tags"%>
 
 <cti:msgScope paths="widgets.infrastructureWarnings">
--- yukon-client/yukon-web/src/com/cannontech/web/stars/infrastructureWarnings/InfrastructureWarningsController.java
+++ yukon-client/yukon-web/src/com/cannontech/web/stars/infrastructureWarnings/InfrastructureWarningsController.java
@@ -12,12 +12,12 @@ import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.Logger;
 import org.joda.time.Instant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -158,6 +158,7 @@ public class InfrastructureWarningsController {
             deviceLabel = accessor.getMessage(baseKey + "meters");
             model.addAttribute("deviceLabel", deviceLabel);
             model.addAttribute("deviceType", InfrastructureWarningDeviceCategory.IPLINK_METER);
+            break;
         default:
             model.addAttribute("deviceTotalCount", summary.getTotalGateways());
             model.addAttribute("deviceWarningsCount", summary.getWarningGateways());
