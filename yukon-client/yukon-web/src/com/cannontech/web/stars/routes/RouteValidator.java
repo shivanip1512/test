41452978e5e9ecd62d94145b4f6bdc752a096078
e0693848 <ShivaniPacharne@eaton.com>
2022-07-18 17:17:31 +0530

    YUK-26780 : defect fixes

--- yukon-client/yukon-web/src/com/cannontech/web/stars/routes/RouteValidator.java
+++ yukon-client/yukon-web/src/com/cannontech/web/stars/routes/RouteValidator.java
@@ -25,16 +25,10 @@ public class RouteValidator<T extends RouteBaseModel<?>> extends SimpleValidator
     protected void doValidation(T routeBaseModel, Errors errors) {
         String deviceId = routeBaseModel.getDeviceId() != null ? routeBaseModel.getDeviceId().toString() : null;
 
-        yukonValidationHelper.validatePaoName(routeBaseModel.getDeviceName(),
-                routeBaseModel.getDeviceType(),
-                errors,
-                yukonValidationHelper.getMessage("yukon.common.name"),
-                deviceId,
-                "deviceName");
+        yukonValidationHelper.validatePaoName(routeBaseModel.getDeviceName(), routeBaseModel.getDeviceType(), errors,
+                yukonValidationHelper.getMessage("yukon.common.name"), deviceId, "deviceName");
 
-        yukonValidationHelper.checkIfFieldRequired("signalTransmitterId",
-                errors,
-                routeBaseModel.getSignalTransmitterId(),
+        yukonValidationHelper.checkIfFieldRequired("signalTransmitterId", errors, routeBaseModel.getSignalTransmitterId(),
                 yukonValidationHelper.getMessage(key + "selectSignalTransmitter"));
     }
 }
