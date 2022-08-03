c0dbebb3438c40f4ddcd64e9948cc94825f548c3
E0425883 <E0425883@INPU1WHP6099158.eaton.ad.etn.com>
2022-08-03 13:48:29 +0530

    YUK-26911 - UI implementation for Create(Macro) routes
    Updated code.

--- yukon-client/yukon-web/WebContent/resources/js/pages/yukon.assets.macroRoutes.js
+++ yukon-client/yukon-web/WebContent/resources/js/pages/yukon.assets.macroRoutes.js
@@ -25,6 +25,51 @@ yukon.assets.macroRoutes = (function () {
                 yukon.ui.blockPage();
                 $('#delete-macroRoute-form').submit();
             });
+            
+            $(document).on("click", ".js-add", function () {
+                var picker = yukon.pickers['js-signal-transmitter-picker'],
+                    selectedContainer = $('#js-assigned-signal-transmitter-container');
+            
+                picker.selectedItems.forEach(function (route) {
+                    var templateRow = $('.template-row').clone();
+                    templateRow.attr('data-id', route.paoId);
+                    var children = templateRow.children();
+                    templateRow.text(route.paoName).append(children);
+                    templateRow.removeClass('dn template-row');
+                    templateRow.appendTo(selectedContainer);
+                    picker.disableItem(route.paoId);
+                });
+                selectedContainer.closest('.select-box').find('.js-with-movables').trigger('yukon:ordered-selection:added-removed');
+                picker.clearEntireSelection();
+            });
+
+            $(document).on('click', '.js-remove', function () {
+                var picker = yukon.pickers['js-signal-transmitter-picker'],
+                    selectedId = $(this).parent().attr('data-id');
+                $(this).parent().remove();
+                $('#js-assigned-signal-transmitter-container').closest('.select-box').find('.js-with-movables').trigger('yukon:ordered-selection:added-removed');
+                picker.enableItem(selectedId);
+            });
+            
+            $(document).on('click', '#js-save-macro-routes', function (ev) {
+                var container = $('#js-assigned-signal-transmitter-container'),
+                    routeList = [];
+                
+                container.find('.select-box-item').each(function (idx, item) {
+                    if ($(item).is(':visible')) {
+                        var routeObj = new Object();
+                        routeObj.routeId = $(item).data('id');
+                        routeObj.routeName = $(item).text();
+                        routeList.push(routeObj);
+                    }
+                });
+                $('input[name="routeListJsonString"]').val(JSON.stringify(routeList));
+                $('#js-macro-route-form').submit();
+            });
+            
+            if ($("#js-unassigned-signal-transmitter-container").exists()) {
+                yukon.pickers['js-signal-transmitter-picker'].show();
+            }
 
             _initialized = true;
         }
