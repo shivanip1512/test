46e354dfcfc8b00df1402d67dd8642a0013ae97c
E0534908 <JohnMJustin@Eaton.com>
2022-08-11 11:59:12 -0500

    YUK-27129 SETO Endpoints: Non-Required fields not defaulting.
    Set default EdgeBroadcastMessagePriority to LOW
    Set EdgeUnicastPriority queuePriority to HIGH for Unicast
    Set EdgeUnicastPriority networkPriority to HIGH for Unicast
    Set EdgeUnicastPriority queuePriority to HIGH for Multipoint
    Set EdgeUnicastPriority networkPriority to HIGH for Multipoint

--- yukon-client/yukon-web/src/com/cannontech/web/api/der/edge/EdgeBroadcastRequest.java
+++ yukon-client/yukon-web/src/com/cannontech/web/api/der/edge/EdgeBroadcastRequest.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class EdgeBroadcastRequest {
     private String payload;
-    private EdgeBroadcastMessagePriority priority;
+    private EdgeBroadcastMessagePriority priority = EdgeBroadcastMessagePriority.LOW;
 
     public String getPayload() {
         return this.payload;
